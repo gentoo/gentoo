963ee83a64124299f376b56246113e207b6e6540
Mike Frysinger <vapier@gentoo.org>
2015-09-17 12:43:52 -0400

    net-misc/chrome-remote-desktop: get resizing working again
    Mostly it's about adding documentation for how to trigger the randr Xvfb
    behavior, but we also switch to a simpler method for passing in the size
    options.

--- net-misc/chrome-remote-desktop/files/chrome-remote-desktop.conf.d
+++ net-misc/chrome-remote-desktop/files/chrome-remote-desktop.conf.d
@@ -3,8 +3,12 @@
 # List of users to start Chrome Remote Desktop for.
 CHROME_REMOTING_USERS=''
 
-# Default resolutions to make available to the session.
-#CHROME_REMOTE_DESKTOP_DEFAULT_DESKTOP_SIZES='1600x1200,3840x1600'
+# Options to pass to chrome-remote-desktop.  Only the -s option is interesting.
+# Note: In order to support resizing, you need to:
+# (1) Apply this patch to xorg-server (via epatch_user):
+#     http://patchwork.freedesktop.org/patch/51428/
+# (2) Create a symlink /usr/bin/Xvfb-randr -> Xvfb
+#OPTIONS='-s 1600x1200 -s 3840x1600'
 
 # Directory to use for storing log files.
 #CHROME_REMOTE_DESKTOP_LOG_DIR='/var/log'
