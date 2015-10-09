gentoo/gentoo

commit 69258e0492d0b0625097a51825a29512aa5968cd

dev-python/python-keystoneclient: updating deps for bug 562510
Package-Manager: portage-2.2.20.1

dev-python/python-keystoneclient/python-keystoneclient-1.7.2-r1.ebuild
@@ -3,7 +3,7 @@
 # $Id$
 
 EAPI=5
-PYTHON_COMPAT=( python2_7 python3_3 python3_4 )
+PYTHON_COMPAT=( python2_7 python3_4 )
 
 inherit distutils-r1
 
@@ -25,9 +25,10 @@ TCDEPEND="
 	>=dev-python/lxml-2.3[${PYTHON_USEDEP}]
 	>=dev-python/mock-1.2[${PYTHON_USEDEP}]
 	>=dev-python/requests-mock-0.6.0[${PYTHON_USEDEP}]
-	>=dev-python/tempest-lib-0.6.1[${PYTHON_USEDEP}]
+	>=dev-python/tempest-lib-0.8.0[${PYTHON_USEDEP}]
 	>=dev-python/testresources-0.2.4[${PYTHON_USEDEP}]
 	>=dev-python/webob-1.2.3[${PYTHON_USEDEP}]
+	>=dev-python/bandit-0.13.2[${PYTHON_USEDEP}]
 "
 DEPEND="
 	dev-python/setuptools[${PYTHON_USEDEP}]

commit 5d60545105f87b53ff49bf43c01096c3d8cce53c

dev-python/python-keystoneclient: readd python3.3 for revdeps
Re-add python3_3 support in order to fix dependency graphs of reverse
dependencies that support python3_3 and require this version of
python-keystoneclient.

Fixes: 69258e0492d (updating deps for bug 562510)

Package-Manager: portage-2.2.22

dev-python/python-keystoneclient/python-keystoneclient-1.7.2-r1.ebuild
@@ -3,7 +3,7 @@
 # $Id$
 
 EAPI=5
-PYTHON_COMPAT=( python2_7 python3_4 )
+PYTHON_COMPAT=( python2_7 python3_3 python3_4 )
 
 inherit distutils-r1
 