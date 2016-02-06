gentoo/gentoo

commit b284e54e1905734b24500bfebdadc2e0b0c5a849

python-utils-r1.eclass: Reorder implementations to semi-ascending order
Reorder the Python implementations to ascending version order, with
CPython listed first and other implementations in descending preference.

The previous ordering has been used for two reasons:

1. There were packages which supported Python 3.x or PyPy partially but
their documentation builds or test functions required CPython 2.x.
The specific ordering caused python_export_best (the predecessor of
python_setup) to use CPython 2.x for those tasks. This is now replaced
by explicit implementation restrictions in python_setup.

2. PyPy setup runs were usually slower than CPython, and CPython 3.x
runs were often slower due to 2to3 calls. Combined with parallel build
runs, this ordering caused slower builds to start earlier and sometimes
resulted in more efficient use of threads. However, nowadays we no
longer do parallel builds.

Therefore, it seems reasonable to finally reorder the implementations
into a more intuitive order.

eclass/python-utils-r1.eclass
@@ -41,10 +41,10 @@ inherit toolchain-funcs
 # @DESCRIPTION:
 # All supported Python implementations, most preferred last.
 declare -g -r _PYTHON_ALL_IMPLS=(
-	jython2_5 jython2_7
-	pypy pypy3
-	python3_3 python3_4 python3_5
 	python2_7
+	python3_3 python3_4 python3_5
+	pypy pypy3
+	jython2_5 jython2_7
 )
 
 # @FUNCTION: _python_impl_supported

commit ab6a4a2f5347e69108ade31c06f750263d83c8b6

python-utils-r1.eclass: Do not prefer Jython & PyPy over CPython
Fix _PYTHON_ALL_IMPLS to list Jython & PyPy before CPython versions,
therefore causing them not to be implicitly preferred over CPython.

The implementation reordering done in b284e54 ("reorder implementations
in semi-ascending order") has caused PyPy & Jython to be listed after
CPython. While seemingly it was more intuitive to list third-party
implementations after the reference one, it also implicitly caused them
to be preferred by python-exec.

eclass/python-utils-r1.eclass
@@ -41,10 +41,10 @@ inherit toolchain-funcs
 # @DESCRIPTION:
 # All supported Python implementations, most preferred last.
 _PYTHON_ALL_IMPLS=(
+	jython2_7
+	pypy pypy3
 	python2_7
 	python3_3 python3_4 python3_5
-	pypy pypy3
-	jython2_7
 )
 readonly _PYTHON_ALL_IMPLS
 