d7ec853863e66b2a6a3c17d126f2c6bffcc79229
Aaron Bauman <bman@gentoo.org>
2016-04-26 17:23:34 +0900

    net-irc/srvx: cleaned per security bug 521892

--- net-irc/srvx/files/srvx.conf.d
+++
@@ -1,7 +0,0 @@
-# Copyright 1999-2004 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Id$
-
-# user and group to run srvx as
-SRVX_USER="srvx"
-SRVX_GROUP="srvx"
