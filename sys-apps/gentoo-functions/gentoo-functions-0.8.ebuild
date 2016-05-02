gentoo/gentoo

commit ca1af4526d676d5162292af3ad9d6db4f84a8083

sys-apps/gentoo-functions: remove old versions
Package-Manager: portage-2.2.26

sys-apps/gentoo-functions/Manifest
@@ -1,5 +1,1 @@
-DIST consoletype-1.0.sh 616 SHA256 b6f837be496024941b81402e6c11300eb4a53987368c5c6d9afb5fd8b1c34d28 SHA512 ab6edd0035ad2ac855bc7b94679a78aad63f647b906dfda39a9844b676e6cdfc872fd1fc897f6158a5709906905a4da5ce8b4665f9e2bc0bcd1e88bb9ecace07 WHIRLPOOL b2d5f13e7411d8e6b5c4dae875041a95bed3ed7b20e765971b86fd16b1a970e8ad10288683d2efa530ef8568c6c58a07f9fc5a4a1b6b00ee6ed731da90e472ed
 DIST gentoo-functions-0.10.tar.gz 11432 SHA256 709c8b22f404001a512e47a7a4d3192070b3e150fb9d0f943de09736d665b0db SHA512 2622cede032089c7b8a2dec576e848852f9964e0198ab161f0ff5c426c542d0849bd3d30a40c296805bfec90b533c419df43c7975ed85678471483c827ca4cdb WHIRLPOOL 6559c62083eca8a016f005d1003c8a0b6e2c89836c130cec9e0a40575e1604039f669313547c63f6cf84f1a40cdd7249d227c6f0b57f282764df8d20ef239051
-DIST gentoo-functions-0.7.tar.bz2 10656 SHA256 0034ff7a65deb8df902683deb1662ce561b7f76ff8561b4ada46cfb18effc7f3 SHA512 7b7c779daeaa37476be488123222af591b357ce1071d8a68694797ce35c8f9c1492da55e4d3059443eb20e08cc7d88eee0e1914efa2b322628c3660c277f7a64 WHIRLPOOL 4b50d0cf0e355d267c2cb6db18211a8d48f48a248190e38227449ef01e39b3968b10a4276ca22b43af9d40706b804730ac81e6ba83efbfcc426a6781dd007cc1
-DIST gentoo-functions-0.8.tar.bz2 10647 SHA256 89dc0a3289a2e3312efb560e904add13ececdd5eb554151cb30d0b96f1bd8116 SHA512 34f04f5e7ecd888197b6486c131c346a84a6937501f68a54481ec3caacaee86e54345a6c34364a5953c55b1d4aeb4ca2173b70864cdd7b81e74964352132b322 WHIRLPOOL 6ba223987953fa1dd80b79590c5f72ab8b5d0eca47cc1df23559e295f03faa369a372e5c27ed47d19d605dfe072c22c7702f3d4443a8ea2489d25092274670f3
-DIST gentoo-functions-0.9.tar.gz 11439 SHA256 63e51e0288c82912e51c490512c695a2d77b86a3a7e02180e313d19978ba4a2c SHA512 c8b99448f6a295597e135803cd903e81fb3886de9dff725137a341cd667575e5269d3e0369ad3d19cd448ba0716e48b12752462f310f5e8c54e132777ceb684f WHIRLPOOL aa30d196161bec7c1a7bed89dda6faf56d5c7a7fe555ae8ded93a61d9b74551a6db81e696b28e2c7694c8ee2cf58758197d1616e4db31e5bfdc195ac43f70bf5

sys-apps/gentoo-functions/gentoo-functions-0.8.ebuild
@@ -1,34 +1,0 @@
-# Copyright 1999-2015 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Id$
-
-EAPI=5
-
-if [[ ${PV} = 9999* ]]; then
-	inherit git-r3
-	EGIT_REPO_URI="git://github.com/gentoo/${PN}.git"
-else
-	SRC_URI="https://dev.gentoo.org/~williamh/dist/${P}.tar.bz2
-	prefix? ( https://dev.gentoo.org/~grobian/distfiles/consoletype-1.0.sh )"
-	KEYWORDS="alpha amd64 arm arm64 hppa ia64 m68k ~mips ppc ppc64 s390 sh sparc x86 ~ppc-aix ~amd64-fbsd ~sparc-fbsd ~x86-fbsd ~x64-freebsd ~x86-freebsd ~hppa-hpux ~ia64-hpux ~x86-interix ~amd64-linux ~arm-linux ~ia64-linux ~x86-linux ~ppc-macos ~x64-macos ~x86-macos ~m68k-mint ~sparc-solaris ~sparc64-solaris ~x64-solaris ~x86-solaris ~x86-winnt"
-fi
-
-inherit toolchain-funcs
-
-DESCRIPTION="base functions required by all gentoo systems"
-HOMEPAGE="https://www.gentoo.org"
-
-LICENSE="GPL-2"
-SLOT="0"
-IUSE=""
-
-src_prepare() {
-	tc-export CC
-
-	# in prefix, use shell script, for the C-code doesn't compile
-	use prefix && cp "${DISTDIR}"/consoletype-1.0.sh consoletype
-}
-
-src_install() {
-	emake install DESTDIR="${ED}"
-}

sys-apps/gentoo-functions/gentoo-functions-0.7.ebuild
@@ -1,26 +1,0 @@
-# Copyright 1999-2014 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Id$
-
-EAPI=5
-
-if [[ ${PV} = 9999* ]]; then
-	inherit git-r3
-	EGIT_REPO_URI="git://github.com/gentoo/${PN}.git"
-else
-	SRC_URI="https://dev.gentoo.org/~williamh/dist/${P}.tar.bz2"
-	KEYWORDS="alpha amd64 arm arm64 hppa ia64 ~m68k ~mips ppc ppc64 ~s390 ~sh sparc x86 ~ppc-aix ~amd64-fbsd ~sparc-fbsd ~x86-fbsd ~x64-freebsd ~x86-freebsd ~hppa-hpux ~ia64-hpux ~x86-interix ~amd64-linux ~arm-linux ~ia64-linux ~x86-linux ~ppc-macos ~x64-macos ~x86-macos ~m68k-mint ~sparc-solaris ~sparc64-solaris ~x64-solaris ~x86-solaris ~x86-winnt"
-fi
-
-inherit toolchain-funcs
-
-DESCRIPTION="base functions required by all gentoo systems"
-HOMEPAGE="https://www.gentoo.org"
-
-LICENSE="GPL-2"
-SLOT="0"
-IUSE=""
-
-src_prepare() {
-	tc-export CC
-}

sys-apps/gentoo-functions/gentoo-functions-0.9.ebuild
@@ -1,30 +1,0 @@
-# Copyright 1999-2015 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Id$
-
-EAPI=5
-
-if [[ ${PV} = 9999* ]]; then
-	inherit git-r3
-	EGIT_REPO_URI="git://github.com/gentoo/${PN}.git"
-else
-	SRC_URI="https://github.com/gentoo/${PN}/archive/${PV}.tar.gz -> ${P}.tar.gz"
-	KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~m68k ~mips ~ppc ~ppc64 ~s390 ~sh ~sparc ~x86 ~ppc-aix ~amd64-fbsd ~sparc-fbsd ~x86-fbsd ~x64-freebsd ~x86-freebsd ~hppa-hpux ~ia64-hpux ~x86-interix ~amd64-linux ~arm-linux ~ia64-linux ~x86-linux ~ppc-macos ~x64-macos ~x86-macos ~m68k-mint ~sparc-solaris ~sparc64-solaris ~x64-solaris ~x86-solaris ~x86-winnt"
-fi
-
-inherit toolchain-funcs
-
-DESCRIPTION="base functions required by all gentoo systems"
-HOMEPAGE="https://www.gentoo.org"
-
-LICENSE="GPL-2"
-SLOT="0"
-IUSE=""
-
-src_prepare() {
-	tc-export CC
-}
-
-src_install() {
-	emake install DESTDIR="${ED}"
-}

commit a15037b85d09ab1f440ff997beca4136e7d75342

sys-apps/gentoo-functions: Add back stable gentoo-functions-0.8.ebuild.
Since commit ca1af4526d676d5162292af3ad9d6db4f84a8083 broke the tree due to a
couple of packages relying on gentoo-functions for exotic arches, add back 0.8
till 0.10 is stabilised across all arches to mend the breakage.

Package-Manager: portage-2.2.26

sys-apps/gentoo-functions/Manifest
@@ -1,1 +1,3 @@
+DIST consoletype-1.0.sh 616 SHA256 b6f837be496024941b81402e6c11300eb4a53987368c5c6d9afb5fd8b1c34d28 SHA512 ab6edd0035ad2ac855bc7b94679a78aad63f647b906dfda39a9844b676e6cdfc872fd1fc897f6158a5709906905a4da5ce8b4665f9e2bc0bcd1e88bb9ecace07 WHIRLPOOL b2d5f13e7411d8e6b5c4dae875041a95bed3ed7b20e765971b86fd16b1a970e8ad10288683d2efa530ef8568c6c58a07f9fc5a4a1b6b00ee6ed731da90e472ed
 DIST gentoo-functions-0.10.tar.gz 11432 SHA256 709c8b22f404001a512e47a7a4d3192070b3e150fb9d0f943de09736d665b0db SHA512 2622cede032089c7b8a2dec576e848852f9964e0198ab161f0ff5c426c542d0849bd3d30a40c296805bfec90b533c419df43c7975ed85678471483c827ca4cdb WHIRLPOOL 6559c62083eca8a016f005d1003c8a0b6e2c89836c130cec9e0a40575e1604039f669313547c63f6cf84f1a40cdd7249d227c6f0b57f282764df8d20ef239051
+DIST gentoo-functions-0.8.tar.bz2 10647 SHA256 89dc0a3289a2e3312efb560e904add13ececdd5eb554151cb30d0b96f1bd8116 SHA512 34f04f5e7ecd888197b6486c131c346a84a6937501f68a54481ec3caacaee86e54345a6c34364a5953c55b1d4aeb4ca2173b70864cdd7b81e74964352132b322 WHIRLPOOL 6ba223987953fa1dd80b79590c5f72ab8b5d0eca47cc1df23559e295f03faa369a372e5c27ed47d19d605dfe072c22c7702f3d4443a8ea2489d25092274670f3

sys-apps/gentoo-functions/gentoo-functions-0.8.ebuild
@@ -1,0 +1,34 @@
+# Copyright 1999-2016 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+# $Id$
+
+EAPI=5
+
+if [[ ${PV} = 9999* ]]; then
+	inherit git-r3
+	EGIT_REPO_URI="git://github.com/gentoo/${PN}.git"
+else
+	SRC_URI="https://dev.gentoo.org/~williamh/dist/${P}.tar.bz2
+	prefix? ( https://dev.gentoo.org/~grobian/distfiles/consoletype-1.0.sh )"
+	KEYWORDS="alpha amd64 arm arm64 hppa ia64 m68k ~mips ppc ppc64 s390 sh sparc x86 ~ppc-aix ~amd64-fbsd ~sparc-fbsd ~x86-fbsd ~x64-freebsd ~x86-freebsd ~hppa-hpux ~ia64-hpux ~x86-interix ~amd64-linux ~arm-linux ~ia64-linux ~x86-linux ~ppc-macos ~x64-macos ~x86-macos ~m68k-mint ~sparc-solaris ~sparc64-solaris ~x64-solaris ~x86-solaris ~x86-winnt"
+fi
+
+inherit toolchain-funcs
+
+DESCRIPTION="base functions required by all gentoo systems"
+HOMEPAGE="https://www.gentoo.org"
+
+LICENSE="GPL-2"
+SLOT="0"
+IUSE=""
+
+src_prepare() {
+	tc-export CC
+
+	# in prefix, use shell script, for the C-code doesn't compile
+	use prefix && cp "${DISTDIR}"/consoletype-1.0.sh consoletype
+}
+
+src_install() {
+	emake install DESTDIR="${ED}"
+}