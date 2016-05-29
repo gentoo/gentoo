725bcc657fc026420b7e851cdc4163784889f36d
Hans de Graaff <graaff@gentoo.org>
2016-05-29 15:31:46 +0200

    app-emacs/markdown-mode: add 2.1
    Backport upstream patch to not add .text to auto-mode-alist.
    
    Add auto-mode-alist entries for .md and .markdown in site-init.
    
    Package-Manager: portage-2.2.28

--- app-emacs/markdown-mode/files/50markdown-mode-gentoo.el
+++ app-emacs/markdown-mode/files/50markdown-mode-gentoo.el
@@ -1,3 +1,5 @@
 (add-to-list 'load-path "@SITELISP@")
 (autoload 'markdown-mode "markdown-mode"
   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
