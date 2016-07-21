gentoo/gentoo

commit 2632c5199d8d2de43051f09f0dbfac46c0361cdf

media-sound/cmus: rename USE 'wma' to USE 'ffmpeg'
Reported-by: xpue <r9ku1q@gmail.com>
Gentoo-Bug: 553834

Package-Manager: portage-2.2.28

media-sound/cmus/cmus-2.7.1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2015 Gentoo Foundation
+# Copyright 1999-2016 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 # $Id$
 
@@ -12,9 +12,9 @@ SRC_URI="https://github.com/cmus/cmus/archive/v${PV}.tar.gz -> ${P}.tar.gz"
 LICENSE="GPL-2"
 SLOT="0"
 KEYWORDS="amd64 ~ppc ~ppc64 x86 ~x86-fbsd ~amd64-linux ~x86-linux ~x86-solaris"
-IUSE="aac alsa ao cue cdio cddb discid debug examples +flac jack libsamplerate
+IUSE="aac alsa ao cue cdio cddb discid debug examples ffmpeg +flac jack libsamplerate
 	+mad mikmod modplug mp4 musepack opus oss pidgin pulseaudio tremor +unicode
-	+vorbis wavpack wma"
+	+vorbis wavpack"
 
 CDEPEND="sys-libs/ncurses[unicode?]
 	aac? ( media-libs/faad2 )
@@ -24,6 +24,7 @@ CDEPEND="sys-libs/ncurses[unicode?]
 	cdio? ( dev-libs/libcdio-paranoia )
 	cddb? ( media-libs/libcddb )
 	discid? ( media-libs/libdiscid )
+	ffmpeg? ( media-video/ffmpeg:= )
 	flac? ( media-libs/flac )
 	jack? ( media-sound/jack-audio-connection-kit )
 	libsamplerate? ( media-libs/libsamplerate )
@@ -36,8 +37,7 @@ CDEPEND="sys-libs/ncurses[unicode?]
 	pulseaudio? ( media-sound/pulseaudio )
 	tremor? ( media-libs/tremor )
 	!tremor? ( vorbis? ( >=media-libs/libvorbis-1.0 ) )
-	wavpack? ( media-sound/wavpack )
-	wma? ( media-video/ffmpeg:= )"
+	wavpack? ( media-sound/wavpack )"
 DEPEND="${CDEPEND}
 	virtual/pkgconfig"
 RDEPEND="${CDEPEND}
@@ -101,4 +101,9 @@ src_install() {
 	if use pidgin; then
 		newbin contrib/cmus-updatepidgin.py cmus-updatepidgin
 	fi
+}
+
+pkg_postinst() {
+	einfo "USE flag 'wma' was replaced by 'ffmpeg'"
+	einfo "Details here - https://bugs.gentoo.org/show_bug.cgi?id=553834"
 }

media-sound/cmus/metadata.xml
@@ -12,7 +12,6 @@
     <flag name="pidgin">install support script for <pkg>net-im/pidgin</pkg></flag>
     <flag name="opus">enable support for <pkg>media-libs/opusfile</pkg></flag>
     <flag name="tremor">use libivorbis from <pkg>media-libs/tremor</pkg> instead of <pkg>media-libs/libvorbis</pkg></flag>
-    <flag name="wma">add support for Windows Media Audio</flag>
   </use>
   <upstream>
     <remote-id type="github">cmus/cmus</remote-id>

commit e7c288ceaa619cb7fd4f52b2fc9ed84eeaa266a7

media-sound/cmus: fix breakage introduced by 2632c5199d8d2de43051f09f0dbfac46c0361cdf
Gentoo-bug: 589244

Package-Manager: portage-2.3.0

media-sound/cmus/cmus-2.7.1.ebuild
@@ -74,7 +74,7 @@ src_configure() {
 	my_config wavpack CONFIG_WAVPACK
 	my_config mp4 CONFIG_MP4
 	my_config aac CONFIG_AAC
-	my_config wma CONFIG_FFMPEG
+	my_config ffmpeg CONFIG_FFMPEG
 	my_config cue CONFIG_CUE
 	my_config pulseaudio CONFIG_PULSE
 	my_config alsa CONFIG_ALSA