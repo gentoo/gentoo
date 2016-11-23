6d7e6608cc3cf229300e7bea7b24c030b2f1294c
Guillaume Seren <guillaumeseren@gmail.com>
2016-11-23 19:15:30 +0100

    dev-php/symfony-process: Add 2.8.12 version
    This is needed for composer-1.2.2, bug 439206.

---
+++ dev-php/symfony-process/files/autoload.php
@@ -0,0 +1,9 @@
+<?php
+/* Autoloader for symfony-process and its dependencies */
+
+$vendorDir = '/usr/share/php';
+if (!class_exists('Fedora\\Autoloader\\Autoload', false)) {
+    require_once '/usr/share/php/Fedora/Autoloader/autoload.php';
+}
+
+\Fedora\Autoloader\Autoload::addPsr4('Symfony\\Component\\Process\\', __DIR__);
