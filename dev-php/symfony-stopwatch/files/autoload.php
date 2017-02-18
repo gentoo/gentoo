1c9f6adb6294ffac40922cea327acea44bf133ff
Guillaume Seren <guillaumeseren@gmail.com>
2017-02-18 14:23:54 +0100

    dev-php/symfony-stopwatch: Add version 3.2.2
    Add version 3.2.2, needed by dev-php/composer-1.3.2
    
    Gentoo-Bug: 608136

---
+++ dev-php/symfony-stopwatch/files/autoload.php
@@ -0,0 +1,8 @@
+<?php
+/* Autoloader for dev-php/symfony-stopwatch and its dependencies */
+
+if (!class_exists('Fedora\\Autoloader\\Autoload', false)) {
+    require_once '/usr/share/php/Fedora/Autoloader/autoload.php';
+}
+
+\Fedora\Autoloader\Autoload::addPsr4('Symfony\\Component\\Stopwatch\\', __DIR__);
