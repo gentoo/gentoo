e1100985cbebfc3430f6669039b228d94c2f441a
Michael Orlitzky <mjo@gentoo.org>
2017-04-25 18:35:11 -0400

    dev-php/symfony-config: new version 2.8.19.
    Package-Manager: Portage-2.3.3, Repoman-2.3.1

--- dev-php/symfony-config/files/autoload.php
+++ dev-php/symfony-config/files/autoload.php
@@ -1,8 +1,15 @@
 <?php
 /* Autoloader for dev-php/symfony-config and its dependencies */
 
+$vendor_dir = '/usr/share/php';
+
 if (!class_exists('Fedora\\Autoloader\\Autoload', false)) {
-    require_once '/usr/share/php/Fedora/Autoloader/autoload.php';
+  require_once("${vendor_dir}/Fedora/Autoloader/autoload.php");
 }
 
 \Fedora\Autoloader\Autoload::addPsr4('Symfony\\Component\\Config\\', __DIR__);
+
+// Dependencies
+\Fedora\Autoloader\Dependencies::required(array(
+  "${vendor_dir}/Symfony/Component/Filesystem/autoload.php"
+));
