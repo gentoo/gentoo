e8519fd6678a9b7c901f8c861b694b31a9063b70
Hans de Graaff <graaff@gentoo.org>
2017-05-03 10:55:02 +0200

    app-emacs/yaml-mode: add 0.0.13, update auto-mode-alist accordingly
    Package-Manager: Portage-2.3.5, Repoman-2.3.1

--- app-emacs/yaml-mode/files/50yaml-mode-gentoo.el
+++ app-emacs/yaml-mode/files/50yaml-mode-gentoo.el
@@ -1,3 +1,3 @@
 (add-to-list 'load-path "@SITELISP@")
 (autoload 'yaml-mode "yaml-mode" "Simple mode to edit YAML." t)
-(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.\\(e?ya?\\|ra\\)ml\\'" . yaml-mode))
