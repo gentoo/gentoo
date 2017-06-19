17d488952196169bd603ef61581f5653c3840eb3
Slawomir Lis <slis@gentoo.org>
2017-06-19 13:57:29 +0200

    net-analyzer/ntopng: version bump to 3.0
    Package-Manager: Portage-2.3.6, Repoman-2.3.2

--- net-analyzer/ntopng/files/ntopng.init.d
+++ net-analyzer/ntopng/files/ntopng.init.d
@@ -1,6 +1,7 @@
 #!/sbin/openrc-run
-# Copyright 1999-2015 Gentoo Foundation
+# Copyright 1999-2017 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
+# $Header: $
 
 depend() {
     need net redis
