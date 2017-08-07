gentoo/gentoo

commit 66e1778c95aa54f432305e5174a01f0f6be41b89

net-vpn/openvpn: Force /bin/{ifconfig,route}
openvpn build prefers ifconfig/route in /sbin, even if you are trying to
move away from those because net-tools now installs then in /bin and you
only have symlinks in /sbin for migration. Force a bump to rebuild with
explicitly referencing /bin and a new-enough version of net-tools.

Package-Manager: portage-2.3.6

net-vpn/openvpn/openvpn-2.4.3-r1.ebuild
@@ -12,7 +12,7 @@ HOMEPAGE="http://openvpn.net/"
 
 LICENSE="GPL-2"
 SLOT="0"
-KEYWORDS="alpha amd64 arm hppa ia64 ~mips ppc ppc64 ~s390 ~sh sparc x86 ~sparc-fbsd ~x86-fbsd ~amd64-linux ~arm-linux ~x86-linux ~x86-macos"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~mips ~ppc ~ppc64 ~s390 ~sh ~sparc ~x86 ~sparc-fbsd ~x86-fbsd ~amd64-linux ~arm-linux ~x86-linux ~x86-macos"
 
 IUSE="down-root examples inotify iproute2 libressl lz4 +lzo mbedtls pam"
 IUSE+=" pkcs11 +plugins selinux +ssl static systemd test userland_BSD"
@@ -26,7 +26,7 @@ REQUIRED_USE="static? ( !plugins !pkcs11 )
 CDEPEND="
 	kernel_linux? (
 		iproute2? ( sys-apps/iproute2[-minimal] )
-		!iproute2? ( >=sys-apps/net-tools-1.60_p20160215155418 )
+		!iproute2? ( sys-apps/net-tools )
 	)
 	pam? ( virtual/pam )
 	ssl? (
@@ -68,6 +68,8 @@ src_configure() {
 	use static && append-ldflags -Xcompiler -static
 	SYSTEMD_UNIT_DIR=$(systemd_get_systemunitdir) \
 	TMPFILES_DIR="/usr/lib/tmpfiles.d" \
+	IFCONFIG=/bin/ifconfig \
+	ROUTE=/bin/route \
 	econf \
 		$(usex mbedtls '--with-crypto-library=mbedtls' '') \
 		$(use_enable inotify async-push) \

commit 76d196ba473c74ba967b0eebe80804f16e23e31e

net-vpn/openvpn: dep change in wrong ebuild.
Fixes: 66e1778c95aa54f432305e5174a01f0f6be41b89
Package-Manager: portage-2.3.6

net-vpn/openvpn/openvpn-2.4.2-r1.ebuild
@@ -26,7 +26,7 @@ REQUIRED_USE="static? ( !plugins !pkcs11 )
 CDEPEND="
 	kernel_linux? (
 		iproute2? ( sys-apps/iproute2[-minimal] )
-		!iproute2? ( >=sys-apps/net-tools-1.60_p20160215155418 )
+		!iproute2? ( sys-apps/net-tools )
 	)
 	pam? ( virtual/pam )
 	ssl? (

net-vpn/openvpn/openvpn-2.4.3-r1.ebuild
@@ -26,7 +26,7 @@ REQUIRED_USE="static? ( !plugins !pkcs11 )
 CDEPEND="
 	kernel_linux? (
 		iproute2? ( sys-apps/iproute2[-minimal] )
-		!iproute2? ( sys-apps/net-tools )
+		!iproute2? ( >=sys-apps/net-tools-1.60_p20160215155418 )
 	)
 	pam? ( virtual/pam )
 	ssl? (