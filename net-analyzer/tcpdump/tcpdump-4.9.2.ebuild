gentoo/gentoo

commit 2b45ef99159553b83e9a0bac9a597a1a300fe025

net-analyzer/tcpdump: Remove 4.9.2.
Package-Manager: Portage-2.3.8, Repoman-2.3.3

net-analyzer/tcpdump/tcpdump-4.9.2.ebuild
@@ -1,103 +1,0 @@
-# Copyright 1999-2017 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=6
-inherit eutils flag-o-matic toolchain-funcs user
-
-DESCRIPTION="A Tool for network monitoring and data acquisition"
-EGIT_REPO_URI="https://github.com/the-tcpdump-group/tcpdump"
-HOMEPAGE="
-	http://www.tcpdump.org/
-	${EGIT_REPO_URI}
-"
-
-LICENSE="BSD"
-SLOT="0"
-IUSE="+drop-root libressl smi ssl samba suid test"
-if [[ ${PV} == "9999" ]] ; then
-	inherit git-r3
-	KEYWORDS=""
-else
-	#SRC_URI="https://github.com/the-${PN}-group/${PN}/archive/${P}.tar.gz"
-	SRC_URI="https://dev.gentoo.org/~zerochaos/distfiles/${P}.tar.gz"
-	KEYWORDS="~alpha amd64 ~arm ~arm64 ~hppa ~ia64 ~mips ~ppc ~ppc64 ~s390 ~sh ~sparc x86 ~x86-fbsd ~amd64-linux ~arm-linux ~x86-linux"
-fi
-
-RDEPEND="
-	drop-root? ( sys-libs/libcap-ng )
-	net-libs/libpcap
-	smi? ( net-libs/libsmi )
-	ssl? (
-		!libressl? ( >=dev-libs/openssl-0.9.6m:0 )
-		libressl? ( dev-libs/libressl )
-	)
-"
-DEPEND="
-	${RDEPEND}
-	drop-root? ( virtual/pkgconfig )
-	test? (
-		|| ( app-arch/sharutils sys-freebsd/freebsd-ubin )
-		dev-lang/perl
-	)
-"
-
-pkg_setup() {
-	if use drop-root || use suid; then
-		enewgroup tcpdump
-		enewuser tcpdump -1 -1 -1 tcpdump
-	fi
-}
-
-src_configure() {
-	# tcpdump needs some optimization. see bug #108391
-	# but do not replace -Os
-	filter-flags -O[0-9]
-	has -O? ${CFLAGS} || append-cflags -O2
-
-	filter-flags -finline-functions
-
-	if use drop-root; then
-		append-cppflags -DHAVE_CAP_NG_H
-		export LIBS=$( $(tc-getPKG_CONFIG) --libs libcap-ng )
-	fi
-
-	econf \
-		$(use_enable samba smb) \
-		$(use_with drop-root chroot '') \
-		$(use_with smi) \
-		$(use_with ssl crypto "${EPREFIX}/usr") \
-		$(usex drop-root "--with-user=tcpdump" "")
-}
-
-src_test() {
-	if [[ ${EUID} -ne 0 ]] || ! use drop-root; then
-		sed -i -e '/^\(espudp1\|eapon1\)/d;' tests/TESTLIST || die
-		emake check
-	else
-		ewarn "If you want to run the test suite, make sure you either"
-		ewarn "set FEATURES=userpriv or set USE=-drop-root"
-	fi
-}
-
-src_install() {
-	dosbin tcpdump
-	doman tcpdump.1
-	dodoc *.awk
-	dodoc CHANGES CREDITS README.md
-
-	if use suid; then
-		fowners root:tcpdump /usr/sbin/tcpdump
-		fperms 4110 /usr/sbin/tcpdump
-	fi
-}
-
-pkg_preinst() {
-	if use drop-root || use suid; then
-		enewgroup tcpdump
-		enewuser tcpdump -1 -1 -1 tcpdump
-	fi
-}
-
-pkg_postinst() {
-	use suid && elog "To let normal users run tcpdump add them into tcpdump group."
-}

net-analyzer/tcpdump/Manifest
@@ -1,3 +1,2 @@
 DIST tcpdump-4.9.0.tar.gz 1260309 SHA256 eae98121cbb1c9adbedd9a777bf2eae9fa1c1c676424a54740311c8abcee5a5e SHA512 e98d73ae706d42e96b4069e196cf17af892eb97935664eebd08779b55b4da858bc1732d714efd16924f862aad0ba5550ceb2213c68414ed48907c46456c9fc82 WHIRLPOOL bd2284919bd3a2af166c6e05254e6fb7fa757b76f269d1a4951fe0ccfb6ad282a50f5670d3f8a38f6bffd1834bc809effd2e8575f9f0dd2fd7eaa4b4fdce5552
 DIST tcpdump-4.9.1.tar.gz 1192832 SHA256 4d1a69eab083d5df91effb805024abd4406e8a592e98558ed8722ef160c58119 SHA512 82ff20ece4c197f4b0142e45227310dc513facc9668c1423cb402eb2b62a18909a172c432ee6b20c012c172d47752ef65e2747f9d93d335073ea0ce3c007f5c9 WHIRLPOOL ed59af89e0c67477db3f10bd2d8c2b9b2dc2b3c7e3906abe37b0eba5024084e93afa98b39690c1c3af94cc6a0955023ee01ccfc5547a454dafe952957807295f
-DIST tcpdump-4.9.2.tar.gz 2298386 SHA256 798b3536a29832ce0cbb07fafb1ce5097c95e308a6f592d14052e1ef1505fe79 SHA512 e1bc19a5867d6e3628f3941bdf3ec831bf13784f1233ca1bccc46aac1702f47ee9357d7ff0ca62cddf211b3c8884488c21144cabddd92c861e32398cd8f7c44b WHIRLPOOL 6652a5bb83a082bc8df050610ed640f0119fb846f2938a5148be3008506548e73c3c6437a62752eafdd00910c35b2b5decfd21c207719d156fb7dc8ca9e3443a

commit b698a62ba12a09474e84f1b75d81da25f6809207

net-analyzer/tcpdump: Restore stable 4.9.2 for amd64 and x86
Restoring stable keywords for amd64 and x86 that were removed in commit
2b45ef99159553b83e9a0bac9a597a1a300fe025.

Fixes: 2b45ef99159553b83e9a0bac9a597a1a300fe025
Gentoo-Bug: 630110

Package-Manager: Portage-2.3.6, Repoman-2.3.1

net-analyzer/tcpdump/tcpdump-4.9.2.ebuild
@@ -15,7 +15,7 @@ SRC_URI="
 
 LICENSE="BSD"
 SLOT="0"
-KEYWORDS="alpha ~amd64 arm ~arm64 ~hppa ia64 ~mips ~ppc ~ppc64 ~s390 ~sh ~sparc ~x86 ~x86-fbsd ~amd64-linux ~arm-linux ~x86-linux"
+KEYWORDS="alpha amd64 arm ~arm64 ~hppa ia64 ~mips ~ppc ~ppc64 ~s390 ~sh ~sparc x86 ~x86-fbsd ~amd64-linux ~arm-linux ~x86-linux"
 IUSE="+drop-root libressl smi ssl samba suid test"
 
 RDEPEND="