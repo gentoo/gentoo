gentoo/gentoo

commit 684adb614c24171ce50d520a628b16e1e1860a8d

media-libs/x264: use https for git repo

media-libs/x264/x264-9999.ebuild
@@ -9,7 +9,7 @@ DESCRIPTION="A free library for encoding X264/AVC streams"
 HOMEPAGE="https://www.videolan.org/developers/x264.html"
 if [[ ${PV} == 9999 ]]; then
 	inherit git-2
-	EGIT_REPO_URI="git://git.videolan.org/x264.git"
+	EGIT_REPO_URI="https://git.videolan.org/x264.git"
 else
 	inherit versionator
 	MY_P="x264-snapshot-$(get_version_component_range 3)-2245"

media-libs/x264/x264-0.0.20160712.ebuild
@@ -9,7 +9,7 @@ DESCRIPTION="A free library for encoding X264/AVC streams"
 HOMEPAGE="https://www.videolan.org/developers/x264.html"
 if [[ ${PV} == 9999 ]]; then
 	inherit git-2
-	EGIT_REPO_URI="git://git.videolan.org/x264.git"
+	EGIT_REPO_URI="https://git.videolan.org/x264.git"
 else
 	inherit versionator
 	MY_P="x264-snapshot-$(get_version_component_range 3)-2245"

media-libs/x264/x264-0.0.20170701.ebuild
@@ -9,7 +9,7 @@ DESCRIPTION="A free library for encoding X264/AVC streams"
 HOMEPAGE="https://www.videolan.org/developers/x264.html"
 if [[ ${PV} == 9999 ]]; then
 	inherit git-2
-	EGIT_REPO_URI="git://git.videolan.org/x264.git"
+	EGIT_REPO_URI="https://git.videolan.org/x264.git"
 else
 	inherit versionator
 	MY_P="x264-snapshot-$(get_version_component_range 3)-2245"

commit 683b6ca705b046d838d3bb68d399bf469fa0cc87

media-libs/x264: fix git uri
Bug: https://bugs.gentoo.org/641168
Fixes: 684adb614c24 ("media-libs/x264: use https for git repo")
Acked-by: Luca Barbato <lu_zero@gentoo.org>
Package-Manager: Portage-2.3.17, Repoman-2.3.6

media-libs/x264/x264-9999.ebuild
@@ -9,7 +9,7 @@ DESCRIPTION="A free library for encoding X264/AVC streams"
 HOMEPAGE="https://www.videolan.org/developers/x264.html"
 if [[ ${PV} == 9999 ]]; then
 	inherit git-2
-	EGIT_REPO_URI="https://git.videolan.org/x264.git"
+	EGIT_REPO_URI="https://git.videolan.org/git/x264.git"
 else
 	inherit versionator
 	MY_P="x264-snapshot-$(get_version_component_range 3)-2245"