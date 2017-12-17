gentoo/gentoo

commit 018ec8a7d8411fb44a08b8e7882951537567cec7

media-libs/libbdplus: use https for SRC_URI, HOMEPAGE, and git repo

media-libs/libbdplus/libbdplus-9999.ebuild
@@ -6,14 +6,14 @@ inherit autotools-multilib
 
 if [[ ${PV} == 9999 ]] ; then
 	inherit git-r3
-	EGIT_REPO_URI="git://git.videolan.org/${PN}.git"
+	EGIT_REPO_URI="https://git.videolan.org/${PN}.git"
 else
-	SRC_URI="http://downloads.videolan.org/pub/videolan/${PN}/${PV}/${P}.tar.bz2"
+	SRC_URI="https://downloads.videolan.org/pub/videolan/${PN}/${PV}/${P}.tar.bz2"
 	KEYWORDS="~amd64 ~x86"
 fi
 
 DESCRIPTION="Blu-ray library for BD+ decryption"
-HOMEPAGE="http://www.videolan.org/developers/libbdplus.html"
+HOMEPAGE="https://www.videolan.org/developers/libbdplus.html"
 
 LICENSE="LGPL-2.1"
 SLOT="0"

media-libs/libbdplus/libbdplus-0.1.2.ebuild
@@ -6,14 +6,14 @@ inherit autotools-multilib
 
 if [[ ${PV} == 9999 ]] ; then
 	inherit git-r3
-	EGIT_REPO_URI="git://git.videolan.org/${PN}.git"
+	EGIT_REPO_URI="https://git.videolan.org/${PN}.git"
 else
-	SRC_URI="http://downloads.videolan.org/pub/videolan/${PN}/${PV}/${P}.tar.bz2"
+	SRC_URI="https://downloads.videolan.org/pub/videolan/${PN}/${PV}/${P}.tar.bz2"
 	KEYWORDS="amd64 ppc ppc64 sparc x86"
 fi
 
 DESCRIPTION="Blu-ray library for BD+ decryption"
-HOMEPAGE="http://www.videolan.org/developers/libbdplus.html"
+HOMEPAGE="https://www.videolan.org/developers/libbdplus.html"
 
 LICENSE="LGPL-2.1"
 SLOT="0"

commit f435431483d009ab8a82676f96f9e25bdcc3cb2a

media-libs/libbdplus: fix git uri
Bug: https://bugs.gentoo.org/641168
Fixes: 018ec8a7d841 ("media-libs/libbdplus: use https for SRC_URI, HOMEPAGE, and git repo")
Acked-by: Luca Barbato <lu_zero@gentoo.org>
Package-Manager: Portage-2.3.17, Repoman-2.3.6

media-libs/libbdplus/libbdplus-9999.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2015 Gentoo Foundation
+# Copyright 1999-2017 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=5
@@ -6,7 +6,7 @@ inherit autotools-multilib
 
 if [[ ${PV} == 9999 ]] ; then
 	inherit git-r3
-	EGIT_REPO_URI="https://git.videolan.org/${PN}.git"
+	EGIT_REPO_URI="https://git.videolan.org/git/${PN}.git"
 else
 	SRC_URI="https://downloads.videolan.org/pub/videolan/${PN}/${PV}/${P}.tar.bz2"
 	KEYWORDS="~amd64 ~x86"