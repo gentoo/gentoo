83e1de2d357ce997bc931398b6a6dac2d3f7def5
Rob Levitsky <kitsunenokenja@protonmail.ch>
2018-01-07 16:25:27 -0500

    dev-php/sebastian-environment: version bump
    Package-Manager: Portage-2.3.13, Repoman-2.3.3

---
+++ dev-php/sebastian-environment/files/autoload-3.1.0.php
@@ -0,0 +1,5 @@
+<?php
+/* Autoloader for dev-php/sebastian-environment */
+require_once '/usr/share/php/Fedora/Autoloader/autoload.php';
+
+\Fedora\Autoloader\Autoload::addPsr4('SebastianBergmann\\Environment', __DIR__);
