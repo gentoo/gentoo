gentoo/gentoo

commit c5bd4ddefd1b1f6e8b9f69da68e6aef5249bc5a5

sys-apps/portage: disable rsync-verify for stage1 build
Package-Manager: Portage-2.3.24, Repoman-2.3.6

sys-apps/portage/portage-9999.ebuild
@@ -44,6 +44,11 @@ RDEPEND="
 		>=app-admin/eselect-1.2
 		$(python_gen_cond_dep 'dev-python/pyblake2[${PYTHON_USEDEP}]' \
 			python{2_7,3_4,3_5} pypy)
+		rsync-verify? (
+			>=app-portage/gemato-10
+			app-crypt/gentoo-keys
+			app-crypt/gnupg[ssl(-)]
+		)
 	)
 	elibc_FreeBSD? ( sys-freebsd/freebsd-bin )
 	elibc_glibc? ( >=sys-apps/sandbox-2.2 )
@@ -122,7 +127,7 @@ python_prepare_all() {
 			|| die "failed to append to make.globals"
 	fi
 
-	if ! use rsync-verify; then
+	if use build || ! use rsync-verify; then
 		sed -e '/^sync-rsync-verify-metamanifest/s|yes|no|' \
 			-i cnf/repos.conf || die "sed failed"
 	fi

sys-apps/portage/portage-2.3.24.ebuild
@@ -44,17 +44,17 @@ RDEPEND="
 		>=app-admin/eselect-1.2
 		$(python_gen_cond_dep 'dev-python/pyblake2[${PYTHON_USEDEP}]' \
 			python{2_7,3_4,3_5} pypy)
+		rsync-verify? (
+			>=app-portage/gemato-10
+			app-crypt/gentoo-keys
+			app-crypt/gnupg[ssl(-)]
+		)
 	)
 	elibc_FreeBSD? ( sys-freebsd/freebsd-bin )
 	elibc_glibc? ( >=sys-apps/sandbox-2.2 )
 	elibc_musl? ( >=sys-apps/sandbox-2.2 )
 	elibc_uclibc? ( >=sys-apps/sandbox-2.2 )
 	>=app-misc/pax-utils-0.1.17
-	rsync-verify? (
-		>=app-portage/gemato-10
-		app-crypt/gentoo-keys
-		app-crypt/gnupg[ssl(-)]
-	)
 	selinux? ( >=sys-libs/libselinux-2.0.94[python,${PYTHON_USEDEP}] )
 	xattr? ( kernel_linux? (
 		>=sys-apps/install-xattr-0.3
@@ -123,7 +123,7 @@ python_prepare_all() {
 			|| die "failed to append to make.globals"
 	fi
 
-	if ! use rsync-verify; then
+	if use build || ! use rsync-verify; then
 		sed -e '/^sync-rsync-verify-metamanifest/s|yes|no|' \
 			-i cnf/repos.conf || die "sed failed"
 	fi

commit 59ca95e102c561a38696804e7faee2dadbf96451

sys-apps/portage: remove duplicate rsync-verify deps in 9999
Fixes: c5bd4ddefd1b ("sys-apps/portage: disable rsync-verify for stage1 build")
Package-Manager: Portage-2.3.24, Repoman-2.3.6

sys-apps/portage/portage-9999.ebuild
@@ -55,11 +55,6 @@ RDEPEND="
 	elibc_musl? ( >=sys-apps/sandbox-2.2 )
 	elibc_uclibc? ( >=sys-apps/sandbox-2.2 )
 	>=app-misc/pax-utils-0.1.17
-	rsync-verify? (
-		>=app-portage/gemato-11
-		app-crypt/gentoo-keys
-		app-crypt/gnupg[ssl(-)]
-	)
 	selinux? ( >=sys-libs/libselinux-2.0.94[python,${PYTHON_USEDEP}] )
 	xattr? ( kernel_linux? (
 		>=sys-apps/install-xattr-0.3