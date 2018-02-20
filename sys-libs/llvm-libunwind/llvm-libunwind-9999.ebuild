gentoo/gentoo

commit 14f34132fedfa241528b7699d19bebe3c98ded63

sys-libs/llvm-libunwind: fix when test is off (*9999)
Also avoid fetching libcxx unless test is used.

Closes: https://github.com/gentoo/gentoo/pull/7227

sys-libs/llvm-libunwind/llvm-libunwind-9999.ebuild
@@ -42,15 +42,17 @@ pkg_setup() {
 
 src_unpack() {
 	# we need headers & test utilities
-	git-r3_fetch "https://git.llvm.org/git/libcxx.git
-		https://github.com/llvm-mirror/libcxx.git"
+	if use test
+		git-r3_fetch "https://git.llvm.org/git/libcxx.git
+			https://github.com/llvm-mirror/libcxx.git"
+	fi
 	git-r3_fetch
 
 	if use test; then
 		git-r3_checkout https://llvm.org/git/libcxx.git \
 			"${WORKDIR}"/libcxx '' include utils/libcxx
-		git-r3_checkout
 	fi
+	git-r3_checkout
 }
 
 multilib_src_configure() {

sys-libs/llvm-libunwind/llvm-libunwind-6.0.9999.ebuild
@@ -43,15 +43,17 @@ pkg_setup() {
 
 src_unpack() {
 	# we need headers & test utilities
-	git-r3_fetch "https://git.llvm.org/git/libcxx.git
-		https://github.com/llvm-mirror/libcxx.git"
+	if use test; then
+		git-r3_fetch "https://git.llvm.org/git/libcxx.git
+			https://github.com/llvm-mirror/libcxx.git"
+	fi
 	git-r3_fetch
 
 	if use test; then
 		git-r3_checkout https://llvm.org/git/libcxx.git \
 			"${WORKDIR}"/libcxx '' include utils/libcxx
-		git-r3_checkout
 	fi
+	git-r3_checkout
 }
 
 multilib_src_configure() {

commit 4e1d81037b0d3417a75b1d954b4fb579c1a485ac

sys-libs/llvm-libunwind: Fix syntax error for 9999
Fixes: 14f34132fedfa241528b7699d19bebe3c98ded63
Package-Manager: Portage-2.3.24, Repoman-2.3.6

sys-libs/llvm-libunwind/llvm-libunwind-9999.ebuild
@@ -42,7 +42,7 @@ pkg_setup() {
 
 src_unpack() {
 	# we need headers & test utilities
-	if use test
+	if use test; then
 		git-r3_fetch "https://git.llvm.org/git/libcxx.git
 			https://github.com/llvm-mirror/libcxx.git"
 	fi