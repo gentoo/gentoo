gentoo/gentoo

commit ade5b255f1e8e69db29967e99da4282624334970

sys-libs/compiler-rt: fix eclass use by LLVM_SLOT -> LLVM_MAX_SLOT

sys-libs/compiler-rt/compiler-rt-4.0.1.ebuild
@@ -20,13 +20,13 @@ SLOT="${PV%_*}"
 KEYWORDS="amd64 ~arm64 x86"
 IUSE="+clang test"
 
-LLVM_SLOT=${SLOT%%.*}
+LLVM_MAX_SLOT=${SLOT%%.*}
 RDEPEND="!=sys-libs/compiler-rt-${SLOT}*:0"
 # llvm-4 needed for --cmakedir
 DEPEND="
 	>=sys-devel/llvm-4
 	clang? ( sys-devel/clang )
-	test? ( =sys-devel/clang-${PV%_*}*:${LLVM_SLOT} )
+	test? ( =sys-devel/clang-${PV%_*}*:${LLVM_MAX_SLOT} )
 	${PYTHON_DEPS}"
 
 S=${WORKDIR}/${P/_/}.src
@@ -88,9 +88,9 @@ src_test() {
 	# prepare a test compiler
 	# copy clang over since resource_dir is located relatively to binary
 	# therefore, we can put our new libraries in it
-	mkdir -p "${BUILD_DIR}"/lib/{llvm/${LLVM_SLOT}{/bin,$(get_libdir)},clang/${SLOT}/include} || die
-	cp "${EPREFIX}"/usr/lib/llvm/${LLVM_SLOT}/bin/clang{,++} \
-		"${BUILD_DIR}"/lib/llvm/${LLVM_SLOT}/bin/ || die
+	mkdir -p "${BUILD_DIR}"/lib/{llvm/${LLVM_MAX_SLOT}{/bin,$(get_libdir)},clang/${SLOT}/include} || die
+	cp "${EPREFIX}"/usr/lib/llvm/${LLVM_MAX_SLOT}/bin/clang{,++} \
+		"${BUILD_DIR}"/lib/llvm/${LLVM_MAX_SLOT}/bin/ || die
 	cp "${EPREFIX}/usr/lib/clang/${SLOT}/include"/*.h \
 		"${BUILD_DIR}/lib/clang/${SLOT}/include/" || die
 
@@ -128,7 +128,7 @@ src_test() {
 		einfo "Running tests for ABI=${ABI}"
 		# use -k to run all tests even if some fail
 		emake -k \
-			CC="${BUILD_DIR}/lib/llvm/${LLVM_SLOT}/bin/clang" \
+			CC="${BUILD_DIR}/lib/llvm/${LLVM_MAX_SLOT}/bin/clang" \
 			CFLAGS="$(get_abi_CFLAGS)" \
 			CPPFLAGS='-I../../../lib/builtins' \
 			LDFLAGS='-rtlib=compiler-rt' \

sys-libs/compiler-rt/compiler-rt-5.0.1.ebuild
@@ -19,14 +19,14 @@ SLOT="${PV%_*}"
 KEYWORDS="amd64 ~arm64 x86 ~amd64-linux ~ppc-macos ~x64-macos ~x86-macos"
 IUSE="+clang test"
 
-LLVM_SLOT=${SLOT%%.*}
+LLVM_MAX_SLOT=${SLOT%%.*}
 # llvm-4 needed for --cmakedir
 DEPEND="
 	>=sys-devel/llvm-4
 	clang? ( sys-devel/clang )
 	test? (
 		$(python_gen_any_dep "~dev-python/lit-${PV}[\${PYTHON_USEDEP}]")
-		=sys-devel/clang-${PV%_*}*:${LLVM_SLOT} )
+		=sys-devel/clang-${PV%_*}*:${LLVM_MAX_SLOT} )
 	${PYTHON_DEPS}"
 
 S=${WORKDIR}/${P/_/}.src
@@ -88,8 +88,8 @@ src_configure() {
 		mycmakeargs+=(
 			-DLIT_COMMAND="${EPREFIX}/usr/bin/lit"
 
-			-DCOMPILER_RT_TEST_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_SLOT}/bin/clang"
-			-DCOMPILER_RT_TEST_CXX_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_SLOT}/bin/clang++"
+			-DCOMPILER_RT_TEST_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_MAX_SLOT}/bin/clang"
+			-DCOMPILER_RT_TEST_CXX_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_MAX_SLOT}/bin/clang++"
 		)
 	fi
 

sys-libs/compiler-rt/compiler-rt-6.0.0.ebuild
@@ -21,14 +21,14 @@ KEYWORDS="~amd64 ~arm64 ~x86 ~amd64-fbsd ~amd64-linux ~ppc-macos ~x64-macos ~x86
 IUSE="+clang test"
 RESTRICT="!test? ( test ) !clang? ( test )"
 
-LLVM_SLOT=${SLOT%%.*}
+LLVM_MAX_SLOT=${SLOT%%.*}
 # llvm-6 for new lit options
 DEPEND="
 	>=sys-devel/llvm-6
 	clang? ( sys-devel/clang )
 	test? (
 		$(python_gen_any_dep "~dev-python/lit-${PV}[\${PYTHON_USEDEP}]")
-		=sys-devel/clang-${PV%_*}*:${LLVM_SLOT} )
+		=sys-devel/clang-${PV%_*}*:${LLVM_MAX_SLOT} )
 	${PYTHON_DEPS}"
 
 S=${WORKDIR}/${P/_/}.src
@@ -93,8 +93,8 @@ src_configure() {
 			-DLLVM_EXTERNAL_LIT="${EPREFIX}/usr/bin/lit"
 			-DLLVM_LIT_ARGS="-vv;-j;${LIT_JOBS:-$(makeopts_jobs "${MAKEOPTS}" "$(get_nproc)")}"
 
-			-DCOMPILER_RT_TEST_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_SLOT}/bin/clang"
-			-DCOMPILER_RT_TEST_CXX_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_SLOT}/bin/clang++"
+			-DCOMPILER_RT_TEST_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_MAX_SLOT}/bin/clang"
+			-DCOMPILER_RT_TEST_CXX_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_MAX_SLOT}/bin/clang++"
 		)
 	fi
 

sys-libs/compiler-rt/compiler-rt-6.0.9999.ebuild
@@ -25,14 +25,14 @@ KEYWORDS=""
 IUSE="+clang test"
 RESTRICT="!test? ( test ) !clang? ( test )"
 
-LLVM_SLOT=${SLOT%%.*}
+LLVM_MAX_SLOT=${SLOT%%.*}
 # llvm-6 for new lit options
 DEPEND="
 	>=sys-devel/llvm-6
 	clang? ( sys-devel/clang )
 	test? (
 		$(python_gen_any_dep "~dev-python/lit-${PV}[\${PYTHON_USEDEP}]")
-		=sys-devel/clang-${PV%_*}*:${LLVM_SLOT} )
+		=sys-devel/clang-${PV%_*}*:${LLVM_MAX_SLOT} )
 	${PYTHON_DEPS}"
 
 # least intrusive of all
@@ -95,8 +95,8 @@ src_configure() {
 			-DLLVM_EXTERNAL_LIT="${EPREFIX}/usr/bin/lit"
 			-DLLVM_LIT_ARGS="-vv;-j;${LIT_JOBS:-$(makeopts_jobs "${MAKEOPTS}" "$(get_nproc)")}"
 
-			-DCOMPILER_RT_TEST_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_SLOT}/bin/clang"
-			-DCOMPILER_RT_TEST_CXX_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_SLOT}/bin/clang++"
+			-DCOMPILER_RT_TEST_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_MAX_SLOT}/bin/clang"
+			-DCOMPILER_RT_TEST_CXX_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_MAX_SLOT}/bin/clang++"
 		)
 	fi
 

sys-libs/compiler-rt/compiler-rt-9999.ebuild
@@ -24,14 +24,14 @@ KEYWORDS=""
 IUSE="+clang test"
 RESTRICT="!test? ( test ) !clang? ( test )"
 
-LLVM_SLOT=${SLOT%%.*}
+LLVM_MAX_SLOT=${SLOT%%.*}
 # llvm-6 for new lit options
 DEPEND="
 	>=sys-devel/llvm-6
 	clang? ( sys-devel/clang )
 	test? (
 		$(python_gen_any_dep "~dev-python/lit-${PV}[\${PYTHON_USEDEP}]")
-		=sys-devel/clang-${PV%_*}*:${LLVM_SLOT} )
+		=sys-devel/clang-${PV%_*}*:${LLVM_MAX_SLOT} )
 	${PYTHON_DEPS}"
 
 # least intrusive of all
@@ -94,8 +94,8 @@ src_configure() {
 			-DLLVM_EXTERNAL_LIT="${EPREFIX}/usr/bin/lit"
 			-DLLVM_LIT_ARGS="-vv;-j;${LIT_JOBS:-$(makeopts_jobs "${MAKEOPTS}" "$(get_nproc)")}"
 
-			-DCOMPILER_RT_TEST_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_SLOT}/bin/clang"
-			-DCOMPILER_RT_TEST_CXX_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_SLOT}/bin/clang++"
+			-DCOMPILER_RT_TEST_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_MAX_SLOT}/bin/clang"
+			-DCOMPILER_RT_TEST_CXX_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_MAX_SLOT}/bin/clang++"
 		)
 	fi
 

commit 682b9c2a05fd86ba79a916d151c3029ecb779d92

sys-libs/compiler-rt: Fix LLVM_MAX_SLOT -> CLANG_SLOT
The variable was intentionally chosen not to use the eclass, and its
my mistake of conflating the two. Name it CLANG_SLOT to make it look
more purposeful.

Closes: https://bugs.gentoo.org/650750
Fixes: ade5b255f1e8 (sys-libs/compiler-rt: fix eclass use by LLVM_SLOT...)

sys-libs/compiler-rt/compiler-rt-4.0.1.ebuild
@@ -20,13 +20,13 @@ SLOT="${PV%_*}"
 KEYWORDS="amd64 ~arm64 x86"
 IUSE="+clang test"
 
-LLVM_MAX_SLOT=${SLOT%%.*}
+CLANG_SLOT=${SLOT%%.*}
 RDEPEND="!=sys-libs/compiler-rt-${SLOT}*:0"
 # llvm-4 needed for --cmakedir
 DEPEND="
 	>=sys-devel/llvm-4
 	clang? ( sys-devel/clang )
-	test? ( =sys-devel/clang-${PV%_*}*:${LLVM_MAX_SLOT} )
+	test? ( =sys-devel/clang-${PV%_*}*:${CLANG_SLOT} )
 	${PYTHON_DEPS}"
 
 S=${WORKDIR}/${P/_/}.src
@@ -88,9 +88,9 @@ src_test() {
 	# prepare a test compiler
 	# copy clang over since resource_dir is located relatively to binary
 	# therefore, we can put our new libraries in it
-	mkdir -p "${BUILD_DIR}"/lib/{llvm/${LLVM_MAX_SLOT}{/bin,$(get_libdir)},clang/${SLOT}/include} || die
-	cp "${EPREFIX}"/usr/lib/llvm/${LLVM_MAX_SLOT}/bin/clang{,++} \
-		"${BUILD_DIR}"/lib/llvm/${LLVM_MAX_SLOT}/bin/ || die
+	mkdir -p "${BUILD_DIR}"/lib/{llvm/${CLANG_SLOT}{/bin,$(get_libdir)},clang/${SLOT}/include} || die
+	cp "${EPREFIX}"/usr/lib/llvm/${CLANG_SLOT}/bin/clang{,++} \
+		"${BUILD_DIR}"/lib/llvm/${CLANG_SLOT}/bin/ || die
 	cp "${EPREFIX}/usr/lib/clang/${SLOT}/include"/*.h \
 		"${BUILD_DIR}/lib/clang/${SLOT}/include/" || die
 
@@ -128,7 +128,7 @@ src_test() {
 		einfo "Running tests for ABI=${ABI}"
 		# use -k to run all tests even if some fail
 		emake -k \
-			CC="${BUILD_DIR}/lib/llvm/${LLVM_MAX_SLOT}/bin/clang" \
+			CC="${BUILD_DIR}/lib/llvm/${CLANG_SLOT}/bin/clang" \
 			CFLAGS="$(get_abi_CFLAGS)" \
 			CPPFLAGS='-I../../../lib/builtins' \
 			LDFLAGS='-rtlib=compiler-rt' \

sys-libs/compiler-rt/compiler-rt-5.0.1.ebuild
@@ -19,14 +19,14 @@ SLOT="${PV%_*}"
 KEYWORDS="amd64 ~arm64 x86 ~amd64-linux ~ppc-macos ~x64-macos ~x86-macos"
 IUSE="+clang test"
 
-LLVM_MAX_SLOT=${SLOT%%.*}
+CLANG_SLOT=${SLOT%%.*}
 # llvm-4 needed for --cmakedir
 DEPEND="
 	>=sys-devel/llvm-4
 	clang? ( sys-devel/clang )
 	test? (
 		$(python_gen_any_dep "dev-python/lit[\${PYTHON_USEDEP}]")
-		=sys-devel/clang-${PV%_*}*:${LLVM_MAX_SLOT} )
+		=sys-devel/clang-${PV%_*}*:${CLANG_SLOT} )
 	${PYTHON_DEPS}"
 
 S=${WORKDIR}/${P/_/}.src
@@ -88,8 +88,8 @@ src_configure() {
 		mycmakeargs+=(
 			-DLIT_COMMAND="${EPREFIX}/usr/bin/lit"
 
-			-DCOMPILER_RT_TEST_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_MAX_SLOT}/bin/clang"
-			-DCOMPILER_RT_TEST_CXX_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_MAX_SLOT}/bin/clang++"
+			-DCOMPILER_RT_TEST_COMPILER="${EPREFIX}/usr/lib/llvm/${CLANG_SLOT}/bin/clang"
+			-DCOMPILER_RT_TEST_CXX_COMPILER="${EPREFIX}/usr/lib/llvm/${CLANG_SLOT}/bin/clang++"
 		)
 	fi
 

sys-libs/compiler-rt/compiler-rt-6.0.0.ebuild
@@ -21,14 +21,14 @@ KEYWORDS="~amd64 ~arm64 ~x86 ~amd64-fbsd ~amd64-linux ~ppc-macos ~x64-macos ~x86
 IUSE="+clang test"
 RESTRICT="!test? ( test ) !clang? ( test )"
 
-LLVM_MAX_SLOT=${SLOT%%.*}
+CLANG_SLOT=${SLOT%%.*}
 # llvm-6 for new lit options
 DEPEND="
 	>=sys-devel/llvm-6
 	clang? ( sys-devel/clang )
 	test? (
 		$(python_gen_any_dep "dev-python/lit[\${PYTHON_USEDEP}]")
-		=sys-devel/clang-${PV%_*}*:${LLVM_MAX_SLOT} )
+		=sys-devel/clang-${PV%_*}*:${CLANG_SLOT} )
 	${PYTHON_DEPS}"
 
 S=${WORKDIR}/${P/_/}.src
@@ -93,8 +93,8 @@ src_configure() {
 			-DLLVM_EXTERNAL_LIT="${EPREFIX}/usr/bin/lit"
 			-DLLVM_LIT_ARGS="-vv;-j;${LIT_JOBS:-$(makeopts_jobs "${MAKEOPTS}" "$(get_nproc)")}"
 
-			-DCOMPILER_RT_TEST_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_MAX_SLOT}/bin/clang"
-			-DCOMPILER_RT_TEST_CXX_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_MAX_SLOT}/bin/clang++"
+			-DCOMPILER_RT_TEST_COMPILER="${EPREFIX}/usr/lib/llvm/${CLANG_SLOT}/bin/clang"
+			-DCOMPILER_RT_TEST_CXX_COMPILER="${EPREFIX}/usr/lib/llvm/${CLANG_SLOT}/bin/clang++"
 		)
 	fi
 

sys-libs/compiler-rt/compiler-rt-6.0.9999.ebuild
@@ -25,14 +25,14 @@ KEYWORDS=""
 IUSE="+clang test"
 RESTRICT="!test? ( test ) !clang? ( test )"
 
-LLVM_MAX_SLOT=${SLOT%%.*}
+CLANG_SLOT=${SLOT%%.*}
 # llvm-6 for new lit options
 DEPEND="
 	>=sys-devel/llvm-6
 	clang? ( sys-devel/clang )
 	test? (
 		$(python_gen_any_dep "dev-python/lit[\${PYTHON_USEDEP}]")
-		=sys-devel/clang-${PV%_*}*:${LLVM_MAX_SLOT} )
+		=sys-devel/clang-${PV%_*}*:${CLANG_SLOT} )
 	${PYTHON_DEPS}"
 
 # least intrusive of all
@@ -95,8 +95,8 @@ src_configure() {
 			-DLLVM_EXTERNAL_LIT="${EPREFIX}/usr/bin/lit"
 			-DLLVM_LIT_ARGS="-vv;-j;${LIT_JOBS:-$(makeopts_jobs "${MAKEOPTS}" "$(get_nproc)")}"
 
-			-DCOMPILER_RT_TEST_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_MAX_SLOT}/bin/clang"
-			-DCOMPILER_RT_TEST_CXX_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_MAX_SLOT}/bin/clang++"
+			-DCOMPILER_RT_TEST_COMPILER="${EPREFIX}/usr/lib/llvm/${CLANG_SLOT}/bin/clang"
+			-DCOMPILER_RT_TEST_CXX_COMPILER="${EPREFIX}/usr/lib/llvm/${CLANG_SLOT}/bin/clang++"
 		)
 	fi
 

sys-libs/compiler-rt/compiler-rt-9999.ebuild
@@ -24,14 +24,14 @@ KEYWORDS=""
 IUSE="+clang test"
 RESTRICT="!test? ( test ) !clang? ( test )"
 
-LLVM_MAX_SLOT=${SLOT%%.*}
+CLANG_SLOT=${SLOT%%.*}
 # llvm-6 for new lit options
 DEPEND="
 	>=sys-devel/llvm-6
 	clang? ( sys-devel/clang )
 	test? (
 		$(python_gen_any_dep "dev-python/lit[\${PYTHON_USEDEP}]")
-		=sys-devel/clang-${PV%_*}*:${LLVM_MAX_SLOT} )
+		=sys-devel/clang-${PV%_*}*:${CLANG_SLOT} )
 	${PYTHON_DEPS}"
 
 # least intrusive of all
@@ -94,8 +94,8 @@ src_configure() {
 			-DLLVM_EXTERNAL_LIT="${EPREFIX}/usr/bin/lit"
 			-DLLVM_LIT_ARGS="-vv;-j;${LIT_JOBS:-$(makeopts_jobs "${MAKEOPTS}" "$(get_nproc)")}"
 
-			-DCOMPILER_RT_TEST_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_MAX_SLOT}/bin/clang"
-			-DCOMPILER_RT_TEST_CXX_COMPILER="${EPREFIX}/usr/lib/llvm/${LLVM_MAX_SLOT}/bin/clang++"
+			-DCOMPILER_RT_TEST_COMPILER="${EPREFIX}/usr/lib/llvm/${CLANG_SLOT}/bin/clang"
+			-DCOMPILER_RT_TEST_CXX_COMPILER="${EPREFIX}/usr/lib/llvm/${CLANG_SLOT}/bin/clang++"
 		)
 	fi
 