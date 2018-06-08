gentoo/gentoo

commit ed1d845779b056dd89930a60f0b522b512b60729

app-crypt/efitools: support "static" USE flag
If "static" USE flag is set, it makes sense to do static link for
user-space executables.

app-crypt/efitools/efitools-1.8.1.ebuild
@@ -12,15 +12,21 @@ SRC_URI="https://git.kernel.org/pub/scm/linux/kernel/git/jejb/efitools.git/snaps
 LICENSE="GPL-2 LGPL-2.1"
 SLOT="0"
 KEYWORDS="~amd64 ~arm64 ~x86"
-IUSE="libressl"
+IUSE="libressl static"
 
-RDEPEND="!libressl? ( dev-libs/openssl:0= )
-	libressl? ( dev-libs/libressl:0= )
+LIB_DEPEND="!libressl? ( dev-libs/openssl:0=[static-libs(+)] )
+	libressl? ( dev-libs/libressl:0=[static-libs(+)] )"
+
+RDEPEND="!static? ( ${LIB_DEPEND//\[static-libs(+)]} )
 	sys-apps/util-linux"
 
 DEPEND="${RDEPEND}
 	app-crypt/sbsigntool
 	dev-perl/File-Slurp
+	static? (
+		${LIB_DEPEND}
+		dev-util/pkgconfig
+	)
 	sys-apps/help2man
 	sys-boot/gnu-efi
 	virtual/pkgconfig"
@@ -31,6 +37,11 @@ PATCHES=(
 )
 
 src_prepare() {
+	if use static; then
+		append-ldflags -static
+		sed -i 's/-lcrypto/`pkg-config --static --libs libcrypto`/g' Makefile || die
+	fi
+
 	# Respect users CFLAGS
 	sed -i -e 's/CFLAGS.*= -O2 -g/CFLAGS += /' Make.rules || die
 

commit d8e208a3705a938ef2e81de5ad493f6109678914

app-crypt/efitools: fix dependency and use toolchain-funcs
This change fixes the issues addressed during the code review at
https://github.com/gentoo/gentoo/pull/8648 .

Fixes: ed1d845779b0 ("app-crypt/efitools: support "static" USE flag")

app-crypt/efitools/efitools-1.8.1.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=6
 
-inherit flag-o-matic
+inherit flag-o-matic toolchain-funcs
 
 DESCRIPTION="Tools for manipulating UEFI secure boot platforms"
 HOMEPAGE="https://git.kernel.org/cgit/linux/kernel/git/jejb/efitools.git"
@@ -23,10 +23,7 @@ RDEPEND="!static? ( ${LIB_DEPEND//\[static-libs(+)]} )
 DEPEND="${RDEPEND}
 	app-crypt/sbsigntool
 	dev-perl/File-Slurp
-	static? (
-		${LIB_DEPEND}
-		dev-util/pkgconfig
-	)
+	static? ( ${LIB_DEPEND} )
 	sys-apps/help2man
 	sys-boot/gnu-efi
 	virtual/pkgconfig"
@@ -39,7 +36,8 @@ PATCHES=(
 src_prepare() {
 	if use static; then
 		append-ldflags -static
-		sed -i 's/-lcrypto/`pkg-config --static --libs libcrypto`/g' Makefile || die
+		sed -i "s/-lcrypto\b/$($(tc-getPKG_CONFIG) --static --libs libcrypto)/g" \
+			Makefile || die
 	fi
 
 	# Respect users CFLAGS