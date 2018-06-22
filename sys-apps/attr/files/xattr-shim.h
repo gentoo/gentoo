ef6b72f4e4bcaa783ed748de0aff02a034c4b6c8
Lars Wendler <polynomial-c@gentoo.org>
2018-06-22 15:37:06 +0200

    sys-apps/attr: Revbump to also define ENOATTR in xattr.h shim file.
    Bug: https://bugs.gentoo.org/658744
    Package-Manager: Portage-2.3.40, Repoman-2.3.9

--- sys-apps/attr/files/xattr-shim.h
+++ sys-apps/attr/files/xattr-shim.h
@@ -3,4 +3,7 @@
 #define __XATTR_H__
 #include <sys/xattr.h>
 #warning "Please change your <attr/xattr.h> includes to <sys/xattr.h>"
+# ifndef ENOATTR
+#  define ENOATTR ENODATA
+# endif /* ENOATTR */
 #endif
