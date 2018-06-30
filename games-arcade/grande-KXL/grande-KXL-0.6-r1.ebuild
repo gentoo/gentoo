gentoo/gentoo

commit b4a70d4715f763e2a3296d28ec7c0367214965e6

games-arcade/grande-KXL: Stop using games.eclass
Package-Manager: Portage-2.3.28, Repoman-2.3.9

games-arcade/grande-KXL/grande-KXL-0.6-r1.ebuild
@@ -1,0 +1,34 @@
+# Copyright 1999-2018 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=6
+inherit autotools desktop
+
+DESCRIPTION="ZANAC type game"
+HOMEPAGE="http://triring.net/ps2linux/games/kxl/kxlgames.html"
+SRC_URI="mirror://gentoo/${P}.tar.gz"
+
+LICENSE="GPL-2"
+SLOT="0"
+KEYWORDS="~amd64 ~x86"
+IUSE=""
+
+DEPEND="dev-games/KXL"
+RDEPEND="${DEPEND}
+	media-fonts/font-adobe-100dpi
+"
+
+src_prepare() {
+	default
+	eapply "${FILESDIR}"/${P}-{configure.in,cflags}.patch
+	mv configure.{in,ac}
+	rm aclocal.m4
+	eautoreconf
+}
+
+src_install() {
+	dodir /var/lib
+	default
+	newicon src/bmp/boss1.bmp ${PN}.bmp
+	make_desktop_entry grande Grande /usr/share/pixmaps/${PN}.bmp
+}

commit 1c1cdc8aacaf2fd38d081238b87442247fe99e4e

games-arcade/grande-KXL: Fix botched games.eclass conversion
* The games.eclass conversion was broken from the start
  in commit b4a70d4715f7.

Closes: https://bugs.gentoo.org/653402
Package-Manager: Portage-2.3.41, Repoman-2.3.9

games-arcade/grande-KXL/files/grande-KXL-0.6-cflags.patch
@@ -4,8 +4,8 @@ Subject: build system
 
 respect CFLAGS
 
---- src/Makefile.am
-+++ src/Makefile.am
+--- a/src/Makefile.am
++++ b/src/Makefile.am
 @@ -8,7 +8,7 @@
  
  SUBDIRS = bmp wav stg

games-arcade/grande-KXL/files/grande-KXL-0.6-configure.in.patch
@@ -1,5 +1,5 @@
---- configure.in
-+++ configure.in
+--- a/configure.in
++++ b/configure.in
 @@ -17,12 +17,8 @@
  AC_CHECK_HEADERS(unistd.h)
  

games-arcade/grande-KXL/grande-KXL-0.6-r1.ebuild
@@ -2,6 +2,7 @@
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=6
+
 inherit autotools desktop
 
 DESCRIPTION="ZANAC type game"
@@ -15,20 +16,25 @@ IUSE=""
 
 DEPEND="dev-games/KXL"
 RDEPEND="${DEPEND}
-	media-fonts/font-adobe-100dpi
-"
+	media-fonts/font-adobe-100dpi"
+
+PATCHES=(
+	"${FILESDIR}"/${P}-configure.in.patch
+	"${FILESDIR}"/${P}-cflags.patch
+)
 
 src_prepare() {
 	default
-	eapply "${FILESDIR}"/${P}-{configure.in,cflags}.patch
-	mv configure.{in,ac}
-	rm aclocal.m4
+
+	mv configure.{in,ac} || die
+	rm aclocal.m4 || die
 	eautoreconf
 }
 
 src_install() {
 	dodir /var/lib
 	default
+
 	newicon src/bmp/boss1.bmp ${PN}.bmp
 	make_desktop_entry grande Grande /usr/share/pixmaps/${PN}.bmp
 }