gentoo/gentoo

commit b913bf8ca79ad11cade01da8b51440008f1c482b

net-libs/loudmouth: drop unused doc building deps
Unconditional dependency on dev-util/gtk-doc{,-am} was declared, but the
documentation building was not implemented.

Package-Manager: Portage-2.3.40, Repoman-2.3.9

net-libs/loudmouth/loudmouth-1.5.3-r1.ebuild
@@ -27,8 +27,6 @@ RDEPEND="
 DEPEND="${RDEPEND}
 	test? ( dev-libs/check )
 	virtual/pkgconfig
-	>=dev-util/gtk-doc-1
-	>=dev-util/gtk-doc-am-1
 "
 
 PATCHES=( "${FILESDIR}"/${P}-gcc7.patch )

commit 9c7bcd7457aa4e2808d90810f5646a7b5eebdc03

net-libs/loudmouth: properly get rid of gtk-doc dep
Terribly sorry for not testing my changes thoroughly.

Fixes: b913bf8ca79a ("net-libs/loudmouth: drop unused doc building deps")
Package-Manager: Portage-2.3.40, Repoman-2.3.9

net-libs/loudmouth/files/loudmouth-1.5.3-skip-gtk-doc.patch
@@ -1,0 +1,12 @@
+--- a/docs/reference/Makefile.am	2018-08-22 18:11:17.138111534 +0100
++++ b/docs/reference/Makefile.am	2018-08-22 18:11:23.105118175 +0100
+@@ -80,9 +80,6 @@
+ 	$(top_builddir)/loudmouth/libloudmouth.la
+ 
+ 
+-# This includes the standard gtk-doc make rules, copied by gtkdocize.
+-include $(top_srcdir)/gtk-doc.make
+-
+ # Other files to distribute
+ # e.g. EXTRA_DIST += version.xml.in
+ EXTRA_DIST +=

net-libs/loudmouth/loudmouth-1.5.3-r1.ebuild
@@ -29,7 +29,10 @@ DEPEND="${RDEPEND}
 	virtual/pkgconfig
 "
 
-PATCHES=( "${FILESDIR}"/${P}-gcc7.patch )
+PATCHES=(
+	"${FILESDIR}"/${P}-gcc7.patch
+	"${FILESDIR}"/${P}-skip-gtk-doc.patch
+)
 
 src_prepare() {
 	default