gentoo/gentoo

commit 224e7a63d6f24664abfae1de60d81a9e3631e987

gnome-extra/synapse: drop optional zeitgeist support
Package-Manager: Portage-2.3.24, Repoman-2.3.6

gnome-extra/synapse/synapse-0.2.99.2.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2017 Gentoo Foundation
+# Copyright 1999-2018 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=5
@@ -16,7 +16,7 @@ LICENSE="GPL-3"
 SLOT="0"
 KEYWORDS="amd64 x86"
 # "ayatana" support pending on GTK+-3.x version of synapse wrt #411613
-IUSE="plugins +zeitgeist"
+IUSE="plugins"
 
 RDEPEND="
 	dev-libs/libgee:0.8
@@ -32,11 +32,8 @@ RDEPEND="
 	x11-libs/libnotify
 	x11-libs/pango
 	x11-themes/adwaita-icon-theme
-	plugins? ( >=net-libs/rest-0.7 )
-	zeitgeist? (
-		dev-libs/libzeitgeist
-		>=gnome-extra/zeitgeist-0.9.14[fts]
-		)"
+	plugins? ( >=net-libs/rest-0.7 )"
+
 	#ayatana? ( dev-libs/libappindicator )
 DEPEND="${RDEPEND}
 	$(vala_depend)
@@ -53,7 +50,6 @@ src_configure() {
 	local myeconfargs=(
 		--enable-indicator=no
 		$(use_enable plugins librest yes)
-		$(use_enable zeitgeist)
 		)
 	autotools-utils_src_configure
 }

gnome-extra/synapse/metadata.xml
@@ -7,7 +7,6 @@
   </maintainer>
   <use>
     <flag name="plugins">Build support for plugins using librest</flag>
-    <flag name="zeitgeist">Add support for Zeitgeist event tracking system</flag>
   </use>
   <upstream>
     <remote-id type="launchpad">synapse-project</remote-id>

commit 61045627abfe9cf8dde61690de28f01288960bb8

gnome-extra/synapse: Set --disable-zeitgeist
Fixes: https://bugs.gentoo.org/660286
Fixes: 224e7a63d6f24664abfae1de60d81a9e3631e987
Package-Manager: Portage-2.3.51, Repoman-2.3.11
Signed-off-by: Jeroen Roovers <jer@gentoo.org>

gnome-extra/synapse/synapse-0.2.99.2.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2018 Gentoo Foundation
+# Copyright 1999-2018 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=5
@@ -48,8 +48,9 @@ src_prepare() {
 
 src_configure() {
 	local myeconfargs=(
-		--enable-indicator=no
 		$(use_enable plugins librest yes)
+		--disable-zeitgeist
+		--enable-indicator=no
 		)
 	autotools-utils_src_configure
 }