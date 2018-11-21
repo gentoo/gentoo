gentoo/gentoo

commit 64bc534bf4412e0088839c628ff79fa456ae60bb

net-libs/nodejs: adjust dev-lib/openssl requirement
- We are patching dev-libs/openssl to ship some EC curves even with
  dev-libs/openssl[bindist] so it is no longer required to enforce
  dev-libs/openssl[-bindist].

- NodeJS >=10 requires OpenSSL 1.1 or newer.

Closes: https://bugs.gentoo.org/670574
Package-Manager: Portage-2.3.51, Repoman-2.3.12
Signed-off-by: Thomas Deutschmann <whissi@gentoo.org>

net-libs/nodejs/nodejs-10.13.0.ebuild
@@ -29,7 +29,7 @@ RDEPEND="
 	>=net-libs/nghttp2-1.34.0
 	sys-libs/zlib
 	icu? ( >=dev-libs/icu-62.1:= )
-	ssl? ( =dev-libs/openssl-1.1.0*:0=[-bindist] )
+	ssl? ( >=dev-libs/openssl-1.1:0= )
 "
 DEPEND="
 	${RDEPEND}

net-libs/nodejs/nodejs-11.2.0.ebuild
@@ -28,7 +28,7 @@ RDEPEND="
 	>=net-libs/nghttp2-1.34.0
 	sys-libs/zlib
 	icu? ( >=dev-libs/icu-63.1:= )
-	ssl? ( =dev-libs/openssl-1.1.0*:0=[-bindist] )
+	ssl? ( >=dev-libs/openssl-1.1:0= )
 "
 DEPEND="
 	${RDEPEND}

net-libs/nodejs/nodejs-99999999.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2018 Gentoo Foundation
+# Copyright 1999-2018 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=6
@@ -28,7 +28,7 @@ RDEPEND="
 	sys-libs/zlib
 	icu? ( >=dev-libs/icu-61.1:= )
 	npm? ( ${PYTHON_DEPS} )
-	ssl? ( >=dev-libs/openssl-1.0.2g:0=[-bindist] )
+	ssl? ( >=dev-libs/openssl-1.1:0= )
 "
 DEPEND="
 	${RDEPEND}

commit 7aff0f0339d3bdeb6fc22975d54f960a62be7528

net-libs/nodejs: Revert dev-libs/openssl-1.1* dep change
With openssl-1.1.1a I see lots and lots of test failures that I do not see with
1.1.0* -- these reflect actual errors in using crypto functions.

While there, resolve an implicit dependency on net-dns/c-ares that was
missed in the 8, 9, 10 and 11 branch version bumps.

Fixes: 64bc534bf4412e0088839c628ff79fa456ae60bb
Bug: https://bugs.gentoo.org/670574
Package-Manager: Portage-2.3.52, Repoman-2.3.12
Signed-off-by: Jeroen Roovers <jer@gentoo.org>

net-libs/nodejs/nodejs-10.13.0.ebuild
@@ -29,7 +29,7 @@ RDEPEND="
 	>=net-libs/nghttp2-1.34.0
 	sys-libs/zlib
 	icu? ( >=dev-libs/icu-62.1:= )
-	ssl? ( >=dev-libs/openssl-1.1:0= )
+	ssl? ( =dev-libs/openssl-1.1.0*:0= )
 "
 DEPEND="
 	${RDEPEND}

net-libs/nodejs/nodejs-11.2.0.ebuild
@@ -24,11 +24,12 @@ REQUIRED_USE="
 
 RDEPEND="
 	>=dev-libs/libuv-1.23.2:=
+	>=net-dns/c-ares-1.15.0
 	>=net-libs/http-parser-2.8.0:=
 	>=net-libs/nghttp2-1.34.0
 	sys-libs/zlib
 	icu? ( >=dev-libs/icu-63.1:= )
-	ssl? ( >=dev-libs/openssl-1.1:0= )
+	ssl? ( =dev-libs/openssl-1.1.0*:0= )
 "
 DEPEND="
 	${RDEPEND}
@@ -92,7 +93,7 @@ src_prepare() {
 }
 
 src_configure() {
-	local myconf=( --shared-http-parser --shared-libuv --shared-nghttp2 --shared-zlib )
+	local myconf=( --shared-cares --shared-http-parser --shared-libuv --shared-nghttp2 --shared-zlib )
 	use debug && myconf+=( --debug )
 	use icu && myconf+=( --with-intl=system-icu ) || myconf+=( --with-intl=none )
 	use inspector || myconf+=( --without-inspector )

net-libs/nodejs/nodejs-8.13.0.ebuild
@@ -25,6 +25,7 @@ REQUIRED_USE="
 
 RDEPEND="
 	>=dev-libs/libuv-1.23.2:=
+	>=net-dns/c-ares-1.10.1
 	>=net-libs/http-parser-2.8.0:=
 	>=net-libs/nghttp2-1.33.0
 	sys-libs/zlib
@@ -97,7 +98,7 @@ src_prepare() {
 }
 
 src_configure() {
-	local myconf=( --shared-http-parser --shared-libuv --shared-nghttp2 --shared-zlib )
+	local myconf=( --shared-cares --shared-http-parser --shared-libuv --shared-nghttp2 --shared-zlib )
 	use debug && myconf+=( --debug )
 	use icu && myconf+=( --with-intl=system-icu ) || myconf+=( --with-intl=none )
 	use inspector || myconf+=( --without-inspector )

net-libs/nodejs/nodejs-9.11.2.ebuild
@@ -24,6 +24,7 @@ REQUIRED_USE="
 
 RDEPEND="
 	>=dev-libs/libuv-1.19.2:=
+	>=net-dns/c-ares-1.13.0
 	>=net-libs/http-parser-2.8.0:=
 	>=net-libs/nghttp2-1.32.0
 	sys-libs/zlib
@@ -92,7 +93,7 @@ src_prepare() {
 }
 
 src_configure() {
-	local myconf=( --shared-http-parser --shared-libuv --shared-nghttp2 --shared-zlib )
+	local myconf=( --shared-cares --shared-http-parser --shared-libuv --shared-nghttp2 --shared-zlib )
 	use debug && myconf+=( --debug )
 	use icu && myconf+=( --with-intl=system-icu ) || myconf+=( --with-intl=none )
 	use inspector || myconf+=( --without-inspector )

net-libs/nodejs/nodejs-99999999.ebuild
@@ -23,6 +23,7 @@ REQUIRED_USE="
 
 RDEPEND="
 	>=dev-libs/libuv-1.19.2:=
+	>=net-dns/c-ares-1.15.0
 	>=net-libs/http-parser-2.8.0:=
 	>=net-libs/nghttp2-1.29.0
 	sys-libs/zlib
@@ -91,7 +92,7 @@ src_prepare() {
 }
 
 src_configure() {
-	local myconf=( --shared-http-parser --shared-libuv --shared-nghttp2 --shared-zlib )
+	local myconf=( --shared-cares --shared-http-parser --shared-libuv --shared-nghttp2 --shared-zlib )
 	use debug && myconf+=( --debug )
 	use icu && myconf+=( --with-intl=system-icu ) || myconf+=( --with-intl=none )
 	use inspector || myconf+=( --without-inspector )