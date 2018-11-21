gentoo/gentoo

commit c75ad8a62ae9e5ffd404add7b201e0a7b745be6e

www-client/vivaldi: bump to v2.1.1337.47
- EAPI bumped to EAPI=7

Package-Manager: Portage-2.3.51, Repoman-2.3.12
RepoMan-Options: --force
Signed-off-by: Thomas Deutschmann <whissi@gentoo.org>

www-client/vivaldi/vivaldi-2.1.1337.47_p1.ebuild
@@ -1,0 +1,127 @@
+# Copyright 1999-2018 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI="6"
+CHROMIUM_LANGS="
+	am ar bg bn ca cs da de el en-GB en-US es es-419 et fa fi fil fr gu he hi
+	hr hu id it ja kn ko lt lv ml mr ms nb nl pl pt-BR pt-PT ro ru sk sl sr sv
+	sw ta te th tr uk vi zh-CN zh-TW
+"
+inherit chromium-2 gnome2-utils multilib unpacker toolchain-funcs xdg-utils
+
+#VIVALDI_BRANCH="snapshot"
+
+VIVALDI_PN="${PN}-${VIVALDI_BRANCH:-stable}"
+VIVALDI_BIN="${PN}${VIVALDI_BRANCH/snapshot/-snapshot}"
+VIVALDI_HOME="opt/${VIVALDI_BIN}"
+DESCRIPTION="A browser for our friends"
+HOMEPAGE="https://vivaldi.com/"
+VIVALDI_BASE_URI="https://downloads.${PN}.com/${VIVALDI_BRANCH:-stable}/${VIVALDI_PN}_${PV/_p/-}_"
+SRC_URI="
+	amd64? ( ${VIVALDI_BASE_URI}amd64.deb -> ${P}-amd64.deb )
+	arm? ( ${VIVALDI_BASE_URI}armhf.deb -> ${P}-armhf.deb )
+	x86? ( ${VIVALDI_BASE_URI}i386.deb -> ${P}-i386.deb )
+"
+
+LICENSE="Vivaldi"
+SLOT="0"
+KEYWORDS="-* amd64 ~arm x86"
+RESTRICT="bindist mirror"
+
+DEPEND="
+	virtual/libiconv
+"
+RDEPEND="
+	dev-libs/expat
+	dev-libs/glib:2
+	dev-libs/nspr
+	dev-libs/nss
+	media-libs/alsa-lib
+	media-libs/fontconfig
+	media-libs/freetype
+	media-libs/speex
+	net-print/cups
+	sys-apps/dbus
+	sys-libs/libcap
+	x11-libs/cairo
+	x11-libs/gdk-pixbuf
+	x11-libs/gtk+:2
+	x11-libs/libX11
+	x11-libs/libXScrnSaver
+	x11-libs/libXcomposite
+	x11-libs/libXcursor
+	x11-libs/libXdamage
+	x11-libs/libXext
+	x11-libs/libXfixes
+	x11-libs/libXi
+	x11-libs/libXrandr
+	x11-libs/libXrender
+	x11-libs/libXtst
+	x11-libs/pango[X]
+"
+QA_PREBUILT="*"
+S=${WORKDIR}
+
+src_unpack() {
+	unpack_deb ${A}
+}
+
+src_prepare() {
+	iconv -c -t UTF-8 usr/share/applications/${VIVALDI_PN}.desktop > "${T}"/${VIVALDI_PN}.desktop || die
+	mv "${T}"/${VIVALDI_PN}.desktop usr/share/applications/${VIVALDI_PN}.desktop || die
+
+	sed -i \
+		-e "s|${VIVALDI_BIN}|${PN}|g" \
+		usr/share/applications/${VIVALDI_PN}.desktop \
+		usr/share/xfce4/helpers/${VIVALDI_BIN}.desktop || die
+
+	mv usr/share/doc/${VIVALDI_PN} usr/share/doc/${PF} || die
+	chmod 0755 usr/share/doc/${PF} || die
+
+	gunzip usr/share/doc/${PF}/changelog.gz || die
+
+	rm \
+		_gpgbuilder \
+		etc/cron.daily/${VIVALDI_BIN} \
+		${VIVALDI_HOME}/libwidevinecdm.so \
+		|| die
+	rmdir \
+		etc/cron.daily/ \
+		etc/ \
+		|| die
+
+	local c d
+	for d in 16 22 24 32 48 64 128 256; do
+		mkdir -p usr/share/icons/hicolor/${d}x${d}/apps || die
+		cp \
+			${VIVALDI_HOME}/product_logo_${d}.png \
+			usr/share/icons/hicolor/${d}x${d}/apps/${PN}.png || die
+	done
+
+	pushd "${VIVALDI_HOME}/locales" > /dev/null || die
+	chromium_remove_language_paks
+	popd > /dev/null || die
+
+	eapply_user
+}
+
+src_install() {
+	mv * "${D}" || die
+	dosym /${VIVALDI_HOME}/${PN} /usr/bin/${PN}
+
+	fperms 4711 /${VIVALDI_HOME}/${PN}-sandbox
+}
+
+pkg_preinst() {
+	gnome2_icon_savelist
+}
+
+pkg_postrm() {
+	gnome2_icon_cache_update
+	xdg_desktop_database_update
+}
+
+pkg_postinst() {
+	gnome2_icon_cache_update
+	xdg_desktop_database_update
+}

www-client/vivaldi/Manifest
@@ -4,3 +4,6 @@ DIST vivaldi-2.0.1309.42_p1-i386.deb 55525322 BLAKE2B 15b93162b5880adc2159d026b0
 DIST vivaldi-2.1.1337.36_p1-amd64.deb 57337500 BLAKE2B 1a5dddb02539e797a4c78d17ff21c116bcc8106ba13514c0d0c23dbec3abdbf025bdb78a930c9c228ae6e16c01f2a9373d09a276e9f0b9ad7323278025cfac83 SHA512 9ba3f539125d1739d7af64fca2b3896f87f7b81f86b4f190c24abbc2c6fbcfb52613533b2e681ff15df7f5c0048788b21b5037c662a6cd7db98ea5b01434782b
 DIST vivaldi-2.1.1337.36_p1-armhf.deb 50280548 BLAKE2B 37a645a5593bd4ed8c866ff1acc98250ef2ed0fe0fd3a75968d3fa2ab97340b16b0f41e1d2fdbde58bf147339bf17dfc5d3d2498a74b795bc9be02a7fcd05edf SHA512 09f85f2513513c8256fc077239c169067e1617f05f7d75f9560b44e8490376a7e543b1c7098e6f3a8d3f335ab22e0dc555ac8816fc149463e1376fd88540f203
 DIST vivaldi-2.1.1337.36_p1-i386.deb 56258752 BLAKE2B 4fd0ce20a60e7bc7b00d0da138286490ef8b38e66512c4c07a27d336acf8bb31cb370771b9c1169972df32d417f6b09f939d2900ceec5c3dfa4bcef644f77cce SHA512 347ec30a39fd501f0abf1cef6cf9c8ef194c8d5fbbef6510eae33f8b85834eca01348c17ae08fb10c39f290dcf1b828b3ebf022fd9115a80825502e5f983b844
+DIST vivaldi-2.1.1337.47_p1-amd64.deb 57339998 BLAKE2B 4bbcab43d31289bda0bb5b7f15a3411495f8c887b5e9431c768aee585d58228cb6e15a3923ff7bcfa6fe488888e74eae3b24812b5097b35973b706786391f573 SHA512 d8004beea0d58cbb60aa629ab2645f0eddfc6c3e5601232f01ac5105b9121fad6d74e24fae9a4995c69720f33bef4186803b15e284fa35961f347cdb9c58ca30
+DIST vivaldi-2.1.1337.47_p1-armhf.deb 50300100 BLAKE2B 462a14cc7f01946c8fe4e68fcfae95836ec0917c84402d7d189e1a5d7c6576d45a6a49aef4ff6d0e3ccb7d7fd6333d5807ab5d38e3155c2dda2aba8ff93a765e SHA512 5653468415b95ec617b7f5e02dafc8d4d6dd88c72913a3a76054e42ef9de6f43d291aa9efa0854d067ab3e749c5dec53110f4cff9a101fe90022144af0a3de31
+DIST vivaldi-2.1.1337.47_p1-i386.deb 56268380 BLAKE2B e5f100d09166983537add232266e3415195b8370a7a73d2da1a1b9ae1c413b8ad2be35b7b69ebb5f3ca7e3236529d2a5499a713838b70c929d8b4c6f835f6a5b SHA512 5aa988df0bded02a8b864952171ae028d0d4b662e90619142915677b3058ea309e9ae8de3a0eefedea2033e9e8fb692f54b6e0d2a2f5b0ac19a7ce7904dfe615

commit c35fb074ce1f8f50721ac1257b9d73faed7534e2

www-client/vivaldi: Revert forced stabilisation
Package-Manager: Portage-2.3.52, Repoman-2.3.12
Fixes: c75ad8a62ae9e5ffd404add7b201e0a7b745be6e
Signed-off-by: Jeroen Roovers <jer@gentoo.org>

www-client/vivaldi/vivaldi-2.1.1337.47_p1.ebuild
@@ -25,7 +25,7 @@ SRC_URI="
 
 LICENSE="Vivaldi"
 SLOT="0"
-KEYWORDS="-* amd64 ~arm x86"
+KEYWORDS="-* ~amd64 ~arm ~x86"
 RESTRICT="bindist mirror"
 
 DEPEND="