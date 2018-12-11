57a1955d9599c2960db5bc5e345a29ed3aac9344
Michał Górny <mgorny@gentoo.org>
2018-12-11 14:37:49 +0100

    dev-lang/gnu-smalltalk: Remove last-rited pkg
    Closes: https://bugs.gentoo.org/645690
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- dev-lang/gnu-smalltalk/files/50gnu-smalltalk-gentoo.el
+++
@@ -1,7 +0,0 @@
-
-;;; gnu-smalltalk site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(autoload 'smalltalk-mode "smalltalk-mode" "Autoload for smalltalk-mode" t)
-(autoload 'gst "gst-mode" "Autoload for gst" t)
-(add-to-list 'auto-mode-alist '("\\.st\\'" . smalltalk-mode))
