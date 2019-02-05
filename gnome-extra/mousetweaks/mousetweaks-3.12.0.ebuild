gentoo/gentoo

commit 24a1522c7c0d28bd2d093c19c79d2fc6fb82d4ce

gnome-extra/mousetweaks: updated Homepage URL
Signed-off-by: Dennis Lamm <expeditioneer@gentoo.org>

gnome-extra/mousetweaks/mousetweaks-3.12.0.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2018 Gentoo Authors
+# Copyright 1999-2019 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI="5"
@@ -7,7 +7,7 @@ GCONF_DEBUG="no"
 inherit gnome2
 
 DESCRIPTION="Mouse accessibility enhancements for the GNOME desktop"
-HOMEPAGE="https://live.gnome.org/Mousetweaks/Home"
+HOMEPAGE="https://wiki.gnome.org/action/show/Projects/Mousetweaks"
 
 LICENSE="GPL-3"
 SLOT="0"

commit 6290b1918d09b2b2366f5acd2ec5fef3c42b9657

gnome-extra/mousetweaks: fix HOMEPAGE
Use correct homepage, not a long path where you end up only
when being redirected from old URL.

Fixes: 24a1522c7c0d28bd2d093c19c79d2fc6fb82d4ce
Package-Manager: Portage-2.3.52, Repoman-2.3.12
Signed-off-by: Mart Raudsepp <leio@gentoo.org>

gnome-extra/mousetweaks/mousetweaks-3.12.0.ebuild
@@ -7,7 +7,7 @@ GCONF_DEBUG="no"
 inherit gnome2
 
 DESCRIPTION="Mouse accessibility enhancements for the GNOME desktop"
-HOMEPAGE="https://wiki.gnome.org/action/show/Projects/Mousetweaks"
+HOMEPAGE="https://wiki.gnome.org/Projects/Mousetweaks"
 
 LICENSE="GPL-3"
 SLOT="0"