gentoo/gentoo

commit 1cd31691dafbb1f442db47873d8b6fb819146999

sys-apps/util-linux: adjust copyright
Bug: https://bugs.gentoo.org/670702
Package-Manager: Portage-2.3.67, Repoman-2.3.14
Signed-off-by: Thomas Deutschmann <whissi@gentoo.org>

sys-apps/util-linux/util-linux-2.33.2.ebuild
@@ -1,5 +1,4 @@
 # Copyright 1999-2019 Gentoo Authors and others
-# Copyright 2018 Sony Interactive Entertainment Inc.
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=6

sys-apps/util-linux/util-linux-2.34_rc2.ebuild
@@ -1,5 +1,4 @@
 # Copyright 1999-2019 Gentoo Authors and others
-# Copyright 2018 Sony Interactive Entertainment Inc.
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7

sys-apps/util-linux/util-linux-9999.ebuild
@@ -1,5 +1,4 @@
 # Copyright 1999-2019 Gentoo Authors and others
-# Copyright 2018 Sony Interactive Entertainment Inc.
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=6

commit ab14a9533c31391642433f8dfe07ab9ca66bb5ed

sys-apps/util-linux: Fix copyright line.
"And others" is already implied with Gentoo Authors.

Fixes: 1cd31691dafbb1f442db47873d8b6fb819146999
Package-Manager: Portage-2.3.67, Repoman-2.3.14
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

sys-apps/util-linux/util-linux-2.33.2.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2019 Gentoo Authors and others
+# Copyright 1999-2019 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=6

sys-apps/util-linux/util-linux-2.34_rc2.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2019 Gentoo Authors and others
+# Copyright 1999-2019 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7

sys-apps/util-linux/util-linux-9999.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2019 Gentoo Authors and others
+# Copyright 1999-2019 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=6