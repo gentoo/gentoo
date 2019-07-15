gentoo/gentoo

commit 200ce5260c7f827b5ca9e3f1faf0a692d2b573a6

app-vim/vim-tmux: keyword for arm/ia64/alpha platforms.
Signed-off-by: Patrice Clement <monsieurp@gentoo.org>
Package-Manager: Portage-2.3.51, Repoman-2.3.11
RepoMan-Options: --force

app-vim/vim-tmux/vim-tmux-3.0.0.ebuild
@@ -9,4 +9,4 @@ DESCRIPTION="vim plugin: tmux support for vim"
 HOMEPAGE="https://github.com/tmux-plugins/vim-tmux"
 SRC_URI="https://github.com/tmux-plugins/${PN}/archive/v${PV}.tar.gz -> ${P}.tar.gz"
 LICENSE="vim.org"
-KEYWORDS="~alpha amd64 ~arm ~arm64 ~hppa ~ia64 ~mips ppc ppc64 ~sparc x86 ~amd64-fbsd"
+KEYWORDS="~alpha amd64 ~arm ~arm64 ~hppa ~ia64 ~mips ppc ppc64 ~sparc x86 ~amd64-fbsd alpha ia64 arm"

commit fc3e2b24e715c39f178008a68aec511a3e1e3a58

app-vim/vim-tmux: [QA] Fix duplicate keywords
Fixes: 200ce5260c7f827b5ca9e3f1faf0a692d2b573a6
Signed-off-by: Michał Górny <mgorny@gentoo.org>

app-vim/vim-tmux/vim-tmux-3.0.0.ebuild
@@ -9,4 +9,4 @@ DESCRIPTION="vim plugin: tmux support for vim"
 HOMEPAGE="https://github.com/tmux-plugins/vim-tmux"
 SRC_URI="https://github.com/tmux-plugins/${PN}/archive/v${PV}.tar.gz -> ${P}.tar.gz"
 LICENSE="public-domain MIT"
-KEYWORDS="~alpha alpha amd64 ~arm arm ~arm64 ~hppa ia64 ~ia64 ~mips ppc ppc64 ~s390 ~sparc x86 ~amd64-fbsd"
+KEYWORDS="alpha amd64 arm ~arm64 ~hppa ia64 ~mips ppc ppc64 ~s390 ~sparc x86 ~amd64-fbsd"