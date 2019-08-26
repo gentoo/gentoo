gentoo/gentoo

commit e0e382d4f934129b82ce13cea8d20bf664801978

mail-client/trojita: Add USE addressbook
Upstream commit c3a317ac1e085535034ef862d9e9dbf4551cfb99

Package-Manager: Portage-2.3.66, Repoman-2.3.12
Signed-off-by: Andreas Sturmlechner <asturm@gentoo.org>

mail-client/trojita/trojita-9999.ebuild
@@ -17,7 +17,7 @@ HOMEPAGE="http://trojita.flaska.net/"
 
 LICENSE="|| ( GPL-2 GPL-3 )"
 SLOT="0"
-IUSE="+crypt +dbus debug +password +spell test +zlib"
+IUSE="addressbook +crypt +dbus debug +password +spell test +zlib"
 
 REQUIRED_USE="password? ( dbus )"
 
@@ -34,6 +34,7 @@ DEPEND="
 	dev-qt/qtsvg:5
 	dev-qt/qtwebkit:5
 	dev-qt/qtwidgets:5
+	addressbook? ( kde-apps/akonadi-contacts:5 )
 	crypt? (
 		>=app-crypt/gpgme-1.8.0[cxx,qt5]
 		dev-libs/mimetic
@@ -57,6 +58,8 @@ src_prepare() {
 
 src_configure() {
 	local mycmakeargs=(
+		-DWITH_ABOOKADDRESSBOOK_PLUGIN=OFF
+		-DWITH_AKONADIADDRESSBOOK_PLUGIN=$(usex addressbook)
 		-DWITH_CRYPTO_MESSAGES=$(usex crypt)
 		-DWITH_GPGMEPP=$(usex crypt)
 		-DWITH_MIMETIC=$(usex crypt)

mail-client/trojita/metadata.xml
@@ -14,6 +14,7 @@
 		<name>Gentoo KDE Project</name>
 	</maintainer>
 	<use>
+		<flag name="addressbook">Build <pkg>kde-apps/akonadi</pkg> addressbook plugin</flag>
 		<flag name="password">Store passwords securely via <pkg>dev-libs/qtkeychain</pkg></flag>
 	</use>
 	<upstream>

commit 8af623db730e032e36f23c4a607c9959c85235c1

mail-client/trojita: Unconditionally enable the abook plugin
Trojita supports different implementations of an "address book" via
plugins that are loaded at runtime. Only one plugin can be active at a
time, but any number of them can be built and installed. The user can
pick and select an appropriate plugin via a GUI configuration.

There is no need to disable the "abook" plugin, and upstream expects
this plugin to be always available. There are no external dependencies,
and the `abook` binary is not used.

Fixes: e0e382d4f9341 (mail-client/trojita: Add USE addressbook)
Closes: https://bugs.gentoo.org/692214
Signed-off-by: Jan Kundrát <jkt@kde.org>

mail-client/trojita/trojita-9999.ebuild
@@ -58,7 +58,7 @@ src_prepare() {
 
 src_configure() {
 	local mycmakeargs=(
-		-DWITH_ABOOKADDRESSBOOK_PLUGIN=OFF
+		-DWITH_ABOOKADDRESSBOOK_PLUGIN=ON
 		-DWITH_AKONADIADDRESSBOOK_PLUGIN=$(usex addressbook)
 		-DWITH_CRYPTO_MESSAGES=$(usex crypt)
 		-DWITH_GPGMEPP=$(usex crypt)