gentoo/gentoo

commit ddc9f77b3d2588c1782127b96a193eb62021ac0a

app-emacs/emacs-common-gentoo: ALLARCHES
Signed-off-by: Aaron Bauman <bman@gentoo.org>

app-emacs/emacs-common-gentoo/emacs-common-gentoo-1.6-r3.ebuild
@@ -11,7 +11,7 @@ SRC_URI="https://dev.gentoo.org/~ulm/emacs/${P}.tar.xz"
 
 LICENSE="GPL-3+"
 SLOT="0"
-KEYWORDS="~alpha amd64 ~arm ~arm64 ~hppa ia64 ~mips ppc ppc64 ~riscv ~s390 ~sh sparc x86 ~amd64-fbsd ~x86-fbsd ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~x86-macos ~sparc-solaris ~x86-solaris"
+KEYWORDS="alpha amd64 arm arm64 ~hppa ia64 ~mips ppc ppc64 riscv ~s390 ~sh sparc x86 ~amd64-fbsd ~x86-fbsd ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~x86-macos ~sparc-solaris ~x86-solaris"
 IUSE="games X"
 
 RDEPEND="games? ( acct-group/gamestat )"

commit 2926fba9be692cbf8a4ae0865d5fa852a96013db

app-emacs/emacs-common-gentoo: hppa stable per allarches policy, bug 693382.
Fixes: ddc9f77b3d2588c1782127b96a193eb62021ac0a
Package-Manager: Portage-2.3.75, Repoman-2.3.17
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

app-emacs/emacs-common-gentoo/emacs-common-gentoo-1.6-r3.ebuild
@@ -11,7 +11,7 @@ SRC_URI="https://dev.gentoo.org/~ulm/emacs/${P}.tar.xz"
 
 LICENSE="GPL-3+"
 SLOT="0"
-KEYWORDS="alpha amd64 arm arm64 ~hppa ia64 ~mips ppc ppc64 ~riscv ~s390 ~sh sparc x86 ~amd64-fbsd ~x86-fbsd ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~x86-macos ~sparc-solaris ~x86-solaris"
+KEYWORDS="alpha amd64 arm arm64 hppa ia64 ~mips ppc ppc64 ~riscv ~s390 ~sh sparc x86 ~amd64-fbsd ~x86-fbsd ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~x86-macos ~sparc-solaris ~x86-solaris"
 IUSE="games X"
 
 RDEPEND="games? ( acct-group/gamestat )"