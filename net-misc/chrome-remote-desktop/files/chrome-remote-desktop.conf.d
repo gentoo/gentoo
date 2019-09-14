61f019900fa24121bed4e2a53afdb54c28e97d72
Mike Frysinger <vapier@gentoo.org>
2019-09-14 02:10:59 -0400

    net-misc/chrome-remote-desktop: drop CHROME_REMOTE_DESKTOP_LOG_DIR setting
    Upstream no longer respects this env var and only writes logs to
    /tmp without any way to change it.  Drop the conf.d setting to
    avoid confusing people.
    
    Also clean up stale randr comments.  Latest version dynamically
    handles things now.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- net-misc/chrome-remote-desktop/files/chrome-remote-desktop.conf.d
+++ net-misc/chrome-remote-desktop/files/chrome-remote-desktop.conf.d
@@ -4,11 +4,4 @@
 CHROME_REMOTING_USERS=''
 
 # Options to pass to chrome-remote-desktop.  Only the -s option is interesting.
-# Note: In order to support resizing, you need to:
-# (1) Apply this patch to xorg-server (via epatch_user):
-#     http://patchwork.freedesktop.org/patch/51428/
-# (2) Create a symlink /usr/bin/Xvfb-randr -> Xvfb
 #OPTIONS='-s 1600x1200 -s 3840x1600'
-
-# Directory to use for storing log files.
-#CHROME_REMOTE_DESKTOP_LOG_DIR='/var/log'
