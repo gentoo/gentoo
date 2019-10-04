gentoo/gentoo

commit 59c843408f8d826c0a698bd24747deaeaad35734

net-mail/dot-forward-0.71-r3: remove usage of qmail eclass
It was only called to get the same fixed group ids as other qmail programs.
Use new acct-group dependencies instead.

Signed-off-by: Rolf Eike Beer <eike@sf-mail.de>
Signed-off-by: Joonas Niilola <juippis@gentoo.org>

net-mail/dot-forward/dot-forward-0.71-r3.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=0
 
-inherit eutils fixheadtails qmail
+inherit eutils fixheadtails
 
 DESCRIPTION="reads sendmail's .forward files under qmail"
 HOMEPAGE="http://cr.yp.to/dot-forward.html"
@@ -17,12 +17,12 @@ IUSE=""
 # See bug 97850
 RESTRICT="test"
 
-DEPEND=""
-RDEPEND=""
+DEPEND="
+	acct-group/nofiles
+	acct-group/qmail
+"
 
-pkg_setup() {
-	qmail_create_groups
-}
+RDEPEND="${DEPEND}"
 
 src_unpack() {
 	unpack ${A}

commit 61d826deb81381cdcdf32fab6e3bdfda0aeb6d75

net-mail/dot-forward: add back missing dependency
The qmail eclass is also needed for qmail_set_cc.

Broken in 59c843408f8d826c0a698bd24747deaeaad35734

Signed-off-by: Rolf Eike Beer <eike@sf-mail.de>

net-mail/dot-forward/dot-forward-0.71-r3.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=0
 
-inherit eutils fixheadtails
+inherit eutils fixheadtails qmail
 
 DESCRIPTION="reads sendmail's .forward files under qmail"
 HOMEPAGE="http://cr.yp.to/dot-forward.html"