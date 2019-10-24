gentoo/gentoo

commit 33d249d4b12616985c6b3b67d484e989827de983

sys-apps/dstat: fix src_install() for prefix
Using 'default' as done in the version bump made files get
installed outside the prefix, directly into /usr.

Package-Manager: Portage-2.3.78, Repoman-2.3.17
Signed-off-by: Guilherme Amadio <amadio@gentoo.org>

sys-apps/dstat/dstat-0.7.4.ebuild
@@ -40,7 +40,8 @@ src_prepare() {
 }
 
 src_install() {
-	default
+	emake DESTDIR="${ED}"
+	einstalldocs
 
 	if use examples; then
 		dodoc examples/{mstat,read}.py

commit c4a088199cd63467f2d0e607735fbecbec761229

sys-apps/dstat: 0.7.4: add missing install target.
Fixes broken src_install() introduced in commit
33d249d4b12616985c6b3b67d484e989827de983.

Closes: https://bugs.gentoo.org/698456
Package-Manager: Portage-2.3.78, Repoman-2.3.17
Signed-off-by: Piotr Karbowski <slashbeast@gentoo.org>

sys-apps/dstat/dstat-0.7.4.ebuild
@@ -40,7 +40,7 @@ src_prepare() {
 }
 
 src_install() {
-	emake DESTDIR="${ED}"
+	emake DESTDIR="${ED}" install
 	einstalldocs
 
 	if use examples; then