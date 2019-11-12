gentoo/gentoo

commit a429a28dc3f9d208e5dd1c2b4f0f4473645453c9

sys-libs/db: Fixed "DuplicateFiles" QA message...
Package-Manager: Portage-2.3.79, Repoman-2.3.18
Signed-off-by: Lars Wendler <polynomial-c@gentoo.org>

sys-libs/db/db-4.5.20_p2-r2.ebuild
@@ -38,7 +38,7 @@ RDEPEND="tcl? ( dev-lang/tcl )
 	java? ( >=virtual/jre-1.4 )"
 
 PATCHES=(
-	"${FILESDIR}"/"${PN}"-"${SLOT}"-libtool.patch
+	"${FILESDIR}"/"${PN}"-4.4-libtool.patch
 
 	# use the includes from the prefix
 	"${FILESDIR}"/"${PN}"-4.3-jni-check-prefix-first.patch

sys-libs/db/db-4.6.21_p4-r1.ebuild
@@ -38,7 +38,7 @@ RDEPEND="tcl? ( dev-lang/tcl )
 	java? ( >=virtual/jre-1.4 )"
 
 PATCHES=(
-	"${FILESDIR}"/"${PN}"-"${SLOT}"-libtool.patch
+	"${FILESDIR}"/"${PN}"-4.4-libtool.patch
 
 	# use the includes from the prefix
 	"${FILESDIR}"/"${PN}"-"${SLOT}"-jni-check-prefix-first.patch

sys-libs/db/db-4.3.29_p1-r2.ebuild
@@ -41,8 +41,8 @@ PATCHES=(
 	"${FILESDIR}"/"${PN}"-"${SLOT}"-libtool.patch
 
 	# use the includes from the prefix
-	"${FILESDIR}"/"${PN}"-"${SLOT}"-jni-check-prefix-first.patch
-	"${FILESDIR}"/"${PN}"-"${SLOT}"-listen-to-java-options.patch
+	"${FILESDIR}"/"${PN}"-4.2-jni-check-prefix-first.patch
+	"${FILESDIR}"/"${PN}"-4.2-listen-to-java-options.patch
 
 	"${FILESDIR}"/"${PN}"-4.3.27-fix-dep-link.patch
 )

sys-libs/db/files/db-4.3-jni-check-prefix-first.patch
@@ -1,30 +1,0 @@
---- a/dist/aclocal_java/ac_jni_include_dirs.ac
-+++ b/dist/aclocal_java/ac_jni_include_dirs.ac
-@@ -43,14 +43,19 @@
- *)	AC_MSG_ERROR([$_ACJNI_JAVAC is not an absolute path name]);;
- esac
- 
--_ACJNI_FOLLOW_SYMLINKS("$_ACJNI_JAVAC")
--_JTOPDIR=`echo "$_ACJNI_FOLLOWED" | sed -e 's://*:/:g' -e 's:/[[^/]]*$::'`
--case "$host_os" in
--	darwin*)	_JTOPDIR=`echo "$_JTOPDIR" | sed -e 's:/[[^/]]*$::'`
--			_JINC="$_JTOPDIR/Headers";;
--	*)		_JINC="$_JTOPDIR/include";;
--esac
--
-+# If JAVAPREFIX is defined, look there first
-+if test -r "$JAVAPREFIX/include/jni.h"; then
-+	_JTOPDIR="$JAVAPREFIX"
-+	_JINC="$JAVAPREFIX/include"
-+else
-+	_ACJNI_FOLLOW_SYMLINKS("$_ACJNI_JAVAC")
-+	_JTOPDIR=`echo "$_ACJNI_FOLLOWED" | sed -e 's://*:/:g' -e 's:/[[^/]]*$::'`
-+	case "$host_os" in
-+		darwin*)	_JTOPDIR=`echo "$_JTOPDIR" | sed -e 's:/[[^/]]*$::'`
-+				_JINC="$_JTOPDIR/Headers";;
-+		*)		_JINC="$_JTOPDIR/include";;
-+	esac
-+fi
- # If we find jni.h in /usr/include, then it's not a java-only tree, so
- # don't add /usr/include or subdirectories to the list of includes.
- # An extra -I/usr/include can foul things up with newer gcc's.

sys-libs/db/files/db-4.3-listen-to-java-options.patch
@@ -1,10 +1,0 @@
---- a/dist/configure.ac
-+++ b/dist/configure.ac
-@@ -385,6 +385,7 @@
-         # A classpath that includes . is needed to check for Java
- 	CLASSPATH=".:$CLASSPATH"
- 	export CLASSPATH
-+	AC_JAVA_OPTIONS
- 	AC_PROG_JAVAC
- 	AC_PROG_JAR
- 	AC_PROG_JAVA

sys-libs/db/files/db-4.5-libtool.patch
@@ -1,47 +1,0 @@
---- a/dist/configure
-+++ b/dist/configure
-@@ -5765,7 +5765,7 @@
-   echo $ECHO_N "(cached) $ECHO_C" >&6
- else
-   # I'd rather use --version here, but apparently some GNU lds only accept -v.
--case `$LD -v 2>&1 </dev/null` in
-+case `"$LD" -v 2>&1 </dev/null` in
- *GNU* | *'with BFD'*)
-   lt_cv_prog_gnu_ld=yes
-   ;;
-@@ -8753,7 +8753,7 @@
-   	whole_archive_flag_spec=
-     fi
-     supports_anon_versioning=no
--    case `$LD -v 2>/dev/null` in
-+    case `"$LD" -v 2>/dev/null` in
-       *\ [01].* | *\ 2.[0-9].* | *\ 2.10.*) ;; # catch versions < 2.11
-       *\ 2.11.93.0.2\ *) supports_anon_versioning=yes ;; # RH7.3 ...
-       *\ 2.11.92.0.12\ *) supports_anon_versioning=yes ;; # Mandrake 8.2 ...
-@@ -11855,7 +11855,7 @@
-   echo $ECHO_N "(cached) $ECHO_C" >&6
- else
-   # I'd rather use --version here, but apparently some GNU lds only accept -v.
--case `$LD -v 2>&1 </dev/null` in
-+case `"$LD" -v 2>&1 </dev/null` in
- *GNU* | *'with BFD'*)
-   lt_cv_prog_gnu_ld=yes
-   ;;
-@@ -15842,7 +15842,7 @@
-   	whole_archive_flag_spec_F77=
-     fi
-     supports_anon_versioning=no
--    case `$LD -v 2>/dev/null` in
-+    case `"$LD" -v 2>/dev/null` in
-       *\ [01].* | *\ 2.[0-9].* | *\ 2.10.*) ;; # catch versions < 2.11
-       *\ 2.11.93.0.2\ *) supports_anon_versioning=yes ;; # RH7.3 ...
-       *\ 2.11.92.0.12\ *) supports_anon_versioning=yes ;; # Mandrake 8.2 ...
-@@ -18300,7 +18300,7 @@
-   	whole_archive_flag_spec_GCJ=
-     fi
-     supports_anon_versioning=no
--    case `$LD -v 2>/dev/null` in
-+    case `"$LD" -v 2>/dev/null` in
-       *\ [01].* | *\ 2.[0-9].* | *\ 2.10.*) ;; # catch versions < 2.11
-       *\ 2.11.93.0.2\ *) supports_anon_versioning=yes ;; # RH7.3 ...
-       *\ 2.11.92.0.12\ *) supports_anon_versioning=yes ;; # Mandrake 8.2 ...

sys-libs/db/files/db-4.6-libtool.patch
@@ -1,47 +1,0 @@
---- a/dist/configure
-+++ b/dist/configure
-@@ -5765,7 +5765,7 @@
-   echo $ECHO_N "(cached) $ECHO_C" >&6
- else
-   # I'd rather use --version here, but apparently some GNU lds only accept -v.
--case `$LD -v 2>&1 </dev/null` in
-+case `"$LD" -v 2>&1 </dev/null` in
- *GNU* | *'with BFD'*)
-   lt_cv_prog_gnu_ld=yes
-   ;;
-@@ -8753,7 +8753,7 @@
-   	whole_archive_flag_spec=
-     fi
-     supports_anon_versioning=no
--    case `$LD -v 2>/dev/null` in
-+    case `"$LD" -v 2>/dev/null` in
-       *\ [01].* | *\ 2.[0-9].* | *\ 2.10.*) ;; # catch versions < 2.11
-       *\ 2.11.93.0.2\ *) supports_anon_versioning=yes ;; # RH7.3 ...
-       *\ 2.11.92.0.12\ *) supports_anon_versioning=yes ;; # Mandrake 8.2 ...
-@@ -11855,7 +11855,7 @@
-   echo $ECHO_N "(cached) $ECHO_C" >&6
- else
-   # I'd rather use --version here, but apparently some GNU lds only accept -v.
--case `$LD -v 2>&1 </dev/null` in
-+case `"$LD" -v 2>&1 </dev/null` in
- *GNU* | *'with BFD'*)
-   lt_cv_prog_gnu_ld=yes
-   ;;
-@@ -15842,7 +15842,7 @@
-   	whole_archive_flag_spec_F77=
-     fi
-     supports_anon_versioning=no
--    case `$LD -v 2>/dev/null` in
-+    case `"$LD" -v 2>/dev/null` in
-       *\ [01].* | *\ 2.[0-9].* | *\ 2.10.*) ;; # catch versions < 2.11
-       *\ 2.11.93.0.2\ *) supports_anon_versioning=yes ;; # RH7.3 ...
-       *\ 2.11.92.0.12\ *) supports_anon_versioning=yes ;; # Mandrake 8.2 ...
-@@ -18300,7 +18300,7 @@
-   	whole_archive_flag_spec_GCJ=
-     fi
-     supports_anon_versioning=no
--    case `$LD -v 2>/dev/null` in
-+    case `"$LD" -v 2>/dev/null` in
-       *\ [01].* | *\ 2.[0-9].* | *\ 2.10.*) ;; # catch versions < 2.11
-       *\ 2.11.93.0.2\ *) supports_anon_versioning=yes ;; # RH7.3 ...
-       *\ 2.11.92.0.12\ *) supports_anon_versioning=yes ;; # Mandrake 8.2 ...

commit 61cc07d408dafe031432ac4f5783edf8febd8e29

sys-libs/db: Fixed regressions introduced by commit a429a28d
Closes: https://bugs.gentoo.org/699940
Package-Manager: Portage-2.3.79, Repoman-2.3.18
Signed-off-by: Lars Wendler <polynomial-c@gentoo.org>

sys-libs/db/db-18.1.32.ebuild
@@ -53,7 +53,7 @@ PATCHES=(
 
 	# use the includes from the prefix
 	"${FILESDIR}"/${PN}-6.2.32-jni-check-prefix-first.patch
-	"${FILESDIR}"/${PN}-4.3-listen-to-java-options.patch
+	"${FILESDIR}"/${PN}-4.2-listen-to-java-options.patch
 
 	# sqlite configure call has an extra leading ..
 	# upstreamed:5.2.36, missing in 5.3.x/6.x

sys-libs/db/db-4.4.20_p4-r2.ebuild
@@ -42,8 +42,8 @@ PATCHES=(
 	"${FILESDIR}"/"${PN}"-"${SLOT}"-libtool.patch
 
 	# use the includes from the prefix
-	"${FILESDIR}"/"${PN}"-4.3-jni-check-prefix-first.patch
-	"${FILESDIR}"/"${PN}"-4.3-listen-to-java-options.patch
+	"${FILESDIR}"/"${PN}"-4.2-jni-check-prefix-first.patch
+	"${FILESDIR}"/"${PN}"-4.2-listen-to-java-options.patch
 )
 
 # Required to avoid unpack attempt of patches

sys-libs/db/db-4.5.20_p2-r2.ebuild
@@ -41,8 +41,8 @@ PATCHES=(
 	"${FILESDIR}"/"${PN}"-4.4-libtool.patch
 
 	# use the includes from the prefix
-	"${FILESDIR}"/"${PN}"-4.3-jni-check-prefix-first.patch
-	"${FILESDIR}"/"${PN}"-4.3-listen-to-java-options.patch
+	"${FILESDIR}"/"${PN}"-4.2-jni-check-prefix-first.patch
+	"${FILESDIR}"/"${PN}"-4.2-listen-to-java-options.patch
 )
 
 # Required to avoid unpack attempt of patches

sys-libs/db/db-4.6.21_p4-r1.ebuild
@@ -42,7 +42,7 @@ PATCHES=(
 
 	# use the includes from the prefix
 	"${FILESDIR}"/"${PN}"-"${SLOT}"-jni-check-prefix-first.patch
-	"${FILESDIR}"/"${PN}"-4.3-listen-to-java-options.patch
+	"${FILESDIR}"/"${PN}"-4.2-listen-to-java-options.patch
 )
 
 # Required to avoid unpack attempt of patches

sys-libs/db/db-4.7.25_p4-r1.ebuild
@@ -41,11 +41,11 @@ RDEPEND="tcl? ( dev-lang/tcl )
 	java? ( >=virtual/jre-1.5 )"
 
 PATCHES=(
-	"${FILESDIR}"/"${PN}"-4.6-libtool.patch
+	"${FILESDIR}"/"${PN}"-4.4-libtool.patch
 
 	# use the includes from the prefix
 	"${FILESDIR}"/"${PN}"-4.6-jni-check-prefix-first.patch
-	"${FILESDIR}"/"${PN}"-4.3-listen-to-java-options.patch
+	"${FILESDIR}"/"${PN}"-4.2-listen-to-java-options.patch
 )
 
 # Required to avoid unpack attempt of patches

sys-libs/db/db-4.8.30-r3.ebuild
@@ -49,7 +49,7 @@ PATCHES=(
 
 	# use the includes from the prefix
 	"${FILESDIR}"/${PN}-4.6-jni-check-prefix-first.patch
-	"${FILESDIR}"/${PN}-4.3-listen-to-java-options.patch
+	"${FILESDIR}"/${PN}-4.2-listen-to-java-options.patch
 )
 
 src_prepare() {

sys-libs/db/db-5.1.29-r2.ebuild
@@ -48,7 +48,7 @@ PATCHES=(
 
 	# use the includes from the prefix
 	"${FILESDIR}"/${PN}-4.6-jni-check-prefix-first.patch
-	"${FILESDIR}"/${PN}-4.3-listen-to-java-options.patch
+	"${FILESDIR}"/${PN}-4.2-listen-to-java-options.patch
 
 	# upstream autoconf fails to build DBM when it's supposed to
 	# merged upstream in 5.0.26

sys-libs/db/db-5.3.28-r2.ebuild
@@ -58,7 +58,7 @@ src_prepare() {
 
 	# use the includes from the prefix
 	epatch "${FILESDIR}"/${PN}-4.6-jni-check-prefix-first.patch
-	epatch "${FILESDIR}"/${PN}-4.3-listen-to-java-options.patch
+	epatch "${FILESDIR}"/${PN}-4.2-listen-to-java-options.patch
 
 	# sqlite configure call has an extra leading ..
 	# upstreamed:5.2.36, missing in 5.3.x

sys-libs/db/db-5.3.28-r4.ebuild
@@ -52,7 +52,7 @@ PATCHES=(
 
 	# use the includes from the prefix
 	"${FILESDIR}"/${PN}-4.6-jni-check-prefix-first.patch
-	"${FILESDIR}"/${PN}-4.3-listen-to-java-options.patch
+	"${FILESDIR}"/${PN}-4.2-listen-to-java-options.patch
 
 	# sqlite configure call has an extra leading ..
 	# upstreamed:5.2.36, missing in 5.3.x
@@ -88,7 +88,7 @@ src_prepare() {
 	# This supersedes the unused jarlocation patches.
 	sed -r \
 		-e '/jarfile=.*\.jar$/s,(.jar$),-$(LIBVERSION)\1,g' \
-		dist/Makefile.in || die
+		-i dist/Makefile.in || die
 
 	cd dist || die
 	rm aclocal/libtool.m4 || die

sys-libs/db/db-6.0.35-r2.ebuild
@@ -52,7 +52,7 @@ PATCHES=(
 
 	# use the includes from the prefix
 	"${FILESDIR}"/${PN}-4.6-jni-check-prefix-first.patch
-	"${FILESDIR}"/${PN}-4.3-listen-to-java-options.patch
+	"${FILESDIR}"/${PN}-4.2-listen-to-java-options.patch
 
 	# sqlite configure call has an extra leading ..
 	# upstreamed:5.2.36, missing in 5.3.x/6.x

sys-libs/db/db-6.1.38.ebuild
@@ -52,7 +52,7 @@ PATCHES=(
 
 	# use the includes from the prefix
 	"${FILESDIR}"/${PN}-6.2-jni-check-prefix-first.patch
-	"${FILESDIR}"/${PN}-4.3-listen-to-java-options.patch
+	"${FILESDIR}"/${PN}-4.2-listen-to-java-options.patch
 
 	# sqlite configure call has an extra leading ..
 	# upstreamed:5.2.36, missing in 5.3.x/6.x

sys-libs/db/db-6.2.38.ebuild
@@ -52,7 +52,7 @@ PATCHES=(
 
 	# use the includes from the prefix
 	"${FILESDIR}"/${PN}-6.2.32-jni-check-prefix-first.patch
-	"${FILESDIR}"/${PN}-4.3-listen-to-java-options.patch
+	"${FILESDIR}"/${PN}-4.2-listen-to-java-options.patch
 
 	# sqlite configure call has an extra leading ..
 	# upstreamed:5.2.36, missing in 5.3.x/6.x