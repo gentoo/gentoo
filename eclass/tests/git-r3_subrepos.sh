9519ad8ec2b316c4a519c4d63c1e6d95721afeae
Michał Górny <mgorny@gentoo.org>
2019-11-20 09:58:24 +0100

    eclass/tests/git-r3_subrepos.sh: Use a supported EAPI
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- eclass/tests/git-r3_subrepos.sh
+++ eclass/tests/git-r3_subrepos.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# Copyright 1999-2015 Gentoo Foundation
+# Copyright 1999-2019 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
 
 source tests-common.sh
 
