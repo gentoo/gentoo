cf35a4ada900116b9f7590b785f76e7c9de632ce
Ulrich Müller <ulm@gentoo.org>
2019-12-15 00:16:41 +0100

    app-admin/puppet: Drop XEmacs support from version 4.10.12.
    This has been dropped in later versions already, and puppet-4.10.12 is
    the last ebuild in the tree that inherits xemacs-elisp-common.eclass.
    
    Closes: https://bugs.gentoo.org/702952
    Acked-by: Matthew Thode <prometheanfire@gentoo.org>
    Package-Manager: Portage-2.3.81, Repoman-2.3.20
    Signed-off-by: Ulrich Müller <ulm@gentoo.org>

--- app-admin/puppet/files/50puppet-mode-gentoo.el
+++
@@ -1,6 +0,0 @@
-
-;;; puppet-mode site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(autoload 'puppet-mode "puppet-mode" "Major mode for editing puppet manifests")
-(add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
