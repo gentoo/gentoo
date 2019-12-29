gentoo/gentoo

commit 6c797b166d8ead3e36ec6e92b566560d88c202e9

gnome-extra/seahorse-nautilus: Doesn't really need libgnome-keyring
Closes: https://bugs.gentoo.org/696904
Package-Manager: Portage-2.3.84, Repoman-2.3.20
Signed-off-by: Pacho Ramos <pacho@gentoo.org>

gnome-extra/seahorse-nautilus/seahorse-nautilus-3.11.92.ebuild
@@ -1,11 +1,10 @@
 # Copyright 1999-2019 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-EAPI="5"
-GCONF_DEBUG="no"
+EAPI="6"
 GNOME2_LA_PUNT="yes"
 
-inherit gnome2
+inherit autotools gnome2
 
 DESCRIPTION="Nautilus extension for encrypting and decrypting files with GnuPG"
 HOMEPAGE="https://wiki.gnome.org/Apps/Seahorse"
@@ -44,6 +43,11 @@ src_prepare() {
 	# Fix icon location, upstream bug #719763
 	sed -i 's/pixmaps\/seahorse-plugins\/48x48/pixmaps\/cryptui\/48x48/' \
 		tool/seahorse-notification.c || die
+
+	# Doesn't really need libgnome-keyring (from Fedora, fixed in
+	# 'master')
+	eapply "${FILESDIR}"/${P}-remove-libgnome-keyring.patch
+	eautoreconf
 
 	gnome2_src_prepare
 }

gnome-extra/seahorse-nautilus/files/seahorse-nautilus-3.11.92-remove-libgnome-keyring.patch
@@ -1,0 +1,31 @@
+From 8193f85dc3caeae23e5931c10f52ac9c5324d9d9 Mon Sep 17 00:00:00 2001
+From: Stef Walter <stefw@redhat.com>
+Date: Fri, 8 Jun 2018 12:40:09 +0200
+Subject: [PATCH] Remove libgnome-keyring dependency
+
+It's not needed by seahorse-nautilus
+---
+ configure.ac | 7 -------
+ 1 file changed, 7 deletions(-)
+
+diff --git a/configure.ac b/configure.ac
+index 471b34d..8d1f39f 100644
+--- a/configure.ac
++++ b/configure.ac
+@@ -146,13 +146,6 @@ else
+ fi
+ AC_SUBST(NAUTILUS_EXTENSION_DIR)
+ 
+-# -----------------------------------------------------------------------------
+-# Keyring
+-
+-PKG_CHECK_MODULES([GNOME_KEYRING], [gnome-keyring-1])
+-SEAHORSE_CFLAGS="$SEAHORSE_CFLAGS $GNOME_KEYRING_CFLAGS"
+-SEAHORSE_LIBS="$SEAHORSE_LIBS $GNOME_KEYRING_LIBS"
+-
+ # -----------------------------------------------------------------------------
+ # DBus
+ 
+-- 
+2.14.4
+

commit bd4ac8a58f5d0e36d6ee5e8072ea12026da4cece

gnome-extra/seahorse-nautilus: fix eautoreconf handling
Fixes: 6c797b166d8ead3e36ec6e92b566560d88c202e9
Bug: https://bugs.gentoo.org/696904
Package-Manager: Portage-2.3.79, Repoman-2.3.12
Signed-off-by: Mart Raudsepp <leio@gentoo.org>

gnome-extra/seahorse-nautilus/seahorse-nautilus-3.11.92.ebuild
@@ -3,8 +3,9 @@
 
 EAPI="6"
 GNOME2_LA_PUNT="yes"
+GNOME2_EAUTORECONF="yes"
 
-inherit autotools gnome2
+inherit gnome2
 
 DESCRIPTION="Nautilus extension for encrypting and decrypting files with GnuPG"
 HOMEPAGE="https://wiki.gnome.org/Apps/Seahorse"
@@ -46,8 +47,7 @@ src_prepare() {
 
 	# Doesn't really need libgnome-keyring (from Fedora, fixed in
 	# 'master')
-	eapply "${FILESDIR}"/${P}-remove-libgnome-keyring.patch
-	eautoreconf
+	eapply "${FILESDIR}"/${P}-remove-libgnome-keyring.patch # needs eautoreconf
 
 	gnome2_src_prepare
 }