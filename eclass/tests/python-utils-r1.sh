785fa30a4d12d3c2ab9036b3064e811c9635d770
Michał Górny <mgorny@gentoo.org>
2019-12-30 13:32:33 +0100

    python-utils-r1.eclass: Eliminate virtual/pypy
    Depend on dev-python/pypy directly, as that's the common package
    with the new layout.
    
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- eclass/tests/python-utils-r1.sh
+++ eclass/tests/python-utils-r1.sh
@@ -109,7 +109,7 @@ if [[ -x /usr/bin/pypy ]]; then
 	test_var PYTHON_SITEDIR pypy "/usr/lib*/pypy2.7/site-packages"
 	test_var PYTHON_INCLUDEDIR pypy "/usr/lib*/pypy2.7/include"
 fi
-test_var PYTHON_PKG_DEP pypy '*virtual/pypy*:0='
+test_var PYTHON_PKG_DEP pypy '*dev-python/pypy*:0='
 test_var PYTHON_SCRIPTDIR pypy /usr/lib/python-exec/pypy
 
 test_var EPYTHON pypy3 pypy3
@@ -118,7 +118,7 @@ if [[ -x /usr/bin/pypy3 ]]; then
 	test_var PYTHON_SITEDIR pypy3 "/usr/lib*/pypy3.?/site-packages"
 	test_var PYTHON_INCLUDEDIR pypy3 "/usr/lib*/pypy3.?/include"
 fi
-test_var PYTHON_PKG_DEP pypy3 '*virtual/pypy3*:0='
+test_var PYTHON_PKG_DEP pypy3 '*dev-python/pypy3*:0='
 test_var PYTHON_SCRIPTDIR pypy3 /usr/lib/python-exec/pypy3
 
 test_is "python_is_python3 python2.7" 1
