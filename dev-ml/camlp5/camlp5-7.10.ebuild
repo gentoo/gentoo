gentoo/gentoo

commit be6dfd1fc817b21fa3b5d77b7b7678c2bee61417

dev-ml/camlp5: version bump to 7.10
Signed-off-by: Drake Allen Donahue donahue95@comcast.net
Bug: https://bugs.gentoo.org/704408
Bug: https://bugs.gentoo.org/704366
Bug: https://bugs.gentoo.org/644162

dev-ml/camlp5/camlp5-7.10.ebuild
@@ -1,0 +1,72 @@
+# Copyright 1999-2019 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI="7"
+
+inherit multilib findlib eutils
+
+MY_P=${P%_p*}
+echo $MY_P
+DESCRIPTION="A preprocessor-pretty-printer of ocaml"
+HOMEPAGE="https://camlp5.github.io/"
+SRC_URI="https://github.com/camlp5/camlp5/archive/rel$(ver_rs 1- '').tar.gz -> ${P}.tar.gz"
+echo $SRC_URI
+S="${WORKDIR}/${PN}-rel$(ver_rs 1- '')"
+echo $S
+LICENSE="BSD"
+SLOT="0/${PV}"
+KEYWORDS="~alpha ~amd64 ~ppc ~x86"
+IUSE="doc +ocamlopt"
+
+DEPEND="
+	>=dev-lang/ocaml-3.10:=[ocamlopt?]
+	<=dev-lang/ocaml-4.6.0
+"
+RDEPEND="${DEPEND}"
+
+PATCHLEVEL=${PV#*_p}
+PATCHLIST=""
+
+if [ "${PATCHLEVEL}" != "${PV}" ] ; then
+	for (( i=1; i<=PATCHLEVEL; i++ )) ; do
+		SRC_URI="${SRC_URI}
+			http://pauillac.inria.fr/~ddr/camlp5/distrib/src/patch-${PV%_p*}-${i} -> ${MY_P}-patch-${i}.patch"
+		PATCHLIST="${PATCHLIST} ${MY_P}-patch-${i}.patch"
+	done
+fi
+
+src_prepare() {
+	
+	for i in ${PATCHLIST} ; do
+		epatch "${DISTDIR}/${i}"
+	done
+	eapply_user
+}
+
+src_configure() {
+	./configure \
+		--strict \
+		-prefix /usr \
+		-bindir /usr/bin \
+		-libdir /usr/$(get_libdir)/ocaml \
+		-mandir /usr/share/man || die "configure failed"
+}
+
+src_compile(){
+	emake out
+	if use ocamlopt; then
+		emake  opt
+		emake  opt.opt
+	fi
+}
+
+src_install() {
+	emake DESTDIR="${D}" install
+	# findlib support
+	insinto "$(ocamlfind printconf destdir)/${PN}"
+	doins etc/META
+
+	use doc && dohtml -r doc/*
+
+	dodoc CHANGES DEVEL ICHANGES README.md UPGRADING MODE
+}

commit ba55cc7888fcc68446e3a18e7931df8fa39af345

dev-ml/camlp5-7.10: delete dohtml, add DIST for 7.10 to Manifest
Signed-off-by: Drake Allen Donahue <donahue95@comcast.net>
Bug: https://bugs.gentoo.org/704408
Bug: https://bugs.gentoo.org/704366
Bug: https://bugs.gentoo.org/644162
Fixes: commit be6dfd1fc817b21fa3b5d77b7b7678c2bee61417

dev-ml/camlp5/Manifest
@@ -3,3 +3,4 @@ DIST camlp5-7.00.tar.gz 755750 BLAKE2B b047422ff34c4d15c21b65bd7473ccdad8bfc1f48
 DIST camlp5-7.01.tar.gz 757118 BLAKE2B 5d7c6f39f24bfee545552aae5c126305d2c767d06762aa516c21bcc5b327b312b7c38d742717eb507b92ae1e27de9c0fcde4d616754a7ce616b5f56689c8dc5a SHA512 9930957f707d9d2c8903f87f5f3444f71bad3d84c944436c7abc6de19209595c974098cd60a6a391f8ae87c4fb53b5122cba8e42e34fcf46d798dbdc28840008
 DIST camlp5-7.03.tar.gz 758136 BLAKE2B 4e1169adf7e119f1f659fe684a8b06c4730db3bcd7807169552de0e3fc115f8d45bc11961f3aee0a9c2d86e1130ec776a1bb7753d7d4e95f661d65d1f841406c SHA512 0c76d469c5f8c727088f3136107b6690349b50791ef8baefffdaa90141230907f977a7a886a9e8d88d39d8fa315637d882d0205c57d752e16575c888a8a1f54d
 DIST camlp5-7.10.tar.gz 849559 BLAKE2B b05624156e914c2657d76afbf3dfacd295fe5985ae3809959a40029a8052669be6ae8a6ff889fbf2c602d3f19c9995f013b388c140738f4bb4f4da9970911ed5 SHA512 faa9e88f56477162038fc80429d5839c93b3e38aec90366730babe737e2e6bb260dc39975947a6601212cc0c68f465bdbccb6337da23c07181909dfb11cf8640
+

dev-ml/camlp5/camlp5-7.10.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2019 Gentoo Authors
+# Copyright 1999-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI="7"
@@ -66,5 +66,6 @@ src_install() {
 	insinto "$(ocamlfind printconf destdir)/${PN}"
 	doins etc/META
 
+	use doc && dodoc -r doc/.
 	dodoc CHANGES DEVEL ICHANGES README.md UPGRADING MODE
 }