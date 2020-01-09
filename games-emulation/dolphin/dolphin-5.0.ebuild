gentoo/gentoo

commit 7a5765256c9a8cf089c304f1dc4b2fe8fc663842

games-emulation/dolphin: update live ebuild
Changes by Samuel Bauer and by Anthony Parsons:
- drop disappeared dependencies and flags: USE="ao openal portaudio"
- drop 'sed' calls and use cmake options instead
- disable automagic ccache autodetection

Bug: https://bugs.gentoo.org/561914
Package-Manager: Portage-2.3.48, Repoman-2.3.10

games-emulation/dolphin/dolphin-9999.ebuild
@@ -22,16 +22,16 @@ HOMEPAGE="https://www.dolphin-emu.org/"
 
 LICENSE="GPL-2"
 SLOT="0"
-IUSE="alsa ao bluetooth discord-presence doc egl +evdev ffmpeg libav log lto openal portaudio profile pulseaudio +qt5 sdl systemd upnp"
+IUSE="alsa bluetooth discord-presence doc egl +evdev ffmpeg libav log lto profile pulseaudio +qt5 sdl systemd upnp"
 
 RDEPEND="
-	>=media-libs/libsfml-2.1
-	>net-libs/enet-1.3.7
-	>=net-libs/mbedtls-2.1.1:=
 	dev-libs/hidapi:0=
 	dev-libs/lzo:2=
 	dev-libs/pugixml:0=
 	media-libs/libpng:0=
+	media-libs/libsfml
+	net-libs/enet:1.3
+	net-libs/mbedtls
 	net-misc/curl:0=
 	sys-libs/readline:0=
 	sys-libs/zlib:0=
@@ -41,7 +41,6 @@ RDEPEND="
 	virtual/libusb:1
 	virtual/opengl
 	alsa? ( media-libs/alsa-lib )
-	ao? ( media-libs/libao )
 	bluetooth? ( net-wireless/bluez )
 	egl? ( media-libs/mesa[egl] )
 	evdev? (
@@ -52,11 +51,6 @@ RDEPEND="
 		libav? ( media-video/libav:= )
 		!libav? ( media-video/ffmpeg:= )
 	)
-	openal? (
-		media-libs/openal
-		media-libs/libsoundtouch
-	)
-	portaudio? ( media-libs/portaudio )
 	profile? ( dev-util/oprofile )
 	pulseaudio? ( media-sound/pulseaudio )
 	qt5? (
@@ -66,7 +60,7 @@ RDEPEND="
 	)
 	sdl? ( media-libs/libsdl2[haptic,joystick] )
 	systemd? ( sys-apps/systemd:0= )
-	upnp? ( >=net-libs/miniupnpc-1.7 )
+	upnp? ( net-libs/miniupnpc )
 "
 DEPEND="${RDEPEND}
 	app-arch/zip
@@ -77,26 +71,6 @@ DEPEND="${RDEPEND}
 
 src_prepare() {
 	cmake-utils_src_prepare
-
-	# Remove automatic dependencies to prevent building without flags enabled.
-	if use !alsa; then
-		sed -i -e '/include(FindALSA/d' CMakeLists.txt || die
-	fi
-	if use !ao; then
-		sed -i -e '/check_lib(AO/d' CMakeLists.txt || die
-	fi
-	if use !bluetooth; then
-		sed -i -e '/check_lib(BLUEZ/d' CMakeLists.txt || die
-	fi
-	if use !openal; then
-		sed -i -e '/include(FindOpenAL/d' CMakeLists.txt || die
-	fi
-	if use !portaudio; then
-		sed -i -e '/CMAKE_REQUIRED_LIBRARIES portaudio/d' CMakeLists.txt || die
-	fi
-	if use !pulseaudio; then
-		sed -i -e '/check_lib(PULSEAUDIO/d' CMakeLists.txt || die
-	fi
 
 	# Remove all the bundled libraries that support system-installed
 	# preference. See CMakeLists.txt for conditional 'add_subdirectory' calls.
@@ -155,6 +129,13 @@ src_configure() {
 
 		-DUSE_EGL=$(usex egl)
 		-DUSE_UPNP=$(usex upnp)
+
+		-DENABLE_ALSA=$(usex alsa)
+		-DENABLE_BLUEZ=$(usex bluetooth)
+		# Use ccache only when user did set FEATURES=ccache (or similar)
+		# not when ccache binary is present in system (automagic).
+		-DCCACHE_BIN=CCACHE_BIN-NOTFOUND
+		-DENABLE_PULSEAUDIO=$(usex pulseaudio)
 	)
 
 	cmake-utils_src_configure
@@ -176,12 +157,6 @@ src_install() {
 pkg_postinst() {
 	# Add pax markings for hardened systems
 	pax-mark -m "${EPREFIX}"/usr/games/bin/"${PN}"-emu
-
-	if ! use portaudio; then
-		ewarn "If you want microphone capabilities in dolphin-emu, rebuild with"
-		ewarn "USE=\"portaudio\""
-	fi
-
 	gnome2_icon_cache_update
 }
 

commit 7b60b2936174d0dcf62caded07e1e5e18f3d04ea

games-emulation/dolphin: depend on mbedtls subslot, bug #705046
It's a fix of regression introduced by
https://gitweb.gentoo.org/repo/gentoo.git/commit/?id=7a5765256c9a8cf089c304f1dc4b2fe8fc663842

Reported-by: Ben Kohler
Closes: https://bugs.gentoo.org/705046
Package-Manager: Portage-2.3.84, Repoman-2.3.20
Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

games-emulation/dolphin/dolphin-5.0.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2019 Gentoo Authors
+# Copyright 1999-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=5
@@ -24,7 +24,7 @@ RESTRICT=test
 RDEPEND="
 	>=media-libs/libsfml-2.1
 	>net-libs/enet-1.3.7
-	>=net-libs/mbedtls-2.1.1:=
+	>=net-libs/mbedtls-2.1.1:0=
 	dev-libs/lzo
 	media-libs/libpng:0=
 	sys-libs/readline:0=

games-emulation/dolphin/dolphin-9999.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2019 Gentoo Authors
+# Copyright 1999-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=6
@@ -32,7 +32,7 @@ RDEPEND="
 	media-libs/libsfml
 	media-libs/mesa[egl]
 	net-libs/enet:1.3
-	net-libs/mbedtls
+	net-libs/mbedtls:0=
 	net-misc/curl:0=
 	sys-libs/readline:0=
 	sys-libs/zlib:0=