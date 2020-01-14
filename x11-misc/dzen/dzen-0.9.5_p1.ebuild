gentoo/gentoo

commit 7e73e0f99db785f461b8b80d000a5571bb77fda3

x11-misc/dzen: [QA] Fix HomepageInSrcUri
Signed-off-by: David Seifert <soap@gentoo.org>

x11-misc/dzen/dzen-0.9.5_p1.ebuild
@@ -8,7 +8,7 @@ COMMITID="488ab66019f475e35e067646621827c18a879ba1"
 
 DESCRIPTION="A general purpose messaging, notification and menuing program for X11"
 HOMEPAGE="https://github.com/robm/dzen"
-SRC_URI="${HOMEPAGE}/tarball/${COMMITID} -> ${P}.tar.gz"
+SRC_URI="https://github.com/robm/dzen/tarball/${COMMITID} -> ${P}.tar.gz"
 
 LICENSE="MIT"
 KEYWORDS="~amd64 ~x86"

commit f513303ad716c202d7ad3d5468cef039e43ff8c6

x11-misc/dzen: Fix copyright(QA), SRC_URI
Package-Manager: Portage-2.3.84, Repoman-2.3.20
Fixes: 7e73e0f99db785f461b8b80d000a5571bb77fda3
Signed-off-by: Jeroen Roovers <jer@gentoo.org>

x11-misc/dzen/dzen-0.9.5_p1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2018 Gentoo Authors
+# Copyright 1999-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
@@ -8,7 +8,7 @@ COMMITID="488ab66019f475e35e067646621827c18a879ba1"
 
 DESCRIPTION="A general purpose messaging, notification and menuing program for X11"
 HOMEPAGE="https://github.com/robm/dzen"
-SRC_URI="https://github.com/robm/dzen/tarball/${COMMITID} -> ${P}.tar.gz"
+SRC_URI="https://github.com/robm/${PN}/tarball/${COMMITID} -> ${P}.tar.gz"
 
 LICENSE="MIT"
 KEYWORDS="~amd64 ~x86"