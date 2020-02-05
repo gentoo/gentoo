gentoo/gentoo

commit 4db35b3a2131c1973835b865106d3034d2e0635a

net-analyzer/nmap: Patch live ebuild
Package-Manager: Portage-2.3.83, Repoman-2.3.20
Signed-off-by: Jeroen Roovers <jer@gentoo.org>

net-analyzer/nmap/files/nmap-9999-constify-continued.patch
@@ -1,0 +1,22 @@
+--- a/libnetutil/EthernetHeader.cc
++++ b/libnetutil/EthernetHeader.cc
+@@ -284,7 +284,7 @@ int EthernetHeader::setEtherType(u16 val){
+ 
+ 
+ /** Returns destination port in HOST byte order */
+-u16 EthernetHeader::getEtherType() const {
++const u16 EthernetHeader::getEtherType() const {
+   return ntohs(this->h.eth_type);
+ } /* End of getEtherType() */
+ 
+--- a/scan_engine.cc
++++ b/scan_engine.cc
+@@ -166,7 +166,7 @@ extern "C" int g_has_npcap_loopback;
+ #endif
+ 
+ 
+-int HssPredicate::operator() (const HostScanStats *lhs, const HostScanStats *rhs) const {
++const int HssPredicate::operator() (const HostScanStats *lhs, const HostScanStats *rhs) const {
+   const struct sockaddr_storage *lss, *rss;
+   lss = (lhs) ? lhs->target->TargetSockAddr() : ss;
+   rss = (rhs) ? rhs->target->TargetSockAddr() : ss;

net-analyzer/nmap/nmap-9999.ebuild
@@ -66,6 +66,8 @@ PATCHES=(
 	"${FILESDIR}"/${PN}-7.25-libpcre.patch
 	"${FILESDIR}"/${PN}-7.31-libnl.patch
 	"${FILESDIR}"/${PN}-7.80-ac-config-subdirs.patch
+	"${FILESDIR}"/${PN}-9999-constify-continued.patch
+	"${FILESDIR}"/${PN}-9999-netutil-else.patch
 )
 S="${WORKDIR}/${MY_P}"
 

net-analyzer/nmap/files/nmap-9999-netutil-else.patch
@@ -1,0 +1,20 @@
+--- a/libnetutil/netutil.cc
++++ b/libnetutil/netutil.cc
+@@ -2571,7 +2571,7 @@ const char *ippackethdrinfo(const u8 *packet, u32 len, int detail) {
+ 
+     /* CASE 4: where we (finally!) have a full 20 byte TCP header so we can
+      * safely print all fields */
+-    else { /* if (datalen >= 20) */
++    else if (datalen >= 20) {
+ 
+       /* TCP Flags */
+       p = tflags;
+@@ -2633,7 +2633,7 @@ const char *ippackethdrinfo(const u8 *packet, u32 len, int detail) {
+           (tcpoptinfo[0]!='\0') ? " " : "",
+           tcpoptinfo, ipinfo);
+       }
+-    } else{
++    } else {
+       /* If the packet does not fall into any other category, then we have a
+          really screwed-up packet. */
+       /* This ought to be unreachable; if static analysis flags it as such, delete it. */

commit 2ba4a5ba2151f6960d6a2c7c5df318ecc6994075

net-analyzer/nmap: Drop obsolete patch
Package-Manager: Portage-2.3.87, Repoman-2.3.20
Fixes: 4db35b3a2131c1973835b865106d3034d2e0635a
Signed-off-by: Jeroen Roovers <jer@gentoo.org>

net-analyzer/nmap/files/nmap-9999-constify-continued.patch
@@ -1,22 +1,0 @@
---- a/libnetutil/EthernetHeader.cc
-+++ b/libnetutil/EthernetHeader.cc
-@@ -284,7 +284,7 @@ int EthernetHeader::setEtherType(u16 val){
- 
- 
- /** Returns destination port in HOST byte order */
--u16 EthernetHeader::getEtherType() const {
-+const u16 EthernetHeader::getEtherType() const {
-   return ntohs(this->h.eth_type);
- } /* End of getEtherType() */
- 
---- a/scan_engine.cc
-+++ b/scan_engine.cc
-@@ -166,7 +166,7 @@ extern "C" int g_has_npcap_loopback;
- #endif
- 
- 
--int HssPredicate::operator() (const HostScanStats *lhs, const HostScanStats *rhs) const {
-+const int HssPredicate::operator() (const HostScanStats *lhs, const HostScanStats *rhs) const {
-   const struct sockaddr_storage *lss, *rss;
-   lss = (lhs) ? lhs->target->TargetSockAddr() : ss;
-   rss = (rhs) ? rhs->target->TargetSockAddr() : ss;

net-analyzer/nmap/nmap-9999.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2019 Gentoo Authors
+# Copyright 1999-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
@@ -66,7 +66,6 @@ PATCHES=(
 	"${FILESDIR}"/${PN}-7.25-libpcre.patch
 	"${FILESDIR}"/${PN}-7.31-libnl.patch
 	"${FILESDIR}"/${PN}-7.80-ac-config-subdirs.patch
-	"${FILESDIR}"/${PN}-9999-constify-continued.patch
 	"${FILESDIR}"/${PN}-9999-netutil-else.patch
 )
 S="${WORKDIR}/${MY_P}"