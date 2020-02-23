gentoo/gentoo

commit 61a9ee210b2d44c4fa953342504293a89395bef0

dev-libs/folks: fix USE=bluetooth build with meson-0.53
Package-Manager: Portage-2.3.84, Repoman-2.3.20
Signed-off-by: Mart Raudsepp <leio@gentoo.org>

dev-libs/folks/folks-0.12.1-r1.ebuild
@@ -49,6 +49,7 @@ BDEPEND="
 PATCHES=(
 	"${FILESDIR}"/${PV}-conditional-tests.patch # Allow not building lots of test executables when tests are disabled
 	"${FILESDIR}"/${PV}-no-tracker-tests.patch # TODO: Tracker tests fail; this removed them for now
+	"${FILESDIR}"/${PV}-meson-0.53-compat.patch # https://gitlab.gnome.org/GNOME/folks/issues/119
 )
 
 src_prepare() {

dev-libs/folks/files/0.12.1-meson-0.53-compat.patch
@@ -1,0 +1,47 @@
+From 1853567d79024c6ce960d9dc187cff8aaf9a37ce Mon Sep 17 00:00:00 2001
+From: Niels De Graef <nielsdegraef@gmail.com>
+Date: Mon, 3 Feb 2020 08:57:52 +0100
+Subject: [PATCH] meson: Use python.dependency() correctly
+
+From https://github.com/mesonbuild/meson/issues/6470:
+
+> So, to further clarify, passing an argument to
+> py_installation.dependency() should be an error, since this method
+> will only ever return a python dependency (for embedding python
+> itself, etc.) and not dbusmock or anything else.
+
+Fixxes https://gitlab.gnome.org/GNOME/folks/issues/119
+---
+ meson.build | 9 ++++++---
+ 1 file changed, 6 insertions(+), 3 deletions(-)
+
+diff --git a/meson.build b/meson.build
+index d5115e28..f3644457 100644
+--- a/meson.build
++++ b/meson.build
+@@ -1,7 +1,7 @@
+ project('folks', [ 'vala', 'c' ],
+   version: '0.13.2',
+   license: 'LGPL2.1+',
+-  meson_version: '>= 0.49',
++  meson_version: '>= 0.51',
+ )
+ 
+ gnome = import('gnome')
+@@ -132,8 +132,11 @@ if bluez_backend_enabled
+   libebook_dep = dependency('libebook-1.2', version: '>=' + min_eds_version)
+   # Needed for the BlueZ tests
+   pymod = import('python')
+-  py_installation = pymod.find_installation('python3')
+-  python_dbusmock = py_installation.dependency('dbusmock')
++  py_installation = pymod.find_installation('python3',
++    modules: [
++      'dbusmock',
++    ],
++  )
+ endif
+ 
+ if import_tool_enabled
+-- 
+2.20.1
+

commit 2609ba9466d268b305cc0626b86587a13eb75d96

dev-libs/folks: don't require dbusmock without tests being built
The upstream meson-0.53 fix made the dbusmock check actually effective,
erroring out if not found when bluetooth is enabled. We already have
a patch downstream to make build of tests conditional, so build on
top of that to skip the dbusmock check when tests aren't being built.

Fixes: 61a9ee210b2d44c4fa953342504293a89395bef0
Closes: https://bugs.gentoo.org/710548
Package-Manager: Portage-2.3.84, Repoman-2.3.20
Signed-off-by: Mart Raudsepp <leio@gentoo.org>

dev-libs/folks/files/0.12.1-meson-0.53-compat2.patch
@@ -1,0 +1,31 @@
+From f8a59fb6bf12be06595c94d0b00afed4e1d2f1ee Mon Sep 17 00:00:00 2001
+From: Mart Raudsepp <leio@gentoo.org>
+Date: Sun, 23 Feb 2020 19:28:51 +0200
+Subject: [PATCH] meson: Conditionalize dbusmock check to tests being built
+
+---
+ meson.build | 2 ++
+ 1 file changed, 2 insertions(+)
+
+diff --git a/meson.build b/meson.build
+index 74320634..64f28d2b 100644
+--- a/meson.build
++++ b/meson.build
+@@ -131,12 +131,14 @@ endif
+ if bluez_backend_enabled
+   libebook_dep = dependency('libebook-1.2', version: '>=' + min_eds_version)
+   # Needed for the BlueZ tests
++  if get_option('test')
+   pymod = import('python')
+   py_installation = pymod.find_installation('python3',
+     modules: [
+       'dbusmock',
+     ],
+   )
++  endif
+ endif
+ 
+ if import_tool_enabled
+-- 
+2.20.1
+

dev-libs/folks/folks-0.12.1-r1.ebuild
@@ -48,8 +48,9 @@ BDEPEND="
 
 PATCHES=(
 	"${FILESDIR}"/${PV}-conditional-tests.patch # Allow not building lots of test executables when tests are disabled
-	"${FILESDIR}"/${PV}-no-tracker-tests.patch # TODO: Tracker tests fail; this removed them for now
+	"${FILESDIR}"/${PV}-no-tracker-tests.patch # TODO: Tracker tests fail; this removes them for now
 	"${FILESDIR}"/${PV}-meson-0.53-compat.patch # https://gitlab.gnome.org/GNOME/folks/issues/119
+	"${FILESDIR}"/${PV}-meson-0.53-compat2.patch # Don't require dbusmock if tests aren't built (with our downstream conditional-tests patch)
 )
 
 src_prepare() {