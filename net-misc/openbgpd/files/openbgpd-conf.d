61f64aa6fe82379c5ab88470a523e340afc11d50
Alarig Le Lay <alarig@swordarmor.fr>
2020-03-24 19:38:17 +0100

    net-misc/openbgpd: OpenBGPD, BGPv4 from OpenBSD (new ebuild)
    Package-Manager: Portage-2.3.89, Repoman-2.3.20
    Signed-off-by: Alarig Le Lay <alarig@swordarmor.fr>

---
+++ net-misc/openbgpd/files/openbgpd-conf.d
@@ -0,0 +1,7 @@
+# /etc/conf.d/bgpd: config file for openbsd's bgpd
+
+# See bgpd(8) man page ... some popular options:
+#     -f file     Use file as the configuration file, instead of the default
+#                 /etc/bgpd.conf.
+#
+BGPD_OPTS=""
