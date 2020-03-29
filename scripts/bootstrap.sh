gentoo/gentoo

commit b9e8ca9b4aa18689ef7fa7ec46e4bbb7e311eced

Make sure we rebuild actual libc and not the virtual package. Thanks to Zac (zmedico) for the pointer.
Signed-off-by: Jorge Manuel B. S. Vicetto (jmbsvicetto) <jmbsvicetto@gentoo.org>

scripts/bootstrap.sh
@@ -272,7 +272,7 @@ for atom in portage.settings.packages:
 [[ -z ${myBINUTILS}   ]] && myBINUTILS="binutils"
 [[ -z ${myGCC}        ]] && myGCC="gcc"
 [[ -z ${myGETTEXT}    ]] && myGETTEXT="gettext"
-[[ -z ${myLIBC}       ]] && myLIBC="virtual/libc"
+[[ -z ${myLIBC}       ]] && myLIBC="$(portageq expand_virtual / virtual/libc)"
 [[ -z ${myTEXINFO}    ]] && myTEXINFO="sys-apps/texinfo"
 [[ -z ${myZLIB}       ]] && myZLIB="zlib"
 [[ -z ${myNCURSES}    ]] && myNCURSES="ncurses"

commit b73bd14850491738363f3c8e60519b22d6ece063

scripts/bootstrap.sh: Emerge the actual libc
The python program creates a set of bash variable assignments like

myGCC="sys-devel/gcc";

which are used to make a list of packages to be rebuilt during
catalyst's stage 2. The toolchain, including libc, is supposed to be
rebuilt in this stage, but 'portage.settings.packages' contains
'virtual/libc' and not a provider, so it generates:

myLIBC="virtual/libc";

This results in catalyst not rebuilding the libc itself but instead
reemerging virtual/libc.

Commit b9e8ca9b4aa1 (Make sure we rebuild actual libc and not the
virtual package.) tried to fix this, but obviously missed the mark as
any testing would have shown.

The solution is to have the python program expand the virtual itself.

Fixes: b9e8ca9b4aa1 (Make sure we rebuild actual libc and not the virtual package.)
Closes: https://bugs.gentoo.org/511694
Signed-off-by: Matt Turner <mattst88@gentoo.org>

scripts/bootstrap.sh
@@ -259,11 +259,14 @@ done
 
 eval $(pycmd '
 import portage
+from portage.dbapi._expand_new_virt import expand_new_virt
 import sys
+root = portage.settings["EROOT"]
 for atom in portage.settings.packages:
 	if not isinstance(atom, portage.dep.Atom):
 		atom = portage.dep.Atom(atom.lstrip("*"))
 	varname = "my" + portage.catsplit(atom.cp)[1].upper().replace("-", "_")
+	atom = list(expand_new_virt(portage.db[root]["vartree"].dbapi, atom))[0]
 	sys.stdout.write("%s=\"%s\"; " % (varname, atom))
 ')
 