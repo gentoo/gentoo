1309f27b471430f1f3e60363d4833fa2d0904d69
Michał Górny <mgorny@gentoo.org>
2020-03-30 15:10:11 +0200

    eclass/tests/python-utils-r1.sh: Remove tests for jython & pypy
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- eclass/tests/python-utils-r1.sh
+++ eclass/tests/python-utils-r1.sh
@@ -101,23 +101,6 @@ fi
 test_var PYTHON_PKG_DEP python3_7 '*dev-lang/python*:3.7'
 test_var PYTHON_SCRIPTDIR python3_7 /usr/lib/python-exec/python3.7
 
-test_var EPYTHON jython2_7 jython2.7
-test_var PYTHON jython2_7 /usr/bin/jython2.7
-if [[ -x /usr/bin/jython2.7 ]]; then
-	test_var PYTHON_SITEDIR jython2_7 /usr/share/jython-2.7/Lib/site-packages
-fi
-test_var PYTHON_PKG_DEP jython2_7 '*dev-java/jython*:2.7'
-test_var PYTHON_SCRIPTDIR jython2_7 /usr/lib/python-exec/jython2.7
-
-test_var EPYTHON pypy pypy
-test_var PYTHON pypy /usr/bin/pypy
-if [[ -x /usr/bin/pypy ]]; then
-	test_var PYTHON_SITEDIR pypy "/usr/lib*/pypy2.7/site-packages"
-	test_var PYTHON_INCLUDEDIR pypy "/usr/lib*/pypy2.7/include"
-fi
-test_var PYTHON_PKG_DEP pypy '*dev-python/pypy*:0='
-test_var PYTHON_SCRIPTDIR pypy /usr/lib/python-exec/pypy
-
 test_var EPYTHON pypy3 pypy3
 test_var PYTHON pypy3 /usr/bin/pypy3
 if [[ -x /usr/bin/pypy3 ]]; then
@@ -129,16 +112,13 @@ test_var PYTHON_SCRIPTDIR pypy3 /usr/lib/python-exec/pypy3
 
 test_is "python_is_python3 python2.7" 1
 test_is "python_is_python3 python3.2" 0
-test_is "python_is_python3 jython2.7" 1
 test_is "python_is_python3 pypy" 1
 test_is "python_is_python3 pypy3" 0
 
 # generic shebangs
 test_fix_shebang '#!/usr/bin/python' python2.7 '#!/usr/bin/python2.7'
 test_fix_shebang '#!/usr/bin/python' python3.6 '#!/usr/bin/python3.6'
-test_fix_shebang '#!/usr/bin/python' pypy '#!/usr/bin/pypy'
 test_fix_shebang '#!/usr/bin/python' pypy3 '#!/usr/bin/pypy3'
-test_fix_shebang '#!/usr/bin/python' jython2.7 '#!/usr/bin/jython2.7'
 
 # python2/python3 matching
 test_fix_shebang '#!/usr/bin/python2' python2.7 '#!/usr/bin/python2.7'
@@ -155,12 +135,8 @@ test_fix_shebang '#!/usr/bin/python2.7' python3.2 '#!/usr/bin/python3.2' --force
 test_fix_shebang '#!/usr/bin/python3.2' python3.2 '#!/usr/bin/python3.2'
 test_fix_shebang '#!/usr/bin/python3.2' python2.7 FAIL
 test_fix_shebang '#!/usr/bin/python3.2' python2.7 '#!/usr/bin/python2.7' --force
-test_fix_shebang '#!/usr/bin/pypy' pypy '#!/usr/bin/pypy'
 test_fix_shebang '#!/usr/bin/pypy' python2.7 FAIL
 test_fix_shebang '#!/usr/bin/pypy' python2.7 '#!/usr/bin/python2.7' --force
-test_fix_shebang '#!/usr/bin/jython2.7' jython2.7 '#!/usr/bin/jython2.7'
-test_fix_shebang '#!/usr/bin/jython2.7' jython3.2 FAIL
-test_fix_shebang '#!/usr/bin/jython2.7' jython3.2 '#!/usr/bin/jython3.2' --force
 
 # fancy path handling
 test_fix_shebang '#!/mnt/python2/usr/bin/python' python3.6 \
@@ -196,39 +172,32 @@ test_is "_python_impl_supported pypy1_9" 1
 test_is "_python_impl_supported pypy2_0" 1
 test_is "_python_impl_supported pypy" 1
 test_is "_python_impl_supported pypy3" 0
-test_is "_python_impl_supported jython2_7" 1
 
 # check _python_impl_matches behavior
 test_is "_python_impl_matches python2_7 -2" 0
 test_is "_python_impl_matches python3_6 -2" 1
 test_is "_python_impl_matches python3_7 -2" 1
-test_is "_python_impl_matches pypy -2" 0
 test_is "_python_impl_matches pypy3 -2" 1
 test_is "_python_impl_matches python2_7 -3" 1
 test_is "_python_impl_matches python3_6 -3" 0
 test_is "_python_impl_matches python3_7 -3" 0
-test_is "_python_impl_matches pypy -3" 1
 test_is "_python_impl_matches pypy3 -3" 0
 test_is "_python_impl_matches python2_7 -2 python3_6" 0
 test_is "_python_impl_matches python3_6 -2 python3_6" 0
 test_is "_python_impl_matches python3_7 -2 python3_6" 1
-test_is "_python_impl_matches pypy -2 python3_6" 0
 test_is "_python_impl_matches pypy3 -2 python3_6" 1
 test_is "_python_impl_matches python2_7 pypy3 -2 python3_6" 0
 test_is "_python_impl_matches python3_6 pypy3 -2 python3_6" 0
 test_is "_python_impl_matches python3_7 pypy3 -2 python3_6" 1
-test_is "_python_impl_matches pypy pypy3 -2 python3_6" 0
 test_is "_python_impl_matches pypy3 pypy3 -2 python3_6" 0
 set -f
 test_is "_python_impl_matches python2_7 pypy*" 1
 test_is "_python_impl_matches python3_6 pypy*" 1
 test_is "_python_impl_matches python3_7 pypy*" 1
-test_is "_python_impl_matches pypy pypy*" 0
 test_is "_python_impl_matches pypy3 pypy*" 0
 test_is "_python_impl_matches python2_7 python*" 0
 test_is "_python_impl_matches python3_6 python*" 0
 test_is "_python_impl_matches python3_7 python*" 0
-test_is "_python_impl_matches pypy python*" 1
 test_is "_python_impl_matches pypy3 python*" 1
 set +f
 
