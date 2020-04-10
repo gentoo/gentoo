gentoo/gentoo

commit 4dd021ff78c52b61bb16fe78b2627a6fe6a184e7

dev-php/PHP_CodeCoverage: Mark new revision stable; drop old
Pure code dependency change

Signed-off-by: Brian Evans <grknight@gentoo.org>

dev-php/PHP_CodeCoverage/PHP_CodeCoverage-4.0.7-r2.ebuild
@@ -1,7 +1,7 @@
-# Copyright 1999-2017 Gentoo Foundation
+# Copyright 1999-2019 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-EAPI=6
+EAPI=7
 
 MY_PN="php-code-coverage"
 
@@ -11,28 +11,26 @@ SRC_URI="https://github.com/sebastianbergmann/${MY_PN}/archive/${PV}.tar.gz -> $
 
 LICENSE="BSD"
 SLOT="0"
-KEYWORDS="~alpha amd64 arm hppa ia64 ppc ppc64 ~s390 ~sh sparc x86"
+KEYWORDS="~alpha amd64 arm ~hppa ia64 ppc ppc64 ~s390 ~sh sparc x86"
 IUSE=""
 
 S="${WORKDIR}/${MY_PN}-${PV}"
 
 RDEPEND="dev-php/fedora-autoloader
 	>=dev-php/File_Iterator-1.3
-	!>=dev-php/File_Iterator-2.0
+	<dev-php/File_Iterator-2.0
 	>=dev-php/Text_Template-1.2
-	!>=dev-php/Text_Template-2.0
+	<dev-php/Text_Template-2.0
 	>=dev-php/PHP_TokenStream-1.4
-	!>=dev-php/PHP_TokenStream-3.0
+	<dev-php/PHP_TokenStream-3.0
 	>=dev-php/sebastian-environment-1.3.2
-	!>=dev-php/sebastian-environment-3.0
+	<dev-php/sebastian-environment-3.0
 	>=dev-php/sebastian-version-1.0
-	!>=dev-php/sebastian-version-3.0
+	<dev-php/sebastian-version-3.0
 	dev-php/sebastian-code-unit-reverse-lookup
-	!>=dev-php/sebastian-code-unit-reverse-lookup-2.0
+	<dev-php/sebastian-code-unit-reverse-lookup-2.0
 	|| (
-		dev-lang/php:5.6[xml,xmlwriter]
-		dev-lang/php:7.0[xml,xmlwriter]
-		dev-lang/php:7.1[xml,xmlwriter]
+		dev-lang/php:7.2[xml(-),xmlwriter(-)]
 	)"
 
 src_install() {

dev-php/PHP_CodeCoverage/Manifest
@@ -1,2 +1,1 @@
-DIST PHP_CodeCoverage-1.2.13.tgz 158772 BLAKE2B 7f9703cf724dece9535fce4e5dbc7a354302227b38daabf9f8c4417a43980f369807dfbcd65fde79215a14531971d0bf6cf096a6b01c2c6b99a5bc326c5d5b66 SHA512 6a3426c945ad6abf35be3068cf0f7e17215179a382e93b7be3a5e436611b327c9b7bc715f7acc900231856678f578c1f614648d2679baca9b6960dfafd897dc6
 DIST PHP_CodeCoverage-4.0.7.tar.gz 349906 BLAKE2B 2ba665efa2d20a5eb66c66375a095425057e883697dd1d3ba7214e56de29c96451516adb2843589e64732c994aac7289afa0e34a74d56ef9bc36f36e635eb944 SHA512 8418efa2585d05b3004ab50e7ba77513429d649f71fdb2479412424a0f40a698a962e1d87852db08fec8f92e569a9a5767dad66d5d64e072637dde33182e71e9

dev-php/PHP_CodeCoverage/PHP_CodeCoverage-1.2.13.ebuild
@@ -1,24 +1,0 @@
-# Copyright 1999-2017 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=6
-
-PHP_PEAR_DOMAIN="pear.phpunit.de"
-PHP_PEAR_CHANNEL="${FILESDIR}/channel.xml"
-
-inherit php-pear-r2
-
-DESCRIPTION="Collection, processing, and rendering for PHP code coverage"
-HOMEPAGE="http://pear.phpunit.de"
-SRC_URI="http://${PHP_PEAR_URI}/get/${PEAR_P}.tgz"
-
-LICENSE="BSD"
-SLOT="0"
-KEYWORDS="amd64 hppa x86"
-IUSE=""
-
-DEPEND=">=dev-php/pear-1.9.4"
-RDEPEND="${DEPEND}
-	>=dev-php/File_Iterator-1.3.0
-	>=dev-php/PHP_TokenStream-1.1.3
-	>=dev-php/Text_Template-1.1.1"

dev-php/PHP_CodeCoverage/PHP_CodeCoverage-4.0.7-r1.ebuild
@@ -1,47 +1,0 @@
-# Copyright 1999-2018 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=7
-
-MY_PN="php-code-coverage"
-
-DESCRIPTION="Collection, processing, and rendering for PHP code coverage"
-HOMEPAGE="http://phpunit.de"
-SRC_URI="https://github.com/sebastianbergmann/${MY_PN}/archive/${PV}.tar.gz -> ${P}.tar.gz"
-
-LICENSE="BSD"
-SLOT="0"
-KEYWORDS="~alpha ~amd64 ~arm ~hppa ~ia64 ~ppc ~ppc64 ~s390 ~sh ~sparc ~x86"
-IUSE=""
-
-S="${WORKDIR}/${MY_PN}-${PV}"
-
-RDEPEND="dev-php/fedora-autoloader
-	>=dev-php/File_Iterator-1.3
-	<dev-php/File_Iterator-2.0
-	>=dev-php/Text_Template-1.2
-	<dev-php/Text_Template-2.0
-	>=dev-php/PHP_TokenStream-1.4
-	<dev-php/PHP_TokenStream-3.0
-	>=dev-php/sebastian-environment-1.3.2
-	<dev-php/sebastian-environment-3.0
-	>=dev-php/sebastian-version-1.0
-	<dev-php/sebastian-version-3.0
-	dev-php/sebastian-code-unit-reverse-lookup
-	<dev-php/sebastian-code-unit-reverse-lookup-2.0
-	|| (
-		dev-lang/php:5.6[xml,xmlwriter]
-		dev-lang/php:7.0[xml,xmlwriter]
-		dev-lang/php:7.1[xml,xmlwriter]
-	)"
-
-src_install() {
-	insinto /usr/share/php/PHP/CodeCoverage
-	doins -r src/*
-	doins "${FILESDIR}/autoload.php"
-}
-
-pkg_postinst() {
-	ewarn "This library now loads via /usr/share/php/PHP/CodeCoverage/autoload.php"
-	ewarn "Please update any scripts to require the autoloader"
-}

dev-php/PHP_CodeCoverage/files/channel.xml
@@ -1,16 +1,0 @@
-<?xml version="1.0" encoding="UTF-8" ?>
-<channel version="1.0" xmlns="http://pear.php.net/channel-1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://pear.php.net/channel-1.0 http://pear.php.net/dtd/channel-1.0.xsd">
-    <name>pear.phpunit.de</name>
-    <summary>PHPUnit PEAR Channel</summary>
-    <suggestedalias>phpunit</suggestedalias>
-    <servers>
-        <primary>
-            <rest>
-                <baseurl type="REST1.0">http://pear.phpunit.de/rest/</baseurl>
-                <baseurl type="REST1.1">http://pear.phpunit.de/rest/</baseurl>
-                <baseurl type="REST1.2">http://pear.phpunit.de/rest/</baseurl>
-                <baseurl type="REST1.3">http://pear.phpunit.de/rest/</baseurl>
-            </rest>
-        </primary>
-    </servers>
-</channel>

commit 990b69eb483a64599654eafff73d0afb32b58eb7

dev-php/PHP_CodeCoverage: stable 4.0.7-r2 for hppa under ALLARCHES policy
Fixes: 4dd021ff78c52b61bb16fe78b2627a6fe6a184e7
Package-Manager: Portage-2.3.89, Repoman-2.3.20
RepoMan-Options: --include-arches="hppa"
Signed-off-by: Rolf Eike Beer <eike@sf-mail.de>
Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

dev-php/PHP_CodeCoverage/PHP_CodeCoverage-4.0.7-r2.ebuild
@@ -11,7 +11,7 @@ SRC_URI="https://github.com/sebastianbergmann/${MY_PN}/archive/${PV}.tar.gz -> $
 
 LICENSE="BSD"
 SLOT="0"
-KEYWORDS="~alpha amd64 arm ~hppa ~ia64 ppc ppc64 ~s390 sparc x86"
+KEYWORDS="~alpha amd64 arm hppa ~ia64 ppc ppc64 ~s390 sparc x86"
 IUSE=""
 
 S="${WORKDIR}/${MY_PN}-${PV}"