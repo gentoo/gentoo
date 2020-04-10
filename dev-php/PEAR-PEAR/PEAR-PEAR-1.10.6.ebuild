gentoo/gentoo

commit 8658b7e01b3a15a2aa9db27bd8572f017601783f

dev-php/PEAR-PEAR: Stable 1.10.6 under ALLARCHES policy
Bug: https://bugs.gentoo.org/666510
Package-Manager: Portage-2.3.49, Repoman-2.3.10

dev-php/PEAR-PEAR/PEAR-PEAR-1.10.6.ebuild
@@ -10,7 +10,7 @@ DESCRIPTION="PEAR Base System"
 HOMEPAGE="http://pear.php.net/package/${MY_PN}"
 SRC_URI="http://pear.php.net/get/${MY_P}.tgz"
 LICENSE="MIT"
-KEYWORDS="~alpha ~amd64 ~arm ~hppa ia64 ~ppc ~ppc64 ~s390 ~sh ~sparc ~x86"
+KEYWORDS="alpha amd64 arm ~hppa ia64 ppc ppc64 ~s390 ~sh sparc x86"
 SLOT="0"
 IUSE=""
 

commit 2c2aadd77c57cc2f82d83735a219ee37f38ef577

dev-php/PEAR-PEAR: stable 1.10.6 for hppa under ALLARCHES, bug #666510
Fixes: 8658b7e01b3a15a2aa9db27bd8572f017601783f
Package-Manager: Portage-2.3.89, Repoman-2.3.20
RepoMan-Options: --include-arches="hppa"
Signed-off-by: Rolf Eike Beer <eike@sf-mail.de>
Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

dev-php/PEAR-PEAR/PEAR-PEAR-1.10.6.ebuild
@@ -10,7 +10,7 @@ DESCRIPTION="PEAR Base System"
 HOMEPAGE="https://pear.php.net/package/PEAR"
 SRC_URI="https://pear.php.net/get/${MY_P}.tgz"
 LICENSE="MIT"
-KEYWORDS="~alpha amd64 arm ~hppa ~ia64 ppc ppc64 ~s390 sparc x86"
+KEYWORDS="~alpha amd64 arm hppa ~ia64 ppc ppc64 ~s390 sparc x86"
 SLOT="0"
 IUSE=""
 