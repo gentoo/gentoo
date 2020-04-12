gentoo/gentoo

commit 4c53e8798031cfdcef17d4158ce52e8bc00b3216

dev-python/pydot: 1.4.1 allarches stablized
Stablized for Openstack Stein

Package-Manager: Portage-2.3.62, Repoman-2.3.12
Signed-off-by: Matthew Thode <prometheanfire@gentoo.org>

dev-python/pydot/pydot-1.4.1.ebuild
@@ -13,7 +13,7 @@ SRC_URI="https://github.com/erocarrera/pydot/archive/v${PV}.tar.gz -> ${P}.tar.g
 
 LICENSE="MIT"
 SLOT="0"
-KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~mips ~ppc ~ppc64 ~s390 ~sh ~sparc ~x86 ~amd64-fbsd ~x86-fbsd ~amd64-linux ~ppc-macos ~x64-macos ~x86-macos ~sparc-solaris"
+KEYWORDS="alpha amd64 arm ~arm64 hppa ia64 ~mips ppc ppc64 s390 ~sh ~sparc x86 ~amd64-fbsd ~x86-fbsd ~amd64-linux ~ppc-macos ~x64-macos ~x86-macos ~sparc-solaris"
 IUSE="test"
 
 RDEPEND="

commit f644a992db6de3f543361eab9fe76a0eb50e7362

dev-python/pydot: stable 1.4.1 for sparc under ALLARCHES
Fixes: 4c53e8798031cfdcef17d4158ce52e8bc00b3216
Package-Manager: Portage-2.3.89, Repoman-2.3.20
RepoMan-Options: --include-arches="sparc"
Signed-off-by: Rolf Eike Beer <eike@sf-mail.de>
Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

dev-python/pydot/pydot-1.4.1.ebuild
@@ -13,7 +13,7 @@ SRC_URI="https://github.com/erocarrera/pydot/archive/v${PV}.tar.gz -> ${P}.tar.g
 
 LICENSE="MIT"
 SLOT="0"
-KEYWORDS="~alpha amd64 arm ~arm64 hppa ~ia64 ~mips ppc ppc64 s390 ~sparc x86 ~amd64-linux ~ppc-macos ~x64-macos ~x86-macos ~sparc-solaris"
+KEYWORDS="~alpha amd64 arm ~arm64 hppa ~ia64 ~mips ppc ppc64 s390 sparc x86 ~amd64-linux ~ppc-macos ~x64-macos ~x86-macos ~sparc-solaris"
 IUSE="test"
 RESTRICT="!test? ( test )"
 