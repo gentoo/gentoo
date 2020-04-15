gentoo/gentoo

commit 6df79dfa10bd801c99c1698f81701f9ae0aa31d9

dev-python/pexpect: 4.8.0 + QA
Closes: https://bugs.gentoo.org/703100
Signed-off-by: Sebastian Pipping <sping@gentoo.org>
Package-Manager: Portage-2.3.99, Repoman-2.3.22

dev-python/pexpect/pexpect-4.8.0.ebuild
@@ -1,0 +1,45 @@
+# Copyright 1999-2020 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+
+PYTHON_COMPAT=( python2_7 python3_{6,7,8} pypy3 )
+PYTHON_REQ_USE="threads(+)"
+
+inherit distutils-r1
+
+DESCRIPTION="Python module for spawning child apps and responding to expected patterns"
+HOMEPAGE="https://pexpect.readthedocs.io/ https://pypi.org/project/pexpect/ https://github.com/pexpect/pexpect/"
+SRC_URI="mirror://pypi/${PN:0:1}/${PN}/${P}.tar.gz"
+
+LICENSE="MIT"
+SLOT="0"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~m68k ~mips ~ppc ~ppc64 ~s390 ~sparc ~x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~x86-macos"
+IUSE="doc examples"
+
+RDEPEND=">=dev-python/ptyprocess-0.5[${PYTHON_USEDEP}]"
+DEPEND="
+	doc? ( dev-python/sphinx )"
+
+PATCHES=(
+	"${FILESDIR}"/${P}-sphinx-3.patch
+)
+
+distutils_enable_tests pytest
+
+python_compile_all() {
+	use doc && emake -C doc html
+}
+
+python_install_all() {
+	use doc && local HTML_DOCS=( doc/_build/html/. )
+	if use examples; then
+		dodoc -r examples
+		docompress -x /usr/share/doc/${PF}/examples
+	fi
+	distutils-r1_python_install_all
+
+	# Address byte-compile QA warning, see https://bugs.gentoo.org/703100
+	python_setup -2
+	rm "${D}$(python_get_sitedir)"/pexpect/_async.py || die
+}

dev-python/pexpect/Manifest
@@ -1,1 +1,2 @@
 DIST pexpect-4.6.0.tar.gz 148966 BLAKE2B 0ed1a3c31133bf618e4c75ea099efaa9bda9e2b7f92f46c54ad553ecc56487e5415b750334376a95599ada53e17e91efe7d4b65ad80bb4f6a1cf9d655a3c9685 SHA512 9a2b1ef19d885e69cfd17ba0519581f782574019010043e66d8b68f98ac86b359f4a1ca42f8bb0059a5dfdd43275ad93531c076cc0ac2049e109408961b46bbd
+DIST pexpect-4.8.0.tar.gz 157037 BLAKE2B 742642bd6b9ec3f6cdfad054d4fd22db56b4a55b746d675c27a8cdf824ea749ec4589e296dffa08778195f3ccd20feb56bc0fd5212984396ea5aa0555c41ca96 SHA512 7447ae2d1e13be422c894a8fd51c5aaa788e37ea7f0c798c88b77afd401fb3631400a637077ccbb83c2e3876b0d0c5e1dbd5fdc9d3739d785b4d5ad7c0192580

dev-python/pexpect/files/pexpect-4.8.0-sphinx-3.patch
@@ -1,0 +1,25 @@
+From 506bcd97fa1d20d1412d399353c38f0ff3546a1a Mon Sep 17 00:00:00 2001
+From: Sebastian Pipping <sebastian@pipping.org>
+Date: Mon, 13 Apr 2020 23:26:09 +0200
+Subject: [PATCH] Fix compilation of docs with Sphinx 3.0.1
+
+https://github.com/pexpect/pexpect/issues/637
+---
+ doc/sphinxext/github.py | 1 -
+ 1 file changed, 1 deletion(-)
+
+diff --git a/doc/sphinxext/github.py b/doc/sphinxext/github.py
+index 519e146..771dccc 100644
+--- a/doc/sphinxext/github.py
++++ b/doc/sphinxext/github.py
+@@ -146,7 +146,6 @@ def setup(app):
+     
+     :param app: Sphinx application context.
+     """
+-    app.info('Initializing GitHub plugin')
+     app.add_role('ghissue', ghissue_role)
+     app.add_role('ghpull', ghissue_role)
+     app.add_role('ghuser', ghuser_role)
+-- 
+2.26.0
+

commit a094582d244bad3a87c2edb7effc0f528ca20466

dev-python/pexpect: fix build with python2 disabled
Fixes: 6df79dfa10bd801c99c1698f81701f9ae0aa31d9
Bug: https://bugs.gentoo.org/703100
Package-Manager: Portage-2.3.99, Repoman-2.3.22_p29
Signed-off-by: Mike Gilbert <floppym@gentoo.org>

dev-python/pexpect/pexpect-4.8.0.ebuild
@@ -31,6 +31,14 @@ python_compile_all() {
 	use doc && emake -C doc html
 }
 
+python_install() {
+	distutils-r1_python_install
+	if ! python_is_python3; then
+		# https://bugs.gentoo.org/703100
+		rm "${D}$(python_get_sitedir)/pexpect/_async.py" || die
+	fi
+}
+
 python_install_all() {
 	use doc && local HTML_DOCS=( doc/_build/html/. )
 	if use examples; then
@@ -38,8 +46,4 @@ python_install_all() {
 		docompress -x /usr/share/doc/${PF}/examples
 	fi
 	distutils-r1_python_install_all
-
-	# Address byte-compile QA warning, see https://bugs.gentoo.org/703100
-	python_setup -2
-	rm "${D}$(python_get_sitedir)"/pexpect/_async.py || die
 }