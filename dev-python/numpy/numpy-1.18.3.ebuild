gentoo/gentoo

commit 48dd16c7839fdfc87c5d32b7e23959e9e48ed52d

dev-python/numpy: bump version to 1.18.3
Signed-off-by: Victor Payno <vpayno+gentoo@gmail.com>
Signed-off-by: Patrick McLean <chutzpah@gentoo.org>

dev-python/numpy/numpy-1.18.3.ebuild
@@ -1,0 +1,152 @@
+# Copyright 1999-2020 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+
+PYTHON_COMPAT=( python3_{6,7,8} )
+PYTHON_REQ_USE="threads(+)"
+
+FORTRAN_NEEDED=lapack
+
+DISTUTILS_USE_SETUPTOOLS=rdepend
+
+inherit autotools distutils-r1 flag-o-matic fortran-2 multiprocessing toolchain-funcs
+
+DOC_PV="1.16.4"
+DESCRIPTION="Fast array and numerical python library"
+HOMEPAGE="https://www.numpy.org"
+SRC_URI="
+	mirror://pypi/${PN:0:1}/${PN}/${P}.zip
+	doc? (
+		https://numpy.org/doc/$(ver_cut 1-2 ${DOC_PV})/numpy-html.zip -> numpy-html-${DOC_PV}.zip
+		https://numpy.org/doc/$(ver_cut 1-2 ${DOC_PV})/numpy-ref.pdf -> numpy-ref-${DOC_PV}.pdf
+		https://numpy.org/doc/$(ver_cut 1-2 ${DOC_PV})/numpy-user.pdf -> numpy-user-${DOC_PV}.pdf
+	)"
+LICENSE="BSD"
+SLOT="0"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~x86"
+IUSE="doc lapack"
+
+RDEPEND="
+	lapack? (
+		>=virtual/cblas-3.8
+		>=virtual/lapack-3.8
+	)
+"
+BDEPEND="
+	${RDEPEND}
+	app-arch/unzip
+	>=dev-python/cython-0.29.15[${PYTHON_USEDEP}]
+	lapack? (
+		>=sys-devel/automake-1.16.2
+		>=sys-devel/gcc-6.5.0-r1[fortran]
+		virtual/pkgconfig
+	)
+	test? (
+		>=dev-python/hypothesis-5.8.0[${PYTHON_USEDEP}]
+		>=dev-python/pytz-2019.3[${PYTHON_USEDEP}]
+		>=dev-python/cffi-1.14.0[${PYTHON_USEDEP}]
+	)
+"
+
+PATCHES=(
+	"${FILESDIR}"/${PN}-1.17.4-no-hardcode-blasv2.patch
+)
+
+distutils_enable_tests pytest
+
+src_unpack() {
+	default
+	if use doc; then
+		unzip -qo "${DISTDIR}"/numpy-html-${DOC_PV}.zip -d html || die
+	fi
+}
+
+src_prepare() {
+	default
+	eautomake
+}
+
+python_prepare_all() {
+	if use lapack; then
+		local incdir="${EPREFIX}"/usr/include
+		local libdir="${EPREFIX}"/usr/$(get_libdir)
+		cat >> site.cfg <<-EOF || die
+			[blas]
+			include_dirs = ${incdir}
+			library_dirs = ${libdir}
+			blas_libs = cblas,blas
+			[lapack]
+			library_dirs = ${libdir}
+			lapack_libs = lapack
+		EOF
+	else
+		export {ATLAS,PTATLAS,BLAS,LAPACK,MKL}=None
+	fi
+
+	export CC="$(tc-getCC) ${CFLAGS}"
+
+	append-flags -fno-strict-aliasing
+
+	# See progress in http://projects.scipy.org/scipy/numpy/ticket/573
+	# with the subtle difference that we don't want to break Darwin where
+	# -shared is not a valid linker argument
+	if [[ ${CHOST} != *-darwin* ]]; then
+		append-ldflags -shared
+	fi
+
+	# only one fortran to link with:
+	# linking with cblas and lapack library will force
+	# autodetecting and linking to all available fortran compilers
+	append-fflags -fPIC
+	if use lapack; then
+		NUMPY_FCONFIG="config_fc --noopt --noarch"
+		# workaround bug 335908
+		[[ $(tc-getFC) == *gfortran* ]] && NUMPY_FCONFIG+=" --fcompiler=gnu95"
+	fi
+
+	# don't version f2py, we will handle it.
+	sed -i -e '/f2py_exe/s: + os\.path.*$::' numpy/f2py/setup.py || die
+
+	# disable fuzzed tests
+	find numpy/*/tests -name '*.py' -exec sed -i \
+		-e 's:def \(.*_fuzz\):def _\1:' {} + || die
+	# very memory- and disk-hungry
+	sed -i -e 's:test_large_zip:_&:' numpy/lib/tests/test_io.py || die
+
+	distutils-r1_python_prepare_all
+}
+
+python_compile() {
+	distutils-r1_python_compile ${NUMPY_FCONFIG}
+}
+
+python_test() {
+	distutils_install_for_testing --single-version-externally-managed \
+		--record "${TMPDIR}/record.txt" ${NUMPY_FCONFIG}
+
+	cd "${TMPDIR}" || die
+
+	"${EPYTHON}" -c "
+import numpy, sys
+r = numpy.test(label='full', verbose=3)
+sys.exit(0 if r else 1)" || die "Tests fail with ${EPYTHON}"
+}
+
+python_install() {
+	# https://github.com/numpy/numpy/issues/16005
+	local mydistutilsargs=( build_src )
+	distutils-r1_python_install ${NUMPY_FCONFIG}
+	python_optimize
+}
+
+python_install_all() {
+	local DOCS=( LICENSE.txt README.md THANKS.txt )
+
+	if use doc; then
+		local HTML_DOCS=( "${WORKDIR}"/html/. )
+		DOCS+=( "${DISTDIR}"/${PN}-{user,ref}-${DOC_PV}.pdf )
+	fi
+
+	distutils-r1_python_install_all
+}

dev-python/numpy/Manifest
@@ -1,5 +1,6 @@
 DIST numpy-1.16.5.zip 5138208 BLAKE2B f5511650e66939a6ce21cfad7874aecdeb6c07a70b32419a808956e42c7819d82bb364f1632da91b4d8a5237001c8ae3cbdcac10651fa0acca9558bdeafb5437 SHA512 6dd790ff57fdb7e7b0bddb47ea2b70b2667edefe632548ec68934e12a16af4a51bed9726e495c4993aad4c448dc4a204d0a087577d43dcfc9fa0164eb1d4b898
 DIST numpy-1.17.4.zip 6366657 BLAKE2B 89f729bb0740fa721dfe8d1165094a96302fde1e8c272689c11cb4a9bb39d11afaf6eda3ded8a370333b47b9d1ee25e8cb0a602c8cf44964612342c50ea5824f SHA512 f3264be7152437d71718da4728222962013ebd7fe02288d5063f1c763d1c4eee26485c27e8088f4dff79a4d491bc9f191849748c6007d093d22e89c6bff317f3
+DIST numpy-1.18.3.zip 5435268 BLAKE2B aaf14627ca24e27806e6a5498a8b91f85c2582794db2ef2ec315e88b107435aeb2e8f288c68ba00593150787d6deb887faa029f850df4c6cb65426499f7ee83f SHA512 afb14dec81ade556e10b7de372e9d7b01b4bf246609c5eea26ad76ce93e6d9caa95479ae70dbfb725f28ac4802f94203c92e1e991852d2866b5225fc9d68e68d
 DIST numpy-html-1.16.4.zip 12097069 BLAKE2B db1f897342fbc80a761fbaee7b0eee3761dbfa04ef6c46a137bb2392caaec8256580467ce06498cceeada2639151a1ec75dafab484d64cb897c008727d6d32a5 SHA512 c44c5e1e0cb22f9dd46fe9e302030292d85e567456178fee296746a414271634e906509fb6665ac63fbfebdd13c5383574f0aa47b4fbc639063da31addc3316a
 DIST numpy-ref-1.16.4.pdf 5176571 BLAKE2B 9a875ebd1473b241d463d810a6d191581aef961158f2f82959671b554ace3ad482795e4f3e1cb7dd4632cff926c1e864c675a7624fb0f4e4f297948bf50f0564 SHA512 983ba0f34a70c011886bdbc9fd8f3f75a3a967ca29217acc76fa46e7da391296a5628c9a557ac76efb393271abfb8ee4f376d401c1cc1e5c30622e54b9325d09
 DIST numpy-user-1.16.4.pdf 596203 BLAKE2B da3ad1f0ec1c965d20656f73a970911cbb58efa73a5df89e2e6485e0dd763f483e35eea1395ac919bf35d496fb216cee954fc5d31edebc1796a24a2a7a2d12b9 SHA512 3083008547213bfa98ffa5cd8e2cea13f6947f42d83aef61502a2a852b0d5f27d727c76b42d281d7a2f4f76c78e88b12ee3d9b164b39fe1f0213ace7097357d6

commit 0d784579f7d2fa66ab44c80e2ff0fba353f08dbf

dev-python/numpy: fix regressions introduced by 48dd16c7839fd.
Bug: https://github.com/gentoo/gentoo/pull/15315
Closes: https://bugs.gentoo.org/660754
Package-Manager: Portage-2.3.88, Repoman-2.3.18
Signed-off-by: Benda Xu <heroxbd@gentoo.org>

dev-python/numpy/numpy-1.18.3.ebuild
@@ -10,7 +10,7 @@ FORTRAN_NEEDED=lapack
 
 DISTUTILS_USE_SETUPTOOLS=rdepend
 
-inherit autotools distutils-r1 flag-o-matic fortran-2 multiprocessing toolchain-funcs
+inherit distutils-r1 flag-o-matic fortran-2 multiprocessing toolchain-funcs
 
 DOC_PV="1.16.4"
 DESCRIPTION="Fast array and numerical python library"
@@ -24,7 +24,7 @@ SRC_URI="
 	)"
 LICENSE="BSD"
 SLOT="0"
-KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~x86"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~mips ~ppc ~ppc64 ~s390 ~sparc ~x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~x86-macos ~sparc-solaris ~x64-solaris ~x86-solaris"
 IUSE="doc lapack"
 
 RDEPEND="
@@ -37,11 +37,7 @@ BDEPEND="
 	${RDEPEND}
 	app-arch/unzip
 	>=dev-python/cython-0.29.15[${PYTHON_USEDEP}]
-	lapack? (
-		>=sys-devel/automake-1.16.2
-		>=sys-devel/gcc-6.5.0-r1[fortran]
-		virtual/pkgconfig
-	)
+	lapack? ( virtual/pkgconfig )
 	test? (
 		>=dev-python/hypothesis-5.8.0[${PYTHON_USEDEP}]
 		>=dev-python/pytz-2019.3[${PYTHON_USEDEP}]
@@ -60,11 +56,6 @@ src_unpack() {
 	if use doc; then
 		unzip -qo "${DISTDIR}"/numpy-html-${DOC_PV}.zip -d html || die
 	fi
-}
-
-src_prepare() {
-	default
-	eautomake
 }
 
 python_prepare_all() {
@@ -118,6 +109,8 @@ python_prepare_all() {
 }
 
 python_compile() {
+	export MAKEOPTS=-j1 #660754
+
 	distutils-r1_python_compile ${NUMPY_FCONFIG}
 }
 

profiles/arch/powerpc/package.use.mask
@@ -1,6 +1,10 @@
 # Copyright 1999-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
+# Benda Xu <heroxbd@gentoo.org> (2020-04-27)
+# tests optionally depend on dev-python/hypothesis
+dev-python/numpy test
+
 # Lars Wendler <polynomial-c@gentoo.org> (2020-04-20)
 # Not keyworded on ppc
 net-misc/whois xcrypt

profiles/arch/sparc/package.use.mask
@@ -1,6 +1,10 @@
 # Copyright 1999-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
+# Benda Xu <heroxbd@gentoo.org> (2020-04-27)
+# tests optionally depend on dev-python/hypothesis
+dev-python/numpy test
+
 # Rolf Eike Beer <eike@sf-mail.de> (2020-04-26)
 # tests optionally depend on dev-ruby/coffee-script needing nodejs
 dev-ruby/tilt test