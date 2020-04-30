gentoo/gentoo

commit 95b02b1136ba1544e770910644daf4fc804c3ec9

app-metrics/collectd: add Python 3.8 support
Package-Manager: Portage-2.3.99, Repoman-2.3.22
Signed-off-by: Thomas Deutschmann <whissi@gentoo.org>

app-metrics/collectd/collectd-5.11.0.ebuild
@@ -3,7 +3,7 @@
 
 EAPI="6"
 
-PYTHON_COMPAT=( python{3_6,3_7} )
+PYTHON_COMPAT=( python3_{6,3,7} )
 JAVA_PKG_OPT_USE="collectd_plugins_java"
 
 inherit autotools fcaps flag-o-matic java-pkg-opt-2 linux-info multilib perl-functions python-single-r1 systemd tmpfiles user

app-metrics/collectd/collectd-5.10.0.ebuild
@@ -3,7 +3,7 @@
 
 EAPI="6"
 
-PYTHON_COMPAT=( python{3_6,3_7} )
+PYTHON_COMPAT=( python3_{6,7,8} )
 JAVA_PKG_OPT_USE="collectd_plugins_java"
 
 inherit autotools fcaps flag-o-matic java-pkg-opt-2 linux-info multilib perl-functions python-single-r1 systemd tmpfiles user

commit 83fd5efb8c4913b8e7d6b85ac73f54b9fa7250cc

app-metrics/collectd: really enable Python 3.8
Fixes: 95b02b11 ("app-metrics/collectd: add Python 3.8 support")
Package-Manager: Portage-2.3.99, Repoman-2.3.22
Signed-off-by: Thomas Deutschmann <whissi@gentoo.org>

app-metrics/collectd/collectd-5.11.0.ebuild
@@ -3,7 +3,7 @@
 
 EAPI="6"
 
-PYTHON_COMPAT=( python3_{6,3,7} )
+PYTHON_COMPAT=( python3_{6,7,8} )
 JAVA_PKG_OPT_USE="collectd_plugins_java"
 
 inherit autotools fcaps flag-o-matic java-pkg-opt-2 linux-info multilib perl-functions python-single-r1 systemd tmpfiles user