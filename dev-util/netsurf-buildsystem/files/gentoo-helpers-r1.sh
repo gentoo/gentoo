ae58cbc4df32bfc463141f4b8d93012ae86485c6
Jaco Kroon <jaco@uls.co.za>
2020-05-03 19:44:15 +0200

    dev-util/netsurf-buildsystem: BUILD_CC, CXX and BUILD_CXX
    Closes: https://bugs.gentoo.org/718010
    Package-Manager: Portage-2.3.89, Repoman-2.3.20
    Signed-off-by: Jaco Kroon <jaco@uls.co.za>
    Closes: https://github.com/gentoo/gentoo/pull/15629
    Signed-off-by: Joonas Niilola <juippis@gentoo.org>

--- dev-util/netsurf-buildsystem/files/gentoo-helpers-r1.sh
+++ dev-util/netsurf-buildsystem/files/gentoo-helpers-r1.sh
@@ -9,6 +9,9 @@ netsurf_define_makeconf() {
 		CC="$(tc-getCC)"
 		LD="$(tc-getLD)"
 		HOST_CC="\$(CC)"
+		BUILD_CC="$(tc-getBUILD_CC)"
+		CXX="$(tc-getCXX)"
+		BUILD_CXX="$(tc-getBUILD_CXX)"
 		CCOPT=
 		CCNOOPT=
 		CCDBG=
