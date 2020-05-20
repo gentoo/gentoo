gentoo/gentoo

commit b0222a2948853f723eb7b52e0b972702f1684b29

net-analyzer/wireshark: Remove /usr/share/appdata
Package-Manager: Portage-2.3.99, Repoman-2.3.22
Signed-off-by: Jeroen Roovers <jer@gentoo.org>

net-analyzer/wireshark/wireshark-3.2.4.ebuild
@@ -225,6 +225,8 @@ src_install() {
 			newins image/WiresharkDoc-${s}.png application-vnd.tcpdump.pcap.png
 		done
 	fi
+
+	rm -r "${D}"/usr/share/appdata || die
 }
 
 pkg_postinst() {

net-analyzer/wireshark/wireshark-99999999.ebuild
@@ -219,6 +219,8 @@ src_install() {
 			newins image/WiresharkDoc-${s}.png application-vnd.tcpdump.pcap.png
 		done
 	fi
+
+	rm -r "${D}"/usr/share/appdata || die
 }
 
 pkg_postinst() {

commit 5a61d35c67d0c12cae64b104107ccab3d216eb5e

net-analyzer/wireshark: Remove /usr/share/appdata when present
Package-Manager: Portage-2.3.99, Repoman-2.3.22
Fixes: b0222a2948853f723eb7b52e0b972702f1684b29
Signed-off-by: Jeroen Roovers <jer@gentoo.org>

net-analyzer/wireshark/wireshark-3.2.4.ebuild
@@ -226,7 +226,9 @@ src_install() {
 		done
 	fi
 
-	rm -r "${D}"/usr/share/appdata || die
+	if [[ -d "${D}"/usr/share/appdata ]]; then
+		rm -r "${D}"/usr/share/appdata || die
+	fi
 }
 
 pkg_postinst() {

net-analyzer/wireshark/wireshark-99999999.ebuild
@@ -220,7 +220,9 @@ src_install() {
 		done
 	fi
 
-	rm -r "${D}"/usr/share/appdata || die
+	if [[ -d "${D}"/usr/share/appdata ]]; then
+		rm -r "${D}"/usr/share/appdata || die
+	fi
 }
 
 pkg_postinst() {