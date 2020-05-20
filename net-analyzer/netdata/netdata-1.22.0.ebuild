gentoo/gentoo

commit 0521c7e831f3ee976bd81a1942a766ae8d33db49

net-analyzer/netdata: Disable Netdata Cloud
Bug: https://github.com/netdata/netdata/issues/8961
Package-Manager: Portage-2.3.99, Repoman-2.3.22
Signed-off-by: Craig Andrews <candrews@gentoo.org>

net-analyzer/netdata/netdata-9999.ebuild
@@ -98,6 +98,7 @@ src_configure() {
 	econf \
 		--localstatedir="${EPREFIX}"/var \
 		--with-user=netdata \
+		--disable-cloud \ # https://github.com/netdata/netdata/issues/8961
 		$(use_enable jsonc) \
 		$(use_enable cups plugin-cups) \
 		$(use_enable dbengine) \

commit 76d83e996cc0303245dbb0617014ac7100e887be

net-analyzer/netdata: fix ebuild ignoring USE
An ebuild syntax error (stray comment breaking backslash line continuation)
introduced in 0521c7e8 causes ./configure to issue a number of warnings
and failes to pass any arguments to it.

net-analyzer/netdata/netdata-1.22.0.ebuild
@@ -95,10 +95,11 @@ src_prepare() {
 }
 
 src_configure() {
+	# --disable-cloud: https://github.com/netdata/netdata/issues/8961
 	econf \
 		--localstatedir="${EPREFIX}"/var \
 		--with-user=netdata \
-		--disable-cloud \ # https://github.com/netdata/netdata/issues/8961
+		--disable-cloud \
 		$(use_enable jsonc) \
 		$(use_enable cups plugin-cups) \
 		$(use_enable dbengine) \

net-analyzer/netdata/netdata-1.22.1.ebuild
@@ -95,10 +95,11 @@ src_prepare() {
 }
 
 src_configure() {
+	# --disable-cloud: https://github.com/netdata/netdata/issues/8961
 	econf \
 		--localstatedir="${EPREFIX}"/var \
 		--with-user=netdata \
-		--disable-cloud \ # https://github.com/netdata/netdata/issues/8961
+		--disable-cloud \
 		$(use_enable jsonc) \
 		$(use_enable cups plugin-cups) \
 		$(use_enable dbengine) \

net-analyzer/netdata/netdata-9999.ebuild
@@ -95,10 +95,11 @@ src_prepare() {
 }
 
 src_configure() {
+	# --disable-cloud: https://github.com/netdata/netdata/issues/8961
 	econf \
 		--localstatedir="${EPREFIX}"/var \
 		--with-user=netdata \
-		--disable-cloud \ # https://github.com/netdata/netdata/issues/8961
+		--disable-cloud \
 		$(use_enable jsonc) \
 		$(use_enable cups plugin-cups) \
 		$(use_enable dbengine) \