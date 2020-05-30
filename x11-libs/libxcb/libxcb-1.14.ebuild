gentoo/gentoo

commit 9b6a2669d8c2398497af3c9716ecafbcb15911d1

x11-libs/libxcb: move xcb-proto to BDEPEND
Closes: https://bugs.gentoo.org/558774
Signed-off-by: Mike Gilbert <floppym@gentoo.org>

x11-libs/libxcb/libxcb-1.14.ebuild
@@ -28,9 +28,10 @@ DEPEND="${RDEPEND}
 	test? ( dev-libs/check[${MULTILIB_USEDEP}] )
 	doc? ( app-doc/doxygen[dot] )
 	dev-libs/libxslt
-	${PYTHON_DEPS}
-	$(python_gen_any_dep \
-		">=x11-base/xcb-proto-1.14[${MULTILIB_USEDEP},\${PYTHON_USEDEP}]")"
+"
+BDEPEND="${PYTHON_DEPS}
+	$(python_gen_any_dep '>=x11-base/xcb-proto-1.14[${PYTHON_USEDEP}]')
+"
 
 python_check_deps() {
 	has_version -b ">=x11-base/xcb-proto-1.14[${PYTHON_USEDEP}]"

commit 64076dbcde1bf8db53e694f62fc1a775c2e18632

x11-libs/libxcb: Readd multilib dependency on xcb-proto
While fixing a build dependency with Python the target DEPEND lost
multilib references. This partially reverts to the correct behaviour

Ack-by: Matt Turner <mattst88@gentoo.org>
Closes: https://bugs.gentoo.org/724612
Fixes: 9b6a2669d8c2398497af3c9716ecafbcb15911d1
Signed-off-by: Brian Evans <grknight@gentoo.org>

x11-libs/libxcb/libxcb-1.14.ebuild
@@ -25,6 +25,7 @@ RDEPEND="
 	>=x11-libs/libXdmcp-1.1.1-r1[${MULTILIB_USEDEP}]"
 # Note: ${PYTHON_USEDEP} needs to go verbatim
 DEPEND="${RDEPEND}
+	>=x11-base/xcb-proto-1.14[${MULTILIB_USEDEP}]
 	test? ( dev-libs/check[${MULTILIB_USEDEP}] )
 	doc? ( app-doc/doxygen[dot] )
 	dev-libs/libxslt