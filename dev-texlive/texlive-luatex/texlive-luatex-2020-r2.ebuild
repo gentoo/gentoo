gentoo/gentoo

commit 1b3e9353ac014fb3601b7351d89accf3e73d1321

dev-texlive/texlive-luatex: add fontsrecommended dep
Closes: https://bugs.gentoo.org/show_bug.cgi?id=730006
Package-Manager: Portage-2.3.99, Repoman-2.3.22
Signed-off-by: Mikle Kolyada <zlogene@gentoo.org>

dev-texlive/texlive-luatex/texlive-luatex-2020-r2.ebuild
@@ -3,7 +3,7 @@
 
 EAPI="7"
 
-TEXLIVE_MODULE_CONTENTS="addliga auto-pst-pdf-lua barracuda bezierplot checkcites chickenize combofont cstypo ctablestack emoji enigma interpreter kanaparser lm lua-uca lua-ul lua-visual-debug luacode luacolor luahyphenrules luaimageembed luaindex luainputenc luaintro lualatex-doc lualatex-math lualatex-truncate lualibs luamplib luaotfload luapackageloader luarandom luatex85 luatexbase luatexko luatextra luavlna luaxml nodetree odsfile optex pdfarticle placeat plantuml selnolig spelling typewriter collection-luatex
+TEXLIVE_MODULE_CONTENTS="addliga auto-pst-pdf-lua barracuda bezierplot checkcites chickenize combofont cstypo ctablestack emoji enigma interpreter kanaparser lua-uca lua-ul lua-visual-debug luacode luacolor luahyphenrules luaimageembed luaindex luainputenc luaintro lualatex-doc lualatex-math lualatex-truncate lualibs luamplib luaotfload luapackageloader luarandom luatex85 luatexbase luatexko luatextra luavlna luaxml nodetree odsfile optex pdfarticle placeat plantuml selnolig spelling typewriter collection-luatex
 "
 TEXLIVE_MODULE_DOC_CONTENTS="addliga.doc auto-pst-pdf-lua.doc barracuda.doc bezierplot.doc checkcites.doc chickenize.doc combofont.doc cstypo.doc ctablestack.doc emoji.doc enigma.doc interpreter.doc kanaparser.doc lua-uca.doc lua-ul.doc lua-visual-debug.doc luacode.doc luacolor.doc luahyphenrules.doc luaimageembed.doc luaindex.doc luainputenc.doc luaintro.doc lualatex-doc.doc lualatex-math.doc lualatex-truncate.doc lualibs.doc luamplib.doc luaotfload.doc luapackageloader.doc luarandom.doc luatex85.doc luatexbase.doc luatexko.doc luatextra.doc luavlna.doc luaxml.doc nodetree.doc odsfile.doc optex.doc pdfarticle.doc placeat.doc plantuml.doc selnolig.doc spelling.doc typewriter.doc "
 TEXLIVE_MODULE_SRC_CONTENTS="chickenize.source ctablestack.source lua-uca.source lua-ul.source luacode.source luacolor.source luaindex.source luainputenc.source lualatex-doc.source lualatex-math.source lualatex-truncate.source lualibs.source luamplib.source luaotfload.source luatex85.source luatexbase.source luatextra.source nodetree.source placeat.source "
@@ -17,6 +17,7 @@ IUSE=""
 DEPEND=">=dev-texlive/texlive-basic-2020
 "
 RDEPEND="${DEPEND} dev-texlive/texlive-latexrecommended
+dev-texlive/texlive-fontsrecommended
 "
 TEXLIVE_MODULE_BINSCRIPTS="
 	texmf-dist/scripts/luaotfload/luaotfload-tool.lua

dev-texlive/texlive-luatex/Manifest
@@ -27,7 +27,6 @@ DIST tl-interpreter-2020.tar.xz 6224 BLAKE2B 3059e1455571891f241dc68c574e5d50a77
 DIST tl-interpreter.doc-2020.tar.xz 122152 BLAKE2B 462274df025e3e18fefc2ee1f305097214653200f2a7c30535d8fe88adeb44208bbefcb21c1bc4ea22ac941e3aaf82ed37d7dff87605b59cbddc3d96a0c288fc SHA512 3408bcb3cd3e6da67294c830870534d3cd620b7abc3ad35d4833e30c831bf93d11dc06d00547888268870c75f6a6e2e861448a0fa197e3e5a3c10aaccc6e4dd8
 DIST tl-kanaparser-2020.tar.xz 4620 BLAKE2B 68674ab8fed99591ebbf664e39e696f8f0fd1b1b6079157b8536091797914950948617e1d879a332bc44f0cd239411daa03762459622e49a97f73abd898a7591 SHA512 a9db1fc66330bafc5fc26ed600f1779dce328d6234ebe930aaff65a02a0d740188e73c5f73a9c75d69926323a610dca4b37044e7357a141eae9bb3231fd3e272
 DIST tl-kanaparser.doc-2020.tar.xz 367048 BLAKE2B d3acc70338bf10ec26ffcee1b4a696b859e6ad143e7599dd0d31d7629e1b435de25a98be48a018e9e0f59a43e194d16ece7b1d67d598b7c692611fd4a91edd60 SHA512 9eb00a9652f22411f9ae0123bd2bda62af617f39ed3eb8266c4ca38ff0783722a3d1eb05a0fe1ef6a238a3132ade65c5ebd882309f154ca416e2191b9db44c4c
-DIST tl-lm-2020.tar.xz 11944808 BLAKE2B 0da72d50ed6681c9b473a24af1cc8b627377e64390f8d240a9d13d9a924168cc98fe655db52ed44027327e642bb3a11afafbe1fcbe2f52554edbcb843ad2c908 SHA512 f94af6c169ee05d3a2dceb00794feffb7808a4bedc05da7655ea74fbbf675047661f49f4f2542373e5fef60fc0f91ea9931b25bb10c1449f593b84ebf65cd154
 DIST tl-lua-uca-2020.tar.xz 588 BLAKE2B 2284226e2ba729ef956e1a16bb6b2c497b324275aca3876a1d6420232716751119d0be1469e8a431c2f7ecab6405d45b17d394006fc44ed4b78e72712be82499 SHA512 b131db0ac33891becaf6f3ac778e462ca1dd1b5b63de86bd9f46da5cf562dd43a55c7818f38c9ff1b9a3315ff5e89962d8b21d45953b8dd0eb8a013e6eaf0783
 DIST tl-lua-uca.doc-2020.tar.xz 58412 BLAKE2B 683332d997001c9d7e82c3945e095da2710f541a37899fecf125a19afdf3381ad402e591bf596604b51f16140643513e197cd94aa9033decb70f7bafe8efcc21 SHA512 a60dce7f573dc23246366f7ddc4b74a303b1bea889c5f4c646a35f4bbf84427ac54c65b8e8af93dfc3c30bb96f31dd5eb04f2f203db4ecdfb7c386febf34088e
 DIST tl-lua-uca.source-2020.tar.xz 70916 BLAKE2B d83efc49811073edebfcdc7118100c41b40d7b1cfe36d6a566d3179838c3edd18555b49b5a7ebb83e237077a2f0eff566eb4f8e1cb0ab41402cdf7e1a4286bea SHA512 1cabe6647dd31e145c47ea99db72fd90a8a922d6d09bf3a4806e0c329a5a192a48a6a34ae1c187eaf1363a142f389942ce62f07eb5469311f9e6c6d9a03b1106

commit 56c2aa96489ea150844f7b26cfe82b9ebdf082b7

dev-texlive/texlive-luatex: Drop redundant dependency.
Commit 1b3e9353ac01 and commit 73f8e6f7a42d both removed the lm module
from TEXLIVE_MODULE_CONTENTS and added a dependency on
texlive-fontsrecommended in RDEPEND and DEPEND, respectively.
Curiously, "git pull --rebase" didn't flag this as a merge conflict.

Keep the dependency in DEPEND because bug 727334 is about a build
failure.

Fixes: 1b3e9353ac014fb3601b7351d89accf3e73d1321
Fixes: 73f8e6f7a42d87884a3946d5a97e7195001b72a3
Acked-by: Mikle Kolyada <zlogene@gentoo.org>
Package-Manager: Portage-2.3.103, Repoman-2.3.23
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

dev-texlive/texlive-luatex/texlive-luatex-2020-r2.ebuild
@@ -18,7 +18,6 @@ DEPEND=">=dev-texlive/texlive-basic-2020
 >=dev-texlive/texlive-fontsrecommended-2020
 "
 RDEPEND="${DEPEND} dev-texlive/texlive-latexrecommended
-dev-texlive/texlive-fontsrecommended
 "
 TEXLIVE_MODULE_BINSCRIPTS="
 	texmf-dist/scripts/luaotfload/luaotfload-tool.lua