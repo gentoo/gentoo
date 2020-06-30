gentoo/gentoo

commit 33c253fdeddb6c2175affffe85bd9cc214a68e0f

www-apps/roundup: version bumps with Py3 support
Closes: https://bugs.gentoo.org/709008
Closes: https://bugs.gentoo.org/710054
Closes: https://bugs.gentoo.org/722520
Package-Manager: Portage-2.3.99, Repoman-2.3.23
Signed-off-by: Cédric Krier <cedk@gentoo.org>

www-apps/roundup/roundup-2.0.0_beta0.ebuild
@@ -1,0 +1,47 @@
+# Copyright 1999-2020 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+PYTHON_COMPAT=( python3_{6..8} )
+
+inherit distutils-r1
+MY_P=${P/_/}
+
+DESCRIPTION="Issue-tracking system with command-line, web, and e-mail interfaces"
+HOMEPAGE="http://roundup.sourceforge.net https://pypi.org/project/roundup/"
+SRC_URI="mirror://pypi/${PN:0:1}/${PN}/${MY_P}.tar.gz"
+S="${WORKDIR}/${MY_P}"
+
+LICENSE="MIT ZPL"
+SLOT="0"
+KEYWORDS="~amd64 ~ppc ~sparc ~x86"
+IUSE="+tz sqlite mysql postgres xapian whoosh ssl jinja pyjwt markdown"
+
+DEPEND=""
+RDEPEND="${DEPEND}
+	tz? ( dev-python/pytz[$PYTHON_USEDEP] )
+	sqlite? ( dev-lang/python:*[sqlite] )
+	mysql? ( dev-python/mysqlclient[$PYTHON_USEDEP] )
+	postgres? ( >=dev-python/psycopg-2.8[$PYTHON_USEDEP] )
+	xapian? ( >=dev-libs/xapian-bindings-1.0.0[python,$PYTHON_USEDEP] )
+	whoosh? ( >=dev-python/whoosh-2.5.7[$PYTHON_USEDEP] )
+	ssl? ( dev-python/pyopenssl[$PYTHON_USEDEP] )
+	jinja? ( dev-python/jinja[$PYTHON_USEDEP] )
+	pyjwt? ( dev-python/pyjwt[$PYTHON_USEDEP] )
+	markdown? (
+		dev-python/markdown[$PYTHON_USEDEP]
+		dev-python/markdown2[$PYTHON_USEDEP]
+		dev-python/mistune[$PYTHON_USEDEP]
+		)"
+
+DOCS="CHANGES.txt doc/*.txt"
+
+python_install_all() {
+	distutils-r1_python_install_all
+	rm -r "${ED}"/usr/share/doc/${PN} || die
+}
+
+pkg_postinst() {
+	ewarn "See installation.txt for initialisation instructions."
+	ewarn "See upgrading.txt for upgrading instructions."
+}

www-apps/roundup/Manifest
@@ -1,2 +1,3 @@
 DIST roundup-1.5.1.tar.gz 2618886 BLAKE2B 5ccca10ce7f30b35b0875340c4cae87aa19e5384e6e5973576ea8e2de79eb83d32447580944f472d73e7cdb5f43a4ed7f805d51242e22cc2f756b3deae4004b2 SHA512 d7cdeaafb682ce7f202cacddeb1a42312f22778a2c83b52b4e838c27b1e7141a94b2ac2b670b0edee0efcfe27d74e31e6f267ae1380e90359def27385ca68d58
 DIST roundup-1.6.1.tar.gz 3128618 BLAKE2B 7fa87006582feac74f0fc455bc201f2485326b8f6ed616164f667992da114d45e8d8cef370a825450b5a4cb757ba0453d5fe3ace47164bd83e78a8c6f28b9d05 SHA512 b7666c5515c76baa4fa3ac3d0efea82feace6270af55bbc70e603b2ce0c617b4a303a15133310c1e00d3b6442baa58060d5387f568014652b1b781d9248f0052
+DIST roundup-2.0.0beta0.tar.gz 3461319 BLAKE2B 713f454bf289b27c9a14b4446ab65313a2fb6a0e754ee3e4217bfc2146b8bb2f3ddfce7472fb30041aff2e44e48888893b592b8447945bcb4ad2d3409e8ace84 SHA512 3e9b4a12ee172964f35bbcef6ae07153e76060531e92d78a2c8852d088395067cf933d141e26bdbb6462df15652d5c2a5fa5ee82288c7e85667b87df6a7f55f8

www-apps/roundup/metadata.xml
@@ -17,5 +17,8 @@
 		<flag name="tz">Enable full support of timezone</flag>
 		<flag name="xapian">Enable Xapian full-text indexer</flag>
 		<flag name="whoosh">Enable Whoosh full-text indexer</flag>
+		<flag name="jinja">Enable Jinja2 support</flag>
+		<flag name="pyjwt">Enable jwt tokens for login</flag>
+		<flag name="markdown">Enable markdown rendering</flag>
 	</use>
 </pkgmetadata>

commit 03b42aa75d69b627f9d999cabe93767013555ac2

www-apps/roundup: drop Python 3.8 support
Not all required Python deps are keyworded for all architectures.

Fixes: 33c253fdeddb6c2175affffe85bd9cc214a68e0f
Package-Manager: Portage-2.3.103, Repoman-2.3.23
Signed-off-by: Thomas Deutschmann <whissi@gentoo.org>

www-apps/roundup/roundup-2.0.0_beta0.ebuild
@@ -2,7 +2,7 @@
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
-PYTHON_COMPAT=( python3_{6..8} )
+PYTHON_COMPAT=( python3_{6..7} )
 
 inherit distutils-r1
 MY_P=${P/_/}
@@ -14,7 +14,7 @@ S="${WORKDIR}/${MY_P}"
 
 LICENSE="MIT ZPL"
 SLOT="0"
-KEYWORDS="~amd64 ~ppc ~sparc ~x86"
+KEYWORDS="~amd64 ~x86"
 IUSE="+tz sqlite mysql postgres xapian whoosh ssl jinja pyjwt markdown"
 
 DEPEND=""