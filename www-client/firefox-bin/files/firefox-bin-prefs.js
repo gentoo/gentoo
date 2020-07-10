84d80527d35fc6b4b4d90ea3c695d415c648d393
Michael Mair-Keimberger <m.mairkeimberger@gmail.com>
2020-07-10 20:23:57 +0200

    www-client/firefox-bin: remove unused files
    Package-Manager: Portage-2.3.103, Repoman-2.3.23
    Signed-off-by: Michael Mair-Keimberger <m.mairkeimberger@gmail.com>
    Closes: https://github.com/gentoo/gentoo/pull/16662
    Signed-off-by: Joonas Niilola <juippis@gentoo.org>

--- www-client/firefox-bin/files/firefox-bin-prefs.js
+++
@@ -1,6 +0,0 @@
-pref("app.update.enabled",                 false);
-pref("app.update.autoInstallEnabled",      false);
-pref("browser.display.use_system_colors",  true);
-pref("browser.shell.checkDefaultBrowser",  false); 
-pref("browser.EULA.override",              true); 
-pref("intl.locale.matchOS",                true);
