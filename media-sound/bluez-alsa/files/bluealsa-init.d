746f33742add5e99aa8a8c65a0f233c7fece1936
SuNova <sunova@tutanota.com>
2020-08-11 18:35:59 +0430

    media-sound/bluez-alsa: Copy config file to alsa datadir to preserve it
    Signed-off-by: sunova <sunova@tutanota.com>

--- media-sound/bluez-alsa/files/bluealsa-init.d
+++ media-sound/bluez-alsa/files/bluealsa-init.d
@@ -1,5 +1,5 @@
 #!/sbin/openrc-run
-# Copyright 1999-2017 Gentoo Foundation
+# Copyright 1999-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 pidfile="/var/run/bluealsa.pid"
