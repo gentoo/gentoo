gentoo/gentoo

commit 8246f9809943bf639e34c201273994be63680480

dev-lang/c-intercal: Work around test failure with gcc 10.
Bug: https://bugs.gentoo.org/722862
Package-Manager: Portage-2.3.99, Repoman-2.3.22
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

dev-lang/c-intercal/c-intercal-31.0.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=7
 
-inherit autotools elisp-common
+inherit autotools elisp-common flag-o-matic
 
 # C-INTERCAL uses minor-major ordering of version components and
 # negative version numbers. We map version components -1, -2, ...
@@ -45,6 +45,11 @@ src_prepare() {
 	eapply "${FILESDIR}"/${P}-no-common.patch
 	eapply_user
 	eautoreconf
+}
+
+src_configure() {
+	append-flags -fno-toplevel-reorder		#722862
+	econf
 }
 
 src_compile() {

commit fac3f5f9a4b75e6e0b08c24530392bb5c70bab9a

dev-lang/c-intercal: Append the flag safely.
Fixes: 8246f9809943bf639e34c201273994be63680480
Package-Manager: Portage-3.0.2, Repoman-2.3.23
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

dev-lang/c-intercal/c-intercal-31.0.ebuild
@@ -48,7 +48,7 @@ src_prepare() {
 }
 
 src_configure() {
-	append-flags -fno-toplevel-reorder		#722862
+	append-cflags $(test-flags-CC -fno-toplevel-reorder)	#722862
 	econf
 }
 