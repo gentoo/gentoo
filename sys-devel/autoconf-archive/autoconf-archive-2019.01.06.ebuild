gentoo/gentoo

commit 8d2c1872fbdcc58121df34c5e075573bae5c8f2b

sys-devel/autoconf-archive: x86 stable, applying ALLARCHES policy
Package-Manager: Portage-3.0.2, Repoman-2.3.23
Signed-off-by: Thomas Deutschmann <whissi@gentoo.org>

sys-devel/autoconf-archive/autoconf-archive-2019.01.06.ebuild
@@ -1,7 +1,7 @@
 # Copyright 1999-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-EAPI="6"
+EAPI="7"
 
 DESCRIPTION="GNU Autoconf Macro Archive"
 HOMEPAGE="https://www.gnu.org/software/autoconf-archive/"
@@ -9,10 +9,10 @@ SRC_URI="mirror://gnu/${PN}/${P}.tar.xz"
 
 LICENSE="GPL-3"
 SLOT="0"
-KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~m68k ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86 ~ppc-aix ~x64-cygwin ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~x86-macos ~sparc-solaris ~sparc64-solaris ~x64-solaris ~x86-solaris"
+KEYWORDS="~alpha amd64 arm arm64 hppa ~ia64 ~m68k ~mips ppc ppc64 s390 sparc x86 ~ppc-aix ~x64-cygwin ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~x86-macos ~sparc-solaris ~sparc64-solaris ~x64-solaris ~x86-solaris"
 IUSE=""
 
 # File collisions. #540246
-RDEPEND="!=gnome-base/gnome-common-3.14.0
+RDEPEND="!=gnome-base/gnome-common-3.14.0-r0
 	!>=gnome-base/gnome-common-3.14.0-r1[-autoconf-archive(+)]"
 DEPEND=""

commit 408b8193e1be09db7baf28d99306a40096d92ba7

sys-devel/autoconf-archive: restore accidentally removed riscv keyword
Fixes: 8d2c1872fbd ("sys-devel/autoconf-archive: x86 stable, applying ALLARCHES policy")
Package-Manager: Portage-3.0.2, Repoman-2.3.23
Signed-off-by: Thomas Deutschmann <whissi@gentoo.org>

sys-devel/autoconf-archive/autoconf-archive-2019.01.06.ebuild
@@ -9,7 +9,7 @@ SRC_URI="mirror://gnu/${PN}/${P}.tar.xz"
 
 LICENSE="GPL-3"
 SLOT="0"
-KEYWORDS="~alpha amd64 arm arm64 hppa ~ia64 ~m68k ~mips ppc ppc64 s390 sparc x86 ~ppc-aix ~x64-cygwin ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~x86-macos ~sparc-solaris ~sparc64-solaris ~x64-solaris ~x86-solaris"
+KEYWORDS="~alpha amd64 arm arm64 hppa ~ia64 ~m68k ~mips ppc ppc64 ~riscv s390 sparc x86 ~ppc-aix ~x64-cygwin ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~x86-macos ~sparc-solaris ~sparc64-solaris ~x64-solaris ~x86-solaris"
 IUSE=""
 
 # File collisions. #540246