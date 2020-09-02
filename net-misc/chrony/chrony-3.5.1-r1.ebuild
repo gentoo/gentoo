gentoo/gentoo

commit 6fbce4846282a2d77a9a8094e3d2fcd7176afcd6

net-misc/chrony: Disable non-default seccomp filter option
With `-F 1' chronyd sets up a syscall filter and has itself killed when
a "forbidden" syscall is made. Since we cannot control that (as
explained in the chronyd(8) manual) we should disable the filter by
default.

Package-Manager: Portage-3.0.4, Repoman-3.0.1
Bug: https://bugs.gentoo.org/739714
Signed-off-by: Jeroen Roovers <jer@gentoo.org>

net-misc/chrony/chrony-3.5.1-r1.ebuild
@@ -67,13 +67,14 @@ src_prepare() {
 		-e 's|pkg-config|${PKG_CONFIG}|g' \
 		configure || die
 
-	# Copy for potential user fixup
-	cp "${FILESDIR}"/chronyd.conf-r1 "${T}"/chronyd.conf
-	cp examples/chronyd.service "${T}"/chronyd.service
+	sed \
+		-e 's/-F 1/-F 0/' \
+		examples/chronyd.service > "${T}"/chronyd.service || die
+
+	cp "${FILESDIR}"/chronyd.conf-r1 "${T}"/chronyd.conf || die
 }
 
 src_configure() {
-	# Set config for privdrop
 	if ! use caps; then
 		sed -i \
 			-e 's/-u ntp//' \
@@ -82,7 +83,7 @@ src_configure() {
 
 	if ! use seccomp; then
 		sed -i \
-			-e 's/-F 1//' \
+			-e 's/-F 0//' \
 			"${T}"/chronyd.conf "${T}"/chronyd.service || die
 	fi
 

net-misc/chrony/chrony-4.0_pre3.ebuild
@@ -67,13 +67,14 @@ src_prepare() {
 		-e 's|pkg-config|${PKG_CONFIG}|g' \
 		configure || die
 
-	# Copy for potential user fixup
-	cp "${FILESDIR}"/chronyd.conf-r1 "${T}"/chronyd.conf
-	cp examples/chronyd.service "${T}"/chronyd.service
+	sed \
+		-e 's/-F 1/-F 0/' \
+		examples/chronyd.service > "${T}"/chronyd.service || die
+
+	cp "${FILESDIR}"/chronyd.conf-r1 "${T}"/chronyd.conf || die
 }
 
 src_configure() {
-	# Set config for privdrop
 	if ! use caps; then
 		sed -i \
 			-e 's/-u ntp//' \
@@ -82,7 +83,7 @@ src_configure() {
 
 	if ! use seccomp; then
 		sed -i \
-			-e 's/-F 1//' \
+			-e 's/-F 0//' \
 			"${T}"/chronyd.conf "${T}"/chronyd.service || die
 	fi
 

net-misc/chrony/chrony-9999.ebuild
@@ -67,13 +67,14 @@ src_prepare() {
 		-e 's|pkg-config|${PKG_CONFIG}|g' \
 		configure || die
 
-	# Copy for potential user fixup
-	cp "${FILESDIR}"/chronyd.conf-r1 "${T}"/chronyd.conf
-	cp examples/chronyd.service "${T}"/chronyd.service
+	sed \
+		-e 's/-F 1/-F 0/' \
+		examples/chronyd.service > "${T}"/chronyd.service || die
+
+	cp "${FILESDIR}"/chronyd.conf-r1 "${T}"/chronyd.conf || die
 }
 
 src_configure() {
-	# Set config for privdrop
 	if ! use caps; then
 		sed -i \
 			-e 's/-u ntp//' \
@@ -82,7 +83,7 @@ src_configure() {
 
 	if ! use seccomp; then
 		sed -i \
-			-e 's/-F 1//' \
+			-e 's/-F 0//' \
 			"${T}"/chronyd.conf "${T}"/chronyd.service || die
 	fi
 

net-misc/chrony/files/chronyd.conf-r1
@@ -9,4 +9,4 @@ CFGFILE="/etc/chrony/chrony.conf"
 # The combination of "-s -r" allows chronyd to perform long term averaging of
 # the gain or loss rate across system reboots and shutdowns.
 
-ARGS="-u ntp -F 1"
+ARGS="-u ntp -F 0"

commit f7179379b3d676aab1cd6eceafd28145bfed2ae1

net-misc/chrony: Simplify setting systemd default options
Package-Manager: Portage-3.0.5, Repoman-3.0.1
Bug: https://bugs.gentoo.org/show_bug.cgi?id=739714
Fixes: 6fbce4846282a2d77a9a8094e3d2fcd7176afcd6
Signed-off-by: Jeroen Roovers <jer@gentoo.org>

net-misc/chrony/chrony-3.5.1-r1.ebuild
@@ -68,24 +68,20 @@ src_prepare() {
 		-e 's|pkg-config|${PKG_CONFIG}|g' \
 		configure || die
 
-	sed \
-		-e 's/-F 1/-F 0/' \
-		examples/chronyd.service > "${T}"/chronyd.service || die
-
 	cp "${FILESDIR}"/chronyd.conf-r1 "${T}"/chronyd.conf || die
 }
 
 src_configure() {
 	if ! use caps; then
 		sed -i \
-			-e 's/-u ntp//' \
-			"${T}"/chronyd.conf "${T}"/chronyd.service || die
+			-e 's/ -u ntp//' \
+			"${T}"/chronyd.conf examples/chronyd.service || die
 	fi
 
 	if ! use seccomp; then
 		sed -i \
-			-e 's/-F 0//' \
-			"${T}"/chronyd.conf "${T}"/chronyd.service || die
+			-e 's/ -F 0//' \
+			"${T}"/chronyd.conf examples/chronyd.service || die
 	fi
 
 	tc-export CC PKG_CONFIG
@@ -176,7 +172,7 @@ src_install() {
 	insinto /etc/logrotate.d
 	newins "${FILESDIR}"/chrony-2.4-r1.logrotate chrony
 
-	systemd_dounit "${T}"/chronyd.service
+	systemd_dounit examples/chronyd.service
 	systemd_dounit examples/chrony-wait.service
 	systemd_enable_ntpunit 50-chrony chronyd.service
 }

net-misc/chrony/chrony-4.0_pre3.ebuild
@@ -68,24 +68,20 @@ src_prepare() {
 		-e 's|pkg-config|${PKG_CONFIG}|g' \
 		configure || die
 
-	sed \
-		-e 's/-F 1/-F 0/' \
-		examples/chronyd.service > "${T}"/chronyd.service || die
-
 	cp "${FILESDIR}"/chronyd.conf-r1 "${T}"/chronyd.conf || die
 }
 
 src_configure() {
 	if ! use caps; then
 		sed -i \
-			-e 's/-u ntp//' \
-			"${T}"/chronyd.conf "${T}"/chronyd.service || die
+			-e 's/ -u ntp//' \
+			"${T}"/chronyd.conf examples/chronyd.service || die
 	fi
 
 	if ! use seccomp; then
 		sed -i \
-			-e 's/-F 0//' \
-			"${T}"/chronyd.conf "${T}"/chronyd.service || die
+			-e 's/ -F 0//' \
+			"${T}"/chronyd.conf examples/chronyd.service || die
 	fi
 
 	tc-export CC PKG_CONFIG
@@ -176,7 +172,7 @@ src_install() {
 	insinto /etc/logrotate.d
 	newins "${FILESDIR}"/chrony-2.4-r1.logrotate chrony
 
-	systemd_dounit "${T}"/chronyd.service
+	systemd_dounit examples/chronyd.service
 	systemd_dounit examples/chrony-wait.service
 	systemd_enable_ntpunit 50-chrony chronyd.service
 }

net-misc/chrony/chrony-9999.ebuild
@@ -68,24 +68,20 @@ src_prepare() {
 		-e 's|pkg-config|${PKG_CONFIG}|g' \
 		configure || die
 
-	sed \
-		-e 's/-F 1/-F 0/' \
-		examples/chronyd.service > "${T}"/chronyd.service || die
-
 	cp "${FILESDIR}"/chronyd.conf-r1 "${T}"/chronyd.conf || die
 }
 
 src_configure() {
 	if ! use caps; then
 		sed -i \
-			-e 's/-u ntp//' \
-			"${T}"/chronyd.conf "${T}"/chronyd.service || die
+			-e 's/ -u ntp//' \
+			"${T}"/chronyd.conf examples/chronyd.service || die
 	fi
 
 	if ! use seccomp; then
 		sed -i \
-			-e 's/-F 0//' \
-			"${T}"/chronyd.conf "${T}"/chronyd.service || die
+			-e 's/ -F 0//' \
+			"${T}"/chronyd.conf examples/chronyd.service || die
 	fi
 
 	tc-export CC PKG_CONFIG
@@ -176,7 +172,7 @@ src_install() {
 	insinto /etc/logrotate.d
 	newins "${FILESDIR}"/chrony-2.4-r1.logrotate chrony
 
-	systemd_dounit "${T}"/chronyd.service
+	systemd_dounit examples/chronyd.service
 	systemd_dounit examples/chrony-wait.service
 	systemd_enable_ntpunit 50-chrony chronyd.service
 }

net-misc/chrony/files/chrony-3.5-r3-systemd-gentoo.patch
@@ -6,7 +6,7 @@
  PIDFile=/run/chrony/chronyd.pid
 -EnvironmentFile=-/etc/sysconfig/chronyd
 -ExecStart=/usr/sbin/chronyd $OPTIONS
-+ExecStart=/usr/sbin/chronyd -u ntp -F 1
++ExecStart=/usr/sbin/chronyd -u ntp -F 0
  PrivateTmp=yes
  ProtectHome=yes
  ProtectSystem=full