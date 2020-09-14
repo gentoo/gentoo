gentoo/gentoo

commit 4acc40cdbc2d2489ccea83abed1ef11b050cfe90

dev-libs/zziplib: unconditionally install man pages
Package-Manager: Portage-3.0.4, Repoman-3.0.1
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/zziplib/zziplib-0.13.71_p20200419.ebuild
@@ -15,16 +15,13 @@ SRC_URI="https://github.com/gdraheim/${PN}/archive/${MY_COMMIT}.tar.gz -> ${P}.t
 LICENSE="|| ( LGPL-2.1 MPL-1.1 )"
 SLOT="0/13"
 KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~mips ~ppc ~ppc64 ~s390 ~sparc ~x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~x86-macos ~sparc-solaris ~sparc64-solaris ~x64-solaris ~x86-solaris"
-IUSE="doc sdl static-libs test"
+IUSE="sdl static-libs test"
 
 RESTRICT="!test? ( test )"
 
 BDEPEND="
-	doc? ( ${PYTHON_DEPS} )
-	test? (
-		app-arch/zip
-		${PYTHON_DEPS}
-	)
+	${PYTHON_DEPS}
+	test? ( app-arch/zip )
 "
 DEPEND="
 	sys-libs/zlib
@@ -40,7 +37,7 @@ PATCHES=(
 )
 
 pkg_setup() {
-	if use doc || use test; then
+	if use test; then
 		python-any-r1_pkg_setup
 	fi
 }
@@ -53,7 +50,7 @@ src_configure() {
 		-DBUILD_STATIC_LIBS="$(usex static-libs)"
 		-DBUILD_TESTS="$(usex test)"
 		-DZZIPTEST="$(usex test)"
-		-DZZIPDOCS="$(usex doc)"
+		-DZZIPDOCS=ON
 		-DZZIPWRAP=OFF
 	)
 

commit 1fa3fcaaf55b8f8a92f1501427b7a1b33c6cec43

dev-libs/zziplib: unconditionally call pkg_setup
Fixes: 4acc40cdbc2d2489ccea83abed1ef11b050cfe90
Package-Manager: Portage-3.0.4, Repoman-3.0.1
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/zziplib/zziplib-0.13.71_p20200419.ebuild
@@ -37,9 +37,7 @@ PATCHES=(
 )
 
 pkg_setup() {
-	if use test; then
-		python-any-r1_pkg_setup
-	fi
+	python-any-r1_pkg_setup
 }
 
 src_configure() {