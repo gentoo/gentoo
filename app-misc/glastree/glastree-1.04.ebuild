gentoo/gentoo

commit fd505a12a41c8be8dfe8be94750295bfed0b339f

app-misc/glastree: port older to EAPI 7 too
Preferable to not wait for stabilisation of the newer one
to drop EAPI 4.

Bug: https://bugs.gentoo.org/742557
Package-Manager: Portage-3.0.4, Repoman-3.0.1
Signed-off-by: Sam James <sam@gentoo.org>

app-misc/glastree/glastree-1.04.ebuild
@@ -1,7 +1,7 @@
-# Copyright 1999-2012 Gentoo Foundation
+# Copyright 1999-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-EAPI=4
+EAPI=7
 
 inherit eutils
 
@@ -12,17 +12,16 @@ DEPEND="dev-lang/perl
 	dev-perl/Date-Calc"
 SLOT="0"
 KEYWORDS="~amd64 ppc x86"
-IUSE=""
 LICENSE="public-domain"
 
-src_prepare() {
-	epatch "${FILESDIR}"/${PN}-posix-make.patch
-}
+PATCHES=(
+	"${FILESDIR}"/${PN}-posix-make.patch
+)
 
 src_compile() { :; }
 
 src_install() {
 	dodir /usr/share/man/man1
-	emake INSTROOT="${D}"/usr INSTMAN=share/man install
+	emake INSTROOT="${ED}"/usr INSTMAN=share/man install
 	dodoc README CHANGES THANKS TODO
 }

app-misc/glastree/files/glastree-posix-make.patch
@@ -1,8 +1,8 @@
 Make sure the perl code is valid both with and without the backslashes.
 Some make versions strip them, others don't.
 
---- glastree-1.04/Makefile
-+++ glastree-1.04/Makefile
+--- a/Makefile
++++ b/Makefile
 @@ -24,8 +24,8 @@
  
  install:

commit 4a8c34ab35309ab6e1134f7eeca40e4d4535e750

app-misc/glastree: drop eutils in 1.04
Not present in the snapshot ebuild from yesterday,
but was left behind in the 1.04 quickport.

Fixes: fd505a12a41c8be8dfe8be94750295bfed0b339f
Package-Manager: Portage-3.0.4, Repoman-3.0.1
Signed-off-by: Sam James <sam@gentoo.org>

app-misc/glastree/glastree-1.04.ebuild
@@ -3,8 +3,6 @@
 
 EAPI=7
 
-inherit eutils
-
 DESCRIPTION="glastree is a poor mans snapshot utility using hardlinks written in perl"
 HOMEPAGE="http://www.igmus.org/code/"
 SRC_URI="http://www.igmus.org/files/${P}.tar.gz"