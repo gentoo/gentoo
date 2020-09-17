cb65d530c8fcf0cc704d5c5e677d901b4b955e1b
Lars Wendler <polynomial-c@gentoo.org>
2020-09-17 09:44:54 +0200

    media-sound/bluez-alsa: Change /var/run to /run in openrc init script
    Package-Manager: Portage-3.0.7, Repoman-3.0.1
    Signed-off-by: Lars Wendler <polynomial-c@gentoo.org>

--- media-sound/bluez-alsa/files/bluealsa-init.d
+++ media-sound/bluez-alsa/files/bluealsa-init.d
@@ -2,7 +2,7 @@
 # Copyright 1999-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-pidfile="/var/run/bluealsa.pid"
+pidfile="/run/bluealsa.pid"
 command="/usr/bin/bluealsa"
 command_args="${BLUEALSA_CONF}"
 command_background="true"
@@ -13,5 +13,5 @@ depend() {
 }
 
 start_pre() {
-	checkpath -q -D -m 0770 -o :audio /var/run/bluealsa
+	checkpath -q -D -m 0770 -o :audio /run/bluealsa
 }
