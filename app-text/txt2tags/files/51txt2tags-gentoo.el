a42d892c70ebc43dc07efa2db24754a89b20ab39
Michał Górny <mgorny@gentoo.org>
2020-09-19 09:53:44 +0200

    app-text/txt2tags: Remove old
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- app-text/txt2tags/files/51txt2tags-gentoo.el
+++
@@ -1,6 +0,0 @@
-
-;;; txt2tags site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(autoload  't2t-mode "txt2tags-mode" "Major mode for editing Txt2Tags files" t)
-(add-to-list 'auto-mode-alist '("\\.t2t\\'" . t2t-mode))
