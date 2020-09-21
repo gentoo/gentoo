a9ec4f776d7342e0e89251b8e8611436a2b55cab
Michał Górny <mgorny@gentoo.org>
2020-09-21 07:56:09 +0200

    games-action/openclonk: Remove last-rited pkg
    Closes: https://bugs.gentoo.org/646748
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- games-action/openclonk/files/openclonk-wrapper-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# wrapper prevents the game to look for .ocp files in the current dir
-# which can lead to weird behavior and game freeze
-
-[ -d ~/.clonk/openclonk ] || mkdir -p ~/.clonk/openclonk
-
-cd ~/.clonk/openclonk
-
-exec clonk "$@"
