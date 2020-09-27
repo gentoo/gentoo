gentoo/gentoo

commit 5fb754ce4aa2586479db82c354ebc31f18ffd90a

sys-kernel/gentoo-kernel-bin: bump to 5.8.12 for arm64
Package-Manager: Portage-3.0.4, Repoman-3.0.1
Signed-off-by: Sam James <sam@gentoo.org>

sys-kernel/gentoo-kernel-bin/gentoo-kernel-bin-5.8.12-r1.ebuild
@@ -1,0 +1,53 @@
+# Copyright 2020 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+
+inherit kernel-install
+
+MY_P=${P/-bin/}-1
+DESCRIPTION="Pre-built Linux kernel with genpatches"
+HOMEPAGE="https://www.kernel.org/"
+SRC_URI+="
+	amd64? (
+		https://dev.gentoo.org/~mgorny/binpkg/amd64/kernel/sys-kernel/gentoo-kernel/${MY_P}.xpak
+			-> ${MY_P}.amd64.xpak
+	)
+	x86? (
+		https://dev.gentoo.org/~mgorny/binpkg/x86/kernel/sys-kernel/gentoo-kernel/${MY_P}.xpak
+			-> ${MY_P}.x86.xpak
+	)"
+S=${WORKDIR}
+
+LICENSE="GPL-2"
+KEYWORDS="~amd64 ~x86"
+
+RDEPEND="
+	!sys-kernel/gentoo-kernel:${SLOT}
+	!sys-kernel/vanilla-kernel:${SLOT}
+	!sys-kernel/vanilla-kernel-bin:${SLOT}"
+
+QA_PREBUILT='*'
+
+pkg_pretend() {
+	ewarn "Starting with 5.7.9, Distribution Kernels are switching from Arch"
+	ewarn "Linux configs to Fedora.  Please keep a backup kernel just in case."
+
+	kernel-install_pkg_pretend
+}
+
+src_unpack() {
+	ebegin "Unpacking ${MY_P}.${ARCH}.xpak"
+	tar -x < <(xz -c -d --single-stream "${DISTDIR}/${MY_P}.${ARCH}.xpak")
+	eend ${?} || die "Unpacking ${MY_P} failed"
+}
+
+src_test() {
+	kernel-install_test "${PV}" \
+		"${WORKDIR}/usr/src/linux-${PV}/$(kernel-install_get_image_path)" \
+		"lib/modules/${PV}"
+}
+
+src_install() {
+	mv * "${ED}" || die
+}

commit b2abbd99735bbe023347076cf48ab402ff1376ba

sys-kernel/gentoo-kernel-bin: use correct template for arm64
Fixes: 5fb754ce4aa2586479db82c354ebc31f18ffd90a
Fixes: c744ed5dcf1f74a5b5548fb1db30caaf22698b6d
Package-Manager: Portage-3.0.4, Repoman-3.0.1
Signed-off-by: Sam James <sam@gentoo.org>

sys-kernel/gentoo-kernel-bin/Manifest
@@ -23,6 +23,7 @@ DIST gentoo-kernel-5.4.67-1.amd64.xpak 61551155 BLAKE2B 55263b960a7278ddb1ff63c0
 DIST gentoo-kernel-5.4.67-1.arm64.xpak 56452313 BLAKE2B 9d18f2796455427df43a0fe5a73666b446dee817ca1cd9bd9a80d9726f25787e09cb3d669834c20cd3e0594d209068db4f6d4bb4ed7ef9b2c7bc29916ed17325 SHA512 5a9b747bee7d64100090b1e6b02a3d011fe2c4caa0c2a182e736f03b241b1fea26e9ea1a56f1e41eadc27784cf33efb0c46cc1ef0705d21ba5e015cb994aad80
 DIST gentoo-kernel-5.4.67-1.x86.xpak 53349995 BLAKE2B a871a00a1629f46c06474a00c3ab696cf5a924633b6b88587a742a2357a2bfacf387b56040e207270be85c76d8c5355fa53b22f628acd15d4d0c2948d992a533 SHA512 72a1e44af59d29854841db24020825aa8454e3c15d33c2120dceb8d73bc69e797c471af8a3f3f2217cba63516e6bd20a601b808af1e2c9455adbd6bf31eda44a
 DIST gentoo-kernel-5.4.68-1.amd64.xpak 61493744 BLAKE2B 3f1797046b421bd1b374e1bfda0cf407a3b13de8d222adbed33bf5d6d3dcb7fce30bc29d823ed34d854701535dbf7e211f3e60022ae8a2da2d52c511af3938d3 SHA512 b7fda56c7d43a962c964acad088a32a763e1fca468fee4b78503c7cf4a75790e3e0d6fb24abbab52d2b8ffb8faf243bfa57f061902755ac08402072409ed3cd9
+DIST gentoo-kernel-5.4.68-1.arm64.xpak 56462206 BLAKE2B 75e9b787a0b87d82adeb319501ac03bfa5409086dc6729f18f5bfa1eb0bf101774791ac99ca8949618e4c6185152f325a227875d7284d018ffb027cf31995a7f SHA512 5198cee93955e87ffd654c68e60bebb512f9488531d0b26fd769a2ad8c172fa6c54e0c9119074227f7a04352761036228f52f293037051f344df88bed0c4f624
 DIST gentoo-kernel-5.4.68-1.x86.xpak 53358098 BLAKE2B 6a35264a7514c1daa61e590d64c1400a298b602bfe5ef09c4a1f00ad8f5da1e10e6446152b69dd9e6d0eda6d5976d9a225a257efda9a6efb24928909816e9630 SHA512 f0c8ca982dbf382fff4642ffcad6f2bd295a605d7169eec8a2c5c434b663c6030e2679526dae59be30c18ac64bc1842a0e95dd31d604fb7b311a4498fea12999
 DIST gentoo-kernel-5.7.17-1.amd64.xpak 63476985 BLAKE2B 1a31855979a309c7bb4565208aa92dcac9d4fcf0714ea224c2a909b534ca65bac2778f6c2cee24cb5dbcfd825bc896b52cb14eb37cc36aedffadcc0217a32269 SHA512 4071bb9eb192da7823834365b8d6817dd043c256ff0803eb70a35278aa92b63a66fa1c8d86b2c91d90fceb7c7b4fb1ac41b347457d6ccda78e02c6e0f123c044
 DIST gentoo-kernel-5.7.17-1.arm64.xpak 59769706 BLAKE2B e7f3180e3d4373a7923250b8fe12141c0138e04f81d08b014386270defcc25517028ef404163c5d81097c72d6605cd5a6b4f1ad6df15a0f77d13daebb6045268 SHA512 59b33b487c170bad68de3527506c3a13a1edd232de40281cfac463fd8641222c0e86c4aef3b47127746a54dde94456a7b21fb150ca66674f1efadaf5c5db7d34
@@ -40,6 +41,7 @@ DIST gentoo-kernel-5.8.11-1.amd64.xpak 64083966 BLAKE2B 98a770773074e41f232921a1
 DIST gentoo-kernel-5.8.11-1.arm64.xpak 60542737 BLAKE2B 134a5d129217507ad84eed33448d150b4161d25fcb8284dd85a2b4cc05258503dcd82bac51efc4d1cda5e57cc34c53bc717928fcefdda4d51865a9c08ef85afd SHA512 494dabb509d55ed7ca242b6379079078fc31e7b9236135d330c50f7714b915acecca7e4f7f4b094f3dd6963c179d8bd208949a9175510c8c68f919741d3fda33
 DIST gentoo-kernel-5.8.11-1.x86.xpak 56120164 BLAKE2B 9f7bf8fb5c818bf31411411b3c148e40a48cbe166268cfc6598430ce3292625e2047ee9ad6dfb1927ba7a31f01682ab22a9946a96f5161367582aab261f86ada SHA512 863d3de8bfe710305ceb5a82b148ba0e8d127061b2111af19fa12be761a783a53531aeeb5d56095e621120ec041608c060152b0c0dd0c548dbe62acd7b245dfa
 DIST gentoo-kernel-5.8.12-1.amd64.xpak 64106657 BLAKE2B a8167bc714130a6f05811ec522c14c84b5038a6130a4a0ca7d581cad6ada7f47b55a9940198524ecf4a28debad79286929a888cd2f433a7b22e4bce4342254e3 SHA512 e952c7efbd007ad12be3074ffb488593dc5ddd4fb7dba70d7c73173d9ffdfbfc5013c06307ead53daed43c7d816789a608feb2a5d48818341d179e947edbc446
+DIST gentoo-kernel-5.8.12-1.arm64.xpak 60541802 BLAKE2B 3d92912c25ee3ebbd48316550fb16f6960d2845a380db93026174a2f147ad1f32eea28c06dc1e350c15122939ecd64401e7b825268b2ce9d38343da3503499d3 SHA512 d937892936ba556529ee15d49acd207592e1142ce4a035d90920575c9ae3a5a8ef0325fddeb3b318b849df47ba436cf50b34fa8c00a01651f241cc805cdf8a09
 DIST gentoo-kernel-5.8.12-1.x86.xpak 56133532 BLAKE2B c969778b746896cde2af60afc5b7049db6dba8906079a26db905b7342ef099b1dc400ea482ed2548f3a160b8e3a4ac32eed3243315c4cff6f26e9423e9c8fe8b SHA512 2b1470d9d4d08c29ad0635a65b1ea9e2291210f814ed26c47c52cbe4155cc6bdd59b829a85163f76ad377da6cbc7022771fc5fdbc6f540e43a01ab3a5038e0c0
 DIST gentoo-kernel-5.8.3-1.amd64.xpak 64633724 BLAKE2B af5a903684829c3761fa29e7f84b050870479b5bb5105b91d5d53c7c2773a90cede547e2b03b95c0da08fbc6f75c2f48316960a3d8948acf8eb419d48a8a4dbf SHA512 f09ac10e1e26703db6e0a4bf1384ec90fdafacafdbd5595f953648112278acb78c37646dffa8bd6be09502224cd63fd706aadfbe4b9601dfd5a41eeb4efe8db9
 DIST gentoo-kernel-5.8.3-1.arm64.xpak 60976315 BLAKE2B 6cd9a830735d6a3e3acc0d8115032cc373b6238fc314308784862ccb9e0994a0c023bd13e058a5ba61e1b3f3fc2ce7ab468078e46c092534da117e534076bd59 SHA512 b5f5451e0a4661a47f00fb7eec8f836cafdc94e6c2b879036b3a3b37e237ea1a930a6d4fc61edf9655f6becf1fd8714fd854933e8f707d2d437cf71fc0ef06bc

sys-kernel/gentoo-kernel-bin/gentoo-kernel-bin-5.4.68-r1.ebuild
@@ -13,6 +13,10 @@ SRC_URI+="
 		https://dev.gentoo.org/~mgorny/binpkg/amd64/kernel/sys-kernel/gentoo-kernel/${MY_P}.xpak
 			-> ${MY_P}.amd64.xpak
 	)
+	arm64? (
+		https://dev.gentoo.org/~sam/binpkg/arm64/kernel/sys-kernel/gentoo-kernel/${MY_P}.xpak
+			-> ${MY_P}.arm64.xpak
+	)
 	x86? (
 		https://dev.gentoo.org/~mgorny/binpkg/x86/kernel/sys-kernel/gentoo-kernel/${MY_P}.xpak
 			-> ${MY_P}.x86.xpak
@@ -20,7 +24,7 @@ SRC_URI+="
 S=${WORKDIR}
 
 LICENSE="GPL-2"
-KEYWORDS="~amd64 ~x86"
+KEYWORDS="~arm64"
 
 RDEPEND="
 	!sys-kernel/gentoo-kernel:${SLOT}

sys-kernel/gentoo-kernel-bin/gentoo-kernel-bin-5.8.12-r1.ebuild
@@ -13,6 +13,10 @@ SRC_URI+="
 		https://dev.gentoo.org/~mgorny/binpkg/amd64/kernel/sys-kernel/gentoo-kernel/${MY_P}.xpak
 			-> ${MY_P}.amd64.xpak
 	)
+	arm64? (
+		https://dev.gentoo.org/~sam/binpkg/arm64/kernel/sys-kernel/gentoo-kernel/${MY_P}.xpak
+			-> ${MY_P}.arm64.xpak
+	)
 	x86? (
 		https://dev.gentoo.org/~mgorny/binpkg/x86/kernel/sys-kernel/gentoo-kernel/${MY_P}.xpak
 			-> ${MY_P}.x86.xpak
@@ -20,7 +24,7 @@ SRC_URI+="
 S=${WORKDIR}
 
 LICENSE="GPL-2"
-KEYWORDS="~amd64 ~x86"
+KEYWORDS="~arm64"
 
 RDEPEND="
 	!sys-kernel/gentoo-kernel:${SLOT}