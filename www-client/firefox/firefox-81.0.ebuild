gentoo/gentoo

commit d523c2051222bb5ab591efe8f1abc03508bdc4d5

www-client/firefox: fix gold linker deps
Closes: https://bugs.gentoo.org/732712
Package-Manager: Portage-3.0.8, Repoman-3.0.1
Signed-off-by: Thomas Deutschmann <whissi@gentoo.org>

www-client/firefox/firefox-81.0.ebuild
@@ -64,22 +64,23 @@ BDEPEND="${PYTHON_DEPS}
 	|| (
 		(
 			sys-devel/clang:10
-			!clang? ( sys-devel/llvm:10 )
+			sys-devel/llvm
 			clang? (
 				=sys-devel/lld-10*
-				sys-devel/llvm:10[gold]
 				pgo? ( =sys-libs/compiler-rt-sanitizers-10*[profile] )
 			)
 		)
 		(
 			sys-devel/clang:9
-			!clang? ( sys-devel/llvm:9 )
+			sys-devel/llvm:9
 			clang? (
 				=sys-devel/lld-9*
-				sys-devel/llvm:9[gold]
 				pgo? ( =sys-libs/compiler-rt-sanitizers-9*[profile] )
 			)
 		)
+	)
+	lto? (
+		!clang? ( sys-devel/binutils[gold] )
 	)
 	amd64? ( >=dev-lang/yasm-1.1 )
 	x86? ( >=dev-lang/yasm-1.1 )

www-client/firefox/firefox-78.3.0.ebuild
@@ -64,22 +64,23 @@ BDEPEND="${PYTHON_DEPS}
 	|| (
 		(
 			sys-devel/clang:10
-			!clang? ( sys-devel/llvm:10 )
+			sys-devel/llvm:10
 			clang? (
 				=sys-devel/lld-10*
-				sys-devel/llvm:10[gold]
 				pgo? ( =sys-libs/compiler-rt-sanitizers-10*[profile] )
 			)
 		)
 		(
 			sys-devel/clang:9
-			!clang? ( sys-devel/llvm:9 )
+			sys-devel/llvm:9
 			clang? (
 				=sys-devel/lld-9*
-				sys-devel/llvm:9[gold]
 				pgo? ( =sys-libs/compiler-rt-sanitizers-9*[profile] )
 			)
 		)
+	)
+	lto? (
+		!clang? ( sys-devel/binutils[gold] )
 	)
 	amd64? ( >=dev-lang/yasm-1.1 )
 	x86? ( >=dev-lang/yasm-1.1 )

commit 28332d77c1a4159d6af8d3b70e0b0eff0f805942

www-client/firefox: add missing slot on clang
Spotted-by: Arfrever Frehtes Taifersar Arahesis <Arfrever@Apache.Org>
Fixes: d523c20 ("www-client/firefox: fix gold linker deps")
Package-Manager: Portage-3.0.8, Repoman-3.0.1
Signed-off-by: Thomas Deutschmann <whissi@gentoo.org>

www-client/firefox/firefox-81.0.ebuild
@@ -64,7 +64,7 @@ BDEPEND="${PYTHON_DEPS}
 	|| (
 		(
 			sys-devel/clang:10
-			sys-devel/llvm
+			sys-devel/llvm:10
 			clang? (
 				=sys-devel/lld-10*
 				pgo? ( =sys-libs/compiler-rt-sanitizers-10*[profile] )