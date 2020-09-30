gentoo/gentoo

commit 1a9d2a5e0166770db4aefdd7f688e332a32f858a

sci-libs/lapack: bump to 3.9.0
Closes: https://bugs.gentoo.org/738258
Package-Manager: Portage-3.0.4, Repoman-3.0.1
Signed-off-by: Sam James <sam@gentoo.org>

sci-libs/lapack/lapack-3.9.0.ebuild
@@ -1,0 +1,95 @@
+# Copyright 1999-2020 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+
+#CMAKE_MAKEFILE_GENERATOR="emake"
+inherit cmake
+
+DESCRIPTION="BLAS,CBLAS,LAPACK,LAPACKE reference implementations"
+HOMEPAGE="http://www.netlib.org/lapack/"
+SRC_URI="https://github.com/Reference-LAPACK/lapack/archive/v${PV}.tar.gz -> ${P}.tar.gz"
+
+LICENSE="BSD"
+SLOT="0"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~mips ~ppc ~ppc64 ~s390 ~sparc ~x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~x86-macos"
+IUSE="lapacke doc eselect-ldso test"
+# TODO: static-libs 64bit-index
+RESTRICT="!test? ( test )"
+
+RDEPEND="
+	eselect-ldso? ( >=app-eselect/eselect-blas-0.2
+	>=app-eselect/eselect-lapack-0.2 )
+	!app-eselect/eselect-cblas
+	!sci-libs/blas-reference
+	!sci-libs/cblas-reference
+	!sci-libs/lapack-reference
+	!sci-libs/lapacke-reference
+	virtual/fortran
+	doc? ( app-doc/blas-docs )"
+DEPEND="${RDEPEND}
+	virtual/pkgconfig"
+
+src_configure() {
+	local mycmakeargs=(
+		-DCBLAS=ON
+		-DLAPACKE=$(usex lapacke)
+		-DBUILD_SHARED_LIBS=ON
+		-DBUILD_TESTING=ON
+	)
+
+	cmake_src_configure
+}
+
+src_install() {
+	cmake_src_install
+
+	use eselect-ldso || return
+	# Create private lib directory for eselect::blas (ld.so.conf)
+	dodir /usr/$(get_libdir)/blas/reference
+	dosym ../../libblas.so usr/$(get_libdir)/blas/reference/libblas.so
+	dosym ../../libblas.so.3 usr/$(get_libdir)/blas/reference/libblas.so.3
+	dosym ../../libcblas.so usr/$(get_libdir)/blas/reference/libcblas.so
+	dosym ../../libcblas.so.3 usr/$(get_libdir)/blas/reference/libcblas.so.3
+
+	# Create private lib directory for eselect::lapack (ld.so.conf)
+	dodir /usr/$(get_libdir)/lapack/reference
+	dosym ../../liblapack.so usr/$(get_libdir)/lapack/reference/liblapack.so
+	dosym ../../liblapack.so.3 usr/$(get_libdir)/lapack/reference/liblapack.so.3
+}
+
+pkg_postinst() {
+	use eselect-ldso || return
+
+	local me=reference libdir=$(get_libdir)
+	# check eselect-blas
+	eselect blas add ${libdir} "${EROOT}"/usr/${libdir}/blas/${me} ${me}
+	local current_blas=$(eselect blas show ${libdir} | cut -d' ' -f2)
+	if [[ ${current_blas} == ${me} || -z ${current_blas} ]]; then
+		eselect blas set ${libdir} ${me}
+		elog "Current eselect: BLAS ($libdir) -> [${current_blas}]."
+	else
+		elog "Current eselect: BLAS ($libdir) -> [${current_blas}]."
+		elog "To use blas [${me}] implementation, you have to issue (as root):"
+		elog "\t eselect blas set ${libdir} ${me}"
+	fi
+
+	# check eselect-lapack
+	eselect lapack add ${libdir} "${EROOT}"/usr/${libdir}/lapack/${me} ${me}
+	local current_lapack=$(eselect lapack show ${libdir} | cut -d' ' -f2)
+	if [[ ${current_lapack} == ${me} || -z ${current_lapack} ]]; then
+		eselect lapack set ${libdir} ${me}
+		elog "Current eselect: LAPACK ($libdir) -> [${current_lapack}]."
+	else
+		elog "Current eselect: LAPACK ($libdir) -> [${current_lapack}]."
+		elog "To use lapack [${me}] implementation, you have to issue (as root):"
+		elog "\t eselect lapack set ${libdir} ${me}"
+	fi
+}
+
+pkg_postrm() {
+	use eselect-ldso || return
+
+	eselect blas validate
+	eselect lapack validate
+}

sci-libs/lapack/Manifest
@@ -1,1 +1,2 @@
 DIST lapack-3.8.0.tar.gz 7426094 BLAKE2B 27cd75eea498250382b2df6f5d7dc95fdee8d37b1a0faba5296ca0145ec40a1165a4ecc727aa1647086be6783b50cf475f79cea0f30f541da789051db99b2f37 SHA512 17786cb7306fccdc9b4a242de7f64fc261ebe6a10b6ec55f519deb4cb673cb137e8742aa5698fd2dc52f1cd56d3bd116af3f593a01dcf6770c4dcc86c50b2a7f
+DIST lapack-3.9.0.tar.gz 7534567 BLAKE2B 408013a11a72e0262d144adfdf6db28a6efa3bb5446f9ff959bb88fe10459dfa621149ed0144630a55e73e3b5be3c704269a716eb195f6db7b39d345b4db8270 SHA512 424956ad941a60a4b71e0d451ad48db12a692f8a71a90f3ca7f71d6ecc1922f392746ea84df1c47a46577ed2db32e9e47ec44ad248207c5ac7da179becb712ef

sci-libs/lapack/files/lapack-3.9.0-build-tests.patch
@@ -1,0 +1,20 @@
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index d9dbf97..9635ba4 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -127,7 +127,7 @@ set(PKG_CONFIG_DIR ${CMAKE_INSTALL_LIBDIR}/pkgconfig)
+ 
+ # --------------------------------------------------
+ # Testing
+-option(BUILD_TESTING "Build tests" ${_is_coverage_build})
++option(BUILD_TESTING "Build tests")
+ include(CTest)
+ message(STATUS "Build tests: ${BUILD_TESTING}")
+ 
+@@ -443,4 +443,4 @@ install(FILES
+   DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/lapack-${LAPACK_VERSION}
+   COMPONENT Development
+   )
+-  
+\ No newline at end of file
++  

commit 2eed2da849b1b4d2cac23b9e913259450957ce6e

sci-libs/lapack: build tests conditionally
This significantly reduces build time on non-test builds.

Fixes: 1a9d2a5e0166770db4aefdd7f688e332a32f858a
Package-Manager: Portage-3.0.4, Repoman-3.0.1
Signed-off-by: Sam James <sam@gentoo.org>

sci-libs/lapack/lapack-3.9.0.ebuild
@@ -30,6 +30,10 @@ RDEPEND="
 DEPEND="${RDEPEND}
 	virtual/pkgconfig"
 
+PATCHES=(
+	"${FILESDIR}/${PN}-3.9.0-build-tests.patch"
+)
+
 src_configure() {
 	local mycmakeargs=(
 		-DCBLAS=ON