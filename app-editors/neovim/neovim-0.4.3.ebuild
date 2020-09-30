gentoo/gentoo

commit f60946ba6398fac3388cb690d6c687b03d7d8d4d

app-editors/neovim: bump to 0.4.4
* Bump to 0.4.4
* Pave the way for tests, but we can't yet because
  dev-lua/busted lacks luajit support (needed for tests).
* Make LTO optional.

Bug: https://bugs.gentoo.org/584694
Closes: https://bugs.gentoo.org/744184
Package-Manager: Portage-3.0.4, Repoman-3.0.1
Signed-off-by: Sam James <sam@gentoo.org>

app-editors/neovim/neovim-0.4.3.ebuild
@@ -26,7 +26,6 @@ BDEPEND="
 	virtual/libintl
 	virtual/pkgconfig
 "
-
 DEPEND="
 	dev-libs/libuv:0=
 	>=dev-libs/libvterm-0.1.2
@@ -45,15 +44,12 @@ DEPEND="
 		>=dev-libs/unibilium-2.0.0:0=
 	)
 "
-
 RDEPEND="
 	${DEPEND}
 	app-eselect/eselect-vi
 "
 
 CMAKE_BUILD_TYPE=Release
-
-PATCHES=( "${FILESDIR}"/${P}-gcc-10-fix.patch )
 
 src_prepare() {
 	# use our system vim dir

app-editors/neovim/Manifest
@@ -1,1 +1,2 @@
 DIST neovim-0.4.3.tar.gz 9556199 BLAKE2B 831f4d4950f4fa2cd9c7393824bbb5eb571ae5759d13af9f320e0fa351fa155413a5be580f010f2c7ab43ca7bc10c569ccf6e3ba29efc7f5a035576b030b216d SHA512 e13853fa296eda8618f389c71b6cbbd6f01d561615e80cc92959131dd10e395b1c6732a7d9ef6dbb9fe3ea9da4c11485b464547e2d46b22e59b8a20214e861f5
+DIST neovim-0.4.4.tar.gz 9558246 BLAKE2B b1d44898bb1e1ab40a7e1d7fb4448076c991ebe1a0956674548de82fe7a931d9eaffbf35ba03cf9c88466dcd628a55f1d7bf9e7a13f3dad74424d6d653ded60c SHA512 ca5c2fe1784ac7b0d2117948ba2e9ae5d94e36d22ff9e0967047e1e03e605537672d85543897af335103215ad462c86962f25267d352a77d61bc3d1cafb3c183

app-editors/neovim/metadata.xml
@@ -6,6 +6,7 @@
 		<name>Gentoo Vim Project</name>
 	</maintainer>
 	<use>
+		<flag name="lto">Build with Link Time Optimization (LTO)</flag>
 		<flag name="nvimpager">Install nvimpager symlink to less.sh macro</flag>
 		<flag name="tui">Build the neovim unix tui</flag>
 	</use>

app-editors/neovim/neovim-0.4.4.ebuild
@@ -33,7 +33,6 @@ BDEPEND="
 # Once dev-lua/busted has luajit support, we can add tests.
 # bug #584694
 DEPEND="
-	dev-libs/libutf8proc:=
 	dev-libs/libuv:0=
 	>=dev-libs/libvterm-0.1.2
 	dev-libs/msgpack:0=

commit 3d70680747d1f223b52a6508c4031aac655634b0

app-editors/neovim: restore gcc 10 patch in 0.4.3
Accidentally removed earlier as began editing other ebuild.
This version was immediately shadowed by 0.4.4 however.

Fixes: f60946ba6398fac3388cb690d6c687b03d7d8d4d
Package-Manager: Portage-3.0.4, Repoman-3.0.1
Signed-off-by: Sam James <sam@gentoo.org>

app-editors/neovim/neovim-0.4.3.ebuild
@@ -51,6 +51,8 @@ RDEPEND="
 
 CMAKE_BUILD_TYPE=Release
 
+PATCHES=( "${FILESDIR}"/${P}-gcc-10-fix.patch )
+
 src_prepare() {
 	# use our system vim dir
 	sed -e "/^# define SYS_VIMRC_FILE/s|\$VIM|${EPREFIX}/etc/vim|" \