9f97522a927d5650204c5228dab773cb480d67de
Thomas Deutschmann <whissi@gentoo.org>
2020-10-03 18:13:58 +0200

    www-client/firefox-bin: bump to v81.0.1
    Package-Manager: Portage-3.0.8, Repoman-3.0.1
    Signed-off-by: Thomas Deutschmann <whissi@gentoo.org>

---
+++ www-client/firefox-bin/files/gentoo-default-prefs.js
@@ -0,0 +1,12 @@
+pref("general.smoothScroll",               true);
+pref("general.autoScroll",                 false);
+pref("browser.urlbar.hideGoButton",        true);
+pref("accessibility.typeaheadfind",        true);
+pref("browser.shell.checkDefaultBrowser",  false);
+pref("browser.EULA.override",              true);
+pref("general.useragent.locale",           "chrome://global/locale/intl.properties");
+pref("intl.locale.requested",              "");
+/* Disable DoH by default */
+pref("network.trr.mode",                   5);
+/* Disable use of Mozilla Normandy service by default */
+pref("app.normandy.enabled",               false);
