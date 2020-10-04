gentoo/gentoo

commit 249a76e82566d2597d8a5f5c1445f49bce6e54e4

sci-calculators/units: USE flag 'units_cur' uses reserved underscore character
Package-Manager: Portage-2.3.76, Repoman-2.3.17
Signed-off-by: Jeroen Roovers <jer@gentoo.org>

sci-calculators/units/metadata.xml
@@ -6,6 +6,6 @@
 <name>Jeroen Roovers</name>
 </maintainer>
 <use>
-<flag name="units_cur">Install the units_cur python script that updates currencies</flag>
+<flag name="units--cur">Install the units_cur python script that updates currencies</flag>
 </use>
 </pkgmetadata>

sci-calculators/units/units-2.18.ebuild
@@ -12,12 +12,12 @@ SRC_URI="mirror://gnu/${PN}/${P}.tar.gz"
 LICENSE="FDL-1.3 GPL-3"
 SLOT="0"
 KEYWORDS="~alpha amd64 ~arm hppa ~ppc ~ppc64 ~sparc x86 ~amd64-linux ~x86-linux"
-IUSE="+units_cur"
-REQUIRED_USE="units_cur? ( ${PYTHON_REQUIRED_USE} )"
+IUSE="+units--cur"
+REQUIRED_USE="units--cur? ( ${PYTHON_REQUIRED_USE} )"
 
 RDEPEND="
 	sys-libs/readline:=
-	units_cur? (
+	units--cur? (
 		${PYTHON_DEPS}
 		dev-python/future[${PYTHON_USEDEP}]
 		dev-python/requests[${PYTHON_USEDEP}]
@@ -31,7 +31,7 @@ PATCHES=(
 )
 
 pkg_setup() {
-	use units_cur && python-single-r1_pkg_setup
+	use units--cur && python-single-r1_pkg_setup
 }
 
 src_configure() {
@@ -52,7 +52,7 @@ src_install() {
 	# we're intentionally delaying this since 'make install' would
 	# get confused if we shove 'units_cur' there, and there is no real
 	# need to add more complexity for it
-	if use units_cur; then
+	if use units--cur; then
 		sed \
 			-e "/^outfile/s|'.*'|'/usr/share/units/currency.units'|g" \
 			-e 's|^#!|&/usr/bin/python|g' \

sci-calculators/units/units-2.16.ebuild
@@ -12,12 +12,12 @@ SRC_URI="mirror://gnu/${PN}/${P}.tar.gz"
 LICENSE="FDL-1.3 GPL-3"
 SLOT="0"
 KEYWORDS="alpha amd64 ~arm hppa ppc ppc64 ~sparc x86 ~amd64-linux ~x86-linux"
-IUSE="+units_cur"
-REQUIRED_USE="units_cur? ( ${PYTHON_REQUIRED_USE} )"
+IUSE="+units--cur"
+REQUIRED_USE="units--cur? ( ${PYTHON_REQUIRED_USE} )"
 
 RDEPEND="
 	sys-libs/readline:=
-	units_cur? (
+	units--cur? (
 		${PYTHON_DEPS}
 		dev-python/future[${PYTHON_USEDEP}]
 		dev-python/requests[${PYTHON_USEDEP}]
@@ -28,7 +28,7 @@ DEPEND="
 "
 
 pkg_setup() {
-	use units_cur && python-single-r1_pkg_setup
+	use units--cur && python-single-r1_pkg_setup
 }
 
 src_configure() {
@@ -47,7 +47,7 @@ src_install() {
 	# we're intentionally delaying this since 'make install' would
 	# get confused if we shove 'units_cur' there, and there is no real
 	# need to add more complexity for it
-	if use units_cur; then
+	if use units--cur; then
 		sed \
 			-e "/^outfile/s|'.*'|'/usr/share/units/currency.units'|g" \
 			-e 's|^#!|&/usr/bin/python|g' \

sci-calculators/units/units-2.19.ebuild
@@ -12,12 +12,12 @@ SRC_URI="mirror://gnu/${PN}/${P}.tar.gz"
 LICENSE="FDL-1.3 GPL-3"
 SLOT="0"
 KEYWORDS="~alpha ~amd64 ~arm ~hppa ~ppc ~ppc64 ~sparc ~x86 ~amd64-linux ~x86-linux"
-IUSE="+units_cur"
-REQUIRED_USE="units_cur? ( ${PYTHON_REQUIRED_USE} )"
+IUSE="+units--cur"
+REQUIRED_USE="units--cur? ( ${PYTHON_REQUIRED_USE} )"
 
 RDEPEND="
 	sys-libs/readline:=
-	units_cur? (
+	units--cur? (
 		${PYTHON_DEPS}
 		dev-python/future[${PYTHON_USEDEP}]
 		dev-python/requests[${PYTHON_USEDEP}]
@@ -29,9 +29,12 @@ DEPEND="
 PATCHES=(
 	"${FILESDIR}"/${PN}-2.17-network-sandbox.patch
 )
+DOCS=(
+	ChangeLog NEWS README
+)
 
 pkg_setup() {
-	use units_cur && python-single-r1_pkg_setup
+	use units--cur && python-single-r1_pkg_setup
 }
 
 src_configure() {
@@ -45,14 +48,12 @@ src_compile() {
 }
 
 src_install() {
-	emake DESTDIR="${D}" install
-
-	dodoc ChangeLog NEWS README
+	default
 
 	# we're intentionally delaying this since 'make install' would
 	# get confused if we shove 'units_cur' there, and there is no real
 	# need to add more complexity for it
-	if use units_cur; then
+	if use units--cur; then
 		sed \
 			-e "/^outfile/s|'.*'|'/usr/share/units/currency.units'|g" \
 			-e 's|^#!|&/usr/bin/python|g' \

commit d1d3eef2eccc25444da4f05b1cae90dccf402b80

sci-calculators/units: Fix unwanted double hyphen in USE flag name
Package-Manager: Portage-3.0.8, Repoman-3.0.1
Fixes: 249a76e82566d2597d8a5f5c1445f49bce6e54e4
Signed-off-by: Jeroen Roovers <jer@gentoo.org>

sci-calculators/units/metadata.xml
@@ -6,6 +6,6 @@
 <name>Jeroen Roovers</name>
 </maintainer>
 <use>
-<flag name="units--cur">Install the units_cur python script that updates currencies</flag>
+<flag name="units-cur">Install the units_cur python script that updates currencies</flag>
 </use>
 </pkgmetadata>

sci-calculators/units/units-2.16-r1.ebuild
@@ -12,12 +12,12 @@ SRC_URI="mirror://gnu/${PN}/${P}.tar.gz"
 LICENSE="FDL-1.3 GPL-3"
 SLOT="0"
 KEYWORDS="~alpha amd64 ~arm hppa ppc ppc64 ~sparc x86 ~amd64-linux ~x86-linux"
-IUSE="+units--cur"
-REQUIRED_USE="units--cur? ( ${PYTHON_REQUIRED_USE} )"
+IUSE="+units-cur"
+REQUIRED_USE="units-cur? ( ${PYTHON_REQUIRED_USE} )"
 
 RDEPEND="
 	sys-libs/readline:=
-	units--cur? (
+	units-cur? (
 		${PYTHON_DEPS}
 		$(python_gen_cond_dep '
 			dev-python/future[${PYTHON_MULTI_USEDEP}]
@@ -30,7 +30,7 @@ DEPEND="
 "
 
 pkg_setup() {
-	use units--cur && python-single-r1_pkg_setup
+	use units-cur && python-single-r1_pkg_setup
 }
 
 src_configure() {
@@ -49,7 +49,7 @@ src_install() {
 	# we're intentionally delaying this since 'make install' would
 	# get confused if we shove 'units_cur' there, and there is no real
 	# need to add more complexity for it
-	if use units--cur; then
+	if use units-cur; then
 		sed \
 			-e "/^outfile/s|'.*'|'/usr/share/units/currency.units'|g" \
 			-e 's|^#!|&/usr/bin/python|g' \

sci-calculators/units/units-2.18.ebuild
@@ -12,12 +12,12 @@ SRC_URI="mirror://gnu/${PN}/${P}.tar.gz"
 LICENSE="FDL-1.3 GPL-3"
 SLOT="0"
 KEYWORDS="~alpha amd64 ~arm hppa ~ppc ~ppc64 ~sparc x86 ~amd64-linux ~x86-linux"
-IUSE="+units--cur"
-REQUIRED_USE="units--cur? ( ${PYTHON_REQUIRED_USE} )"
+IUSE="+units-cur"
+REQUIRED_USE="units-cur? ( ${PYTHON_REQUIRED_USE} )"
 
 RDEPEND="
 	sys-libs/readline:=
-	units--cur? (
+	units-cur? (
 		${PYTHON_DEPS}
 		$(python_gen_cond_dep '
 			dev-python/future[${PYTHON_MULTI_USEDEP}]
@@ -33,7 +33,7 @@ PATCHES=(
 )
 
 pkg_setup() {
-	use units--cur && python-single-r1_pkg_setup
+	use units-cur && python-single-r1_pkg_setup
 }
 
 src_configure() {
@@ -54,7 +54,7 @@ src_install() {
 	# we're intentionally delaying this since 'make install' would
 	# get confused if we shove 'units_cur' there, and there is no real
 	# need to add more complexity for it
-	if use units--cur; then
+	if use units-cur; then
 		sed \
 			-e "/^outfile/s|'.*'|'/usr/share/units/currency.units'|g" \
 			-e 's|^#!|&/usr/bin/python|g' \

sci-calculators/units/units-2.19-r1.ebuild
@@ -12,12 +12,12 @@ SRC_URI="mirror://gnu/${PN}/${P}.tar.gz"
 LICENSE="FDL-1.3 GPL-3"
 SLOT="0"
 KEYWORDS="~alpha amd64 ~arm hppa ~ppc ~ppc64 ~sparc x86 ~amd64-linux ~x86-linux"
-IUSE="+units--cur"
-REQUIRED_USE="units--cur? ( ${PYTHON_REQUIRED_USE} )"
+IUSE="+units-cur"
+REQUIRED_USE="units-cur? ( ${PYTHON_REQUIRED_USE} )"
 
 RDEPEND="
 	sys-libs/readline:=
-	units--cur? (
+	units-cur? (
 		${PYTHON_DEPS}
 		$(python_gen_cond_dep '
 			dev-python/future[${PYTHON_MULTI_USEDEP}]
@@ -36,7 +36,7 @@ DOCS=(
 )
 
 pkg_setup() {
-	use units--cur && python-single-r1_pkg_setup
+	use units-cur && python-single-r1_pkg_setup
 }
 
 src_configure() {
@@ -55,7 +55,7 @@ src_install() {
 	# we're intentionally delaying this since 'make install' would
 	# get confused if we shove 'units_cur' there, and there is no real
 	# need to add more complexity for it
-	if use units--cur; then
+	if use units-cur; then
 		sed \
 			-e "/^outfile/s|'.*'|'/usr/share/units/currency.units'|g" \
 			-e 's|^#!|&/usr/bin/python|g' \

sci-calculators/units/units-2.20.ebuild
@@ -12,12 +12,12 @@ SRC_URI="mirror://gnu/${PN}/${P}.tar.gz"
 LICENSE="FDL-1.3 GPL-3"
 SLOT="0"
 KEYWORDS="~alpha ~amd64 ~arm ~hppa ~ppc ~ppc64 ~sparc ~x86 ~amd64-linux ~x86-linux"
-IUSE="+units--cur"
-REQUIRED_USE="units--cur? ( ${PYTHON_REQUIRED_USE} )"
+IUSE="+units-cur"
+REQUIRED_USE="units-cur? ( ${PYTHON_REQUIRED_USE} )"
 
 RDEPEND="
 	sys-libs/readline:=
-	units--cur? (
+	units-cur? (
 		${PYTHON_DEPS}
 		$(python_gen_cond_dep '
 			dev-python/future[${PYTHON_USEDEP}]
@@ -50,7 +50,7 @@ src_compile() {
 src_install() {
 	default
 
-	if use units--cur; then
+	if use units-cur; then
 		sed \
 			-e "/^outfile/s|'.*'|'/usr/share/units/currency.units'|g" \
 			-e 's|^#!|&/usr/bin/python|g' \