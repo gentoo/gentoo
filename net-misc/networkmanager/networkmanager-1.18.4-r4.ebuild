gentoo/gentoo

commit 1b636741c5d5a793b49abfa914f2d11241fec178

net-misc/networkmanager: drop USE=consolekit
Package-Manager: Portage-3.0.4, Repoman-3.0.1
Signed-off-by: Sam James <sam@gentoo.org>
Signed-off-by: Mikle Kolyada <zlogene@gentoo.org>

net-misc/networkmanager/networkmanager-1.18.4-r4.ebuild
@@ -15,7 +15,7 @@ HOMEPAGE="https://wiki.gnome.org/Projects/NetworkManager"
 LICENSE="GPL-2+"
 SLOT="0" # add subslot if libnm-util.so.2 or libnm-glib.so.4 bumps soname version
 
-IUSE="audit bluetooth connection-sharing consolekit +dhclient dhcpcd elogind gnutls +introspection iwd json kernel_linux +nss +modemmanager ncurses ofono ovs policykit +ppp resolvconf selinux systemd teamd test vala +wext +wifi"
+IUSE="audit bluetooth connection-sharing +dhclient dhcpcd elogind gnutls +introspection iwd json kernel_linux +nss +modemmanager ncurses ofono ovs policykit +ppp resolvconf selinux systemd teamd test vala +wext +wifi"
 RESTRICT="!test? ( test )"
 
 REQUIRED_USE="
@@ -24,7 +24,7 @@ REQUIRED_USE="
 	vala? ( introspection )
 	wext? ( wifi )
 	|| ( nss gnutls )
-	?? ( consolekit elogind systemd )
+	?? ( elogind systemd )
 "
 
 KEYWORDS="~alpha amd64 arm arm64 ~ia64 ppc ppc64 ~sparc x86"
@@ -47,7 +47,6 @@ COMMON_DEPEND="
 	connection-sharing? (
 		net-dns/dnsmasq[dbus,dhcp]
 		net-firewall/iptables )
-	consolekit? ( >=sys-auth/consolekit-1.0.0 )
 	dhclient? ( >=net-misc/dhcp-4[client] )
 	dhcpcd? ( net-misc/dhcpcd )
 	elogind? ( >=sys-auth/elogind-219 )
@@ -104,7 +103,7 @@ DEPEND="${COMMON_DEPEND}
 
 PATCHES=(
 	"${FILESDIR}"/${PN}-data-fix-the-ID_NET_DRIVER-udev-rule.patch
-	"${FILESDIR}"/1.18.4-iwd1-compat.patch # included in 1.21.3+
+	"${FILESDIR}"/${PV}-iwd1-compat.patch # included in 1.21.3+
 )
 
 python_check_deps() {
@@ -185,10 +184,6 @@ multilib_src_configure() {
 		--with-ebpf=yes
 		$(multilib_native_enable concheck)
 		--with-crypto=$(usex nss nss gnutls)
-		--with-session-tracking=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind $(multilib_native_usex consolekit consolekit no)))
-		# ConsoleKit has no build-time dependency, so use it as the default case.
-		# There is no off switch, and we do not support upower.
-		--with-suspend-resume=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind consolekit))
 		$(multilib_native_use_with audit libaudit)
 		$(multilib_native_use_enable bluetooth bluez5-dun)
 		$(use_with dhclient)
@@ -214,6 +209,18 @@ multilib_src_configure() {
 		$(multilib_native_use_with wext)
 		$(multilib_native_use_enable wifi)
 	)
+
+	if use systemd; then
+		myconf+=(
+			--with-session-tracking=systemd
+			--with-suspend-resume=systemd
+		)
+	elif use elogind; then
+		myconf+=(
+			--with-session-tracking=elogind
+			--with-suspend-resume=elogind
+		)
+	fi
 
 	# Same hack as net-dialup/pptpd to get proper plugin dir for ppp, bug #519986
 	if use ppp; then
@@ -285,7 +292,7 @@ multilib_src_install_all() {
 	einstalldocs
 	! use systemd && readme.gentoo_create_doc
 
-	newinitd "${FILESDIR}/init.d.NetworkManager-r2" NetworkManager
+	newinitd "${FILESDIR}/init.d.NetworkManager-r1" NetworkManager
 	newconfd "${FILESDIR}/conf.d.NetworkManager" NetworkManager
 
 	# Need to keep the /etc/NetworkManager/dispatched.d for dispatcher scripts

net-misc/networkmanager/networkmanager-1.18.6-r1.ebuild
@@ -5,7 +5,7 @@ EAPI=6
 GNOME_ORG_MODULE="NetworkManager"
 GNOME2_LA_PUNT="yes"
 VALA_USE_DEPEND="vapigen"
-PYTHON_COMPAT=( python3_{6,7,8} )
+PYTHON_COMPAT=( python{3_6,3_7} )
 
 inherit bash-completion-r1 gnome2 linux-info multilib python-any-r1 systemd readme.gentoo-r1 vala virtualx udev multilib-minimal
 
@@ -13,9 +13,9 @@ DESCRIPTION="A set of co-operative tools that make networking simple and straigh
 HOMEPAGE="https://wiki.gnome.org/Projects/NetworkManager"
 
 LICENSE="GPL-2+"
-SLOT="0"
+SLOT="0" # add subslot if libnm-util.so.2 or libnm-glib.so.4 bumps soname version
 
-IUSE="audit bluetooth connection-sharing consolekit dhclient dhcpcd elogind gnutls +introspection iwd json kernel_linux +nss +modemmanager ncurses ofono ovs policykit +ppp resolvconf selinux systemd teamd test vala +wext +wifi"
+IUSE="audit bluetooth connection-sharing +dhclient dhcpcd elogind gnutls +introspection iwd json kernel_linux +nss +modemmanager ncurses ofono ovs policykit +ppp resolvconf selinux systemd teamd test vala +wext +wifi"
 RESTRICT="!test? ( test )"
 
 REQUIRED_USE="
@@ -24,14 +24,16 @@ REQUIRED_USE="
 	vala? ( introspection )
 	wext? ( wifi )
 	|| ( nss gnutls )
-	?? ( consolekit elogind systemd )
+	?? ( elogind systemd )
 "
 
-KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~ia64 ~ppc ~ppc64 ~sparc ~x86"
+KEYWORDS="~alpha amd64 arm arm64 ~ia64 ppc ppc64 ~sparc x86"
 
 # gobject-introspection-0.10.3 is needed due to gnome bug 642300
 # wpa_supplicant-0.7.3-r3 is needed due to bug 359271
 COMMON_DEPEND="
+	>=sys-apps/dbus-1.2[${MULTILIB_USEDEP}]
+	>=dev-libs/dbus-glib-0.100[${MULTILIB_USEDEP}]
 	>=dev-libs/glib-2.40:2[${MULTILIB_USEDEP}]
 	policykit? ( >=sys-auth/polkit-0.106 )
 	net-libs/libndp[${MULTILIB_USEDEP}]
@@ -45,7 +47,6 @@ COMMON_DEPEND="
 	connection-sharing? (
 		net-dns/dnsmasq[dbus,dhcp]
 		net-firewall/iptables )
-	consolekit? ( >=sys-auth/consolekit-1.0.0 )
 	dhclient? ( >=net-misc/dhcp-4[client] )
 	dhcpcd? ( net-misc/dhcpcd )
 	elogind? ( >=sys-auth/elogind-219 )
@@ -99,6 +100,11 @@ DEPEND="${COMMON_DEPEND}
 			dev-python/pygobject:3[${PYTHON_USEDEP}]')
 	)
 "
+
+PATCHES=(
+	"${FILESDIR}"/${PN}-data-fix-the-ID_NET_DRIVER-udev-rule.patch
+	"${FILESDIR}"/1.18.4-iwd1-compat.patch # included in 1.21.3+
+)
 
 python_check_deps() {
 	if use introspection; then
@@ -163,11 +169,14 @@ multilib_src_configure() {
 		--disable-more-warnings
 		--disable-static
 		--localstatedir=/var
-		--with-runstatedir=/run
 		--disable-lto
+		--disable-config-plugin-ibft
 		--disable-qt
 		--without-netconfig
 		--with-dbus-sys-dir=/etc/dbus-1/system.d
+		# We need --with-libnm-glib (and dbus-glib dep) as reverse deps are
+		# still not ready for removing that lib, bug #665338
+		--with-libnm-glib
 		$(multilib_native_with nmcli)
 		--with-udev-dir="$(get_udevdir)"
 		--with-config-plugins-default=keyfile
@@ -175,16 +184,10 @@ multilib_src_configure() {
 		--with-ebpf=yes
 		$(multilib_native_enable concheck)
 		--with-crypto=$(usex nss nss gnutls)
-		--with-session-tracking=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind $(multilib_native_usex consolekit consolekit no)))
-		# ConsoleKit has no build-time dependency, so use it as the default case.
-		# There is no off switch, and we do not support upower.
-		--with-suspend-resume=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind consolekit))
 		$(multilib_native_use_with audit libaudit)
 		$(multilib_native_use_enable bluetooth bluez5-dun)
-		--without-dhcpcanon
 		$(use_with dhclient)
 		$(use_with dhcpcd)
-		--with-config-dhcp-default=internal
 		$(multilib_native_use_enable introspection)
 		$(use_enable json json-validation)
 		$(multilib_native_use_enable ppp)
@@ -207,6 +210,19 @@ multilib_src_configure() {
 		$(multilib_native_use_enable wifi)
 	)
 
+	# There is no off switch, and we do not support upower.
+	if use systemd; then
+		myconf+=(
+			--with-session-tracking=systemd
+			--with-suspend-resume=systemd
+		)
+	elif use elogind; then
+		myconf+=(
+			--with-session-tracking=elogind
+			--with-suspend-resume=elogind
+		)
+	fi
+
 	# Same hack as net-dialup/pptpd to get proper plugin dir for ppp, bug #519986
 	if use ppp; then
 		local PPPD_VER=`best_version net-dialup/ppp`
@@ -225,7 +241,7 @@ multilib_src_configure() {
 		ln -s "${S}/man" man || die
 	fi
 
-	ECONF_SOURCE=${S} gnome2_src_configure "${myconf[@]}"
+	ECONF_SOURCE=${S} runstatedir="/run" gnome2_src_configure "${myconf[@]}"
 }
 
 multilib_src_compile() {
@@ -234,6 +250,9 @@ multilib_src_compile() {
 	else
 		local targets=(
 			libnm/libnm.la
+			libnm-util/libnm-util.la
+			libnm-glib/libnm-glib.la
+			libnm-glib/libnm-glib-vpn.la
 		)
 		emake "${targets[@]}"
 	fi
@@ -255,7 +274,14 @@ multilib_src_install() {
 	else
 		local targets=(
 			install-libLTLIBRARIES
+			install-libdeprecatedHEADERS
+			install-libnm_glib_libnmvpnHEADERS
+			install-libnm_glib_libnmincludeHEADERS
+			install-libnm_util_libnm_util_includeHEADERS
 			install-libnmincludeHEADERS
+			install-nodist_libnm_glib_libnmincludeHEADERS
+			install-nodist_libnm_glib_libnmvpnHEADERS
+			install-nodist_libnm_util_libnm_util_includeHEADERS
 			install-nodist_libnmincludeHEADERS
 			install-pkgconfigDATA
 		)
@@ -327,13 +353,5 @@ pkg_postinst() {
 		ewarn "You have psk-flags=1 setting in above files, you will need to"
 		ewarn "either reconfigure affected networks or, at least, set the flag"
 		ewarn "value to '0'."
-	fi
-
-	if use dhclient || use dhcpcd; then
-		ewarn "You have enabled USE=dhclient and/or USE=dhcpcd, but NetworkManager since"
-		ewarn "version 1.20 defaults to the internal DHCP client. If the internal client"
-		ewarn "works for you, and you're happy with, the alternative USE flags can be"
-		ewarn "disabled. If you want to use dhclient or dhcpcd, then you need to tweak"
-		ewarn "the main.dhcp configuration option to use one of them instead of internal."
 	fi
 }

net-misc/networkmanager/networkmanager-1.20.12-r1.ebuild
@@ -15,7 +15,7 @@ HOMEPAGE="https://wiki.gnome.org/Projects/NetworkManager"
 LICENSE="GPL-2+"
 SLOT="0"
 
-IUSE="audit bluetooth connection-sharing consolekit dhclient dhcpcd elogind gnutls +introspection iwd json kernel_linux +nss +modemmanager ncurses ofono ovs policykit +ppp resolvconf selinux systemd teamd test vala +wext +wifi"
+IUSE="audit bluetooth connection-sharing dhclient dhcpcd elogind gnutls +introspection iwd json kernel_linux +nss +modemmanager ncurses ofono ovs policykit +ppp resolvconf selinux systemd teamd test vala +wext +wifi"
 RESTRICT="!test? ( test )"
 
 REQUIRED_USE="
@@ -24,10 +24,10 @@ REQUIRED_USE="
 	vala? ( introspection )
 	wext? ( wifi )
 	|| ( nss gnutls )
-	?? ( consolekit elogind systemd )
+	?? ( elogind systemd )
 "
 
-KEYWORDS="~alpha amd64 arm arm64 ~ia64 ppc ppc64 ~sparc x86"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~ia64 ~ppc ~ppc64 ~sparc ~x86"
 
 # gobject-introspection-0.10.3 is needed due to gnome bug 642300
 # wpa_supplicant-0.7.3-r3 is needed due to bug 359271
@@ -45,14 +45,12 @@ COMMON_DEPEND="
 	connection-sharing? (
 		net-dns/dnsmasq[dbus,dhcp]
 		net-firewall/iptables )
-	consolekit? ( >=sys-auth/consolekit-1.0.0 )
 	dhclient? ( >=net-misc/dhcp-4[client] )
 	dhcpcd? ( net-misc/dhcpcd )
 	elogind? ( >=sys-auth/elogind-219 )
 	introspection? ( >=dev-libs/gobject-introspection-0.10.3:= )
 	json? ( >=dev-libs/jansson-2.5[${MULTILIB_USEDEP}] )
-	modemmanager? ( >=net-misc/modemmanager-0.7.991:0=
-		net-misc/mobile-broadband-provider-info )
+	modemmanager? ( >=net-misc/modemmanager-0.7.991:0= )
 	ncurses? ( >=dev-libs/newt-0.52.15 )
 	nss? ( >=dev-libs/nss-3.11:=[${MULTILIB_USEDEP}] )
 	!nss? ( gnutls? (
@@ -175,12 +173,7 @@ multilib_src_configure() {
 		--with-iptables=/sbin/iptables
 		--with-ebpf=yes
 		$(multilib_native_enable concheck)
-		--with-nm-cloud-setup=$(multilib_is_native_abi && echo yes || echo no)
 		--with-crypto=$(usex nss nss gnutls)
-		--with-session-tracking=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind $(multilib_native_usex consolekit consolekit no)))
-		# ConsoleKit has no build-time dependency, so use it as the default case.
-		# There is no off switch, and we do not support upower.
-		--with-suspend-resume=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind consolekit))
 		$(multilib_native_use_with audit libaudit)
 		$(multilib_native_use_enable bluetooth bluez5-dun)
 		--without-dhcpcanon
@@ -196,6 +189,7 @@ multilib_src_configure() {
 		$(multilib_native_use_with ofono)
 		$(multilib_native_use_enable ovs)
 		$(multilib_native_use_enable policykit polkit)
+		$(multilib_native_use_enable policykit polkit-agent)
 		$(multilib_native_use_with resolvconf)
 		$(multilib_native_use_with selinux)
 		$(multilib_native_use_with systemd systemd-journal)
@@ -207,6 +201,19 @@ multilib_src_configure() {
 		$(multilib_native_use_with wext)
 		$(multilib_native_use_enable wifi)
 	)
+
+	# There is no off switch, and we do not support upower.
+	if use systemd; then
+		myconf+=(
+			--with-session-tracking=systemd
+			--with-suspend-resume=systemd
+		)
+	elif use elogind; then
+		myconf+=(
+			--with-session-tracking=elogind
+			--with-suspend-resume=elogind
+		)
+	fi
 
 	# Same hack as net-dialup/pptpd to get proper plugin dir for ppp, bug #519986
 	if use ppp; then

net-misc/networkmanager/networkmanager-1.22.14-r1.ebuild
@@ -15,7 +15,7 @@ HOMEPAGE="https://wiki.gnome.org/Projects/NetworkManager"
 LICENSE="GPL-2+"
 SLOT="0"
 
-IUSE="audit bluetooth connection-sharing consolekit dhclient dhcpcd elogind gnutls +introspection iwd json kernel_linux +nss +modemmanager ncurses ofono ovs policykit +ppp resolvconf selinux systemd teamd test vala +wext +wifi"
+IUSE="audit bluetooth connection-sharing dhclient dhcpcd elogind gnutls +introspection iwd json kernel_linux +nss +modemmanager ncurses ofono ovs policykit +ppp resolvconf selinux systemd teamd test vala +wext +wifi"
 RESTRICT="!test? ( test )"
 
 REQUIRED_USE="
@@ -24,7 +24,7 @@ REQUIRED_USE="
 	vala? ( introspection )
 	wext? ( wifi )
 	|| ( nss gnutls )
-	?? ( consolekit elogind systemd )
+	?? ( elogind systemd )
 "
 
 KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~ia64 ~ppc ~ppc64 ~sparc ~x86"
@@ -45,7 +45,6 @@ COMMON_DEPEND="
 	connection-sharing? (
 		net-dns/dnsmasq[dbus,dhcp]
 		net-firewall/iptables )
-	consolekit? ( >=sys-auth/consolekit-1.0.0 )
 	dhclient? ( >=net-misc/dhcp-4[client] )
 	dhcpcd? ( net-misc/dhcpcd )
 	elogind? ( >=sys-auth/elogind-219 )
@@ -177,10 +176,6 @@ multilib_src_configure() {
 		$(multilib_native_enable concheck)
 		--with-nm-cloud-setup=$(multilib_is_native_abi && echo yes || echo no)
 		--with-crypto=$(usex nss nss gnutls)
-		--with-session-tracking=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind $(multilib_native_usex consolekit consolekit no)))
-		# ConsoleKit has no build-time dependency, so use it as the default case.
-		# There is no off switch, and we do not support upower.
-		--with-suspend-resume=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind consolekit))
 		$(multilib_native_use_with audit libaudit)
 		$(multilib_native_use_enable bluetooth bluez5-dun)
 		--without-dhcpcanon
@@ -208,6 +203,19 @@ multilib_src_configure() {
 		$(multilib_native_use_with wext)
 		$(multilib_native_use_enable wifi)
 	)
+
+	# There is no off switch, and we do not support upower.
+	if use systemd; then
+		myconf+=(
+			--with-session-tracking=systemd
+			--with-suspend-resume=systemd
+		)
+	elif use elogind; then
+		myconf+=(
+			--with-session-tracking=elogind
+			--with-suspend-resume=elogind
+		)
+	fi
 
 	# Same hack as net-dialup/pptpd to get proper plugin dir for ppp, bug #519986
 	if use ppp; then

net-misc/networkmanager/networkmanager-1.24.2-r1.ebuild
@@ -15,7 +15,7 @@ HOMEPAGE="https://wiki.gnome.org/Projects/NetworkManager"
 LICENSE="GPL-2+"
 SLOT="0"
 
-IUSE="audit bluetooth connection-sharing consolekit dhclient dhcpcd elogind gnutls +introspection iwd json kernel_linux +nss +modemmanager ncurses ofono ovs policykit +ppp resolvconf selinux systemd teamd test vala +wext +wifi"
+IUSE="audit bluetooth connection-sharing dhclient dhcpcd elogind gnutls +introspection iwd json kernel_linux +nss +modemmanager ncurses ofono ovs policykit +ppp resolvconf selinux systemd teamd test vala +wext +wifi"
 RESTRICT="!test? ( test )"
 
 REQUIRED_USE="
@@ -24,7 +24,7 @@ REQUIRED_USE="
 	vala? ( introspection )
 	wext? ( wifi )
 	|| ( nss gnutls )
-	?? ( consolekit elogind systemd )
+	?? ( elogind systemd )
 "
 
 KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~ia64 ~ppc ~ppc64 ~sparc ~x86"
@@ -45,7 +45,6 @@ COMMON_DEPEND="
 	connection-sharing? (
 		net-dns/dnsmasq[dbus,dhcp]
 		net-firewall/iptables )
-	consolekit? ( >=sys-auth/consolekit-1.0.0 )
 	dhclient? ( >=net-misc/dhcp-4[client] )
 	dhcpcd? ( net-misc/dhcpcd )
 	elogind? ( >=sys-auth/elogind-219 )
@@ -177,10 +176,6 @@ multilib_src_configure() {
 		$(multilib_native_enable concheck)
 		--with-nm-cloud-setup=$(multilib_is_native_abi && echo yes || echo no)
 		--with-crypto=$(usex nss nss gnutls)
-		--with-session-tracking=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind $(multilib_native_usex consolekit consolekit no)))
-		# ConsoleKit has no build-time dependency, so use it as the default case.
-		# There is no off switch, and we do not support upower.
-		--with-suspend-resume=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind consolekit))
 		$(multilib_native_use_with audit libaudit)
 		$(multilib_native_use_enable bluetooth bluez5-dun)
 		--without-dhcpcanon
@@ -207,6 +202,19 @@ multilib_src_configure() {
 		$(multilib_native_use_with wext)
 		$(multilib_native_use_enable wifi)
 	)
+
+	# There is no off switch, and we do not support upower.
+	if use systemd; then
+		myconf+=(
+			--with-session-tracking=systemd
+			--with-suspend-resume=systemd
+		)
+	elif use elogind; then
+		myconf+=(
+			--with-session-tracking=elogind
+			--with-suspend-resume=elogind
+		)
+	fi
 
 	# Same hack as net-dialup/pptpd to get proper plugin dir for ppp, bug #519986
 	if use ppp; then

commit 90701330ab4b859f2e1d57124bd092a85ba7d8cb

net-misc/networkmanager: drop USE=consolekit again
Two problems with the initial draft PR:
1) elogind does not have multilib, so...
2) we must (ab)use 'consolekit' as a dummy option
   for the build system in the non-native multilib
   case.

Fixes: 1b636741c5d5a793b49abfa914f2d11241fec178
Closes: https://bugs.gentoo.org/747358
Package-Manager: Portage-3.0.4, Repoman-3.0.1
Signed-off-by: Sam James <sam@gentoo.org>

net-misc/networkmanager/networkmanager-1.18.4-r4.ebuild
@@ -184,6 +184,12 @@ multilib_src_configure() {
 		--with-ebpf=yes
 		$(multilib_native_enable concheck)
 		--with-crypto=$(usex nss nss gnutls)
+		# elogind lacks multilib for now, and consolekit doesn't require linking against, so we use it as a fake option
+		# This SHOULD be removable once elogind has that. We abuse the fact that 'consolekit' does nothing at buildtime.
+		# (There is no off switch, and we do not support upower.)
+		# bug #747358
+		--with-session-tracking=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind consolekit))
+		--with-suspend-resume=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind consolekit))
 		$(multilib_native_use_with audit libaudit)
 		$(multilib_native_use_enable bluetooth bluez5-dun)
 		$(use_with dhclient)
@@ -209,18 +215,6 @@ multilib_src_configure() {
 		$(multilib_native_use_with wext)
 		$(multilib_native_use_enable wifi)
 	)
-
-	if use systemd; then
-		myconf+=(
-			--with-session-tracking=systemd
-			--with-suspend-resume=systemd
-		)
-	elif use elogind; then
-		myconf+=(
-			--with-session-tracking=elogind
-			--with-suspend-resume=elogind
-		)
-	fi
 
 	# Same hack as net-dialup/pptpd to get proper plugin dir for ppp, bug #519986
 	if use ppp; then

net-misc/networkmanager/networkmanager-1.18.6-r1.ebuild
@@ -184,6 +184,12 @@ multilib_src_configure() {
 		--with-ebpf=yes
 		$(multilib_native_enable concheck)
 		--with-crypto=$(usex nss nss gnutls)
+		# elogind lacks multilib for now, and consolekit doesn't require linking against, so we use it as a fake option
+		# This SHOULD be removable once elogind has that. We abuse the fact that 'consolekit' does nothing at buildtime.
+		# (There is no off switch, and we do not support upower.)
+		# bug #747358
+		--with-session-tracking=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind consolekit))
+		--with-suspend-resume=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind consolekit))
 		$(multilib_native_use_with audit libaudit)
 		$(multilib_native_use_enable bluetooth bluez5-dun)
 		$(use_with dhclient)
@@ -209,19 +215,6 @@ multilib_src_configure() {
 		$(multilib_native_use_with wext)
 		$(multilib_native_use_enable wifi)
 	)
-
-	# There is no off switch, and we do not support upower.
-	if use systemd; then
-		myconf+=(
-			--with-session-tracking=systemd
-			--with-suspend-resume=systemd
-		)
-	elif use elogind; then
-		myconf+=(
-			--with-session-tracking=elogind
-			--with-suspend-resume=elogind
-		)
-	fi
 
 	# Same hack as net-dialup/pptpd to get proper plugin dir for ppp, bug #519986
 	if use ppp; then

net-misc/networkmanager/networkmanager-1.20.12-r1.ebuild
@@ -174,6 +174,12 @@ multilib_src_configure() {
 		--with-ebpf=yes
 		$(multilib_native_enable concheck)
 		--with-crypto=$(usex nss nss gnutls)
+		# elogind lacks multilib for now, and consolekit doesn't require linking against, so we use it as a fake option
+		# This SHOULD be removable once elogind has that. We abuse the fact that 'consolekit' does nothing at buildtime.
+		# (There is no off switch, and we do not support upower.)
+		# bug #747358
+		--with-session-tracking=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind consolekit))
+		--with-suspend-resume=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind consolekit))
 		$(multilib_native_use_with audit libaudit)
 		$(multilib_native_use_enable bluetooth bluez5-dun)
 		--without-dhcpcanon
@@ -201,19 +207,6 @@ multilib_src_configure() {
 		$(multilib_native_use_with wext)
 		$(multilib_native_use_enable wifi)
 	)
-
-	# There is no off switch, and we do not support upower.
-	if use systemd; then
-		myconf+=(
-			--with-session-tracking=systemd
-			--with-suspend-resume=systemd
-		)
-	elif use elogind; then
-		myconf+=(
-			--with-session-tracking=elogind
-			--with-suspend-resume=elogind
-		)
-	fi
 
 	# Same hack as net-dialup/pptpd to get proper plugin dir for ppp, bug #519986
 	if use ppp; then

net-misc/networkmanager/networkmanager-1.22.14-r1.ebuild
@@ -176,6 +176,12 @@ multilib_src_configure() {
 		$(multilib_native_enable concheck)
 		--with-nm-cloud-setup=$(multilib_is_native_abi && echo yes || echo no)
 		--with-crypto=$(usex nss nss gnutls)
+		# elogind lacks multilib for now, and consolekit doesn't require linking against, so we use it as a fake option
+		# This SHOULD be removable once elogind has that. We abuse the fact that 'consolekit' does nothing at buildtime.
+		# (There is no off switch, and we do not support upower.)
+		# bug #747358
+		--with-session-tracking=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind consolekit))
+		--with-suspend-resume=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind consolekit))
 		$(multilib_native_use_with audit libaudit)
 		$(multilib_native_use_enable bluetooth bluez5-dun)
 		--without-dhcpcanon
@@ -203,19 +209,6 @@ multilib_src_configure() {
 		$(multilib_native_use_with wext)
 		$(multilib_native_use_enable wifi)
 	)
-
-	# There is no off switch, and we do not support upower.
-	if use systemd; then
-		myconf+=(
-			--with-session-tracking=systemd
-			--with-suspend-resume=systemd
-		)
-	elif use elogind; then
-		myconf+=(
-			--with-session-tracking=elogind
-			--with-suspend-resume=elogind
-		)
-	fi
 
 	# Same hack as net-dialup/pptpd to get proper plugin dir for ppp, bug #519986
 	if use ppp; then

net-misc/networkmanager/networkmanager-1.24.2-r1.ebuild
@@ -176,6 +176,12 @@ multilib_src_configure() {
 		$(multilib_native_enable concheck)
 		--with-nm-cloud-setup=$(multilib_is_native_abi && echo yes || echo no)
 		--with-crypto=$(usex nss nss gnutls)
+		# elogind lacks multilib for now, and consolekit doesn't require linking against, so we use it as a fake option
+		# This SHOULD be removable once elogind has that. We abuse the fact that 'consolekit' does nothing at buildtime.
+		# (There is no off switch, and we do not support upower.)
+		# bug #747358
+		--with-session-tracking=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind consolekit))
+		--with-suspend-resume=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind consolekit))
 		$(multilib_native_use_with audit libaudit)
 		$(multilib_native_use_enable bluetooth bluez5-dun)
 		--without-dhcpcanon
@@ -202,19 +208,6 @@ multilib_src_configure() {
 		$(multilib_native_use_with wext)
 		$(multilib_native_use_enable wifi)
 	)
-
-	# There is no off switch, and we do not support upower.
-	if use systemd; then
-		myconf+=(
-			--with-session-tracking=systemd
-			--with-suspend-resume=systemd
-		)
-	elif use elogind; then
-		myconf+=(
-			--with-session-tracking=elogind
-			--with-suspend-resume=elogind
-		)
-	fi
 
 	# Same hack as net-dialup/pptpd to get proper plugin dir for ppp, bug #519986
 	if use ppp; then

net-misc/networkmanager/networkmanager-1.26.0-r1.ebuild
@@ -176,6 +176,12 @@ multilib_src_configure() {
 		$(multilib_native_enable concheck)
 		--with-nm-cloud-setup=$(multilib_is_native_abi && echo yes || echo no)
 		--with-crypto=$(usex nss nss gnutls)
+		# elogind lacks multilib for now, and consolekit doesn't require linking against, so we use it as a fake option
+		# This SHOULD be removable once elogind has that. We abuse the fact that 'consolekit' does nothing at buildtime.
+		# (There is no off switch, and we do not support upower.)
+		# bug #747358
+		--with-session-tracking=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind consolekit))
+		--with-suspend-resume=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind consolekit))
 		$(multilib_native_use_with audit libaudit)
 		$(multilib_native_use_enable bluetooth bluez5-dun)
 		--without-dhcpcanon
@@ -202,19 +208,6 @@ multilib_src_configure() {
 		$(multilib_native_use_with wext)
 		$(multilib_native_use_enable wifi)
 	)
-
-	# There is no off switch, and we do not support upower.
-	if use systemd; then
-		myconf+=(
-			--with-session-tracking=systemd
-			--with-suspend-resume=systemd
-		)
-	elif use elogind; then
-		myconf+=(
-			--with-session-tracking=elogind
-			--with-suspend-resume=elogind
-		)
-	fi
 
 	# Same hack as net-dialup/pptpd to get proper plugin dir for ppp, bug #519986
 	if use ppp; then

net-misc/networkmanager/networkmanager-1.26.2-r1.ebuild
@@ -176,6 +176,12 @@ multilib_src_configure() {
 		$(multilib_native_enable concheck)
 		--with-nm-cloud-setup=$(multilib_is_native_abi && echo yes || echo no)
 		--with-crypto=$(usex nss nss gnutls)
+		# elogind lacks multilib for now, and consolekit doesn't require linking against, so we use it as a fake option
+		# This SHOULD be removable once elogind has that. We abuse the fact that 'consolekit' does nothing at buildtime.
+		# (There is no off switch, and we do not support upower.)
+		# bug #747358
+		--with-session-tracking=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind consolekit))
+		--with-suspend-resume=$(multilib_native_usex systemd systemd $(multilib_native_usex elogind elogind consolekit))
 		$(multilib_native_use_with audit libaudit)
 		$(multilib_native_use_enable bluetooth bluez5-dun)
 		--without-dhcpcanon
@@ -202,19 +208,6 @@ multilib_src_configure() {
 		$(multilib_native_use_with wext)
 		$(multilib_native_use_enable wifi)
 	)
-
-	# There is no off switch, and we do not support upower.
-	if use systemd; then
-		myconf+=(
-			--with-session-tracking=systemd
-			--with-suspend-resume=systemd
-		)
-	elif use elogind; then
-		myconf+=(
-			--with-session-tracking=elogind
-			--with-suspend-resume=elogind
-		)
-	fi
 
 	# Same hack as net-dialup/pptpd to get proper plugin dir for ppp, bug #519986
 	if use ppp; then