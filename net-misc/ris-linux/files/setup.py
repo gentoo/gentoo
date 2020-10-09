76e16e88135ef42dc6d16f074acf10f1c70708cc
Michał Górny <mgorny@gentoo.org>
2020-10-09 09:37:58 +0200

    net-misc/ris-linux: Remove last-rited pkg
    Closes: https://bugs.gentoo.org/735428
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- net-misc/ris-linux/files/setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
-
-setup(
-    name = 'ris-linux',
-    version = 'VERSION',
-    scripts = [ 'binlsrv.py', 'decode.py', 'infparser.py', 'fixloader.py', 'modldr.py' ]
-)
