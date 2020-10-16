gentoo/gentoo

commit 19968c73cf72445a2733d60a0ddcea645b2835ca

dev-ml/ppx_assert: Fix deps and compile fail
Thanks to toralf for reporting.

Bug: https://bugs.gentoo.org/749017
Package-Manager: Portage-3.0.8, Repoman-3.0.1
Signed-off-by: Mark Wright <gienah@gentoo.org>

dev-ml/ppx_assert/ppx_assert-0.14.0.ebuild
@@ -11,17 +11,19 @@ SRC_URI="https://github.com/janestreet/${PN}/archive/v${PV}.tar.gz -> ${P}.tar.g
 
 LICENSE="Apache-2.0"
 SLOT="0/${PV}"
-KEYWORDS="~amd64 ~arm ~arm64 ~x86"
+KEYWORDS="~amd64 ~arm ~arm64"
 IUSE="+ocamlopt"
 
 DEPEND="
-	dev-ml/base:=
-	dev-ml/ppx_cold:=
-	dev-ml/ppx_compare:=
-	dev-ml/ppx_here:=
-	dev-ml/ppx_sexp_conv:=
-	dev-ml/ppxlib:=
-		dev-ml/ocaml-migrate-parsetree:=
-			dev-ml/result:=
+	>=dev-ml/base-0.14.0:=
+	dev-ml/findlib:=
+	>=dev-ml/ppx_cold-0.14.0:=
+	>=dev-ml/ppx_compare-0.14.0:=
+	>=dev-ml/ppx_here-0.14.0:=
+	>=dev-ml/ppx_sexp_conv-0.14.1:=
+	>=dev-ml/ppxlib-0.18.0:=
+		>=dev-ml/ocaml-compiler-libs-0.11.0:=
+		>=dev-ml/ocaml-migrate-parsetree-2.0.0:=
+			dev-ml/cinaps:=
 "
 RDEPEND="${DEPEND}"

commit 115deb4d7b9ba023e7aa5927aeaa350891ec25ce

dev-ml: Add some ~x86 keywords to resolve breakage
Fixes: 19968c73cf7 ("dev-ml/ppx_assert: Fix deps and compile fail")
Signed-off-by: Matt Turner <mattst88@gentoo.org>

dev-ml/ppx_assert/ppx_assert-0.14.0.ebuild
@@ -11,7 +11,7 @@ SRC_URI="https://github.com/janestreet/${PN}/archive/v${PV}.tar.gz -> ${P}.tar.g
 
 LICENSE="Apache-2.0"
 SLOT="0/${PV}"
-KEYWORDS="~amd64 ~arm ~arm64"
+KEYWORDS="~amd64 ~arm ~arm64 ~x86"
 IUSE="+ocamlopt"
 
 DEPEND="

dev-ml/ppx_compare/ppx_compare-0.14.0.ebuild
@@ -11,7 +11,7 @@ SRC_URI="https://github.com/janestreet/${PN}/archive/v${PV}.tar.gz -> ${P}.tar.g
 
 LICENSE="Apache-2.0"
 SLOT="0/${PV}"
-KEYWORDS="~amd64 ~arm ~arm64"
+KEYWORDS="~amd64 ~arm ~arm64 ~x86"
 IUSE="+ocamlopt"
 
 DEPEND="

dev-ml/ppx_sexp_conv/ppx_sexp_conv-0.14.1.ebuild
@@ -11,7 +11,7 @@ SRC_URI="https://github.com/janestreet/ppx_sexp_conv/archive/v${PV}.tar.gz -> ${
 
 LICENSE="Apache-2.0"
 SLOT="0/${PV}"
-KEYWORDS="~amd64 ~arm ~arm64"
+KEYWORDS="~amd64 ~arm ~arm64 ~x86"
 IUSE="+ocamlopt"
 
 DEPEND="