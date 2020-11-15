gentoo/gentoo

commit 785c90cfa242ac84cff475d5ec5e7f0ed64c14c3

sci-libs/cln: disable static libs
Closes: https://bugs.gentoo.org/725420
Package-Manager: Portage-3.0.4, Repoman-3.0.1
Signed-off-by: Jakov Smolic <jakov.smolic@sartura.hr>
Signed-off-by: David Seifert <soap@gentoo.org>

sci-libs/cln/cln-1.3.3.ebuild
@@ -12,7 +12,7 @@ SRC_URI="ftp://ftpthep.physik.uni-mainz.de/pub/gnu/${P}.tar.bz2"
 LICENSE="GPL-2"
 SLOT="1"
 KEYWORDS="~alpha amd64 arm arm64 hppa ~ia64 ppc ppc64 sparc x86 ~amd64-linux ~x86-linux"
-IUSE="doc examples static-libs"
+IUSE="doc examples"
 
 RDEPEND="dev-libs/gmp:0="
 DEPEND="${RDEPEND}
@@ -35,6 +35,10 @@ src_prepare() {
 	autotools-utils_src_prepare
 }
 
+src_configure() {
+	econf --disable-static
+}
+
 src_compile() {
 	autotools-utils_src_compile
 	if use doc; then
@@ -53,4 +57,6 @@ src_install() {
 		insinto /usr/share/doc/${PF}/examples
 		doins examples/*.cc
 	fi
+
+	find "${ED}" -name '*.la' -delete || die
 }

sci-libs/cln/cln-1.3.4.ebuild
@@ -12,7 +12,7 @@ SRC_URI="https://www.ginac.de/CLN/${P}.tar.bz2"
 LICENSE="GPL-2"
 SLOT="1"
 KEYWORDS="~alpha amd64 arm arm64 hppa ~ia64 ppc ppc64 sparc x86 ~amd64-linux ~x86-linux"
-IUSE="doc examples static-libs"
+IUSE="doc examples"
 
 RDEPEND="dev-libs/gmp:0="
 DEPEND="${RDEPEND}
@@ -35,6 +35,10 @@ src_prepare() {
 	autotools-utils_src_prepare
 }
 
+src_configure() {
+	econf --disable-static
+}
+
 src_compile() {
 	autotools-utils_src_compile
 	if use doc; then
@@ -53,4 +57,6 @@ src_install() {
 		insinto /usr/share/doc/${PF}/examples
 		doins examples/*.cc
 	fi
+
+	find "${ED}" -name '*.la' -delete || die
 }

sci-libs/cln/cln-1.3.6.ebuild
@@ -12,7 +12,7 @@ SRC_URI="https://www.ginac.de/CLN/${P}.tar.bz2"
 LICENSE="GPL-2"
 SLOT="1"
 KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~ppc ~ppc64 ~sparc ~x86 ~amd64-linux ~x86-linux"
-IUSE="doc examples static-libs"
+IUSE="doc examples"
 
 RDEPEND="dev-libs/gmp:0="
 DEPEND="${RDEPEND}
@@ -33,6 +33,10 @@ src_prepare() {
 		Makefile.in || die
 }
 
+src_configure() {
+	econf --disable-static
+}
+
 src_compile() {
 	default
 	if use doc; then
@@ -51,4 +55,6 @@ src_install() {
 		docinto examples
 		dodoc examples/*.cc
 	fi
+
+	find "${ED}" -name '*.la' -delete || die
 }

commit de47aee45fc4d78212179938093608c03ce7cbeb

sci-libs/cln: fix build failure
autotools-utils can do out-of-source builds,
and it does by default, but we need to invoke
autotools-utils_src_configure for it to have
the right behaviour.

This is counterintuitive given more modern
examples like CMake use src_prepare for the
same functionality.

Fixes: 785c90cfa242ac84cff475d5ec5e7f0ed64c14c3
Bug: https://bugs.gentoo.org/725420
Package-Manager: Portage-3.0.8, Repoman-3.0.2
Signed-off-by: Sam James <sam@gentoo.org>

sci-libs/cln/cln-1.3.3.ebuild
@@ -36,7 +36,8 @@ src_prepare() {
 }
 
 src_configure() {
-	econf --disable-static
+	local myeconfargs=( --disable-static )
+	autotools-utils_src_configure
 }
 
 src_compile() {

sci-libs/cln/cln-1.3.4.ebuild
@@ -36,7 +36,8 @@ src_prepare() {
 }
 
 src_configure() {
-	econf --disable-static
+	local myeconfargs=( --disable-static )
+	autotools-utils_src_configure
 }
 
 src_compile() {