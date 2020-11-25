gentoo/gentoo

commit 198726fcc4ff4420bbd8c32bb3e4754b1741ac0d

sec-policy: Release of SELinux policies 2.20200818-r1
Package-Manager: Portage-3.0.8, Repoman-3.0.1
Signed-off-by: Jason Zaman <perfinion@gentoo.org>

sec-policy/apparmor-profiles/apparmor-profiles-2.20200818-r1.ebuild
@@ -1,0 +1,37 @@
+# Copyright 1999-2020 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+
+inherit git-r3
+
+DESCRIPTION="A collection of profiles for the AppArmor application security system"
+HOMEPAGE="https://gitlab.com/apparmor/apparmor/wikis/home"
+
+LICENSE="GPL-2"
+SLOT="0"
+KEYWORDS=""
+IUSE="minimal vanilla"
+
+RESTRICT="test"
+
+S=${WORKDIR}/${P}/profiles
+
+pkg_setup() {
+	if use vanilla; then
+		EGIT_REPO_URI="https://gitlab.com/apparmor/apparmor.git"
+		EGIT_BRANCH="master"
+	else
+		EGIT_REPO_URI="https://github.com/kensington/apparmor.git"
+		EGIT_BRANCH="gentoo"
+	fi
+}
+
+src_install() {
+	if use minimal ; then
+		insinto /etc/apparmor.d
+		doins -r apparmor.d/{abstractions,tunables}
+	else
+		default
+	fi
+}

commit 1617275dc0d2e6af74cc6de9483b70f8d768270d

sec-policy/apparmor-profiles: remove bogus version bump
This appears to have been introduced accidentally in
198726fcc4ff4420bbd8c32bb3e4754b1741ac0d.

Package-Manager: Portage-3.0.9, Repoman-3.0.2
Signed-off-by: Michael Palimaka <kensington@gentoo.org>

sec-policy/apparmor-profiles/apparmor-profiles-2.20200818-r1.ebuild
@@ -1,37 +1,0 @@
-# Copyright 1999-2020 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=7
-
-inherit git-r3
-
-DESCRIPTION="A collection of profiles for the AppArmor application security system"
-HOMEPAGE="https://gitlab.com/apparmor/apparmor/wikis/home"
-
-LICENSE="GPL-2"
-SLOT="0"
-KEYWORDS=""
-IUSE="minimal vanilla"
-
-RESTRICT="test"
-
-S=${WORKDIR}/${P}/profiles
-
-pkg_setup() {
-	if use vanilla; then
-		EGIT_REPO_URI="https://gitlab.com/apparmor/apparmor.git"
-		EGIT_BRANCH="master"
-	else
-		EGIT_REPO_URI="https://github.com/kensington/apparmor.git"
-		EGIT_BRANCH="gentoo"
-	fi
-}
-
-src_install() {
-	if use minimal ; then
-		insinto /etc/apparmor.d
-		doins -r apparmor.d/{abstractions,tunables}
-	else
-		default
-	fi
-}