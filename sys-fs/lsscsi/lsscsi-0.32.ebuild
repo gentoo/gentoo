gentoo/gentoo

commit bdde8ea749fb9d1481edeef5834e3f79bed40af2

www-client/firefox: fix screen sharing for non-US users
Package-Manager: Portage-3.0.10, Repoman-3.0.2
Signed-off-by: Thomas Deutschmann <whissi@gentoo.org>

sys-fs/lsscsi/lsscsi-0.32.ebuild
@@ -1,0 +1,12 @@
+# Copyright 1999-2020 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+
+DESCRIPTION="SCSI sysfs query tool"
+HOMEPAGE="http://sg.danny.cz/scsi/lsscsi.html"
+SRC_URI="http://sg.danny.cz/scsi/${P}.tgz"
+
+LICENSE="GPL-2"
+SLOT="0"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~mips ~ppc ~ppc64 ~sparc ~x86"

www-client/firefox/Manifest
@@ -95,7 +95,7 @@ DIST firefox-78.5.0esr-zh-CN.xpi 564767 BLAKE2B b5c3e773c3bc58ba80180bd018467df1
 DIST firefox-78.5.0esr-zh-TW.xpi 564724 BLAKE2B d55faedb630ecc2629d1e2b1adcfd4cde51d119fd4599d25ef0800e5875efea65db5102fec36366b6531791f16d0f94e8e0dee0bdc18868ad9bf5208471d65c3 SHA512 95e4fa378cf07927b13a9b0e9d37d6320c4cba26f26df6dfab65fcfe1ddd21d38cb5a387663c8c96b6b3e2015999af4d355c0b672848f874101c998abd117047
 DIST firefox-78.5.0esr.source.tar.xz 333995288 BLAKE2B 35140afb6b4d15052858e7864753ca74099edb24ed5e459f1f48114f0946f5e007486b5561ba9a545566033d71a69f0e6163dd17204cab717c441323972f089e SHA512 0d16013342b6e8d67adb5c111177ea4796db4fb593da8aa254d0d95bdf33fad798c2dbb235d44db4177c32dd2d7b3ac26b938b476342753ee8d6c83d968d0281
 DIST firefox-78esr-patches-07.tar.xz 66156 BLAKE2B 8f69883432dcbd484b1fb15e3fde1630b1424e502ba78d9ec3cbe70cb2676a6876853d3477c8bfe35e92bca91b200e2d14543de1778d895676a52d2608109af2 SHA512 b4a01d3499002bd33a7fb45a1a80e9e73ba27a0986b6bfa43b3482dd8d82a686a14ae0c5e013402c8c8f359a642632a40bf4c362a0e83a1f53b453914f42e3a7
-DIST firefox-83-patches-03.tar.xz 151504 BLAKE2B ad4471c9be1b7752940df88cea322f08f34e965143e7f2af1abb6c930641320e2e6dda231f69326f4ee0295003bae116735f47edd86aa44e70031b4383df71ca SHA512 f07fc3ed5fc8eaf89676c53e4ca2b7df54b29f5e4cd700afb5679e9b27c6eaa59f3a9782fdec3344c6f9355ff2db92a1d85a53e8814131c59d4307af4bc759d9
+DIST firefox-83-patches-04.tar.xz 151540 BLAKE2B cc51a844b41bef09a83f03d5d42f128528dcdb9506a9e0d6c570ddc9149bcaee56aecdb7e48d0bfe175bbee79e2dcf0a9042e5c1f9437b28869799e5ea738607 SHA512 329d01a0e67c9a77f19dfc0e5ceff52ae9ef6c1d6a02d895a641df48f244a69b6122da5d8b6e14d6fcb2685ea03a18c031c3d6a178012765537dfd420303c3c6
 DIST firefox-83.0-ach.xpi 463987 BLAKE2B 2173c637ae155ec474d18250adf8cd67ff7732ed9ecc7533004296934f82dfdd2f83f5f59dbd6b095b519bf5d3caf888f5e07913004a27771a13376ac080978d SHA512 cb1644bb1451311923bcbbbafe727fe8b35c33cf9643dceac603e802b82a2684451f9005ef6bfdd1ddebde01548926eb62dcd511e1f8a960248ea5b74f5f72e0
 DIST firefox-83.0-af.xpi 431349 BLAKE2B b4f912e706b464728e779bfc3cbdb7304bd763c55347140c32bdf309b3b4869ebc4bc1433f5cf5c970774f69aabddc4b5f5fce568dc32c50c4aaf90e9d07e13f SHA512 076e9084ae00dda0cfde5b09cf2dae7ed8994543c8de645a70b2f116d01da9e024159114d4d6ef21ce134b7d7cd16b95678121b41300ec5174a36112d41da4ed
 DIST firefox-83.0-an.xpi 511284 BLAKE2B 0b6332b8dcb254bf9df13fde4e34d01c2ef6398c329cce630a0b05a91b1f4ef02c8177f1726f489e8526ffd9fa9e822a010bfd6a669f5a598e7ed60f8328a278 SHA512 43559e760a96092b4c4bfeab6830f021e69c8c7c56a187f49109ea94443372e6323d503f1f175df1d77c685fcbbab2077f7fb02dd031804cde8a7dec2aa1d988

www-client/firefox/firefox-83.0.ebuild
@@ -3,7 +3,7 @@
 
 EAPI="7"
 
-FIREFOX_PATCHSET="firefox-83-patches-03.tar.xz"
+FIREFOX_PATCHSET="firefox-83-patches-04.tar.xz"
 
 LLVM_MAX_SLOT=11
 

commit a36cf665880b1ef98c096bf2390a75b1fdaae301

sys-fs/lsscsi: remove accidentally added ebuild
Fixes: bdde8ea749f ("www-client/firefox: fix screen sharing for non-US users")
Package-Manager: Portage-3.0.10, Repoman-3.0.2
Signed-off-by: Thomas Deutschmann <whissi@gentoo.org>

sys-fs/lsscsi/lsscsi-0.32.ebuild
@@ -1,12 +1,0 @@
-# Copyright 1999-2020 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=7
-
-DESCRIPTION="SCSI sysfs query tool"
-HOMEPAGE="http://sg.danny.cz/scsi/lsscsi.html"
-SRC_URI="http://sg.danny.cz/scsi/${P}.tgz"
-
-LICENSE="GPL-2"
-SLOT="0"
-KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~mips ~ppc ~ppc64 ~sparc ~x86"