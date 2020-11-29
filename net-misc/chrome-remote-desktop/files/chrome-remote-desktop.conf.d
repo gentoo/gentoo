6c2d3c1d0708f90a241888868488285c3cc64af1
Mike Frysinger <vapier@gentoo.org>
2020-11-29 00:49:11 -0500

    Revert "net-misc/chrome-remote-desktop: remove package"
    This reverts commit c5c9e0d4dc94f19ba7b00b918a3c65818c23e0e4.
    
    Cleanup mess made by careless devs.
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

---
+++ net-misc/chrome-remote-desktop/files/chrome-remote-desktop.conf.d
@@ -0,0 +1,7 @@
+# /etc/conf.d/chrome-remote-desktop: config file for /etc/init.d/chrome-remote-desktop
+
+# List of users to start Chrome Remote Desktop for.
+CHROME_REMOTING_USERS=''
+
+# Options to pass to chrome-remote-desktop.  Only the -s option is interesting.
+#OPTIONS='-s 1600x1200 -s 3840x1600'
