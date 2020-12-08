gentoo/gentoo

commit d565d3c807ea093cc4faacd6877f4705f5df5fe9

dev-lang/luajit: move to EAPI 7
Closes: https://bugs.gentoo.org/758854
Signed-off-by: William Hubbs <williamh@gentoo.org>

dev-lang/luajit/luajit-2.0.5-r2.ebuild
@@ -1,13 +1,13 @@
 # Copyright 1999-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-EAPI=6
+EAPI=7
 
-inherit pax-utils toolchain-funcs versionator
+inherit pax-utils toolchain-funcs
 
-MY_PV="$(get_version_component_range 1-3)"
+MY_PV="$(ver_cut 1-3)"
 MY_P="LuaJIT-${MY_PV}"
-if [[ -n $(get_version_component_range 4) ]]; then
+if [[ -n $(ver_cut 4) ]]; then
 	HOTFIX="v${PV}"
 	HOTFIX="${HOTFIX/_p/_hotfix}.patch"
 fi

dev-lang/luajit/luajit-2.1.0_beta3-r1.ebuild
@@ -1,11 +1,11 @@
 # Copyright 1999-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-EAPI=6
+EAPI=7
 
-inherit pax-utils toolchain-funcs versionator
+inherit pax-utils toolchain-funcs
 
-MY_PV="$(get_version_component_range 1-4)"
+MY_PV="$(ver_cut 1-4)"
 MY_PV="${MY_PV/_beta/-beta}"
 MY_P="LuaJIT-${MY_PV}"
 

commit e5ef3a31ca3c032eb00d779dce4ac3d0ceecd71e

dev-lang/luajit: fix slashes
Fixes: d565d3c8 ("dev-lang/luajit: move to EAPI 7")
Signed-off-by: Thomas Deutschmann <whissi@gentoo.org>

dev-lang/luajit/luajit-2.0.5-r2.ebuild
@@ -50,7 +50,7 @@ _emake() {
 		TARGET_AR="$(tc-getAR) rcus" \
 		BUILDMODE="$(usex static-libs mixed dynamic)" \
 		TARGET_STRIP="true" \
-		INSTALL_LIB="${ED%/}/usr/$(get_libdir)" \
+		INSTALL_LIB="${ED}/usr/$(get_libdir)" \
 		"$@"
 }
 
@@ -61,7 +61,7 @@ src_compile() {
 src_install() {
 	_emake install
 
-	pax-mark m "${ED}usr/bin/luajit-${MY_PV}"
+	pax-mark m "${ED}/usr/bin/luajit-${MY_PV}"
 
 	HTML_DOCS="doc/." einstalldocs
 }

dev-lang/luajit/luajit-2.1.0_beta3-r1.ebuild
@@ -39,7 +39,7 @@ _emake() {
 		TARGET_AR="$(tc-getAR) rcus" \
 		BUILDMODE="$(usex static-libs mixed dynamic)" \
 		TARGET_STRIP="true" \
-		INSTALL_LIB="${ED%/}/usr/$(get_libdir)" \
+		INSTALL_LIB="${ED}/usr/$(get_libdir)" \
 		"$@"
 }
 
@@ -50,7 +50,7 @@ src_compile() {
 src_install() {
 	_emake install
 
-	pax-mark m "${ED}usr/bin/luajit-${MY_PV}"
+	pax-mark m "${ED}/usr/bin/luajit-${MY_PV}"
 
 	HTML_DOCS="doc/." einstalldocs
 }