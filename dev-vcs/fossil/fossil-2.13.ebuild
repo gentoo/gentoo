gentoo/gentoo

commit 10515a7623557e60bf0a68749c8d8bba6c333085

dev-vcs/fossil: Bump to 2.13
Closes: https://bugs.gentoo.org/761118
Signed-off-by: Aaron W. Swenson <titanofold@gentoo.org>

dev-vcs/fossil/fossil-2.13.ebuild
@@ -1,0 +1,71 @@
+# Copyright 1999-2020 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+
+inherit toolchain-funcs
+
+DESCRIPTION="Simple, high-reliability, source control management, and more"
+HOMEPAGE="https://www.fossil-scm.org/"
+SRC_URI="https://fossil-scm.org/home/uv/fossil-src-${PV}.tar.gz"
+
+LICENSE="BSD-2"
+SLOT="0"
+KEYWORDS="amd64 arm ppc ppc64 x86"
+IUSE="debug fusefs json -miniz system-sqlite +ssl static tcl tcl-stubs
+		  tcl-private-stubs th1-docs th1-hooks"
+
+REQUIRED_USE="ssl? ( !miniz )"
+
+RDEPEND="
+		sys-libs/zlib
+		|| (
+				sys-libs/readline:0
+				dev-libs/libedit
+		)
+		system-sqlite? ( >=dev-db/sqlite-3.34.0:3 )
+		ssl? ( dev-libs/openssl:0 )
+		tcl? ( dev-lang/tcl:0= )
+"
+
+# Either tcl or jimtcl need to be present to build Fossil (Bug #675778)
+DEPEND="${RDEPEND}
+		!tcl? (
+				|| (
+						dev-lang/tcl:*
+						dev-lang/jimtcl:*
+				)
+		)
+"
+
+# Tests can't be run from the build directory
+RESTRICT="test"
+
+# fossil-2.10-check-lib64-for-tcl.patch: Bug 690828
+PATCHES=( "${FILESDIR}"/fossil-2.10-check-lib64-for-tcl.patch )
+
+src_configure() {
+		# this is not an autotools situation so don't make it seem like one
+		# --with-tcl: works
+		# --without-tcl: dies
+		local myconf="--with-openssl=$(usex ssl auto none)"
+		use debug         && myconf+=' --fossil-debug'
+		use json          && myconf+=' --json'
+		use system-sqlite && myconf+=' --disable-internal-sqlite'
+		use static        && myconf+=' --static'
+		use tcl           && myconf+=' --with-tcl=1'
+		use fusefs        || myconf+=' --disable-fusefs'
+
+		local u useflags
+		useflags=( miniz tcl-stubs tcl-private-stubs th1-docs th1-hooks )
+		for u in ${useflags[@]} ; do
+				use ${u} &&  myconf+=" --with-${u}"
+		done
+
+		tc-export CC CXX
+		CC_FOR_BUILD=${CC} ./configure ${myconf} || die
+}
+
+src_install() {
+		dobin fossil
+}

dev-vcs/fossil/Manifest
@@ -1,1 +1,2 @@
 DIST fossil-src-2.12.1.tar.gz 6011181 BLAKE2B f9437c4fb20314706bbbec70efbb6a53e2ac7760986baf88ea72505f03877a5d1069d0d2828aa2bfefac96c1358e4c8f824405d61fd24d490442fa8b768711e9 SHA512 08b7fef5a3071e968dedbc645aebf2e873ecf80049b5986d38f851a407b2b8b0c854cfcdfa5ec80eac5bc1b158c5fac3bb4b263d1adb8be470529e67dc6e8cee
+DIST fossil-src-2.13.tar.gz 6121223 BLAKE2B 41bb83b40faf05d38ccbcc13bde858ee31ab0182193631d738a6e287414b2a93bc49bfa5ed0a16aabbc26190c1a3cd993ce9f1706ecd05b214461313b13c1086 SHA512 bd756aaac2f39005b690230c5a4ef4d618a3200a8eb499aa964285050c247edfb0abfa5db5522269e0a5ee2737c71393a523dc2d7e37eae076364185137964f8

commit ede9b9f147f30b4d174ca3bfb2d7eb944faeee1a

dev-vcs/fossil: revert to ~arch
Fixes: 10515a7623557e60bf0a68749c8d8bba6c333085
Package-Manager: Portage-3.0.12-prefix, Repoman-3.0.2
Signed-off-by: Sam James <sam@gentoo.org>

dev-vcs/fossil/fossil-2.13.ebuild
@@ -11,7 +11,7 @@ SRC_URI="https://fossil-scm.org/home/uv/fossil-src-${PV}.tar.gz"
 
 LICENSE="BSD-2"
 SLOT="0"
-KEYWORDS="amd64 arm ppc ppc64 x86"
+KEYWORDS="~amd64 ~arm ~ppc ~ppc64 ~x86"
 IUSE="debug fusefs json -miniz system-sqlite +ssl static tcl tcl-stubs
 		  tcl-private-stubs th1-docs th1-hooks"
 