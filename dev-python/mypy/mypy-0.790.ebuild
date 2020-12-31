gentoo/gentoo

commit 16a690c00be2c81f32d9f083b83d49013a3b16cd

dev-python/mypy: Stabilize 0.790 ALLARCHES, #762736
Signed-off-by: Sam James <sam@gentoo.org>

dev-python/mypy/mypy-0.790.ebuild
@@ -22,7 +22,7 @@ SRC_URI="
 
 LICENSE="MIT"
 SLOT="0"
-KEYWORDS="~alpha amd64 ~arm arm64 ~ia64 ~ppc ~ppc64 ~sparc x86"
+KEYWORDS="~alpha amd64 arm arm64 ~ia64 ~ppc ~ppc64 ~sparc x86"
 
 # stubgen collides with this package: https://bugs.gentoo.org/585594
 RDEPEND="

commit 80f9b7ff7a485957f7e8325b6ae231ed6c878c2d

dev-python/mypy: Stabilize 0.790 ALLARCHES (again)
Nattka - rightly - couldn't read my mind there.

Fixes: 16a690c00be2c81f32d9f083b83d49013a3b16cd
Package-Manager: Portage-3.0.9, Repoman-3.0.2
Signed-off-by: Sam James <sam@gentoo.org>

dev-python/mypy/mypy-0.790.ebuild
@@ -22,7 +22,7 @@ SRC_URI="
 
 LICENSE="MIT"
 SLOT="0"
-KEYWORDS="~alpha amd64 arm arm64 ~ia64 ~ppc ~ppc64 ~sparc x86"
+KEYWORDS="~alpha amd64 arm arm64 ~ia64 ppc ppc64 sparc x86"
 
 # stubgen collides with this package: https://bugs.gentoo.org/585594
 RDEPEND="