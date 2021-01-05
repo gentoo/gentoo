f8ed334c4a1f2a9802f898663b3facac5ec15e01
Sergei Trofimovich <slyfox@gentoo.org>
2021-01-05 22:40:59 +0000

    toolchain-funcs.eclass: fix or1k* tuple detection
    Before the change the only recognised CHOST was 'or1k'.
    After the change CHOSTs like 'or1k-linux-musl' are
    also recognised as 'openrisc'.
    
    Reported-by: adam@pimentel.space
    Bug: https://bugs.gentoo.org/763606
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

--- eclass/tests/toolchain-funcs.sh
+++ eclass/tests/toolchain-funcs.sh
@@ -27,7 +27,8 @@ test-tc-arch-kernel() {
 tbegin "tc-arch-kernel() (KV=2.6.30)"
 test-tc-arch-kernel 2.6.30 \
 	i{3..6}86:x86 x86_64:x86 \
-	powerpc{,64}:powerpc i{3..6}86-gentoo-freebsd:i386
+	powerpc{,64}:powerpc i{3..6}86-gentoo-freebsd:i386 \
+	or1k:openrisc or1k-linux-musl:openrisc
 tend $?
 
 #
