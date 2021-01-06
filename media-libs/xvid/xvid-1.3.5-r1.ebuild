gentoo/gentoo

commit 04055238ba89f38adb0d7bbc93863e2ef44e71e1

media-libs/xvid: drop x86-macos
Package-Manager: Portage-3.0.12, Repoman-3.0.2
Signed-off-by: Fabian Groffen <grobian@gentoo.org>

media-libs/xvid/xvid-1.3.5-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2020 Gentoo Authors
+# Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
@@ -13,7 +13,7 @@ SRC_URI="http://downloads.xvid.org/downloads/${MY_P}.tar.bz2"
 
 LICENSE="GPL-2"
 SLOT="0"
-KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~mips ~ppc ~ppc64 ~sparc ~x86 ~amd64-linux ~x86-linux ~ppc-macos ~x86-macos ~sparc-solaris ~x86-solaris"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~mips ~ppc ~ppc64 ~sparc ~x86 ~amd64-linux ~x86-linux ~ppc-macos ~sparc-solaris ~x86-solaris"
 IUSE="elibc_FreeBSD examples pic +threads"
 
 NASM=">=dev-lang/nasm-2"

media-libs/xvid/xvid-1.3.5.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2020 Gentoo Authors
+# Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=6
@@ -13,7 +13,7 @@ SRC_URI="http://downloads.xvid.org/downloads/${MY_P}.tar.bz2"
 
 LICENSE="GPL-2"
 SLOT="0"
-KEYWORDS="~alpha amd64 arm arm64 ~hppa ~ia64 ~mips ppc ppc64 sparc x86 ~amd64-linux ~x86-linux ~ppc-macos ~x86-macos ~sparc-solaris ~x86-solaris"
+KEYWORDS="~alpha amd64 arm arm64 ~hppa ~ia64 ~mips ppc ppc64 sparc x86 ~amd64-linux ~x86-linux ~ppc-macos ~sparc-solaris ~x86-solaris"
 IUSE="elibc_FreeBSD examples pic +threads"
 
 NASM=">=dev-lang/nasm-2"

media-libs/xvid/xvid-1.3.7.ebuild
@@ -13,7 +13,7 @@ SRC_URI="https://downloads.xvid.com/downloads/${MY_P}.tar.bz2"
 
 LICENSE="GPL-2"
 SLOT="0"
-KEYWORDS="~alpha amd64 arm arm64 ~hppa ~ia64 ~mips ppc ppc64 sparc ~x86 ~amd64-linux ~x86-linux ~ppc-macos ~x86-macos ~sparc-solaris ~x86-solaris"
+KEYWORDS="~alpha amd64 arm arm64 ~hppa ~ia64 ~mips ppc ppc64 sparc ~x86 ~amd64-linux ~x86-linux ~ppc-macos ~sparc-solaris ~x86-solaris"
 IUSE="elibc_FreeBSD examples pic +threads"
 
 NASM=">=dev-lang/nasm-2"

commit 3bbc1c8bd36d41ec5bc97b2ac4b2a90c54ff6933

media-libs/xvid: [QA] Fix dependencies
Removal of x86-macos from ARCH broken the use-conditional dependencies.

Fixes: 04055238ba89f38adb0d7bbc93863e2ef44e71e1
Package-Manager: Portage-3.0.12, Repoman-3.0.2
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

media-libs/xvid/xvid-1.3.5-r1.ebuild
@@ -22,7 +22,6 @@ YASM=">=dev-lang/yasm-1"
 BDEPEND="sys-apps/grep"
 DEPEND="amd64? ( || ( ${YASM} ${NASM} ) )
 	x86? ( || ( ${YASM} ${NASM} ) )
-	x86-macos? ( ${NASM} )
 	x64-macos? ( ${NASM} )"
 
 S="${WORKDIR}/${MY_PN}/build/generic"

media-libs/xvid/xvid-1.3.5.ebuild
@@ -24,7 +24,6 @@ DEPEND="sys-apps/grep
 	amd64-fbsd? ( ${NASM} )
 	x86? ( || ( ${YASM} ${NASM} ) )
 	x86-fbsd? ( ${NASM} )
-	x86-macos? ( ${NASM} )
 	x64-macos? ( ${NASM} )"
 RDEPEND=""
 

media-libs/xvid/xvid-1.3.7.ebuild
@@ -22,7 +22,6 @@ YASM=">=dev-lang/yasm-1"
 BDEPEND="sys-apps/grep"
 DEPEND="amd64? ( || ( ${YASM} ${NASM} ) )
 	x86? ( || ( ${YASM} ${NASM} ) )
-	x86-macos? ( ${NASM} )
 	x64-macos? ( ${NASM} )"
 
 S="${WORKDIR}/${MY_PN}/build/generic"