gentoo/gentoo

commit f52400a5c6d8a1e77597711e6e47da8369c452e0

dev-util/colm: add blocker on earlier dev-util/ragel
libfsm moved from ragel to colm.

Thanks-to: josef64
Package-Manager: Portage-3.0.12, Repoman-3.0.2
Signed-off-by: Sam James <sam@gentoo.org>

dev-util/colm/colm-0.14.6.ebuild
@@ -20,6 +20,8 @@ BDEPEND="
 		dev-python/pygments
 	)
 "
+# libfsm moved from ragel -> colm
+RDEPEND="!<dev-util/ragel-7.0.0.12"
 
 src_prepare() {
 	default

commit 975ad15a7065e440a8f0e5d3e9778b0e081b3839

dev-util/colm: update blocker
Fixes: f52400a5c6d8a1e77597711e6e47da8369c452e0
Package-Manager: Portage-3.0.12, Repoman-3.0.2
Signed-off-by: Sam James <sam@gentoo.org>

dev-util/colm/colm-0.14.6.ebuild
@@ -21,7 +21,7 @@ BDEPEND="
 	)
 "
 # libfsm moved from ragel -> colm
-RDEPEND="!<dev-util/ragel-7.0.0.12"
+RDEPEND="!<dev-util/ragel-7.0.3"
 
 src_prepare() {
 	default