gentoo/gentoo

commit 29a78bb9eea46318fc02a457e3431788db15a462

dev-ml/dune-configurator: lock to same dune-private-libs version
This will also ensure we have the right version of Dune.

Reported-by: John Helmert III <jchelmert3@posteo.net>
Package-Manager: Portage-3.0.14, Repoman-3.0.2
Signed-off-by: Sam James <sam@gentoo.org>

dev-ml/dune-configurator/dune-configurator-2.8.2.ebuild
@@ -17,7 +17,7 @@ IUSE="+ocamlopt test"
 RESTRICT="!test? ( test )"
 
 DEPEND="
-	dev-ml/dune-private-libs:=
+	~dev-ml/dune-private-libs-${PV}
 	dev-ml/csexp:=
 	dev-ml/result:=
 "

commit 2d57f8778ce7ee54185fb2413ac2ab49f1344453

dev-ml/dune-configurator: keep subslot dep on private-libs
Fixes: 29a78bb9eea46318fc02a457e3431788db15a462
Package-Manager: Portage-3.0.14, Repoman-3.0.2
Signed-off-by: Sam James <sam@gentoo.org>

dev-ml/dune-configurator/dune-configurator-2.8.2.ebuild
@@ -17,7 +17,7 @@ IUSE="+ocamlopt test"
 RESTRICT="!test? ( test )"
 
 DEPEND="
-	~dev-ml/dune-private-libs-${PV}
+	~dev-ml/dune-private-libs-${PV}:=
 	dev-ml/csexp:=
 	dev-ml/result:=
 "