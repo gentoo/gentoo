gentoo/gentoo

commit eee598673c7af3dcb75c673f87e60ccc21985ee9

app-misc/devtodo: port to EAPI 7
Package-Manager: Portage-3.0.14, Repoman-3.0.2
Signed-off-by: Sam James <sam@gentoo.org>

app-misc/devtodo/devtodo-0.1.20-r3.ebuild
@@ -1,12 +1,9 @@
-# Copyright 1999-2020 Gentoo Authors
+# Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-EAPI=5
+EAPI=7
 
-AUTOTOOLS_AUTORECONF=1
-AUTOTOOLS_IN_SOURCE_BUILD=1
-
-inherit autotools-utils bash-completion-r1 eutils flag-o-matic toolchain-funcs
+inherit autotools bash-completion-r1 flag-o-matic
 
 DESCRIPTION="A nice command line todo list for developers"
 HOMEPAGE="http://swapoff.org/DevTodo"
@@ -15,7 +12,6 @@ SRC_URI="http://swapoff.org/files/${PN}/${P}.tar.gz"
 LICENSE="GPL-2"
 SLOT="0"
 KEYWORDS="~alpha amd64 arm ~hppa ~ia64 ~m68k ~mips ppc ppc64 ~s390 sparc x86 ~amd64-linux ~x86-linux ~ppc-macos"
-IUSE=""
 
 RDEPEND="
 	>=sys-libs/ncurses-5.2:0=
@@ -31,6 +27,10 @@ PATCHES=(
 )
 
 src_prepare() {
+	default
+
+	mv configure.{in,ac} || die
+
 	# fix regex.h issue on case-insensitive file-systems #332235
 	sed \
 		-e 's/Regex.h/DTRegex.h/' \
@@ -41,7 +41,7 @@ src_prepare() {
 		-e "/^LIBS/s:$: $($(tc-getPKG_CONFIG) --libs ncursesw):g" \
 		-i src/Makefile.am  || die
 
-	autotools-utils_src_prepare
+	eautoreconf
 }
 
 src_configure() {
@@ -50,11 +50,12 @@ src_configure() {
 	local myeconfargs=(
 		--sysconfdir="${EPREFIX}/etc/devtodo"
 	)
-	autotools-utils_src_configure
+
+	econf "${myeconfargs[@]}"
 }
 
 src_install() {
-	autotools-utils_src_install
+	default
 
 	newbashcomp contrib/${PN}.bash-completion ${PN}
 	rm contrib/${PN}.bash-completion || die 'rm failed'

app-misc/devtodo/files/devtodo-0.1.20-bashcom_spaces.patch
@@ -1,8 +1,3 @@
- contrib/devtodo.bash-completion | 8 ++++----
- 1 file changed, 4 insertions(+), 4 deletions(-)
-
-diff --git a/contrib/devtodo.bash-completion b/contrib/devtodo.bash-completion
-index 07139b0..c058dad 100644
 --- a/contrib/devtodo.bash-completion
 +++ b/contrib/devtodo.bash-completion
 @@ -15,20 +15,20 @@ _devtodo() {

app-misc/devtodo/files/devtodo-0.1.20-gcc43.patch
@@ -1,5 +1,5 @@
---- devtodo-0.1.20.orig/src/todoterm.cc
-+++ devtodo-0.1.20/src/todoterm.cc
+--- a/src/todoterm.cc
++++ b/src/todoterm.cc
 @@ -2,6 +2,7 @@
  #include "todoterm.h"
  

app-misc/devtodo/files/devtodo-0.1.20-gentoo.diff
@@ -1,5 +1,5 @@
---- devtodo-0.1.20.orig/doc/devtodo.1.in	2004-12-17 10:21:09.135350872 -0500
-+++ devtodo-0.1.20/doc/devtodo.1.in	2004-12-17 10:21:34.050210242 -0500
+--- a/doc/devtodo.1.in
++++ b/doc/devtodo.1.in
 @@ -20,9 +20,6 @@
  .TP 
  \fBtdd \fI<indices>\fP\fP
@@ -16,13 +16,13 @@
  User-specific options are specified in this file. Please refer to the section \fBTODORC\fR for more information.
 +.SH "BUGS"
 +.LP
-+Because of a conflict with app-misc/tdl, the tdl symbolic link and the tdl manual page have been removed.  
++Because of a conflict with app-misc/tdl, the tdl symbolic link and the tdl manual page have been removed.
 +See \fIhttp://bugs.gentoo.org/show_bug.cgi?id=55371\fR for more information.
  .SH "AUTHORS"
  .LP 
  Alec Thomas \fI<alec@swapoff.org>\fR
---- devtodo-0.1.20.orig/doc/Makefile.am	2004-12-17 10:21:09.135350872 -0500
-+++ devtodo-0.1.20/doc/Makefile.am	2004-12-17 10:21:34.050210242 -0500
+--- a/doc/Makefile.am
++++ b/doc/Makefile.am
 @@ -1,5 +1,5 @@
  man_MANS=devtodo.1
 -manlinks=tda tdr tdd tde tdl todo
@@ -30,8 +30,8 @@
  
  install-data-local:
  	mkdir -p $(DESTDIR)$(mandir)/man1
---- devtodo-0.1.20.orig/src/TodoDB.cc	2004-12-17 10:21:09.136350706 -0500
-+++ devtodo-0.1.20/src/TodoDB.cc	2004-12-17 10:21:34.054209577 -0500
+--- a/src/TodoDB.cc
++++ b/src/TodoDB.cc
 @@ -949,7 +948,11 @@
  	if (out.bad()) 
  		throw exception("couldn't open TODO for generation");
@@ -45,8 +45,8 @@
  	if (options.verbose > 1)
  		cout << "todo: generating using format '" << options.format["verbose-generated"] << "'" << endl;
  	filterView();
---- devtodo-0.1.20/contrib/devtodo.bash-completion	{cset 9b4d9f02-fdb7-4641-b07f-7359b794d6b0}
-+++ devtodo-0.1.20/contrib/devtodo.bash-completion	{local clone}
+--- a/contrib/devtodo.bash-completion
++++ b/contrib/devtodo.bash-completion
 @@ -35,6 +35,6 @@ _devtodo() {
              ;;
      esac
@@ -55,8 +55,8 @@
 +complete -o filenames -F _devtodo devtodo todo tda tde tdr tdd
  
  # vim: set ft=sh tw=80 sw=4 et :
---- devtodo-0.1.20/src/support.cc	{cset 5f5bd9cb-18fb-4046-bff5-4f3dbd781e47}
-+++ devtodo-0.1.20/src/support.cc	{local clone}
+--- a/src/support.cc
++++ b/src/support.cc
 @@ -289,11 +289,10 @@ enum { Help = -100, Remove, Version, Tit
  					<< "Where <arguments> can be any of the following:" << endl;
  				args.displayHelp(cout, options.columns);
@@ -72,8 +72,8 @@
  					<< "eg. tde 1" << endl;
  				exit(0);
  			break;
---- devtodo-0.1.20/src/Makefile.am	2007-09-17 12:12:55.000000000 +0200
-+++ devtodo-0.1.20/src/Makefile.am	2007-09-17 12:13:43.000000000 +0200
+--- a/src/Makefile.am
++++ b/src/Makefile.am
 @@ -1,7 +1,7 @@
  bin_PROGRAMS=devtodo
  devtodo_SOURCES=Todo.cc Todo.h TodoDB.cc TodoDB.h support.cc support.h \

app-misc/klavaro/Manifest
@@ -1,1 +1,2 @@
 DIST klavaro-3.01.tar.bz2 1140146 BLAKE2B fcd26cc6dfabd7ae0147fc279361a890ab928e42e02837a3322eae51fc6e959b02896ce789fae6a10b2f600eeb026c45955571d5264a29950cc7183eeb22a4cf SHA512 5d58269867d2b39309e07ca9dd6c525d67acaca93dea0010332e39e8f3dd444070dbc75abad00ac4ca3fd4155f41cf871cb8ccee3dc332aa31f85c8b17d7c48b
+DIST klavaro-3.11.tar.bz2 1266392 BLAKE2B 1b19caaf7910147478e744383a282ab81d2de63d099531bfd71f887df8fb95b507d7939f1c4a8721fc733b35991e29223213c55f6a884ac76043ed37c667d616 SHA512 7344a9e047e99f8a0ab1bfbea2950e2b49b8603dcac0dfe7391ac5a2a985c2b09dc9dd453d28daefab8a3d6bd92736075dc53d0d5f3b801efd061c16594cbe20

commit 177728c6b35b55815743503cdf94c5d25337de7f

app-misc/devtodo: further modernisation
* .diff -> .patch
* Missing BDEPEND on virtual/pkgconfig (we call it in the ebuild!)

Fixes: eee598673c7af3dcb75c673f87e60ccc21985ee9
Package-Manager: Portage-3.0.14, Repoman-3.0.2
Signed-off-by: Sam James <sam@gentoo.org>

app-misc/devtodo/devtodo-0.1.20-r3.ebuild
@@ -13,6 +13,7 @@ LICENSE="GPL-2"
 SLOT="0"
 KEYWORDS="~alpha amd64 arm ~hppa ~ia64 ~m68k ~mips ppc ppc64 ~s390 sparc x86 ~amd64-linux ~x86-linux ~ppc-macos"
 
+BDEPEND="virtual/pkgconfig"
 RDEPEND="
 	>=sys-libs/ncurses-5.2:0=
 	>=sys-libs/readline-4.1:0="
@@ -21,7 +22,7 @@ DEPEND="${RDEPEND}"
 DOCS=( AUTHORS ChangeLog QuickStart README doc/scripts.sh doc/scripts.tcsh doc/todorc.example )
 
 PATCHES=(
-	"${FILESDIR}"/${P}-gentoo.diff
+	"${FILESDIR}"/${P}-gentoo.patch
 	"${FILESDIR}"/${P}-gcc43.patch
 	"${FILESDIR}"/${P}-bashcom_spaces.patch
 )