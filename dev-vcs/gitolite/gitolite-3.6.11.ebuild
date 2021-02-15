gentoo/gentoo

commit 83b1554e9e3e291f97ba493db460fe4709bc4d24

dev-vcs/gitolite: fix double KEYWORDS
Double KEYWORDS lines break tools like ekeyword and Nattka.
As per PMS, blank/empty KEYWORDS is implied by not defining
the variable.

See: https://projects.gentoo.org/qa/policy-guide/ebuild-format.html#pg0105
Package-Manager: Portage-3.0.14, Repoman-3.0.2
Signed-off-by: Sam James <sam@gentoo.org>

dev-vcs/gitolite/gitolite-3.6.11.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2019 Gentoo Authors
+# Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=6
@@ -12,7 +12,6 @@ DESCRIPTION="Highly flexible server for git directory version tracker"
 HOMEPAGE="https://github.com/sitaramc/gitolite"
 if [[ ${PV} != *9999 ]]; then
 	SRC_URI="https://github.com/sitaramc/${PN}/archive/v${PV}.tar.gz -> ${P}.tar.gz"
-	KEYWORDS="amd64 arm x86"
 else
 	SRC_URI=""
 	KEYWORDS="amd64 arm x86"

commit e16382d44037819e8ad686dddbe991d52e411144

dev-vcs/gitolite: fix KEYWORDS
Fixes: 83b1554e9e3e291f97ba493db460fe4709bc4d24
Package-Manager: Portage-3.0.14, Repoman-3.0.2
Signed-off-by: Sam James <sam@gentoo.org>

dev-vcs/gitolite/gitolite-3.6.11.ebuild
@@ -12,9 +12,9 @@ DESCRIPTION="Highly flexible server for git directory version tracker"
 HOMEPAGE="https://github.com/sitaramc/gitolite"
 if [[ ${PV} != *9999 ]]; then
 	SRC_URI="https://github.com/sitaramc/${PN}/archive/v${PV}.tar.gz -> ${P}.tar.gz"
+	KEYWORDS="amd64 arm x86"
 else
 	SRC_URI=""
-	KEYWORDS="amd64 arm x86"
 fi
 
 LICENSE="GPL-2"