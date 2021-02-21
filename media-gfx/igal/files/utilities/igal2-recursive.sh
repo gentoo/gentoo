2c1ba4820ac12ae9bdd6568a543937aa6e7bd874
blaindoc anderson <doc23584@gmail.com>
2021-02-20 21:59:49 -0500

    media-gfx/igal: bump to igal-2.4
    Package-Manager: Portage-3.0.14, Repoman-3.0.2
    Signed-off-by: Blain doc Anderson <doc235@email.com>

--- media-gfx/igal/files/utilities/igal2-recursive.sh
+++
@@ -1,2 +0,0 @@
-#
-find $1 -type d -xdev ! -name "\.igal" -exec igal2 -n --bigy 510 -xy 140 --dest ".igal" --AddSubdir -u -www -pagination 50 -d "{}" \;
--- media-gfx/igal/files/utilities/igal2.sh
+++
@@ -1,3 +0,0 @@
-#
-#
-/usr/local/bin/igal2 -n --bigy 510 -y 140 --dest ".igal" --AddSubdir -u -www -pagination 50 -d "$1"
