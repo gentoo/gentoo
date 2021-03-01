gentoo/gentoo

commit 9301a0c8c6254149480248d2c35f4134c1a136d1

media-libs/freeimage: added ~arm64
Package-Manager: Portage-3.0.14, Repoman-3.0.2
Signed-off-by: Roy Bamford <neddyseagoon@gentoo.org>
Signed-off-by: Sam James <sam@gentoo.org>

media-libs/freeimage/freeimage-3.18.0-r2.ebuild
@@ -17,7 +17,7 @@ SRC_URI="mirror://sourceforge/${PN}/${MY_P}.zip
 
 LICENSE="|| ( GPL-2 FIPL-1.0 )"
 SLOT="0"
-KEYWORDS="amd64 ~arm x86 ~amd64-linux ~x86-linux"
+KEYWORDS="amd64 ~arm ~arm64 ~x86 ~amd64-linux ~x86-linux"
 IUSE="jpeg jpeg2k mng openexr png raw static-libs tiff webp"
 
 # The tiff/ilmbase isn't a typo.  The TIFF plugin cheats and

commit 08383fc2049c6f212649483f3be40eb38c11543a

media-libs/freeimage: restore x86 keyword
Fixes: 9301a0c8c ("media-libs/freeimage: added ~arm64")
Package-Manager: Portage-3.0.15, Repoman-3.0.2
Signed-off-by: Thomas Deutschmann <whissi@gentoo.org>

media-libs/freeimage/freeimage-3.18.0-r2.ebuild
@@ -17,7 +17,7 @@ SRC_URI="mirror://sourceforge/${PN}/${MY_P}.zip
 
 LICENSE="|| ( GPL-2 FIPL-1.0 )"
 SLOT="0"
-KEYWORDS="amd64 ~arm ~arm64 ~x86 ~amd64-linux ~x86-linux"
+KEYWORDS="amd64 ~arm ~arm64 x86 ~amd64-linux ~x86-linux"
 IUSE="jpeg jpeg2k mng openexr png raw static-libs tiff webp"
 
 # The tiff/ilmbase isn't a typo.  The TIFF plugin cheats and