gentoo/gentoo

commit bb706c5383db445f085b3ac628790a319ed9d309

games-strategy/0ad: fix USE=-nvtt installation
Signed-off-by: Sam James <sam@gentoo.org>

games-strategy/0ad/0ad-0.0.24b_alpha.ebuild
@@ -209,7 +209,9 @@ src_install() {
 	# Install bundled SpiderMonkey and nvtt
 	# bug #771147 (comment 1)
 	exeinto /usr/$(get_libdir)/${PN}
-	doexe binaries/system/{libCollada,libmozjs78-ps-release,libnvtt,libnvcore,libnvimage,libnvmath}.so
+	doexe binaries/system/{libCollada,libmozjs78-ps-release}
+
+	use nvtt && doexe /binaries/system/{libnvtt,libnvcore,libnvimage,libnvmath}.so
 	use editor && doexe binaries/system/libAtlasUI.so
 
 	dodoc binaries/system/readme.txt

commit 1a0d70cdd420a40dcbfa1d35485c76ead61ef151

games-strategy/0ad: fix doexe typos
Fixes: bb706c5383db445f085b3ac628790a319ed9d309
Bug: https://bugs.gentoo.org/773574
Closes: https://bugs.gentoo.org/773565
Signed-off-by: Sam James <sam@gentoo.org>

games-strategy/0ad/0ad-0.0.24b_alpha.ebuild
@@ -213,9 +213,9 @@ src_install() {
 	# Install bundled SpiderMonkey and nvtt
 	# bug #771147 (comment 1)
 	exeinto /usr/$(get_libdir)/${PN}
-	doexe binaries/system/{libCollada,libmozjs78-ps-release}
+	doexe binaries/system/{libCollada,libmozjs78-ps-release}.so
 
-	use nvtt && doexe /binaries/system/{libnvtt,libnvcore,libnvimage,libnvmath}.so
+	use nvtt && doexe binaries/system/{libnvtt,libnvcore,libnvimage,libnvmath}.so
 	use editor && doexe binaries/system/libAtlasUI.so
 
 	dodoc binaries/system/readme.txt