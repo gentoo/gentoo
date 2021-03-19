gentoo/gentoo

commit 115464be8a78924624f616ab0b9ae9b61708041c

dev-libs/libtomcrypt: fix REQUIRED_USE to enforce at least one math provider
Closes: https://bugs.gentoo.org/772935
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/libtomcrypt/libtomcrypt-1.18.2-r2.ebuild
@@ -20,6 +20,10 @@ SLOT="0/1"
 KEYWORDS="~alpha amd64 arm arm64 ~hppa ~ia64 ~mips ppc ppc64 ~s390 sparc x86 ~x64-macos"
 IUSE="+gmp +libtommath tomsfastmath"
 
+# Enforce at least one math provider
+# bug #772935
+REQUIRED_USE="?? ( gmp libtommath tomsfastmath )"
+
 BDEPEND="
 	sys-devel/libtool
 	virtual/pkgconfig

commit dfad13e8044d36db7f610e00cb19d00ead659bcd

dev-libs/libtomcrypt: use any-of
Bug: https://bugs.gentoo.org/772935
Fixes: 115464be8a78924624f616ab0b9ae9b61708041c
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/libtomcrypt/libtomcrypt-1.18.2-r2.ebuild
@@ -22,7 +22,7 @@ IUSE="+gmp +libtommath tomsfastmath"
 
 # Enforce at least one math provider
 # bug #772935
-REQUIRED_USE="?? ( gmp libtommath tomsfastmath )"
+REQUIRED_USE="|| ( gmp libtommath tomsfastmath )"
 
 BDEPEND="
 	sys-devel/libtool