gentoo/gentoo

commit 13879e90b2639feb21725785ec5070b79e5d8757

sys-kernel/raspberrypi-sources: rename tarball for 5.10 (for now)
Closes: https://bugs.gentoo.org/762766
Package-Manager: Portage-3.0.14, Repoman-3.0.2
Signed-off-by: Sam James <sam@gentoo.org>

sys-kernel/raspberrypi-sources/raspberrypi-sources-5.10.11_p20210201.ebuild
@@ -14,13 +14,18 @@ detect_arch
 
 KV_FULL=$(ver_cut 4-)
 KV_FULL="raspberrypi-kernel_1.${KV_FULL/p/}-1"
+
 DESCRIPTION="Raspberry Pi kernel sources"
 HOMEPAGE="https://github.com/raspberrypi/linux"
-SRC_URI="https://github.com/raspberrypi/linux/archive/${KV_FULL}.tar.gz"
-S="${WORKDIR}/linux-${KV_FULL}"
+SRC_URI="https://github.com/raspberrypi/linux/archive/${KV_FULL}.tar.gz -> linux-${PV}-raspberrypi.tar.gz"
+S="${WORKDIR}/linux-${PV}-raspberrypi"
 
 KEYWORDS="~arm ~arm64"
 
 src_unpack() {
 	default
+
+	# We want to rename the unpacked directory to a nice normalised string
+	# bug #762766
+	mv "${WORKDIR}"/linux-${KV_FULL} "${WORKDIR}"/linux-${PV}-raspberrypi || die
 }

sys-kernel/raspberrypi-sources/Manifest
@@ -1,2 +1,2 @@
+DIST linux-5.10.11_p20210201-raspberrypi.tar.gz 186311431 BLAKE2B 1ba95e54232fafc421eee4a1966c9ead0420d51bc32a87050aeb15d8f75217fbaa727f5f41855d3771feb720e6395eef13453ff83b1bd8c486deafec11926e38 SHA512 776b71e7be700c1ec14a0fddbb6374c84d5645323a72871a4e76d095c73c26891e6fac548aebbadd34f0b8d5d4c340935e281cdec314b29bf7d3bf41351958b9
 DIST raspberrypi-kernel_1.20201201-1.tar.gz 175159649 BLAKE2B 6d7f4aed5696a2640ef591438b624434efa55af96216d7bf79888486a840171d8cd64548050c10890f1e636e8b42a7c97da0562ca36e44c07c2d24325942d374 SHA512 a8427e209a639b35088f32c4efa13ebd2d0efad7ad6f8ab40fbebec3c857d1acc4947aa0ea42e1edc1f8fd687b1bf8170764c811c0c876e92f0d66c269c91489
-DIST raspberrypi-kernel_1.20210201-1.tar.gz 186311431 BLAKE2B 1ba95e54232fafc421eee4a1966c9ead0420d51bc32a87050aeb15d8f75217fbaa727f5f41855d3771feb720e6395eef13453ff83b1bd8c486deafec11926e38 SHA512 776b71e7be700c1ec14a0fddbb6374c84d5645323a72871a4e76d095c73c26891e6fac548aebbadd34f0b8d5d4c340935e281cdec314b29bf7d3bf41351958b9

commit 2be341e38bae40dc056de898bca6e2f457432985

sys-kernel/raspberrypi-sources: fix USE=symlink redux
Previous symlink fix was intended for 5.4.79 but was applied
only to 5.10.11 then broken by the source renaming.

As a replacement fix, no longer set KV_FULL which works
out thanks to the source renaming.

Fixes: 13879e90b2639feb21725785ec5070b79e5d8757
Fixes: 40696e5680e8d00928e9ebca1fea5c4ed2035673
Closes: https://bugs.gentoo.org/770460
Package-Manager: Portage-3.0.14, Repoman-3.0.2
Signed-off-by: Ionen Wolkens <sudinave@gmail.com>
Signed-off-by: Sam James <sam@gentoo.org>

sys-kernel/raspberrypi-sources/raspberrypi-sources-5.10.11_p20210201.ebuild
@@ -6,19 +6,16 @@ EAPI=6
 ETYPE=sources
 K_DEFCONFIG="bcmrpi_defconfig"
 K_SECURITY_UNSUPPORTED=1
-EXTRAVERSION="-${PN}/-*"
 
 inherit kernel-2 eapi7-ver
 detect_version
-detect_arch
 
-KV_FULL=$(ver_cut 4-)
-KV_FULL="raspberrypi-kernel_1.${KV_FULL/p/}-1"
+MY_P=$(ver_cut 4-)
+MY_P="raspberrypi-kernel_1.${MY_P/p/}-1"
 
 DESCRIPTION="Raspberry Pi kernel sources"
 HOMEPAGE="https://github.com/raspberrypi/linux"
-SRC_URI="https://github.com/raspberrypi/linux/archive/${KV_FULL}.tar.gz -> linux-${PV}-raspberrypi.tar.gz"
-S="${WORKDIR}/linux-${PV}-raspberrypi"
+SRC_URI="https://github.com/raspberrypi/linux/archive/${MY_P}.tar.gz -> linux-${KV_FULL}.tar.gz"
 
 KEYWORDS="~arm ~arm64"
 
@@ -27,5 +24,5 @@ src_unpack() {
 
 	# We want to rename the unpacked directory to a nice normalised string
 	# bug #762766
-	mv "${WORKDIR}"/linux-${KV_FULL} "${WORKDIR}"/linux-${PV}-raspberrypi || die
+	mv "${WORKDIR}"/linux-${MY_P} "${WORKDIR}"/linux-${KV_FULL} || die
 }

sys-kernel/raspberrypi-sources/raspberrypi-sources-5.4.79_p20201201-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2020 Gentoo Authors
+# Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=6
@@ -6,21 +6,23 @@ EAPI=6
 ETYPE=sources
 K_DEFCONFIG="bcmrpi_defconfig"
 K_SECURITY_UNSUPPORTED=1
-EXTRAVERSION="-${PN}/-*"
 
 inherit kernel-2 eapi7-ver
 detect_version
-detect_arch
 
-MY_PV=$(ver_cut 4-)
-MY_PV=${MY_PV/p/}
+MY_P=$(ver_cut 4-)
+MY_P="raspberrypi-kernel_1.${MY_P/p/}-1"
+
 DESCRIPTION="Raspberry Pi kernel sources"
 HOMEPAGE="https://github.com/raspberrypi/linux"
-SRC_URI="https://github.com/raspberrypi/linux/archive/raspberrypi-kernel_1.${MY_PV}-1.tar.gz"
-S="${WORKDIR}/linux-raspberrypi-kernel_1.${MY_PV}-1"
+SRC_URI="https://github.com/raspberrypi/linux/archive/${MY_P}.tar.gz"
 
 KEYWORDS="~arm ~arm64"
 
 src_unpack() {
 	default
+
+	# We want to rename the unpacked directory to a nice normalised string
+	# bug #762766
+	mv "${WORKDIR}"/linux-${MY_P} "${WORKDIR}"/linux-${KV_FULL} || die
 }