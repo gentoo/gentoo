gentoo/gentoo

commit 717e0fca0dd5c3780f9574e4104f2b923c0c15f4

app-text/groonga: replace invalid use of EROOT
Signed-off-by: Sam James <sam@gentoo.org>

app-text/groonga/groonga-8.0.2.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2020 Gentoo Authors
+# Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=6
@@ -71,7 +71,7 @@ src_configure() {
 		$(use_with lzo)
 		$(use_with mecab)
 		$(use_enable msgpack message-pack)
-		$(use_with msgpack message-pack "${EROOT%/}/usr")
+		$(use_with msgpack message-pack "${EPREFIX}/usr")
 		$(use_enable nfkc)
 		$(use_with sphinx sphinx-build)
 		$(use_enable static-libs static)

app-text/groonga/groonga-6.1.2.ebuild
@@ -63,7 +63,7 @@ src_configure() {
 		$(use_with libevent) \
 		$(use_with lzo) \
 		$(use_with mecab) \
-		$(use_with msgpack message-pack "${EROOT}usr") \
+		$(use_with msgpack message-pack "${EPREFIX}/usr") \
 		$(use_enable nfkc) \
 		$(use_with sphinx sphinx-build) \
 		$(use_enable static-libs static) \
@@ -74,6 +74,7 @@ src_configure() {
 
 src_install() {
 	local DOCS=( README.md )
+
 	default
 
 	prune_libtool_files

commit 6874875df21e0244d59125034678fd7773b95023

app-text/groonga: EPREFIX->ESYSROOT in EAPI 7
We can use ${ESYSROOT} which is more apt in EAPI 7.
Can't for the earlier version.

Fixes: 717e0fca0dd5c3780f9574e4104f2b923c0c15f4
Signed-off-by: Sam James <sam@gentoo.org>

app-text/groonga/groonga-8.0.2.ebuild
@@ -78,7 +78,7 @@ src_configure() {
 		$(use_with lzo)
 		$(use_with mecab)
 		$(use_enable msgpack message-pack)
-		$(use_with msgpack message-pack "${EPREFIX}/usr")
+		$(use_with msgpack message-pack "${ESYSROOT}/usr")
 		$(use_enable nfkc)
 		$(use_with sphinx sphinx-build)
 		$(use_enable static-libs static)