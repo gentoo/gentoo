dcd2c38ebbc23a3c6b8ef6b185eda538d897e8ea
scardracs <marco@scardovi.com>
2021-03-27 13:52:13 +0100

    www-apps/radicale: bump to 3.0.6
    Bumped to new version and added dev-python/defusedxml as RDEPEND.
    Tested against python3.8/3.9 and gcc10. Not tested with musl.
    
    Package-Manager: Portage-3.0.17, Repoman-3.0.2
    Signed-off-by: Marco Scardovi <marco@scardovi.com>

--- www-apps/radicale/files/radicale-r2.init.d
+++ www-apps/radicale/files/radicale-r2.init.d
@@ -1,5 +1,5 @@
 #!/sbin/openrc-run
-# Copyright 1999-2019 Gentoo Authors
+# Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 PIDFILE=/run/radicale.pid
