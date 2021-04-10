84c6724d8211d61a2679d07c8d8f57800464f7f6
Scott Martin <om@organizedmagnetism.com>
2021-04-10 00:51:29 -0500

    media-tv/plex-media-server: restore package (new ebuild version 1.23.1.4602)
    This package does bundle deps (Python, optionally OpenSSL).
    
    [Note by sam@:
    
    - Restoring because this is a particularly popular piece of software.
    - Yes, Python is "bundled", but it's a stripped down copy, modified/patched copy
    with missing functionality, and we'll do best to treat it as if it's not there.
    - Users are better served by keeping an up-to-date version in tree, even if it's
    less-than-ideal for upstream to include Python in this way.]
    
    Closes: https://bugs.gentoo.org/769782
    Signed-off-by: Scott Martin <om@organizedmagnetism.com>
    Closes: https://github.com/gentoo/gentoo/pull/20320
    Signed-off-by: Sam James <sam@gentoo.org>

---
+++ media-tv/plex-media-server/files/plex-media-server.conf.d
@@ -0,0 +1,7 @@
+# Config file for /etc/init.d/plex-media-server
+
+PLEX_PIDFILE="/var/run/plex-media-server.pid"
+PLEX_OUTLOG="/var/log/pms/out.log"
+PLEX_ERRLOG="/var/log/pms/err.log"
+PLEX_USER="plex"
+PLEX_SCRIPT="/usr/sbin/start_pms"
