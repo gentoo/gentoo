gentoo/gentoo

commit 5b8909f23c0a764a9a481adbb27bfa161229775e

app-arch/xar: call pkg-config via toolchain-funcs.eclass helper
This ensures we call the correct pkg-config in e.g. cross.

Package-Manager: Portage-3.0.14-prefix, Repoman-3.0.2
Signed-off-by: Sam James <sam@gentoo.org>

app-arch/xar/xar-1.8-r2.ebuild
@@ -14,7 +14,7 @@ SLOT="0"
 KEYWORDS="~alpha amd64 arm arm64 ~hppa ~ia64 ~ppc ppc64 sparc x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~sparc-solaris ~sparc64-solaris ~x64-solaris ~x86-solaris"
 IUSE="libressl kernel_Darwin"
 
-DEPEND="
+RDEPEND="
 	!kernel_Darwin? (
 		!kernel_SunOS? ( virtual/acl )
 		!libressl? ( dev-libs/openssl:0=[${MULTILIB_USEDEP}] )
@@ -24,7 +24,10 @@ DEPEND="
 	sys-libs/zlib[${MULTILIB_USEDEP}]
 	dev-libs/libxml2[${MULTILIB_USEDEP}]
 "
-RDEPEND="${DEPEND}"
+DEPEND="
+	${RDEPEND}
+	virtual/pkgconfig
+"
 
 PATCHES=(
 	"${FILESDIR}"/${PN}-1.6.1-ext2.patch
@@ -36,7 +39,7 @@ PATCHES=(
 S=${WORKDIR}/${PN}-${APPLE_PV}/${PN}
 
 multilib_src_configure() {
-	use kernel_Darwin || append-libs $(pkg-config --libs openssl)
+	use kernel_Darwin || append-libs $($(tc-getPKG_CONFIG) --libs openssl)
 	ECONF_SOURCE=${S} \
 	econf \
 		--disable-static

app-arch/xar/xar-1.8-r3.ebuild
@@ -14,7 +14,7 @@ SLOT="0"
 KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~ppc ~ppc64 ~riscv ~sparc ~x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~sparc-solaris ~sparc64-solaris ~x64-solaris ~x86-solaris"
 IUSE="libressl kernel_Darwin"
 
-DEPEND="
+RDEPEND="
 	!kernel_Darwin? (
 		!kernel_SunOS? ( virtual/acl )
 		!libressl? ( dev-libs/openssl:0=[${MULTILIB_USEDEP}] )
@@ -24,7 +24,10 @@ DEPEND="
 	sys-libs/zlib[${MULTILIB_USEDEP}]
 	dev-libs/libxml2[${MULTILIB_USEDEP}]
 "
-RDEPEND="${DEPEND}"
+DEPEND="
+	${RDEPEND}
+	virtual/pkgconfig
+"
 
 PATCHES=(
 	"${FILESDIR}"/${PN}-1.6.1-ext2.patch
@@ -54,7 +57,7 @@ src_prepare() {
 }
 
 multilib_src_configure() {
-	use kernel_Darwin || append-libs $(pkg-config --libs openssl)
+	use kernel_Darwin || append-libs $($(tc-getPKG_CONFIG) --libs openssl)
 	ECONF_SOURCE=${S} \
 	econf \
 		--disable-static

app-arch/xar/xar-1.8-r4.ebuild
@@ -24,6 +24,7 @@ DEPEND="
 	dev-libs/libxml2[${MULTILIB_USEDEP}]
 "
 RDEPEND="${DEPEND}"
+BDEPEND="virtual/pkgconfig"
 
 PATCHES=(
 	"${FILESDIR}"/${PN}-1.6.1-ext2.patch
@@ -65,8 +66,8 @@ src_prepare() {
 }
 
 multilib_src_configure() {
-	append-libs $(pkg-config --libs openssl)
-	use elibc_musl && append-libs $(pkg-config --libs fts-standalone)
+	append-libs $($(tc_getPKG_CONFIG) --libs openssl)
+	use elibc_musl && append-libs $($(tc-getPKG_CONFIG) --libs fts-standalone)
 	ECONF_SOURCE=${S} \
 	econf \
 		--disable-static

commit 39bd0aeaca1ece542f8050a9f921c77193374511

app-arch/xar: add toolchain-funcs
Fixes: 5b8909f23c0a764a9a481adbb27bfa161229775e
Closes: https://bugs.gentoo.org/782649
Signed-off-by: Sam James <sam@gentoo.org>

app-arch/xar/xar-1.8-r2.ebuild
@@ -2,7 +2,7 @@
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=6
-inherit flag-o-matic multilib-minimal
+inherit flag-o-matic toolchain-funcs multilib-minimal
 
 APPLE_PV=400
 DESCRIPTION="An easily extensible archive format"

app-arch/xar/xar-1.8-r3.ebuild
@@ -2,7 +2,7 @@
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=6
-inherit flag-o-matic multilib-minimal multilib
+inherit flag-o-matic toolchain-funcs multilib-minimal multilib
 
 APPLE_PV=417.1
 DESCRIPTION="An easily extensible archive format"

app-arch/xar/xar-1.8-r4.ebuild
@@ -2,7 +2,7 @@
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
-inherit flag-o-matic multilib-minimal multilib
+inherit flag-o-matic toolchain-funcs multilib-minimal multilib
 
 APPLE_PV=417.1
 DESCRIPTION="An easily extensible archive format"
@@ -66,7 +66,7 @@ src_prepare() {
 }
 
 multilib_src_configure() {
-	append-libs $($(tc_getPKG_CONFIG) --libs openssl)
+	append-libs $($(tc-getPKG_CONFIG) --libs openssl)
 	use elibc_musl && append-libs $($(tc-getPKG_CONFIG) --libs fts-standalone)
 	ECONF_SOURCE=${S} \
 	econf \