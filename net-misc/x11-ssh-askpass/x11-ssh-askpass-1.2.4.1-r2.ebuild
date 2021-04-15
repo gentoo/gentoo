gentoo/gentoo

commit 9d02b021fb62f75f7df091530f19b0582aa601af

net-misc/x11-ssh-askpass: fix build with -native-symlinks
Closes: https://bugs.gentoo.org/725200
Signed-off-by: Ionen Wolkens <sudinave@gmail.com>
Signed-off-by: Joonas Niilola <juippis@gentoo.org>

net-misc/x11-ssh-askpass/x11-ssh-askpass-1.2.4.1-r2.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2020 Gentoo Authors
+# Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
@@ -21,16 +21,16 @@ RDEPEND="virtual/ssh
 	x11-libs/libXt"
 DEPEND="${RDEPEND}"
 BDEPEND="app-text/rman
-	x11-misc/imake"
+	>=x11-misc/imake-1.0.8-r1"
 
 src_configure() {
 	econf --libexecdir=/usr/"$(get_libdir)"/misc \
 		--disable-installing-app-defaults
-	xmkmf || die "xmkmf failed"
+	CC="$(tc-getBUILD_CC)" LD="$(tc-getLD)" \
+		IMAKECPP="${IMAKECPP:-$(tc-getCPP)}" xmkmf -a || die "xmkmf failed"
 }
 
 src_compile() {
-	emake includes
 	emake CC="$(tc-getCC)" CDEBUGFLAGS="${CFLAGS}"
 }
 

commit fa4e6f8caa3eb57d9aa17d757f9a4d071cf702fa

net-misc/x11-ssh-askpass: remove unneeded command call
simplifying using xmkmf -a here was not a good idea, surprising
that it worked at all preventing this from being noticed.

Fixes: 9d02b021fb62f75f7df091530f19b0582aa601af
Closes: https://bugs.gentoo.org/782988
Signed-off-by: Ionen Wolkens <sudinave@gmail.com>

net-misc/x11-ssh-askpass/x11-ssh-askpass-1.2.4.1-r2.ebuild
@@ -27,10 +27,11 @@ src_configure() {
 	econf --libexecdir=/usr/"$(get_libdir)"/misc \
 		--disable-installing-app-defaults
 	CC="$(tc-getBUILD_CC)" LD="$(tc-getLD)" \
-		IMAKECPP="${IMAKECPP:-$(tc-getCPP)}" xmkmf -a || die "xmkmf failed"
+		IMAKECPP="${IMAKECPP:-$(tc-getCPP)}" xmkmf || die "xmkmf failed"
 }
 
 src_compile() {
+	emake includes
 	emake CC="$(tc-getCC)" CDEBUGFLAGS="${CFLAGS}"
 }
 