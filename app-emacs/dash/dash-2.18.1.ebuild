gentoo/gentoo

commit d4bc07ec157acc2eeb684ac885e94be5b8151c16

app-emacs/dash: Version bump to 2.18.1
Closes: https://bugs.gentoo.org/785058
Package-Manager: Portage-3.0.18, Repoman-3.0.3
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

app-emacs/dash/dash-2.18.1.ebuild
@@ -1,0 +1,25 @@
+# Copyright 1999-2020 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+
+inherit elisp
+
+DESCRIPTION="A modern list library for Emacs"
+HOMEPAGE="https://github.com/magnars/dash.el"
+SRC_URI="https://github.com/magnars/dash.el/archive/${PV}.tar.gz -> ${P}.tar.gz"
+
+LICENSE="GPL-3+"
+SLOT="0"
+KEYWORDS="~amd64 ~arm ~arm64 ~ppc64 ~x86 ~amd64-linux ~x86-linux"
+
+BDEPEND="sys-apps/texinfo"
+
+S="${WORKDIR}/${PN}.el-${PV}"
+SITEFILE="50${PN}-gentoo.el"
+ELISP_TEXINFO="dash.texi"
+DOCS="README.md"
+
+src_test() {
+	./run-tests.sh || die
+}

app-emacs/dash/Manifest
@@ -1,3 +1,4 @@
 DIST dash-2.14.1.tar.gz 141115 BLAKE2B 87fc7a02c66161d1c16debc54c6336f36cc47cf7c7c470d190424b9c463723058ef6f72cadf3f4d4215b36f369b9e5356101c4ea16fede10cefde90b5e0b384b SHA512 cfd4ed7a9eab881a0c3bd2744e78c5ef99a92db4d3afee8c632c0a766adcdbf1999f4e9770e2c4e941526d488dd6f9327937389490cc2310d79b38c5bf2ddd12
 DIST dash-2.16.0.tar.gz 148151 BLAKE2B 1c51d926a6dd4ecdbc2ad132347af7bbd27e604d6e2c61a6cfe548802b08a57678ca26aba69f8ae27ca7cdeca84cd469bd9a7849bf28c2abae44f7172ad2659d SHA512 a0196327c8e81d1b343bb56de00a3c2f59b1d3f86dc4e923cf350658590fa106aa0be933aa0f51ff3c02d9aff8450c5e055d169ae8fb876be068bf15a52af216
 DIST dash-2.17.0.tar.gz 149379 BLAKE2B d88106b08d1454b483f08db59e641680e7361b4572b47e238b096977ba308eb4c2810621c99e98126a94e4afdba9faf966e3c56cb6f4ad1292ef05fe7cc0546f SHA512 e53fa1bb9e854b446931d943caac719646218c96160f7c50615b5e95866f4db9dffa6e752596e9c223bcd5b39a80c0c75748b0e0564d72ce5825715a29b80d01
+DIST dash-2.18.1.tar.gz 150343 BLAKE2B 5bd0db651830d828c3c51739ab90fb34c1a70b9e72eb434d9dafca0b6e5c6f57bd121eca45fc01a20ce32f20d0c3b49a34bb4a214bacb19712a931e8a587a40b SHA512 f9f0eea41cbf589e2024955f43ec7cd00c37947d07cfd68fe267e541baec1f0c86066c39ad2fb4e467c1cc579c2369a0d75496d176775122e7e48dd24954640c

commit d31a80f55ba20fffcffeeb961b82745c29e645c8

app-emacs/dash: fix copyright year
Fixes: d4bc07ec157acc2eeb684ac885e94be5b8151c16 (time machine!)
Signed-off-by: Sam James <sam@gentoo.org>

app-emacs/dash/dash-2.18.1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2020 Gentoo Authors
+# Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7