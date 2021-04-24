gentoo/gentoo

commit b2e176f40fd0f64e3cd9651e73369bc2afb8b490

games-board/iagno: set VALA_MAX_API_VERSION to 0.50
Signed-off-by: Christophe Lermytte <gentoo@lermytte.be>
Signed-off-by: Mart Raudsepp <leio@gentoo.org>

games-board/iagno/iagno-3.38.1.ebuild
@@ -3,6 +3,7 @@
 
 EAPI=7
 VALA_MIN_API_VERSION="0.44"
+VALA_MIN_API_VERSION="0.50"
 
 inherit gnome.org gnome2-utils meson vala xdg
 

commit 9a7175f6c6e441ef82af055a9b9cf837e65d95f9

games-board/iagno: fix VALA_MAX_API_VERSION setting
Fixes: b2e176f40fd0f64e3cd9651e73369bc2afb8b490
Package-Manager: Portage-3.0.12, Repoman-3.0.2
Signed-off-by: Mart Raudsepp <leio@gentoo.org>

games-board/iagno/iagno-3.38.1.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=7
 VALA_MIN_API_VERSION="0.44"
-VALA_MIN_API_VERSION="0.50"
+VALA_MAX_API_VERSION="0.50"
 
 inherit gnome.org gnome2-utils meson vala xdg
 