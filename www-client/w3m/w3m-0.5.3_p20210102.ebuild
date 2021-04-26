gentoo/gentoo

commit 50c53cbcbe84fe1473244e2bd6ad5e4533601bac

www-client/w3m: further fixups for autoreconf patch
Closes: https://bugs.gentoo.org/778482
Signed-off-by: Sam James <sam@gentoo.org>

www-client/w3m/w3m-0.5.3_p20210102.ebuild
@@ -11,6 +11,7 @@ MY_PV="${PV/_p/+git}"
 DESCRIPTION="Text based WWW browser, supports tables and frames"
 HOMEPAGE="https://github.com/tats/w3m"
 SRC_URI="https://github.com/tats/w3m/archive/v${MY_PV}.tar.gz -> ${MY_P}.tar.gz"
+S="${WORKDIR}/${P/_p/-git}"
 
 LICENSE="w3m"
 SLOT="0"
@@ -47,16 +48,17 @@ BDEPEND="
 	virtual/pkgconfig
 "
 
-PATCHES=( "${FILESDIR}/lang.patch" )
-
-S="${WORKDIR}/${P/_p/-git}"
+PATCHES=(
+	"${FILESDIR}"/${PN}-0.5.3_p20210102-restore-gettext-macro.patch
+)
 
 src_prepare() {
 	default
+
 	sed -i "/^AR=/s:ar:$(tc-getAR):" {.,${PN}img,libwc}/Makefile.in || die
 	hprefixify acinclude.m4
 
-	eautoconf
+	AT_M4DIR="m4" eautoreconf
 }
 
 src_configure() {

www-client/w3m/files/w3m-0.5.3_p20210102-restore-gettext-macro.patch
@@ -1,5 +1,5 @@
-new file mode 100644
-index 00000000..da1b2aef
+https://sources.debian.org/patches/gnokii/0.6.30+dfsg-1.3/0001-Add-codeset.m4-to-m4-to-prevent-build-failure-with-g.patch/
+https://bugs.gentoo.org/778482
 --- /dev/null
 +++ b/m4/codeset.m4
 @@ -0,0 +1,25 @@
@@ -29,4 +29,3 @@ index 00000000..da1b2aef
 +])
 +
 --
-2.20.1

commit 8003dbb063a24b67ad723b6b153617b24ab7d697

www-client/w3m: revert back to eautoconf for now
Earlier, we switched to eautoreconf because:
- it's a noop when *.am is missing anyway
- fixing the build system is preferable
- this allows patches to be added without
first having to fix an unrelated issue first

Let's roll back to eautoconf for now because
of a gettext mismatch.

Fixes: 50c53cbcbe84fe1473244e2bd6ad5e4533601bac
Fixes: 5d3abfa9559c11bd9ac3087a9bf3debd51cf0c30
Bug: https://bugs.gentoo.org/778482
Closes: https://bugs.gentoo.org/785760
Signed-off-by: Sam James <sam@gentoo.org>

www-client/w3m/w3m-0.5.3_p20210102.ebuild
@@ -58,7 +58,7 @@ src_prepare() {
 	sed -i "/^AR=/s:ar:$(tc-getAR):" {.,${PN}img,libwc}/Makefile.in || die
 	hprefixify acinclude.m4
 
-	AT_M4DIR="m4" eautoreconf
+	AT_M4DIR="m4" eautoconf
 }
 
 src_configure() {