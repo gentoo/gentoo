gentoo/gentoo

commit 0b0163a009b3269f74b3584f3700c577e9df3aa3

net-analyzer/vnstat: use live template, add verify-sig support
Signed-off-by: Sam James <sam@gentoo.org>

net-analyzer/vnstat/vnstat-2.6.ebuild
@@ -7,11 +7,24 @@ inherit systemd user tmpfiles
 
 DESCRIPTION="Console-based network traffic monitor that keeps statistics of network usage"
 HOMEPAGE="https://humdi.net/vnstat/"
-SRC_URI="https://humdi.net/vnstat/${P}.tar.gz"
+
+if [[ ${PV} == *9999 ]] ; then
+	EGIT_REPO_URI="https://github.com/vergoh/vnstat"
+	inherit git-r3
+else
+	VERIFY_SIG_OPENPGP_KEY_PATH=/usr/share/openpgp-keys/teemutoivola.asc
+	inherit verify-sig
+
+	SRC_URI="https://humdi.net/vnstat/${P}.tar.gz"
+	SRC_URI+=" verify-sig? ( https://humdi.net/vnstat/${P}.tar.gz.asc )"
+
+	KEYWORDS="~amd64 ~arm ~arm64 ~hppa ~mips ~ppc ~ppc64 ~sparc ~x86"
+
+	BDEPEND="verify-sig? ( app-crypt/openpgp-keys-teemutoivola )"
+fi
 
 LICENSE="GPL-2"
 SLOT="0"
-KEYWORDS="amd64 arm arm64 ~hppa ~mips ppc ppc64 sparc x86"
 IUSE="gd selinux test"
 RESTRICT="!test? ( test )"
 
@@ -61,6 +74,7 @@ src_install() {
 	newtmpfiles "${FILESDIR}"/vnstatd.tmpfile vnstatd.conf
 
 	use gd && doman man/vnstati.1
+
 	doman man/vnstat.1 man/vnstatd.8
 
 	newdoc INSTALL README.setup

net-analyzer/vnstat/Manifest
@@ -1,2 +1,4 @@
 DIST vnstat-1.17.tar.gz 238397 BLAKE2B 73aaffda3c91a1146b1d6a21f637272be227a15a7964aad90fb76c407c5f7fb0085e6f9f54408e8ebc667475c15f260d701c5924737f20921ebc0a35affd52be SHA512 c9c119a89054dacdc15026c766a20d88336d7689dd4264530d0347fcc58f3cbfc806b271e0f83e76c3b4a31ab793a628ee7d7b696943da919ee4cd2fdb481cc3
+DIST vnstat-1.17.tar.gz.asc 819 BLAKE2B 3186e28dc29edb37eb0bbef9b29f1ea682e4a9de208b69b83a8bc99c81f272eb6865176aaa30e415bf8423d94f385e0725e1256eeb822c3152638eaace23b749 SHA512 c26af29daefe6431c1769812d04ba5bff92ac1241a6a49df11e28f0e51b21d4eb84ff1f1fb133a388fd8ac59967162bc5d60d5f02c409d34be5414acd34f7818
 DIST vnstat-2.6.tar.gz 275751 BLAKE2B 7351b265d66e42d1e5450dae6f5eaa973a9cdc171b865689540b5838ddeb2629da968bc2ba655cb6ff909227a859f0334900a1597c78b772124a993343a4cf23 SHA512 1a62fc9c2379b050208945e85bad04f602a0f2c71e6bd069d241c1df77c76ec6258948fc3fdf633b15c52665782cae4f995a916103d330bfb46cab0291d3a1ea
+DIST vnstat-2.6.tar.gz.asc 833 BLAKE2B 5e79d1b622dd4d37603a209b78018a3e88f2c404c700e1f439e4b98e2c2dd5bdf26e8ecd3b1b1242e14a725d7839c5a8b3b5448dc27e1f917cbeaf6012546656 SHA512 c643152bec14b70317465f8adc2256b41b8686e106b35ce492ad5030c2d5b32162441ac5fb2f309071da855efdf444076288cf075d8073bad0c0907067366e7f

net-analyzer/vnstat/vnstat-1.17.ebuild
@@ -3,11 +3,13 @@
 
 EAPI=7
 
-inherit toolchain-funcs user
+VERIFY_SIG_OPENPGP_KEY_PATH=/usr/share/openpgp-keys/teemutoivola.asc
+inherit toolchain-funcs user verify-sig
 
 DESCRIPTION="Console-based network traffic monitor that keeps statistics of network usage"
 HOMEPAGE="https://humdi.net/vnstat/"
 SRC_URI="https://humdi.net/vnstat/${P}.tar.gz"
+SRC_URI+=" verify-sig? ( https://humdi.net/vnstat/${P}.tar.gz.asc )"
 
 LICENSE="GPL-2"
 SLOT="0"
@@ -21,6 +23,7 @@ DEPEND="
 	test? ( dev-libs/check )
 "
 RDEPEND+=" selinux? ( sec-policy/selinux-vnstatd )"
+BDEPEND="verify-sig? ( app-crypt/openpgp-keys-teemutoivola )"
 
 pkg_setup() {
 	enewgroup vnstat

net-analyzer/vnstat/vnstat-9999.ebuild
@@ -3,11 +3,25 @@
 
 EAPI=7
 
-inherit git-r3 systemd user tmpfiles
+inherit systemd user tmpfiles
 
 DESCRIPTION="Console-based network traffic monitor that keeps statistics of network usage"
 HOMEPAGE="https://humdi.net/vnstat/"
-EGIT_REPO_URI="https://github.com/vergoh/vnstat"
+
+if [[ ${PV} == *9999 ]] ; then
+	EGIT_REPO_URI="https://github.com/vergoh/vnstat"
+	inherit git-r3
+else
+	VERIFY_SIG_OPENPGP_KEY_PATH=/usr/share/openpgp-keys/teemutoivola.asc
+	inherit verify-sig
+
+	SRC_URI="https://humdi.net/vnstat/${P}.tar.gz"
+	SRC_URI+=" verify-sig? ( https://humdi.net/vnstat/${P}.tar.gz.asc )"
+
+	KEYWORDS="~amd64 ~arm ~arm64 ~hppa ~mips ~ppc ~ppc64 ~sparc ~x86"
+
+	BDEPEND="verify-sig? ( app-crypt/openpgp-keys-teemutoivola )"
+fi
 
 LICENSE="GPL-2"
 SLOT="0"
@@ -60,6 +74,7 @@ src_install() {
 	newtmpfiles "${FILESDIR}"/vnstatd.tmpfile vnstatd.conf
 
 	use gd && doman man/vnstati.1
+
 	doman man/vnstat.1 man/vnstatd.1
 
 	newdoc INSTALL.md README.setup

commit e2f259c68c517f4d88a9e6d730dc8fff417d316d

net-analyzer/vnstat: fix sync with live
Fixes: 0b0163a009b3269f74b3584f3700c577e9df3aa3
Signed-off-by: Sam James <sam@gentoo.org>

net-analyzer/vnstat/vnstat-2.6.ebuild
@@ -18,7 +18,7 @@ else
 	SRC_URI="https://humdi.net/vnstat/${P}.tar.gz"
 	SRC_URI+=" verify-sig? ( https://humdi.net/vnstat/${P}.tar.gz.asc )"
 
-	KEYWORDS="~amd64 ~arm ~arm64 ~hppa ~mips ~ppc ~ppc64 ~sparc ~x86"
+	KEYWORDS="amd64 arm arm64 ~hppa ~mips ppc ppc64 sparc x86"
 
 	BDEPEND="verify-sig? ( app-crypt/openpgp-keys-teemutoivola )"
 fi