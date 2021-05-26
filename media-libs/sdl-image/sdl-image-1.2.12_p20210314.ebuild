gentoo/gentoo

commit cd6eddbe2449e844783bc615ad4947ab4466b882

media-libs/sdl-image: Fix for multilib + slibtool
Closes: https://bugs.gentoo.org/791679
Signed-off-by: Sam James <sam@gentoo.org>

media-libs/sdl-image/sdl-image-1.2.12_p20210314.ebuild
@@ -48,6 +48,10 @@ multilib_src_configure() {
 		--enable-xv
 }
 
+multilib_src_compile() {
+	emake OBJC="$(tc-getCC)"
+}
+
 multilib_src_install() {
 	emake DESTDIR="${D}" install
 	dobin .libs/showimage

commit 30465d2e085264f1c1cea2071b0087f430a371c5

media-libs/sdl-image: add explicit toolchain-funcs inherit
Fixes: cd6eddbe2449e844783bc615ad4947ab4466b882
Bug: https://bugs.gentoo.org/791679
Signed-off-by: Sam James <sam@gentoo.org>

media-libs/sdl-image/sdl-image-1.2.12_p20210314.ebuild
@@ -2,7 +2,7 @@
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
-inherit multilib-minimal
+inherit toolchain-funcs multilib-minimal
 
 MY_P="${P/sdl-/SDL_}"
 MY_COMMIT="5d792dde2f764daf15dc48521774a3354330db69"