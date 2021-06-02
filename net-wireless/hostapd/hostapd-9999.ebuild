gentoo/gentoo

commit 1eb56d9532c0a7a708492ce294448fccbe27ff29

net-wireless/hostapd: remove libressl support
Package-Manager: Portage-3.0.18, Repoman-3.0.2
Signed-off-by: Mikle Kolyada <zlogene@gentoo.org>

net-wireless/hostapd/hostapd-9999.ebuild
@@ -28,17 +28,12 @@ fi
 
 LICENSE="BSD"
 SLOT="0"
-IUSE="internal-tls ipv6 libressl logwatch netlink sqlite +suiteb +wps +crda"
-
-# suiteb impl uses openssl feature not available in libressl, see bug 710992
-REQUIRED_USE="?? ( libressl suiteb )"
+IUSE="internal-tls ipv6 logwatch netlink sqlite +suiteb +wps +crda"
 
 DEPEND="
-	libressl? ( dev-libs/libressl:0= )
-	!libressl? (
 		internal-tls? ( dev-libs/libtommath )
 		!internal-tls? ( dev-libs/openssl:0=[-bindist] )
-	)
+
 	kernel_linux? (
 		dev-libs/libnl:3
 		crda? ( net-wireless/crda )
@@ -50,11 +45,7 @@ RDEPEND="${DEPEND}"
 
 pkg_pretend() {
 	if use internal-tls; then
-		if use libressl; then
-			elog "libressl flag takes precedence over internal-tls"
-		else
 			ewarn "internal-tls implementation is experimental and provides fewer features"
-		fi
 	fi
 }
 
@@ -107,7 +98,7 @@ src_configure() {
 		echo "CONFIG_SUITEB192=y" >> ${CONFIG} || die
 	fi
 
-	if use internal-tls && ! use libressl; then
+	if use internal-tls ; then
 		echo "CONFIG_TLS=internal" >> ${CONFIG} || die
 	else
 		# SSL authentication methods
@@ -211,7 +202,7 @@ src_configure() {
 src_compile() {
 	emake V=1
 
-	if use libressl || ! use internal-tls; then
+	if  use internal-tls; then
 		emake V=1 nt_password_hash
 		emake V=1 hlr_auc_gw
 	fi
@@ -226,7 +217,7 @@ src_install() {
 	dosbin ${PN}
 	dobin ${PN}_cli
 
-	if use libressl || ! use internal-tls; then
+	if ! use internal-tls; then
 		dobin nt_password_hash hlr_auc_gw
 	fi
 

net-wireless/hostapd/hostapd-2.9-r3.ebuild
@@ -28,17 +28,12 @@ fi
 
 LICENSE="BSD"
 SLOT="0"
-IUSE="internal-tls ipv6 libressl logwatch netlink sqlite +suiteb +wps +crda"
-
-# suiteb impl uses openssl feature not available in libressl, see bug 710992
-REQUIRED_USE="?? ( libressl suiteb )"
+IUSE="internal-tls ipv6 logwatch netlink sqlite +suiteb +wps +crda"
 
 DEPEND="
-	libressl? ( dev-libs/libressl:0= )
-	!libressl? (
 		internal-tls? ( dev-libs/libtommath )
 		!internal-tls? ( dev-libs/openssl:0=[-bindist] )
-	)
+
 	kernel_linux? (
 		dev-libs/libnl:3
 		crda? ( net-wireless/crda )
@@ -50,11 +45,7 @@ RDEPEND="${DEPEND}"
 
 pkg_pretend() {
 	if use internal-tls; then
-		if use libressl; then
-			elog "libressl flag takes precedence over internal-tls"
-		else
 			ewarn "internal-tls implementation is experimental and provides fewer features"
-		fi
 	fi
 }
 
@@ -107,7 +98,7 @@ src_configure() {
 		echo "CONFIG_SUITEB192=y" >> ${CONFIG} || die
 	fi
 
-	if use internal-tls && ! use libressl; then
+	if use internal-tls ; then
 		echo "CONFIG_TLS=internal" >> ${CONFIG} || die
 	else
 		# SSL authentication methods
@@ -211,7 +202,7 @@ src_configure() {
 src_compile() {
 	emake V=1
 
-	if use libressl || ! use internal-tls; then
+	if ! use internal-tls; then
 		emake V=1 nt_password_hash
 		emake V=1 hlr_auc_gw
 	fi
@@ -226,7 +217,7 @@ src_install() {
 	dosbin ${PN}
 	dobin ${PN}_cli
 
-	if use libressl || ! use internal-tls; then
+	if ! use internal-tls; then
 		dobin nt_password_hash hlr_auc_gw
 	fi
 

commit 7c3847f54c77bc1f89b5f3379da76af8bf48e3db

net-wireless/hostapd: fix USE=-internal-tls
Fixes: 1eb56d95 ("net-wireless/hostapd: remove libressl support")
Package-Manager: Portage-3.0.19, Repoman-3.0.3
Signed-off-by: Thomas Deutschmann <whissi@gentoo.org>

net-wireless/hostapd/hostapd-9999.ebuild
@@ -204,7 +204,7 @@ src_configure() {
 src_compile() {
 	emake V=1
 
-	if  use internal-tls; then
+	if ! use internal-tls; then
 		emake V=1 nt_password_hash
 		emake V=1 hlr_auc_gw
 	fi