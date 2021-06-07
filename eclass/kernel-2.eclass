gentoo/gentoo-gitmig-20150809-draft

commit 906501be57c01ac198dcdd4728098b1e30399fe2

adding symlink use flag support

eclass/kernel-2.eclass
@@ -1,6 +1,6 @@
 # Copyright 1999-2005 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-x86/eclass/kernel-2.eclass,v 1.83 2005/01/14 11:19:05 johnm Exp $
+# $Header: /var/cvsroot/gentoo-x86/eclass/kernel-2.eclass,v 1.84 2005/01/14 12:15:15 johnm Exp $
 
 # Description: kernel.eclass rewrite for a clean base regarding the 2.6
 #              series of kernel with back-compatibility for 2.4
@@ -163,7 +163,7 @@ then
 		
 	SLOT="${PVR}"
 	DESCRIPTION="Sources for the Linux kernel"
-	IUSE="${IUSE} build doc"
+	IUSE="${IUSE} symlink build doc"
 elif [ "${ETYPE}" == "headers" ]
 then
 	DESCRIPTION="Linux system headers"
@@ -387,6 +387,9 @@ preinst_headers() {
 postinst_sources() {
 	local MAKELINK=0
 	
+	# if we have USE=symlink, then force K_SYMLINK=1
+	use symlink && K_SYMLINK=1
+
 	# if we are to forcably symlink, delete it if it already exists first.
 	if [[ -n ${K_SYMLINK} ]] ; then
 		[[ -h ${ROOT}usr/src/linux ]] && rm ${ROOT}usr/src/linux

gentoo/gentoo

commit 754bb59b8c14d4a822e46cad465e7c3a6328a8ce

eclass/kernel-2: respect USE=symlink
kernel-2 eclass postinst_sources() creates /usr/usr/linux symlink
regardless of USE=symlink. MAKELINK variable controls the creation
of /usr/src/linux, however MAKELINK is always set to 1 only if
/usr/src/linux does not currently exist with no consideration for
USE=symlink.

This change simplify the whole /usr/usr/linux symlink creation to
instead depend on K_SYMLINK which is set if USE=symlink.

This change also refuse to remove existing /usr/usr/linux if it
currently exist while not being a symlink

Note that this is broken since 906501be57c0 ("adding symlink use flag
support")

Bug: https://bugs.gentoo.org/695592
Signed-off-by: Bertrand Jacquin <bertrand@jacquin.bzh>

eclass/kernel-2.eclass
@@ -945,8 +945,6 @@ preinst_headers() {
 # see inline comments
 
 postinst_sources() {
-	local MAKELINK=0
-
 	# if we have USE=symlink, then force K_SYMLINK=1
 	use symlink && K_SYMLINK=1
 
@@ -960,14 +958,10 @@ postinst_sources() {
 
 	# if we are to forcably symlink, delete it if it already exists first.
 	if [[ ${K_SYMLINK} -gt 0 ]]; then
-		[[ -h ${EROOT%/}/usr/src/linux ]] && { rm "${EROOT%/}"/usr/src/linux || die; }
-		MAKELINK=1
-	fi
+		if [[ -e ${EROOT%/}/usr/src/linux && ! -L ${EROOT%/}/usr/src/linux ]] ; then
+			die "${EROOT%/}/usr/src/linux exist and is not a symlink"
+		fi
 
-	# if the link doesnt exist, lets create it
-	[[ ! -h ${EROOT%/}/usr/src/linux ]] && MAKELINK=1
-
-	if [[ ${MAKELINK} == 1 ]]; then
 		ln -sf linux-${KV_FULL} "${EROOT%/}"/usr/src/linux || die
 	fi
 