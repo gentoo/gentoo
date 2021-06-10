gentoo/gentoo

commit f81f95471c25e346bac4c489a058af14f4e9b68e

sys-block/thin-provisioning-tools: Improved live ebuild.
Package-Manager: Portage-2.3.64, Repoman-2.3.12
Signed-off-by: Lars Wendler <polynomial-c@gentoo.org>

sys-block/thin-provisioning-tools/thin-provisioning-tools-9999.ebuild
@@ -1,11 +1,9 @@
-# Copyright 1999-2018 Gentoo Foundation
+# Copyright 1999-2019 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=6
 
-[[ ${PV} == *9999 ]] && SCM="git-r3"
-EGIT_REPO_URI='https://github.com/jthornber/thin-provisioning-tools.git'
-inherit autotools flag-o-matic $SCM
+inherit autotools flag-o-matic
 
 DESCRIPTION="A suite of tools for thin provisioning on Linux"
 HOMEPAGE="https://github.com/jthornber/thin-provisioning-tools"
@@ -13,6 +11,9 @@ HOMEPAGE="https://github.com/jthornber/thin-provisioning-tools"
 if [[ ${PV} != *9999 ]]; then
 	SRC_URI="https://github.com/jthornber/${PN}/archive/v${PV}.tar.gz -> ${P}.tar.gz"
 	KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~mips ~ppc ~ppc64 ~s390 ~sh ~sparc ~x86 ~amd64-linux ~x86-linux"
+else
+	inherit git-r3
+	EGIT_REPO_URI='https://github.com/jthornber/thin-provisioning-tools.git'
 fi
 
 LICENSE="GPL-3"
@@ -25,14 +26,20 @@ RDEPEND="!static? ( ${LIB_DEPEND//\[static-libs(+)]} )"
 DEPEND="${RDEPEND}
 	static? ( ${LIB_DEPEND} )
 	test? (
-		|| ( dev-lang/ruby:2.5 dev-lang/ruby:2.4 dev-lang/ruby:2.3 )
+		|| (
+			dev-lang/ruby:2.6
+			dev-lang/ruby:2.5
+			dev-lang/ruby:2.4
+		)
 		>=dev-cpp/gtest-1.8.0
 		dev-util/cucumber
 		dev-util/aruba
 	)
 	dev-libs/boost"
 
-PATCHES=( "${FILESDIR}"/${PN}-0.7.0-build-fixes.patch )
+PATCHES=(
+	"${FILESDIR}"/${PN}-0.7.0-build-fixes.patch
+)
 
 src_prepare() {
 	default
@@ -41,11 +48,13 @@ src_prepare() {
 
 src_configure() {
 	use static && append-ldflags -static
-	STRIP=true econf \
-		--prefix="${EPREFIX}"/ \
-		--bindir="${EPREFIX}"/sbin \
-		--with-optimisation='' \
+	local myeconfargs=(
+		--prefix="${EPREFIX}"/
+		--bindir="${EPREFIX}"/sbin
+		--with-optimisation=''
 		$(use_enable test testing)
+	)
+	STRIP=true econf "${myeconfargs[@]}"
 }
 
 src_compile() {
@@ -58,6 +67,6 @@ src_test() {
 }
 
 src_install() {
-	emake DESTDIR="${D}" DATADIR="${ED%/}/usr/share" install
+	emake DESTDIR="${D}" DATADIR="${D%/}/usr/share" install
 	dodoc README.md TODO.org
 }

commit 58880f713595b3da0e8f9a0b9d9efeef33cad210

sys-block/thin-provisioning-tools: fix bug 795189
Fixes: f81f95471c25e346bac4c489a058af14f4e9b68e
Closes: https://bugs.gentoo.org/795189
Package-Manager: Portage-3.0.19, Repoman-3.0.3
Signed-off-by: Guilherme Amadio <amadio@gentoo.org>

sys-block/thin-provisioning-tools/thin-provisioning-tools-0.9.0.ebuild
@@ -68,6 +68,6 @@ src_test() {
 }
 
 src_install() {
-	emake DESTDIR="${D}" DATADIR="${D}/usr/share" install
+	emake DESTDIR="${D}" DATADIR="${ED}/usr/share" install
 	dodoc README.md TODO.org
 }

sys-block/thin-provisioning-tools/thin-provisioning-tools-9999.ebuild
@@ -68,6 +68,6 @@ src_test() {
 }
 
 src_install() {
-	emake DESTDIR="${D}" DATADIR="${D}/usr/share" install
+	emake DESTDIR="${D}" DATADIR="${ED}/usr/share" install
 	dodoc README.md TODO.org
 }