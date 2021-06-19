gentoo/gentoo

commit f498bcbd7c3fd328bd5f66f3286521b6db926f5e

eclass/findlib.eclass: add rdep on ocaml:=
Signed-off-by: Alfredo Tupone <tupone@gentoo.org>

eclass/findlib.eclass
@@ -16,6 +16,7 @@ QA_FLAGS_IGNORED='.*'
 # From this findlib version, there is proper stublibs support.
 DEPEND=">=dev-ml/findlib-1.0.4-r1"
 [[ ${FINDLIB_USE} ]] && DEPEND="${FINDLIB_USE}? ( ${DEPEND} )"
+RDEPEND="dev-lang/ocaml:=[ocamlopt?]"
 
 check_ocamlfind() {
 	if [ ! -x "${EPREFIX}"/usr/bin/ocamlfind ] ; then

commit 94105a4ed58bb5977ccbbcbe1084f04e3218e08a

findlib.eclass: define IUSE="ocamlopt"
Fixes failures such as:
```
dev-ml/camlbz2-0.7.0::gentoo (masked by: invalid: RDEPEND: USE flag 'ocamlopt'
referenced in conditional 'ocamlopt?' in atom 'dev-lang/ocaml:=[ocamlopt?]' is not in IUSE)
```
in findlib.eclass consumers lacking IUSE="ocamlopt".

Fixes: f498bcbd7c3fd328bd5f66f3286521b6db926f5e
Signed-off-by: Sam James <sam@gentoo.org>

eclass/findlib.eclass
@@ -1,5 +1,4 @@
-# Copyright 1999-2021 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
+# Copyright 1999-2021 Gentoo Author# Distributed under the terms of the GNU General Public License v2
 
 # @ECLASS: findlib.eclass
 # @MAINTAINER:
@@ -12,6 +11,9 @@
 
 # Do not complain about CFLAGS etc since ML projects do not use them.
 QA_FLAGS_IGNORED='.*'
+
+# Required to use the ocamlopt? dep in RDEPEND below
+IUSE="ocamlopt"
 
 # From this findlib version, there is proper stublibs support.
 DEPEND=">=dev-ml/findlib-1.0.4-r1"