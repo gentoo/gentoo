gentoo/gentoo

commit c7c1d593a8afec705fde43123856ae1370ff37eb

sci-physics/geant: version bump to 4.11.0_beta1
Package-Manager: Portage-3.0.20, Repoman-3.0.3
Signed-off-by: Guilherme Amadio <amadio@gentoo.org>

sci-physics/geant/geant-4.11.0_beta1.ebuild
@@ -1,0 +1,106 @@
+# Copyright 1999-2021 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+
+inherit cmake
+
+MY_P=${PN}$(ver_cut 1-2).$(printf %02d $(ver_cut 3))
+
+case ${PV} in
+*_beta*)
+	MY_P+=.b$(printf %02d $(ver_cut 5))
+	DOCS="ReleaseNotes/Beta$(ver_cut 1-3)-*.txt"
+	;;
+*)
+	if [[ $(ver_cut 4) -gt 0 ]]; then
+		MY_P+=.p$(printf %02d $(ver_cut 4))
+		DOCS="ReleaseNotes/Patch$(ver_cut 1-3)-*.txt"
+	fi
+	HTML_DOCS="ReleaseNotes/ReleaseNotes$(ver_cut 1-3).html"
+	;;
+esac
+
+DESCRIPTION="Toolkit for simulation of passage of particles through matter"
+HOMEPAGE="https://geant4.web.cern.ch/"
+SRC_URI="https://geant4-data.web.cern.ch/geant4-data/releases/${MY_P}.tar.gz"
+
+LICENSE="geant4"
+SLOT="4"
+KEYWORDS="~amd64 ~x86 ~amd64-linux ~x86-linux"
+IUSE="+c++11 c++14 c++17 +data dawn doc examples freetype gdml geant3 hdf5
+	inventor motif opengl qt5 raytracerx static-libs threads vrml"
+
+REQUIRED_USE="
+	^^ ( c++11 c++14 c++17 )
+	inventor? ( opengl )
+	motif? ( opengl )
+	qt5? ( opengl )
+"
+
+RDEPEND="
+	dev-libs/expat
+	>=sci-physics/clhep-2.4.4.2:2=[threads?]
+	data? ( ~sci-physics/geant-data-${PV} )
+	dawn? ( media-gfx/dawn )
+	doc? ( app-doc/geant-docs )
+	gdml? ( dev-libs/xerces-c )
+	hdf5? ( sci-libs/hdf5[threads?] )
+	inventor? ( media-libs/SoXt )
+	motif? ( x11-libs/motif:0 )
+	opengl? ( virtual/opengl )
+	qt5? (
+		dev-qt/qtcore:5
+		dev-qt/qtgui:5
+		dev-qt/qtprintsupport:5
+		dev-qt/qtwidgets:5
+		opengl? ( dev-qt/qtopengl:5 )
+	)
+	raytracerx? (
+		x11-libs/libX11
+		x11-libs/libXmu
+	)"
+
+S="${WORKDIR}/${MY_P}"
+
+src_configure() {
+	local mycmakeargs=(
+		-DCMAKE_INSTALL_DATADIR="${EPREFIX}/usr/share/geant4"
+		-DGEANT4_BUILD_CXXSTD=$((usev c++11 || usev c++14 || usev c++17) | cut -c4-)
+		-DGEANT4_BUILD_MULTITHREADED=$(usex threads)
+		-DGEANT4_BUILD_STORE_TRAJECTORY=OFF
+		-DGEANT4_BUILD_TLS_MODEL=$(usex threads global-dynamic initial-exec)
+		-DGEANT4_BUILD_VERBOSE_CODE=OFF
+		-DGEANT4_INSTALL_DATA=OFF
+		-DGEANT4_INSTALL_DATADIR="${EPREFIX}/usr/share/geant4/data"
+		-DGEANT4_INSTALL_EXAMPLES=$(usex examples)
+		-DGEANT4_INSTALL_PACKAGE_CACHE=OFF
+		-DGEANT4_USE_FREETYPE=$(usex freetype)
+		-DGEANT4_USE_G3TOG4=$(usex geant3)
+		-DGEANT4_USE_GDML=$(usex gdml)
+		-DGEANT4_USE_HDF5=$(usex hdf5)
+		-DGEANT4_USE_INVENTOR=$(usex inventor)
+		-DGEANT4_USE_NETWORKDAWN=$(usex dawn)
+		-DGEANT4_USE_NETWORKVRML=$(usex vrml)
+		-DGEANT4_USE_OPENGL_X11=$(usex opengl)
+		-DGEANT4_USE_QT=$(usex qt5)
+		-DGEANT4_USE_RAYTRACER_X11=$(usex raytracerx)
+		-DGEANT4_USE_SYSTEM_CLHEP=ON
+		-DGEANT4_USE_SYSTEM_EXPAT=ON
+		-DGEANT4_USE_SYSTEM_ZLIB=ON
+		-DGEANT4_USE_XM=$(usex motif)
+		-DBUILD_STATIC_LIBS=$(usex static-libs)
+		${EXTRA_ECONF}
+	)
+	cmake_src_configure
+}
+
+src_install() {
+	# adjust clhep linking flags for system clhep
+	# binmake.gmk is only useful for legacy build systems
+	sed -i -e 's/-lG4clhep/-lCLHEP/' config/binmake.gmk || die
+	cmake_src_install
+	rm "${ED}"/usr/bin/*.{sh,csh} || die "failed to remove obsolete shell scripts"
+
+	einstalldocs
+}

sci-physics/geant/Manifest
@@ -1,3 +1,4 @@
 DIST geant4.10.07.p01.tar.gz 36217226 BLAKE2B 3bc5868db1328ac08582fef4a5246e2f3fc1b2d34bca668d27c6c272af3de230f680cff02c3a8b53ab5abc4186645935b042b456cdff3cba01bc180e0646a50d SHA512 d314d649bf349a4fc5dd09229897d3e4ca3bfba071daeb67ab4d4bde187ec59c120b2478d2b28d1c88615189bd38097666bf4a0c25ce5cb4bc3cf0d9815bea5d
 DIST geant4.10.07.p02.tar.gz 35729676 BLAKE2B 1727ab4e35f4dafe0b406bb189a008955c660922a4fc855a765a269ec8837242ba2372b346c189de6bd6d36c434e0636c483c292f90573726fa6d3b4cc67eb12 SHA512 fe5a387880218782570ff2b28cf8f6abc1457437652f349b8b81ac1045bdec7ee51fa2930451c1271708c02914ac60effd0ee8933d42c29d05819fe320356092
 DIST geant4.10.07.tar.gz 36422877 BLAKE2B d81a37e0968f3c13bb846efc252b1b31cb01a8642610b079bcf6ec1741e7f5da09d50f4abe4054b7a06433dc7d9b611110c4f9a1f971a5f1df27580d49b8906d SHA512 057ab2c6ee5d5ad7012bd889bf883faa3ef1fa72a93190d00d1320f250b7b52e21cbae552cbc35065ad5f5bd46cd91fd1536dd4d5e620afa0abb6b065bd7c86b
+DIST geant4.11.00.b01.tar.gz 35617362 BLAKE2B 654679f3876081931a80b8ee8dbf579ef45cbd8edb4e8a641ad1facc2edab470ff19f557672e901321e1993453110b4f1f5c5d7a6b0bb293e710def2d5871d12 SHA512 abc39b33d0570bc07db0e957a2e33db91b47e8ebe650bb79cb8f6fdf6f0e4938607a96011f9b0fba0294828afcf7e6f3ee20367e319f2b388f3b315bd615a6db

commit 0c5d4e2b0e549028a9c768f9680da975d3a0864b

sci-physics/geant: fix bug #799020
Closes: https://bugs.gentoo.org/799020
Fixes: c7c1d593a8afec705fde43123856ae1370ff37eb

Geant4 11.0 beta only supports C++17 and above.

Package-Manager: Portage-3.0.20, Repoman-3.0.3
Signed-off-by: Guilherme Amadio <amadio@gentoo.org>

sci-physics/geant/geant-4.11.0_beta1.ebuild
@@ -28,11 +28,11 @@ SRC_URI="https://geant4-data.web.cern.ch/geant4-data/releases/${MY_P}.tar.gz"
 LICENSE="geant4"
 SLOT="4"
 KEYWORDS="~amd64 ~x86 ~amd64-linux ~x86-linux"
-IUSE="+c++11 c++14 c++17 +data dawn doc examples freetype gdml geant3 hdf5
-	inventor motif opengl qt5 raytracerx static-libs threads vrml"
+IUSE="+c++17 c++20 +data dawn doc examples freetype gdml geant3 hdf5 inventor motif opengl
+	qt5 raytracerx static-libs threads vrml"
 
 REQUIRED_USE="
-	^^ ( c++11 c++14 c++17 )
+	^^ ( c++17 c++20 )
 	inventor? ( opengl )
 	motif? ( opengl )
 	qt5? ( opengl )
@@ -66,7 +66,7 @@ S="${WORKDIR}/${MY_P}"
 src_configure() {
 	local mycmakeargs=(
 		-DCMAKE_INSTALL_DATADIR="${EPREFIX}/usr/share/geant4"
-		-DGEANT4_BUILD_CXXSTD=$((usev c++11 || usev c++14 || usev c++17) | cut -c4-)
+		-DCMAKE_CXX_STANDARD=$((usev c++17 || usev c++20) | cut -c4-)
 		-DGEANT4_BUILD_MULTITHREADED=$(usex threads)
 		-DGEANT4_BUILD_STORE_TRAJECTORY=OFF
 		-DGEANT4_BUILD_TLS_MODEL=$(usex threads global-dynamic initial-exec)

sci-physics/geant/metadata.xml
@@ -18,6 +18,7 @@
     <flag name="c++11">Build using the C++11 standard</flag>
     <flag name="c++14">Build using the C++14 standard</flag>
     <flag name="c++17">Build using the C++17 standard</flag>
+    <flag name="c++20">Build using the C++20 standard</flag>
     <flag name="data">Add a lot of standard physics data files for geant4</flag>
     <flag name="dawn">Add support for <pkg>media-gfx/dawn</pkg> (3D postscript rendering)</flag>
     <flag name="freetype">Add support for <pkg>media-libs/freetype</pkg></flag>