gentoo/gentoo

commit 6d638c6d336ad527e9f206ca922b6b0ea9d24aa2

dev-lisp/ecls: add dev-libs/libffi subslot dep
Signed-off-by: Sam James <sam@gentoo.org>

dev-lisp/ecls/ecls-16.1.3-r1.ebuild
@@ -19,8 +19,8 @@ SLOT="0/${PV}"
 KEYWORDS="amd64 ~ppc ~sparc x86"
 IUSE="cxx debug emacs gengc precisegc cpu_flags_x86_sse +threads +unicode +libatomic X"
 
-CDEPEND="dev-libs/gmp:0
-		dev-libs/libffi
+CDEPEND="dev-libs/gmp:0=
+		dev-libs/libffi=
 		libatomic? ( dev-libs/libatomic_ops )
 		>=dev-libs/boehm-gc-7.1[threads?]
 		>=dev-lisp/asdf-2.33-r3:="

dev-lisp/ecls/ecls-20.4.24-r1.ebuild
@@ -19,8 +19,8 @@ SLOT="0/${PV}"
 KEYWORDS="~amd64 ~ppc ~sparc ~x86 ~amd64-linux"
 IUSE="cxx debug emacs gengc precisegc cpu_flags_x86_sse +threads +unicode +libatomic X"
 
-CDEPEND="dev-libs/gmp:0
-		dev-libs/libffi
+CDEPEND="dev-libs/gmp:0=
+		dev-libs/libffi:=
 		libatomic? ( dev-libs/libatomic_ops )
 		>=dev-libs/boehm-gc-7.1[threads?]
 		>=dev-lisp/asdf-2.33-r3:="
@@ -35,6 +35,15 @@ PATCHES=(
 	"${FILESDIR}/${PN}-16.1.3-headers-gentoo.patch"
 	"${FILESDIR}/${PN}-16.1.3-build.patch"
 )
+
+pkg_setup() {
+	if use gengc || use precisegc ; then
+		ewarn "You have enabled the generational garbage collector or"
+		ewarn "the precise collection routines. These features are not very stable"
+		ewarn "at the moment and may cause crashes."
+		ewarn "Don't enable them unless you know what you're doing."
+	fi
+}
 
 src_prepare() {
 	default

dev-lisp/ecls/ecls-21.2.1-r2.ebuild
@@ -17,11 +17,11 @@ SRC_URI="https://common-lisp.net/project/ecl/static/files/release/${MY_P}.tgz"
 LICENSE="BSD-2 LGPL-2.1+"
 SLOT="0/${PV}"
 KEYWORDS="~amd64 ~ppc ~sparc ~x86 ~amd64-linux"
-IUSE="cxx debug emacs gengc precisegc cpu_flags_x86_sse +threads +unicode X"
+IUSE="cxx debug emacs gengc precisegc cpu_flags_x86_sse +threads +unicode +libatomic X"
 
-CDEPEND="dev-libs/gmp:0
-		dev-libs/libffi
-		dev-libs/libatomic_ops
+CDEPEND="dev-libs/gmp:0=
+		dev-libs/libffi:=
+		libatomic? ( dev-libs/libatomic_ops )
 		>=dev-libs/boehm-gc-7.1[threads?]
 		>=dev-lisp/asdf-2.33-r3:="
 DEPEND="${CDEPEND}
@@ -47,11 +47,11 @@ src_configure() {
 		--enable-boehm=system \
 		--enable-longdouble=yes \
 		--with-dffi \
-		--enable-libatomic=system \
 		$(use_with cxx) \
 		$(use_enable gengc) \
 		$(use_enable precisegc) \
 		$(use_with debug debug-cflags) \
+		$(use_enable libatomic libatomic system) \
 		$(use_with cpu_flags_x86_sse sse) \
 		$(use_enable threads) \
 		$(use_with threads __thread) \

dev-lisp/ecls/ecls-21.2.1-r3.ebuild
@@ -17,11 +17,11 @@ SRC_URI="https://common-lisp.net/project/ecl/static/files/release/${MY_P}.tgz"
 LICENSE="BSD-2 LGPL-2.1+"
 SLOT="0/${PV}"
 KEYWORDS="~amd64 ~ppc ~sparc ~x86 ~amd64-linux"
-IUSE="cxx debug emacs gengc precisegc cpu_flags_x86_sse +threads +unicode +libatomic X"
+IUSE="cxx debug emacs gengc precisegc cpu_flags_x86_sse +threads +unicode X"
 
-CDEPEND="dev-libs/gmp:0
-		dev-libs/libffi
-		libatomic? ( dev-libs/libatomic_ops )
+CDEPEND="dev-libs/gmp:0=
+		dev-libs/libffi:=
+		dev-libs/libatomic_ops
 		>=dev-libs/boehm-gc-7.1[threads?]
 		>=dev-lisp/asdf-2.33-r3:="
 DEPEND="${CDEPEND}
@@ -36,15 +36,6 @@ PATCHES=(
 	"${FILESDIR}/${PN}-16.1.3-build.patch"
 )
 
-pkg_setup() {
-	if use gengc || use precisegc ; then
-		ewarn "You have enabled the generational garbage collector or"
-		ewarn "the precise collection routines. These features are not very stable"
-		ewarn "at the moment and may cause crashes."
-		ewarn "Don't enable them unless you know what you're doing."
-	fi
-}
-
 src_prepare() {
 	default
 	cp "${EPREFIX}"/usr/share/common-lisp/source/asdf/build/asdf.lisp contrib/asdf/ || die
@@ -56,11 +47,11 @@ src_configure() {
 		--enable-boehm=system \
 		--enable-longdouble=yes \
 		--with-dffi \
+		--enable-libatomic=system \
 		$(use_with cxx) \
 		$(use_enable gengc) \
 		$(use_enable precisegc) \
 		$(use_with debug debug-cflags) \
-		$(use_enable libatomic libatomic system) \
 		$(use_with cpu_flags_x86_sse sse) \
 		$(use_enable threads) \
 		$(use_with threads __thread) \

commit 102827c6762cffcb873a72ada74817ea2ec901a7

dev-lisp/ecls: fix libffi dep
Fixes: 6d638c6d336ad527e9f206ca922b6b0ea9d24aa2
Signed-off-by: Sam James <sam@gentoo.org>

dev-lisp/ecls/ecls-16.1.3-r1.ebuild
@@ -20,7 +20,7 @@ KEYWORDS="amd64 ~ppc ~sparc x86"
 IUSE="cxx debug emacs gengc precisegc cpu_flags_x86_sse +threads +unicode +libatomic X"
 
 CDEPEND="dev-libs/gmp:0=
-		dev-libs/libffi=
+		dev-libs/libffi:=
 		libatomic? ( dev-libs/libatomic_ops )
 		>=dev-libs/boehm-gc-7.1[threads?]
 		>=dev-lisp/asdf-2.33-r3:="