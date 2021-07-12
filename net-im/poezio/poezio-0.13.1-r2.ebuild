gentoo/gentoo

commit df39492b535fa7af6b84eddbd71a65c6fefa60bc

net-im/poezio: install in correct paths, use xdg and optfeature eclass
Also fixes test dependencies and HTML documentation (for the live
ebuild for now).

Closes: https://bugs.gentoo.org/801877
Signed-off-by: Florian Schmaus <flow@gentoo.org>

net-im/poezio/poezio-0.13.1-r2.ebuild
@@ -21,14 +21,14 @@ if [[ "${PV}" == "9999" ]]; then
 	BDEPEND="dev-python/sphinx"
 else
 	SRC_URI="mirror://pypi/${PN:0:1}/${PN}/${P}.tar.gz"
-	KEYWORDS="~amd64"
+	KEYWORDS="amd64"
 fi
 
 RDEPEND="
 	dev-python/aiodns[${PYTHON_USEDEP}]
 	dev-python/pyasn1-modules[${PYTHON_USEDEP}]
 	dev-python/pyasn1[${PYTHON_USEDEP}]
-	>=dev-python/slixmpp-1.7.1[${PYTHON_USEDEP}]
+	>=dev-python/slixmpp-1.5.0[${PYTHON_USEDEP}]
 "
 DEPEND="test? ( ${RDEPEND} )"
 

net-im/poezio/files/Do-not-install-man-pages-and-files-in-usr-share-poez.patch
@@ -1,0 +1,31 @@
+From 343c1e9be4cbf76c0ae437143b4000e36a1488ca Mon Sep 17 00:00:00 2001
+From: Florian Schmaus <flo@geekplace.eu>
+Date: Mon, 12 Jul 2021 15:08:30 +0200
+Subject: [PATCH] Do not install man pages and files in /usr/share/poezio
+
+Signed-off-by: Florian Schmaus <flo@geekplace.eu>
+---
+ setup.py | 4 ----
+ 1 file changed, 4 deletions(-)
+
+diff --git a/setup.py b/setup.py
+index d1dde4d2f02f..bee8e4834b81 100755
+--- a/setup.py
++++ b/setup.py
+@@ -156,13 +156,9 @@ setup(
+     scripts=['scripts/poezio_logs'],
+     entry_points={'console_scripts': ['poezio = poezio.__main__:run']},
+     data_files=([
+-            ('share/man/man1/', ['data/poezio.1', 'data/poezio_logs.1']),
+-            ('share/poezio/', ['README.rst', 'COPYING', 'CHANGELOG']),
+             ('share/applications/', ['data/io.poez.Poezio.desktop']),
+             ('share/metainfo/', ['data/io.poez.Poezio.appdata.xml'])
+         ]
+-        + find_doc('share/doc/poezio/source', 'source')
+-        + find_doc('share/doc/poezio/html', 'build/html')
+         + sphinx_files_found
+     ),
+     install_requires=['slixmpp>=1.6.0', 'aiodns', 'pyasn1_modules', 'pyasn1', 'typing_extensions', 'setuptools'],
+-- 
+2.31.1
+

commit 2d57d4ecf93a05d8eae349d8a5ec4a61d2b90299

net-im/poezio: destabilize 0.13.1-r2 for ~amd64
-r2 was not meant to be commited with stable keywords. :/

Fixes: df39492b535f ("net-im/poezio: install in correct paths, use xdg and optfeature eclass")
Signed-off-by: Florian Schmaus <flow@gentoo.org>

net-im/poezio/poezio-0.13.1-r2.ebuild
@@ -21,7 +21,7 @@ if [[ "${PV}" == "9999" ]]; then
 	BDEPEND="dev-python/sphinx"
 else
 	SRC_URI="mirror://pypi/${PN:0:1}/${PN}/${P}.tar.gz"
-	KEYWORDS="amd64"
+	KEYWORDS="~amd64"
 fi
 
 RDEPEND="