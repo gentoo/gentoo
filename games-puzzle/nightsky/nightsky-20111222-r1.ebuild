gentoo/gentoo

commit 4ed59a3993bfef4e0c9e7d87abdd24c85e7f585d

games-puzzle/nightsky: port to EAPI 7, games.eclass--
Signed-off-by: Sam James <sam@gentoo.org>

games-puzzle/nightsky/nightsky-20111222-r1.ebuild
@@ -1,24 +1,28 @@
-# Copyright 1999-2018 Gentoo Authors
+# Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-EAPI=5
+EAPI=7
 
-inherit eutils gnome2-utils games
+inherit desktop gnome2-utils
 
 DESCRIPTION="Puzzle game that puts you inside and ambient and mysterious universe"
 HOMEPAGE="http://www.nicalis.com/nightsky/"
 SRC_URI="nightskyhd-linux-1324519044.tar.gz"
+S="${WORKDIR}"/NightSky
 
 LICENSE="all-rights-reserved"
 SLOT="0"
 KEYWORDS="-* ~amd64 ~x86"
 IUSE="bundled-libs"
+
 RESTRICT="bindist fetch splitdebug"
 
-MYGAMEDIR=${GAMES_PREFIX_OPT}/${PN}
-QA_PREBUILT="${MYGAMEDIR#/}/NightSky*
+MYGAMEDIR=opt/${PN}
+QA_PREBUILT="
+	${MYGAMEDIR#/}/NightSky*
 	${MYGAMEDIR#/}/lib/*
-	${MYGAMEDIR#/}/lib64/*"
+	${MYGAMEDIR#/}/lib64/*
+"
 
 RDEPEND="
 	virtual/glu
@@ -36,8 +40,6 @@ RDEPEND="
 		sys-libs/zlib
 	)"
 
-S=${WORKDIR}/NightSky
-
 pkg_nofetch() {
 	einfo "Please buy & download ${SRC_URI} from:"
 	einfo "  ${HOMEPAGE}"
@@ -45,12 +47,12 @@ pkg_nofetch() {
 }
 
 src_prepare() {
-	einfo "removing ${ARCH} unrelated files..."
+	einfo "Removing ${ARCH} unrelated files..."
 	rm -v NightSkyHD$(usex amd64 "" "_64") || die
 	rm -rv lib$(usex amd64 "" "64") || die
 
 	if ! use bundled-libs ; then
-		einfo "removing bundled libs..."
+		einfo "Removing bundled libs..."
 		rm -rv lib* || die
 	fi
 
@@ -58,12 +60,12 @@ src_prepare() {
 	rm -r Settings || die
 
 	sed \
-		-e "s#@GAMES_PREFIX_OPT@#${GAMES_PREFIX_OPT}#" \
+		-e "s#@GAMES_PREFIX_OPT@#/opt#" \
 		"${FILESDIR}"/${PN}-wrapper > "${T}"/${PN} || die
 }
 
 src_install() {
-	dogamesbin "${T}"/${PN}
+	dobin "${T}"/${PN}
 
 	insinto "${MYGAMEDIR}"
 	doins -r *
@@ -72,16 +74,13 @@ src_install() {
 	make_desktop_entry ${PN}
 
 	fperms +x "${MYGAMEDIR}"/NightSkyHD$(usex amd64 "_64" "")
-	prepgamesdirs
 }
 
 pkg_preinst() {
-	games_pkg_preinst
 	gnome2_icon_savelist
 }
 
 pkg_postinst() {
-	games_pkg_postinst
 	gnome2_icon_cache_update
 
 	echo

commit b26d60e8afaee37f6768a40e80960facda8dda08

games-puzzle/nightsky: EAPI 7->8, hopefully fix install
Like others it is known broken given fperms will not take
arguments without a leading /

Somewhat minimal changes given do not have the distfile to test.

Fixes: 4ed59a3993bfef4e0c9e7d87abdd24c85e7f585d
Signed-off-by: Ionen Wolkens <ionen@gentoo.org>

games-puzzle/nightsky/nightsky-20111222-r1.ebuild
@@ -1,28 +1,25 @@
 # Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-EAPI=7
+EAPI=8
 
-inherit desktop gnome2-utils
+inherit desktop
 
 DESCRIPTION="Puzzle game that puts you inside and ambient and mysterious universe"
 HOMEPAGE="http://www.nicalis.com/nightsky/"
 SRC_URI="nightskyhd-linux-1324519044.tar.gz"
-S="${WORKDIR}"/NightSky
+S="${WORKDIR}/NightSky"
 
 LICENSE="all-rights-reserved"
 SLOT="0"
 KEYWORDS="-* ~amd64 ~x86"
 IUSE="bundled-libs"
+RESTRICT="bindist fetch"
 
-RESTRICT="bindist fetch splitdebug"
-
-MYGAMEDIR=opt/${PN}
 QA_PREBUILT="
-	${MYGAMEDIR#/}/NightSky*
-	${MYGAMEDIR#/}/lib/*
-	${MYGAMEDIR#/}/lib64/*
-"
+	opt/${PN}/NightSky*
+	opt/${PN}/lib/*
+	opt/${PN}/lib64/*"
 
 RDEPEND="
 	virtual/glu
@@ -50,8 +47,8 @@ src_prepare() {
 	default
 
 	einfo "Removing ${ARCH} unrelated files..."
-	rm -v NightSkyHD$(usex amd64 "" "_64") || die
-	rm -rv lib$(usex amd64 "" "64") || die
+	rm -v NightSkyHD$(usev !amd64 _64) || die
+	rm -rv lib$(usev !amd64 64) || die
 
 	if ! use bundled-libs ; then
 		einfo "Removing bundled libs..."
@@ -61,35 +58,24 @@ src_prepare() {
 	# empty dir, we create symlink here later
 	rm -r Settings || die
 
-	sed \
-		-e "s#@GAMES_PREFIX_OPT@#/opt#" \
+	sed "s|@GAMES_PREFIX_OPT@|${EPREFIX}/opt|" \
 		"${FILESDIR}"/${PN}-wrapper > "${T}"/${PN} || die
 }
 
 src_install() {
 	dobin "${T}"/${PN}
 
-	insinto "${MYGAMEDIR}"
-	doins -r *
+	insinto /opt/${PN}
+	doins -r .
 
-	newicon -s 128 "World/The Void/Physical"/Circle72.png ${PN}.png
-	make_desktop_entry ${PN}
+	newicon "World/The Void/Physical"/Circle72.png ${PN}.png
+	make_desktop_entry ${PN} "NightSky HD"
 
-	fperms +x "${MYGAMEDIR}"/NightSkyHD$(usex amd64 "_64" "")
-}
-
-pkg_preinst() {
-	gnome2_icon_savelist
+	fperms +x /opt/${PN}/NightSkyHD$(usev amd64 _64)
 }
 
 pkg_postinst() {
-	gnome2_icon_cache_update
-
-	echo
-	elog "Saves and Settings are in ~/.nightsky/Settings"
-	echo
-}
-
-pkg_postrm() {
-	gnome2_icon_cache_update
+	if [[ -z ${REPLACING_VERSIONS} ]]; then
+		elog "Saves and Settings are in ~/.nightsky/Settings"
+	fi
 }