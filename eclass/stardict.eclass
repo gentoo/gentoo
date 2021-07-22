gentoo/gentoo

commit 0c35580f8e9a728965508cd9c0824e2b54c02df5

stardict.eclass: Rename IUSE=gzip->zlib
The flag actually controls a dependency on app-arch/gzip, but you
already have that too. Name the flag IUSE=zlib so it'll be enabled by
default.

Signed-off-by: Matt Turner <mattst88@gentoo.org>
Closes: https://github.com/gentoo/gentoo/pull/21558
Signed-off-by: Matt Turner <mattst88@gentoo.org>

eclass/stardict.eclass
@@ -39,7 +39,7 @@ S="${WORKDIR}"/${DICT_P}
 
 LICENSE="GPL-2"
 SLOT="0"
-IUSE="gzip"
+IUSE="+zlib"
 
 DEPEND="
 	|| (
@@ -47,7 +47,7 @@ DEPEND="
 		app-text/sdcv
 		app-text/goldendict
 	)
-	gzip? (
+	zlib? (
 		app-arch/gzip
 		app-text/dictd
 	)"

commit a487c1be1bd85208acb16e2ad80d8f07595dcade

stardict.eclass: fix remaining USE=gzip reference
Fixes: 0c35580f8e9a728965508cd9c0824e2b54c02df5
Closes: https://bugs.gentoo.org/803425
Signed-off-by: Sam James <sam@gentoo.org>

eclass/stardict.eclass
@@ -53,7 +53,7 @@ DEPEND="
 	)"
 
 stardict_src_compile() {
-	if use gzip; then
+	if use zlib; then
 		for file in *.idx; do
 			[[ -f $file ]] && gzip ${file}
 		done