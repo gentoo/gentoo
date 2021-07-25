gentoo/gentoo

commit c010009af59f0a3453c3d6249f0eb56e2b113fb6

dev-libs/libverto: add || die on find
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/libverto/libverto-0.3.2.ebuild
@@ -44,5 +44,8 @@ multilib_src_configure() {
 
 multilib_src_install_all() {
 	default
-	use static-libs || find "${ED}" -name '*.la' -delete
+
+	if ! use static-libs ; then
+		find "${ED}" -name '*.la' -delete
+	fi
 }

commit 0e7a51e6ceb9b503f6cdf8e817ffb8619483ad27

dev-libs/libverto: add missing || die
Fixes: c010009af59f0a3453c3d6249f0eb56e2b113fb6
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/libverto/libverto-0.3.2.ebuild
@@ -45,5 +45,5 @@ multilib_src_configure() {
 multilib_src_install_all() {
 	default
 
-	find "${ED}" -name '*.la' -delete
+	find "${ED}" -name '*.la' -delete || die
 }