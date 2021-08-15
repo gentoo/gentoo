gentoo/gentoo

commit 0d6f1fbaa1198c04f6efb5bb8be505b4394c3541

autotools.eclass: add EAPI 8 support
Signed-off-by: Sam James <sam@gentoo.org>
Closes: https://github.com/gentoo/gentoo/pull/21584
Signed-off-by: Lars Wendler <polynomial-c@gentoo.org>

eclass/autotools.eclass
@@ -4,7 +4,7 @@
 # @ECLASS: autotools.eclass
 # @MAINTAINER:
 # base-system@gentoo.org
-# @SUPPORTED_EAPIS: 5 6 7
+# @SUPPORTED_EAPIS: 5 6 7 8
 # @BLURB: Regenerates auto* build scripts
 # @DESCRIPTION:
 # This eclass is for safely handling autotooled software packages that need to
@@ -31,7 +31,7 @@ case ${EAPI} in
 		# Needed for eqawarn
 		inherit eutils
 		;;
-	7) ;;
+	7|8) ;;
 	*) die "${ECLASS}: EAPI ${EAPI} not supported" ;;
 esac
 

commit 30cc49585b9a980245f2607ea13272e4304062af

autotools.eclass: fix EAPI 8 support
Fixes: 0d6f1fbaa1198c04f6efb5bb8be505b4394c3541
Reported-by: Joonas Niilola <juippis@gentoo.org>
Signed-off-by: Sam James <sam@gentoo.org>

eclass/autotools.eclass
@@ -131,7 +131,7 @@ RDEPEND=""
 if [[ ${AUTOTOOLS_AUTO_DEPEND} != "no" ]] ; then
 	case ${EAPI} in
 		5|6) DEPEND=${AUTOTOOLS_DEPEND} ;;
-		7) BDEPEND=${AUTOTOOLS_DEPEND} ;;
+		*) BDEPEND=${AUTOTOOLS_DEPEND} ;;
 	esac
 fi
 __AUTOTOOLS_AUTO_DEPEND=${AUTOTOOLS_AUTO_DEPEND} # See top of eclass