gentoo/gentoo

commit 9ed5fb34a77d5d488a7252f4041edaeff7da26a8

games-emulation/gnuboy: fix implicit declarations
Signed-off-by: Sam James <sam@gentoo.org>

games-emulation/gnuboy/gnuboy-1.0.3-r2.ebuild
@@ -27,6 +27,7 @@ PATCHES=(
 	"${FILESDIR}"/${P}-exec-stack.patch
 	"${FILESDIR}"/${P}-linux-headers.patch
 	"${FILESDIR}"/${P}-include.patch
+	"${FILESDIR}"/${P}-fix-implicit-decl-sprintf.patch
 )
 
 src_prepare() {

commit a833125f7240f9764fcca9da36bbf4dc5cdcb2c0

games-emulation/gnuboy: add missing patch
Fixes: 9ed5fb34a77d5d488a7252f4041edaeff7da26a8
Signed-off-by: Sam James <sam@gentoo.org>

games-emulation/gnuboy/files/gnuboy-1.0.3-fix-implicit-decl-sprintf.patch
@@ -1,0 +1,25 @@
+From 2c881f1ec456bab7deb4163a2f96814b479a81d7 Mon Sep 17 00:00:00 2001
+From: Sam James <sam@gentoo.org>
+Date: Thu, 19 Aug 2021 01:19:30 +0100
+Subject: [PATCH] Fix implicit declaration of sprintf
+
+--- a/exports.c
++++ b/exports.c
+@@ -1,6 +1,7 @@
+ 
+ 
+ #include <stdlib.h>
++#include <stdio.h>
+ 
+ #include "rc.h"
+ 
+--- a/sys/linux/fbdev.c
++++ b/sys/linux/fbdev.c
+@@ -9,6 +9,7 @@
+ 
+ 
+ #include <stdlib.h>
++#include <stdio.h>
+ #include <string.h>
+ #include <unistd.h>
+ #include <sys/mman.h>

games-emulation/gnuboy/gnuboy-1.0.3-r2.ebuild
@@ -12,7 +12,7 @@ SRC_URI="mirror://gentoo/${P}.tar.gz"
 LICENSE="GPL-2"
 SLOT="0"
 KEYWORDS="~amd64 ~x86"
-IUSE="X sdl"
+IUSE="X +sdl"
 REQUIRED_USE="!X? ( sdl )"
 
 RDEPEND="