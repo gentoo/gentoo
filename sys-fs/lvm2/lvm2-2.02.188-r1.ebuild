gentoo/gentoo

commit 6dbacd04e9ca6e8288089f5d566eb29e1c672799

sys-fs/udev: depend on static libudev when also building static binaries
Bug: https://bugs.gentoo.org/811438
Closes: https://github.com/gentoo/gentoo/pull/22206
Signed-off-by: Rolf Eike Beer <eike@sf-mail.de>
Signed-off-by: Mike Gilbert <floppym@gentoo.org>

sys-fs/lvm2/lvm2-2.02.188-r1.ebuild
@@ -41,6 +41,7 @@ DEPEND="${DEPEND_COMMON}
 	static? (
 		selinux? ( sys-libs/libselinux[static-libs] )
 		>=sys-apps/util-linux-2.16[static-libs]
+		udev? ( >=virtual/libudev-208:=[static-libs] )"
 	)"
 BDEPEND="
 	sys-devel/autoconf-archive

sys-fs/lvm2/lvm2-2.03.13-r1.ebuild
@@ -41,6 +41,7 @@ DEPEND="${DEPEND_COMMON}
 	static? (
 		selinux? ( sys-libs/libselinux[static-libs] )
 		>=sys-apps/util-linux-2.16[static-libs]
+		udev? ( >=virtual/libudev-208:=[static-libs] )"
 	)"
 BDEPEND="
 	sys-devel/autoconf-archive

sys-fs/lvm2/lvm2-2.02.187-r3.ebuild
@@ -41,6 +41,7 @@ DEPEND="${DEPEND_COMMON}
 	static? (
 		selinux? ( sys-libs/libselinux[static-libs] )
 		>=sys-apps/util-linux-2.16[static-libs]
+		udev? ( >=virtual/libudev-208:=[static-libs] )
 	)"
 BDEPEND="
 	sys-devel/autoconf-archive

commit bee913293f7dc9492a247e10fdb8374e7e6cdd95

sys-fs/lvm2: fix syntax error
Bug: https://bugs.gentoo.org/811438
Fixes: 6dbacd04e9ca6e8288089f5d566eb29e1c672799
Signed-off-by: Sam James <sam@gentoo.org>

sys-fs/lvm2/lvm2-2.02.188-r1.ebuild
@@ -41,7 +41,7 @@ DEPEND="${DEPEND_COMMON}
 	static? (
 		selinux? ( sys-libs/libselinux[static-libs] )
 		>=sys-apps/util-linux-2.16[static-libs]
-		udev? ( >=virtual/libudev-208:=[static-libs] )"
+		udev? ( >=virtual/libudev-208:=[static-libs] )
 	)"
 BDEPEND="
 	sys-devel/autoconf-archive

sys-fs/lvm2/lvm2-2.03.13-r1.ebuild
@@ -41,7 +41,7 @@ DEPEND="${DEPEND_COMMON}
 	static? (
 		selinux? ( sys-libs/libselinux[static-libs] )
 		>=sys-apps/util-linux-2.16[static-libs]
-		udev? ( >=virtual/libudev-208:=[static-libs] )"
+		udev? ( >=virtual/libudev-208:=[static-libs] )
 	)"
 BDEPEND="
 	sys-devel/autoconf-archive