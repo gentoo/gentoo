gentoo/gentoo

commit ff3c1baa23b2f4513e297407b3bce19682ffe830

dev-util/bats: respect "makeopts_jobs" in test suite
The fact that the testsuite might run a random number of jobs if
"sys-process/parallel" is installed was raised in a bug. While this
commit does not solve the bug in question, it makes the test respect
MAKEOPTS"-jX".

Bug: https://bugs.gentoo.org/734358
Signed-off-by: Henning Schild <henning@hennsch.de>
Closes: https://github.com/gentoo/gentoo/pull/21883
Signed-off-by: Joonas Niilola <juippis@gentoo.org>

dev-util/bats/bats-1.4.1.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=7
 
-inherit optfeature
+inherit multiprocessing optfeature
 
 MY_PN="bats-core"
 DESCRIPTION="Bats-core: Bash Automated Testing System"
@@ -20,7 +20,11 @@ RDEPEND="${DEPEND}"
 S="${WORKDIR}/${MY_PN}-${PV}"
 
 src_test() {
-	bin/bats --tap test || die "Tests failed"
+	local my_jobs=$(makeopts_jobs)
+	if ! command -v parallel; then
+		my_jobs=1
+	fi
+	bin/bats --tap --jobs "$my_jobs" test || die "Tests failed"
 }
 
 src_install() {

commit 2541ac27b4361444eda274ebc1a3895513f22a9b

dev-util/bats: silence "command -v" output
We just want to know whether it is there but do not care about the
output in our logs.

Fixes: ff3c1baa23b2 ("dev-util/bats: respect "makeopts_jobs" in test suite")
Signed-off-by: Henning Schild <henning@hennsch.de>

dev-util/bats/bats-1.4.1.ebuild
@@ -27,7 +27,7 @@ src_prepare() {
 
 src_test() {
 	local my_jobs=$(makeopts_jobs)
-	if ! command -v parallel; then
+	if ! command -v parallel >/dev/null; then
 		my_jobs=1
 	fi
 	bin/bats --tap --jobs "${my_jobs}" test || die "Tests failed"