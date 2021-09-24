gentoo/gentoo

commit df13bd514e0d04eb52593e0b43c40c422ce09ad4

net-misc/socat: sync live
Package-Manager: Portage-3.0.12, Repoman-3.0.2
Signed-off-by: Sam James <sam@gentoo.org>

net-misc/socat/socat-9999.ebuild
@@ -1,8 +1,9 @@
-# Copyright 1999-2020 Gentoo Authors
+# Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
-inherit autotools flag-o-matic git-r3 toolchain-funcs
+
+inherit autotools flag-o-matic toolchain-funcs
 
 DESCRIPTION="Multipurpose relay (SOcket CAT)"
 HOMEPAGE="http://www.dest-unreach.org/socat/"
@@ -10,9 +11,9 @@ EGIT_REPO_URI="https://repo.or.cz/${PN}.git"
 
 LICENSE="GPL-2"
 SLOT="0"
-KEYWORDS=""
-IUSE="libressl ssl readline ipv6 tcpd"
+IUSE="bindist libressl ssl readline ipv6 tcpd"
 
+BDEPEND="app-text/yodl"
 DEPEND="
 	ssl? (
 		!libressl? ( dev-libs/openssl:0= )
@@ -21,24 +22,16 @@ DEPEND="
 	readline? ( sys-libs/readline:= )
 	tcpd? ( sys-apps/tcp-wrappers )
 "
-BDEPEND="
-	app-text/yodl
-"
-DEPEND="
-	${RDEPEND}
-"
+RDEPEND="${DEPEND}"
+
+# Tests are a large bash script
+# Hard to disable individual tests needing network or privileges
 RESTRICT="
 	test
 	ssl? ( readline? ( bindist ) )
 "
-DOCS=(
-	BUGREPORTS CHANGES DEVELOPMENT EXAMPLES FAQ FILES PORTING README SECURITY
-)
 
-PATCHES=(
-	"${FILESDIR}"/${PN}-1.7.3.4-fno-common.patch
-	"${FILESDIR}"/${PN}-9999-stddef_h.patch
-)
+DOCS=( BUGREPORTS CHANGES DEVELOPMENT EXAMPLES FAQ FILES PORTING README SECURITY )
 
 pkg_setup() {
 	# bug #587740
@@ -51,13 +44,13 @@ pkg_setup() {
 
 src_prepare() {
 	default
-
 	eautoreconf
 }
 
 src_configure() {
 	filter-flags '-Wno-error*' #293324
 	tc-export AR
+
 	econf \
 		$(use_enable ssl openssl) \
 		$(use_enable readline) \

net-misc/socat/files/socat-9999-stddef_h.patch
@@ -1,38 +1,0 @@
-When disabling OpenSSL support, nestlex.c still needs stddef.h which is
-magically included through the OpenSSL headers otherwise.
-
---- a/configure.ac
-+++ b/configure.ac
-@@ -61,7 +61,7 @@
- AC_CHECK_HEADERS(inttypes.h)
- AC_HEADER_SYS_WAIT
- AC_CHECK_HEADERS(fcntl.h limits.h strings.h sys/param.h sys/ioctl.h sys/time.h syslog.h unistd.h)
--AC_CHECK_HEADERS(pwd.h grp.h stdint.h sys/types.h poll.h sys/poll.h sys/socket.h sys/uio.h sys/stat.h netdb.h sys/un.h)
-+AC_CHECK_HEADERS(pwd.h grp.h stddef.h stdint.h sys/types.h poll.h sys/poll.h sys/socket.h sys/uio.h sys/stat.h netdb.h sys/un.h)
- AC_CHECK_HEADERS(pty.h)
- AC_CHECK_HEADERS(netinet/in.h netinet/in_systm.h)
- AC_CHECK_HEADERS(netinet/ip.h, [], [], [AC_INCLUDES_DEFAULT
---- a/config.h.in
-+++ b/config.h.in
-@@ -189,6 +189,9 @@
- /* Define if you have the <grp.h> header file.  */
- #undef HAVE_GRP_H
- 
-+/* Define if you have the <stddef.h> header file.  */
-+#undef HAVE_STDDEF_H
-+
- /* Define if you have the <stdint.h> header file.  */
- #undef HAVE_STDINT_H
- 
---- a/sysincludes.h
-+++ b/sysincludes.h
-@@ -51,6 +51,9 @@
- #if HAVE_SYS_TIME_H
- #include <sys/time.h>	/* select(); OpenBSD: struct timespec */
- #endif
-+#if HAVE_STDDEF_H
-+#include <stddef.h>
-+#endif
- #if HAVE_STDINT_H
- #include <stdint.h>	/* uint8_t */
- #endif

commit a6879406ac48246a96799e710176fcd8f0d3bca4

net-misc/socat: drop (broken) 9999
Apparently I broke it (when syncing with release ebuilds)
9 months ago and nobody has noticed. Let's get rid of it.

Fixes: df13bd514e0d04eb52593e0b43c40c422ce09ad4
Signed-off-by: Sam James <sam@gentoo.org>

net-misc/socat/socat-9999.ebuild
@@ -1,64 +1,0 @@
-# Copyright 1999-2021 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=7
-
-inherit autotools flag-o-matic toolchain-funcs
-
-DESCRIPTION="Multipurpose relay (SOcket CAT)"
-HOMEPAGE="http://www.dest-unreach.org/socat/ https://repo.or.cz/socat.git"
-EGIT_REPO_URI="https://repo.or.cz/${PN}.git"
-
-LICENSE="GPL-2"
-SLOT="0"
-IUSE="bindist ipv6 readline ssl tcpd"
-
-BDEPEND="app-text/yodl"
-DEPEND="
-	ssl? ( dev-libs/openssl:0= )
-	readline? ( sys-libs/readline:= )
-	tcpd? ( sys-apps/tcp-wrappers )
-"
-RDEPEND="${DEPEND}"
-
-# Tests are a large bash script
-# Hard to disable individual tests needing network or privileges
-RESTRICT="
-	test
-	ssl? ( readline? ( bindist ) )
-"
-
-DOCS=( BUGREPORTS CHANGES DEVELOPMENT EXAMPLES FAQ FILES PORTING README SECURITY )
-
-pkg_setup() {
-	# bug #587740
-	if use readline && use ssl ; then
-		elog "You are enabling both readline and openssl USE flags, the licenses"
-		elog "for these packages conflict. You may not be able to legally"
-		elog "redistribute the resulting binary."
-	fi
-}
-
-src_prepare() {
-	default
-	eautoreconf
-}
-
-src_configure() {
-	# bug #293324
-	filter-flags '-Wno-error*'
-	tc-export AR
-
-	econf \
-		$(use_enable ssl openssl) \
-		$(use_enable readline) \
-		$(use_enable ipv6 ip6) \
-		$(use_enable tcpd libwrap)
-}
-
-src_install() {
-	default
-
-	docinto html
-	dodoc doc/*.html doc/*.css
-}