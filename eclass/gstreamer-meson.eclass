gentoo/gentoo

commit 0f014c97162ec8ee23f1355f00bf488c9178e6b9

gstreamer-meson.eclass: Set @PROVIDES
Signed-off-by: Michał Górny <mgorny@gentoo.org>

eclass/gstreamer-meson.eclass
@@ -14,6 +14,7 @@
 # Steven Newbury
 # @SUPPORTED_EAPIS: 7
 # @BLURB: Helps building core & split gstreamer plugins
+# @PROVIDES: meson multilib-minimal
 # @DESCRIPTION:
 # Eclass to make external gst-plugins emergable on a per-plugin basis
 # and to solve the problem with gst-plugins generating far too much

commit 20a377ce9c27ac9c5edbcb5f7ba2e4ecdbb87395

gstreamer-meson.eclass: Fix eclass documentation
Fixes: 0f014c97162ec8ee23f1355f00bf488c9178e6b9
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

eclass/gstreamer-meson.eclass
@@ -13,8 +13,8 @@
 # zaheerm <zaheerm@gentoo.org>
 # Steven Newbury
 # @SUPPORTED_EAPIS: 7
-# @BLURB: Helps building core & split gstreamer plugins
 # @PROVIDES: meson multilib-minimal
+# @BLURB: Helps building core & split gstreamer plugins
 # @DESCRIPTION:
 # Eclass to make external gst-plugins emergable on a per-plugin basis
 # and to solve the problem with gst-plugins generating far too much