gentoo/gentoo

commit 01f46d826bd65c3ee490ffa3d67782e1e2385c50

net-libs/neon: 0.32.1 version bump
Closes: https://bugs.gentoo.org/814041
Package-Manager: Portage-3.0.23, Repoman-3.0.3
Signed-off-by: Craig Andrews <candrews@gentoo.org>
Closes: https://github.com/gentoo/gentoo/pull/22340
Signed-off-by: Sam James <sam@gentoo.org>

net-libs/neon/neon-0.32.1.ebuild
@@ -1,0 +1,105 @@
+# Copyright 2001-2021 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+
+inherit autotools libtool multilib-minimal
+
+DESCRIPTION="HTTP and WebDAV client library"
+HOMEPAGE="https://notroj.github.io/neon/"
+SRC_URI="https://notroj.github.io/neon/${P}.tar.gz"
+
+LICENSE="GPL-2"
+SLOT="0/27"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~sparc-solaris ~sparc64-solaris ~x64-solaris ~x86-solaris"
+IUSE="doc expat gnutls kerberos libproxy nls pkcs11 ssl static-libs zlib"
+RESTRICT="test"
+
+RDEPEND="expat? ( dev-libs/expat:0=[${MULTILIB_USEDEP}] )
+	!expat? ( dev-libs/libxml2:2=[${MULTILIB_USEDEP}] )
+	kerberos? ( virtual/krb5:0=[${MULTILIB_USEDEP}] )
+	libproxy? ( net-libs/libproxy:0=[${MULTILIB_USEDEP}] )
+	nls? ( virtual/libintl:0=[${MULTILIB_USEDEP}] )
+	ssl? (
+		gnutls? (
+			app-misc/ca-certificates
+			net-libs/gnutls:0=[${MULTILIB_USEDEP}]
+		)
+		!gnutls? (
+			dev-libs/openssl:0=[${MULTILIB_USEDEP}]
+		)
+		pkcs11? ( dev-libs/pakchois:0=[${MULTILIB_USEDEP}] )
+	)
+	zlib? ( sys-libs/zlib:0=[${MULTILIB_USEDEP}] )"
+DEPEND="${RDEPEND}
+	virtual/pkgconfig"
+
+MULTILIB_CHOST_TOOLS=(
+	/usr/bin/neon-config
+)
+
+src_prepare() {
+	# Use CHOST-prefixed version of xml2-config for cross-compilation.
+	sed -e "s/AC_CHECK_PROG(XML2_CONFIG,/AC_CHECK_TOOL(XML2_CONFIG,/" -i macros/neon-xml-parser.m4 || die "sed failed"
+
+	# Fix compatibility with OpenSSL >=1.1.
+	sed -e "s/RSA_F_RSA_PRIVATE_ENCRYPT/RSA_F_RSA_OSSL_PRIVATE_ENCRYPT/" -i src/ne_pkcs11.c || die "sed failed"
+
+	eapply_user
+
+	AT_M4DIR="macros" eautoreconf
+
+	elibtoolize
+
+	multilib_copy_sources
+}
+
+multilib_src_configure() {
+	local myconf=()
+
+	if has_version sys-libs/glibc; then
+		einfo "Enabling SSL library thread-safety using POSIX threads..."
+		myconf+=(--enable-threadsafe-ssl=posix)
+	fi
+
+	if use expat; then
+		myconf+=(--with-expat)
+	else
+		myconf+=(--with-libxml2)
+	fi
+
+	if use ssl; then
+		if use gnutls; then
+			myconf+=(--with-ssl=gnutls --with-ca-bundle="${EPREFIX}/etc/ssl/certs/ca-certificates.crt")
+		else
+			myconf+=(--with-ssl=openssl)
+		fi
+	fi
+
+	econf \
+		--enable-shared \
+		$(use_with kerberos gssapi) \
+		$(use_with libproxy) \
+		$(use_enable nls) \
+		$(use_with pkcs11 pakchois) \
+		$(use_enable static-libs static) \
+		$(use_with zlib) \
+		"${myconf[@]}"
+}
+
+multilib_src_install() {
+	emake DESTDIR="${D}" install-{config,headers,lib,man,nls}
+
+	if multilib_is_native_abi && use doc; then
+		(
+			docinto html
+			dodoc -r doc/html/*
+		)
+	fi
+}
+
+multilib_src_install_all() {
+	find "${D}" -name "*.la" -type f -delete || die
+
+	dodoc AUTHORS BUGS NEWS README.md THANKS TODO
+}

net-libs/neon/Manifest
@@ -2,3 +2,4 @@ DIST neon-0.30.2.tar.gz 932779 BLAKE2B 0536e4f1c8634ede9a1806180c315772a65b9cb1e
 DIST neon-0.31.0.tar.gz 867476 BLAKE2B 84981d0fcca97bfa495f9e8cbf060a06e04c931ce3b5eca41ff1d76a47fd863139b8bf3e102c5e4e8d22d5cffcebc64cee9b8f746f8d6669f0cbccf124fed9cc SHA512 62c55e16609d657ac56bb4a2fd32a10ffa72d5694aa1369be533491a6683034ed42843d5f19ed06cf8c1b7b8ba11b74f9839cdfeef4afc3b2d7b11372ed85cd2
 DIST neon-0.31.1.tar.gz 867810 BLAKE2B 7b7d0c8ab635446c98f3db7072f1dabbb74ad9f579086f209bb3af5460363520be38886a299d22dc0f27a091f322c6d5972a00d4e6c612c710dca12e7620f850 SHA512 00d7297c102ee828d84eb28c9b6a57e66290904acf5327f8a6d3d1759ffacf88329bb2157dcef418c69e93d1370ccbd591829da06c01c3f44aea6c7b92462c13
 DIST neon-0.31.2.tar.gz 867914 BLAKE2B 8ae1301dd5f9ac1a6b518f99919a1b7e7b7c6499413b523ff82e00d364d6d9c5ad96875795330e2fd9c42a42b073e3eea51b6cc0ee9e9a21850763f0cdd0b97e SHA512 1e402b40a0445f68ed24d2697ee60d21636f61ebc98edcde37ff9e26c54430acabf3969ac22a942d1dd51bddee0f312c04073b423b0af3a3e7c9bf60cd53e48c
+DIST neon-0.32.1.tar.gz 880434 BLAKE2B 91b942cb0d7184f4da26d7d45e846a0f80aa834226e5ff0689df75650da79cbfad0854c718dbffdd31cc0f09f25458ea37ca39bb38071643a6a609ef6102b896 SHA512 a231865c3aad3668f6effded97a58eb981655f64c71b0954fcb2e667bdec50895d11bf296f4f454bd63e6ce9b02c2fd13cbb17cb82631e16f100000ecd7e1977

commit 99439e7c2f92d7add59d544dc110a11e599c5aae

net-libs/neon: fix missing build dependencies
Fixes: 01f46d826bd65c3ee490ffa3d67782e1e2385c50
Closes: https://bugs.gentoo.org/815049
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

net-libs/neon/neon-0.32.1.ebuild
@@ -31,8 +31,12 @@ RDEPEND="expat? ( dev-libs/expat:0=[${MULTILIB_USEDEP}] )
 		pkcs11? ( dev-libs/pakchois:0=[${MULTILIB_USEDEP}] )
 	)
 	zlib? ( sys-libs/zlib:0=[${MULTILIB_USEDEP}] )"
-DEPEND="${RDEPEND}
-	virtual/pkgconfig"
+DEPEND="${RDEPEND}"
+BDEPEND="
+	app-text/docbook-xml-dtd:4.5
+	app-text/xmlto
+	virtual/pkgconfig
+"
 
 MULTILIB_CHOST_TOOLS=(
 	/usr/bin/neon-config