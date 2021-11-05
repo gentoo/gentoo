gentoo/gentoo

commit ee017a055b8f6d3d4ea04ae0342320a4892cdbd6

app-admin/gamin: Port to EAPI 8
Closes: https://bugs.gentoo.org/819702
Signed-off-by: Jakov Smolić <jsmolic@gentoo.org>
Closes: https://github.com/gentoo/gentoo/pull/22761
Signed-off-by: Jakov Smolić <jsmolic@gentoo.org>

app-admin/gamin/gamin-0.1.10-r1.ebuild
@@ -1,21 +1,20 @@
 # Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-EAPI=5
+EAPI=8
 
-inherit multilib-build
+inherit multilib-minimal
 
 DESCRIPTION="Meta package providing the File Alteration Monitor API & Server"
 HOMEPAGE="https://www.gnome.org/~veillard/gamin/"
-SRC_URI=""
 
 LICENSE="LGPL-2"
 SLOT="0"
 KEYWORDS="~alpha amd64 arm arm64 ~hppa ~ia64 ~m68k ~mips ppc ppc64 ~riscv ~s390 sparc x86 ~amd64-linux ~x86-linux ~sparc-solaris ~x64-solaris ~x86-solaris"
-IUSE=""
 
-RDEPEND="!app-admin/fam
-	>=dev-libs/libgamin-0.1.10-r4[${MULTILIB_USEDEP}]"
-DEPEND=""
+RDEPEND="
+	!app-admin/fam
+	>=dev-libs/libgamin-0.1.10-r4[${MULTILIB_USEDEP}]
+"
 
 PDEPEND=">=app-admin/gam-server-0.1.10"

commit ed796e98ccd39b39bdec3cd5e449b47b1cbb3769

app-admin/gamin: Inherit multilib-build instead of multilib-minimal
Eclass change was staged by mistake

Fixes: ee017a055b8f6d3d4ea04ae0342320a4892cdbd6
Closes: https://bugs.gentoo.org/821964
Signed-off-by: Jakov Smolić <jsmolic@gentoo.org>

app-admin/gamin/gamin-0.1.10-r1.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-inherit multilib-minimal
+inherit multilib-build
 
 DESCRIPTION="Meta package providing the File Alteration Monitor API & Server"
 HOMEPAGE="https://www.gnome.org/~veillard/gamin/"