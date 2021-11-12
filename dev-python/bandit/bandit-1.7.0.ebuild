gentoo/gentoo

commit 20c7c7acd861c0a462c611702587993a004faf14

dev-python/bandit: 1.7.0 bump
This was approved by the maintainer.

Closes: https://bugs.gentoo.org/761858
Signed-off-by: William Hubbs <williamh@gentoo.org>

dev-python/bandit/bandit-1.7.0.ebuild
@@ -1,0 +1,49 @@
+# Copyright 1999-2021 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+PYTHON_COMPAT=( python3_{7..9} )
+DISTUTILS_USE_SETUPTOOLS=rdepend
+
+inherit distutils-r1
+
+DESCRIPTION="A security linter from OpenStack Security"
+HOMEPAGE="https://github.com/PyCQA/bandit"
+SRC_URI="mirror://pypi/${PN:0:1}/${PN}/${P}.tar.gz"
+
+LICENSE="Apache-2.0"
+SLOT="0"
+KEYWORDS="~amd64 ~arm ~arm64 ~s390 ~x86"
+IUSE="test"
+RESTRICT="!test? ( test )"
+
+DEPEND="
+	>=dev-python/pbr-2.0.0[${PYTHON_USEDEP}]
+	test? (
+		>=dev-python/coverage-4.5.4[${PYTHON_USEDEP}]
+		>=dev-python/fixtures-3.0.0[${PYTHON_USEDEP}]
+		>=dev-python/hacking-2.0.0[${PYTHON_USEDEP}]
+		>=dev-python/mock-3.0.5[${PYTHON_USEDEP}]
+		>=dev-python/stestr-2.5.0
+		>=dev-python/testscenarios-0.5.0[${PYTHON_USEDEP}]
+		>=dev-python/testtools-2.3.0[${PYTHON_USEDEP}]
+		>=dev-python/beautifulsoup-4.8.0[${PYTHON_USEDEP}]
+		>=dev-python/pylint-1.9.4[${PYTHON_USEDEP}]
+	)"
+RDEPEND="
+	>=dev-python/pbr-2.0.0[${PYTHON_USEDEP}]
+	>=dev-python/GitPython-1.0.1[${PYTHON_USEDEP}]
+	>=dev-python/pyyaml-5.3.1[${PYTHON_USEDEP}]
+	>=dev-python/six-1.10.0[${PYTHON_USEDEP}]
+	>=dev-python/stevedore-1.20.0[${PYTHON_USEDEP}]"
+
+python_prepare() {
+	sed -i 's/yaml.load/yaml.safe_load/g' tests/unit/formatters/test_yaml.py || die
+	distutils-r1_python_prepare_all
+}
+
+python_test() {
+	distutils_install_for_testing
+	stestr init
+	stestr run || die
+}

dev-python/bandit/Manifest
@@ -1,3 +1,4 @@
 DIST bandit-1.6.2.tar.gz 498567 BLAKE2B ab7885df634ff8258fc7620523b56995cb2f262f4290193a9063e216f66ba8ecb31ec17f09d0b72a0472923266953c46e47e74fcdfb39784873738c033e00c6c SHA512 9facce98411ceb9e33e5a978ca4aad2dab541ffe215e480806ac921b7f7067572445d8e32e8d473ef30bb57155b72b2ffd4e06d458a3da82e2a9fb1b1d8a4b9f
 DIST bandit-1.6.2_py38-1.patch 3892 BLAKE2B 750160be3d234b0d638307e74fd130921f43f460edca8d0ef0f9a98f34db709e0649b1e481668abd094942fa05ee710b82b3161fd82bc56bb0f5e73d90a6e70f SHA512 dc1e55bdcbf180e70757dd92aa590fe1348d84a2143af73e48c35c62864cf0720f6b055482eb274a4261adff37c84f998c75916b8ab70d99264bd88d09611e06
 DIST bandit-1.6.2_py38-2.patch 3724 BLAKE2B 8ce857419d99b059ce1b28aba7c04218397cad82f0fdb6e7eb3e57f698d6360de7b0f4402c40540e41d510db1aa0b75e006e0e15a20a8c5534b7e5a3cf78a0e8 SHA512 25f73b087b296867556d9b9bb7f789a4df64bd2930509086bc24312c43a3a98209d63a5b00009e31fd2653e89c85e62849c7af6ba044abc10c5789a6e3d9a968
+DIST bandit-1.7.0.tar.gz 506225 BLAKE2B 6e2383576b2091ab98757381bfd5915331fc7b08a251e817a6964d963de60b070f7c4dc7958dd366cf95333f4abc157e80433490ab8b923580cb45d855c13092 SHA512 c51af7ab4c667c376a11f142fa35881a776d420bd3ef41421bd29a713e0b56bbf1622e24cd878e24f57594f03d06b7c2b8258fcf34f57e3a68df4200a34becdc

commit 63fe55de2af709e5e360e8e638d47866fbabecbd

dev-python/bandit: fix prepare calls
Fixes: 20c7c7acd861c0a462c611702587993a004faf14
Closes: https://bugs.gentoo.org/802564
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

dev-python/bandit/bandit-1.7.0.ebuild
@@ -37,9 +37,9 @@ RDEPEND="
 	>=dev-python/six-1.10.0[${PYTHON_USEDEP}]
 	>=dev-python/stevedore-1.20.0[${PYTHON_USEDEP}]"
 
-python_prepare() {
+src_prepare() {
 	sed -i 's/yaml.load/yaml.safe_load/g' tests/unit/formatters/test_yaml.py || die
-	distutils-r1_python_prepare_all
+	distutils-r1_src_prepare
 }
 
 python_test() {