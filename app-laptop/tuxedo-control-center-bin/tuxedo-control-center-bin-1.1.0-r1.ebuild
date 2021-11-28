gentoo/gentoo

commit 703ca218b4f1e0b9e22bf12c4cd8f41fab60508c

app-laptop/tuxedo-control-center-bin: added openrc script
Closes: https://bugs.gentoo.org/747802
Signed-off-by: Marc Schiffbauer <mschiff@gentoo.org>

app-laptop/tuxedo-control-center-bin/tuxedo-control-center-bin-1.1.0-r1.ebuild
@@ -47,6 +47,8 @@ src_install() {
 
 	systemd_dounit opt/tuxedo-control-center/resources/dist/tuxedo-control-center/data/dist-data/tccd.service
 	systemd_dounit opt/tuxedo-control-center/resources/dist/tuxedo-control-center/data/dist-data/tccd-sleep.service
+
+	doinitd "${FILES}/tccd.initd"
 }
 
 pkg_config() {

app-laptop/tuxedo-control-center-bin/files/tccd.initd
@@ -1,0 +1,25 @@
+#!/sbin/openrc-run
+# Copyright 2021 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+name=$RC_SVCNAME
+description="TUXEDO Control Center Service"
+supervisor="supervise-daemon"
+command="/opt/tuxedo-control-center/resources/dist/tuxedo-control-center/data/service/tccd"
+command_args="--start"
+
+depend() {
+	need localmount
+}
+
+start(){
+	ebegin "Starting $RC_SVCNAME"
+	start-stop-daemon -b --start /opt/tuxedo-control-center/resources/dist/tuxedo-control-center/data/service/tccd -- --start
+	eend $?
+}
+
+stop() {
+	ebegin "Stopping $RC_SVCNAME"
+	/opt/tuxedo-control-center/resources/dist/tuxedo-control-center/data/service/tccd --stop
+	eend $?
+}

commit 334e359af34fb31503a6392f9757b2ce1542c909

app-laptop/tuxedo-control-center-bin: [QA] fix installation of init script
Bug: https://bugs.gentoo.org/747802
Fixes: 703ca218b4f1e0b9e22bf12c4cd8f41fab60508c
Signed-off-by: Sam James <sam@gentoo.org>

app-laptop/tuxedo-control-center-bin/tuxedo-control-center-bin-1.1.0-r1.ebuild
@@ -48,7 +48,7 @@ src_install() {
 	systemd_dounit opt/tuxedo-control-center/resources/dist/tuxedo-control-center/data/dist-data/tccd.service
 	systemd_dounit opt/tuxedo-control-center/resources/dist/tuxedo-control-center/data/dist-data/tccd-sleep.service
 
-	doinitd "${FILES}/tccd.initd"
+	doinitd "${FILESDIR}/tccd.initd"
 }
 
 pkg_config() {