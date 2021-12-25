gentoo/gentoo

commit 8f162d2e7717b30d130925a72129d56a4fbd4daa

gui-apps/mako: Add sys-libs/basu sd-bus-provider
Add sys-libs/basu as alternative sd-bus-provider besides elogind and
systemd

Bug: https://bugs.gentoo.org/783156
Package-Manager: Portage-3.0.28, Repoman-3.0.3
Signed-off-by: Michael Kupfer <mkupfer99@gmail.com>
Closes: https://github.com/gentoo/gentoo/pull/23314
Signed-off-by: Sam James <sam@gentoo.org>

gui-apps/mako/mako-9999.ebuild
@@ -27,6 +27,7 @@ DEPEND="
 	|| (
 		sys-apps/systemd
 		sys-auth/elogind
+		sys-libs/basu
 	)
 	sys-apps/dbus
 	icons? (

gui-apps/mako/mako-1.6-r2.ebuild
@@ -27,6 +27,7 @@ DEPEND="
 	|| (
 		sys-apps/systemd
 		sys-auth/elogind
+		sys-libs/basu
 	)
 	sys-apps/dbus
 	icons? (

commit 97985fb97c421b1301b70c750f90051fbcf70bae

gui-apps/mako: fix automagic basu usage
Not sure how I missed that before!

Fixes: 8f162d2e7717b30d130925a72129d56a4fbd4daa
Closes: https://bugs.gentoo.org/783156
Reported-by: Arsen Arsenovic <arsen@aarsen.me>
Signed-off-by: Sam James <sam@gentoo.org>

gui-apps/mako/mako-1.6-r3.ebuild
@@ -13,20 +13,20 @@ if [[ ${PV} == 9999 ]]; then
 	EGIT_REPO_URI="https://github.com/emersion/${PN}.git"
 else
 	SRC_URI="https://github.com/emersion/${PN}/archive/v${PV}.tar.gz -> ${P}.tar.gz"
-	KEYWORDS="amd64 arm64 x86"
+	KEYWORDS="~amd64 ~arm64 ~x86"
 fi
 
 LICENSE="MIT"
 SLOT="0"
-IUSE="+icons"
+IUSE="elogind +icons systemd"
 
 DEPEND="
 	dev-libs/wayland
 	x11-libs/pango
 	x11-libs/cairo
 	|| (
-		sys-apps/systemd
-		sys-auth/elogind
+		systemd? ( sys-apps/systemd )
+		elogind? ( sys-auth/elogind )
 		sys-libs/basu
 	)
 	sys-apps/dbus
@@ -49,5 +49,14 @@ src_configure() {
 		-Dicons=$(usex icons enabled disabled)
 		"-Dwerror=false"
 	)
+
+	if use systemd ; then
+		emesonargs+=( -Dsd-bus-provider=libsystemd )
+	elif use elogind ; then
+		emesonargs+=( -Dsd-bus-provider=libelogind )
+	else
+		emesonargs+=( -Dsd-bus-provider=basu )
+	fi
+
 	meson_src_configure
 }

gui-apps/mako/mako-9999.ebuild
@@ -18,15 +18,15 @@ fi
 
 LICENSE="MIT"
 SLOT="0"
-IUSE="+icons"
+IUSE="elogind +icons systemd"
 
 DEPEND="
 	dev-libs/wayland
 	x11-libs/pango
 	x11-libs/cairo
 	|| (
-		sys-apps/systemd
-		sys-auth/elogind
+		systemd? ( sys-apps/systemd )
+		elogind? ( sys-auth/elogind )
 		sys-libs/basu
 	)
 	sys-apps/dbus
@@ -37,7 +37,7 @@ DEPEND="
 "
 RDEPEND="
 	${DEPEND}
-	dev-libs/wayland-protocols
+	>=dev-libs/wayland-protocols-1.21
 "
 BDEPEND="
 	virtual/pkgconfig
@@ -49,5 +49,14 @@ src_configure() {
 		-Dicons=$(usex icons enabled disabled)
 		"-Dwerror=false"
 	)
+
+	if use systemd ; then
+		emesonargs+=( -Dsd-bus-provider=libsystemd )
+	elif use elogind ; then
+		emesonargs+=( -Dsd-bus-provider=libelogind )
+	else
+		emesonargs+=( -Dsd-bus-provider=basu )
+	fi
+
 	meson_src_configure
 }