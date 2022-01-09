gentoo/gentoo

commit 600e7a856320093e8da1abe5d1cb2ed8cd73da9f

app-text/trang: fix VariableScope (EROOT)
Signed-off-by: Sam James <sam@gentoo.org>

app-text/trang/trang-20151127.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2016 Gentoo Foundation
+# Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=5
@@ -47,7 +47,7 @@ src_configure() {
 	# Because this crazy package uses XSLT, we need to escape this.
 	sed -i 's:\${gentoo\.classpath}:${{gentoo.classpath}}:g' build.xsl || die
 
-	EANT_EXTRA_ARGS="-Djavacc.dir=${EROOT}usr/share/javacc/lib"
+	EANT_EXTRA_ARGS="-Djavacc.dir=${EPREFIX}usr/share/javacc/lib"
 	export LOCALCLASSPATH=$(java-pkg_getjars --build-only --with-dependencies saxon-6.5)
 }
 

commit 1f22008fcd83435f2de32bd6f138caed9f5aa8a8

app-text/trang: Add missing slash in javacc.dir path
Fixes: 600e7a856320093e8da1abe5d1cb2ed8cd73da9f
Closes: https://bugs.gentoo.org/793422
Package-Manager: Portage-3.0.30, Repoman-3.0.3
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

app-text/trang/trang-20151127.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2021 Gentoo Authors
+# Copyright 1999-2022 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=5
@@ -47,7 +47,7 @@ src_configure() {
 	# Because this crazy package uses XSLT, we need to escape this.
 	sed -i 's:\${gentoo\.classpath}:${{gentoo.classpath}}:g' build.xsl || die
 
-	EANT_EXTRA_ARGS="-Djavacc.dir=${EPREFIX}usr/share/javacc/lib"
+	EANT_EXTRA_ARGS="-Djavacc.dir=${EPREFIX}/usr/share/javacc/lib"
 	export LOCALCLASSPATH=$(java-pkg_getjars --build-only --with-dependencies saxon-6.5)
 }
 