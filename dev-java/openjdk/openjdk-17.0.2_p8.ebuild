gentoo/gentoo

commit 5e22bb7a45589eb71e5f428c83ec1d0788b7c000

dev-java/openjdk: fix logic in !system-bootstrap
Signed-off-by: Georgy Yakovlev <gyakovlev@gentoo.org>

dev-java/openjdk/openjdk-17.0.2_p8.ebuild
@@ -150,7 +150,7 @@ pkg_setup() {
 		local xpakvar="${ARCH^^}_XPAK"
 		export JDK_HOME="${WORKDIR}/openjdk-bootstrap-${!xpakvar}"
 	else
-		if [[ ${MERGE_TYPE} != "binary" ]]; then
+		if [[ ${MERGE_TYPE} == "binary" ]]; then
 			JDK_HOME=$(best_version --host-root dev-java/openjdk-bin:${SLOT})
 			[[ -n ${JDK_HOME} ]] || die "Build VM not found!"
 			JDK_HOME=${JDK_HOME#*/}

dev-java/openjdk/openjdk-11.0.14_p9.ebuild
@@ -138,7 +138,7 @@ pkg_setup() {
 			fi
 		done
 	else
-		[[ ${MERGE_TYPE} != "binary" ]] && return
+		[[ ${MERGE_TYPE} == "binary" ]] && return
 		local xpakvar="${ARCH^^}_XPAK"
 		export JDK_HOME="${WORKDIR}/openjdk-bootstrap-${!xpakvar}"
 	fi

commit 66e6e94d2da2ebb0b239a67895cc58f05a475d21

dev-java/openjdk: fix bootstrap logic err in 17
commit 5e22bb7a45589eb71e5f428c83ec1d0788b7c000 broke it.
should have been applied only for 11

Closes: https://bugs.gentoo.org/831574
Signed-off-by: Georgy Yakovlev <gyakovlev@gentoo.org>

dev-java/openjdk/openjdk-17.0.2_p8.ebuild
@@ -151,7 +151,7 @@ pkg_setup() {
 		local xpakvar="${ARCH^^}_XPAK"
 		export JDK_HOME="${WORKDIR}/openjdk-bootstrap-${!xpakvar}"
 	else
-		if [[ ${MERGE_TYPE} == "binary" ]]; then
+		if [[ ${MERGE_TYPE} != "binary" ]]; then
 			JDK_HOME=$(best_version --host-root dev-java/openjdk-bin:${SLOT})
 			[[ -n ${JDK_HOME} ]] || die "Build VM not found!"
 			JDK_HOME=${JDK_HOME#*/}