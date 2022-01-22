gentoo/gentoo

commit c4bb8ddd7188712c4c098165091bdeb3a96ac869

dev-python/vpython: add 7.6.3
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

dev-python/vpython/vpython-7.6.3.ebuild
@@ -1,0 +1,38 @@
+# Copyright 1999-2022 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+PYTHON_COMPAT=( python3_{8..10} )
+
+inherit distutils-r1
+
+DESCRIPTION="VPython for Jupyter notebook"
+HOMEPAGE="https://www.vpython.org/ https://pypi.org/project/vpython/"
+SRC_URI="https://github.com/${PN}/${PN}-jupyter/archive/refs/tags/${PV}.tar.gz -> ${P}.tar.gz"
+S="${WORKDIR}/${PN}-jupyter-${PV}"
+
+RDEPEND="
+	>=dev-python/autobahn-18.8.2[${PYTHON_USEDEP}]
+	dev-python/ipykernel[${PYTHON_USEDEP}]
+	dev-python/jupyter[${PYTHON_USEDEP}]
+	dev-python/jupyter-server-proxy[${PYTHON_USEDEP}]
+	dev-python/numpy[${PYTHON_USEDEP}]
+"
+BDEPEND="
+	dev-python/cython[${PYTHON_USEDEP}]
+	dev-python/versioneer[${PYTHON_USEDEP}]
+"
+DEPEND="
+	${RDEPEND}
+	${BDEPEND}
+"
+
+LICENSE="BSD-2"
+SLOT="0"
+KEYWORDS="~amd64"
+
+RESTRICT="test"
+PATCHES="${FILESDIR}/${P}-fix-python310-detection.patch"
+
+distutils_enable_tests pytest

dev-python/vpython/Manifest
@@ -1,2 +1,3 @@
 DIST vpython-7.6.1.tar.gz 3552831 BLAKE2B 78281ebbdae2bf09d139569cd344f32aa77aad59ce988a72c57b9984774bc376348c6e69c9119822fe1fbd5dbe6cecf81f17da3977b257c64321e4d4f2deb755 SHA512 8a06bf41865a6bb61c6616c853889b24128f32a9ce1eb8cd9ff861e4995f700ea24e7ffc04fd1edf9cd823a1a592083202abecb1373d5f544facc8646f0cf30b
 DIST vpython-7.6.2.tar.gz 4461606 BLAKE2B 67e59d5ac8e061ffa62433bcc1c3285cf659482e298b706d2b59ef8160c154c42efa8677fce19856a88024af06fb71882ff280ba2e1312a18da272168a4eb1a9 SHA512 fb8f0e9219950cab6d800a44a7fa473bd39e67895170b5cd7636a78cefd83f740d5b4da245b3f252402bfae6e576948b5831d230bf365ac7141ba6bf5f1ef6d6
+DIST vpython-7.6.3.tar.gz 4461187 BLAKE2B 4698396fb9f846db59233bb6795748cadf7a0c8c4b1969dfe439965fa29db703806197a6e91b5d91935b8fdcf5c626dac6613a73e06b88c899bdddd0b19ef69e SHA512 0270103eb5c67ecbf7461e1821e3502295976f430d290fbefc5281643802963bf6ccd729eb37b19e50b2403f7cf0dc09f869d4cd30726ccd086bacb6180f868e

commit 3f4f11976fad148b0c00db7709d7ec99698c5b28

dev-python/vpython: fix ebuild
Pushed wrong ebuild.

- Remove patch as is was merged.
- fix variable order
- remove unnneeded DEPEND variable

Fixes: c4bb8ddd7188712c4c098165091bdeb3a96ac869
Closes: https://bugs.gentoo.org/831812
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

dev-python/vpython/vpython-7.6.3.ebuild
@@ -12,6 +12,10 @@ HOMEPAGE="https://www.vpython.org/ https://pypi.org/project/vpython/"
 SRC_URI="https://github.com/${PN}/${PN}-jupyter/archive/refs/tags/${PV}.tar.gz -> ${P}.tar.gz"
 S="${WORKDIR}/${PN}-jupyter-${PV}"
 
+LICENSE="BSD-2"
+SLOT="0"
+KEYWORDS="~amd64"
+
 RDEPEND="
 	>=dev-python/autobahn-18.8.2[${PYTHON_USEDEP}]
 	dev-python/ipykernel[${PYTHON_USEDEP}]
@@ -23,16 +27,7 @@ BDEPEND="
 	dev-python/cython[${PYTHON_USEDEP}]
 	dev-python/versioneer[${PYTHON_USEDEP}]
 "
-DEPEND="
-	${RDEPEND}
-	${BDEPEND}
-"
-
-LICENSE="BSD-2"
-SLOT="0"
-KEYWORDS="~amd64"
 
 RESTRICT="test"
-PATCHES="${FILESDIR}/${P}-fix-python310-detection.patch"
 
 distutils_enable_tests pytest