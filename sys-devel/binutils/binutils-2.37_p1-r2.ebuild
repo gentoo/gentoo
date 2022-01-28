gentoo/gentoo

commit 57737c9f2a9ce3e1b2492760e98932b7b9f9eea2

sys-devel/binutils: disable PGO for cross compiles
It doesn't make sense there as we have to run the tests
(execute cross-compiled binaries).

Closes: https://bugs.gentoo.org/832174
Signed-off-by: Sam James <sam@gentoo.org>

sys-devel/binutils/binutils-2.37_p1-r2.ebuild
@@ -276,7 +276,7 @@ src_configure() {
 		$(use_enable pgo pgo-build lto)
 	)
 
-	if use pgo ; then
+	if use pgo && ! is_cross ; then
 		export BUILD_CFLAGS="${CFLAGS}"
 	fi
 

sys-devel/binutils/binutils-9999.ebuild
@@ -277,7 +277,7 @@ src_configure() {
 		$(use_enable pgo pgo-build lto)
 	)
 
-	if use pgo ; then
+	if use pgo && ! is_cross ; then
 		export BUILD_CFLAGS="${CFLAGS}"
 	fi
 

commit bc56495544b69df93fd54ea44c5f03841bae027f

sys-devel/binutils: really fix PGO for cross (disable it)
Closes: https://bugs.gentoo.org/832174
Fixes: 57737c9f2a9ce3e1b2492760e98932b7b9f9eea2
Signed-off-by: Sam James <sam@gentoo.org>

sys-devel/binutils/binutils-2.37_p1-r2.ebuild
@@ -272,9 +272,16 @@ src_configure() {
 		# Ideally we would like automagic-or-disabled here.
 		# But the check does not quite work on i686: bug #760926.
 		$(use_enable cet)
-
-		$(use_enable pgo pgo-build lto)
 	)
+
+	if ! is_cross ; then
+		myconf+=( $(use_enable pgo pgo-build lto) )
+
+		if use pgo ; then
+			export BUILD_CFLAGS="${CFLAGS}"
+		fi
+	fi
+
 
 	if use pgo && ! is_cross ; then
 		export BUILD_CFLAGS="${CFLAGS}"

sys-devel/binutils/binutils-9999.ebuild
@@ -273,9 +273,16 @@ src_configure() {
 		# Ideally we would like automagic-or-disabled here.
 		# But the check does not quite work on i686: bug #760926.
 		$(use_enable cet)
-
-		$(use_enable pgo pgo-build lto)
 	)
+
+	if ! is_cross ; then
+		myconf+=( $(use_enable pgo pgo-build lto) )
+
+		if use pgo ; then
+			export BUILD_CFLAGS="${CFLAGS}"
+		fi
+	fi
+
 
 	if use pgo && ! is_cross ; then
 		export BUILD_CFLAGS="${CFLAGS}"