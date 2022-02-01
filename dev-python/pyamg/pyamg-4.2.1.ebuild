gentoo/gentoo

commit 31f75efcb11aefe2502f36facf2f1c329b022233

dev-python/pyamg: add 4.2.1, fix tests
Closes: https://bugs.gentoo.org/829783
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

dev-python/pyamg/pyamg-4.2.1.ebuild
@@ -1,0 +1,38 @@
+# Copyright 1999-2021 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+PYTHON_COMPAT=( python3_{8..10} )
+inherit distutils-r1
+
+DESCRIPTION="Algebraic multigrid solvers in Python"
+HOMEPAGE="https://pyamg.org"
+SRC_URI="
+	https://github.com/pyamg/pyamg/archive/v${PV}.tar.gz
+		-> ${P}.gh.tar.gz
+"
+
+LICENSE="BSD"
+SLOT="0"
+KEYWORDS="~amd64 ~x86"
+
+RDEPEND="
+	dev-python/numpy[${PYTHON_USEDEP}]
+	dev-python/scipy[${PYTHON_USEDEP}]"
+BDEPEND="
+	dev-python/pybind11[${PYTHON_USEDEP}]
+	dev-python/numpy[${PYTHON_USEDEP}]
+	test? (
+		dev-python/matplotlib[${PYTHON_USEDEP}]
+	)"
+
+distutils_enable_tests pytest
+
+export SETUPTOOLS_SCM_PRETEND_VERSION=${PV}
+
+python_test() {
+	cp -r -l -n pyamg "${BUILD_DIR}/lib" || die
+	cd "${BUILD_DIR}/lib" || die
+	epytest
+}

dev-python/pyamg/Manifest
@@ -1,2 +1,3 @@
 DIST pyamg-4.1.0.tar.gz 749906 BLAKE2B 31c05d64317b32cbdbd616e982557f0a94bc17b2fce3d982fe93e629d56ad526516603032b3cba9785bf5ebed038481ae641f55a5a1ad621442dde08791981cb SHA512 311642cbbe8dca621cb58bebb1bb05b7dc7b2364713886df96937ba56575137bcb4fb7006a232d2c14380cd41f4337f6f09a8a9794e279a155f27287497d2b08
 DIST pyamg-4.2.0.gh.tar.gz 3900381 BLAKE2B df556553f74ebbc3b83d5a9479af25a54a4fd999edeadaeb0951893612abdb7c2aa79909f6eb4196322752ae87c5636e23c8c9b7dcdbb1f8d515651526e8aff4 SHA512 71e8e1937437888fec275e8522a30f9eb35cb79cc07d6da8032b653ed58e086285701ebb40dbe3b9fe82b53dc6e112e18cf62d42b18233326639417cf808b7c1
+DIST pyamg-4.2.1.gh.tar.gz 3900395 BLAKE2B ad4427eb297f044b4782274ce55799bd9da4abec108267c4c0aaedab38e2fb4d89c4c365eca90e4045c411254640ead667fa0e511d9597ff00081c0ebacde9b2 SHA512 933f5f1da808cb02ff5fa91df54a48a971bb3b017582f5a829cf71c94f698469476b9f0306962140dc38f7265266c9b53f98dc7aaa99945b45bfa83003d26df9

commit 9cd9122b917025bf51694d5ed45e6bead8747be6

dev-python/pyamg: fix test calls
Use better way to handle pytest C errors

Fixes: 31f75efcb11aefe2502f36facf2f1c329b022233
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

dev-python/pyamg/pyamg-4.2.1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2021 Gentoo Authors
+# Copyright 1999-2022 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -32,7 +32,6 @@ distutils_enable_tests pytest
 export SETUPTOOLS_SCM_PRETEND_VERSION=${PV}
 
 python_test() {
-	cp -r -l -n pyamg "${BUILD_DIR}/lib" || die
-	cd "${BUILD_DIR}/lib" || die
-	epytest
+	cd "${T}" || die
+	epytest --pyargs pyamg
 }