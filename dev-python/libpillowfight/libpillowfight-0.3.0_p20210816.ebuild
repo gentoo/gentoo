gentoo/gentoo

commit 9b96d2dfc7572357be8dbd92fc9302660c196129

dev-python/libpillowfight: add 0.3.0_p20210816, enable tests
- Use new snapshot as the test suite was fixed to use correct images
- Use tarball from upstream repo to include tests
- enable tests
- enable py3.10

Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

dev-python/libpillowfight/libpillowfight-0.3.0_p20210816.ebuild
@@ -1,0 +1,38 @@
+# Copyright 1999-2021 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+
+PYTHON_COMPAT=( python3_{8..10} )
+
+inherit distutils-r1
+
+COMMIT="50d965879eb89fdef9be09d6e934329486ff585d"
+
+DESCRIPTION="Small library containing various image processing algorithms"
+HOMEPAGE="https://gitlab.gnome.org/World/OpenPaperwork/libpillowfight"
+SRC_URI="https://gitlab.gnome.org/World/OpenPaperwork/${PN}/-/archive/${COMMIT}/${P}.tar.gz"
+S="${WORKDIR}/${PN}-${COMMIT}"
+
+LICENSE="GPL-3"
+SLOT="0"
+KEYWORDS="~amd64 ~x86"
+
+RDEPEND="dev-python/pillow[${PYTHON_USEDEP}]"
+
+distutils_enable_tests nose
+
+python_prepare_all() {
+	sed -e "/'nose>=1.0'/d" -i setup.py || die
+	cat > src/pillowfight/_version.h <<- EOF || die
+		#define INTERNAL_PILLOWFIGHT_VERSION "$(ver_cut 1-3)"
+	EOF
+	distutils-r1_python_prepare_all
+}
+
+python_test() {
+	cp -r -l -n tests "${BUILD_DIR}/lib" || die
+	cd "${BUILD_DIR}/lib" || die
+	distutils-r1_python_test
+	rm -r tests || die
+}

dev-python/libpillowfight/Manifest
@@ -1,1 +1,2 @@
+DIST libpillowfight-0.3.0_p20210816.tar.gz 89491019 BLAKE2B 24a658ce461928750582560ef27fbc1ea233bb7c6290053bee7ab3437816fd0e9d7933e36a7a90c75706bdc17024200d16a17ec81c0c30563f043e6d15cc074e SHA512 a731a7d8c8492f5f6229315c8ff48bcdf215acd501d1a5f726f60d6417f77d04b702579d972aaa90f9d01d27b7b167b47f15f435c0fe79598aedb55ac397835b
 DIST pypillowfight-0.3.0.tar.gz 41065 BLAKE2B 8e945bb5a96d7ebd610435fd868d12df371c29fbc26a9c57152988a990ad83daf70382bb94e8b5591144e18d6c54702c0a578ea5f29044fcdadd169effcf933c SHA512 2782030eabb665d20cf49de584220fb65417616c3a0e2a0376c91dc2c228b571768ea3a248ebd5ec4d244a1b825e98609a35b736f7f2fdbb3f674b08e891d9a7

commit deb427c10768078cfbd23a14eb9cb569bf7d5d94

dev-python/libpillowfight: fix test calls
Use better way to handle pytest C errors

Fixes: 9b96d2dfc7572357be8dbd92fc9302660c196129
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

dev-python/libpillowfight/libpillowfight-0.3.0_p20210816.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2021 Gentoo Authors
+# Copyright 1999-2022 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
@@ -23,6 +23,7 @@ RDEPEND="dev-python/pillow[${PYTHON_USEDEP}]"
 distutils_enable_tests nose
 
 python_prepare_all() {
+	ln -s "${S}"/tests "${T}"/tests || die
 	sed -e "/'nose>=1.0'/d" -i setup.py || die
 	cat > src/pillowfight/_version.h <<- EOF || die
 		#define INTERNAL_PILLOWFIGHT_VERSION "$(ver_cut 1-3)"
@@ -31,8 +32,6 @@ python_prepare_all() {
 }
 
 python_test() {
-	cp -r -l -n tests "${BUILD_DIR}/lib" || die
-	cd "${BUILD_DIR}/lib" || die
-	distutils-r1_python_test
-	rm -r tests || die
+	cd "${T}" || die
+	epytest "${S}"/tests -o addopts=
 }