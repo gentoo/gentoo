gentoo/gentoo

commit 38695c204978839a830c4800e9ab669dcbc391f0

dev-python/gssapi: add 1.7.2, enable tests, enable py3.10
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

dev-python/gssapi/gssapi-1.7.2.ebuild
@@ -1,0 +1,37 @@
+# Copyright 1999-2021 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+PYTHON_COMPAT=( python3_{8..10} )
+inherit distutils-r1
+
+DESCRIPTION="Low and high level wrappers around the GSSAPI C libraries"
+HOMEPAGE="https://github.com/pythongssapi/python-gssapi https://pypi.org/project/gssapi/"
+SRC_URI="mirror://pypi/${P:0:1}/${PN}/${P}.tar.gz"
+
+LICENSE="MIT"
+SLOT="0"
+KEYWORDS="~amd64 ~x86"
+
+RDEPEND="
+	dev-python/decorator[${PYTHON_USEDEP}]
+	virtual/krb5
+"
+
+BDEPEND="
+	dev-python/cython[${PYTHON_USEDEP}]
+	virtual/krb5
+	test? (
+		dev-python/k5test[${PYTHON_USEDEP}]
+		dev-python/parameterized[${PYTHON_USEDEP}]
+	)
+"
+
+distutils_enable_tests pytest
+
+python_test() {
+	cp -r -l -n gssapi "${BUILD_DIR}/lib" || die
+	cd "${BUILD_DIR}/lib" || die
+	epytest
+}

dev-python/gssapi/Manifest
@@ -1,1 +1,2 @@
 DIST gssapi-1.6.12.tar.gz 1064583 BLAKE2B 0daae73e37d1f101f77030db4b4f5d78ec720157d25542c234da22ae4615b329ab54379b9a7f76f4c9340c849a1ec3c7d0ed3b4b5bad45411f807f8e21b45f1e SHA512 bfc6154dbed065227d1022218329ace3fde228173d1695c10e293b6c01a36030781afcea50cdd41ace243b6b783542fe828d027e79d4a79ea597b84ea578f90f
+DIST gssapi-1.7.2.tar.gz 1227616 BLAKE2B f03a1a2833359d8317fd08fcdeaeb69c54180dca82466db46cbd7110272f5f8ad6f7977095b539655c2a1ef9dea979d1ef229d03cc4de6317d579633f51553a7 SHA512 2d14c286e50c3dda3a2e58588860abbda249fa3ff4fe8e7a759cd8bb009ca7d20a6a06cf9b5b7eea42dce4ba740c8a8cebf190d085ffa3e66ec79882bda882ed

commit 07c89b58e2103d0ab87ff403e46b9d37dd0a560f

dev-python/gssapi: fix test calls
Use better way to handle pytest C errors

Fixes: 38695c204978839a830c4800e9ab669dcbc391f0
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

dev-python/gssapi/gssapi-1.7.2.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2021 Gentoo Authors
+# Copyright 1999-2022 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -31,7 +31,6 @@ BDEPEND="
 distutils_enable_tests pytest
 
 python_test() {
-	cp -r -l -n gssapi "${BUILD_DIR}/lib" || die
-	cd "${BUILD_DIR}/lib" || die
-	epytest
+	cd "${T}" || die
+	epytest --pyargs gssapi
 }