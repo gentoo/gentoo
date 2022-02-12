gentoo/gentoo

commit ba65a523a982f51e0037f74e3ad36c6da3f58ed9

games-action/dxx-rebirth: enable py3.10
Also add PYTHON_DEPS for completeness, although this is technically
all already handled by scons-utils (imagine python-any-r1 was added
because dxx-rebirth has its own python scripts too, but doing so
means should add python deps normally).

Signed-off-by: Ionen Wolkens <ionen@gentoo.org>

games-action/dxx-rebirth/dxx-rebirth-0.61.0_pre20210623.ebuild
@@ -1,9 +1,9 @@
-# Copyright 2017-2021 Gentoo Authors
+# Copyright 2017-2022 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
 
-PYTHON_COMPAT=( python3_{7,8,9} )
+PYTHON_COMPAT=( python3_{8..10} )
 
 inherit desktop python-any-r1 scons-utils toolchain-funcs xdg
 
@@ -151,7 +151,9 @@ REQUIRED_USE='
 	sdl2? ( opengl )
 '
 
-BDEPEND="virtual/pkgconfig"
+BDEPEND="
+	${PYTHON_DEPS}
+	virtual/pkgconfig"
 
 # As of this writing, IUSE_RUNTIME is a GLEP, but not an implemented
 # feature.  This variable is stored here to be ready to activate when

games-action/dxx-rebirth/dxx-rebirth-9999.ebuild
@@ -1,9 +1,9 @@
-# Copyright 2017-2021 Gentoo Authors
+# Copyright 2017-2022 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
 
-PYTHON_COMPAT=( python3_{7,8,9} )
+PYTHON_COMPAT=( python3_{8..10} )
 
 inherit desktop python-any-r1 scons-utils toolchain-funcs xdg
 
@@ -153,7 +153,9 @@ REQUIRED_USE='
 	sdl2? ( opengl )
 '
 
-BDEPEND="virtual/pkgconfig"
+BDEPEND="
+	${PYTHON_DEPS}
+	virtual/pkgconfig"
 
 # As of this writing, IUSE_RUNTIME is a GLEP, but not an implemented
 # feature.  This variable is stored here to be ready to activate when

commit c45b16abf06b88f48703b3bb2103901972297454

games-action/dxx-rebirth: cleanup unnecessarily added deps
On closer look I don't think these scripts are used, and adding
the eclass regardless of these is normal given how scons-utils
works. So it only makes PYTHON_DEPS redundant.

Fixes: ba65a523a982f51e0037f74e3ad36c6da3f58ed9
Signed-off-by: Ionen Wolkens <ionen@gentoo.org>

games-action/dxx-rebirth/dxx-rebirth-0.61.0_pre20210623.ebuild
@@ -151,9 +151,7 @@ REQUIRED_USE='
 	sdl2? ( opengl )
 '
 
-BDEPEND="
-	${PYTHON_DEPS}
-	virtual/pkgconfig"
+BDEPEND="virtual/pkgconfig"
 
 # As of this writing, IUSE_RUNTIME is a GLEP, but not an implemented
 # feature.  This variable is stored here to be ready to activate when

games-action/dxx-rebirth/dxx-rebirth-9999.ebuild
@@ -153,9 +153,7 @@ REQUIRED_USE='
 	sdl2? ( opengl )
 '
 
-BDEPEND="
-	${PYTHON_DEPS}
-	virtual/pkgconfig"
+BDEPEND="virtual/pkgconfig"
 
 # As of this writing, IUSE_RUNTIME is a GLEP, but not an implemented
 # feature.  This variable is stored here to be ready to activate when