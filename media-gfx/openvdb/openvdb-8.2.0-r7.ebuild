gentoo/gentoo

commit 31e4936280f16a043f443bd1ae004406aea34293

media-gfx/openvdb: blosc requires zlib
Revbump as it could lead to automagic zlib usage, however unlikely that
might be to be an issue.

See: https://forums.gentoo.org/viewtopic-t-1147443.html
See: https://github.com/AcademySoftwareFoundation/openvdb/commit/3fbae286e8e0ffe4d039d9ba7ba26d4cb3fc10a3
Signed-off-by: Sam James <sam@gentoo.org>

media-gfx/openvdb/openvdb-8.2.0-r7.ebuild
@@ -18,6 +18,7 @@ IUSE="cpu_flags_x86_avx cpu_flags_x86_sse4_2 +blosc doc numpy python static-libs
 RESTRICT="!test? ( test )"
 
 REQUIRED_USE="
+	blosc? ( zlib )
 	numpy? ( python )
 	^^ ( abi6-compat abi7-compat abi8-compat )
 	python? ( ${PYTHON_REQUIRED_USE} )

media-gfx/openvdb/openvdb-9.0.0-r4.ebuild
@@ -14,15 +14,15 @@ SRC_URI="https://github.com/AcademySoftwareFoundation/${PN}/archive/v${PV}.tar.g
 LICENSE="MPL-2.0"
 SLOT="0/9"
 KEYWORDS="~amd64 ~arm ~arm64 ~ppc64 ~x86"
-IUSE="cpu_flags_x86_avx cpu_flags_x86_sse4_2 +blosc cuda doc +nanovdb numpy python static-libs test utils zlib abi6-compat abi7-compat abi8-compat +abi9-compat"
+IUSE="cpu_flags_x86_avx cpu_flags_x86_sse4_2 +blosc cuda doc +nanovdb numpy python static-libs test utils +zlib abi6-compat abi7-compat abi8-compat +abi9-compat"
 RESTRICT="!test? ( test )"
 
-REQUIRED_USE="
+REQUIRED_USE="blosc? ( zlib )
 	numpy? ( python )
 	cuda? ( nanovdb )
 	^^ ( abi6-compat abi7-compat abi8-compat abi9-compat )
-	python? ( ${PYTHON_REQUIRED_USE} )
-"
+	python? ( ${PYTHON_REQUIRED_USE} )"
+
 RDEPEND="
 	>=dev-cpp/tbb-2020.3:=
 	dev-libs/boost:=

commit 787619dce77b284521aae1f46459d1a69a9c4298

media-gfx/openvdb: set +zlib in 8.2.0-r7
To match +blosc which needs it. Already done in 9.x.

Fixes: 31e4936280f16a043f443bd1ae004406aea34293
Signed-off-by: Sam James <sam@gentoo.org>

media-gfx/openvdb/openvdb-8.2.0-r7.ebuild
@@ -14,7 +14,7 @@ SRC_URI="https://github.com/AcademySoftwareFoundation/${PN}/archive/v${PV}.tar.g
 LICENSE="MPL-2.0"
 SLOT="0/8"
 KEYWORDS="~amd64 ~arm ~arm64 ~ppc64 ~x86"
-IUSE="cpu_flags_x86_avx cpu_flags_x86_sse4_2 +blosc doc numpy python static-libs test zlib abi6-compat abi7-compat +abi8-compat"
+IUSE="cpu_flags_x86_avx cpu_flags_x86_sse4_2 +blosc doc numpy python static-libs test +zlib abi6-compat abi7-compat +abi8-compat"
 RESTRICT="!test? ( test )"
 
 REQUIRED_USE="