gentoo/gentoo

commit c4398dfb476bd4f3be8f3b78b695d9f5996f697c

www-client/netsurf: drop old/incorrect spidermonkey dependency
The mozjs/spidermonkey dep was dropped upstream a while ago.

Signed-off-by: Sam James <sam@gentoo.org>

www-client/netsurf/netsurf-3.10-r6.ebuild
@@ -38,8 +38,7 @@ RDEPEND="
 		x11-libs/gtk+:2 )
 	javascript? (
 		>=dev-libs/nsgenbind-0.7
-		duktape? ( dev-lang/duktape:= )
-		!duktape? ( dev-lang/spidermonkey:0= )
+		dev-lang/duktape:=
 	)
 	jpeg? ( >=virtual/jpeg-0-r2:0 )
 	mng? ( >=media-libs/libmng-1.0.10-r2 )
@@ -84,9 +83,8 @@ _emake() {
 		NETSURF_USE_NSPSL=$(usex psl YES NO)
 		NETSURF_USE_MNG=$(usex mng YES NO)
 		NETSURF_USE_WEBP=$(usex webp YES NO)
-		NETSURF_USE_MOZJS=$(usex javascript $(usex duktape NO YES) NO)
 		NETSURF_USE_JS=NO
-		NETSURF_USE_DUKTAPE=$(usex javascript $(usex duktape YES NO) NO)
+		NETSURF_USE_DUKTAPE=$(usex javascript YES NO)
 		NETSURF_USE_NSSVG=$(usex svg $(usex svgtiny YES NO) NO)
 		NETSURF_USE_RSVG=$(usex svg $(usex svgtiny NO YES) NO)
 		NETSURF_USE_ROSPRITE=$(usex rosprite YES NO)

commit 7d7f9207012d7ed63cda301848b7a572b4bc2e9f

www-client/netsurf: drop remaining IUSE=duktape
Bug: https://bugs.gentoo.org/729444
Fixes: c4398dfb476bd4f3be8f3b78b695d9f5996f697c
Signed-off-by: Sam James <sam@gentoo.org>

www-client/netsurf/netsurf-3.10-r6.ebuild
@@ -12,11 +12,10 @@ SRC_URI="http://download.netsurf-browser.org/netsurf/releases/source/${P}-src.ta
 LICENSE="GPL-2 MIT"
 SLOT="0"
 KEYWORDS="~amd64 ~arm ~arm64 ~ppc ~ppc64 ~x86"
-IUSE="bmp +duktape fbcon truetype +gif +gtk gtk2 +javascript +jpeg mng
+IUSE="bmp fbcon truetype +gif +gtk gtk2 +javascript +jpeg mng
 	+png +psl rosprite +svg +svgtiny +webp"
 
-REQUIRED_USE="|| ( fbcon gtk gtk2 )
-	duktape? ( javascript )"
+REQUIRED_USE="|| ( fbcon gtk gtk2 )"
 
 RDEPEND="
 	>=dev-libs/libcss-0.9
@@ -50,7 +49,7 @@ RDEPEND="
 	webp? ( >=media-libs/libwebp-0.3.0 )"
 DEPEND="${RDEPEND}"
 BDEPEND="
-	duktape? ( app-editors/vim-core )
+	javascript? ( app-editors/vim-core )
 	dev-libs/check
 	dev-perl/HTML-Parser
 	>=dev-util/netsurf-buildsystem-1.7-r1