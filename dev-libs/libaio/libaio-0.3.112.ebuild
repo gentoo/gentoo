gentoo/gentoo

commit b7d492ff93212de55b9844dc6aabcef5493c69d5

dev-libs/libaio: [QA] fix tc-get* quoting
This can cause build problems for e.g. 32-bit (gcc -m32 ...)

Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/libaio/libaio-0.3.112.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2021 Gentoo Authors
+# Copyright 1999-2022 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI="7"
@@ -46,17 +46,17 @@ multilib_src_configure() {
 	if use arm ; then
 		# When building for thumb, we can't allow frame pointers.
 		# http://crbug.com/464517
-		if $(tc-getCPP) ${CFLAGS} ${CPPFLAGS} - <<<$'#ifndef __thumb__\n#error\n#endif' >&/dev/null ; then
+		if "$(tc-getCPP)" ${CFLAGS} ${CPPFLAGS} - <<<$'#ifndef __thumb__\n#error\n#endif' >&/dev/null ; then
 			append-flags -fomit-frame-pointer
 		fi
 	fi
 }
 
 _emake() {
-	CC=$(tc-getCC) \
-	AR=$(tc-getAR) \
-	RANLIB=$(tc-getRANLIB) \
-	ABI_LIBDIR=$(get_libdir) \
+	CC="$(tc-getCC)" \
+	AR="$(tc-getAR)" \
+	RANLIB="$(tc-getRANLIB)" \
+	ABI_LIBDIR="$(get_libdir)" \
 	CFLAGS_WERROR= \
 	emake "$@"
 }

dev-libs/libaio/libaio-9999.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2021 Gentoo Authors
+# Copyright 1999-2022 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI="7"
@@ -46,17 +46,17 @@ multilib_src_configure() {
 	if use arm ; then
 		# When building for thumb, we can't allow frame pointers.
 		# http://crbug.com/464517
-		if $(tc-getCPP) ${CFLAGS} ${CPPFLAGS} - <<<$'#ifndef __thumb__\n#error\n#endif' >&/dev/null ; then
+		if "$(tc-getCPP)" ${CFLAGS} ${CPPFLAGS} - <<<$'#ifndef __thumb__\n#error\n#endif' >&/dev/null ; then
 			append-flags -fomit-frame-pointer
 		fi
 	fi
 }
 
 _emake() {
-	CC=$(tc-getCC) \
-	AR=$(tc-getAR) \
-	RANLIB=$(tc-getRANLIB) \
-	ABI_LIBDIR=$(get_libdir) \
+	CC="$(tc-getCC)" \
+	AR="$(tc-getAR)" \
+	RANLIB="$(tc-getRANLIB)" \
+	ABI_LIBDIR="$(get_libdir)" \
 	CFLAGS_WERROR= \
 	emake "$@"
 }

commit 7421ae946ca9d19a417d2fcf3e48b4de8f80202b

dev-libs/libaio: fix tc-getCPP
Fixes: b7d492ff93212de55b9844dc6aabcef5493c69d5
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/libaio/libaio-0.3.112.ebuild
@@ -46,7 +46,7 @@ multilib_src_configure() {
 	if use arm ; then
 		# When building for thumb, we can't allow frame pointers.
 		# http://crbug.com/464517
-		if "$(tc-getCPP)" ${CFLAGS} ${CPPFLAGS} - <<<$'#ifndef __thumb__\n#error\n#endif' >&/dev/null ; then
+		if $(tc-getCPP) ${CFLAGS} ${CPPFLAGS} - <<<$'#ifndef __thumb__\n#error\n#endif' >&/dev/null ; then
 			append-flags -fomit-frame-pointer
 		fi
 	fi

dev-libs/libaio/libaio-9999.ebuild
@@ -46,7 +46,7 @@ multilib_src_configure() {
 	if use arm ; then
 		# When building for thumb, we can't allow frame pointers.
 		# http://crbug.com/464517
-		if "$(tc-getCPP)" ${CFLAGS} ${CPPFLAGS} - <<<$'#ifndef __thumb__\n#error\n#endif' >&/dev/null ; then
+		if $(tc-getCPP) ${CFLAGS} ${CPPFLAGS} - <<<$'#ifndef __thumb__\n#error\n#endif' >&/dev/null ; then
 			append-flags -fomit-frame-pointer
 		fi
 	fi