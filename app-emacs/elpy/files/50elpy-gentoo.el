bab7113a6852ac300bee085aee8eb24a231dd482
Maciej Barć <xgqt@gentoo.org>
2022-04-08 15:59:14 +0200

    app-emacs/elpy: new package; add version 1.35.0_p20220321
    2022.03.21 snapshot
    
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

---
+++ app-emacs/elpy/files/50elpy-gentoo.el
@@ -0,0 +1,5 @@
+(add-to-list 'load-path "@SITELISP@")
+(autoload 'elpy-enable "elpy"
+  "Enable Elpy in all future Python buffers." t)
+(autoload 'elpy-mode "elpy"
+  "Minor mode in Python buffers for the Emacs Lisp Python Environment." t)
