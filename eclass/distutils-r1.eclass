gentoo/gentoo

commit 6451cf647f04b31a56ea2dc8c3f84a70c2468f87

distutils-r1.eclass: Issue a QA warning if "build" exists
Signed-off-by: Michał Górny <mgorny@gentoo.org>

eclass/distutils-r1.eclass
@@ -1115,7 +1115,13 @@ distutils-r1_python_compile() {
 	# call setup.py build when using setuptools (either via PEP517
 	# or in legacy mode)
 	if [[ ${DISTUTILS_USE_PEP517:-setuptools} == setuptools ]]; then
-		if [[ ! ${DISTUTILS_USE_PEP517} ]]; then
+		if [[ ${GPEP517_TESTING} ]]; then
+			if [[ -d build ]]; then
+				eqawarn "A 'build' directory exists already.  Artifacts from this directory may"
+				eqawarn "be picked up by setuptools when building for another interpreter."
+				eqawarn "Please remove this directory prior to building."
+			fi
+		else
 			_distutils-r1_copy_egg_info
 		fi
 

commit 65701019d3531f43e471c0060fe9724bf3b7751a

distutils-r1.eclass: don't call copy_egg_info if USE_PEP517=setuptools
Fixes: 6451cf647f04b31a56ea2dc8c3f84a70c2468f87
Thanks-to: Ionen Wolkens <ionen@gentoo.org>
Signed-off-by: Sam James <sam@gentoo.org>

eclass/distutils-r1.eclass
@@ -1168,7 +1168,7 @@ distutils-r1_python_compile() {
 				eqawarn "be picked up by setuptools when building for another interpreter."
 				eqawarn "Please remove this directory prior to building."
 			fi
-		else
+		elif [[ ! ${DISTUTILS_USE_PEP517} ]]; then
 			_distutils-r1_copy_egg_info
 		fi
 