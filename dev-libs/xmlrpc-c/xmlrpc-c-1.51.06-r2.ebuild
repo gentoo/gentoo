gentoo/gentoo

commit c96c1ecd22c1fda96cb02bed3e33d7643e383996

dev-libs/xmlrpc-c: fix tests
Reported-by: Toralf Förster <toralf@gentoo.org>
Closes: https://bugs.gentoo.org/836469
Package-Manager: Portage-3.0.30, Repoman-3.0.3
Signed-off-by: Sergey Popov <pinkbyte@gentoo.org>

dev-libs/xmlrpc-c/xmlrpc-c-1.51.06-r2.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2021 Gentoo Authors
+# Copyright 1999-2022 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
@@ -64,6 +64,8 @@ multilib_src_configure() {
 
 multilib_src_compile() {
 	default_src_compile
+	# Needed for tests, bug #836469
+	cp "${BUILD_DIR}"/include/xmlrpc-c/config.h "${S}"/include/xmlrpc-c || die
 	# Tools building is broken in this release
 	#multilib_is_native_abi && use tools && emake -rC "${S}"/tools
 }

commit 80d29e9c5330d6cecdf0225e564a94427610b702

dev-libs/xmlrpc: fix multilib builds
Previosly broken by commit c96c1ecd22c1fda96cb02bed3e33d7643e383996

Reported-by: Michael Cook <mackal.cook@gmail.com>
Closes: https://bugs.gentoo.org/837539
Bug: https://bugs.gentoo.org/836469
Package-Manager: Portage-3.0.30, Repoman-3.0.3
Signed-off-by: Sergey Popov <pinkbyte@gentoo.org>

dev-libs/xmlrpc-c/xmlrpc-c-1.51.06-r2.ebuild
@@ -64,10 +64,14 @@ multilib_src_configure() {
 
 multilib_src_compile() {
 	default_src_compile
-	# Needed for tests, bug #836469
-	cp "${BUILD_DIR}"/include/xmlrpc-c/config.h "${S}"/include/xmlrpc-c || die
 	# Tools building is broken in this release
 	#multilib_is_native_abi && use tools && emake -rC "${S}"/tools
+}
+
+multilib_src_test() {
+	# Needed for tests, bug #836469
+	cp "${BUILD_DIR}"/include/xmlrpc-c/config.h "${S}"/include/xmlrpc-c || die
+	default_src_test
 }
 
 #multilib_src_install_all() {

dev-libs/xmlrpc-c/xmlrpc-c-1.54.05.ebuild
@@ -64,10 +64,14 @@ multilib_src_configure() {
 
 multilib_src_compile() {
 	default_src_compile
-	# Needed for tests, bug #836469
-	cp "${BUILD_DIR}"/include/xmlrpc-c/config.h "${S}"/include/xmlrpc-c || die
 	# Tools building is broken in this release
 	#multilib_is_native_abi && use tools && emake -rC "${S}"/tools
+}
+
+multilib_src_test() {
+	# Needed for tests, bug #836469
+	cp "${BUILD_DIR}"/include/xmlrpc-c/config.h "${S}"/include/xmlrpc-c || die
+	default_src_test
 }
 
 #multilib_src_install_all() {