gentoo/gentoo

commit 5a7ea1cef34c3c3026a999d566bc4badc1dccc9c

toolchain.eclass: allow overriding S in ebuilds
Useful for RCs-but-we-pretend-they're-not-RCs.

Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -244,7 +244,7 @@ fi
 if tc_has_feature sanitize ; then
 	# libsanitizer relies on 'crypt.h' to be present
 	# on target. glibc user to provide it unconditionally.
-	# Nowadays it's a standalone library: #802648
+	# Nowadays it's a standalone library: bug #802648
 	DEPEND+=" sanitize? ( virtual/libcrypt )"
 fi
 
@@ -268,15 +268,17 @@ PDEPEND=">=sys-devel/gcc-config-2.3"
 
 # Set the source directory depending on whether we're using
 # a live git tree, snapshot, or release tarball.
-S=$(
-	if tc_is_live ; then
-		echo ${EGIT_CHECKOUT_DIR}
-	elif [[ -n ${SNAPSHOT} ]] ; then
-		echo ${WORKDIR}/gcc-${SNAPSHOT}
-	else
-		echo ${WORKDIR}/gcc-${GCC_RELEASE_VER}
-	fi
-)
+if [[ -z ${S} ]] ; then
+	S=$(
+		if tc_is_live ; then
+			echo ${EGIT_CHECKOUT_DIR}
+		elif [[ -n ${SNAPSHOT} ]] ; then
+			echo ${WORKDIR}/gcc-${SNAPSHOT}
+		else
+			echo ${WORKDIR}/gcc-${GCC_RELEASE_VER}
+		fi
+	)
+fi
 
 gentoo_urls() {
 	local devspace="

commit 5b80c7730d73c42607c826270e2de85fce40ac79

toolchain.eclass: fix S definition
S isn't _blank_ by default.

Fixes: 5a7ea1cef34c3c3026a999d566bc4badc1dccc9c
Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -266,9 +266,11 @@ PDEPEND=">=sys-devel/gcc-config-2.3"
 
 #---->> S + SRC_URI essentials <<----
 
+: ${TOOLCHAIN_SET_S:=yes}
+
 # Set the source directory depending on whether we're using
 # a live git tree, snapshot, or release tarball.
-if [[ -z ${S} ]] ; then
+if [[ ${TOOLCHAIN_SET_S} == yes ]] ; then
 	S=$(
 		if tc_is_live ; then
 			echo ${EGIT_CHECKOUT_DIR}