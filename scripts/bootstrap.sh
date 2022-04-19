c26dfbbcfeb7239fbb527b0fceab36c7371f4b9d
Mike Gilbert <floppym@gentoo.org>
2022-04-19 17:16:35 -0400

    scripts/bootstrap.sh: maintainer = releng
    Signed-off-by: Mike Gilbert <floppym@gentoo.org>

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,6 +2,8 @@
 # Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
+# Maintainer: releng@gentoo.org
+
 file_version="2021.0"		# update manually: <year>.<counter>
 
 # people who were here:
