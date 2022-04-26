gentoo/gentoo

commit 1bff2846eaa16d29f9c023508d855931a727eebd

sys-apps/diffutils: fix build on loong
Upstream has updated gnulib but hasn't tagged a new release yet, so only
add the bare minimum to fix build on loong.

This has been inside loongson-overlay for a while, and is tested on real
loong hardware.

See: https://github.com/gentoo/gentoo/pull/25189
Acked-by: Andreas K. Hüttel <dilfridge@gentoo.org>
Signed-off-by: WANG Xuerui <xen0n@gentoo.org>

sys-apps/diffutils/diffutils-3.8.ebuild
@@ -23,7 +23,10 @@ IUSE="nls static"
 BDEPEND="nls? ( sys-devel/gettext )
 	verify-sig? ( sec-keys/openpgp-keys-diffutils )"
 
-PATCHES=( "${FILESDIR}/ppc-musl.patch" )
+PATCHES=(
+	"${FILESDIR}/ppc-musl.patch"
+	"${FILESDIR}/loong-fix-build.patch"
+)
 
 src_configure() {
 	use static && append-ldflags -static

sys-apps/diffutils/files/loong-fix-build.patch
@@ -1,0 +1,26 @@
+From 34add045fd2ec3f1031dee961c4d9cc4285486a7 Mon Sep 17 00:00:00 2001
+From: Sun Haiyong <youbest@sina.com>
+Date: Tue, 31 Aug 2021 11:11:52 +0800
+Subject: [PATCH] stack-direction: Add support for loongarch CPU
+
+* m4/stack-direction.m4 (SV_STACK_DIRECTION): When the CPU is loongarch,
+set "sv_cv_stack_direction" to "-1" .
+---
+ m4/stack-direction.m4 | 1 +
+ 1 file changed, 1 insertion(+)
+
+diff --git a/m4/stack-direction.m4 b/m4/stack-direction.m4
+index 93287254b..e6c3284ce 100644
+--- a/m4/stack-direction.m4
++++ b/m4/stack-direction.m4
+@@ -32,6 +32,7 @@ AC_DEFUN([SV_STACK_DIRECTION],
+       i?86 | x86_64 | \
+       i860 | \
+       ia64 | \
++      loongarch* | \
+       m32r | \
+       m68* | \
+       m88k | \
+-- 
+2.17.2
+

commit bc5fb076ca43a7ee614f1afe2c742794718bd8e2

sys-apps/diffutils: avoid invoking autoconf
Normally we'd just eautoreconf, but this is base-system and we cannot
depend on autoconf, so we have to patch the generated files ourselves.
This can go away once the loong fix patch is no longer necessary.

Fixes: 1bff2846eaa ("sys-apps/diffutils: fix build on loong")
Signed-off-by: WANG Xuerui <xen0n@gentoo.org>

sys-apps/diffutils/diffutils-3.8.ebuild
@@ -28,6 +28,16 @@ PATCHES=(
 	"${FILESDIR}/loong-fix-build.patch"
 )
 
+src_prepare() {
+	default
+
+	# touch generated files after patching m4, to avoid activating maintainer
+	# mode
+	# remove when loong-fix-build.patch is no longer necessary
+	touch ./aclocal.m4 lib/config.hin ./configure || die
+	find . -name Makefile.in -exec touch {} + || die
+}
+
 src_configure() {
 	use static && append-ldflags -static
 

sys-apps/diffutils/files/loong-fix-build.patch
@@ -1,19 +1,23 @@
-From 34add045fd2ec3f1031dee961c4d9cc4285486a7 Mon Sep 17 00:00:00 2001
+[xen0n: this is https://github.com/sunhaiyong1978/CLFS-for-LoongArch/blob/1.0/patches/stack-direction-add-loongarch.patch with change to generated file added as well.]
 From: Sun Haiyong <youbest@sina.com>
 Date: Tue, 31 Aug 2021 11:11:52 +0800
 Subject: [PATCH] stack-direction: Add support for loongarch CPU
 
 * m4/stack-direction.m4 (SV_STACK_DIRECTION): When the CPU is loongarch,
 set "sv_cv_stack_direction" to "-1" .
----
- m4/stack-direction.m4 | 1 +
- 1 file changed, 1 insertion(+)
-
-diff --git a/m4/stack-direction.m4 b/m4/stack-direction.m4
-index 93287254b..e6c3284ce 100644
 --- a/m4/stack-direction.m4
 +++ b/m4/stack-direction.m4
 @@ -32,6 +32,7 @@ AC_DEFUN([SV_STACK_DIRECTION],
+       i?86 | x86_64 | \
+       i860 | \
+       ia64 | \
++      loongarch* | \
+       m32r | \
+       m68* | \
+       m88k | \
+--- a/configure
++++ b/configure
+@@ -34088,6 +34088,7 @@ else $as_nop
        i?86 | x86_64 | \
        i860 | \
        ia64 | \