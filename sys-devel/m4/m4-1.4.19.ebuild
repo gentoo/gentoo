gentoo/gentoo

commit 43dd58338de06e5f214e616425c17578908adfec

sys-devel/m4: fix build on loong
Upstream has updated gnulib but hasn't tagged a new release yet, so only
add the bare minimum to fix build on loong.

This has been inside loongson-overlay for a while, and is tested on real
loong hardware.

See: https://github.com/gentoo/gentoo/pull/25189
Acked-by: Andreas K. Hüttel <dilfridge@gentoo.org>
Signed-off-by: WANG Xuerui <xen0n@gentoo.org>

sys-devel/m4/m4-1.4.19.ebuild
@@ -38,6 +38,7 @@ BDEPEND="app-arch/xz-utils
 
 PATCHES=(
 	"${FILESDIR}"/ppc-musl.patch
+	"${FILESDIR}"/loong-fix-build.patch
 	"${WORKDIR}"/${P}-test-198-sysval-r1.patch
 )
 

sys-devel/m4/files/loong-fix-build.patch
@@ -1,0 +1,26 @@
+From 34add045fd2ec3f1031dee961c4d9cc4285486a7 Mon Sep 17 00:00:00 2001
+From: Sun Haiyong <youbest@sina.com>
+Date: Tue, 31 Aug 2021 11:11:52 +0800
+Subject: [PATCH] stack-direction: Add support for loongarch CPU
+
+* m4/stack-direction.m4 (SV_STACK_DIRECTION): When the CPU is loongarch,
+set "sv_cv_stack_direction" to "-1" .
+---
+ m4/stack-direction.m4 | 1 +
+ 1 file changed, 1 insertion(+)
+
+diff --git a/m4/stack-direction.m4 b/m4/stack-direction.m4
+index 93287254b..e6c3284ce 100644
+--- a/m4/stack-direction.m4
++++ b/m4/stack-direction.m4
+@@ -32,6 +32,7 @@ AC_DEFUN([SV_STACK_DIRECTION],
+       i?86 | x86_64 | \
+       i860 | \
+       ia64 | \
++      loongarch* | \
+       m32r | \
+       m68* | \
+       m88k | \
+-- 
+2.17.2
+

commit 5dcddbb5214cd29e6217edb4002be36df4d60c66

sys-devel/m4: fix incomplete deactivation of maintainer mode
Commit 4abfea67ec7 ("sys-devel/m4: avoid invoking autoconf") did an
incomplete job of deactivating the refresh of various files after
patching the m4 and configure part of sources; turns out dependency on
makeinfo should be avoided as well.

Tested with sys-apps/texinfo unmerged.

Fixes: 43dd58338de ("sys-devel/m4: fix build on loong")
Closes: https://bugs.gentoo.org/840984
Signed-off-by: WANG Xuerui <xen0n@gentoo.org>

sys-devel/m4/m4-1.4.19.ebuild
@@ -57,7 +57,7 @@ src_prepare() {
 	# touch generated files after patching m4, to avoid activating maintainer
 	# mode
 	# remove when loong-fix-build.patch is no longer necessary
-	touch ./aclocal.m4 ./lib/config.hin ./configure || die
+	touch ./aclocal.m4 ./lib/config.hin ./configure ./doc/stamp-vti || die
 	find . -name Makefile.in -exec touch {} + || die
 }
 