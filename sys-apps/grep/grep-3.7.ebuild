gentoo/gentoo

commit 07611b62174bab556022d7fe24fc6a204647afbf

sys-apps/grep: fix build on loong
Upstream has updated gnulib but hasn't tagged a new release yet, so only
add the bare minimum to fix build on loong.

This has been inside loongson-overlay for a while, and is tested on real
loong hardware.

See: https://github.com/gentoo/gentoo/pull/25189
Acked-by: Andreas K. Hüttel <dilfridge@gentoo.org>
Signed-off-by: WANG Xuerui <xen0n@gentoo.org>

sys-apps/grep/files/loong-fix-build.patch
@@ -1,0 +1,26 @@
+From 34add045fd2ec3f1031dee961c4d9cc4285486a7 Mon Sep 17 00:00:00 2001
+From: Sun Haiyong <youbest@sina.com>
+Date: Tue, 31 Aug 2021 11:11:52 +0800
+Subject: [PATCH] stack-direction: Add support for loongarch CPU
+
+* m4/stack-direction.m4 (SV_STACK_DIRECTION): When the CPU is loongarch,
+set "sv_cv_stack_direction" to "-1" .
+---
+ m4/stack-direction.m4 | 1 +
+ 1 file changed, 1 insertion(+)
+
+diff --git a/m4/stack-direction.m4 b/m4/stack-direction.m4
+index 93287254b..e6c3284ce 100644
+--- a/m4/stack-direction.m4
++++ b/m4/stack-direction.m4
+@@ -32,6 +32,7 @@ AC_DEFUN([SV_STACK_DIRECTION],
+       i?86 | x86_64 | \
+       i860 | \
+       ia64 | \
++      loongarch* | \
+       m32r | \
+       m68* | \
+       m88k | \
+-- 
+2.17.2
+

sys-apps/grep/grep-3.7.ebuild
@@ -31,7 +31,10 @@ BDEPEND="
 	nls? ( sys-devel/gettext )
 	verify-sig? ( sec-keys/openpgp-keys-grep )"
 
-PATCHES=( "${FILESDIR}/ppc-musl.patch" )
+PATCHES=(
+	"${FILESDIR}/ppc-musl.patch"
+	"${FILESDIR}/loong-fix-build.patch"
+)
 
 DOCS=( AUTHORS ChangeLog NEWS README THANKS TODO )
 

commit 58ed4c3055ed33a8b25153c7e2738b012c7afe24

sys-apps/grep: avoid invoking autoconf
Normally we'd just eautoreconf, but this is base-system and we cannot
depend on autoconf, so we have to patch the generated files ourselves.
This can go away once the loong fix patch is no longer necessary.

Fixes: 07611b62174 ("sys-apps/grep: fix build on loong")
Signed-off-by: WANG Xuerui <xen0n@gentoo.org>

sys-apps/grep/files/loong-fix-build.patch
@@ -1,19 +1,23 @@
-From 34add045fd2ec3f1031dee961c4d9cc4285486a7 Mon Sep 17 00:00:00 2001
+[xen0n: this is https://github.com/sunhaiyong1978/CLFS-for-LoongArch/blob/1.0/patches/stack-direction-add-loongarch.patch with change to generated file added as well.]
 From: Sun Haiyong <youbest@sina.com>
 Date: Tue, 31 Aug 2021 11:11:52 +0800
 Subject: [PATCH] stack-direction: Add support for loongarch CPU
 
 * m4/stack-direction.m4 (SV_STACK_DIRECTION): When the CPU is loongarch,
 set "sv_cv_stack_direction" to "-1" .
----
- m4/stack-direction.m4 | 1 +
- 1 file changed, 1 insertion(+)
-
-diff --git a/m4/stack-direction.m4 b/m4/stack-direction.m4
-index 93287254b..e6c3284ce 100644
 --- a/m4/stack-direction.m4
 +++ b/m4/stack-direction.m4
 @@ -32,6 +32,7 @@ AC_DEFUN([SV_STACK_DIRECTION],
+       i?86 | x86_64 | \
+       i860 | \
+       ia64 | \
++      loongarch* | \
+       m32r | \
+       m68* | \
+       m88k | \
+--- a/configure
++++ b/configure
+@@ -35668,6 +35668,7 @@ else $as_nop
        i?86 | x86_64 | \
        i860 | \
        ia64 | \

sys-apps/grep/grep-3.7.ebuild
@@ -45,6 +45,12 @@ src_prepare() {
 		src/egrep.sh || die #523898
 
 	default
+
+	# touch generated files after patching m4, to avoid activating maintainer
+	# mode
+	# remove when loong-fix-build.patch is no longer necessary
+	touch ./aclocal.m4 ./config.hin ./configure || die
+	find . -name Makefile.in -exec touch {} + || die
 }
 
 src_configure() {