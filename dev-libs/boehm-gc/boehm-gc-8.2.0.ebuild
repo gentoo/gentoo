gentoo/gentoo

commit 679ce9130be8485f3dde641a6b1d4bd7dda6808d

dev-libs/boehm-gc: add 8.2.0
Dropping keywords because this version has been marked by upstream as
"Pre-release": https://github.com/ivmai/bdwgc/releases/tag/v8.2.0

Tests passed on amd64.

Signed-off-by: WANG Xuerui <xen0n@gentoo.org>

dev-libs/boehm-gc/Manifest
@@ -1,2 +1,3 @@
 DIST gc-8.0.4.tar.gz 1160528 BLAKE2B ec42378c964fc573e53f6f58aa22fa63a0b421c91b81336acfeb2e2afdb5c89514edace3a9500699e13ff1cadc4205ea245af4f0dfa5e439ed9b78cabf6058f7 SHA512 57ccca15c6e50048d306a30de06c1a844f36103a84c2d1c17cbccbbc0001e17915488baec79737449982da99ce5d14ce527176afae9ae153cbbb5a19d986366e
 DIST gc-8.0.6.tar.gz 1168660 BLAKE2B c23cb10db54db0e8b16c429e039791086dfec4216ad65fabf072bc0f05df66364d019fccf945bffc32ce12868edfbaf6d9482acb2949d1e233bd89925ab55ebd SHA512 2ea25003d585118e3ac0e12be9240e8195db511c6e94949f20453dc3cb771380bd5b956c04243b2a8ce31508587aa32de4f0f10a813577e6dbe8367688b7614e
+DIST gc-8.2.0.tar.gz 1183225 BLAKE2B 382c28041b8cff9df794db48727c0bb478ed9222b15bcf11eda7132b1776970774576b63823e62a87c6563a06359a3d0572effea1ca63fb4f6fdadf1ea350cbe SHA512 ff781360bca667f1f95fbfed073e9035f63134cac1bda4f7e4217664a3713c2846f750a3ce92732972c0435c69355b051f6e8eb6f2698b0d48d9107ff960bacf

dev-libs/boehm-gc/boehm-gc-8.2.0.ebuild
@@ -1,0 +1,54 @@
+# Copyright 1999-2022 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+
+inherit multilib-minimal libtool
+
+MY_P="gc-${PV}"
+
+DESCRIPTION="The Boehm-Demers-Weiser conservative garbage collector"
+HOMEPAGE="https://www.hboehm.info/gc/ https://github.com/ivmai/bdwgc/"
+SRC_URI="https://github.com/ivmai/bdwgc/releases/download/v${PV}/${MY_P}.tar.gz"
+S="${WORKDIR}/${MY_P}"
+
+LICENSE="boehm-gc"
+# SONAME: libgc.so.1 libgccpp.so.1
+# We've been using subslot 0 for these instead of "1.1".
+SLOT="0"
+# Upstream marked this version as "Pre-release"
+KEYWORDS=""
+IUSE="cxx +large static-libs +threads"
+
+RDEPEND=">=dev-libs/libatomic_ops-7.4[${MULTILIB_USEDEP}]"
+DEPEND="${RDEPEND}"
+BDEPEND="virtual/pkgconfig"
+
+src_prepare() {
+	default
+	elibtoolize #594754
+}
+
+multilib_src_configure() {
+	local config=(
+		--disable-docs
+		--with-libatomic-ops
+		$(use_enable cxx cplusplus)
+		$(use_enable static-libs static)
+		$(use threads || echo --disable-threads)
+		$(use_enable large large-config)
+	)
+
+	ECONF_SOURCE=${S} econf "${config[@]}"
+}
+
+multilib_src_install_all() {
+	local HTML_DOCS=( doc/*.md )
+	einstalldocs
+	dodoc doc/README{.environment,.linux,.macros}
+
+	# package provides .pc files
+	find "${ED}" -name '*.la' -delete || die
+
+	newman doc/gc.man GC_malloc.1
+}

commit e70f62a2157efbb0914a18c5a4f412c79df45995

dev-libs/boehm-gc: fix missing installed headers for 8.2.0
The patch is generated by cherry-picking upstream commit
https://github.com/ivmai/bdwgc/commit/056ad8279e9a89e9, then refreshing
generated files.

Fixes: 679ce9130be8 ("dev-libs/boehm-gc: add 8.2.0")
Closes: https://github.com/gentoo/gentoo/pull/25282
Reviewed-by: David Seifert <soap@gentoo.org>
Signed-off-by: WANG Xuerui <xen0n@gentoo.org>

dev-libs/boehm-gc/Manifest
@@ -1,3 +1,4 @@
+DIST boehm-gc-8.2.0-fix-headers-install.patch.xz 55320 BLAKE2B 86f6a27c49c1c36a89e836ef2adf17e6563256ad0700e05235461b9df562029781894d1b4b76ed4fb44faf9c014e7674b909cd8a097d6d3c29cb50be55b83856 SHA512 cbb963b5f4286846ae4548216677cb34042bd8757f6792d1838663b07699837c25a727740d6d9461b582c5428ebe8e0bc670134be1d68cdc61d055f8624a76bc
 DIST gc-8.0.4.tar.gz 1160528 BLAKE2B ec42378c964fc573e53f6f58aa22fa63a0b421c91b81336acfeb2e2afdb5c89514edace3a9500699e13ff1cadc4205ea245af4f0dfa5e439ed9b78cabf6058f7 SHA512 57ccca15c6e50048d306a30de06c1a844f36103a84c2d1c17cbccbbc0001e17915488baec79737449982da99ce5d14ce527176afae9ae153cbbb5a19d986366e
 DIST gc-8.0.6.tar.gz 1168660 BLAKE2B c23cb10db54db0e8b16c429e039791086dfec4216ad65fabf072bc0f05df66364d019fccf945bffc32ce12868edfbaf6d9482acb2949d1e233bd89925ab55ebd SHA512 2ea25003d585118e3ac0e12be9240e8195db511c6e94949f20453dc3cb771380bd5b956c04243b2a8ce31508587aa32de4f0f10a813577e6dbe8367688b7614e
 DIST gc-8.2.0.tar.gz 1183225 BLAKE2B 382c28041b8cff9df794db48727c0bb478ed9222b15bcf11eda7132b1776970774576b63823e62a87c6563a06359a3d0572effea1ca63fb4f6fdadf1ea350cbe SHA512 ff781360bca667f1f95fbfed073e9035f63134cac1bda4f7e4217664a3713c2846f750a3ce92732972c0435c69355b051f6e8eb6f2698b0d48d9107ff960bacf

dev-libs/boehm-gc/boehm-gc-8.2.0.ebuild
@@ -9,7 +9,8 @@ MY_P="gc-${PV}"
 
 DESCRIPTION="The Boehm-Demers-Weiser conservative garbage collector"
 HOMEPAGE="https://www.hboehm.info/gc/ https://github.com/ivmai/bdwgc/"
-SRC_URI="https://github.com/ivmai/bdwgc/releases/download/v${PV}/${MY_P}.tar.gz"
+SRC_URI="https://github.com/ivmai/bdwgc/releases/download/v${PV}/${MY_P}.tar.gz
+	https://dev.gentoo.org/~xen0n/distfiles/${P}-fix-headers-install.patch.xz"
 S="${WORKDIR}/${MY_P}"
 
 LICENSE="boehm-gc"
@@ -23,6 +24,10 @@ IUSE="cxx +large static-libs +threads"
 RDEPEND=">=dev-libs/libatomic_ops-7.4[${MULTILIB_USEDEP}]"
 DEPEND="${RDEPEND}"
 BDEPEND="virtual/pkgconfig"
+
+PATCHES=(
+	"${WORKDIR}/${P}-fix-headers-install.patch"
+)
 
 src_prepare() {
 	default