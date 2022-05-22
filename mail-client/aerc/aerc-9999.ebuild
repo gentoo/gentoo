gentoo/gentoo

commit 721c5b6eb1f07871c4d00f5f30bcf2590c5b5906

mail-client/aerc: sync live
Package-Manager: Portage-3.0.30, Repoman-3.0.3
Signed-off-by: William Hubbs <williamh@gentoo.org>

mail-client/aerc/aerc-9999.ebuild
@@ -2,8 +2,7 @@
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
-
-inherit go-module
+inherit go-module xdg
 
 DESCRIPTION="Email client for your terminal"
 HOMEPAGE="https://aerc-mail.org"
@@ -12,12 +11,8 @@ if [[ ${PV} == *9999 ]]; then
 	inherit git-r3
 	EGIT_REPO_URI="https://git.sr.ht/~rjarry/aerc"
 else
-	EGO_SUM=(
-		# to be filled on bumps
-	)
-	go-module_set_globals
-	SRC_URI="https://git.sr.ht/~sircmpwn/${PN}/archive/${PV}.tar.gz -> ${P}.tar.gz
-		${EGO_SUM_SRC_URI}"
+	SRC_URI="https://git.sr.ht/~rjarry/${PN}/archive/${PV}.tar.gz -> ${P}.tar.gz"
+	SRC_URI+=" https://dev.gentoo.org/~williamh/dist/${P}-deps.tar.gz"
 	KEYWORDS="~amd64 ~ppc64"
 fi
 
@@ -25,13 +20,12 @@ LICENSE="Apache-2.0 BSD BSD-2 MIT"
 SLOT="0"
 IUSE="notmuch"
 
+DEPEND="notmuch? ( net-mail/notmuch:= )"
+RDEPEND="${DEPEND}"
 BDEPEND="
 	>=app-text/scdoc-1.9.7
 	>=dev-lang/go-1.13
 "
-
-DEPEND="notmuch? ( net-mail/notmuch:= )"
-RDEPEND="${DEPEND}"
 
 src_unpack() {
 	if [[ ${PV} == *9999 ]]; then
@@ -43,8 +37,9 @@ src_unpack() {
 }
 
 src_compile() {
-	use notmuch && export GOFLAGS="-tags=notmuch"
-	emake PREFIX="${EPREFIX}/usr" all
+	LDFLAGS= \
+	emake GOFLAGS="-mod=vendor $(usex notmuch "-tags=notmuch" "")" \
+		PREFIX="${EPREFIX}/usr"VERSION=${PV}  all
 }
 
 src_install() {
@@ -68,4 +63,5 @@ pkg_postinst() {
 			elog "need to use them."
 		fi
 	done
+	xdg_pkg_postinst
 }

commit 558f932af50bc74b2296402ef2dd21cd0dbca238

mail-client/aerc: Add missing space between variables
Fixes: 721c5b6eb1f0 ("mail-client/aerc: sync live")
Signed-off-by: Petr Vaněk <arkamar@atlas.cz>

mail-client/aerc/aerc-9999.ebuild
@@ -39,7 +39,7 @@ src_unpack() {
 src_compile() {
 	LDFLAGS= \
 	emake GOFLAGS="-mod=vendor $(usex notmuch "-tags=notmuch" "")" \
-		PREFIX="${EPREFIX}/usr"VERSION=${PV}  all
+		PREFIX="${EPREFIX}/usr" VERSION=${PV}  all
 }
 
 src_install() {