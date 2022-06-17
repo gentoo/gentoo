gentoo/gentoo

commit 0b69db1f3e1f9a7f502d1d03603dce168ee88a05

dev-ruby/sassc: fix unquoted variable ESYSROOT
Signed-off-by: Thomas Bracht Laumann Jespersen <t@laumann.xyz>
Signed-off-by: Sam James <sam@gentoo.org>

dev-ruby/sassc/sassc-2.4.0-r1.ebuild
@@ -34,7 +34,7 @@ all_ruby_prepare() {
 	# Use unbundled libsass
 	rm -rf ext || die
 
-	sed -i -e '/ffi_lib/ s:__dir__:"'${ESYSROOT}'/usr/'$(get_libdir)'":' \
+	sed -i -e "/ffi_lib/ s:__dir__:${ESYSROOT}/usr/$(get_libdir):" \
 		lib/sassc/native.rb || die
 
 	# Avoid version-specific test so newer libsass versions can be used.

commit 1839af3a287bebfd128b170324d81533402ef6a4

dev-ruby/sassc: Fix quoting of ESYSROOT in sed expression
In a fix for an unquoted ESYSROOT variable, I inadvertently removed the
quotes that were intended to be inserted by the sed expression.

Closes: https://bugs.gentoo.org/852674
Fixes: 0b69db1f3e1 ("dev-ruby/sassc: fix unquoted variable ESYSROOT")
Signed-off-by: Thomas Bracht Laumann Jespersen <t@laumann.xyz>

dev-ruby/sassc/sassc-2.4.0-r1.ebuild
@@ -34,7 +34,7 @@ all_ruby_prepare() {
 	# Use unbundled libsass
 	rm -rf ext || die
 
-	sed -i -e "/ffi_lib/ s:__dir__:${ESYSROOT}/usr/$(get_libdir):" \
+	sed -i -e "/ffi_lib/ s:__dir__:\"${ESYSROOT}/usr/$(get_libdir)\":" \
 		lib/sassc/native.rb || die
 
 	# Avoid version-specific test so newer libsass versions can be used.