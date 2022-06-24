gentoo/gentoo

commit eed2ee4b79e40099d84db0dde29dced1e3346bb6

gnome-base/gnome-shell: Drop old versions
Signed-off-by: Matt Turner <mattst88@gentoo.org>

gnome-base/gnome-shell/files/42.1-polkit-g_autoptr.patch
@@ -1,53 +1,0 @@
-https://gitlab.gnome.org/GNOME/gnome-shell/-/commit/1d0a08b5e25fea7b0e792ec9798e68a7c5606a75
-https://bugs.gentoo.org/844919
-
-From: =?UTF-8?q?Florian=20M=C3=BCllner?= <fmuellner@gnome.org>
-Date: Tue, 17 May 2022 15:36:43 +0200
-Subject: [PATCH] build: Do not redefine polkit autocleanup
-
-PolkitAgent recently added autocleanup functions itself, so check
-for their existence at configure time and only define our own when
-they don't exist upstream.
-
-https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/5490
-
-Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2296>
---- a/config.h.meson
-+++ b/config.h.meson
-@@ -33,3 +33,6 @@
- 
- /* Define if fdwalk is available in libc */
- #mesondefine HAVE_FDWALK
-+
-+/* Define if polkit defines autocleanup functions */
-+#mesondefine HAVE_POLKIT_AUTOCLEANUP
---- a/meson.build
-+++ b/meson.build
-@@ -169,6 +169,13 @@ cdata.set('HAVE_FDWALK',
-           cc.has_function('fdwalk')
- )
- 
-+polkit_has_autocleanup = cc.compiles(
-+  '#define POLKIT_AGENT_I_KNOW_API_IS_SUBJECT_TO_CHANGE
-+  #include <polkitagent/polkitagent.h>
-+  void main(void) { g_autoptr(PolkitAgentListener) agent = NULL; }',
-+  dependencies: polkit_dep)
-+cdata.set('HAVE_POLKIT_AUTOCLEANUP', polkit_has_autocleanup)
-+
- buildtype = get_option('buildtype')
- if buildtype != 'plain'
-   all_warnings = [
---- a/src/shell-polkit-authentication-agent.h
-+++ b/src/shell-polkit-authentication-agent.h
-@@ -14,8 +14,10 @@
- 
- G_BEGIN_DECLS
- 
-+#ifndef HAVE_POLKIT_AUTOCLEANUP
- /* Polkit doesn't have g_autoptr support, thus we have to manually set the autoptr function here */
- G_DEFINE_AUTOPTR_CLEANUP_FUNC (PolkitAgentListener, g_object_unref)
-+#endif
- 
- #define SHELL_TYPE_POLKIT_AUTHENTICATION_AGENT (shell_polkit_authentication_agent_get_type())
- 
-GitLab

gnome-base/gnome-shell/gnome-shell-42.1-r3.ebuild
@@ -1,206 +1,0 @@
-# Copyright 1999-2022 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=8
-PYTHON_COMPAT=( python3_{8..10} )
-
-inherit gnome.org gnome2-utils meson python-single-r1 virtualx xdg
-
-DESCRIPTION="Provides core UI functions for the GNOME desktop"
-HOMEPAGE="https://wiki.gnome.org/Projects/GnomeShell"
-
-LICENSE="GPL-2+ LGPL-2+"
-SLOT="0"
-IUSE="+bluetooth +browser-extension elogind gtk-doc +ibus +networkmanager systemd telepathy test"
-REQUIRED_USE="${PYTHON_REQUIRED_USE}
-	?? ( elogind systemd )"
-RESTRICT="!test? ( test )"
-
-KEYWORDS="~amd64 ~arm ~arm64 ~ppc64 ~riscv ~x86"
-
-# libXfixes-5.0 needed for pointer barriers and #include <X11/extensions/Xfixes.h>
-# FIXME:
-#  * gstreamer/pipewire support is currently automagic
-DEPEND="
-	>=gnome-extra/evolution-data-server-3.33.1:=
-	>=app-crypt/gcr-3.7.5[introspection]
-	>=dev-libs/glib-2.68:2
-	>=dev-libs/gobject-introspection-1.49.1:=
-	>=dev-libs/gjs-1.71.1
-	>=x11-libs/gtk+-3.15.0:3[introspection]
-	>=x11-wm/mutter-42.0:0/10[introspection,test?]
-	>=sys-auth/polkit-0.120_p20220509[introspection]
-	>=gnome-base/gsettings-desktop-schemas-42_beta[introspection]
-	>=x11-libs/startup-notification-0.11
-	>=app-i18n/ibus-1.5.19
-	>=gnome-base/gnome-desktop-3.35.90:3=[introspection]
-	bluetooth? ( net-wireless/gnome-bluetooth:3=[introspection] )
-	>=media-libs/gstreamer-0.11.92:1.0
-	media-libs/gst-plugins-base:1.0
-	>=media-video/pipewire-0.3.0:=
-	networkmanager? (
-		>=net-misc/networkmanager-1.10.4:=[introspection]
-		net-libs/libnma[introspection]
-		>=app-crypt/libsecret-0.18
-		dev-libs/dbus-glib )
-	systemd? ( >=sys-apps/systemd-242
-		>=gnome-base/gnome-desktop-3.34.2:3=[systemd] )
-	elogind? ( >=sys-auth/elogind-237 )
-
-	app-arch/gnome-autoar
-	dev-libs/json-glib
-
-	>=app-accessibility/at-spi2-atk-2.5.3
-	x11-libs/gdk-pixbuf:2[introspection]
-	dev-libs/libxml2:2
-	x11-libs/libX11
-
-	>=media-sound/pulseaudio-2[glib]
-	>=dev-libs/atk-2[introspection]
-	dev-libs/libical:=
-	>=x11-libs/libXfixes-5.0
-
-	gui-libs/gtk:4[introspection]
-
-	${PYTHON_DEPS}
-	$(python_gen_cond_dep '
-		dev-python/pygobject:3[${PYTHON_USEDEP}]
-	')
-	media-libs/mesa[X(+)]
-"
-# Runtime-only deps are probably incomplete and approximate.
-# Introspection deps generated from inspection of the output of:
-#  for i in `rg -INUo 'const(?s).*imports.gi' |cut -d= -f1 |cut -c7- |sort -u`; do echo $i ;done |cut -d, -f1 |sort -u
-# or
-#  rg -INUo 'const(?s).*imports.gi' |cut -d= -f1 |cut -c7- | sed -e 's:[{}]::g' | awk '{$1=$1; print}' | awk -F',' '{$1=$1;print}' | tr ' ' '\n' | sort -u | sed -e 's/://g'
-# These will give a lot of unnecessary things due to greedy matching (TODO), and `(?s).*?` doesn't seem to work as desired.
-# Compare with `grep -rhI 'imports.gi.versions' |sort -u` for any SLOT requirements
-# Each block:
-# 1. Introspection stuff needed via imports.gi (those that build time check may be listed above already)
-# 2. gnome-session needed for shutdown/reboot/inhibitors/etc
-# 3. Control shell settings
-# 4. xdg-utils needed for xdg-open, used by extension tool
-# 5. adwaita-icon-theme needed for various icons & arrows (3.26 for new video-joined-displays-symbolic and co icons; review for 3.28+)
-# 6. mobile-broadband-provider-info, timezone-data for shell-mobile-providers.c  # TODO: Review
-# 7. IBus is needed for nls integration
-# 8. Optional telepathy chat integration
-# 9. Cantarell font used in gnome-shell global CSS (if removing this for some reason, make sure it's pulled in somehow for non-meta users still too)
-# 10. xdg-desktop-portal-gtk for various integration, e.g. #764632
-# 11. TODO: semi-optional webkit-gtk[introspection] for captive portal helper
-RDEPEND="${DEPEND}
-	>=sys-apps/accountsservice-0.6.14[introspection]
-	app-accessibility/at-spi2-core:2[introspection]
-	app-misc/geoclue[introspection]
-	media-libs/graphene[introspection]
-	>=dev-libs/libgweather-4.0.0:4[introspection]
-	x11-libs/pango[introspection]
-	net-libs/libsoup:2.4[introspection]
-	>=sys-power/upower-0.99:=[introspection]
-	gnome-base/librsvg:2[introspection]
-
-	>=gnome-base/gnome-session-2.91.91
-	>=gnome-base/gnome-settings-daemon-3.8.3
-
-	x11-misc/xdg-utils
-
-	>=x11-themes/adwaita-icon-theme-3.26
-
-	networkmanager? (
-		net-misc/mobile-broadband-provider-info
-		sys-libs/timezone-data )
-	ibus? ( >=app-i18n/ibus-1.5.26[gtk3,gtk4,introspection] )
-	telepathy? (
-		>=net-im/telepathy-logger-0.2.4[introspection]
-		>=net-libs/telepathy-glib-0.19[introspection] )
-	media-fonts/cantarell
-
-	|| ( sys-apps/xdg-desktop-portal-gnome <sys-apps/xdg-desktop-portal-gtk-1.14.0 )
-"
-# avoid circular dependency, see bug #546134
-PDEPEND="
-	>=gnome-base/gdm-3.5[introspection(+)]
-	>=gnome-base/gnome-control-center-3.26[bluetooth(+)?,networkmanager(+)?]
-	browser-extension? ( gnome-extra/chrome-gnome-shell )
-"
-BDEPEND="
-	dev-libs/libxslt
-	>=dev-util/gdbus-codegen-2.45.3
-	dev-util/glib-utils
-	gtk-doc? ( >=dev-util/gtk-doc-1.17
-		app-text/docbook-xml-dtd:4.5 )
-	>=sys-devel/gettext-0.19.8
-	virtual/pkgconfig
-"
-# These are not needed from tarballs, unless stylesheets or manpage get patched with patchset:
-# dev-lang/sassc
-# app-text/asciidoc
-
-PATCHES=(
-	# Fix automagic gnome-bluetooth dep, bug #398145
-	"${FILESDIR}"/42.0-optional-bluetooth.patch
-	# Change favorites defaults, bug #479918
-	"${FILESDIR}"/40.0-defaults.patch
-
-	# Upstream backport, bug #844919
-	"${FILESDIR}"/42.1-polkit-g_autoptr.patch
-)
-
-src_prepare() {
-	default
-	xdg_environment_reset
-	# Hack in correct python shebang
-	sed -e "s:python\.full_path():'/usr/bin/env ${EPYTHON}':" -i src/meson.build || die
-}
-
-src_configure() {
-	local emesonargs=(
-		$(meson_use bluetooth)
-		-Dextensions_tool=true
-		-Dextensions_app=true
-		$(meson_use gtk-doc gtk_doc)
-		-Dman=true
-		$(meson_use test tests)
-		$(meson_use networkmanager)
-		$(meson_use systemd) # this controls journald integration and desktop file user services related property only as of 3.34.4
-		# (structured logging and having gnome-shell launched apps use its own identifier instead of gnome-session)
-		# suspend support is runtime optional via /run/systemd/seats presence and org.freedesktop.login1.Manager dbus interface; elogind should provide what's necessary
-		-Dsoup2=true # libslot SLOT needs to match with what libgweather is using
-	)
-	meson_src_configure
-}
-
-src_test() {
-	gnome2_environment_reset # Avoid dconf that looks at XDG_DATA_DIRS, which can sandbox fail if flatpak is installed
-	virtx meson_src_test
-}
-
-pkg_postinst() {
-	xdg_pkg_postinst
-	gnome2_schemas_update
-
-	if ! has_version 'media-libs/gst-plugins-good:1.0' || \
-	   ! has_version 'media-plugins/gst-plugins-vpx:1.0'; then
-		ewarn "To make use of GNOME Shell's built-in screen recording utility,"
-		ewarn "you need to either install media-libs/gst-plugins-good:1.0"
-		ewarn "and media-plugins/gst-plugins-vpx:1.0, or use dconf-editor to change"
-		ewarn "apps.gnome-shell.recorder/pipeline to what you want to use."
-	fi
-
-	if ! has_version "media-libs/mesa[llvm]"; then
-		elog "llvmpipe is used as fallback when no 3D acceleration"
-		elog "is available. You will need to enable llvm USE for"
-		elog "media-libs/mesa if you do not have hardware 3D setup."
-	fi
-
-	# https://bugs.gentoo.org/show_bug.cgi?id=563084
-	# TODO: Is this still the case after various fixed in 3.28 for detecting non-working KMS for wayland (to fall back to X)?
-	if has_version "x11-drivers/nvidia-drivers[-kms]"; then
-		ewarn "You will need to enable kms support in x11-drivers/nvidia-drivers,"
-		ewarn "otherwise Gnome will fail to start"
-	fi
-}
-
-pkg_postrm() {
-	xdg_pkg_postrm
-	gnome2_schemas_update
-}

gnome-base/gnome-shell/Manifest
@@ -1,5 +1,2 @@
-DIST gnome-shell-41.4.tar.xz 1899464 BLAKE2B 202408be8886bb5eb5615328a774993bea8dfe2fb78d304fde2806096ca68eff3fae7d5bdb4972b627cf10192ff76e3d457dc37eaa806cd4ed57d1558c235a48 SHA512 6ed5f215fcf9a1cf450b406620568aeceb3d255e0bb979151ccd2260565748e69825d7f5f7fb0c6768ed584a530599a8dcbc713c1ec12617551ae9df782c5ad7
-DIST gnome-shell-41.5.tar.xz 1899888 BLAKE2B 73491f382413c313956fc6ab878a54d8a881190ee93cac5d334d169636d8dd21557c78719142f717c94463e02c7ca4eaf12a6e4ca6167e48a002ece9379b6d03 SHA512 b638103528413dcfa12d65d0c7add45716d21334294babbadd0a05e3410ca068ae336b28c30eb8811222b9f90e5efdf0d1a6e0a2e3f104b49061bb28333f066e
-DIST gnome-shell-42.0.tar.xz 1918044 BLAKE2B 742d13ad92795fb71bbe8376a86b8e9a828882288f105f026cf98304ef2e9eae8fb967fa978ec322f5c621303a1798d3490c001bbd63c704b0cddc5db688389d SHA512 39b56d3c09a238d9ca387145fdbddfaa929b124d4006f097b792de2dceb882e50cbce1255b0b4585aea04b522a92a4e7a743367f43a863f95bb6386401e73935
 DIST gnome-shell-42.1.tar.xz 1928924 BLAKE2B c8a1aed79a93d713a52ef0161ff9f06c39f7fb9a762086c78505d220e1e8ec43d7a0e376421f912dc91b33da8fd9394c6a233bb2d9d8b361d63ea2ad0406cee6 SHA512 3d27fdbcf5359f631a71b0fa6495fa50e2b43bb2b3c5c0efd740d9a6a88128ae54fe0ba11ea88990ba10ee2fc9f55211d0a21e60beca250bd197535d944d07ed
 DIST gnome-shell-42.2.tar.xz 1928948 BLAKE2B e263acfc9e6929accfb46b65b91b1173cfa4c778405c0852e93b83d78e1bf3a85e3b5c49b73445f604c0fdecb48f294931ab2403806661013bcef3a71b99f911 SHA512 dd77195772e23d8260e55a2db1103fa6dee8945ad168478c293e43e9971698aa191e17f7f5ef46cf46eeaae5051c34f0eb5df134690f7dc5ca98d6ca747e4eb0

gnome-base/gnome-shell/files/40.0-optional-bluetooth.patch
@@ -1,59 +1,0 @@
-From 1ef0033f8148466256b403c9d8c79b9fed1dd343 Mon Sep 17 00:00:00 2001
-From: Mart Raudsepp <leio@gentoo.org>
-Date: Thu, 28 Feb 2019 00:50:19 +0200
-Subject: [PATCH 2/2] build: Make bluetooth support optional
-
----
- js/misc/meson.build | 2 +-
- meson.build         | 7 ++++++-
- meson_options.txt   | 6 ++++++
- 3 files changed, 13 insertions(+), 2 deletions(-)
-
-diff --git a/js/misc/meson.build b/js/misc/meson.build
-index 2702c3dbc..7a6c831b9 100644
---- a/js/misc/meson.build
-+++ b/js/misc/meson.build
-@@ -3,7 +3,7 @@ jsconf.set('PACKAGE_NAME', meson.project_name())
- jsconf.set('PACKAGE_VERSION', meson.project_version())
- jsconf.set('GETTEXT_PACKAGE', meson.project_name())
- jsconf.set('LIBMUTTER_API_VERSION', mutter_api_version)
--jsconf.set10('HAVE_BLUETOOTH', bt_dep.found())
-+jsconf.set10('HAVE_BLUETOOTH', have_bluetooth)
- jsconf.set10('HAVE_NETWORKMANAGER', have_networkmanager)
- jsconf.set('datadir', datadir)
- jsconf.set('libexecdir', libexecdir)
-diff --git a/meson.build b/meson.build
-index 14b75c1c9..8b1c7a380 100644
---- a/meson.build
-+++ b/meson.build
-@@ -94,7 +94,12 @@ x11_dep = dependency('x11')
- schemas_dep = dependency('gsettings-desktop-schemas', version: schemas_req)
- gnome_desktop_dep = dependency('gnome-desktop-3.0', version: gnome_desktop_req)
- 
--bt_dep = dependency('gnome-bluetooth-1.0', version: bt_req, required: false)
-+if get_option('bluetooth')
-+  bt_dep = dependency('gnome-bluetooth-1.0', version: bt_req)
-+  have_bluetooth = true
-+else
-+  have_bluetooth = false
-+endif
- gst_dep = dependency('gstreamer-1.0', version: gst_req, required: false)
- gst_base_dep = dependency('gstreamer-base-1.0', required: false)
- pipewire_dep = dependency('libpipewire-0.3', required: false)
-diff --git a/meson_options.txt b/meson_options.txt
-index ef76b73c3..41cc0ff9d 100644
---- a/meson_options.txt
-+++ b/meson_options.txt
-@@ -1,3 +1,9 @@
-+option('bluetooth',
-+  type: 'boolean',
-+  value: true,
-+  description: 'Enable bluetooth support'
-+)
-+
- option('extensions_tool',
-   type: 'boolean',
-   value: true,
--- 
-2.26.2
-

gnome-base/gnome-shell/files/41.3-fix-build-with-meson-0.61.1.patch
@@ -1,84 +1,0 @@
-https://gitlab.gnome.org/GNOME/gnome-shell/-/commit/65450a836ee9e0722a2d4c3327f52345eae293c6
-https://bugs.gentoo.org/831921
-
-From: =?UTF-8?q?Florian=20M=C3=BCllner?= <fmuellner@gnome.org>
-Date: Thu, 23 Dec 2021 17:18:16 +0100
-Subject: [PATCH] build: Drop incorrect positional arg
-
-Unlike other targets that take a name, i18n.merge_file() does not.
-
-Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2078>
----
- data/meson.build                                      | 2 +-
- src/calendar-server/meson.build                       | 2 +-
- subprojects/extensions-app/data/meson.build           | 2 +-
- subprojects/extensions-app/data/metainfo/meson.build  | 2 +-
- subprojects/extensions-tool/src/templates/meson.build | 2 +-
- 5 files changed, 5 insertions(+), 5 deletions(-)
-
-diff --git a/data/meson.build b/data/meson.build
-index f924fdf806..76ae45c93a 100644
---- a/data/meson.build
-+++ b/data/meson.build
-@@ -16,7 +16,7 @@ desktopconf.set('bindir', bindir)
- desktopconf.set('systemd_hidden', have_systemd ? 'true' : 'false')
- 
- foreach desktop_file : desktop_files
--  i18n.merge_file('desktop',
-+  i18n.merge_file(
-     input: configure_file(
-       input: desktop_file + '.in.in',
-       output: desktop_file + '.in',
-diff --git a/src/calendar-server/meson.build b/src/calendar-server/meson.build
-index 7363282a59..8b4ef411c8 100644
---- a/src/calendar-server/meson.build
-+++ b/src/calendar-server/meson.build
-@@ -27,7 +27,7 @@ configure_file(
-   install_dir: servicedir
- )
- 
--i18n.merge_file('evolution-calendar.desktop',
-+i18n.merge_file(
-   input: 'evolution-calendar.desktop.in',
-   output: 'evolution-calendar.desktop',
-   po_dir: po_dir,
-diff --git a/subprojects/extensions-app/data/meson.build b/subprojects/extensions-app/data/meson.build
-index d7e7d4001c..4b601e8bd1 100644
---- a/subprojects/extensions-app/data/meson.build
-+++ b/subprojects/extensions-app/data/meson.build
-@@ -14,7 +14,7 @@ desktopconf.set('bindir', bindir)
- desktopconf.set('app_id', app_id)
- desktopconf.set('prgname', prgname)
- 
--i18n.merge_file('desktop',
-+i18n.merge_file(
-   input: configure_file(
-     input: base_id + '.desktop.in.in',
-     output: desktop_file + '.in',
-diff --git a/subprojects/extensions-app/data/metainfo/meson.build b/subprojects/extensions-app/data/metainfo/meson.build
-index c4962c0576..a19bfa80a4 100644
---- a/subprojects/extensions-app/data/metainfo/meson.build
-+++ b/subprojects/extensions-app/data/metainfo/meson.build
-@@ -1,5 +1,5 @@
- metainfo = app_id + '.metainfo.xml'
--i18n.merge_file(metainfo,
-+i18n.merge_file(
-   input: base_id + '.metainfo.xml.in',
-   output: metainfo,
-   po_dir: po_dir,
-diff --git a/subprojects/extensions-tool/src/templates/meson.build b/subprojects/extensions-tool/src/templates/meson.build
-index 670e2bf448..d693bfaddb 100644
---- a/subprojects/extensions-tool/src/templates/meson.build
-+++ b/subprojects/extensions-tool/src/templates/meson.build
-@@ -4,7 +4,7 @@ template_metas = [
- ]
- template_deps = []
- foreach template : template_metas
--  template_deps += i18n.merge_file(template,
-+  template_deps += i18n.merge_file(
-     input: template + '.in',
-     output: template,
-     po_dir: po_dir,
--- 
-GitLab
-

gnome-base/gnome-shell/files/41.5-environment-Require-GnomeBluetooth-1.0.patch
@@ -1,35 +1,0 @@
-From 9ce6756235ba272b8602d50d1f005dc2ef265889 Mon Sep 17 00:00:00 2001
-From: Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
-Date: Sun, 31 Oct 2021 17:24:58 -0300
-Subject: [PATCH 1/2] environment: Require GnomeBluetooth 1.0
-
-libgnome-bluetooth will start to offer a 2.0 library version
-depending on GTK4. Given that GNOME Shell already depends on
-GTK3, it cannot use this next version of gnome-bluetooth. And
-since GJS will automatically try and use the latest version
-available of any library, Shell must specify it wants 1.0
-explicitly.
-
-Add a required GnomeBluetooth version number when importing it
-for the status indicator.
-
-Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2015>
----
- js/ui/environment.js | 1 +
- 1 file changed, 1 insertion(+)
-
-diff --git a/js/ui/environment.js b/js/ui/environment.js
-index ef53e6022..e906fa6c6 100644
---- a/js/ui/environment.js
-+++ b/js/ui/environment.js
-@@ -6,6 +6,7 @@ const Config = imports.misc.config;
- imports.gi.versions.Clutter = Config.LIBMUTTER_API_VERSION;
- imports.gi.versions.Gio = '2.0';
- imports.gi.versions.GdkPixbuf = '2.0';
-+imports.gi.versions.GnomeBluetooth = '1.0';
- imports.gi.versions.Gtk = '3.0';
- imports.gi.versions.Soup = '3.0';
- imports.gi.versions.TelepathyGLib = '0.12';
--- 
-2.34.1
-

gnome-base/gnome-shell/files/41.5-environment-Require-gnome-desktop-3.0.patch
@@ -1,30 +1,0 @@
-From 795312b8d5634d3005679b3c1313114e41e538fd Mon Sep 17 00:00:00 2001
-From: Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
-Date: Thu, 11 Nov 2021 16:20:39 -0300
-Subject: [PATCH 2/2] environment: Require gnome-desktop 3.0
-
-Pretty much the exact same case of 9ce6756235.
-
-libgnome-desktop saw a major version bump, and we have to
-stick with 3.0 for a while.
-
-Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2030>
----
- js/ui/environment.js | 1 +
- 1 file changed, 1 insertion(+)
-
-diff --git a/js/ui/environment.js b/js/ui/environment.js
-index e906fa6c6..cb254e319 100644
---- a/js/ui/environment.js
-+++ b/js/ui/environment.js
-@@ -7,6 +7,7 @@ imports.gi.versions.Clutter = Config.LIBMUTTER_API_VERSION;
- imports.gi.versions.Gio = '2.0';
- imports.gi.versions.GdkPixbuf = '2.0';
- imports.gi.versions.GnomeBluetooth = '1.0';
-+imports.gi.versions.GnomeDesktop = '3.0';
- imports.gi.versions.Gtk = '3.0';
- imports.gi.versions.Soup = '3.0';
- imports.gi.versions.TelepathyGLib = '0.12';
--- 
-2.34.1
-

commit dea2c90c1aee7d012a467eeb937fc4a7ecae9237

gnome-base/gnome-shell: restore 42.1-r3 for Polkit fix
Bug: https://bugs.gentoo.org/844919
Bug: https://bugs.gentoo.org/849566
Fixes: eed2ee4b79e40099d84db0dde29dced1e3346bb6
Signed-off-by: Sam James <sam@gentoo.org>

gnome-base/gnome-shell/files/42.1-polkit-g_autoptr.patch
@@ -1,0 +1,53 @@
+https://gitlab.gnome.org/GNOME/gnome-shell/-/commit/1d0a08b5e25fea7b0e792ec9798e68a7c5606a75
+https://bugs.gentoo.org/844919
+
+From: =?UTF-8?q?Florian=20M=C3=BCllner?= <fmuellner@gnome.org>
+Date: Tue, 17 May 2022 15:36:43 +0200
+Subject: [PATCH] build: Do not redefine polkit autocleanup
+
+PolkitAgent recently added autocleanup functions itself, so check
+for their existence at configure time and only define our own when
+they don't exist upstream.
+
+https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/5490
+
+Part-of: <https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2296>
+--- a/config.h.meson
++++ b/config.h.meson
+@@ -33,3 +33,6 @@
+ 
+ /* Define if fdwalk is available in libc */
+ #mesondefine HAVE_FDWALK
++
++/* Define if polkit defines autocleanup functions */
++#mesondefine HAVE_POLKIT_AUTOCLEANUP
+--- a/meson.build
++++ b/meson.build
+@@ -169,6 +169,13 @@ cdata.set('HAVE_FDWALK',
+           cc.has_function('fdwalk')
+ )
+ 
++polkit_has_autocleanup = cc.compiles(
++  '#define POLKIT_AGENT_I_KNOW_API_IS_SUBJECT_TO_CHANGE
++  #include <polkitagent/polkitagent.h>
++  void main(void) { g_autoptr(PolkitAgentListener) agent = NULL; }',
++  dependencies: polkit_dep)
++cdata.set('HAVE_POLKIT_AUTOCLEANUP', polkit_has_autocleanup)
++
+ buildtype = get_option('buildtype')
+ if buildtype != 'plain'
+   all_warnings = [
+--- a/src/shell-polkit-authentication-agent.h
++++ b/src/shell-polkit-authentication-agent.h
+@@ -14,8 +14,10 @@
+ 
+ G_BEGIN_DECLS
+ 
++#ifndef HAVE_POLKIT_AUTOCLEANUP
+ /* Polkit doesn't have g_autoptr support, thus we have to manually set the autoptr function here */
+ G_DEFINE_AUTOPTR_CLEANUP_FUNC (PolkitAgentListener, g_object_unref)
++#endif
+ 
+ #define SHELL_TYPE_POLKIT_AUTHENTICATION_AGENT (shell_polkit_authentication_agent_get_type())
+ 
+GitLab

gnome-base/gnome-shell/gnome-shell-42.1-r3.ebuild
@@ -1,0 +1,206 @@
+# Copyright 1999-2022 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+PYTHON_COMPAT=( python3_{8..10} )
+
+inherit gnome.org gnome2-utils meson python-single-r1 virtualx xdg
+
+DESCRIPTION="Provides core UI functions for the GNOME desktop"
+HOMEPAGE="https://wiki.gnome.org/Projects/GnomeShell"
+
+LICENSE="GPL-2+ LGPL-2+"
+SLOT="0"
+IUSE="+bluetooth +browser-extension elogind gtk-doc +ibus +networkmanager systemd telepathy test"
+REQUIRED_USE="${PYTHON_REQUIRED_USE}
+	?? ( elogind systemd )"
+RESTRICT="!test? ( test )"
+
+KEYWORDS="~amd64 ~arm ~arm64 ~ppc64 ~riscv ~x86"
+
+# libXfixes-5.0 needed for pointer barriers and #include <X11/extensions/Xfixes.h>
+# FIXME:
+#  * gstreamer/pipewire support is currently automagic
+DEPEND="
+	>=gnome-extra/evolution-data-server-3.33.1:=
+	>=app-crypt/gcr-3.7.5[introspection]
+	>=dev-libs/glib-2.68:2
+	>=dev-libs/gobject-introspection-1.49.1:=
+	>=dev-libs/gjs-1.71.1
+	>=x11-libs/gtk+-3.15.0:3[introspection]
+	>=x11-wm/mutter-42.0:0/10[introspection,test?]
+	>=sys-auth/polkit-0.120_p20220509[introspection]
+	>=gnome-base/gsettings-desktop-schemas-42_beta[introspection]
+	>=x11-libs/startup-notification-0.11
+	>=app-i18n/ibus-1.5.19
+	>=gnome-base/gnome-desktop-3.35.90:3=[introspection]
+	bluetooth? ( net-wireless/gnome-bluetooth:3=[introspection] )
+	>=media-libs/gstreamer-0.11.92:1.0
+	media-libs/gst-plugins-base:1.0
+	>=media-video/pipewire-0.3.0:=
+	networkmanager? (
+		>=net-misc/networkmanager-1.10.4:=[introspection]
+		net-libs/libnma[introspection]
+		>=app-crypt/libsecret-0.18
+		dev-libs/dbus-glib )
+	systemd? ( >=sys-apps/systemd-242
+		>=gnome-base/gnome-desktop-3.34.2:3=[systemd] )
+	elogind? ( >=sys-auth/elogind-237 )
+
+	app-arch/gnome-autoar
+	dev-libs/json-glib
+
+	>=app-accessibility/at-spi2-atk-2.5.3
+	x11-libs/gdk-pixbuf:2[introspection]
+	dev-libs/libxml2:2
+	x11-libs/libX11
+
+	>=media-sound/pulseaudio-2[glib]
+	>=dev-libs/atk-2[introspection]
+	dev-libs/libical:=
+	>=x11-libs/libXfixes-5.0
+
+	gui-libs/gtk:4[introspection]
+
+	${PYTHON_DEPS}
+	$(python_gen_cond_dep '
+		dev-python/pygobject:3[${PYTHON_USEDEP}]
+	')
+	media-libs/mesa[X(+)]
+"
+# Runtime-only deps are probably incomplete and approximate.
+# Introspection deps generated from inspection of the output of:
+#  for i in `rg -INUo 'const(?s).*imports.gi' |cut -d= -f1 |cut -c7- |sort -u`; do echo $i ;done |cut -d, -f1 |sort -u
+# or
+#  rg -INUo 'const(?s).*imports.gi' |cut -d= -f1 |cut -c7- | sed -e 's:[{}]::g' | awk '{$1=$1; print}' | awk -F',' '{$1=$1;print}' | tr ' ' '\n' | sort -u | sed -e 's/://g'
+# These will give a lot of unnecessary things due to greedy matching (TODO), and `(?s).*?` doesn't seem to work as desired.
+# Compare with `grep -rhI 'imports.gi.versions' |sort -u` for any SLOT requirements
+# Each block:
+# 1. Introspection stuff needed via imports.gi (those that build time check may be listed above already)
+# 2. gnome-session needed for shutdown/reboot/inhibitors/etc
+# 3. Control shell settings
+# 4. xdg-utils needed for xdg-open, used by extension tool
+# 5. adwaita-icon-theme needed for various icons & arrows (3.26 for new video-joined-displays-symbolic and co icons; review for 3.28+)
+# 6. mobile-broadband-provider-info, timezone-data for shell-mobile-providers.c  # TODO: Review
+# 7. IBus is needed for nls integration
+# 8. Optional telepathy chat integration
+# 9. Cantarell font used in gnome-shell global CSS (if removing this for some reason, make sure it's pulled in somehow for non-meta users still too)
+# 10. xdg-desktop-portal-gtk for various integration, e.g. #764632
+# 11. TODO: semi-optional webkit-gtk[introspection] for captive portal helper
+RDEPEND="${DEPEND}
+	>=sys-apps/accountsservice-0.6.14[introspection]
+	app-accessibility/at-spi2-core:2[introspection]
+	app-misc/geoclue[introspection]
+	media-libs/graphene[introspection]
+	>=dev-libs/libgweather-4.0.0:4[introspection]
+	x11-libs/pango[introspection]
+	net-libs/libsoup:2.4[introspection]
+	>=sys-power/upower-0.99:=[introspection]
+	gnome-base/librsvg:2[introspection]
+
+	>=gnome-base/gnome-session-2.91.91
+	>=gnome-base/gnome-settings-daemon-3.8.3
+
+	x11-misc/xdg-utils
+
+	>=x11-themes/adwaita-icon-theme-3.26
+
+	networkmanager? (
+		net-misc/mobile-broadband-provider-info
+		sys-libs/timezone-data )
+	ibus? ( >=app-i18n/ibus-1.5.26[gtk3,gtk4,introspection] )
+	telepathy? (
+		>=net-im/telepathy-logger-0.2.4[introspection]
+		>=net-libs/telepathy-glib-0.19[introspection] )
+	media-fonts/cantarell
+
+	|| ( sys-apps/xdg-desktop-portal-gnome <sys-apps/xdg-desktop-portal-gtk-1.14.0 )
+"
+# avoid circular dependency, see bug #546134
+PDEPEND="
+	>=gnome-base/gdm-3.5[introspection(+)]
+	>=gnome-base/gnome-control-center-3.26[bluetooth(+)?,networkmanager(+)?]
+	browser-extension? ( gnome-extra/chrome-gnome-shell )
+"
+BDEPEND="
+	dev-libs/libxslt
+	>=dev-util/gdbus-codegen-2.45.3
+	dev-util/glib-utils
+	gtk-doc? ( >=dev-util/gtk-doc-1.17
+		app-text/docbook-xml-dtd:4.5 )
+	>=sys-devel/gettext-0.19.8
+	virtual/pkgconfig
+"
+# These are not needed from tarballs, unless stylesheets or manpage get patched with patchset:
+# dev-lang/sassc
+# app-text/asciidoc
+
+PATCHES=(
+	# Fix automagic gnome-bluetooth dep, bug #398145
+	"${FILESDIR}"/42.0-optional-bluetooth.patch
+	# Change favorites defaults, bug #479918
+	"${FILESDIR}"/40.0-defaults.patch
+
+	# Upstream backport, bug #844919
+	"${FILESDIR}"/42.1-polkit-g_autoptr.patch
+)
+
+src_prepare() {
+	default
+	xdg_environment_reset
+	# Hack in correct python shebang
+	sed -e "s:python\.full_path():'/usr/bin/env ${EPYTHON}':" -i src/meson.build || die
+}
+
+src_configure() {
+	local emesonargs=(
+		$(meson_use bluetooth)
+		-Dextensions_tool=true
+		-Dextensions_app=true
+		$(meson_use gtk-doc gtk_doc)
+		-Dman=true
+		$(meson_use test tests)
+		$(meson_use networkmanager)
+		$(meson_use systemd) # this controls journald integration and desktop file user services related property only as of 3.34.4
+		# (structured logging and having gnome-shell launched apps use its own identifier instead of gnome-session)
+		# suspend support is runtime optional via /run/systemd/seats presence and org.freedesktop.login1.Manager dbus interface; elogind should provide what's necessary
+		-Dsoup2=true # libslot SLOT needs to match with what libgweather is using
+	)
+	meson_src_configure
+}
+
+src_test() {
+	gnome2_environment_reset # Avoid dconf that looks at XDG_DATA_DIRS, which can sandbox fail if flatpak is installed
+	virtx meson_src_test
+}
+
+pkg_postinst() {
+	xdg_pkg_postinst
+	gnome2_schemas_update
+
+	if ! has_version 'media-libs/gst-plugins-good:1.0' || \
+	   ! has_version 'media-plugins/gst-plugins-vpx:1.0'; then
+		ewarn "To make use of GNOME Shell's built-in screen recording utility,"
+		ewarn "you need to either install media-libs/gst-plugins-good:1.0"
+		ewarn "and media-plugins/gst-plugins-vpx:1.0, or use dconf-editor to change"
+		ewarn "apps.gnome-shell.recorder/pipeline to what you want to use."
+	fi
+
+	if ! has_version "media-libs/mesa[llvm]"; then
+		elog "llvmpipe is used as fallback when no 3D acceleration"
+		elog "is available. You will need to enable llvm USE for"
+		elog "media-libs/mesa if you do not have hardware 3D setup."
+	fi
+
+	# https://bugs.gentoo.org/show_bug.cgi?id=563084
+	# TODO: Is this still the case after various fixed in 3.28 for detecting non-working KMS for wayland (to fall back to X)?
+	if has_version "x11-drivers/nvidia-drivers[-kms]"; then
+		ewarn "You will need to enable kms support in x11-drivers/nvidia-drivers,"
+		ewarn "otherwise Gnome will fail to start"
+	fi
+}
+
+pkg_postrm() {
+	xdg_pkg_postrm
+	gnome2_schemas_update
+}