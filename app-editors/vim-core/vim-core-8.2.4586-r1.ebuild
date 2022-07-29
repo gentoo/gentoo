gentoo/gentoo

commit 5e9a3926fd3e0e573f529fd6aefebba53e082f4a

app-editors/vim-core: fix incorrect conditional use of extglob
bash-5.2 is stricter with this and upstream say this was never
supposed to work (and indeed may have done odd things at runtime
anyway).

We're going to have to avoid any conditional use of extglob
like this as the parser has no idea if we've enabled extglob
or not at the point it runs.

Fixes a sourcing error:
```
vim-core-9999.ebuild: line 195: syntax error near unexpected token `('
vim-core-9999.ebuild: line 195: `               ignore=$(rm -fr "${ED}${vimfiles}"/colors/!(${keep_colors}).vim )'
```

Thanks-to: tirnanog (mangled his suggestion in this commit)
Thanks-to: Ionen Wolkens <ionen@gentoo.org>
Signed-off-by: Sam James <sam@gentoo.org>

app-editors/vim-core/vim-core-9999.ebuild
@@ -6,7 +6,7 @@ EAPI=8
 # Please bump with app-editors/vim and app-editors/gvim
 
 VIM_VERSION="9.0"
-inherit estack vim-doc flag-o-matic bash-completion-r1 prefix desktop xdg-utils
+inherit vim-doc flag-o-matic bash-completion-r1 prefix desktop xdg-utils
 
 if [[ ${PV} == 9999* ]] ; then
 	inherit git-r3
@@ -185,24 +185,21 @@ src_install() {
 	if use minimal; then
 		# To save space, install only a subset of the files.
 		# Helps minimalize the livecd, bug 65144.
-		eshopts_push -s extglob
+		rm -rv "${ED}${vimfiles}"/{compiler,doc,ftplugin,indent} || die
+		rm -rv "${ED}${vimfiles}"/{macros,print,tools,tutor} || die
+		rm -v "${ED}"/usr/bin/vimtutor || die
 
-		rm -rv "${ED}${vimfiles}"/{compiler,doc,ftplugin,indent} || die "rm failed"
-		rm -rv "${ED}${vimfiles}"/{macros,print,tools,tutor} || die "rm failed"
-		rm -v "${ED}"/usr/bin/vimtutor || die "rm failed"
+		for f in "${ED}${vimfiles}"/colors/*.vim; do
+			if [[ ${f} != */@(default).vim ]] ; then
+				printf '%s\0' "${f}"
+			fi
+		done | xargs -0 rm -f || die
 
-		local keep_colors="default"
-		ignore=$(rm -fr "${ED}${vimfiles}"/colors/!(${keep_colors}).vim )
-
-		local keep_syntax="conf|crontab|fstab|inittab|resolv|sshdconfig"
-		# tinkering with the next line might make bad things happen ...
-		keep_syntax="${keep_syntax}|syntax|nosyntax|synload"
-		ignore=$(rm -fr "${ED}${vimfiles}"/syntax/!(${keep_syntax}).vim )
-
-		# Delete skip_defaults_vim config not supported by vim[minimal]
-		sed -i '/skip_defaults_vim/d' "${ED}"/etc/vim/vimrc || die "sed failed"
-
-		eshopts_pop
+		for f in "${ED}${vimfiles}"/syntax/*.vim; do
+			if [[ ${f} != */@(conf|crontab|fstab|inittab|resolv|sshdconfig|syntax|nosyntax|synload).vim ]] ; then
+				printf '%s\0' "${f}"
+			fi
+		done | xargs -0 rm -f || die
 	fi
 
 	newbashcomp "${FILESDIR}"/xxd-completion xxd

app-editors/vim-core/vim-core-8.2.4586.ebuild
@@ -6,7 +6,7 @@ EAPI=7
 # Please bump with app-editors/vim and app-editors/gvim
 
 VIM_VERSION="8.2"
-inherit estack vim-doc flag-o-matic bash-completion-r1 prefix desktop xdg-utils
+inherit vim-doc flag-o-matic bash-completion-r1 prefix desktop xdg-utils
 
 if [[ ${PV} == 9999* ]] ; then
 	inherit git-r3
@@ -188,24 +188,24 @@ src_install() {
 	if use minimal; then
 		# To save space, install only a subset of the files.
 		# Helps minimalize the livecd, bug 65144.
-		eshopts_push -s extglob
+		rm -rv "${ED}${vimfiles}"/{compiler,doc,ftplugin,indent} || die
+		rm -rv "${ED}${vimfiles}"/{macros,print,tools,tutor} || die
+		rm -v "${ED}"/usr/bin/vimtutor || die
 
-		rm -rv "${ED}${vimfiles}"/{compiler,doc,ftplugin,indent} || die "rm failed"
-		rm -rv "${ED}${vimfiles}"/{macros,print,tools,tutor} || die "rm failed"
-		rm -v "${ED}"/usr/bin/vimtutor || die "rm failed"
+		for f in "${ED}${vimfiles}"/colors/*.vim; do
+			if [[ ${f} != */@(default).vim ]] ; then
+				printf '%s\0' "${f}"
+			fi
+		done | xargs -0 rm -f || die
 
-		local keep_colors="default"
-		ignore=$(rm -fr "${ED}${vimfiles}"/colors/!(${keep_colors}).vim )
-
-		local keep_syntax="conf|crontab|fstab|inittab|resolv|sshdconfig"
-		# tinkering with the next line might make bad things happen ...
-		keep_syntax="${keep_syntax}|syntax|nosyntax|synload"
-		ignore=$(rm -fr "${ED}${vimfiles}"/syntax/!(${keep_syntax}).vim )
+		for f in "${ED}${vimfiles}"/syntax/*.vim; do
+			if [[ ${f} != */@(conf|crontab|fstab|inittab|resolv|sshdconfig|syntax|nosyntax|synload).vim ]] ; then
+				printf '%s\0' "${f}"
+			fi
+		done | xargs -0 rm -f || die
 
 		# Delete skip_defaults_vim config not supported by vim[minimal]
-		sed -i '/skip_defaults_vim/d' "${ED}"/etc/vim/vimrc || die "sed failed"
-
-		eshopts_pop
+		sed -i '/skip_defaults_vim/d' "${ED}"/etc/vim/vimrc || die
 	fi
 
 	newbashcomp "${FILESDIR}"/xxd-completion xxd

app-editors/vim-core/vim-core-8.2.5066-r2.ebuild
@@ -6,7 +6,7 @@ EAPI=8
 # Please bump with app-editors/vim and app-editors/gvim
 
 VIM_VERSION="8.2"
-inherit estack vim-doc flag-o-matic bash-completion-r1 prefix desktop xdg-utils
+inherit vim-doc flag-o-matic bash-completion-r1 prefix desktop xdg-utils
 
 if [[ ${PV} == 9999* ]] ; then
 	inherit git-r3
@@ -189,24 +189,21 @@ src_install() {
 	if use minimal; then
 		# To save space, install only a subset of the files.
 		# Helps minimalize the livecd, bug 65144.
-		eshopts_push -s extglob
+		rm -rv "${ED}${vimfiles}"/{compiler,doc,ftplugin,indent} || die
+		rm -rv "${ED}${vimfiles}"/{macros,print,tools,tutor} || die
+		rm -v "${ED}"/usr/bin/vimtutor || die
 
-		rm -rv "${ED}${vimfiles}"/{compiler,doc,ftplugin,indent} || die "rm failed"
-		rm -rv "${ED}${vimfiles}"/{macros,print,tools,tutor} || die "rm failed"
-		rm -v "${ED}"/usr/bin/vimtutor || die "rm failed"
+		for f in "${ED}${vimfiles}"/colors/*.vim; do
+			if [[ ${f} != */@(default).vim ]] ; then
+				printf '%s\0' "${f}"
+			fi
+		done | xargs -0 rm -f || die
 
-		local keep_colors="default"
-		ignore=$(rm -fr "${ED}${vimfiles}"/colors/!(${keep_colors}).vim )
-
-		local keep_syntax="conf|crontab|fstab|inittab|resolv|sshdconfig"
-		# tinkering with the next line might make bad things happen ...
-		keep_syntax="${keep_syntax}|syntax|nosyntax|synload"
-		ignore=$(rm -fr "${ED}${vimfiles}"/syntax/!(${keep_syntax}).vim )
-
-		# Delete skip_defaults_vim config not supported by vim[minimal]
-		sed -i '/skip_defaults_vim/d' "${ED}"/etc/vim/vimrc || die "sed failed"
-
-		eshopts_pop
+		for f in "${ED}${vimfiles}"/syntax/*.vim; do
+			if [[ ${f} != */@(conf|crontab|fstab|inittab|resolv|sshdconfig|syntax|nosyntax|synload).vim ]] ; then
+				printf '%s\0' "${f}"
+			fi
+		done | xargs -0 rm -f || die
 	fi
 
 	newbashcomp "${FILESDIR}"/xxd-completion xxd

app-editors/vim-core/vim-core-9.0.0049-r1.ebuild
@@ -6,7 +6,7 @@ EAPI=8
 # Please bump with app-editors/vim and app-editors/gvim
 
 VIM_VERSION="9.0"
-inherit estack vim-doc flag-o-matic bash-completion-r1 prefix desktop xdg-utils
+inherit vim-doc flag-o-matic bash-completion-r1 prefix desktop xdg-utils
 
 if [[ ${PV} == 9999* ]] ; then
 	inherit git-r3
@@ -185,24 +185,21 @@ src_install() {
 	if use minimal; then
 		# To save space, install only a subset of the files.
 		# Helps minimalize the livecd, bug 65144.
-		eshopts_push -s extglob
+		rm -rv "${ED}${vimfiles}"/{compiler,doc,ftplugin,indent} || die
+		rm -rv "${ED}${vimfiles}"/{macros,print,tools,tutor} || die
+		rm -v "${ED}"/usr/bin/vimtutor || die
 
-		rm -rv "${ED}${vimfiles}"/{compiler,doc,ftplugin,indent} || die "rm failed"
-		rm -rv "${ED}${vimfiles}"/{macros,print,tools,tutor} || die "rm failed"
-		rm -v "${ED}"/usr/bin/vimtutor || die "rm failed"
+		for f in "${ED}${vimfiles}"/colors/*.vim; do
+			if [[ ${f} != */@(default).vim ]] ; then
+				printf '%s\0' "${f}"
+			fi
+		done | xargs -0 rm -f || die
 
-		local keep_colors="default"
-		ignore=$(rm -fr "${ED}${vimfiles}"/colors/!(${keep_colors}).vim )
-
-		local keep_syntax="conf|crontab|fstab|inittab|resolv|sshdconfig"
-		# tinkering with the next line might make bad things happen ...
-		keep_syntax="${keep_syntax}|syntax|nosyntax|synload"
-		ignore=$(rm -fr "${ED}${vimfiles}"/syntax/!(${keep_syntax}).vim )
-
-		# Delete skip_defaults_vim config not supported by vim[minimal]
-		sed -i '/skip_defaults_vim/d' "${ED}"/etc/vim/vimrc || die "sed failed"
-
-		eshopts_pop
+		for f in "${ED}${vimfiles}"/syntax/*.vim; do
+			if [[ ${f} != */@(conf|crontab|fstab|inittab|resolv|sshdconfig|syntax|nosyntax|synload).vim ]] ; then
+				printf '%s\0' "${f}"
+			fi
+		done | xargs -0 rm -f || die
 	fi
 
 	newbashcomp "${FILESDIR}"/xxd-completion xxd

commit 84f989fbd929cacc1668e52b370639aae08942fa

app-editors/vim-core: Don't try to assign to g:skip_defaults_vim for minimal builds
A recent adjustment to some of the vim-core ebuilds accidentally removed a sed
command that removes the following line from /etc/vim/vimrc, provided that the
"minimal" USE flag was in effect.

let g:skip_defaults_vim = 1

The reason for doing so is that this assignment raises an error in the case of
a minimal build. Rather than re-instate the missing sed commands, let's update
the vimrc template so as to employ a feature guard, relying on the fact that
minimal builds lack the +eval feature.

Fixes: 5e9a3926fd3e0e573f529fd6aefebba53e082f4a
Signed-off-by: Kerin Millar <kfm@plushkava.net>
Signed-off-by: Sam James <sam@gentoo.org>

app-editors/vim-core/files/vimrc-r6
@@ -190,8 +190,11 @@ endif " has("autocmd")
 " }}}
 
 " We don't want VIM to load their own built-in defaults, preferring ours here
-" instead:
-let g:skip_defaults_vim = 1
+" instead. This option cannot apply to minimal builds, so it is guarded by a
+" test that's guaranteed to fail for those, owing to the lack of +eval.
+if 1
+  let g:skip_defaults_vim = 1
+endif
 
 " Enable Omni completion when opening a file only if a specific plugin does
 " not already exist for that filetype. This allows Omni completion

app-editors/vim-core/vim-core-8.2.4586-r1.ebuild
@@ -1,7 +1,7 @@
 # Copyright 1999-2022 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-EAPI=8
+EAPI=7
 
 # Please bump with app-editors/vim and app-editors/gvim
 
@@ -14,8 +14,8 @@ if [[ ${PV} == 9999* ]] ; then
 	EGIT_CHECKOUT_DIR=${WORKDIR}/vim-${PV}
 else
 	SRC_URI="https://github.com/vim/vim/archive/v${PV}.tar.gz -> vim-${PV}.tar.gz
-		https://dev.gentoo.org/~mattst88/distfiles/vim-8.2.5066-gentoo-patches.tar.xz"
-	KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~m68k ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86 ~x64-cygwin ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~sparc-solaris ~sparc64-solaris ~x64-solaris ~x86-solaris"
+		https://dev.gentoo.org/~zlogene/distfiles/app-editors/vim/vim-8.2.0360-gentoo-patches.tar.xz"
+	KEYWORDS="~alpha amd64 arm arm64 hppa ~ia64 ~loong ~m68k ~mips ppc ppc64 ~riscv ~s390 sparc x86 ~x64-cygwin ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~sparc-solaris ~sparc64-solaris ~x64-solaris ~x86-solaris"
 fi
 S="${WORKDIR}"/vim-${PV}
 
@@ -26,11 +26,10 @@ LICENSE="vim"
 SLOT="0"
 IUSE="nls acl minimal"
 
-# ncurses is only needed by ./configure, so no subslot operator required
-DEPEND=">=sys-libs/ncurses-5.2-r2:0"
 BDEPEND="sys-devel/autoconf"
 # Avoid icon file collision, bug #673880
-RDEPEND="!<app-editors/vim-8.2.4328-r1"
+RDEPEND="!!<app-editors/gvim-8.1.0648
+	!<app-editors/vim-8.2.4328-r1"
 PDEPEND="!minimal? ( app-vim/gentoo-syntax )"
 
 pkg_setup() {
@@ -81,7 +80,7 @@ src_prepare() {
 	# which isn't even in the source file being invalid, we'll do some trickery
 	# to make the error never occur. bug 66162 (02 October 2004 ciaranm)
 	find "${S}" -name '*.c' | while read c; do
-		echo >> "$c" || die "echo failed"
+	    echo >> "$c" || die "echo failed"
 	done
 
 	# Try to avoid sandbox problems. Bug #114475.
@@ -183,7 +182,7 @@ src_install() {
 	# default vimrc is installed by vim-core since it applies to
 	# both vim and gvim
 	insinto /etc/vim/
-	newins "${FILESDIR}"/vimrc-r5 vimrc
+	newins "${FILESDIR}"/vimrc-r6 vimrc
 	eprefixify "${ED}"/etc/vim/vimrc
 
 	if use minimal; then

app-editors/vim-core/vim-core-8.2.5066-r3.ebuild
@@ -1,7 +1,7 @@
 # Copyright 1999-2022 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-EAPI=7
+EAPI=8
 
 # Please bump with app-editors/vim and app-editors/gvim
 
@@ -14,8 +14,8 @@ if [[ ${PV} == 9999* ]] ; then
 	EGIT_CHECKOUT_DIR=${WORKDIR}/vim-${PV}
 else
 	SRC_URI="https://github.com/vim/vim/archive/v${PV}.tar.gz -> vim-${PV}.tar.gz
-		https://dev.gentoo.org/~zlogene/distfiles/app-editors/vim/vim-8.2.0360-gentoo-patches.tar.xz"
-	KEYWORDS="~alpha amd64 arm arm64 hppa ~ia64 ~loong ~m68k ~mips ppc ppc64 ~riscv ~s390 sparc x86 ~x64-cygwin ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~sparc-solaris ~sparc64-solaris ~x64-solaris ~x86-solaris"
+		https://dev.gentoo.org/~mattst88/distfiles/vim-8.2.5066-gentoo-patches.tar.xz"
+	KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~m68k ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86 ~x64-cygwin ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~sparc-solaris ~sparc64-solaris ~x64-solaris ~x86-solaris"
 fi
 S="${WORKDIR}"/vim-${PV}
 
@@ -26,10 +26,11 @@ LICENSE="vim"
 SLOT="0"
 IUSE="nls acl minimal"
 
+# ncurses is only needed by ./configure, so no subslot operator required
+DEPEND=">=sys-libs/ncurses-5.2-r2:0"
 BDEPEND="sys-devel/autoconf"
 # Avoid icon file collision, bug #673880
-RDEPEND="!!<app-editors/gvim-8.1.0648
-	!<app-editors/vim-8.2.4328-r1"
+RDEPEND="!<app-editors/vim-8.2.4328-r1"
 PDEPEND="!minimal? ( app-vim/gentoo-syntax )"
 
 pkg_setup() {
@@ -80,7 +81,7 @@ src_prepare() {
 	# which isn't even in the source file being invalid, we'll do some trickery
 	# to make the error never occur. bug 66162 (02 October 2004 ciaranm)
 	find "${S}" -name '*.c' | while read c; do
-	    echo >> "$c" || die "echo failed"
+		echo >> "$c" || die "echo failed"
 	done
 
 	# Try to avoid sandbox problems. Bug #114475.
@@ -182,7 +183,7 @@ src_install() {
 	# default vimrc is installed by vim-core since it applies to
 	# both vim and gvim
 	insinto /etc/vim/
-	newins "${FILESDIR}"/vimrc-r5 vimrc
+	newins "${FILESDIR}"/vimrc-r6 vimrc
 	eprefixify "${ED}"/etc/vim/vimrc
 
 	if use minimal; then
@@ -203,9 +204,6 @@ src_install() {
 				printf '%s\0' "${f}"
 			fi
 		done | xargs -0 rm -f || die
-
-		# Delete skip_defaults_vim config not supported by vim[minimal]
-		sed -i '/skip_defaults_vim/d' "${ED}"/etc/vim/vimrc || die
 	fi
 
 	newbashcomp "${FILESDIR}"/xxd-completion xxd

app-editors/vim-core/vim-core-9.0.0049-r2.ebuild
@@ -179,7 +179,7 @@ src_install() {
 	# default vimrc is installed by vim-core since it applies to
 	# both vim and gvim
 	insinto /etc/vim/
-	newins "${FILESDIR}"/vimrc-r5 vimrc
+	newins "${FILESDIR}"/vimrc-r6 vimrc
 	eprefixify "${ED}"/etc/vim/vimrc
 
 	if use minimal; then

app-editors/vim-core/vim-core-9999.ebuild
@@ -179,7 +179,7 @@ src_install() {
 	# default vimrc is installed by vim-core since it applies to
 	# both vim and gvim
 	insinto /etc/vim/
-	newins "${FILESDIR}"/vimrc-r5 vimrc
+	newins "${FILESDIR}"/vimrc-r6 vimrc
 	eprefixify "${ED}"/etc/vim/vimrc
 
 	if use minimal; then