gentoo/gentoo

commit b88db9002a8eb1fb3f8a161111c57912955e5f7c

net-im/bitlbee: Install pkgconfig to /usr/$(get_libdir)/pkgconfig
This commit indirectly solves issues #678128 and #678510 because
pkg-config searches bitlbee.pc in /usr/lib64/pkgconfig by default for
ARCH=amd64.

Closes: https://bugs.gentoo.org/678128
Closes: https://bugs.gentoo.org/678510
Package-Manager: Portage-2.3.76, Repoman-2.3.16
Signed-off-by: Petr Vaněk <arkamar@atlas.cz>
Closes: https://github.com/gentoo/gentoo/pull/13554
Signed-off-by: Joonas Niilola <juippis@gentoo.org>

net-im/bitlbee/bitlbee-9999.ebuild
@@ -109,6 +109,7 @@ src_configure() {
 		--datadir=/usr/share/bitlbee \
 		--etcdir=/etc/bitlbee \
 		--plugindir=/usr/$(get_libdir)/bitlbee \
+		--pcdir=/usr/$(get_libdir)/pkgconfig \
 		--systemdsystemunitdir=$(systemd_get_systemunitdir) \
 		--doc=1 \
 		--strip=0 \

net-im/bitlbee/bitlbee-3.5.1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2017 Gentoo Foundation
+# Copyright 1999-2019 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=6
@@ -114,6 +114,7 @@ src_configure() {
 		--datadir=/usr/share/bitlbee \
 		--etcdir=/etc/bitlbee \
 		--plugindir=/usr/$(get_libdir)/bitlbee \
+		--pcdir=/usr/$(get_libdir)/pkgconfig \
 		--systemdsystemunitdir=$(systemd_get_systemunitdir) \
 		--doc=1 \
 		--strip=0 \

net-im/bitlbee/bitlbee-3.6.ebuild
@@ -109,6 +109,7 @@ src_configure() {
 		--datadir=/usr/share/bitlbee \
 		--etcdir=/etc/bitlbee \
 		--plugindir=/usr/$(get_libdir)/bitlbee \
+		--pcdir=/usr/$(get_libdir)/pkgconfig \
 		--systemdsystemunitdir=$(systemd_get_systemunitdir) \
 		--doc=1 \
 		--strip=0 \

commit b4b79322f9a289ec648933f7da47e34a24db65ef

net-im/bitlbee: set correct --libdir
The libdir is currently not used to install any files, however, it is
used in generated bitlbee.pc, where it should point to correct directory.

Closes: https://bugs.gentoo.org/861383
Fixes: b88db9002a8e ("net-im/bitlbee: Install pkgconfig to /usr/$(get_libdir)/pkgconfig")
Signed-off-by: Petr Vaněk <arkamar@atlas.cz>

net-im/bitlbee/bitlbee-3.6-r2.ebuild
@@ -115,6 +115,7 @@ src_configure() {
 		--prefix=/usr \
 		--datadir=/usr/share/bitlbee \
 		--etcdir=/etc/bitlbee \
+		--libdir=/usr/$(get_libdir) \
 		--plugindir=/usr/$(get_libdir)/bitlbee \
 		--pcdir=/usr/$(get_libdir)/pkgconfig \
 		--systemdsystemunitdir=$(systemd_get_systemunitdir) \

net-im/bitlbee/bitlbee-9999.ebuild
@@ -111,6 +111,7 @@ src_configure() {
 		--prefix=/usr \
 		--datadir=/usr/share/bitlbee \
 		--etcdir=/etc/bitlbee \
+		--libdir=/usr/$(get_libdir) \
 		--plugindir=/usr/$(get_libdir)/bitlbee \
 		--pcdir=/usr/$(get_libdir)/pkgconfig \
 		--systemdsystemunitdir=$(systemd_get_systemunitdir) \