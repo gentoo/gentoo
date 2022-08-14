gentoo/gentoo

commit 89e4ee59e0465a9ac90a5094fdea919f0ebe02a2

dev-python/frozendict: fix test phase
Signed-off-by: Petr Vaněk <arkamar@atlas.cz>
Signed-off-by: Michał Górny <mgorny@gentoo.org>

dev-python/frozendict/frozendict-2.3.2.ebuild
@@ -21,7 +21,7 @@ LICENSE="MIT"
 SLOT="0"
 KEYWORDS="~amd64 ~ppc64"
 
-# few tests fail with python3_9 and one with python3_10
-RESTRICT="test"
-
-distutils_enable_tests pytest
+python_test() {
+	cd "${T}" || die
+	epytest "${S}/test"
+}

commit 5b08d7b7a21c55856feb66a52729678f4e37ff42

dev-python/frozendict: fix pytest dependencies
Fixes: 89e4ee59e046 ("dev-python/frozendict: fix test phase")
Fixes: 880f511ea944 ("dev-python/frozendict: add 2.3.4")
Closes: https://bugs.gentoo.org/865163
Signed-off-by: Petr Vaněk <arkamar@atlas.cz>

dev-python/frozendict/frozendict-2.3.2.ebuild
@@ -21,6 +21,8 @@ LICENSE="MIT"
 SLOT="0"
 KEYWORDS="~amd64 ~ppc64"
 
+distutils_enable_tests pytest
+
 python_test() {
 	cd "${T}" || die
 	epytest "${S}/test"

dev-python/frozendict/frozendict-2.3.4.ebuild
@@ -23,6 +23,8 @@ LICENSE="MIT"
 SLOT="0"
 KEYWORDS="~amd64 ~ppc64"
 
+distutils_enable_tests pytest
+
 python_test() {
 	cd "${T}" || die
 	epytest "${S}/test"