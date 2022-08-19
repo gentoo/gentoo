gentoo/gentoo

commit eb108d8b5224d9986d607b683ec8bcd6c9e56633

net-im/bitlbee: force plugins to be enabled for tests
Closes: https://bugs.gentoo.org/show_bug.cgi?id=617604

net-im/bitlbee/bitlbee-9999.ebuild
@@ -25,6 +25,7 @@ IUSE="debug +gnutls ipv6 libevent libressl nss otr +plugins selinux test xinetd
 REQUIRED_USE="
 	|| ( purple xmpp msn oscar )
 	xmpp? ( !nss )
+	test? ( plugins )
 "
 
 COMMON_DEPEND="

net-im/bitlbee/bitlbee-3.5.1.ebuild
@@ -25,6 +25,7 @@ IUSE="debug +gnutls ipv6 libevent libressl nss otr +plugins selinux test xinetd
 REQUIRED_USE="
 	|| ( purple xmpp msn oscar )
 	xmpp? ( !nss )
+	test? ( plugins )
 "
 
 COMMON_DEPEND="

commit 502a1326303a83bbcb516a4bd314869803967a38

net-im/bitlbee: tests require xmpp enabled not plugins
plugins use flag is not required for test to work but xmpp is, because of
tests/check_jabber_* tests.

The test? ( plugin ) requirement was wrongly introduced in commit
eb108d8b5224 ("net-im/bitlbee: force plugins to be enabled for tests")
as a solution for bug https://bugs.gentoo.org/617604 which was later
reported again in bug https://bugs.gentoo.org/739510 and correctly fixed
in commit 05dc33a126ab ("net-im/bitlbee: fix compilation with disabled
plugins use").

Signed-off-by: Petr Vaněk <arkamar@atlas.cz>

net-im/bitlbee/bitlbee-3.6-r2.ebuild
@@ -28,7 +28,7 @@ RESTRICT="!test? ( test )"
 REQUIRED_USE="
 	|| ( purple xmpp )
 	purple? ( plugins )
-	test? ( plugins )
+	test? ( xmpp )
 	xmpp? ( !nss )
 "
 

net-im/bitlbee/bitlbee-3.6-r3.ebuild
@@ -28,7 +28,7 @@ RESTRICT="!test? ( test )"
 REQUIRED_USE="
 	|| ( purple xmpp )
 	purple? ( plugins )
-	test? ( plugins )
+	test? ( xmpp )
 	xmpp? ( !nss )
 "
 

net-im/bitlbee/bitlbee-9999.ebuild
@@ -28,7 +28,7 @@ RESTRICT="!test? ( test )"
 REQUIRED_USE="
 	|| ( purple xmpp )
 	purple? ( plugins )
-	test? ( plugins )
+	test? ( xmpp )
 	xmpp? ( !nss )
 "
 