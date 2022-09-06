gentoo/gentoo

commit f3e5d39c4f044506712d77732c02108352761a1a

net-libs/neon: add 0.32.3
Signed-off-by: Craig Andrews <candrews@gentoo.org>

net-libs/neon/neon-0.32.3.ebuild
@@ -1,0 +1,107 @@
+# Copyright 2001-2022 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI="8"
+
+inherit autotools libtool multilib-minimal
+
+DESCRIPTION="HTTP and WebDAV client library"
+HOMEPAGE="https://notroj.github.io/neon/ https://github.com/notroj/neon"
+SRC_URI="https://notroj.github.io/neon/${P}.tar.gz"
+
+LICENSE="GPL-2"
+SLOT="0/27"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~mips ppc ppc64 ~riscv ~s390 sparc x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~sparc-solaris ~sparc64-solaris ~x64-solaris ~x86-solaris"
+IUSE="doc +expat gnutls kerberos libproxy nls pkcs11 ssl test zlib"
+RESTRICT="!test? ( test )"
+
+BDEPEND="virtual/pkgconfig
+	test? (
+		ssl? (
+			dev-libs/openssl:0
+			pkcs11? ( dev-libs/nss )
+		)
+	)"
+DEPEND="expat? ( dev-libs/expat:0=[${MULTILIB_USEDEP}] )
+	!expat? ( dev-libs/libxml2:2=[${MULTILIB_USEDEP}] )
+	kerberos? ( virtual/krb5:0=[${MULTILIB_USEDEP}] )
+	libproxy? ( net-libs/libproxy:0=[${MULTILIB_USEDEP}] )
+	nls? ( virtual/libintl:0=[${MULTILIB_USEDEP}] )
+	ssl? (
+		gnutls? (
+			app-misc/ca-certificates
+			net-libs/gnutls:0=[${MULTILIB_USEDEP}]
+		)
+		!gnutls? ( dev-libs/openssl:0=[${MULTILIB_USEDEP}] )
+		pkcs11? ( dev-libs/pakchois:0=[${MULTILIB_USEDEP}] )
+	)
+	zlib? ( sys-libs/zlib:0=[${MULTILIB_USEDEP}] )"
+RDEPEND="${DEPEND}"
+
+MULTILIB_CHOST_TOOLS=(
+	/usr/bin/neon-config
+)
+
+DOCS=( AUTHORS BUGS NEWS README.md THANKS TODO )
+
+src_prepare() {
+	if use gnutls; then
+		# Ignore failure of test pkcs11.
+		# https://github.com/notroj/neon/issues/72
+		sed -e "s/T(pkcs11)/T_XFAIL(pkcs11)/" -i test/ssl.c || die
+	fi
+
+	eapply_user
+
+	AT_M4DIR="macros" eautoreconf
+
+	elibtoolize
+
+	multilib_copy_sources
+}
+
+multilib_src_configure() {
+	local myconf=()
+
+	if has_version sys-libs/glibc; then
+		einfo "Enabling SSL library thread-safety using POSIX threads..."
+		myconf+=(--enable-threadsafe-ssl=posix)
+	fi
+
+	if use expat; then
+		myconf+=(--with-expat)
+	else
+		myconf+=(--with-libxml2)
+	fi
+
+	if use ssl; then
+		if use gnutls; then
+			myconf+=(--with-ssl=gnutls --with-ca-bundle="${EPREFIX}/etc/ssl/certs/ca-certificates.crt")
+		else
+			myconf+=(--with-ssl=openssl)
+		fi
+	fi
+
+	econf \
+		--enable-shared \
+		$(use_with kerberos gssapi) \
+		$(use_with libproxy) \
+		$(use_enable nls) \
+		$(use_with pkcs11 pakchois) \
+		$(use_with zlib) \
+		"${myconf[@]}"
+}
+
+multilib_src_install() {
+	emake DESTDIR="${D}" install-{config,headers,lib,man,nls}
+
+	if multilib_is_native_abi && use doc; then
+		dodoc -r doc/html
+	fi
+}
+
+multilib_src_install_all() {
+	find "${ED}" -name "*.la" -delete || die
+
+	einstalldocs
+}

net-libs/neon/Manifest
@@ -1,1 +1,2 @@
 DIST neon-0.32.2.tar.gz 880651 BLAKE2B 3629eee38e10ada5e001bf0afb77c70814083bf553596fc150a6cf21f0b66cf4de28841b242f68faaeb44fa5e0ad260939102b015d2a543ef59012876256b364 SHA512 2580a3c8c3cf4aff2d399f72a721ccfb4e68434ef92da4af8103c126812d779b9fbe5cafbab512c79f4365bbb3d3eac61568748136470e86f7aa7b98b27053c8
+DIST neon-0.32.3.tar.gz 884939 BLAKE2B e0f0b242bb65f595d2e357c08a770faf503c70df69caf200b62bb50a90ae34360854d051d2b46900f18c1f46d9570bec9302e1f27837efcb859f7197f810ff26 SHA512 8dec5cbc823e38506725529c9e8fad95c93145d9e5d54131f8ceca6bbfe1b64f7f0eb1ce117072ea2f28edc67fa2f5a7f53bcc08ee644010fea76403ab6bbaad

commit eba5d382ae075e723905c1f45747bd48d92e90c6

net-libs/neon: destabilize 0.32.3 for ~ppc, ~ppc64, ~sparc, ~x86
Non-maintainer commit, and new version was added with stable keywords.

Fixes: f3e5d39c4f044506712d77732c02108352761a1a
Signed-off-by: Jakov Smolić <jsmolic@gentoo.org>

net-libs/neon/neon-0.32.3.ebuild
@@ -11,7 +11,7 @@ SRC_URI="https://notroj.github.io/neon/${P}.tar.gz"
 
 LICENSE="GPL-2"
 SLOT="0/27"
-KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~mips ppc ppc64 ~riscv ~s390 sparc x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~sparc-solaris ~sparc64-solaris ~x64-solaris ~x86-solaris"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~sparc-solaris ~sparc64-solaris ~x64-solaris ~x86-solaris"
 IUSE="doc +expat gnutls kerberos libproxy nls pkcs11 ssl test zlib"
 RESTRICT="!test? ( test )"
 