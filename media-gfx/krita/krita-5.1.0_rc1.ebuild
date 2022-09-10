gentoo/gentoo

commit ceca8a7c857f200204c2ec835d1ffd3b625a5397

media-gfx/krita: depend on quazip[qt5(+)]
Signed-off-by: Andrew Ammerlaan <andrewammerlaan@gentoo.org>

media-gfx/krita/krita-5.1.0_rc1.ebuild
@@ -29,7 +29,7 @@ RESTRICT="test"
 
 RDEPEND="${PYTHON_DEPS}
 	dev-libs/boost:=
-	dev-libs/quazip:0=
+	dev-libs/quazip:0=[qt5(+)]
 	$(python_gen_cond_dep '
 		dev-python/PyQt5[declarative,gui,widgets,${PYTHON_USEDEP}]
 		dev-python/sip:=[${PYTHON_USEDEP}]

media-gfx/krita/krita-5.0.8.ebuild
@@ -28,7 +28,7 @@ RESTRICT="test"
 
 RDEPEND="${PYTHON_DEPS}
 	dev-libs/boost:=
-	dev-libs/quazip:0=
+	dev-libs/quazip:0=[qt5(+)]
 	$(python_gen_cond_dep '
 		dev-python/PyQt5[declarative,gui,widgets,${PYTHON_USEDEP}]
 		dev-python/sip:=[${PYTHON_USEDEP}]

commit d100a4e3db4b91ce0549b0cb946f8d62f50321b4

media-gfx/krita: revbump for quazip USE-dep change
Fixes: ceca8a7c857f200204c2ec835d1ffd3b625a5397
Signed-off-by: Sam James <sam@gentoo.org>

media-gfx/krita/krita-5.1.0_rc1.ebuild
@@ -13,7 +13,7 @@ inherit ecm kde.org python-single-r1
 if [[ ${KDE_BUILD_TYPE} = release ]]; then
 	SRC_URI="mirror://kde/unstable/${PN}/${PV/_rc/-RC}/${PN}-${PV/_rc/-RC}.tar.xz"
 	S="${WORKDIR}/${PN}-${PV/_rc/-RC}"
-# 	KEYWORDS="~amd64 ~arm64 ~ppc64 ~riscv ~x86"
+	#KEYWORDS="~amd64 ~arm64 ~ppc64 ~riscv ~x86"
 fi
 
 DESCRIPTION="Free digital painting application. Digital Painting, Creative Freedom!"