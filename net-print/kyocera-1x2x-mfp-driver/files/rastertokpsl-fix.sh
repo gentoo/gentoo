455d60bfdaa584f8ed764903d5cdfee087208719
asperg <33286590+asperg@users.noreply.github.com>
2022-09-12 18:31:09 +0300

    Update rastertokpsl-fix.sh
    fix crash rastertokpsl with option

--- net-print/kyocera-1x2x-mfp-driver/files/rastertokpsl-fix.sh
+++ net-print/kyocera-1x2x-mfp-driver/files/rastertokpsl-fix.sh
@@ -4,4 +4,25 @@
 # the original version.
 # Distributed under the terms of the GNU General Public License v2.
 jobname="$(echo "$3" | grep -Eo "[[:alnum:]]" | tr -d "\n" | tail -c 20)"
-/usr/libexec/cups/filter/rastertokpsl "$1" "$2" "${jobname}" "$4" "$5"
+
+#there may be options that cause the application to crash
+function filterOptions {
+        # Split options by IFS (default: by whitespaces).
+        local optionsArray=($1)
+
+        local resultArray
+        local option
+        for option in "${optionsArray[@]}"; do
+            case "$option" in
+                Resolution*|PageSize*|fit-to-page*)
+                    resultArray+=("$option")
+                    ;;
+                *)
+                    ;; # Ignore any unsupported option.
+            esac
+        done
+
+        echo -n "${resultArray[*]}"
+    }
+
+/usr/libexec/cups/filter/rastertokpsl "$1" "$2" "$jobname" "$4" "$(filterOptions "$5")"
