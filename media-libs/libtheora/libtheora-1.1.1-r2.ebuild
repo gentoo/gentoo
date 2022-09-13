gentoo/gentoo

commit f7f0dc3aea8e956f0a806e54794f8570b5b48c6b

media-libs/libtheora: limit multilib features (drop doc)
Signed-off-by: Sam James <sam@gentoo.org>

media-libs/libtheora/libtheora-1.1.1-r2.ebuild
@@ -56,19 +56,13 @@ multilib_src_configure() {
 	use doc || export ac_cv_prog_HAVE_DOXYGEN=false
 
 	local myconf=(
+		# --disable-spec because LaTeX documentation has been prebuilt
 		--disable-spec
 		$(use_enable encode)
+		$(multilib_native_use_enable examples)
 		$(use_enable static-libs static)
 	)
 
-	if [[ "${ABI}" = "${DEFAULT_ABI}" ]] ; then
-		myconf+=( $(use_enable examples) )
-	else
-		# those will be overwritten anyway
-		myconf+=( --disable-examples )
-	fi
-
-	# --disable-spec because LaTeX documentation has been prebuilt
 	ECONF_SOURCE="${S}" econf "${myconf[@]}"
 }
 
@@ -77,23 +71,24 @@ multilib_src_install() {
 		DESTDIR="${D}" \
 		docdir="${EPREFIX}"/usr/share/doc/${PF} \
 		install
+}
 
-	if use examples && [[ "${ABI}" = "${DEFAULT_ABI}" ]]; then
+multilib_src_install_all() {
+	find "${ED}" -name '*.la' -delete || die
+
+	einstalldocs
+
+	if use examples ; then
 		dobin examples/.libs/png2theora
 		for bin in dump_{psnr,video} {encoder,player}_example; do
 			newbin examples/.libs/${bin} theora_${bin}
 		done
-	fi
-}
 
-multilib_src_install_all() {
-	find "${D}" -name '*.la' -delete || die
-	einstalldocs
-
-	if use examples && use doc; then
-		docinto examples
-		dodoc examples/*.[ch]
-		docompress -x /usr/share/doc/${PF}/examples
-		docinto .
+		if use doc ; then
+			docinto examples
+			dodoc examples/*.[ch]
+			docompress -x /usr/share/doc/${PF}/examples
+			docinto .
+		fi
 	fi
 }

commit e7d1d2bd3bf8659496481b4b708c4370b7d9dc41

media-libs/libtheora: fix examples installation
Closes: https://bugs.gentoo.org/867694
Fixes: f7f0dc3aea8e956f0a806e54794f8570b5b48c6b
Signed-off-by: Sam James <sam@gentoo.org>

media-libs/libtheora/libtheora-1.1.1-r2.ebuild
@@ -71,6 +71,15 @@ multilib_src_install() {
 		DESTDIR="${D}" \
 		docdir="${EPREFIX}"/usr/share/doc/${PF} \
 		install
+
+	if multilib_is_native_abi && use examples ; then
+		dobin examples/.libs/png2theora
+
+		local bin
+		for bin in dump_{psnr,video} {encoder,player}_example; do
+			newbin examples/.libs/${bin} theora_${bin}
+		done
+	fi
 }
 
 multilib_src_install_all() {
@@ -79,16 +88,9 @@ multilib_src_install_all() {
 	einstalldocs
 
 	if use examples ; then
-		dobin examples/.libs/png2theora
-		for bin in dump_{psnr,video} {encoder,player}_example; do
-			newbin examples/.libs/${bin} theora_${bin}
-		done
-
-		if use doc ; then
-			docinto examples
-			dodoc examples/*.[ch]
-			docompress -x /usr/share/doc/${PF}/examples
-			docinto .
-		fi
+		docinto examples
+		dodoc examples/*.[ch]
+		docompress -x /usr/share/doc/${PF}/examples
+		docinto .
 	fi
 }