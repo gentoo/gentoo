gentoo/gentoo

commit d12bc87963fe1b3fae352771cd590e9daad228f4

sci-libs/fflas-ffpack: fix duplicate pkg_setup
Signed-off-by: Sam James <sam@gentoo.org>

sci-libs/fflas-ffpack/fflas-ffpack-2.4.3-r2.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2021 Gentoo Authors
+# Copyright 1999-2022 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
@@ -42,16 +42,14 @@ pkg_setup() {
 	[[ ${MERGE_TYPE} != binary ]] && use openmp && tc-check-openmp
 }
 
-pkg_setup() {
-	tc-export PKG_CONFIG
-}
-
 src_prepare() {
 	default
 	eautoreconf
 }
 
 src_configure() {
+	 tc-export PKG_CONFIG
+
 	econf \
 		--enable-precompilation \
 		$(use_enable openmp) \

sci-libs/fflas-ffpack/fflas-ffpack-2.4.3-r3.ebuild
@@ -43,16 +43,14 @@ pkg_setup() {
 	[[ ${MERGE_TYPE} != binary ]] && use openmp && tc-check-openmp
 }
 
-pkg_setup() {
-	tc-export PKG_CONFIG
-}
-
 src_prepare() {
 	default
 	eautoreconf
 }
 
 src_configure() {
+	tc-export PKG_CONFIG
+
 	econf \
 		--enable-precompilation \
 		$(use_enable openmp) \

commit c23ec367253357ec5f5d931e5af40e87c61c7bf6

sci-libs/fflas-ffpack: fix indentation
Fixes: d12bc87963fe1b3fae352771cd590e9daad228f4
Signed-off-by: Sam James <sam@gentoo.org>

sci-libs/fflas-ffpack/fflas-ffpack-2.4.3-r2.ebuild
@@ -48,7 +48,7 @@ src_prepare() {
 }
 
 src_configure() {
-	 tc-export PKG_CONFIG
+	tc-export PKG_CONFIG
 
 	econf \
 		--enable-precompilation \