gentoo/gentoo

commit 1fa5bec5df273399c01c263bc3881f50ce0b2736

sys-libs/libomp: filter LTO
Filter LTO on sys-libs/libomp as it causes strange issues on the system like Firefox not building.

Closes: https://github.com/gentoo/gentoo/pull/27502
Closes: https://bugs.gentoo.org/870127
Signed-off-by: Ian Jordan <immoloism@gmail.com>
Signed-off-by: Sam James <sam@gentoo.org>

sys-libs/libomp/libomp-13.0.1.ebuild
@@ -4,7 +4,7 @@
 EAPI=7
 
 PYTHON_COMPAT=( python3_{8..10} )
-inherit cmake-multilib linux-info llvm llvm.org python-any-r1
+inherit flag-o-matic cmake-multilib linux-info llvm llvm.org python-any-r1
 
 DESCRIPTION="OpenMP runtime library for LLVM/clang compiler"
 HOMEPAGE="https://openmp.llvm.org"
@@ -75,6 +75,8 @@ pkg_setup() {
 }
 
 multilib_src_configure() {
+	filter-lto # LTO causes issues in other packages building https://bugs.gentoo.org/870127
+
 	# LLVM_ENABLE_ASSERTIONS=NO does not guarantee this for us, #614844
 	use debug || local -x CPPFLAGS="${CPPFLAGS} -DNDEBUG"
 

sys-libs/libomp/libomp-14.0.6-r1.ebuild
@@ -4,7 +4,7 @@
 EAPI=8
 
 PYTHON_COMPAT=( python3_{8..10} )
-inherit cmake-multilib linux-info llvm llvm.org python-any-r1
+inherit flag-o-matic cmake-multilib linux-info llvm llvm.org python-any-r1
 
 DESCRIPTION="OpenMP runtime library for LLVM/clang compiler"
 HOMEPAGE="https://openmp.llvm.org"
@@ -83,6 +83,8 @@ pkg_setup() {
 }
 
 multilib_src_configure() {
+	filter-lto # LTO causes issues in other packages building https://bugs.gentoo.org/870127
+
 	# LLVM_ENABLE_ASSERTIONS=NO does not guarantee this for us, #614844
 	use debug || local -x CPPFLAGS="${CPPFLAGS} -DNDEBUG"
 

sys-libs/libomp/libomp-15.0.1.ebuild
@@ -4,7 +4,7 @@
 EAPI=8
 
 PYTHON_COMPAT=( python3_{8..11} )
-inherit cmake-multilib linux-info llvm llvm.org python-any-r1
+inherit flag-o-matic cmake-multilib linux-info llvm llvm.org python-any-r1
 
 DESCRIPTION="OpenMP runtime library for LLVM/clang compiler"
 HOMEPAGE="https://openmp.llvm.org"
@@ -76,6 +76,8 @@ pkg_setup() {
 }
 
 multilib_src_configure() {
+	filter-lto # LTO causes issues in other packages building https://bugs.gentoo.org/870127
+
 	# LLVM_ENABLE_ASSERTIONS=NO does not guarantee this for us, #614844
 	use debug || local -x CPPFLAGS="${CPPFLAGS} -DNDEBUG"
 

sys-libs/libomp/libomp-15.0.2.9999.ebuild
@@ -4,7 +4,7 @@
 EAPI=8
 
 PYTHON_COMPAT=( python3_{8..11} )
-inherit cmake-multilib linux-info llvm llvm.org python-any-r1
+inherit flag-o-matic cmake-multilib linux-info llvm llvm.org python-any-r1
 
 DESCRIPTION="OpenMP runtime library for LLVM/clang compiler"
 HOMEPAGE="https://openmp.llvm.org"
@@ -76,6 +76,8 @@ pkg_setup() {
 }
 
 multilib_src_configure() {
+	filter-lto # LTO causes issues in other packages building https://bugs.gentoo.org/870127
+
 	# LLVM_ENABLE_ASSERTIONS=NO does not guarantee this for us, #614844
 	use debug || local -x CPPFLAGS="${CPPFLAGS} -DNDEBUG"
 

sys-libs/libomp/libomp-16.0.0.9999.ebuild
@@ -4,7 +4,7 @@
 EAPI=8
 
 PYTHON_COMPAT=( python3_{8..11} )
-inherit cmake-multilib linux-info llvm llvm.org python-any-r1
+inherit flag-o-matic cmake-multilib linux-info llvm llvm.org python-any-r1
 
 DESCRIPTION="OpenMP runtime library for LLVM/clang compiler"
 HOMEPAGE="https://openmp.llvm.org"
@@ -76,6 +76,8 @@ pkg_setup() {
 }
 
 multilib_src_configure() {
+	filter-lto # LTO causes issues in other packages building https://bugs.gentoo.org/870127
+
 	# LLVM_ENABLE_ASSERTIONS=NO does not guarantee this for us, #614844
 	use debug || local -x CPPFLAGS="${CPPFLAGS} -DNDEBUG"
 

sys-libs/libomp/libomp-16.0.0_pre20220918.ebuild
@@ -4,7 +4,7 @@
 EAPI=8
 
 PYTHON_COMPAT=( python3_{8..11} )
-inherit cmake-multilib linux-info llvm llvm.org python-any-r1
+inherit flag-o-matic cmake-multilib linux-info llvm llvm.org python-any-r1
 
 DESCRIPTION="OpenMP runtime library for LLVM/clang compiler"
 HOMEPAGE="https://openmp.llvm.org"
@@ -76,6 +76,8 @@ pkg_setup() {
 }
 
 multilib_src_configure() {
+	filter-lto # LTO causes issues in other packages building https://bugs.gentoo.org/870127
+
 	# LLVM_ENABLE_ASSERTIONS=NO does not guarantee this for us, #614844
 	use debug || local -x CPPFLAGS="${CPPFLAGS} -DNDEBUG"
 

commit c0986ceb6dd52b0d61bc3babd112f158e633c4fb

sys-libs/libomp: fix comment style
Fixes: 1fa5bec5df273399c01c263bc3881f50ce0b2736
Signed-off-by: Sam James <sam@gentoo.org>

sys-libs/libomp/libomp-13.0.1.ebuild
@@ -75,7 +75,8 @@ pkg_setup() {
 }
 
 multilib_src_configure() {
-	filter-lto # LTO causes issues in other packages building https://bugs.gentoo.org/870127
+	# LTO causes issues in other packages building, #870127
+	filter-lto
 
 	# LLVM_ENABLE_ASSERTIONS=NO does not guarantee this for us, #614844
 	use debug || local -x CPPFLAGS="${CPPFLAGS} -DNDEBUG"

sys-libs/libomp/libomp-14.0.6-r1.ebuild
@@ -83,7 +83,8 @@ pkg_setup() {
 }
 
 multilib_src_configure() {
-	filter-lto # LTO causes issues in other packages building https://bugs.gentoo.org/870127
+	# LTO causes issues in other packages building, #870127
+	filter-lto
 
 	# LLVM_ENABLE_ASSERTIONS=NO does not guarantee this for us, #614844
 	use debug || local -x CPPFLAGS="${CPPFLAGS} -DNDEBUG"

sys-libs/libomp/libomp-15.0.1.ebuild
@@ -76,7 +76,8 @@ pkg_setup() {
 }
 
 multilib_src_configure() {
-	filter-lto # LTO causes issues in other packages building https://bugs.gentoo.org/870127
+	# LTO causes issues in other packages building, #870127
+	filter-lto
 
 	# LLVM_ENABLE_ASSERTIONS=NO does not guarantee this for us, #614844
 	use debug || local -x CPPFLAGS="${CPPFLAGS} -DNDEBUG"

sys-libs/libomp/libomp-15.0.2.9999.ebuild
@@ -76,7 +76,8 @@ pkg_setup() {
 }
 
 multilib_src_configure() {
-	filter-lto # LTO causes issues in other packages building https://bugs.gentoo.org/870127
+	# LTO causes issues in other packages building, #870127
+	filter-lto
 
 	# LLVM_ENABLE_ASSERTIONS=NO does not guarantee this for us, #614844
 	use debug || local -x CPPFLAGS="${CPPFLAGS} -DNDEBUG"

sys-libs/libomp/libomp-16.0.0.9999.ebuild
@@ -76,7 +76,8 @@ pkg_setup() {
 }
 
 multilib_src_configure() {
-	filter-lto # LTO causes issues in other packages building https://bugs.gentoo.org/870127
+	# LTO causes issues in other packages building, #870127
+	filter-lto
 
 	# LLVM_ENABLE_ASSERTIONS=NO does not guarantee this for us, #614844
 	use debug || local -x CPPFLAGS="${CPPFLAGS} -DNDEBUG"

sys-libs/libomp/libomp-16.0.0_pre20220918.ebuild
@@ -76,7 +76,8 @@ pkg_setup() {
 }
 
 multilib_src_configure() {
-	filter-lto # LTO causes issues in other packages building https://bugs.gentoo.org/870127
+	# LTO causes issues in other packages building, #870127
+	filter-lto
 
 	# LLVM_ENABLE_ASSERTIONS=NO does not guarantee this for us, #614844
 	use debug || local -x CPPFLAGS="${CPPFLAGS} -DNDEBUG"