gentoo/gentoo

commit 3b58c05c00f7ebc1b9cb3d431cec220ba4dcc91d

dev-util/libabigail: pre-generate man pages
Signed-off-by: Sam James <sam@gentoo.org>

dev-util/libabigail/Manifest
@@ -1,2 +1,3 @@
 DIST libabigail-2.0.tar.gz 286383045 BLAKE2B 2ba6f59bcd7a1484c4a60c6c9f212ad76af239a552d2cc9e47748e0db52363afadd7a99ef3e123c263de34b416f71b9d679bf2a5c542c419bb38ee75f3d5c57b SHA512 288f63f3495f0cd38258c50b78f30a573e43ab60494fefa22c8cba6d6776c5f94742ffea26297a232b78d25f6804f1b3f51febd59ec487733e6ef683cef2c180
+DIST libabigail-2.1-docs.tar.xz 28220 BLAKE2B cd2966bbe6d18c5575601a190bac6d6bf56ef13a31845caf15e2de41ce6e649f074b194635ad5801324d0e8bac182da50cec9a70ec6b59ea1a860db0e7b20b54 SHA512 c90fe4c3a92fc13d29087585b22b2a7cb41a2670eeafaaac147f9c0693616d1b4da2c2e840a3d7ebc87748380995b0d030c3dae56d8cc61b7efbda3911fdb175
 DIST libabigail-2.1.tar.gz 405861148 BLAKE2B 28c8c7cfc33b5002d10b94e2eb9e16ef403dcce73c1404b8af011d8bf71b142b62a5079154af078a479b4528f8464c420322b814e75bb3127a09992e7ca8f84f SHA512 4fe1d3323f078a76a7529c15a809d0c0d3ac43cabd5ab4bb5c2d03992cbbb61bae3ec858e40796bf9f65029cbc779d258ae4559455178fb95cb8b53bf92bcc9a

dev-util/libabigail/libabigail-2.1-r1.ebuild
@@ -3,6 +3,16 @@
 
 EAPI=8
 
+# Generate using https://github.com/thesamesam/sam-gentoo-scripts/blob/main/niche/generate-libabigail-docs
+# Set to 1 if prebuilt, 0 if not
+# (the construct below is to allow overriding from env for script)
+LIBABIGAIL_DOCS_PREBUILT=${LIBABIGAIL_DOCS_PREBUILT:=1}
+LIBABIGAIL_DOCS_PREBUILT_DEV=sam
+LIBABIGAIL_DOCS_VERSION="${PV}"
+# Default to generating docs (inc. man pages) if no prebuilt; overridden later
+# bug #830088
+LIBABIGAIL_DOCS_USEFLAG="+doc"
+
 PYTHON_COMPAT=( python3_{8..11} )
 
 inherit autotools bash-completion-r1 python-any-r1 out-of-source
@@ -10,11 +20,15 @@ inherit autotools bash-completion-r1 python-any-r1 out-of-source
 DESCRIPTION="Suite of tools for checking ABI differences between ELF objects"
 HOMEPAGE="https://sourceware.org/libabigail/"
 SRC_URI="https://mirrors.kernel.org/sourceware/libabigail/${P}.tar.gz"
+if [[ ${LIBABIGAIL_DOCS_PREBUILT} == 1 ]] ; then
+	SRC_URI+=" !doc? ( https://dev.gentoo.org/~${LIBABIGAIL_DOCS_PREBUILT_DEV}/distfiles/${CATEGORY}/${PN}/${PN}-${LIBABIGAIL_DOCS_VERSION}-docs.tar.xz )"
+	LIBABIGAIL_DOCS_USEFLAG="doc"
+fi
 
 LICENSE="Apache-2.0-with-LLVM-exceptions"
 SLOT="0"
 KEYWORDS="~amd64 ~arm ~arm64 ~ppc64 ~riscv"
-IUSE="doc test"
+IUSE="${LIBABIGAIL_DOCS_USEFLAG} test"
 RESTRICT="!test? ( test )"
 
 RDEPEND="
@@ -59,7 +73,10 @@ my_src_compile() {
 my_src_install() {
 	emake DESTDIR="${D}" install
 
-	if use doc; then
+	# If USE=doc, there'll be newly generated docs which we install instead.
+	if ! use doc && [[ ${LIBABIGAIL_DOCS_PREBUILT} == 1 ]] ; then
+		doman "${WORKDIR}"/${PN}-${LIBABIGAIL_DOCS_VERSION}-docs/docs/*.[0-8]
+	elif use doc; then
 		doman doc/manuals/man/*
 		doinfo doc/manuals/texinfo/abigail.info
 

commit 2d53cac8776d5a2df15a9150439fe759272f10b5

dev-util/libabigail: fix pre-generated man install path, add info pages
Fixes: 3b58c05c00f7ebc1b9cb3d431cec220ba4dcc91d
Signed-off-by: Sam James <sam@gentoo.org>

dev-util/libabigail/Manifest
@@ -1,3 +1,3 @@
 DIST libabigail-2.0.tar.gz 286383045 BLAKE2B 2ba6f59bcd7a1484c4a60c6c9f212ad76af239a552d2cc9e47748e0db52363afadd7a99ef3e123c263de34b416f71b9d679bf2a5c542c419bb38ee75f3d5c57b SHA512 288f63f3495f0cd38258c50b78f30a573e43ab60494fefa22c8cba6d6776c5f94742ffea26297a232b78d25f6804f1b3f51febd59ec487733e6ef683cef2c180
-DIST libabigail-2.1-docs.tar.xz 28220 BLAKE2B cd2966bbe6d18c5575601a190bac6d6bf56ef13a31845caf15e2de41ce6e649f074b194635ad5801324d0e8bac182da50cec9a70ec6b59ea1a860db0e7b20b54 SHA512 c90fe4c3a92fc13d29087585b22b2a7cb41a2670eeafaaac147f9c0693616d1b4da2c2e840a3d7ebc87748380995b0d030c3dae56d8cc61b7efbda3911fdb175
+DIST libabigail-2.1-docs.tar.xz 45532 BLAKE2B e59198a4ecdc6e0eef2b53ce62daa7a313e3dd3f28b71bc7184ec44745b165fab05bb08d22aa9a5e7f886c3b9156f0713737aa2899b579555b18daf8894e6a43 SHA512 078639bd08fdf190b8e71fe147495e0b6b14c7947e82d7aa5f5943ba00cf02763dc8c63a7d39189958a6cfb5175efff6a87cd74938fb3df8f7e123c5586605a6
 DIST libabigail-2.1.tar.gz 405861148 BLAKE2B 28c8c7cfc33b5002d10b94e2eb9e16ef403dcce73c1404b8af011d8bf71b142b62a5079154af078a479b4528f8464c420322b814e75bb3127a09992e7ca8f84f SHA512 4fe1d3323f078a76a7529c15a809d0c0d3ac43cabd5ab4bb5c2d03992cbbb61bae3ec858e40796bf9f65029cbc779d258ae4559455178fb95cb8b53bf92bcc9a

dev-util/libabigail/libabigail-2.1-r1.ebuild
@@ -75,7 +75,8 @@ my_src_install() {
 
 	# If USE=doc, there'll be newly generated docs which we install instead.
 	if ! use doc && [[ ${LIBABIGAIL_DOCS_PREBUILT} == 1 ]] ; then
-		doman "${WORKDIR}"/${PN}-${LIBABIGAIL_DOCS_VERSION}-docs/docs/*.[0-8]
+		doinfo "${WORKDIR}"/${PN}-${LIBABIGAIL_DOCS_VERSION}-docs/texinfo/*.info
+		doman "${WORKDIR}"/${PN}-${LIBABIGAIL_DOCS_VERSION}-docs/man/*.[0-8]
 	elif use doc; then
 		doman doc/manuals/man/*
 		doinfo doc/manuals/texinfo/abigail.info