gentoo/gentoo

commit 2430298af74a2e0fcb4be5747ac30d33939eb231

media-plugins/vdr-undelete: fix version
Signed-off-by: Sam James <sam@gentoo.org>

media-plugins/vdr-undelete/vdr-undelete-2.3.1_pre1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2021 Gentoo Authors
+# Copyright 1999-2022 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
@@ -7,12 +7,11 @@ inherit vdr-plugin-2
 
 DESCRIPTION="VDR Plugin: Recover deleted recordings of VDR"
 HOMEPAGE="http://phivdr.dyndns.org/vdr/"
-SRC_URI="http://phivdr.dyndns.org/vdr/vdr-undelete/vdr-undelete-2.3.1-pre1.tgz -> ${P}.tgz"
+SRC_URI="http://phivdr.dyndns.org/vdr/vdr-undelete/vdr-undelete-${PV/_pre/-pre}.tgz -> ${P}.tgz"
+S="${WORKDIR}"/${PN}-${PV/_pre/-pre}
 
 SLOT="0"
 LICENSE="GPL-2"
 KEYWORDS="amd64 ~arm x86"
 
 DEPEND="media-video/vdr"
-
-S="${WORKDIR}/undelete-2.3.1-pre1"

media-plugins/vdr-undelete/Manifest
@@ -1,2 +1,2 @@
 DIST vdr-undelete-0.0.7.tgz 34655 BLAKE2B 81f40ad993d06386112ef46c3ba1c1402211c2ec2138aa113dfe8376340416097a691e7b5f4c4fee24e85e7a0b5c16501dc6853374c3e8b27e0a84437dcb0f74 SHA512 c4d634580d91c3d3601767bb29cfe12e0bd13f9d8fd5bab31a3388db1f8724544b371b4ba333cd524aa5243554015517f487f6e8f2de31bd747f1094ba1c84ff
-DIST vdr-undelete-2.3.0_pre1.tgz 13865 BLAKE2B 065550c0c6aa150677550450f458db496ac55e6ad1b8024de22abca37093259adfdd6ffc7b9c07a66e76df573c3c1d4d69920bcf56872406234a4898f99021d2 SHA512 3200efcbd2eec5ef04d66fcf9dbbd1973ef7488a332618a6d2f5dc4bc20323e2b88746f02883e9488436b32f21aea2fb60e7e675e247af392ee121423af4b878
+DIST vdr-undelete-2.3.1_pre1.tgz 13865 BLAKE2B 065550c0c6aa150677550450f458db496ac55e6ad1b8024de22abca37093259adfdd6ffc7b9c07a66e76df573c3c1d4d69920bcf56872406234a4898f99021d2 SHA512 3200efcbd2eec5ef04d66fcf9dbbd1973ef7488a332618a6d2f5dc4bc20323e2b88746f02883e9488436b32f21aea2fb60e7e675e247af392ee121423af4b878

commit 7047f494298e906b8a0fee0e3a798a0c37aca469

media-plugins/vdr-undelete: fix S
Closes: https://bugs.gentoo.org/873790
Fixes: 2430298af74a2e0fcb4be5747ac30d33939eb231
Signed-off-by: Sam James <sam@gentoo.org>

media-plugins/vdr-undelete/vdr-undelete-2.3.1_pre1.ebuild
@@ -8,7 +8,7 @@ inherit vdr-plugin-2
 DESCRIPTION="VDR Plugin: Recover deleted recordings of VDR"
 HOMEPAGE="http://phivdr.dyndns.org/vdr/"
 SRC_URI="http://phivdr.dyndns.org/vdr/vdr-undelete/vdr-undelete-${PV/_pre/-pre}.tgz -> ${P}.tgz"
-S="${WORKDIR}"/${PN}-${PV/_pre/-pre}
+S="${WORKDIR}"/${PN/vdr-}-${PV/_pre/-pre}
 
 SLOT="0"
 LICENSE="GPL-2"