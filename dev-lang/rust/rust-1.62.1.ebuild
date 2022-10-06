gentoo/gentoo

commit 07da00e100b4c9ea0c6c5c790ce0c7d20aa1118f

dev-lang/rust: fix has_version default-libcxx test for Clang
We moved the flag to sys-devel/clang-common to prepare for proper
config file support (which is backported to 15.0.2, although it's
going to be better in 16.x).

Go for the hack for now, but keep the bug open as we want to
switch to the proper link test from the LLVM ebuilds.

Bug: https://bugs.gentoo.org/875563
Signed-off-by: Sam James <sam@gentoo.org>

dev-lang/rust/rust-1.62.1.ebuild
@@ -310,7 +310,7 @@ src_configure() {
 	# https://bugs.gentoo.org/732632
 	if tc-is-clang; then
 		local clang_slot="$(clang-major-version)"
-		if { has_version "sys-devel/clang:${clang_slot}[default-libcxx]" || is-flagq -stdlib=libc++; }; then
+		if { has_version "sys-devel/clang:${clang_slot}[default-libcxx(-)]" || has_version "sys-devel/clang-common:${clang_slot}[default-libcxx(-)]" || is-flagq -stdlib=libc++; }; then
 			use_libcxx="true"
 		fi
 	fi

dev-lang/rust/rust-1.63.0-r1.ebuild
@@ -340,7 +340,7 @@ src_configure() {
 	# https://bugs.gentoo.org/732632
 	if tc-is-clang; then
 		local clang_slot="$(clang-major-version)"
-		if { has_version "sys-devel/clang:${clang_slot}[default-libcxx]" || is-flagq -stdlib=libc++; }; then
+		if { has_version "sys-devel/clang:${clang_slot}[default-libcxx(-)]" || has_version "sys-devel/clang-common:${clang_slot}[default-libcxx(-)]" || is-flagq -stdlib=libc++; }; then
 			use_libcxx="true"
 		fi
 	fi

dev-lang/rust/rust-1.64.0-r1.ebuild
@@ -338,7 +338,7 @@ src_configure() {
 	# https://bugs.gentoo.org/732632
 	if tc-is-clang; then
 		local clang_slot="$(clang-major-version)"
-		if { has_version "sys-devel/clang:${clang_slot}[default-libcxx]" || is-flagq -stdlib=libc++; }; then
+		if { has_version "sys-devel/clang:${clang_slot}[default-libcxx(-)]" || has_version "sys-devel/clang-common:${clang_slot}[default-libcxx(-)]" || is-flagq -stdlib=libc++; }; then
 			use_libcxx="true"
 		fi
 	fi

commit e51d85c825d1ad84ff357a038b1eff6411972873

dev-lang/rust: fix has_version rust check
Bug: https://bugs.gentoo.org/875563
Fixes: 07da00e100b4c9ea0c6c5c790ce0c7d20aa1118f
Signed-off-by: Sam James <sam@gentoo.org>

dev-lang/rust/rust-1.62.1.ebuild
@@ -310,7 +310,7 @@ src_configure() {
 	# https://bugs.gentoo.org/732632
 	if tc-is-clang; then
 		local clang_slot="$(clang-major-version)"
-		if { has_version "sys-devel/clang:${clang_slot}[default-libcxx(-)]" || has_version "sys-devel/clang-common:${clang_slot}[default-libcxx(-)]" || is-flagq -stdlib=libc++; }; then
+		if { has_version "sys-devel/clang:${clang_slot}[default-libcxx(-)]" || has_version "=sys-devel/clang-common-${clang_slot}*[default-libcxx(-)]" || is-flagq -stdlib=libc++; }; then
 			use_libcxx="true"
 		fi
 	fi

dev-lang/rust/rust-1.63.0-r1.ebuild
@@ -340,7 +340,7 @@ src_configure() {
 	# https://bugs.gentoo.org/732632
 	if tc-is-clang; then
 		local clang_slot="$(clang-major-version)"
-		if { has_version "sys-devel/clang:${clang_slot}[default-libcxx(-)]" || has_version "sys-devel/clang-common:${clang_slot}[default-libcxx(-)]" || is-flagq -stdlib=libc++; }; then
+		if { has_version "sys-devel/clang:${clang_slot}[default-libcxx(-)]" || has_version "=sys-devel/clang-common-${clang_slot}*[default-libcxx(-)]" || is-flagq -stdlib=libc++; }; then
 			use_libcxx="true"
 		fi
 	fi

dev-lang/rust/rust-1.64.0-r1.ebuild
@@ -338,7 +338,7 @@ src_configure() {
 	# https://bugs.gentoo.org/732632
 	if tc-is-clang; then
 		local clang_slot="$(clang-major-version)"
-		if { has_version "sys-devel/clang:${clang_slot}[default-libcxx(-)]" || has_version "sys-devel/clang-common:${clang_slot}[default-libcxx(-)]" || is-flagq -stdlib=libc++; }; then
+		if { has_version "sys-devel/clang:${clang_slot}[default-libcxx(-)]" || has_version "=sys-devel/clang-common-${clang_slot}*[default-libcxx(-)]" || is-flagq -stdlib=libc++; }; then
 			use_libcxx="true"
 		fi
 	fi