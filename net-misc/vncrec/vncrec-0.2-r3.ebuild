gentoo/gentoo

commit 0491a4264baf9951dd726b96094459a7b3363b95

net-misc/vncrec: further fix w/ upcoming clang16
When last tested this package, did not know clang16 was newly enabling
-Werror=incompatible-pointer-types yet and hadn't looked at this.

Bit dodgy here given we don't know what the X11 headers want, i.e.
_X_NORETURN could be set or not set, and an update could remove it
from XtErrorhandler entirely (so passing it may break later) -- so
pass -Wno-error instead (should be harmless in this case).

Bug: https://bugs.gentoo.org/871000
Signed-off-by: Ionen Wolkens <ionen@gentoo.org>

net-misc/vncrec/vncrec-0.2-r3.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-inherit toolchain-funcs
+inherit flag-o-matic toolchain-funcs
 
 DESCRIPTION="VNC session recorder and player"
 HOMEPAGE="https://wiki.gentoo.org/wiki/No_homepage"
@@ -42,6 +42,11 @@ src_prepare() {
 }
 
 src_configure() {
+	# XtErrorHandler usage matches docs (seems right), but headers "may" add
+	# __attribute__((noreturn)) giving an incompatible type error with clang-16
+	# (could alternatively use private _X_NORETURN but this may be fragile).
+	append-cflags -Wno-error=incompatible-function-pointer-types #871000
+
 	CC="$(tc-getBUILD_CC)" LD="$(tc-getLD)" \
 		IMAKECPP="${IMAKECPP:-${CHOST}-gcc -E}" xmkmf -a || die
 }

commit e7878ba74f45f95af5d6b354ffb6d1d67d503c46

net-misc/vncrec: pass -std=gnu89, fixup previous clang fix
Does not build with `clang -std=c2x`, early workaround for
when this will become a default.

(not actively hunting for these, merely revisiting packages
previously looked at for clang16 even if not an issue "yet").

wrt previous fix, thought could get away using a more precise
-Wno-error but gcc does not like it.

Fixes: 0491a4264baf9951dd726b96094459a7b3363b95
Signed-off-by: Ionen Wolkens <ionen@gentoo.org>

net-misc/vncrec/vncrec-0.2-r3.ebuild
@@ -45,7 +45,8 @@ src_configure() {
 	# XtErrorHandler usage matches docs (seems right), but headers "may" add
 	# __attribute__((noreturn)) giving an incompatible type error with clang-16
 	# (could alternatively use private _X_NORETURN but this may be fragile).
-	append-cflags -Wno-error=incompatible-function-pointer-types #871000
+	append-cflags -Wno-error=incompatible-pointer-types #871000
+	append-cflags -std=gnu89 # old codebase, incompatible with c2x
 
 	CC="$(tc-getBUILD_CC)" LD="$(tc-getLD)" \
 		IMAKECPP="${IMAKECPP:-${CHOST}-gcc -E}" xmkmf -a || die