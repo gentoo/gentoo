gentoo/gentoo

commit b86d9482f6ff14401970092b3034ef1cc2813163

kernel-build.eclass: use pbzip2/lbzip2 for parallel compression if available
Note that this will only take effect if compressing
modules with bzip2 (COMPRESS_MODULE_BZIP2 is set) or
compressing the kernel with bzip2 (CONFIG_KERNEL_BZIP2 is set).

Signed-off-by: Sam James <sam@gentoo.org>

eclass/kernel-build.eclass
@@ -98,11 +98,17 @@ kernel-build_src_configure() {
 		export ZSTD_NBTHREADS="$(makeopts_jobs)"
 	fi
 
-	# pigz needs to take an argument, not an env var,
-	# for its options, which won't work because of how the kernel build system
+	# pigz/pbzip2/lbzip2 all need to take an argument, not an env var,
+	# for their options, which won't work because of how the kernel build system
 	# uses the variables (e.g. passes directly to tar as an executable).
 	if type -P pigz ; then
 		MAKEARGS+=( KGZIP="pigz" )
+	fi
+
+	if type -P pbzip2 ; then
+		MAKEARGS+=( KBZIP2="pbzip2" )
+	elif type -P lbzip2 ; then
+		MAKEARGS+=( KBZIP2="lbzip2" )
 	fi
 
 	restore_config .config

commit 0ca217742778ccea10ca35472bdcba47cd47c167

kernel-build.eclass: silence tools search
Fixes: b86d9482f6ff14401970092b3034ef1cc2813163
Fixes: 61f0eab4e3c0272449024e83f32734b0e6d392bb
Signed-off-by: Sam James <sam@gentoo.org>

eclass/kernel-build.eclass
@@ -90,24 +90,24 @@ kernel-build_src_configure() {
 		ARCH=$(tc-arch-kernel)
 	)
 
-	if type -P xz ; then
+	if type -P xz &>/dev/null ; then
 		export XZ_OPT="-T$(makeopts_jobs)"
 	fi
 
-	if type -P zstd ; then
+	if type -P zstd &>/dev/null ; then
 		export ZSTD_NBTHREADS="$(makeopts_jobs)"
 	fi
 
 	# pigz/pbzip2/lbzip2 all need to take an argument, not an env var,
 	# for their options, which won't work because of how the kernel build system
 	# uses the variables (e.g. passes directly to tar as an executable).
-	if type -P pigz ; then
+	if type -P pigz &>/dev/null ; then
 		MAKEARGS+=( KGZIP="pigz" )
 	fi
 
-	if type -P pbzip2 ; then
+	if type -P pbzip2 &>/dev/null ; then
 		MAKEARGS+=( KBZIP2="pbzip2" )
-	elif type -P lbzip2 ; then
+	elif type -P lbzip2 &>/dev/null ; then
 		MAKEARGS+=( KBZIP2="lbzip2" )
 	fi
 