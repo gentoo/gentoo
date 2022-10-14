gentoo/gentoo

commit 5493c697dd05162ef850be8d0476fa09c3e6d623

sys-apps/man-pages: add maintenance notes
Based on a conversation with Alex upstream.

Signed-off-by: Sam James <sam@gentoo.org>

sys-apps/man-pages/man-pages-6.00.ebuild
@@ -3,14 +3,22 @@
 
 EAPI=8
 
-# Sometimes there's no dist tarball available post-release and upstream
-# encourage distros to make their own. Set to 1 if none is available, 0 otherwise.
+# Maintenance notes:
+# - Upstream are very friendly, do approach them if have any questions;
+#
+# - It's considered fine (and somewhat encouraged) for us to make snapshots
+#   if we want specific fixes, perhaps aligned with newer kernels, glibc, etc.
+#   Just generate it with 'make dist'. We can set DISTVERSION if we want a vanity
+#   name or if the comit hash is too long;
+#
+# - If we do use a snapshot, *don't* grab it directly from git and use it
+#   raw in the ebuild. Use 'make dist' as above;
+#
+# - Sometimes there's no dist tarball available post-release and upstream
+#   encourage distros to make their own. Set MAN_PAGES_GENTOO_DIST to 1 if none is
+#   available, 0 otherwise.
 MAN_PAGES_GENTOO_DIST=0
 GENTOO_PATCH=2
-
-if [[ ${PV} != 6.00 ]] ; then
-	die "Please use 9999 as a template for the next bump for SRC_URI fixes."
-fi
 
 DESCRIPTION="A somewhat comprehensive collection of Linux man pages"
 HOMEPAGE="https://www.kernel.org/doc/man-pages/"

sys-apps/man-pages/man-pages-9999.ebuild
@@ -3,8 +3,20 @@
 
 EAPI=8
 
-# Sometimes there's no dist tarball available post-release and upstream
-# encourage distros to make their own. Set to 1 if none is available, 0 otherwise.
+# Maintenance notes:
+# - Upstream are very friendly, do approach them if have any questions;
+#
+# - It's considered fine (and somewhat encouraged) for us to make snapshots
+#   if we want specific fixes, perhaps aligned with newer kernels, glibc, etc.
+#   Just generate it with 'make dist'. We can set DISTVERSION if we want a vanity
+#   name or if the comit hash is too long;
+#
+# - If we do use a snapshot, *don't* grab it directly from git and use it
+#   raw in the ebuild. Use 'make dist' as above;
+#
+# - Sometimes there's no dist tarball available post-release and upstream
+#   encourage distros to make their own. Set MAN_PAGES_GENTOO_DIST to 1 if none is
+#   available, 0 otherwise.
 MAN_PAGES_GENTOO_DIST=0
 GENTOO_PATCH=2
 

commit 97c29eed36005809e58ab98865035d04f6480de1

sys-apps/man-pages: restore 9999 if/die
Restore note about 9999 so we copy from that for next release.

Fixes: 5493c697dd05162ef850be8d0476fa09c3e6d623
Signed-off-by: Sam James <sam@gentoo.org>

sys-apps/man-pages/man-pages-6.00.ebuild
@@ -20,6 +20,10 @@ EAPI=8
 MAN_PAGES_GENTOO_DIST=0
 GENTOO_PATCH=2
 
+if [[ ${PV} != 6.00 ]] ; then
+	die "Please use 9999 as a template for the next bump for SRC_URI fixes."
+fi
+
 DESCRIPTION="A somewhat comprehensive collection of Linux man pages"
 HOMEPAGE="https://www.kernel.org/doc/man-pages/"
 if [[ ${PV} == 9999 ]] ; then