gentoo/gentoo

commit ddbae51c4cfc2f579deb0294f82f20e0609c4353

dev-lang/python: fix CFLAGS for cross-compilation
My CFLAGS for sparc etc were the same (generic) as for CBUILD, so didn't
notice before.

Closes: https://bugs.gentoo.org/878211
Fixes: 29d2314810bd8e1de73df5c3ebe30c826281c253
Signed-off-by: Sam James <sam@gentoo.org>

dev-lang/python/python-3.9.15.ebuild
@@ -232,6 +232,12 @@ src_configure() {
 	local -x CFLAGS= LDFLAGS=
 
 	if tc-is-cross-compiler ; then
+		# pass system CFLAGS & LDFLAGS as _NODIST, otherwise they'll get
+		# propagated to sysconfig for built extensions
+		local -x CFLAGS_NODIST=${CFLAGS_FOR_BUILD}
+		local -x LDFLAGS_NODIST=${LDFLAGS_FOR_BUILD}
+		local -x CFLAGS= LDFLAGS=
+
 		# We need to build our own Python on CBUILD first, and feed it in.
 		# bug #847910 and bug #864911.
 		local myeconfargs_cbuild=(
@@ -275,6 +281,12 @@ src_configure() {
 		emake
 		popd &> /dev/null || die
 	fi
+
+	# pass system CFLAGS & LDFLAGS as _NODIST, otherwise they'll get
+	# propagated to sysconfig for built extensions
+	local -x CFLAGS_NODIST=${CFLAGS}
+	local -x LDFLAGS_NODIST=${LDFLAGS}
+	local -x CFLAGS= LDFLAGS=
 
 	econf "${myeconfargs[@]}"
 

dev-lang/python/python-3.10.8.ebuild
@@ -231,13 +231,14 @@ src_configure() {
 
 	# disable implicit optimization/debugging flags
 	local -x OPT=
-	# pass system CFLAGS & LDFLAGS as _NODIST, otherwise they'll get
-	# propagated to sysconfig for built extensions
-	local -x CFLAGS_NODIST=${CFLAGS}
-	local -x LDFLAGS_NODIST=${LDFLAGS}
-	local -x CFLAGS= LDFLAGS=
 
 	if tc-is-cross-compiler ; then
+		# pass system CFLAGS & LDFLAGS as _NODIST, otherwise they'll get
+		# propagated to sysconfig for built extensions
+		local -x CFLAGS_NODIST=${CFLAGS_FOR_BUILD}
+		local -x LDFLAGS_NODIST=${LDFLAGS_FOR_BUILD}
+		local -x CFLAGS= LDFLAGS=
+
 		# We need to build our own Python on CBUILD first, and feed it in.
 		# bug #847910 and bug #864911.
 		local myeconfargs_cbuild=(
@@ -281,6 +282,12 @@ src_configure() {
 		emake
 		popd &> /dev/null || die
 	fi
+
+	# pass system CFLAGS & LDFLAGS as _NODIST, otherwise they'll get
+	# propagated to sysconfig for built extensions
+	local -x CFLAGS_NODIST=${CFLAGS}
+	local -x LDFLAGS_NODIST=${LDFLAGS}
+	local -x CFLAGS= LDFLAGS=
 
 	econf "${myeconfargs[@]}"
 

dev-lang/python/python-3.11.0.ebuild
@@ -220,13 +220,14 @@ src_configure() {
 
 	# disable implicit optimization/debugging flags
 	local -x OPT=
-	# pass system CFLAGS & LDFLAGS as _NODIST, otherwise they'll get
-	# propagated to sysconfig for built extensions
-	local -x CFLAGS_NODIST=${CFLAGS}
-	local -x LDFLAGS_NODIST=${LDFLAGS}
-	local -x CFLAGS= LDFLAGS=
 
 	if tc-is-cross-compiler ; then
+		# pass system CFLAGS & LDFLAGS as _NODIST, otherwise they'll get
+		# propagated to sysconfig for built extensions
+		local -x CFLAGS_NODIST=${CFLAGS_FOR_BUILD}
+		local -x LDFLAGS_NODIST=${LDFLAGS_FOR_BUILD}
+		local -x CFLAGS= LDFLAGS=
+
 		# We need to build our own Python on CBUILD first, and feed it in.
 		# bug #847910
 		local myeconfargs_cbuild=(
@@ -273,6 +274,12 @@ src_configure() {
 		emake
 		popd &> /dev/null || die
 	fi
+
+	# pass system CFLAGS & LDFLAGS as _NODIST, otherwise they'll get
+	# propagated to sysconfig for built extensions
+	local -x CFLAGS_NODIST=${CFLAGS}
+	local -x LDFLAGS_NODIST=${LDFLAGS}
+	local -x CFLAGS= LDFLAGS=
 
 	econf "${myeconfargs[@]}"
 

dev-lang/python/python-3.8.15.ebuild
@@ -184,13 +184,14 @@ src_configure() {
 
 	# disable implicit optimization/debugging flags
 	local -x OPT=
-	# pass system CFLAGS & LDFLAGS as _NODIST, otherwise they'll get
-	# propagated to sysconfig for built extensions
-	local -x CFLAGS_NODIST=${CFLAGS}
-	local -x LDFLAGS_NODIST=${LDFLAGS}
-	local -x CFLAGS= LDFLAGS=
 
 	if tc-is-cross-compiler ; then
+		# pass system CFLAGS & LDFLAGS as _NODIST, otherwise they'll get
+		# propagated to sysconfig for built extensions
+		local -x CFLAGS_NODIST=${CFLAGS_FOR_BUILD}
+		local -x LDFLAGS_NODIST=${LDFLAGS_FOR_BUILD}
+		local -x CFLAGS= LDFLAGS=
+
 		# We need to build our own Python on CBUILD first, and feed it in.
 		# bug #847910 and bug #864911.
 		local myeconfargs_cbuild=(
@@ -234,6 +235,12 @@ src_configure() {
 		emake
 		popd &> /dev/null || die
 	fi
+
+	# pass system CFLAGS & LDFLAGS as _NODIST, otherwise they'll get
+	# propagated to sysconfig for built extensions
+	local -x CFLAGS_NODIST=${CFLAGS}
+	local -x LDFLAGS_NODIST=${LDFLAGS}
+	local -x CFLAGS= LDFLAGS=
 
 	econf "${myeconfargs[@]}"
 

commit 28482b456cd9bbe9fff822ca6ef736540f029a9f

dev-lang/python: fix py3.9 CFLAGS too
Bug: https://bugs.gentoo.org/878211
Fixes: ddbae51c4cfc2f579deb0294f82f20e0609c4353
Signed-off-by: Sam James <sam@gentoo.org>

dev-lang/python/python-3.9.15.ebuild
@@ -225,11 +225,6 @@ src_configure() {
 
 	# disable implicit optimization/debugging flags
 	local -x OPT=
-	# pass system CFLAGS & LDFLAGS as _NODIST, otherwise they'll get
-	# propagated to sysconfig for built extensions
-	local -x CFLAGS_NODIST=${CFLAGS}
-	local -x LDFLAGS_NODIST=${LDFLAGS}
-	local -x CFLAGS= LDFLAGS=
 
 	if tc-is-cross-compiler ; then
 		# pass system CFLAGS & LDFLAGS as _NODIST, otherwise they'll get