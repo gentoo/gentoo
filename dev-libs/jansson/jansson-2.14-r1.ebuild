gentoo/gentoo

commit 19508b21a9e8854d472133717f4eeb39b3638a21

dev-libs/jansson: drop multilib
No multilib reverse dependencies.

Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/jansson/jansson-2.14-r1.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-inherit multilib-minimal toolchain-funcs
+inherit toolchain-funcs
 
 DESCRIPTION="C library for encoding, decoding and manipulating JSON data"
 HOMEPAGE="https://www.digip.org/jansson/"
@@ -18,23 +18,23 @@ BDEPEND="doc? ( dev-python/sphinx )"
 
 PATCHES=( "${FILESDIR}/${P}-test-symbols.patch" )
 
-multilib_src_configure() {
+src_configure() {
 	tc-ld-force-bfd
 
-	ECONF_SOURCE="${S}" econf $(use_enable static-libs static)
+	econf $(use_enable static-libs static)
 }
 
-multilib_src_compile() {
+src_compile() {
 	default
 
-	if multilib_is_native_abi && use doc ; then
+	if use doc ; then
 		emake html
 		HTML_DOCS=( "${BUILD_DIR}"/doc/_build/html/. )
 	fi
 }
 
-multilib_src_install() {
+src_install() {
 	default
 
-	find "${D}" -name '*.la' -delete || die
+	find "${ED}" -name '*.la' -delete || die
 }

commit 01c2a478480540be33ebfe1a9492f1093baa551b

dev-libs/jansson: fix doc install
Closes: https://bugs.gentoo.org/878597
Fixes: 19508b21a9e8854d472133717f4eeb39b3638a21
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/jansson/jansson-2.14-r1.ebuild
@@ -29,7 +29,7 @@ src_compile() {
 
 	if use doc ; then
 		emake html
-		HTML_DOCS=( "${BUILD_DIR}"/doc/_build/html/. )
+		HTML_DOCS=( doc/_build/html/. )
 	fi
 }
 