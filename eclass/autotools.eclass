gentoo/gentoo

commit cece5b154234aa8c9982feae05f67df8db39e270

autotools.eclass: crank up autoconf minimum versions
This ensures we have versions which have Clang 16 / GCC 14 / stricter
compiler fixes, so 'eautoreconf' will Do The Right Thing.

Closes: https://bugs.gentoo.org/870412
Signed-off-by: Sam James <sam@gentoo.org>

eclass/autotools.eclass
@@ -93,9 +93,9 @@ fi
 if [[ -n ${WANT_AUTOCONF} ]] ; then
 	case ${WANT_AUTOCONF} in
 		none)       _autoconf_atom="" ;; # some packages don't require autoconf at all
-		2.1)        _autoconf_atom="~sys-devel/autoconf-2.13" ;;
+		2.1)        _autoconf_atom="~sys-devel/autoconf-2.13-r7" ;;
 		# if you change the "latest" version here, change also autotools_env_setup
-		latest|2.5) _autoconf_atom=">=sys-devel/autoconf-2.71" ;;
+		latest|2.5) _autoconf_atom=">=sys-devel/autoconf-2.71-r5" ;;
 		*)          die "Invalid WANT_AUTOCONF value '${WANT_AUTOCONF}'" ;;
 	esac
 	export WANT_AUTOCONF

commit 141c29e6e6a3bf3ffdcfa3be99c0ef410dcb8bc9

autotools.eclass: fix autoconf 2.13 minimum bound
Fixes: cece5b154234aa8c9982feae05f67df8db39e270
Signed-off-by: Sam James <sam@gentoo.org>

eclass/autotools.eclass
@@ -93,7 +93,7 @@ fi
 if [[ -n ${WANT_AUTOCONF} ]] ; then
 	case ${WANT_AUTOCONF} in
 		none)       _autoconf_atom="" ;; # some packages don't require autoconf at all
-		2.1)        _autoconf_atom="~sys-devel/autoconf-2.13-r7" ;;
+		2.1)        _autoconf_atom=">=sys-devel/autoconf-2.13-r7:2.1" ;;
 		# if you change the "latest" version here, change also autotools_env_setup
 		latest|2.5) _autoconf_atom=">=sys-devel/autoconf-2.71-r5" ;;
 		*)          die "Invalid WANT_AUTOCONF value '${WANT_AUTOCONF}'" ;;