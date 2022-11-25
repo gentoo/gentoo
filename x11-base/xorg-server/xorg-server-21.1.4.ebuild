gentoo/gentoo

commit 20d3f0132014dc3f5c321b0d457d985c1e04171d

x11-base/xorg-server: replace -Os w/ -O2
Closes: https://bugs.gentoo.org/835653
Signed-off-by: Sam James <sam@gentoo.org>

x11-base/xorg-server/xorg-server-21.1.4.ebuild
@@ -5,7 +5,7 @@ EAPI=8
 
 XORG_TARBALL_SUFFIX="xz"
 XORG_EAUTORECONF="no"
-inherit xorg-3 meson
+inherit flag-o-matic xorg-3 meson
 EGIT_REPO_URI="https://gitlab.freedesktop.org/xorg/xserver.git"
 
 DESCRIPTION="X.Org X servers"
@@ -103,10 +103,12 @@ PATCHES=(
 )
 
 src_configure() {
+	# bug #835653
+	use amd64 && replace-flags -Os -O2
+
 	# localstatedir is used for the log location; we need to override the default
 	#	from ebuild.sh
 	# sysconfdir is used for the xorg.conf location; same applies
-
 	local emesonargs=(
 		--localstatedir "${EPREFIX}/var"
 		--sysconfdir "${EPREFIX}/etc/X11"

x11-base/xorg-server/xorg-server-9999.ebuild
@@ -5,7 +5,7 @@ EAPI=8
 
 XORG_TARBALL_SUFFIX="xz"
 XORG_EAUTORECONF="no"
-inherit xorg-3 meson
+inherit flag-o-matic xorg-3 meson
 EGIT_REPO_URI="https://gitlab.freedesktop.org/xorg/xserver.git"
 
 DESCRIPTION="X.Org X servers"
@@ -103,10 +103,12 @@ PATCHES=(
 )
 
 src_configure() {
+	# bug #835653
+	use x86 && replace-flags -Os -O2
+
 	# localstatedir is used for the log location; we need to override the default
 	#	from ebuild.sh
 	# sysconfdir is used for the xorg.conf location; same applies
-
 	local emesonargs=(
 		--localstatedir "${EPREFIX}/var"
 		--sysconfdir "${EPREFIX}/etc/X11"

commit 772a48472355bc5185035119de3ca823a88f5049

x11-base/xorg-server: fix replace-flags for just x86
Missync from 20d3f0132014dc3f5c321b0d457d985c1e04171d.

Closes: https://bugs.gentoo.org/835653
Fixes: 20d3f0132014dc3f5c321b0d457d985c1e04171d
Signed-off-by: Sam James <sam@gentoo.org>

x11-base/xorg-server/xorg-server-21.1.4.ebuild
@@ -104,7 +104,7 @@ PATCHES=(
 
 src_configure() {
 	# bug #835653
-	use amd64 && replace-flags -Os -O2
+	use x86 && replace-flags -Os -O2
 
 	# localstatedir is used for the log location; we need to override the default
 	#	from ebuild.sh