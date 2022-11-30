gentoo/gentoo

commit 476e425a52e834eb067f44c98a8c1faefdef260b

Revert "toolchain.eclass: add initial sphinx support for 13"
This reverts commit 803ac64d367c4c865a2c30f1196dfec95dc7fee1.

This was dropped upstream and no snapshots of GCC 13
in Gentoo use the variables from this commit anymore.

Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -100,12 +100,6 @@ tc_version_is_between() {
 # @DESCRIPTION:
 # Indicate the developer who hosts the patchset for an ebuild.
 
-# @ECLASS_VARIABLE: TOOLCHAIN_SPHINX_BUILD
-# @DEFAULT_UNSET
-# @DESCRIPTION:
-# Indicate if Sphinx is needed for this build. Enabled by default for
-# >= 13.*.9999 (this is when upstream changed their docs from texinfo->sphinx).
-
 # @ECLASS_VARIABLE: GCC_PV
 # @INTERNAL
 # @DESCRIPTION:
@@ -295,8 +289,7 @@ fi
 
 #---->> DEPEND <<----
 
-RDEPEND="
-	sys-libs/zlib
+RDEPEND="sys-libs/zlib
 	virtual/libiconv
 	nls? ( virtual/libintl )
 "
@@ -327,18 +320,10 @@ BDEPEND="
 	test? (
 		>=dev-util/dejagnu-1.4.4
 		>=sys-devel/autogen-5.5.4
-	)
-"
+	)"
 DEPEND="${RDEPEND}"
 
-# Snapshots don't contain info or man pages.
 if [[ ${PN} == gcc && ${PV} == *_p* ]] ; then
-	# >= GCC 13 needs sphinx to generate the info and man pages.
-	# It still uses texinfo as well.
-	if [[ -n ${TOOLCHAIN_SPHINX_BUILD} ]] ; then
-		BDEPEND+=" dev-python/sphinx"
-	fi
-
 	# Snapshots don't contain info pages.
 	# If they start to, adjust gcc_cv_prog_makeinfo_modern logic in toolchain_src_configure.
 	# Needed unless/until https://gcc.gnu.org/bugzilla/show_bug.cgi?id=106899 is fixed

commit 6a6a30d1d686f58d7285ac336cab569d4548e8da

toolchain.eclass: restore style changes lost in GCC 13 sphinx revert
Fixes: 476e425a52e834eb067f44c98a8c1faefdef260b
Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -291,7 +291,8 @@ fi
 
 #---->> DEPEND <<----
 
-RDEPEND="sys-libs/zlib
+RDEPEND="
+	sys-libs/zlib
 	virtual/libiconv
 	nls? ( virtual/libintl )
 "
@@ -322,7 +323,8 @@ BDEPEND="
 	test? (
 		>=dev-util/dejagnu-1.4.4
 		>=sys-devel/autogen-5.5.4
-	)"
+	)
+"
 DEPEND="${RDEPEND}"
 
 if [[ ${PN} == gcc && ${PV} == *_p* ]] ; then