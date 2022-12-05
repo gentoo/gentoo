gentoo/gentoo

commit adf44514cf590a860b3140efae31187854cf8cea

toolchain.eclass: allow default-znow, default-stack-clash-protection for 12.2.1_p20221203
Bug: https://bugs.gentoo.org/675050
Bug: https://bugs.gentoo.org/876923
Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -278,8 +278,8 @@ if [[ ${PN} != kgcc64 && ${PN} != gcc-* ]] ; then
 	tc_version_is_at_least 11 && IUSE+=" valgrind" TC_FEATURES+=( valgrind )
 	tc_version_is_at_least 11 && IUSE+=" custom-cflags"
 	tc_version_is_at_least 12 && IUSE+=" ieee-long-double"
-	tc_version_is_at_least 12.99 && IUSE+=" default-znow"
-	tc_version_is_at_least 12.99 && IUSE+=" default-stack-clash-protection"
+	tc_version_is_at_least 12.2.1_p20221203 && IUSE+=" default-znow"
+	tc_version_is_at_least 12.2.1_p20221203 && IUSE+=" default-stack-clash-protection"
 fi
 
 if tc_version_is_at_least 10; then

commit 5bb51e9a29ab1683a36ed45c5aa56228b6968d3f

toolchain.eclass: fix default-* addition for new GCC 12 snapshot
Fixes: adf44514cf590a860b3140efae31187854cf8cea
Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -278,8 +278,8 @@ if [[ ${PN} != kgcc64 && ${PN} != gcc-* ]] ; then
 	tc_version_is_at_least 11 && IUSE+=" valgrind" TC_FEATURES+=( valgrind )
 	tc_version_is_at_least 11 && IUSE+=" custom-cflags"
 	tc_version_is_at_least 12 && IUSE+=" ieee-long-double"
-	tc_version_is_at_least 12.2.1_p20221203 && IUSE+=" default-znow"
-	tc_version_is_at_least 12.2.1_p20221203 && IUSE+=" default-stack-clash-protection"
+	tc_version_is_at_least 12.2.1_p20221203 ${PV} && IUSE+=" default-znow"
+	tc_version_is_at_least 12.2.1_p20221203 ${PV} && IUSE+=" default-stack-clash-protection"
 fi
 
 if tc_version_is_at_least 10; then