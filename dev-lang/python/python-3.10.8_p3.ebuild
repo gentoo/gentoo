gentoo/gentoo

commit 3478cabb5f94e9d90ae1ed9e3f7909a1146aa471

dev-lang/python: disable _ctypes, _crypt for cross (CBUILD) Python
* Use a hack to deduce libdir to nudge the build system in the right
  direction because get_libdir can't handle BROOT/CBUILD right now.

* Disable _ctypes (libffi) and _crypt (lib(x)crypt) modules for cross (CBUILD)
  Python as they're not needed and Python struggles to find the libraries
  correctly.

  This avoids an issue where setup.py can't handle deducing libdir
  (or even being passed it) for cross-compilation. Fortunately, cpython
  is migrating away from setup.py to autotools.

Note that this isn't the same as what bug 633712 would've been at the time
it was reported, but it certainly showed up the same way on more modern
Pythons since we introduced the CBUILD part a few months ago.

Bug: https://bugs.gentoo.org/633712
Bug: https://bugs.gentoo.org/794181
Bug: https://bugs.gentoo.org/864911
Signed-off-by: Sam James <sam@gentoo.org>

dev-lang/python/python-3.10.8_p3.ebuild
@@ -230,6 +230,10 @@ src_configure() {
 	local -x OPT=
 
 	if tc-is-cross-compiler ; then
+		# Hack to workaround get_libdir not being able to handle CBUILD, bug #794181
+		local cbuild_libdir=$(unset PKG_CONFIG_PATH ; $(tc-getBUILD_PKG_CONFIG) --keep-system-libs --libs-only-L libffi)
+		append-flags "${cbuild_libdir}"
+
 		# pass system CFLAGS & LDFLAGS as _NODIST, otherwise they'll get
 		# propagated to sysconfig for built extensions
 		local -x CFLAGS_NODIST=${CFLAGS_FOR_BUILD}
@@ -240,6 +244,8 @@ src_configure() {
 		# bug #847910 and bug #864911.
 		local myeconfargs_cbuild=(
 			"${myeconfargs[@]}"
+
+			--libdir="${cbuild_libdir:2}"
 
 			# As minimal as possible for the mini CBUILD Python
 			# we build just for cross.
@@ -253,7 +259,10 @@ src_configure() {
 
 		mkdir "${WORKDIR}"/${P}-${CBUILD} || die
 		pushd "${WORKDIR}"/${P}-${CBUILD} &> /dev/null || die
-		ECONF_SOURCE="${S}" econf_build "${myeconfargs_cbuild[@]}"
+		# We disable _ctypes and _crypt for CBUILD because Python's setup.py can't handle locating
+		# libdir correctly for cross.
+		PYTHON_DISABLE_MODULES="${PYTHON_DISABLE_MODULES} _ctypes _crypt" \
+			ECONF_SOURCE="${S}" econf_build "${myeconfargs_cbuild[@]}"
 
 		# Avoid as many dependencies as possible for the cross build.
 		cat >> Makefile <<-EOF || die
@@ -276,7 +285,7 @@ src_configure() {
 		# not in src_compile, because CHOST configure for Python
 		# will check the existence of the Python it was pointed to
 		# immediately.
-		emake
+		PYTHON_DISABLE_MODULES="${PYTHON_DISABLE_MODULES} _ctypes _crypt" emake
 		popd &> /dev/null || die
 	fi
 

dev-lang/python/python-3.11.0_p2.ebuild
@@ -219,6 +219,10 @@ src_configure() {
 	local -x OPT=
 
 	if tc-is-cross-compiler ; then
+		# Hack to workaround get_libdir not being able to handle CBUILD, bug #794181
+		local cbuild_libdir=$(unset PKG_CONFIG_PATH ; $(tc-getBUILD_PKG_CONFIG) --keep-system-libs --libs-only-L libffi)
+		append-flags "${cbuild_libdir}"
+
 		# pass system CFLAGS & LDFLAGS as _NODIST, otherwise they'll get
 		# propagated to sysconfig for built extensions
 		local -x CFLAGS_NODIST=${CFLAGS_FOR_BUILD}
@@ -229,6 +233,8 @@ src_configure() {
 		# bug #847910
 		local myeconfargs_cbuild=(
 			"${myeconfargs[@]}"
+
+			--libdir="${cbuild_libdir:2}"
 
 			# As minimal as possible for the mini CBUILD Python
 			# we build just for cross to satisfy --with-build-python.
@@ -245,7 +251,10 @@ src_configure() {
 
 		mkdir "${WORKDIR}"/${P}-${CBUILD} || die
 		pushd "${WORKDIR}"/${P}-${CBUILD} &> /dev/null || die
-		ECONF_SOURCE="${S}" econf_build "${myeconfargs_cbuild[@]}"
+		# We disable _ctypes and _crypt for CBUILD because Python's setup.py can't handle locating
+		# libdir correctly for cross.
+		PYTHON_DISABLE_MODULES="${PYTHON_DISABLE_MODULES} _ctypes _crypt" \
+			ECONF_SOURCE="${S}" econf_build "${myeconfargs_cbuild[@]}"
 
 		# Avoid as many dependencies as possible for the cross build.
 		cat >> Makefile <<-EOF || die
@@ -268,7 +277,7 @@ src_configure() {
 		# not in src_compile, because CHOST configure for Python
 		# will check the existence of the --with-build-python value
 		# immediately.
-		emake
+		PYTHON_DISABLE_MODULES="${PYTHON_DISABLE_MODULES} _ctypes _crypt" emake
 		popd &> /dev/null || die
 	fi
 

dev-lang/python/python-3.12.0_alpha2.ebuild
@@ -215,6 +215,10 @@ src_configure() {
 	local -x OPT=
 
 	if tc-is-cross-compiler ; then
+		# Hack to workaround get_libdir not being able to handle CBUILD, bug #794181
+		local cbuild_libdir=$(unset PKG_CONFIG_PATH ; $(tc-getBUILD_PKG_CONFIG) --keep-system-libs --libs-only-L libffi)
+		append-flags "${cbuild_libdir}"
+
 		# pass system CFLAGS & LDFLAGS as _NODIST, otherwise they'll get
 		# propagated to sysconfig for built extensions
 		local -x CFLAGS_NODIST=${CFLAGS_FOR_BUILD}
@@ -225,6 +229,8 @@ src_configure() {
 		# bug #847910
 		local myeconfargs_cbuild=(
 			"${myeconfargs[@]}"
+
+			--libdir="${cbuild_libdir:2}"
 
 			# As minimal as possible for the mini CBUILD Python
 			# we build just for cross to satisfy --with-build-python.
@@ -241,7 +247,10 @@ src_configure() {
 
 		mkdir "${WORKDIR}"/${P}-${CBUILD} || die
 		pushd "${WORKDIR}"/${P}-${CBUILD} &> /dev/null || die
-		ECONF_SOURCE="${S}" econf_build "${myeconfargs_cbuild[@]}"
+		# We disable _ctypes and _crypt for CBUILD because Python's setup.py can't handle locating
+		# libdir correctly for cross.
+		PYTHON_DISABLE_MODULES="${PYTHON_DISABLE_MODULES} _ctypes _crypt" \
+			ECONF_SOURCE="${S}" econf_build "${myeconfargs_cbuild[@]}"
 
 		# Avoid as many dependencies as possible for the cross build.
 		cat >> Makefile <<-EOF || die
@@ -264,7 +273,7 @@ src_configure() {
 		# not in src_compile, because CHOST configure for Python
 		# will check the existence of the --with-build-python value
 		# immediately.
-		emake
+		PYTHON_DISABLE_MODULES="${PYTHON_DISABLE_MODULES} _ctypes _crypt" emake
 		popd &> /dev/null || die
 	fi
 

dev-lang/python/python-3.8.15_p3.ebuild
@@ -184,6 +184,10 @@ src_configure() {
 	local -x OPT=
 
 	if tc-is-cross-compiler ; then
+		# Hack to workaround get_libdir not being able to handle CBUILD, bug #794181
+		local cbuild_libdir=$(unset PKG_CONFIG_PATH ; $(tc-getBUILD_PKG_CONFIG) --keep-system-libs --libs-only-L libffi)
+		append-flags "${cbuild_libdir}"
+
 		# pass system CFLAGS & LDFLAGS as _NODIST, otherwise they'll get
 		# propagated to sysconfig for built extensions
 		local -x CFLAGS_NODIST=${CFLAGS_FOR_BUILD}
@@ -194,6 +198,8 @@ src_configure() {
 		# bug #847910 and bug #864911.
 		local myeconfargs_cbuild=(
 			"${myeconfargs[@]}"
+
+			--libdir="${cbuild_libdir:2}"
 
 			# As minimal as possible for the mini CBUILD Python
 			# we build just for cross.
@@ -207,7 +213,10 @@ src_configure() {
 
 		mkdir "${WORKDIR}"/${P}-${CBUILD} || die
 		pushd "${WORKDIR}"/${P}-${CBUILD} &> /dev/null || die
-		ECONF_SOURCE="${S}" econf_build "${myeconfargs_cbuild[@]}"
+		# We disable _ctypes and _crypt for CBUILD because Python's setup.py can't handle locating
+		# libdir correctly for cross.
+		PYTHON_DISABLE_MODULES="${PYTHON_DISABLE_MODULES} _ctypes _crypt" \
+			ECONF_SOURCE="${S}" econf_build "${myeconfargs_cbuild[@]}"
 
 		# Avoid as many dependencies as possible for the cross build.
 		cat >> Makefile <<-EOF || die
@@ -230,7 +239,7 @@ src_configure() {
 		# not in src_compile, because CHOST configure for Python
 		# will check the existence of the Python it was pointed to
 		# immediately.
-		emake
+		PYTHON_DISABLE_MODULES="${PYTHON_DISABLE_MODULES} _ctypes _crypt" emake
 		popd &> /dev/null || die
 	fi
 

dev-lang/python/python-3.9.15_p3.ebuild
@@ -224,6 +224,10 @@ src_configure() {
 	local -x OPT=
 
 	if tc-is-cross-compiler ; then
+		# Hack to workaround get_libdir not being able to handle CBUILD, bug #794181
+		local cbuild_libdir=$(unset PKG_CONFIG_PATH ; $(tc-getBUILD_PKG_CONFIG) --keep-system-libs --libs-only-L libffi)
+		append-flags "${cbuild_libdir}"
+
 		# pass system CFLAGS & LDFLAGS as _NODIST, otherwise they'll get
 		# propagated to sysconfig for built extensions
 		local -x CFLAGS_NODIST=${CFLAGS_FOR_BUILD}
@@ -234,6 +238,8 @@ src_configure() {
 		# bug #847910 and bug #864911.
 		local myeconfargs_cbuild=(
 			"${myeconfargs[@]}"
+
+			--libdir="${cbuild_libdir:2}"
 
 			# As minimal as possible for the mini CBUILD Python
 			# we build just for cross.
@@ -247,7 +253,10 @@ src_configure() {
 
 		mkdir "${WORKDIR}"/${P}-${CBUILD} || die
 		pushd "${WORKDIR}"/${P}-${CBUILD} &> /dev/null || die
-		ECONF_SOURCE="${S}" econf_build "${myeconfargs_cbuild[@]}"
+		# We disable _ctypes and _crypt for CBUILD because Python's setup.py can't handle locating
+		# libdir correctly for cross.
+		PYTHON_DISABLE_MODULES="${PYTHON_DISABLE_MODULES} _ctypes _crypt" \
+			ECONF_SOURCE="${S}" econf_build "${myeconfargs_cbuild[@]}"
 
 		# Avoid as many dependencies as possible for the cross build.
 		cat >> Makefile <<-EOF || die
@@ -270,7 +279,7 @@ src_configure() {
 		# not in src_compile, because CHOST configure for Python
 		# will check the existence of the Python it was pointed to
 		# immediately.
-		emake
+		PYTHON_DISABLE_MODULES="${PYTHON_DISABLE_MODULES} _ctypes _crypt" emake
 		popd &> /dev/null || die
 	fi
 

commit c66b74303dc1292ac24677af472acde2a86c67f7

dev-lang/python: drop leftover debugging append-flags
Fixes: 3478cabb5f94e9d90ae1ed9e3f7909a1146aa471
Signed-off-by: Sam James <sam@gentoo.org>

dev-lang/python/python-3.10.8_p3.ebuild
@@ -232,7 +232,6 @@ src_configure() {
 	if tc-is-cross-compiler ; then
 		# Hack to workaround get_libdir not being able to handle CBUILD, bug #794181
 		local cbuild_libdir=$(unset PKG_CONFIG_PATH ; $(tc-getBUILD_PKG_CONFIG) --keep-system-libs --libs-only-L libffi)
-		append-flags "${cbuild_libdir}"
 
 		# pass system CFLAGS & LDFLAGS as _NODIST, otherwise they'll get
 		# propagated to sysconfig for built extensions

dev-lang/python/python-3.11.0_p2.ebuild
@@ -221,7 +221,6 @@ src_configure() {
 	if tc-is-cross-compiler ; then
 		# Hack to workaround get_libdir not being able to handle CBUILD, bug #794181
 		local cbuild_libdir=$(unset PKG_CONFIG_PATH ; $(tc-getBUILD_PKG_CONFIG) --keep-system-libs --libs-only-L libffi)
-		append-flags "${cbuild_libdir}"
 
 		# pass system CFLAGS & LDFLAGS as _NODIST, otherwise they'll get
 		# propagated to sysconfig for built extensions

dev-lang/python/python-3.12.0_alpha2.ebuild
@@ -217,7 +217,6 @@ src_configure() {
 	if tc-is-cross-compiler ; then
 		# Hack to workaround get_libdir not being able to handle CBUILD, bug #794181
 		local cbuild_libdir=$(unset PKG_CONFIG_PATH ; $(tc-getBUILD_PKG_CONFIG) --keep-system-libs --libs-only-L libffi)
-		append-flags "${cbuild_libdir}"
 
 		# pass system CFLAGS & LDFLAGS as _NODIST, otherwise they'll get
 		# propagated to sysconfig for built extensions

dev-lang/python/python-3.8.15_p3.ebuild
@@ -186,7 +186,6 @@ src_configure() {
 	if tc-is-cross-compiler ; then
 		# Hack to workaround get_libdir not being able to handle CBUILD, bug #794181
 		local cbuild_libdir=$(unset PKG_CONFIG_PATH ; $(tc-getBUILD_PKG_CONFIG) --keep-system-libs --libs-only-L libffi)
-		append-flags "${cbuild_libdir}"
 
 		# pass system CFLAGS & LDFLAGS as _NODIST, otherwise they'll get
 		# propagated to sysconfig for built extensions

dev-lang/python/python-3.9.15_p3.ebuild
@@ -226,7 +226,6 @@ src_configure() {
 	if tc-is-cross-compiler ; then
 		# Hack to workaround get_libdir not being able to handle CBUILD, bug #794181
 		local cbuild_libdir=$(unset PKG_CONFIG_PATH ; $(tc-getBUILD_PKG_CONFIG) --keep-system-libs --libs-only-L libffi)
-		append-flags "${cbuild_libdir}"
 
 		# pass system CFLAGS & LDFLAGS as _NODIST, otherwise they'll get
 		# propagated to sysconfig for built extensions