b4d297f4e808301e89d253268b9f3d278477eb83
Maciej Barć <xgqt@gentoo.org>
2022-12-12 13:22:52 +0100

    app-emacs/cider: new package; add 1.5.0
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

---
+++ app-emacs/cider/files/50cider-gentoo.el
@@ -0,0 +1,5 @@
+(add-to-list 'load-path "@SITELISP@")
+(autoload 'cider "cider"
+  "Start a connection of any type interactively." t)
+(autoload 'cider-mode "cider-mode"
+  "Minor mode for REPL interaction from a Clojure buffer." t)
