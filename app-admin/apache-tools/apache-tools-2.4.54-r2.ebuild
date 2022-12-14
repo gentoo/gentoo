gentoo/gentoo

commit 5e0cb606997d6b1857974ca7b6e6042fdfc3ed07

app-admin/apache-tools: drop 2.4.53-r1, 2.4.54-r1, 2.4.54-r2
Signed-off-by: Sam James <sam@gentoo.org>

app-admin/apache-tools/apache-tools-2.4.54-r2.ebuild
@@ -1,109 +1,0 @@
-# Copyright 1999-2022 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=8
-
-inherit autotools toolchain-funcs
-
-DESCRIPTION="Useful Apache tools - htdigest, htpasswd, ab, htdbm"
-HOMEPAGE="https://httpd.apache.org/"
-SRC_URI="mirror://apache/httpd/httpd-${PV}.tar.bz2"
-
-LICENSE="Apache-2.0"
-SLOT="0"
-KEYWORDS="~alpha amd64 arm arm64 ~hppa ~ia64 ~loong ~mips ~ppc ~ppc64 ~riscv ~s390 sparc x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~sparc64-solaris ~x64-solaris"
-IUSE="ssl"
-RESTRICT="test"
-
-RDEPEND=">=dev-libs/apr-1.5.0:1=
-	dev-libs/apr-util:1=
-	dev-libs/expat
-	dev-libs/libpcre
-	virtual/libcrypt:=
-	kernel_linux? ( sys-apps/util-linux )
-	ssl? ( dev-libs/openssl:0= )"
-DEPEND="${RDEPEND}
-	sys-devel/libtool"
-BDEPEND="
-	virtual/pkgconfig
-"
-
-S="${WORKDIR}/httpd-${PV}"
-
-PATCHES=(
-	"${FILESDIR}/${PN}-2.4.54-Makefile.patch" #459446
-	"${FILESDIR}/${PN}-2.4.54-no-which.patch" #844868
-	"${FILESDIR}/${PN}-2.4.54-libtool.patch" #858833
-)
-
-src_prepare() {
-	default
-
-	# This package really should upgrade to using pcre's .pc file.
-	cat <<-\EOF >"${T}"/pcre-config
-	#!/bin/bash
-	flags=()
-	for flag; do
-		if [[ ${flag} == "--version" ]]; then
-			flags+=( --modversion )
-		else
-			flags+=( "${flag}" )
-		fi
-	done
-	exec ${PKG_CONFIG} libpcre "${flags[@]}"
-	EOF
-	chmod a+x "${T}"/pcre-config || die
-
-	# Only here for libtool and which patches
-	eautoreconf
-}
-
-src_configure() {
-	# Brain dead check.
-	tc-is-cross-compiler && export ap_cv_void_ptr_lt_long="no"
-
-	tc-export PKG_CONFIG
-
-	local myeconfargs=(
-		--libexecdir="${EPREFIX}"/usr/$(get_libdir)/apache2/modules
-		--sbindir="${EPREFIX}"/usr/sbin
-		--with-z="${EPREFIX}"/usr
-		--with-apr="${ESYSROOT}"/usr
-		--with-apr-util="${ESYSROOT}"/usr
-		--with-pcre="${T}"/pcre-config
-		$(use_enable ssl)
-		$(usex ssl '--with-ssl="${EPREFIX}"/usr' '')
-	)
-	# econf overwrites the stuff from config.layout.
-	ac_cv_path_PKGCONFIG="${PKG_CONFIG}" \
-	econf "${myeconfargs[@]}"
-	sed -i \
-		-e '/^LTFLAGS/s:--silent::' \
-		build/rules.mk build/config_vars.mk || die
-}
-
-src_compile() {
-	emake -C support
-}
-
-src_install() {
-	emake -C support DESTDIR="${D}" install
-	dodoc CHANGES
-	doman docs/man/{dbmmanage,htdigest,htpasswd,htdbm,ab,logresolve}.1 \
-		docs/man/{htcacheclean,rotatelogs}.8
-
-	# Providing compatiblity symlinks for #177697 (which we'll stop to install
-	# at some point).
-	pushd "${ED}"/usr/sbin >/dev/null || die
-	local i
-	for i in *; do
-		dosym ${i} /usr/sbin/${i}2
-	done
-	popd >/dev/null || die
-
-	# Provide a symlink for ab-ssl
-	if use ssl ; then
-		dosym ab /usr/bin/ab-ssl
-		dosym ab /usr/bin/ab2-ssl
-	fi
-}

app-admin/apache-tools/Manifest
@@ -1,2 +1,1 @@
-DIST httpd-2.4.53.tar.bz2 7431942 BLAKE2B 44acd51a2c18ae66878d64922947f2f24f0ddf8a5fa70718f788572032e18dafd06888fed3cf559e63e2a9e69d9b0a8dcc79e11fc9d8f216b5b073f769494727 SHA512 07ef59594251a30a864cc9cc9a58ab788c2d006cef85b728f29533243927c63cb063e0867f2a306f37324c3adb9cf7dcb2402f3516b05c2c6f32469d475dd756
 DIST httpd-2.4.54.tar.bz2 7434530 BLAKE2B be2ba2d33368f9dd619e36e63dd3635fbcf55773a539ac2840251bda04311f3eb535c08a37ab58d8b184c71e75b90b92e2bdc4e59c722cfb146de516f6e1ed04 SHA512 228493b2ff32c4142c6e484d304f2ea12e467498605fe12adce2b61388d8efe7b2e96ae2fd0abd1dc88a5f12d625e007d8da0ae5628cff2a5272806754f41e18

app-admin/apache-tools/apache-tools-2.4.53-r1.ebuild
@@ -1,104 +1,0 @@
-# Copyright 1999-2022 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=8
-inherit toolchain-funcs
-
-DESCRIPTION="Useful Apache tools - htdigest, htpasswd, ab, htdbm"
-HOMEPAGE="https://httpd.apache.org/"
-SRC_URI="mirror://apache/httpd/httpd-${PV}.tar.bz2"
-
-LICENSE="Apache-2.0"
-SLOT="0"
-KEYWORDS="~alpha amd64 arm arm64 ~hppa ~ia64 ~mips ppc ppc64 ~riscv ~s390 sparc x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~sparc64-solaris ~x64-solaris"
-IUSE="ssl"
-RESTRICT="test"
-
-RDEPEND=">=dev-libs/apr-1.5.0:1=
-	dev-libs/apr-util:1=
-	dev-libs/expat
-	dev-libs/libpcre
-	virtual/libcrypt:=
-	kernel_linux? ( sys-apps/util-linux )
-	ssl? ( dev-libs/openssl:0= )"
-DEPEND="${RDEPEND}
-	sys-devel/libtool"
-BDEPEND="
-	sys-apps/which
-	virtual/pkgconfig
-"
-
-S="${WORKDIR}/httpd-${PV}"
-
-PATCHES=(
-	"${FILESDIR}/${PN}-2.4.7-Makefile.patch" #459446
-)
-
-src_prepare() {
-	default
-
-	# This package really should upgrade to using pcre's .pc file.
-	cat <<-\EOF >"${T}"/pcre-config
-	#!/bin/bash
-	flags=()
-	for flag; do
-		if [[ ${flag} == "--version" ]]; then
-			flags+=( --modversion )
-		else
-			flags+=( "${flag}" )
-		fi
-	done
-	exec ${PKG_CONFIG} libpcre "${flags[@]}"
-	EOF
-	chmod a+x "${T}"/pcre-config || die
-}
-
-src_configure() {
-	# Brain dead check.
-	tc-is-cross-compiler && export ap_cv_void_ptr_lt_long="no"
-
-	tc-export PKG_CONFIG
-
-	local myeconfargs=(
-		--libexecdir="${EPREFIX}"/usr/$(get_libdir)/apache2/modules
-		--sbindir="${EPREFIX}"/usr/sbin
-		--with-z="${EPREFIX}"/usr
-		--with-apr="${ESYSROOT}"/usr
-		--with-apr-util="${ESYSROOT}"/usr
-		--with-pcre="${T}"/pcre-config
-		$(use_enable ssl)
-		$(usex ssl '--with-ssl="${EPREFIX}"/usr' '')
-	)
-	# econf overwrites the stuff from config.layout.
-	ac_cv_path_PKGCONFIG="${PKG_CONFIG}" \
-	econf "${myeconfargs[@]}"
-	sed -i \
-		-e '/^LTFLAGS/s:--silent::' \
-		build/rules.mk build/config_vars.mk || die
-}
-
-src_compile() {
-	emake -C support
-}
-
-src_install() {
-	emake -C support DESTDIR="${D}" install
-	dodoc CHANGES
-	doman docs/man/{dbmmanage,htdigest,htpasswd,htdbm,ab,logresolve}.1 \
-		docs/man/{htcacheclean,rotatelogs}.8
-
-	# Providing compatiblity symlinks for #177697 (which we'll stop to install
-	# at some point).
-	pushd "${ED}"/usr/sbin >/dev/null || die
-	local i
-	for i in *; do
-		dosym ${i} /usr/sbin/${i}2
-	done
-	popd >/dev/null || die
-
-	# Provide a symlink for ab-ssl
-	if use ssl ; then
-		dosym ab /usr/bin/ab-ssl
-		dosym ab /usr/bin/ab2-ssl
-	fi
-}

app-admin/apache-tools/apache-tools-2.4.54-r1.ebuild
@@ -1,108 +1,0 @@
-# Copyright 1999-2022 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=8
-
-inherit autotools toolchain-funcs
-
-DESCRIPTION="Useful Apache tools - htdigest, htpasswd, ab, htdbm"
-HOMEPAGE="https://httpd.apache.org/"
-SRC_URI="mirror://apache/httpd/httpd-${PV}.tar.bz2"
-
-LICENSE="Apache-2.0"
-SLOT="0"
-KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~sparc64-solaris ~x64-solaris"
-IUSE="ssl"
-RESTRICT="test"
-
-RDEPEND=">=dev-libs/apr-1.5.0:1=
-	dev-libs/apr-util:1=
-	dev-libs/expat
-	dev-libs/libpcre
-	virtual/libcrypt:=
-	kernel_linux? ( sys-apps/util-linux )
-	ssl? ( dev-libs/openssl:0= )"
-DEPEND="${RDEPEND}
-	sys-devel/libtool"
-BDEPEND="
-	virtual/pkgconfig
-"
-
-S="${WORKDIR}/httpd-${PV}"
-
-PATCHES=(
-	"${FILESDIR}/${PN}-2.4.7-Makefile.patch" #459446
-	"${FILESDIR}/${PN}-2.4.54-no-which.patch" #844868
-)
-
-src_prepare() {
-	default
-
-	# This package really should upgrade to using pcre's .pc file.
-	cat <<-\EOF >"${T}"/pcre-config
-	#!/bin/bash
-	flags=()
-	for flag; do
-		if [[ ${flag} == "--version" ]]; then
-			flags+=( --modversion )
-		else
-			flags+=( "${flag}" )
-		fi
-	done
-	exec ${PKG_CONFIG} libpcre "${flags[@]}"
-	EOF
-	chmod a+x "${T}"/pcre-config || die
-
-	# Only here for which patch
-	eautoreconf
-}
-
-src_configure() {
-	# Brain dead check.
-	tc-is-cross-compiler && export ap_cv_void_ptr_lt_long="no"
-
-	tc-export PKG_CONFIG
-
-	local myeconfargs=(
-		--libexecdir="${EPREFIX}"/usr/$(get_libdir)/apache2/modules
-		--sbindir="${EPREFIX}"/usr/sbin
-		--with-z="${EPREFIX}"/usr
-		--with-apr="${ESYSROOT}"/usr
-		--with-apr-util="${ESYSROOT}"/usr
-		--with-pcre="${T}"/pcre-config
-		$(use_enable ssl)
-		$(usex ssl '--with-ssl="${EPREFIX}"/usr' '')
-	)
-	# econf overwrites the stuff from config.layout.
-	ac_cv_path_PKGCONFIG="${PKG_CONFIG}" \
-	econf "${myeconfargs[@]}"
-	sed -i \
-		-e '/^LTFLAGS/s:--silent::' \
-		build/rules.mk build/config_vars.mk || die
-}
-
-src_compile() {
-	emake -C support
-}
-
-src_install() {
-	emake -C support DESTDIR="${D}" install
-	dodoc CHANGES
-	doman docs/man/{dbmmanage,htdigest,htpasswd,htdbm,ab,logresolve}.1 \
-		docs/man/{htcacheclean,rotatelogs}.8
-
-	# Providing compatiblity symlinks for #177697 (which we'll stop to install
-	# at some point).
-	pushd "${ED}"/usr/sbin >/dev/null || die
-	local i
-	for i in *; do
-		dosym ${i} /usr/sbin/${i}2
-	done
-	popd >/dev/null || die
-
-	# Provide a symlink for ab-ssl
-	if use ssl ; then
-		dosym ab /usr/bin/ab-ssl
-		dosym ab /usr/bin/ab2-ssl
-	fi
-}

commit 572d5f09dcbce6db7dce0f9e3191b1e4f4a34445

app-admin/apache-tools: restore 2.4.52-r2
Fixes: 5e0cb606997d6b1857974ca7b6e6042fdfc3ed07
Signed-off-by: Sam James <sam@gentoo.org>

app-admin/apache-tools/apache-tools-2.4.54-r2.ebuild
@@ -1,0 +1,109 @@
+# Copyright 1999-2022 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit autotools toolchain-funcs
+
+DESCRIPTION="Useful Apache tools - htdigest, htpasswd, ab, htdbm"
+HOMEPAGE="https://httpd.apache.org/"
+SRC_URI="mirror://apache/httpd/httpd-${PV}.tar.bz2"
+
+LICENSE="Apache-2.0"
+SLOT="0"
+KEYWORDS="~alpha amd64 arm arm64 ~hppa ~ia64 ~loong ~mips ~ppc ~ppc64 ~riscv ~s390 sparc x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~sparc64-solaris ~x64-solaris"
+IUSE="ssl"
+RESTRICT="test"
+
+RDEPEND=">=dev-libs/apr-1.5.0:1=
+	dev-libs/apr-util:1=
+	dev-libs/expat
+	dev-libs/libpcre
+	virtual/libcrypt:=
+	kernel_linux? ( sys-apps/util-linux )
+	ssl? ( dev-libs/openssl:0= )"
+DEPEND="${RDEPEND}
+	sys-devel/libtool"
+BDEPEND="
+	virtual/pkgconfig
+"
+
+S="${WORKDIR}/httpd-${PV}"
+
+PATCHES=(
+	"${FILESDIR}/${PN}-2.4.54-Makefile.patch" #459446
+	"${FILESDIR}/${PN}-2.4.54-no-which.patch" #844868
+	"${FILESDIR}/${PN}-2.4.54-libtool.patch" #858833
+)
+
+src_prepare() {
+	default
+
+	# This package really should upgrade to using pcre's .pc file.
+	cat <<-\EOF >"${T}"/pcre-config
+	#!/bin/bash
+	flags=()
+	for flag; do
+		if [[ ${flag} == "--version" ]]; then
+			flags+=( --modversion )
+		else
+			flags+=( "${flag}" )
+		fi
+	done
+	exec ${PKG_CONFIG} libpcre "${flags[@]}"
+	EOF
+	chmod a+x "${T}"/pcre-config || die
+
+	# Only here for libtool and which patches
+	eautoreconf
+}
+
+src_configure() {
+	# Brain dead check.
+	tc-is-cross-compiler && export ap_cv_void_ptr_lt_long="no"
+
+	tc-export PKG_CONFIG
+
+	local myeconfargs=(
+		--libexecdir="${EPREFIX}"/usr/$(get_libdir)/apache2/modules
+		--sbindir="${EPREFIX}"/usr/sbin
+		--with-z="${EPREFIX}"/usr
+		--with-apr="${ESYSROOT}"/usr
+		--with-apr-util="${ESYSROOT}"/usr
+		--with-pcre="${T}"/pcre-config
+		$(use_enable ssl)
+		$(usex ssl '--with-ssl="${EPREFIX}"/usr' '')
+	)
+	# econf overwrites the stuff from config.layout.
+	ac_cv_path_PKGCONFIG="${PKG_CONFIG}" \
+	econf "${myeconfargs[@]}"
+	sed -i \
+		-e '/^LTFLAGS/s:--silent::' \
+		build/rules.mk build/config_vars.mk || die
+}
+
+src_compile() {
+	emake -C support
+}
+
+src_install() {
+	emake -C support DESTDIR="${D}" install
+	dodoc CHANGES
+	doman docs/man/{dbmmanage,htdigest,htpasswd,htdbm,ab,logresolve}.1 \
+		docs/man/{htcacheclean,rotatelogs}.8
+
+	# Providing compatiblity symlinks for #177697 (which we'll stop to install
+	# at some point).
+	pushd "${ED}"/usr/sbin >/dev/null || die
+	local i
+	for i in *; do
+		dosym ${i} /usr/sbin/${i}2
+	done
+	popd >/dev/null || die
+
+	# Provide a symlink for ab-ssl
+	if use ssl ; then
+		dosym ab /usr/bin/ab-ssl
+		dosym ab /usr/bin/ab2-ssl
+	fi
+}