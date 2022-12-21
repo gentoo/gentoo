6891faa4a5d8e70e28fc7faca054921acc2dd99e
Andrey Grozin <grozin@gentoo.org>
2022-12-21 23:00:41 +0700

    sci-visualization/gle: bump to 4.3.3
    Signed-off-by: Andrey Grozin <grozin@gentoo.org>

--- sci-visualization/gle/files/64gle-gentoo.el
+++ sci-visualization/gle/files/64gle-gentoo.el
@@ -2,5 +2,5 @@
 ;;; gle site-lisp configuration
 
 (add-to-list 'load-path "@SITELISP@")
-(autoload 'gle-mode "gle-mode")
 (add-to-list 'auto-mode-alist '("\\.gle\\'" . gle-mode))
+(autoload 'gle-mode "gle-mode" "Major mode for gle files." t)
---
+++ sci-visualization/gle/files/gle.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export GLE_TOP=/usr/share/gle
+exec /usr/bin/gle.bin $*
