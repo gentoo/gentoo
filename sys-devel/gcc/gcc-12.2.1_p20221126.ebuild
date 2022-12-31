gentoo/gentoo

commit add25cd2d8789812e33791df51620b65aa9a53a8

sys-devel/gcc: drop 12.2.1_p20221008, 12.2.1_p20221126
Signed-off-by: Sam James <sam@gentoo.org>

sys-devel/gcc/Manifest
@@ -10,12 +10,9 @@ DIST gcc-11.3.0-patches-7.tar.xz 15184 BLAKE2B 426f00feaa982a35d2c459e7a2c2e9d29
 DIST gcc-11.3.0.tar.xz 81141364 BLAKE2B 7e562d25446ca4ab9fe8cdb714866f66aba3744d78bf84f31bfb097c1a981e4c7f990cb1e6bcfec5ae6671836a4984e2b70eb8fed81dcef5e244f88da8623469 SHA512 f0be5ad705c73b84477128a69c047f57dd47002f375eb60e1e842e08cf2009a509e92152bca345823926d550b7395ae6d4de7db51d1ee371c2dc37313881fca7
 DIST gcc-11.4.0-musl-patches-0.tar.xz 3844 BLAKE2B f3f1be6718e3cf08e553a1a2319a85e13368279849f6f3ab89ad7abe505316afacf5084846c0cb133bdcb5c918244644242c194e6ce55455a26e67b3983d73ef SHA512 a81db52d21de364137461dcff7e1af696df4a02fa26452f90439ae4af14ccbffe68eb4ee93ae56257490ab8aa36b4bff37c4e1f2cf4dd4624b3a6fb4cb50b7ac
 DIST gcc-11.4.0-patches-3.tar.xz 15200 BLAKE2B a393df941a2a31164f477d6bc0032e28319e8f0bd87200eb453708f85aa8c0bf5d67e733ee7b75cf6d14743e18cc941cee1b77a8247f8c5683d17a98cbeb280f SHA512 6f6096d95c7c62780d1070097739882393a8df5172c3a5ae811a8d4d1c98a0cd14a34f3f7e95410c8f210aec434330ac3b8876c688faeb130d7bb8fa3333ece9
-DIST gcc-12-20221008.tar.xz 79145884 BLAKE2B ff3d684d887e282c79fe95952a449ed5770d4bd74077553baf31c34dbd77177e680b19ac741c04aab26ac2c9bb2b89924f51f63e2f22b440decf2c65274a7640 SHA512 a35a6207e7f4375abca3ae57376c98b082846a88e83522c322d713e759367ceb2b3ad68488bed6f1645e47a106b0bbb2efbca6cb14b077672289acb4373e616f
-DIST gcc-12-20221126.tar.xz 79631864 BLAKE2B 3bb97ef4a1a17ab8b92b431b1f792c11131085a1b523efa08c5b4f995b44bf628be371564813e5affb1f8e2320bb8a9b04532e57b2deeef26b33f517783daa5e SHA512 163e0aa39dc5c24980f84000bd6ccd4b7c24581831b7a4cdbd63e4726be5c2b1933d034ce12c65cf95e4c0ebe1175af7e32facace465bfd7c7c377401350a2df
 DIST gcc-12-20221203.tar.xz 79639180 BLAKE2B 5b237eca5934c6ea869d50396279b0bbb72c9377f71c7ad9b457d1e2422337f7ff6c9cd8021f3c9f94f62b1d31348d550ed3afe49f4231099bb889c27c2c23cb SHA512 87ebbf26544e0b4e20fc36dffcb343bc074603e409e204079f3559f4b336da98af7d1c702254a54ad243c230aca60027f80029f14d7c0ff7ee13e31ecf9b73d7
 DIST gcc-12.2.0-musl-patches-1.tar.xz 3844 BLAKE2B 004432806696f7d0a32366270ac45695e858abee73a255e44dc6e0a225339e4cad40aab0d51dfe9a55d5aa4b4001f5726064bb2eea3212a0874b2d27e229abd7 SHA512 c354edcd641a9dfaf902a0ff4c44c75065d0cf1c36bbf6c3c35c2e23bff22b8f70bcf4458a5a0ef86371e9f2fafca8a54ca822e35820ff442c9458a2819c3606
 DIST gcc-12.2.0-patches-1.tar.xz 12864 BLAKE2B a5ae0a85dfc1b6f0bd968f0d5262ebed14ec9cdb3249e3a4c571578c54eda0e53708ee7fe8e03e506366c7f3cf7926eced9b390d9dee948c1249298c0fabd9fb SHA512 f3d793b89a2a0385d596162fb0c653d4acdf21ae418cb792c1786a01fde0391bd0719715dbf07d53636f127168f5cd99108a1dc11cf6cea889b7d82385bcc258
-DIST gcc-12.2.0-patches-2.tar.xz 13952 BLAKE2B a9e798911acaf45c3f585f69721130cfbecec016727a50ac0f4767b37a2ab9df39e0828abb07f1e429619cc102808f81af8422fef0265415c287d303d9d97fc0 SHA512 1808a05e98094577099d115ff5f3e136e3fc8f39fbe35c5183fd7775383f94439212e5e4174ab8d0ae8c0874e227ff84a958ffdfa1c99c9a8306a9842d126ef9
 DIST gcc-12.2.0-patches-4.tar.xz 19052 BLAKE2B 74027e339125ad412b2e03265d2d797fdf4b794ed37f5d5ed33915f9e1f2592f6f7e1d8edf822700de5300fb39ac45fb3b671c6fac852246057f97f3ec4d411d SHA512 14a26185c96225c2babed80b29ef9897a601862d32cdc116abbd7efb551d1e25f639d5c5059996af53d30bb049f08c66af366a854a8d2eb2967f87ea390a37a0
 DIST gcc-12.2.0.tar.xz 84645292 BLAKE2B 715574af9ad678f9dc8cfd19c866bf910c7edfd479e7e9681337feaa9b54e5d304ddb85483816b8d89754d502405823ae2eff0d1e444538763f40be8e428d8da SHA512 e9e857bd81bf7a370307d6848c81b2f5403db8c7b5207f54bce3f3faac3bde63445684092c2bc1a2427cddb6f7746496d9fbbef05fbbd77f2810b2998f1f9173
 DIST gcc-13-20221106.tar.xz 80001320 BLAKE2B 4a86b5aedf67725b577a69112ab241b22b3b0eed6c86e2d9ed06e5045a1de1c4bc6f2e7a281f1a034b9b227e5ef72954d17900369bed6015e064f303c89e6cf2 SHA512 bd3160d23bdebd0e423800736212e340eab0a71891d579145d85931fe8fa3c9e196a3422d70a98e17d1990759bf57b3e4eb08b25544db5e4fec8448d1a33ee20

sys-devel/gcc/gcc-12.2.1_p20221126.ebuild
@@ -1,52 +1,0 @@
-# Copyright 1999-2022 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=8
-
-TOOLCHAIN_PATCH_DEV="sam"
-PATCH_VER="2"
-PATCH_GCC_VER="12.2.0"
-MUSL_VER="1"
-MUSL_GCC_VER="12.2.0"
-
-if [[ $(ver_cut 3) == 9999 ]] ; then
-	MY_PV_2=$(ver_cut 2)
-	if [[ ${MY_PV_2} == 0 ]] ; then
-		MY_PV_2=0
-	else
-		MY_PV_2=$(($(ver_cut 2) - 1))
-	fi
-
-	# e.g. 12.2.9999 -> 12.1.1
-	TOOLCHAIN_GCC_PV=$(ver_cut 1).${MY_PV_2}.$(($(ver_cut 3) - 9998))
-elif [[ -n ${TOOLCHAIN_GCC_RC} ]] ; then
-	# Cheesy hack for RCs
-	MY_PV=$(ver_cut 1).$((($(ver_cut 2) + 1))).$((($(ver_cut 3) - 1)))-RC-$(ver_cut 5)
-	MY_P=${PN}-${MY_PV}
-	GCC_TARBALL_SRC_URI="https://gcc.gnu.org/pub/gcc/snapshots/${MY_PV}/${MY_P}.tar.xz"
-	TOOLCHAIN_SET_S=no
-	S="${WORKDIR}"/${MY_P}
-fi
-
-inherit toolchain
-
-# Needs to be after inherit (for now?), bug #830908
-EGIT_BRANCH=releases/gcc-$(ver_cut 1)
-
-# Don't keyword live ebuilds
-#if ! tc_is_live && [[ -z ${TOOLCHAIN_USE_GIT_PATCHES} ]] ; then
-#	KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~m68k ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86"
-#fi
-
-# Technically only if USE=hardened *too* right now, but no point in complicating it further.
-# If GCC is enabling CET by default, we need glibc to be built with support for it.
-# bug #830454
-RDEPEND="elibc_glibc? ( sys-libs/glibc[cet(-)?] )"
-DEPEND="${RDEPEND}"
-BDEPEND=">=${CATEGORY}/binutils-2.30[cet(-)?]"
-
-src_prepare() {
-	toolchain_src_prepare
-
-	eapply_user
-}

sys-devel/gcc/gcc-12.2.1_p20221008.ebuild
@@ -1,52 +1,0 @@
-# Copyright 1999-2022 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=8
-
-TOOLCHAIN_PATCH_DEV="sam"
-PATCH_VER="1"
-PATCH_GCC_VER="12.2.0"
-MUSL_VER="1"
-MUSL_GCC_VER="12.2.0"
-
-if [[ $(ver_cut 3) == 9999 ]] ; then
-	MY_PV_2=$(ver_cut 2)
-	if [[ ${MY_PV_2} == 0 ]] ; then
-		MY_PV_2=0
-	else
-		MY_PV_2=$(($(ver_cut 2) - 1))
-	fi
-
-	# e.g. 12.2.9999 -> 12.1.1
-	TOOLCHAIN_GCC_PV=$(ver_cut 1).${MY_PV_2}.$(($(ver_cut 3) - 9998))
-elif [[ -n ${TOOLCHAIN_GCC_RC} ]] ; then
-	# Cheesy hack for RCs
-	MY_PV=$(ver_cut 1).$((($(ver_cut 2) + 1))).$((($(ver_cut 3) - 1)))-RC-$(ver_cut 5)
-	MY_P=${PN}-${MY_PV}
-	GCC_TARBALL_SRC_URI="https://gcc.gnu.org/pub/gcc/snapshots/${MY_PV}/${MY_P}.tar.xz"
-	TOOLCHAIN_SET_S=no
-	S="${WORKDIR}"/${MY_P}
-fi
-
-inherit toolchain
-
-# Needs to be after inherit (for now?), bug #830908
-EGIT_BRANCH=releases/gcc-$(ver_cut 1)
-
-# Don't keyword live ebuilds
-if ! tc_is_live && [[ -z ${TOOLCHAIN_USE_GIT_PATCHES} ]] ; then
-	KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~m68k ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86"
-fi
-
-# Technically only if USE=hardened *too* right now, but no point in complicating it further.
-# If GCC is enabling CET by default, we need glibc to be built with support for it.
-# bug #830454
-RDEPEND="elibc_glibc? ( sys-libs/glibc[cet(-)?] )"
-DEPEND="${RDEPEND}"
-BDEPEND=">=${CATEGORY}/binutils-2.30[cet(-)?]"
-
-src_prepare() {
-	toolchain_src_prepare
-
-	eapply_user
-}

commit 82c68ba6bb3290b6b39e178d6840399752bacfbc

sys-devel/gcc: restore 12.2.1_p20221126 for musl
Fixes: add25cd2d8789812e33791df51620b65aa9a53a8
Bug: https://bugs.gentoo.org/885501
Signed-off-by: Sam James <sam@gentoo.org>

sys-devel/gcc/Manifest
@@ -15,10 +15,12 @@ DIST gcc-11.3.0.tar.xz 81141364 BLAKE2B 7e562d25446ca4ab9fe8cdb714866f66aba3744d
 DIST gcc-11.4.0-musl-patches-0.tar.xz 3844 BLAKE2B f3f1be6718e3cf08e553a1a2319a85e13368279849f6f3ab89ad7abe505316afacf5084846c0cb133bdcb5c918244644242c194e6ce55455a26e67b3983d73ef SHA512 a81db52d21de364137461dcff7e1af696df4a02fa26452f90439ae4af14ccbffe68eb4ee93ae56257490ab8aa36b4bff37c4e1f2cf4dd4624b3a6fb4cb50b7ac
 DIST gcc-11.4.0-patches-3.tar.xz 15200 BLAKE2B a393df941a2a31164f477d6bc0032e28319e8f0bd87200eb453708f85aa8c0bf5d67e733ee7b75cf6d14743e18cc941cee1b77a8247f8c5683d17a98cbeb280f SHA512 6f6096d95c7c62780d1070097739882393a8df5172c3a5ae811a8d4d1c98a0cd14a34f3f7e95410c8f210aec434330ac3b8876c688faeb130d7bb8fa3333ece9
 DIST gcc-11.4.0-patches-5.tar.xz 15136 BLAKE2B 202bc321d1a75a543bcbb54ddd7aae2a6d81764b1ae752e95bbcb7b1e68cbac33e2066a1c935c705fc7cea6603e93a6dcb3041645131a5e76c986242a8613908 SHA512 96ec2dec4e663d739fa7b80e63df32aa6d72c193e124bc93580cc1c0f9b8d3578dee3faf0e765b766cc6bb46136958a4b0e7095962d73b6e2a8066b8be558a5c
+DIST gcc-12-20221126.tar.xz 79631864 BLAKE2B 3bb97ef4a1a17ab8b92b431b1f792c11131085a1b523efa08c5b4f995b44bf628be371564813e5affb1f8e2320bb8a9b04532e57b2deeef26b33f517783daa5e SHA512 163e0aa39dc5c24980f84000bd6ccd4b7c24581831b7a4cdbd63e4726be5c2b1933d034ce12c65cf95e4c0ebe1175af7e32facace465bfd7c7c377401350a2df
 DIST gcc-12-20221210.tar.xz 79645632 BLAKE2B 063222037d420f356d452d0419fc4621c30393beaca1f2661d7070c087bca88831f27ae96d8fb7433efaeb59cb338495fb210611a1c37931a1d00373cba9812b SHA512 00399148932b5c57dbe442dcde01e856cbb981c7cc6703760ea51b28e0784cbc6e016663f195b69714da8c7e2cdcfbc03cb1939bab3e6b507b64846e1e814fa6
 DIST gcc-12-20221224.tar.xz 79647600 BLAKE2B 9c99e6fee09457ac56f8b975650aa0b7dc82f05e990b722af16383ef75b9c02ea592fc37ba3a098e5a601c2115fe2c2414699830175e375325f99743d8ed952f SHA512 018c68d323d9fbe25295807211f750fa80efb947b77e8557860cfd9e97c90198e50c5b3f589afe7fd287a226ff5105b59c6134e4a6cd616ac3d551a5914a15e8
 DIST gcc-12.2.0-musl-patches-1.tar.xz 3844 BLAKE2B 004432806696f7d0a32366270ac45695e858abee73a255e44dc6e0a225339e4cad40aab0d51dfe9a55d5aa4b4001f5726064bb2eea3212a0874b2d27e229abd7 SHA512 c354edcd641a9dfaf902a0ff4c44c75065d0cf1c36bbf6c3c35c2e23bff22b8f70bcf4458a5a0ef86371e9f2fafca8a54ca822e35820ff442c9458a2819c3606
 DIST gcc-12.2.0-patches-1.tar.xz 12864 BLAKE2B a5ae0a85dfc1b6f0bd968f0d5262ebed14ec9cdb3249e3a4c571578c54eda0e53708ee7fe8e03e506366c7f3cf7926eced9b390d9dee948c1249298c0fabd9fb SHA512 f3d793b89a2a0385d596162fb0c653d4acdf21ae418cb792c1786a01fde0391bd0719715dbf07d53636f127168f5cd99108a1dc11cf6cea889b7d82385bcc258
+DIST gcc-12.2.0-patches-2.tar.xz 13952 BLAKE2B a9e798911acaf45c3f585f69721130cfbecec016727a50ac0f4767b37a2ab9df39e0828abb07f1e429619cc102808f81af8422fef0265415c287d303d9d97fc0 SHA512 1808a05e98094577099d115ff5f3e136e3fc8f39fbe35c5183fd7775383f94439212e5e4174ab8d0ae8c0874e227ff84a958ffdfa1c99c9a8306a9842d126ef9
 DIST gcc-12.2.0-patches-6.tar.xz 19096 BLAKE2B 9d97f4b1d4c82c00d6bac923963444198fb8144753bfb25f0068d96e4c7444f6163a7cd2e6ed2b808f608131f3208746776b2d061986b8d50311ca6cd8cbb917 SHA512 9af71c64490a289d959e95b2df34e62861c161cb161632020f66a4fbdfdbc7f08f6f2956d08e0a7995332d010c974f58a8bc8e4d595769c5632c4755a4e86b31
 DIST gcc-12.2.0-patches-7.tar.xz 14492 BLAKE2B 95347cc4a7affab3ac1ed2b89ed7dcb239e17b28c93ab45f248765d297ca37e2ab127b13fba2a38256166c2daf43156e574ac783868ac48d561909af5fe82139 SHA512 661daf478f6abeab8c3164282aef591c44e18ab99deaa1aee2102de4c8d05da4145bf3ea371ebcf7e4f2455d5ae97c6d94e321b98d373750c784c1b278efb87a
 DIST gcc-12.2.0.tar.xz 84645292 BLAKE2B 715574af9ad678f9dc8cfd19c866bf910c7edfd479e7e9681337feaa9b54e5d304ddb85483816b8d89754d502405823ae2eff0d1e444538763f40be8e428d8da SHA512 e9e857bd81bf7a370307d6848c81b2f5403db8c7b5207f54bce3f3faac3bde63445684092c2bc1a2427cddb6f7746496d9fbbef05fbbd77f2810b2998f1f9173

sys-devel/gcc/gcc-12.2.1_p20221126.ebuild
@@ -1,0 +1,52 @@
+# Copyright 1999-2022 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+TOOLCHAIN_PATCH_DEV="sam"
+PATCH_VER="2"
+PATCH_GCC_VER="12.2.0"
+MUSL_VER="1"
+MUSL_GCC_VER="12.2.0"
+
+if [[ $(ver_cut 3) == 9999 ]] ; then
+	MY_PV_2=$(ver_cut 2)
+	if [[ ${MY_PV_2} == 0 ]] ; then
+		MY_PV_2=0
+	else
+		MY_PV_2=$(($(ver_cut 2) - 1))
+	fi
+
+	# e.g. 12.2.9999 -> 12.1.1
+	TOOLCHAIN_GCC_PV=$(ver_cut 1).${MY_PV_2}.$(($(ver_cut 3) - 9998))
+elif [[ -n ${TOOLCHAIN_GCC_RC} ]] ; then
+	# Cheesy hack for RCs
+	MY_PV=$(ver_cut 1).$((($(ver_cut 2) + 1))).$((($(ver_cut 3) - 1)))-RC-$(ver_cut 5)
+	MY_P=${PN}-${MY_PV}
+	GCC_TARBALL_SRC_URI="https://gcc.gnu.org/pub/gcc/snapshots/${MY_PV}/${MY_P}.tar.xz"
+	TOOLCHAIN_SET_S=no
+	S="${WORKDIR}"/${MY_P}
+fi
+
+inherit toolchain
+
+# Needs to be after inherit (for now?), bug #830908
+EGIT_BRANCH=releases/gcc-$(ver_cut 1)
+
+# Don't keyword live ebuilds
+#if ! tc_is_live && [[ -z ${TOOLCHAIN_USE_GIT_PATCHES} ]] ; then
+#	KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~m68k ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86"
+#fi
+
+# Technically only if USE=hardened *too* right now, but no point in complicating it further.
+# If GCC is enabling CET by default, we need glibc to be built with support for it.
+# bug #830454
+RDEPEND="elibc_glibc? ( sys-libs/glibc[cet(-)?] )"
+DEPEND="${RDEPEND}"
+BDEPEND=">=${CATEGORY}/binutils-2.30[cet(-)?]"
+
+src_prepare() {
+	toolchain_src_prepare
+
+	eapply_user
+}