gentoo/gentoo

commit 98dcd65a8e3e750ab30f0e5c7215335201130c5b

sys-apps/texinfo: add missing dependency on help2man
Signed-off-by: Andreas K. Hüttel <dilfridge@gentoo.org>

sys-apps/texinfo/texinfo-7.0.1-r1.ebuild
@@ -33,7 +33,10 @@ RDEPEND="
 	nls? ( virtual/libintl )
 "
 DEPEND="${RDEPEND}"
-BDEPEND="nls? ( >=sys-devel/gettext-0.19.6 )"
+BDEPEND="
+	sys-apps/help2man
+	nls? ( >=sys-devel/gettext-0.19.6 )
+"
 
 PATCHES=(
 	"${FILESDIR}"/${P}-lto-type-mismatch.patch

sys-apps/texinfo/texinfo-7.0.1.ebuild
@@ -33,7 +33,10 @@ RDEPEND="
 	nls? ( virtual/libintl )
 "
 DEPEND="${RDEPEND}"
-BDEPEND="nls? ( >=sys-devel/gettext-0.19.6 )"
+BDEPEND="
+	sys-apps/help2man
+	nls? ( >=sys-devel/gettext-0.19.6 )
+"
 
 src_prepare() {
 	default

commit 24c3f5e13d994194972806ebbe8dff0fda37840e

sys-apps/texinfo: drop help2man dep; repair prefix help2man usage too
- help2man was being called for the normal case (non-prefix) because
  our patch from the 7.0.0 release branch was touching install-info.c which
  led to it trying to regenerate install-info.1.

  Update the timestamp of install-info.1 to be newer than the C file so
  it doesn't try to regenerate.

- For Prefix, we've had a sed for a while to fix shebangs of some files,
  but the touch trick (as above) seems to have gone stale. Update it to
  touch more files to avoid the same problem as above with help2man.

Fixes: 98dcd65a8e3e750ab30f0e5c7215335201130c5b
Closes: https://bugs.gentoo.org/870814
Signed-off-by: Sam James <sam@gentoo.org>

sys-apps/texinfo/texinfo-7.0.1-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2022 Gentoo Authors
+# Copyright 1999-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 # Note: if your package uses the texi2dvi utility, it must depend on the
@@ -34,7 +34,6 @@ RDEPEND="
 "
 DEPEND="${RDEPEND}"
 BDEPEND="
-	sys-apps/help2man
 	nls? ( >=sys-devel/gettext-0.19.6 )
 "
 
@@ -46,9 +45,12 @@ PATCHES=(
 src_prepare() {
 	default
 
+	# Drop after 7.0.1 (patch touches install-info which causes regeneration)
+	touch man/install-info.1 || die
+
 	if use prefix ; then
 		sed -i -e '1c\#!/usr/bin/env sh' util/texi2dvi util/texi2pdf || die
-		touch doc/{texi2dvi,texi2pdf,pdftexi2dvi}.1
+		touch {doc,man}/{texi2dvi,texi2pdf,pdftexi2dvi}.1 || die
 	fi
 }
 