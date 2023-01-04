gentoo/gentoo

commit 840c7ad0eab3c72b94c4416e63a35432308306d8

app-misc/solaar: bump version to 1.1.8
Also increase PYTHON_COMPAT and set DISTUTILS_USE_PEP517

Signed-off-by: Martin Kemp <me@martinke.mp>
Closes: https://github.com/gentoo/gentoo/pull/28949
Signed-off-by: Sam James <sam@gentoo.org>

app-misc/solaar/solaar-1.1.8.ebuild
@@ -16,7 +16,7 @@ if [[ ${PV} =~ 9999* ]]; then
 	EGIT_REPO_URI="https://github.com/pwr-Solaar/Solaar"
 else
 	SRC_URI="https://github.com/pwr-Solaar/Solaar/archive/${PV/_rc/rc}.tar.gz -> ${P/_rc/rc}.tar.gz"
-	KEYWORDS="~amd64 ~arm ~x86"
+	KEYWORDS="amd64 ~arm x86"
 	S="${WORKDIR}"/Solaar-${PV/_rc/rc}
 fi
 

app-misc/solaar/Manifest
@@ -3,3 +3,4 @@ DIST solaar-1.0.7.tar.gz 1265664 BLAKE2B 6bb221d603dc74bf21ac44ca6611d817889285c
 DIST solaar-1.1.1.tar.gz 1418025 BLAKE2B c6fd053002727a372bf9672fc22c0acc123f814003c14172f7d13e59b82ab42ecee9b71a09289b2d234874850e0921debaa069506a07d8957f52a2e9953a47f4 SHA512 c4da0c5521724c00afbc54ee9d10a4709246bb6f57635637b77e5944293b285a055b7df623e7e2a810a0764021813929e0f4fa7f21d4afc39abd516416a2bf25
 DIST solaar-1.1.2rc2.tar.gz 1462081 BLAKE2B b5bcf37f75631d39ac726b564bfd7f329f80e9931461fceb5ce7442113df50db82679dabfd08150a87ebe9a9de68aeb111138a005e450c3e025c57e5f1219312 SHA512 65320d7193124d1444ecddf5a162e925f373c7c06d0e582af29b1c9d82ea16a32a69accd53f53568b228474691eacd5c0f16bce1bccef1e9a1846151e86dc203
 DIST solaar-1.1.3.tar.gz 1478166 BLAKE2B 79bfe8a88cecfefc9795d65027f18df5d39c3d43df3e20d3b167bd41365a721ed670e5a60c883b10a2a4cf34d476536003f71d20882e4f30dca590edc441ddeb SHA512 6175d72d8c13b23d0543728315a3d8b5ec506cbaafd821efda92398d640b127d8eb6742c4a22240dca08cdc1ba3ea00e824e2232e41b4fb88ad938dc0df270b8
+DIST solaar-1.1.8.tar.gz 1550947 BLAKE2B a33879e3c1b161d86c4c8a2c8edcc79eff285cb8937bf1e6621f3595ba5cf420e75fe700902b9f375698085bb929401154fa2bd25ae9663265e38c8406205bf6 SHA512 543c6257b36181813c9ed36747e53a15403da251fdd431c32525cada3ac88611da2c5f2a8212767ba3c7bb0071a3c2024f114805d9a33c761701bf958df29d85

commit 8858c7de308bd96913796a8a27b37d41857467c4

app-misc/solaar: destabilize 1.1.8 for ~amd64, ~x86
Unintentionally pushed as stable.

Fixes: 840c7ad0eab3c72b94c4416e63a35432308306d8
Signed-off-by: Sam James <sam@gentoo.org>

app-misc/solaar/solaar-1.1.8.ebuild
@@ -16,7 +16,7 @@ if [[ ${PV} =~ 9999* ]]; then
 	EGIT_REPO_URI="https://github.com/pwr-Solaar/Solaar"
 else
 	SRC_URI="https://github.com/pwr-Solaar/Solaar/archive/${PV/_rc/rc}.tar.gz -> ${P/_rc/rc}.tar.gz"
-	KEYWORDS="amd64 ~arm x86"
+	KEYWORDS="~amd64 ~arm ~x86"
 	S="${WORKDIR}"/Solaar-${PV/_rc/rc}
 fi
 