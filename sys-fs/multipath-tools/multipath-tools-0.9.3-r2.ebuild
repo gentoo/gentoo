gentoo/gentoo

commit 69322f09c73a949705b50485ab26552491d8a3cd

sys-fs/multipath-tools: drop 0.8.9, 0.9.0, 0.9.3-r2
Signed-off-by: Sam James <sam@gentoo.org>

sys-fs/multipath-tools/multipath-tools-0.9.3-r2.ebuild
@@ -1,97 +1,0 @@
-# Copyright 1999-2022 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=8
-
-inherit linux-info systemd toolchain-funcs udev tmpfiles
-
-DESCRIPTION="Device mapper target autoconfig"
-HOMEPAGE="http://christophe.varoqui.free.fr/"
-SRC_URI="https://github.com/opensvc/${PN}/archive/refs/tags/${PV}.tar.gz -> ${P}.tar.gz"
-
-LICENSE="GPL-2"
-SLOT="0"
-KEYWORDS="~alpha amd64 ~arm arm64 ~ia64 ~loong ppc ppc64 ~riscv x86"
-IUSE="systemd test"
-RESTRICT="!test? ( test )"
-
-RDEPEND="
-	dev-libs/json-c:=
-	dev-libs/libaio
-	dev-libs/userspace-rcu:=
-	>=sys-fs/lvm2-2.02.45
-	>=virtual/libudev-232-r3
-	sys-libs/readline:=
-	systemd? ( sys-apps/systemd )"
-DEPEND="${RDEPEND}
-	test? ( dev-util/cmocka )"
-BDEPEND="virtual/pkgconfig"
-
-CONFIG_CHECK="~DM_MULTIPATH"
-
-src_prepare() {
-	default
-
-	sed -r -i -e '/^(CPPFLAGS|CFLAGS)\>/s,^(CPPFLAGS|CFLAGS)\>[[:space:]]+:=,\1 := $(GENTOO_\1),' \
-		"${S}"/Makefile.inc || die
-}
-
-src_compile() {
-	tc-export CC
-
-	# LIBDM_API_FLUSH involves grepping files in /usr/include,
-	# so force the test to go the way we want #411337.
-	emake \
-		prefix="${EPREFIX}/usr" \
-		plugindir="${EPREFIX}/$(get_libdir)/multipath" \
-		LIB="$(get_libdir)" \
-		LIBDM_API_FLUSH=1 \
-		PKGCONFIG="$(tc-getPKG_CONFIG)" \
-		GENTOO_CFLAGS="${CFLAGS}" \
-		GENTOO_CPPFLAGS="${CPPFLAGS}" \
-		FAKEVAR=1
-}
-
-src_install() {
-	dodir /sbin
-
-	# Please clean this up > 0.9.3: https://github.com/opensvc/multipath-tools/pull/53
-	# $(prefix) doesn't work correctly in makefile in 0.9.3.
-	emake \
-		DESTDIR="${ED}" \
-		prefix="${EPREFIX}" \
-		LIB="$(get_libdir)" \
-		RUN=run \
-		plugindir="${EPREFIX}/$(get_libdir)/multipath" \
-		unitdir="$(systemd_get_systemunitdir)" \
-		libudevdir="${EPREFIX}/$(get_udevdir)" \
-		pkgconfdir="${EPREFIX}/usr/$(get_libdir)/pkgconfig" \
-		GENTOO_CFLAGS="${CFLAGS}" \
-		GENTOO_CPPFLAGS="${CPPFLAGS}" \
-		install
-
-	rmdir "${ED}"/usr/include
-	rmdir "${ED}"/usr/share
-	mv "${ED}"/include "${ED}"/usr/include || die
-	mv "${ED}"/share "${ED}"/usr/share || die
-	einstalldocs
-
-	newinitd "${FILESDIR}"/multipathd-r1.rc multipathd
-	newinitd "${FILESDIR}"/multipath.rc multipath
-
-	find "${ED}" -type f -name '*.la' -delete || die
-}
-
-pkg_postinst() {
-	tmpfiles_process /usr/lib/tmpfiles.d/multipath.conf
-	udev_reload
-
-	if [[ -z ${REPLACING_VERSIONS} ]] ; then
-		elog "If you need multipath on your system, you must"
-		elog "add 'multipath' into your boot runlevel!"
-	fi
-}
-
-pkg_postrm() {
-	udev_reload
-}

sys-fs/multipath-tools/Manifest
@@ -1,3 +1,1 @@
-DIST multipath-tools-0.8.9.tar.gz 534866 BLAKE2B b5ebf3c393f6b60e85678ac07378ae07056b6777409fc1bc4f4133cdd3f8c75a3d76f6e9342208df7fed8fe7812b089eba8f6b769e47e1dd6c8b7fd321bdbd30 SHA512 25f2a5d436af6a343804988cef45ca1574d4a981655a2b91563ddb89138619158befdf5af92d836a17c95d6dcf901072b614473c2129274e5dcdb1a1d64edb4d
-DIST multipath-tools-0.9.0.tar.gz 537311 BLAKE2B e7d4958d24d1e19da2b80be23585ad37f6b0c1b66dc1377014d825ddd2be10ea3b8c30162cb7a23b120427bf0c957c6cad8560018e1cb058c284f37afd5d50d4 SHA512 6c417f6d1d116fa43bedb9f77769ece9cbb7b35b78a9b3558c41df2360e52a65a07314b12ab7e4a7bbc867b9755250de9db96a2f7eb4a6a37f0b0b3f0bbc840e
 DIST multipath-tools-0.9.3.tar.gz 552442 BLAKE2B e2065119cdd68bd8c2b4420bd0de9ce370601597972dba32c076bfc4df6d0a1a65356454a2f96f95f27cf681387685edcbac2eb41258d4a7049a1ef62cdf0949 SHA512 4faa2ee5a96a9d5d752219931ebc885cb70ed6b022d45ede985ad7919c043a3aee166e6f126d32dffd187c5c32d5cbce91747d87d0b55557e2f7f68b279583da

sys-fs/multipath-tools/files/multipath-tools-0.8.7-respect-flags.patch
@@ -1,14 +1,0 @@
---- a/Makefile.inc
-+++ b/Makefile.inc
-@@ -101,8 +101,9 @@ OPTFLAGS	:= -O2 -g $(STACKPROT) --param=ssp-buffer-size=4
- WARNFLAGS	:= -Werror -Wall -Wextra -Wformat=2 $(WFORMATOVERFLOW) -Werror=implicit-int \
- 		  -Werror=implicit-function-declaration -Werror=format-security \
- 		  $(WNOCLOBBERED) -Werror=cast-qual $(ERROR_DISCARDED_QUALIFIERS)
--CPPFLAGS	:= -Wp,-D_FORTIFY_SOURCE=2
--CFLAGS		:= --std=gnu99 $(CFLAGS) $(OPTFLAGS) $(WARNFLAGS) -pipe \
-+CPPFLAGS	?= -Wp,-D_FORTIFY_SOURCE=2
-+CFLAGS		?= --std=gnu99 $(CFLAGS) $(OPTFLAGS) $(WARNFLAGS) -pipe
-+CFLAGS		+= --std=gnu99 \
- 		   -DBIN_DIR=\"$(bindir)\" -DLIB_STRING=\"${LIB}\" -DRUN_DIR=\"${RUN}\" \
- 		   -MMD -MP
- BIN_CFLAGS	= -fPIE -DPIE

sys-fs/multipath-tools/files/multipath-tools-0.9.0-respect-flags.patch
@@ -1,15 +1,0 @@
---- a/Makefile.inc
-+++ b/Makefile.inc
-@@ -140,10 +140,9 @@
- WARNFLAGS	:= -Werror -Wall -Wextra -Wformat=2 $(WFORMATOVERFLOW) -Werror=implicit-int \
- 		  -Werror=implicit-function-declaration -Werror=format-security \
- 		  $(WNOCLOBBERED) -Werror=cast-qual $(ERROR_DISCARDED_QUALIFIERS)
--CPPFLAGS	:= $(FORTIFY_OPT) \
--		   -DBIN_DIR=\"$(bindir)\" -DMULTIPATH_DIR=\"$(plugindir)\" -DRUN_DIR=\"${RUN}\" \
-+CPPFLAGS	+= -DBIN_DIR=\"$(bindir)\" -DMULTIPATH_DIR=\"$(plugindir)\" -DRUN_DIR=\"${RUN}\" \
- 		   -DCONFIG_DIR=\"$(configdir)\" -DEXTRAVERSION=\"$(EXTRAVERSION)\" -MMD -MP
--CFLAGS		:= --std=gnu99 $(CFLAGS) $(OPTFLAGS) $(WARNFLAGS) -pipe
-+CFLAGS		+= -std=gnu99 $(WARNFLAGS)
- BIN_CFLAGS	= -fPIE -DPIE
- LIB_CFLAGS	= -fPIC
- SHARED_FLAGS	= -shared

sys-fs/multipath-tools/metadata.xml
@@ -5,9 +5,6 @@
 		<email>base-system@gentoo.org</email>
 		<name>Gentoo Base System</name>
 	</maintainer>
-	<use>
-		<flag name="rbd">Enable Rados block device support via <pkg>sys-cluster/ceph</pkg></flag>
-	</use>
 	<upstream>
 		<remote-id type="github">opensvc/multipath-tools</remote-id>
 	</upstream>

sys-fs/multipath-tools/multipath-tools-0.8.9.ebuild
@@ -1,88 +1,0 @@
-# Copyright 1999-2022 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI="7"
-
-inherit linux-info systemd toolchain-funcs udev
-
-DESCRIPTION="Device mapper target autoconfig"
-HOMEPAGE="http://christophe.varoqui.free.fr/"
-SRC_URI="https://github.com/opensvc/${PN}/archive/refs/tags/${PV}.tar.gz -> ${P}.tar.gz"
-
-LICENSE="GPL-2"
-SLOT="0"
-KEYWORDS="~alpha amd64 ~arm arm64 ~ia64 ~loong ppc ppc64 ~riscv x86"
-IUSE="systemd rbd test"
-RESTRICT="!test? ( test )"
-
-RDEPEND="
-	dev-libs/json-c:=
-	dev-libs/libaio
-	dev-libs/userspace-rcu:=
-	>=sys-fs/lvm2-2.02.45
-	>=virtual/libudev-232-r3
-	sys-libs/readline:0=
-	rbd? ( sys-cluster/ceph )
-	systemd? ( sys-apps/systemd )
-"
-DEPEND="${RDEPEND}
-	test? ( dev-util/cmocka )"
-BDEPEND="virtual/pkgconfig"
-
-CONFIG_CHECK="~DM_MULTIPATH"
-
-PATCHES=(
-	"${FILESDIR}"/${PN}-0.8.7-respect-flags.patch
-)
-
-src_prepare() {
-	default
-
-	# The upstream lacks any way to configure the build at present
-	# and ceph is a huge dependency, so we're using sed to make it
-	# optional until the upstream has a proper configure system
-	if ! use rbd ; then
-		sed \
-			-e "s/libcheckrbd.so/# libcheckrbd.so/" \
-			-e "s/-lrados//" \
-			-i libmultipath/checkers/Makefile \
-			|| die
-	fi
-}
-
-src_compile() {
-	tc-export CC
-
-	# LIBDM_API_FLUSH involves grepping files in /usr/include,
-	# so force the test to go the way we want #411337.
-	emake \
-		LIB="${EPREFIX}/$(get_libdir)" \
-		LIBDM_API_FLUSH=1 \
-		PKGCONFIG="$(tc-getPKG_CONFIG)"
-}
-
-src_install() {
-	dodir /sbin /usr/share/man/man{3,5,8}
-	emake \
-		DESTDIR="${D}" \
-		LIB="${EPREFIX}/$(get_libdir)" \
-		RUN=run \
-		unitdir="$(systemd_get_systemunitdir)" \
-		libudevdir='${prefix}'/"$(get_udevdir)" \
-		pkgconfdir='${prefix}'/usr/'${LIB}'/pkgconfig \
-		install
-
-	newinitd "${FILESDIR}"/multipathd-r1.rc multipathd
-	newinitd "${FILESDIR}"/multipath.rc multipath
-
-	einstalldocs
-
-	find "${ED}" -type f -name "*.la" -delete || die
-}
-
-pkg_postinst() {
-	if [[ -z ${REPLACING_VERSIONS} ]] ; then
-		elog "If you need multipath on your system, you must"
-		elog "add 'multipath' into your boot runlevel!"
-	fi
-}

sys-fs/multipath-tools/multipath-tools-0.9.0.ebuild
@@ -1,76 +1,0 @@
-# Copyright 1999-2022 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=8
-
-inherit linux-info systemd toolchain-funcs udev
-
-DESCRIPTION="Device mapper target autoconfig"
-HOMEPAGE="http://christophe.varoqui.free.fr/"
-SRC_URI="https://github.com/opensvc/${PN}/archive/refs/tags/${PV}.tar.gz -> ${P}.tar.gz"
-
-LICENSE="GPL-2"
-SLOT="0"
-KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~ia64 ~loong ~ppc ~ppc64 ~riscv ~x86"
-IUSE="systemd test"
-RESTRICT="!test? ( test )"
-
-RDEPEND="
-	dev-libs/json-c:=
-	dev-libs/libaio
-	dev-libs/userspace-rcu:=
-	>=sys-fs/lvm2-2.02.45
-	>=virtual/libudev-232-r3
-	sys-libs/readline:=
-	systemd? ( sys-apps/systemd )"
-DEPEND="${RDEPEND}
-	test? ( dev-util/cmocka )"
-BDEPEND="virtual/pkgconfig"
-
-CONFIG_CHECK="~DM_MULTIPATH"
-
-PATCHES=( "${FILESDIR}"/${PN}-0.9.0-respect-flags.patch )
-
-src_compile() {
-	tc-export CC
-
-	# LIBDM_API_FLUSH involves grepping files in /usr/include,
-	# so force the test to go the way we want #411337.
-	emake \
-		prefix="${EPREFIX}" \
-		LIB="$(get_libdir)" \
-		LIBDM_API_FLUSH=1 \
-		PKGCONFIG="$(tc-getPKG_CONFIG)"
-}
-
-src_install() {
-	dodir /sbin /usr/share/man/man{3,5,8}
-	emake \
-		DESTDIR="${D}" \
-		prefix="${EPREFIX}" \
-		LIB="$(get_libdir)" \
-		RUN=run \
-		unitdir="$(systemd_get_systemunitdir)" \
-		libudevdir='$(prefix)'/$(get_udevdir) \
-		pkgconfdir='$(prefix)/usr/$(LIB)/pkgconfig' \
-		install
-	einstalldocs
-
-	newinitd "${FILESDIR}"/multipathd-r1.rc multipathd
-	newinitd "${FILESDIR}"/multipath.rc multipath
-
-	find "${ED}" -type f -name '*.la' -delete || die
-}
-
-pkg_postinst() {
-	udev_reload
-
-	if [[ -z ${REPLACING_VERSIONS} ]] ; then
-		elog "If you need multipath on your system, you must"
-		elog "add 'multipath' into your boot runlevel!"
-	fi
-}
-
-pkg_postrm() {
-	udev_reload
-}

commit 6170bcae6a911697812969d6b4e095d07c740fae

sys-fs/multipath-tools: restore 0.9.3-r2
Dropped accidentally.

Reported at https://github.com/flatcar/portage-stable/pull/401#issuecomment-1369035847.

Fixes: 69322f09c73a949705b50485ab26552491d8a3cd~1
Signed-off-by: Sam James <sam@gentoo.org>

sys-fs/multipath-tools/multipath-tools-0.9.3-r2.ebuild
@@ -1,0 +1,97 @@
+# Copyright 1999-2023 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit linux-info systemd toolchain-funcs udev tmpfiles
+
+DESCRIPTION="Device mapper target autoconfig"
+HOMEPAGE="http://christophe.varoqui.free.fr/"
+SRC_URI="https://github.com/opensvc/${PN}/archive/refs/tags/${PV}.tar.gz -> ${P}.tar.gz"
+
+LICENSE="GPL-2"
+SLOT="0"
+KEYWORDS="~alpha amd64 ~arm arm64 ~ia64 ~loong ppc ppc64 ~riscv x86"
+IUSE="systemd test"
+RESTRICT="!test? ( test )"
+
+RDEPEND="
+	dev-libs/json-c:=
+	dev-libs/libaio
+	dev-libs/userspace-rcu:=
+	>=sys-fs/lvm2-2.02.45
+	>=virtual/libudev-232-r3
+	sys-libs/readline:=
+	systemd? ( sys-apps/systemd )"
+DEPEND="${RDEPEND}
+	test? ( dev-util/cmocka )"
+BDEPEND="virtual/pkgconfig"
+
+CONFIG_CHECK="~DM_MULTIPATH"
+
+src_prepare() {
+	default
+
+	sed -r -i -e '/^(CPPFLAGS|CFLAGS)\>/s,^(CPPFLAGS|CFLAGS)\>[[:space:]]+:=,\1 := $(GENTOO_\1),' \
+		"${S}"/Makefile.inc || die
+}
+
+src_compile() {
+	tc-export CC
+
+	# LIBDM_API_FLUSH involves grepping files in /usr/include,
+	# so force the test to go the way we want #411337.
+	emake \
+		prefix="${EPREFIX}/usr" \
+		plugindir="${EPREFIX}/$(get_libdir)/multipath" \
+		LIB="$(get_libdir)" \
+		LIBDM_API_FLUSH=1 \
+		PKGCONFIG="$(tc-getPKG_CONFIG)" \
+		GENTOO_CFLAGS="${CFLAGS}" \
+		GENTOO_CPPFLAGS="${CPPFLAGS}" \
+		FAKEVAR=1
+}
+
+src_install() {
+	dodir /sbin
+
+	# Please clean this up > 0.9.3: https://github.com/opensvc/multipath-tools/pull/53
+	# $(prefix) doesn't work correctly in makefile in 0.9.3.
+	emake \
+		DESTDIR="${ED}" \
+		prefix="${EPREFIX}" \
+		LIB="$(get_libdir)" \
+		RUN=run \
+		plugindir="${EPREFIX}/$(get_libdir)/multipath" \
+		unitdir="$(systemd_get_systemunitdir)" \
+		libudevdir="${EPREFIX}/$(get_udevdir)" \
+		pkgconfdir="${EPREFIX}/usr/$(get_libdir)/pkgconfig" \
+		GENTOO_CFLAGS="${CFLAGS}" \
+		GENTOO_CPPFLAGS="${CPPFLAGS}" \
+		install
+
+	rmdir "${ED}"/usr/include
+	rmdir "${ED}"/usr/share
+	mv "${ED}"/include "${ED}"/usr/include || die
+	mv "${ED}"/share "${ED}"/usr/share || die
+	einstalldocs
+
+	newinitd "${FILESDIR}"/multipathd-r1.rc multipathd
+	newinitd "${FILESDIR}"/multipath.rc multipath
+
+	find "${ED}" -type f -name '*.la' -delete || die
+}
+
+pkg_postinst() {
+	tmpfiles_process /usr/lib/tmpfiles.d/multipath.conf
+	udev_reload
+
+	if [[ -z ${REPLACING_VERSIONS} ]] ; then
+		elog "If you need multipath on your system, you must"
+		elog "add 'multipath' into your boot runlevel!"
+	fi
+}
+
+pkg_postrm() {
+	udev_reload
+}