gentoo/gentoo

commit 0fc591ecb6046131b65a1996fd6cd8710baeb8fd

dev-java/protobuf-java: use 'assert' instead of 'die' for shell pipelines
Signed-off-by: Florian Schmaus <flow@gentoo.org>

dev-java/protobuf-java/protobuf-java-3.21.12.ebuild
@@ -55,7 +55,8 @@ src_prepare() {
 		"${S}/core/pom.xml") > "${T}/core_proto" || die "echo to core_proto failed"
 	# Copy them from ../src/google/protobuf to JAVA_RESOURCE_DIRS
 	pushd "${JAVA_RESOURCE_DIRS}" || die
-		jar cv "@${T}/core_proto" | jar xv || die "Copying protos failed"
+	jar cv "@${T}/core_proto" | jar xv
+	assert"Copying protos failed"
 	popd || die
 
 	# https://github.com/protocolbuffers/protobuf/blob/v21.12/java/core/generate-sources-build.xml

dev-java/protobuf-java/protobuf-java-3.21.9-r2.ebuild
@@ -52,7 +52,8 @@ src_prepare() {
 		"${S}/core/pom.xml") > "${T}/core_proto" || die "echo to core_proto failed"
 	# Copy them from ../src/google/protobuf to JAVA_RESOURCE_DIRS
 	pushd "${JAVA_RESOURCE_DIRS}" || die
-		jar cv "@${T}/core_proto" | jar xv || die "Copying protos failed"
+	jar cv "@${T}/core_proto" | jar xv
+	assert "Copying protos failed"
 	popd || die
 
 	# https://github.com/protocolbuffers/protobuf/blob/v21.9/java/core/generate-sources-build.xml

commit 8fa612a042c2caeac5836fc01d0e8b6121273cc5

dev-java/protobuf-java: fix typo
Closes: https://bugs.gentoo.org/890479
Fixes: 0fc591ecb6046131b65a1996fd6cd8710baeb8fd
Signed-off-by: Sam James <sam@gentoo.org>

dev-java/protobuf-java/protobuf-java-3.21.12.ebuild
@@ -56,7 +56,7 @@ src_prepare() {
 	# Copy them from ../src/google/protobuf to JAVA_RESOURCE_DIRS
 	pushd "${JAVA_RESOURCE_DIRS}" || die
 	jar cv "@${T}/core_proto" | jar xv
-	assert"Copying protos failed"
+	assert "Copying protos failed"
 	popd || die
 
 	# https://github.com/protocolbuffers/protobuf/blob/v21.12/java/core/generate-sources-build.xml