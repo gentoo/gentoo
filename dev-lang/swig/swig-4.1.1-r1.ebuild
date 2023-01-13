gentoo/gentoo

commit a9ac283e99d7992fd6aceecee6dc96c30e282d31

dev-lang/swig: silence autoreconf warnings
When it runs autoreconf for the subdirs (CCache), it warns about
the extra dirs we pass being missing. Symlink to silence it.

Signed-off-by: Sam James <sam@gentoo.org>

dev-lang/swig/swig-4.1.1-r1.ebuild
@@ -35,6 +35,7 @@ src_prepare() {
 	default
 
 	# Only needed for Clang 16 patch
+	ln -s "${S}"/Tools CCache/
 	AT_M4DIR="Tools/config" eautoreconf
 }
 

commit cb6a1b131019acbbfd74b0404f5c8a8693c331b9

dev-lang/swig: add missing die
Fixes: a9ac283e99d7992fd6aceecee6dc96c30e282d31
Signed-off-by: Sam James <sam@gentoo.org>

dev-lang/swig/swig-4.1.1-r1.ebuild
@@ -35,7 +35,7 @@ src_prepare() {
 	default
 
 	# Only needed for Clang 16 patch
-	ln -s "${S}"/Tools CCache/
+	ln -s "${S}"/Tools CCache/ || die
 	AT_M4DIR="Tools/config" eautoreconf
 }
 