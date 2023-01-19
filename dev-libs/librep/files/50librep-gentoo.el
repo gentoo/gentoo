87cd6fedaa38900cd9ae8b20220c629c1c4ab64d
Michał Górny <mgorny@gentoo.org>
2023-01-19 14:57:18 +0100

    dev-libs/librep: Remove last-rited pkg
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- dev-libs/librep/files/50librep-gentoo.el
+++
@@ -1,6 +0,0 @@
-
-;;; librep site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(autoload 'rep-debugger "rep-debugger"
-  "Run the rep debugger on program FILE in buffer *gud-FILE*." t)
