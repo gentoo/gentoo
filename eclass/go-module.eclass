gentoo/gentoo

commit f4dade77c0882aab06f35f8db1154d0a52f0ba1b

go-module.eclass: add -buildmode=pie
Bug: https://bugs.gentoo.org/702598
Signed-off-by: William Hubbs <williamh@gentoo.org>

eclass/go-module.eclass
@@ -1,4 +1,4 @@
-# Copyright 2019-2022 Gentoo Authors
+# Copyright 2019-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 # @ECLASS: go-module.eclass
@@ -95,11 +95,12 @@ export GOCACHE="${T}/go-build"
 export GOMODCACHE="${WORKDIR}/go-mod"
 
 # The following go flags should be used for all builds.
+# -buildmode=pie builds position independent executables
 # -buildvcs=false omits version control information
 # -modcacherw makes the build cache read/write
 # -v prints the names of packages as they are compiled
 # -x prints commands as they are executed
-export GOFLAGS="-buildvcs=false -modcacherw -v -x"
+export GOFLAGS="-buildmod=pie -buildvcs=false -modcacherw -v -x"
 
 # Do not complain about CFLAGS etc since go projects do not use them.
 QA_FLAGS_IGNORED='.*'

commit 156c19492867660e906b3d4c79bece66fb102b78

go-module.eclass: fix typo in buildmode
Fixes: f4dade77c0882aab06f35f8db1154d0a52f0ba1b
Bug: https://bugs.gentoo.org/702598
Signed-off-by: Sam James <sam@gentoo.org>

eclass/go-module.eclass
@@ -100,7 +100,7 @@ export GOMODCACHE="${WORKDIR}/go-mod"
 # -modcacherw makes the build cache read/write
 # -v prints the names of packages as they are compiled
 # -x prints commands as they are executed
-export GOFLAGS="-buildmod=pie -buildvcs=false -modcacherw -v -x"
+export GOFLAGS="-buildmode=pie -buildvcs=false -modcacherw -v -x"
 
 # Do not complain about CFLAGS etc since go projects do not use them.
 QA_FLAGS_IGNORED='.*'