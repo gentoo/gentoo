gentoo/gentoo

commit 69504c316cdb25bc2d97e27ea16b8e25ecdb9c35

app-admin/lnav: fix build w/ gcc 13
Signed-off-by: Sam James <sam@gentoo.org>

app-admin/lnav/lnav-0.11.1-r1.ebuild
@@ -36,6 +36,7 @@ DOCS=( AUTHORS NEWS.md README )
 
 PATCHES=(
 	"${FILESDIR}"/${PN}-0.11.0-disable-tests.patch
+	"${FILESDIR}"/0001-Fix-build-with-GCC-13-add-missing-cstdint-include.patch
 )
 
 src_prepare() {

app-admin/lnav/files/lnav-0.11.1-0001-Fix-build-with-GCC-13-add-missing-cstdint-include.patch
@@ -1,0 +1,26 @@
+https://github.com/tstack/lnav/pull/1112
+
+From e25d3e3c69fcf57bbaec2333846b9a43c4d1fc90 Mon Sep 17 00:00:00 2001
+From: Sam James <sam@gentoo.org>
+Date: Sat, 21 Jan 2023 18:11:20 +0000
+Subject: [PATCH] Fix build with GCC 13 (add missing <cstdint> include)
+
+GCC 13 (as usual for new compiler releases) shuffles around some
+internal includes and so <cstdint> is no longer transitively included.
+
+Explicitly include <cstdint> for uint32_t.
+
+Signed-off-by: Sam James <sam@gentoo.org>
+--- a/src/base/lnav_log.hh
++++ b/src/base/lnav_log.hh
+@@ -32,6 +32,7 @@
+ #ifndef lnav_log_hh
+ #define lnav_log_hh
+ 
++#include <cstdint>
+ #include <string>
+ 
+ #include <stdio.h>
+-- 
+2.39.1
+

commit 1653e5d34a6079edb6d3b3b0d4ec0460be7bd457

app-admin/lnav: fix patch filename
Closes: https://bugs.gentoo.org/891631
Fixes: 69504c316cdb25bc2d97e27ea16b8e25ecdb9c35
Signed-off-by: Sam James <sam@gentoo.org>

app-admin/lnav/lnav-0.11.1-r1.ebuild
@@ -36,7 +36,7 @@ DOCS=( AUTHORS NEWS.md README )
 
 PATCHES=(
 	"${FILESDIR}"/${PN}-0.11.0-disable-tests.patch
-	"${FILESDIR}"/0001-Fix-build-with-GCC-13-add-missing-cstdint-include.patch
+	"${FILESDIR}"/${PN}-0.11.1-0001-Fix-build-with-GCC-13-add-missing-cstdint-include.patch
 )
 
 src_prepare() {