gentoo/gentoo

commit 0c445de884c7ff22725c84d1b0ebed4099463434

app-shells/fzf: add 0.37.0
Signed-off-by: Sam James <sam@gentoo.org>

app-shells/fzf/Manifest
@@ -2,5 +2,6 @@ DIST fzf-0.34.0-deps.tar.xz 16800704 BLAKE2B b518f7bff1ea533af32efcb0e5307580aa9
 DIST fzf-0.34.0.tar.gz 214641 BLAKE2B ccb7f57df4305cf07dc8e0ba7384c3a446ddce083727ad98719a327150d853782bf781b0179d02f3f10664ffce501ebab55ccd2d5f61a2e690fa41f1d0f479c1 SHA512 6e2bd9a7c30862c09f627c73504f2a52951476abbd9af64941445862cc9bf783699cc9ccf215e25c49bbb3f9656841581b5805af4658b681b590ca14b5697331
 DIST fzf-0.35.1-deps.tar.xz 16796112 BLAKE2B 61c22a914bb1eb8cb6da1daf4b25a19d98e7b0dcd1575b47b6da995ae0fb5e5bc47612b7abafe2917d12b4900a69e604ffff87bf31f811de449522fe800af8f4 SHA512 eed0a0c5a48c5dfee2b6c4d3320280d75f19c16841e3ad731371060da55885c3ddd8d0490dbfc53a68f095dcff02727e8d804494bfa8edf6da041ffcb197dc74
 DIST fzf-0.35.1.tar.gz 218773 BLAKE2B 79b95e23a85865ea9218a222eba522a0a9d7f042f9119b308bc711cccc5327098dae271ba10fff9619f3ba7bc88037c70459f1caf511d5069264b931c875c49f SHA512 4e32836aa331835b6a7370ea342d2ddfa47a2059f5f7c5eec0b2c0dc9fdede14940bf64977c91df201775b3f4a06763a92593df7a50693d04d13b1befb194753
-DIST fzf-0.36.0-deps.tar.xz 16806808 BLAKE2B f121b3000bc5a0d72ee5cb66f59f4f44c41af30f28ca935a607b424399d12dc762f25811aea999e5de126e2727f8bda614e026da6983ed535d048a7229a07065 SHA512 121c6b1be0cd3af752b11461a72c22e3bc55eec12c12367b354a4ff4d4dc75fb0215cf19cec52008473045c6330a4af71b2e3a14a9a0a124e18aacc1aaad97a9
 DIST fzf-0.36.0.tar.gz 229126 BLAKE2B faa04870b7654b1ffb2f1c453636e33eab94e682c7db4702719419a5eca5ea136c657179248dd646573a625f575c823f9dcfd0023a96390bbf689e7455224411 SHA512 8e87dbd3b5ae9e0dc6a76855f7c75c9b4edc8ce797b2cd4eda053c4e1d618bb9afb2b24602e01776a49e47a00f2cd803ed433043f0acec8d385f741b13583e9e
+DIST fzf-0.37.0-deps.tar.xz 16806808 BLAKE2B f121b3000bc5a0d72ee5cb66f59f4f44c41af30f28ca935a607b424399d12dc762f25811aea999e5de126e2727f8bda614e026da6983ed535d048a7229a07065 SHA512 121c6b1be0cd3af752b11461a72c22e3bc55eec12c12367b354a4ff4d4dc75fb0215cf19cec52008473045c6330a4af71b2e3a14a9a0a124e18aacc1aaad97a9
+DIST fzf-0.37.0.tar.gz 230469 BLAKE2B 3281ae0e9b13f8be9d2bc5eb5bfab534a7b56279346ed500932840fb90e3bf0137c5610fdc5928faa99fadbb2cb10b98c46eea7a41f3a76cff3805ed52d255a7 SHA512 bb8cb4b6c83e978c45de757784f29be883070ef7abab2fe2dd89bc668be7628a3f476f97abf82f98220942c37317df0404e3fd2205c2e93ec506a6ca8beaf74c

app-shells/fzf/fzf-0.37.0.ebuild
@@ -11,11 +11,11 @@ HOMEPAGE="https://github.com/junegunn/fzf"
 # For fancy versioning only. Bump on the next release!
 # MY_GIT_REV is the first 7 characters of release commit, we truncate it
 # after to make it easier to copy/paste.
-MY_GIT_REV=20230117636c9dfa3ed6f6428c5bc4c51b02f60a
+MY_GIT_REV=2023012408ff892d43441fce0385943bafdbb52b
 MY_GIT_REV=${MY_GIT_REV:0:7}
 
 SRC_URI="https://github.com/junegunn/${PN}/archive/${PV}.tar.gz -> ${P}.tar.gz"
-#SRC_URI+=" https://dev.gentoo.org/~sam/distfiles/${CATEGORY}/${PN}/${P}-deps.tar.xz"
+SRC_URI+=" https://dev.gentoo.org/~sam/distfiles/${CATEGORY}/${PN}/${P}-deps.tar.xz"
 
 LICENSE="MIT BSD-with-disclosure"
 SLOT="0"

commit ee8b80471e198064f9477fc3f5a1d1fa66b0ec27

app-shells/fzf: fix 0.36.0 SRC_URI
When bumping to 0.37.0 earlier, I first typo'd the version number, which
made it touch 0.36.0 first and sed out SRC_URI to regenerate the dep tarball -
which I didn't realise I'd not reset before committing 0.37.0 :(

Closes: https://bugs.gentoo.org/892047
Fixes: 0c445de884c7ff22725c84d1b0ebed4099463434
Signed-off-by: Sam James <sam@gentoo.org>

app-shells/fzf/Manifest
@@ -2,6 +2,7 @@ DIST fzf-0.34.0-deps.tar.xz 16800704 BLAKE2B b518f7bff1ea533af32efcb0e5307580aa9
 DIST fzf-0.34.0.tar.gz 214641 BLAKE2B ccb7f57df4305cf07dc8e0ba7384c3a446ddce083727ad98719a327150d853782bf781b0179d02f3f10664ffce501ebab55ccd2d5f61a2e690fa41f1d0f479c1 SHA512 6e2bd9a7c30862c09f627c73504f2a52951476abbd9af64941445862cc9bf783699cc9ccf215e25c49bbb3f9656841581b5805af4658b681b590ca14b5697331
 DIST fzf-0.35.1-deps.tar.xz 16796112 BLAKE2B 61c22a914bb1eb8cb6da1daf4b25a19d98e7b0dcd1575b47b6da995ae0fb5e5bc47612b7abafe2917d12b4900a69e604ffff87bf31f811de449522fe800af8f4 SHA512 eed0a0c5a48c5dfee2b6c4d3320280d75f19c16841e3ad731371060da55885c3ddd8d0490dbfc53a68f095dcff02727e8d804494bfa8edf6da041ffcb197dc74
 DIST fzf-0.35.1.tar.gz 218773 BLAKE2B 79b95e23a85865ea9218a222eba522a0a9d7f042f9119b308bc711cccc5327098dae271ba10fff9619f3ba7bc88037c70459f1caf511d5069264b931c875c49f SHA512 4e32836aa331835b6a7370ea342d2ddfa47a2059f5f7c5eec0b2c0dc9fdede14940bf64977c91df201775b3f4a06763a92593df7a50693d04d13b1befb194753
+DIST fzf-0.36.0-deps.tar.xz 16806808 BLAKE2B f121b3000bc5a0d72ee5cb66f59f4f44c41af30f28ca935a607b424399d12dc762f25811aea999e5de126e2727f8bda614e026da6983ed535d048a7229a07065 SHA512 121c6b1be0cd3af752b11461a72c22e3bc55eec12c12367b354a4ff4d4dc75fb0215cf19cec52008473045c6330a4af71b2e3a14a9a0a124e18aacc1aaad97a9
 DIST fzf-0.36.0.tar.gz 229126 BLAKE2B faa04870b7654b1ffb2f1c453636e33eab94e682c7db4702719419a5eca5ea136c657179248dd646573a625f575c823f9dcfd0023a96390bbf689e7455224411 SHA512 8e87dbd3b5ae9e0dc6a76855f7c75c9b4edc8ce797b2cd4eda053c4e1d618bb9afb2b24602e01776a49e47a00f2cd803ed433043f0acec8d385f741b13583e9e
 DIST fzf-0.37.0-deps.tar.xz 16806808 BLAKE2B f121b3000bc5a0d72ee5cb66f59f4f44c41af30f28ca935a607b424399d12dc762f25811aea999e5de126e2727f8bda614e026da6983ed535d048a7229a07065 SHA512 121c6b1be0cd3af752b11461a72c22e3bc55eec12c12367b354a4ff4d4dc75fb0215cf19cec52008473045c6330a4af71b2e3a14a9a0a124e18aacc1aaad97a9
 DIST fzf-0.37.0.tar.gz 230469 BLAKE2B 3281ae0e9b13f8be9d2bc5eb5bfab534a7b56279346ed500932840fb90e3bf0137c5610fdc5928faa99fadbb2cb10b98c46eea7a41f3a76cff3805ed52d255a7 SHA512 bb8cb4b6c83e978c45de757784f29be883070ef7abab2fe2dd89bc668be7628a3f476f97abf82f98220942c37317df0404e3fd2205c2e93ec506a6ca8beaf74c

app-shells/fzf/fzf-0.36.0.ebuild
@@ -15,7 +15,7 @@ MY_GIT_REV=20230117636c9dfa3ed6f6428c5bc4c51b02f60a
 MY_GIT_REV=${MY_GIT_REV:0:7}
 
 SRC_URI="https://github.com/junegunn/${PN}/archive/${PV}.tar.gz -> ${P}.tar.gz"
-#SRC_URI+=" https://dev.gentoo.org/~sam/distfiles/${CATEGORY}/${PN}/${P}-deps.tar.xz"
+SRC_URI+=" https://dev.gentoo.org/~sam/distfiles/${CATEGORY}/${PN}/${P}-deps.tar.xz"
 
 LICENSE="MIT BSD-with-disclosure"
 SLOT="0"