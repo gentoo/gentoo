gentoo/gentoo

commit 50546c3db84904399e5624eb8f163e3d2be284e3

distutils-r1.eclass: adjust setuptools dep to avoid known-bad versions
79071eb9f6f4a5725c1a89700bcfd7f120101179 tried to mitigate this but
blockers don't affect dependency resolution (ordering), so let's add
a || ( <dev-python/setuptools-... >=dev-python/setuptools-...) dep
in the eclass to ensure that the bad in-between versions (which were
in ~arch, not stable, for a period, and are no longer in tree) aren't
considered good enough to install any distutils-r1 PEP517 packages.

We can clean this up once newer setuptools is stable & then simplify
(and tighten) the dep.

(Worth keeping in mind that Portage (rightly) doesn't aggressively
update things listed in RDEPEND in that order simply because they're
in RDEPEND. It might update something listed in RDEPEND after the
package listing it provided there's no >= or otherwise dep.)

Bug: https://bugs.gentoo.org/892529
Bug: https://bugs.gentoo.org/892525
Bug: https://bugs.gentoo.org/893538
Bug: https://bugs.gentoo.org/893632
Bug: https://bugs.gentoo.org/893630
Bug: https://bugs.gentoo.org/893634
Signed-off-by: Sam James <sam@gentoo.org>

eclass/distutils-r1.eclass
@@ -248,8 +248,14 @@ _distutils_set_globals() {
 				'
 				;;
 			setuptools)
+				# || ( ... ) dep is a workaround for bug #892525
+				# It can be removed once >=67.2.0 is stable and replaced with
+				# a simple >=67.2.0 dep.
 				bdep+='
-					>=dev-python/setuptools-65.7.0[${PYTHON_USEDEP}]
+					|| (
+						>=dev-python/setuptools-67.2.0[${PYTHON_USEDEP}]
+						<dev-python/setuptools-65.6.4[${PYTHON_USEDEP}]
+					)
 					>=dev-python/wheel-0.38.4[${PYTHON_USEDEP}]
 				'
 				;;

commit b6a70b6b3c8cdc1db9bad1366e2e07feba76623f

distutils-r1.eclass: fix typo in setuptools version
Fixes: 50546c3db84904399e5624eb8f163e3d2be284e3
Signed-off-by: Sam James <sam@gentoo.org>

eclass/distutils-r1.eclass
@@ -254,7 +254,7 @@ _distutils_set_globals() {
 				bdep+='
 					|| (
 						>=dev-python/setuptools-67.2.0[${PYTHON_USEDEP}]
-						<dev-python/setuptools-65.6.4[${PYTHON_USEDEP}]
+						<dev-python/setuptools-65.7.1[${PYTHON_USEDEP}]
 					)
 					>=dev-python/wheel-0.38.4[${PYTHON_USEDEP}]
 				'