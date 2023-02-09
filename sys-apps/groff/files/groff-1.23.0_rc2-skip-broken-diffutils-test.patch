gentoo/gentoo

commit 882ac9b1db71efea7e03c41c12b21278c50461ea

sys-apps/groff: skip broken test w/ diffutils-3.9
Signed-off-by: Sam James <sam@gentoo.org>

sys-apps/groff/groff-1.22.4.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2022 Gentoo Authors
+# Copyright 1999-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
@@ -55,6 +55,13 @@ src_prepare() {
 	.ds operating-system Gentoo${pfx}/${KERNEL}
 	.ds default-operating-system Gentoo${pfx}/${KERNEL}
 	EOF
+
+	# https://savannah.gnu.org/bugs/?63726
+	sed -i \
+		-e '/TESTS += contrib\/gdiffmk\/tests\/gdiffmk_tests.sh/d' \
+		-e '/\tcontrib\/gdiffmk\/tests\/gdiffmk_tests.sh \\/d' \
+		contrib/gdiffmk/tests/gdiffmk_tests.sh \
+		contrib/gdiffmk/gdiffmk.am || die
 
 	# make sure we don't get a crappy `g' nameprefix on UNIX systems with real
 	# troff (GROFF_G macro runs some test to see, its own troff doesn't satisfy)

sys-apps/groff/groff-1.23.0_rc2.ebuild
@@ -61,6 +61,13 @@ src_prepare() {
 	.ds default-operating-system Gentoo${pfx}/${KERNEL}
 	EOF
 
+	# https://savannah.gnu.org/bugs/?63726
+	sed -i \
+		-e '/TESTS += contrib\/gdiffmk\/tests\/gdiffmk_tests.sh/d' \
+		-e '/\tcontrib\/gdiffmk\/tests\/gdiffmk_tests.sh \\/d' \
+		contrib/gdiffmk/tests/gdiffmk_tests.sh \
+		contrib/gdiffmk/gdiffmk.am || die
+
 	# make sure we don't get a crappy `g' nameprefix on UNIX systems with real
 	# troff (GROFF_G macro runs some test to see, its own troff doesn't satisfy)
 	sed -i -e 's/^[ \t]\+g=g$/g=/' configure || die

commit 2e23a14c7fb085f1004612e39cec7cbac96d2119

sys-apps/groff: use patch for disabling broken diffutils tests
Avoid maintainer mode.

Fixes: 882ac9b1db71efea7e03c41c12b21278c50461ea
Signed-off-by: Sam James <sam@gentoo.org>

sys-apps/groff/groff-1.22.4.ebuild
@@ -39,6 +39,7 @@ S="${WORKDIR}/${MY_P}"
 
 PATCHES=(
 	"${FILESDIR}"/${PN}-1.19.2-man-unicode-dashes.patch #16108 #17580 #121502
+	"${FILESDIR}"/${PN}-1.22.4-skip-broken-diffutils-test.patch
 )
 
 src_prepare() {
@@ -55,13 +56,6 @@ src_prepare() {
 	.ds operating-system Gentoo${pfx}/${KERNEL}
 	.ds default-operating-system Gentoo${pfx}/${KERNEL}
 	EOF
-
-	# https://savannah.gnu.org/bugs/?63726
-	sed -i \
-		-e '/TESTS += contrib\/gdiffmk\/tests\/gdiffmk_tests.sh/d' \
-		-e '/\tcontrib\/gdiffmk\/tests\/gdiffmk_tests.sh \\/d' \
-		contrib/gdiffmk/tests/gdiffmk_tests.sh \
-		contrib/gdiffmk/gdiffmk.am || die
 
 	# make sure we don't get a crappy `g' nameprefix on UNIX systems with real
 	# troff (GROFF_G macro runs some test to see, its own troff doesn't satisfy)

sys-apps/groff/groff-1.23.0_rc2.ebuild
@@ -44,6 +44,7 @@ DOCS=( BUG-REPORT ChangeLog MORE.STUFF NEWS PROBLEMS PROJECTS README TODO )
 PATCHES=(
 	# bug #16108, bug #17580, bug #121502
 	"${FILESDIR}"/${PN}-1.19.2-man-unicode-dashes.patch
+	"${FILESDIR}"/${PN}-1.23.0_rc2-skip-broken-diffutils-test.patch
 )
 
 src_prepare() {
@@ -60,13 +61,6 @@ src_prepare() {
 	.ds operating-system Gentoo${pfx}/${KERNEL}
 	.ds default-operating-system Gentoo${pfx}/${KERNEL}
 	EOF
-
-	# https://savannah.gnu.org/bugs/?63726
-	sed -i \
-		-e '/TESTS += contrib\/gdiffmk\/tests\/gdiffmk_tests.sh/d' \
-		-e '/\tcontrib\/gdiffmk\/tests\/gdiffmk_tests.sh \\/d' \
-		contrib/gdiffmk/tests/gdiffmk_tests.sh \
-		contrib/gdiffmk/gdiffmk.am || die
 
 	# make sure we don't get a crappy `g' nameprefix on UNIX systems with real
 	# troff (GROFF_G macro runs some test to see, its own troff doesn't satisfy)

sys-apps/groff/files/groff-1.22.4-skip-broken-diffutils-test.patch
@@ -1,0 +1,37 @@
+https://savannah.gnu.org/bugs/?63726
+--- a/Makefile.in
++++ b/Makefile.in
+@@ -4195,8 +4195,7 @@ SUFFIXES = .roff .in .ps .mom .pdf .me .ms .ps .html .txt .texi .dvi \
+ 
+ # tests launched by make check
+ check_SCRIPTS = 
+-TESTS = contrib/gdiffmk/tests/gdiffmk_tests.sh $(am__append_5) \
+-	$(am__append_8)
++TESTS = $(am__append_5) $(am__append_8)
+ AM_TESTS_ENVIRONMENT = \
+   abs_top_srcdir=$(abs_top_srcdir) \
+   abs_top_builddir=$(abs_top_builddir) \
+@@ -9570,13 +9569,6 @@ recheck: all $(check_PROGRAMS) $(check_SCRIPTS)
+ 	        am__force_recheck=am--force-recheck \
+ 	        TEST_LOGS="$$log_list"; \
+ 	exit $$?
+-contrib/gdiffmk/tests/gdiffmk_tests.sh.log: contrib/gdiffmk/tests/gdiffmk_tests.sh
+-	@p='contrib/gdiffmk/tests/gdiffmk_tests.sh'; \
+-	b='contrib/gdiffmk/tests/gdiffmk_tests.sh'; \
+-	$(am__check_pre) $(LOG_DRIVER) --test-name "$$f" \
+-	--log-file $$b.log --trs-file $$b.trs \
+-	$(am__common_driver_flags) $(AM_LOG_DRIVER_FLAGS) $(LOG_DRIVER_FLAGS) -- $(LOG_COMPILE) \
+-	"$$tst" $(AM_TESTS_FD_REDIRECT)
+ contrib/hdtbl/examples/test-hdtbl.sh.log: contrib/hdtbl/examples/test-hdtbl.sh
+ 	@p='contrib/hdtbl/examples/test-hdtbl.sh'; \
+ 	b='contrib/hdtbl/examples/test-hdtbl.sh'; \
+--- a/contrib/gdiffmk/gdiffmk.am
++++ b/contrib/gdiffmk/gdiffmk.am
+@@ -26,7 +26,6 @@
+ ########################################################################
+ gdiffmk_srcdir = $(top_srcdir)/contrib/gdiffmk
+ bin_SCRIPTS += gdiffmk
+-TESTS += contrib/gdiffmk/tests/gdiffmk_tests.sh
+ man1_MANS += contrib/gdiffmk/gdiffmk.1
+ EXTRA_DIST += \
+   contrib/gdiffmk/gdiffmk.1.man \

sys-apps/groff/files/groff-1.23.0_rc2-skip-broken-diffutils-test.patch
@@ -1,0 +1,37 @@
+https://savannah.gnu.org/bugs/?63726
+--- a/Makefile.in
++++ b/Makefile.in
+@@ -4449,8 +4449,7 @@ SUFFIXES = .roff .in .ps .mom .pdf .me.in .me .man
+ 
+ # tests launched by make check
+ check_SCRIPTS = 
+-TESTS = contrib/gdiffmk/tests/gdiffmk_tests.sh $(hdtbl_TESTS) \
+-	$(mm_TESTS) $(am__append_14) $(rfc1345_TESTS) \
++TESTS = $(hdtbl_TESTS) $(mm_TESTS) $(am__append_14) $(rfc1345_TESTS) \
+ 	$(font_devpdf_TESTS) $(grotty_TESTS) $(eqn_TESTS) \
+ 	$(preconv_TESTS) $(refer_TESTS) $(tbl_TESTS) $(groff_TESTS) \
+ 	$(nroff_TESTS) $(grog_TESTS) $(tmac_TESTS)
+@@ -10558,13 +10557,6 @@ recheck: all $(check_PROGRAMS) $(check_SCRIPTS)
+ 	        am__force_recheck=am--force-recheck \
+ 	        TEST_LOGS="$$log_list"; \
+ 	exit $$?
+-contrib/gdiffmk/tests/gdiffmk_tests.sh.log: contrib/gdiffmk/tests/gdiffmk_tests.sh
+-	@p='contrib/gdiffmk/tests/gdiffmk_tests.sh'; \
+-	b='contrib/gdiffmk/tests/gdiffmk_tests.sh'; \
+-	$(am__check_pre) $(LOG_DRIVER) --test-name "$$f" \
+-	--log-file $$b.log --trs-file $$b.trs \
+-	$(am__common_driver_flags) $(AM_LOG_DRIVER_FLAGS) $(LOG_DRIVER_FLAGS) -- $(LOG_COMPILE) \
+-	"$$tst" $(AM_TESTS_FD_REDIRECT)
+ contrib/hdtbl/examples/test-hdtbl.sh.log: contrib/hdtbl/examples/test-hdtbl.sh
+ 	@p='contrib/hdtbl/examples/test-hdtbl.sh'; \
+ 	b='contrib/hdtbl/examples/test-hdtbl.sh'; \
+--- a/contrib/gdiffmk/gdiffmk.am
++++ b/contrib/gdiffmk/gdiffmk.am
+@@ -22,7 +22,6 @@
+ ########################################################################
+ gdiffmk_srcdir = $(top_srcdir)/contrib/gdiffmk
+ bin_SCRIPTS += gdiffmk
+-TESTS += contrib/gdiffmk/tests/gdiffmk_tests.sh
+ man1_MANS += contrib/gdiffmk/gdiffmk.1
+ EXTRA_DIST += \
+   contrib/gdiffmk/gdiffmk.1.man \