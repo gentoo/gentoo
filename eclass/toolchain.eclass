gentoo/gentoo

commit a39d1669ca0f42a2477c978d14ba5c22728aa6cb

toolchain.eclass: adjust D dep for non-gcc
Needed for sys-devel/kgcc64.

Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -324,11 +324,11 @@ if tc_has_feature zstd ; then
 	RDEPEND+=" zstd? ( app-arch/zstd:= )"
 fi
 
-if tc_has_feature valgrind; then
+if tc_has_feature valgrind ; then
 	BDEPEND+=" valgrind? ( dev-util/valgrind )"
 fi
 
-if tc_version_is_at_least 12.0 ; then
+if tc_has_feature d && tc_version_is_at_least 12.0 ; then
 	# D in 12+ is self-hosting and needs D to bootstrap.
 	# TODO: package some binary we can use, like for Ada
 	# bug #840182

commit 9e51c9704162d07ab528179c4c3b05f36f1f21b3

toolchain.eclass: Register the D tc_feature
This behavior is relied on elsewhere, for setting BDEPENDs correctly.

Fixes: a39d1669ca0f4 ("toolchain.eclass: adjust D dep for non-gcc")
Signed-off-by: Arsen Arsenović <arsen@gentoo.org>

eclass/toolchain.eclass
@@ -1,4 +1,4 @@
-# Copyright 1999-2022 Gentoo Authors
+# Copyright 1999-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 # @ECLASS: toolchain.eclass
@@ -271,7 +271,7 @@ if [[ ${PN} != kgcc64 && ${PN} != gcc-* ]] ; then
 	tc_version_is_at_least 8.0 &&
 		IUSE+=" systemtap" TC_FEATURES+=( systemtap )
 
-	tc_version_is_at_least 9.0 && IUSE+=" d"
+	tc_version_is_at_least 9.0 && IUSE+=" d" TC_FEATURES+=( d )
 	tc_version_is_at_least 9.1 && IUSE+=" lto"
 	tc_version_is_at_least 10 && IUSE+=" cet"
 	tc_version_is_at_least 10 && IUSE+=" zstd" TC_FEATURES+=( zstd )