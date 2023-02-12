gentoo/gentoo

commit 4cf54334acda0f6e46bd32bab70eed33f26561aa

app-editors/emacs: Migrate from net-libs/webkit-gtk:4 to :4.1
Live ebuild for the emacs-29 branch. master will follow when it
is merged.

Bug: https://bugs.gentoo.org/893670
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

app-editors/emacs/emacs-29.0.9999-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2022 Gentoo Authors
+# Copyright 1999-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -67,7 +67,7 @@ X_DEPEND="x11-libs/libICE
 	gtk? (
 		x11-libs/gtk+:3
 		xwidgets? (
-			net-libs/webkit-gtk:4=
+			net-libs/webkit-gtk:4.1=
 			x11-libs/libXcomposite
 		)
 	)

commit e4f05fe7924ef1756d32bb50fed32e56fd3c2fe0

app-editors/emacs: Fix webkit-gtk dependency in live ebuild
Fixes: 4cf54334acda0f6e46bd32bab70eed33f26561aa
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

app-editors/emacs/emacs-29.0.9999-r1.ebuild
@@ -137,7 +137,7 @@ RDEPEND="app-emacs/emacs-common[games?,gui(-)?]
 					>=dev-libs/libotf-0.9.4
 					>=dev-libs/m17n-lib-1.5.1
 				)
-				xwidgets? ( net-libs/webkit-gtk:4= )
+				xwidgets? ( net-libs/webkit-gtk:4.1= )
 			) )
 			!gtk? ( ${X_DEPEND} )
 			X? ( ${X_DEPEND} )