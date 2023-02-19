gentoo/gentoo

commit e7e6e85fa6ad70ce179cbae21be690d0eb93978d

dev-libs/starpu: [QA] use tc-check-openmp correctly
Signed-off-by: David Seifert <soap@gentoo.org>

dev-libs/starpu/starpu-1.2.6-r1.ebuild
@@ -40,6 +40,15 @@ BDEPEND="
 	test? ( gcc-plugin? ( dev-scheme/guile ) )
 "
 
+pkg_pretend() {
+	[[ ${MERGE_TYPE} != binary ]] && use openmp && tc-check-openmp
+}
+
+pkg_setup() {
+	[[ ${MERGE_TYPE} != binary ]] && use openmp && tc-check-openmp
+	fortran-2_pkg_setup
+}
+
 src_prepare() {
 	default
 

dev-libs/starpu/starpu-1.3.9.ebuild
@@ -42,8 +42,13 @@ BDEPEND="
 	doc? ( app-doc/doxygen virtual/latex-base )
 "
 
+pkg_pretend() {
+	[[ ${MERGE_TYPE} != binary ]] && use openmp && tc-check-openmp
+}
+
 pkg_setup() {
 	[[ ${MERGE_TYPE} != binary ]] && use openmp && tc-check-openmp
+	fortran-2_pkg_setup
 }
 
 src_prepare() {

commit 7368d878e955819dddd5413bf6be2ea8d629bb58

dev-libs/starpu: drop openmp refs in old
Rather than hook up openmp in configure for the old version, let's
just drop the references to it in pkg_*. Newer versions are set up
correctly.

Closes: https://bugs.gentoo.org/895446
Fixes: e7e6e85fa6ad70ce179cbae21be690d0eb93978d
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/starpu/starpu-1.2.6-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2022 Gentoo Authors
+# Copyright 1999-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
@@ -39,15 +39,6 @@ BDEPEND="
 	doc? ( app-doc/doxygen virtual/latex-base )
 	test? ( gcc-plugin? ( dev-scheme/guile ) )
 "
-
-pkg_pretend() {
-	[[ ${MERGE_TYPE} != binary ]] && use openmp && tc-check-openmp
-}
-
-pkg_setup() {
-	[[ ${MERGE_TYPE} != binary ]] && use openmp && tc-check-openmp
-	fortran-2_pkg_setup
-}
 
 src_prepare() {
 	default