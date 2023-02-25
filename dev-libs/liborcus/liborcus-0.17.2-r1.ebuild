gentoo/gentoo

commit 632310480c2bd41af986d9c6580969fe9d29cab3

dev-libs/liborcus: python3_11
Tests pass.

Signed-off-by: Andreas Sturmlechner <asturm@gentoo.org>

dev-libs/liborcus/liborcus-0.17.2-r1.ebuild
@@ -1,0 +1,70 @@
+# Copyright 1999-2023 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+PYTHON_COMPAT=( python3_{9,11} )
+inherit autotools python-single-r1
+
+DESCRIPTION="Standalone file import filter library for spreadsheet documents"
+HOMEPAGE="https://gitlab.com/orcus/orcus/blob/master/README.md"
+
+if [[ ${PV} == *9999* ]]; then
+	MDDS_SLOT="1/2.0"
+	EGIT_REPO_URI="https://gitlab.com/orcus/orcus.git"
+	inherit git-r3
+else
+	MDDS_SLOT="1/2.0"
+	SRC_URI="https://kohei.us/files/orcus/src/${P}.tar.xz"
+	KEYWORDS="~amd64 ~arm ~arm64 ~loong ~ppc ~ppc64 ~x86"
+fi
+
+LICENSE="MIT"
+SLOT="0/0.17" # based on SONAME of liborcus.so
+IUSE="python +spreadsheet-model test tools"
+
+REQUIRED_USE="python? ( ${PYTHON_REQUIRED_USE} )"
+RESTRICT="!test? ( test )"
+
+RDEPEND="
+	dev-libs/boost:=[zlib(+)]
+	sys-libs/zlib
+	python? ( ${PYTHON_DEPS} )
+	spreadsheet-model? ( dev-libs/libixion:${SLOT} )
+"
+DEPEND="${RDEPEND}
+	dev-util/mdds:${MDDS_SLOT}
+"
+
+PATCHES=(
+	"${FILESDIR}"/${P}-clang.patch
+	"${FILESDIR}"/${P}-gcc-13.patch
+	"${FILESDIR}"/${P}-python-optional.patch
+)
+
+pkg_setup() {
+	use python && python-single-r1_pkg_setup
+}
+
+src_prepare() {
+	# bug 713586
+	use test && eapply "${FILESDIR}/${PN}-0.17.0-test-fix.patch"
+
+	default
+	eautoreconf
+}
+
+src_configure() {
+	local myeconfargs=(
+		--disable-werror
+		$(use_enable python)
+		$(use_enable spreadsheet-model)
+		$(use_with tools)
+	)
+	econf "${myeconfargs[@]}"
+}
+
+src_install() {
+	default
+	find "${D}" -name '*.la' -type f -delete || die
+}

dev-libs/liborcus/files/liborcus-0.17.2-python-optional.patch
@@ -1,0 +1,26 @@
+From 14d15bf331b5cc3ca2c1cdb8097dfe918173cba1 Mon Sep 17 00:00:00 2001
+From: matoro <11910244-matoro3@users.noreply.gitlab.com>
+Date: Wed, 24 Aug 2022 13:42:27 -0400
+Subject: [PATCH] Only recurse into src/python when BUILD_PYTHON is set
+
+---
+ src/Makefile.am | 6 +++++-
+ 1 file changed, 5 insertions(+), 1 deletion(-)
+
+diff --git a/src/Makefile.am b/src/Makefile.am
+index d586299c..7e44aed8 100644
+--- a/src/Makefile.am
++++ b/src/Makefile.am
+@@ -1,4 +1,8 @@
+-SUBDIRS = include test parser mso liborcus spreadsheet python
++SUBDIRS = include test parser mso liborcus spreadsheet
++
++if BUILD_PYTHON
++SUBDIRS += python
++endif
+ 
+ AM_CPPFLAGS = \
+ 	-I$(top_srcdir)/include \
+-- 
+GitLab
+

commit c0e3e49c269a2f325af1109d1768e240a19cbf37

dev-libs/liborcus: enable py3.10 (restore it)
Closes: https://bugs.gentoo.org/897734
Fixes: 632310480c2bd41af986d9c6580969fe9d29cab3
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/liborcus/liborcus-0.17.2-r1.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-PYTHON_COMPAT=( python3_{9,11} )
+PYTHON_COMPAT=( python3_{9..11} )
 inherit autotools python-single-r1
 
 DESCRIPTION="Standalone file import filter library for spreadsheet documents"