gentoo/gentoo

commit 349ac256c44fb15a2fa5f13305179192a27a2e8f

sci-libs/proj: set BUILD_BENCHMARKS conditionally
Only defined for tests.

Closes: https://bugs.gentoo.org/900717
Signed-off-by: Sam James <sam@gentoo.org>

sci-libs/proj/proj-9.2.0.ebuild
@@ -49,7 +49,12 @@ src_configure() {
 		-DENABLE_TIFF=$(usex tiff)
 	)
 
-	use test && mycmakeargs+=( -DUSE_EXTERNAL_GTEST=ON )
+	if use test ; then
+		mycmakeargs+=(
+			-DUSE_EXTERNAL_GTEST=ON
+			-DBUILD_BENCHMARKS=OFF
+		)
+	fi
 
 	cmake_src_configure
 }

commit 081ce43c0fc4c512ac7da6d88173e748f0b0c17b

sci-libs/proj: drop leftover unconditional BUILD_BENCHMARKS
Closes: https://bugs.gentoo.org/900719
Bug: https://bugs.gentoo.org/900717
Fixes: 349ac256c44fb15a2fa5f13305179192a27a2e8f
Signed-off-by: Sam James <sam@gentoo.org>

sci-libs/proj/proj-9.2.0.ebuild
@@ -44,7 +44,6 @@ src_configure() {
 	local mycmakeargs=(
 		-DBUILD_TESTING=$(usex test)
 		-DBUILD_PROJSYNC=$(usex curl)
-		-DBUILD_BENCHMARKS=OFF
 		-DENABLE_CURL=$(usex curl)
 		-DENABLE_TIFF=$(usex tiff)
 	)