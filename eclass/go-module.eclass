gentoo/gentoo

commit d4ff29b30952ca957b58b5878550f5d23f908dbf

go-module.eclass: use tc-endian from toolchain-funcs.eclass
matoro reports that any Go packages fail on ppc64 right now as
USE=big-endian is not an implicit USE flag. Switch to tc-endian
from toolchain-funcs.eclass instead.

Signed-off-by: Sam James <sam@gentoo.org>

eclass/go-module.eclass
@@ -352,7 +352,7 @@ go-module_setup_proxy() {
 # - Otherwise do a normal unpack.
 go-module_src_unpack() {
 	if use amd64 || use arm || use arm64 ||
-		( use ppc64 && ! use big-endian ) || use s390 || use x86; then
+		( use ppc64 && [[ $(tc-endian) == "little" ]] ) || use s390 || use x86; then
 			GOFLAGS="-buildmode=pie ${GOFLAGS}"
 	fi
 	GOFLAGS="${GOFLAGS} -p=$(makeopts_jobs)"

commit c8e928d996b520af0cbe1eb48224c8184a9a7e2a

go-module.eclass: inherit toolchain-funcs for tc-endian
Closes: https://bugs.gentoo.org/901017
Fixes: d4ff29b30952ca957b58b5878550f5d23f908dbf
Signed-off-by: Sam James <sam@gentoo.org>

eclass/go-module.eclass
@@ -65,7 +65,7 @@ case ${EAPI} in
 	*) die "${ECLASS}: EAPI ${EAPI:-0} not supported" ;;
 esac
 
-inherit multiprocessing
+inherit multiprocessing toolchain-funcs
 
 if [[ -z ${_GO_MODULE} ]]; then
 