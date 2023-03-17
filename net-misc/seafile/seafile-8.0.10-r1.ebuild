gentoo/gentoo

commit adfaf2ed6d9d1b3c7f15c2f46ce858360c3d53c6

net-misc/seafile: fix commits used for tags
Closes: https://bugs.gentoo.org/901013
Signed-off-by: Sam James <sam@gentoo.org>

net-misc/seafile/seafile-8.0.10-r1.ebuild
@@ -6,17 +6,19 @@ EAPI=8
 PYTHON_COMPAT=( python3_{9..11} )
 
 # Upstream is moving tags repeatedly, then we use commit hash.
-RELEASE_COMMIT="6bd36a957573eda6a53f3d1bc538dbfc001aa9fd"
+# (Please file a bug upstream if this happens!)
+#RELEASE_COMMIT="6bd36a957573eda6a53f3d1bc538dbfc001aa9fd"
 
 inherit autotools python-single-r1 vala
 
 DESCRIPTION="File syncing and sharing software with file encryption and group sharing"
 HOMEPAGE="https://www.seafile.com/ https://github.com/haiwen/seafile/"
-SRC_URI="https://github.com/haiwen/${PN}/archive/${RELEASE_COMMIT}.tar.gz -> ${P}.tar.gz"
+#SRC_URI="https://github.com/haiwen/${PN}/archive/${RELEASE_COMMIT}.tar.gz -> ${P}.tar.gz"
+SRC_URI="https://github.com/haiwen/seafile/archive/refs/tags/v${PV}.tar.gz -> ${P}.gh.tar.gz"
 
 LICENSE="GPL-2+-with-openssl-exception"
 SLOT="0"
-KEYWORDS="amd64 ~x86"
+KEYWORDS="~amd64 ~x86"
 REQUIRED_USE="${PYTHON_REQUIRED_USE}"
 
 RDEPEND="${PYTHON_DEPS}

net-misc/seafile/seafile-8.0.9-r1.ebuild
@@ -6,17 +6,19 @@ EAPI=8
 PYTHON_COMPAT=( python3_{9..11} )
 
 # Upstream is moving tags repeatedly, then we use commit hash.
-RELEASE_COMMIT="740763934a097577b92f0446cd73b555ca86b436"
+# (Please file a bug upstream if this happens!)
+#RELEASE_COMMIT="6bd36a957573eda6a53f3d1bc538dbfc001aa9fd"
 
 inherit autotools python-single-r1 vala
 
 DESCRIPTION="File syncing and sharing software with file encryption and group sharing"
 HOMEPAGE="https://www.seafile.com/ https://github.com/haiwen/seafile/"
-SRC_URI="https://github.com/haiwen/${PN}/archive/${RELEASE_COMMIT}.tar.gz -> ${P}.tar.gz"
+#SRC_URI="https://github.com/haiwen/${PN}/archive/${RELEASE_COMMIT}.tar.gz -> ${P}.tar.gz"
+SRC_URI="https://github.com/haiwen/seafile/archive/refs/tags/v${PV}.tar.gz -> ${P}.gh.tar.gz"
 
 LICENSE="GPL-2+-with-openssl-exception"
 SLOT="0"
-KEYWORDS="~amd64 ~x86"
+KEYWORDS="amd64 ~x86"
 REQUIRED_USE="${PYTHON_REQUIRED_USE}"
 
 RDEPEND="${PYTHON_DEPS}

net-misc/seafile/Manifest
@@ -1,6 +1,6 @@
-DIST seafile-8.0.10.tar.gz 741596 BLAKE2B 231ba41db372c966a391d8baa28b45ca717ec550d1e8e6bf1e5b375d28061da5f1d25a7e5adb7b4a00ab8f01c8063ed75cca0e5459c3fa82b246755382186fb3 SHA512 3c527bea0687a893f00734034a1049633dfed34ab0e7586ceb4cb5345b380c622b8b62486f608e3579ab2eaca872c2bbb3d250dea12927b38a0b0cafb34a9120
+DIST seafile-8.0.10.gh.tar.gz 749686 BLAKE2B 76c5f6e71dcf12c6950bfde17e529ea8d24efed78069d9bbc93c78f04167be371884aa069b6665afc21be5114352b7a73e5cf09d6f7f9cea6bad323c956ef254 SHA512 9ef1f01d180fe08452c39bd35683610844ff849fa261d98ac7366fde6af27886e823976f6fa2e82407fb844dcb4de8160658abd807eb1d4932e999a4ad75dd50
 DIST seafile-8.0.5.tar.gz 738268 BLAKE2B a142acd9613b4a4aba62640486ee3e79de13274f068e8db69693d93092123f73150e579a38e2aaffc0144cb03a88c56cdac20ca33c0c9a5d09bb8684f184e193 SHA512 712a5d445f586beb9cbd425298734181f2eef302dc2f1f7634c4182bd113a7818c948475e9981ce32af218fcc635254e4d5fea28d514ad6eafe169c4a41eb6f9
 DIST seafile-8.0.6.tar.gz 738248 BLAKE2B c36904c459429985bf5a6b34546be7af2ba913b4dc5df860858cf73c0d0fe4797fc1fd64a70d8feb3ac2888404e30705540672cdbcc7cea4b91e6229939ed1e4 SHA512 261e1c58018f3e427e79a811f67322a2d0ce4cc8bfa913f029bd42bdfa5498f35ec5003cafc8eaf583b0b0ef9cd7159bc4f57136f4fd4270b38327a79e0f95cd
 DIST seafile-8.0.7.tar.gz 738238 BLAKE2B edb6ab4f1e61455a24ed43c4b516b58f0cd44fd57f63ebb7aca6daaa206888689c9139fd443acfb2fb35ace100921470e207c338ae5bcdd093e02e79ab57ef80 SHA512 4ce16b2abed36ff33237df1119be7ecb3a01caa9d29c954a6618dfbc94d4ba32c0e30bcf259a6f05bf6bfe0e2aaca7d394c6b6adf69c37447e27a9ed5ac0d3df
 DIST seafile-8.0.8.tar.gz 738321 BLAKE2B ec8719890c5945c1dab0433c86e5e2b7ec7dc45ce9edb0b82760a4fffaf719892c1a650d2eb5df463f413df1c09c8062e45f35b13669fbc2c6312e0056590016 SHA512 a531acd582ce1f80918979305a2c822e37e25ad8cd80551359834ddb22434f2de5c63b9a364886bd12fe3aa8307087f85238271dbe9e3258e9e9c65f18ea9307
-DIST seafile-8.0.9.tar.gz 741345 BLAKE2B a19d31e350e2a971f30ea1a659ced0a3611857901f000dcf28e94ffef070ae2ef9e1803cf09f0ce833378378998e2aff869336d7b327d203fcf4143af66832a4 SHA512 47332bc6296cd3a6fe2e0c3539e0ee20df848143c1a05f0d5377a2505924d065bd555f807c79ffffb65a67211062e92b8c4061f8e632ed33e65acd545a3104e8
+DIST seafile-8.0.9.gh.tar.gz 740920 BLAKE2B c62c7ccea536934a92166db5d2acea45b65f5508c1e5d4f70749c922c12d29da11bc2265203c91e87fe0096cc3f4b6ec4f6125b538a4de88725583871b05e67e SHA512 c4621ea4c08fcd9c5d4cf683152f7504c37212272b5a9535da869de846b71a7f5e9a72c5544f9b7487ad8025a6cdf2866c15788828cc27937209d8242fc13cb8

commit 2fb08fe65f209fc798d5e98eef6081501dc41dbb

net-misc/seafile: fix S
Closes: https://bugs.gentoo.org/901733
Fixes: adfaf2ed6d9d1b3c7f15c2f46ce858360c3d53c6
Signed-off-by: Sam James <sam@gentoo.org>

net-misc/seafile/seafile-8.0.10-r1.ebuild
@@ -14,6 +14,7 @@ inherit autotools python-single-r1 vala
 DESCRIPTION="File syncing and sharing software with file encryption and group sharing"
 HOMEPAGE="https://www.seafile.com/ https://github.com/haiwen/seafile/"
 #SRC_URI="https://github.com/haiwen/${PN}/archive/${RELEASE_COMMIT}.tar.gz -> ${P}.tar.gz"
+#S="${WORKDIR}/${PN}-${RELEASE_COMMIT}"
 SRC_URI="https://github.com/haiwen/seafile/archive/refs/tags/v${PV}.tar.gz -> ${P}.gh.tar.gz"
 
 LICENSE="GPL-2+-with-openssl-exception"
@@ -38,8 +39,6 @@ RDEPEND="${PYTHON_DEPS}
 DEPEND="${RDEPEND}"
 BDEPEND="${PYTHON_DEPS}
 	$(vala_depend)"
-
-S="${WORKDIR}/${PN}-${RELEASE_COMMIT}"
 
 pkg_setup() {
 	python-single-r1_pkg_setup

net-misc/seafile/seafile-8.0.9-r1.ebuild
@@ -14,6 +14,7 @@ inherit autotools python-single-r1 vala
 DESCRIPTION="File syncing and sharing software with file encryption and group sharing"
 HOMEPAGE="https://www.seafile.com/ https://github.com/haiwen/seafile/"
 #SRC_URI="https://github.com/haiwen/${PN}/archive/${RELEASE_COMMIT}.tar.gz -> ${P}.tar.gz"
+#S="${WORKDIR}/${PN}-${RELEASE_COMMIT}"
 SRC_URI="https://github.com/haiwen/seafile/archive/refs/tags/v${PV}.tar.gz -> ${P}.gh.tar.gz"
 
 LICENSE="GPL-2+-with-openssl-exception"
@@ -38,8 +39,6 @@ RDEPEND="${PYTHON_DEPS}
 DEPEND="${RDEPEND}"
 BDEPEND="${PYTHON_DEPS}
 	$(vala_depend)"
-
-S="${WORKDIR}/${PN}-${RELEASE_COMMIT}"
 
 pkg_setup() {
 	python-single-r1_pkg_setup