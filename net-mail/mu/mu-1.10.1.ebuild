gentoo/gentoo

commit ef53a79b555cb05b1fe9adfcf337d49fffb0de25

net-mail/mu: add 1.10.1
This fixes a particularly annoying bug Arsen pointed out wrt reply-to behaviour
on mailing lists.

I've left a TODO wrt guile support as we can restore it now given it's
been reworked (and wasn't actually to be entirely removed, the release notes
were confusing).

Signed-off-by: Sam James <sam@gentoo.org>

net-mail/mu/mu-1.10.1.ebuild
@@ -1,0 +1,92 @@
+# Copyright 1999-2023 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit elisp-common meson
+
+DESCRIPTION="Set of tools to deal with Maildirs, in particular, searching and indexing"
+HOMEPAGE="https://www.djcbsoftware.nl/code/mu/ https://github.com/djcb/mu"
+SRC_URI="https://github.com/djcb/mu/releases/download/v${PV}/${P}.tar.xz"
+
+LICENSE="GPL-3+"
+SLOT="0"
+KEYWORDS="~amd64 ~arm ~arm64 ~riscv ~x86 ~x64-macos"
+IUSE="emacs readline"
+
+DEPEND="
+	dev-libs/glib:2
+	dev-libs/gmime:3.0
+	>=dev-libs/xapian-1.4:=
+	emacs? ( >=app-editors/emacs-25.3:* )
+	readline? ( sys-libs/readline:= )"
+RDEPEND="${DEPEND}"
+BDEPEND="
+	sys-apps/texinfo
+	virtual/pkgconfig
+"
+
+SITEFILE="70mu-gentoo-autoload.el"
+
+PATCHES=(
+	"${FILESDIR}"/${PN}-1.8.13-add-cstdint-inc.patch
+)
+
+src_prepare() {
+	default
+
+	# Don't install NEWS.org into /usr/share/doc.
+	sed -i '/NEWS.org/,+1 d' meson.build || die
+	sed -i '/mu4e-about.org/d' mu4e/meson.build || die
+
+	# Don't compress the info file.
+	sed -i '/gzip/d' build-aux/meson-install-info.sh || die
+
+	# Instead, put it in /usr/share/doc/${PF}.
+	sed -i "/MU_DOC_DIR/s/mu/${PF}/" mu4e/meson.build || die
+}
+
+src_configure() {
+	local emesonargs=(
+		$(meson_feature readline)
+		-Demacs="$(usex emacs "${EMACS}" emacs-not-enabled)"
+		# TODO: revisit this, it's not actually deprecated, just been reworked
+		-Dguile=disabled
+	)
+	meson_src_configure
+}
+
+src_install() {
+	meson_src_install
+
+	# Since meson no longer installs NEWS.org, install it with dodoc.
+	# Also, it must be uncompressed so that it can be viewed with
+	# mu4e-info.
+	docompress -x /usr/share/doc/${PF}/NEWS.org
+	dodoc NEWS.org
+
+	# Same as above.
+	docompress -x /usr/share/doc/${PF}/mu4e-about.org
+	dodoc mu4e/mu4e-about.org
+}
+
+pkg_preinst() {
+	if [[ -n ${REPLACING_VERSIONS} ]]; then
+		elog "After upgrading from an old major version, you should"
+		elog "rebuild your mail index."
+	fi
+}
+
+pkg_postinst() {
+	if use emacs; then
+		einfo "To use mu4e you need to configure it in your .emacs file"
+		einfo "See the manual for more information:"
+		einfo "https://www.djcbsoftware.nl/code/mu/mu4e/"
+
+		elisp-site-regen
+	fi
+}
+
+pkg_postrm() {
+	use emacs && elisp-site-regen
+}

net-mail/mu/Manifest
@@ -1,2 +1,3 @@
+DIST mu-1.10.1.tar.xz 569024 BLAKE2B 4be2a8461d146df7a2abd618c669972d5d1c97815ccaf2fec9bb8a10c682003726b3cd4ae1b8c84a5ae25889787f6f4d88537dd967d497a42f3f86ddf0376da6 SHA512 56f6389b85dde2bd2dec1de5edbb81dc87becbf814a42b3c0e431d8e9dc50d36d5b1006e0001f94360d3c8e7435e85913eb66d851b15f5f089becdd0522da58b
 DIST mu-1.8.13.tar.xz 543696 BLAKE2B 7e96040b12630d8dd1320475e8b7e62b7f349767d331237838e2dd092df51c4f94cf8605d1266d26f4d2fa9aa092ba03546ed21968448302c3a12495702e46bb SHA512 c9dd7564070191e251e8e072042c0d2957e9c9c31064aececf24b9dc99a9b51c6bd60138e88bd86f175365775e98ba49e3bdb5bea02f7738b5e1e68efd7e1a7c
 DIST mu-1.8.14.tar.xz 543828 BLAKE2B d64e52a66f3b68a2d5b039eaff668c5ecf0da8b1e77719de49316cbe96bb9d34c3e4f8636cfd63cf473218ea6ec428f8c2be2d80c94d1b1456e4b7f17c5ab52c SHA512 8d5514cf08891180163e4d7caff7a3d75dd39a6397c0c8608a85b613f83a4f714f772400c2a057c1f9ef46dedd4f240828ced4e8a1b38ebe4340041059a3ad44

commit d068c1ac835cd5e55c9ec27eacc485d59ea21a0c

net-mail/mu: drop obsolete patch
I must have tested 1.8.x instead?

Fixes: ef53a79b555cb05b1fe9adfcf337d49fffb0de25
Signed-off-by: Sam James <sam@gentoo.org>

net-mail/mu/mu-1.10.1.ebuild
@@ -28,10 +28,6 @@ BDEPEND="
 
 SITEFILE="70mu-gentoo-autoload.el"
 
-PATCHES=(
-	"${FILESDIR}"/${PN}-1.8.13-add-cstdint-inc.patch
-)
-
 src_prepare() {
 	default
 