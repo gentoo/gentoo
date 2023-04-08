gentoo/gentoo

commit 1e50829273a858f6a0d01490963a33464e12ab76

net-print/hplip: drop 3.22.6, 3.22.10-r1
Signed-off-by: Daniel Pielmeier <billie@gentoo.org>

net-print/hplip/Manifest
@@ -1,4 +1,3 @@
 DIST hplip-3.22.10.tar.gz 29333247 BLAKE2B 484f470cb1ca639f6bbfa380230e94d8d219c2bc65a145e410e0be6c47ac82e7ac177d5d12a1ef9401682da2db6c7eba97424c2b2f9a2435b17f3a9ab4114e83 SHA512 0be9cd48cddebffb2472f36669ba92edbcf9019cb3b561a9630457949a6e6f13eab552582027b02468b4732e376f17321929502ad4d1f6bb4d5ea1262b171ef7
 DIST hplip-3.22.6-patches-1.tar.xz 17948 BLAKE2B ab6f0482dfebba2a1974cf6adb8037846a7e8990f15a74641af4db73d363b02d7c37baf958a8a9e83eebfc8bcccd058959c3cee448a832b26fd8a2097614c4fd SHA512 a6e3921d15ab75386f71a5e4a1aab48a014279cffd5141b152a7c1e985c7685892fd555eeb5578ab5f266ce8763c98d30d4630c3ec69ba1209a9856d6d5b0ead
-DIST hplip-3.22.6.tar.gz 29331408 BLAKE2B 7f2bc09ab2d7950d8e84b4c23d35fd5370ddc51d1d61c674fad0ce1667f8ecf9c25e0b57c155b688d932db583d3c6af528da86348ebe64ce8da9c8d051b2be34 SHA512 cb25c07c767d3d8921468429ef154401a4df9d2fdf87ead3ab18f0d06e1bd3de610843b8131641a1af8d920c7e15e290a0923405bf609cdc0a3fba9df93ddb5e
 DIST hplip-3.23.3.tar.gz 29438944 BLAKE2B c11e3f4114ae65f26cc6d4072f039179035c910175ae24b384b2e1919ddeb5fcc424aa678b54ea18b0b86aa1fd185e6103657e54eae41ad25f93ce190460d078 SHA512 b3c061d59ec4c4b8c2cb5a90a66a706439a75362d19bc7a14b6a8656812334f6de4358cfa8d5ef37e597eaaabf914f9a00f69c5b68e61bc1aac6e71521eee8a8

net-print/hplip/hplip-3.22.6.ebuild
@@ -1,290 +1,0 @@
-# Copyright 1999-2023 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=8
-
-PYTHON_COMPAT=( python3_{9,10} )
-PYTHON_REQ_USE="threads(+),xml(+)"
-
-inherit autotools linux-info python-single-r1 readme.gentoo-r1 udev
-
-DESCRIPTION="HP Linux Imaging and Printing - Print, scan, fax drivers and service tools"
-HOMEPAGE="https://developers.hp.com/hp-linux-imaging-and-printing"
-SRC_URI="mirror://sourceforge/hplip/${P}.tar.gz
-		https://dev.gentoo.org/~billie/distfiles/${PN}-3.22.6-patches-1.tar.xz"
-
-LICENSE="GPL-2"
-SLOT="0"
-KEYWORDS="amd64 arm arm64 ppc ppc64 x86"
-
-IUSE="doc fax +hpcups hpijs kde libnotify libusb0 minimal parport policykit qt5 scanner +snmp static-ppds X"
-
-COMMON_DEPEND="
-	net-print/cups
-	sys-apps/dbus
-	virtual/jpeg:0
-	hpijs? ( net-print/cups-filters[foomatic] )
-	!libusb0? ( virtual/libusb:1 )
-	libusb0? ( virtual/libusb:0 )
-	${PYTHON_DEPS}
-	!minimal? (
-		scanner? (
-			media-gfx/sane-backends
-		)
-		snmp? (
-			dev-libs/openssl:0=
-			net-analyzer/net-snmp:=
-			net-dns/avahi[dbus,python,${PYTHON_SINGLE_USEDEP}]
-		)
-	)
-"
-BDEPEND="
-	virtual/pkgconfig
-"
-DEPEND="
-	${COMMON_DEPEND}
-"
-RDEPEND="
-	${COMMON_DEPEND}
-	app-text/ghostscript-gpl
-	!minimal? (
-		$(python_gen_cond_dep 'dev-python/pygobject:3[${PYTHON_USEDEP}]' 'python3*')
-		kernel_linux? ( virtual/udev )
-		$(python_gen_cond_dep '
-			>=dev-python/dbus-python-1.2.0-r1[${PYTHON_USEDEP}]
-			dev-python/distro[${PYTHON_USEDEP}]
-			fax? ( dev-python/reportlab[${PYTHON_USEDEP}] )
-			qt5? (
-				>=dev-python/PyQt5-5.5.1[dbus,gui,widgets,${PYTHON_USEDEP}]
-				libnotify? ( dev-python/notify2[${PYTHON_USEDEP}] )
-			)
-			scanner? (
-				>=dev-python/reportlab-3.2[${PYTHON_USEDEP}]
-				>=dev-python/pillow-3.1.1[${PYTHON_USEDEP}]
-				X? (
-					|| (
-						kde? ( kde-misc/skanlite )
-						media-gfx/xsane
-						media-gfx/sane-frontends
-					)
-				)
-			)
-		')
-	)
-	policykit? ( sys-auth/polkit )
-"
-
-REQUIRED_USE="${PYTHON_REQUIRED_USE}"
-
-PATCHES=(
-	"${WORKDIR}/patches"
-)
-
-CONFIG_CHECK="~PARPORT ~PPDEV"
-ERROR_PARPORT="Please make sure kernel parallel port support is enabled (PARPORT and PPDEV)."
-
-#DISABLE_AUTOFORMATTING="yes"
-DOC_CONTENTS="
-For more information on setting up your printer please take
-a look at the hplip section of the gentoo printing guide:
-https://wiki.gentoo.org/wiki/Printing
-"
-
-pkg_setup() {
-	python-single-r1_pkg_setup
-
-	use scanner && ! use X && ewarn "You need USE=X for the scanner GUI."
-
-	use parport && linux-info_pkg_setup
-
-	if use minimal ; then
-		ewarn "Installing driver portions only, make sure you know what you are doing."
-		ewarn "Depending on the USE flags set for hpcups or hpijs the appropiate driver"
-		ewarn "is installed. If both USE flags are set hpijs overrides hpcups."
-		ewarn "This also disables fax, network, scanner and gui support!"
-	fi
-
-	if ! use hpcups && ! use hpijs ; then
-		ewarn "Installing neither hpcups (USE=-hpcups) nor hpijs (USE=-hpijs) driver,"
-		ewarn "which is probably not what you want."
-		ewarn "You will almost certainly not be able to print."
-	fi
-}
-
-src_prepare() {
-	default
-
-	python_fix_shebang .
-
-	# Make desktop files follow the specification
-	# Gentoo bug: https://bugs.gentoo.org/show_bug.cgi?id=443680
-	# Upstream bug: https://bugs.launchpad.net/hplip/+bug/1080324
-	sed -i -e '/^Categories=/s/Application;//' \
-		-e '/^Encoding=.*/d' hplip.desktop.in || die
-	sed -i -e '/^Categories=/s/Application;//' \
-		-e '/^Version=.*/d' \
-		-e '/^Comment=.*/d' hplip-systray.desktop.in || die
-
-	# Fix for Gentoo bug https://bugs.gentoo.org/show_bug.cgi?id=345725
-	# Upstream bug: https://bugs.launchpad.net/hplip/+bug/880847,
-	# https://bugs.launchpad.net/hplip/+bug/500086
-	local udevdir=$(get_udevdir)
-	sed -i -e "s|/etc/udev|${udevdir}|g" \
-		$(find . -type f -exec grep -l /etc/udev {} +) || die
-
-	# Force recognition of Gentoo distro by hp-check
-	sed -i \
-		-e "s:file('/etc/issue', 'r').read():'Gentoo':" \
-		installer/core_install.py || die
-
-	eautoreconf
-}
-
-src_configure() {
-	local drv_build minimal_build
-
-	if use hpcups ; then
-		drv_build="$(use_enable hpcups hpcups-install)"
-		if use static-ppds ; then
-			drv_build="${drv_build} --enable-cups-ppd-install"
-			drv_build="${drv_build} --disable-cups-drv-install"
-		else
-			drv_build="${drv_build} --enable-cups-drv-install"
-			drv_build="${drv_build} --disable-cups-ppd-install"
-		fi
-	else
-		drv_build="--disable-hpcups-install"
-		drv_build="${drv_build} --disable-cups-drv-install"
-		drv_build="${drv_build} --disable-cups-ppd-install"
-	fi
-
-	if use hpijs ; then
-		drv_build="${drv_build} $(use_enable hpijs hpijs-install)"
-		if use static-ppds ; then
-			drv_build="${drv_build} --enable-foomatic-ppd-install"
-			drv_build="${drv_build} --disable-foomatic-drv-install"
-		else
-			drv_build="${drv_build} --enable-foomatic-drv-install"
-			drv_build="${drv_build} --disable-foomatic-ppd-install"
-		fi
-	else
-		drv_build="${drv_build} --disable-hpijs-install"
-		drv_build="${drv_build} --disable-foomatic-drv-install"
-		drv_build="${drv_build} --disable-foomatic-ppd-install"
-	fi
-
-	if use minimal ; then
-		if use hpijs ; then
-			minimal_build="--enable-hpijs-only-build"
-		else
-			minimal_build="--disable-hpijs-only-build"
-		fi
-		if use hpcups ; then
-			minimal_build="${minimal_build} --enable-hpcups-only-build"
-		else
-			minimal_build="${minimal_build} --disable-hpcups-only-build"
-		fi
-		minimal_build="${minimal_build} --disable-fax-build"
-		minimal_build="${minimal_build} --disable-network-build"
-		minimal_build="${minimal_build} --disable-scan-build"
-		minimal_build="${minimal_build} --disable-gui-build"
-	else
-		if use fax ; then
-			minimal_build="${minimal_build} --enable-fax-build"
-		else
-			minimal_build="${minimal_build} --disable-fax-build"
-		fi
-		if use snmp ; then
-			minimal_build="${minimal_build} --enable-network-build"
-		else
-			minimal_build="${minimal_build} --disable-network-build"
-		fi
-		if use scanner ; then
-			minimal_build="${minimal_build} --enable-scan-build"
-		else
-			minimal_build="${minimal_build} --disable-scan-build"
-		fi
-		if use qt5 ; then
-			minimal_build="${minimal_build} --enable-qt5"
-			minimal_build="${minimal_build} --enable-gui-build"
-		else
-			minimal_build="${minimal_build} --disable-gui-build"
-			minimal_build="${minimal_build} --disable-qt5"
-		fi
-	fi
-
-	# disable class driver for now
-	econf \
-		--disable-class-driver \
-		--disable-cups11-build \
-		--disable-foomatic-rip-hplip-install \
-		--disable-imageProcessor-build \
-		--disable-lite-build \
-		--disable-shadow-build \
-		--disable-qt3 \
-		--disable-qt4 \
-		--disable-udev_sysfs_rules \
-		--with-cupsbackenddir=$(cups-config --serverbin)/backend \
-		--with-cupsfilterdir=$(cups-config --serverbin)/filter \
-		--with-docdir=/usr/share/doc/${PF} \
-		--with-htmldir=/usr/share/doc/${PF}/html \
-		--enable-hpps-install \
-		--enable-dbus-build \
-		${drv_build} \
-		${minimal_build} \
-		$(use_enable doc doc-build) \
-		$(use_enable libusb0 libusb01_build) \
-		$(use_enable parport pp-build) \
-		$(use_enable policykit)
-
-	# hpijs ppds are created at configure time but are not installed (3.17.11)
-
-	# Use system foomatic-rip for hpijs driver instead of foomatic-rip-hplip
-	# The hpcups driver does not use foomatic-rip
-	#local i
-	#for i in ppd/hpijs/*.ppd.gz ; do
-	#	rm -f ${i}.temp || die
-	#	gunzip -c ${i} | sed 's/foomatic-rip-hplip/foomatic-rip/g' | \
-	#		gzip > ${i}.temp || die
-	#	mv ${i}.temp ${i} || die
-	#done
-}
-
-src_install() {
-	# Disable parallel install
-	# Gentoo Bug: https://bugs.gentoo.org/show_bug.cgi?id=578018
-	emake -j1 DESTDIR="${D}" install
-	einstalldocs
-	# default
-
-	# Installed by sane-backends
-	# Gentoo Bug: https://bugs.gentoo.org/show_bug.cgi?id=201023
-	rm -f "${ED}"/etc/sane.d/dll.conf || die
-
-	# Remove desktop and autostart files
-	# Gentoo Bug: https://bugs.gentoo.org/show_bug.cgi?id=638770
-	use qt5 || {
-		rm -Rf "${ED}"/usr/share/applications "${ED}"/etc/xdg
-	}
-
-	rm -f "${ED}"/usr/share/doc/${PF}/{copyright,README_LIBJPG,COPYING} || die
-	rmdir --ignore-fail-on-non-empty "${ED}"/usr/share/doc/${PF}/ || die
-
-	# Remove hal fdi files
-	rm -rf "${ED}"/usr/share/hal || die
-
-	find "${D}" -name '*.la' -delete || die
-
-	python_optimize "${ED}"/usr/share/hplip
-
-	readme.gentoo_create_doc
-}
-
-pkg_postinst() {
-	udev_reload
-	readme.gentoo_print_elog
-}
-
-pkg_postrm() {
-	udev_reload
-}

net-print/hplip/hplip-3.22.10-r1.ebuild
@@ -1,290 +1,0 @@
-# Copyright 1999-2023 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=8
-
-PYTHON_COMPAT=( python3_{9,10,11} )
-PYTHON_REQ_USE="threads(+),xml(+)"
-
-inherit autotools linux-info python-single-r1 readme.gentoo-r1 udev
-
-DESCRIPTION="HP Linux Imaging and Printing - Print, scan, fax drivers and service tools"
-HOMEPAGE="https://developers.hp.com/hp-linux-imaging-and-printing"
-SRC_URI="mirror://sourceforge/hplip/${P}.tar.gz
-		https://dev.gentoo.org/~billie/distfiles/${PN}-3.22.6-patches-1.tar.xz"
-
-LICENSE="GPL-2"
-SLOT="0"
-KEYWORDS="~amd64 ~arm ~arm64 ~ppc ~ppc64 ~x86"
-
-IUSE="doc fax +hpcups hpijs kde libnotify libusb0 minimal parport policykit qt5 scanner +snmp static-ppds X"
-
-COMMON_DEPEND="
-	net-print/cups
-	sys-apps/dbus
-	media-libs/libjpeg-turbo:0
-	hpijs? ( net-print/cups-filters[foomatic] )
-	!libusb0? ( virtual/libusb:1 )
-	libusb0? ( virtual/libusb:0 )
-	${PYTHON_DEPS}
-	!minimal? (
-		scanner? (
-			media-gfx/sane-backends
-		)
-		snmp? (
-			dev-libs/openssl:0=
-			net-analyzer/net-snmp:=[${PYTHON_SINGLE_USEDEP}]
-			net-dns/avahi[dbus,python,${PYTHON_SINGLE_USEDEP}]
-		)
-	)
-"
-BDEPEND="
-	virtual/pkgconfig
-"
-DEPEND="
-	${COMMON_DEPEND}
-"
-RDEPEND="
-	${COMMON_DEPEND}
-	app-text/ghostscript-gpl
-	!minimal? (
-		$(python_gen_cond_dep 'dev-python/pygobject:3[${PYTHON_USEDEP}]' 'python3*')
-		kernel_linux? ( virtual/udev )
-		$(python_gen_cond_dep '
-			>=dev-python/dbus-python-1.2.0-r1[${PYTHON_USEDEP}]
-			dev-python/distro[${PYTHON_USEDEP}]
-			fax? ( dev-python/reportlab[${PYTHON_USEDEP}] )
-			qt5? (
-				>=dev-python/PyQt5-5.5.1[dbus,gui,widgets,${PYTHON_USEDEP}]
-				libnotify? ( dev-python/notify2[${PYTHON_USEDEP}] )
-			)
-			scanner? (
-				>=dev-python/reportlab-3.2[${PYTHON_USEDEP}]
-				>=dev-python/pillow-3.1.1[${PYTHON_USEDEP}]
-				X? (
-					|| (
-						kde? ( kde-misc/skanlite )
-						media-gfx/xsane
-						media-gfx/sane-frontends
-					)
-				)
-			)
-		')
-	)
-	policykit? ( sys-auth/polkit )
-"
-
-REQUIRED_USE="${PYTHON_REQUIRED_USE}"
-
-PATCHES=(
-	"${WORKDIR}/patches"
-)
-
-CONFIG_CHECK="~PARPORT ~PPDEV"
-ERROR_PARPORT="Please make sure kernel parallel port support is enabled (PARPORT and PPDEV)."
-
-#DISABLE_AUTOFORMATTING="yes"
-DOC_CONTENTS="
-For more information on setting up your printer please take
-a look at the hplip section of the gentoo printing guide:
-https://wiki.gentoo.org/wiki/Printing
-"
-
-pkg_setup() {
-	python-single-r1_pkg_setup
-
-	use scanner && ! use X && ewarn "You need USE=X for the scanner GUI."
-
-	use parport && linux-info_pkg_setup
-
-	if use minimal ; then
-		ewarn "Installing driver portions only, make sure you know what you are doing."
-		ewarn "Depending on the USE flags set for hpcups or hpijs the appropiate driver"
-		ewarn "is installed. If both USE flags are set hpijs overrides hpcups."
-		ewarn "This also disables fax, network, scanner and gui support!"
-	fi
-
-	if ! use hpcups && ! use hpijs ; then
-		ewarn "Installing neither hpcups (USE=-hpcups) nor hpijs (USE=-hpijs) driver,"
-		ewarn "which is probably not what you want."
-		ewarn "You will almost certainly not be able to print."
-	fi
-}
-
-src_prepare() {
-	default
-
-	python_fix_shebang .
-
-	# Make desktop files follow the specification
-	# Gentoo bug: https://bugs.gentoo.org/show_bug.cgi?id=443680
-	# Upstream bug: https://bugs.launchpad.net/hplip/+bug/1080324
-	sed -i -e '/^Categories=/s/Application;//' \
-		-e '/^Encoding=.*/d' hplip.desktop.in || die
-	sed -i -e '/^Categories=/s/Application;//' \
-		-e '/^Version=.*/d' \
-		-e '/^Comment=.*/d' hplip-systray.desktop.in || die
-
-	# Fix for Gentoo bug https://bugs.gentoo.org/show_bug.cgi?id=345725
-	# Upstream bug: https://bugs.launchpad.net/hplip/+bug/880847,
-	# https://bugs.launchpad.net/hplip/+bug/500086
-	local udevdir=$(get_udevdir)
-	sed -i -e "s|/etc/udev|${udevdir}|g" \
-		$(find . -type f -exec grep -l /etc/udev {} +) || die
-
-	# Force recognition of Gentoo distro by hp-check
-	sed -i \
-		-e "s:file('/etc/issue', 'r').read():'Gentoo':" \
-		installer/core_install.py || die
-
-	eautoreconf
-}
-
-src_configure() {
-	local drv_build minimal_build
-
-	if use hpcups ; then
-		drv_build="$(use_enable hpcups hpcups-install)"
-		if use static-ppds ; then
-			drv_build="${drv_build} --enable-cups-ppd-install"
-			drv_build="${drv_build} --disable-cups-drv-install"
-		else
-			drv_build="${drv_build} --enable-cups-drv-install"
-			drv_build="${drv_build} --disable-cups-ppd-install"
-		fi
-	else
-		drv_build="--disable-hpcups-install"
-		drv_build="${drv_build} --disable-cups-drv-install"
-		drv_build="${drv_build} --disable-cups-ppd-install"
-	fi
-
-	if use hpijs ; then
-		drv_build="${drv_build} $(use_enable hpijs hpijs-install)"
-		if use static-ppds ; then
-			drv_build="${drv_build} --enable-foomatic-ppd-install"
-			drv_build="${drv_build} --disable-foomatic-drv-install"
-		else
-			drv_build="${drv_build} --enable-foomatic-drv-install"
-			drv_build="${drv_build} --disable-foomatic-ppd-install"
-		fi
-	else
-		drv_build="${drv_build} --disable-hpijs-install"
-		drv_build="${drv_build} --disable-foomatic-drv-install"
-		drv_build="${drv_build} --disable-foomatic-ppd-install"
-	fi
-
-	if use minimal ; then
-		if use hpijs ; then
-			minimal_build="--enable-hpijs-only-build"
-		else
-			minimal_build="--disable-hpijs-only-build"
-		fi
-		if use hpcups ; then
-			minimal_build="${minimal_build} --enable-hpcups-only-build"
-		else
-			minimal_build="${minimal_build} --disable-hpcups-only-build"
-		fi
-		minimal_build="${minimal_build} --disable-fax-build"
-		minimal_build="${minimal_build} --disable-network-build"
-		minimal_build="${minimal_build} --disable-scan-build"
-		minimal_build="${minimal_build} --disable-gui-build"
-	else
-		if use fax ; then
-			minimal_build="${minimal_build} --enable-fax-build"
-		else
-			minimal_build="${minimal_build} --disable-fax-build"
-		fi
-		if use snmp ; then
-			minimal_build="${minimal_build} --enable-network-build"
-		else
-			minimal_build="${minimal_build} --disable-network-build"
-		fi
-		if use scanner ; then
-			minimal_build="${minimal_build} --enable-scan-build"
-		else
-			minimal_build="${minimal_build} --disable-scan-build"
-		fi
-		if use qt5 ; then
-			minimal_build="${minimal_build} --enable-qt5"
-			minimal_build="${minimal_build} --enable-gui-build"
-		else
-			minimal_build="${minimal_build} --disable-gui-build"
-			minimal_build="${minimal_build} --disable-qt5"
-		fi
-	fi
-
-	# disable class driver for now
-	econf \
-		--disable-class-driver \
-		--disable-cups11-build \
-		--disable-foomatic-rip-hplip-install \
-		--disable-imageProcessor-build \
-		--disable-lite-build \
-		--disable-shadow-build \
-		--disable-qt3 \
-		--disable-qt4 \
-		--disable-udev_sysfs_rules \
-		--with-cupsbackenddir=$(cups-config --serverbin)/backend \
-		--with-cupsfilterdir=$(cups-config --serverbin)/filter \
-		--with-docdir=/usr/share/doc/${PF} \
-		--with-htmldir=/usr/share/doc/${PF}/html \
-		--enable-hpps-install \
-		--enable-dbus-build \
-		${drv_build} \
-		${minimal_build} \
-		$(use_enable doc doc-build) \
-		$(use_enable libusb0 libusb01_build) \
-		$(use_enable parport pp-build) \
-		$(use_enable policykit)
-
-	# hpijs ppds are created at configure time but are not installed (3.17.11)
-
-	# Use system foomatic-rip for hpijs driver instead of foomatic-rip-hplip
-	# The hpcups driver does not use foomatic-rip
-	#local i
-	#for i in ppd/hpijs/*.ppd.gz ; do
-	#	rm -f ${i}.temp || die
-	#	gunzip -c ${i} | sed 's/foomatic-rip-hplip/foomatic-rip/g' | \
-	#		gzip > ${i}.temp || die
-	#	mv ${i}.temp ${i} || die
-	#done
-}
-
-src_install() {
-	# Disable parallel install
-	# Gentoo Bug: https://bugs.gentoo.org/show_bug.cgi?id=578018
-	emake -j1 DESTDIR="${D}" install
-	einstalldocs
-	# default
-
-	# Installed by sane-backends
-	# Gentoo Bug: https://bugs.gentoo.org/show_bug.cgi?id=201023
-	rm -f "${ED}"/etc/sane.d/dll.conf || die
-
-	# Remove desktop and autostart files
-	# Gentoo Bug: https://bugs.gentoo.org/show_bug.cgi?id=638770
-	use qt5 || {
-		rm -Rf "${ED}"/usr/share/applications "${ED}"/etc/xdg
-	}
-
-	rm -f "${ED}"/usr/share/doc/${PF}/{copyright,README_LIBJPG,COPYING} || die
-	rmdir --ignore-fail-on-non-empty "${ED}"/usr/share/doc/${PF}/ || die
-
-	# Remove hal fdi files
-	rm -rf "${ED}"/usr/share/hal || die
-
-	find "${D}" -name '*.la' -delete || die
-
-	python_optimize "${ED}"/usr/share/hplip
-
-	readme.gentoo_create_doc
-}
-
-pkg_postinst() {
-	udev_reload
-	readme.gentoo_print_elog
-}
-
-pkg_postrm() {
-	udev_reload
-}

commit 835be149e0a5756ca1c0422917b0a98a4766613d

net-print/hplip: [QA] Restore 3.22.6
The previous commit broke stable dependencies for
net-print/hplip-plugin-3.22.6, which depends on ~net-print/hplip-${PV}.

Fixes: 1e50829273a858f6a0d01490963a33464e12ab76
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

net-print/hplip/Manifest
@@ -1,3 +1,4 @@
 DIST hplip-3.22.10.tar.gz 29333247 BLAKE2B 484f470cb1ca639f6bbfa380230e94d8d219c2bc65a145e410e0be6c47ac82e7ac177d5d12a1ef9401682da2db6c7eba97424c2b2f9a2435b17f3a9ab4114e83 SHA512 0be9cd48cddebffb2472f36669ba92edbcf9019cb3b561a9630457949a6e6f13eab552582027b02468b4732e376f17321929502ad4d1f6bb4d5ea1262b171ef7
 DIST hplip-3.22.6-patches-1.tar.xz 17948 BLAKE2B ab6f0482dfebba2a1974cf6adb8037846a7e8990f15a74641af4db73d363b02d7c37baf958a8a9e83eebfc8bcccd058959c3cee448a832b26fd8a2097614c4fd SHA512 a6e3921d15ab75386f71a5e4a1aab48a014279cffd5141b152a7c1e985c7685892fd555eeb5578ab5f266ce8763c98d30d4630c3ec69ba1209a9856d6d5b0ead
+DIST hplip-3.22.6.tar.gz 29331408 BLAKE2B 7f2bc09ab2d7950d8e84b4c23d35fd5370ddc51d1d61c674fad0ce1667f8ecf9c25e0b57c155b688d932db583d3c6af528da86348ebe64ce8da9c8d051b2be34 SHA512 cb25c07c767d3d8921468429ef154401a4df9d2fdf87ead3ab18f0d06e1bd3de610843b8131641a1af8d920c7e15e290a0923405bf609cdc0a3fba9df93ddb5e
 DIST hplip-3.23.3.tar.gz 29438944 BLAKE2B c11e3f4114ae65f26cc6d4072f039179035c910175ae24b384b2e1919ddeb5fcc424aa678b54ea18b0b86aa1fd185e6103657e54eae41ad25f93ce190460d078 SHA512 b3c061d59ec4c4b8c2cb5a90a66a706439a75362d19bc7a14b6a8656812334f6de4358cfa8d5ef37e597eaaabf914f9a00f69c5b68e61bc1aac6e71521eee8a8

net-print/hplip/hplip-3.22.6.ebuild
@@ -1,0 +1,290 @@
+# Copyright 1999-2023 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+PYTHON_COMPAT=( python3_{9,10} )
+PYTHON_REQ_USE="threads(+),xml(+)"
+
+inherit autotools linux-info python-single-r1 readme.gentoo-r1 udev
+
+DESCRIPTION="HP Linux Imaging and Printing - Print, scan, fax drivers and service tools"
+HOMEPAGE="https://developers.hp.com/hp-linux-imaging-and-printing"
+SRC_URI="mirror://sourceforge/hplip/${P}.tar.gz
+		https://dev.gentoo.org/~billie/distfiles/${PN}-3.22.6-patches-1.tar.xz"
+
+LICENSE="GPL-2"
+SLOT="0"
+KEYWORDS="amd64 arm arm64 ppc ppc64 x86"
+
+IUSE="doc fax +hpcups hpijs kde libnotify libusb0 minimal parport policykit qt5 scanner +snmp static-ppds X"
+
+COMMON_DEPEND="
+	net-print/cups
+	sys-apps/dbus
+	virtual/jpeg:0
+	hpijs? ( net-print/cups-filters[foomatic] )
+	!libusb0? ( virtual/libusb:1 )
+	libusb0? ( virtual/libusb:0 )
+	${PYTHON_DEPS}
+	!minimal? (
+		scanner? (
+			media-gfx/sane-backends
+		)
+		snmp? (
+			dev-libs/openssl:0=
+			net-analyzer/net-snmp:=
+			net-dns/avahi[dbus,python,${PYTHON_SINGLE_USEDEP}]
+		)
+	)
+"
+BDEPEND="
+	virtual/pkgconfig
+"
+DEPEND="
+	${COMMON_DEPEND}
+"
+RDEPEND="
+	${COMMON_DEPEND}
+	app-text/ghostscript-gpl
+	!minimal? (
+		$(python_gen_cond_dep 'dev-python/pygobject:3[${PYTHON_USEDEP}]' 'python3*')
+		kernel_linux? ( virtual/udev )
+		$(python_gen_cond_dep '
+			>=dev-python/dbus-python-1.2.0-r1[${PYTHON_USEDEP}]
+			dev-python/distro[${PYTHON_USEDEP}]
+			fax? ( dev-python/reportlab[${PYTHON_USEDEP}] )
+			qt5? (
+				>=dev-python/PyQt5-5.5.1[dbus,gui,widgets,${PYTHON_USEDEP}]
+				libnotify? ( dev-python/notify2[${PYTHON_USEDEP}] )
+			)
+			scanner? (
+				>=dev-python/reportlab-3.2[${PYTHON_USEDEP}]
+				>=dev-python/pillow-3.1.1[${PYTHON_USEDEP}]
+				X? (
+					|| (
+						kde? ( kde-misc/skanlite )
+						media-gfx/xsane
+						media-gfx/sane-frontends
+					)
+				)
+			)
+		')
+	)
+	policykit? ( sys-auth/polkit )
+"
+
+REQUIRED_USE="${PYTHON_REQUIRED_USE}"
+
+PATCHES=(
+	"${WORKDIR}/patches"
+)
+
+CONFIG_CHECK="~PARPORT ~PPDEV"
+ERROR_PARPORT="Please make sure kernel parallel port support is enabled (PARPORT and PPDEV)."
+
+#DISABLE_AUTOFORMATTING="yes"
+DOC_CONTENTS="
+For more information on setting up your printer please take
+a look at the hplip section of the gentoo printing guide:
+https://wiki.gentoo.org/wiki/Printing
+"
+
+pkg_setup() {
+	python-single-r1_pkg_setup
+
+	use scanner && ! use X && ewarn "You need USE=X for the scanner GUI."
+
+	use parport && linux-info_pkg_setup
+
+	if use minimal ; then
+		ewarn "Installing driver portions only, make sure you know what you are doing."
+		ewarn "Depending on the USE flags set for hpcups or hpijs the appropiate driver"
+		ewarn "is installed. If both USE flags are set hpijs overrides hpcups."
+		ewarn "This also disables fax, network, scanner and gui support!"
+	fi
+
+	if ! use hpcups && ! use hpijs ; then
+		ewarn "Installing neither hpcups (USE=-hpcups) nor hpijs (USE=-hpijs) driver,"
+		ewarn "which is probably not what you want."
+		ewarn "You will almost certainly not be able to print."
+	fi
+}
+
+src_prepare() {
+	default
+
+	python_fix_shebang .
+
+	# Make desktop files follow the specification
+	# Gentoo bug: https://bugs.gentoo.org/show_bug.cgi?id=443680
+	# Upstream bug: https://bugs.launchpad.net/hplip/+bug/1080324
+	sed -i -e '/^Categories=/s/Application;//' \
+		-e '/^Encoding=.*/d' hplip.desktop.in || die
+	sed -i -e '/^Categories=/s/Application;//' \
+		-e '/^Version=.*/d' \
+		-e '/^Comment=.*/d' hplip-systray.desktop.in || die
+
+	# Fix for Gentoo bug https://bugs.gentoo.org/show_bug.cgi?id=345725
+	# Upstream bug: https://bugs.launchpad.net/hplip/+bug/880847,
+	# https://bugs.launchpad.net/hplip/+bug/500086
+	local udevdir=$(get_udevdir)
+	sed -i -e "s|/etc/udev|${udevdir}|g" \
+		$(find . -type f -exec grep -l /etc/udev {} +) || die
+
+	# Force recognition of Gentoo distro by hp-check
+	sed -i \
+		-e "s:file('/etc/issue', 'r').read():'Gentoo':" \
+		installer/core_install.py || die
+
+	eautoreconf
+}
+
+src_configure() {
+	local drv_build minimal_build
+
+	if use hpcups ; then
+		drv_build="$(use_enable hpcups hpcups-install)"
+		if use static-ppds ; then
+			drv_build="${drv_build} --enable-cups-ppd-install"
+			drv_build="${drv_build} --disable-cups-drv-install"
+		else
+			drv_build="${drv_build} --enable-cups-drv-install"
+			drv_build="${drv_build} --disable-cups-ppd-install"
+		fi
+	else
+		drv_build="--disable-hpcups-install"
+		drv_build="${drv_build} --disable-cups-drv-install"
+		drv_build="${drv_build} --disable-cups-ppd-install"
+	fi
+
+	if use hpijs ; then
+		drv_build="${drv_build} $(use_enable hpijs hpijs-install)"
+		if use static-ppds ; then
+			drv_build="${drv_build} --enable-foomatic-ppd-install"
+			drv_build="${drv_build} --disable-foomatic-drv-install"
+		else
+			drv_build="${drv_build} --enable-foomatic-drv-install"
+			drv_build="${drv_build} --disable-foomatic-ppd-install"
+		fi
+	else
+		drv_build="${drv_build} --disable-hpijs-install"
+		drv_build="${drv_build} --disable-foomatic-drv-install"
+		drv_build="${drv_build} --disable-foomatic-ppd-install"
+	fi
+
+	if use minimal ; then
+		if use hpijs ; then
+			minimal_build="--enable-hpijs-only-build"
+		else
+			minimal_build="--disable-hpijs-only-build"
+		fi
+		if use hpcups ; then
+			minimal_build="${minimal_build} --enable-hpcups-only-build"
+		else
+			minimal_build="${minimal_build} --disable-hpcups-only-build"
+		fi
+		minimal_build="${minimal_build} --disable-fax-build"
+		minimal_build="${minimal_build} --disable-network-build"
+		minimal_build="${minimal_build} --disable-scan-build"
+		minimal_build="${minimal_build} --disable-gui-build"
+	else
+		if use fax ; then
+			minimal_build="${minimal_build} --enable-fax-build"
+		else
+			minimal_build="${minimal_build} --disable-fax-build"
+		fi
+		if use snmp ; then
+			minimal_build="${minimal_build} --enable-network-build"
+		else
+			minimal_build="${minimal_build} --disable-network-build"
+		fi
+		if use scanner ; then
+			minimal_build="${minimal_build} --enable-scan-build"
+		else
+			minimal_build="${minimal_build} --disable-scan-build"
+		fi
+		if use qt5 ; then
+			minimal_build="${minimal_build} --enable-qt5"
+			minimal_build="${minimal_build} --enable-gui-build"
+		else
+			minimal_build="${minimal_build} --disable-gui-build"
+			minimal_build="${minimal_build} --disable-qt5"
+		fi
+	fi
+
+	# disable class driver for now
+	econf \
+		--disable-class-driver \
+		--disable-cups11-build \
+		--disable-foomatic-rip-hplip-install \
+		--disable-imageProcessor-build \
+		--disable-lite-build \
+		--disable-shadow-build \
+		--disable-qt3 \
+		--disable-qt4 \
+		--disable-udev_sysfs_rules \
+		--with-cupsbackenddir=$(cups-config --serverbin)/backend \
+		--with-cupsfilterdir=$(cups-config --serverbin)/filter \
+		--with-docdir=/usr/share/doc/${PF} \
+		--with-htmldir=/usr/share/doc/${PF}/html \
+		--enable-hpps-install \
+		--enable-dbus-build \
+		${drv_build} \
+		${minimal_build} \
+		$(use_enable doc doc-build) \
+		$(use_enable libusb0 libusb01_build) \
+		$(use_enable parport pp-build) \
+		$(use_enable policykit)
+
+	# hpijs ppds are created at configure time but are not installed (3.17.11)
+
+	# Use system foomatic-rip for hpijs driver instead of foomatic-rip-hplip
+	# The hpcups driver does not use foomatic-rip
+	#local i
+	#for i in ppd/hpijs/*.ppd.gz ; do
+	#	rm -f ${i}.temp || die
+	#	gunzip -c ${i} | sed 's/foomatic-rip-hplip/foomatic-rip/g' | \
+	#		gzip > ${i}.temp || die
+	#	mv ${i}.temp ${i} || die
+	#done
+}
+
+src_install() {
+	# Disable parallel install
+	# Gentoo Bug: https://bugs.gentoo.org/show_bug.cgi?id=578018
+	emake -j1 DESTDIR="${D}" install
+	einstalldocs
+	# default
+
+	# Installed by sane-backends
+	# Gentoo Bug: https://bugs.gentoo.org/show_bug.cgi?id=201023
+	rm -f "${ED}"/etc/sane.d/dll.conf || die
+
+	# Remove desktop and autostart files
+	# Gentoo Bug: https://bugs.gentoo.org/show_bug.cgi?id=638770
+	use qt5 || {
+		rm -Rf "${ED}"/usr/share/applications "${ED}"/etc/xdg
+	}
+
+	rm -f "${ED}"/usr/share/doc/${PF}/{copyright,README_LIBJPG,COPYING} || die
+	rmdir --ignore-fail-on-non-empty "${ED}"/usr/share/doc/${PF}/ || die
+
+	# Remove hal fdi files
+	rm -rf "${ED}"/usr/share/hal || die
+
+	find "${D}" -name '*.la' -delete || die
+
+	python_optimize "${ED}"/usr/share/hplip
+
+	readme.gentoo_create_doc
+}
+
+pkg_postinst() {
+	udev_reload
+	readme.gentoo_print_elog
+}
+
+pkg_postrm() {
+	udev_reload
+}