gentoo/gentoo

commit a01d23b808edb51caeafc1518cabb694b312824b

dev-ruby/http: add 5.1.1
Signed-off-by: Sam James <sam@gentoo.org>

dev-ruby/http/http-5.1.1.ebuild
@@ -1,0 +1,61 @@
+# Copyright 1999-2023 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+USE_RUBY="ruby27 ruby30 ruby31"
+
+RUBY_FAKEGEM_RECIPE_TEST="rspec3"
+RUBY_FAKEGEM_EXTRADOC="CHANGES.md README.md"
+RUBY_FAKEGEM_GEMSPEC="${PN}.gemspec"
+inherit ruby-fakegem
+
+DESCRIPTION="An easy-to-use client library for making requests from Ruby"
+HOMEPAGE="https://github.com/tarcieri/http"
+
+LICENSE="MIT"
+SLOT="$(ver_cut 1)"
+KEYWORDS="~amd64 ~arm ~arm64 ~hppa ~ppc ~ppc64 ~sparc ~x86"
+IUSE=""
+
+ruby_add_rdepend "=dev-ruby/addressable-2*
+	>=dev-ruby/addressable-2.8
+	=dev-ruby/http-cookie-1*
+	>=dev-ruby/http-form_data-2.2:2
+	>=dev-ruby/llhttp-ffi-0.4.0:0/0.4"
+
+ruby_add_bdepend "
+	test? (
+		=dev-ruby/certificate_authority-1*
+		dev-ruby/rspec-its
+		dev-ruby/webrick
+	)"
+
+all_ruby_prepare() {
+	# Avoid specs that require network access
+	sed -i -e '/.persistent/,/^  end/ s:^:#:' \
+		spec/lib/http_spec.rb || die
+	sed -i -e '/with non-ASCII URLs/,/^    end/ s:^:#:' \
+		spec/lib/http/client_spec.rb || die
+
+	# Avoid spec that may fail with a running web server
+	sed -i -e '/unifies socket errors into HTTP::ConnectionError/,/^  end/ s:^:#:' spec/lib/http_spec.rb || die
+
+	# Fix spec for production release
+	sed -i -e '/User-Agent:/ s/.dev//' spec/lib/http/features/logging_spec.rb || die
+
+	# Avoid specs also failing upstream due to some certificate issue
+	sed -i -e '/context "ssl"/,/^      end/ s:^:#:' spec/lib/http_spec.rb || die
+	sed -i -e '/describe "working with SSL"/,/^  end/ s:^:#:' spec/lib/http/client_spec.rb || die
+
+	# Disable coverage
+	sed -i -e 's/require_relative ".\/support\/simplecov"//g' "spec/spec_helper.rb" || die
+
+	# Submitted upstream as https://github.com/httprb/http/pull/719
+	sed -i -e '4i require "cgi"' "spec/lib/http/client_spec.rb" || die
+}
+
+each_ruby_test() {
+	# disables dev-ruby/fuubar dep
+	CI=1 each_fakegem_test
+}

dev-ruby/http/Manifest
@@ -1,2 +1,3 @@
 DIST http-4.4.1.gem 75776 BLAKE2B ab34a6a567842d52f9b7cbe3fc3fb8e4757f5cf1303deacdf2f2130a44e82fde64f0db561c3f8fc28100faea277a9c4a9352f95e6cb1359730b34e83a7b36d72 SHA512 6e43a8ae379d7c8a807a9a03c87bfb5ad1719f9838e26ac7a695220ee2bc50344accc2db268d30da175328fd5468b87ec8532d17ce42a5b74d6c2c4c281d1bc9
 DIST http-5.1.0.gem 81920 BLAKE2B cc80149b89eabd5c6b468ffe28b33e17845bd996fa3bf8b9c38c517fe79994412b376b3bac365e8debbeb272cf4333aebd398d1955a358faa27ba9f124671443 SHA512 b99da5318da54e7b64abd48df0b68cde9a02e1ae92b51fc43302e3dea28ba9672e2b7a25e31c342235835e16b2e1b98c94b6d4efa7916a0aa99258fc8290260f
+DIST http-5.1.1.gem 82944 BLAKE2B bd44ede401712ddd13b76f9e3266622aefd78f37a89c84217c7f8206214e9e2ee994fe375a6e7897315dd83853d4e8ab2c0c9c34a59fe47c1a0617f3c126fdab SHA512 a39d88297b6f2f0a51a22fe502d1f7883d44a3fa0af250e1a8142f093fb3865b02b797bc46780d9129f0c67e18f97cc551aa1dbd794b3b4cda6bdb5495fe95e3

commit 870ac53fd238d77c5ec47125dd63f536f7f24581

dev-ruby/http: drop obsolete sed
Fixes: a01d23b808edb51caeafc1518cabb694b312824b
Signed-off-by: Sam James <sam@gentoo.org>

dev-ruby/http/http-5.1.1.ebuild
@@ -50,9 +50,6 @@ all_ruby_prepare() {
 
 	# Disable coverage
 	sed -i -e 's/require_relative ".\/support\/simplecov"//g' "spec/spec_helper.rb" || die
-
-	# Submitted upstream as https://github.com/httprb/http/pull/719
-	sed -i -e '4i require "cgi"' "spec/lib/http/client_spec.rb" || die
 }
 
 each_ruby_test() {