423aa2c1b2a1e173c769cf847bb11ff112864f1e
Arsen Arsenović <arsen@gentoo.org>
2023-04-18 01:44:34 +0200

    app-emacs/jinx: new package, add 0.6
    Signed-off-by: Arsen Arsenović <arsen@gentoo.org>

---
+++ app-emacs/jinx/files/50jinx-gentoo.el
@@ -0,0 +1,4 @@
+;;; app-emacs/jinx site configuration  -*- lexical-binding: t; -*-
+(add-to-list 'load-path "@SITELISP@")
+(add-to-list 'load-path "@EMACSMODULES@")
+(load "@SITELISP@/jinx-autoload.el" nil t)
