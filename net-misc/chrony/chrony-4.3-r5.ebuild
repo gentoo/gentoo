gentoo/gentoo

commit c2d7111508fbb75291df76216d88c4a173369d5b

net-misc/chrony: add missing libtomcrypt dep
Closes: https://bugs.gentoo.org/904911
Signed-off-by: Sam James <sam@gentoo.org>

net-misc/chrony/chrony-4.3-r5.ebuild
@@ -19,7 +19,7 @@ else
 	SRC_URI+=" verify-sig? ( https://download.tuxfamily.org/chrony/${P/_/-}-tar-gz-asc.txt -> ${P/_/-}.tar.gz.asc )"
 
 	if [[ ${PV} != *_pre* ]] ; then
-		KEYWORDS="~alpha amd64 arm arm64 hppa ~m68k ~mips ppc ppc64 ~riscv sparc x86"
+		KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~m68k ~mips ~ppc ~ppc64 ~riscv ~sparc ~x86"
 	fi
 fi
 
@@ -44,6 +44,7 @@ DEPEND="
 		acct-user/ntp
 		sys-libs/libcap
 	)
+	libtomcrypt? ( dev-libs/libtomcrypt:= )
 	nettle? ( dev-libs/nettle:= )
 	nss? ( dev-libs/nss:= )
 	nts? ( net-libs/gnutls:= )

net-misc/chrony/chrony-9999.ebuild
@@ -44,6 +44,7 @@ DEPEND="
 		acct-user/ntp
 		sys-libs/libcap
 	)
+	libtomcrypt? ( dev-libs/libtomcrypt:= )
 	nettle? ( dev-libs/nettle:= )
 	nss? ( dev-libs/nss:= )
 	nts? ( net-libs/gnutls:= )

commit 385f50ef08285f06963e2a82494790505c2ab6fa

net-misc/chrony: stabilize 4.3-r5
Dropped earlier when syncing w/ live ebuild. Thanks to William for noticing.

Fixes: c2d7111508fbb75291df76216d88c4a173369d5b
Signed-off-by: Sam James <sam@gentoo.org>

net-misc/chrony/chrony-4.3-r5.ebuild
@@ -19,7 +19,7 @@ else
 	SRC_URI+=" verify-sig? ( https://download.tuxfamily.org/chrony/${P/_/-}-tar-gz-asc.txt -> ${P/_/-}.tar.gz.asc )"
 
 	if [[ ${PV} != *_pre* ]] ; then
-		KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~m68k ~mips ~ppc ~ppc64 ~riscv ~sparc ~x86"
+		KEYWORDS="~alpha amd64 arm arm64 hppa ~m68k ~mips ppc ppc64 ~riscv sparc x86"
 	fi
 fi
 