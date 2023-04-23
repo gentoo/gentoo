gentoo/gentoo

commit 4d55cae162bfb3da0982dd859a6f4cb56716ad12

app-eselect/eselect-lib-bin-symlink: keyword ~arm64-macos
Bug: https://bugs.gentoo.org/904474
Signed-off-by: Sam James <sam@gentoo.org>

app-eselect/eselect-lib-bin-symlink/eselect-lib-bin-symlink-9999.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2019 Gentoo Authors
+# Copyright 1999-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
@@ -11,7 +11,7 @@ EGIT_REPO_URI="https://github.com/mgorny/eselect-lib-bin-symlink.git"
 
 LICENSE="GPL-2"
 SLOT="0"
-KEYWORDS=""
+KEYWORDS="~arm64-macos"
 IUSE=""
 
 RDEPEND="app-admin/eselect"

app-eselect/eselect-lib-bin-symlink/eselect-lib-bin-symlink-0.1.1-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2022 Gentoo Authors
+# Copyright 1999-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
@@ -9,7 +9,7 @@ SRC_URI="https://github.com/mgorny/eselect-lib-bin-symlink/releases/download/${P
 
 LICENSE="GPL-2"
 SLOT="0"
-KEYWORDS="~alpha amd64 arm arm64 hppa ~ia64 ~loong ~m68k ~mips ppc ppc64 ~riscv ~s390 sparc x86 ~x64-cygwin ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~sparc-solaris ~sparc64-solaris ~x64-solaris ~x86-solaris"
+KEYWORDS="~alpha amd64 arm arm64 hppa ~ia64 ~loong ~m68k ~mips ppc ppc64 ~riscv ~s390 sparc x86 ~x64-cygwin ~amd64-linux ~x86-linux ~arm64-macos ~ppc-macos ~x64-macos ~sparc-solaris ~sparc64-solaris ~x64-solaris ~x86-solaris"
 IUSE=""
 
 RDEPEND="app-admin/eselect"

commit d89bbcf64981ef1278b8df81aae55d705df1eccb

app-eselect/eselect-lib-bin-symlink: unkeyword 9999 for ~arm64-macos
Bug: https://bugs.gentoo.org/904474
Fixes: 4d55cae162bfb3da0982dd859a6f4cb56716ad12
Signed-off-by: Sam James <sam@gentoo.org>

app-eselect/eselect-lib-bin-symlink/eselect-lib-bin-symlink-9999.ebuild
@@ -11,7 +11,7 @@ EGIT_REPO_URI="https://github.com/mgorny/eselect-lib-bin-symlink.git"
 
 LICENSE="GPL-2"
 SLOT="0"
-KEYWORDS="~arm64-macos"
+KEYWORDS=""
 IUSE=""
 
 RDEPEND="app-admin/eselect"