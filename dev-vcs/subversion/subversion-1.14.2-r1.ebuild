gentoo/gentoo

commit 03a57709708195c17c54966634cdfd51acc905b7

dev-vcs/subversion: tweak ruby detection logic
- Prefer newer/later ruby versions (don't bail out early once we find one)
- Check for virtual/rubygems too for each ruby

See https://forums.gentoo.org/viewtopic-t-1163029.html.

Signed-off-by: Sam James <sam@gentoo.org>

dev-vcs/subversion/subversion-1.14.2-r1.ebuild
@@ -134,12 +134,12 @@ pkg_setup() {
 		local rbslot
 		RB_VER=""
 		for rbslot in $(sed 's@\([[:digit:]]\+\)\([[:digit:]]\)@\1.\2@g' <<< ${USE_RUBY//ruby}) ; do
-			if has_version dev-lang/ruby:${rbslot} ;  then
+			# No break here as we want to pick the best (latest)
+			if has_version "dev-lang/ruby:${rbslot}" && has_version "virtual/rubygems[ruby_targets_ruby${rbslot}(-)]" ; then
 				RB_VER="${rbslot/.}"
-				break
 			fi
 		done
-		[[ -z "${RB_VER}" ]] && die "No useable ruby version found"
+		[[ -z "${RB_VER}" ]] && die "No usable ruby version found"
 	fi
 }
 

commit 24a9db84673db6e5a91956b5c7d6c280832c8877

dev-vcs/subversion: fix rubygems USE
Fixes: 03a57709708195c17c54966634cdfd51acc905b7
Closes: https://bugs.gentoo.org/905006
Signed-off-by: Sam James <sam@gentoo.org>

dev-vcs/subversion/subversion-1.14.2-r1.ebuild
@@ -135,7 +135,7 @@ pkg_setup() {
 		RB_VER=""
 		for rbslot in $(sed 's@\([[:digit:]]\+\)\([[:digit:]]\)@\1.\2@g' <<< ${USE_RUBY//ruby}) ; do
 			# No break here as we want to pick the best (latest)
-			if has_version "dev-lang/ruby:${rbslot}" && has_version "virtual/rubygems[ruby_targets_ruby${rbslot}(-)]" ; then
+			if has_version "dev-lang/ruby:${rbslot}" && has_version "virtual/rubygems[ruby_targets_ruby${rbslot/.}(-)]" ; then
 				RB_VER="${rbslot/.}"
 			fi
 		done