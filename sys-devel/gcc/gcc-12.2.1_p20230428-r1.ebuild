gentoo/gentoo

commit ae7c8d24243939e59063bad6972c7b07dd3bfe02

sys-devel/gcc: add 12.2.1_p20230428
Keyword it despite the fact 11.4.0 will be here soonish given it's long-shadowed
in ~arch so most people are only building the stable ones anyway, and some testing
is good.

Plus there's a miscompilation fix in this (https://gcc.gnu.org/bugzilla/show_bug.cgi?id=109585).

There's also a fix for the annoying -Wstringop-overflow false positive
(https://gcc.gnu.org/bugzilla/show_bug.cgi?id=107087).

Signed-off-by: Sam James <sam@gentoo.org>

sys-devel/gcc/Manifest
@@ -15,10 +15,13 @@ DIST gcc-11.4.0-patches-9.tar.xz 14704 BLAKE2B 9aa2ecbac493d3040694e9930738e57d5
 DIST gcc-12-20230121.tar.xz 79652428 BLAKE2B f4f0e6474d179454dc56dd05c823fca919a894e9efdc64625bb13045c7bd549a7bab59dfb4052d45d594d4484801ab5b18cf3f3d6277be54eaec24a53abd439c SHA512 b6c2486916418a64fab64c3655329bc18ca93ee4eca240e8779bd6d8280124fcd07b1aa8eff979fd317656646ecdba9353107887338354d8bd2c1f68c1609349
 DIST gcc-12-20230304.tar.xz 79683276 BLAKE2B e02622f1193ba17dd1041eae0c2f594b26806b00cfb269e683cc356d6d405ae8a49e60823ed6f814d2aac820ef01d9004f68d628a853be139fa6a70e8601ba2a SHA512 0c23a8845c8f202b0c0e4aceb3104bf76c0aca9171ba65d292057703d160fc4ce0b0d7c8876cf1bb30f998c4a5a0a8f3c4165dd4d8d316019cff0dc19aa62182
 DIST gcc-12-20230421.tar.xz 79737436 BLAKE2B 2522ecbe91513d4f008b6a3f2acfaf13f925b9f95293dcbcb04499cf69b7c6cd1c5042113a0e545ee2afd78cf99c96c36042150e450ef33ff040c9fa9fb9eefe SHA512 140ff8e4f387284e070b22f22e51f3aa09ada233dc2bca90894f4b0dbd3e9532f266c8606bea6152afed3eedb853548247f133e332e23f7c6bf380fa61b54b96
+DIST gcc-12-20230428.tar.xz 79744932 BLAKE2B f9d0000cdd2f1b0af79f281851b3b0fe0ac50f14468844e1ee596f8ed1ccf33f55c7617464d88df7e4b3435d58fedecf640592e0661fd1be9ba163acd26dae68 SHA512 992f9cd71f9910efea401048540ba2b43360170f76ce992cf356b1ab383240f481e29c5b0ad32fa4efb2bdbb397477506cb83a825268cfbdd2cde7044d43c758
 DIST gcc-12.2.0-musl-patches-7.tar.xz 3652 BLAKE2B 295ea907efea854a3c2447d472bedb24714d78b7b725476b1eac3898d89c0c549da8e69db28911205bb3293f62cf344a90ade0898ecf5fed57ff00fcf8547a9f SHA512 7a7a440bc08de8dcb37c080ea0c27904adaaadaaaafaab24d90becd2d009243dcfc886d8f674ce3ccddb4ebb5ad17f04d841064d9ec2e2e2e96a1afac3002111
 DIST gcc-12.2.0-patches-10.tar.xz 14252 BLAKE2B 99368db982227728a6a9e2b1f694999f041f38db4a3806b6a740c86218735f9b561197bc150423a3caad0b5fd335c95d1aca130a519754423cd44a083e655373 SHA512 f7f0d8185e908926046f579793dba5916dc27392b85bfe50a7e70c213c3ba29479dfdb1b27514765b76bc897c4dbb530680e59c4deca7a245a808ed566120e4c
 DIST gcc-12.2.0-patches-13.tar.xz 13756 BLAKE2B df0388a669458f2991951d2f4027badbff3f802e6a0422478206b5cdda76e70b531a474d2ac42817e9e7f8200e9ac35ca8276af539839b4ebb2e22903ec25a23 SHA512 2970de9d2806d59c5d0f77e66000860c816ebd674489db8114f48f6aa11575644154431480222580b520b9e312647b06f94ab38959a49a8cda818158d6babf1d
 DIST gcc-12.2.0-patches-15.tar.xz 15352 BLAKE2B 36e5a0fa40d1a202da02187bc752f20868375e5e2d95b88b2d5f90625da223286cd006f258d390a004d0fa9d019e04063bb79900590333854b5d668233c7a91c SHA512 150ec959d45c883bf15b9801baf53665b78d57aeb6fe135af6eadad5fff36fb38431e51f95e5e3bf4c43b54e873f16f26d7746bf4c81ac57fff7acbff0db342a
+DIST gcc-12.3.0-musl-patches-1.tar.xz 3572 BLAKE2B c7bf65f7c9ea8023ddaac821ee2b778622fa310ac72a72b2f7032494a8f304eac86217f9204622e6c21aaef9952bece0d09bc126facd4f42b602927909815ab5 SHA512 babc279fea2c1fd4c018815f2f5630214fd46015ce9f365c28af242681d51818eaca30ce298eddcab1eed5ac5f2759e2b47b2335afab9d722b4469a6d4fec326
+DIST gcc-12.3.0-patches-1.tar.xz 13496 BLAKE2B f38cf0235842393bb92b9c399e013fb713c9affeb5138d54f6ed86aa52a429f17f1f85ef35e511e756c1456de70f184918d93cc57e09c42788209756ec8c35b1 SHA512 dfb04a6330b29cd8035e2da36a5674f210b8ab7c71c1d13b8491e7b0961df0868ce8a279dcdb135ae66eee97dfcfdbbe819e7194e86acee3ba15ed38d7e107c8
 DIST gcc-13.1.0.tar.xz 87451196 BLAKE2B b956a773cffe8b43bd6c9a99fe614f53f665438925a6b320975d301f547923e45d4d6fa577a143467fb03e0a9c1bab2b6719a1011d672367f3e644433a2d7606 SHA512 6cf06dfc48f57f5e67f7efe3248019329a14d690c728d9f2f7ef5fa0d58f1816f309586ba7ea2eac20d0b60a2d1b701f68392e9067dd46f827ba0efd7192db33
 DIST gcc-13.2.0-musl-patches-1.tar.xz 3572 BLAKE2B a5fd1dd6f94aa9f2b35815e193bdecb9c343874e5181761524abf9db6e796bad6b57eb7de9b7aa41b61c370df526f4fc2b56766eb4b2d430b10785e3c287d544 SHA512 d55f0097053ea38ec1e2880059407bf726c5f6282c2abe659646e3af7ac0844c80ffb06e9006f6320e8a19752d90a2b960403239f17f88bdb5ab43fb9d80c1d2
 DIST gcc-13.2.0-patches-1.tar.xz 15792 BLAKE2B 2ffa9ac013c0f9a91eb93e87e3bc6c378d02cca32ad3f8a84d1249e257eb02b206ec10fa425738a4bae2ffb7db8506afcee2a8dd855357e4b48636608aa24e50 SHA512 917ea178f3696724f9ec08aebb6976f03c5edf8ee21bb4bad7e2d820507452f6dace6e847c4f342a52870642550f34da0346ee635a60ba0c1bff667e0e31d195

sys-devel/gcc/gcc-12.2.1_p20230428.ebuild
@@ -4,7 +4,9 @@
 EAPI=8
 
 TOOLCHAIN_PATCH_DEV="sam"
+PATCH_VER="1"
 PATCH_GCC_VER="12.3.0"
+MUSL_VER="1"
 MUSL_GCC_VER="12.3.0"
 
 if [[ $(ver_cut 3) == 9999 ]] ; then
@@ -46,13 +48,6 @@ if [[ ${CATEGORY} != cross-* ]] ; then
 fi
 
 src_prepare() {
-	local p upstreamed_patches=(
-		# add them here
-	)
-	for p in "${upstreamed_patches[@]}"; do
-		rm -v "${WORKDIR}/patch/${p}" || die
-	done
-
 	toolchain_src_prepare
 
 	eapply_user

commit 819e47815178f7cd72adc9f3d661876b8cf84a6e

sys-devel/gcc: really backport the -Wstringop-overflow fix to 12.x
Fixes: ae7c8d24243939e59063bad6972c7b07dd3bfe02
Signed-off-by: Sam James <sam@gentoo.org>

sys-devel/gcc/Manifest
@@ -21,7 +21,7 @@ DIST gcc-12.2.0-patches-10.tar.xz 14252 BLAKE2B 99368db982227728a6a9e2b1f694999f
 DIST gcc-12.2.0-patches-13.tar.xz 13756 BLAKE2B df0388a669458f2991951d2f4027badbff3f802e6a0422478206b5cdda76e70b531a474d2ac42817e9e7f8200e9ac35ca8276af539839b4ebb2e22903ec25a23 SHA512 2970de9d2806d59c5d0f77e66000860c816ebd674489db8114f48f6aa11575644154431480222580b520b9e312647b06f94ab38959a49a8cda818158d6babf1d
 DIST gcc-12.2.0-patches-15.tar.xz 15352 BLAKE2B 36e5a0fa40d1a202da02187bc752f20868375e5e2d95b88b2d5f90625da223286cd006f258d390a004d0fa9d019e04063bb79900590333854b5d668233c7a91c SHA512 150ec959d45c883bf15b9801baf53665b78d57aeb6fe135af6eadad5fff36fb38431e51f95e5e3bf4c43b54e873f16f26d7746bf4c81ac57fff7acbff0db342a
 DIST gcc-12.3.0-musl-patches-1.tar.xz 3572 BLAKE2B c7bf65f7c9ea8023ddaac821ee2b778622fa310ac72a72b2f7032494a8f304eac86217f9204622e6c21aaef9952bece0d09bc126facd4f42b602927909815ab5 SHA512 babc279fea2c1fd4c018815f2f5630214fd46015ce9f365c28af242681d51818eaca30ce298eddcab1eed5ac5f2759e2b47b2335afab9d722b4469a6d4fec326
-DIST gcc-12.3.0-patches-1.tar.xz 13496 BLAKE2B f38cf0235842393bb92b9c399e013fb713c9affeb5138d54f6ed86aa52a429f17f1f85ef35e511e756c1456de70f184918d93cc57e09c42788209756ec8c35b1 SHA512 dfb04a6330b29cd8035e2da36a5674f210b8ab7c71c1d13b8491e7b0961df0868ce8a279dcdb135ae66eee97dfcfdbbe819e7194e86acee3ba15ed38d7e107c8
+DIST gcc-12.3.0-patches-2.tar.xz 14236 BLAKE2B 06c43662c9abb0fa3cf90d5e2273c69aecb9158e6cf6f23c04754ed5d686be2616e81e2502eb16f4298ddd50518ca7677e13249da9e9b091162d3931ff65e4fe SHA512 b0d4b85dbf1a54aaaa1997203b872e3106906b4ad49cde57e1428bc22b2f8e2d73b8cb10ae0d0b792aebaf664594da4d1dea04c44ddc34e59047410766db04d1
 DIST gcc-13.1.0.tar.xz 87451196 BLAKE2B b956a773cffe8b43bd6c9a99fe614f53f665438925a6b320975d301f547923e45d4d6fa577a143467fb03e0a9c1bab2b6719a1011d672367f3e644433a2d7606 SHA512 6cf06dfc48f57f5e67f7efe3248019329a14d690c728d9f2f7ef5fa0d58f1816f309586ba7ea2eac20d0b60a2d1b701f68392e9067dd46f827ba0efd7192db33
 DIST gcc-13.2.0-musl-patches-1.tar.xz 3572 BLAKE2B a5fd1dd6f94aa9f2b35815e193bdecb9c343874e5181761524abf9db6e796bad6b57eb7de9b7aa41b61c370df526f4fc2b56766eb4b2d430b10785e3c287d544 SHA512 d55f0097053ea38ec1e2880059407bf726c5f6282c2abe659646e3af7ac0844c80ffb06e9006f6320e8a19752d90a2b960403239f17f88bdb5ab43fb9d80c1d2
 DIST gcc-13.2.0-patches-1.tar.xz 15792 BLAKE2B 2ffa9ac013c0f9a91eb93e87e3bc6c378d02cca32ad3f8a84d1249e257eb02b206ec10fa425738a4bae2ffb7db8506afcee2a8dd855357e4b48636608aa24e50 SHA512 917ea178f3696724f9ec08aebb6976f03c5edf8ee21bb4bad7e2d820507452f6dace6e847c4f342a52870642550f34da0346ee635a60ba0c1bff667e0e31d195

sys-devel/gcc/gcc-12.2.1_p20230428-r1.ebuild
@@ -4,7 +4,7 @@
 EAPI=8
 
 TOOLCHAIN_PATCH_DEV="sam"
-PATCH_VER="1"
+PATCH_VER="2"
 PATCH_GCC_VER="12.3.0"
 MUSL_VER="1"
 MUSL_GCC_VER="12.3.0"