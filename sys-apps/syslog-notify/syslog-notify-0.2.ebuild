gentoo/gentoo

commit 21a70945a1e83ee49ffea7cfdb3d707b2cc13c49

sys-apps/syslog-notify: Inline mirror://github
Signed-off-by: Michał Górny <mgorny@gentoo.org>

sys-apps/syslog-notify/syslog-notify-0.2.ebuild
@@ -6,7 +6,7 @@ inherit readme.gentoo
 
 DESCRIPTION="Notifications for syslog entries via libnotify"
 HOMEPAGE="https://jtniehof.github.com/syslog-notify/"
-SRC_URI="mirror://github/jtniehof/${PN}/${P}.tar.bz2"
+SRC_URI="https://github.com/downloads/jtniehof/${PN}/${P}.tar.bz2"
 
 LICENSE="GPL-2"
 SLOT="0"

commit 1acbf3db4c9327bf9a5b950379213a2c4e6644ca

sys-apps/syslog-notify: update SRC_URI to drop github.com/downloads
Fixes: 21a70945a1e83ee49ffea7cfdb3d707b2cc13c49
Signed-off-by: Sam James <sam@gentoo.org>

sys-apps/syslog-notify/Manifest
@@ -1,1 +1,1 @@
-DIST syslog-notify-0.2.tar.bz2 77178 BLAKE2B 6b4b723da297c12d26ab1fc169a5360552db5796e82359137de451d34a5914673052f46b066dccdf2ca3de4c1ddc03ecfcc445efcfbff2106f4d77735c8acf16 SHA512 724696d9ecfe34ebea5cd37d122b1151a376fc57d747b4f3bf35ab897a9afddd5565dd174c9c576ba10c8441cd57cdf48141d0dab5b35c5ab27e766d10d27b6b
+DIST syslog-notify-0.2.tar.gz 14191 BLAKE2B f984d0a32c8987c93e7d794eb8809c533bd722899358c47e7fef61541de13575029f4b7f2adfdb407fbf7f194b2e482771f6a6ea77171d0866aedc2deb5c0474 SHA512 e2e444e1c12c994e36fe284929508c1771b84974198a3c4c7e60b4b04812d0982fee55e33f3b43f16bc0cb58ef473b48cf6299ea42a39639eb95bdf51f5698ee

sys-apps/syslog-notify/syslog-notify-0.2.ebuild
@@ -3,11 +3,11 @@
 
 EAPI=7
 
-inherit readme.gentoo-r1
+inherit autotools readme.gentoo-r1
 
 DESCRIPTION="Notifications for syslog entries via libnotify"
 HOMEPAGE="https://jtniehof.github.io/syslog-notify/"
-SRC_URI="https://github.com/downloads/jtniehof/${PN}/${P}.tar.bz2"
+SRC_URI="https://github.com/jtniehof/syslog-notify/archive/refs/tags/v${PV}.tar.gz -> ${P}.tar.gz"
 
 LICENSE="GPL-2"
 SLOT="0"
@@ -20,6 +20,12 @@ RDEPEND="
 		app-admin/syslog-ng
 		app-admin/rsyslog
 	)"
+
+src_prepare() {
+	default
+
+	eautoreconf
+}
 
 src_install() {
 	default