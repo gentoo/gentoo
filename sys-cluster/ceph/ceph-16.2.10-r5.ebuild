gentoo/gentoo

commit 52f4d1ee90e19550b1a7c5dff230b88ce93b6948

sys-cluster/ceph: fix Python dep
Signed-off-by: Sam James <sam@gentoo.org>

sys-cluster/ceph/ceph-16.2.10-r5.ebuild
@@ -93,7 +93,7 @@ DEPEND="
 	spdk? ( dev-util/cunit )
 	sqlite? ( dev-db/sqlite:= )
 	system-boost? ( dev-libs/boost:=[context,python,${PYTHON_USEDEP},zlib] )
-	!system-boost? ( $(python_gen_impl_dep '' 3.8 3.9) )
+	!system-boost? ( $(python_gen_impl_dep '' 3.9 3.10) )
 	uring? ( sys-libs/liburing:= )
 	xfs? ( sys-fs/xfsprogs:= )
 	zbd? ( sys-block/libzbd:= )

sys-cluster/ceph/ceph-16.2.12.ebuild
@@ -93,7 +93,7 @@ DEPEND="
 	spdk? ( dev-util/cunit )
 	sqlite? ( dev-db/sqlite:= )
 	system-boost? ( dev-libs/boost:=[context,python,${PYTHON_USEDEP},zlib] )
-	!system-boost? ( $(python_gen_impl_dep '' 3.8 3.9) )
+	!system-boost? ( $(python_gen_impl_dep '' 3.9 3.10) )
 	uring? ( sys-libs/liburing:= )
 	xfs? ( sys-fs/xfsprogs:= )
 	zbd? ( sys-block/libzbd:= )

commit 34afa8e8efeaca295954dbf844d9ab46f7b9e2e6

sys-cluster/ceph: fix Python dep harder
Fixes: 52f4d1ee90e19550b1a7c5dff230b88ce93b6948
Signed-off-by: Sam James <sam@gentoo.org>

sys-cluster/ceph/ceph-16.2.10-r5.ebuild
@@ -93,7 +93,7 @@ DEPEND="
 	spdk? ( dev-util/cunit )
 	sqlite? ( dev-db/sqlite:= )
 	system-boost? ( dev-libs/boost:=[context,python,${PYTHON_USEDEP},zlib] )
-	!system-boost? ( $(python_gen_impl_dep '' 3.9 3.10) )
+	!system-boost? ( $(python_gen_impl_dep '' 3.10 3.11) )
 	uring? ( sys-libs/liburing:= )
 	xfs? ( sys-fs/xfsprogs:= )
 	zbd? ( sys-block/libzbd:= )
@@ -236,7 +236,7 @@ pkg_pretend() {
 
 pkg_setup() {
 	if ! use system-boost; then
-		python_setup 3.8 3.9
+		python_setup 3.10 3.11
 	else
 		python_setup
 	fi

sys-cluster/ceph/ceph-16.2.12.ebuild
@@ -93,7 +93,7 @@ DEPEND="
 	spdk? ( dev-util/cunit )
 	sqlite? ( dev-db/sqlite:= )
 	system-boost? ( dev-libs/boost:=[context,python,${PYTHON_USEDEP},zlib] )
-	!system-boost? ( $(python_gen_impl_dep '' 3.9 3.10) )
+	!system-boost? ( $(python_gen_impl_dep '' 3.10 3.11) )
 	uring? ( sys-libs/liburing:= )
 	xfs? ( sys-fs/xfsprogs:= )
 	zbd? ( sys-block/libzbd:= )
@@ -234,7 +234,7 @@ pkg_pretend() {
 
 pkg_setup() {
 	if ! use system-boost; then
-		python_setup 3.8 3.9
+		python_setup 3.10 3.11
 	else
 		python_setup
 	fi