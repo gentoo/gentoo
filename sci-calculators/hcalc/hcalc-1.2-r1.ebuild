gentoo/gentoo

commit e39eedce3a06b70bf99aa8e149c392c608e4e889

sci-calculators/hcalc: Inline mirror://github
Signed-off-by: Michał Górny <mgorny@gentoo.org>

sci-calculators/hcalc/hcalc-1.2-r1.ebuild
@@ -7,7 +7,7 @@ inherit cmake-utils
 
 DESCRIPTION="DJ's Hex Calculator"
 HOMEPAGE="http://www.delorie.com/store/hcalc/ https://github.com/jlec/hcalc"
-SRC_URI="mirror://github/jlec/hcalc/${P}.tar.xz"
+SRC_URI="https://github.com/downloads/jlec/hcalc/${P}.tar.xz"
 
 LICENSE="GPL-2"
 SLOT="0"

commit efab9eb991eb7cd871324e0637d776e817615f1d

sci-calculators/hcalc: update SRC_URI to drop github.com/downloads
github.com/downloads is gone. Tarball contents are the same.

Fixes: e39eedce3a06b70bf99aa8e149c392c608e4e889
Signed-off-by: Sam James <sam@gentoo.org>

sci-calculators/hcalc/Manifest
@@ -1,1 +1,1 @@
-DIST hcalc-1.2.tar.xz 15456 BLAKE2B 0423d9e208eb804289f20ebe2011620adc35eb926645934d5a46141ce250831234fb6317ccf532ba02a990789d647ccceb1732b1dd10dd368786204ed68241f3 SHA512 c21dac80d31f7adc5a1e7a02e5a755ac8d68547a9f25622d55e6d87c924a2535ac496ecfebcae52b1995aa414362a9005be4e2b390d0e01dab22bd692b16bf49
+DIST hcalc-1.2.gh.tar.gz 18601 BLAKE2B 72ac8e60fa4bacb106314a9b2712b296ab42684f2b7d3c05bfa5baf0541a679c542ea1cb98071479fb009c0a6b6a05a817f743ca45dffa9fa93ed1a4a68c4fa5 SHA512 3e341095aa099cd3c592a727ede3202cdf72a7843b51d57605c42aa7c28bf23d23baa3859152dbea107f247acb4b851fce9d5f5d671233f12f4396d02f5a4bc8

sci-calculators/hcalc/hcalc-1.2-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2022 Gentoo Authors
+# Copyright 1999-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
@@ -7,7 +7,7 @@ inherit cmake
 
 DESCRIPTION="DJ's Hex Calculator"
 HOMEPAGE="http://www.delorie.com/store/hcalc/ https://github.com/jlec/hcalc"
-SRC_URI="https://github.com/downloads/jlec/hcalc/${P}.tar.xz"
+SRC_URI="https://github.com/jlec/hcalc/archive/refs/tags/${PV}.tar.gz -> ${P}.gh.tar.gz"
 
 LICENSE="GPL-2"
 SLOT="0"