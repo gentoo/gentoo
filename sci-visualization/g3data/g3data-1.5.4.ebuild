gentoo/gentoo

commit c3f5daaba2be4cd2ecf085f17e991fc768ef6b86

sci-visualization/g3data: Inline mirror://github
Signed-off-by: Michał Górny <mgorny@gentoo.org>

sci-visualization/g3data/g3data-1.5.4.ebuild
@@ -7,7 +7,7 @@ inherit autotools-utils eutils
 
 DESCRIPTION="Tool for extracting data from graphs"
 HOMEPAGE="https://github.com/pn2200/g3data"
-SRC_URI="mirror://github/pn2200/g3data/${P}.tar.gz"
+SRC_URI="https://github.com/downloads/pn2200/g3data/${P}.tar.gz"
 
 LICENSE="GPL-2"
 SLOT="0"

commit 3a10a810b9feda311cfccbaba52a79becaf84bf5

sci-visualization/g3data: update SRC_URI to drop github.com/downloads
github.com/downloads is gone. Tarball contents are the same.

Fixes: c3f5daaba2be4cd2ecf085f17e991fc768ef6b86
Signed-off-by: Sam James <sam@gentoo.org>

sci-visualization/g3data/Manifest
@@ -1,1 +1,1 @@
-DIST g3data-1.5.4.tar.gz 127973 BLAKE2B 19e6e6aaa7b33a4c4aa061c76aa2465bdb305ac9023e861dc1db42821cdb83c9b8221f6a5552f37731c3fe21fedc4174d46a7d2c01732916534539d382e6fac2 SHA512 b4651eb6ba87ddb745c46520ccf2d7146d179e69a614d7c6c247b82eaf188b30e955d60e7df59419f63a304a5c5215829d3bea3cc233d2729ff599c523d852e2
+DIST g3data-1.5.4.gh.tar.gz 40296 BLAKE2B ac821f80323ba9d9fe59274bf1b9746ea53273a9b46b2a1f8366be28c5b58c6f098322293a4971ce80d6261de8e9719cea1f527b0c57a4bc3505be966ff14b23 SHA512 965e3d1f68865eea20e38bf428d32974c26407e3ccb736d2049141472d4ddf5bbe3e82fcea00195febb1de021396105217cfa8098b6724b9cedfe9dbbb3c5f76

sci-visualization/g3data/g3data-1.5.4.ebuild
@@ -1,11 +1,13 @@
-# Copyright 1999-2021 Gentoo Authors
+# Copyright 1999-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
 
+inherit autotools
+
 DESCRIPTION="Tool for extracting data from graphs"
 HOMEPAGE="https://github.com/pn2200/g3data"
-SRC_URI="https://github.com/downloads/pn2200/g3data/${P}.tar.gz"
+SRC_URI="https://github.com/pn2200/g3data/archive/refs/tags/v${PV}.tar.gz -> ${P}.gh.tar.gz"
 
 LICENSE="GPL-2"
 SLOT="0"
@@ -16,3 +18,9 @@ DEPEND="${RDEPEND}"
 BDEPEND="virtual/pkgconfig"
 
 PATCHES=( "${FILESDIR}"/${P}-desktop-QA.patch )
+
+src_prepare() {
+	default
+
+	eautoreconf
+}