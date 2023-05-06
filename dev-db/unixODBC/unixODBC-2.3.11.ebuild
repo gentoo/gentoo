gentoo/gentoo

commit 57836a8512f5f787c78608bbccb8a949020db044

dev-db/unixODBC: add 2.3.11
Signed-off-by: Sam James <sam@gentoo.org>

dev-db/unixODBC/unixODBC-2.3.11.ebuild
@@ -1,0 +1,71 @@
+# Copyright 1999-2023 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit multilib-minimal
+
+DESCRIPTION="Complete ODBC driver manager"
+HOMEPAGE="https://www.unixodbc.org/"
+SRC_URI="https://www.unixodbc.org/unixODBC-${PV}.tar.gz"
+
+LICENSE="GPL-2 LGPL-2.1"
+SLOT="0"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~m68k ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~sparc-solaris ~sparc64-solaris ~x64-solaris ~x86-solaris"
+IUSE="+minimal odbcmanual static-libs unicode"
+
+RDEPEND="
+	dev-libs/libltdl:0[${MULTILIB_USEDEP}]
+	>=sys-libs/readline-6.2_p5-r1:=[${MULTILIB_USEDEP}]
+	>=sys-libs/ncurses-5.9-r3:=[${MULTILIB_USEDEP}]
+	>=virtual/libiconv-0-r1[${MULTILIB_USEDEP}]
+"
+DEPEND="
+	${RDEPEND}
+	sys-devel/bison
+	sys-devel/flex
+"
+
+MULTILIB_CHOST_TOOLS=( /usr/bin/odbc_config )
+MULTILIB_WRAPPED_HEADERS=( /usr/include/unixodbc_conf.h )
+
+PATCHES=(
+	"${FILESDIR}"/${P}-clang16.patch
+)
+
+multilib_src_configure() {
+	# Needs flex, bison
+	export LEX=flex
+	unset YACC
+
+	# --enable-driver-conf is --enable-driverc as per configure.in
+	local myeconfargs=(
+		--cache-file="${BUILD_DIR}"/config.cache
+		--sysconfdir="${EPREFIX}"/etc/${PN}
+		--disable-editline
+		--disable-static
+		--enable-iconv
+		--enable-shared
+		$(use_enable static-libs static)
+		$(use_enable !minimal drivers)
+		$(use_enable !minimal driverc)
+		$(use_with unicode iconv-char-enc UTF8)
+		$(use_with unicode iconv-ucode-enc UTF16LE)
+	)
+	ECONF_SOURCE="${S}" econf "${myeconfargs[@]}"
+}
+
+multilib_src_install_all() {
+	einstalldocs
+
+	if use odbcmanual ; then
+		# We could simply run "make install-html" if we'd not had
+		# out-of-source builds here.
+		docinto html
+		dodoc -r doc/.
+		find "${ED}/usr/share/doc/${PF}/html" -name "Makefile*" -delete || die
+	fi
+
+	use prefix && dodoc README*
+	find "${ED}" -type f -name '*.la' -delete || die
+}

dev-db/unixODBC/Manifest
@@ -1,1 +1,2 @@
+DIST unixODBC-2.3.11.tar.gz 1752585 BLAKE2B fa1502d7b83c8c10ee9d54b23281096156a3e3ec893b81acf125cd95c1de0f4ae7657ab3b4c5214a5c6a0fc9209e53cd9acdf6f99e65840e0a05366182af5600 SHA512 dddc32f90a7962e6988e1130a8093c6fb8b9ff532cad270d572250324aecbc739f45f9d8021d217313910bab25b08e69009b4f87456575535e93be1f46f5f13d
 DIST unixODBC-2.3.9.tar.gz 1676145 BLAKE2B f4078f9266cea753b6f78d234290efe95a6fa9551088a6ce352b0b6ac5391bf2724f872dcc2df0368e98befd24f995ab4eb6958a34bb080b0f6d08993991a0b7 SHA512 6637eab751401522e0af775cb104cd07693b82927453a98e5af28e079f4b9f40e1cfab8cb36f509c46dced89b45244bc5ed1a3dda17ba5a52a844e8e82f187bb

commit ce5c52a0de38d71d1b3824b5589917234d3490d3

dev-db/unixODBC: fix patch & install
I'm sorry, I must have tested the old version instead.

Closes: https://bugs.gentoo.org/905819
Fixes: 57836a8512f5f787c78608bbccb8a949020db044
Signed-off-by: Sam James <sam@gentoo.org>

dev-db/unixODBC/files/unixODBC-2.3.11-config-no-install.patch
@@ -1,0 +1,33 @@
+https://github.com/lurcher/unixODBC/commit/685df9e2b4fbbd191ab081a2020956aa78295fe0
+
+From 685df9e2b4fbbd191ab081a2020956aa78295fe0 Mon Sep 17 00:00:00 2001
+From: Hugh McMaster <hugh.mcmaster@outlook.com>
+Date: Fri, 13 May 2022 22:03:37 +1000
+Subject: [PATCH] Makefile.am: Do not install config.h
+
+--- a/Makefile.am
++++ b/Makefile.am
+@@ -1,4 +1,4 @@
+-ACLOCAL_AMFLAGS=-I m4 
++ACLOCAL_AMFLAGS=-I m4
+ 
+ pkgconfigdir = $(libdir)/pkgconfig
+ pkgconfig_DATA = \
+@@ -21,7 +21,7 @@ SUBDIRS = \
+ 	include \
+ 	doc \
+ 	man \
+-	samples 
++	samples
+ 
+ EXTRA_DIST = \
+ 	README.OSX \
+@@ -47,7 +47,7 @@ EXTRA_DIST = \
+ 
+ include_HEADERS = unixodbc.h
+ 
+-pkginclude_HEADERS = unixodbc_conf.h config.h
++pkginclude_HEADERS = unixodbc_conf.h
+ 
+ install-data-hook:
+ 	-$(MKDIR_P) $(DESTDIR)$(sysconfdir)/ODBCDataSources

dev-db/unixODBC/unixODBC-2.3.11.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-inherit multilib-minimal
+inherit autotools multilib-minimal
 
 DESCRIPTION="Complete ODBC driver manager"
 HOMEPAGE="https://www.unixodbc.org/"
@@ -27,11 +27,19 @@ DEPEND="
 "
 
 MULTILIB_CHOST_TOOLS=( /usr/bin/odbc_config )
-MULTILIB_WRAPPED_HEADERS=( /usr/include/unixodbc_conf.h )
+MULTILIB_WRAPPED_HEADERS=( /usr/include/unixODBC/unixodbc_conf.h /usr/include/unixodbc.h )
 
 PATCHES=(
-	"${FILESDIR}"/${P}-clang16.patch
+	"${FILESDIR}"/${PN}-2.3.9-clang16.patch
+	"${FILESDIR}"/${P}-config-no-install.patch
 )
+
+src_prepare() {
+	default
+
+	# Only needed for config.h install patch
+	eautoreconf
+}
 
 multilib_src_configure() {
 	# Needs flex, bison