gentoo/gentoo

commit 55b81a908d6e069ec4a0792e84ff37dc0099a972

app-misc/khal: add 0.11.1
Fix test deps while at it.

Closes: https://bugs.gentoo.org/896582
Signed-off-by: Sam James <sam@gentoo.org>

app-misc/khal/khal-0.11.1.ebuild
@@ -1,0 +1,44 @@
+# Copyright 1999-2023 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+PYTHON_COMPAT=( python3_10 )
+PYTHON_REQ_USE="sqlite"
+DISTUTILS_USE_PEP517=setuptools
+
+inherit distutils-r1 pypi
+
+DESCRIPTION="A CalDAV based calendar"
+HOMEPAGE="https://lostpackets.de/khal/ https://github.com/pimutils/khal"
+
+LICENSE="MIT"
+SLOT="0"
+KEYWORDS="~amd64 ~arm ~arm64 ~riscv ~x86"
+
+RDEPEND="
+	dev-python/click[${PYTHON_USEDEP}]
+	dev-python/click-log[${PYTHON_USEDEP}]
+	dev-python/icalendar[${PYTHON_USEDEP}]
+	dev-python/urwid[${PYTHON_USEDEP}]
+	dev-python/pyxdg[${PYTHON_USEDEP}]
+	dev-python/pytz[${PYTHON_USEDEP}]
+	dev-python/python-dateutil[${PYTHON_USEDEP}]
+	dev-python/configobj[${PYTHON_USEDEP}]
+	dev-python/atomicwrites[${PYTHON_USEDEP}]
+	dev-python/tzlocal[${PYTHON_USEDEP}]
+	dev-python/setproctitle[${PYTHON_USEDEP}]
+"
+BDEPEND="
+	dev-python/setuptools-scm[${PYTHON_USEDEP}]
+	test? (
+		dev-python/freezegun[${PYTHON_USEDEP}]
+		dev-python/hypothesis[${PYTHON_USEDEP}]
+		dev-python/packaging[${PYTHON_USEDEP}]
+		dev-python/vdirsyncer[${PYTHON_USEDEP}]
+	)
+"
+
+DOCS=( AUTHORS.txt CHANGELOG.rst CONTRIBUTING.rst README.rst khal.conf.sample )
+
+distutils_enable_tests pytest

app-misc/khal/Manifest
@@ -1,2 +1,3 @@
 DIST khal-0.10.4.tar.gz 185736 BLAKE2B 0cb9deb793f577bbcedcbba4788988973ee9d0ab71fdd30a01142a54434767d74b1d10e5bfa5f6a2feb9f18a8a9fe22717d4a38a07ab5e30b331c87b79a79f39 SHA512 b32f1bfe5d3a3976278726a6570eb345a5ad44f63a52b75f045fd0509d145edd5b76e11a10acffd9c3982edc124f93daec34a37c39299c2311796c0314016eb6
 DIST khal-0.10.5.tar.gz 188468 BLAKE2B 195e61fcff7cff1308f2819ec54c5733acebb5ace18f88afa8fc561926857dab3e0e0b82d1f7e3e0235e69ed9ad56544d6eeb95a9e9829dc078d49ebf96c0485 SHA512 f7b1e4d4934757ec7458fc4e73c6b6c5ee6b1c64f6cdc3e61771a2f05041515e4afb73b4a17cbc577747039b689ddd66503e120648c4bfe4e33113a3de6f3d14
+DIST khal-0.11.1.tar.gz 193942 BLAKE2B 12f98781b9a64710537f7728a087f24ef1d73600e957eac7b699948047c1676a9c91a38a415741c6acd94a7ba72014d4b009425edab249225c8bef23b94c0387 SHA512 c4fd25dc4981f4937562949b78f81d3e57b7ef54f7f6d96855645ca239eea9dec7ee6f3c957a67261e7ae2975ab55b36a686db476b03f4fd0bacce94e834b239

commit fd797b3cab3759df30e9f23a68eedc5bbc7975df

app-misc/khal: enable py3.11
I thought I'd done this in the bump, but I hadn't!

Bug: https://bugs.gentoo.org/896582
Fixes: 55b81a908d6e069ec4a0792e84ff37dc0099a972
Signed-off-by: Sam James <sam@gentoo.org>

app-misc/khal/khal-0.11.1.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-PYTHON_COMPAT=( python3_10 )
+PYTHON_COMPAT=( python3_{10..11} )
 PYTHON_REQ_USE="sqlite"
 DISTUTILS_USE_PEP517=setuptools
 