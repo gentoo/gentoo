gentoo/gentoo

commit 570a3e436ff2a642f9470809fdc8ef63ce560494

dev-libs/gmp: strip-flags & filter-lto for miscompilation
Miscompiled on arm64 with LTO at least. strip-flags as well because gmp is so
brittle (see e.g. the past configure mess).

Bug: https://bugs.gentoo.org/889948
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/gmp/gmp-6.2.1-r2.ebuild
@@ -66,6 +66,11 @@ src_prepare() {
 }
 
 multilib_src_configure() {
+	# Generally a very fragile package
+	strip-flags
+	# Miscompiled with LTO at least on arm64, bug #889948
+	filter-lto
+
 	# Because of our 32-bit userland, 1.0 is the only HPPA ABI that works
 	# https://gmplib.org/manual/ABI-and-ISA.html#ABI-and-ISA (bug #344613)
 	if [[ ${CHOST} == hppa2.0-* ]] ; then

dev-libs/gmp/gmp-6.2.1-r5.ebuild
@@ -94,6 +94,11 @@ src_prepare() {
 }
 
 multilib_src_configure() {
+	# Generally a very fragile package
+	strip-flags
+	# Miscompiled with LTO at least on arm64, bug #889948
+	filter-lto
+
 	# Because of our 32-bit userland, 1.0 is the only HPPA ABI that works
 	# https://gmplib.org/manual/ABI-and-ISA.html#ABI-and-ISA (bug #344613)
 	if [[ ${CHOST} == hppa2.0-* ]] ; then

commit 28a7be8c9e3b551771c4ba5ad6f31cd28c4074d9

dev-libs/gmp: fix MissingInherits
Closes: https://bugs.gentoo.org/905920
Fixes: 570a3e436ff2a642f9470809fdc8ef63ce560494
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/gmp/gmp-6.2.1-r2.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=7
 
-inherit libtool multilib-minimal toolchain-funcs
+inherit libtool flag-o-matic multilib-minimal toolchain-funcs
 
 MY_PV=${PV/_p*}
 MY_PV=${MY_PV/_/-}

dev-libs/gmp/gmp-6.2.1-r5.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-inherit gnuconfig libtool multilib-minimal toolchain-funcs
+inherit gnuconfig libtool flag-o-matic multilib-minimal toolchain-funcs
 
 MY_PV=${PV/_p*}
 MY_PV=${MY_PV/_/-}