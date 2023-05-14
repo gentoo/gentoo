gentoo/gentoo

commit 6952d230a11be325d68ec744cc7f8cffadbcde60

dev-vcs/cssc: drop invalid USE=valgrind flag (for test suite)
We don't, in general, run Valgrind for test suites in Gentoo because it's
not portable enough and gives inconsistent results, especially under sandbox.

Plus it's confusing given USE=valgrind is effectively a global USE flag (even
if it isn't in use.desc yet) for meaning "enable annotations to enable better
debugging under Valgrind", rather than "run tests under it".

Signed-off-by: Sam James <sam@gentoo.org>

dev-vcs/cssc/cssc-1.4.1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2021 Gentoo Authors
+# Copyright 1999-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
@@ -13,16 +13,15 @@ HOMEPAGE="https://www.gnu.org/software/cssc/"
 SLOT="0"
 LICENSE="GPL-3"
 KEYWORDS="amd64 x86 ~amd64-linux ~x86-linux ~ppc-macos"
-
-IUSE="test valgrind"
+IUSE="test"
 RESTRICT="!test? ( test )"
-DEPEND="
-	test? ( valgrind? ( dev-util/valgrind ) )
-"
+
 DOCS=( AUTHORS ChangeLog NEWS README )
+
 PATCHES=(
 	"${FILESDIR}"/${PN}-1.4.1-m4.patch
 )
+
 S=${WORKDIR}/${CSSC_P}
 
 src_prepare() {
@@ -31,8 +30,9 @@ src_prepare() {
 }
 
 src_configure() {
+	# Valgrind is only used for tests
 	econf \
-		$(use test && use_with valgrind) \
+		--disable-valgrind \
 		--enable-binary
 }
 

dev-vcs/cssc/metadata.xml
@@ -2,9 +2,6 @@
 <!DOCTYPE pkgmetadata SYSTEM "https://www.gentoo.org/dtd/metadata.dtd">
 <pkgmetadata>
   <!-- maintainer-needed -->
-  <use>
-    <flag name="valgrind">Enable usage of <pkg>dev-util/valgrind</pkg> in tests</flag>
-  </use>
   <upstream>
     <remote-id type="savannah">cssc</remote-id>
   </upstream>

commit b297ef3e92e4e810409341e2f9e77b475ddb3cce

dev-vcs/cssc: fix disable-valgrind flag
Closes: https://bugs.gentoo.org/906318
Fixes: 6952d230a11be325d68ec744cc7f8cffadbcde60
Signed-off-by: Sam James <sam@gentoo.org>

dev-vcs/cssc/cssc-1.4.1.ebuild
@@ -2,16 +2,18 @@
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
+
 inherit autotools
 
 CSSC_PN="${PN^^}"
 CSSC_P="${CSSC_PN}-${PV}"
 
 DESCRIPTION="The GNU Project's replacement for SCCS"
-SRC_URI="mirror://gnu/${PN}/${CSSC_P}.tar.gz"
 HOMEPAGE="https://www.gnu.org/software/cssc/"
-SLOT="0"
+SRC_URI="mirror://gnu/${PN}/${CSSC_P}.tar.gz"
+
 LICENSE="GPL-3"
+SLOT="0"
 KEYWORDS="amd64 x86 ~amd64-linux ~x86-linux ~ppc-macos"
 IUSE="test"
 RESTRICT="!test? ( test )"
@@ -32,7 +34,7 @@ src_prepare() {
 src_configure() {
 	# Valgrind is only used for tests
 	econf \
-		--disable-valgrind \
+		--without-valgrind \
 		--enable-binary
 }
 