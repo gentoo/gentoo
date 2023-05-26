gentoo/gentoo

commit 77f67752cd0fa1d306b5d82723954ac8dbc6cd6c

*/*: remove x64-cygwin keyword
Signed-off-by: Fabian Groffen <grobian@gentoo.org>

app-admin/cygwin-rebase/cygwin-rebase-4.4.4.ebuild
@@ -31,7 +31,7 @@ else
 		https://github.com/haubi/cygwin-rebase/compare/rebase-${MY_PV}...rebase-${MY_PV}_merge-files-flag-0.patch -> ${P}_merge-files-flag-0.patch
 	"
 	PATCHES=( "${DISTDIR}/${P}_merge-files-flag-0.patch" )
-	KEYWORDS="-* ~x64-cygwin"
+	KEYWORDS="-*"
 fi
 
 DEPEND=""

app-admin/cygwin-rebase/cygwin-rebase-9999.ebuild
@@ -31,7 +31,7 @@ else
 		https://github.com/haubi/cygwin-rebase/compare/rebase-${MY_PV}...rebase-${MY_PV}_merge-files-flag-0.patch -> ${P}_merge-files-flag-0.patch
 	"
 	PATCHES=( "${DISTDIR}/${P}_merge-files-flag-0.patch" )
-	KEYWORDS="-* ~x64-cygwin"
+	KEYWORDS="-*"
 fi
 
 DEPEND=""

commit 725b358582559e4ddaff5e8a77c4fc898209eeb7

app-admin/cygwin-rebase: Remove invalid KEYWORDS
Fixes: 77f67752cd0fa1d306b5d82723954ac8dbc6cd6c
Signed-off-by: Michał Górny <mgorny@gentoo.org>

app-admin/cygwin-rebase/cygwin-rebase-4.4.4.ebuild
@@ -1,4 +1,4 @@
-# Copyright 2019-2020 Gentoo Authors
+# Copyright 2019-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
@@ -31,7 +31,7 @@ else
 		https://github.com/haubi/cygwin-rebase/compare/rebase-${MY_PV}...rebase-${MY_PV}_merge-files-flag-0.patch -> ${P}_merge-files-flag-0.patch
 	"
 	PATCHES=( "${DISTDIR}/${P}_merge-files-flag-0.patch" )
-	KEYWORDS="-*"
+	KEYWORDS=""
 fi
 
 DEPEND=""

app-admin/cygwin-rebase/cygwin-rebase-9999.ebuild
@@ -1,4 +1,4 @@
-# Copyright 2019-2020 Gentoo Authors
+# Copyright 2019-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
@@ -31,7 +31,7 @@ else
 		https://github.com/haubi/cygwin-rebase/compare/rebase-${MY_PV}...rebase-${MY_PV}_merge-files-flag-0.patch -> ${P}_merge-files-flag-0.patch
 	"
 	PATCHES=( "${DISTDIR}/${P}_merge-files-flag-0.patch" )
-	KEYWORDS="-*"
+	KEYWORDS=""
 fi
 
 DEPEND=""