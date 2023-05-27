gentoo/gentoo

commit a0eece665f3c1c43e6bed799ead6190591296cfa

dev-util/pkgcheck: enable py3.12
Signed-off-by: Sam James <sam@gentoo.org>

dev-util/pkgcheck/pkgcheck-0.10.24.ebuild
@@ -4,7 +4,7 @@
 EAPI=8
 
 DISTUTILS_USE_PEP517=setuptools
-PYTHON_COMPAT=( python3_{9..11} )
+PYTHON_COMPAT=( python3_{10..12} )
 inherit elisp-common distutils-r1 optfeature
 
 if [[ ${PV} == *9999 ]] ; then
@@ -50,7 +50,9 @@ BDEPEND="${RDEPEND}
 	dev-python/wheel
 	test? (
 		dev-python/pytest[${PYTHON_USEDEP}]
-		dev-python/requests[${PYTHON_USEDEP}]
+		$(python_gen_cond_dep '
+			dev-python/requests[${PYTHON_USEDEP}]
+		' python3_{10..11} )
 		dev-vcs/git
 	)
 "
@@ -69,6 +71,16 @@ src_compile() {
 	   elisp-compile *.el
 	   popd >/dev/null || die
 	fi
+}
+
+python_test() {
+	if ! has_version -b "dev-python/requests[${PYTHON_USEDEP}" ; then
+		EPYTEST_DESELECT=(
+			tests/checks/test_all.py::TestNetworkCheck::test_network_enabled
+		)
+	fi
+
+	epytest
 }
 
 python_install_all() {

dev-util/pkgcheck/pkgcheck-9999.ebuild
@@ -4,7 +4,7 @@
 EAPI=8
 
 DISTUTILS_USE_PEP517=setuptools
-PYTHON_COMPAT=( python3_{9..11} )
+PYTHON_COMPAT=( python3_{10..12} )
 inherit elisp-common distutils-r1 optfeature
 
 if [[ ${PV} == *9999 ]] ; then
@@ -50,7 +50,9 @@ BDEPEND="${RDEPEND}
 	dev-python/wheel
 	test? (
 		dev-python/pytest[${PYTHON_USEDEP}]
-		dev-python/requests[${PYTHON_USEDEP}]
+		$(python_gen_cond_dep '
+			dev-python/requests[${PYTHON_USEDEP}]
+		' python3_{10..11} )
 		dev-vcs/git
 	)
 "
@@ -69,6 +71,16 @@ src_compile() {
 	   elisp-compile *.el
 	   popd >/dev/null || die
 	fi
+}
+
+python_test() {
+	if ! has_version -b "dev-python/requests[${PYTHON_USEDEP}" ; then
+		EPYTEST_DESELECT=(
+			tests/checks/test_all.py::TestNetworkCheck::test_network_enabled
+		)
+	fi
+
+	epytest
 }
 
 python_install_all() {

commit 19d05920ea8d2c712657e4dc8c1bf3c4c45112d0

dev-util/pkgcheck: fix typo
Fixes: a0eece665f3c1c43e6bed799ead6190591296cfa
Closes: https://bugs.gentoo.org/907248
Signed-off-by: Sam James <sam@gentoo.org>

dev-util/pkgcheck/pkgcheck-0.10.24.ebuild
@@ -74,7 +74,7 @@ src_compile() {
 }
 
 python_test() {
-	if ! has_version -b "dev-python/requests[${PYTHON_USEDEP}" ; then
+	if ! has_version -b "dev-python/requests[${PYTHON_USEDEP}]" ; then
 		EPYTEST_DESELECT=(
 			tests/checks/test_all.py::TestNetworkCheck::test_network_enabled
 		)

dev-util/pkgcheck/pkgcheck-9999.ebuild
@@ -74,7 +74,7 @@ src_compile() {
 }
 
 python_test() {
-	if ! has_version -b "dev-python/requests[${PYTHON_USEDEP}" ; then
+	if ! has_version -b "dev-python/requests[${PYTHON_USEDEP}]" ; then
 		EPYTEST_DESELECT=(
 			tests/checks/test_all.py::TestNetworkCheck::test_network_enabled
 		)