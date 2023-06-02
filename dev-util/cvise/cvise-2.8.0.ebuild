gentoo/gentoo

commit 5849d652941d202823b36b7e246d890f25af3c25

dev-util/cvise: Keyword 2.8.0 riscv, #907672
Signed-off-by: Yixun Lan <dlan@gentoo.org>

dev-util/cvise/cvise-2.8.0.ebuild
@@ -15,7 +15,11 @@ SRC_URI="
 
 LICENSE="UoI-NCSA"
 SLOT="0"
-KEYWORDS="amd64 ~arm ~arm64 ~ppc ~ppc64 ~sparc x86"
+<<<<<<< HEAD
+KEYWORDS="amd64 ~arm ~arm64 ~ppc ~ppc64 ~riscv ~sparc x86"
+=======
+KEYWORDS="~amd64 ~arm ~arm64 ~ppc ~ppc64 ~riscv ~sparc ~x86"
+>>>>>>> 2a0536d75d4e (dev-util/cvise: Keyword 2.8.0 riscv, #907672)
 IUSE="test"
 RESTRICT="!test? ( test )"
 REQUIRED_USE=${PYTHON_REQUIRED_USE}

commit 3ba9230c6d42b8ac0eb0c73fa03870b54de0d2b7

dev-util/cvise: fix merge conflict markers in KEYWORDS
Thanks to username234 for reporting.

Fixes: 5849d652941d202823b36b7e246d890f25af3c25
Signed-off-by: Sam James <sam@gentoo.org>

dev-util/cvise/cvise-2.8.0.ebuild
@@ -15,11 +15,7 @@ SRC_URI="
 
 LICENSE="UoI-NCSA"
 SLOT="0"
-<<<<<<< HEAD
 KEYWORDS="amd64 ~arm ~arm64 ~ppc ~ppc64 ~riscv ~sparc x86"
-=======
-KEYWORDS="~amd64 ~arm ~arm64 ~ppc ~ppc64 ~riscv ~sparc ~x86"
->>>>>>> 2a0536d75d4e (dev-util/cvise: Keyword 2.8.0 riscv, #907672)
 IUSE="test"
 RESTRICT="!test? ( test )"
 REQUIRED_USE=${PYTHON_REQUIRED_USE}