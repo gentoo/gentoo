gentoo/gentoo

commit d5638e49ee79c0f7e4672d5537e97a4ccc7f2eb2

eapi8-dosym.eclass: Prevent globbing of argument in _dosym8_canonicalize
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

eclass/eapi8-dosym.eclass
@@ -31,7 +31,7 @@ esac
 _dosym8_canonicalize() {
 	local path slash i prev out IFS=/
 
-	path=( $1 )
+	read -r -d '' -a path <<< "$1"
 	[[ $1 == /* ]] && slash=/
 
 	while true; do
@@ -39,7 +39,7 @@ _dosym8_canonicalize() {
 		# or as a special case, "/.." at the beginning of the path.
 		# Also drop empty and "." path components as we go along.
 		prev=
-		for i in ${!path[@]}; do
+		for i in "${!path[@]}"; do
 			if [[ -z ${path[i]} || ${path[i]} == . ]]; then
 				unset "path[i]"
 			elif [[ ${path[i]} != .. ]]; then

commit 6010e062fbaf504c3f3d4c78f6e7fa506af8a49d

eapi8-dosym.eclass: Don't add a spurious newline to the path
Bash's <<< operator will append a newline to the string, therefore use
echo -n instead.

Add a couple of test cases that would have caught this.

Fixes: d5638e49ee79c0f7e4672d5537e97a4ccc7f2eb2
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

eclass/eapi8-dosym.eclass
@@ -31,7 +31,7 @@ esac
 _dosym8_canonicalize() {
 	local path slash i prev out IFS=/
 
-	read -r -d '' -a path <<< "$1"
+	read -r -d '' -a path < <(echo -n "$1")
 	[[ $1 == /* ]] && slash=/
 
 	while true; do

eclass/tests/eapi8-dosym.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2020 Gentoo Authors
+# Copyright 2020-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
@@ -50,8 +50,10 @@ done
 teq . _dosym8_canonicalize .
 teq foo _dosym8_canonicalize foo
 teq foo _dosym8_canonicalize ./foo
+teq foo _dosym8_canonicalize foo/.
 teq ../foo _dosym8_canonicalize ../foo
 teq ../baz _dosym8_canonicalize foo/bar/../../../baz
+teq '*' _dosym8_canonicalize '*'
 
 for f in ref_dosym_r "dosym8 -r"; do
 	teq ../../bin/foo ${f} /bin/foo /usr/bin/foo