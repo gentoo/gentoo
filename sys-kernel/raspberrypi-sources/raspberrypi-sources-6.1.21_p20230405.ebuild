gentoo/gentoo

commit 2e0f585e9ce72744abbd44df02152ab9eadd9f53

sys-kernel/raspberrypi-sources: add 6.1.21_p20230405
Signed-off-by: Violet Purcell <vimproved@inventati.org>
Closes: https://github.com/gentoo/gentoo/pull/31041
Signed-off-by: Sam James <sam@gentoo.org>

sys-kernel/raspberrypi-sources/raspberrypi-sources-6.1.21_p20230405.ebuild
@@ -1,0 +1,75 @@
+# Copyright 1999-2023 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+ETYPE=sources
+K_DEFCONFIG="bcmrpi_defconfig"
+K_SECURITY_UNSUPPORTED=1
+EXTRAVERSION="-${PN}/-*"
+
+K_EXP_GENPATCHES_NOUSE=1
+K_GENPATCHES_VER=25
+K_DEBLOB_AVAILABLE=0
+K_WANT_GENPATCHES="base extras"
+
+inherit kernel-2 linux-info
+detect_version
+detect_arch
+
+MY_P=$(ver_cut 4-)
+MY_P="1.${MY_P/p/}"
+
+DESCRIPTION="Raspberry Pi kernel sources"
+HOMEPAGE="https://github.com/raspberrypi/linux"
+SRC_URI="
+	https://github.com/raspberrypi/linux/archive/${MY_P}.tar.gz -> linux-${KV_FULL}.tar.gz
+	${GENPATCHES_URI}
+"
+
+KEYWORDS="~arm ~arm64"
+
+PATCHES=("${FILESDIR}"/${PN}-5.15.32-gentoo-kconfig.patch)
+
+UNIPATCH_EXCLUDE="
+	10*
+	15*
+	2000
+	29*
+	3000
+	4567"
+
+pkg_setup() {
+	ewarn ""
+	ewarn "${PN} is *not* supported by the Gentoo Kernel Project in any way."
+	ewarn "If you need support, please contact the raspberrypi developers directly."
+	ewarn "Do *not* open bugs in Gentoo's bugzilla unless you have issues with"
+	ewarn "the ebuilds. Thank you."
+	ewarn ""
+
+	kernel-2_pkg_setup
+}
+
+universal_unpack() {
+	unpack linux-${KV_FULL}.tar.gz
+
+	# We want to rename the unpacked directory to a nice normalised string
+	# bug #762766
+	mv "${WORKDIR}"/linux-${MY_P} "${WORKDIR}"/linux-${KV_FULL} || die
+
+	# remove all backup files
+	find . -iname "*~" -exec rm {} \; 2>/dev/null
+}
+
+src_prepare() {
+	default
+	kernel-2_src_prepare
+}
+
+pkg_postinst() {
+	kernel-2_pkg_postinst
+}
+
+pkg_postrm() {
+	kernel-2_pkg_postrm
+}

sys-kernel/raspberrypi-sources/Manifest
@@ -1,4 +1,7 @@
 DIST genpatches-5.15-22.base.tar.xz 986832 BLAKE2B 48fcd267e6e28850d226693b902fd66696024e75db6a75bb78ec79c7ac8afdac62643c4113ff205c301024d245f075716e0bf8083f97917dcfc8b1fcd71b674e SHA512 e8c64bd91caedc3dcd98301dbcbd2003b4aceb5a9713bd908a69448eaa2aea945fa8162e57442862b03daa0e1a4e7642e4504701b1f34a6edb127692e582e0e0
 DIST genpatches-5.15-22.extras.tar.xz 3876 BLAKE2B 95daac6c95b82d628c0551b4e18e7c3ad9a5722b5ab8f087889b15bdeea8c6f5e232ee8b365bfb4a7c6730207d4735a0fb648b389626f35cda2593caa83ff3e2 SHA512 0616c4a976d22a5491039a6bc3babdd7e9095a2c53e2a1530d1ecfcf50a2dbfca637c1b6771078efc92e3371ac15f332f85f653d267ea8e6262ffd305be2723a
+DIST genpatches-6.1-25.base.tar.xz 1200876 BLAKE2B 7050f6e95a28f5886cce9b53823f6219a22a5b23cad5471bfa9e83eca208b8369395075e9aabdbe4d33b53b8c01aff3d3d0886115850e327baf12a946b851e1d SHA512 ea30e0d44ab64a2aa36ad7a83cf2df7924ba5e699c92268bc5f3d54acadefc9c836a00e7e27cc02400a6751ffe1c3cd45e6b9891a3fc0aa8d23ad8e535f18e4c
+DIST genpatches-6.1-25.extras.tar.xz 3812 BLAKE2B f73698c57031c9d7f5edd9ba2b865e32064c38b528649a8144f856bacf35ae3570d1fc936ee25a88bd52fd0765fff05bec42686b8b3fc5566df187bd57d59be5 SHA512 ac2bdccbc6bd1de82cbdfe4edac978e068445b8e064bcc6a833b04f15b86592304c19651884dc0769719ade1b220c8c0f704beca240ce24ab370621ad6849aee
 DIST linux-5.15.76_p20221104-raspberrypi.tar.gz 199660690 BLAKE2B 899714102e52904b7a37d155e42e94ed4b0e6c6600ac40fbd3cf66d15f84bb6d06afa7acf5c9624f44e9888d03d101823b419547d7ad8e566b4e55aaa077f11b SHA512 648262937e0afdb331678e0b2f913ad22788f1373f9ecd0d495e748a78399d6a69e93024268e1c0567acb4b1c9242b3ca6bd060e73b91a77529a93e4e29181c6
 DIST linux-5.15.84_p20230106-raspberrypi.tar.gz 199714807 BLAKE2B f3579d4b16921141055c0a62787084d2fb7db057bf9205c45e4a6515969bd3136a0dfe08c26848e691a5ea74d1f23533e4352c8fe70edf4b7000785958caf6ea SHA512 97ed02f35b42f144db72f802339f4952148596e3e5a0725fedd7a57618c1695cb802d58f6183e257ebfd242d35bf69f1eefea78ab9df109f6158af713d7d5f30
+DIST linux-6.1.21_p20230405-raspberrypi.tar.gz 221783059 BLAKE2B 5e9e13b6410df1b9e4bcdef85a6b33de83621a74d89bb6760dcff86cd736262513f613e9279a8dccf5ca306a807c16652084a3e6b4cb34c3658a8e9e3196c5d1 SHA512 0c8252833bb737977c0981ed48764ff9742de7cb494fefec532c90312e0d8e0e48a230dd14a0d6f99b54b015e6c91e647b579f2ef7408b80e349a547767d9925

commit 1bfd36623c00d5d6c4b234b3c69b21a0826807f8

sys-kernel/raspberrypi-sources: Fix patch application
See: https://github.com/gentoo/gentoo/pull/31041#issuecomment-1583677573
Fixes: 2e0f585e9ce72744abbd44df02152ab9eadd9f53
Signed-off-by: Violet Purcell <vimproved@inventati.org>
Closes: https://github.com/gentoo/gentoo/pull/31361
Signed-off-by: Sam James <sam@gentoo.org>

sys-kernel/raspberrypi-sources/files/raspberrypi-sources-6.1.21-gentoo-kconfig.patch
@@ -1,0 +1,13 @@
+--- a/drivers/base/firmware_loader/Kconfig
++++ b/drivers/base/firmware_loader/Kconfig
+@@ -75,6 +75,7 @@ config EXTRA_FIRMWARE_DIR
+ 
+ config FW_LOADER_USER_HELPER
+ 	bool "Enable the firmware sysfs fallback mechanism"
++	depends on !GENTOO_LINUX_INIT_SYSTEMD
+ 	select FW_LOADER_SYSFS
+ 	select FW_LOADER_PAGED_BUF
+ 	help
+-- 
+2.41.0
+

sys-kernel/raspberrypi-sources/raspberrypi-sources-6.1.21_p20230405.ebuild
@@ -29,11 +29,12 @@ SRC_URI="
 
 KEYWORDS="~arm ~arm64"
 
-PATCHES=("${FILESDIR}"/${PN}-5.15.32-gentoo-kconfig.patch)
+PATCHES=("${FILESDIR}"/${PN}-6.1.21-gentoo-kconfig.patch)
 
 UNIPATCH_EXCLUDE="
 	10*
 	15*
+	1700
 	2000
 	29*
 	3000