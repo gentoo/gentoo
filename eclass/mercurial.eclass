gentoo/gentoo

commit 216f39ea557e3950aa4d0c174f36df737afcb5fe

*/*: Reassign junghans's packages
Signed-off-by: Michał Górny <mgorny@gentoo.org>

eclass/mercurial.eclass
@@ -1,9 +1,9 @@
-# Copyright 1999-2022 Gentoo Authors
+# Copyright 1999-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 # @ECLASS: mercurial.eclass
 # @MAINTAINER:
-# Christoph Junghans <junghans@gentoo.org>
+# No maintainer <maintainer-needed@gentoo.org.
 # @AUTHOR:
 # Next gen author: Krzysztof Pawlik <nelchael@gentoo.org>
 # Original author: Aron Griffis <agriffis@gentoo.org>

app-alternatives/awk/metadata.xml
@@ -1,10 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE pkgmetadata SYSTEM "https://www.gentoo.org/dtd/metadata.dtd">
 <pkgmetadata>
-	<maintainer type="person">
-		<email>junghans@gentoo.org</email>
-		<name>Christoph Junghans</name>
-	</maintainer>
 	<maintainer type="project">
 		<email>base-system@gentoo.org</email>
 		<name>Gentoo Base System</name>

app-backup/rdumpfs/metadata.xml
@@ -1,10 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE pkgmetadata SYSTEM "https://www.gentoo.org/dtd/metadata.dtd">
 <pkgmetadata>
-  <maintainer type="person">
-    <email>junghans@gentoo.org</email>
-    <name>Christoph Junghans</name>
-  </maintainer>
+  <!-- maintainer-needed -->
   <upstream>
     <remote-id type="github">chneukirchen/rdumpfs</remote-id>
   </upstream>

app-eselect/eselect-timezone/metadata.xml
@@ -1,9 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE pkgmetadata SYSTEM "https://www.gentoo.org/dtd/metadata.dtd">
 <pkgmetadata>
-	<maintainer type="person">
-		<email>junghans@gentoo.org</email>
-		<name>Christoph Junghans</name>
-	</maintainer>
+	<!-- maintainer-needed -->
 	<stabilize-allarches/>
 </pkgmetadata>

app-laptop/hdapsd/metadata.xml
@@ -2,10 +2,6 @@
 <!DOCTYPE pkgmetadata SYSTEM "https://www.gentoo.org/dtd/metadata.dtd">
 <pkgmetadata>
   <maintainer type="person">
-    <email>junghans@gentoo.org</email>
-    <name>Christoph Junghans</name>
-  </maintainer>
-  <maintainer type="person">
     <email>mjo@gentoo.org</email>
     <name>Michael Orlitzky</name>
   </maintainer>

app-laptop/tpacpi-bat/metadata.xml
@@ -1,10 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE pkgmetadata SYSTEM "https://www.gentoo.org/dtd/metadata.dtd">
 <pkgmetadata>
-	<maintainer type="person">
-		<email>junghans@gentoo.org</email>
-		<name>Christoph Junghans</name>
-	</maintainer>
+	<!-- maintainer-needed -->
 	<upstream>
 		<remote-id type="github">teleshoes/tpacpi-bat</remote-id>
 	</upstream>

app-shells/mpibash/metadata.xml
@@ -1,10 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE pkgmetadata SYSTEM "https://www.gentoo.org/dtd/metadata.dtd">
 <pkgmetadata>
-  <maintainer type="person">
-    <email>junghans@gentoo.org</email>
-    <name>Christoph Junghans</name>
-  </maintainer>
   <maintainer type="project">
     <email>sci@gentoo.org</email>
     <name>Gentoo Science Project</name>

dev-cpp/kokkos/metadata.xml
@@ -2,10 +2,6 @@
 <!DOCTYPE pkgmetadata SYSTEM "https://www.gentoo.org/dtd/metadata.dtd">
 <pkgmetadata>
 	<maintainer type="person">
-		<email>junghans@gentoo.org</email>
-		<name>Christoph Junghans</name>
-	</maintainer>
-	<maintainer type="person">
 		<email>tamiko@gentoo.org</email>
 		<name>Matthias Maier</name>
 	</maintainer>

commit 688771f0eb496d8948ced8506b0e836a32f90411

mercurial.eclass: Fix typo in eclassdoc
Fixes: 216f39ea557e3950aa4d0c174f36df737afcb5fe
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

eclass/mercurial.eclass
@@ -3,7 +3,7 @@
 
 # @ECLASS: mercurial.eclass
 # @MAINTAINER:
-# No maintainer <maintainer-needed@gentoo.org.
+# No maintainer <maintainer-needed@gentoo.org>
 # @AUTHOR:
 # Next gen author: Krzysztof Pawlik <nelchael@gentoo.org>
 # Original author: Aron Griffis <agriffis@gentoo.org>