gentoo/gentoo

commit 71d0d48fe392b55b1ed9fdd3727d9dd4a9310ca2

app-text/ansifilter: avoid dep on glib for headless
Signed-off-by: Sam James <sam@gentoo.org>

app-text/ansifilter/ansifilter-2.20.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-inherit desktop toolchain-funcs qmake-utils xdg
+inherit desktop toolchain-funcs qmake-utils xdg-utils
 
 DESCRIPTION="Handles text files containing ANSI terminal escape codes"
 HOMEPAGE="http://www.andre-simon.de/"
@@ -72,13 +72,22 @@ src_install() {
 }
 
 pkg_preinst() {
-	use gui && xdg_pkg_preinst
+	if use gui; then
+		xdg_desktop_database_update
+		xdg_icon_cache_update
+	fi
 }
 
 pkg_postrm() {
-	use gui && xdg_pkg_postrm
+	if use gui; then
+		xdg_desktop_database_update
+		xdg_icon_cache_update
+	fi
 }
 
 pkg_postinst() {
-	use gui && xdg_pkg_postinst
+	if use gui; then
+		xdg_desktop_database_update
+		xdg_icon_cache_update
+	fi
 }

commit a44a2dd6d7f339c9cd0e7d1d8c39e88376e0fc2e

app-text/ansifilter: drop pkg_preinst
Closes: https://bugs.gentoo.org/908865
Fixes: 71d0d48fe392b55b1ed9fdd3727d9dd4a9310ca2
Signed-off-by: Sam James <sam@gentoo.org>

app-text/ansifilter/ansifilter-2.20.ebuild
@@ -71,13 +71,6 @@ src_install() {
 	fi
 }
 
-pkg_preinst() {
-	if use gui; then
-		xdg_desktop_database_update
-		xdg_icon_cache_update
-	fi
-}
-
 pkg_postrm() {
 	if use gui; then
 		xdg_desktop_database_update