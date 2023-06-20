gentoo/gentoo

commit 2c430c1db25cdde20a30dcca291976fdb96963e8

dev-perl/Mojolicious: add 9.330.0
Signed-off-by: Sam James <sam@gentoo.org>

dev-perl/Mojolicious/Manifest
@@ -1,2 +1,3 @@
 DIST Mojolicious-9.19.tar.gz 841263 BLAKE2B 7510b268c1a5793ba78d8717f5683c3ca53fa95c0af8b374b5767ff4a7c532481a788c08ac66c88295e55307fbf164784547568ce1edbc35b3b03f340ed60336 SHA512 14c29473f040548de2efef8bf5cd5676554f9f518b52e0ee03ed30415942fbeff5f5f2a6dd7592798e433e3e83653ce815697d4614e4680c9a6b8c699b0a171a
 DIST Mojolicious-9.22.tar.gz 841358 BLAKE2B 5f49c7ca71b30994181a3a16ac12faa21b3f7782beefae94c185aac87aa4d223ee696e49159bb1e044d389c640a737a265cd289774e647ff40d4057ccae9c4b2 SHA512 53175c4d6054a4fe9c53368420714ff87cd826c3f61565d1b9dd52a69a15636d080852a62518f0bada865675d90026d78bdc1c210fa55939a81e4866ba894e40
+DIST Mojolicious-9.23.tar.gz 846183 BLAKE2B 47cc37c5783354ed54a1954ce60453f3a735f8f12775e2ec9decb1c4e34542f4ebd67386b478d2058c874af2694f746fddeb66f21b161f82b57b1a0b1e77d5d8 SHA512 065a4348c759a8b820ccff743ef3ebf3f87dafa841222a49cdc1736faea493c27a7a3aec60f5a921dcb7ce51c32ade8350a582de4261f1b7d98d70b0fa6a1ae5

dev-perl/Mojolicious/Mojolicious-9.330.0.ebuild
@@ -1,0 +1,38 @@
+# Copyright 1999-2023 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+DIST_AUTHOR=SRI
+DIST_VERSION=9.23
+DIST_EXAMPLES=("examples/*")
+
+inherit perl-module
+
+DESCRIPTION="Real-time web framework"
+
+LICENSE="Artistic-2"
+SLOT="0"
+KEYWORDS="~amd64 ~arm ~ppc ~ppc64 ~riscv ~x86"
+IUSE="minimal"
+
+RDEPEND="
+	!minimal? (
+		>=dev-perl/Cpanel-JSON-XS-4.90.0
+		>=dev-perl/EV-4.0.0
+	)
+	>=virtual/perl-IO-Socket-IP-0.370.0
+	>=virtual/perl-Scalar-List-Utils-1.410.0
+"
+BDEPEND="
+	${RDEPEND}
+	virtual/perl-ExtUtils-MakeMaker
+	test? (
+		virtual/perl-Test-Simple
+	)
+"
+
+PERL_RM_FILES=(
+	t/pod.t
+	t/pod_coverage.t
+)

commit 8c55fe4118ffd5ef99e4aa1112261251772fe2bb

dev-perl/Mojolicious: fix version
Fixes: 2c430c1db25cdde20a30dcca291976fdb96963e8
Signed-off-by: Sam James <sam@gentoo.org>

dev-perl/Mojolicious/Manifest
@@ -1,3 +1,3 @@
 DIST Mojolicious-9.19.tar.gz 841263 BLAKE2B 7510b268c1a5793ba78d8717f5683c3ca53fa95c0af8b374b5767ff4a7c532481a788c08ac66c88295e55307fbf164784547568ce1edbc35b3b03f340ed60336 SHA512 14c29473f040548de2efef8bf5cd5676554f9f518b52e0ee03ed30415942fbeff5f5f2a6dd7592798e433e3e83653ce815697d4614e4680c9a6b8c699b0a171a
 DIST Mojolicious-9.22.tar.gz 841358 BLAKE2B 5f49c7ca71b30994181a3a16ac12faa21b3f7782beefae94c185aac87aa4d223ee696e49159bb1e044d389c640a737a265cd289774e647ff40d4057ccae9c4b2 SHA512 53175c4d6054a4fe9c53368420714ff87cd826c3f61565d1b9dd52a69a15636d080852a62518f0bada865675d90026d78bdc1c210fa55939a81e4866ba894e40
-DIST Mojolicious-9.23.tar.gz 846183 BLAKE2B 47cc37c5783354ed54a1954ce60453f3a735f8f12775e2ec9decb1c4e34542f4ebd67386b478d2058c874af2694f746fddeb66f21b161f82b57b1a0b1e77d5d8 SHA512 065a4348c759a8b820ccff743ef3ebf3f87dafa841222a49cdc1736faea493c27a7a3aec60f5a921dcb7ce51c32ade8350a582de4261f1b7d98d70b0fa6a1ae5
+DIST Mojolicious-9.33.tar.gz 854105 BLAKE2B b0f2981c089822a0b64f6b3aa19b2f079da4922e4cd58beadd00ebfe34fddfb6c80e102270998b40ae0c425dee6875310648a1004ee36dc6de8bb756b448a822 SHA512 331df20e96e5b0cc647bb8608c99347c6191ee10b4741dee1dd0aaeb5808b704a0e84bb2e944e5fc4bbb1561a701b20cc3e0ba5c2804b089d10dcaab6ad87c1f

dev-perl/Mojolicious/Mojolicious-9.330.0-r1.ebuild
@@ -4,7 +4,7 @@
 EAPI=8
 
 DIST_AUTHOR=SRI
-DIST_VERSION=9.23
+DIST_VERSION=9.33
 DIST_EXAMPLES=("examples/*")
 
 inherit perl-module