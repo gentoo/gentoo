gentoo/gentoo

commit 5e64b2e22c37dbcb065c8e113fa22ecd7e86e2b2

net-vpn/networkmanager-openconnect: Version bump to 1.2.10
Signed-off-by: Guillermo Joandet <gjoandet@gmail.com>
Closes: https://github.com/gentoo/gentoo/pull/31092
Signed-off-by: Matt Turner <mattst88@gentoo.org>

net-vpn/networkmanager-openconnect/networkmanager-openconnect-1.2.10.ebuild
@@ -1,0 +1,64 @@
+# Copyright 1999-2023 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+GNOME_ORG_MODULE="NetworkManager-${PN##*-}"
+
+inherit gnome2
+
+DESCRIPTION="NetworkManager OpenConnect plugin"
+HOMEPAGE="https://wiki.gnome.org/Projects/NetworkManager https://gitlab.gnome.org/GNOME/NetworkManager-openconnect"
+
+LICENSE="GPL-2+"
+SLOT="0"
+KEYWORDS="~amd64 ~arm ~arm64 ~loong ~ppc64 ~riscv ~x86"
+IUSE="gtk"
+
+DEPEND="
+	>=net-misc/networkmanager-1.2:=
+	>=dev-libs/glib-2.34:2
+	>=dev-libs/dbus-glib-0.74
+	dev-libs/libxml2:2
+	dev-libs/glib:2
+	>=net-vpn/openconnect-3.02:=
+	gtk? (
+		>=app-crypt/libsecret-0.18
+
+		>=app-crypt/gcr-3.4:0=
+		>=x11-libs/gtk+-3.12:3
+
+		>=gui-libs/gtk-4.0:4
+		>=net-libs/libnma-1.8.36
+		net-libs/webkit-gtk:4.1
+	)
+"
+
+RDEPEND="
+	${DEPEND}
+	acct-group/nm-openconnect
+	acct-user/nm-openconnect
+"
+
+BDEPEND="
+	sys-devel/gettext
+	dev-util/intltool
+	virtual/pkgconfig
+"
+
+PATCHES+=(
+	"${FILESDIR}"/${P}-check-webkit-if-gtk.patch
+)
+
+src_configure() {
+	local myconf=(
+	gnome2_src_configure
+		--disable-more-warnings
+		--disable-static
+		--without-libnm-glib
+		$(use_with gtk gnome)
+		$(use_with gtk authdlg)
+		$(use_with gtk gtk4)
+	)
+	econf "${myconf[@]}"
+}

net-vpn/networkmanager-openconnect/Manifest
@@ -1,1 +1,2 @@
+DIST NetworkManager-openconnect-1.2.10.tar.xz 920560 BLAKE2B 8e631bacf7ac99bf3abec6eeea9bfda070dfeac4572280b74771746942bf9700f8d943501fc63933052bf07735751f5353fcc6ccd6654b57a893ed87a442650f SHA512 c61f5edee04475b013afbb91ba11e102cf4c925a0b2e16477473e0de9528d793121c1324f2c4d5ae03e0c8fb00dd9de444130c22e681f7a3408bc9a179ad7509
 DIST NetworkManager-openconnect-1.2.8.tar.xz 857808 BLAKE2B b9dd1f75eb34b3149b1d8520fa34caec9547c581affd8065116376f5ee9b0332dc8791af63a88b918445b17b9fd5655b5c154a32c150da62a015ae94221c4634 SHA512 5209fb15ce22511f9b9ccde5e19c86136c3236490c4793afcc2d4081ad87dc4c5fde7f6335db1f50cbbb674b1afc7c42a8c89a6d046ba3945691b2dfb3691d38

net-vpn/networkmanager-openconnect/files/networkmanager-openconnect-1.2.10-check-webkit-if-gtk.patch
@@ -1,0 +1,26 @@
+# https://gitlab.gnome.org/GNOME/NetworkManager-openconnect/-/issues/99
+# diff --git a/configure.ac b/configure.ac
+--- a/configure.ac	2023-05-21 14:05:26.029361118 -0300
++++ b/configure.ac	2023-05-21 14:07:12.505617426 -0300
+@@ -145,10 +145,6 @@
+ 	PKG_CHECK_MODULES(LIBNMA_GTK4, libnma-gtk4 >= 1.8.33)
+ fi
+ 
+-PKG_CHECK_MODULES(WEBKIT, webkit2gtk-4.1, [wekbit=4.1],
+-			  [PKG_CHECK_MODULES(WEBKIT, webkit2gtk-4.0,
+-			  [webkit=4.0], AC_MSG_ERROR(Neither webkit2gtk-4.0 nor wekit2gtk-4.1 found))])
+-
+ PKG_CHECK_MODULES(LIBNM, libnm >= 1.2.0)
+ LIBNM_CFLAGS="$LIBNM_CFLAGS -DNM_VERSION_MIN_REQUIRED=NM_VERSION_1_2"
+ LIBNM_CFLAGS="$LIBNM_CFLAGS -DNM_VERSION_MAX_ALLOWED=NM_VERSION_1_4"
+@@ -166,6 +162,10 @@
+ fi
+ if test x"$with_authdlg" != xno; then
+ 	PKG_CHECK_MODULES(OPENCONNECT, openconnect >= 3.02)
++	PKG_CHECK_MODULES(WEBKIT, webkit2gtk-4.1, [wekbit=4.1],
++			  [PKG_CHECK_MODULES(WEBKIT, webkit2gtk-4.0,
++			  [webkit=4.0], AC_MSG_ERROR(Neither webkit2gtk-4.0 nor wekit2gtk-4.1 found))])
++
+ fi
+ AM_CONDITIONAL(WITH_AUTHDLG, test x"$with_authdlg" != xno)
+ 

commit b18c4ce529af51f5308d56d8e757efa9450db87f

net-vpn/networkmanager-openconnect: run eautoreconf before configure Need to run autoreconf to regenerate configure script to apply changes from patch
Fixes: 5e64b2e22c37dbcb065c8e113fa22ecd7e86e2b2 (net-vpn/networkmanager-openconnect: Version bump to 1.2.10)
Bug: 906947

Signed-off-by: Maksym Chukhrii <cmvu@protonmail.com>

net-vpn/networkmanager-openconnect/networkmanager-openconnect-1.2.10.ebuild
@@ -5,7 +5,7 @@ EAPI=8
 
 GNOME_ORG_MODULE="NetworkManager-${PN##*-}"
 
-inherit gnome2
+inherit gnome2 autotools
 
 DESCRIPTION="NetworkManager OpenConnect plugin"
 HOMEPAGE="https://wiki.gnome.org/Projects/NetworkManager https://gitlab.gnome.org/GNOME/NetworkManager-openconnect"
@@ -51,6 +51,7 @@ PATCHES+=(
 )
 
 src_configure() {
+	eautoreconf
 	local myconf=(
 	gnome2_src_configure
 		--disable-more-warnings