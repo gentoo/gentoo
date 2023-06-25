gentoo/gentoo

commit 5a80377b5bf000d2efbc5ee3a5732c19c010a7a9

net-misc/openssh: drop elibc_Cygwin cruft
Support is already gone.

Closes: https://bugs.gentoo.org/909154
Signed-off-by: Sam James <sam@gentoo.org>

net-misc/openssh/openssh-9.3_p1-r1.ebuild
@@ -185,6 +185,7 @@ src_configure() {
 		--datadir="${EPREFIX}"/usr/share/openssh
 		--with-privsep-path="${EPREFIX}"/var/empty
 		--with-privsep-user=sshd
+		--enable-hardening
 		$(use_with audit audit linux)
 		$(use_with kerberos kerberos5 "${EPREFIX}"/usr)
 		$(use_with ldns)
@@ -195,7 +196,6 @@ src_configure() {
 		$(use_with security-key security-key-builtin)
 		$(use_with ssl openssl)
 		$(use_with ssl ssl-engine)
-		$(use_with !elibc_Cygwin hardening) #659210
 	)
 
 	if use elibc_musl; then

commit 912850f59174a65693859c4a171ef5e98fbdab6b

net-misc/openssh: fix configure arg
Fixes: 5a80377b5bf000d2efbc5ee3a5732c19c010a7a9
Signed-off-by: Sam James <sam@gentoo.org>

net-misc/openssh/openssh-9.3_p1-r1.ebuild
@@ -185,7 +185,7 @@ src_configure() {
 		--datadir="${EPREFIX}"/usr/share/openssh
 		--with-privsep-path="${EPREFIX}"/var/empty
 		--with-privsep-user=sshd
-		--enable-hardening
+		--with-hardening
 		$(use_with audit audit linux)
 		$(use_with kerberos kerberos5 "${EPREFIX}"/usr)
 		$(use_with ldns)