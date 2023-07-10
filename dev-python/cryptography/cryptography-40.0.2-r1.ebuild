gentoo/gentoo

commit d92f1991a0f3461bf226de3e239b5588b63305b0

dev-python/cryptography: filter-lto
Closes: https://bugs.gentoo.org/903908
Signed-off-by: Sam James <sam@gentoo.org>

dev-python/cryptography/cryptography-41.0.1-r1.ebuild
@@ -10,87 +10,59 @@ PYTHON_COMPAT=( python3_{10..12} pypy3 )
 PYTHON_REQ_USE="threads(+)"
 
 CRATES="
-	Inflector-0.11.4
-	aliasable-0.1.3
-	android_system_properties-0.1.5
-	asn1-0.13.0
-	asn1_derive-0.13.0
-	autocfg-1.1.0
-	base64-0.13.1
-	bitflags-1.3.2
-	bumpalo-3.10.0
-	cc-1.0.79
-	cfg-if-1.0.0
-	chrono-0.4.24
-	codespan-reporting-0.11.1
-	core-foundation-sys-0.8.3
-	cxx-1.0.86
-	cxx-build-1.0.86
-	cxxbridge-flags-1.0.86
-	cxxbridge-macro-1.0.86
-	foreign-types-0.3.2
-	foreign-types-shared-0.1.1
-	iana-time-zone-0.1.54
-	iana-time-zone-haiku-0.1.1
-	indoc-1.0.4
-	instant-0.1.12
-	js-sys-0.3.61
-	libc-0.2.140
-	link-cplusplus-1.0.8
-	lock_api-0.4.9
-	log-0.4.17
-	memoffset-0.8.0
-	num-integer-0.1.45
-	num-traits-0.2.15
-	once_cell-1.14.0
-	openssl-0.10.50
-	openssl-macros-0.1.0
-	openssl-sys-0.9.85
-	ouroboros-0.15.6
-	ouroboros_macro-0.15.6
-	parking_lot-0.11.2
-	parking_lot_core-0.8.6
-	pem-1.1.1
-	pkg-config-0.3.26
-	proc-macro-error-1.0.4
-	proc-macro-error-attr-1.0.4
-	proc-macro2-1.0.53
-	pyo3-0.18.3
-	pyo3-build-config-0.18.3
-	pyo3-ffi-0.18.3
-	pyo3-macros-0.18.3
-	pyo3-macros-backend-0.18.3
-	quote-1.0.26
-	redox_syscall-0.2.16
-	scopeguard-1.1.0
-	scratch-1.0.5
-	smallvec-1.10.0
-	syn-1.0.109
-	target-lexicon-0.12.4
-	termcolor-1.2.0
-	unicode-ident-1.0.8
-	unicode-width-0.1.10
-	unindent-0.1.11
-	vcpkg-0.2.15
-	version_check-0.9.4
-	wasm-bindgen-0.2.84
-	wasm-bindgen-backend-0.2.84
-	wasm-bindgen-macro-0.2.84
-	wasm-bindgen-macro-support-0.2.84
-	wasm-bindgen-shared-0.2.84
-	winapi-0.3.9
-	winapi-i686-pc-windows-gnu-0.4.0
-	winapi-util-0.1.5
-	winapi-x86_64-pc-windows-gnu-0.4.0
-	windows-0.46.0
-	windows-targets-0.42.2
-	windows_aarch64_gnullvm-0.42.2
-	windows_aarch64_msvc-0.42.2
-	windows_i686_gnu-0.42.2
-	windows_i686_msvc-0.42.2
-	windows_x86_64_gnu-0.42.2
-	windows_x86_64_gnullvm-0.42.2
-	windows_x86_64_msvc-0.42.2
+	Inflector@0.11.4
+	aliasable@0.1.3
+	asn1@0.15.2
+	asn1_derive@0.15.2
+	autocfg@1.1.0
+	base64@0.13.1
+	bitflags@1.3.2
+	cc@1.0.79
+	cfg-if@1.0.0
+	foreign-types-shared@0.1.1
+	foreign-types@0.3.2
+	indoc@1.0.9
+	libc@0.2.144
+	lock_api@0.4.9
+	memoffset@0.8.0
+	once_cell@1.17.2
+	openssl-macros@0.1.1
+	openssl-sys@0.9.88
+	openssl@0.10.54
+	ouroboros@0.15.6
+	ouroboros_macro@0.15.6
+	parking_lot@0.12.1
+	parking_lot_core@0.9.7
+	pem@1.1.1
+	pkg-config@0.3.27
+	proc-macro-error-attr@1.0.4
+	proc-macro-error@1.0.4
+	proc-macro2@1.0.59
+	pyo3-build-config@0.18.3
+	pyo3-ffi@0.18.3
+	pyo3-macros-backend@0.18.3
+	pyo3-macros@0.18.3
+	pyo3@0.18.3
+	quote@1.0.28
+	redox_syscall@0.2.16
+	scopeguard@1.1.0
+	smallvec@1.10.0
+	syn@1.0.109
+	syn@2.0.18
+	target-lexicon@0.12.7
+	unicode-ident@1.0.9
+	unindent@0.1.11
+	vcpkg@0.2.15
+	version_check@0.9.4
+	windows-sys@0.45.0
+	windows-targets@0.42.2
+	windows_aarch64_gnullvm@0.42.2
+	windows_aarch64_msvc@0.42.2
+	windows_i686_gnu@0.42.2
+	windows_i686_msvc@0.42.2
+	windows_x86_64_gnu@0.42.2
+	windows_x86_64_gnullvm@0.42.2
+	windows_x86_64_msvc@0.42.2
 "
 
 inherit cargo distutils-r1 multiprocessing pypi
@@ -102,8 +74,7 @@ HOMEPAGE="
 	https://pypi.org/project/cryptography/
 "
 SRC_URI+="
-	https://dev.gentoo.org/~mgorny/dist/${P}-pyo3-0.18.patch.bz2
-	$(cargo_crate_uris ${CRATES})
+	${CARGO_CRATE_URIS}
 	test? (
 		$(pypi_sdist_url cryptography_vectors "$(ver_cut 1-3)")
 	)
@@ -116,7 +87,7 @@ LICENSE+="
 	Unicode-DFS-2016
 "
 SLOT="0"
-KEYWORDS="amd64 arm arm64 ppc ppc64 ~riscv ~s390 sparc x86"
+KEYWORDS="~amd64 ~arm ~arm64 ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86"
 
 RDEPEND="
 	>=dev-libs/openssl-1.0.2o-r6:0=
@@ -150,10 +121,6 @@ src_unpack() {
 }
 
 src_prepare() {
-	local PATCHES=(
-		"${WORKDIR}/${P}-pyo3-0.18.patch"
-	)
-
 	default
 
 	sed -i -e 's:--benchmark-disable::' pyproject.toml || die

dev-python/cryptography/cryptography-41.0.1.ebuild
@@ -136,6 +136,10 @@ src_prepare() {
 	fi
 }
 
+python_configure_all() {
+	filter-lto # bug #903908
+}
+
 python_test() {
 	local -x PYTHONPATH="${PYTHONPATH}:${WORKDIR}/cryptography_vectors-${PV}"
 	local EPYTEST_IGNORE=(

commit ba9d213f4a0a2ff78c5e94e9bc3760910db8d92e

dev-python/cryptography: fix MissingInherits
Ran my archtesting alias too soon :(

Fixes: d92f1991a0f3461bf226de3e239b5588b63305b0
Signed-off-by: Sam James <sam@gentoo.org>

dev-python/cryptography/cryptography-40.0.2-r1.ebuild
@@ -93,7 +93,7 @@ CRATES="
 	windows_x86_64_msvc-0.42.2
 "
 
-inherit cargo distutils-r1 multiprocessing pypi
+inherit cargo distutils-r1 flag-o-matic multiprocessing pypi
 
 VEC_P=cryptography_vectors-$(ver_cut 1-3)
 DESCRIPTION="Library providing cryptographic recipes and primitives"

dev-python/cryptography/cryptography-41.0.1-r1.ebuild
@@ -65,7 +65,7 @@ CRATES="
 	windows_x86_64_msvc@0.42.2
 "
 
-inherit cargo distutils-r1 multiprocessing pypi
+inherit cargo distutils-r1 flag-o-matic multiprocessing pypi
 
 VEC_P=cryptography_vectors-$(ver_cut 1-3)
 DESCRIPTION="Library providing cryptographic recipes and primitives"

dev-python/cryptography/cryptography-41.0.1.ebuild
@@ -65,7 +65,7 @@ CRATES="
 	windows_x86_64_msvc@0.42.2
 "
 
-inherit cargo distutils-r1 multiprocessing pypi
+inherit cargo distutils-r1 flag-o-matic multiprocessing pypi
 
 VEC_P=cryptography_vectors-$(ver_cut 1-3)
 DESCRIPTION="Library providing cryptographic recipes and primitives"