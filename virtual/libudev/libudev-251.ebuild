gentoo/gentoo

commit 04a137e8ed2c29610b1872b76e0b1b8d8b8f4546

virtual/libudev: Version bump to 251
Signed-off-by: Matt Turner <mattst88@gentoo.org>

virtual/libudev/libudev-251.ebuild
@@ -1,0 +1,19 @@
+# Copyright 1999-2023 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit multilib-build
+
+DESCRIPTION="Virtual for libudev providers"
+
+SLOT="0/1"
+KEYWORDS="~alpha amd64 arm arm64 hppa ~ia64 ~loong ~m68k ~mips ppc ppc64 ~riscv ~s390 sparc x86"
+IUSE="systemd"
+
+# eudev does not provide v251 APIs, see
+# https://github.com/eudev-project/eudev/issues/249
+RDEPEND="
+	!systemd? ( >=sys-apps/systemd-utils-251[udev,${MULTILIB_USEDEP}] )
+	systemd? ( >=sys-apps/systemd-251:0/2[${MULTILIB_USEDEP}] )
+"

commit 7fddd76a09db930ba892644870fdae6861ef18e8

virtual/libudev: destabilize 251
See https://forums.gentoo.org/viewtopic-t-1164332.html, let's not inflict
the conflict wrt eudev on more people than necessary while we see what's
going to happen.

Fixes: 04a137e8ed2c29610b1872b76e0b1b8d8b8f4546
Signed-off-by: Sam James <sam@gentoo.org>

virtual/libudev/libudev-251.ebuild
@@ -8,7 +8,7 @@ inherit multilib-build
 DESCRIPTION="Virtual for libudev providers"
 
 SLOT="0/1"
-KEYWORDS="~alpha amd64 arm arm64 hppa ~ia64 ~loong ~m68k ~mips ppc ppc64 ~riscv ~s390 sparc x86"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~m68k ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86"
 IUSE="systemd"
 
 # eudev does not provide v251 APIs, see