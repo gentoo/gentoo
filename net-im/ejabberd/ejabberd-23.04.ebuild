gentoo/gentoo

commit 6524848310ecbab4736508fb3ea282deddabaefe

app-text/kramdown-rfc: add 1.6.39
Signed-off-by: Florian Schmaus <flow@gentoo.org>

net-im/ejabberd/ejabberd-23.04.ebuild
@@ -17,7 +17,7 @@ KEYWORDS="~amd64 ~arm ~ia64 ~sparc ~x86"
 REQUIRED_USE="mssql? ( odbc )"
 # TODO: Add 'tools' flag.
 IUSE="captcha debug full-xml ldap mssql mysql odbc pam postgres redis
-	roster-gw selinux sip sqlite +stun zlib"
+	roster-gw selinux sip sqlite +stun +tools zlib"
 
 RESTRICT="test"
 
@@ -129,6 +129,7 @@ src_configure() {
 		$(use_enable roster-gw roster-gateway-workaround) \
 		$(use_enable sqlite) \
 		$(use_enable sip) \
+		$(use_enable tools) \
 		$(use_enable stun) \
 		$(use_enable zlib)
 

app-text/kramdown-rfc/Manifest
@@ -1,2 +1,3 @@
 DIST kramdown-rfc2629-1.6.27.gem 57344 BLAKE2B e43d46a02e804268ebf5697fde7948c1c1e90f2cb33f0aa66c93c629e6f68f7932ecf02cd7169c93d06f21ee77e9c98b2717c6c18629fc78d2532098ef4e4f5f SHA512 7ad473bf9221f8369afaa6374751d77931f12e7af3500576da7567cc4e36844349de412626e735432dea098d71d155cb1fd1d87f9e3ea053bebd3be84da81c17
 DIST kramdown-rfc2629-1.6.36.gem 60928 BLAKE2B e1f5a3299ddca462b7ab3935019b95f58aebfc5de52271142b41cbf03102a574181b0d29d5701e0f6b710eb7c8b85dda0f9700b8e47698b6d079cd8c07d720b0 SHA512 d3c32bd0e692ebf5dfb428617ce6ae37ac91ff99021a59e11df205e88c76699c8fabd8dd7922f6e9a5653b12e3c113eeedbb38b6edd656d67cd16b5f03a3136e
+DIST kramdown-rfc2629-1.6.39.gem 62464 BLAKE2B 4063bab5ef8081c5a2aba3d881990d9ea4e6dd5ce01bf85e0ce6bc63b0fc6c1e8e9be413b00588c5e6e473ee26dfb6060aa8767ce6b4053c1b8750f79182e540 SHA512 bdfc611895f734a28e65b8d6b750fec387bb504d35feb8c6bbf4dbaf83b19b65fb61187e9b5e58ec64dbe72cbc9cc6efa78a349752340634ef2dc565bb096589

app-text/kramdown-rfc/kramdown-rfc-1.6.39.ebuild
@@ -1,0 +1,41 @@
+# Copyright 1999-2023 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+USE_RUBY="ruby31"
+
+# The software got renamed from kramdown-rfc2629 to kramdown-rfc,
+# however the gem coordinate is still kramdown-rfc2629.
+RUBY_FAKEGEM_NAME="${PN}2629"
+RUBY_FAKEGEM_RECIPE_TEST="none"
+RUBY_FAKEGEM_EXTRADOC="README.md"
+RUBY_FAKEGEM_EXTRAINSTALL="data"
+# Explicitly use the gemspec file over the metadata file of the gem, as
+# the latter contains dependencies that we patched out.
+RUBY_FAKEGEM_GEMSPEC="${RUBY_FAKEGEM_NAME}.gemspec"
+
+inherit ruby-fakegem
+
+DESCRIPTION="An XML2RFC (RFC799x) backend for Thomas Leitner's kramdown markdown parser"
+HOMEPAGE="https://github.com/cabo/kramdown-rfc"
+
+LICENSE="MIT"
+SLOT="0"
+KEYWORDS="~amd64"
+
+PATCHES=(
+	"${FILESDIR}"/${PN}-1.6.27-Drop-dependency-on-certified.patch
+	"${FILESDIR}"/${PN}-1.6.35-Remove-bin-echars.patch
+)
+
+ruby_add_rdepend "
+	>=dev-ruby/json-2.0.0
+	>=dev-ruby/kramdown-2.4.0
+	>=dev-ruby/kramdown-parser-gfm-1.1.0
+	>=dev-ruby/net-http-persistent-4.0
+"
+
+all_ruby_prepare() {
+	sed -i 's/json_pure/json/' "${RUBY_FAKEGEM_GEMSPEC}" || die
+}

commit 04b5be7df1595fb7fc1d6b164423b7048e51d09b

net-im/ejabberd: revert accidentally pushed changes
Fixes: 6524848310ec ("app-text/kramdown-rfc: add 1.6.39")
Signed-off-by: Florian Schmaus <flow@gentoo.org>

net-im/ejabberd/ejabberd-23.04.ebuild
@@ -17,7 +17,7 @@ KEYWORDS="~amd64 ~arm ~ia64 ~sparc ~x86"
 REQUIRED_USE="mssql? ( odbc )"
 # TODO: Add 'tools' flag.
 IUSE="captcha debug full-xml ldap mssql mysql odbc pam postgres redis
-	roster-gw selinux sip sqlite +stun +tools zlib"
+	roster-gw selinux sip sqlite +stun zlib"
 
 RESTRICT="test"
 
@@ -129,7 +129,6 @@ src_configure() {
 		$(use_enable roster-gw roster-gateway-workaround) \
 		$(use_enable sqlite) \
 		$(use_enable sip) \
-		$(use_enable tools) \
 		$(use_enable stun) \
 		$(use_enable zlib)
 