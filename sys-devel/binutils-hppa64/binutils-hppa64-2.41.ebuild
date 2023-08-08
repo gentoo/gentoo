gentoo/gentoo

commit dcdef53cc104310eb50e31e3a450cd54c22a18d0

sys-devel/binutils-hppa64: pass --disable-dependency-tracking, --disable-silent-rules
We already do this for gdb, do the same for binutils. Portage/econf only greps
the top-level configure which has neither of these, but the subconfigure dirs
do.

Signed-off-by: Sam James <sam@gentoo.org>

sys-devel/binutils-hppa64/binutils-hppa64-2.41.ebuild
@@ -241,6 +241,11 @@ src_configure() {
 		--libdir="${EPREFIX}"${LIBPATH}
 		--libexecdir="${EPREFIX}"${LIBPATH}
 		--includedir="${EPREFIX}"${INCPATH}
+		# portage's econf() does not detect presence of --d-d-t
+		# because it greps only top-level ./configure. But not
+		# libiberty's or bfd's configure.
+		--disable-dependency-tracking
+		--disable-silent-rules
 		--enable-obsolete
 		--enable-shared
 		--enable-threads

commit 91736e72744ca4736f26c0331c8f4f3ffb8f5d8f

sys-devel/binutils-hppa64: cleanup obsolete V=1
Fixes: dcdef53cc104310eb50e31e3a450cd54c22a18d0
Signed-off-by: Sam James <sam@gentoo.org>

sys-devel/binutils-hppa64/binutils-hppa64-2.41.ebuild
@@ -328,11 +328,11 @@ src_compile() {
 	cd "${MY_BUILDDIR}" || die
 
 	# see Note [tooldir hack for ldscripts]
-	emake V=1 tooldir="${EPREFIX}${TOOLPATH}" all
+	emake tooldir="${EPREFIX}${TOOLPATH}" all
 
 	# only build info pages if the user wants them
 	if use doc ; then
-		emake V=1 info
+		emake info
 	fi
 
 	# we nuke the manpages when we're left with junk
@@ -346,7 +346,7 @@ src_test() {
 	# bug #637066
 	filter-flags -Wall -Wreturn-type
 
-	emake -k V=1 check
+	emake -k check
 }
 
 src_install() {
@@ -355,7 +355,7 @@ src_install() {
 	cd "${MY_BUILDDIR}" || die
 
 	# see Note [tooldir hack for ldscripts]
-	emake V=1 DESTDIR="${D}" tooldir="${EPREFIX}${LIBPATH}" install
+	emake DESTDIR="${D}" tooldir="${EPREFIX}${LIBPATH}" install
 	rm -rf "${ED}"/${LIBPATH}/bin || die
 	use static-libs || find "${ED}" -name '*.la' -delete
 