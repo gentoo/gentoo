gentoo/gentoo

commit 6fa2a9610635912a3b9982c10273fab8b70573c1

licenses: Rename 9base to LPL-1.2
Delete initial text which isn't part of the license itself.
Add LPL-1.2 to license-mapping.conf.

Update all ebuilds using it.

Signed-off-by: Ulrich Müller <ulm@gentoo.org>

dev-perl/DBIx-Simple/DBIx-Simple-1.370.0.ebuild
@@ -12,7 +12,7 @@ DESCRIPTION="Very complete easy-to-use OO interface to DBI"
 # Upstream says "Any OSI approved license from https://opensource.org/licenses/alphabetical"
 LICENSE="|| ( AFL-3.0 AGPL-3 APL-1.0 Apache-2.0 BSD-2 Boost-1.0 CDDL CPAL-1.0
 	EPL-1.0 ECL-2.0 EUPL-1.1 GPL-2 GPL-3 LGPL-2.1 LGPL-3 HPND IBM IPAfont ISC
-	LPPL-1.3c 9base MIT MPL-2.0 NOSA nethack PHP-3 POSTGRESQL PYTHON CNRI
+	LPPL-1.3c LPL-1.2 MIT MPL-2.0 NOSA nethack PHP-3 POSTGRESQL PYTHON CNRI
 	QPL-1.0 OFL-1.1 Sleepycat Watcom-1.0 W3C wxWinLL-3 ZLIB libpng
 )"
 SLOT="0"

metadata/license-mapping.conf
@@ -152,6 +152,7 @@ LGPL-3.0-or-later = LGPL-3+
 Libpng = libpng
 libpng-2.0 = libpng2
 libtiff = libtiff
+LPL-1.2 = LPL-1.2
 LPPL-1.2 = LPPL-1.2
 LPPL-1.3c = LPPL-1.3c
 MirOS = MirOS

licenses/LPL-1.2
@@ -1,27 +1,3 @@
-The Plan 9 software is provided under the terms of the
-Lucent Public License, Version 1.02, reproduced below,
-with the following notable exceptions:
-
-1. No right is granted to create derivative works of or
-   to redistribute (other than with the Plan 9 Operating System)
-   the screen imprinter fonts identified in subdirectory
-   /lib/font/bit/lucida and printer fonts (Lucida Sans Unicode, Lucida
-   Sans Italic, Lucida Sans Demibold, Lucida Typewriter, Lucida Sans
-   Typewriter83), identified in subdirectory /sys/lib/postscript/font.
-   These directories contain material copyrights by B&H Inc. and Y&Y Inc.
-
-2. The printer fonts identified in subdirectory /sys/lib/ghostscript/font
-   are subject to the GNU GPL, reproduced in the file /LICENSE.gpl.
-
-3. The ghostscript program in the subdirectory /sys/src/cmd/gs is
-   covered by the Aladdin Free Public License, reproduced in the file
-   /LICENSE.afpl.
-
-Other, less notable exceptions are marked in the file tree with
-COPYING, COPYRIGHT, or LICENSE files.
-
-===================================================================
-
 Lucent Public License Version 1.02
 
 THE ACCOMPANYING PROGRAM IS PROVIDED UNDER THE TERMS OF THIS PUBLIC
@@ -153,7 +129,7 @@ create potential liability for Contributors. Therefore, if a
 Distributor includes the Program in a commercial product offering,
 such Distributor ("Commercial Distributor") hereby agrees to defend
 and indemnify every Contributor ("Indemnified Contributor") against
-any losses, damages and costs (collectively"Losses") arising from
+any losses, damages and costs (collectively "Losses") arising from
 claims, lawsuits and other legal actions brought by a third party
 against the Indemnified Contributor to the extent caused by the acts
 or omissions of such Commercial Distributor in connection with its
@@ -181,7 +157,7 @@ damages.
 5. NO WARRANTY
 
 EXCEPT AS EXPRESSLY SET FORTH IN THIS AGREEMENT, THE PROGRAM IS
-PROVIDED ON AN"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 KIND, EITHER EXPRESS OR IMPLIED INCLUDING, WITHOUT LIMITATION, ANY
 WARRANTIES OR CONDITIONS OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY
 OR FITNESS FOR A PARTICULAR PURPOSE. Each Recipient is solely

sys-apps/heirloom-tools/heirloom-tools-070715-r5.ebuild
@@ -10,7 +10,7 @@ HOMEPAGE="https://heirloom.sourceforge.net/tools.html"
 SRC_URI="mirror://sourceforge/heirloom/heirloom/${PV}/heirloom-${PV}.tar.bz2"
 S="${WORKDIR}/heirloom-${PV}"
 
-LICENSE="CDDL GPL-2+ LGPL-2.1+ 9base ZLIB"
+LICENSE="CDDL GPL-2+ LGPL-2.1+ LPL-1.2 ZLIB"
 SLOT="0"
 KEYWORDS="~amd64 ~x86"
 # slightly broken

commit e7d4d57ddad22ef5e99e05ef69f9e847b04cd534

licenses: The Lucent Public License is version 1.02, not 1.2
Fixes: 6fa2a9610635912a3b9982c10273fab8b70573c1
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

dev-perl/DBIx-Simple/DBIx-Simple-1.370.0.ebuild
@@ -12,7 +12,7 @@ DESCRIPTION="Very complete easy-to-use OO interface to DBI"
 # Upstream says "Any OSI approved license from https://opensource.org/licenses/alphabetical"
 LICENSE="|| ( AFL-3.0 AGPL-3 APL-1.0 Apache-2.0 BSD-2 Boost-1.0 CDDL CPAL-1.0
 	EPL-1.0 ECL-2.0 EUPL-1.1 GPL-2 GPL-3 LGPL-2.1 LGPL-3 HPND IBM IPAfont ISC
-	LPPL-1.3c LPL-1.2 MIT MPL-2.0 NOSA nethack PHP-3 POSTGRESQL PYTHON CNRI
+	LPPL-1.3c LPL-1.02 MIT MPL-2.0 NOSA nethack PHP-3 POSTGRESQL PYTHON CNRI
 	QPL-1.0 OFL-1.1 Sleepycat Watcom-1.0 W3C wxWinLL-3 ZLIB libpng
 )"
 SLOT="0"

metadata/license-mapping.conf
@@ -152,7 +152,7 @@ LGPL-3.0-or-later = LGPL-3+
 Libpng = libpng
 libpng-2.0 = libpng2
 libtiff = libtiff
-LPL-1.2 = LPL-1.2
+LPL-1.02 = LPL-1.02
 LPPL-1.2 = LPPL-1.2
 LPPL-1.3c = LPPL-1.3c
 MirOS = MirOS

profiles/license_groups
@@ -18,14 +18,14 @@
 GPL-COMPATIBLE AGPL-3 AGPL-3+ Apache-2.0 Apache-2.0-with-LLVM-exceptions Artistic-2 Boost-1.0 BSD BSD-2 CC0-1.0 CeCILL-2 Clarified-Artistic Clear-BSD ECL-2.0 FSFAP FTL gcc-runtime-library-exception-3.1 GPL-1 GPL-1+ GPL-2 GPL-2+ GPL-2+-with-openssl-exception GPL-2+-with-eCos-exception-2 GPL-2-with-classpath-exception GPL-2-with-exceptions GPL-2-with-font-exception GPL-2-with-linking-exception GPL-2-with-MySQL-FLOSS-exception GPL-2+-with-Pyinstaller-Bootloader-exception GPL-3 GPL-3+ GPL-3+-with-autoconf-exception GPL-3+-with-font-exception GPL-3-with-font-exception GPL-3-with-openssl-exception HPND IJG ISC LGPL-2 LGPL-2+ LGPL-2-with-linking-exception LGPL-2.1 LGPL-2.1+ LGPL-2.1-with-linking-exception LGPL-3 LGPL-3+ LGPL-3-with-linking-exception libgcc libstdc++ metapackage MIT MPL-2.0 Nokia-Qt-LGPL-Exception-1.1 OPENLDAP PSF-2 PSF-2.4 public-domain PYTHON qwt Ruby Ruby-BSD SGI-B-2.0 Sleepycat tanuki-community Transmission-OpenSSL-exception unicode Unlicense UoI-NCSA UPX-exception vim W3C WTFPL-2 wxWinLL-3.1 ZLIB ZPL
 
 # Free software licenses approved by the FSF
-FSF-APPROVED @GPL-COMPATIBLE AFL-2.1 AFL-3.0 Apache-1.0 Apache-1.1 APSL-2 BSD-4 CDDL CeCILL-B CeCILL-C CNRI CPAL-1.0 CPL-1.0 EPL-1.0 EPL-2.0 EUPL-1.1 EUPL-1.2 FraunhoferFDK gnuplot IBM LPL-1.2 LPPL-1.2 MPL-1.0 MPL-1.1 Ms-PL Ms-RL NPL-1.1 openssl OSL-1.1 OSL-2.0 OSL-2.1 PHP-3.01 QPL-1.0 Zend-2.0
+FSF-APPROVED @GPL-COMPATIBLE AFL-2.1 AFL-3.0 Apache-1.0 Apache-1.1 APSL-2 BSD-4 CDDL CeCILL-B CeCILL-C CNRI CPAL-1.0 CPL-1.0 EPL-1.0 EPL-2.0 EUPL-1.1 EUPL-1.2 FraunhoferFDK gnuplot IBM LPL-1.02 LPPL-1.2 MPL-1.0 MPL-1.1 Ms-PL Ms-RL NPL-1.1 openssl OSL-1.1 OSL-2.0 OSL-2.1 PHP-3.01 QPL-1.0 Zend-2.0
 
 # Licenses approved by the Open Source Initiative
 # https://www.opensource.org/licenses
 # Note that the FSF lists these as nonfree: Artistic NOSA Watcom-1.0
 # We exclude NOSA and Watcom-1.0, but we keep Artistic because Debian
 # considers it DFSG-free.
-OSI-APPROVED-FREE 0BSD AFL-3.0 AGPL-3 AGPL-3+ Apache-1.1 Apache-2.0 APL-1.0 APSL-2 Artistic Artistic-2 Boost-1.0 BSD BSD-2 CDDL CNRI CPAL-1.0 CPL-1.0 ECL-2.0 EPL-1.0 EPL-2.0 EUPL-1.1 EUPL-1.2 GPL-1+ GPL-2 GPL-2+ GPL-3 GPL-3+ HPND IBM IPAfont ISC LGPL-2+ LGPL-2.1 LGPL-2.1+ LGPL-3 LGPL-3+ LPL-1.2 LPPL-1.3c MirOS MIT MIT-0 MPL-1.0 MPL-1.1 MPL-2.0 Ms-PL Ms-RL nethack OFL-1.1 OSL-2.1 PHP-3 PHP-3.01 POSTGRESQL PSF-2 QPL-1.0 Sleepycat Unicode-DFS-2016 UoI-NCSA W3C wxWinLL-3 ZLIB ZPL
+OSI-APPROVED-FREE 0BSD AFL-3.0 AGPL-3 AGPL-3+ Apache-1.1 Apache-2.0 APL-1.0 APSL-2 Artistic Artistic-2 Boost-1.0 BSD BSD-2 CDDL CNRI CPAL-1.0 CPL-1.0 ECL-2.0 EPL-1.0 EPL-2.0 EUPL-1.1 EUPL-1.2 GPL-1+ GPL-2 GPL-2+ GPL-3 GPL-3+ HPND IBM IPAfont ISC LGPL-2+ LGPL-2.1 LGPL-2.1+ LGPL-3 LGPL-3+ LPL-1.02 LPPL-1.3c MirOS MIT MIT-0 MPL-1.0 MPL-1.1 MPL-2.0 Ms-PL Ms-RL nethack OFL-1.1 OSL-2.1 PHP-3 PHP-3.01 POSTGRESQL PSF-2 QPL-1.0 Sleepycat Unicode-DFS-2016 UoI-NCSA W3C wxWinLL-3 ZLIB ZPL
 
 # Misc licenses that are probably free software, i.e. follow the
 # Free Software Definition at https://www.gnu.org/philosophy/free-sw.html

sys-apps/heirloom-tools/heirloom-tools-070715-r6.ebuild
@@ -10,7 +10,7 @@ HOMEPAGE="https://heirloom.sourceforge.net/tools.html"
 SRC_URI="mirror://sourceforge/heirloom/heirloom/${PV}/heirloom-${PV}.tar.bz2"
 S="${WORKDIR}/heirloom-${PV}"
 
-LICENSE="ZLIB BSD BSD-4 CDDL GPL-2+ LGPL-2.1+ LPL-1.2 Info-ZIP public-domain"
+LICENSE="ZLIB BSD BSD-4 CDDL GPL-2+ LGPL-2.1+ LPL-1.02 Info-ZIP public-domain"
 SLOT="0"
 KEYWORDS="~amd64 ~x86"
 # slightly broken