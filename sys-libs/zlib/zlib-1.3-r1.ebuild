gentoo/gentoo

commit 491bf8bd3ef621da49b168736a04f23aa4c7719c

sys-libs/zlib: drop macro renaming hacks
Renaming zlib's `OF()` macro has caused problems for various packages; fortunately
C23 kills K&R prototypes so its use should be dying out anyway. If anyone is still
using it, we should just patch it out entirely rather than renaming it.

Other distros have been coping fine without the rename anyway. The most important
point being that this is against POLA and leads to problems when people try to build
things using zlib that haven't been fixed yet.

See also the legendary godot patch to workaround this:
https://github.com/godotengine/godot/blob/93409b8e64a9bc3c271ab4a7489b59a43bc0d048/thirdparty/minizip/patches/unbreak-gentoo.patch.

As of zlib-1.3, zlib doesn't even use it by itself.

Bug: https://bugs.gentoo.org/383179
Signed-off-by: Sam James <sam@gentoo.org>

sys-libs/zlib/zlib-1.3-r1.ebuild
@@ -130,12 +130,6 @@ multilib_src_compile() {
 	use minizip && emake -C contrib/minizip
 }
 
-sed_macros() {
-	# Clean up namespace a little, bug #383179
-	# We do it here so we only have to tweak 2 files
-	sed -i -r 's:\<(O[FN])\>:_Z_\1:g' "$@" || die
-}
-
 multilib_src_install() {
 	case ${CHOST} in
 		*-mingw*|mingw*)

profiles/package.mask
@@ -34,8 +34,8 @@
 #--- END OF EXAMPLES ---
 
 # Sam James <sam@gentoo.org> (2023-08-18)
-# Masked for testing. Breaks pigz at least: https://github.com/madler/pigz/issues/111.
-=sys-libs/zlib-1.3
+# Masked for testing.
+=sys-libs/zlib-1.3-r1
 
 # Jonas Stein <jstein@gentoo.org> (2023-08-17)
 # Upstream warns that it is broken and there is no fix since 2010

commit e18e4412818bcffa768a1a8df4c100f3c6288b6f

sys-libs/zlib: drop sed_macros call
Fixes: 491bf8bd3ef621da49b168736a04f23aa4c7719c
Signed-off-by: Sam James <sam@gentoo.org>

sys-libs/zlib/zlib-1.3-r1.ebuild
@@ -152,11 +152,8 @@ multilib_src_install() {
 			;;
 	esac
 
-	sed_macros "${ED}"/usr/include/*.h
-
 	if use minizip ; then
 		emake -C contrib/minizip install DESTDIR="${D}"
-		sed_macros "${ED}"/usr/include/minizip/*.h
 
 		# This might not exist if slibtool is used.
 		# bug #816756