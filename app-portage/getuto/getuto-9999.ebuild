gentoo/gentoo

commit 0087a6ddb25033d62df89895a7851e2189657ed2

app-portage/getuto: needs openssl too
Used for passphrase generation.

Signed-off-by: Sam James <sam@gentoo.org>

app-portage/getuto/getuto-9999.ebuild
@@ -18,7 +18,7 @@ else
 	KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~m68k ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86 ~amd64-linux ~x86-linux"
 fi
 
-SRC_URI+=" test? ( https://mirror.bytemark.co.uk/gentoo/releases/amd64/binpackages/17.1/x86-64/virtual/libc/libc-1-r1-1.gpkg.tar )"
+SRC_URI+=" test? ( mirror://gentoo/releases/amd64/binpackages/17.1/x86-64/virtual/libc/libc-1-r1-1.gpkg.tar )"
 
 LICENSE="GPL-2"
 SLOT="0"
@@ -27,6 +27,7 @@ RESTRICT="!test? ( test )"
 
 RDEPEND="
 	app-crypt/gnupg
+	dev-libs/openssl
 	sec-keys/openpgp-keys-gentoo-release
 "
 

app-portage/getuto/getuto-1.0.ebuild
@@ -15,6 +15,7 @@ KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~m68k ~mips ~ppc ~ppc64 ~
 
 RDEPEND="
 	app-crypt/gnupg
+	dev-libs/openssl
 	sec-keys/openpgp-keys-gentoo-release
 "
 

commit 277dc7912e384d4223374bebdc7c0013f4008672

app-portage/getuto: fix test SRC_URI
I didn't mean to commit that bit as was just testing.

Fixes: 0087a6ddb25033d62df89895a7851e2189657ed2
Signed-off-by: Sam James <sam@gentoo.org>

app-portage/getuto/getuto-9999.ebuild
@@ -18,7 +18,7 @@ else
 	KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~m68k ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86 ~amd64-linux ~x86-linux"
 fi
 
-SRC_URI+=" test? ( mirror://gentoo/releases/amd64/binpackages/17.1/x86-64/virtual/libc/libc-1-r1-1.gpkg.tar )"
+SRC_URI+=" test? ( https://mirror.bytemark.co.uk/gentoo/releases/amd64/binpackages/17.1/x86-64/virtual/libc/libc-1-r1-1.gpkg.tar )"
 
 LICENSE="GPL-2"
 SLOT="0"