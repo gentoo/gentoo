gentoo/gentoo

commit 80f7f6542c8b7bd57bbfa4b27722c33c41383b12

media-libs/alsa-ucm-conf: add 1.2.10
Signed-off-by: Sam James <sam@gentoo.org>

media-libs/alsa-ucm-conf/alsa-ucm-conf-1.2.10.ebuild
@@ -1,0 +1,24 @@
+# Copyright 1999-2023 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+DESCRIPTION="ALSA ucm configuration files"
+HOMEPAGE="https://alsa-project.org/wiki/Main_Page"
+SRC_URI="https://www.alsa-project.org/files/pub/lib/${P}.tar.bz2"
+
+LICENSE="BSD"
+SLOT="0"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~m68k ~mips ~ppc ~ppc64 ~riscv ~sparc ~x86"
+
+RDEPEND="!<media-libs/alsa-lib-1.2.1"
+DEPEND="${RDEPEND}"
+
+PATCHES=(
+	"${FILESDIR}"/${P}-fix-sof-essx8336.patch
+)
+
+src_install() {
+	insinto /usr/share/alsa
+	doins -r ucm{,2}
+}

media-libs/alsa-ucm-conf/Manifest
@@ -1,2 +1,3 @@
+DIST alsa-ucm-conf-1.2.10.tar.bz2 44749 BLAKE2B 215842f1f59c2dea5e3f3fe880dc49d6263998746a50c2591b8a9786ead3afae2c713e179ff6e8caa518b3a8516a1b031872c1fad2a17fbd3afbb0390a803693 SHA512 172956a2c127e9afa5be9c6ada43bfa9fda825823f8f6acbe252edef3381afe3adfa3299c1419f04375599be7721a551c6800be99db44b091611aeb824ffcd72
 DIST alsa-ucm-conf-1.2.8.tar.bz2 37843 BLAKE2B f2281a3a5d8b4fcbc4c75ce99a911a4ce3230abfa6f6d0aff8e175fe1b870750f43c087339490ed9f4d5ab314fda391e06e41cbed79239a16326a8f2d02ac262 SHA512 a15a9ca52c108ee95a421b91f3fad4329f1e9fb1c004a40d5ff1a57d6701bad19a31d5a296426d26439be9b87d2d543ce51588a98402d24c17f0f92aaf1096ca
 DIST alsa-ucm-conf-1.2.9.tar.bz2 42802 BLAKE2B fe71790aa8658ab12378baa55f95a03aaebd3eb0aef2535b65b89884de6cb404a75b44fd238d2089e0a005ca81720f3c44d1912452908e97aa75fcda513908cf SHA512 3aced4d4df76044a5525f1217851e05f7ad177389d2008e3e3dd0fb4d2cf72cd0897b140659a7729cc37d5b737c2d0cf5b607df4f576647044accd560d8cb87b

commit 2ea073824ea3c89746cfcdaab54c71ac5807d13e

media-libs/alsa-ucm-conf: drop redundant patch
Closes: https://bugs.gentoo.org/913485
Fixes: 80f7f6542c8b7bd57bbfa4b27722c33c41383b12
Signed-off-by: Sam James <sam@gentoo.org>

media-libs/alsa-ucm-conf/alsa-ucm-conf-1.2.10.ebuild
@@ -14,10 +14,6 @@ KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~m68k ~mips ~ppc ~ppc64 ~
 RDEPEND="!<media-libs/alsa-lib-1.2.1"
 DEPEND="${RDEPEND}"
 
-PATCHES=(
-	"${FILESDIR}"/${P}-fix-sof-essx8336.patch
-)
-
 src_install() {
 	insinto /usr/share/alsa
 	doins -r ucm{,2}