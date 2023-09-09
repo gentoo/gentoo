gentoo/gentoo

commit a2b7cc29cbb6ac57ff4cddbc39c50a060e8cac66

dev-qt/qtwebchannel: adjust qml check
Works either way but Qt6Qml produces cmake noise if
qtdeclarative is not installed.

Bug: https://bugs.gentoo.org/913692
Signed-off-by: Ionen Wolkens <ionen@gentoo.org>

dev-qt/qtwebchannel/qtwebchannel-6.5.9999.ebuild
@@ -21,7 +21,7 @@ DEPEND="${RDEPEND}"
 
 src_configure() {
 	local mycmakeargs=(
-		$(cmake_use_find_package qml Qt6Qml)
+		$(cmake_use_find_package qml Qt6Quick)
 	)
 
 	qt6-build_src_configure

dev-qt/qtwebchannel/qtwebchannel-6.9999.ebuild
@@ -21,7 +21,7 @@ DEPEND="${RDEPEND}"
 
 src_configure() {
 	local mycmakeargs=(
-		$(cmake_use_find_package qml Qt6Qml)
+		$(cmake_use_find_package qml Qt6Quick)
 	)
 
 	qt6-build_src_configure

dev-qt/qtwebchannel/qtwebchannel-6.5.2-r1.ebuild
@@ -21,7 +21,7 @@ DEPEND="${RDEPEND}"
 
 src_configure() {
 	local mycmakeargs=(
-		$(cmake_use_find_package qml Qt6Qml)
+		$(cmake_use_find_package qml Qt6Quick)
 	)
 
 	qt6-build_src_configure

commit e9ed045ffa2d1fcd41997826519b0b619c8193b4

dev-qt/qtwebchannel: adjust qml check again
Package only checks for Quick in top level CMakeLists.txt, but then uses
Qt::Qml (implicit from Quick) anyway. Current disable method does block
Quick target stuff but does not fully prevent from linking with Qt6Qml.

Revert, but do a has_version to avoid noise.

Bug: https://bugs.gentoo.org/913692
Fixes: a2b7cc29cbb6ac57ff4cddbc39c50a060e8cac66
Signed-off-by: Ionen Wolkens <ionen@gentoo.org>

dev-qt/qtwebchannel/qtwebchannel-6.5.2-r2.ebuild
@@ -20,9 +20,8 @@ RDEPEND="
 DEPEND="${RDEPEND}"
 
 src_configure() {
-	local mycmakeargs=(
-		$(cmake_use_find_package qml Qt6Quick)
-	)
+	has_version dev-qt/qtdeclarative:6 && #913692
+		local mycmakeargs=( $(cmake_use_find_package qml Qt6Qml) )
 
 	qt6-build_src_configure
 }

dev-qt/qtwebchannel/qtwebchannel-6.5.9999.ebuild
@@ -20,9 +20,8 @@ RDEPEND="
 DEPEND="${RDEPEND}"
 
 src_configure() {
-	local mycmakeargs=(
-		$(cmake_use_find_package qml Qt6Quick)
-	)
+	has_version dev-qt/qtdeclarative:6 && #913692
+		local mycmakeargs=( $(cmake_use_find_package qml Qt6Qml) )
 
 	qt6-build_src_configure
 }

dev-qt/qtwebchannel/qtwebchannel-6.9999.ebuild
@@ -20,9 +20,8 @@ RDEPEND="
 DEPEND="${RDEPEND}"
 
 src_configure() {
-	local mycmakeargs=(
-		$(cmake_use_find_package qml Qt6Quick)
-	)
+	has_version dev-qt/qtdeclarative:6 && #913692
+		local mycmakeargs=( $(cmake_use_find_package qml Qt6Qml) )
 
 	qt6-build_src_configure
 }