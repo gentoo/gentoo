gentoo/gentoo

commit 1d61e152357e6c2ce6dcd45335a5b6c86000584e

dev-util/cmake: fix Qt 6 automagic; migrate to USE=gui
* Migrate to USE=gui per QA policy (PG0802, https://projects.gentoo.org/qa/policy-guide/use-flags.html#pg0802)
  which has the nice consequence of helping to avoid circular dependencies (iirc,
  been a while since I've seen it, although it's obvious for Qt 6)
  for users who are just trying to prefer Qt 5/6 over GTK or whatever in make.conf.

* Just require Qt 6 for USE=gui instead of allowing Qt 5 too given Qt 5 is
  on the way out and we have to start shifting preference at some point. Pretty
  low risk for CMake given the GUI for it isn't that well-used anyway AFAIK.

Closes: https://bugs.gentoo.org/914489
Signed-off-by: Sam James <sam@gentoo.org>

dev-util/cmake/cmake-3.27.6-r1.ebuild
@@ -9,7 +9,8 @@ EAPI=8
 : ${CMAKE_DOCS_PREBUILT:=1}
 
 CMAKE_DOCS_PREBUILT_DEV=sam
-CMAKE_DOCS_VERSION=$(ver_cut 1-3)
+#CMAKE_DOCS_VERSION=$(ver_cut 1-3)
+CMAKE_DOCS_VERSION=3.27.0
 # Default to generating docs (inc. man pages) if no prebuilt; overridden later
 # See bug #784815
 CMAKE_DOCS_USEFLAG="+doc"
@@ -100,6 +101,9 @@ PATCHES=(
 	"${FILESDIR}"/${PN}-3.27.0_rc1-0006-Filter-out-distcc-warnings-to-avoid-confusing-CMake.patch
 
 	# Upstream fixes (can usually be removed with a version bump)
+	# pkgconf
+	# fixes https://github.com/pkgconf/pkgconf/issues/317
+	"${FILESDIR}"/${PN}-3.27.4-0001-FindPkgConfig-ignore-whitespace-separators-in-versio.patch
 )
 
 cmake_src_bootstrap() {

profiles/arch/hppa/package.use.mask
@@ -4,6 +4,10 @@
 # NOTE: When masking a USE flag due to missing keywords, please file a keyword
 # request bug for the hppa arch.
 
+# Sam James <sam@gentoo.org> (2023-09-22)
+# Qt 6 not keyworded here.
+dev-util/cmake gui
+
 # Andrew Ammerlaan <andrewammerlaan@gentoo.org> (2023-09-20)
 # Libei not keyworded here yet
 x11-base/xwayland libei

profiles/arch/ia64/package.use.mask
@@ -1,6 +1,10 @@
 # Copyright 1999-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
+# Sam James <sam@gentoo.org> (2023-09-22)
+# Qt 6 not keyworded here.
+dev-util/cmake gui
+
 # Andrew Ammerlaan <andrewammerlaan@gentoo.org> (2023-09-20)
 # Libei not keyworded here yet
 x11-base/xwayland libei

profiles/arch/loong/package.use.mask
@@ -1,6 +1,10 @@
 # Copyright 2022-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
+# Sam James <sam@gentoo.org> (2023-09-22)
+# Qt 6 not keyworded here.
+dev-util/cmake gui
+
 # WANG Xuerui <xen0n@gentoo.org> (2023-09-21)
 # dev-ruby/asciidoctor is not keyworded yet.
 dev-util/librnp man

profiles/arch/powerpc/ppc32/package.use.mask
@@ -2,6 +2,10 @@
 # Distributed under the terms of the GNU General Public License v2
 
 # Sam James <sam@gentoo.org> (2023-09-22)
+# Qt 6 not keyworded here.
+dev-util/cmake gui
+
+# Sam James <sam@gentoo.org> (2023-09-22)
 # sys-apps/flatpak is not keyworded here
 sys-apps/xdg-desktop-portal flatpak
 

profiles/arch/powerpc/ppc64/package.use.mask
@@ -1,6 +1,10 @@
 # Copyright 1999-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
+# Sam James <sam@gentoo.org> (2023-09-22)
+# Qt 6 not keyworded here.
+dev-util/cmake gui
+
 # Joonas Niilola <juippis@gentoo.org> (2023-08-24)
 # media-video/libva-utils is masked on ppc64, so hwaccel on Firefox
 # wouldn't work.

profiles/arch/riscv/package.use.mask
@@ -1,6 +1,10 @@
 # Copyright 2019-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
+# Sam James <sam@gentoo.org> (2023-09-22)
+# Qt 6 not keyworded here.
+dev-util/cmake gui
+
 # Jon Janzen <jon@jonjanzen.com> (2023-09-11)
 # librttopo has not been tested on this arch
 dev-db/spatialite rttopo

profiles/arch/s390/package.use.mask
@@ -1,6 +1,10 @@
 # Copyright 1999-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
+# Sam James <sam@gentoo.org> (2023-09-22)
+# Qt 6 not keyworded here.
+dev-util/cmake gui
+
 # Andrew Ammerlaan <andrewammerlaan@gentoo.org> (2023-09-20)
 # Libei not keyworded here yet
 x11-base/xwayland libei

commit 953082928dc68db16ef76ee48e055fdf0104c860

dev-util/cmake: fix pkg_* from USE=gui rename
Fixes: 1d61e152357e6c2ce6dcd45335a5b6c86000584e
Signed-off-by: Sam James <sam@gentoo.org>

dev-util/cmake/cmake-3.27.6-r1.ebuild
@@ -273,7 +273,7 @@ src_install() {
 pkg_postinst() {
 	use emacs && elisp-site-regen
 
-	if use qt5; then
+	if use gui; then
 		xdg_icon_cache_update
 		xdg_desktop_database_update
 		xdg_mimeinfo_database_update
@@ -283,7 +283,7 @@ pkg_postinst() {
 pkg_postrm() {
 	use emacs && elisp-site-regen
 
-	if use qt5; then
+	if use gui; then
 		xdg_icon_cache_update
 		xdg_desktop_database_update
 		xdg_mimeinfo_database_update

dev-util/cmake/cmake-9999.ebuild
@@ -269,7 +269,7 @@ src_install() {
 pkg_postinst() {
 	use emacs && elisp-site-regen
 
-	if use qt5; then
+	if use gui; then
 		xdg_icon_cache_update
 		xdg_desktop_database_update
 		xdg_mimeinfo_database_update
@@ -279,7 +279,7 @@ pkg_postinst() {
 pkg_postrm() {
 	use emacs && elisp-site-regen
 
-	if use qt5; then
+	if use gui; then
 		xdg_icon_cache_update
 		xdg_desktop_database_update
 		xdg_mimeinfo_database_update