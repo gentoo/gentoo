gentoo/gentoo

commit 3da4094e366ed63fd4eab3696b48c642469f051b

dev-python/versioneer: remanifest 0.29
Tiny change to get_keywords.

Closes: https://bugs.gentoo.org/906566
Signed-off-by: Sam James <sam@gentoo.org>

dev-python/versioneer/Manifest
@@ -1,1 +1,1 @@
-DIST python-versioneer-0.29.gh.tar.gz 78801 BLAKE2B e0af2887a8aaf5d4aee14d8370fa47d4ad9f0a5d385382de5b14f34fb1d6388daacaeab48677523b157ef685c8ad6e18b89bc8546ad8c991cde06e5c78da701c SHA512 10dce0923e2a48036117b64c8b746bb57db4b33f9f752ba950bf877319184451928d1eb37693ffb1a4f95afd774a4883cf265b8a82f35bd77b26c0967caa9ad6
+DIST python-versioneer-0.29.gh.tar.gz 78792 BLAKE2B 6a82c57968d13fbc16d5aa67d721dede32762c6327ed5b2ed721c29fdba24f6277aaa0a19cf3ec2fe7e449bc815ad164e905c5869fbd24d59b6eff0d9e88d8f9 SHA512 36bc69021c7d6c2bfa96b35e797336f8807908fc9ca3948241c18266984ca77af3141d7ff98525168e9c3604f62268b7964627bd211e04e9c0732cde56b62841

commit 9a0f4fbc5c8436c7d5e8761b6a8abe63a2c29281

dev-python/versioneer: rename distfile for retag
Bug: https://bugs.gentoo.org/906566
Fixes: 3da4094e366ed63fd4eab3696b48c642469f051b
Signed-off-by: Sam James <sam@gentoo.org>

dev-python/versioneer/Manifest
@@ -1,1 +1,1 @@
-DIST python-versioneer-0.29.gh.tar.gz 78792 BLAKE2B 6a82c57968d13fbc16d5aa67d721dede32762c6327ed5b2ed721c29fdba24f6277aaa0a19cf3ec2fe7e449bc815ad164e905c5869fbd24d59b6eff0d9e88d8f9 SHA512 36bc69021c7d6c2bfa96b35e797336f8807908fc9ca3948241c18266984ca77af3141d7ff98525168e9c3604f62268b7964627bd211e04e9c0732cde56b62841
+DIST python-versioneer-0.29.retag.gh.tar.gz 78792 BLAKE2B 6a82c57968d13fbc16d5aa67d721dede32762c6327ed5b2ed721c29fdba24f6277aaa0a19cf3ec2fe7e449bc815ad164e905c5869fbd24d59b6eff0d9e88d8f9 SHA512 36bc69021c7d6c2bfa96b35e797336f8807908fc9ca3948241c18266984ca77af3141d7ff98525168e9c3604f62268b7964627bd211e04e9c0732cde56b62841

dev-python/versioneer/versioneer-0.29-r1.ebuild
@@ -8,6 +8,7 @@ PYTHON_COMPAT=( python3_{10..12} pypy3 )
 
 inherit distutils-r1
 
+# XXX: drop .retag on next version
 MY_P=python-versioneer-${PV}
 DESCRIPTION="Easy VCS-based management of project version strings"
 HOMEPAGE="
@@ -16,7 +17,7 @@ HOMEPAGE="
 "
 SRC_URI="
 	https://github.com/python-versioneer/python-versioneer/archive/${PV}.tar.gz
-		-> ${MY_P}.gh.tar.gz
+		-> ${MY_P}.retag.gh.tar.gz
 "
 S=${WORKDIR}/${MY_P}
 