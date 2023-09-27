gentoo/gentoo

commit ffa678b96f88ec7be12cd57237ead3849ac9c453

perl-module.eclass: set NONINTERACTIVE_TESTING=1
See https://www.perlmonks.org/?node_id=1225311

I did consider the others, but:
* AUTOMATES_TESTING appears inappropriate for us, as it affects
  exit codes and might mask failures if configuration is wrong.
* EXTENDED_TESTING is something we could consider if we had
  some way to opt-in to expensive tests.

so just set NONINTERACTIVE_TESTING=1 for now, not the others mentioned
in the link.

Signed-off-by: Sam James <sam@gentoo.org>

eclass/perl-module.eclass
@@ -358,6 +358,13 @@ perl-module_src_test() {
 		export NO_NETWORK_TESTING=1
 	fi
 
+	# See https://www.perlmonks.org/?node_id=1225311
+	# * AUTOMATES_TESTING appears inappropriate for us, as it affects
+	# exit codes and might mask failures if configuration is wrong.
+	# * EXTENDED_TESTING is something we could consider if we had
+	# some way to opt-in to expensive tests.
+	export NONINTERACTIVE_TESTING=1
+
 	case ${EAPI} in
 		7)
 			;;

commit 2be63f3ff46248581cf1922f2992bfb52b2d11fb

perl-module.eclass: fix typo in comment
Fixes: ffa678b96f88ec7be12cd57237ead3849ac9c453
Signed-off-by: Sam James <sam@gentoo.org>

eclass/perl-module.eclass
@@ -361,7 +361,7 @@ perl-module_src_test() {
 	fi
 
 	# See https://www.perlmonks.org/?node_id=1225311
-	# * AUTOMATES_TESTING appears inappropriate for us, as it affects
+	# * AUTOMATED_TESTING appears inappropriate for us, as it affects
 	# exit codes and might mask failures if configuration is wrong.
 	# * EXTENDED_TESTING is something we could consider if we had
 	# some way to opt-in to expensive tests.