gentoo/gentoo

commit 2a59f9ae0a1df4425d7864353d8e085b555ffb0f

dev-libs/tree-sitter-javascript: add 0.20.1
Signed-off-by: Matthew Smith <matthew@gentoo.org>

dev-libs/tree-sitter-javascript/Manifest
@@ -1,1 +1,2 @@
 DIST tree-sitter-javascript-0.20.0.tar.gz 278831 BLAKE2B cd66005e7ebdae154a192ff556d4cb35e91d93a2ab645a24b9492217858278adc1f7917a7dc5033bbcfd84ca7ef41a3e7ebb1ff934bb09b79c8923469110e1de SHA512 778e0f2f5bf276d7ffb6eab0fe3f7473b56605de707c8e2f491fbb5826da94067b9f1bc476306013267001b5ecaab11a956e896b78520cdedd27006fac97fe41
+DIST tree-sitter-javascript-0.20.1.tar.gz 278831 BLAKE2B cd66005e7ebdae154a192ff556d4cb35e91d93a2ab645a24b9492217858278adc1f7917a7dc5033bbcfd84ca7ef41a3e7ebb1ff934bb09b79c8923469110e1de SHA512 778e0f2f5bf276d7ffb6eab0fe3f7473b56605de707c8e2f491fbb5826da94067b9f1bc476306013267001b5ecaab11a956e896b78520cdedd27006fac97fe41

dev-libs/tree-sitter-javascript/tree-sitter-javascript-0.20.1.ebuild
@@ -1,0 +1,14 @@
+# Copyright 1999-2023 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+TS_PV=rust-0.20.0
+inherit tree-sitter-grammar
+
+DESCRIPTION="JavaScript grammar for Tree-sitter"
+HOMEPAGE="https://github.com/tree-sitter/tree-sitter-javascript"
+
+LICENSE="MIT"
+SLOT="0"
+KEYWORDS="~amd64"

commit b437179beb53324b7a821c90f60083ab35557019

dev-libs/tree-sitter-javascript: fix SRC_URI
TS_PV meant that the old version was still used after the version bump.

Fixes: 2a59f9ae0a1df4425d7864353d8e085b555ffb0f
Signed-off-by: Matthew Smith <matthew@gentoo.org>

dev-libs/tree-sitter-javascript/Manifest
@@ -1,2 +1,2 @@
 DIST tree-sitter-javascript-0.20.0.tar.gz 278831 BLAKE2B cd66005e7ebdae154a192ff556d4cb35e91d93a2ab645a24b9492217858278adc1f7917a7dc5033bbcfd84ca7ef41a3e7ebb1ff934bb09b79c8923469110e1de SHA512 778e0f2f5bf276d7ffb6eab0fe3f7473b56605de707c8e2f491fbb5826da94067b9f1bc476306013267001b5ecaab11a956e896b78520cdedd27006fac97fe41
-DIST tree-sitter-javascript-0.20.1.tar.gz 278831 BLAKE2B cd66005e7ebdae154a192ff556d4cb35e91d93a2ab645a24b9492217858278adc1f7917a7dc5033bbcfd84ca7ef41a3e7ebb1ff934bb09b79c8923469110e1de SHA512 778e0f2f5bf276d7ffb6eab0fe3f7473b56605de707c8e2f491fbb5826da94067b9f1bc476306013267001b5ecaab11a956e896b78520cdedd27006fac97fe41
+DIST tree-sitter-javascript-0.20.1-2.tar.gz 371027 BLAKE2B 5bd9b11f1e10ff37950b2142031bebd3b5f6111818a21bf5a7d97a8a27a5340bd09cd139d76846141ed64a824f82f1e1394b90d8622192524d7db9a02ed95223 SHA512 5341311b419cf763c0a6d23348eba0e38b417ba07c2def341027223347d850c28b86db6add136dbf73cd5296133823a4548493ee649e09247dfbe5bcbaaab76d

dev-libs/tree-sitter-javascript/tree-sitter-javascript-0.20.1-r1.ebuild
@@ -3,7 +3,6 @@
 
 EAPI=8
 
-TS_PV=rust-0.20.0
 inherit tree-sitter-grammar
 
 DESCRIPTION="JavaScript grammar for Tree-sitter"
@@ -12,3 +11,8 @@ HOMEPAGE="https://github.com/tree-sitter/tree-sitter-javascript"
 LICENSE="MIT"
 SLOT="0"
 KEYWORDS="~amd64"
+
+# Override eclass SRC_URI as 0.20.1 was bumped incorrectly.
+SRC_URI="https://github.com/tree-sitter/${PN}/archive/v${PV}.tar.gz
+	-> ${P}-2.tar.gz"
+S="${WORKDIR}"/${PN}-${PV}/src