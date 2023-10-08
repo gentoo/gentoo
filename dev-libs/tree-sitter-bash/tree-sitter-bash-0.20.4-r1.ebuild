gentoo/gentoo

commit 184a1048e77f12b73a5b3ded572a2351220f1fdf

dev-libs/tree-sitter-bash: add 0.20.4
Signed-off-by: Matthew Smith <matthew@gentoo.org>

dev-libs/tree-sitter-bash/Manifest
@@ -1,3 +1,4 @@
 DIST tree-sitter-bash-0.19.0.tar.gz 187286 BLAKE2B 8812a593366a65a9cae3eaac5bb81c71e95f4abe8558a01b908333e471f05714b1c768b71e66d3890d1e63202819290813e499a5e8ba49e72bb466b9b0f245f8 SHA512 62bc8bc25c999f9c79632bd6b236edd21ef15b14365dd1a956237b11a32f5e60a2f420ec67b60d46203699b7841d4bc10978fc0307d0c4013b376c6c024d89ae
 DIST tree-sitter-bash-0.20.3.tar.gz 334446 BLAKE2B e33f841e48458a63e6e084f66a251c6847c004140a1b727752452d031d240756068f5188d1bd90c4194aaf51d040187f3995636673210bc5f93f2ed50ff73a97 SHA512 558a8eef4a04e786c43bef0dd24f022619b77faffdd5788ef47ba28f133d5a55df144169503343cdb46ac4ed66deb36ca483b963e3eb686da30c324c65c9eac3
 DIST tree-sitter-bash-0.20.3_p20230921.tar.gz 442105 BLAKE2B c7755e38488544e4c3f6b02191417e9a1a8be08dcef9391e37e201e8b4d6d10155f3e9f0c67b3ecfbae529a538117ed4e55245144f8f0ed1ac53042accb0bb1c SHA512 c6546e207a4a58acd53407eb3ff67fe4352cbccd8147860c3d679feab709216d6d1d1efe813df2c8fa2d3f2469545a453834d0d319c0584fd8e11589302e4dd5
+DIST tree-sitter-bash-0.20.4.tar.gz 442105 BLAKE2B c7755e38488544e4c3f6b02191417e9a1a8be08dcef9391e37e201e8b4d6d10155f3e9f0c67b3ecfbae529a538117ed4e55245144f8f0ed1ac53042accb0bb1c SHA512 c6546e207a4a58acd53407eb3ff67fe4352cbccd8147860c3d679feab709216d6d1d1efe813df2c8fa2d3f2469545a453834d0d319c0584fd8e11589302e4dd5

dev-libs/tree-sitter-bash/tree-sitter-bash-0.20.4.ebuild
@@ -1,0 +1,15 @@
+# Copyright 1999-2023 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+TS_PV="fd4e40dab883d6456da4d847de8321aee9c80805"
+
+inherit tree-sitter-grammar
+
+DESCRIPTION="Bash grammar for Tree-sitter"
+HOMEPAGE="https://github.com/tree-sitter/tree-sitter-bash"
+
+LICENSE="MIT"
+SLOT="0"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86 ~x64-macos"

commit 437ed7fdfb1188e0314f800b5499099720945eac

dev-libs/tree-sitter-bash: fix SRC_URI
TS_PV meant that the old version was still used after the version bump.

Fixes: 184a1048e77f12b73a5b3ded572a2351220f1fdf
Signed-off-by: Matthew Smith <matthew@gentoo.org>

dev-libs/tree-sitter-bash/Manifest
@@ -1,4 +1,4 @@
 DIST tree-sitter-bash-0.19.0.tar.gz 187286 BLAKE2B 8812a593366a65a9cae3eaac5bb81c71e95f4abe8558a01b908333e471f05714b1c768b71e66d3890d1e63202819290813e499a5e8ba49e72bb466b9b0f245f8 SHA512 62bc8bc25c999f9c79632bd6b236edd21ef15b14365dd1a956237b11a32f5e60a2f420ec67b60d46203699b7841d4bc10978fc0307d0c4013b376c6c024d89ae
 DIST tree-sitter-bash-0.20.3.tar.gz 334446 BLAKE2B e33f841e48458a63e6e084f66a251c6847c004140a1b727752452d031d240756068f5188d1bd90c4194aaf51d040187f3995636673210bc5f93f2ed50ff73a97 SHA512 558a8eef4a04e786c43bef0dd24f022619b77faffdd5788ef47ba28f133d5a55df144169503343cdb46ac4ed66deb36ca483b963e3eb686da30c324c65c9eac3
 DIST tree-sitter-bash-0.20.3_p20230921.tar.gz 442105 BLAKE2B c7755e38488544e4c3f6b02191417e9a1a8be08dcef9391e37e201e8b4d6d10155f3e9f0c67b3ecfbae529a538117ed4e55245144f8f0ed1ac53042accb0bb1c SHA512 c6546e207a4a58acd53407eb3ff67fe4352cbccd8147860c3d679feab709216d6d1d1efe813df2c8fa2d3f2469545a453834d0d319c0584fd8e11589302e4dd5
-DIST tree-sitter-bash-0.20.4.tar.gz 442105 BLAKE2B c7755e38488544e4c3f6b02191417e9a1a8be08dcef9391e37e201e8b4d6d10155f3e9f0c67b3ecfbae529a538117ed4e55245144f8f0ed1ac53042accb0bb1c SHA512 c6546e207a4a58acd53407eb3ff67fe4352cbccd8147860c3d679feab709216d6d1d1efe813df2c8fa2d3f2469545a453834d0d319c0584fd8e11589302e4dd5
+DIST tree-sitter-bash-0.20.4-2.tar.gz 444183 BLAKE2B 31016de876ed84ab991ebc1e531dd750c416a86db5f91097db54e7362de673ea89bbf8863a9df487f75386f19a7e8ddde48a2e727af918d1beea8acad3ce67c6 SHA512 074f659ea475c7a442957a3df234f967ae9a6ace885ecfad6548a1d0f2af7535e5460d81bcd160a04bda1b902af0a9f741fc0c7b22309cd1d11ab2bc33ff0a22

dev-libs/tree-sitter-bash/tree-sitter-bash-0.20.4-r1.ebuild
@@ -3,8 +3,6 @@
 
 EAPI=8
 
-TS_PV="fd4e40dab883d6456da4d847de8321aee9c80805"
-
 inherit tree-sitter-grammar
 
 DESCRIPTION="Bash grammar for Tree-sitter"
@@ -13,3 +11,8 @@ HOMEPAGE="https://github.com/tree-sitter/tree-sitter-bash"
 LICENSE="MIT"
 SLOT="0"
 KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86 ~x64-macos"
+
+# Override eclass SRC_URI as 0.20.4 was bumped incorrectly.
+SRC_URI="https://github.com/tree-sitter/${PN}/archive/v${PV}.tar.gz
+	-> ${P}-2.tar.gz"
+S="${WORKDIR}"/${PN}-${PV}/src