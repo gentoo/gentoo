gentoo/gentoo

commit b76af2ea3f8272cb147ae97400fd22f0623ee781

dev-libs/tree-sitter-go: add 0.20.0
Signed-off-by: Matthew Smith <matthew@gentoo.org>

dev-libs/tree-sitter-go/Manifest
@@ -1,1 +1,2 @@
 DIST tree-sitter-go-0.19.1.tar.gz 160180 BLAKE2B f92bbb342eae3a7705dbd6f6564a16fdbad1a25c405970bca1e8df01ff6d0c20b45f9849a0d13d53e2f56d95b71db0f7813765f64e0dcdfd9ca617d1f71e688d SHA512 8390dd3e39daa42ec5254b4c6c0b15fec0caa7ae64a9ec3b9116a10f9e9f1b6940cd88c8cf14dc50b28b97ea588ff302ebd16f3c88e0bd6983e76b5526a31b45
+DIST tree-sitter-go-0.20.0.tar.gz 160180 BLAKE2B f92bbb342eae3a7705dbd6f6564a16fdbad1a25c405970bca1e8df01ff6d0c20b45f9849a0d13d53e2f56d95b71db0f7813765f64e0dcdfd9ca617d1f71e688d SHA512 8390dd3e39daa42ec5254b4c6c0b15fec0caa7ae64a9ec3b9116a10f9e9f1b6940cd88c8cf14dc50b28b97ea588ff302ebd16f3c88e0bd6983e76b5526a31b45

dev-libs/tree-sitter-go/tree-sitter-go-0.20.0.ebuild
@@ -1,0 +1,14 @@
+# Copyright 1999-2023 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+TS_PV=rust-0.19.1
+inherit tree-sitter-grammar
+
+DESCRIPTION="Go grammar for Tree-sitter"
+HOMEPAGE="https://github.com/tree-sitter/tree-sitter-go"
+
+LICENSE="MIT"
+SLOT="0"
+KEYWORDS="~amd64"

commit 9bad57ac3bd87d01821283483f7a963219ef2136

dev-libs/tree-sitter-go: fix SRC_URI
TS_PV meant that the old version was still used after the version bump.

Fixes: b76af2ea3f8272cb147ae97400fd22f0623ee781
Signed-off-by: Matthew Smith <matthew@gentoo.org>

dev-libs/tree-sitter-go/Manifest
@@ -1,2 +1,2 @@
 DIST tree-sitter-go-0.19.1.tar.gz 160180 BLAKE2B f92bbb342eae3a7705dbd6f6564a16fdbad1a25c405970bca1e8df01ff6d0c20b45f9849a0d13d53e2f56d95b71db0f7813765f64e0dcdfd9ca617d1f71e688d SHA512 8390dd3e39daa42ec5254b4c6c0b15fec0caa7ae64a9ec3b9116a10f9e9f1b6940cd88c8cf14dc50b28b97ea588ff302ebd16f3c88e0bd6983e76b5526a31b45
-DIST tree-sitter-go-0.20.0.tar.gz 160180 BLAKE2B f92bbb342eae3a7705dbd6f6564a16fdbad1a25c405970bca1e8df01ff6d0c20b45f9849a0d13d53e2f56d95b71db0f7813765f64e0dcdfd9ca617d1f71e688d SHA512 8390dd3e39daa42ec5254b4c6c0b15fec0caa7ae64a9ec3b9116a10f9e9f1b6940cd88c8cf14dc50b28b97ea588ff302ebd16f3c88e0bd6983e76b5526a31b45
+DIST tree-sitter-go-0.20.0-2.tar.gz 195753 BLAKE2B eca2965c0d7bba3c639e092845ac98d05936e9d87290dca2a283dcd6afb1151f8d63aba907337fee79e9fd9b0d901edfda62ec936ef948f57dc663c9d906715b SHA512 ff438a6c84454f38cdefe506f5da787007cb50b3d41bcb9c7f26ed7d951a3268e6bf0b32f2e750d0e4233516c1e4a827c24af831020f81f268fe825adcd5f836

dev-libs/tree-sitter-go/tree-sitter-go-0.20.0-r1.ebuild
@@ -3,7 +3,6 @@
 
 EAPI=8
 
-TS_PV=rust-0.19.1
 inherit tree-sitter-grammar
 
 DESCRIPTION="Go grammar for Tree-sitter"
@@ -12,3 +11,8 @@ HOMEPAGE="https://github.com/tree-sitter/tree-sitter-go"
 LICENSE="MIT"
 SLOT="0"
 KEYWORDS="~amd64"
+
+# Override eclass SRC_URI as 0.20.0 was bumped incorrectly.
+SRC_URI="https://github.com/tree-sitter/${PN}/archive/v${PV}.tar.gz
+	-> ${P}-2.tar.gz"
+S="${WORKDIR}"/${PN}-${PV}/src