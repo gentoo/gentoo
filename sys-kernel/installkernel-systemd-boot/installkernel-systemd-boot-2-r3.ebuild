gentoo/gentoo

commit 4eeeef05b0b3d50b919a7e352e78dbbd2c26732a

sys-kernel/installkernel-systemd-boot: fix systemd dep for boot
Signed-off-by: Sam James <sam@gentoo.org>

sys-kernel/installkernel-systemd-boot/installkernel-systemd-boot-2-r3.ebuild
@@ -14,7 +14,8 @@ KEYWORDS="~amd64 ~arm64 ~loong ~ppc64 ~x86"
 RDEPEND="
 	!sys-kernel/installkernel-gentoo
 	|| (
-		sys-apps/systemd
+		sys-apps/systemd[gnuefi(-)]
+		sys-apps/systemd[boot(-)]
 		sys-apps/systemd-utils[boot]
 	)
 "

commit d8139eeaa48ba16b399aff35986af5077286b3bd

sys-kernel/installkernel-systemd-boot: fix filename
Didn't realise PF was used.

Fixes: 4eeeef05b0b3d50b919a7e352e78dbbd2c26732a
Signed-off-by: Sam James <sam@gentoo.org>

sys-kernel/installkernel-systemd-boot/installkernel-systemd-boot-2-r3.ebuild
@@ -30,6 +30,6 @@ src_install() {
 	EOF
 
 	exeinto /usr/lib/kernel/install.d/
-	newexe "${FILESDIR}/${PF}-00-00machineid-directory.install" \
+	newexe "${FILESDIR}/${PN}-2-r2-00-00machineid-directory.install" \
 		00-00machineid-directory.install
 }