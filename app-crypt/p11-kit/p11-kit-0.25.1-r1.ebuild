gentoo/gentoo

commit 4798ad6134c3978c821512fc7850c64514b1f373

app-crypt/p11-kit: backport tty fix; drop castxml dep
* Backport tty fix
* Drop castxml dep, as it's not required, and castxml needs Clang. If we used it
  only if installed, we'd get a non-deterministic build.

Closes: https://bugs.gentoo.org/916376
Signed-off-by: Sam James <sam@gentoo.org>

app-crypt/p11-kit/p11-kit-0.25.1-r1.ebuild
@@ -26,11 +26,14 @@ DEPEND="${RDEPEND}"
 BDEPEND="
 	${PYTHON_DEPS}
 	app-text/docbook-xsl-stylesheets
-	dev-libs/castxml
 	virtual/pkgconfig
 	gtk-doc? ( dev-util/gtk-doc )
 	nls? ( sys-devel/gettext )
 "
+
+PATCHES=(
+	"${FILESDIR}"/${P}-fix-tty-comparison.patch
+)
 
 src_prepare() {
 	default
@@ -46,7 +49,18 @@ multilib_src_configure() {
 	# Disable unsafe tests, bug#502088
 	export FAKED_MODE=1
 
+	local native_file="${T}"/meson.${CHOST}.${ABI}.ini.local
+
+	# p11-kit doesn't need this to build and castxml needs Clang. To get
+	# a deterministic non-automagic build, always disable the search for
+	# castxml.
+	cat >> ${native_file} <<-EOF || die
+	[binaries]
+	castxml='castxml-falseified'
+	EOF
+
 	local emesonargs=(
+		--native-file "${native_file}"
 		-Dbashcompdir="$(get_bashcompdir)"
 		-Dtrust_module=enabled
 		-Dtrust_paths="${EPREFIX}"/etc/ssl/certs/ca-certificates.crt

app-crypt/p11-kit/files/p11-kit-0.25.1-fix-tty-comparison.patch
@@ -1,0 +1,22 @@
+https://github.com/p11-glue/p11-kit/commit/cdc4720d29466e717aa694a85a7aee86ebb94172
+
+From cdc4720d29466e717aa694a85a7aee86ebb94172 Mon Sep 17 00:00:00 2001
+From: Erdem Meydanli <meydanli@amazon.com>
+Date: Thu, 26 Oct 2023 15:18:24 +0000
+Subject: [PATCH] fix: improper comparison
+
+Compare the return value of the readpassphrase function to NULL.
+
+Signed-off-by: Erdem Meydanli <meydanli@amazon.com>
+--- a/p11-kit/tty.c
++++ b/p11-kit/tty.c
+@@ -66,7 +66,7 @@ p11_pin_tty_callback (const char *pin_source,
+ 	if (asprintf (&prompt, "%s: ", pin_description) < 0)
+ 		return NULL;
+ 
+-	if (readpassphrase (prompt, buf, sizeof(buf), 0) < 0)
++	if (readpassphrase (prompt, buf, sizeof(buf), 0) == NULL)
+ 		goto cleanup;
+ 
+ 	pin = p11_kit_pin_new_for_string (buf);
+

commit bf7b8a46a8af8c33cbd7d66f315269d97bc6a361

app-crypt/p11-kit: restore keywords
I'd meant to do this in 4798ad6134c3978c821512fc7850c64514b1f373, as the reason
to drop the keywords became obsolete.

Bug: https://bugs.gentoo.org/916376
Fixes: 4798ad6134c3978c821512fc7850c64514b1f373
Signed-off-by: Sam James <sam@gentoo.org>

app-crypt/p11-kit/p11-kit-0.25.1-r1.ebuild
@@ -12,7 +12,7 @@ SRC_URI="https://github.com/p11-glue/p11-kit/releases/download/${PV}/${P}.tar.xz
 
 LICENSE="MIT"
 SLOT="0"
-KEYWORDS="~alpha ~amd64 ~arm ~m68k ~mips ~riscv ~x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~x64-solaris"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~m68k ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~x64-solaris"
 IUSE="+libffi gtk-doc nls systemd test"
 RESTRICT="!test? ( test )"
 

app-crypt/p11-kit/p11-kit-0.25.3.ebuild
@@ -12,7 +12,7 @@ SRC_URI="https://github.com/p11-glue/p11-kit/releases/download/${PV}/${P}.tar.xz
 
 LICENSE="MIT"
 SLOT="0"
-KEYWORDS="~alpha ~amd64 ~arm ~m68k ~mips ~riscv ~x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~x64-solaris"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~m68k ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~x64-solaris"
 IUSE="+libffi gtk-doc nls systemd test"
 RESTRICT="!test? ( test )"
 