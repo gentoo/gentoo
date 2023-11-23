gentoo/gentoo

commit bc29e400979b556cd9c835a21fa401ec94504c0a

dev-python/pytest-xdist: drop pytest-forked dependency
It is unused. "forked" was originally split out of "xdist", and a
backwards compat alias was included that would read the old pytest
plugin argument for xdist (boxed) and set the "forked" option instead.
Thus, it was expected functionality for xdist itself to be able to
invoke --boxed, which depended on "forked" in its install_requires.

This backwards compat alias was dropped in xdist 3.0.0, so we should
drop that install requirement too.

Signed-off-by: Eli Schwartz <eschwartz93@gmail.com>
Closes: https://github.com/gentoo/gentoo/pull/33210
Signed-off-by: Sam James <sam@gentoo.org>

dev-python/pytest-xdist/pytest-xdist-3.3.1-r1.ebuild
@@ -1,0 +1,44 @@
+# Copyright 1999-2023 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+DISTUTILS_USE_PEP517=setuptools
+PYPI_NO_NORMALIZE=1
+PYTHON_COMPAT=( python3_{10..12} pypy3 )
+
+inherit distutils-r1 pypi
+
+DESCRIPTION="Distributed testing and loop-on-failing modes"
+HOMEPAGE="
+	https://pypi.org/project/pytest-xdist/
+	https://github.com/pytest-dev/pytest-xdist/
+"
+
+SLOT="0"
+LICENSE="MIT"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~m68k ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86 ~x64-macos"
+
+RDEPEND="
+	dev-python/execnet[${PYTHON_USEDEP}]
+	dev-python/psutil[${PYTHON_USEDEP}]
+	>=dev-python/pytest-6.2.0[${PYTHON_USEDEP}]
+"
+
+BDEPEND="
+	dev-python/setuptools-scm[${PYTHON_USEDEP}]
+	test? (
+		dev-python/filelock[${PYTHON_USEDEP}]
+	)
+"
+
+distutils_enable_tests pytest
+
+python_test() {
+	# disable autoloading plugins in nested pytest calls
+	local -x PYTEST_DISABLE_PLUGIN_AUTOLOAD=1
+	# since we disabled autoloading, force loading necessary plugins
+	local -x PYTEST_PLUGINS=xdist.plugin,xdist.looponfail,pytest_forked
+
+	epytest
+}

commit 8d4421e90d39ef7b93063e7fa83addcd84c7ad25

dev-python/pytest-xdist: remove forced plugin autoload for forked
Followup to commit bc29e400979b556cd9c835a21fa401ec94504c0a. The
dependency was removed, but it wasn't also removed from the testsuite
loader, which caused pytest to fail early on when it could not find such
a plugin to load.

As noted in the original commit, it's unused. We certainly shouldn't be
autoloading it in the testsuite, then.

Signed-off-by: Eli Schwartz <eschwartz93@gmail.com>
Fixes: bc29e400979b556cd9c835a21fa401ec94504c0a
Closes: https://github.com/gentoo/gentoo/pull/33939
Signed-off-by: Michał Górny <mgorny@gentoo.org>

dev-python/pytest-xdist/pytest-xdist-3.3.1-r1.ebuild
@@ -38,7 +38,7 @@ python_test() {
 	# disable autoloading plugins in nested pytest calls
 	local -x PYTEST_DISABLE_PLUGIN_AUTOLOAD=1
 	# since we disabled autoloading, force loading necessary plugins
-	local -x PYTEST_PLUGINS=xdist.plugin,xdist.looponfail,pytest_forked
+	local -x PYTEST_PLUGINS=xdist.plugin,xdist.looponfail
 
 	epytest
 }

dev-python/pytest-xdist/pytest-xdist-3.4.0.ebuild
@@ -38,7 +38,7 @@ python_test() {
 	# disable autoloading plugins in nested pytest calls
 	local -x PYTEST_DISABLE_PLUGIN_AUTOLOAD=1
 	# since we disabled autoloading, force loading necessary plugins
-	local -x PYTEST_PLUGINS=xdist.plugin,xdist.looponfail,pytest_forked
+	local -x PYTEST_PLUGINS=xdist.plugin,xdist.looponfail
 
 	epytest
 }

dev-python/pytest-xdist/pytest-xdist-3.5.0.ebuild
@@ -38,7 +38,7 @@ python_test() {
 	# disable autoloading plugins in nested pytest calls
 	local -x PYTEST_DISABLE_PLUGIN_AUTOLOAD=1
 	# since we disabled autoloading, force loading necessary plugins
-	local -x PYTEST_PLUGINS=xdist.plugin,xdist.looponfail,pytest_forked
+	local -x PYTEST_PLUGINS=xdist.plugin,xdist.looponfail
 
 	epytest
 }