gentoo/gentoo

commit 648233dbc2b56af3c7abee7da59a061beaaa8958

net-libs/ngtcp2: drop 0.14.1
Signed-off-by: Craig Andrews <candrews@gentoo.org>

net-libs/ngtcp2/Manifest
@@ -1,3 +1,2 @@
-DIST ngtcp2-0.14.1.tar.xz 585788 BLAKE2B 68574767c701432e7558fa85cbad0d49934d220e292ceb82cdf19a715f247c6ec9b3a2b87def1176ade067a78c84bd3495936847a9a53fcadaff7b66971eca8d SHA512 52c55bc322d979d020ab3124a7cc0033d0f3e5649e3c8ee5f00aa64dbad83c4f199d50685704cc9372821e7fc81c59eea79f33894260b5dca171712d5b3899a0
 DIST ngtcp2-1.0.1.tar.xz 590100 BLAKE2B 3893e849777f5770d2c2f01e57fae8db81e2f1035b4f3c50d9bbc506780fe66dbc8744d3efa82ee8585862b72cb0fdde2f185e0e37f84845dc0b51f0985e38f2 SHA512 d3f7c05bd4f06ee4b98c2bea983bc281794af6d007900231972bfd7fc2d7f034e22323d26fa3a7e0a936cfecc2476d77e80ae6a8c81cca317e4ab17b25c44d61
 DIST ngtcp2-1.1.0.tar.xz 595536 BLAKE2B df37cede58fe67e9121d3c985a6d26b3534a2ef2805a9fb0ee92369d87e849164916e36c23b0e3055b4fa817307b1aa47cc1c0bfe3b03690401f00c0f050b9e8 SHA512 0de7d1aa67a5dbbf718313f90b7535980d29902a3d7e7a8a5e0362ec54b5ffe00ec2e844262ab05ddf6a0f3a1acc716a9b6df181121f8c3b66e138a91cfbe067

net-libs/ngtcp2/ngtcp2-0.14.1.ebuild
@@ -1,52 +1,0 @@
-# Copyright 1999-2023 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=8
-
-inherit cmake-multilib
-
-if [[ ${PV} == 9999 ]] ; then
-	EGIT_REPO_URI="https://github.com/ngtcp2/ngtcp2.git"
-	inherit git-r3
-else
-	SRC_URI="https://github.com/ngtcp2/ngtcp2/releases/download/v${PV}/${P}.tar.xz"
-	KEYWORDS="amd64 ~hppa ~riscv x86"
-fi
-
-DESCRIPTION="Implementation of the IETF QUIC Protocol"
-HOMEPAGE="https://github.com/ngtcp2/ngtcp2/"
-
-LICENSE="MIT"
-SLOT="0/0"
-IUSE="+gnutls openssl +ssl static-libs test"
-REQUIRED_USE="ssl? ( || ( gnutls openssl ) )"
-
-BDEPEND="virtual/pkgconfig"
-RDEPEND="
-	ssl? (
-		gnutls? ( >=net-libs/gnutls-3.7.2:0= )
-		openssl? (
-			>=dev-libs/openssl-1.1.1:0=
-		)
-	)"
-DEPEND="${RDEPEND}
-	test? ( >=dev-util/cunit-2.1[${MULTILIB_USEDEP}] )"
-RESTRICT="!test? ( test )"
-
-multilib_src_configure() {
-	local mycmakeargs=(
-		-DENABLE_STATIC_LIB=$(usex static-libs)
-		-DENABLE_GNUTLS=$(usex gnutls)
-		-DENABLE_OPENSSL=$(usex openssl)
-		-DENABLE_BORINGSSL=OFF
-		-DENABLE_PICOTLS=OFF
-		-DENABLE_WOLFSSL=OFF
-		-DCMAKE_DISABLE_FIND_PACKAGE_Libev=ON
-		-DCMAKE_DISABLE_FIND_PACKAGE_Libnghttp3=ON
-	)
-	cmake_src_configure
-}
-
-multilib_src_test() {
-	cmake_build check
-}

commit 7e3dfe87b0e8598201a943902edf748bba080ad4

net-libs/ngtcp2: re-add 0.14.1 which is amd64 stable
Reverts 648233dbc2b5 which accidentally removed this stable version

Signed-off-by: Craig Andrews <candrews@gentoo.org>

net-libs/ngtcp2/Manifest
@@ -1,1 +1,2 @@
+DIST ngtcp2-0.14.1.tar.xz 585788 BLAKE2B 68574767c701432e7558fa85cbad0d49934d220e292ceb82cdf19a715f247c6ec9b3a2b87def1176ade067a78c84bd3495936847a9a53fcadaff7b66971eca8d SHA512 52c55bc322d979d020ab3124a7cc0033d0f3e5649e3c8ee5f00aa64dbad83c4f199d50685704cc9372821e7fc81c59eea79f33894260b5dca171712d5b3899a0
 DIST ngtcp2-1.1.0.tar.xz 595536 BLAKE2B df37cede58fe67e9121d3c985a6d26b3534a2ef2805a9fb0ee92369d87e849164916e36c23b0e3055b4fa817307b1aa47cc1c0bfe3b03690401f00c0f050b9e8 SHA512 0de7d1aa67a5dbbf718313f90b7535980d29902a3d7e7a8a5e0362ec54b5ffe00ec2e844262ab05ddf6a0f3a1acc716a9b6df181121f8c3b66e138a91cfbe067

net-libs/ngtcp2/ngtcp2-0.14.1.ebuild
@@ -1,0 +1,52 @@
+# Copyright 1999-2023 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit cmake-multilib
+
+if [[ ${PV} == 9999 ]] ; then
+	EGIT_REPO_URI="https://github.com/ngtcp2/ngtcp2.git"
+	inherit git-r3
+else
+	SRC_URI="https://github.com/ngtcp2/ngtcp2/releases/download/v${PV}/${P}.tar.xz"
+	KEYWORDS="amd64 ~hppa ~riscv x86"
+fi
+
+DESCRIPTION="Implementation of the IETF QUIC Protocol"
+HOMEPAGE="https://github.com/ngtcp2/ngtcp2/"
+
+LICENSE="MIT"
+SLOT="0/0"
+IUSE="+gnutls openssl +ssl static-libs test"
+REQUIRED_USE="ssl? ( || ( gnutls openssl ) )"
+
+BDEPEND="virtual/pkgconfig"
+RDEPEND="
+	ssl? (
+		gnutls? ( >=net-libs/gnutls-3.7.2:0= )
+		openssl? (
+			>=dev-libs/openssl-1.1.1:0=
+		)
+	)"
+DEPEND="${RDEPEND}
+	test? ( >=dev-util/cunit-2.1[${MULTILIB_USEDEP}] )"
+RESTRICT="!test? ( test )"
+
+multilib_src_configure() {
+	local mycmakeargs=(
+		-DENABLE_STATIC_LIB=$(usex static-libs)
+		-DENABLE_GNUTLS=$(usex gnutls)
+		-DENABLE_OPENSSL=$(usex openssl)
+		-DENABLE_BORINGSSL=OFF
+		-DENABLE_PICOTLS=OFF
+		-DENABLE_WOLFSSL=OFF
+		-DCMAKE_DISABLE_FIND_PACKAGE_Libev=ON
+		-DCMAKE_DISABLE_FIND_PACKAGE_Libnghttp3=ON
+	)
+	cmake_src_configure
+}
+
+multilib_src_test() {
+	cmake_build check
+}