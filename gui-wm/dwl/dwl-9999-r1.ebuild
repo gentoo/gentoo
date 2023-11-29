gentoo/gentoo

commit 1fe91d18f06754d46bce87c3ead16d8021dfdf36

gui-wm/dwl: update HOMEPAGE
Signed-off-by: Leonardo Hernández Hernández <leohdz172@proton.me>
Signed-off-by: Sam James <sam@gentoo.org>

gui-wm/dwl/dwl-9999-r1.ebuild
@@ -23,7 +23,7 @@ if [[ ${PV} == *9999* ]]; then
 			;;
 	esac
 else
-	SRC_URI="https://github.com/djpohly/${PN}/releases/download/v${PV}/${MY_P}.tar.gz -> ${P}.gh.tar.gz"
+	SRC_URI="https://codeberg.org/${PN}/${PN}/releases/download/v${PV}/${MY_P}.tar.gz"
 	S="${WORKDIR}/${MY_P}"
 	KEYWORDS="~amd64 ~x86"
 fi

gui-wm/dwl/dwl-9999.ebuild
@@ -8,7 +8,7 @@ inherit savedconfig toolchain-funcs
 MY_P="${PN}-v${PV}"
 WLROOTS_SLOT="0/17"
 if [[ ${PV} == *9999* ]]; then
-	EGIT_REPO_URI="https://github.com/djpohly/dwl"
+	EGIT_REPO_URI="https://codeberg.org/dwl/dwl.git"
 	inherit git-r3
 
 	# 9999-r0: main (latest stable wlroots release)
@@ -23,13 +23,13 @@ if [[ ${PV} == *9999* ]]; then
 			;;
 	esac
 else
-	SRC_URI="https://github.com/djpohly/${PN}/releases/download/v${PV}/${MY_P}.tar.gz -> ${P}.gh.tar.gz"
+	SRC_URI="https://codeberg.org/${PN}/${PN}/releases/download/v${PV}/${MY_P}.tar.gz"
 	S="${WORKDIR}/${MY_P}"
 	KEYWORDS="~amd64 ~x86"
 fi
 
 DESCRIPTION="dwm for Wayland"
-HOMEPAGE="https://github.com/djpohly/dwl"
+HOMEPAGE="https://codeberg.org/dwl/dwl"
 
 LICENSE="CC0-1.0 GPL-3+ MIT"
 SLOT="0"

gui-wm/dwl/metadata.xml
@@ -10,7 +10,4 @@
       <email>proxy-maint@gentoo.org</email>
       <name>Proxy Maintainers</name>
     </maintainer>
-    <upstream>
-        <remote-id type="github">djpohly/dwl</remote-id>
-    </upstream>
 </pkgmetadata>

commit 22466ae049a313d515adb28fee85d48c4a3e37eb

gui-wm/dwl: fix last remnants of the old HOMEPAGE
Fixes: 1fe91d18f06754d46bce87c3ead16d8021dfdf36
Signed-off-by: Leonardo Hernández Hernández <leohdz172@proton.me>

gui-wm/dwl/dwl-9999-r1.ebuild
@@ -8,7 +8,7 @@ inherit savedconfig toolchain-funcs
 MY_P="${PN}-v${PV}"
 WLROOTS_SLOT="0/18"
 if [[ ${PV} == *9999* ]]; then
-	EGIT_REPO_URI="https://github.com/djpohly/dwl"
+	EGIT_REPO_URI="https://codeberg.org/dwl/dwl.git"
 	inherit git-r3
 
 	# 9999-r0: main (latest stable wlroots release)
@@ -29,7 +29,7 @@ else
 fi
 
 DESCRIPTION="dwm for Wayland"
-HOMEPAGE="https://github.com/djpohly/dwl"
+HOMEPAGE="https://codeberg.org/dwl/dwl"
 
 LICENSE="CC0-1.0 GPL-3+ MIT"
 SLOT="0"