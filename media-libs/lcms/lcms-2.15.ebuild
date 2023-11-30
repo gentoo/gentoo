gentoo/gentoo

commit 8d58cbcb2637204926be7d4f64ab3d9494d64e70

media-libs/lcms: increase test timeout
Bug: https://bugs.gentoo.org/913067
Signed-off-by: Matoro Mahri <matoro_gentoo@matoro.tk>
Signed-off-by: Sam James <sam@gentoo.org>

media-libs/lcms/lcms-2.15.ebuild
@@ -43,6 +43,11 @@ multilib_src_configure() {
 	meson_src_configure
 }
 
+multilib_src_test() {
+	# fast_float_testbed on hppa -> 1458s from default timeout of 600, #913067
+	meson_src_test --timeout-mutliplier=3
+}
+
 multilib_src_install_all() {
 	use doc && dodoc doc/*.pdf
 }

commit ae47df466cdd5c3ab54159d930fcfbe8a4c0b1f4

media-libs/lcms: fix typo
Fixes: 8d58cbcb2637204926be7d4f64ab3d9494d64e70
Bug: https://bugs.gentoo.org/913067
Signed-off-by: Matoro Mahri <matoro_gentoo@matoro.tk>
Signed-off-by: Sam James <sam@gentoo.org>

media-libs/lcms/lcms-2.15.ebuild
@@ -45,7 +45,7 @@ multilib_src_configure() {
 
 multilib_src_test() {
 	# fast_float_testbed on hppa -> 1458s from default timeout of 600, #913067
-	meson_src_test --timeout-mutliplier=3
+	meson_src_test --timeout-multiplier=3
 }
 
 multilib_src_install_all() {