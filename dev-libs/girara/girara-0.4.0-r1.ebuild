gentoo/gentoo

commit 81e700213dba4fce523e2b99e3379efea316dd2e

dev-libs/girara: add missing xorg-proto dep
Signed-off-by: Joonas Niilola <juippis@gentoo.org>

dev-libs/girara/girara-0.4.0-r1.ebuild
@@ -22,7 +22,7 @@ SLOT="0"
 IUSE="doc libnotify test"
 RESTRICT="!test? ( test )"
 
-DEPEND="
+RDEPEND="
 	app-accessibility/at-spi2-core
 	dev-libs/glib:2
 	dev-libs/json-glib:=
@@ -33,7 +33,8 @@ DEPEND="
 	x11-libs/pango
 	libnotify? ( x11-libs/libnotify )
 "
-RDEPEND="${DEPEND}"
+DEPEND="${DEPEND}
+	x11-base/xorg-proto"
 # Tests are run under virtx
 BDEPEND="
 	virtual/pkgconfig

dev-libs/girara/girara-9999.ebuild
@@ -23,7 +23,7 @@ IUSE="doc libnotify test"
 
 RESTRICT="!test? ( test )"
 
-DEPEND="
+RDEPEND="
 	app-accessibility/at-spi2-core
 	dev-libs/glib:2
 	dev-libs/json-glib:=
@@ -34,7 +34,8 @@ DEPEND="
 	x11-libs/pango
 	libnotify? ( x11-libs/libnotify )
 "
-RDEPEND="${DEPEND}"
+DEPEND="${DEPEND}
+	x11-base/xorg-proto"
 # Tests are run under virtx
 BDEPEND="
 	virtual/pkgconfig

commit 4050af87d021564fd6a353e51d5ae9a52392259e

dev-libs/girara: fix DEPEND
Fixes: 81e700213dba4fce523e2b99e3379efea316dd2e
Closes: https://bugs.gentoo.org/919697
Signed-off-by: Leonardo Hernández Hernández <leohdz172@proton.me>

dev-libs/girara/girara-0.4.0-r1.ebuild
@@ -33,8 +33,10 @@ RDEPEND="
 	x11-libs/pango
 	libnotify? ( x11-libs/libnotify )
 "
-DEPEND="${DEPEND}
-	x11-base/xorg-proto"
+DEPEND="
+	${RDEPEND}
+	x11-base/xorg-proto
+"
 # Tests are run under virtx
 BDEPEND="
 	virtual/pkgconfig

dev-libs/girara/girara-9999.ebuild
@@ -34,8 +34,10 @@ RDEPEND="
 	x11-libs/pango
 	libnotify? ( x11-libs/libnotify )
 "
-DEPEND="${DEPEND}
-	x11-base/xorg-proto"
+DEPEND="
+	${RDEPEND}
+	x11-base/xorg-proto
+"
 # Tests are run under virtx
 BDEPEND="
 	virtual/pkgconfig