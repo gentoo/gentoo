gentoo/gentoo

commit 0d54becbae04dcc8410b9c4b041cb3467d1bd6bd

gui-libs/xdg-desktop-portal-wlr: install wlroots-portals.conf
Closes: https://bugs.gentoo.org/915702

Signed-off-by: Matthew Thode <prometheanfire@gentoo.org>

gui-libs/xdg-desktop-portal-wlr/xdg-desktop-portal-wlr-9999.ebuild
@@ -55,3 +55,9 @@ src_configure() {
 	fi
 	meson_src_configure
 }
+
+src_install() {
+	default
+	insinto /usr/share/xdg-desktop-portal/portals
+	doins "${WORKDIR}/${P}/contrib/wlroots-portals.conf"
+}

commit 843c3f23884f3d8df9fec3deccc391836f2297ed

gui-libs/xdg-desktop-portal-wlr: fix src_install for live
Bug: https://bugs.gentoo.org/915702
Fixes: 0d54becbae04dcc8410b9c4b041cb3467d1bd6bd
Signed-off-by: Sam James <sam@gentoo.org>

gui-libs/xdg-desktop-portal-wlr/xdg-desktop-portal-wlr-9999.ebuild
@@ -57,7 +57,8 @@ src_configure() {
 }
 
 src_install() {
-	default
+	meson_src_install
+
 	insinto /usr/share/xdg-desktop-portal/portals
 	doins "${WORKDIR}/${P}/contrib/wlroots-portals.conf"
 }