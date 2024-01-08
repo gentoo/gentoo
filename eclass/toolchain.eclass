gentoo/gentoo

commit b8300e99e7137925c73f1d067caed8af4f77dc1b

toolchain.eclass: sys-devel/bison -> app-alternatives/yacc
All of these will be using app-alternatives/yacc anyway as they're not unsetting
YACC or LEX, so make the dep reflect reality.

(Included both YACC and LEX out of conservatism.)

Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -303,7 +303,7 @@ if tc_has_feature graphite ; then
 fi
 
 BDEPEND="
-	>=sys-devel/bison-1.875
+	>=app-alternatives/yacc-1.875
 	>=app-alternatives/lex-2.5.4
 	nls? ( sys-devel/gettext )
 	test? (

commit 736816f859cb89432b26e0505d89418baba99d30

toolchain.eclass: fix lex, yacc versions
Fixes: b8300e99e7137925c73f1d067caed8af4f77dc1b
Fixes: 4c621abb2923e1085cc4aaf67c30901510d8b3c0
Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -303,8 +303,8 @@ if tc_has_feature graphite ; then
 fi
 
 BDEPEND="
-	>=app-alternatives/yacc-1.875
-	>=app-alternatives/lex-2.5.4
+	app-alternatives/yacc
+	app-alternatives/lex
 	nls? ( sys-devel/gettext )
 	test? (
 		>=dev-util/dejagnu-1.4.4