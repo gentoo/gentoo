gentoo/gentoo

commit d77f5a3f40122879f39a97420f170fca226e6e58

sys-process/systemd-cron: disable PCH
By policy in Gentoo, we don't do PCH as it's too brittle.

Closes: https://bugs.gentoo.org/917646
Signed-off-by: Sam James <sam@gentoo.org>

sys-process/systemd-cron/systemd-cron-2.3.0-r2.ebuild
@@ -1,0 +1,100 @@
+# Copyright 1999-2024 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+inherit systemd toolchain-funcs
+
+DESCRIPTION="systemd units to create timers for cron directories and crontab"
+HOMEPAGE="https://github.com/systemd-cron/systemd-cron/"
+SRC_URI="https://github.com/systemd-cron/${PN}/archive/v${PV}.tar.gz -> systemd-cron-${PV}.tar.gz"
+
+LICENSE="MIT"
+SLOT="0"
+KEYWORDS="~amd64 ~arm ~arm64 ~hppa ~ia64 ~ppc ~ppc64 ~riscv ~sparc ~x86"
+IUSE="cron-boot etc-crontab-systemd minutely +runparts setgid yearly"
+RESTRICT="test"
+
+BDEPEND="virtual/pkgconfig"
+
+RDEPEND=">=sys-apps/systemd-253
+	dev-libs/openssl
+	runparts? ( sys-apps/debianutils )
+	!sys-process/cronie[anacron]
+	!etc-crontab-systemd? ( !sys-process/dcron )
+	sys-process/cronbase
+	acct-user/_cron-failure
+	acct-group/_cron-failure"
+
+DEPEND="sys-process/cronbase"
+
+PATCHES=(
+	"${FILESDIR}"/${PN}-2.3.0-pch.patch
+)
+
+pkg_pretend() {
+	if use runparts && ! [ -x /usr/bin/run-parts ] ; then
+			eerror "Please complete the migration to merged-usr."
+			eerror "https://wiki.gentoo.org/wiki/Merge-usr"
+			die "systemd-cron no longer supports split-usr"
+	fi
+}
+
+src_prepare() {
+	sed -i \
+		-e 's/^crontab/crontab-systemd/' \
+		-e 's/^CRONTAB/CRONTAB-SYSTEMD/' \
+		-- "${S}/src/man/crontab."{1,5}".in" || die
+
+	if use etc-crontab-systemd
+	then	sed -i \
+			-e "s!/etc/crontab!/etc/crontab-systemd!" \
+			-- "${S}/src/man/crontab."{1,5}".in" \
+			"${S}/src/bin/systemd-crontab-generator.cpp" \
+			"${S}/test/test-generator" || die
+	fi
+
+	default
+}
+
+my_use_enable() {
+	if use ${1}; then
+		echo --enable-${2:-${1}}=yes
+	else
+		echo --enable-${2:-${1}}=no
+	fi
+}
+
+src_configure() {
+	tc-export PKG_CONFIG CXX CC
+
+	./configure \
+		--prefix="${EPREFIX}/usr" \
+		--mandir="${EPREFIX}/usr/share/man" \
+		--unitdir="$(systemd_get_systemunitdir)" \
+		--generatordir="$(systemd_get_systemgeneratordir)" \
+		$(my_use_enable cron-boot boot) \
+		$(my_use_enable minutely) \
+		$(my_use_enable runparts) \
+		$(my_use_enable yearly) \
+		$(my_use_enable yearly quarterly) \
+		$(my_use_enable yearly semi_annually) || die
+
+	export CRONTAB=crontab-systemd
+}
+
+src_compile() {
+	emake PCH=
+}
+
+src_install() {
+	default
+	rm -f "${ED}"/usr/lib/sysusers.d/systemd-cron.conf
+}
+
+pkg_postinst() {
+	elog "This package now supports USE=runparts which is enabled by default."
+	elog "This enables the traditional run-parts behavior."
+	elog "If you disable this flag you will get the new behavior of having"
+	elog "multiple jobs for each cron.* entry run in parallel with"
+	elog "separate services/logs/etc."
+}

sys-process/systemd-cron/files/systemd-cron-2.3.0-pch.patch
@@ -1,0 +1,46 @@
+https://bugs.gentoo.org/917646
+https://github.com/systemd-cron/systemd-cron/issues/141
+https://github.com/systemd-cron/systemd-cron/commit/1662b899b206f00face30b9d4671551427262b07
+
+From 1662b899b206f00face30b9d4671551427262b07 Mon Sep 17 00:00:00 2001
+From: =?UTF-8?q?=D0=BD=D0=B0=D0=B1?= <nabijaczleweli@nabijaczleweli.xyz>
+Date: Tue, 21 Nov 2023 19:40:05 +0100
+Subject: [PATCH] Add PCH= for broken compilers like #141
+
+--- a/Makefile.in
++++ b/Makefile.in
+@@ -1,6 +1,7 @@
+ CFLAGS ?= -O2
+ SHELLCHECK ?= shellcheck
+ CRONTAB ?= crontab
++PCH ?= y
+ 
+ version		:= @version@
+ schedules		:= @schedules@
+@@ -208,12 +209,12 @@ $(builddir)/include/%.hpp: $(srcdir)/include/%.hpp
+ CXXVER := $(shell $(CXX) --version | { read -r l; echo "$$l"; })
+ ifneq "$(findstring clang,$(CXXVER))" ""
+ 	# clang doesn't use PCHs automatically
+-	PCH_ARG := -include-pch $(builddir)/include/libvoreutils.hpp.gch -Wno-gcc-compat
++	PCH_ARG := $(if $(PCH),-include-pch $(builddir)/include/libvoreutils.hpp.gch) -Wno-gcc-compat
+ else
+ 	PCH_ARG :=
+ endif
+ 
+-common_headers := $(builddir)/include/configuration.hpp $(builddir)/include/libvoreutils.hpp.gch $(builddir)/include/util.hpp
++common_headers := $(builddir)/include/configuration.hpp $(builddir)/include/libvoreutils.hpp$(if $(PCH),.gch) $(builddir)/include/util.hpp
+ CFLAGS += -Wall -Wextra -fno-exceptions -Wno-psabi
+ $(builddir)/include/libvoreutils.hpp.gch : $(builddir)/include/libvoreutils.hpp
+ 	$(CXX) $(CFLAGS) $(CPPFLAGS) -std=c++20 -I $(builddir)/include            $< -o $@
+--- a/README.md
++++ b/README.md
+@@ -146,6 +146,8 @@ without the override, the jobs would run twice since native-timer detection woul
+ If there is already a perfect 1:1 mapping between `/etc/cron.<freq>/<job>` and `/usr/lib/systemd/system/<job>.timer`,
+ then it is not needed to add an entry to these tables.
+ 
++If your compiler's [PCH compilation is broken](https://github.com/systemd-cron/systemd-cron/issues/141), build with `make PCH=`.
++
+ ### Caveat
+ 
+ Your package should also run these extra commands before starting cron.target
+

commit d4c75bbeaccd78d51241734b277d3c2b6b176b54

sys-process/systemd-cron: disable PCH harder
Closes: https://bugs.gentoo.org/917646
Fixes: d77f5a3f40122879f39a97420f170fca226e6e58
Signed-off-by: Sam James <sam@gentoo.org>

sys-process/systemd-cron/systemd-cron-2.3.0-r2.ebuild
@@ -93,7 +93,7 @@ src_compile() {
 }
 
 src_install() {
-	default
+	emake DESTDIR="${D}" PCH= install
 	rm -f "${ED}"/usr/lib/sysusers.d/systemd-cron.conf
 }
 