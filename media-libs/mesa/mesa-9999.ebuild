gentoo/gentoo

commit f21a150abe95c332adf035356dede1c54cce5da2

media-libs/mesa: Depend on intel_clc for VIDEO_CARDS=intel
New requirement added by upstream commit a512c2a8b57 ("meson: enforce
build of intel-clc with anv/iris").

And now that `-Dintel_clc=system` is required for more than just
ray-tracing support, set the new `-Dintel-rt=` option as well.

Signed-off-by: Matt Turner <mattst88@gentoo.org>

media-libs/mesa/mesa-9999.ebuild
@@ -139,16 +139,12 @@ BDEPEND="
 	app-alternatives/lex
 	virtual/pkgconfig
 	$(python_gen_any_dep ">=dev-python/mako-0.8.0[\${PYTHON_USEDEP}]")
-	vulkan? (
-		dev-util/glslang
-		llvm? (
-			video_cards_intel? (
-				amd64? (
-					$(python_gen_any_dep "dev-python/ply[\${PYTHON_USEDEP}]")
-					~dev-util/intel_clc-${PV}
-					dev-libs/libclc[spirv(-)]
-				)
-			)
+	video_cards_intel? (
+		~dev-util/intel_clc-${PV}
+		dev-libs/libclc[spirv(-)]
+		vulkan? (
+			dev-util/glslang
+			$(python_gen_any_dep "dev-python/ply[\${PYTHON_USEDEP}]")
 		)
 	)
 	wayland? ( dev-util/wayland-scanner )
@@ -359,12 +355,6 @@ multilib_src_configure() {
 	use vulkan-overlay && vulkan_layers+=",overlay"
 	emesonargs+=(-Dvulkan-layers=${vulkan_layers#,})
 
-	if use llvm && use vulkan && use video_cards_intel && use amd64; then
-		emesonargs+=(-Dintel-clc=system)
-	else
-		emesonargs+=(-Dintel-clc=disabled)
-	fi
-
 	if use opengl || use gles1 || use gles2; then
 		emesonargs+=(
 			-Degl=enabled
@@ -398,8 +388,10 @@ multilib_src_configure() {
 		$(meson_use osmesa)
 		$(meson_use selinux)
 		$(meson_feature unwind libunwind)
+		$(meson_feature video_cards_intel intel-rt)
 		$(meson_feature zstd)
 		$(meson_use cpu_flags_x86_sse2 sse2)
+		-Dintel-clc=$(usex video_cards_intel system auto)
 		-Dvalgrind=$(usex valgrind auto disabled)
 		-Dvideo-codecs=$(usex proprietary-codecs "all" "all_free")
 		-Dgallium-drivers=$(driver_list "${GALLIUM_DRIVERS[*]}")

commit 7765a772c3ca5cf925936b2f15454b5268cac2d1

media-libs/mesa: Fix dependency on glslang
Bug introduced in commit f21a150abe95 ("media-libs/mesa: Depend on
intel_clc for VIDEO_CARDS=intel")

Signed-off-by: Matt Turner <mattst88@gentoo.org>

media-libs/mesa/mesa-9999.ebuild
@@ -140,13 +140,12 @@ BDEPEND="
 	virtual/pkgconfig
 	$(python_gen_any_dep ">=dev-python/mako-0.8.0[\${PYTHON_USEDEP}]")
 	video_cards_intel? (
-		~dev-util/intel_clc-${PV}
-		dev-libs/libclc[spirv(-)]
-		vulkan? (
-			dev-util/glslang
+			~dev-util/intel_clc-${PV}
+			dev-libs/libclc[spirv(-)]
 			$(python_gen_any_dep "dev-python/ply[\${PYTHON_USEDEP}]")
 		)
 	)
+	vulkan? ( dev-util/glslang )
 	wayland? ( dev-util/wayland-scanner )
 "
 