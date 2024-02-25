gentoo/gentoo

commit 574290a65df445a55d5456024e093f26459f32c6

dev-util/buildbot-worker: add 3.11.1
Upstream point release officially drops dev-python/future as a
requirement by replacing it with dev-python/six instead, as a python2
compatibility library.

This is actually the only change for buildbot-worker, although there is
one commit each to buildbot and buildbot-www as well. This point release
truly is mild, like all the best bugfix patch releases.

Anyways, this is nice, because six is a tolerable dependency that we
have broad use of in the tree, whereas future is a hot mess, deprecated,
and broken entirely if you depend on it in python 3.12, so delayed
updating PYTHON_COMPAT in consumers.

Drop attempts at patching future out of buildbot-worker, it's not needed
anymore.

n.b. The new buildbot tag is not yet on PyPI, but buildbot gets
simultaneously released to Github Releases on a regular basis. Take this
opportunity to add the latter as a secondary download location.

Signed-off-by: Eli Schwartz <eschwartz93@gmail.com>
Signed-off-by: Sam James <sam@gentoo.org>

dev-util/buildbot-worker/Manifest
@@ -1,4 +1,3 @@
-DIST buildbot-worker-3.11.0-remove_py27.tar.gz 7134 BLAKE2B 8c92f8425b9a8d60e46228aab6aef49352090c931e21818d684167100f0c5c184e5e4e6aabdbb420bfe0b124d572194b33889deccf123b934754ad4e75037af0 SHA512 d9158e7936336d6147c0938ea212da55775ffe1c431b94b1b89274113fa45cbd62b7042737bac8a6580e268aca823fa072ee3d3bb49bad97aa99bd56995d7134
-DIST buildbot-worker-3.11.0.tar.gz 131028 BLAKE2B 644e10f1d90c75a9f20439bc902764d9f69b16781d710e56bf4cad40d30f06f137d6a00d4c450c1589909ca93c98ff9a0cd6e1363a8feff58a4832e0096aada7 SHA512 73f42495bd252e75cd984d3222663260079887caadff29b69c23f188a1f3208fc6e2acfa38553fb5615ba1c3d4996655c96faa16ba0eb7335c6290ce3ce58679
+DIST buildbot-worker-3.11.1.tar.gz 131129 BLAKE2B d8466c086261c612e976586b4589ca20617f5b9a7b69ccf276500fd351caead1425ae24897c4db242024de53f83c98b869dd9035bc366d9ee0376290307922a0 SHA512 c4560f69a15434cf93a43521bd1bbb568937a6ad8be338bca9089c9f449c3d0c5b8f000abf06a5fcfe4ebf5b206a9c81417a91cee633b67ca89ee67497a8368a
 DIST buildbot-worker-3.6.1.tar.gz 120224 BLAKE2B 010893b7b4e5710654299b2541c4cdcdad47b3b763dbc7e2336e5480c841908e04d8350c7cb42edd9ea99d16fd26f36a524a6d6bec8c0f933665a02ded369f89 SHA512 f1e3bf7c0da2f31dd446dc16487e32fddaecc0c2f9c17c469146707322a3d7bb2cc77db4f6145c62530bce58dd61aa19e008e944a7f39a17dc66f1317ee26e1b
 DIST buildbot-worker-3.9.0.tar.gz 125913 BLAKE2B e053cd150cf21cc0e5dfb2d49c186c49d340670429b760ce9f697aa97a245d0e6a12f0ea84eb1256a610891042096877c89244b1f2862bf74160502170f9124c SHA512 08b74d6f019ca85f65c598a34c81bc47fdf873abad17205072788d938fcd5450f80aa6ca9d324edc01893947f4ac8e2968889dd73be854a82b4ea54e4730613b

dev-util/buildbot-worker/buildbot-worker-3.11.1.ebuild
@@ -19,13 +19,16 @@ KEYWORDS="~amd64 ~arm64 ~sparc ~amd64-linux ~x86-linux"
 IUSE="test"
 RESTRICT="!test? ( test )"
 
-SRC_URI="${SRC_URI} https://dev.gentoo.org/~zorry/patches/buildbot/buildbot-worker-${PV}-remove_py27.tar.gz"
+SRC_URI+="
+	https://github.com/buildbot/buildbot/releases/download/v${PV}/${P}.tar.gz
+"
 
 RDEPEND="
 	acct-user/buildbot
 	!<dev-util/buildbot-3.0.0
 	>=dev-python/autobahn-0.16.0[${PYTHON_USEDEP}]
 	>=dev-python/msgpack-0.6.0[${PYTHON_USEDEP}]
+	dev-python/six[${PYTHON_USEDEP}]
 	>=dev-python/twisted-18.7.0[${PYTHON_USEDEP}]
 "
 BDEPEND="
@@ -48,8 +51,6 @@ src_prepare() {
 	# Remove shipped windows start script
 	sed -e "/'buildbot_worker_windows_service=buildbot_worker.scripts.windows_service:HandleCommandLine',/d" \
 		-i setup.py || die
-	# applay remove py 2.7 patch
-	eapply "${WORKDIR}/buildbot-worker-${PV}-remove_py27.patch"
 
 	distutils-r1_src_prepare
 }

commit 20855fed3f49bf153fbb15eb780c664f7b3868ec

dev-util/buildbot-worker: restore 3.11.0
Apparently dev-util/buildbot depends on the precise version...

Fixes: 574290a65df445a55d5456024e093f26459f32c6
Signed-off-by: Sam James <sam@gentoo.org>

dev-util/buildbot-worker/Manifest
@@ -1,3 +1,5 @@
+DIST buildbot-worker-3.11.0-remove_py27.tar.gz 7134 BLAKE2B 8c92f8425b9a8d60e46228aab6aef49352090c931e21818d684167100f0c5c184e5e4e6aabdbb420bfe0b124d572194b33889deccf123b934754ad4e75037af0 SHA512 d9158e7936336d6147c0938ea212da55775ffe1c431b94b1b89274113fa45cbd62b7042737bac8a6580e268aca823fa072ee3d3bb49bad97aa99bd56995d7134
+DIST buildbot-worker-3.11.0.tar.gz 131028 BLAKE2B 644e10f1d90c75a9f20439bc902764d9f69b16781d710e56bf4cad40d30f06f137d6a00d4c450c1589909ca93c98ff9a0cd6e1363a8feff58a4832e0096aada7 SHA512 73f42495bd252e75cd984d3222663260079887caadff29b69c23f188a1f3208fc6e2acfa38553fb5615ba1c3d4996655c96faa16ba0eb7335c6290ce3ce58679
 DIST buildbot-worker-3.11.1.tar.gz 131129 BLAKE2B d8466c086261c612e976586b4589ca20617f5b9a7b69ccf276500fd351caead1425ae24897c4db242024de53f83c98b869dd9035bc366d9ee0376290307922a0 SHA512 c4560f69a15434cf93a43521bd1bbb568937a6ad8be338bca9089c9f449c3d0c5b8f000abf06a5fcfe4ebf5b206a9c81417a91cee633b67ca89ee67497a8368a
 DIST buildbot-worker-3.6.1.tar.gz 120224 BLAKE2B 010893b7b4e5710654299b2541c4cdcdad47b3b763dbc7e2336e5480c841908e04d8350c7cb42edd9ea99d16fd26f36a524a6d6bec8c0f933665a02ded369f89 SHA512 f1e3bf7c0da2f31dd446dc16487e32fddaecc0c2f9c17c469146707322a3d7bb2cc77db4f6145c62530bce58dd61aa19e008e944a7f39a17dc66f1317ee26e1b
 DIST buildbot-worker-3.9.0.tar.gz 125913 BLAKE2B e053cd150cf21cc0e5dfb2d49c186c49d340670429b760ce9f697aa97a245d0e6a12f0ea84eb1256a610891042096877c89244b1f2862bf74160502170f9124c SHA512 08b74d6f019ca85f65c598a34c81bc47fdf873abad17205072788d938fcd5450f80aa6ca9d324edc01893947f4ac8e2968889dd73be854a82b4ea54e4730613b

dev-util/buildbot-worker/buildbot-worker-3.11.0.ebuild
@@ -1,0 +1,139 @@
+# Copyright 1999-2024 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+DISTUTILS_USE_PEP517=setuptools
+PYPI_NO_NORMALIZE=1
+PYTHON_COMPAT=( python3_{10..12} )
+inherit readme.gentoo-r1 systemd distutils-r1 pypi
+
+DESCRIPTION="BuildBot Worker (slave) Daemon"
+HOMEPAGE="https://buildbot.net/
+	https://github.com/buildbot/buildbot
+	https://pypi.org/project/buildbot-worker/"
+
+LICENSE="GPL-2"
+SLOT="0"
+KEYWORDS="~amd64 ~arm64 ~sparc ~amd64-linux ~x86-linux"
+IUSE="test"
+RESTRICT="!test? ( test )"
+
+SRC_URI="${SRC_URI} https://dev.gentoo.org/~zorry/patches/buildbot/buildbot-worker-${PV}-remove_py27.tar.gz"
+
+RDEPEND="
+	acct-user/buildbot
+	!<dev-util/buildbot-3.0.0
+	>=dev-python/autobahn-0.16.0[${PYTHON_USEDEP}]
+	>=dev-python/msgpack-0.6.0[${PYTHON_USEDEP}]
+	>=dev-python/twisted-18.7.0[${PYTHON_USEDEP}]
+"
+BDEPEND="
+	test? (
+		${RDEPEND}
+		dev-python/parameterized[${PYTHON_USEDEP}]
+		dev-python/psutil[${PYTHON_USEDEP}]
+	)
+"
+
+DOC_CONTENTS="The \"buildbot\" user and the \"buildbot_worker\" init script has been added
+to support starting buildbot_worker through Gentoo's init system. To use this,
+execute \"emerge --config =${CATEGORY}/${PF}\" to create a new instance.
+Set up your build worker following the documentation, make sure the
+resulting directories are owned by the \"buildbot\" user and point
+\"${ROOT}/etc/conf.d/buildbot_worker.myinstance\" at the right location.
+The scripts can	run as a different user if desired."
+
+src_prepare() {
+	# Remove shipped windows start script
+	sed -e "/'buildbot_worker_windows_service=buildbot_worker.scripts.windows_service:HandleCommandLine',/d" \
+		-i setup.py || die
+	# applay remove py 2.7 patch
+	eapply "${WORKDIR}/buildbot-worker-${PV}-remove_py27.patch"
+
+	distutils-r1_src_prepare
+}
+
+python_test() {
+	"${EPYTHON}" -m twisted.trial buildbot_worker || die "Tests failed with ${EPYTHON}"
+}
+
+python_install_all() {
+
+	distutils-r1_python_install_all
+
+	doman docs/buildbot-worker.1
+
+	newconfd "${FILESDIR}/buildbot_worker.confd2" buildbot_worker
+	newinitd "${FILESDIR}/buildbot_worker.initd2" buildbot_worker
+	systemd_dounit "${FILESDIR}/buildbot_worker.target"
+	systemd_newunit "${FILESDIR}/buildbot_worker_at.service" "buildbot_worker@.service"
+	systemd_install_serviced "${FILESDIR}/buildbot_worker_at.service.conf" "buildbot_worker@.service"
+
+	dodir /var/lib/buildbot_worker
+	cp "${FILESDIR}/buildbot.tac.sample" "${D}/var/lib/buildbot_worker"|| die "Install failed!"
+
+	readme.gentoo_create_doc
+}
+
+pkg_postinst() {
+	readme.gentoo_print_elog
+
+	if [[ -n ${REPLACING_VERSIONS} ]]; then
+		ewarn
+		ewarn "More than one instance of a buildbot_worker can be run simultaneously."
+		ewarn " Note that \"BASEDIR\" in the buildbot_worker configuration file"
+		ewarn "is now the common base directory for all instances. If you are migrating from an older"
+		ewarn "version, make sure that you copy the current contents of \"BASEDIR\" to a subdirectory."
+		ewarn "The name of the subdirectory corresponds to the name of the buildbot_worker instance."
+		ewarn "In order to start the service running OpenRC-based systems need to link to the init file:"
+		ewarn "    ln --symbolic --relative /etc/init.d/buildbot_worker /etc/init.d/buildbot_worker.myinstance"
+		ewarn "    rc-update add buildbot_worker.myinstance default"
+		ewarn "    /etc/init.d/buildbot_worker.myinstance start"
+		ewarn "Systems using systemd can do the following:"
+		ewarn "    systemctl enable buildbot_worker@myinstance.service"
+		ewarn "    systemctl enable buildbot_worker.target"
+		ewarn "    systemctl start buildbot_worker.target"
+	fi
+}
+
+pkg_config() {
+	local buildworker_path="/var/lib/buildbot_worker"
+	local log_path="/var/log/buildbot_worker"
+
+	einfo "This will prepare a new buildbot_worker instance in ${buildworker_path}."
+	einfo "Press Control-C to abort."
+
+	einfo "Enter the name for the new instance: "
+	read instance_name
+	[[ -z "${instance_name}" ]] && die "Invalid instance name"
+
+	local instance_path="${buildworker_path}/${instance_name}"
+	local instance_log_path="${log_path}/${instance_name}"
+
+	if [[ -e "${instance_path}" ]]; then
+		eerror "The instance with the specified name already exists:"
+		eerror "${instance_path}"
+		die "Instance already exists"
+	fi
+
+	if [[ ! -d "${instance_path}" ]]; then
+		mkdir --parents "${instance_path}" || die "Unable to create directory ${buildworker_path}"
+	fi
+	chown --recursive buildbot:buildbot "${instance_path}" || die "Setting permissions for instance failed"
+	cp "${buildworker_path}/buildbot.tac.sample" "${instance_path}/buildbot.tac" \
+		|| die "Moving sample configuration failed"
+	ln --symbolic --relative "/etc/init.d/buildbot_worker" "/etc/init.d/buildbot_worker.${instance_name}" \
+		|| die "Unable to create link to init file"
+
+	if [[ ! -d "${instance_log_path}" ]]; then
+		mkdir --parents "${instance_log_path}" || die "Unable to create directory ${instance_log_path}"
+		chown --recursive buildbot:buildbot "${instance_log_path}" \
+			|| die "Setting permissions for instance failed"
+	fi
+	ln --symbolic --relative "${instance_log_path}/twistd.log" "${instance_path}/twistd.log" \
+		|| die "Unable to create link to log file"
+
+	einfo "Successfully created a buildbot_worker instance at ${instance_path}."
+	einfo "To change the default settings edit the buildbot.tac file in this directory."
+}