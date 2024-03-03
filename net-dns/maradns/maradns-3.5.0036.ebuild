gentoo/gentoo

commit 73937e86b92dcf813bf021a83451e2ae4dbc423c

net-dns/maradns: add 3.5.0036, security bump
Rebased patch, removed fix_shebang due to the tool no longer being
present.

Bug: https://bugs.gentoo.org/906113
Signed-off-by: Christopher Fore <csfore@posteo.net>
Closes: https://github.com/gentoo/gentoo/pull/34684
Signed-off-by: Joonas Niilola <juippis@gentoo.org>

net-dns/maradns/maradns-3.5.0036.ebuild
@@ -1,0 +1,89 @@
+# Copyright 1999-2024 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+PYTHON_COMPAT=( python3_{10..11} )
+inherit python-any-r1 systemd toolchain-funcs
+
+DESCRIPTION="A security-aware DNS server"
+HOMEPAGE="https://maradns.samiam.org"
+SRC_URI="https://maradns.samiam.org/download/${PV%.*}/${PV}/${P}.tar.xz"
+
+# The GPL-2 covers the init script, bug 426018.
+LICENSE="BSD-2 GPL-2"
+SLOT="0"
+KEYWORDS="~amd64 ~mips ~ppc ~x86"
+IUSE="examples"
+
+BDEPEND="${PYTHON_DEPS}
+	dev-lang/perl"
+DEPEND="
+	acct-group/maradns
+	acct-user/duende
+	acct-user/maradns"
+RDEPEND="${DEPEND}"
+
+PATCHES=(
+	"${FILESDIR}"/${PN}-3.5.0022-flags.patch
+)
+
+src_configure() {
+	tc-export CC
+	./configure --ipv6 || die "Failed to configure"
+}
+
+src_install() {
+	# Install the MaraDNS and Deadwood binaries
+	dosbin server/maradns
+	dosbin tcp/zoneserver
+	dosbin deadwood-${PV}/src/Deadwood
+	dobin tcp/{getzone,fetchzone}
+	dobin tools/{askmara,askmara-tcp,duende}
+
+	# MaraDNS docs, manpages, misc
+	docompress -x /usr/share/doc/${PF}/maradns.gpg.key
+	dodoc {CHANGELOG.TXT,COPYING,maradns.gpg.key}
+	dodoc doc/en/{QuickStart,faq.*,*.md,README}
+	dodoc -r doc/en/{text,tutorial}
+	docinto deadwood
+	dodoc deadwood-${PV}/doc/{*.txt,*.html,CHANGELOG,Deadwood-HOWTO}
+	dodoc -r deadwood-${PV}/doc/internals
+
+	# Install examples (optional)
+	if use examples ; then
+		docinto examples
+		dodoc doc/en/examples/example_*
+	fi
+
+	# Install manpages
+	doman doc/en/man/*.[1-9]
+
+	# Example configurations.
+	insinto /etc/maradns
+	newins doc/en/examples/example_full_mararc mararc_full.dist
+	newins doc/en/examples/example_csv2 example_csv2.dist
+	newins deadwood-${PV}/doc/dwood3rc-all dwood3rc_all.dist
+	keepdir /etc/maradns/logger
+
+	# Init scripts.
+	newinitd "${FILESDIR}"/maradns2 maradns
+	newinitd "${FILESDIR}"/zoneserver2 zoneserver
+	newinitd "${FILESDIR}"/deadwood deadwood
+
+	# systemd unit
+	# please keep paths in sync!
+	sed -e "s^@bindir@^${EPREFIX}/usr/sbin^" \
+		-e "s^@sysconfdir@^${EPREFIX}/etc/maradns^" \
+		"${FILESDIR}"/maradns.service.in > "${T}"/maradns.service \
+		|| die "failed to create the maradns.service file (sed)"
+
+	systemd_dounit "${T}"/maradns.service
+}
+
+pkg_postinst() {
+	elog "Examples of configuration files can be found in the"
+	elog "/etc/maradns directory, feel free use it like:"
+	elog "     cp /etc/maradns/mararc{_full.dist,}"
+	elog "and edit /etc/maradns/mararc as described in man mararc."
+}

net-dns/maradns/Manifest
@@ -1,1 +1,2 @@
 DIST maradns-3.5.0022.tar.xz 7375408 BLAKE2B d79fd800099f41e6395537b8a9d945820cd295c9a3be8bec0850b8c85df65a607b07c3a0949a6ed058d1155514836d5a72a3ab0512a4369a979b021c80159908 SHA512 0c4df5aac0a979a74f811c9ccd30e2708f6b21816d9d70b88bf31ac4a65175b46c3a53adf439e098c57aa9b79250c26a38c8cdf26e0ee52637092a184d852076
+DIST maradns-3.5.0036.tar.xz 9192932 BLAKE2B 43d2f84b937a176b794aff68fc4fe73b5aada96df27ebf16dde04be1b7883ff5a95a07b5ce37dc65b305979e1b4379d5b540e59f917bbf1518d4f909bab717a1 SHA512 0d8248ad858064aec162a1d4bbdcf48eca39e07403b243a318c6512e4fee57429c6aa216e434cb0cb6ae86182474ea9069dc8f69622e9eadb339cfa3a4dc554e

net-dns/maradns/files/maradns-3.5.0022-flags.patch
@@ -1,23 +1,15 @@
+Rebased on 2024-01-20, version 3.5.0036
+
 From 15470045421448a7a6813e2a4a96e18bba3b0aa4 Mon Sep 17 00:00:00 2001
 From: Victor Kustov <ktrace@yandex.ru>
 Date: Wed, 27 Jul 2022 21:28:31 +0300
 Subject: [PATCH] 2nd
 
----
- build/Makefile.linux | 32 ++++++++++++++++----------------
- 1 file changed, 16 insertions(+), 16 deletions(-)
-
 diff --git a/build/Makefile.linux b/build/Makefile.linux
-index 965c7fb..572dccc 100644
+index 9d8117e..22a7518 100644
 --- a/build/Makefile.linux
 +++ b/build/Makefile.linux
-@@ -24,32 +24,32 @@ EXECS=server/maradns
- # end the Solaris section
- # Non-Solaris version of "M"
- V="VERSION=$(VERSION)"
--Q="DEFINES=-DSELECT_PROBLEM"
-+Q="$(LDFLAGS) DEFINES=-DSELECT_PROBLEM"
- 
+@@ -29,29 +29,27 @@ Q="DEFINES=-DSELECT_PROBLEM"
  # Debug
  
  FLAGS = -O2 -Wall -DSELECT_PROBLEM
@@ -34,7 +26,9 @@ index 965c7fb..572dccc 100644
 -	make $(M) $(V) COMPILED=\"$(COMPILED)\" ; \
 -	cd ../tools ; make $(M) ; \
 -	cd ../deadwood-*/src/ ; make FLAGS=-O2 ; \
--	cd ../../tcp ; make $(M) $(V) ; cat ../00README.FIRST
+-	cd ../tools/blockhash ; make CFLAGS=-O2 ; \
+-	cd ../../../coLunacyDNS/ ; make ; \
+-	cd ../tcp ; make $(M) $(V) ; cat ../00README.FIRST
 +	cd libs ; $(MAKE) $(M) ; cd ../dns ; $(MAKE) $(M) ; \
 +	cd ../rng ; $(MAKE) $(M) ; cd ../parse ; $(MAKE) $(M) ; \
 +	cd ../qual ; $(MAKE) $(M) ; cd ../server ; \
@@ -60,6 +54,3 @@ index 965c7fb..572dccc 100644
  
  clean:
  	rm -f $(OBJECTS) core $(EXECS) ; \
--- 
-2.35.1
-

commit 0c153250617ed0d50f1ee4ac1e7aa53bad9e87af

net-dns/maradns: fix totally broken patch
In commit 73937e86b92dcf813bf021a83451e2ae4dbc423c, a version bump was
applied, and a patch file in FILESDIR which was applied to the old
version was "rebased" against the new release and applied in-place for
both the old and new versions.

Result: stable ebuild does not compile because patch doesn't match.

Fixes: 73937e86b92dcf813bf021a83451e2ae4dbc423c
Signed-off-by: Eli Schwartz <eschwartz93@gmail.com>
Signed-off-by: Sam James <sam@gentoo.org>

net-dns/maradns/files/maradns-3.5.0036-flags.patch
@@ -1,23 +1,15 @@
+Rebased on 2024-01-20, version 3.5.0036
+
 From 15470045421448a7a6813e2a4a96e18bba3b0aa4 Mon Sep 17 00:00:00 2001
 From: Victor Kustov <ktrace@yandex.ru>
 Date: Wed, 27 Jul 2022 21:28:31 +0300
 Subject: [PATCH] 2nd
 
----
- build/Makefile.linux | 32 ++++++++++++++++----------------
- 1 file changed, 16 insertions(+), 16 deletions(-)
-
 diff --git a/build/Makefile.linux b/build/Makefile.linux
-index 965c7fb..572dccc 100644
+index 9d8117e..22a7518 100644
 --- a/build/Makefile.linux
 +++ b/build/Makefile.linux
-@@ -24,32 +24,32 @@ EXECS=server/maradns
- # end the Solaris section
- # Non-Solaris version of "M"
- V="VERSION=$(VERSION)"
--Q="DEFINES=-DSELECT_PROBLEM"
-+Q="$(LDFLAGS) DEFINES=-DSELECT_PROBLEM"
- 
+@@ -29,29 +29,27 @@ Q="DEFINES=-DSELECT_PROBLEM"
  # Debug
  
  FLAGS = -O2 -Wall -DSELECT_PROBLEM
@@ -34,7 +26,9 @@ index 965c7fb..572dccc 100644
 -	make $(M) $(V) COMPILED=\"$(COMPILED)\" ; \
 -	cd ../tools ; make $(M) ; \
 -	cd ../deadwood-*/src/ ; make FLAGS=-O2 ; \
--	cd ../../tcp ; make $(M) $(V) ; cat ../00README.FIRST
+-	cd ../tools/blockhash ; make CFLAGS=-O2 ; \
+-	cd ../../../coLunacyDNS/ ; make ; \
+-	cd ../tcp ; make $(M) $(V) ; cat ../00README.FIRST
 +	cd libs ; $(MAKE) $(M) ; cd ../dns ; $(MAKE) $(M) ; \
 +	cd ../rng ; $(MAKE) $(M) ; cd ../parse ; $(MAKE) $(M) ; \
 +	cd ../qual ; $(MAKE) $(M) ; cd ../server ; \
@@ -60,6 +54,3 @@ index 965c7fb..572dccc 100644
  
  clean:
  	rm -f $(OBJECTS) core $(EXECS) ; \
--- 
-2.35.1
-

net-dns/maradns/maradns-3.5.0036.ebuild
@@ -25,7 +25,7 @@ DEPEND="
 RDEPEND="${DEPEND}"
 
 PATCHES=(
-	"${FILESDIR}"/${PN}-3.5.0022-flags.patch
+	"${FILESDIR}"/${PN}-3.5.0036-flags.patch
 )
 
 src_configure() {