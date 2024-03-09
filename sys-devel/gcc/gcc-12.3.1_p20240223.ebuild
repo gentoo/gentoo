gentoo/gentoo

commit e4dfb164264174461dd6ac8a826fb01b00e5dbf8

Revert "sys-devel/gcc: drop obsolete cross fixincludes patch for 13"
This reverts commit 456eee6e374b50a4d9108a642ca29c5573ac5cb8.

Restore the fixincludes revert. The situation has improved a bit though,
because we now only run fixincludes when it's truly needed, so having
this patch is less risky to begin with.

Caveat emptor for trying to remove this in future: make sure to test
clean builds without mingw already built, and with.

Bug: https://bugs.gentoo.org/905118
Bug: https://bugs.gentoo.org/925204
Closes: https://bugs.gentoo.org/926059
Signed-off-by: Sam James <sam@gentoo.org>

sys-devel/gcc/gcc-12.3.1_p20240223.ebuild
@@ -60,5 +60,6 @@ src_prepare() {
 
 	toolchain_src_prepare
 
+	eapply "${FILESDIR}"/${PN}-13-fix-cross-fixincludes.patch
 	eapply_user
 }

sys-devel/gcc/gcc-12.3.1_p20240301.ebuild
@@ -60,5 +60,6 @@ src_prepare() {
 
 	toolchain_src_prepare
 
+	eapply "${FILESDIR}"/${PN}-13-fix-cross-fixincludes.patch
 	eapply_user
 }

sys-devel/gcc/files/gcc-13-fix-cross-fixincludes.patch
@@ -1,0 +1,19 @@
+Revert of https://gcc.gnu.org/git/?p=gcc.git;a=commitdiff;h=59e4c98173a79fcaa2c33253261409f38856c384
+for now to fix cross fixincludes builds.
+
+https://bugs.gentoo.org/905118
+--- a/gcc/Makefile.in
++++ b/gcc/Makefile.in
+@@ -532,11 +532,7 @@ LINKER_PLUGIN_API_H = $(srcdir)/../include/plugin-api.h
+ # Default native SYSTEM_HEADER_DIR, to be overridden by targets.
+ NATIVE_SYSTEM_HEADER_DIR = @NATIVE_SYSTEM_HEADER_DIR@
+ # Default cross SYSTEM_HEADER_DIR, to be overridden by targets.
+-ifeq (@includedir@,$(prefix)/include)
+-  CROSS_SYSTEM_HEADER_DIR = @CROSS_SYSTEM_HEADER_DIR@
+-else
+-  CROSS_SYSTEM_HEADER_DIR = @includedir@
+-endif
++CROSS_SYSTEM_HEADER_DIR = @CROSS_SYSTEM_HEADER_DIR@
+ 
+ # autoconf sets SYSTEM_HEADER_DIR to one of the above.
+ # Purge it of unnecessary internal relative paths

sys-devel/gcc/gcc-13.2.1_p20240113-r1.ebuild
@@ -60,5 +60,6 @@ src_prepare() {
 
 	toolchain_src_prepare
 
+	eapply "${FILESDIR}"/${PN}-13-fix-cross-fixincludes.patch
 	eapply_user
 }

sys-devel/gcc/gcc-13.2.1_p20240210.ebuild
@@ -60,5 +60,6 @@ src_prepare() {
 
 	toolchain_src_prepare
 
+	eapply "${FILESDIR}"/${PN}-13-fix-cross-fixincludes.patch
 	eapply_user
 }

sys-devel/gcc/gcc-13.2.1_p20240224.ebuild
@@ -60,5 +60,6 @@ src_prepare() {
 
 	toolchain_src_prepare
 
+	eapply "${FILESDIR}"/${PN}-13-fix-cross-fixincludes.patch
 	eapply_user
 }

sys-devel/gcc/gcc-13.2.1_p20240302.ebuild
@@ -60,5 +60,6 @@ src_prepare() {
 
 	toolchain_src_prepare
 
+	eapply "${FILESDIR}"/${PN}-13-fix-cross-fixincludes.patch
 	eapply_user
 }

sys-devel/gcc/gcc-14.0.1_pre20240225.ebuild
@@ -60,5 +60,6 @@ src_prepare() {
 
 	toolchain_src_prepare
 
+	eapply "${FILESDIR}"/${PN}-13-fix-cross-fixincludes.patch
 	eapply_user
 }

commit 7b5ed51e8bbcd6ceee69937627db4f8c013da93b

sys-devel/gcc: don't apply fixincludes patch to <13
Fixes: e4dfb164264174461dd6ac8a826fb01b00e5dbf8
Signed-off-by: Sam James <sam@gentoo.org>

sys-devel/gcc/gcc-12.3.1_p20240223.ebuild
@@ -60,6 +60,5 @@ src_prepare() {
 
 	toolchain_src_prepare
 
-	eapply "${FILESDIR}"/${PN}-13-fix-cross-fixincludes.patch
 	eapply_user
 }

sys-devel/gcc/gcc-12.3.1_p20240301.ebuild
@@ -60,6 +60,5 @@ src_prepare() {
 
 	toolchain_src_prepare
 
-	eapply "${FILESDIR}"/${PN}-13-fix-cross-fixincludes.patch
 	eapply_user
 }