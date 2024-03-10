gentoo/gentoo

commit 1351ba3350e8706c0c280a76c1f1541668a291a8

*/*: unkeyword 1.27
Signed-off-by: Ian Jordan <immoloism@gmail.com>
Closes: https://github.com/gentoo/gentoo/pull/34340
Signed-off-by: Sam James <sam@gentoo.org>

x11-misc/mate-notification-daemon/mate-notification-daemon-1.27.1.ebuild
@@ -7,8 +7,11 @@ MATE_LA_PUNT="yes"
 
 inherit mate
 
-if [[ ${PV} != 9999 ]]; then
+MINOR=$((ver_cut 2) % 2)
+if [[ ${MINOR} -eq 0 ]]; then
 	KEYWORDS="~amd64 ~arm ~arm64 ~loong ~riscv ~x86"
+else
+	KEYWORDS=""
 fi
 
 DESCRIPTION="MATE Notification daemon"

app-arch/engrampa/engrampa-1.27.1.ebuild
@@ -3,12 +3,11 @@
 
 EAPI=8
 
-if [[ ${PV} == 9999* ]]; then
-	EGIT_REPO_URI="https://github.com/mate-desktop/${PN}.git"
-	inherit git-r3
-else
-	SRC_URI="https://github.com/mate-desktop/${PN}/archive/${P}.tar.xz"
+MINOR=$((ver_cut 2) % 2)
+if [[ ${MINOR} -eq 0 ]]; then
 	KEYWORDS="~amd64 ~arm ~arm64 ~loong ~riscv ~x86"
+else
+	KEYWORDS=""
 fi
 
 inherit mate optfeature

app-editors/pluma/pluma-1.27.0.ebuild
@@ -9,9 +9,15 @@ inherit mate python-single-r1 virtualx
 
 DESCRIPTION="Pluma text editor for the MATE desktop"
 
+MINOR=$((ver_cut 2) % 2)
+if [[ ${MINOR} -eq 0 ]]; then
+	KEYWORDS="~amd64 ~arm ~arm64 ~loong ~riscv ~x86"
+else
+	KEYWORDS=""
+fi
+
 LICENSE="FDL-1.1+ GPL-2+ LGPL-2+"
-SLOT="0"
-KEYWORDS="~amd64 ~arm ~arm64 ~loong ~riscv ~x86"
+SLOT=0
 IUSE="+introspection spell test"
 REQUIRED_USE="${PYTHON_REQUIRED_USE}"
 RESTRICT="!test? ( test )"

mate-base/caja/caja-1.27.2.ebuild
@@ -7,8 +7,11 @@ MATE_LA_PUNT="yes"
 
 inherit mate
 
-if [[ ${PV} != 9999 ]]; then
-	KEYWORDS="~amd64 ~arm ~arm64 ~loong ~riscv ~x86"
+MINOR=$((ver_cut 2) % 2)
+if [[ ${MINOR} -eq 0 ]]; then
+        KEYWORDS="~amd64 ~arm ~arm64 ~loong ~riscv ~x86"
+else
+	KEYWORDS=""
 fi
 
 DESCRIPTION="Caja file manager for the MATE desktop"

mate-base/libmatekbd/libmatekbd-1.27.1.ebuild
@@ -7,8 +7,11 @@ MATE_LA_PUNT="yes"
 
 inherit mate
 
-if [[ ${PV} != 9999 ]]; then
+MINOR=$((ver_cut 2) % 2)
+if [[ ${MINOR} -eq 0 ]]; then
 	KEYWORDS="~amd64 ~arm ~arm64 ~loong ~riscv ~x86"
+else
+	KEYWORDS=""
 fi
 
 DESCRIPTION="MATE keyboard configuration library"

mate-base/mate-applets-meta/mate-applets-meta-1.27.1.ebuild
@@ -3,11 +3,12 @@
 
 EAPI=8
 
-if [[ ${PV} == 9999 ]]; then
-	MATE_BRANCH=9999
-else
-	MATE_BRANCH="$(ver_cut 1-2)"
+MATE_BRANCH="$(ver_cut 1-2)"
+MINOR=$((ver_cut 2) % 2)
+if [[ ${MINOR} -eq 0 ]]; then
 	KEYWORDS="~amd64 ~arm ~arm64 ~loong ~riscv ~x86"
+else
+	KEYWORDS=""
 fi
 
 DESCRIPTION="Meta package for MATE panel applets"

mate-base/mate-applets/mate-applets-1.27.1.ebuild
@@ -5,7 +5,12 @@ EAPI=8
 
 inherit mate
 
-KEYWORDS="~amd64 ~arm ~arm64 ~loong ~riscv ~x86"
+MINOR=$((ver_cut 2) % 2)
+if [[ ${MINOR} -eq 0 ]]; then
+	KEYWORDS="~amd64 ~arm ~arm64 ~loong ~riscv ~x86"
+else
+	KEYWORDS=""
+fi
 
 DESCRIPTION="Applets for the MATE Desktop and Panel"
 LICENSE="CC-BY-SA-3.0 FDL-1.1+ GPL-2+ GPL-3+ LGPL-2+"

mate-base/mate-common/Manifest
@@ -1,3 +1,2 @@
 DIST mate-common-1.26.0.tar.xz 69604 BLAKE2B 73c4b065eba0287d3f16ec3fdff8145dd97b765283f4017b31a7b86cb45949a4f7dde0207c244dc5c8047570830b85714955c23165889ae994a566f2f00a752c SHA512 9bfde79c9243d394f9ebc743f6a3c27199ec1e857f80020be0ff8b64a48cf1183fffd88783c1d375f2983a32c7437ca49b2f03b3e844e147c5750f71dbfb93bd
-DIST mate-common-1.27.0.tar.xz 70444 BLAKE2B 670f6ffad51abd2786171206fdf1636b92a99704617b6fb96a2a3b95dd29ea0958a2795b5a83dd4aa2d29a39125915128b898c47d6e5c1bc51e84b49c3089fe3 SHA512 45e301982090a6ed37d04bf351cc9d5a61d37b52e9ca2732d9186918f05cb4d1a413d8837161e6c02e90d0fb90e6bc23fc73ffee04a8695cb3b121909b7ea812
 DIST mate-common-1.27.1.tar.xz 70492 BLAKE2B 5a9766d71f749bfae00e742e647dcfa0d34a971f9d63a9aa6c2ed608b1fc2ac65e9221b5d70ed5162c17b5d92ef819b544d2f05d98d14723583e9b6dde8d821c SHA512 91f63c3f3317bea78e9413021dc96269feb2d9e0e1572d72bb7f2b3da62a20ea1f32269b7c2e54645f31bd16d84f1b3ae6858f8476ad92cb7503e31957f10635

commit 0a6c6f923447c9a3c6da56ed3235487affff8925

x11-misc/mate-notification-daemon: fix MultipleKeywordsLines
Fixes: 1351ba3350e8706c0c280a76c1f1541668a291a8
Signed-off-by: Sam James <sam@gentoo.org>

x11-misc/mate-notification-daemon/mate-notification-daemon-1.27.1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2023 Gentoo Authors
+# Copyright 1999-2024 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -10,8 +10,6 @@ inherit mate
 MINOR=$(($(ver_cut 2) % 2))
 if [[ ${MINOR} -eq 0 ]]; then
 	KEYWORDS="~amd64 ~arm ~arm64 ~loong ~riscv ~x86"
-else
-	KEYWORDS=""
 fi
 
 DESCRIPTION="MATE Notification daemon"