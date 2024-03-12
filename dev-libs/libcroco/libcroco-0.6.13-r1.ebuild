gentoo/gentoo

commit d9dce45487c3a80c68400fb690e86faaa4afe48f

dev-libs/libcroco: filter LTO, no-SA
Archived upstream and a complicated situation with all the bundled copies too..

Closes: https://bugs.gentoo.org/855704
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/libcroco/libcroco-0.6.13-r1.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=7
 
-inherit gnome2 multilib-minimal
+inherit flag-o-matic gnome2 multilib-minimal
 
 DESCRIPTION="Generic Cascading Style Sheet (CSS) parsing and manipulation toolkit"
 HOMEPAGE="https://gitlab.gnome.org/Archive/libcroco"
@@ -34,6 +34,14 @@ src_prepare() {
 	fi
 
 	gnome2_src_prepare
+}
+
+src_configure() {
+	# bug #855704
+	append-flags -fno-strict-aliasing
+	filter-lto
+
+	gnome2_src_configure
 }
 
 multilib_src_configure() {

commit 98d7552f37f5daf90ecfb09bb73368e572f3ad85

dev-libs/libcroco: fix configure
Fixes: d9dce45487c3a80c68400fb690e86faaa4afe48f
Closes: https://bugs.gentoo.org/926800
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/libcroco/libcroco-0.6.13-r1.ebuild
@@ -41,7 +41,7 @@ src_configure() {
 	append-flags -fno-strict-aliasing
 	filter-lto
 
-	gnome2_src_configure
+	multilib-minimal_src_configure
 }
 
 multilib_src_configure() {