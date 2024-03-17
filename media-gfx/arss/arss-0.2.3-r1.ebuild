gentoo/gentoo

commit e886fb559492e6f472ff70aa2794c33e5225c59e

media-gfx/arss: mark as LTO-unsafe, strict-aliasing unsafe
Closes: https://bugs.gentoo.org/859604
Signed-off-by: Eli Schwartz <eschwartz93@gmail.com>
Signed-off-by: Sam James <sam@gentoo.org>

media-gfx/arss/arss-0.2.3-r1.ebuild
@@ -1,11 +1,11 @@
-# Copyright 1999-2023 Gentoo Authors
+# Copyright 1999-2024 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
 
 CMAKE_IN_SOURCE_BUILD="true"
 MY_P="${P}-src"
-inherit cmake
+inherit cmake flag-o-matic
 
 DESCRIPTION="Analysis & Resynthesis Sound Spectrograph"
 HOMEPAGE="https://arss.sourceforge.net"
@@ -22,3 +22,15 @@ RDEPEND="${DEPEND}"
 PATCHES=( "${FILESDIR}"/${P}-fno-common.patch )
 
 DOCS=( ../AUTHORS ../ChangeLog )
+
+src_compile() {
+	# -Werror=strict-aliasing
+	# https://bugs.gentoo.org/859604
+	# Upstream is on sourceforge, inactive since 2009. No bug filed.
+	#
+	# Do not trust it for LTO either.
+	append-flags -fno-strict-aliasing
+	filter-lto
+
+	default
+}

commit 5ccfe32874dab1d3531dc007f8695ada8c738888

media-gfx/arss: fix LTO properly this time
Unsure what happened to me but this logic did not work as the wrong
phase was modified.

Fixes: e886fb559492e6f472ff70aa2794c33e5225c59e
Bug: https://bugs.gentoo.org/859604
Closes: https://bugs.gentoo.org/926752
Signed-off-by: Eli Schwartz <eschwartz93@gmail.com>
Signed-off-by: Sam James <sam@gentoo.org>

media-gfx/arss/arss-0.2.3-r1.ebuild
@@ -23,7 +23,7 @@ PATCHES=( "${FILESDIR}"/${P}-fno-common.patch )
 
 DOCS=( ../AUTHORS ../ChangeLog )
 
-src_compile() {
+src_configure() {
 	# -Werror=strict-aliasing
 	# https://bugs.gentoo.org/859604
 	# Upstream is on sourceforge, inactive since 2009. No bug filed.
@@ -32,5 +32,5 @@ src_compile() {
 	append-flags -fno-strict-aliasing
 	filter-lto
 
-	default
+	cmake_src_configure
 }