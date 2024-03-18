gentoo/gentoo

commit 37d9b41aa4872eef5ecfa27263874b798d794257

sci-electronics/kicad: Fix failing tests
Closes: https://bugs.gentoo.org/895012
Closes: https://github.com/gentoo/gentoo/pull/35798
Signed-off-by: Zoltan Puskas <zoltan@sinustrom.info>
Signed-off-by: Yixun Lan <dlan@gentoo.org>

sci-electronics/kicad/kicad-7.0.7.ebuild
@@ -58,6 +58,9 @@ COMMON_DEPEND="
 	nls? (
 		sys-devel/gettext
 	)
+	test? (
+		media-gfx/cairosvg
+	)
 "
 DEPEND="${COMMON_DEPEND}"
 RDEPEND="${COMMON_DEPEND}
@@ -129,7 +132,8 @@ src_compile() {
 
 src_test() {
 	# Test cannot find library in Portage's sandbox. Let's create a link so test can run.
-	ln -s "${BUILD_DIR}/eeschema/_eeschema.kiface" "${BUILD_DIR}/qa/eeschema/_eeschema.kiface" || die
+	mkdir -p "${BUILD_DIR}/qa/eeschema/" || die
+	dosym "${BUILD_DIR}/eeschema/_eeschema.kiface" "${BUILD_DIR}/qa/eeschema/_eeschema.kiface" || die
 
 	# LD_LIBRARY_PATH is there to help it pick up the just-built libraries
 	LD_LIBRARY_PATH="${BUILD_DIR}/3d-viewer/3d_cache/sg:${LD_LIBRARY_PATH}" cmake_src_test

sci-electronics/kicad/kicad-7.0.10.ebuild
@@ -60,6 +60,9 @@ COMMON_DEPEND="
 	nls? (
 		sys-devel/gettext
 	)
+	test? (
+		media-gfx/cairosvg
+	)
 "
 DEPEND="${COMMON_DEPEND}"
 RDEPEND="${COMMON_DEPEND}
@@ -133,7 +136,8 @@ src_compile() {
 
 src_test() {
 	# Test cannot find library in Portage's sandbox. Let's create a link so test can run.
-	ln -s "${BUILD_DIR}/eeschema/_eeschema.kiface" "${BUILD_DIR}/qa/eeschema/_eeschema.kiface" || die
+	mkdir -p "${BUILD_DIR}/qa/eeschema/" || die
+	dosym "${BUILD_DIR}/eeschema/_eeschema.kiface" "${BUILD_DIR}/qa/eeschema/_eeschema.kiface" || die
 
 	# LD_LIBRARY_PATH is there to help it pick up the just-built libraries
 	LD_LIBRARY_PATH="${BUILD_DIR}/3d-viewer/3d_cache/sg:${LD_LIBRARY_PATH}" cmake_src_test

sci-electronics/kicad/kicad-7.0.11.ebuild
@@ -60,6 +60,9 @@ COMMON_DEPEND="
 	nls? (
 		sys-devel/gettext
 	)
+	test? (
+		media-gfx/cairosvg
+	)
 "
 DEPEND="${COMMON_DEPEND}"
 RDEPEND="${COMMON_DEPEND}
@@ -133,7 +136,8 @@ src_compile() {
 
 src_test() {
 	# Test cannot find library in Portage's sandbox. Let's create a link so test can run.
-	ln -s "${BUILD_DIR}/eeschema/_eeschema.kiface" "${BUILD_DIR}/qa/eeschema/_eeschema.kiface" || die
+	mkdir -p "${BUILD_DIR}/qa/eeschema/" || die
+	dosym "${BUILD_DIR}/eeschema/_eeschema.kiface" "${BUILD_DIR}/qa/eeschema/_eeschema.kiface" || die
 
 	# LD_LIBRARY_PATH is there to help it pick up the just-built libraries
 	LD_LIBRARY_PATH="${BUILD_DIR}/3d-viewer/3d_cache/sg:${LD_LIBRARY_PATH}" cmake_src_test

sci-electronics/kicad/kicad-8.0.0.ebuild
@@ -62,6 +62,9 @@ COMMON_DEPEND="
 	nls? (
 		sys-devel/gettext
 	)
+	test? (
+		media-gfx/cairosvg
+	)
 "
 DEPEND="${COMMON_DEPEND}"
 RDEPEND="${COMMON_DEPEND}
@@ -134,7 +137,8 @@ src_compile() {
 
 src_test() {
 	# Test cannot find library in Portage's sandbox. Let's create a link so test can run.
-	ln -s "${BUILD_DIR}/eeschema/_eeschema.kiface" "${BUILD_DIR}/qa/eeschema/_eeschema.kiface" || die
+	mkdir -p "${BUILD_DIR}/qa/eeschema/" || die
+	dosym "${BUILD_DIR}/eeschema/_eeschema.kiface" "${BUILD_DIR}/qa/eeschema/_eeschema.kiface" || die
 
 	# LD_LIBRARY_PATH is there to help it pick up the just-built libraries
 	LD_LIBRARY_PATH="${BUILD_DIR}/3d-viewer/3d_cache/sg:${LD_LIBRARY_PATH}" cmake_src_test

sci-electronics/kicad/kicad-8.0.1.ebuild
@@ -62,6 +62,9 @@ COMMON_DEPEND="
 	nls? (
 		sys-devel/gettext
 	)
+	test? (
+		media-gfx/cairosvg
+	)
 "
 DEPEND="${COMMON_DEPEND}"
 RDEPEND="${COMMON_DEPEND}
@@ -134,7 +137,8 @@ src_compile() {
 
 src_test() {
 	# Test cannot find library in Portage's sandbox. Let's create a link so test can run.
-	ln -s "${BUILD_DIR}/eeschema/_eeschema.kiface" "${BUILD_DIR}/qa/eeschema/_eeschema.kiface" || die
+	mkdir -p "${BUILD_DIR}/qa/eeschema/" || die
+	dosym "${BUILD_DIR}/eeschema/_eeschema.kiface" "${BUILD_DIR}/qa/eeschema/_eeschema.kiface" || die
 
 	# LD_LIBRARY_PATH is there to help it pick up the just-built libraries
 	LD_LIBRARY_PATH="${BUILD_DIR}/3d-viewer/3d_cache/sg:${LD_LIBRARY_PATH}" cmake_src_test

sci-electronics/kicad/kicad-9999.ebuild
@@ -62,6 +62,9 @@ COMMON_DEPEND="
 	nls? (
 		sys-devel/gettext
 	)
+	test? (
+		media-gfx/cairosvg
+	)
 "
 DEPEND="${COMMON_DEPEND}"
 RDEPEND="${COMMON_DEPEND}
@@ -134,7 +137,8 @@ src_compile() {
 
 src_test() {
 	# Test cannot find library in Portage's sandbox. Let's create a link so test can run.
-	ln -s "${BUILD_DIR}/eeschema/_eeschema.kiface" "${BUILD_DIR}/qa/eeschema/_eeschema.kiface" || die
+	mkdir -p "${BUILD_DIR}/qa/eeschema/" || die
+	dosym "${BUILD_DIR}/eeschema/_eeschema.kiface" "${BUILD_DIR}/qa/eeschema/_eeschema.kiface" || die
 
 	# LD_LIBRARY_PATH is there to help it pick up the just-built libraries
 	LD_LIBRARY_PATH="${BUILD_DIR}/3d-viewer/3d_cache/sg:${LD_LIBRARY_PATH}" cmake_src_test

commit e54678bf83c04f5f2725e0b3a2127cf53ac4feca

sci-electronics/kicad: fix UnstatedIuse for USE=test
Note that I've not checked if the configure args need to be added to 7.0.7,
just fixing CI.

Bug: https://bugs.gentoo.org/895012
Fixes: 37d9b41aa4872eef5ecfa27263874b798d794257
Signed-off-by: Sam James <sam@gentoo.org>

sci-electronics/kicad/kicad-7.0.7.ebuild
@@ -28,9 +28,11 @@ fi
 # BSD for bundled pybind
 LICENSE="GPL-2+ GPL-3+ Boost-1.0 BSD"
 SLOT="0"
-IUSE="doc examples nls openmp"
+IUSE="doc examples nls openmp test"
 
 REQUIRED_USE="${PYTHON_REQUIRED_USE}"
+
+RESTRICT="!test? ( test )"
 
 # Contains bundled pybind but it's patched for wx
 # See https://gitlab.com/kicad/code/kicad/-/commit/74e4370a9b146b21883d6a2d1df46c7a10bd0424