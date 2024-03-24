gentoo/gentoo

commit 0e09cd6a8296d4ee95598eecbb473a9d7c8e8d86

sys-fs/fuse-overlayfs: add kernel check in 1.13, drop 1.{7.1,10,11}
proxied-maint

Signed-off-by: Rahil Bhimjiani <me@rahil.rocks>
Signed-off-by: Sam James <sam@gentoo.org>

sys-fs/fuse-overlayfs/fuse-overlayfs-1.13.ebuild
@@ -17,6 +17,10 @@ DEPEND="sys-fs/fuse:3"
 RDEPEND="${DEPEND}"
 BDEPEND="virtual/pkgconfig"
 
+pkg_pretend() {
+	kernel_is -lt 4 18 && eerror "Linux Kernel > v4.18.0 is required" && die
+}
+
 src_prepare() {
 	default
 	eautoreconf

sys-fs/fuse-overlayfs/Manifest
@@ -1,5 +1,2 @@
-DIST fuse-overlayfs-1.10.tar.gz 104797 BLAKE2B 112dc4814fa817f41dbf938d835366ac38cfc1963cffa7d2e84baaed6e91c7f7db8aa28b3e898b4334aa3f5a4cec8489490a8973ccb3a9bf5781ed6e7e859856 SHA512 cb399a56f2cb0ccf3d294d82cfaa9682db6812e709b1b6d3edf6ce4f7653ddddeffb2810d1c5f8a4178dcdb42e0f65af878fd5c007b31ed18538464482ad1dcf
-DIST fuse-overlayfs-1.11.tar.gz 104921 BLAKE2B d4048957681255a8cce0293b2e899ebd0709ca33c45eaebc5a58ac3db470a573ddf47fe22d1fa460bc2ea4ac6c90003bdd2ce82ed2edd8fb3d64668afd9d07eb SHA512 871424424f76c3d35a9b6005c1fa3d392044581f27d62a6b6c04f4d57ad12421301415c5ada7798e0fbf71043aa90a0194e49922b638b988a355e09fef909d61
 DIST fuse-overlayfs-1.12.tar.gz 99684 BLAKE2B e7ef075c043738d506efcf973ecef7eb321e9f2b2812b318a4087d2a913f022240ed4b0d46e47463daff6976dd99fb587a5819a22c7e58dd52029930e92abffc SHA512 f113ac20b389d2f1c5e5ff160a60c308017e74c9c85d74a7200bab81a4cfa30335a64740c173f17c91ab4feddffb138ca4378e92894645a67eea5ac73d42890f
 DIST fuse-overlayfs-1.13.tar.gz 99775 BLAKE2B 7a0643a5ef029db7f3d1009e9dd4e7a647bf936205723fc3656dfd6d14d622e98f9df15590783492f056ab3f151fd658b6fe65f7177f9499532d02411138ac7c SHA512 2f005b7eb4f0cc31dd75a6c453d81fbbf1c77cb6c1e8f991a53465c1e8c0109c999063a3b2088978abeea2272677985cd79ecbbbcd7aaaac0088dcdd34981f4b
-DIST fuse-overlayfs-1.7.1.tar.gz 103575 BLAKE2B 1fc92852f3afdead707af8745db03e2f5b0cf1f154f12c13850e5c37b30a0ad84122aa06c7f96c3825cd89cc2d26874abecb8ffbf0ad4edec5e0ffc854962944 SHA512 2199f9a5040832537b57792fe361943065c6132d31613bb1f838e931049703011902fca3abc25f6d7136efb2eb5505161f11c252b91dfb19158ac663e1ec15c2

sys-fs/fuse-overlayfs/fuse-overlayfs-1.10.ebuild
@@ -1,26 +1,0 @@
-# Copyright 2019-2023 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=8
-
-inherit autotools
-
-DESCRIPTION="FUSE implementation for overlayfs"
-HOMEPAGE="https://github.com/containers/fuse-overlayfs"
-EGIT_COMMIT="v${PV}"
-SRC_URI="https://github.com/containers/fuse-overlayfs/archive/${EGIT_COMMIT}.tar.gz -> ${P}.tar.gz"
-
-LICENSE="GPL-3"
-SLOT="0"
-KEYWORDS="amd64 arm64 ~ppc64 ~riscv"
-
-RDEPEND=">=sys-fs/fuse-3:="
-DEPEND="${RDEPEND}"
-BDEPEND="virtual/pkgconfig"
-
-S="${WORKDIR}/${PN}-${EGIT_COMMIT#v}"
-
-src_prepare() {
-	default
-	eautoreconf
-}

sys-fs/fuse-overlayfs/fuse-overlayfs-1.11.ebuild
@@ -1,23 +1,0 @@
-# Copyright 2019-2023 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=8
-
-inherit autotools
-
-DESCRIPTION="FUSE implementation for overlayfs"
-HOMEPAGE="https://github.com/containers/fuse-overlayfs"
-SRC_URI="https://github.com/containers/fuse-overlayfs/archive/v${PV}.tar.gz -> ${P}.tar.gz"
-
-LICENSE="GPL-3"
-SLOT="0"
-KEYWORDS="amd64 arm64 ~ppc64 ~riscv"
-
-DEPEND="sys-fs/fuse:3"
-RDEPEND="${DEPEND}"
-BDEPEND="virtual/pkgconfig"
-
-src_prepare() {
-	default
-	eautoreconf
-}

sys-fs/fuse-overlayfs/fuse-overlayfs-1.7.1.ebuild
@@ -1,26 +1,0 @@
-# Copyright 2019-2021 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=7
-
-inherit autotools
-
-DESCRIPTION="FUSE implementation for overlayfs"
-HOMEPAGE="https://github.com/containers/fuse-overlayfs"
-EGIT_COMMIT="v${PV}"
-SRC_URI="https://github.com/containers/fuse-overlayfs/archive/${EGIT_COMMIT}.tar.gz -> ${P}.tar.gz"
-
-LICENSE="GPL-3"
-SLOT="0"
-KEYWORDS="amd64 arm64 ~ppc64"
-
-RDEPEND=">=sys-fs/fuse-3:="
-DEPEND="${RDEPEND}"
-BDEPEND="virtual/pkgconfig"
-
-S="${WORKDIR}/${PN}-${EGIT_COMMIT#v}"
-
-src_prepare() {
-	default
-	eautoreconf
-}

sys-fs/fuse-overlayfs/metadata.xml
@@ -8,4 +8,12 @@
 		<email>zmedico@gentoo.org</email>
 		<name>Zac Medico</name>
 	</maintainer>
+	<maintainer type="person" proxied="yes">
+		<email>me@rahil.rocks</email>
+		<name>Rahil Bhimjiani</name>
+	</maintainer>
+	<maintainer type="project" proxied="proxy">
+		<email>proxy-maint@gentoo.org</email>
+		<name>Proxy Maintainers</name>
+	</maintainer>
 </pkgmetadata>

commit 116b684f7b5227af20383cd6acba85ac7dfec8f0

sys-fs/fuse-overlayfs: add missing linux-info inherit
Fixes: 0e09cd6a8296d4ee95598eecbb473a9d7c8e8d86
Signed-off-by: Sam James <sam@gentoo.org>

sys-fs/fuse-overlayfs/fuse-overlayfs-1.13.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-inherit autotools
+inherit autotools linux-info
 
 DESCRIPTION="FUSE implementation for overlayfs"
 HOMEPAGE="https://github.com/containers/fuse-overlayfs"