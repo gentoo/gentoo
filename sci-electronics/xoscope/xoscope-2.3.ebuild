gentoo/gentoo

commit 3e255fb3f9352a6662923b47114ad8d7cc4f4e35

sci-electronics/xoscope: mark as LTO-unsafe
Upstream is:
- sourceforge
- inactive for 3 years

I did not bother to report a bug as it seems all pain, no gain.

Closes: https://bugs.gentoo.org/878065
Signed-off-by: Eli Schwartz <eschwartz93@gmail.com>
Signed-off-by: Sam James <sam@gentoo.org>

sci-electronics/xoscope/xoscope-2.3.ebuild
@@ -1,9 +1,9 @@
-# Copyright 1999-2023 Gentoo Authors
+# Copyright 1999-2024 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
 
-inherit autotools linux-info
+inherit autotools flag-o-matic linux-info
 
 DESCRIPTION="Soundcard Oscilloscope for X"
 HOMEPAGE="http://xoscope.sourceforge.net"
@@ -31,4 +31,13 @@ PATCHES=( "${FILESDIR}"/${PN}-2.2-man_no_-Tutf8.patch )
 src_prepare() {
 	default
 	eautoreconf
+}
+
+src_compile() {
+	# -Werror=lto-type-mismatch
+	# https://bugs.gentoo.org/878065
+	# Upstream is a) sourceforge, b) inactive for 3 years. Bug not reported.
+	filter-lto
+
+	default
 }

commit 7b2039ce713a4a5643b35142177066f75425e078

sci-electronics/xoscope: fix the LTO fix to actually apply
It was applied too late for ./configure. oops.

While we are at it, update the comment. I ended up filing a bug. Was
very iffy on doing so, but 2 days ago, upstream demonstrated signs of
life via committing an update to git:

> spelling fix in man page (from Debian; fixed by Graham Inggs; reported by Petter Reinholdtsen)

Fixes: 3e255fb3f9352a6662923b47114ad8d7cc4f4e35
Closes: https://bugs.gentoo.org/927483
Signed-off-by: Eli Schwartz <eschwartz93@gmail.com>
Signed-off-by: Sam James <sam@gentoo.org>

sci-electronics/xoscope/xoscope-2.3.ebuild
@@ -33,10 +33,10 @@ src_prepare() {
 	eautoreconf
 }
 
-src_compile() {
+src_configure() {
 	# -Werror=lto-type-mismatch
 	# https://bugs.gentoo.org/878065
-	# Upstream is a) sourceforge, b) inactive for 3 years. Bug not reported.
+	# https://sourceforge.net/p/xoscope/bugs/23/
 	filter-lto
 
 	default