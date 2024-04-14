gentoo/gentoo

commit 4ad1a02c2fa34c183bf564623d5bc4ca7ad7bd10

app-containers/containers-common: Make 0.57.0 require older containers-image
This version requires a version of containers-image less than 5.29.2
which provides these files that collide with newer versions of
containers-common:

    /etc/containers/policy.json
    /etc/containers/registries.d/default.yaml

Bug: https://bugs.gentoo.org/930027
Signed-off-by: Zac Medico <zmedico@gentoo.org>

app-containers/containers-common/containers-common-0.57.0.ebuild
@@ -19,7 +19,7 @@ LICENSE="Apache-2.0"
 SLOT="0"
 RESTRICT="test"
 RDEPEND="
-	app-containers/containers-image
+	<app-containers/containers-image-5.29.2
 	app-containers/containers-storage
 	app-containers/containers-shortnames
 	!<app-containers/podman-4.5.0-r1

commit 4764a9acb220d0c847225c35d76ce86f5cc6ecab

app-containers/containers-common: destabilize 0.57.0 for ~arm64 (resolve NonsolvableDepsInStable)
This version requires containers-image-5.29.0 in order to avoid
a file collsion, but that version was never stabilized for arm64.

Fixes: 4ad1a02c2fa3 ("app-containers/containers-common: Make 0.57.0 require older containers-image")
Bug: https://bugs.gentoo.org/930027
Signed-off-by: Zac Medico <zmedico@gentoo.org>

app-containers/containers-common/containers-common-0.57.0.ebuild
@@ -12,7 +12,7 @@ if [[ ${PV} == 9999* ]]; then
 else
 	SRC_URI="https://github.com/containers/common/archive/v${PV}.tar.gz -> ${P}.tar.gz"
 	S="${WORKDIR}/${P#containers-}"
-	KEYWORDS="amd64 arm64 ~riscv"
+	KEYWORDS="amd64 ~arm64 ~riscv"
 fi
 
 LICENSE="Apache-2.0"