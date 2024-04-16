gentoo/gentoo

commit d58730757b79ce429d5fbe8f35dede6074d4c243

sys-libs/libseccomp: backport fix for arch-syscall-check
Bug: https://bugs.gentoo.org/926648
Signed-off-by: Mike Gilbert <floppym@gentoo.org>

sys-libs/libseccomp/libseccomp-2.5.5-r1.ebuild
@@ -49,6 +49,7 @@ PATCHES=(
 	"${FILESDIR}"/libseccomp-python-shared.patch
 	"${FILESDIR}"/libseccomp-2.5.3-skip-valgrind.patch
 	"${FILESDIR}"/libseccomp-2.5.5-which-hunt.patch
+	"${FILESDIR}"/libseccomp-2.5.5-arch-syscall-check.patch
 )
 
 src_prepare() {

sys-libs/libseccomp/files/libseccomp-2.5.5-arch-syscall-check.patch
@@ -1,0 +1,45 @@
+From 744c9a897b74ad66d065791593e25a05e4b6f6a1 Mon Sep 17 00:00:00 2001
+From: Michal Privoznik <mprivozn@redhat.com>
+Date: Tue, 1 Nov 2022 11:59:51 +0100
+Subject: [PATCH] src: Make arch-syscall-check work in VPATH build
+
+The aim of arch-syscall-check test is to check for syscalls
+missing implementation. It does so by comparing two files:
+
+  1) src/syscalls.csv
+  2) include/seccomp-syscalls.h
+
+However, due to use of relative paths these files are not found
+when doing a VPATH build. But, we can re-use an idea from GNU
+coreutils and get an absolute path to the source dir. All that's
+needed then is to prefix those two paths with the source dir
+path.
+
+Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
+Acked-by: Tom Hromatka <tom.hromatka@oracle.com>
+Signed-off-by: Paul Moore <paul@paul-moore.com>
+---
+ src/arch-syscall-check | 7 +++++--
+ 1 file changed, 5 insertions(+), 2 deletions(-)
+
+diff --git a/src/arch-syscall-check b/src/arch-syscall-check
+index ae67daa..9c7fd41 100755
+--- a/src/arch-syscall-check
++++ b/src/arch-syscall-check
+@@ -22,8 +22,11 @@
+ # along with this library; if not, see <http://www.gnu.org/licenses>.
+ #
+ 
+-SYSCALL_CSV="./syscalls.csv"
+-SYSCALL_HDR="../include/seccomp-syscalls.h"
++# Based on an idea from GNU coreutils
++abs_topsrcdir="$(unset CDPATH; cd $(dirname $0)/.. && pwd)"
++
++SYSCALL_CSV="$abs_topsrcdir/src/syscalls.csv"
++SYSCALL_HDR="$abs_topsrcdir/include/seccomp-syscalls.h"
+ 
+ function check_snr() {
+ 	(export LC_ALL=C; diff \
+-- 
+2.44.0
+

commit a8078cab1663057ea3245c645db6e3e48387e24a

sys-libs/libseccomp: rebase experimental-loong patches
The fix backported in d58730757b79ce429d5fbe8f35dede6074d4c243 was
already in the loong tree which makes the patch fail to apply.  Rebase
this and also now rather than having a huge patch that doesn't
correspond to VCS with a bunch of checked-in autotools stuff, use the
github diff and eautoreconf for 9999 and loong.

Fixes: d58730757b79 ("sys-libs/libseccomp: backport fix for arch-syscall-check")
Bug: https://bugs.gentoo.org/926648
Signed-off-by: Matoro Mahri <matoro_gentoo@matoro.tk>

sys-libs/libseccomp/Manifest
@@ -1,2 +1,3 @@
+DIST libseccomp-2.5.5-loongarch-r1.patch 119822 BLAKE2B 4aa75c1ac87b2ca25cf6be38dfd760879c7255ca8e6cf86be3ac6e354f76cdaf3c8e2f59b646254414ffb0f1ffe6b7c50478f4db895a6ce632db8782c9807e91 SHA512 f7cd768d672a25448b2a3ceda27db52e0d62b5d9ab3eeb906226b6ebc19332c89332e0b870aaf82d4ffcfd642c2deb6029a30ae9a6bd702ebad9fdd40622b582
 DIST libseccomp-2.5.5-loongarch64-20231204.patch.xz 41236 BLAKE2B 7831b28c275771bb675b4b5045e657d96c254e69cee7188a19f4812bc83343e0041772c20be1e7392ec88387726b49f34aa670a906b922dba531a0d35e548b1e SHA512 7870e536d834360dfec446493815b675a305f6bec1b79d0caae6fb546d2f694e77ce52e547bbcdb22cda272272bf3d5a427960b4aa76fb987d06ab035298007d
 DIST libseccomp-2.5.5.tar.gz 642445 BLAKE2B d770cee1f3e02fbbcd9f25655b360ab38160ad800e2829a67f2b9da62b095a90be99ac851a67344cf95bd6810a6268da4655dc1d37d996e58239c4999eb41998 SHA512 f630e7a7e53a21b7ccb4d3e7b37616b89aeceba916677c8e3032830411d77a14c2d74dcf594cd193b1acc11f52595072e28316dc44300e54083d5d7b314a38da

sys-libs/libseccomp/libseccomp-2.5.5-r1.ebuild
@@ -16,10 +16,14 @@ HOMEPAGE="https://github.com/seccomp/libseccomp"
 if [[ ${PV} == *9999 ]] ; then
 	EGIT_REPO_URI="https://github.com/seccomp/libseccomp.git"
 	PRERELEASE="2.6.0"
+	AUTOTOOLS_AUTO_DEPEND=yes
 	inherit autotools git-r3
 else
+	AUTOTOOLS_AUTO_DEPEND=no
+	inherit autotools
 	SRC_URI="https://github.com/seccomp/libseccomp/releases/download/v${PV}/${P}.tar.gz
-		experimental-loong? ( https://dev.gentoo.org/~xen0n/distfiles/${PN}-2.5.5-loongarch64-20231204.patch.xz )"
+		experimental-loong? ( https://github.com/matoro/libseccomp/compare/v${PV}..loongarch-r1.patch
+			-> ${P}-loongarch-r1.patch )"
 	KEYWORDS="-* amd64 arm arm64 ~hppa ~loong ~mips ppc ppc64 ~riscv ~s390 x86 ~amd64-linux ~x86-linux"
 fi
 
@@ -39,6 +43,7 @@ RDEPEND="${DEPEND}"
 BDEPEND="
 	${DEPEND}
 	dev-util/gperf
+	experimental-loong? ( ${AUTOTOOLS_DEPEND} )
 	python? (
 		${DISTUTILS_DEPS}
 		dev-python/cython[${PYTHON_USEDEP}]
@@ -54,10 +59,14 @@ PATCHES=(
 
 src_prepare() {
 	if use experimental-loong; then
-		PATCHES+=( "${WORKDIR}/${PN}-2.5.5-loongarch64-20231204.patch" )
+		PATCHES+=( "${DISTDIR}/${P}-loongarch-r1.patch" )
 	fi
 
 	default
+
+	if [[ ${PV} == *9999 ]] ; then
+		sed -i -e "s/0.0.0/${PRERELEASE}/" configure.ac || die
+	fi
 
 	if use experimental-loong; then
 		# touch generated files to avoid activating maintainer mode
@@ -66,9 +75,8 @@ src_prepare() {
 		find . -name Makefile.in -exec touch {} + || die
 	fi
 
-	if [[ ${PV} == *9999 ]] ; then
-		sed -i -e "s/0.0.0/${PRERELEASE}/" configure.ac || die
-
+	if [[ ${PV} == *9999 ]] || use experimental-loong; then
+		rm -f "include/seccomp.h" || die
 		eautoreconf
 	fi
 }