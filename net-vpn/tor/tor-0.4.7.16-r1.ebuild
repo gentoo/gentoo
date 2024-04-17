gentoo/gentoo

commit 6e604b1d706779f2a93bf349380d4531c5eede5a

net-vpn/tor: skip known-broken sandbox tests on arm32
The seccomp support in Tor, just like seccomp in general, is known to
be brittle and seccomp isn't enabled by default (bug #713690). It's therefore not a
serious failure even if we'd like it to get fixed.

Bug: https://bugs.gentoo.org/713690
Closes: https://bugs.gentoo.org/920905
Signed-off-by: Sam James <sam@gentoo.org>

net-vpn/tor/tor-0.4.7.16-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2023 Gentoo Authors
+# Copyright 1999-2024 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -143,6 +143,17 @@ src_test() {
 		:sandbox/open_filename
 		:sandbox/openat_filename
 	)
+
+	if use arm ; then
+		skip_tests+=(
+			# bug #920905
+			# https://gitlab.torproject.org/tpo/core/tor/-/issues/40912
+			:sandbox/opendir_dirname
+			:sandbox/openat_filename
+			:sandbox/chmod_filename
+			:sandbox/rename_filename
+		)
+	fi
 
 	# The makefile runs these by parallel by chunking them with a script
 	# but that means we lose verbosity and can't skip individual tests easily

net-vpn/tor/tor-0.4.8.10.ebuild
@@ -153,6 +153,17 @@ src_test() {
 		:sandbox/openat_filename
 	)
 
+	if use arm ; then
+		skip_tests+=(
+			# bug #920905
+			# https://gitlab.torproject.org/tpo/core/tor/-/issues/40912
+			:sandbox/opendir_dirname
+			:sandbox/openat_filename
+			:sandbox/chmod_filename
+			:sandbox/rename_filename
+		)
+	fi
+
 	# The makefile runs these by parallel by chunking them with a script
 	# but that means we lose verbosity and can't skip individual tests easily
 	# either.

net-vpn/tor/tor-0.4.8.11.ebuild
@@ -153,6 +153,17 @@ src_test() {
 		:sandbox/openat_filename
 	)
 
+	if use arm ; then
+		skip_tests+=(
+			# bug #920905
+			# https://gitlab.torproject.org/tpo/core/tor/-/issues/40912
+			:sandbox/opendir_dirname
+			:sandbox/openat_filename
+			:sandbox/chmod_filename
+			:sandbox/rename_filename
+		)
+	fi
+
 	# The makefile runs these by parallel by chunking them with a script
 	# but that means we lose verbosity and can't skip individual tests easily
 	# either.

net-vpn/tor/tor-9999.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2023 Gentoo Authors
+# Copyright 1999-2024 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -152,6 +152,17 @@ src_test() {
 		:sandbox/open_filename
 		:sandbox/openat_filename
 	)
+
+	if use arm ; then
+		skip_tests+=(
+			# bug #920905
+			# https://gitlab.torproject.org/tpo/core/tor/-/issues/40912
+			:sandbox/opendir_dirname
+			:sandbox/openat_filename
+			:sandbox/chmod_filename
+			:sandbox/rename_filename
+		)
+	fi
 
 	# The makefile runs these by parallel by chunking them with a script
 	# but that means we lose verbosity and can't skip individual tests easily

commit 61668a070e2bcab90e5af9e99a3f7db7c8f13f3e

net-vpn/tor: add another sandbox skip test for arm
Bug: https://bugs.gentoo.org/920905
Fixes: 6e604b1d706779f2a93bf349380d4531c5eede5a
Signed-off-by: Sam James <sam@gentoo.org>

net-vpn/tor/tor-0.4.7.16-r1.ebuild
@@ -151,6 +151,7 @@ src_test() {
 			:sandbox/opendir_dirname
 			:sandbox/openat_filename
 			:sandbox/chmod_filename
+			:sandbox/chown_filename
 			:sandbox/rename_filename
 		)
 	fi

net-vpn/tor/tor-0.4.8.10.ebuild
@@ -160,6 +160,7 @@ src_test() {
 			:sandbox/opendir_dirname
 			:sandbox/openat_filename
 			:sandbox/chmod_filename
+			:sandbox/chown_filename
 			:sandbox/rename_filename
 		)
 	fi

net-vpn/tor/tor-0.4.8.11.ebuild
@@ -160,6 +160,7 @@ src_test() {
 			:sandbox/opendir_dirname
 			:sandbox/openat_filename
 			:sandbox/chmod_filename
+			:sandbox/chown_filename
 			:sandbox/rename_filename
 		)
 	fi

net-vpn/tor/tor-9999.ebuild
@@ -160,6 +160,7 @@ src_test() {
 			:sandbox/opendir_dirname
 			:sandbox/openat_filename
 			:sandbox/chmod_filename
+			:sandbox/chown_filename
 			:sandbox/rename_filename
 		)
 	fi