gentoo/gentoo

commit a357ebd8c860107ac82c3600114b899b9df27f4b

media-libs/libfpx: EAPI8 bump, fix bug #847412
Signed-off-by: Michael Mair-Keimberger <mmk@levelnine.at>

Closes: https://bugs.gentoo.org/847412
Closes: https://github.com/gentoo/gentoo/pull/35019
Signed-off-by: Conrad Kostecki <conikost@gentoo.org>

media-libs/libfpx/libfpx-1.3.1_p10-r1.ebuild
@@ -1,0 +1,52 @@
+# Copyright 1999-2024 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit flag-o-matic libtool
+
+DESCRIPTION="Library for manipulating FlashPIX images"
+HOMEPAGE="https://github.com/ImageMagick/libfpx"
+SRC_URI="mirror://imagemagick/delegates/${P/_p/-}.tar.bz2"
+S="${WORKDIR}/${P/_p/-}"
+
+LICENSE="Flashpix"
+SLOT="0/1"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~m68k ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86 ~amd64-linux ~x86-linux ~ppc-macos"
+IUSE="static-libs"
+
+PATCHES=(
+	"${FILESDIR}"/${PN}-1.2.0.13-export-symbols.patch
+	"${FILESDIR}"/${PN}-1.3.1_p10-musl-1.2.3-null.patch
+)
+
+src_prepare() {
+	default
+
+	# we're not windows, even though we don't define __unix by default
+	[[ ${CHOST} == *-darwin* ]] && append-flags -D__unix
+
+	elibtoolize
+}
+
+src_configure() {
+	append-ldflags -Wl,--no-undefined
+	econf \
+		$(use_enable static-libs static) \
+		LIBS="-lstdc++ -lm"
+}
+
+src_install() {
+	default
+
+	# bug 847412
+	if ! use static-libs; then
+		find "${ED}" -type f -name '*.la' -delete || die
+	fi
+
+	dodoc AUTHORS ChangeLog doc/*.txt
+
+	docinto pdf
+	dodoc doc/*.pdf
+	docompress -x /usr/share/doc/${PF}/pdf
+}

commit 247bd57530051e238456c86dc082411a65743d9b

media-libs/libfpx: restore LTO workarounds
The PR hadn't been rebased so it lost the changes from 1ebd8326257a79006c4517d93f3a07eadd000834.

Bug: https://bugs.gentoo.org/847412
Bug: https://bugs.gentoo.org/859913
Fixes: a357ebd8c860107ac82c3600114b899b9df27f4b
Signed-off-by: Sam James <sam@gentoo.org>

media-libs/libfpx/libfpx-1.3.1_p10-r1.ebuild
@@ -30,6 +30,14 @@ src_prepare() {
 }
 
 src_configure() {
+	# -Werror=strict-aliasing
+	# https://bugs.gentoo.org/859913
+	# https://github.com/ImageMagick/libfpx/issues/6
+	#
+	# Do not trust for LTO either
+	append-flags -fno-strict-aliasing
+	filter-lto
+
 	append-ldflags -Wl,--no-undefined
 	econf \
 		$(use_enable static-libs static) \