gentoo/gentoo

commit 3cdee62a15218b82803a620b86b6b2cae1d8a8a9

dev-vcs/git-extras: add 7.2.0
Signed-off-by: Sam James <sam@gentoo.org>

dev-vcs/git-extras/git-extras-7.2.0.ebuild
@@ -1,0 +1,69 @@
+# Copyright 1999-2024 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+PYTHON_COMPAT=( python3_{11..12} )
+inherit bash-completion-r1 python-any-r1
+
+DESCRIPTION="Git utilities -- repo summary, repl, changelog population, and many more"
+HOMEPAGE="https://github.com/tj/git-extras"
+SRC_URI="https://github.com/tj/git-extras/archive/${PV}.tar.gz -> ${P}.tar.gz"
+
+LICENSE="MIT"
+SLOT="0"
+KEYWORDS="~amd64 ~arm64 ~x64-macos"
+IUSE="test"
+RESTRICT="!test? ( test )"
+
+RDEPEND="
+	>=app-shells/bash-4.0:*
+	dev-vcs/git
+"
+BDEPEND="
+	test? (
+		$(python_gen_any_dep '
+			>=dev-python/GitPython-3.1.40[${PYTHON_USEDEP}]
+			>=dev-python/pytest-7.2[${PYTHON_USEDEP}]
+		')
+	)
+"
+
+pkg_setup() {
+	use test && python-any-r1_pkg_setup
+}
+
+src_prepare() {
+	default
+
+	# For now, don't force including the git completion
+	sed -i -e "1 i source $(get_bashcompdir)\/git" etc/bash_completion.sh || die
+}
+
+src_compile() {
+	return
+}
+
+src_test() {
+	epytest
+}
+
+src_install() {
+	emake \
+		DESTDIR="${D}" \
+		PREFIX="${EPREFIX}/usr" \
+		SYSCONFDIR="${EPREFIX}/etc" \
+		COMPL_DIR="${D}/$(get_bashcompdir)" \
+		install
+
+	# TODO: Unfortunately, none of the completion seems to
+	# actually work for me yet(?)
+
+	newbashcomp "${S}"/etc/bash_completion.sh ${PN}
+
+	insinto /usr/share/zsh/site-functions
+	newins "${S}"/etc/${PN}-completion.zsh _${PN}
+
+	insinto /usr/share/fish/vendor_completions.d
+	doins "${S}"/etc/${PN}.fish
+}

dev-vcs/git-extras/Manifest
@@ -1,1 +1,2 @@
 DIST git-extras-7.1.0.tar.gz 176733 BLAKE2B 697f09c2993e85595620e272465ad2f4afa3bbd456a1041b92b01a98f962f3aa1fb5f22f1c9ef7e484b87734e8c9e8d15da15f71fb345b9049dd5bb1b98093c9 SHA512 955161b40c48ab92578efe487db78ea8d0fff499aaf8d4da2b13e8194a7f9297a84542c8642dd3a7c61c9a4d1e92342b5c586d341e4a003c9838d5e9731127b9
+DIST git-extras-7.2.0.tar.gz 183043 BLAKE2B 242adf211a39666b97996f0a45073171034930fe29ee3ebbfa07068886367832e17cc76398145d8802d32201c2563c8f2ba4171ed206344fbefbb10c4fadd6f3 SHA512 c97c00bab24415574c8b38f259a0becd5da2edafe8cb5ff373de50917bc2854e39b1c396c42b603d4f203af2853d0ebaefe074bfe353f7692af7b1a13e744586

commit 95c9cb63290d8e22ba06ea12e016c28031911f48

dev-vcs/git-extras: fix test deps
I was clearly working too late last night. Fix obvious error in omission
of python_check_deps and also the testpath dep.

Closes: https://bugs.gentoo.org/930758
Closes: https://bugs.gentoo.org/930800
Fixes: 3cdee62a15218b82803a620b86b6b2cae1d8a8a9
Signed-off-by: Sam James <sam@gentoo.org>

dev-vcs/git-extras/git-extras-7.2.0.ebuild
@@ -25,9 +25,16 @@ BDEPEND="
 		$(python_gen_any_dep '
 			>=dev-python/GitPython-3.1.40[${PYTHON_USEDEP}]
 			>=dev-python/pytest-7.2[${PYTHON_USEDEP}]
+			dev-python/testpath[${PYTHON_USEDEP}]
 		')
 	)
 "
+
+python_check_deps() {
+	python_has_version ">=dev-python/GitPython-3.1.40[${PYTHON_USEDEP}]" &&
+		python_has_version ">=dev-python/pytest-7.2[${PYTHON_USEDEP}]" &&
+		python_has_version "dev-python/testpath[${PYTHON_USEDEP}]"
+}
 
 pkg_setup() {
 	use test && python-any-r1_pkg_setup