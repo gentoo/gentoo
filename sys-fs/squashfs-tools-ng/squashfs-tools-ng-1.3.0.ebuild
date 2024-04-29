gentoo/gentoo

commit 84e655a1f462a58154b701b9b8077ebf3071e82f

sys-fs/squashfs-tools-ng: run elibtoolize in non-live ebuild
When building live, we always run eautoreconf. However, even in release
mode we should still run elibtoolize to get important fixes -- such as
the ones that pass through LTO sanity flags to the link phase.

Signed-off-by: Eli Schwartz <eschwartz93@gmail.com>
Signed-off-by: Sam James <sam@gentoo.org>

sys-fs/squashfs-tools-ng/squashfs-tools-ng-1.3.0.ebuild
@@ -9,7 +9,8 @@ if [[ ${PV} = 9999* ]]; then
 	inherit autotools git-r3
 	EGIT_REPO_URI="https://github.com/AgentD/${PN}.git"
 else
-	KEYWORDS="~alpha amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86"
+	inherit libtool
+	KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86"
 	SRC_URI="https://infraroot.at/pub/squashfs/${P}.tar.xz"
 fi
 
@@ -30,7 +31,11 @@ RDEPEND="${DEPEND}"
 
 src_prepare() {
 	default
-	[[ ${PV} == "9999" ]] && eautoreconf
+	if [[ ${PV} = "9999" ]]; then
+		eautoreconf
+	else
+		elibtoolize
+	fi
 }
 
 src_configure() {

sys-fs/squashfs-tools-ng/squashfs-tools-ng-1.2.0.ebuild
@@ -1,4 +1,4 @@
-# Copyright 2019-2023 Gentoo Authors
+# Copyright 2019-2024 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -9,6 +9,7 @@ if [[ ${PV} = 9999* ]]; then
 	inherit autotools git-r3
 	EGIT_REPO_URI="https://github.com/AgentD/${PN}.git"
 else
+	inherit libtool
 	KEYWORDS="~alpha amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86"
 	SRC_URI="https://infraroot.at/pub/squashfs/${P}.tar.xz"
 fi
@@ -30,7 +31,11 @@ RDEPEND="${DEPEND}"
 
 src_prepare() {
 	default
-	[[ ${PV} == "9999" ]] && eautoreconf
+	if [[ ${PV} = "9999" ]]; then
+		eautoreconf
+	else
+		elibtoolize
+	fi
 }
 
 src_configure() {

sys-fs/squashfs-tools-ng/squashfs-tools-ng-9999.ebuild
@@ -9,6 +9,7 @@ if [[ ${PV} = 9999* ]]; then
 	inherit autotools git-r3
 	EGIT_REPO_URI="https://github.com/AgentD/${PN}.git"
 else
+	inherit libtool
 	KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86"
 	SRC_URI="https://infraroot.at/pub/squashfs/${P}.tar.xz"
 fi
@@ -30,7 +31,11 @@ RDEPEND="${DEPEND}"
 
 src_prepare() {
 	default
-	[[ ${PV} == "9999" ]] && eautoreconf
+	if [[ ${PV} = "9999" ]]; then
+		eautoreconf
+	else
+		elibtoolize
+	fi
 }
 
 src_configure() {

commit 7579afbd4aa1f2356b8de3c2870df2c49ab68d5f

sys-fs/squashfs-tools-ng: stabilize 1.3.0 for amd64
Note that I merged 84e655a1f462a58154b701b9b8077ebf3071e82f badly
as I hit a merge conflict with my earlier 2c5cd6c4e004dc5037761c4a7a64fe956cac4bc4
when rebasing a branch.

I should figure out why merge-driver-ekeyword didn't handle the conflict.

Thanks to Matt for noticing.

Fixes: 84e655a1f462a58154b701b9b8077ebf3071e82f
Signed-off-by: Sam James <sam@gentoo.org>

sys-fs/squashfs-tools-ng/squashfs-tools-ng-1.3.0.ebuild
@@ -10,7 +10,7 @@ if [[ ${PV} = 9999* ]]; then
 	EGIT_REPO_URI="https://github.com/AgentD/${PN}.git"
 else
 	inherit libtool
-	KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86"
+	KEYWORDS="~alpha amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86"
 	SRC_URI="https://infraroot.at/pub/squashfs/${P}.tar.xz"
 fi
 