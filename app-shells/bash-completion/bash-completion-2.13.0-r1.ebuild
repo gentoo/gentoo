gentoo/gentoo

commit 3502e4f637e92eedb871b5aab25aec6ea238308a

app-shells/bash-completion: block old app-text/tree which installs its own completion
>=app-text/tree-2.1.1-r1 defers to the new one in >=bash-completion-2.13.0.

Closes: https://bugs.gentoo.org/931129
Signed-off-by: Sam James <sam@gentoo.org>

app-shells/bash-completion/bash-completion-2.13.0-r1.ebuild
@@ -27,6 +27,7 @@ RESTRICT="!test? ( test )"
 RDEPEND="
 	>=app-shells/bash-4.3_p30-r1:0
 	sys-apps/miscfiles
+	!<app-text/tree/tree-2.1.1-r1
 	!!net-fs/mc
 "
 BDEPEND="

app-shells/bash-completion/bash-completion-9999.ebuild
@@ -20,6 +20,7 @@ RESTRICT="!test? ( test )"
 RDEPEND="
 	>=app-shells/bash-4.3_p30-r1:0
 	sys-apps/miscfiles
+	!<app-text/tree/tree-2.1.1-r1
 	!!net-fs/mc
 "
 BDEPEND="

commit ee38c3bd430aea6267b9dbe51888ab58bb6be7c2

app-shells/bash-completion: fix blocker atom (!)
Fixes: 3502e4f637e92eedb871b5aab25aec6ea238308a
Signed-off-by: Sam James <sam@gentoo.org>

app-shells/bash-completion/bash-completion-2.13.0-r1.ebuild
@@ -27,7 +27,7 @@ RESTRICT="!test? ( test )"
 RDEPEND="
 	>=app-shells/bash-4.3_p30-r1:0
 	sys-apps/miscfiles
-	!<app-text/tree/tree-2.1.1-r1
+	!<app-text/tree-2.1.1-r1
 	!!net-fs/mc
 "
 BDEPEND="

app-shells/bash-completion/bash-completion-9999.ebuild
@@ -20,7 +20,7 @@ RESTRICT="!test? ( test )"
 RDEPEND="
 	>=app-shells/bash-4.3_p30-r1:0
 	sys-apps/miscfiles
-	!<app-text/tree/tree-2.1.1-r1
+	!<app-text/tree-2.1.1-r1
 	!!net-fs/mc
 "
 BDEPEND="