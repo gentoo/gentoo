gentoo/gentoo

commit 42c7cfb8934774ff857d057195bfae649fad7b90

sys-apps/irqbalance: version 1.9.4 doesn't work in systemd-based installations
There's an option (ProtectKernelTunables) in the systemd unit that doesn't
allow irqbalance to work. The upstream patch drops the option from the
unit file.

Signed-off-by: Cristian Othón Martínez Vera <cfuga@cfuga.mx>
Closes: https://github.com/gentoo/gentoo/pull/36535
Signed-off-by: Sam James <sam@gentoo.org>

sys-apps/irqbalance/irqbalance-1.9.4-r2.ebuild
@@ -33,6 +33,10 @@ RDEPEND="
 	selinux? ( sec-policy/selinux-irqbalance )
 "
 
+PATCHES=(
+	"${FILESDIR}"/${P}-drop-protectkerneltunables.patch
+)
+
 pkg_setup() {
 	CONFIG_CHECK="~PCI_MSI"
 	linux-info_pkg_setup

sys-apps/irqbalance/files/irqbalance-1.9.4-drop-protectkerneltunables.patch
@@ -1,0 +1,25 @@
+https://github.com/Irqbalance/irqbalance/issues/308
+https://github.com/Irqbalance/irqbalance/commit/f2c8309a4198d8f51069a783905049c5b7eb7600
+
+From f2c8309a4198d8f51069a783905049c5b7eb7600 Mon Sep 17 00:00:00 2001
+From: Neil Horman <nhorman@openssl.org>
+Date: Mon, 1 Apr 2024 08:05:14 -0400
+Subject: [PATCH] Drop ProtectKernelTunables
+
+It makes /proc/irq read only
+---
+ misc/irqbalance.service | 1 -
+ 1 file changed, 1 deletion(-)
+
+diff --git a/misc/irqbalance.service b/misc/irqbalance.service
+index 87e19c1..b731cc6 100644
+--- a/misc/irqbalance.service
++++ b/misc/irqbalance.service
+@@ -23,7 +23,6 @@ PrivateNetwork=yes
+ PrivateUsers=true
+ ProtectHostname=yes 
+ ProtectClock=yes 
+-ProtectKernelTunables=yes 
+ ProtectKernelModules=yes 
+ ProtectKernelLogs=yes 
+ ProtectControlGroups=yes 

commit f1d33c8ab2b1bd675b08a84094138b17f85a14cd

sys-apps/irqbalance: fix patch application
Note that the Meson build system was moved into the top-level in git
so this ugliness won't be needed in future.

Closes: https://bugs.gentoo.org/931190
Fixes: 42c7cfb8934774ff857d057195bfae649fad7b90
Signed-off-by: Sam James <sam@gentoo.org>

sys-apps/irqbalance/irqbalance-1.9.4-r2.ebuild
@@ -33,10 +33,6 @@ RDEPEND="
 	selinux? ( sec-policy/selinux-irqbalance )
 "
 
-PATCHES=(
-	"${FILESDIR}"/${P}-drop-protectkerneltunables.patch
-)
-
 pkg_setup() {
 	CONFIG_CHECK="~PCI_MSI"
 	linux-info_pkg_setup
@@ -44,6 +40,11 @@ pkg_setup() {
 
 src_prepare() {
 	default
+
+	(
+		cd "${WORKDIR}"/${P} || die
+		eapply "${FILESDIR}"/${P}-drop-protectkerneltunables.patch
+	)
 
 	# Follow systemd policies
 	# https://wiki.gentoo.org/wiki/Project:Systemd/Ebuild_policy