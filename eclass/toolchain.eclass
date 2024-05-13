gentoo/gentoo

commit 74414ea0c4d70c96bbec234df290d7e5f14d8f51

toolchain.eclass: To make "tc_has_feature ada" actually work
tc_has_feature ada is used to add BDEPEND on a ada compiler,.
Without setting TC_FEATURES the test does not work

Signed-off-by: Alfredo Tupone <tupone@gentoo.org>

eclass/toolchain.eclass
@@ -300,7 +300,7 @@ if [[ ${PN} != kgcc64 && ${PN} != gcc-* ]] ; then
 	IUSE+=" go"
 	IUSE+=" +sanitize"  TC_FEATURES+=( sanitize )
 	IUSE+=" graphite" TC_FEATURES+=( graphite )
-	IUSE+=" ada"
+	IUSE+=" ada" TC_FEATURES+=( ada )
 	IUSE+=" vtv"
 	IUSE+=" jit"
 	IUSE+=" +pie +ssp pch"

commit 7010b055a0051e6557cfd2ef644e432fe84dc388

toolchain.eclass: don't depend on self if gnat-gpl
Without this, we get:
```
 * Error: circular dependencies:

(sys-devel/gcc-13.2.1_p20240210:13/13::gentoo, ebuild scheduled for merge) depends on
 (dev-lang/gnat-gpl-2021-r5:10/10::gentoo, ebuild scheduled for merge) (buildtime)
  (sys-devel/gcc-13.2.1_p20240210:13/13::gentoo, ebuild scheduled for merge) (buildtime)
```

Reported on IRC by dormito.

Fixes: 74414ea0c4d70c96bbec234df290d7e5f14d8f51
Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -393,7 +393,7 @@ fi
 
 # TODO: Add a pkg_setup & pkg_pretend check for whether the active compiler
 # supports Ada.
-if tc_has_feature ada ; then
+if [[ ${PN} != gnat-gpl ]] && tc_has_feature ada ; then
 	BDEPEND+=" ada? ( || ( sys-devel/gcc[ada] dev-lang/gnat-gpl[ada] ) )"
 fi
 