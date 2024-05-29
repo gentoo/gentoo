gentoo/gentoo

commit d20e5788358b90edc2545386d0704981699f539c

sys-power/nut: Rename asciidoc-ready documentation sources to *.adoc
https://github.com/networkupstools/nut/commit/83f3d8b18143baea8da0584ab11f3a4f59f56738

Signed-off-by: INODE64 <web@inode64.com>
Closes: https://github.com/gentoo/gentoo/pull/32800
Signed-off-by: Joonas Niilola <juippis@gentoo.org>

sys-power/nut/nut-9999.ebuild
@@ -187,11 +187,11 @@ src_install() {
 		mv "${i}" "${i/.sample/}" || die
 	done
 
-	local DOCS=( AUTHORS MAINTAINERS NEWS README TODO UPGRADING )
+	local DOCS=( AUTHORS MAINTAINERS NEWS.adoc README.adoc TODO.adoc UPGRADING.adoc )
 	einstalldocs
 
 	if use doc; then
-		newdoc lib/README README.lib
+		newdoc lib/README.adoc
 		dodoc docs/*.txt
 		docinto cables
 		dodoc docs/cables/*

commit fde10960ff6053e42cd0b2e882f9e3aa6ba3715b

sys-power/nut: fix newdoc bug introduced in d20e5788358b90edc2545386d0704981699f539c
Closes: https://bugs.gentoo.org/show_bug.cgi?id=932868
Fixes: d20e5788358b90edc2545386d0704981699f539c
Signed-off-by: Robin H. Johnson <robbat2@gentoo.org>

sys-power/nut/nut-2.8.2.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2023 Gentoo Authors
+# Copyright 1999-2024 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -190,7 +190,7 @@ src_install() {
 	einstalldocs
 
 	if use doc; then
-		newdoc lib/README.adoc
+		newdoc lib/README.adoc README.lib.adoc
 		dodoc docs/*.txt
 		docinto cables
 		dodoc docs/cables/*

sys-power/nut/nut-9999.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2023 Gentoo Authors
+# Copyright 1999-2024 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -190,7 +190,7 @@ src_install() {
 	einstalldocs
 
 	if use doc; then
-		newdoc lib/README.adoc
+		newdoc lib/README.adoc README.lib.adoc
 		dodoc docs/*.txt
 		docinto cables
 		dodoc docs/cables/*