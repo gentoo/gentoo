gentoo/gentoo

commit 1bca88987927e0aa8a8b4f7a99c912b27d971a59

dev-texlive/texlive-context: set TEXLIVE_MODULE_OPTIONAL_ENGINE=luajittex
Closes: https://bugs.gentoo.org/928122
Signed-off-by: Florian Schmaus <flow@gentoo.org>

dev-texlive/texlive-context/metadata.xml
@@ -13,4 +13,7 @@
 		Hans Hagen's powerful ConTeXt system, http://pragma-ade.com.
 		Also includes third-party ConTeXt packages.
 	</longdescription>
+	<use>
+		<flag name="luajittex">Add support for LuaJitTeX: LuaTeX based on LuaJIT which is usually faster.</flag>
+	</use>
 </pkgmetadata>

dev-texlive/texlive-context/texlive-context-2023_p69108-r1.ebuild
@@ -53,6 +53,8 @@ TEXLIVE_MODULE_SRC_CONTENTS="
 	context-visualcounter.source.r47085
 "
 
+TEXLIVE_MODULE_OPTIONAL_ENGINE="luajittex"
+
 inherit texlive-module
 
 DESCRIPTION="TeXLive ConTeXt and packages"
@@ -69,7 +71,7 @@ RDEPEND="
 "
 DEPEND="
 	${COMMON_DEPEND}
-	>=app-text/texlive-core-2023[xetex]
+	>=app-text/texlive-core-2023[xetex,luajittex?]
 "
 
 src_prepare() {

commit fa6fadb1b7323f22a157b769d4d383ef439ee2f2

dev-texlive/texlive-{basic,context}: move luajittex from -context into -basic
The motivation for this change is a little bit complex. The gist is,
that luajittex belongs into -basic, even though tlpdb lists luajittex in
collection-context for Tex Live 2023 (and probably in
collection-binextra for TeX Live 2024).

In Gentoo, texlive-basic had already luajittex in IUSE, at least since
TeX Live 2021. And this seems sensible.

We can not easily put the luajittex TeX Live package in
app-text/texlive-core, even though we *build* luajit(hb)tex there. But
the luajittex TeX Live package also has a AddFormat directive in its
tlpobj, which we do not process in texlive-core. This is mostly because
texilve-core only inherits texlive-common.eclass, but the tlpojb
processing, including the AddFormat processing, is done by
texlive-module.eclass.

Therefore, we put luajittex in texlive-basic, since this will always be
installed and the texlive-basic ebuilds processes luajits AddFormat
directive, because it inherits texlive-module.eclass.

Closes: https://bugs.gentoo.org/933520
Bug: https://bugs.gentoo.org/928122
Fixes: 1bca88987927 ("dev-texlive/texlive-context: set TEXLIVE_MODULE_OPTIONAL_ENGINE=luajittex")
Signed-off-by: Florian Schmaus <flow@gentoo.org>

dev-texlive/texlive-basic/Manifest
@@ -34,6 +34,8 @@ DIST lua-alt-getopt.doc.r56414.tar.xz 3320 BLAKE2B 5c165fe265ef36b12f4e4552fcde5
 DIST lua-alt-getopt.r56414.tar.xz 2008 BLAKE2B 9fe768e1070b66d076b6083b0f8a5f4c6320b5081d2218584dc5acbaa3c1f5978cd75215866d68bc933b933dd07c6fd29b376c1676369bb2fee64395790b5eed SHA512 60ad4731ac61f9b5c4360bb3d0c6475e2abd358418e20bed29cc94761fdfb97fcf02829b9785e559ca6052a1e82ee7f0b104b166592b9fd87237679ac3caa2e6
 DIST luahbtex.doc.r66186.tar.xz 30240 BLAKE2B 9f34b53e124b27e90a0fb68dcdfb9cfd0c365941cf21bfc6d9a3ec3f623ca27d4435627c1a0eb5402527b8434afb5e72651f3fca3553fc1ea4242945fb563efe SHA512 3016c12de8386af715932819701a6da1b106c504a7d13ba9bb5a04999737709f474b50c08311c1d4c23b5532ba7c1546cb0585e5375babbcd2091adf52d05664
 DIST luahbtex.r66186.tar.xz 460 BLAKE2B 968bc635fb4bfe101aea1d13a32dd259b77e2e15a3a1d81b9e7ca81699b1d40d393085ae1031ca57e906d159409f441c748396a8f5ebdef0a30d9415a2a7b8cc SHA512 30b1fcd361cff27688c65ecffbcffc65053696200abfccd543a14253b53a35de2af53b37f7ed1580510a8c63293d2d002cf4d9fd2d44f86678e2ecb09f02e4b8
+DIST luajittex.doc.r66186.tar.xz 30360 BLAKE2B 9011b987bee46fa3fd857c4a7c122911e5e614857a74ba6fafc18a768bd99c5c87a6255438c5cf5841e4a2f0aeb53137244313326d3276408591d90259c9b078 SHA512 7637835fae934f4fb1aea954270281a986733d0e0592204346edc290f2cd7d5200ee2fa0d9e15a27be8221c3c990a8c3d4654e314f96441a65c197d3bd259129
+DIST luajittex.r66186.tar.xz 484 BLAKE2B 9832037e5fc311ffa73cf68b63b5c462d9619c3351d8c0801c83d949c163321886d3ce1c9cca8158f288d63d169015e734a92d7ab20d2365c6a2cc21e22dcfcc SHA512 21313a5786f2bea08ce55db3a7beedabaf66f3331bd0eac1f8f3d7b926f68e103b14b1a5beaa271c37b60fc56735cc180e424f91db62f6e740530a65495d8e82
 DIST luatex.doc.r69182.tar.xz 1956816 BLAKE2B 43dea0a99a1db57b98447a8ad58a7c4e2dcee975230d1ba54b5fa700bfd6ac9eb80dc1136e8a71266c35b350650aaef48aa73edd1b9557f9a7a7ef79f48ffaf7 SHA512 36a5dfb6b7ca09adad807d2fa74df854363107eb3fb514977e3ed8c69eeb1181a19a55a49eb8c001a3532601c0a222a3ae9d130827caa19864df03705af7271c
 DIST luatex.r69182.tar.xz 13432 BLAKE2B cebbf54237e3d908746ad7510675ab6b98576ebb61302edcaf360c4cbfd039c7b5f422adc123e21cd2656d975484922d0e0d577326a518f7c87beafd8f5eda77 SHA512 5ddb75ca2e358bea261363cc19d39a37a9e467f9cbf4f8006f036a96fea61cb00f86c4000c09d7dd07f71f9b9a01ec165ddefb862aa84f0bc3da6a387ca1f42b
 DIST metafont.doc.r66186.tar.xz 53096 BLAKE2B 46b3696de3af1c116894bea54cedd035904cd31dcfa7724a55e4bd37ccb3541cb9ed92d2f392743baafb94004a9b4a1c26d48d054b537e20149bc9d2d53da474 SHA512 48b1def32bfba5ab1123712fe41d69f979e33a77456801c21f030de8a39b37fcae179db44a517d7b194d7e6e6eb93d770e179ca49db9014cd433c86ef91b07d0

dev-texlive/texlive-basic/texlive-basic-2023_p59159-r4.ebuild
@@ -22,6 +22,7 @@ TEXLIVE_MODULE_CONTENTS="
 	knuth-local.r57963
 	lua-alt-getopt.r56414
 	luahbtex.r66186
+	luajittex.r66186
 	luatex.r69182
 	metafont.r66186
 	mflogo.r42428
@@ -50,6 +51,7 @@ TEXLIVE_MODULE_DOC_CONTENTS="
 	iftex.doc.r61910
 	lua-alt-getopt.doc.r56414
 	luahbtex.doc.r66186
+	luajittex.doc.r66186
 	luatex.doc.r69182
 	metafont.doc.r66186
 	mflogo.doc.r42428
@@ -95,3 +97,11 @@ TEXLIVE_MODULE_BINSCRIPTS="
 	texmf-dist/scripts/simpdftex/simpdftex
 	texmf-dist/scripts/tlshell/tlshell.tcl
 "
+
+src_prepare() {
+	default
+	if ! use luajittex; then
+		rm -rf texmf-dist/{,scripts,doc}/luajittex
+		rm tlpkg/tlpobj/luajittex.* || die
+	fi
+}

dev-texlive/texlive-context/Manifest
@@ -39,8 +39,6 @@ DIST context-visualcounter.source.r47085.tar.xz 6744 BLAKE2B f8b6822fe8a488888f8
 DIST context.doc.r69665.tar.xz 84867548 BLAKE2B bd7a8817a865c9d3bf7ccad0de4f13333760592a615791c58b7344edeae4bf3ae0cf1394097e81d290f3f4d4752599aaee0d5178f4c399dccc1edce52f47b1c3 SHA512 b2558db5417c5c1ca10dd80be258fca33d9e5fd1b16c3f1baa7c871bb4d65e42a75c95336ae988864e7c4dd350c722eaa96c446839ca6d147949b38fa78461d9
 DIST context.r69665.tar.xz 8275472 BLAKE2B 04d8bb1c999639276a5637df82ca074b1982816e7aac30d407b4d1623ed5678ad175724450a5e9dc3de3f95c142614761383c1c297a3b20cb8f50546153b516b SHA512 8951a79ae04a0bbcf7ec5f4d5f68e0f6ad3f79cfcce7379523b9bc74dc6ce3d0f07f628f3e182198ff37d313ca549b7b2512eeb28ce4a9c5460ec20ed7f2d2e7
 DIST jmn.r45751.tar.xz 40048 BLAKE2B e3f6d9bff7a3a5d438a688baf76c0bdd58797461c0020929031b07f47d49b59359d791ac237165284e48951075c4512619be880e2b3dc1bca761e4703a3050dc SHA512 2e250c3f115911c56f9b8d46d358fdef289c624a5b24c9b4213bbf7818bf42c7b778df55d4bf181bce115b388915cedc90ef7cfa99ee6ad8dd621e7853fc7c29
-DIST luajittex.doc.r66186.tar.xz 30360 BLAKE2B 9011b987bee46fa3fd857c4a7c122911e5e614857a74ba6fafc18a768bd99c5c87a6255438c5cf5841e4a2f0aeb53137244313326d3276408591d90259c9b078 SHA512 7637835fae934f4fb1aea954270281a986733d0e0592204346edc290f2cd7d5200ee2fa0d9e15a27be8221c3c990a8c3d4654e314f96441a65c197d3bd259129
-DIST luajittex.r66186.tar.xz 484 BLAKE2B 9832037e5fc311ffa73cf68b63b5c462d9619c3351d8c0801c83d949c163321886d3ce1c9cca8158f288d63d169015e734a92d7ab20d2365c6a2cc21e22dcfcc SHA512 21313a5786f2bea08ce55db3a7beedabaf66f3331bd0eac1f8f3d7b926f68e103b14b1a5beaa271c37b60fc56735cc180e424f91db62f6e740530a65495d8e82
 DIST tl-collection-context-2021.tar.xz 596 BLAKE2B 319e103a3568f2e4e6fd0ccbdae8b5ae97cf397fa97743bd2131a080c171c10fd20e3198676e6d419776a588241fe80ed122add0dd0352a188b5874d4e1bfdea SHA512 5bd74e1a434549cf31ce31777e9a32f90baa14148e6658633945508a46dbf6611644c4212b53812bb32a399e850517369e3d89bb0e495c89e6f2a979090ed765
 DIST tl-context-2021.tar.xz 8353772 BLAKE2B dfc11d25bff6a85cc780c047b5c6cf6b664dfc39063414910cc55ac6e4abee76dbb82ceb5cbadde8134cf5ab602fe513f564ab696d231166b4b8877e36def79c SHA512 3351a0da659954026a8ac4f25c246f82fda0f63ded2a8dd1dc463bc71b237977b4beffa3e44ee932f2e22e7fbc396ffee3c70ad5a85d153ba032ad46040b176a
 DIST tl-context-account-2021.tar.xz 6324 BLAKE2B 7a519311c1b35e1ead942a9198d60fa652b6cf1bfda03214546e45107f856825623a7ea213fb6ff82829016015163c3b1e73f9e112e3bca7bd77da1c2cd3e677 SHA512 755ddb4c62a496873d5362df01307163c79d2a6c3fabce6ea01b442c16dad2f23d72909df71dd44eb4fbbf5c57366e20eb49bfce240807c2e2a9a52cbd76680a

dev-texlive/texlive-context/metadata.xml
@@ -13,7 +13,4 @@
 		Hans Hagen's powerful ConTeXt system, http://pragma-ade.com.
 		Also includes third-party ConTeXt packages.
 	</longdescription>
-	<use>
-		<flag name="luajittex">Add support for LuaJitTeX: LuaTeX based on LuaJIT which is usually faster.</flag>
-	</use>
 </pkgmetadata>

dev-texlive/texlive-context/texlive-context-2023_p69108-r3.ebuild
@@ -25,7 +25,6 @@ TEXLIVE_MODULE_CONTENTS="
 	context-vim.r62071
 	context-visualcounter.r47085
 	jmn.r45751
-	luajittex.r66186
 "
 TEXLIVE_MODULE_DOC_CONTENTS="
 	context.doc.r69665
@@ -47,13 +46,10 @@ TEXLIVE_MODULE_DOC_CONTENTS="
 	context-typescripts.doc.r60422
 	context-vim.doc.r62071
 	context-visualcounter.doc.r47085
-	luajittex.doc.r66186
 "
 TEXLIVE_MODULE_SRC_CONTENTS="
 	context-visualcounter.source.r47085
 "
-
-TEXLIVE_MODULE_OPTIONAL_ENGINE="luajittex"
 
 inherit texlive-module
 
@@ -71,7 +67,7 @@ RDEPEND="
 "
 DEPEND="
 	${COMMON_DEPEND}
-	>=app-text/texlive-core-2023[xetex,luajittex?]
+	>=app-text/texlive-core-2023[xetex]
 "
 
 TEXLIVE_MODULE_BINSCRIPTS="