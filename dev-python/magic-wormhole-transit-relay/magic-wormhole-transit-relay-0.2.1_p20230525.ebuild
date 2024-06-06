gentoo/gentoo

commit 3bcba35091d347f92a741c3590daf75a548cdea9

dev-python/magic-wormhole-transit-relay: QA
  - add missing test dependency on dev-python/mock
  - as requested in bug #907117, do not use vcs-snapshot - even though
    this makes for much nicer paths when using commit snapshots...

Closes: https://bugs.gentoo.org/907200
Bug: https://bugs.gentoo.org/907117
Signed-off-by: Matthias Maier <tamiko@gentoo.org>

dev-python/magic-wormhole-transit-relay/magic-wormhole-transit-relay-0.2.1_p20230525.ebuild
@@ -10,7 +10,7 @@ DISTUTILS_USE_PEP517=setuptools
 # miscelleanous bufixes
 COMMIT_SHA1="db48e915311d1d10c748bb5299e2345c74e90a1b"
 
-inherit distutils-r1 vcs-snapshot
+inherit distutils-r1
 
 DESCRIPTION="Transit relay server for magic-wormhole"
 HOMEPAGE="https://magic-wormhole.readthedocs.io/en/latest/ https://pypi.org/project/magic-wormhole-transit-relay/"
@@ -19,10 +19,23 @@ SRC_URI="https://github.com/magic-wormhole/${PN}/archive/${COMMIT_SHA1}.tar.gz -
 LICENSE="MIT"
 SLOT="0"
 KEYWORDS="~amd64 ~x86"
-S="${WORKDIR}/${P}.gh"
+S="${WORKDIR}/magic-wormhole-transit-relay-${COMMIT_SHA1}"
 
 RDEPEND="
 	dev-python/autobahn[${PYTHON_USEDEP}]
 	dev-python/twisted[ssl,${PYTHON_USEDEP}]"
 
+BDEPEND="test? (
+	dev-python/mock[${PYTHON_USEDEP}]
+	)"
+
 distutils_enable_tests pytest
+
+python_test() {
+	# deselect test_buff_fill test because it exhibits intermittent hangs,
+	# bug #907200
+	local EPYTEST_DESELECT=(
+		src/wormhole_transit_relay/test/test_backpressure.py::TransitWebSockets::test_buffer_fill
+	)
+	epytest
+}

commit 20c4fe81777a548efb1e837fc04711760014f3aa

dev-python/magic-wormhole-transit-relay: drop unnecessary dependency on mock
Partially reverts commit 3bcba35091d347f92a741c3590daf75a548cdea9:

"dev-python/magic-wormhole-transit-relay: QA"

It was necessary before 2021, ironically the only version in ::gentoo
that *doesn't* depend on it.

Dropped upstream in:
https://github.com/magic-wormhole/magic-wormhole-transit-relay/commit/1efc1313dbef4873593b48d854e7b15d978496ce

Fixes: 3bcba35091d347f92a741c3590daf75a548cdea9
Signed-off-by: Eli Schwartz <eschwartz93@gmail.com>
Signed-off-by: Sam James <sam@gentoo.org>

dev-python/magic-wormhole-transit-relay/magic-wormhole-transit-relay-0.2.1_p20230525.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2023 Gentoo Authors
+# Copyright 1999-2024 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -24,10 +24,6 @@ S="${WORKDIR}/magic-wormhole-transit-relay-${COMMIT_SHA1}"
 RDEPEND="
 	dev-python/autobahn[${PYTHON_USEDEP}]
 	dev-python/twisted[ssl,${PYTHON_USEDEP}]"
-
-BDEPEND="test? (
-	dev-python/mock[${PYTHON_USEDEP}]
-	)"
 
 distutils_enable_tests pytest
 