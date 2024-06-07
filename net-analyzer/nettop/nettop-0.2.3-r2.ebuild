gentoo/gentoo

commit 7863251050a4b3c5640488810ac9b61735906a13

net-analyzer/nettop: fix HOMEPAGE and SRC_URI.
Gentoo-Bug: https://bugs.gentoo.org/353666
Closes: https://github.com/gentoo/gentoo/pull/1848

Signed-off-by: Patrice Clement <monsieurp@gentoo.org>

net-analyzer/nettop/nettop-0.2.3-r2.ebuild
@@ -7,8 +7,8 @@ EAPI=5
 inherit eutils toolchain-funcs
 
 DESCRIPTION="top like program for network activity"
-SRC_URI="http://srparish.net/scripts/${P}.tar.gz"
-HOMEPAGE="http://srparish.net/software/"
+SRC_URI="mirror://gentoo/${P}.tar.gz"
+HOMEPAGE="https://wiki.gentoo.org/wiki/No_homepage"
 
 SLOT="0"
 LICENSE="BSD"

commit 271f3f8969de5fa9ad8a466f7fab4f0ec27a0fba

net-analyzer/nettop: restore useful HOMEPAGE, SRC_URI
Previously, the website disappeared. Upstream is dead. As a result, the
distfile url was set to mirror:// and the homepage wiped away and turned
into "no homepage". But this is unconstructive, though, considering that
archive.org exists. So use that instead.

Fixes: 7863251050a4b3c5640488810ac9b61735906a13
Bug: https://bugs.gentoo.org/353666
Signed-off-by: Eli Schwartz <eschwartz93@gmail.com>
Signed-off-by: Joonas Niilola <juippis@gentoo.org>

net-analyzer/nettop/nettop-0.2.3-r2.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2022 Gentoo Authors
+# Copyright 1999-2024 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -6,8 +6,8 @@ EAPI=8
 inherit autotools toolchain-funcs
 
 DESCRIPTION="top-like program for network activity"
-HOMEPAGE="https://wiki.gentoo.org/wiki/No_homepage"
-SRC_URI="mirror://gentoo/${P}.tar.gz"
+HOMEPAGE="https://web.archive.org/web/20060615083852/http://srparish.net/software/"
+SRC_URI="https://web.archive.org/web/20060705095248if_/http://srparish.net:80/software/nettop-0.2.3.tar.gz"
 
 LICENSE="BSD"
 SLOT="0"