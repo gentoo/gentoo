gentoo/gentoo

commit ccaca6ed84371d1ad2cce872456dd0f116b31435

dev-util/clion: add 2024.1
Signed-off-by: Robert Greener <dev@greener.sh>
Closes: https://github.com/gentoo/gentoo/pull/36454
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

dev-util/clion/clion-2024.1.ebuild
@@ -1,0 +1,105 @@
+# Copyright 1999-2024 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit desktop wrapper
+
+DESCRIPTION="A complete toolset for C and C++ development"
+HOMEPAGE="https://www.jetbrains.com/clion/"
+SRC_URI="https://download.jetbrains.com/cpp/CLion-${PV}.tar.gz"
+
+LICENSE="|| ( IDEA IDEA_Academic IDEA_Classroom IDEA_OpenSource IDEA_Personal )
+	Apache-1.1 Apache-2.0 BSD BSD-2 CC0-1.0 CDDL-1.1 CPL-0.5 CPL-1.0
+	EPL-1.0 EPL-2.0 GPL-2 GPL-2-with-classpath-exception GPL-3 ISC JDOM
+	LGPL-2.1+ LGPL-3 MIT MPL-1.0 MPL-1.1 OFL public-domain PSF-2 UoI-NCSA ZLIB"
+SLOT="0"
+KEYWORDS="~amd64"
+RESTRICT="bindist mirror splitdebug"
+
+BDEPEND="dev-util/patchelf"
+
+RDEPEND="
+	>=app-accessibility/at-spi2-core-2.46.0:2
+	dev-debug/gdb
+	dev-libs/expat
+	dev-libs/glib:2
+	~dev-util/lttng-ust-2.12.1
+	dev-libs/nspr
+	dev-libs/nss
+	dev-libs/wayland
+	dev-build/cmake
+	app-alternatives/ninja
+	media-libs/alsa-lib
+	media-libs/freetype:2
+	media-libs/mesa
+	net-print/cups
+	sys-apps/dbus
+	sys-libs/zlib
+	x11-libs/cairo
+	x11-libs/libdrm
+	x11-libs/libX11
+	x11-libs/libXcomposite
+	x11-libs/libXcursor
+	x11-libs/libXdamage
+	x11-libs/libXext
+	x11-libs/libXfixes
+	x11-libs/libXi
+	x11-libs/libXrandr
+	x11-libs/libXrender
+	x11-libs/libXtst
+	x11-libs/libXxf86vm
+	x11-libs/libxcb
+	x11-libs/libxkbcommon
+	x11-libs/pango"
+
+QA_PREBUILT="opt/${PN}/*"
+
+src_prepare() {
+	default
+
+	local remove_me=(
+		Install-Linux-tar.txt
+		help/ReferenceCardForMac.pdf
+		bin/cmake
+		bin/gdb/linux
+		bin/lldb/linux
+		bin/ninja
+		lib/async-profiler/aarch64
+		plugins/clion-radler/DotFiles/linux-arm64
+		plugins/clion-radler/dotTrace.dotMemory/DotFiles/linux-arm64
+		plugins/remote-dev-server/selfcontained
+		plugins/python-ce/helpers/pydev/pydevd_attach_to_process/attach_linux_aarch64.so
+	)
+
+	rm -rv "${remove_me[@]}" || die
+
+	patchelf --set-rpath '$ORIGIN' "jbr/lib/libjcef.so" || die
+	patchelf --set-rpath '$ORIGIN' "jbr/lib/jcef_helper" || die
+	patchelf --set-rpath '$ORIGIN/../lib' "bin/clang/linux/x64/libclazyPlugin.so" || die
+	patchelf --set-rpath '$ORIGIN/../lib' "bin/clang/linux/x64/libclazyPlugin.so.19git" || die
+}
+
+src_install() {
+	local dir="/opt/${PN}"
+
+	insinto "${dir}"
+	doins -r *
+	fperms 755 "${dir}"/bin/{clion.sh,format.sh,fsnotifier,inspect.sh,jetbrains_client.sh,ltedit.sh,remote-dev-server.sh,repair,restarter,clang/linux/x64/{clangd,clang-tidy,clazy-standalone,llvm-symbolizer}}
+
+	if [[ -d jbr ]]; then
+		fperms 755 "${dir}"/jbr/bin/{java,javac,javadoc,jcmd,jdb,jfr,jhsdb,jinfo,jmap,jps,jrunscript,jstack,jstat,keytool,rmiregistry,serialver}
+		# Fix #763582
+		fperms 755 "${dir}"/jbr/lib/{chrome-sandbox,jcef_helper,jexec,jspawnhelper}
+	fi
+
+	dosym -r "${EPREFIX}/usr/bin/ninja" "${dir}"/bin/ninja/linux/x64/ninja
+
+	make_wrapper "${PN}" "${dir}/bin/${PN}.sh"
+	newicon "bin/${PN}.svg" "${PN}.svg"
+	make_desktop_entry "${PN}" "CLion" "${PN}" "Development;IDE;"
+
+	# recommended by: https://confluence.jetbrains.com/display/IDEADEV/Inotify+Watches+Limit
+	insinto /usr/lib/sysctl.d
+	newins - 30-"${PN}"-inotify-watches.conf <<<"fs.inotify.max_user_watches = 524288"
+}

dev-util/clion/Manifest
@@ -1,3 +1,4 @@
 DIST CLion-2022.3.2.tar.gz 847111855 BLAKE2B 7e1814bcc683126f51eefd350b3d171a22d320ec658bc24b8831db1198756c59ee1be951665d10f2c3e76831f0d8ab55c0ae5fd9abeae0acf4e7009ce00f1faa SHA512 7ca87933323c0e1a4800eb75fbe40d179af987f33de51bca0e6b35264aaffea991712187719b837e07bd11457f8891f9f6038fb10f4e3157170d7818179b7531
 DIST CLion-2023.1.4.tar.gz 858589459 BLAKE2B aed16b6cd6f88279f772fd38f63ff27019998ed1f7e63c1fcaba16fd1b43277d112806dd8ba4ff5e08a23fb5d80b0b80e24b7ef693e19a38220a7129c71d2d98 SHA512 b50ebcdb0b85ec00a9caaad6a309c8f37524a62115a8efeaa8b05819849bdda2d81bf2fae4a723d7d378fb32910a45af26aa5303006aff2e1ad96d5a4749ba90
 DIST CLion-2023.3.4.tar.gz 907669083 BLAKE2B ed0571341141e58b102cffbf3b96c7e6625f628b6092d93cc80c4d9ed5f47f25be3665ff26553e6c6584e41a519e102eceabe576986c44795a5257312dad1b76 SHA512 f5ab02ed8a3f854dc75483e7766267f043c7a493730900786a3a304e3f2fddaf12375eb6760e9438e8184b7a5405131a2e3ea7536be8b7a7ce0509b0901afc09
+DIST CLion-2024.1.tar.gz 1213246002 BLAKE2B b0060b98dc803e72513fb927c0d8b2762b9d0526d4708f75e3ed1b68642a2f01995b468a7df40b403eee4d619db9afe4430259a092506e139983ecee0629460b SHA512 9335b3811414919df8b251e25cf4a4155675c79c35de3a0ff21a30b06b46e130b0684fd2c6dcc17b4c8ee676211c6df9c84d6b6777bf7d88e1b2ec765c35f69b

commit a585318f602fbaf7d7850602a61c98086e977121

dev-util/clion: Fix LICENSE (again!), OFL-1.1 instead of OFL
Fixes: ccaca6ed84371d1ad2cce872456dd0f116b31435
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

dev-util/clion/clion-2024.1.ebuild
@@ -12,7 +12,8 @@ SRC_URI="https://download.jetbrains.com/cpp/CLion-${PV}.tar.gz"
 LICENSE="|| ( IDEA IDEA_Academic IDEA_Classroom IDEA_OpenSource IDEA_Personal )
 	Apache-1.1 Apache-2.0 BSD BSD-2 CC0-1.0 CDDL-1.1 CPL-0.5 CPL-1.0
 	EPL-1.0 EPL-2.0 GPL-2 GPL-2-with-classpath-exception GPL-3 ISC JDOM
-	LGPL-2.1+ LGPL-3 MIT MPL-1.0 MPL-1.1 OFL public-domain PSF-2 UoI-NCSA ZLIB"
+	LGPL-2.1+ LGPL-3 MIT MPL-1.0 MPL-1.1 OFL-1.1 public-domain PSF-2
+	UoI-NCSA ZLIB"
 SLOT="0"
 KEYWORDS="~amd64"
 RESTRICT="bindist mirror splitdebug"