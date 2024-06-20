gentoo/gentoo

commit 341b9bf36b212a4de56cc82c490e3aa4ddcbd480

app-crypt/loop-aes-losetup: optimize src_unpack
As just done in util-linux & btrfs-progs.

Signed-off-by: Sam James <sam@gentoo.org>

app-crypt/loop-aes-losetup/loop-aes-losetup-2.40.1.ebuild
@@ -51,24 +51,17 @@ PATCHES=(
 )
 
 src_unpack() {
-	if use verify-sig ; then
-		mkdir "${T}"/verify-sig || die
-		pushd "${T}"/verify-sig &>/dev/null || die
-
-		# Upstream sign the decompressed .tar
-		# Let's do it separately in ${T} then cleanup to avoid external
-		# effects on normal unpack.
-		cp "${DISTDIR}"/${MY_P}.tar.xz . || die
-		xz -d ${MY_P}.tar.xz || die
-		verify-sig_verify_detached ${MY_P}.tar "${DISTDIR}"/${MY_P}.tar.sign "${BROOT}"/usr/share/openpgp-keys/karelzak.asc
-
-		popd &>/dev/null || die
-		rm -r "${T}"/verify-sig || die
+	# Upstream sign the decompressed .tar
+	if use verify-sig; then
+		einfo "Unpacking ${MY_P}.tar.xz ..."
+		verify-sig_verify_detached - "${DISTDIR}"/${MY_P}.tar.sign \
+			< <(xz -cd "${DISTDIR}"/${MY_P}.tar.xz | tee >(tar -x)) "${BROOT}"/usr/share/openpgp-keys/karelzak.asc
+		assert "Unpack failed"
 
 		verify-sig_verify_detached "${DISTDIR}"/${LOOPAES_P}.tar.bz2{,.sign}
+	else
+		default
 	fi
-
-	default
 }
 
 src_prepare() {

commit 784c18a35d781b60e90f873ce5543301c523a32a

app-crypt/loop-aes-losetup: fix unpacking patch
Closes: https://bugs.gentoo.org/934611
Fixes: 341b9bf36b212a4de56cc82c490e3aa4ddcbd480
Signed-off-by: Sam James <sam@gentoo.org>

app-crypt/loop-aes-losetup/loop-aes-losetup-2.40.1.ebuild
@@ -59,6 +59,7 @@ src_unpack() {
 		assert "Unpack failed"
 
 		verify-sig_verify_detached "${DISTDIR}"/${LOOPAES_P}.tar.bz2{,.sign}
+		unpack ${LOOPAES_P}.tar.bz2
 	else
 		default
 	fi