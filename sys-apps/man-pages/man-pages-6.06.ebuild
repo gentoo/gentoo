gentoo/gentoo

commit 7759152a7abd008031b16c6057cddeae66d4fffd

sys-apps/man-pages: optimize src_unpack
As just done in util-linux & btrfs-progs.

Signed-off-by: Sam James <sam@gentoo.org>

sys-apps/man-pages/man-pages-6.06.ebuild
@@ -95,21 +95,14 @@ src_unpack() {
 	fi
 
 	if [[ ${PV} != *_rc* ]] && ! [[ ${MAN_PAGES_GENTOO_DIST} -eq 1 ]] && use verify-sig ; then
-		mkdir "${T}"/verify-sig || die
-		pushd "${T}"/verify-sig &>/dev/null || die
-
 		# Upstream sign the decompressed .tar
-		# Let's do it separately in ${T} then cleanup to avoid external
-		# effects on normal unpack.
-		cp "${DISTDIR}"/${P}.tar.xz . || die
-		xz -d ${P}.tar.xz || die
-		verify-sig_verify_detached ${P}.tar "${DISTDIR}"/${P}.tar.sign
-
-		popd &>/dev/null || die
-		rm -r "${T}"/verify-sig || die
+		einfo "Unpacking ${P}.tar.xz ..."
+		verify-sig_verify_detached - "${DISTDIR}"/${P}.tar.sign \
+			< <(xz -cd "${DISTDIR}"/${P}.tar.xz | tee >(tar -x))
+		assert "Unpack failed"
+	else
+		default
 	fi
-
-	default
 }
 
 src_prepare() {

sys-apps/man-pages/man-pages-9999.ebuild
@@ -95,21 +95,14 @@ src_unpack() {
 	fi
 
 	if [[ ${PV} != *_rc* ]] && ! [[ ${MAN_PAGES_GENTOO_DIST} -eq 1 ]] && use verify-sig ; then
-		mkdir "${T}"/verify-sig || die
-		pushd "${T}"/verify-sig &>/dev/null || die
-
 		# Upstream sign the decompressed .tar
-		# Let's do it separately in ${T} then cleanup to avoid external
-		# effects on normal unpack.
-		cp "${DISTDIR}"/${P}.tar.xz . || die
-		xz -d ${P}.tar.xz || die
-		verify-sig_verify_detached ${P}.tar "${DISTDIR}"/${P}.tar.sign
-
-		popd &>/dev/null || die
-		rm -r "${T}"/verify-sig || die
+		einfo "Unpacking ${P}.tar.xz ..."
+		verify-sig_verify_detached - "${DISTDIR}"/${P}.tar.sign \
+			< <(xz -cd "${DISTDIR}"/${P}.tar.xz | tee >(tar -x))
+		assert "Unpack failed"
+	else
+		default
 	fi
-
-	default
 }
 
 src_prepare() {

commit a5c2c85b0d87270f0087a22d0eb61da3442413bd

sys-apps/man-pages: fix src_prepare w/ USE=verify-sig
Closes: https://bugs.gentoo.org/934625
Fixes: 7759152a7abd008031b16c6057cddeae66d4fffd
Signed-off-by: Sam James <sam@gentoo.org>

sys-apps/man-pages/man-pages-6.06.ebuild
@@ -100,6 +100,8 @@ src_unpack() {
 		verify-sig_verify_detached - "${DISTDIR}"/${P}.tar.sign \
 			< <(xz -cd "${DISTDIR}"/${P}.tar.xz | tee >(tar -x))
 		assert "Unpack failed"
+
+		unpack man-pages-gentoo-${GENTOO_PATCH}.tar.bz2
 	else
 		default
 	fi

sys-apps/man-pages/man-pages-9999.ebuild
@@ -100,6 +100,8 @@ src_unpack() {
 		verify-sig_verify_detached - "${DISTDIR}"/${P}.tar.sign \
 			< <(xz -cd "${DISTDIR}"/${P}.tar.xz | tee >(tar -x))
 		assert "Unpack failed"
+
+		unpack man-pages-gentoo-${GENTOO_PATCH}.tar.bz2
 	else
 		default
 	fi