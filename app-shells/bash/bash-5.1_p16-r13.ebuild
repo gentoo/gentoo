gentoo/gentoo

commit 9ff9f8199ccf9d55fb1cf066cbbc791101894ab0

app-shells/bash: do not update internal window title of screen and tmux
The k escape sequence changes the internal window title of screen and
tmux, which is an alias given to the window by the user. The alias can
be used to reference the window in internal commands, e.g. by using
"select <alias>" in screen or "select-window -t <alias>" in tmux to
switch to a window by name, and should not be updated by commands
running inside of screen and tmux.

Closes: https://bugs.gentoo.org/934732
Signed-off-by: Sven Wegener <swegener@gentoo.org>

app-shells/bash/files/bashrc.d/10-gentoo-title.bash
@@ -16,8 +16,8 @@ genfun_set_win_title() {
 
 	# Sets the window title with the Set Text Parameters sequence. For
 	# screen, the sequence defines the hardstatus (%h) and for tmux, the
-	# window_name (#W). For graphical terminal emulators, it is normal for
-	# the title bar be affected.
+	# pane_title (#T). For graphical terminal emulators, it is normal for
+	# the title bar to be affected.
 	genfun_set_win_title() {
 		genfun_sanitise_cwd
 		printf '\033]2;%s@%s - %s\007' "${USER}" "${HOSTNAME%%.*}" "${_cwd}"
@@ -26,27 +26,13 @@ genfun_set_win_title() {
 	genfun_set_win_title
 }
 
-# Set window title with the Title Definition String sequence. For screen, the
-# sequence defines the window title (%t) and for tmux, the pane_title (#T).
-# For tmux to be affected requires that its allow-rename option be enabled.
-# https://www.gnu.org/software/screen/manual/html_node/Control-Sequences.html
-case ${TERM} in
-	screen*|tmux*)
-		genfun_set_pane_title() {
-			printf '\033k%s\033\\' "${HOSTNAME%%.*}"
-		}
-		PROMPT_COMMAND+=('genfun_set_pane_title')
-		;;
-	*)
-		# If the TTY is that of sshd(8) then proceed no further. Alas,
-		# there exist many operating environments in which the window
-		# title would otherwise not be restored upon ssh(1) exiting.
-		# Users wishing to coerce the historical behaviour have the
-		# option of setting PROMPT_COMMAND=(genfun_set_win_title).
-		if [[ ${SSH_TTY} && ${SSH_TTY} == "$(tty)" ]]; then
-			return
-		fi
-esac
+# If the TTY is that of sshd(8) then proceed no further. Alas, there exist many
+# operating environments in which the window title would otherwise not be
+# restored upon ssh(1) exiting. Users wishing to coerce the historical
+# behaviour have the option of setting PROMPT_COMMAND=(genfun_set_win_title).
+if [[ ${SSH_TTY} && ${SSH_TTY} == "$(tty)" ]]; then
+	return
+fi
 
 # Determine whether the terminal can handle the Set Text Parameters sequence.
 # The only terminals permitted here are those for which there is empirical

commit 1d15b220376e9da1251127d71f78203f5da3f9d7

app-shells/bash: fix title setting heuristic; set both icon name and title
In bug #934732, Sven Wegener requested for bash to refrain from setting
the "internal" window title. That is, the window title (%t) of GNU
screen and the window_name (#W) of tmux. A patch was attached to the
bug, which I acknowledged and which was subsequently applied by Sven.
Unfortunately - and perhaps owing to my eagerness to have the matter be
resolved - I did not notice that the patch introduces a regression. The
regression is that both screen and tmux are no longer exempted from the
sshd(8) check, resulting in neither the hardstatus (%h) being set for
the former, nor the pane_title (#T) for the latter. This commit restores
the intended behaviour.

In comment #10 of bug #550104, Heui-mun Park makes a compelling case
that the "\033]0;" variant of the Set Text Parameters sequence should be
preferred over other variants. The difference is that it sets the "icon
name" in addition to the "window title". Indeed, I have found that most
applications that employ the sequence make use of this more
comprehensive variant. For many users, such a distinction would amount
to being little more than an artifact of history. Nevertheless, it makes
perfect sense for 10-gentoo-title.bash to be aligned with the practices
of others in this regard. Make it so.

Improve the commentary in 10-gentoo-title.bash, along with the accuracy
and utility of the information conveyed by the ewarn comands in the
ebuilds.

Have the live ebuild (version 9999) install 15-gentoo-bashrc-check.bash,
just as the other recent ebuilds do. It's simpler from a maintenance
standpoint for there to be as few differences between them as is
possible.

Fixes: 9ff9f8199ccf9d55fb1cf066cbbc791101894ab0
Signed-off-by: Kerin Millar <kfm@plushkava.net>
Signed-off-by: Sam James <sam@gentoo.org>

app-shells/bash/bash-5.1_p16-r13.ebuild
@@ -15,7 +15,7 @@ MY_PV=${MY_PV/_/-}
 MY_P=${PN}-${MY_PV}
 MY_PATCHES=()
 
-# Determine the patchlevel. See ftp://ftp.gnu.org/gnu/bash/bash-5.3-patches/.
+# Determine the patchlevel. See ftp://ftp.gnu.org/gnu/bash/bash-5.1-patches/.
 case ${PV} in
 	*_p*)
 		PLEVEL=${PV##*_p}
@@ -30,7 +30,7 @@ esac
 
 # The version of readline this bash normally ships with. Note that we only use
 # the bundled copy of readline for pre-releases.
-READLINE_VER="8.3_alpha"
+READLINE_VER="8.1"
 
 DESCRIPTION="The standard GNU Bourne again shell"
 HOMEPAGE="https://tiswww.case.edu/php/chet/bash/bashtop.html https://git.savannah.gnu.org/cgit/bash.git"
@@ -65,9 +65,9 @@ S=${WORKDIR}/${MY_P}
 LICENSE="GPL-3+"
 SLOT="0"
 if (( PLEVEL >= 0 )); then
-	KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~m68k ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86 ~amd64-linux ~x86-linux ~arm64-macos ~ppc-macos ~x64-macos ~x64-solaris"
+	KEYWORDS="~alpha amd64 arm arm64 hppa ~ia64 ~loong ~m68k ~mips ppc ppc64 ~riscv ~s390 sparc x86 ~amd64-linux ~x86-linux ~arm64-macos ~ppc-macos ~x64-macos ~x64-solaris"
 fi
-IUSE="afs bashlogger examples mem-scramble +net nls plugins pgo +readline"
+IUSE="afs bashlogger examples mem-scramble +net nls plugins +readline"
 
 DEPEND="
 	>=sys-libs/ncurses-5.2-r2:=
@@ -81,18 +81,25 @@ RDEPEND="
 "
 # We only need bison (yacc) when the .y files get patched (bash42-005, bash51-011).
 BDEPEND="
-	pgo? ( dev-util/gperf )
+	sys-devel/bison
 	verify-sig? ( sec-keys/openpgp-keys-chetramey )
 "
 
 # EAPI 8 tries to append it but it doesn't exist here.
 QA_CONFIGURE_OPTIONS="--disable-static"
 
+QA_CONFIG_IMPL_DECL_SKIP+=(
+	# this is fixed in autoconf 2.71, used in bash 5.2. The check fails
+	# regardless of GCC version. bug #916480
+	makedev
+)
+
 PATCHES=(
 	#"${WORKDIR}"/${PN}-${GENTOO_PATCH_VER}/
 
 	# Patches to or from Chet, posted to the bug-bash mailing list.
 	"${FILESDIR}/${PN}-5.0-syslog-history-extern.patch"
+	"${FILESDIR}/${PN}-5.1_p16-configure-clang16.patch"
 )
 
 pkg_setup() {
@@ -152,9 +159,6 @@ src_prepare() {
 	&& touch -r . doc/* \
 	|| die
 
-	# Sometimes hangs (more noticeable w/ pgo), bug #907403.
-	rm tests/run-jobs || die
-
 	eapply -p0 "${PATCHES[@]}"
 	eapply_user
 }
@@ -168,10 +172,6 @@ src_configure() {
 	# configure warns on use of non-Bison but doesn't abort. The result
 	# may misbehave at runtime.
 	unset -v YACC
-
-	# wcsnwidth(), substring() issues with -Wlto-type-mismatch, reported
-	# upstream to Chet by email.
-	filter-lto
 
 	myconf=(
 		--disable-profiling
@@ -236,54 +236,11 @@ src_configure() {
 }
 
 src_compile() {
-	local -a pgo_generate_flags pgo_use_flags
-	local flag
+	emake
 
-	# -fprofile-partial-training because upstream notes the test suite isn't
-	# super comprehensive.
-	# https://documentation.suse.com/sbp/all/html/SBP-GCC-10/index.html#sec-gcc10-pgo
-	if use pgo; then
-		pgo_generate_flags=(
-			-fprofile-update=atomic
-			-fprofile-dir="${T}"/pgo
-			-fprofile-generate="${T}"/pgo
-		)
-		pgo_use_flags=(
-			-fprofile-use="${T}"/pgo
-			-fprofile-dir="${T}"/pgo
-		)
-		if flag=$(test-flags-CC -fprofile-partial-training); then
-			pgo_generate_flags+=( "${flag}" )
-			pgo_use_flags+=( "${flag}" )
-		fi
+	if use plugins; then
+		emake -C examples/loadables all others
 	fi
-
-	emake CFLAGS="${CFLAGS} ${pgo_generate_flags[*]}"
-	use plugins && emake -C examples/loadables CFLAGS="${CFLAGS} ${pgo_generate_flags[*]}" all others
-
-	# Build Bash and run its tests to generate profiles.
-	if (( ${#pgo_generate_flags[@]} )); then
-		# Used in test suite.
-		unset -v A
-
-		emake CFLAGS="${CFLAGS} ${pgo_generate_flags[*]}" -k check
-
-		if tc-is-clang; then
-			llvm-profdata merge "${T}"/pgo --output="${T}"/pgo/default.profdata || die
-		fi
-
-		# Rebuild Bash using the profiling data we just generated.
-		emake clean
-		emake CFLAGS="${CFLAGS} ${pgo_use_flags[*]}"
-		use plugins && emake -C examples/loadables CFLAGS="${CFLAGS} ${pgo_use_flags[*]}" all others
-	fi
-}
-
-src_test() {
-	# Used in test suite.
-	unset -v A
-
-	default
 }
 
 src_install() {
@@ -371,22 +328,31 @@ pkg_postinst() {
 	read -r old_ver <<<"${REPLACING_VERSIONS}"
 	if [[ ! $old_ver ]]; then
 		:
-	elif ver_test "$old_ver" -ge "5.2" && ver_test "$old_ver" -ge "5.2_p26-r1"; then
+	elif ver_test "$old_ver" -ge "5.2" && ver_test "$old_ver" -ge "5.2_p26-r6"; then
 		return
-	elif ver_test "$old_ver" -lt "5.2" && ver_test "$old_ver" -ge "5.1_p16-r8"; then
+	elif ver_test "$old_ver" -lt "5.2" && ver_test "$old_ver" -ge "5.1_p16-r13"; then
 		return
 	fi
 
-	ewarn "Files situated under /etc/bash/bashrc.d must now have a suffix of .sh or .bash."
-	ewarn ""
-	ewarn "Gentoo now defaults to defining PROMPT_COMMAND as an array. Depending on the"
-	ewarn "characteristics of the operating environment, this array may contain commands"
-	ewarn "to set the window and pane title. Users that choose to customise this variable"
-	ewarn "in ~/.bashrc are advised to append their commands, using the following syntax."
-	ewarn ""
-	ewarn "PROMPT_COMMAND+=('custom command goes here')"
-	ewarn ""
-	ewarn "Alternatively, users that wish to opt out of Gentoo's window title setting"
-	ewarn "behaviour may now do so by either unsetting PROMPT_COMMAND or by re-defining it"
-	ewarn "as desired. Previously, there was no formally supported method of opting out."
+	while read -r; do ewarn "${REPLY}"; done <<'EOF'
+Files situated under /etc/bash/bashrc.d must now have a suffix of .sh or .bash.
+
+Gentoo now defaults to defining PROMPT_COMMAND as an array. Depending on the
+characteristics of the operating environment, this array may contain a command
+to set the terminal's window title. Those already choosing to customise the
+PROMPT_COMMAND variable are now advised to append their commands like so:
+
+PROMPT_COMMAND+=('custom command goes here')
+
+Gentoo no longer defaults to having bash manipulate the window title in the case
+that the terminal is controlled by sshd(8), unless screen or tmux are in use.
+Those wanting to set the title unconditionally may adjust ~/.bashrc - or create
+a custom /etc/bash/bashrc.d drop-in - to set PROMPT_COMMMAND like so:
+
+PROMPT_COMMAND=(genfun_set_win_title)
+
+Those who would prefer for bash never to interfere with the window title may
+now opt out of the default title setting behaviour, either with the "unset -v
+PROMPT_COMMAND" command or by re-defining PROMPT_COMMAND as desired.
+EOF
 }

app-shells/bash/bash-5.2_p26-r6.ebuild
@@ -15,7 +15,7 @@ MY_PV=${MY_PV/_/-}
 MY_P=${PN}-${MY_PV}
 MY_PATCHES=()
 
-# Determine the patchlevel. See ftp://ftp.gnu.org/gnu/bash/bash-5.1-patches/.
+# Determine the patchlevel. See ftp://ftp.gnu.org/gnu/bash/bash-5.2-patches/.
 case ${PV} in
 	*_p*)
 		PLEVEL=${PV##*_p}
@@ -30,7 +30,7 @@ esac
 
 # The version of readline this bash normally ships with. Note that we only use
 # the bundled copy of readline for pre-releases.
-READLINE_VER="8.1"
+READLINE_VER="8.2_p1"
 
 DESCRIPTION="The standard GNU Bourne again shell"
 HOMEPAGE="https://tiswww.case.edu/php/chet/bash/bashtop.html https://git.savannah.gnu.org/cgit/bash.git"
@@ -65,9 +65,9 @@ S=${WORKDIR}/${MY_P}
 LICENSE="GPL-3+"
 SLOT="0"
 if (( PLEVEL >= 0 )); then
-	KEYWORDS="~alpha amd64 arm arm64 hppa ~ia64 ~loong ~m68k ~mips ppc ppc64 ~riscv ~s390 sparc x86 ~amd64-linux ~x86-linux ~arm64-macos ~ppc-macos ~x64-macos ~x64-solaris"
+	KEYWORDS="~alpha amd64 arm arm64 ~hppa ~ia64 ~loong ~m68k ~mips ppc ppc64 ~riscv ~s390 sparc x86 ~amd64-linux ~x86-linux ~arm64-macos ~ppc-macos ~x64-macos ~x64-solaris"
 fi
-IUSE="afs bashlogger examples mem-scramble +net nls plugins +readline"
+IUSE="afs bashlogger examples mem-scramble +net nls plugins pgo +readline"
 
 DEPEND="
 	>=sys-libs/ncurses-5.2-r2:=
@@ -81,25 +81,23 @@ RDEPEND="
 "
 # We only need bison (yacc) when the .y files get patched (bash42-005, bash51-011).
 BDEPEND="
-	sys-devel/bison
+	pgo? ( dev-util/gperf )
 	verify-sig? ( sec-keys/openpgp-keys-chetramey )
 "
 
 # EAPI 8 tries to append it but it doesn't exist here.
 QA_CONFIGURE_OPTIONS="--disable-static"
 
-QA_CONFIG_IMPL_DECL_SKIP+=(
-	# this is fixed in autoconf 2.71, used in bash 5.2. The check fails
-	# regardless of GCC version. bug #916480
-	makedev
-)
-
 PATCHES=(
 	#"${WORKDIR}"/${PN}-${GENTOO_PATCH_VER}/
 
 	# Patches to or from Chet, posted to the bug-bash mailing list.
 	"${FILESDIR}/${PN}-5.0-syslog-history-extern.patch"
-	"${FILESDIR}/${PN}-5.1_p16-configure-clang16.patch"
+	"${FILESDIR}/${PN}-5.2_p15-random-ub.patch"
+	"${FILESDIR}/${PN}-5.2_p15-configure-clang16.patch"
+	"${FILESDIR}/${PN}-5.2_p21-wpointer-to-int.patch"
+	"${FILESDIR}/${PN}-5.2_p21-configure-strtold.patch"
+	"${FILESDIR}/${PN}-5.2_p26-memory-leaks.patch"
 )
 
 pkg_setup() {
@@ -158,6 +156,9 @@ src_prepare() {
 	sed -i -E '/^(HS|RL)USER/s:=.*:=:' doc/Makefile.in \
 	&& touch -r . doc/* \
 	|| die
+
+	# Sometimes hangs (more noticeable w/ pgo), bug #907403.
+	rm tests/run-jobs || die
 
 	eapply -p0 "${PATCHES[@]}"
 	eapply_user
@@ -236,11 +237,54 @@ src_configure() {
 }
 
 src_compile() {
-	emake
+	local -a pgo_generate_flags pgo_use_flags
+	local flag
 
-	if use plugins; then
-		emake -C examples/loadables all others
+	# -fprofile-partial-training because upstream notes the test suite isn't
+	# super comprehensive.
+	# https://documentation.suse.com/sbp/all/html/SBP-GCC-10/index.html#sec-gcc10-pgo
+	if use pgo; then
+		pgo_generate_flags=(
+			-fprofile-update=atomic
+			-fprofile-dir="${T}"/pgo
+			-fprofile-generate="${T}"/pgo
+		)
+		pgo_use_flags=(
+			-fprofile-use="${T}"/pgo
+			-fprofile-dir="${T}"/pgo
+		)
+		if flag=$(test-flags-CC -fprofile-partial-training); then
+			pgo_generate_flags+=( "${flag}" )
+			pgo_use_flags+=( "${flag}" )
+		fi
 	fi
+
+	emake CFLAGS="${CFLAGS} ${pgo_generate_flags[*]}"
+	use plugins && emake -C examples/loadables CFLAGS="${CFLAGS} ${pgo_generate_flags[*]}" all others
+
+	# Build Bash and run its tests to generate profiles.
+	if (( ${#pgo_generate_flags[@]} )); then
+		# Used in test suite.
+		unset -v A
+
+		emake CFLAGS="${CFLAGS} ${pgo_generate_flags[*]}" -k check
+
+		if tc-is-clang; then
+			llvm-profdata merge "${T}"/pgo --output="${T}"/pgo/default.profdata || die
+		fi
+
+		# Rebuild Bash using the profiling data we just generated.
+		emake clean
+		emake CFLAGS="${CFLAGS} ${pgo_use_flags[*]}"
+		use plugins && emake -C examples/loadables CFLAGS="${CFLAGS} ${pgo_use_flags[*]}" all others
+	fi
+}
+
+src_test() {
+	# Used in test suite.
+	unset -v A
+
+	default
 }
 
 src_install() {
@@ -328,22 +372,31 @@ pkg_postinst() {
 	read -r old_ver <<<"${REPLACING_VERSIONS}"
 	if [[ ! $old_ver ]]; then
 		:
-	elif ver_test "$old_ver" -ge "5.2" && ver_test "$old_ver" -ge "5.2_p26-r1"; then
+	elif ver_test "$old_ver" -ge "5.2" && ver_test "$old_ver" -ge "5.2_p26-r6"; then
 		return
-	elif ver_test "$old_ver" -lt "5.2" && ver_test "$old_ver" -ge "5.1_p16-r8"; then
+	elif ver_test "$old_ver" -lt "5.2" && ver_test "$old_ver" -ge "5.1_p16-r13"; then
 		return
 	fi
 
-	ewarn "Files situated under /etc/bash/bashrc.d must now have a suffix of .sh or .bash."
-	ewarn ""
-	ewarn "Gentoo now defaults to defining PROMPT_COMMAND as an array. Depending on the"
-	ewarn "characteristics of the operating environment, this array may contain commands"
-	ewarn "to set the window and pane title. Users that choose to customise this variable"
-	ewarn "in ~/.bashrc are advised to append their commands, using the following syntax."
-	ewarn ""
-	ewarn "PROMPT_COMMAND+=('custom command goes here')"
-	ewarn ""
-	ewarn "Alternatively, users that wish to opt out of Gentoo's window title setting"
-	ewarn "behaviour may now do so by either unsetting PROMPT_COMMAND or by re-defining it"
-	ewarn "as desired. Previously, there was no formally supported method of opting out."
+	while read -r; do ewarn "${REPLY}"; done <<'EOF'
+Files situated under /etc/bash/bashrc.d must now have a suffix of .sh or .bash.
+
+Gentoo now defaults to defining PROMPT_COMMAND as an array. Depending on the
+characteristics of the operating environment, this array may contain a command
+to set the terminal's window title. Those already choosing to customise the
+PROMPT_COMMAND variable are now advised to append their commands like so:
+
+PROMPT_COMMAND+=('custom command goes here')
+
+Gentoo no longer defaults to having bash manipulate the window title in the case
+that the terminal is controlled by sshd(8), unless screen or tmux are in use.
+Those wanting to set the title unconditionally may adjust ~/.bashrc - or create
+a custom /etc/bash/bashrc.d drop-in - to set PROMPT_COMMMAND like so:
+
+PROMPT_COMMAND=(genfun_set_win_title)
+
+Those who would prefer for bash never to interfere with the window title may
+now opt out of the default title setting behaviour, either with the "unset -v
+PROMPT_COMMAND" command or by re-defining PROMPT_COMMAND as desired.
+EOF
 }

app-shells/bash/bash-5.3_alpha-r5.ebuild
@@ -15,7 +15,7 @@ MY_PV=${MY_PV/_/-}
 MY_P=${PN}-${MY_PV}
 MY_PATCHES=()
 
-# Determine the patchlevel. See ftp://ftp.gnu.org/gnu/bash/bash-5.2-patches/.
+# Determine the patchlevel. See ftp://ftp.gnu.org/gnu/bash/bash-5.3-patches/.
 case ${PV} in
 	*_p*)
 		PLEVEL=${PV##*_p}
@@ -30,7 +30,7 @@ esac
 
 # The version of readline this bash normally ships with. Note that we only use
 # the bundled copy of readline for pre-releases.
-READLINE_VER="8.2_p1"
+READLINE_VER="8.3_alpha"
 
 DESCRIPTION="The standard GNU Bourne again shell"
 HOMEPAGE="https://tiswww.case.edu/php/chet/bash/bashtop.html https://git.savannah.gnu.org/cgit/bash.git"
@@ -65,7 +65,7 @@ S=${WORKDIR}/${MY_P}
 LICENSE="GPL-3+"
 SLOT="0"
 if (( PLEVEL >= 0 )); then
-	KEYWORDS="~alpha amd64 arm arm64 ~hppa ~ia64 ~loong ~m68k ~mips ppc ppc64 ~riscv ~s390 sparc x86 ~amd64-linux ~x86-linux ~arm64-macos ~ppc-macos ~x64-macos ~x64-solaris"
+	KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~m68k ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86 ~amd64-linux ~x86-linux ~arm64-macos ~ppc-macos ~x64-macos ~x64-solaris"
 fi
 IUSE="afs bashlogger examples mem-scramble +net nls plugins pgo +readline"
 
@@ -93,11 +93,6 @@ PATCHES=(
 
 	# Patches to or from Chet, posted to the bug-bash mailing list.
 	"${FILESDIR}/${PN}-5.0-syslog-history-extern.patch"
-	"${FILESDIR}/${PN}-5.2_p15-random-ub.patch"
-	"${FILESDIR}/${PN}-5.2_p15-configure-clang16.patch"
-	"${FILESDIR}/${PN}-5.2_p21-wpointer-to-int.patch"
-	"${FILESDIR}/${PN}-5.2_p21-configure-strtold.patch"
-	"${FILESDIR}/${PN}-5.2_p26-memory-leaks.patch"
 )
 
 pkg_setup() {
@@ -173,6 +168,10 @@ src_configure() {
 	# configure warns on use of non-Bison but doesn't abort. The result
 	# may misbehave at runtime.
 	unset -v YACC
+
+	# wcsnwidth(), substring() issues with -Wlto-type-mismatch, reported
+	# upstream to Chet by email.
+	filter-lto
 
 	myconf=(
 		--disable-profiling
@@ -372,22 +371,31 @@ pkg_postinst() {
 	read -r old_ver <<<"${REPLACING_VERSIONS}"
 	if [[ ! $old_ver ]]; then
 		:
-	elif ver_test "$old_ver" -ge "5.2" && ver_test "$old_ver" -ge "5.2_p26-r1"; then
+	elif ver_test "$old_ver" -ge "5.2" && ver_test "$old_ver" -ge "5.2_p26-r6"; then
 		return
-	elif ver_test "$old_ver" -lt "5.2" && ver_test "$old_ver" -ge "5.1_p16-r8"; then
+	elif ver_test "$old_ver" -lt "5.2" && ver_test "$old_ver" -ge "5.1_p16-r13"; then
 		return
 	fi
 
-	ewarn "Files situated under /etc/bash/bashrc.d must now have a suffix of .sh or .bash."
-	ewarn ""
-	ewarn "Gentoo now defaults to defining PROMPT_COMMAND as an array. Depending on the"
-	ewarn "characteristics of the operating environment, this array may contain commands"
-	ewarn "to set the window and pane title. Users that choose to customise this variable"
-	ewarn "in ~/.bashrc are advised to append their commands, using the following syntax."
-	ewarn ""
-	ewarn "PROMPT_COMMAND+=('custom command goes here')"
-	ewarn ""
-	ewarn "Alternatively, users that wish to opt out of Gentoo's window title setting"
-	ewarn "behaviour may now do so by either unsetting PROMPT_COMMAND or by re-defining it"
-	ewarn "as desired. Previously, there was no formally supported method of opting out."
+	while read -r; do ewarn "${REPLY}"; done <<'EOF'
+Files situated under /etc/bash/bashrc.d must now have a suffix of .sh or .bash.
+
+Gentoo now defaults to defining PROMPT_COMMAND as an array. Depending on the
+characteristics of the operating environment, this array may contain a command
+to set the terminal's window title. Those already choosing to customise the
+PROMPT_COMMAND variable are now advised to append their commands like so:
+
+PROMPT_COMMAND+=('custom command goes here')
+
+Gentoo no longer defaults to having bash manipulate the window title in the case
+that the terminal is controlled by sshd(8), unless screen or tmux are in use.
+Those wanting to set the title unconditionally may adjust ~/.bashrc - or create
+a custom /etc/bash/bashrc.d drop-in - to set PROMPT_COMMMAND like so:
+
+PROMPT_COMMAND=(genfun_set_win_title)
+
+Those who would prefer for bash never to interfere with the window title may
+now opt out of the default title setting behaviour, either with the "unset -v
+PROMPT_COMMAND" command or by re-defining PROMPT_COMMAND as desired.
+EOF
 }

app-shells/bash/bash-9999.ebuild
@@ -311,6 +311,9 @@ src_install() {
 	insinto /etc/bash/bashrc.d
 	my_prefixify DIR_COLORS "${FILESDIR}"/bashrc.d/10-gentoo-color.bash | newins - 10-gentoo-color.bash
 	doins "${FILESDIR}"/bashrc.d/10-gentoo-title.bash
+	if [[ ! ${EPREFIX} ]]; then
+		doins "${FILESDIR}"/bashrc.d/15-gentoo-bashrc-check.bash
+	fi
 
 	insinto /etc/skel
 	for f in bash{_logout,_profile,rc}; do
@@ -368,22 +371,31 @@ pkg_postinst() {
 	read -r old_ver <<<"${REPLACING_VERSIONS}"
 	if [[ ! $old_ver ]]; then
 		:
-	elif ver_test "$old_ver" -ge "5.2" && ver_test "$old_ver" -ge "5.2_p26-r1"; then
+	elif ver_test "$old_ver" -ge "5.2" && ver_test "$old_ver" -ge "5.2_p26-r6"; then
 		return
-	elif ver_test "$old_ver" -lt "5.2" && ver_test "$old_ver" -ge "5.1_p16-r8"; then
+	elif ver_test "$old_ver" -lt "5.2" && ver_test "$old_ver" -ge "5.1_p16-r13"; then
 		return
 	fi
 
-	ewarn "Files situated under /etc/bash/bashrc.d must now have a suffix of .sh or .bash."
-	ewarn ""
-	ewarn "Gentoo now defaults to defining PROMPT_COMMAND as an array. Depending on the"
-	ewarn "characteristics of the operating environment, this array may contain commands"
-	ewarn "to set the window and pane title. Users that choose to customise this variable"
-	ewarn "in ~/.bashrc are advised to append their commands, using the following syntax."
-	ewarn ""
-	ewarn "PROMPT_COMMAND+=('custom command goes here')"
-	ewarn ""
-	ewarn "Alternatively, users that wish to opt out of Gentoo's window title setting"
-	ewarn "behaviour may now do so by either unsetting PROMPT_COMMAND or by re-defining it"
-	ewarn "as desired. Previously, there was no formally supported method of opting out."
+	while read -r; do ewarn "${REPLY}"; done <<'EOF'
+Files situated under /etc/bash/bashrc.d must now have a suffix of .sh or .bash.
+
+Gentoo now defaults to defining PROMPT_COMMAND as an array. Depending on the
+characteristics of the operating environment, this array may contain a command
+to set the terminal's window title. Those already choosing to customise the
+PROMPT_COMMAND variable are now advised to append their commands like so:
+
+PROMPT_COMMAND+=('custom command goes here')
+
+Gentoo no longer defaults to having bash manipulate the window title in the case
+that the terminal is controlled by sshd(8), unless screen or tmux are in use.
+Those wanting to set the title unconditionally may adjust ~/.bashrc - or create
+a custom /etc/bash/bashrc.d drop-in - to set PROMPT_COMMMAND like so:
+
+PROMPT_COMMAND=(genfun_set_win_title)
+
+Those who would prefer for bash never to interfere with the window title may
+now opt out of the default title setting behaviour, either with the "unset -v
+PROMPT_COMMAND" command or by re-defining PROMPT_COMMAND as desired.
+EOF
 }

app-shells/bash/files/bashrc.d/10-gentoo-title.bash
@@ -20,17 +20,18 @@ genfun_set_win_title() {
 	# the title bar to be affected.
 	genfun_set_win_title() {
 		genfun_sanitise_cwd
-		printf '\033]2;%s@%s - %s\007' "${USER}" "${HOSTNAME%%.*}" "${_cwd}"
+		printf '\033]0;%s@%s - %s\007' "${USER}" "${HOSTNAME%%.*}" "${_cwd}"
 	}
 
 	genfun_set_win_title
 }
 
-# If the TTY is that of sshd(8) then proceed no further. Alas, there exist many
-# operating environments in which the window title would otherwise not be
-# restored upon ssh(1) exiting. Users wishing to coerce the historical
-# behaviour have the option of setting PROMPT_COMMAND=(genfun_set_win_title).
-if [[ ${SSH_TTY} && ${SSH_TTY} == "$(tty)" ]]; then
+# Proceed no further if the TTY is that of sshd(8) and if not running a terminal
+# multiplexer. Alas, there exist many operating environments in which the window
+# title would otherwise not be restored upon ssh(1) exiting. Those who wish for
+# the title to be set unconditionally may adjust ~/.bashrc - or create a custom
+# bashrc.d drop-in - to define PROMPT_COMMAND=(genfun_set_win_title).
+if [[ ${SSH_TTY} && ${TERM} != @(screen|tmux)* && ${SSH_TTY} == "$(tty)" ]]; then
 	return
 fi
 