cf2316321b85b62ea2e409b198a5eef3da37c859
Thibaud CANALE <thican@thican.net>
2024-07-01 06:26:25 +0200

    net-im/discord: fix seccomp test in launcher
    Fix a bug I introduced in
    commit 6368a566a8b05f09e5edfad5d4f7577a83c6334b.
    
    Signed-off-by: Thibaud CANALE <thican@thican.net>

--- net-im/discord/files/launcher.sh
+++ net-im/discord/files/launcher.sh
@@ -6,7 +6,7 @@ declare -a discord_parameters
 
 SECCOMP=false
 
-[[ ! "${SECCOMP}" ]] && discord_parameters+=( --disable-seccomp-filter-sandbox )
+"${SECCOMP}" || discord_parameters+=( --disable-seccomp-filter-sandbox )
 
 [[ -n "${WAYLAND_DISPLAY}" ]] && discord_parameters+=(
 	--enable-features=UseOzonePlatform
