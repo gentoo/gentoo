gentoo/gentoo

commit 4551df6ec5cae19486cb63698e294a3f62015600

dev-libs/nettle: disable altivec for <P7
Thanks to awilfox for the help with ppc cpp.

Closes: https://bugs.gentoo.org/920234
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/nettle/nettle-3.9.1-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2023 Gentoo Authors
+# Copyright 1999-2024 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -82,6 +82,13 @@ multilib_src_configure() {
 		# openssl is just used for benchmarks (bug #427526)
 		--disable-openssl
 	)
+
+	# https://git.lysator.liu.se/nettle/nettle/-/issues/7
+	if use cpu_flags_ppc_altivec && ! tc-cpp-is-true "defined(__VSX__) && __VSX__ == 1" "${CPPFLAGS}" "${CFLAGS}" ; then
+		ewarn "cpu_flags_ppc_altivec is enabled, but nettle's asm requires >=P7."
+		ewarn "Disabling, sorry! See bug #920234."
+		myeconfargs+=( --disable-power-altivec )
+	fi
 
 	ECONF_SOURCE="${S}" econf "${myeconfargs[@]}"
 }

dev-libs/nettle/nettle-3.9-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2023 Gentoo Authors
+# Copyright 1999-2024 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -84,6 +84,13 @@ multilib_src_configure() {
 		# openssl is just used for benchmarks (bug #427526)
 		--disable-openssl
 	)
+
+	# https://git.lysator.liu.se/nettle/nettle/-/issues/7
+	if use cpu_flags_ppc_altivec && ! tc-cpp-is-true "defined(__VSX__) && __VSX__ == 1" "${CPPFLAGS}" "${CFLAGS}" ; then
+		ewarn "cpu_flags_ppc_altivec is enabled, but nettle's asm requires >=P7."
+		ewarn "Disabling, sorry! See bug #920234."
+		myeconfargs+=( --disable-power-altivec )
+	fi
 
 	ECONF_SOURCE="${S}" econf "${myeconfargs[@]}"
 }

commit c627cc231796bf88ce7ef664986863d32de4a5a4

dev-libs/nettle: fix quoting for altivec sanity check
Bug: https://bugs.gentoo.org/920234
Closes: https://bugs.gentoo.org/935410
Fixes: 4551df6ec5cae19486cb63698e294a3f62015600
Thanks-to: Nick Bowler <nbowler@draconx.ca>
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/nettle/nettle-3.10.ebuild
@@ -89,7 +89,7 @@ multilib_src_configure() {
 	)
 
 	# https://git.lysator.liu.se/nettle/nettle/-/issues/7
-	if use cpu_flags_ppc_altivec && ! tc-cpp-is-true "defined(__VSX__) && __VSX__ == 1" "${CPPFLAGS}" "${CFLAGS}" ; then
+	if use cpu_flags_ppc_altivec && ! tc-cpp-is-true "defined(__VSX__) && __VSX__ == 1" ${CPPFLAGS} ${CFLAGS} ; then
 		ewarn "cpu_flags_ppc_altivec is enabled, but nettle's asm requires >=P7."
 		ewarn "Disabling, sorry! See bug #920234."
 		myeconfargs+=( --disable-power-altivec )

dev-libs/nettle/nettle-3.9.1-r1.ebuild
@@ -84,7 +84,7 @@ multilib_src_configure() {
 	)
 
 	# https://git.lysator.liu.se/nettle/nettle/-/issues/7
-	if use cpu_flags_ppc_altivec && ! tc-cpp-is-true "defined(__VSX__) && __VSX__ == 1" "${CPPFLAGS}" "${CFLAGS}" ; then
+	if use cpu_flags_ppc_altivec && ! tc-cpp-is-true "defined(__VSX__) && __VSX__ == 1" ${CPPFLAGS} ${CFLAGS} ; then
 		ewarn "cpu_flags_ppc_altivec is enabled, but nettle's asm requires >=P7."
 		ewarn "Disabling, sorry! See bug #920234."
 		myeconfargs+=( --disable-power-altivec )