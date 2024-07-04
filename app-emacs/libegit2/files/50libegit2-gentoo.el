23158344e41232440bb377ee3d32601d79d57f5f
Maciej Barć <xgqt@gentoo.org>
2024-07-04 14:39:59 +0200

    app-emacs/libegit2: treeclean
    Closes: https://bugs.gentoo.org/928920
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

--- app-emacs/libegit2/files/50libegit2-gentoo.el
+++
@@ -1,4 +0,0 @@
-(add-to-list 'load-path "@SITELISP@")
-(add-to-list 'load-path "@EMACSMODULES@")
-(defvar libgit--build-dir "@EMACSMODULES@")
-(autoload 'libgit-load "libegit2" "Load the `libegit2` dynamic module." t)
