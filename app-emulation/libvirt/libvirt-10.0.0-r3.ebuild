gentoo/gentoo

commit bba723505f488b52bd593869b5b9a0df096ffbb4

app-emulation/libvirt: Drag in dev-python/pytest
As of its upstream commit v9.10.0-rc1~114 libvirt introduced its
own RPC generator written in python and also some tests for it.
But these require pytest. Therefore, generate corresponding
dependency if running tests.

Closes: https://bugs.gentoo.org/932652
Signed-off-by: Michal Privoznik <michal.privoznik@gmail.com>
Closes: https://github.com/gentoo/gentoo/pull/36814
Signed-off-by: Sam James <sam@gentoo.org>

app-emulation/libvirt/libvirt-9999.ebuild
@@ -136,6 +136,11 @@ DEPEND="
 	${BDEPEND}
 	${RDEPEND}
 	${PYTHON_DEPS}
+	test? (
+		$(python_gen_any_dep '
+			dev-python/pytest[${PYTHON_USEDEP}]
+		')
+	)
 "
 # The 'circular' dependency on dev-python/libvirt-python is because of
 # virt-qemu-qmp-proxy.
@@ -148,6 +153,11 @@ PATCHES=(
 	"${FILESDIR}"/${PN}-9.9.0-do-not-use-sysconfig.patch
 	"${FILESDIR}"/${PN}-9.6.0-fix-paths-for-apparmor.patch
 )
+
+python_check_deps() {
+	use test && python_has_version -d "dev-python/pytest[${PYTHON_USEDEP}]"
+	return 0
+}
 
 pkg_setup() {
 	# Check kernel configuration:

app-emulation/libvirt/libvirt-10.0.0-r2.ebuild
@@ -136,6 +136,11 @@ DEPEND="
 	${BDEPEND}
 	${RDEPEND}
 	${PYTHON_DEPS}
+	test? (
+		$(python_gen_any_dep '
+			dev-python/pytest[${PYTHON_USEDEP}]
+		')
+	)
 "
 # The 'circular' dependency on dev-python/libvirt-python is because of
 # virt-qemu-qmp-proxy.
@@ -150,6 +155,11 @@ PATCHES=(
 	"${FILESDIR}"/${PN}-10.1.0-Fix-off-by-one-error-in-udevListInterfacesByStatus.patch
 	"${FILESDIR}"/${PN}-10.2.0-remote-check-for-negative-array-lengths-before-alloc.patch
 )
+
+python_check_deps() {
+	use test && python_has_version -d "dev-python/pytest[${PYTHON_USEDEP}]"
+	return 0
+}
 
 pkg_setup() {
 	# Check kernel configuration:

app-emulation/libvirt/libvirt-10.1.0-r1.ebuild
@@ -136,6 +136,11 @@ DEPEND="
 	${BDEPEND}
 	${RDEPEND}
 	${PYTHON_DEPS}
+	test? (
+		$(python_gen_any_dep '
+			dev-python/pytest[${PYTHON_USEDEP}]
+		')
+	)
 "
 # The 'circular' dependency on dev-python/libvirt-python is because of
 # virt-qemu-qmp-proxy.
@@ -149,6 +154,11 @@ PATCHES=(
 	"${FILESDIR}"/${PN}-9.6.0-fix-paths-for-apparmor.patch
 	"${FILESDIR}"/${PN}-10.2.0-remote-check-for-negative-array-lengths-before-alloc.patch
 )
+
+python_check_deps() {
+	use test && python_has_version -d "dev-python/pytest[${PYTHON_USEDEP}]"
+	return 0
+}
 
 pkg_setup() {
 	# Check kernel configuration:

app-emulation/libvirt/libvirt-10.2.0.ebuild
@@ -136,6 +136,11 @@ DEPEND="
 	${BDEPEND}
 	${RDEPEND}
 	${PYTHON_DEPS}
+	test? (
+		$(python_gen_any_dep '
+			dev-python/pytest[${PYTHON_USEDEP}]
+		')
+	)
 "
 # The 'circular' dependency on dev-python/libvirt-python is because of
 # virt-qemu-qmp-proxy.
@@ -148,6 +153,11 @@ PATCHES=(
 	"${FILESDIR}"/${PN}-9.9.0-do-not-use-sysconfig.patch
 	"${FILESDIR}"/${PN}-9.6.0-fix-paths-for-apparmor.patch
 )
+
+python_check_deps() {
+	use test && python_has_version -d "dev-python/pytest[${PYTHON_USEDEP}]"
+	return 0
+}
 
 pkg_setup() {
 	# Check kernel configuration:

app-emulation/libvirt/libvirt-10.3.0-r1.ebuild
@@ -136,6 +136,11 @@ DEPEND="
 	${BDEPEND}
 	${RDEPEND}
 	${PYTHON_DEPS}
+	test? (
+		$(python_gen_any_dep '
+			dev-python/pytest[${PYTHON_USEDEP}]
+		')
+	)
 "
 # The 'circular' dependency on dev-python/libvirt-python is because of
 # virt-qemu-qmp-proxy.
@@ -149,6 +154,11 @@ PATCHES=(
 	"${FILESDIR}"/${PN}-9.6.0-fix-paths-for-apparmor.patch
 	"${FILESDIR}"/${PN}-10.3.0-vsh-Don-t-init-history-in-cmdComplete.patch
 )
+
+python_check_deps() {
+	use test && python_has_version -d "dev-python/pytest[${PYTHON_USEDEP}]"
+	return 0
+}
 
 pkg_setup() {
 	# Check kernel configuration:

commit 424908701d9854699393101d9a732cfd6a450ef7

app-emulation/libvirt: implement a correct python_check_deps
Due to portage design whereby commands which fail aren't considered
failures unless you explicitly use `|| die`, a common footgun in bash
scripting propagates throughout the portage ecosystem:

the use of `cmd1 && cmd2` for conditional logic.

This python_check_deps function did such, and then handled the case
where `use test` was false by unconditionally ignoring the result of the
previous line by returning 0. Hence, python_check_deps could never
decide that an impl was unable to be used. As a result, if python 3.13
and 3.12 were both installed, but $(python_gen_any_dep ...) discovered
pytest installed solely for 3.12, portage would not reinstall pytest for
3.13 support whereas the eclass would select 3.13 as the preferred
(latest) python impl.

Fix this by correctly using bash, shunning `cmd1 && cmd2`, and instead
using `if cmd1; then cmd2; fi`, which returns correct return values
based on the return value of both cmd1 and cmd2, without requiring
hardcoded `return 0`s of any variety, unconditional or otherwise.

Fixes: bba723505f488b52bd593869b5b9a0df096ffbb4
Bug: https://bugs.gentoo.org/932652
Closes: https://bugs.gentoo.org/935849
Signed-off-by: Eli Schwartz <eschwartz@gentoo.org>

app-emulation/libvirt/libvirt-10.0.0-r3.ebuild
@@ -158,8 +158,9 @@ PATCHES=(
 )
 
 python_check_deps() {
-	use test && python_has_version -d "dev-python/pytest[${PYTHON_USEDEP}]"
-	return 0
+	if use test; then
+		python_has_version -d "dev-python/pytest[${PYTHON_USEDEP}]"
+	fi
 }
 
 pkg_setup() {

app-emulation/libvirt/libvirt-10.1.0-r2.ebuild
@@ -157,8 +157,9 @@ PATCHES=(
 )
 
 python_check_deps() {
-	use test && python_has_version -d "dev-python/pytest[${PYTHON_USEDEP}]"
-	return 0
+	if use test; then
+		python_has_version -d "dev-python/pytest[${PYTHON_USEDEP}]"
+	fi
 }
 
 pkg_setup() {

app-emulation/libvirt/libvirt-10.2.0-r1.ebuild
@@ -156,8 +156,9 @@ PATCHES=(
 )
 
 python_check_deps() {
-	use test && python_has_version -d "dev-python/pytest[${PYTHON_USEDEP}]"
-	return 0
+	if use test; then
+		python_has_version -d "dev-python/pytest[${PYTHON_USEDEP}]"
+	fi
 }
 
 pkg_setup() {

app-emulation/libvirt/libvirt-10.3.0-r2.ebuild
@@ -157,8 +157,9 @@ PATCHES=(
 )
 
 python_check_deps() {
-	use test && python_has_version -d "dev-python/pytest[${PYTHON_USEDEP}]"
-	return 0
+	if use test; then
+		python_has_version -d "dev-python/pytest[${PYTHON_USEDEP}]"
+	fi
 }
 
 pkg_setup() {

app-emulation/libvirt/libvirt-10.5.0.ebuild
@@ -158,8 +158,9 @@ PATCHES=(
 )
 
 python_check_deps() {
-	use test && python_has_version -d "dev-python/pytest[${PYTHON_USEDEP}]"
-	return 0
+	if use test; then
+		python_has_version -d "dev-python/pytest[${PYTHON_USEDEP}]"
+	fi
 }
 
 pkg_setup() {

app-emulation/libvirt/libvirt-9999.ebuild
@@ -158,8 +158,9 @@ PATCHES=(
 )
 
 python_check_deps() {
-	use test && python_has_version -d "dev-python/pytest[${PYTHON_USEDEP}]"
-	return 0
+	if use test; then
+		python_has_version -d "dev-python/pytest[${PYTHON_USEDEP}]"
+	fi
 }
 
 pkg_setup() {