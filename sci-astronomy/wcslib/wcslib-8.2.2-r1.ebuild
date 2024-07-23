gentoo/gentoo

commit 632bccc7eb31b12665622103f3a2091349e80269

sci-astronomy/wcslib: bump subslot to guarantee rebuilds
The soname has changed in the new version, and this was being tracked by
SLOT= so we should ensure that is up to date... oops.

Signed-off-by: Eli Schwartz <eschwartz93@gmail.com>
Signed-off-by: Sam James <sam@gentoo.org>

sci-astronomy/wcslib/wcslib-8.2.2-r1.ebuild
@@ -11,7 +11,7 @@ DESCRIPTION="Astronomical World Coordinate System transformations library"
 HOMEPAGE="https://www.atnf.csiro.au/people/mcalabre/WCS/"
 SRC_URI="ftp://ftp.atnf.csiro.au/pub/software/${PN}/${P}.tar.bz2"
 
-SLOT="0/7"
+SLOT="0/8"
 LICENSE="LGPL-3"
 KEYWORDS="~amd64 ~x86 ~amd64-linux ~x86-linux"
 IUSE="doc fortran fits pgplot static-libs +tools"

commit 1273941fec5688ef7e8bea3f98963aac0196f11a

sci-astronomy/wcslib: use ver_cut to produce SLOT
Previously, bumping to 8.x caused the SLOT to temporarily lag behind
because I didn't pay attention. Later fixed, by bumping the hardcoded
version, but... upstream rigorously guarantees semver:

https://www.atnf.csiro.au/people/mcalabre/WCS/wcslib/versioning.html

So we can use ver_cut to directly derive the correct slot from the major
version, which ensures that doesn't happen again.

Fixes: 632bccc7eb31b12665622103f3a2091349e80269
Signed-off-by: Eli Schwartz <eschwartz@gentoo.org>

sci-astronomy/wcslib/wcslib-8.2.2-r1.ebuild
@@ -11,7 +11,7 @@ DESCRIPTION="Astronomical World Coordinate System transformations library"
 HOMEPAGE="https://www.atnf.csiro.au/people/mcalabre/WCS/"
 SRC_URI="ftp://ftp.atnf.csiro.au/pub/software/${PN}/${P}.tar.bz2"
 
-SLOT="0/8"
+SLOT="0/$(ver_cut 1)"
 LICENSE="LGPL-3"
 KEYWORDS="~amd64 ~x86 ~amd64-linux ~x86-linux"
 IUSE="doc fortran fits pgplot static-libs +tools"