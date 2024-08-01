gentoo/gentoo

commit 943614ccefce87dd83cbaf99849726eb481311ab

dev-db/mysql-connector-c++: fix tragic overcommit of make jobs
Internally, this builds additional dependencies inside of cmake by
having build rules that run another cmake. Inside src_compile, make
forks out to make -j$(nproc) at least once, and perhaps even more times
(this logic is part of their generic handling of included dependencies
but we may only be building protobuf that way anyway).

We end up with a minimum of two times the number of reasonably safe jobs
to run, your system resources get clobbered, and everything freezes
until the OOM killer gets involved.

Golly gee, I *wish* it were merely ignoring MAKEFLAGS. Then we would
eventually succeed, except slower...

Closes: https://bugs.gentoo.org/921309
Signed-off-by: Eli Schwartz <eschwartz@gentoo.org>

dev-db/mysql-connector-c++/mysql-connector-c++-8.0.33-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2023 Gentoo Authors
+# Copyright 1999-2024 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -33,6 +33,16 @@ PATCHES=(
 	"${FILESDIR}"/${PN}-8.0.27-mysqlclient_r.patch
 	"${FILESDIR}"/${P}-jdbc.patch
 )
+
+src_prepare() {
+	cmake_src_prepare
+	# ignores MAKEOPTS and runs recursive make -j$(nproc). Clobbers jobs badly
+	# enough that your system immediately freezes.
+	#
+	# https://bugs.gentoo.org/921309
+	# https://bugs.mysql.com/bug.php?id=115734
+	sed -i 's/prc_cnt AND NOT/FALSE AND NOT/' cdk/cmake/dependency.cmake
+}
 
 src_configure() {
 	local mycmakeargs=(

commit 421fb27e0604278c830d53998635fc7839471278

dev-db/mysql-connector-c++: insert missing die
Not sure how I overlooked this.

Fixes: 943614ccefce87dd83cbaf99849726eb481311ab
Signed-off-by: Eli Schwartz <eschwartz@gentoo.org>

dev-db/mysql-connector-c++/mysql-connector-c++-8.0.33-r1.ebuild
@@ -41,7 +41,7 @@ src_prepare() {
 	#
 	# https://bugs.gentoo.org/921309
 	# https://bugs.mysql.com/bug.php?id=115734
-	sed -i 's/prc_cnt AND NOT/FALSE AND NOT/' cdk/cmake/dependency.cmake
+	sed -i 's/prc_cnt AND NOT/FALSE AND NOT/' cdk/cmake/dependency.cmake || die
 }
 
 src_configure() {