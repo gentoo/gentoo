gentoo/gentoo

commit 7eab20a224f411ef2660f4fecae22e28cad711e7

toolchain.eclass: try harder to suppress -Wformat and friends
* Followup to f66787897a590b8c949d257e3a9107f8995f62ae
* See https://inbox.sourceware.org/gcc-patches/87v80cn24t.fsf@gentoo.org/

Still not quite right though per link above.

Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -1947,8 +1947,11 @@ toolchain_src_test() {
 		# Go doesn't support this and causes noisy warnings
 		filter-flags -Wbuiltin-declaration-mismatch
 
+		local suppress_warn="/-Wno-format/-Wno-format-security/-Wno-trampolines"
+		RUNTESTFLAGS+=" --target_board=unix\{${suppress_warn}"
 		# TODO: Does this handle s390 (-m31) correctly?
-		is_multilib && GCC_TESTS_RUNTESTFLAGS+=" --target_board=unix\{,-m32\}"
+		is_multilib && GCC_TESTS_RUNTESTFLAGS+=" ,-m32/${suppress_warn}"
+		RUNTESTFLAGS+="\}"
 
 		# nonfatal here as we die if the comparison below fails. Also, note that
 		# the exit code of targets other than 'check' may be unreliable.

commit 60bbe4613834d198e3969ce9ffb6124212b53c41

toolchain.eclass: fix typos w/ RUNTESTFLAGS
Fixes: 7eab20a224f411ef2660f4fecae22e28cad711e7
Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -1948,10 +1948,10 @@ toolchain_src_test() {
 		filter-flags -Wbuiltin-declaration-mismatch
 
 		local suppress_warn="/-Wno-format/-Wno-format-security/-Wno-trampolines"
-		RUNTESTFLAGS+=" --target_board=unix\{${suppress_warn}"
+		GCC_TESTS_RUNTESTFLAGS+=" --target_board=unix\{${suppress_warn}"
 		# TODO: Does this handle s390 (-m31) correctly?
-		is_multilib && GCC_TESTS_RUNTESTFLAGS+=" ,-m32/${suppress_warn}"
-		RUNTESTFLAGS+="\}"
+		is_multilib && GCC_TESTS_RUNTESTFLAGS+=",-m32/${suppress_warn}"
+		GCC_TESTS_RUNTESTFLAGS+="\}"
 
 		# nonfatal here as we die if the comparison below fails. Also, note that
 		# the exit code of targets other than 'check' may be unreliable.