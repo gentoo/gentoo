gentoo/gentoo

commit a35501c8975b92912b067a0c5ec442dc1dd702f1

dev-build/make: switch to new guile mechanism
Signed-off-by: Arsen Arsenović <arsen@gentoo.org>

dev-build/make/make-4.4.1-r100.ebuild
@@ -66,6 +66,10 @@ src_prepare() {
 	if [[ ${PV} == 9999 ]] ; then
 		eautoreconf
 	fi
+
+	if use guile; then
+		guile-single_src_prepare
+	fi
 }
 
 pkg_setup() {
@@ -94,4 +98,5 @@ src_install() {
 
 	dosym gmake /usr/bin/make
 	dosym gmake.1 /usr/share/man/man1/make.1
+	guile_unstrip_ccache
 }

profiles/package.mask
@@ -39,6 +39,7 @@
 # Masked until the whole Guile ecosystem is updated.
 dev-scheme/guile:2.2
 dev-scheme/guile:3.0
+>=dev-build/make-4.4.1-r100
 
 # Eli Schwartz <eschwartz@gentoo.org> (2024-08-15)
 # Abandoned upstream since 2011, unmaintained in gentoo since 2017. Tons of

commit 1cdc74dabf98dce9923045dbddbe340b20fea719

dev-build/make: consistently call guile_bump_sources in new guile revs
It seems that I forgot to update this ebuild after refactoring.

Fixes: a35501c8975b (dev-build/make: switch to new guile mechanism)
Signed-off-by: Arsen Arsenović <arsen@gentoo.org>

dev-build/make/make-4.4.1-r100.ebuild
@@ -68,7 +68,7 @@ src_prepare() {
 	fi
 
 	if use guile; then
-		guile-single_src_prepare
+		guile_bump_sources
 	fi
 }
 

dev-build/make/make-9999.ebuild
@@ -66,6 +66,10 @@ src_prepare() {
 	if [[ ${PV} == 9999 ]] ; then
 		eautoreconf
 	fi
+
+	if use guile; then
+		guile_bump_sources
+	fi
 }
 
 pkg_setup() {