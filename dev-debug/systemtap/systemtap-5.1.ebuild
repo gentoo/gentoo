gentoo/gentoo

commit 818cec7cd4efae42c55af03f4c0617a95d069c14

dev-debug/systemtap: add 5.1, add 9999
* Improve test deps while here and use new --disable-Werror
* Workaround bashism in Makefile.am for Python (brace expansion)
* Add live ebuild + template (useful for testing w/ newer kernels)
* Make debuginfod dep optional again after upstream fixes

Bug: https://bugs.gentoo.org/921113
Signed-off-by: Sam James <sam@gentoo.org>

dev-debug/systemtap/systemtap-5.1.ebuild
@@ -1,0 +1,136 @@
+# Copyright 1999-2024 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+PYTHON_COMPAT=( python3_{10..12} pypy3 )
+
+inherit autotools linux-info python-single-r1
+
+DESCRIPTION="Linux trace/probe tool"
+HOMEPAGE="https://sourceware.org/systemtap/"
+if [[ ${PV} == 9999 ]] ; then
+	EGIT_REPO_URI="https://sourceware.org/git/systemtap.git"
+	inherit git-r3
+else
+	SRC_URI="https://sourceware.org/ftp/${PN}/releases/${P}.tar.gz"
+	KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~ia64 ~loong ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86"
+fi
+
+LICENSE="GPL-2"
+SLOT="0"
+IUSE="debuginfod libvirt selinux sqlite +ssl test zeroconf"
+
+CDEPEND="
+	${PYTHON_DEPS}
+
+	dev-libs/boost:=
+	>=dev-libs/elfutils-0.142[debuginfod?]
+	dev-libs/json-c:=
+	sys-libs/ncurses:=
+	sys-libs/readline:=
+
+	libvirt? ( >=app-emulation/libvirt-1.0.2 )
+	selinux? ( sys-libs/libselinux )
+	sqlite? ( dev-db/sqlite:3 )
+	ssl? (
+		dev-libs/nspr
+		dev-libs/nss
+	)
+	zeroconf? ( net-dns/avahi )
+"
+DEPEND="
+	${CDEPEND}
+	app-alternatives/cpio
+	app-text/xmlto
+	$(python_gen_cond_dep 'dev-python/setuptools[${PYTHON_USEDEP}]')
+	>=sys-devel/gettext-0.18.2
+
+	libvirt? ( dev-libs/libxml2 )
+"
+RDEPEND="
+	${CDEPEND}
+	acct-group/stapdev
+	acct-group/stapsys
+	acct-group/stapusr
+"
+BDEPEND="
+	test? (
+		dev-util/dejagnu
+		|| (
+			net-analyzer/netcat
+			net-analyzer/openbsd-netcat
+		)
+		sys-apps/which
+	)
+"
+
+CONFIG_CHECK="~KPROBES ~RELAY ~DEBUG_FS"
+ERROR_KPROBES="${PN} requires support for KProbes Instrumentation (KPROBES) - this can be enabled in 'Instrumentation Support -> Kprobes'."
+ERROR_RELAY="${PN} works with support for user space relay support (RELAY) - this can be enabled in 'General setup -> Kernel->user space relay support (formerly relayfs)'."
+ERROR_DEBUG_FS="${PN} works best with support for Debug Filesystem (DEBUG_FS) - this can be enabled in 'Kernel hacking -> Debug Filesystem'."
+
+DOCS="AUTHORS HACKING NEWS README"
+REQUIRED_USE="${PYTHON_REQUIRED_USE}"
+RESTRICT="!test? ( test )"
+PATCHES=(
+	"${FILESDIR}/${PN}-3.1-ia64.patch"
+)
+
+pkg_setup() {
+	linux-info_pkg_setup
+	python-single-r1_pkg_setup
+}
+
+src_prepare() {
+	python_fix_shebang .
+
+	sed -i \
+		-e 's#$(INSTALL_DATA) $(srcdir)/stap-exporter.options "$(DESTDIR)$(sysconfdir)/sysconfig/stap-exporter"##g' \
+		stap-exporter/Makefile.am || die "Failed to modify stap-exporter Makefile.am"
+	sed -i \
+		-e '\#^EnvironmentFile=-/etc/sysconfig/stap-exporter#d' \
+		-e 's#$PORT $KEEPALIVE $SCRIPTS $OPTIONS#--port 9900 --keepalive 300#g' \
+		stap-exporter/stap-exporter.service || die "Failed to adapt stap-exporter.service"
+
+	default
+	eautoreconf
+}
+
+src_configure() {
+	local myeconfargs=(
+		--cache-file="${S}"/config.cache
+		--disable-docs
+		--disable-grapher
+		--disable-refdocs
+		--disable-server
+		--disable-Werror
+		# Our toolchain sets this for us already and adding in
+		# -D_FORTIFY_SOURCE=2 breaks builds w/ no optimisation.
+		# This option (at least as of 4.5) doesn't pass -fno* etc,
+		# it just doesn't _add_ options, which is good. If it changes
+		# to actually pass -fno-stack-protector and friends, we'll
+		# need to change course. Forcing =2 also has problems for
+		# setting it to 3.
+		# bug #794667.
+		--disable-ssp
+		--enable-pie
+		--with-python3
+		--without-java
+		--without-openssl
+		--without-python2-probes
+		--without-rpm
+		$(use_enable libvirt virt)
+		$(use_enable sqlite)
+		$(use_with debuginfod)
+		$(use_with zeroconf avahi)
+		$(use_with ssl nss)
+		$(use_with selinux)
+	)
+	CONFIG_SHELL="${BROOT}"/bin/bash PYTHON3="${PYTHON}" econf "${myeconfargs[@]}"
+}
+
+src_install() {
+	default
+	python_optimize
+}

dev-debug/systemtap/systemtap-9999.ebuild
@@ -1,0 +1,136 @@
+# Copyright 1999-2024 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+PYTHON_COMPAT=( python3_{10..12} pypy3 )
+
+inherit autotools linux-info python-single-r1
+
+DESCRIPTION="Linux trace/probe tool"
+HOMEPAGE="https://sourceware.org/systemtap/"
+if [[ ${PV} == 9999 ]] ; then
+	EGIT_REPO_URI="https://sourceware.org/git/systemtap.git"
+	inherit git-r3
+else
+	SRC_URI="https://sourceware.org/ftp/${PN}/releases/${P}.tar.gz"
+	KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~ia64 ~loong ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86"
+fi
+
+LICENSE="GPL-2"
+SLOT="0"
+IUSE="debuginfod libvirt selinux sqlite +ssl test zeroconf"
+
+CDEPEND="
+	${PYTHON_DEPS}
+
+	dev-libs/boost:=
+	>=dev-libs/elfutils-0.142[debuginfod?]
+	dev-libs/json-c:=
+	sys-libs/ncurses:=
+	sys-libs/readline:=
+
+	libvirt? ( >=app-emulation/libvirt-1.0.2 )
+	selinux? ( sys-libs/libselinux )
+	sqlite? ( dev-db/sqlite:3 )
+	ssl? (
+		dev-libs/nspr
+		dev-libs/nss
+	)
+	zeroconf? ( net-dns/avahi )
+"
+DEPEND="
+	${CDEPEND}
+	app-alternatives/cpio
+	app-text/xmlto
+	$(python_gen_cond_dep 'dev-python/setuptools[${PYTHON_USEDEP}]')
+	>=sys-devel/gettext-0.18.2
+
+	libvirt? ( dev-libs/libxml2 )
+"
+RDEPEND="
+	${CDEPEND}
+	acct-group/stapdev
+	acct-group/stapsys
+	acct-group/stapusr
+"
+BDEPEND="
+	test? (
+		dev-util/dejagnu
+		|| (
+			net-analyzer/netcat
+			net-analyzer/openbsd-netcat
+		)
+		sys-apps/which
+	)
+"
+
+CONFIG_CHECK="~KPROBES ~RELAY ~DEBUG_FS"
+ERROR_KPROBES="${PN} requires support for KProbes Instrumentation (KPROBES) - this can be enabled in 'Instrumentation Support -> Kprobes'."
+ERROR_RELAY="${PN} works with support for user space relay support (RELAY) - this can be enabled in 'General setup -> Kernel->user space relay support (formerly relayfs)'."
+ERROR_DEBUG_FS="${PN} works best with support for Debug Filesystem (DEBUG_FS) - this can be enabled in 'Kernel hacking -> Debug Filesystem'."
+
+DOCS="AUTHORS HACKING NEWS README"
+REQUIRED_USE="${PYTHON_REQUIRED_USE}"
+RESTRICT="!test? ( test )"
+PATCHES=(
+	"${FILESDIR}/${PN}-3.1-ia64.patch"
+)
+
+pkg_setup() {
+	linux-info_pkg_setup
+	python-single-r1_pkg_setup
+}
+
+src_prepare() {
+	python_fix_shebang .
+
+	sed -i \
+		-e 's#$(INSTALL_DATA) $(srcdir)/stap-exporter.options "$(DESTDIR)$(sysconfdir)/sysconfig/stap-exporter"##g' \
+		stap-exporter/Makefile.am || die "Failed to modify stap-exporter Makefile.am"
+	sed -i \
+		-e '\#^EnvironmentFile=-/etc/sysconfig/stap-exporter#d' \
+		-e 's#$PORT $KEEPALIVE $SCRIPTS $OPTIONS#--port 9900 --keepalive 300#g' \
+		stap-exporter/stap-exporter.service || die "Failed to adapt stap-exporter.service"
+
+	default
+	eautoreconf
+}
+
+src_configure() {
+	local myeconfargs=(
+		--cache-file="${S}"/config.cache
+		--disable-docs
+		--disable-grapher
+		--disable-refdocs
+		--disable-server
+		--disable-Werror
+		# Our toolchain sets this for us already and adding in
+		# -D_FORTIFY_SOURCE=2 breaks builds w/ no optimisation.
+		# This option (at least as of 4.5) doesn't pass -fno* etc,
+		# it just doesn't _add_ options, which is good. If it changes
+		# to actually pass -fno-stack-protector and friends, we'll
+		# need to change course. Forcing =2 also has problems for
+		# setting it to 3.
+		# bug #794667.
+		--disable-ssp
+		--enable-pie
+		--with-python3
+		--without-java
+		--without-openssl
+		--without-python2-probes
+		--without-rpm
+		$(use_enable libvirt virt)
+		$(use_enable sqlite)
+		$(use_with debuginfod)
+		$(use_with zeroconf avahi)
+		$(use_with ssl nss)
+		$(use_with selinux)
+	)
+	CONFIG_SHELL="${BROOT}"/bin/bash PYTHON3="${PYTHON}" econf "${myeconfargs[@]}"
+}
+
+src_install() {
+	default
+	python_optimize
+}

dev-debug/systemtap/Manifest
@@ -1,2 +1,3 @@
 DIST systemtap-4.8.tar.gz 5628003 BLAKE2B 9f5050eba29d390c907b117187f94ef088f16bb739b4971bc32ffe88b1024a4157cd4e9793a8fc1e149a5a182a28330e3b4fd9d51213bb3637d616fbb9867f6d SHA512 fdcbc48ba17b2155c1419d99147a4cfbee2e69db945bfd0e6881b71ab11165bd23ea7ce9456856ae36807fb18f9934880a6b7c44456b63833ea260038744d9f2
 DIST systemtap-5.0.tar.gz 6572411 BLAKE2B db962df2fdcbc5bc11259c03d78073924f03fee4434dca8686a62fcb0818dab1ca91558160dd9d67d43b709f63ffe4f0af30ab1fe6965e19155bdd4c04501a04 SHA512 dc511a05e66abcbbd8c926973962751196180f3c571d0cd2a3b158ae367c5339ad32967a680ecd03224ab5f7ed2c55be7064867e4fb1b1cd7ea1cb21b2436e4c
+DIST systemtap-5.1.tar.gz 6590820 BLAKE2B 22be535b7c55d0ab90a5361a23fad93c436560c14fbbd345aca7ea2c0fcdd0f66b1ef8591800f86a14cc7ae7fe737a120695d76fe15bfdb8b349080228c37e90 SHA512 da0fe237d2124031a5786d1221dbb420d90da5497376715fd43a7a9f61a354a229c1128e67ce6becbc012aa3796dc5d337149e239e3c1def0651b179e5bf199f

dev-debug/systemtap/metadata.xml
@@ -5,6 +5,7 @@
   <email>swegener@gentoo.org</email>
 </maintainer>
   <use>
+    <flag name="debuginfod">Enable debuginfod support via <pkg>dev-libs/elfutils</pkg> libdebuginfod</flag>
     <flag name="libvirt">Support probing of libvirt domains.</flag>
   </use>
 </pkgmetadata>

commit 629ef453e0f9f68567c807416557e2d3c7b6780f

dev-debug/systemtap: add comment wrt bash
I forgot to add this earlier. Turns out there's a bug for it too.

Fixes: 818cec7cd4efae42c55af03f4c0617a95d069c14
Bug: https://bugs.gentoo.org/913947
Signed-off-by: Sam James <sam@gentoo.org>

dev-debug/systemtap/systemtap-5.1.ebuild
@@ -127,6 +127,8 @@ src_configure() {
 		$(use_with ssl nss)
 		$(use_with selinux)
 	)
+
+	# Use bash because of bashisms with brace expansion in Makefile.am (bug #913947)
 	CONFIG_SHELL="${BROOT}"/bin/bash PYTHON3="${PYTHON}" econf "${myeconfargs[@]}"
 }
 

dev-debug/systemtap/systemtap-9999.ebuild
@@ -127,6 +127,8 @@ src_configure() {
 		$(use_with ssl nss)
 		$(use_with selinux)
 	)
+
+	# Use bash because of bashisms with brace expansion in Makefile.am (bug #913947)
 	CONFIG_SHELL="${BROOT}"/bin/bash PYTHON3="${PYTHON}" econf "${myeconfargs[@]}"
 }
 