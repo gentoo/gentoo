gentoo/gentoo

commit 10ee7afe94f44465509290da003e94391e89bd2b

net-misc/passt: add 2024.08.21
Signed-off-by: Michal Privoznik <michal.privoznik@gmail.com>
Closes: https://github.com/gentoo/gentoo/pull/38342
Signed-off-by: Zac Medico <zmedico@gentoo.org>

net-misc/passt/Manifest
@@ -3,3 +3,4 @@ DIST passt-2024.04.05.tar.xz 195772 BLAKE2B 4c237aeda56455f948d4309624d171612d59
 DIST passt-2024.04.26.tar.xz 196148 BLAKE2B f3a9b9d2a9fa42e0dd35894d4c92890af9935965ccfb3b1751c396080ab0482bd541fe22f09ae7b068f336f1e985b6905d2cc93987a17f243ba6ec97d880ee68 SHA512 f25b9bc695de555048f6a24ec9bb603ec6def0449227ce837b6e46f8cda5816c2450aa733257aaa349260b0b7c05b2d5580ad21010a1a45a74e9eb3027e37d98
 DIST passt-2024.05.10.tar.xz 195992 BLAKE2B 9f4b680daa8103c7de0c92c01824df74e1b221a48cf7724c2c09aae13a3575bdc529eeca01d5015095c51194045e1019bbdf38cc7ef2333d61eaa03b4e0249ae SHA512 04add1113ffbc98d61b7c63352899505d8f19bbdf937a0c025726813163ec7fc31abdb94c12ceefed9b49f18b388acc0687298bce8ba156bbb044d1b1bcdaee3
 DIST passt-2024.06.07.tar.xz 198360 BLAKE2B 378d4b18f245309fde54b244ffc435afb417d9d9711247234632ba0ae8dea35cac639c7674e6a6be008a678262af74a1dbe960477088d0b75045e6372d329488 SHA512 103955058c9a4e83264d3f65e3ff6d1a5e8a83b15f2fc73cba45101304fe79ab3c449ce2dc6add398a27f9d6556d5f902cf083223ed5e27228a0bbb506559cf0
+DIST passt-2024.08.21.tar.xz 198360 BLAKE2B 378d4b18f245309fde54b244ffc435afb417d9d9711247234632ba0ae8dea35cac639c7674e6a6be008a678262af74a1dbe960477088d0b75045e6372d329488 SHA512 103955058c9a4e83264d3f65e3ff6d1a5e8a83b15f2fc73cba45101304fe79ab3c449ce2dc6add398a27f9d6556d5f902cf083223ed5e27228a0bbb506559cf0

net-misc/passt/passt-2024.08.21.ebuild
@@ -1,0 +1,36 @@
+# Copyright 1999-2024 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit toolchain-funcs
+
+DESCRIPTION="User-mode networking daemons for VMs and namespaces, replacement for Slirp"
+HOMEPAGE="https://passt.top/"
+
+RELEASE_COMMIT="8a83b53"
+
+if [[ ${PV} == 9999* ]]; then
+	inherit git-r3
+	EGIT_REPO_URI="https://passt.top/passt"
+else
+	SRC_URI="https://passt.top/passt/snapshot/passt-${RELEASE_COMMIT}.tar.xz -> ${P}.tar.xz"
+	S="${WORKDIR}/${PN}-${RELEASE_COMMIT}"
+	KEYWORDS="~amd64 ~arm64 ~loong ~riscv"
+fi
+
+LICENSE="BSD GPL-2+"
+SLOT="0"
+IUSE="static"
+
+src_prepare() {
+	default
+	tc-export CC
+}
+
+src_compile() {
+	[[ ${PV} != 9999* ]] && export VERSION="${PV}"
+	export prefix="${EPREFIX}/usr" docdir="${EPREFIX}/usr/share/doc/${P}"
+
+	emake $(usev static)
+}

commit df9764779da1097f2715c56bc281340702b9ebf3

net-misc/passt: Actually build 2024.08.21 instead of 2024.06.07
Closes: https://bugs.gentoo.org/938882

Signed-off-by: Sasha Finkelstein <fnkl.kernel@gmail.com>
Fixes: 10ee7afe94f4 ("net-misc/passt: add 2024.08.21")
Closes: https://github.com/gentoo/gentoo/pull/38366
Signed-off-by: Zac Medico <zmedico@gentoo.org>

net-misc/passt/Manifest
@@ -3,4 +3,4 @@ DIST passt-2024.04.05.tar.xz 195772 BLAKE2B 4c237aeda56455f948d4309624d171612d59
 DIST passt-2024.04.26.tar.xz 196148 BLAKE2B f3a9b9d2a9fa42e0dd35894d4c92890af9935965ccfb3b1751c396080ab0482bd541fe22f09ae7b068f336f1e985b6905d2cc93987a17f243ba6ec97d880ee68 SHA512 f25b9bc695de555048f6a24ec9bb603ec6def0449227ce837b6e46f8cda5816c2450aa733257aaa349260b0b7c05b2d5580ad21010a1a45a74e9eb3027e37d98
 DIST passt-2024.05.10.tar.xz 195992 BLAKE2B 9f4b680daa8103c7de0c92c01824df74e1b221a48cf7724c2c09aae13a3575bdc529eeca01d5015095c51194045e1019bbdf38cc7ef2333d61eaa03b4e0249ae SHA512 04add1113ffbc98d61b7c63352899505d8f19bbdf937a0c025726813163ec7fc31abdb94c12ceefed9b49f18b388acc0687298bce8ba156bbb044d1b1bcdaee3
 DIST passt-2024.06.07.tar.xz 198360 BLAKE2B 378d4b18f245309fde54b244ffc435afb417d9d9711247234632ba0ae8dea35cac639c7674e6a6be008a678262af74a1dbe960477088d0b75045e6372d329488 SHA512 103955058c9a4e83264d3f65e3ff6d1a5e8a83b15f2fc73cba45101304fe79ab3c449ce2dc6add398a27f9d6556d5f902cf083223ed5e27228a0bbb506559cf0
-DIST passt-2024.08.21.tar.xz 198360 BLAKE2B 378d4b18f245309fde54b244ffc435afb417d9d9711247234632ba0ae8dea35cac639c7674e6a6be008a678262af74a1dbe960477088d0b75045e6372d329488 SHA512 103955058c9a4e83264d3f65e3ff6d1a5e8a83b15f2fc73cba45101304fe79ab3c449ce2dc6add398a27f9d6556d5f902cf083223ed5e27228a0bbb506559cf0
+DIST passt-2024.08.21-r1.tar.xz 209692 BLAKE2B 1b6b540f64528e375e6f0cfd90cec788b2b2281f7ab6dbe0f6df933757dcc4f37e154b2cd8e9e00adbc2b647a044fca05f2c48f49d05c0443c396831a2e13754 SHA512 ec09210b471377cc9f3d0044c97924834fadcfed7523aaab2f6c0fddb2d3d098c0730d1573e7c44963334d33dba5fbf660b0b222d4c71fab27cebf8edb52abf1

net-misc/passt/passt-2024.08.21-r1.ebuild
@@ -8,14 +8,15 @@ inherit toolchain-funcs
 DESCRIPTION="User-mode networking daemons for VMs and namespaces, replacement for Slirp"
 HOMEPAGE="https://passt.top/"
 
-RELEASE_COMMIT="8a83b53"
+RELEASE_COMMIT="1d6142f"
+MY_PV="${PV//./_}.${RELEASE_COMMIT}"
 
 if [[ ${PV} == 9999* ]]; then
 	inherit git-r3
 	EGIT_REPO_URI="https://passt.top/passt"
 else
-	SRC_URI="https://passt.top/passt/snapshot/passt-${RELEASE_COMMIT}.tar.xz -> ${P}.tar.xz"
-	S="${WORKDIR}/${PN}-${RELEASE_COMMIT}"
+	SRC_URI="https://passt.top/passt/snapshot/passt-${MY_PV}.tar.xz -> ${PF}.tar.xz"
+	S="${WORKDIR}/${PN}-${MY_PV}"
 	KEYWORDS="~amd64 ~arm64 ~loong ~riscv"
 fi
 
@@ -30,7 +31,7 @@ src_prepare() {
 
 src_compile() {
 	[[ ${PV} != 9999* ]] && export VERSION="${PV}"
-	export prefix="${EPREFIX}/usr" docdir="${EPREFIX}/usr/share/doc/${P}"
+	export prefix="${EPREFIX}/usr" docdir="${EPREFIX}/usr/share/doc/${PF}"
 
 	emake $(usev static)
 }