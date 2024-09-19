gentoo/gentoo

commit f7909a793ea72d41af9fdad6e9f2db77c51bd63a

dev-java/openjdk: fix handle of systemtap without dtrace-symlink
openjdk accept DTRACE from command line instead of env, see jdk source line
297 of make/autoconf/util_paths.m4, otherwise build will failed:

 configure: WARNING: Ignoring value of DTRACE from the environment. Use command line variables instead.

Bug: https://bugs.gentoo.org/938302
Signed-off-by: Z. Liu <zhixu.liu@gmail.com>
Closes: https://github.com/gentoo/gentoo/pull/38678
Signed-off-by: Sam James <sam@gentoo.org>

dev-java/openjdk/openjdk-17.0.12_p7.ebuild
@@ -177,11 +177,6 @@ src_configure() {
 		export JDK_HOME
 	fi
 
-	# Workaround for bug #938302
-	if use systemtap && has_version "dev-debug/systemtap[-dtrace-symlink(+)]" ; then
-		export DTRACE="${BROOT}"/usr/bin/stap-dtrace
-	fi
-
 	# Work around stack alignment issue, bug #647954. in case we ever have x86
 	use x86 && append-flags -mincoming-stack-boundary=2
 
@@ -240,6 +235,11 @@ src_configure() {
 		else
 			die "${zip} not found or not readable"
 		fi
+	fi
+
+	# Workaround for bug #938302
+	if use systemtap && has_version "dev-debug/systemtap[-dtrace-symlink(+)]" ; then
+		myconf+=( TRACE="${BROOT}"/usr/bin/stap-dtrace )
 	fi
 
 	if use !system-bootstrap ; then

dev-java/openjdk/openjdk-11.0.24_p8.ebuild
@@ -163,11 +163,6 @@ src_configure() {
 		export JDK_HOME="${WORKDIR}/openjdk-bootstrap-${!xpakvar}"
 	fi
 
-	# Workaround for bug #938302
-	if use systemtap && has_version "dev-debug/systemtap[-dtrace-symlink(+)]" ; then
-		export DTRACE="${BROOT}"/usr/bin/stap-dtrace
-	fi
-
 	# Work around stack alignment issue, bug #647954.
 	use x86 && append-flags -mincoming-stack-boundary=2
 
@@ -231,6 +226,11 @@ src_configure() {
 		else
 			die "${zip} not found or not readable"
 		fi
+	fi
+
+	# Workaround for bug #938302
+	if use systemtap && has_version "dev-debug/systemtap[-dtrace-symlink(+)]" ; then
+		myconf+=( DTRACE="${BROOT}"/usr/bin/stap-dtrace )
 	fi
 
 	if use !system-bootstrap ; then

dev-java/openjdk/openjdk-21.0.4_p7.ebuild
@@ -175,11 +175,6 @@ src_configure() {
 		export JDK_HOME
 	fi
 
-	# Workaround for bug #938302
-	if use systemtap && has_version "dev-debug/systemtap[-dtrace-symlink(+)]" ; then
-		export DTRACE="${BROOT}"/usr/bin/stap-dtrace
-	fi
-
 	# Work around stack alignment issue, bug #647954. in case we ever have x86
 	use x86 && append-flags -mincoming-stack-boundary=2
 
@@ -244,6 +239,11 @@ src_configure() {
 		else
 			die "${zip} not found or not readable"
 		fi
+	fi
+
+	# Workaround for bug #938302
+	if use systemtap && has_version "dev-debug/systemtap[-dtrace-symlink(+)]" ; then
+		myconf+=( DTRACE="${BROOT}"/usr/bin/stap-dtrace )
 	fi
 
 	if use !system-bootstrap ; then

commit 8bd568b8453e1b87accf04763e982d2f0bf084fc

dev-java/openjdk: fix DTRACE typo in 17
Fixes: f7909a793ea72d41af9fdad6e9f2db77c51bd63a
Signed-off-by: Sam James <sam@gentoo.org>

dev-java/openjdk/openjdk-17.0.12_p7.ebuild
@@ -239,7 +239,7 @@ src_configure() {
 
 	# Workaround for bug #938302
 	if use systemtap && has_version "dev-debug/systemtap[-dtrace-symlink(+)]" ; then
-		myconf+=( TRACE="${BROOT}"/usr/bin/stap-dtrace )
+		myconf+=( DTRACE="${BROOT}"/usr/bin/stap-dtrace )
 	fi
 
 	if use !system-bootstrap ; then