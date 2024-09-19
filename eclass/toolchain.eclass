gentoo/gentoo

commit b6bf005b843e3d6ee10aa1f088d93c4f89055cc6

toolchain.eclass: Selectively enable cet options per arch
Bug: https://bugs.gentoo.org/916381
Signed-off-by: Andreas K. Hüttel <dilfridge@gentoo.org>

eclass/toolchain.eclass
@@ -1240,10 +1240,8 @@ toolchain_src_configure() {
 	fi
 
 	if in_iuse cet ; then
-		confgcc+=(
-			$(use_enable cet)
-			$(use_enable cet standard-branch-protection)
-		)
+		[[ ${CTARGET} == x86_64-*-gnu* ]] && confgcc+=( $(use_enable cet) )
+		[[ ${CTARGET} == aarch64-*-gnu* ]] && confgcc+=( $(use_enable cet standard-branch-protection) )
 	fi
 
 	if in_iuse systemtap ; then

commit d56d8f8e35fa4f7b8d65dbd6cd535ad6ac69cc9c

toolchain.eclass: disable automagic CET for x86
It's not supported on 32-bit kernels anyway.

This got lost in b6bf005b843e3d6ee10aa1f088d93c4f89055cc6 when wiring
up arm64.

Bug: https://bugs.gentoo.org/916381
Closes: https://bugs.gentoo.org/939874
Fixes: b6bf005b843e3d6ee10aa1f088d93c4f89055cc6
Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -1310,7 +1310,9 @@ toolchain_src_configure() {
 	fi
 
 	if in_iuse cet ; then
-		[[ ${CTARGET} == x86_64-*-gnu* ]] && confgcc+=( $(use_enable cet) )
+		if [[ ${CTARGET} == i[[34567]]86-*-linux* || ${CTARGET} == x86_64-*-gnu* ]] ; then
+			confgcc+=( $(use_enable cet) )
+		fi
 		[[ ${CTARGET} == aarch64-*-gnu* ]] && confgcc+=( $(use_enable cet standard-branch-protection) )
 	fi
 