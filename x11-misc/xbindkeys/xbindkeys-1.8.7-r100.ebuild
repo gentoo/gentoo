gentoo/gentoo

commit fecf67e319addb4a820f79914c55be4b592e7c5d

x11-misc/xbindkeys: switch to new guile mechanism
Signed-off-by: Alfred Wingate <parona@protonmail.com>
Signed-off-by: Arsen Arsenović <arsen@gentoo.org>

x11-misc/xbindkeys/xbindkeys-1.8.7-r100.ebuild
@@ -1,0 +1,53 @@
+# Copyright 1999-2024 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+GUILE_REQ_USE="deprecated"
+GUILE_COMPAT=( 2-2 3-0 )
+inherit autotools guile-single
+
+DESCRIPTION="Tool for launching commands on keystrokes"
+SRC_URI="https://www.nongnu.org/${PN}/${P}.tar.gz"
+HOMEPAGE="https://www.nongnu.org/xbindkeys/xbindkeys.html"
+
+LICENSE="GPL-2"
+SLOT="0"
+KEYWORDS="~amd64 ~arm ~ppc ~ppc64 ~riscv ~sparc ~x86 ~amd64-linux ~x86-linux ~ppc-macos"
+IUSE="guile tk"
+
+REQUIRED_USE="guile? ( ${GUILE_REQUIRED_USE} )"
+
+RDEPEND="
+	x11-libs/libX11
+	guile? ( ${GUILE_DEPS} )
+	tk? ( dev-lang/tk )
+"
+DEPEND="
+	${RDEPEND}
+	x11-base/xorg-proto
+"
+
+DOCS=( AUTHORS BUGS ChangeLog README TODO xbindkeysrc )
+
+src_prepare() {
+	default
+
+	use guile && guile_bump_sources
+
+	# Regenerate to pick up newer versions of Guile macros
+	# bug #828532
+	eautoreconf
+}
+
+src_configure() {
+	econf \
+		$(use_enable guile) \
+		$(use_enable tk)
+}
+
+src_install() {
+	default
+
+	use guile && guile_unstrip_ccache
+}

profiles/package.mask
@@ -395,6 +395,7 @@ dev-scheme/guile:3.0
 >=sci-libs/nlopt-2.7.1-r100
 >=sys-devel/autogen-5.18.16-r100
 >=www-client/elinks-0.16.1.1-r100
+>=x11-misc/xbindkeys-1.8.7-r100
 
 # Eli Schwartz <eschwartz@gentoo.org> (2024-08-15)
 # Abandoned upstream since 2011, unmaintained in gentoo since 2017. Tons of

commit 20c679298cd29c57dba3dce93339b0a1780284e5

x11-misc/xbindkeys: fix pkg_setup w/ USE=-guile
Closes: https://bugs.gentoo.org/940441
Fixes: fecf67e319addb4a820f79914c55be4b592e7c5d
Signed-off-by: Sam James <sam@gentoo.org>

x11-misc/xbindkeys/xbindkeys-1.8.7-r100.ebuild
@@ -30,6 +30,10 @@ DEPEND="
 
 DOCS=( AUTHORS BUGS ChangeLog README TODO xbindkeysrc )
 
+pkg_setup() {
+	use guile && guile-single_pkg_setup
+}
+
 src_prepare() {
 	default
 