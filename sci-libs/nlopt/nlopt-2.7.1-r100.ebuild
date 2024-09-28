gentoo/gentoo

commit 01f1676c357e17cfde5abb0ccca96766e69bbe6f

sci-libs/nlopt: switch to new guile mechanism
Signed-off-by: Alfred Wingate <parona@protonmail.com>
Signed-off-by: Arsen Arsenović <arsen@gentoo.org>

sci-libs/nlopt/nlopt-2.7.1-r100.ebuild
@@ -1,0 +1,115 @@
+# Copyright 1999-2024 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+GUILE_COMPAT=( 2-2 3-0 )
+PYTHON_COMPAT=( python3_{10..12} )
+FORTRAN_NEEDED="test"
+
+inherit python-r1 cmake guile-single fortran-2
+
+DESCRIPTION="Non-linear optimization library"
+HOMEPAGE="https://github.com/stevengj/nlopt"
+SRC_URI="https://github.com/stevengj/nlopt/archive/v${PV}.tar.gz -> ${P}.tar.gz"
+
+LICENSE="LGPL-2.1 MIT"
+KEYWORDS="~amd64 ~arm64 ~ppc ~ppc64 ~riscv ~x86 ~amd64-linux ~x86-linux"
+SLOT="0"
+IUSE="cxx guile octave python test"
+REQUIRED_USE="guile? ( ${GUILE_REQUIRED_USE} ) python? ( ${PYTHON_REQUIRED_USE} )"
+RESTRICT="!test? ( test )"
+
+RDEPEND="
+	guile? ( ${GUILE_DEPS} )
+	octave? ( >=sci-mathematics/octave-6:= )
+	python? (
+		${PYTHON_DEPS}
+		dev-python/numpy[${PYTHON_USEDEP}]
+	)
+	"
+DEPEND="${RDEPEND}"
+BDEPEND="python? ( dev-lang/swig )"
+
+src_prepare() {
+	cmake_src_prepare
+
+	use guile && guile_bump_sources
+	use python && python_copy_sources
+}
+
+src_configure() {
+	# MATLAB detection causes problems (as in bug 826774) if we don't
+	# explicitly disable it.
+	local mycmakeargs=(
+		-DNLOPT_CXX=$(usex cxx)
+		-DNLOPT_FORTRAN=$(usex test)
+		-DNLOPT_GUILE=$(usex guile)
+		-DNLOPT_OCTAVE=$(usex octave)
+		-DNLOPT_MATLAB=OFF
+		-DNLOPT_PYTHON=OFF
+		-DNLOPT_SWIG=$(usex python)
+		-DNLOPT_TESTS=$(usex test)
+	)
+
+	cmake_src_configure
+
+	if use python; then
+		python_configure() {
+			local mycmakeargs=(
+				${mycmakeargs[@]}
+				-DNLOPT_PYTHON=ON
+				-DINSTALL_PYTHON_DIR="$(python_get_sitedir)"
+			)
+
+			cmake_src_configure
+		}
+
+		python_foreach_impl run_in_build_dir python_configure
+	fi
+}
+
+src_compile() {
+	cmake_src_compile
+
+	if use python; then
+		python_foreach_impl run_in_build_dir cmake_src_compile
+	fi
+}
+
+src_test() {
+	do_test() {
+		local a f
+		cd "${BUILD_DIR}"/test
+		for a in {1..$(usex cxx 9 7)}; do
+			for f in {5..9}; do
+				./testopt -a $a -o $f || die "algorithm $a function $f failed"
+			done
+		done
+	}
+
+	do_test
+
+	if use python; then
+		python_foreach_impl run_in_build_dir do_test
+	fi
+}
+
+nlopt_install() {
+	cmake_src_install
+	python_optimize
+}
+
+src_install() {
+	cmake_src_install
+
+	guile_unstrip_ccache
+	if use python; then
+		python_foreach_impl run_in_build_dir nlopt_install
+	fi
+
+	local r
+	for r in */README; do
+		newdoc ${r} README.$(dirname ${r})
+	done
+}

profiles/package.mask
@@ -392,6 +392,7 @@ dev-scheme/guile:3.0
 >=sci-electronics/geda-1.10.2-r100
 >=sci-electronics/gwave-20190116-r100
 >=sci-libs/linux-gpib-4.3.6-r100
+>=sci-libs/nlopt-2.7.1-r100
 
 # Eli Schwartz <eschwartz@gentoo.org> (2024-08-15)
 # Abandoned upstream since 2011, unmaintained in gentoo since 2017. Tons of

commit bf702b6cee0133e094b50378336326b6bff4926b

sci-libs/nlopt: fix pkg_setup w/ USE=-guile
Like in 20c679298cd29c57dba3dce93339b0a1780284e5. This seems to be
the only other such case in-tree.

Note that fortran-2_pkg_setup is fine to call unconditionally
because it checks itself if it should be used if FORTRAN_OPTIONAL is set.

Bug: https://bugs.gentoo.org/940441
Fixes: 01f1676c357e17cfde5abb0ccca96766e69bbe6f
Signed-off-by: Sam James <sam@gentoo.org>

sci-libs/nlopt/nlopt-2.7.1-r100.ebuild
@@ -31,6 +31,11 @@ RDEPEND="
 DEPEND="${RDEPEND}"
 BDEPEND="python? ( dev-lang/swig )"
 
+pkg_setup() {
+	use guile && guile-single_pkg_setup
+	fortran-2_pkg_setup
+}
+
 src_prepare() {
 	cmake_src_prepare
 