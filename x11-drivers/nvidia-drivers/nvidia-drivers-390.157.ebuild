gentoo/gentoo

commit dacc7d5a54fa46a732fd358a0a771eb06b6aeeb0

x11-drivers/nvidia-drivers: include nvidia.conf in dracut always
Dracut ignores /etc/modprobe.d if hostonly=no, this is a problem because if
plymouth is installed then the drm module will be pulled in which will include
the nouveau module. The presence of this module, combined with the lack of
modprobe.d config file blacklisting it, results in the nouveau module being
loaded in the initramfs stage on systems that have plymouth installed and are
not using dracut's hostonly setting.

This needs a proper upstream fix, but for now we can workaround the problem
with this.

See-also: https://github.com/dracut-ng/dracut-ng/issues/674
Signed-off-by: Andrew Ammerlaan <andrewammerlaan@gentoo.org>
Closes: https://github.com/gentoo/gentoo/pull/38628
Signed-off-by: Andrew Ammerlaan <andrewammerlaan@gentoo.org>

x11-drivers/nvidia-drivers/nvidia-drivers-390.157.ebuild
@@ -400,6 +400,13 @@ documentation that is installed alongside this README."
 	# manually if need others or addwrite)
 	insinto /etc/sandbox.d
 	newins - 20nvidia <<<'SANDBOX_PREDICT="/dev/nvidiactl"'
+
+	# Dracut does not include /etc/modprobe.d if hostonly=no, but we do need this
+	# to ensure that the nouveau blacklist is applied
+	# https://github.com/dracut-ng/dracut-ng/issues/674
+	# https://bugs.gentoo.org/932781
+	echo "install_items+=\" ${EPREFIX}/etc/modprobe.d/nvidia.conf \"" >> \
+		"${ED}/usr/lib/dracut/dracut.conf.d/10-${PN}.conf" || die
 }
 
 pkg_preinst() {

x11-drivers/nvidia-drivers/nvidia-drivers-470.256.02-r1.ebuild
@@ -387,6 +387,13 @@ documentation that is installed alongside this README."
 	# ebuilds should handle manually if need others or addwrite)
 	insinto /etc/sandbox.d
 	newins - 20nvidia <<<'SANDBOX_PREDICT="/dev/nvidiactl:/dev/char"'
+
+	# Dracut does not include /etc/modprobe.d if hostonly=no, but we do need this
+	# to ensure that the nouveau blacklist is applied
+	# https://github.com/dracut-ng/dracut-ng/issues/674
+	# https://bugs.gentoo.org/932781
+	echo "install_items+=\" ${EPREFIX}/etc/modprobe.d/nvidia.conf \"" >> \
+		"${ED}/usr/lib/dracut/dracut.conf.d/10-${PN}.conf" || die
 }
 
 pkg_preinst() {

x11-drivers/nvidia-drivers/nvidia-drivers-525.147.05-r1.ebuild
@@ -451,6 +451,13 @@ documentation that is installed alongside this README."
 	# ebuilds should handle manually if need others or addwrite)
 	insinto /etc/sandbox.d
 	newins - 20nvidia <<<'SANDBOX_PREDICT="/dev/nvidiactl:/dev/char"'
+
+	# Dracut does not include /etc/modprobe.d if hostonly=no, but we do need this
+	# to ensure that the nouveau blacklist is applied
+	# https://github.com/dracut-ng/dracut-ng/issues/674
+	# https://bugs.gentoo.org/932781
+	echo "install_items+=\" ${EPREFIX}/etc/modprobe.d/nvidia.conf \"" >> \
+		"${ED}/usr/lib/dracut/dracut.conf.d/10-${PN}.conf" || die
 }
 
 pkg_preinst() {

x11-drivers/nvidia-drivers/nvidia-drivers-535.183.01-r1.ebuild
@@ -451,6 +451,13 @@ documentation that is installed alongside this README."
 	# ebuilds should handle manually if need others or addwrite)
 	insinto /etc/sandbox.d
 	newins - 20nvidia <<<'SANDBOX_PREDICT="/dev/nvidiactl:/dev/char"'
+
+	# Dracut does not include /etc/modprobe.d if hostonly=no, but we do need this
+	# to ensure that the nouveau blacklist is applied
+	# https://github.com/dracut-ng/dracut-ng/issues/674
+	# https://bugs.gentoo.org/932781
+	echo "install_items+=\" ${EPREFIX}/etc/modprobe.d/nvidia.conf \"" >> \
+		"${ED}/usr/lib/dracut/dracut.conf.d/10-${PN}.conf" || die
 }
 
 pkg_preinst() {

x11-drivers/nvidia-drivers/nvidia-drivers-550.107.02-r1.ebuild
@@ -429,6 +429,13 @@ documentation that is installed alongside this README."
 	# ebuilds should handle manually if need others or addwrite)
 	insinto /etc/sandbox.d
 	newins - 20nvidia <<<'SANDBOX_PREDICT="/dev/nvidiactl:/dev/nvidia-caps:/dev/char"'
+
+	# Dracut does not include /etc/modprobe.d if hostonly=no, but we do need this
+	# to ensure that the nouveau blacklist is applied
+	# https://github.com/dracut-ng/dracut-ng/issues/674
+	# https://bugs.gentoo.org/932781
+	echo "install_items+=\" ${EPREFIX}/etc/modprobe.d/nvidia.conf \"" >> \
+		"${ED}/usr/lib/dracut/dracut.conf.d/10-${PN}.conf" || die
 }
 
 pkg_preinst() {

x11-drivers/nvidia-drivers/nvidia-drivers-560.35.03-r1.ebuild
@@ -434,6 +434,13 @@ documentation that is installed alongside this README."
 	# ebuilds should handle manually if need others or addwrite)
 	insinto /etc/sandbox.d
 	newins - 20nvidia <<<'SANDBOX_PREDICT="/dev/nvidiactl:/dev/nvidia-caps:/dev/char"'
+
+	# Dracut does not include /etc/modprobe.d if hostonly=no, but we do need this
+	# to ensure that the nouveau blacklist is applied
+	# https://github.com/dracut-ng/dracut-ng/issues/674
+	# https://bugs.gentoo.org/932781
+	echo "install_items+=\" ${EPREFIX}/etc/modprobe.d/nvidia.conf \"" >> \
+		"${ED}/usr/lib/dracut/dracut.conf.d/10-${PN}.conf" || die
 }
 
 pkg_preinst() {

x11-drivers/nvidia-drivers/nvidia-drivers-550.40.71-r1.ebuild
@@ -431,6 +431,13 @@ documentation that is installed alongside this README."
 	# ebuilds should handle manually if need others or addwrite)
 	insinto /etc/sandbox.d
 	newins - 20nvidia <<<'SANDBOX_PREDICT="/dev/nvidiactl:/dev/nvidia-caps:/dev/char"'
+
+	# Dracut does not include /etc/modprobe.d if hostonly=no, but we do need this
+	# to ensure that the nouveau blacklist is applied
+	# https://github.com/dracut-ng/dracut-ng/issues/674
+	# https://bugs.gentoo.org/932781
+	echo "install_items+=\" ${EPREFIX}/etc/modprobe.d/nvidia.conf \"" >> \
+		"${ED}/usr/lib/dracut/dracut.conf.d/10-${PN}.conf" || die
 }
 
 pkg_preinst() {

commit aefba1ed1e6cd2be0caa41abb600b0a492499424

x11-drivers/nvidia-drivers: fix build with USE=-modules
At same time update/shorten comment a bit, esp. to mention that it may
not be included without initramfs regen and this is more of a best
effort solution (thankfully *most* users would include neither modules
on an initramfs and it wouldn't matter).

Fixes: dacc7d5a54fa46a732fd358a0a771eb06b6aeeb0
Closes: https://bugs.gentoo.org/940564
Signed-off-by: Ionen Wolkens <ionen@gentoo.org>

x11-drivers/nvidia-drivers/nvidia-drivers-390.157.ebuild
@@ -401,12 +401,13 @@ documentation that is installed alongside this README."
 	insinto /etc/sandbox.d
 	newins - 20nvidia <<<'SANDBOX_PREDICT="/dev/nvidiactl"'
 
-	# Dracut does not include /etc/modprobe.d if hostonly=no, but we do need this
-	# to ensure that the nouveau blacklist is applied
-	# https://github.com/dracut-ng/dracut-ng/issues/674
-	# https://bugs.gentoo.org/932781
-	echo "install_items+=\" ${EPREFIX}/etc/modprobe.d/nvidia.conf \"" >> \
-		"${ED}/usr/lib/dracut/dracut.conf.d/10-${PN}.conf" || die
+	# dracut does not use /etc/modprobe.d if hostonly=no, but want to make sure
+	# our settings are used for bug 932781#c8 and nouveau blacklist if either
+	# modules are included (however, just best-effort without initramfs regen)
+	if use modules; then
+		echo "install_items+=\" ${EPREFIX}/etc/modprobe.d/nvidia.conf \"" >> \
+			"${ED}"/usr/lib/dracut/dracut.conf.d/10-${PN}.conf || die
+	fi
 }
 
 pkg_preinst() {

x11-drivers/nvidia-drivers/nvidia-drivers-470.256.02-r1.ebuild
@@ -388,12 +388,13 @@ documentation that is installed alongside this README."
 	insinto /etc/sandbox.d
 	newins - 20nvidia <<<'SANDBOX_PREDICT="/dev/nvidiactl:/dev/char"'
 
-	# Dracut does not include /etc/modprobe.d if hostonly=no, but we do need this
-	# to ensure that the nouveau blacklist is applied
-	# https://github.com/dracut-ng/dracut-ng/issues/674
-	# https://bugs.gentoo.org/932781
-	echo "install_items+=\" ${EPREFIX}/etc/modprobe.d/nvidia.conf \"" >> \
-		"${ED}/usr/lib/dracut/dracut.conf.d/10-${PN}.conf" || die
+	# dracut does not use /etc/modprobe.d if hostonly=no, but want to make sure
+	# our settings are used for bug 932781#c8 and nouveau blacklist if either
+	# modules are included (however, just best-effort without initramfs regen)
+	if use modules; then
+		echo "install_items+=\" ${EPREFIX}/etc/modprobe.d/nvidia.conf \"" >> \
+			"${ED}"/usr/lib/dracut/dracut.conf.d/10-${PN}.conf || die
+	fi
 }
 
 pkg_preinst() {

x11-drivers/nvidia-drivers/nvidia-drivers-525.147.05-r1.ebuild
@@ -452,12 +452,13 @@ documentation that is installed alongside this README."
 	insinto /etc/sandbox.d
 	newins - 20nvidia <<<'SANDBOX_PREDICT="/dev/nvidiactl:/dev/char"'
 
-	# Dracut does not include /etc/modprobe.d if hostonly=no, but we do need this
-	# to ensure that the nouveau blacklist is applied
-	# https://github.com/dracut-ng/dracut-ng/issues/674
-	# https://bugs.gentoo.org/932781
-	echo "install_items+=\" ${EPREFIX}/etc/modprobe.d/nvidia.conf \"" >> \
-		"${ED}/usr/lib/dracut/dracut.conf.d/10-${PN}.conf" || die
+	# dracut does not use /etc/modprobe.d if hostonly=no, but want to make sure
+	# our settings are used for bug 932781#c8 and nouveau blacklist if either
+	# modules are included (however, just best-effort without initramfs regen)
+	if use modules; then
+		echo "install_items+=\" ${EPREFIX}/etc/modprobe.d/nvidia.conf \"" >> \
+			"${ED}"/usr/lib/dracut/dracut.conf.d/10-${PN}.conf || die
+	fi
 }
 
 pkg_preinst() {

x11-drivers/nvidia-drivers/nvidia-drivers-535.183.01-r1.ebuild
@@ -452,12 +452,13 @@ documentation that is installed alongside this README."
 	insinto /etc/sandbox.d
 	newins - 20nvidia <<<'SANDBOX_PREDICT="/dev/nvidiactl:/dev/char"'
 
-	# Dracut does not include /etc/modprobe.d if hostonly=no, but we do need this
-	# to ensure that the nouveau blacklist is applied
-	# https://github.com/dracut-ng/dracut-ng/issues/674
-	# https://bugs.gentoo.org/932781
-	echo "install_items+=\" ${EPREFIX}/etc/modprobe.d/nvidia.conf \"" >> \
-		"${ED}/usr/lib/dracut/dracut.conf.d/10-${PN}.conf" || die
+	# dracut does not use /etc/modprobe.d if hostonly=no, but want to make sure
+	# our settings are used for bug 932781#c8 and nouveau blacklist if either
+	# modules are included (however, just best-effort without initramfs regen)
+	if use modules; then
+		echo "install_items+=\" ${EPREFIX}/etc/modprobe.d/nvidia.conf \"" >> \
+			"${ED}"/usr/lib/dracut/dracut.conf.d/10-${PN}.conf || die
+	fi
 }
 
 pkg_preinst() {

x11-drivers/nvidia-drivers/nvidia-drivers-550.107.02-r1.ebuild
@@ -443,12 +443,13 @@ documentation that is installed alongside this README."
 	insinto /etc/sandbox.d
 	newins - 20nvidia <<<'SANDBOX_PREDICT="/dev/nvidiactl:/dev/nvidia-caps:/dev/char"'
 
-	# Dracut does not include /etc/modprobe.d if hostonly=no, but we do need this
-	# to ensure that the nouveau blacklist is applied
-	# https://github.com/dracut-ng/dracut-ng/issues/674
-	# https://bugs.gentoo.org/932781
-	echo "install_items+=\" ${EPREFIX}/etc/modprobe.d/nvidia.conf \"" >> \
-		"${ED}/usr/lib/dracut/dracut.conf.d/10-${PN}.conf" || die
+	# dracut does not use /etc/modprobe.d if hostonly=no, but want to make sure
+	# our settings are used for bug 932781#c8 and nouveau blacklist if either
+	# modules are included (however, just best-effort without initramfs regen)
+	if use modules; then
+		echo "install_items+=\" ${EPREFIX}/etc/modprobe.d/nvidia.conf \"" >> \
+			"${ED}"/usr/lib/dracut/dracut.conf.d/10-${PN}.conf || die
+	fi
 }
 
 pkg_preinst() {

x11-drivers/nvidia-drivers/nvidia-drivers-550.120.ebuild
@@ -442,12 +442,13 @@ documentation that is installed alongside this README."
 	insinto /etc/sandbox.d
 	newins - 20nvidia <<<'SANDBOX_PREDICT="/dev/nvidiactl:/dev/nvidia-caps:/dev/char"'
 
-	# Dracut does not include /etc/modprobe.d if hostonly=no, but we do need this
-	# to ensure that the nouveau blacklist is applied
-	# https://github.com/dracut-ng/dracut-ng/issues/674
-	# https://bugs.gentoo.org/932781
-	echo "install_items+=\" ${EPREFIX}/etc/modprobe.d/nvidia.conf \"" >> \
-		"${ED}/usr/lib/dracut/dracut.conf.d/10-${PN}.conf" || die
+	# dracut does not use /etc/modprobe.d if hostonly=no, but want to make sure
+	# our settings are used for bug 932781#c8 and nouveau blacklist if either
+	# modules are included (however, just best-effort without initramfs regen)
+	if use modules; then
+		echo "install_items+=\" ${EPREFIX}/etc/modprobe.d/nvidia.conf \"" >> \
+			"${ED}"/usr/lib/dracut/dracut.conf.d/10-${PN}.conf || die
+	fi
 }
 
 pkg_preinst() {

x11-drivers/nvidia-drivers/nvidia-drivers-550.40.75.ebuild
@@ -445,12 +445,13 @@ documentation that is installed alongside this README."
 	insinto /etc/sandbox.d
 	newins - 20nvidia <<<'SANDBOX_PREDICT="/dev/nvidiactl:/dev/nvidia-caps:/dev/char"'
 
-	# Dracut does not include /etc/modprobe.d if hostonly=no, but we do need this
-	# to ensure that the nouveau blacklist is applied
-	# https://github.com/dracut-ng/dracut-ng/issues/674
-	# https://bugs.gentoo.org/932781
-	echo "install_items+=\" ${EPREFIX}/etc/modprobe.d/nvidia.conf \"" >> \
-		"${ED}/usr/lib/dracut/dracut.conf.d/10-${PN}.conf" || die
+	# dracut does not use /etc/modprobe.d if hostonly=no, but want to make sure
+	# our settings are used for bug 932781#c8 and nouveau blacklist if either
+	# modules are included (however, just best-effort without initramfs regen)
+	if use modules; then
+		echo "install_items+=\" ${EPREFIX}/etc/modprobe.d/nvidia.conf \"" >> \
+			"${ED}"/usr/lib/dracut/dracut.conf.d/10-${PN}.conf || die
+	fi
 }
 
 pkg_preinst() {

x11-drivers/nvidia-drivers/nvidia-drivers-560.35.03-r1.ebuild
@@ -448,12 +448,13 @@ documentation that is installed alongside this README."
 	insinto /etc/sandbox.d
 	newins - 20nvidia <<<'SANDBOX_PREDICT="/dev/nvidiactl:/dev/nvidia-caps:/dev/char"'
 
-	# Dracut does not include /etc/modprobe.d if hostonly=no, but we do need this
-	# to ensure that the nouveau blacklist is applied
-	# https://github.com/dracut-ng/dracut-ng/issues/674
-	# https://bugs.gentoo.org/932781
-	echo "install_items+=\" ${EPREFIX}/etc/modprobe.d/nvidia.conf \"" >> \
-		"${ED}/usr/lib/dracut/dracut.conf.d/10-${PN}.conf" || die
+	# dracut does not use /etc/modprobe.d if hostonly=no, but want to make sure
+	# our settings are used for bug 932781#c8 and nouveau blacklist if either
+	# modules are included (however, just best-effort without initramfs regen)
+	if use modules; then
+		echo "install_items+=\" ${EPREFIX}/etc/modprobe.d/nvidia.conf \"" >> \
+			"${ED}"/usr/lib/dracut/dracut.conf.d/10-${PN}.conf || die
+	fi
 }
 
 pkg_preinst() {