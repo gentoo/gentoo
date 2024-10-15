gentoo/gentoo

commit b3e643353b25028826a85390411a840ed0deecec

dev-lang/mercury-extras: mark as LTO-unsafe
It appears to be a fundamental design issue. It is not really C code,
though they do generate it as an intermediate format.

```
ccJwDryZ.ltrans0.ltrans.o:(.data.rel.ro.local+0x330): undefined reference to `<unification predicate for type 'align_right.squeeze'/0 mode 0>'
```

Closes: https://bugs.gentoo.org/855638
Signed-off-by: Eli Schwartz <eschwartz@gentoo.org>

dev-lang/mercury-extras/mercury-extras-22.01.1.ebuild
@@ -191,6 +191,12 @@ src_prepare() {
 }
 
 src_compile() {
+	# ccJwDryZ.ltrans0.ltrans.o:(.data.rel.ro.local+0x330): undefined reference to `<unification predicate for type 'align_right.squeeze'/0 mode 0>'
+	# https://bugs.gentoo.org/855638
+	#
+	# Custom language-specific compiler infrastructure, the main mercury compiler cannot handle LTO either.
+	filter-lto
+
 	for mercury_pkg in $(mercury_pkgs); do
 		mercury_pkg_compile ${mercury_pkg}
 	done

commit 7584b42b435c894fc69811e4af50b13178bca6ca

dev-lang/mercury-extras: fix filter-lto to include inherit
Otherwise filter-lto doesn't actually run. Oops!

Closes: https://bugs.gentoo.org/941515
Fixes: b3e643353b25028826a85390411a840ed0deecec
Signed-off-by: Eli Schwartz <eschwartz@gentoo.org>

dev-lang/mercury-extras/mercury-extras-22.01.1.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-inherit vcs-clean
+inherit flag-o-matic vcs-clean
 
 PATCHSET_VER="1"
 MY_P=mercury-srcdist-${PV}