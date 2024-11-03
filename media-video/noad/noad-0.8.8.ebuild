gentoo/gentoo

commit c5202bba6c7a4dab13c4db91d38764ad498d2f28

media-video/noad: add 0.8.8
new bugfix version 0.8.8
new EAPI 8

Closes: https://bugs.gentoo.org/889656
Signed-off-by: Martin Dummer <martin.dummer@gmx.net>
Closes: https://github.com/gentoo/gentoo/pull/29024
Signed-off-by: Viorel Munteanu <ceamac@gentoo.org>

media-video/noad/noad-0.8.8.ebuild
@@ -1,0 +1,69 @@
+# Copyright 2021-2023 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit autotools
+
+DESCRIPTION="Mark commercial breaks in VDR recordings"
+HOMEPAGE="https://github.com/madmartin/noad"
+SRC_URI="https://github.com/madmartin/noad/archive/v${PV}.tar.gz -> ${P}.tar.gz"
+
+LICENSE="GPL-2+"
+SLOT="0"
+KEYWORDS="~amd64 ~x86"
+IUSE="+ffmpeg imagemagick libmpeg2"
+REQUIRED_USE="|| ( ffmpeg libmpeg2 )"
+
+DEPEND="
+	libmpeg2? ( media-libs/libmpeg2 )
+	ffmpeg? ( media-video/ffmpeg:= )
+	imagemagick? ( media-gfx/imagemagick:= )"
+RDEPEND="${DEPEND}"
+BDEPEND="virtual/pkgconfig"
+
+src_prepare() {
+	default
+	eautoreconf
+}
+
+src_configure() {
+	econf \
+		$(usev imagemagick --with-magick) \
+		$(usev !ffmpeg --without-ffmpeg) \
+		$(usev !libmpeg2 --without-libmpeg2) \
+		--with-tools
+}
+
+src_install() {
+	dobin noad showindex checkMarks
+	use imagemagick && dobin markpics
+
+	dodoc README* INSTALL
+	# example scripts are installed as dokumentation
+	dodoc allnewnoad allnoad allnoadnice allnoaduncut checkAllMarks clearlogos noadcall.sh noadifnew stat2html statupd
+
+	newconfd "${FILESDIR}"/confd_vdraddon.noad vdraddon.noad
+
+	insinto /usr/share/vdr/record
+	doins "${FILESDIR}"/record-50-noad.sh
+
+	insinto /usr/share/vdr/shutdown
+	doins "${FILESDIR}"/pre-shutdown-15-noad.sh
+
+	insinto /etc/vdr/reccmds
+	doins "${FILESDIR}"/reccmds.noad.conf
+
+	exeinto /usr/share/vdr/bin
+	doexe "${FILESDIR}"/noad-reccmd
+}
+
+pkg_postinst() {
+	elog
+	elog "To integrate noad in VDR you should do this:"
+	elog
+	elog "start and set Parameter in /etc/conf.d/vdraddon.noad"
+	elog
+	elog "Note: You can use here all parameters for noad,"
+	elog "please look in the documentation of noad."
+}

media-video/noad/Manifest
@@ -1,1 +1,2 @@
 DIST noad-0.8.7.tar.gz 144339 BLAKE2B e7492f63e7326cdced041332e05051a6bda2e92798f4b694ff626e65306d6ebe606f871eabc37df1613677c2f739b3caed033b443cbe7edce913b83a0365e3e5 SHA512 fa1d0226144175de586f938a594fe0a3c0b8a924a258f1406cf52078d5e6d84f46ae685ee3c9f3ce28e62f9eee719d6a938504e39a6b3b9c96043f4bd4aaf69d
+DIST noad-0.8.8.tar.gz 144269 BLAKE2B 0474c165af16afb93459dd753017e25afe5e9c42439af9e1f2e23e4aeb5d4cd3610a66dec80d63d1c5f71d2ef9f6817ad831a5e368e8aeda53e078d0137f67ae SHA512 1795072e0b416c9904e8769da2bfe08120ea2c55427bd6e7479c6d79bb5a1b25abe82c0ed2cf8b3d5f7a354e2f60b1251e9d1ed102d19b3c45c1cd34a5e8e6d0

commit 4ee14f4cfa568a762db6224d38d869ec0a1cb230

media-video/noad: restore dependency on <ffmpeg-5
Restores commit 43ad97426db5e10cfffd668499bae7e55fb2ddca, which was
backed out by a revbump that did not rebase on the latest version of the
ebuild.

Fixes: c5202bba6c7a4dab13c4db91d38764ad498d2f28
Closes: https://bugs.gentoo.org/834408
Closes: https://bugs.gentoo.org/916548
Signed-off-by: Eli Schwartz <eschwartz@gentoo.org>

media-video/noad/noad-0.8.8.ebuild
@@ -1,4 +1,4 @@
-# Copyright 2021-2023 Gentoo Authors
+# Copyright 2021-2024 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -15,9 +15,10 @@ KEYWORDS="amd64 x86"
 IUSE="+ffmpeg imagemagick libmpeg2"
 REQUIRED_USE="|| ( ffmpeg libmpeg2 )"
 
+# <ffmpeg-5 dep for bug #834408, https://github.com/madmartin/noad/issues/2
 DEPEND="
 	libmpeg2? ( media-libs/libmpeg2 )
-	ffmpeg? ( media-video/ffmpeg:= )
+	ffmpeg? ( <media-video/ffmpeg-5:= )
 	imagemagick? ( media-gfx/imagemagick:= )"
 RDEPEND="${DEPEND}"
 BDEPEND="virtual/pkgconfig"