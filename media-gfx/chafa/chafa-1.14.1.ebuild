gentoo/gentoo

commit 73c5f2eaa9f83705bf6e160d34133cb20d881977

media-gfx/chafa: make sure elibtoolize is applied
Needed to e.g. pass LTO warning flags through to the linker.

Signed-off-by: Eli Schwartz <eschwartz@gentoo.org>

media-gfx/chafa/chafa-1.14.1.ebuild
@@ -27,6 +27,11 @@ QA_CONFIG_IMPL_DECL_SKIP=(
 	_mm_popcnt_u64
 )
 
+src_prepare() {
+	default
+	elibtoolize
+}
+
 src_configure() {
 	# bug 909429
 	use webp && append-ldflags -lwebp

media-gfx/chafa/chafa-1.12.5-r1.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-inherit flag-o-matic
+inherit flag-o-matic libtool
 
 DESCRIPTION="versatile and fast Unicode/ASCII/ANSI graphics renderer"
 HOMEPAGE="https://hpjansson.org/chafa/ https://github.com/hpjansson/chafa"
@@ -27,6 +27,11 @@ QA_CONFIG_IMPL_DECL_SKIP=(
 	# checking for intrinsics, will fail where not supported. bug #927102
 	_mm_popcnt_u64
 )
+
+src_prepare() {
+	default
+	elibtoolize
+}
 
 src_configure() {
 	# bug 909429

commit facd834872d6c369871fe17d42d0f6eb6d4190ef

media-gfx/chafa: add missing libtool inherit for elibtoolize
Closes: https://bugs.gentoo.org/942851
Fixes: 73c5f2eaa9f83705bf6e160d34133cb20d881977
Signed-off-by: Sam James <sam@gentoo.org>

media-gfx/chafa/chafa-1.14.1.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-inherit flag-o-matic
+inherit flag-o-matic libtool
 
 DESCRIPTION="versatile and fast Unicode/ASCII/ANSI graphics renderer"
 HOMEPAGE="https://hpjansson.org/chafa/ https://github.com/hpjansson/chafa"