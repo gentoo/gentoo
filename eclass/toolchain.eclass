gentoo/gentoo

commit d82f81cd65b9a668bd7940151e3fa876d4b0ce92

toolchain.eclass: fix death hooks for JIT build
We want to recover logs from that too.

Bug: https://bugs.gentoo.org/942769
Fixes: 5bed0c77a1c54ac9297db4bb54af2e45c0558ec7
Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -2976,18 +2976,21 @@ XGCC() { get_make_var GCC_FOR_TARGET ; }
 
 has toolchain_death_notice ${EBUILD_DEATH_HOOKS} || EBUILD_DEATH_HOOKS+=" toolchain_death_notice"
 toolchain_death_notice() {
-	if [[ -e "${WORKDIR}"/build ]] ; then
-		pushd "${WORKDIR}"/build >/dev/null
-		(echo '' | $(tc-getCC ${CTARGET}) ${CFLAGS} -v -E - 2>&1) > gccinfo.log
-		[[ -e "${T}"/build.log ]] && cp "${T}"/build.log .
-		tar -acf "${WORKDIR}"/gcc-build-logs.tar.xz \
-			gccinfo.log build.log $(find -name config.log)
-		rm gccinfo.log build.log
-		eerror
-		eerror "Please include ${WORKDIR}/gcc-build-logs.tar.xz in your bug report."
-		eerror
-		popd >/dev/null
-	fi
+	local dir
+	for dir in "${WORKDIR}"/build-jit "${WORKDIR}"/build ; do
+		if [[ -e "${dir}" ]] ; then
+			pushd "${WORKDIR}" >/dev/null
+			(echo '' | $(tc-getCC ${CTARGET}) ${CFLAGS} -v -E - 2>&1) > gccinfo.log
+			[[ -e "${T}"/build.log ]] && cp "${T}"/build.log .
+			tar -arf "${WORKDIR}"/gcc-build-logs.tar.xz \
+				"${dir}"/gccinfo.log "${dir}"/build.log $(find -name "${dir}"/config.log)
+			rm "${dir}"/gccinfo.log "${dir}"/build.log
+			eerror
+			eerror "Please include ${WORKDIR}/gcc-build-logs.tar.xz in your bug report."
+			eerror
+			popd >/dev/null
+		fi
+	done
 }
 
 fi

commit 1bf781b83fdfd96e662ec76a54ff5ca0b9aad5f6

toolchain.eclass: further death hook fixes
Fixes: d82f81cd65b9a668bd7940151e3fa876d4b0ce92
Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -2980,10 +2980,10 @@ toolchain_death_notice() {
 	for dir in "${WORKDIR}"/build-jit "${WORKDIR}"/build ; do
 		if [[ -e "${dir}" ]] ; then
 			pushd "${WORKDIR}" >/dev/null
-			(echo '' | $(tc-getCC ${CTARGET}) ${CFLAGS} -v -E - 2>&1) > gccinfo.log
-			[[ -e "${T}"/build.log ]] && cp "${T}"/build.log .
+			(echo '' | $(tc-getCC ${CTARGET}) ${CFLAGS} -v -E - 2>&1) > "${dir}"/gccinfo.log
+			[[ -e "${T}"/build.log ]] && cp "${T}"/build.log "${dir}"
 			tar -arf "${WORKDIR}"/gcc-build-logs.tar.xz \
-				"${dir}"/gccinfo.log "${dir}"/build.log $(find -name "${dir}"/config.log)
+				"${dir#${WORKDIR}/}"/gccinfo.log "${dir#${WORKDIR}/}"/build.log $(find -name "${dir}"/config.log)
 			rm "${dir}"/gccinfo.log "${dir}"/build.log
 			eerror
 			eerror "Please include ${WORKDIR}/gcc-build-logs.tar.xz in your bug report."