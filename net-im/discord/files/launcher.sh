10edd70862670c0b6a9eb4930e6951192537b27e
Nick Sarnie <sarnex@gentoo.org>
2024-11-08 04:38:42 +0900

    net-im/discord: Fix system log spam on Wayland
    Closes: https://bugs.gentoo.org/942950
    
    Signed-off-by: Nick Sarnie <sarnex@gentoo.org>

--- net-im/discord/files/launcher.sh
+++ net-im/discord/files/launcher.sh
@@ -15,6 +15,7 @@ EBUILD_WAYLAND=false
 	--enable-features=UseOzonePlatform
 	--ozone-platform=wayland
 	--enable-wayland-ime
+	--disable-gpu-memory-buffer-video-frames
 )
 
 @@DESTDIR@@/Discord "${discord_parameters[@]}" "$@"
