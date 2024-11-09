gentoo/gentoo

commit 01ba4dc61965ef7658a24728841c04c9a1ce4871

*/*: update for slotted Rust
Tidyups:

- $(cargo_crate_uris)
- Rust BDEPENDS come from the eclass except in very rare
  circumstances (RUST_OPTIONAL=1)
- RUST_M{AX,IN}_VER where required.
- Suboptimal crate separator (`-` -> `@`)

Signed-off-by: Matt Jolly <kangie@gentoo.org>
Closes: https://github.com/gentoo/gentoo/pull/39218
Signed-off-by: Matt Jolly <kangie@gentoo.org>

net-libs/rustls-ffi/rustls-ffi-0.10.0-r2.ebuild
@@ -1,0 +1,120 @@
+# Copyright 2022-2024 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+CRATES="
+	autocfg@1.1.0
+	base64@0.13.1
+	bumpalo@3.12.0
+	cc@1.0.79
+	cfg-if@1.0.0
+	hashbrown@0.12.3
+	indexmap@1.9.3
+	js-sys@0.3.61
+	libc@0.2.140
+	log@0.4.17
+	memchr@2.5.0
+	num_enum@0.5.11
+	num_enum_derive@0.5.11
+	once_cell@1.17.1
+	proc-macro-crate@1.3.1
+	proc-macro2@1.0.55
+	quote@1.0.26
+	ring@0.16.20
+	rustls@0.21.0
+	rustls-pemfile@0.2.1
+	rustls-webpki@0.100.1
+	rustversion@1.0.12
+	sct@0.7.0
+	spin@0.5.2
+	syn@1.0.109
+	toml_datetime@0.6.1
+	toml_edit@0.19.8
+	unicode-ident@1.0.8
+	untrusted@0.7.1
+	wasm-bindgen@0.2.84
+	wasm-bindgen-backend@0.2.84
+	wasm-bindgen-macro@0.2.84
+	wasm-bindgen-macro-support@0.2.84
+	wasm-bindgen-shared@0.2.84
+	web-sys@0.3.61
+	webpki@0.22.0
+	winapi@0.3.9
+	winapi-i686-pc-windows-gnu@0.4.0
+	winapi-x86_64-pc-windows-gnu@0.4.0
+	winnow@0.4.1
+"
+
+inherit cargo flag-o-matic multilib-minimal rust-toolchain
+
+DESCRIPTION="C-to-rustls bindings"
+HOMEPAGE="https://github.com/rustls/rustls-ffi"
+SRC_URI="https://github.com/rustls/rustls-ffi/archive/refs/tags/v${PV}.tar.gz -> ${P}.tar.gz"
+SRC_URI+=" ${CARGO_CRATE_URIS}"
+
+# From cargo-ebuild (note that webpki is also just ISC)
+LICENSE="|| ( MIT Apache-2.0 ) BSD Boost-1.0 ISC MIT MPL-2.0 Unicode-DFS-2016"
+# Dependent crate licenses
+LICENSE+=" ISC MIT Unicode-DFS-2016"
+# For Ring (see its LICENSE)
+LICENSE+=" ISC openssl SSLeay MIT"
+SLOT="0/${PV}"
+KEYWORDS="~amd64"
+
+BDEPEND="dev-util/cargo-c"
+
+QA_FLAGS_IGNORED="usr/lib.*/librustls.*"
+
+PATCHES=(
+	"${FILESDIR}"/${PN}@0.10.0-cargo-c.patch
+)
+
+src_prepare() {
+	default
+
+	multilib_copy_sources
+}
+
+src_configure() {
+	# bug #927231
+	filter-lto
+
+	multilib-minimal_src_configure
+}
+
+multilib_src_compile() {
+	local cargoargs=(
+		--library-type=cdylib
+		--prefix=/usr
+		--libdir="/usr/$(get_libdir)"
+		--target="$(rust_abi)"
+		$(usev !debug '--release')
+	)
+
+	cargo cbuild "${cargoargs[@]}" || die "cargo cbuild failed"
+}
+
+multilib_src_test() {
+	local cargoargs=(
+		--prefix=/usr
+		--libdir="/usr/$(get_libdir)"
+		--target="$(rust_abi)"
+		$(usex debug '--debug' '--release')
+	)
+
+	cargo ctest "${cargoargs[@]}" || die "cargo ctest failed"
+}
+
+multilib_src_install() {
+	local cargoargs=(
+		--library-type=cdylib
+		--prefix=/usr
+		--libdir="/usr/$(get_libdir)"
+		--target="$(rust_abi)"
+		--destdir="${ED}"
+		$(usex debug '--debug' '--release')
+	)
+
+	cargo cinstall "${cargoargs[@]}" || die "cargo cinstall failed"
+}

net-libs/rustls-ffi/rustls-ffi-0.12.1-r3.ebuild
@@ -1,0 +1,114 @@
+# Copyright 2022-2024 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+CRATES="
+	aho-corasick@1.1.1
+	base64@0.21.5
+	cc@1.0.83
+	cfg-if@1.0.0
+	getrandom@0.2.11
+	libc@0.2.153
+	log@0.4.21
+	memchr@2.6.4
+	regex-automata@0.3.9
+	regex-syntax@0.7.5
+	regex@1.9.6
+	ring@0.17.5
+	rustls-pemfile@2.1.1
+	rustls-pki-types@1.3.1
+	rustls-webpki@0.102.0
+	rustls@0.22.0
+	rustversion@1.0.14
+	spin@0.9.8
+	subtle@2.5.0
+	untrusted@0.9.0
+	wasi@0.11.0+wasi-snapshot-preview1
+	windows-sys@0.48.0
+	windows-targets@0.48.5
+	windows_aarch64_gnullvm@0.48.5
+	windows_aarch64_msvc@0.48.5
+	windows_i686_gnu@0.48.5
+	windows_i686_msvc@0.48.5
+	windows_x86_64_gnu@0.48.5
+	windows_x86_64_gnullvm@0.48.5
+	windows_x86_64_msvc@0.48.5
+	zeroize@1.7.0
+"
+
+inherit cargo flag-o-matic multilib-minimal rust-toolchain
+
+DESCRIPTION="C-to-rustls bindings"
+HOMEPAGE="https://github.com/rustls/rustls-ffi"
+SRC_URI="https://github.com/rustls/rustls-ffi/archive/refs/tags/v${PV}.tar.gz -> ${P}.tar.gz"
+SRC_URI+=" ${CARGO_CRATE_URIS}"
+
+LICENSE="|| ( Apache-2.0 MIT ISC )"
+# Dependent crate licenses
+LICENSE+=" BSD ISC MIT"
+# For Ring (see its LICENSE)
+LICENSE+=" ISC openssl SSLeay MIT"
+SLOT="0/${PV}"
+KEYWORDS="~amd64"
+
+BDEPEND="dev-util/cargo-c"
+
+QA_FLAGS_IGNORED="usr/lib.*/librustls.*"
+
+PATCHES=(
+	"${FILESDIR}"/${PN}@0.12.1-no-rust-nightly.patch
+)
+
+src_prepare() {
+	default
+
+	multilib_copy_sources
+}
+
+src_configure() {
+	# bug #927231
+	filter-lto
+
+	# textrels in ring
+	# Hopefully fixed with https://github.com/rustls/rustls-ffi/pull/389
+	export RUSTFLAGS="${RUSTFLAGS} -C link-arg=-Wl,-z,notext"
+
+	multilib-minimal_src_configure
+}
+
+multilib_src_compile() {
+	local cargoargs=(
+		--library-type=cdylib
+		--prefix="${EPREFIX}"/usr
+		--libdir="${EPREFIX}/usr/$(get_libdir)"
+		--target="$(rust_abi)"
+		$(usev !debug '--release')
+	)
+
+	cargo cbuild "${cargoargs[@]}" || die "cargo cbuild failed"
+}
+
+multilib_src_test() {
+	local cargoargs=(
+		--prefix="${EPREFIX}"/usr
+		--libdir="${EPREFIX}/usr/$(get_libdir)"
+		--target="$(rust_abi)"
+		$(usex debug '--debug' '--release')
+	)
+
+	cargo ctest "${cargoargs[@]}" || die "cargo ctest failed"
+}
+
+multilib_src_install() {
+	local cargoargs=(
+		--library-type=cdylib
+		--prefix="${EPREFIX}"/usr
+		--libdir="${EPREFIX}/usr/$(get_libdir)"
+		--target="$(rust_abi)"
+		--destdir="${D}"
+		$(usex debug '--debug' '--release')
+	)
+
+	cargo cinstall "${cargoargs[@]}" || die "cargo cinstall failed"
+}

net-libs/rustls-ffi/rustls-ffi-0.9.1-r2.ebuild
@@ -1,0 +1,139 @@
+# Copyright 2022-2024 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+CRATES="
+	ansi_term@0.12.1
+	atty@0.2.14
+	autocfg@1.1.0
+	base64@0.13.1
+	bitflags@1.3.2
+	bumpalo@3.11.1
+	cbindgen@0.19.0
+	cc@1.0.77
+	cfg-if@1.0.0
+	clap@2.34.0
+	fastrand@1.8.0
+	hashbrown@0.12.3
+	heck@0.3.3
+	hermit-abi@0.1.19
+	indexmap@1.9.2
+	instant@0.1.12
+	itoa@1.0.4
+	js-sys@0.3.60
+	libc@0.2.138
+	log@0.4.17
+	num_enum@0.5.7
+	num_enum_derive@0.5.7
+	once_cell@1.16.0
+	proc-macro-crate@1.2.1
+	proc-macro2@1.0.47
+	quote@1.0.21
+	redox_syscall@0.2.16
+	remove_dir_all@0.5.3
+	ring@0.16.20
+	rustls@0.20.4
+	rustls-pemfile@0.2.1
+	rustversion@1.0.9
+	ryu@1.0.11
+	sct@0.7.0
+	serde@1.0.149
+	serde_derive@1.0.149
+	serde_json@1.0.89
+	spin@0.5.2
+	strsim@0.8.0
+	syn@1.0.105
+	tempfile@3.3.0
+	textwrap@0.11.0
+	thiserror@1.0.37
+	thiserror-impl@1.0.37
+	toml@0.5.9
+	unicode-ident@1.0.5
+	unicode-segmentation@1.10.0
+	unicode-width@0.1.10
+	untrusted@0.7.1
+	vec_map@0.8.2
+	wasm-bindgen@0.2.83
+	wasm-bindgen-backend@0.2.83
+	wasm-bindgen-macro@0.2.83
+	wasm-bindgen-macro-support@0.2.83
+	wasm-bindgen-shared@0.2.83
+	web-sys@0.3.60
+	webpki@0.22.0
+	winapi@0.3.9
+	winapi-i686-pc-windows-gnu@0.4.0
+	winapi-x86_64-pc-windows-gnu@0.4.0
+"
+
+inherit cargo flag-o-matic multilib-minimal rust-toolchain
+
+DESCRIPTION="C-to-rustls bindings"
+HOMEPAGE="https://github.com/rustls/rustls-ffi"
+SRC_URI="https://github.com/rustls/rustls-ffi/archive/refs/tags/v${PV}.tar.gz -> ${P}.tar.gz"
+SRC_URI+=" ${CARGO_CRATE_URIS}"
+
+# From cargo-ebuild (note that webpki is also just ISC)
+LICENSE="|| ( MIT Apache-2.0 ) BSD Boost-1.0 ISC MIT MPL-2.0 Unicode-DFS-2016"
+# For Ring (see its LICENSE)
+LICENSE+=" ISC openssl SSLeay MIT"
+SLOT="0/${PV}"
+KEYWORDS="~amd64"
+
+BDEPEND="dev-util/cargo-c"
+
+QA_FLAGS_IGNORED="usr/lib.*/librustls.*"
+
+PATCHES=(
+	"${FILESDIR}"/${PN}@0.9.1-cargo-c.patch
+	"${FILESDIR}"/${PN}@0.9.1-tests-32-bit.patch
+)
+
+src_prepare() {
+	default
+
+	multilib_copy_sources
+}
+
+src_configure() {
+	# bug #927231
+	filter-lto
+
+	multilib-minimal_src_configure
+}
+
+multilib_src_compile() {
+	local cargoargs=(
+		--library-type=cdylib
+		--prefix=/usr
+		--libdir="/usr/$(get_libdir)"
+		--target="$(rust_abi)"
+		$(usev !debug '--release')
+	)
+
+	cargo cbuild "${cargoargs[@]}" || die "cargo cbuild failed"
+}
+
+multilib_src_test() {
+	local cargoargs=(
+		--prefix=/usr
+		--libdir="/usr/$(get_libdir)"
+		--target="$(rust_abi)"
+		$(usex debug '--debug' '--release')
+	)
+
+	cargo ctest "${cargoargs[@]}" || die "cargo ctest failed"
+}
+
+multilib_src_install() {
+	local cargoargs=(
+		--library-type=cdylib
+		--prefix=/usr
+		--libdir="/usr/$(get_libdir)"
+		--target="$(rust_abi)"
+		--destdir="${ED}"
+		$(usex debug '--debug' '--release')
+	)
+
+	cargo cinstall "${cargoargs[@]}" || die "cargo cinstall failed"
+}

commit dd83555eb4683658f8f35c287b61e36b00ed2026

net-libs/rustls-ffi: fix PATCHES
Fixes: 01ba4dc61965ef7658a24728841c04c9a1ce4871
Signed-off-by: Sam James <sam@gentoo.org>

net-libs/rustls-ffi/rustls-ffi-0.10.0-r2.ebuild
@@ -67,7 +67,7 @@ BDEPEND="dev-util/cargo-c"
 QA_FLAGS_IGNORED="usr/lib.*/librustls.*"
 
 PATCHES=(
-	"${FILESDIR}"/${PN}@0.10.0-cargo-c.patch
+	"${FILESDIR}"/${PN}-0.10.0-cargo-c.patch
 )
 
 src_prepare() {

net-libs/rustls-ffi/rustls-ffi-0.12.1-r3.ebuild
@@ -57,7 +57,7 @@ BDEPEND="dev-util/cargo-c"
 QA_FLAGS_IGNORED="usr/lib.*/librustls.*"
 
 PATCHES=(
-	"${FILESDIR}"/${PN}@0.12.1-no-rust-nightly.patch
+	"${FILESDIR}"/${PN}-0.12.1-no-rust-nightly.patch
 )
 
 src_prepare() {

net-libs/rustls-ffi/rustls-ffi-0.9.1-r2.ebuild
@@ -85,8 +85,8 @@ BDEPEND="dev-util/cargo-c"
 QA_FLAGS_IGNORED="usr/lib.*/librustls.*"
 
 PATCHES=(
-	"${FILESDIR}"/${PN}@0.9.1-cargo-c.patch
-	"${FILESDIR}"/${PN}@0.9.1-tests-32-bit.patch
+	"${FILESDIR}"/${PN}-0.9.1-cargo-c.patch
+	"${FILESDIR}"/${PN}-0.9.1-tests-32-bit.patch
 )
 
 src_prepare() {