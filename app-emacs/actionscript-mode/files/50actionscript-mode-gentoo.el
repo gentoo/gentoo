97a217aaeefc922d6371516cf0fa15c2b22c579a
Ulrich Müller <ulm@gentoo.org>
2024-11-16 17:11:32 +0100

    app-emacs/actionscript-mode: Use with-eval-after-load in site-init
    Signed-off-by: Ulrich Müller <ulm@gentoo.org>

--- app-emacs/actionscript-mode/files/50actionscript-mode-gentoo.el
+++ app-emacs/actionscript-mode/files/50actionscript-mode-gentoo.el
@@ -2,4 +2,4 @@
 (autoload 'actionscript-mode "actionscript-mode"
   "Major mode for editing Actionscript files." t)
 (add-to-list 'auto-mode-alist '("\\.as\\'" . actionscript-mode))
-(eval-after-load "actionscript-mode" '(load "actionscript-config"))
+(with-eval-after-load "actionscript-mode" (load "actionscript-config"))
