f3adafc99e9e476803e606c5d24db5e672352520
Ulrich Müller <ulm@gentoo.org>
2024-11-16 17:14:21 +0100

    app-emacs/flycheck-package: Pass function to eval-after-load in site-init
    Signed-off-by: Ulrich Müller <ulm@gentoo.org>

--- app-emacs/flycheck-package/files/50flycheck-package-gentoo.el
+++ app-emacs/flycheck-package/files/50flycheck-package-gentoo.el
@@ -1,4 +1,4 @@
 (add-to-list 'load-path "@SITELISP@")
 (autoload 'flycheck-package-setup "flycheck-package"
   "Setup flycheck-package." t nil)
-(eval-after-load 'flycheck '(flycheck-package-setup))
+(eval-after-load 'flycheck #'flycheck-package-setup)
