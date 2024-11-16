9f22441ba7e774d6f603170ec5dc3c2a00582a3d
Ulrich Müller <ulm@gentoo.org>
2024-11-16 17:15:05 +0100

    app-emacs/yasnippet-snippets: Pass function to eval-after-load in site-init
    Signed-off-by: Ulrich Müller <ulm@gentoo.org>

--- app-emacs/yasnippet-snippets/files/50yasnippet-snippets-gentoo.el
+++ app-emacs/yasnippet-snippets/files/50yasnippet-snippets-gentoo.el
@@ -1,5 +1,4 @@
 (add-to-list 'load-path "@SITELISP@")
 (autoload 'yasnippet-snippets-initialize "yasnippet-snippets"
   "Load the `yasnippet-snippets' snippets directory." t)
-(eval-after-load 'yasnippet
-  '(yasnippet-snippets-initialize))
+(eval-after-load 'yasnippet #'yasnippet-snippets-initialize)
