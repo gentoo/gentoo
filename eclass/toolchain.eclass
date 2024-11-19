gentoo/gentoo

commit f6a974f8f2378c6c6a83345c101afce233fdf1d7

toolchain.eclass: fix libdiagnostics handling
Reported by ulm. This was introduced by a last-minute simplification I made,
but USE=libdiagnostics only exists for 9999 and the *next* snapshot, not
the current one, but even then, the code was wrong for < 15 as the use_enable
part was run unconditionally w/ USE=jit.

Fixes: b923a4c0d1a330e40f0a05f3bc94bb4f32ce1cbb
Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -1761,7 +1761,6 @@ toolchain_src_configure() {
 
 			--enable-host-shared
 			--enable-languages=jit
-			$(use_enable libdiagnostics)
 
 			# Might be used for the just-built GCC. Easier to just
 			# respect USE=graphite here in case the user passes some
@@ -1770,6 +1769,10 @@ toolchain_src_configure() {
 			$(use_with zstd)
 			--with-system-zlib
 		)
+
+		if tc_version_is_at_least 15.0.0_pre20241124 ; then
+			confgcc_jit+=( $(use_enable libdiagnostics) )
+		fi
 
 		if tc_version_is_at_least 13.1 ; then
 			confgcc_jit+=( --disable-fixincludes )

commit 14b4ce60bae26089b0b80f9f629900bf7721e9f5

toolchain.eclass: fix libdiagnostics handling (more)
I continue to mix up how tc_version_is_at_least works. One day I'll
change it...

Closes: https://bugs.gentoo.org/944007
Fixes: f6a974f8f2378c6c6a83345c101afce233fdf1d7
Fixes: b923a4c0d1a330e40f0a05f3bc94bb4f32ce1cbb
Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -1770,7 +1770,7 @@ toolchain_src_configure() {
 			--with-system-zlib
 		)
 
-		if tc_version_is_at_least 15.0.0_pre20241124 ; then
+		if tc_version_is_at_least 15.0.0_pre20241124 ${PV} ; then
 			confgcc_jit+=( $(use_enable libdiagnostics) )
 		fi
 