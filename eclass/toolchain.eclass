gentoo/gentoo

commit b923a4c0d1a330e40f0a05f3bc94bb4f32ce1cbb

toolchain.eclass: wire up libdiagonstics
See https://gcc.gnu.org/wiki/libdiagnostics. New shared library for
diagnostics in GCC. Also provides the 'sarif-replay' tool.

Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -333,6 +333,7 @@ if [[ ${PN} != kgcc64 && ${PN} != gcc-* ]] ; then
 	# it was disabled in 13.
 	tc_version_is_at_least 14.0.0_pre20230423 ${PV} && IUSE+=" rust" TC_FEATURES+=( rust )
 	tc_version_is_at_least 14.2.1_p20241026 ${PV} && IUSE+=" time64"
+	tc_version_is_at_least 15.0.0_pre20241124 ${PV} && IUSE+=" libdiagnostics"
 fi
 
 if tc_version_is_at_least 10; then
@@ -1732,8 +1733,8 @@ toolchain_src_configure() {
 		gcc_shell="${BROOT}"/bin/sh
 	fi
 
-	if is_jit ; then
-		einfo "Configuring JIT gcc"
+	if is_jit || _tc_use_if_iuse libdiagnostics ; then
+		einfo "Configuring shared gcc for JIT/libdiagnostics"
 
 		local confgcc_jit=(
 			"${confgcc[@]}"
@@ -1757,8 +1758,11 @@ toolchain_src_configure() {
 			--disable-nls
 			--disable-objc-gc
 			--disable-systemtap
+
 			--enable-host-shared
 			--enable-languages=jit
+			$(use_enable libdiagnostics)
+
 			# Might be used for the just-built GCC. Easier to just
 			# respect USE=graphite here in case the user passes some
 			# graphite flags rather than try strip them out.

sys-devel/gcc/metadata.xml
@@ -77,6 +77,7 @@
 		<flag name="graphite">Add support for the framework for loop optimizations based on a polyhedral intermediate representation</flag>
 		<flag name="ieee-long-double">Use accelerated 128-bit IEEE long double ABI (ppc64le only)</flag>
 		<flag name="jit">Enable libgccjit so other applications can embed gcc for Just-In-Time compilation.</flag>
+		<flag name="libdiagnostics">Provide libdiagnostics (https://gcc.gnu.org/wiki/libdiagnostics). This also installs the 'sarif-replay' tool.</flag>
 		<flag name="libssp">Build SSP support into a dedicated library rather than use the code in the C library (DO NOT ENABLE THIS IF YOU DON'T KNOW WHAT IT DOES)</flag>
 		<flag name="lto">
 			Build using Link Time Optimizations (LTO).

commit f6a974f8f2378c6c6a83345c101afce233fdf1d7

toolchain.eclass: fix libdiagnostics handling
Reported by ulm. This was introduced by a last-minute simplification I made,
but USE=libdiagnostics only exists for 9999 and the *next* snapshot, not
the current one, but even then, the code was wrong for < 15 as the use_enable
part was run unconditionally w/ USE=jit.

Fixes: b923a4c0d1a330e40f0a05f3bc94bb4f32ce1cbb
Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -1761,7 +1761,6 @@ toolchain_src_configure() {
 
 			--enable-host-shared
 			--enable-languages=jit
-			$(use_enable libdiagnostics)
 
 			# Might be used for the just-built GCC. Easier to just
 			# respect USE=graphite here in case the user passes some
@@ -1770,6 +1769,10 @@ toolchain_src_configure() {
 			$(use_with zstd)
 			--with-system-zlib
 		)
+
+		if tc_version_is_at_least 15.0.0_pre20241124 ; then
+			confgcc_jit+=( $(use_enable libdiagnostics) )
+		fi
 
 		if tc_version_is_at_least 13.1 ; then
 			confgcc_jit+=( --disable-fixincludes )