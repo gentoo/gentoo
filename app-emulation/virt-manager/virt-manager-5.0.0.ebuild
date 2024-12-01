gentoo/gentoo

commit c5ca13d818262729e691e4d23487cb427b50665d

app-emulation/virt-manager: Update live ebuild
Virt-manager just switched to meson [1]. Adapt our live ebuild to
it.  Oh, and while at it - drop test useflag. Running tests
doesn't make much sense for virt-manger since the even the
current ebuild ignores pretty much all of them (as it ignores
failed tests from test_cli.py where pretty much 90% of test suite
lives).

The referenced bug below is not closed with this just yet, as
this is just live ebuild. But we can expect new release soon, in
which case the bug will be fixed.

1: https://github.com/virt-manager/virt-manager/commit/cbd2a7ef925cd11e525448561174ce6cc0e14e9a
Bug: https://bugs.gentoo.org/909876
Signed-off-by: Michal Privoznik <michal.privoznik@gmail.com>
Closes: https://github.com/gentoo/gentoo/pull/39305
Signed-off-by: Sam James <sam@gentoo.org>

app-emulation/virt-manager/virt-manager-9999.ebuild
@@ -4,9 +4,7 @@
 EAPI=8
 
 PYTHON_COMPAT=( python3_{10..13} )
-DISTUTILS_SINGLE_IMPL=1
-DISTUTILS_USE_SETUPTOOLS=no
-inherit gnome2 distutils-r1 optfeature
+inherit gnome2 python-single-r1 optfeature meson
 
 DESCRIPTION="A graphical tool for administering virtual machines"
 HOMEPAGE="https://virt-manager.org https://github.com/virt-manager/virt-manager"
@@ -24,6 +22,8 @@ fi
 LICENSE="GPL-2"
 SLOT="0"
 IUSE="gui policykit sasl"
+
+REQUIRED_USE="${PYTHON_REQUIRED_USE}"
 
 RDEPEND="
 	${PYTHON_DEPS}
@@ -53,33 +53,18 @@ BDEPEND="dev-python/docutils"
 
 DOCS=( README.md NEWS.md )
 
-DISTUTILS_ARGS=(
-	--no-update-icon-cache
-	--no-compile-schemas
-)
+src_configure() {
+	# While in the past we did allow test suite to run, any errors from
+	# test_cli.py were ignored. Since that's where like 90% of tests actually
+	# lives, just disable tests (and do not drag additional dependencies).
+	local emesonargs=(
+		-Dcompile-schemas=false
+		-Ddefault-graphics=spice
+		-Dtests=disabled
+		-Dupdate-icon-cache=false
+	)
 
-EPYTEST_IGNORE=(
-	# Wants to use /tmp osinfo config?
-	tests/test_cli.py
-
-	# These seem to be essentially coverage tests
-	tests/test_checkprops.py
-)
-
-distutils_enable_tests pytest
-
-python_configure() {
-	esetup.py configure --default-graphics=spice
-}
-
-python_test() {
-	export VIRTINST_TEST_SUITE_FORCE_LIBOSINFO=0
-
-	epytest
-}
-
-python_install() {
-	esetup.py install
+	meson_src_configure
 }
 
 pkg_preinst() {

commit 2027af3444aabbda6d6923f41dea533312593f16

app-emulation/virt-manager: Fix shebang of installed files
Previously, app-emulation/virt-manager was installed using
distutils which automatically fixed shebang on installed python
scripts. Then virt-manager switched to meson, and our ebuilds
simply started inheriting meson instead of distutils-r1. But this
alone is not enough as shebangs are now broken. Fortunately,
there's python_fix_shebang which can be called to fix shebangs
and in ideal world this function would be called in src_prepare()
but this is not the case. virt-manager and other files are
generated at compile time with also too generic shebang [1].
Therefore, let's do that at the end of src_install().

1: https://github.com/virt-manager/virt-manager/blob/main/scripts/make_bin_wrapper.py
Fixes: c5ca13d818262729e691e4d23487cb427b50665d
Closes: https://bugs.gentoo.org/945329
Signed-off-by: Michal Privoznik <michal.privoznik@gmail.com>

app-emulation/virt-manager/virt-manager-5.0.0.ebuild
@@ -67,6 +67,12 @@ src_configure() {
 	meson_src_configure
 }
 
+src_install() {
+	meson_src_install
+
+	python_fix_shebang "${ED}"
+}
+
 pkg_preinst() {
 	if use gui ; then
 		gnome2_pkg_preinst

app-emulation/virt-manager/virt-manager-9999.ebuild
@@ -67,6 +67,12 @@ src_configure() {
 	meson_src_configure
 }
 
+src_install() {
+	meson_src_install
+
+	python_fix_shebang "${ED}"
+}
+
 pkg_preinst() {
 	if use gui ; then
 		gnome2_pkg_preinst