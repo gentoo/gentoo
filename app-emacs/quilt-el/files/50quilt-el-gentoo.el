842e86174499193fead59ceff5bdccde9e0841dc
Michał Górny <mgorny@gentoo.org>
2024-12-06 05:34:41 +0100

    app-emacs/quilt-el: Remove last-rited pkg
    Closes: https://bugs.gentoo.org/247500
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- app-emacs/quilt-el/files/50quilt-el-gentoo.el
+++
@@ -1,7 +0,0 @@
-(add-to-list 'load-path "@SITELISP@")
-(autoload 'quilt-mode "quilt"
-  "Toggle quilt-mode. With positive arg, enable quilt-mode." t)
-(autoload 'quilt-hook "quilt"
-  "Enable quilt mode for quilt-controlled files.")
-(add-hook 'find-file-hook 'quilt-hook)
-(add-hook 'after-revert-hook 'quilt-hook)
