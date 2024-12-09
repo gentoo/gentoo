gentoo/gentoo

commit 5daa764a2181d57551f5fa9db558a528dc302455

app-emacs/mpg123-el: add 1.65
Closes: https://bugs.gentoo.org/946145
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

app-emacs/mpg123-el/mpg123-el-1.65.ebuild
@@ -1,0 +1,32 @@
+# Copyright 1999-2024 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit elisp toolchain-funcs
+
+DESCRIPTION="Emacs front-end to mpg123 audio player and OggVorbis audio player"
+HOMEPAGE="https://www.gentei.org/~yuuji/software/mpg123el/"
+SRC_URI="https://dev.gentoo.org/~ulm/distfiles/${P}.tar.xz"
+
+LICENSE="mpg123-el"
+SLOT="0"
+KEYWORDS="~amd64 ~ppc ~x86"
+
+RDEPEND="media-sound/mpg123
+	media-sound/alsa-utils"
+
+PATCHES=( "${FILESDIR}/${PN}-1.61-stdlib.patch" )
+SITEFILE="50${PN}-gentoo.el"
+
+src_compile() {
+	$(tc-getCC) ${CFLAGS} ${CPPFLAGS} ${LDFLAGS} -o tagput tagput.c || die
+	$(tc-getCC) ${CFLAGS} ${CPPFLAGS} ${LDFLAGS} -o id3put id3put.c || die
+	elisp-compile *.el
+}
+
+src_install() {
+	dobin tagput id3put
+	elisp-install ${PN} *.el *.elc
+	elisp-site-file-install "${FILESDIR}/${SITEFILE}"
+}

app-emacs/mpg123-el/Manifest
@@ -1,1 +1,2 @@
 DIST mpg123-el-1.61.tar.xz 34216 BLAKE2B 3f0f85bbf0f5854b899aefb443f9b2b011dd5dd85c815fc8be723bade93be805b121e952cab3c56a3897bed41fa65988331c004537e565abcb660b6dd402a0c5 SHA512 96b50a8fc945c6811bce7b8cfa28fe235f2d2ddf9cbd1904c94f272f6df13a991761dbb4f186c1bda04720fb50115515361dbff4838e7623261719f2ecc6698e
+DIST mpg123-el-1.65.tar.xz 35716 BLAKE2B d70d91d9eacb5ea7199746367d5748ece0faf406c4c7380972bb92b345396430e74ae0047f6a0590783f6cf6452556b11011a5656a8214cfdf2a664d6605b548 SHA512 d8891ff7187dea07a2d4e768504ec8936d49c1367b1d9fd00a294a74f2f489497f38feb2725e756f4d3c9801bb83a93fd063552e6943378b5b498e18d6f03f40

commit 1bdd628faee21490d4918bb7959d4968485d5f81

app-emacs/mpg123-el: Actually fix the compile failure
Fixes: 5daa764a2181d57551f5fa9db558a528dc302455
Closes: https://bugs.gentoo.org/946145
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

app-emacs/mpg123-el/mpg123-el-1.65.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-inherit elisp toolchain-funcs
+inherit elisp flag-o-matic toolchain-funcs
 
 DESCRIPTION="Emacs front-end to mpg123 audio player and OggVorbis audio player"
 HOMEPAGE="https://www.gentei.org/~yuuji/software/mpg123el/"
@@ -20,6 +20,7 @@ PATCHES=( "${FILESDIR}/${PN}-1.61-stdlib.patch" )
 SITEFILE="50${PN}-gentoo.el"
 
 src_compile() {
+	append-flags -std=gnu17
 	$(tc-getCC) ${CFLAGS} ${CPPFLAGS} ${LDFLAGS} -o tagput tagput.c || die
 	$(tc-getCC) ${CFLAGS} ${CPPFLAGS} ${LDFLAGS} -o id3put id3put.c || die
 	elisp-compile *.el