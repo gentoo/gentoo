gentoo/gentoo

commit 3ab36cc3531af8a39c1b28bc30f8b724b9242949

toolchain.eclass: don't build with LTO for USE=ada && < gcc-12
There's another -Wlto-type-mismatch issue in GCC 11 with USE=ada and
the patch we used in >=12 doesn't apply cleanly there, not worth spending
the time on for an EOL branch.

Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -1323,8 +1323,13 @@ toolchain_src_configure() {
 	confgcc+=( --enable-lto )
 
 	# Build compiler itself using LTO
-	if tc_version_is_at_least 9.1 && _tc_use_if_iuse lto ; then
-		BUILD_CONFIG_TARGETS+=( bootstrap-lto )
+	if tc_use_if_iuse lto ; then
+		# GCC 11 at least has a -Wlto-type-mismatch issue with Ada
+		if ! tc_version_is_at_least 12.1 && is_ada ; then
+			:;
+		elif tc_version_is_at_least 9.1 ; then
+			BUILD_CONFIG_TARGETS+=( bootstrap-lto )
+		fi
 	fi
 
 	if tc_version_is_at_least 12 && _tc_use_if_iuse cet && [[ -z ${CLANG_DISABLE_CET_HACK} && ${CTARGET} == x86_64-*-gnu* ]] ; then

commit 4490fff1cd5ba88de79a9e956b91241f2a1fecaa

toolchain.eclass: fix _tc_use_if_iuse typo
Fixes: 3ab36cc3531af8a39c1b28bc30f8b724b9242949
Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -1323,7 +1323,7 @@ toolchain_src_configure() {
 	confgcc+=( --enable-lto )
 
 	# Build compiler itself using LTO
-	if tc_use_if_iuse lto ; then
+	if _tc_use_if_iuse lto ; then
 		# GCC 11 at least has a -Wlto-type-mismatch issue with Ada
 		if ! tc_version_is_at_least 12.1 && is_ada ; then
 			:;