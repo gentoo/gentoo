f886d86a8aea1de5f06f02550ec7bde9eb0a4c48
Arsen Arsenović <arsen@gentoo.org>
2025-01-07 20:56:24 +0100

    app-emacs/gruvbox-theme: add 1.30.2
    Signed-off-by: Arsen Arsenović <arsen@gentoo.org>

--- app-emacs/gruvbox-theme/files/50gruvbox-theme-gentoo.el
+++ app-emacs/gruvbox-theme/files/50gruvbox-theme-gentoo.el
@@ -1,2 +1,4 @@
 ;;; app-emacs/gruvbox-theme site configuration  -*- lexical-binding: t; -*-
 (add-to-list 'load-path "@SITELISP@")
+(when (boundp 'custom-theme-load-path)
+  (add-to-list 'custom-theme-load-path "@SITELISP@"))
