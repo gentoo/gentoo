gentoo/gentoo

commit fa52f3b5e558e216c37146a12587de447cb9eee7

x11-wm/mutter: fix empty(!) RDEPEND
This has been there for several weeks now.

Signed-off-by: Sam James <sam@gentoo.org>

x11-wm/mutter/mutter-46.7-r1.ebuild
@@ -33,8 +33,8 @@ RESTRICT="!test? ( test )"
 # this ensures have_enable_ei_portal is always there in xwayland.pc, which affects how Xwayland is launched, thus if it were toggled off
 # in Xwayland after mutter is installed, Xwayland would fail to be started by mutter. mutter already hard-depends on libei, so there's
 # really no extra deps here (besides xdg-desktop-portal, but we want that too, anyhow).
-# v3.32.2 has many excessive or unused *_req variables declared, thus currently the dep order ignores those and goes via dependency() call order
-DEPEND="
+# v3.32.2 has many excessive or unused *_req variables declared, thus currently the dep order ignores those and goes via wdependency() call order
+RDEPEND="
 	>=media-libs/graphene-1.10.2[introspection?]
 	x11-libs/gdk-pixbuf:2
 	>=x11-libs/pango-1.46[introspection?]
@@ -90,7 +90,7 @@ DEPEND="
 "
 # for now upstream has "have_x11 = true" in the meson.build, but sooner or later upstream is going to make X optional.
 #	X? (
-DEPEND+="
+RDEPEND+="
 		>=gui-libs/gtk-4.0.0:4[X,introspection?]
 		>=x11-libs/libX11-1.7.0
 		>=x11-libs/libXcomposite-0.4
@@ -110,8 +110,7 @@ DEPEND+="
 		x11-libs/libXau
 "
 #	)"
-
-DEPEND="${DEPEND}
+DEPEND="${RDEPEND}
 	x11-base/xorg-proto
 	sysprof? ( >=dev-util/sysprof-common-3.38.0 )
 "

x11-wm/mutter/mutter-46.4-r1.ebuild
@@ -34,7 +34,7 @@ RESTRICT="!test? ( test )"
 # in Xwayland after mutter is installed, Xwayland would fail to be started by mutter. mutter already hard-depends on libei, so there's
 # really no extra deps here (besides xdg-desktop-portal, but we want that too, anyhow).
 # v3.32.2 has many excessive or unused *_req variables declared, thus currently the dep order ignores those and goes via dependency() call order
-DEPEND="
+RDEPEND="
 	>=media-libs/graphene-1.10.2[introspection?]
 	x11-libs/gdk-pixbuf:2
 	>=x11-libs/pango-1.46[introspection?]
@@ -90,7 +90,7 @@ DEPEND="
 "
 # for now upstream has "have_x11 = true" in the meson.build, but sooner or later upstream is going to make X optional.
 #	X? (
-DEPEND+="
+RDEPEND+="
 		>=gui-libs/gtk-4.0.0:4[X,introspection?]
 		>=x11-libs/libX11-1.7.0
 		>=x11-libs/libXcomposite-0.4
@@ -111,7 +111,7 @@ DEPEND+="
 "
 #	)"
 
-DEPEND="${DEPEND}
+DEPEND="${RDEPEND}
 	x11-base/xorg-proto
 	sysprof? ( >=dev-util/sysprof-common-3.38.0 )
 "

x11-wm/mutter/mutter-9999.ebuild
@@ -34,7 +34,7 @@ RESTRICT="!test? ( test )"
 # in Xwayland after mutter is installed, Xwayland would fail to be started by mutter. mutter already hard-depends on libei, so there's
 # really no extra deps here (besides xdg-desktop-portal, but we want that too, anyhow).
 # v3.32.2 has many excessive or unused *_req variables declared, thus currently the dep order ignores those and goes via dependency() call order
-DEPEND="
+RDEPEND="
 	>=media-libs/graphene-1.10.2[introspection?]
 	x11-libs/gdk-pixbuf:2
 	>=x11-libs/pango-1.46[introspection?]
@@ -90,7 +90,7 @@ DEPEND="
 "
 # for now upstream has "have_x11 = true" in the meson.build, but sooner or later upstream is going to make X optional.
 #	X? (
-DEPEND+="
+RDEPEND+="
 		>=gui-libs/gtk-4.0.0:4[X,introspection?]
 		>=x11-libs/libX11-1.7.0
 		>=x11-libs/libXcomposite-0.4
@@ -111,7 +111,7 @@ DEPEND+="
 "
 #	)"
 
-DEPEND="${DEPEND}
+DEPEND="${RDEPEND}
 	x11-base/xorg-proto
 	sysprof? ( >=dev-util/sysprof-common-3.38.0 )
 "

commit 08e6b29645565e99e1be19c03efad3c91bb2871c

x11-wm/mutter: fix 'dependency' typo in comment
Fixes: fa52f3b5e558e216c37146a12587de447cb9eee7
Signed-off-by: Sam James <sam@gentoo.org>

x11-wm/mutter/mutter-46.7-r1.ebuild
@@ -33,7 +33,7 @@ RESTRICT="!test? ( test )"
 # this ensures have_enable_ei_portal is always there in xwayland.pc, which affects how Xwayland is launched, thus if it were toggled off
 # in Xwayland after mutter is installed, Xwayland would fail to be started by mutter. mutter already hard-depends on libei, so there's
 # really no extra deps here (besides xdg-desktop-portal, but we want that too, anyhow).
-# v3.32.2 has many excessive or unused *_req variables declared, thus currently the dep order ignores those and goes via wdependency() call order
+# v3.32.2 has many excessive or unused *_req variables declared, thus currently the dep order ignores those and goes via dependency() call order
 RDEPEND="
 	>=media-libs/graphene-1.10.2[introspection?]
 	x11-libs/gdk-pixbuf:2