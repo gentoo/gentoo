gentoo/gentoo

commit 2a74cf2a821e39c1ab316bde82221fb41b913391

dev-util/makeheaders: add 0_p20240823
Signed-off-by: Eric Joldasov <bratishkaerik@landless-city.net>
Signed-off-by: Sam James <sam@gentoo.org>

dev-util/makeheaders/makeheaders-0_p20240823.ebuild
@@ -1,0 +1,42 @@
+# Copyright 1999-2024 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit edo toolchain-funcs
+
+# https://fossil-scm.org/home/finfo?name=tools/makeheaders.c
+# 2024-08-23
+ARTIFACT_C="5f464a5a25ae45978e49e1cc230cc4c6f2fddc8f2f45a044fff02dc074a0bcf6"
+# https://fossil-scm.org/home/finfo?name=tools/makeheaders.html
+# 2022-03-21
+ARTIFACT_HTML="262696252dc50250c896c90cc240dcd946614b9c7727902aa7606640507e9231"
+
+DESCRIPTION="Tool that automatically generates .h files for a C or C++ programming project"
+HOMEPAGE="https://www.hwaci.com/sw/mkhdr/"
+
+SRC_URI="
+	https://fossil-scm.org/home/raw/${ARTIFACT_C}?at=makeheaders.c -> makeheaders-${ARTIFACT_C}.c
+	https://fossil-scm.org/home/raw/${ARTIFACT_HTML}?at=makeheaders.html  -> makeheaders-${ARTIFACT_HTML}.html
+"
+
+LICENSE="BSD-2"
+SLOT="0"
+KEYWORDS="~amd64 ~ppc ~sparc ~x86"
+
+HTML_DOCS=( "makeheaders.html" )
+
+src_unpack() {
+	mkdir "${S}" || die
+	cp "${DISTDIR}"/makeheaders-${ARTIFACT_C}.c "${S}/makeheaders.c" || die
+	cp "${DISTDIR}"/makeheaders-${ARTIFACT_HTML}.html "${S}/makeheaders.html" || die
+}
+
+src_compile() {
+	edo $(tc-getCC) ${CFLAGS} ${LDFLAGS} -o makeheaders makeheaders.c
+}
+
+src_install() {
+	dobin makeheaders
+	einstalldocs
+}

dev-util/makeheaders/Manifest
@@ -1,2 +1,3 @@
 DIST makeheaders-262696252dc50250c896c90cc240dcd946614b9c7727902aa7606640507e9231.html 40335 BLAKE2B 6eec5080226d7759e8bdf97b2238fb33e85700d29b0b539fcb797fa219d9d00461aafdabf2642e2cf1a622cf633cf889d71381bb3342113736ddfdafca3582de SHA512 6cccd5388e8af6f814d735c643abb231dab21a16799fba080858b15885a77b99bf37a5c7bac72958bbfcacea2ed54635ccc6323f5c7d47e2a1a73fffa8952504
+DIST makeheaders-5f464a5a25ae45978e49e1cc230cc4c6f2fddc8f2f45a044fff02dc074a0bcf6.c 101217 BLAKE2B f50aea6f62276ba0572e3dfc060f1062159f1908134069284eef5483131daea3afb665586c062f4aaed6759998e6ac2db9c4f14f7eb8a52949a921d137311cc2 SHA512 0520687a268dcff1c7e1e78c95bb8e13303bc78153ff5c4e543b2fbbc8a03dd869b51b32b50e1adbed8622c05e5f1a12f462b9eab2c800bb804c0b1de1d089a0
 DIST makeheaders-cae894dc3611ee6e0fba4100c0e377fcd3bbc6d8ddb91c997939ce439c50cee2.c 101039 BLAKE2B bff701e0230351b268591c24b3ae255916c0ffcb740375f9029d706b41a4c2a091330d74eee9ff687ff2237b53ebd94913a2e43213315fb01ec862c158b01b63 SHA512 0673648b1d3bb0e665edf05dd8c3075e4e4fa52acd1a051caa1018680d4969d5189c4750d7c36d59ddaa79646432cb75fcfec8181eabca804c56d81b4d8f6600

commit f6556f1aeba114415a3c831129a8dd389d17d3e3

dev-util/makeheaders: get CPPFLAGS back
Fixes: 2a74cf2a821e39c1ab316bde82221fb41b913391
Signed-off-by: Eric Joldasov <bratishkaerik@landless-city.net>

dev-util/makeheaders/makeheaders-0_p20240823.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -33,7 +33,7 @@ src_unpack() {
 }
 
 src_compile() {
-	edo $(tc-getCC) ${CFLAGS} ${LDFLAGS} -o makeheaders makeheaders.c
+	edo $(tc-getCC) ${CFLAGS} ${CPPFLAGS} ${LDFLAGS} -o makeheaders makeheaders.c
 }
 
 src_install() {