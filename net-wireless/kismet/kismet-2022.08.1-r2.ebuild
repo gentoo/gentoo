gentoo/gentoo

commit eac4803723e269b65bed0847b3e57924d347d340

net-wireless/kismet: fixup deps
Adjust deps per qa tools
Remove do nothing sed lines
Use bundled libfmt
Closes: https://bugs.gentoo.org/872608
Signed-off-by: Rick Farina <zerochaos@gentoo.org>

net-wireless/kismet/kismet-2022.08.1-r2.ebuild
@@ -43,7 +43,6 @@ CDEPEND="
 	networkmanager? ( net-misc/networkmanager )
 	dev-libs/glib:2
 	dev-libs/elfutils
-	dev-libs/openssl:=
 	sys-libs/zlib:=
 	dev-db/sqlite:3
 	net-libs/libwebsockets:=[client,lejp]
@@ -84,6 +83,10 @@ DEPEND="${CDEPEND}
 "
 BDEPEND="virtual/pkgconfig"
 
+# https://bugs.gentoo.org/872608
+# drop after 2022.08*
+PATCHES=( "${FILESDIR}/${P}-sandbox-fix.patch" )
+
 src_prepare() {
 	#sed -i -e "s:^\(logtemplate\)=\(.*\):\1=/tmp/\2:" \
 	#	conf/kismet_logging.conf || die
@@ -112,12 +115,16 @@ src_prepare() {
 	if [ "${PV}" = "9999" ]; then
 		eautoreconf
 	fi
+	# drop after 2022.08*
+	# VERSION was incorrectly removed in 4e490cf0b49a287e964df9c5e5c4067f6918909e upstream
+	# https://github.com/kismetwireless/kismet/issues/427
+	# https://bugs.gentoo.org/864298
+	echo "${PV}" > VERSION
 }
 
 src_configure() {
 	econf \
 		$(use_enable libusb libusb) \
-		$(use_enable libusb wifi-coconut) \
 		$(use_enable pcre) \
 		$(use_enable lm-sensors lmsensors) \
 		$(use_enable networkmanager libnm) \

net-wireless/kismet/files/kismet-2022.08.1-sandbox-fix.patch
@@ -1,0 +1,147 @@
+From 8264835a935de9c754c0ff28c632695103b2dc2f Mon Sep 17 00:00:00 2001
+From: Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
+Date: Fri, 6 Jan 2023 15:22:52 -0500
+Subject: [PATCH] python: Merge patch from Rick Farina / Zero_Chaos to make
+ gentoo not complain about python build parameters
+
+---
+ capture_bt_geiger/Makefile.in        | 4 +++-
+ capture_freaklabs_zigbee/Makefile.in | 4 +++-
+ capture_proxy_adsb/Makefile.in       | 4 +++-
+ capture_sdr_rtl433/Makefile.in       | 4 +++-
+ capture_sdr_rtladsb/Makefile.in      | 4 +++-
+ capture_sdr_rtlamr/Makefile.in       | 4 +++-
+ 6 files changed, 18 insertions(+), 6 deletions(-)
+
+diff --git a/capture_bt_geiger/Makefile.in b/capture_bt_geiger/Makefile.in
+index 646069fed..4f644535b 100644
+--- a/capture_bt_geiger/Makefile.in
++++ b/capture_bt_geiger/Makefile.in
+@@ -2,6 +2,8 @@ KIS_SRC_DIR ?= ..
+ 
+ include $(KIS_SRC_DIR)/Makefile.inc
+ 
++DESTDIR ?= /
++
+ DATASOURCE_NAME := $(shell $(PYTHON) setup.py --name)
+ 
+ PROTOBUF_DIR = $(KIS_SRC_DIR)/protobuf_definitions
+@@ -19,7 +21,7 @@ $(DATASOURCE_NAME)/kismetexternal/%_pb2.py: $(PROTOBUF_DIR)/%.proto
+ 	sed -i -E 's/^import kismet_/from . import kismet_/' $@
+ 
+ install:
+-	$(PYTHON) setup.py install --root="/$(DESTDIR)" --prefix="$(prefix)"
++	$(PYTHON) setup.py install --root="$(DESTDIR)" --prefix="$(prefix)"
+ 
+ clean:
+ 	@-$(PYTHON) setup.py clean
+diff --git a/capture_freaklabs_zigbee/Makefile.in b/capture_freaklabs_zigbee/Makefile.in
+index 646069fed..4f644535b 100644
+--- a/capture_freaklabs_zigbee/Makefile.in
++++ b/capture_freaklabs_zigbee/Makefile.in
+@@ -2,6 +2,8 @@ KIS_SRC_DIR ?= ..
+ 
+ include $(KIS_SRC_DIR)/Makefile.inc
+ 
++DESTDIR ?= /
++
+ DATASOURCE_NAME := $(shell $(PYTHON) setup.py --name)
+ 
+ PROTOBUF_DIR = $(KIS_SRC_DIR)/protobuf_definitions
+@@ -19,7 +21,7 @@ $(DATASOURCE_NAME)/kismetexternal/%_pb2.py: $(PROTOBUF_DIR)/%.proto
+ 	sed -i -E 's/^import kismet_/from . import kismet_/' $@
+ 
+ install:
+-	$(PYTHON) setup.py install --root="/$(DESTDIR)" --prefix="$(prefix)"
++	$(PYTHON) setup.py install --root="$(DESTDIR)" --prefix="$(prefix)"
+ 
+ clean:
+ 	@-$(PYTHON) setup.py clean
+diff --git a/capture_proxy_adsb/Makefile.in b/capture_proxy_adsb/Makefile.in
+index d16ba601f..96ba6ceca 100644
+--- a/capture_proxy_adsb/Makefile.in
++++ b/capture_proxy_adsb/Makefile.in
+@@ -2,6 +2,8 @@ KIS_SRC_DIR ?= ..
+ 
+ include $(KIS_SRC_DIR)/Makefile.inc
+ 
++DESTDIR ?= /
++
+ DATASOURCE_NAME := $(shell $(PYTHON) setup.py --name)
+ 
+ PROTOBUF_DIR = $(KIS_SRC_DIR)/protobuf_definitions
+@@ -22,7 +24,7 @@ $(DATASOURCE_NAME)/kismetexternal/%_pb2.py: $(PROTOBUF_DIR)/%.proto
+ 	sed -i -E 's/^import kismet_/from . import kismet_/' $@
+ 
+ install:
+-	$(PYTHON) setup.py install --root="/$(DESTDIR)" --prefix="$(prefix)"
++	$(PYTHON) setup.py install --root="$(DESTDIR)" --prefix="$(prefix)"
+ 
+ clean:
+ 	@-$(PYTHON) setup.py clean
+diff --git a/capture_sdr_rtl433/Makefile.in b/capture_sdr_rtl433/Makefile.in
+index 646069fed..4f644535b 100644
+--- a/capture_sdr_rtl433/Makefile.in
++++ b/capture_sdr_rtl433/Makefile.in
+@@ -2,6 +2,8 @@ KIS_SRC_DIR ?= ..
+ 
+ include $(KIS_SRC_DIR)/Makefile.inc
+ 
++DESTDIR ?= /
++
+ DATASOURCE_NAME := $(shell $(PYTHON) setup.py --name)
+ 
+ PROTOBUF_DIR = $(KIS_SRC_DIR)/protobuf_definitions
+@@ -19,7 +21,7 @@ $(DATASOURCE_NAME)/kismetexternal/%_pb2.py: $(PROTOBUF_DIR)/%.proto
+ 	sed -i -E 's/^import kismet_/from . import kismet_/' $@
+ 
+ install:
+-	$(PYTHON) setup.py install --root="/$(DESTDIR)" --prefix="$(prefix)"
++	$(PYTHON) setup.py install --root="$(DESTDIR)" --prefix="$(prefix)"
+ 
+ clean:
+ 	@-$(PYTHON) setup.py clean
+diff --git a/capture_sdr_rtladsb/Makefile.in b/capture_sdr_rtladsb/Makefile.in
+index d16ba601f..96ba6ceca 100644
+--- a/capture_sdr_rtladsb/Makefile.in
++++ b/capture_sdr_rtladsb/Makefile.in
+@@ -2,6 +2,8 @@ KIS_SRC_DIR ?= ..
+ 
+ include $(KIS_SRC_DIR)/Makefile.inc
+ 
++DESTDIR ?= /
++
+ DATASOURCE_NAME := $(shell $(PYTHON) setup.py --name)
+ 
+ PROTOBUF_DIR = $(KIS_SRC_DIR)/protobuf_definitions
+@@ -22,7 +24,7 @@ $(DATASOURCE_NAME)/kismetexternal/%_pb2.py: $(PROTOBUF_DIR)/%.proto
+ 	sed -i -E 's/^import kismet_/from . import kismet_/' $@
+ 
+ install:
+-	$(PYTHON) setup.py install --root="/$(DESTDIR)" --prefix="$(prefix)"
++	$(PYTHON) setup.py install --root="$(DESTDIR)" --prefix="$(prefix)"
+ 
+ clean:
+ 	@-$(PYTHON) setup.py clean
+diff --git a/capture_sdr_rtlamr/Makefile.in b/capture_sdr_rtlamr/Makefile.in
+index 646069fed..4f644535b 100644
+--- a/capture_sdr_rtlamr/Makefile.in
++++ b/capture_sdr_rtlamr/Makefile.in
+@@ -2,6 +2,8 @@ KIS_SRC_DIR ?= ..
+ 
+ include $(KIS_SRC_DIR)/Makefile.inc
+ 
++DESTDIR ?= /
++
+ DATASOURCE_NAME := $(shell $(PYTHON) setup.py --name)
+ 
+ PROTOBUF_DIR = $(KIS_SRC_DIR)/protobuf_definitions
+@@ -19,7 +21,7 @@ $(DATASOURCE_NAME)/kismetexternal/%_pb2.py: $(PROTOBUF_DIR)/%.proto
+ 	sed -i -E 's/^import kismet_/from . import kismet_/' $@
+ 
+ install:
+-	$(PYTHON) setup.py install --root="/$(DESTDIR)" --prefix="$(prefix)"
++	$(PYTHON) setup.py install --root="$(DESTDIR)" --prefix="$(prefix)"
+ 
+ clean:
+ 	@-$(PYTHON) setup.py clean

commit a5b9bea7e546a4101b1e674c311c729004a7e268

net-wireless/kismet: fix applying patches
Otherwise, we only apply user patches, not the ones in PATCHES(!)

Bug: https://bugs.gentoo.org/872608
Fixes: eac4803723e269b65bed0847b3e57924d347d340
Signed-off-by: Sam James <sam@gentoo.org>

net-wireless/kismet/kismet-2022.08.1-r2.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -110,7 +110,7 @@ src_prepare() {
 	#	log_tools/kismetdb_to_wiglecsv.cc trackedcomponent.h \
 	#	trackedelement.h trackedelement_workers.h
 
-	eapply_user
+	default
 
 	if [ "${PV}" = "9999" ]; then
 		eautoreconf