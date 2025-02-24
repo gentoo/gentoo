gentoo/gentoo

commit 8fcd2710762948cc3f8ad1b060ec86a43e6c35e6

mail-filter/opensmtpd-filter-dkimsign: support ed25519
Closes: https://bugs.gentoo.org/949031
Signed-off-by: Cédric Krier <cedk@gentoo.org>

mail-filter/opensmtpd-filter-dkimsign/files/opensmtpd-filter-dkimsign-0.6-ed25519.patch
@@ -1,0 +1,85 @@
+commit 07480b90f4ee9246fdc55bbcdd0370c031923420
+from: Martijn van Duren <martijn@openbsd.org>
+date: Thu Dec 15 11:16:37 2022 UTC
+
+Remove HAVE_ED25519 now that we have ED25519 in LibreSSL
+
+commit - 60e9f82dc81c599272bc0061157850239f54ae10
+commit + 07480b90f4ee9246fdc55bbcdd0370c031923420
+blob - 19746f9f6e583080de34e95de1b6cf3d9929833d
+blob + 058d1919ba16827a0f2c32e04405586ccb316117
+--- Makefile
++++ Makefile
+@@ -7,9 +7,6 @@ MANDIR=		${LOCALBASE}/man/man
+ 
+ SRCS+=		main.c mheader.c
+ 
+-.ifdef HAVE_ED25519
+-CFLAGS+=	-DHAVE_ED25519
+-.endif
+ .ifdef LIBCRYPTOPC
+ CRYPT_CFLAGS!=	pkg-config --cflags ${LIBCRYPTOPC}
+ CRYPT_LDFLAGS_L!=pkg-config --libs-only-L ${LIBCRYPTOPC}
+blob - 1f97bd28f4172df60f52b0b2bae40bc4fa0817a0
+blob + 011d047d5401c07146103693f7b58fc5289bdf94
+--- Makefile.gnu
++++ Makefile.gnu
+@@ -7,9 +7,6 @@ MANDIR=		${LOCALBASE}/share/man/man8
+ 
+ SRCS+=		main.c mheader.c
+ 
+-ifdef HAVE_ED25519
+-CFLAGS+=	-DHAVE_ED25519
+-endif
+ ifdef LIBCRYPTOPC
+ CRYPT_CFLAGS!=	pkg-config --cflags ${LIBCRYPTOPC}
+ CRYPT_LDFLAGS_L!=pkg-config --libs-only-L ${LIBCRYPTOPC}
+blob - 2961793635330ed82f566e8870166a7f012e7e7a
+blob + 8b2d2131ea121f527e174e6b59a4bdde8f844193
+--- main.c
++++ main.c
+@@ -136,13 +136,11 @@ main(int argc, char *argv[])
+ 				hashalg = optarg + 4;
+ 				keyid = EVP_PKEY_RSA;
+ 				sephash = 0;
+-#ifdef HAVE_ED25519
+ 			} else if (strncmp(optarg, "ed25519-", 8) == 0) {
+ 				hashalg = optarg + 8;
+ 				cryptalg = "ed25519";
+ 				keyid = EVP_PKEY_ED25519;
+ 				sephash = 1;
+-#endif
+ 			} else
+ 				osmtpd_errx(1, "invalid algorithm");
+ 			break;
+@@ -696,7 +694,6 @@ dkim_sign(struct osmtpd_ctx *ctx)
+ 			dkim_errx(message, "Can't finalize signature context");
+ 			goto fail;
+ 		}
+-#ifdef HAVE_ED25519
+ 	} else {
+ 		if (EVP_DigestFinal_ex(message->dctx, bdigest,
+ 		    &digestsz) != 1) {
+@@ -715,7 +712,6 @@ dkim_sign(struct osmtpd_ctx *ctx)
+ 			dkim_errx(message, "Failed to finalize signature");
+ 			goto fail;
+ 		}
+-#endif
+ 	}
+ 	if ((tmp = malloc(linelen)) == NULL) {
+ 		dkim_err(message, "Can't allocate space for signature");
+@@ -726,14 +722,12 @@ dkim_sign(struct osmtpd_ctx *ctx)
+ 			dkim_errx(message, "Failed to finalize signature");
+ 			goto fail;
+ 		}
+-#ifdef HAVE_ED25519
+ 	} else {
+ 		if (EVP_DigestSign(message->dctx, tmp, &linelen, bdigest,
+ 		    digestsz) != 1) {
+ 			dkim_errx(message, "Failed to finalize signature");
+ 			goto fail;
+ 		}
+-#endif
+ 	}
+ 	if ((b = malloc((((linelen + 2) / 3) * 4) + 1)) == NULL) {
+ 		dkim_err(message, "Can't create DKIM signature");

mail-filter/opensmtpd-filter-dkimsign/opensmtpd-filter-dkimsign-0.6-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 2024 Gentoo Authors
+# Copyright 2024-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -17,6 +17,9 @@ DEPEND="
 	dev-libs/openssl
 	"
 RDEPEND="${DEPEND}"
+PATCHES=(
+	"${FILESDIR}/${PN}-ed25519.patch"
+)
 
 src_prepare() {
 	mv -f Makefile.gnu Makefile

commit 5d94f3a89b3f491b078a58f29ba9445d78a06433

mail-filter/opensmtpd-filter-dkimsign: fix src_prepare (and patch)
The ebuild calls 'eapply_user' in src_prepare rather than 'default'
which would do both "eapply on all PATCHES" *and* "eapply_user".

The filename in the ebuild also wasn't right.

Unfortunately, the patch also couldn't then apply because it has
the wrong patch-level, so fixed that too by making it -p1-compliant.

Closes: https://bugs.gentoo.org/949031
Fixes: 8fcd2710762948cc3f8ad1b060ec86a43e6c35e6
Signed-off-by: Sam James <sam@gentoo.org>

mail-filter/opensmtpd-filter-dkimsign/files/opensmtpd-filter-dkimsign-0.6-ed25519.patch
@@ -8,8 +8,8 @@ commit - 60e9f82dc81c599272bc0061157850239f54ae10
 commit + 07480b90f4ee9246fdc55bbcdd0370c031923420
 blob - 19746f9f6e583080de34e95de1b6cf3d9929833d
 blob + 058d1919ba16827a0f2c32e04405586ccb316117
---- Makefile
-+++ Makefile
+--- a/Makefile
++++ b/Makefile
 @@ -7,9 +7,6 @@ MANDIR=		${LOCALBASE}/man/man
  
  SRCS+=		main.c mheader.c
@@ -22,8 +22,8 @@ blob + 058d1919ba16827a0f2c32e04405586ccb316117
  CRYPT_LDFLAGS_L!=pkg-config --libs-only-L ${LIBCRYPTOPC}
 blob - 1f97bd28f4172df60f52b0b2bae40bc4fa0817a0
 blob + 011d047d5401c07146103693f7b58fc5289bdf94
---- Makefile.gnu
-+++ Makefile.gnu
+--- a/Makefile.gnu
++++ b/Makefile.gnu
 @@ -7,9 +7,6 @@ MANDIR=		${LOCALBASE}/share/man/man8
  
  SRCS+=		main.c mheader.c
@@ -36,8 +36,8 @@ blob + 011d047d5401c07146103693f7b58fc5289bdf94
  CRYPT_LDFLAGS_L!=pkg-config --libs-only-L ${LIBCRYPTOPC}
 blob - 2961793635330ed82f566e8870166a7f012e7e7a
 blob + 8b2d2131ea121f527e174e6b59a4bdde8f844193
---- main.c
-+++ main.c
+--- a/main.c
++++ b/main.c
 @@ -136,13 +136,11 @@ main(int argc, char *argv[])
  				hashalg = optarg + 4;
  				keyid = EVP_PKEY_RSA;

mail-filter/opensmtpd-filter-dkimsign/opensmtpd-filter-dkimsign-0.6-r2.ebuild
@@ -20,12 +20,12 @@ DEPEND="
 	"
 RDEPEND="${DEPEND}"
 PATCHES=(
-	"${FILESDIR}/${PN}-ed25519.patch"
+	"${FILESDIR}/${PN}-0.6-ed25519.patch"
 )
 
 src_prepare() {
-	mv -f Makefile.gnu Makefile
-	eapply_user
+	default
+	mv -f Makefile.gnu Makefile || die
 }
 
 src_compile() {