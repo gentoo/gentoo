df5f2d54a6e4b685329a6948239f4574cd8eb679
Anna (navi) Figueiredo Gomes <navi@vlhl.dev>
2025-02-28 15:39:13 +0100

    sys-apps/dbus: remove profile.d script for now
    Some tools (namely plasma) don't check if the script is valid, and then
    deadlocks. Once we have more robust login mechanisms we can re-introduce
    those.
    
    Signed-off-by: Anna (navi) Figueiredo Gomes <navi@vlhl.dev>
    Signed-off-by: Sam James <sam@gentoo.org>

--- sys-apps/dbus/files/dbus.sh
+++
@@ -1,7 +0,0 @@
-# Copyright 1999-2025 Gentoo Authors
-# Distributed under the terms of the GNU General Public License, v2 or later
-#
-if [ -f /run/openrc/softlevel ] && [ -z "$DBUS_SESSION_BUS_ADDRESS" ] \
-		&& rc-service --user --exists dbus 2>&1 >/dev/null; then
-	export DBUS_SESSION_BUS_ADDRESS="unix:path=${XDG_RUNTIME_DIR}/bus"
-fi
