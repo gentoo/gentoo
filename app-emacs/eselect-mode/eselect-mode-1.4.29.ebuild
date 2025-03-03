gentoo/gentoo

commit 94c499cd3a4ae79cd9711bdb589040d95427696d

app-emacs/eselect-mode: Stabilize 1.4.29 allarches, #950493
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

app-emacs/eselect-mode/eselect-mode-1.4.29.ebuild
@@ -18,6 +18,6 @@ S="${WORKDIR}/${MY_P}/misc"
 
 LICENSE="GPL-2+"
 SLOT="0"
-KEYWORDS="~alpha amd64 arm arm64 ~hppa ~loong ~m68k ~mips ~ppc ~ppc64 ~riscv sparc x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos"
+KEYWORDS="~alpha amd64 arm arm64 hppa ~m68k ~mips ppc ppc64 ~riscv sparc x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos"
 
 SITEFILE="50${PN}-gentoo.el"

commit 53eed60501360f00bfe92a52747e6ca9fd57c11f

app-emacs/eselect-mode: Restore accidentally dropped keyword ~loong
Fixes: 94c499cd3a4ae79cd9711bdb589040d95427696d
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

app-emacs/eselect-mode/eselect-mode-1.4.29.ebuild
@@ -18,6 +18,6 @@ S="${WORKDIR}/${MY_P}/misc"
 
 LICENSE="GPL-2+"
 SLOT="0"
-KEYWORDS="~alpha amd64 arm arm64 hppa ~m68k ~mips ppc ppc64 ~riscv sparc x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos"
+KEYWORDS="~alpha amd64 arm arm64 hppa ~loong ~m68k ~mips ppc ppc64 ~riscv sparc x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos"
 
 SITEFILE="50${PN}-gentoo.el"