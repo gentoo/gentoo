f466363886fcb1d391cffd6cc39628cd2bfaf023
Joonas Niilola <juippis@gentoo.org>
2025-03-03 17:40:15 +0200

    www-client/firefox-bin: add 136.0
     - disable pref "permissions.manager.remote.enabled" by default - can be
       enabled by corporations managing their browsers via remote-settings.
    
    Signed-off-by: Joonas Niilola <juippis@gentoo.org>

--- www-client/firefox-bin/files/gentoo-default-prefs.js
+++ www-client/firefox-bin/files/gentoo-default-prefs.js
@@ -9,3 +9,6 @@ pref("intl.locale.requested",              "");
 pref("network.trr.mode",                   5);
 /* Disable use of Mozilla Normandy service by default */
 pref("app.normandy.enabled",               false);
+/* Disable remote-settings from permissions manager by default but don't lock it
+   so corporations can easily turn it back on if there's demand for that */
+pref("permissions.manager.remote.enabled", false);
