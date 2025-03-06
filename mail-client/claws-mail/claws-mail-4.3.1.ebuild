gentoo/gentoo

commit ca41779483c816888406db7c67656d5d33579ba3

mail-client/claws-mail: 4.3.1 bump, add Ayatana indicator support
This drops alpha and hppa for now (not keyworded in new dep)
Sync live ebuild

Closes: https://bugs.gentoo.org/945485
Signed-off-by: Bernard Cafarelli <voyageur@gentoo.org>

mail-client/claws-mail/claws-mail-4.3.1.ebuild
@@ -5,7 +5,7 @@ EAPI=8
 
 PYTHON_COMPAT=( python3_{10..13} )
 
-inherit autotools desktop python-single-r1 xdg
+inherit desktop python-single-r1 xdg
 
 DESCRIPTION="An email client (and news reader) based on GTK+"
 HOMEPAGE="https://www.claws-mail.org/"
@@ -120,12 +120,9 @@ RDEPEND="${COMMONDEPEND}
 
 PATCHES=(
 	"${FILESDIR}/${PN}-3.17.5-enchant-2_default.patch"
-)
+	"${FILESDIR}/${PN}-4.1.1-fix_lto.patch"
 
-src_prepare() {
-	default
-	eautoreconf
-}
+)
 
 src_configure() {
 	local myeconfargs=(

mail-client/claws-mail/Manifest
@@ -1,3 +1,4 @@
 DIST claws-mail-3.21.0.tar.xz 6848648 BLAKE2B 9b2c89b5b9105add54db7b55f082e965645b67ca361edb9cdb7fdc3793146332acb678f06a493b0668c6a7d36a70d471d25948b0d14d6f61b22423057014a67f SHA512 59078bdb2d0314c63d4c63fdf493b0484c731523a2a099dd29816b14586f6ef038e36f794284d60deb45fe22b4c29bd239da49cc3b94549884742ef64d2fdcae
 DIST claws-mail-4.2.0.tar.xz 6674872 BLAKE2B b3118b45f1d3e8ae31a9c7bfa529fd5c8fda3ff8823e88a5ef078432d38d4150b05791deafff2577378ea17b03d965374755d2536ff6835ed46bcd566fefddaf SHA512 2415f246fc88b76263e1c43860c0638e08c581493333a3d846831f26cda21f0f14035a759379031d52e56aef2516bacec147a2cd5a153fc8fecf02ede64077f1
 DIST claws-mail-4.3.0.tar.xz 6831980 BLAKE2B e4dfb35b7f542ee514613b55286a19e2db398babc391a648edbda6d694b2e76973e1e7b6c4b7942b335898a571ffbbfef60fea1831ad4ac5ef7457a454259198 SHA512 3b0e811948d75ca2d003035d545d46d142ed4f2c1a103dc862413f9ff7a5ba612e00a7d5832836657e8d382bc7f94520496aaf60da429c72d9fb146ba12f0dfc
+DIST claws-mail-4.3.1.tar.xz 6782236 BLAKE2B e946d5462ef5455d6bd90aefb65d5ad95a558cbda2a403e3be2494c3d012bd2d9c476c6f869100870c7258d982f86569b9475f8913ef89f1b9806a7a7e1405df SHA512 1ecd34c9d4b529dbad42febdd873a1cb06c965fbfd9c0677b20b13ccfed7dae23602bdcf63285ff267a55c4b07a5565d9e8c60622b849e1ece1b9e3046995fa9

commit 90eb1cd07f75b76782c9f37c7c71ca5e097c9d99

mail-client/claws-mail: [appindicator,libcanberra,libnotify] should require [notification]
Closes: https://bugs.gentoo.org/950681
Fixes: ca41779483c816888406db7c67656d5d33579ba3
Signed-off-by: Alexander Kurakin <kuraga333@mail.ru>

mail-client/claws-mail/claws-mail-4.3.1.ebuild
@@ -23,6 +23,9 @@ SLOT="0"
 
 IUSE="appindicator archive bogofilter calendar clamav dbus debug doc +gnutls +imap ldap +libcanberra +libnotify litehtml networkmanager nls nntp +notification +oauth pdf perl +pgp python rss session sieve smime spamassassin spam-report spell startup-notification svg valgrind webkit xface"
 REQUIRED_USE="
+	appindicator? ( notification )
+	libcanberra? ( notification )
+	libnotify? ( notification )
 	notification? ( || ( appindicator libcanberra libnotify ) )
 	networkmanager? ( dbus )
 	oauth? ( gnutls )

mail-client/claws-mail/claws-mail-9999.ebuild
@@ -23,6 +23,9 @@ SLOT="0"
 
 IUSE="appindicator archive bogofilter calendar clamav dbus debug doc +gnutls +imap ldap +libcanberra +libnotify litehtml networkmanager nls nntp +notification +oauth pdf perl +pgp python rss session sieve smime spamassassin spam-report spell startup-notification svg valgrind webkit xface"
 REQUIRED_USE="
+	appindicator? ( notification )
+	libcanberra? ( notification )
+	libnotify? ( notification )
 	notification? ( || ( appindicator libcanberra libnotify ) )
 	networkmanager? ( dbus )
 	oauth? ( gnutls )