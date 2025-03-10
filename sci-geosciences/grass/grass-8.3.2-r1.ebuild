gentoo/gentoo

commit 0498e6fbc8a5a9c53234d125fc36f3941635c7b5

sci-geosciences/grass: fix addpredict use
Newer Portage bans use of colons in addpredict calls.

Closes: https://bugs.gentoo.org/948968
Signed-off-by: Mario Haustein <mario.haustein@hrz.tu-chemnitz.de>
Closes: https://github.com/gentoo/gentoo/pull/40644
Signed-off-by: Sam James <sam@gentoo.org>

sci-geosciences/grass/grass-8.3.2-r1.ebuild
@@ -156,16 +156,24 @@ src_prepare() {
 	eend $?
 
 	# For testsuite, see https://bugs.gentoo.org/show_bug.cgi?id=500580#c3
+	local ati_cards mesa_cards nvidia_cards render_cards
 	shopt -s nullglob
-	local mesa_cards=$(echo -n /dev/dri/card* /dev/dri/render* | sed 's/ /:/g')
-	if test -n "${mesa_cards}"; then
-		addpredict "${mesa_cards}"
-	fi
-	local ati_cards=$(echo -n /dev/ati/card* | sed 's/ /:/g')
-	if test -n "${ati_cards}"; then
-		addpredict "${ati_cards}"
-	fi
-	shopt -u nullglob
+	ati_cards=$(echo -n /dev/ati/card*)
+	for card in "${ati_cards[@]}"; do
+		addpredict "${card}"
+	done
+	mesa_cards=$(echo -n /dev/dri/card*)
+	for card in "${mesa_cards[@]}"; do
+		addpredict "${card}"
+	done
+	nvidia_cards=$(echo -n /dev/nvidia*)
+	for card in "${nvidia_cards[@]}"; do
+		addpredict "${card}"
+	done
+	render_cards=$(echo -n /dev/dri/renderD128*)
+	for card in "${render_cards[@]}"; do
+		addpredict "${card}"
+	done
 	addpredict /dev/nvidiactl
 }
 

sci-geosciences/grass/grass-8.4.0.ebuild
@@ -156,16 +156,24 @@ src_prepare() {
 	eend $?
 
 	# For testsuite, see https://bugs.gentoo.org/show_bug.cgi?id=500580#c3
+	local ati_cards mesa_cards nvidia_cards render_cards
 	shopt -s nullglob
-	local mesa_cards=$(echo -n /dev/dri/card* /dev/dri/render* | sed 's/ /:/g')
-	if test -n "${mesa_cards}"; then
-		addpredict "${mesa_cards}"
-	fi
-	local ati_cards=$(echo -n /dev/ati/card* | sed 's/ /:/g')
-	if test -n "${ati_cards}"; then
-		addpredict "${ati_cards}"
-	fi
-	shopt -u nullglob
+	ati_cards=$(echo -n /dev/ati/card*)
+	for card in "${ati_cards[@]}"; do
+		addpredict "${card}"
+	done
+	mesa_cards=$(echo -n /dev/dri/card*)
+	for card in "${mesa_cards[@]}"; do
+		addpredict "${card}"
+	done
+	nvidia_cards=$(echo -n /dev/nvidia*)
+	for card in "${nvidia_cards[@]}"; do
+		addpredict "${card}"
+	done
+	render_cards=$(echo -n /dev/dri/renderD128*)
+	for card in "${render_cards[@]}"; do
+		addpredict "${card}"
+	done
 	addpredict /dev/nvidiactl
 }
 

sci-geosciences/grass/grass-8.4.1_rc1.ebuild
@@ -156,16 +156,24 @@ src_prepare() {
 	eend $?
 
 	# For testsuite, see https://bugs.gentoo.org/show_bug.cgi?id=500580#c3
+	local ati_cards mesa_cards nvidia_cards render_cards
 	shopt -s nullglob
-	local mesa_cards=$(echo -n /dev/dri/card* /dev/dri/render* | sed 's/ /:/g')
-	if test -n "${mesa_cards}"; then
-		addpredict "${mesa_cards}"
-	fi
-	local ati_cards=$(echo -n /dev/ati/card* | sed 's/ /:/g')
-	if test -n "${ati_cards}"; then
-		addpredict "${ati_cards}"
-	fi
-	shopt -u nullglob
+	ati_cards=$(echo -n /dev/ati/card*)
+	for card in "${ati_cards[@]}"; do
+		addpredict "${card}"
+	done
+	mesa_cards=$(echo -n /dev/dri/card*)
+	for card in "${mesa_cards[@]}"; do
+		addpredict "${card}"
+	done
+	nvidia_cards=$(echo -n /dev/nvidia*)
+	for card in "${nvidia_cards[@]}"; do
+		addpredict "${card}"
+	done
+	render_cards=$(echo -n /dev/dri/renderD128*)
+	for card in "${render_cards[@]}"; do
+		addpredict "${card}"
+	done
 	addpredict /dev/nvidiactl
 }
 

sci-geosciences/grass/grass-9999.ebuild
@@ -151,16 +151,24 @@ src_prepare() {
 	eend $?
 
 	# For testsuite, see https://bugs.gentoo.org/show_bug.cgi?id=500580#c3
+	local ati_cards mesa_cards nvidia_cards render_cards
 	shopt -s nullglob
-	local mesa_cards=$(echo -n /dev/dri/card* /dev/dri/render* | sed 's/ /:/g')
-	if test -n "${mesa_cards}"; then
-		addpredict "${mesa_cards}"
-	fi
-	local ati_cards=$(echo -n /dev/ati/card* | sed 's/ /:/g')
-	if test -n "${ati_cards}"; then
-		addpredict "${ati_cards}"
-	fi
-	shopt -u nullglob
+	ati_cards=$(echo -n /dev/ati/card*)
+	for card in "${ati_cards[@]}"; do
+		addpredict "${card}"
+	done
+	mesa_cards=$(echo -n /dev/dri/card*)
+	for card in "${mesa_cards[@]}"; do
+		addpredict "${card}"
+	done
+	nvidia_cards=$(echo -n /dev/nvidia*)
+	for card in "${nvidia_cards[@]}"; do
+		addpredict "${card}"
+	done
+	render_cards=$(echo -n /dev/dri/renderD128*)
+	for card in "${render_cards[@]}"; do
+		addpredict "${card}"
+	done
 	addpredict /dev/nvidiactl
 }
 

commit a4fb9adcd287b12f72cf840146a428cd9e12fdd2

sci-geosciences/grass: fix nullglob vs quoting
Bug: https://bugs.gentoo.org/951100
Bug: https://bugs.gentoo.org/948968
Fixes: 0498e6fbc8a5a9c53234d125fc36f3941635c7b5
Fixes: d8b3a440d5094cccf9be4f2f0df11dd79c869828
Signed-off-by: Sam James <sam@gentoo.org>

sci-geosciences/grass/grass-8.3.2-r1.ebuild
@@ -159,19 +159,19 @@ src_prepare() {
 	local ati_cards mesa_cards nvidia_cards render_cards
 	shopt -s nullglob
 	ati_cards=$(echo -n /dev/ati/card*)
-	for card in "${ati_cards[@]}"; do
+	for card in ${ati_cards[@]}; do
 		addpredict "${card}"
 	done
 	mesa_cards=$(echo -n /dev/dri/card*)
-	for card in "${mesa_cards[@]}"; do
+	for card in ${mesa_cards[@]}; do
 		addpredict "${card}"
 	done
 	nvidia_cards=$(echo -n /dev/nvidia*)
-	for card in "${nvidia_cards[@]}"; do
+	for card in ${nvidia_cards[@]}; do
 		addpredict "${card}"
 	done
 	render_cards=$(echo -n /dev/dri/renderD128*)
-	for card in "${render_cards[@]}"; do
+	for card in ${render_cards[@]}; do
 		addpredict "${card}"
 	done
 	addpredict /dev/nvidiactl

sci-geosciences/grass/grass-8.4.0.ebuild
@@ -159,19 +159,19 @@ src_prepare() {
 	local ati_cards mesa_cards nvidia_cards render_cards
 	shopt -s nullglob
 	ati_cards=$(echo -n /dev/ati/card*)
-	for card in "${ati_cards[@]}"; do
+	for card in ${ati_cards[@]}; do
 		addpredict "${card}"
 	done
 	mesa_cards=$(echo -n /dev/dri/card*)
-	for card in "${mesa_cards[@]}"; do
+	for card in ${mesa_cards[@]}; do
 		addpredict "${card}"
 	done
 	nvidia_cards=$(echo -n /dev/nvidia*)
-	for card in "${nvidia_cards[@]}"; do
+	for card in ${nvidia_cards[@]}; do
 		addpredict "${card}"
 	done
 	render_cards=$(echo -n /dev/dri/renderD128*)
-	for card in "${render_cards[@]}"; do
+	for card in ${render_cards[@]}; do
 		addpredict "${card}"
 	done
 	addpredict /dev/nvidiactl

sci-geosciences/grass/grass-8.4.1.ebuild
@@ -160,19 +160,19 @@ src_prepare() {
 	local prev_shopt=$(shopt -p nullglob)
 	shopt -s nullglob
 	ati_cards=$(echo -n /dev/ati/card*)
-	for card in "${ati_cards[@]}"; do
+	for card in ${ati_cards[@]}; do
 		addpredict "${card}"
 	done
 	mesa_cards=$(echo -n /dev/dri/card*)
-	for card in "${mesa_cards[@]}"; do
+	for card in ${mesa_cards[@]}; do
 		addpredict "${card}"
 	done
 	nvidia_cards=$(echo -n /dev/nvidia*)
-	for card in "${nvidia_cards[@]}"; do
+	for card in ${nvidia_cards[@]}; do
 		addpredict "${card}"
 	done
 	render_cards=$(echo -n /dev/dri/renderD128*)
-	for card in "${render_cards[@]}"; do
+	for card in ${render_cards[@]}; do
 		addpredict "${card}"
 	done
 	addpredict /dev/nvidiactl

sci-geosciences/grass/grass-8.4.1_rc1.ebuild
@@ -159,19 +159,19 @@ src_prepare() {
 	local ati_cards mesa_cards nvidia_cards render_cards
 	shopt -s nullglob
 	ati_cards=$(echo -n /dev/ati/card*)
-	for card in "${ati_cards[@]}"; do
+	for card in ${ati_cards[@]}; do
 		addpredict "${card}"
 	done
 	mesa_cards=$(echo -n /dev/dri/card*)
-	for card in "${mesa_cards[@]}"; do
+	for card in ${mesa_cards[@]}; do
 		addpredict "${card}"
 	done
 	nvidia_cards=$(echo -n /dev/nvidia*)
-	for card in "${nvidia_cards[@]}"; do
+	for card in ${nvidia_cards[@]}; do
 		addpredict "${card}"
 	done
 	render_cards=$(echo -n /dev/dri/renderD128*)
-	for card in "${render_cards[@]}"; do
+	for card in ${render_cards[@]}; do
 		addpredict "${card}"
 	done
 	addpredict /dev/nvidiactl

sci-geosciences/grass/grass-9999.ebuild
@@ -155,19 +155,19 @@ src_prepare() {
 	local prev_shopt=$(shopt -p nullglob)
 	shopt -s nullglob
 	ati_cards=$(echo -n /dev/ati/card*)
-	for card in "${ati_cards[@]}"; do
+	for card in ${ati_cards[@]}; do
 		addpredict "${card}"
 	done
 	mesa_cards=$(echo -n /dev/dri/card*)
-	for card in "${mesa_cards[@]}"; do
+	for card in ${mesa_cards[@]}; do
 		addpredict "${card}"
 	done
 	nvidia_cards=$(echo -n /dev/nvidia*)
-	for card in "${nvidia_cards[@]}"; do
+	for card in ${nvidia_cards[@]}; do
 		addpredict "${card}"
 	done
 	render_cards=$(echo -n /dev/dri/renderD128*)
-	for card in "${render_cards[@]}"; do
+	for card in ${render_cards[@]}; do
 		addpredict "${card}"
 	done
 	addpredict /dev/nvidiactl