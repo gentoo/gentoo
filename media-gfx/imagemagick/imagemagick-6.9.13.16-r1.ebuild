gentoo/gentoo

commit d8b3a440d5094cccf9be4f2f0df11dd79c869828

media-gfx/imagemagick: fix addpredict use
Newer Portage bans use of colons in addpredict calls.

Signed-off-by: Sam James <sam@gentoo.org>

media-gfx/imagemagick/imagemagick-6.9.13.16-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -103,22 +103,22 @@ src_prepare() {
 	# For testsuite, see bug #500580#c3
 	local ati_cards mesa_cards nvidia_cards render_cards
 	shopt -s nullglob
-	ati_cards=$(echo -n /dev/ati/card* | sed 's/ /:/g')
-	if test -n "${ati_cards}"; then
-		addpredict "${ati_cards}"
-	fi
-	mesa_cards=$(echo -n /dev/dri/card* | sed 's/ /:/g')
-	if test -n "${mesa_cards}"; then
-		addpredict "${mesa_cards}"
-	fi
-	nvidia_cards=$(echo -n /dev/nvidia* | sed 's/ /:/g')
-	if test -n "${nvidia_cards}"; then
-		addpredict "${nvidia_cards}"
-	fi
-	render_cards=$(echo -n /dev/dri/renderD128* | sed 's/ /:/g')
-	if test -n "${render_cards}"; then
-		addpredict "${render_cards}"
-	fi
+	ati_cards=$(echo -n /dev/ati/card*)
+	for card in "${ati_cards[@]}" ; do
+		addpredict "${card}"
+	done
+	mesa_cards=$(echo -n /dev/dri/card*)
+	for card in "${mesa_cards[@]}" ; do
+                addpredict "${card}"
+	done
+	nvidia_cards=$(echo -n /dev/nvidia*)
+	for card in "${nvidia_cards[@]}" ; do
+		addpredict "${card}"
+	done
+	render_cards=$(echo -n /dev/dri/renderD128*)
+	for card in "${render_cards[@]}" ; do
+		addpredict "${card}"
+	done
 	shopt -u nullglob
 	addpredict /dev/nvidiactl
 }

media-gfx/imagemagick/imagemagick-6.9.13.3.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -103,22 +103,22 @@ src_prepare() {
 	# For testsuite, see bug #500580#c3
 	local ati_cards mesa_cards nvidia_cards render_cards
 	shopt -s nullglob
-	ati_cards=$(echo -n /dev/ati/card* | sed 's/ /:/g')
-	if test -n "${ati_cards}"; then
-		addpredict "${ati_cards}"
-	fi
-	mesa_cards=$(echo -n /dev/dri/card* | sed 's/ /:/g')
-	if test -n "${mesa_cards}"; then
-		addpredict "${mesa_cards}"
-	fi
-	nvidia_cards=$(echo -n /dev/nvidia* | sed 's/ /:/g')
-	if test -n "${nvidia_cards}"; then
-		addpredict "${nvidia_cards}"
-	fi
-	render_cards=$(echo -n /dev/dri/renderD128* | sed 's/ /:/g')
-	if test -n "${render_cards}"; then
-		addpredict "${render_cards}"
-	fi
+	ati_cards=$(echo -n /dev/ati/card*)
+	for card in "${ati_cards[@]}" ; do
+		addpredict "${card}"
+	done
+	mesa_cards=$(echo -n /dev/dri/card*)
+	for card in "${mesa_cards[@]}" ; do
+		addpredict "${card}"
+	done
+	nvidia_cards=$(echo -n /dev/nvidia*)
+	for card in "${nvidia_cards[@]}" ; do
+		addpredict "${card}"
+	done
+	render_cards=$(echo -n /dev/dri/renderD128*)
+	for card in "${render_cards[@]}" ; do
+		addpredict "${card}"
+	done
 	shopt -u nullglob
 	addpredict /dev/nvidiactl
 }

media-gfx/imagemagick/imagemagick-7.1.1.25-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -106,25 +106,25 @@ src_prepare() {
 	#elibtoolize # for Darwin modules
 	eautoreconf
 
-	# For testsuite, see https://bugs.gentoo.org/show_bug.cgi?id=500580#c3
+	# For testsuite, see bug #500580#c3
 	local ati_cards mesa_cards nvidia_cards render_cards
 	shopt -s nullglob
-	ati_cards=$(echo -n /dev/ati/card* | sed 's/ /:/g')
-	if test -n "${ati_cards}"; then
-		addpredict "${ati_cards}"
-	fi
-	mesa_cards=$(echo -n /dev/dri/card* | sed 's/ /:/g')
-	if test -n "${mesa_cards}"; then
-		addpredict "${mesa_cards}"
-	fi
-	nvidia_cards=$(echo -n /dev/nvidia* | sed 's/ /:/g')
-	if test -n "${nvidia_cards}"; then
-		addpredict "${nvidia_cards}"
-	fi
-	render_cards=$(echo -n /dev/dri/renderD128* | sed 's/ /:/g')
-	if test -n "${render_cards}"; then
-		addpredict "${render_cards}"
-	fi
+	ati_cards=$(echo -n /dev/ati/card*)
+	for card in "${ati_cards[@]}" ; do
+		addpredict "${card}"
+	done
+	mesa_cards=$(echo -n /dev/dri/card*)
+	for card in "${mesa_cards[@]}" ; do
+		addpredict "${card}"
+	done
+	nvidia_cards=$(echo -n /dev/nvidia*)
+	for card in "${nvidia_cards[@]}" ; do
+		addpredict "${card}"
+	done
+	render_cards=$(echo -n /dev/dri/renderD128*)
+	for card in "${render_cards[@]}" ; do
+		addpredict "${card}"
+	done
 	shopt -u nullglob
 	addpredict /dev/nvidiactl
 }

media-gfx/imagemagick/imagemagick-7.1.1.38-r2.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -108,25 +108,25 @@ src_prepare() {
 	#elibtoolize # for Darwin modules
 	eautoreconf
 
-	# For testsuite, see https://bugs.gentoo.org/show_bug.cgi?id=500580#c3
+	# For testsuite, see bug #500580#c3
 	local ati_cards mesa_cards nvidia_cards render_cards
 	shopt -s nullglob
-	ati_cards=$(echo -n /dev/ati/card* | sed 's/ /:/g')
-	if test -n "${ati_cards}"; then
-		addpredict "${ati_cards}"
-	fi
-	mesa_cards=$(echo -n /dev/dri/card* | sed 's/ /:/g')
-	if test -n "${mesa_cards}"; then
-		addpredict "${mesa_cards}"
-	fi
-	nvidia_cards=$(echo -n /dev/nvidia* | sed 's/ /:/g')
-	if test -n "${nvidia_cards}"; then
-		addpredict "${nvidia_cards}"
-	fi
-	render_cards=$(echo -n /dev/dri/renderD128* | sed 's/ /:/g')
-	if test -n "${render_cards}"; then
-		addpredict "${render_cards}"
-	fi
+	ati_cards=$(echo -n /dev/ati/card*)
+	for card in "${ati_cards[@]}" ; do
+		addpredict "${card}"
+	done
+	mesa_cards=$(echo -n /dev/dri/card*)
+	for card in "${mesa_cards[@]}" ; do
+		addpredict "${card}"
+	done
+	nvidia_cards=$(echo -n /dev/nvidia*)
+	for card in "${nvidia_cards[@]}" ; do
+		addpredict "${card}"
+	done
+	render_cards=$(echo -n /dev/dri/renderD128*)
+	for card in "${render_cards[@]}" ; do
+		addpredict "${card}"
+	done
 	shopt -u nullglob
 	addpredict /dev/nvidiactl
 }

media-gfx/imagemagick/imagemagick-9999.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -106,25 +106,25 @@ src_prepare() {
 	#elibtoolize # for Darwin modules
 	eautoreconf
 
-	# For testsuite, see https://bugs.gentoo.org/show_bug.cgi?id=500580#c3
+	# For testsuite, see bug #500580#c3
 	local ati_cards mesa_cards nvidia_cards render_cards
 	shopt -s nullglob
-	ati_cards=$(echo -n /dev/ati/card* | sed 's/ /:/g')
-	if test -n "${ati_cards}"; then
-		addpredict "${ati_cards}"
-	fi
-	mesa_cards=$(echo -n /dev/dri/card* | sed 's/ /:/g')
-	if test -n "${mesa_cards}"; then
-		addpredict "${mesa_cards}"
-	fi
-	nvidia_cards=$(echo -n /dev/nvidia* | sed 's/ /:/g')
-	if test -n "${nvidia_cards}"; then
-		addpredict "${nvidia_cards}"
-	fi
-	render_cards=$(echo -n /dev/dri/renderD128* | sed 's/ /:/g')
-	if test -n "${render_cards}"; then
-		addpredict "${render_cards}"
-	fi
+	ati_cards=$(echo -n /dev/ati/card*)
+	for card in "${ati_cards[@]}" ; do
+		addpredict "${card}"
+	done
+	mesa_cards=$(echo -n /dev/dri/card*)
+	for card in "${mesa_cards[@]}" ; do
+		addpredict "${card}"
+	done
+	nvidia_cards=$(echo -n /dev/nvidia*)
+	for card in "${nvidia_cards[@]}" ; do
+		addpredict "${card}"
+	done
+	render_cards=$(echo -n /dev/dri/renderD128*)
+	for card in "${render_cards[@]}" ; do
+		addpredict "${card}"
+	done
 	shopt -u nullglob
 	addpredict /dev/nvidiactl
 }

media-gfx/imagemagick/imagemagick-6.9.13.16.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -103,22 +103,22 @@ src_prepare() {
 	# For testsuite, see bug #500580#c3
 	local ati_cards mesa_cards nvidia_cards render_cards
 	shopt -s nullglob
-	ati_cards=$(echo -n /dev/ati/card* | sed 's/ /:/g')
-	if test -n "${ati_cards}"; then
-		addpredict "${ati_cards}"
-	fi
-	mesa_cards=$(echo -n /dev/dri/card* | sed 's/ /:/g')
-	if test -n "${mesa_cards}"; then
-		addpredict "${mesa_cards}"
-	fi
-	nvidia_cards=$(echo -n /dev/nvidia* | sed 's/ /:/g')
-	if test -n "${nvidia_cards}"; then
-		addpredict "${nvidia_cards}"
-	fi
-	render_cards=$(echo -n /dev/dri/renderD128* | sed 's/ /:/g')
-	if test -n "${render_cards}"; then
-		addpredict "${render_cards}"
-	fi
+	ati_cards=$(echo -n /dev/ati/card*)
+	for card in "${ati_cards[@]}" ; do
+		addpredict "${card}"
+	done
+	mesa_cards=$(echo -n /dev/dri/card*)
+	for card in "${mesa_cards[@]}" ; do
+		addpredict "${card}"
+	done
+	nvidia_cards=$(echo -n /dev/nvidia*)
+	for card in "${nvidia_cards[@]}" ; do
+		addpredict "${card}"
+	done
+	render_cards=$(echo -n /dev/dri/renderD128*)
+	for card in "${render_cards[@]}" ; do
+		addpredict "${card}"
+	done
 	shopt -u nullglob
 	addpredict /dev/nvidiactl
 }

media-gfx/imagemagick/imagemagick-7.1.1.38-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -108,25 +108,25 @@ src_prepare() {
 	#elibtoolize # for Darwin modules
 	eautoreconf
 
-	# For testsuite, see https://bugs.gentoo.org/show_bug.cgi?id=500580#c3
+	# For testsuite, see bug #500580#c3
 	local ati_cards mesa_cards nvidia_cards render_cards
 	shopt -s nullglob
-	ati_cards=$(echo -n /dev/ati/card* | sed 's/ /:/g')
-	if test -n "${ati_cards}"; then
-		addpredict "${ati_cards}"
-	fi
-	mesa_cards=$(echo -n /dev/dri/card* | sed 's/ /:/g')
-	if test -n "${mesa_cards}"; then
-		addpredict "${mesa_cards}"
-	fi
-	nvidia_cards=$(echo -n /dev/nvidia* | sed 's/ /:/g')
-	if test -n "${nvidia_cards}"; then
-		addpredict "${nvidia_cards}"
-	fi
-	render_cards=$(echo -n /dev/dri/renderD128* | sed 's/ /:/g')
-	if test -n "${render_cards}"; then
-		addpredict "${render_cards}"
-	fi
+	ati_cards=$(echo -n /dev/ati/card*)
+	for card in "${ati_cards[@]}" ; do
+		addpredict "${card}"
+	done
+	mesa_cards=$(echo -n /dev/dri/card*)
+	for card in "${mesa_cards[@]}" ; do
+		addpredict "${card}"
+	done
+	nvidia_cards=$(echo -n /dev/nvidia*)
+	for card in "${nvidia_cards[@]}" ; do
+		addpredict "${card}"
+	done
+	render_cards=$(echo -n /dev/dri/renderD128*)
+	for card in "${render_cards[@]}" ; do
+		addpredict "${card}"
+	done
 	shopt -u nullglob
 	addpredict /dev/nvidiactl
 }

media-gfx/imagemagick/imagemagick-7.1.1.38.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -106,25 +106,25 @@ src_prepare() {
 	#elibtoolize # for Darwin modules
 	eautoreconf
 
-	# For testsuite, see https://bugs.gentoo.org/show_bug.cgi?id=500580#c3
+	# For testsuite, see bug #500580#c3
 	local ati_cards mesa_cards nvidia_cards render_cards
 	shopt -s nullglob
-	ati_cards=$(echo -n /dev/ati/card* | sed 's/ /:/g')
-	if test -n "${ati_cards}"; then
-		addpredict "${ati_cards}"
-	fi
-	mesa_cards=$(echo -n /dev/dri/card* | sed 's/ /:/g')
-	if test -n "${mesa_cards}"; then
-		addpredict "${mesa_cards}"
-	fi
-	nvidia_cards=$(echo -n /dev/nvidia* | sed 's/ /:/g')
-	if test -n "${nvidia_cards}"; then
-		addpredict "${nvidia_cards}"
-	fi
-	render_cards=$(echo -n /dev/dri/renderD128* | sed 's/ /:/g')
-	if test -n "${render_cards}"; then
-		addpredict "${render_cards}"
-	fi
+	ati_cards=$(echo -n /dev/ati/card*)
+	for card in "${ati_cards[@]}" ; do
+		addpredict "${card}"
+	done
+	mesa_cards=$(echo -n /dev/dri/card*)
+	for card in "${mesa_cards[@]}" ; do
+		addpredict "${card}"
+	done
+	nvidia_cards=$(echo -n /dev/nvidia*)
+	for card in "${nvidia_cards[@]}" ; do
+		addpredict "${card}"
+	done
+	render_cards=$(echo -n /dev/dri/renderD128*)
+	for card in "${render_cards[@]}" ; do
+		addpredict "${card}"
+	done
 	shopt -u nullglob
 	addpredict /dev/nvidiactl
 }

commit daeee4faff3eeb8bfb7839735d6cccb38a19ea43

media-gfx/imagemagick: fix nullglob vs quoting
Closes: https://bugs.gentoo.org/951100
Fixes: d8b3a440d5094cccf9be4f2f0df11dd79c869828
Signed-off-by: Sam James <sam@gentoo.org>

media-gfx/imagemagick/imagemagick-6.9.13.16-r1.ebuild
@@ -104,19 +104,19 @@ src_prepare() {
 	local ati_cards mesa_cards nvidia_cards render_cards
 	shopt -s nullglob
 	ati_cards=$(echo -n /dev/ati/card*)
-	for card in "${ati_cards[@]}" ; do
+	for card in ${ati_cards[@]} ; do
 		addpredict "${card}"
 	done
 	mesa_cards=$(echo -n /dev/dri/card*)
-	for card in "${mesa_cards[@]}" ; do
+	for card in ${mesa_cards[@]} ; do
                 addpredict "${card}"
 	done
 	nvidia_cards=$(echo -n /dev/nvidia*)
-	for card in "${nvidia_cards[@]}" ; do
+	for card in ${nvidia_cards[@]} ; do
 		addpredict "${card}"
 	done
 	render_cards=$(echo -n /dev/dri/renderD128*)
-	for card in "${render_cards[@]}" ; do
+	for card in ${render_cards[@]} ; do
 		addpredict "${card}"
 	done
 	shopt -u nullglob

media-gfx/imagemagick/imagemagick-6.9.13.3.ebuild
@@ -104,19 +104,19 @@ src_prepare() {
 	local ati_cards mesa_cards nvidia_cards render_cards
 	shopt -s nullglob
 	ati_cards=$(echo -n /dev/ati/card*)
-	for card in "${ati_cards[@]}" ; do
+	for card in ${ati_cards[@]} ; do
 		addpredict "${card}"
 	done
 	mesa_cards=$(echo -n /dev/dri/card*)
-	for card in "${mesa_cards[@]}" ; do
+	for card in ${mesa_cards[@]} ; do
 		addpredict "${card}"
 	done
 	nvidia_cards=$(echo -n /dev/nvidia*)
-	for card in "${nvidia_cards[@]}" ; do
+	for card in ${nvidia_cards[@]} ; do
 		addpredict "${card}"
 	done
 	render_cards=$(echo -n /dev/dri/renderD128*)
-	for card in "${render_cards[@]}" ; do
+	for card in ${render_cards[@]} ; do
 		addpredict "${card}"
 	done
 	shopt -u nullglob

media-gfx/imagemagick/imagemagick-7.1.1.25-r1.ebuild
@@ -110,19 +110,19 @@ src_prepare() {
 	local ati_cards mesa_cards nvidia_cards render_cards
 	shopt -s nullglob
 	ati_cards=$(echo -n /dev/ati/card*)
-	for card in "${ati_cards[@]}" ; do
+	for card in ${ati_cards[@]} ; do
 		addpredict "${card}"
 	done
 	mesa_cards=$(echo -n /dev/dri/card*)
-	for card in "${mesa_cards[@]}" ; do
+	for card in ${mesa_cards[@]} ; do
 		addpredict "${card}"
 	done
 	nvidia_cards=$(echo -n /dev/nvidia*)
-	for card in "${nvidia_cards[@]}" ; do
+	for card in ${nvidia_cards[@]} ; do
 		addpredict "${card}"
 	done
 	render_cards=$(echo -n /dev/dri/renderD128*)
-	for card in "${render_cards[@]}" ; do
+	for card in ${render_cards[@]} ; do
 		addpredict "${card}"
 	done
 	shopt -u nullglob

media-gfx/imagemagick/imagemagick-7.1.1.38-r2.ebuild
@@ -112,19 +112,19 @@ src_prepare() {
 	local ati_cards mesa_cards nvidia_cards render_cards
 	shopt -s nullglob
 	ati_cards=$(echo -n /dev/ati/card*)
-	for card in "${ati_cards[@]}" ; do
+	for card in ${ati_cards[@]} ; do
 		addpredict "${card}"
 	done
 	mesa_cards=$(echo -n /dev/dri/card*)
-	for card in "${mesa_cards[@]}" ; do
+	for card in ${mesa_cards[@]} ; do
 		addpredict "${card}"
 	done
 	nvidia_cards=$(echo -n /dev/nvidia*)
-	for card in "${nvidia_cards[@]}" ; do
+	for card in ${nvidia_cards[@]} ; do
 		addpredict "${card}"
 	done
 	render_cards=$(echo -n /dev/dri/renderD128*)
-	for card in "${render_cards[@]}" ; do
+	for card in ${render_cards[@]} ; do
 		addpredict "${card}"
 	done
 	shopt -u nullglob

media-gfx/imagemagick/imagemagick-9999.ebuild
@@ -110,19 +110,19 @@ src_prepare() {
 	local ati_cards mesa_cards nvidia_cards render_cards
 	shopt -s nullglob
 	ati_cards=$(echo -n /dev/ati/card*)
-	for card in "${ati_cards[@]}" ; do
+	for card in ${ati_cards[@]} ; do
 		addpredict "${card}"
 	done
 	mesa_cards=$(echo -n /dev/dri/card*)
-	for card in "${mesa_cards[@]}" ; do
+	for card in ${mesa_cards[@]} ; do
 		addpredict "${card}"
 	done
 	nvidia_cards=$(echo -n /dev/nvidia*)
-	for card in "${nvidia_cards[@]}" ; do
+	for card in ${nvidia_cards[@]} ; do
 		addpredict "${card}"
 	done
 	render_cards=$(echo -n /dev/dri/renderD128*)
-	for card in "${render_cards[@]}" ; do
+	for card in ${render_cards[@]} ; do
 		addpredict "${card}"
 	done
 	shopt -u nullglob