gentoo/gentoo

commit 14e674bb7a8cb8aa50498582e392d0ecd21f2d4a

toolchain.eclass: allow current slot for ada-bootstrap dep
Otherwise, you don't get a clean path for 14-with-14.

Bug: https://bugs.gentoo.org/940472
Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -411,16 +411,29 @@ if tc_has_feature valgrind ; then
 fi
 
 if [[ ${PN} != gnat-gpl ]] && tc_has_feature ada ; then
-	BDEPEND+="
-		ada? (
-			|| (
-				sys-devel/gcc:${SLOT}[ada]
-				<sys-devel/gcc-${SLOT}[ada]
-				<dev-lang/ada-bootstrap-${SLOT}
-				dev-lang/gnat-gpl[ada]
+	if tc_use_major_version_only ; then
+		BDEPEND+="
+			ada? (
+				|| (
+					sys-devel/gcc:${SLOT}[ada]
+					<sys-devel/gcc-${SLOT}[ada]
+					<dev-lang/ada-bootstrap-$((${SLOT} + 1))
+					dev-lang/gnat-gpl[ada]
+				)
 			)
-		)
-	"
+		"
+	else
+                BDEPEND+="
+                        ada? (
+                                || (
+                                        sys-devel/gcc:${SLOT}[ada]
+                                        <sys-devel/gcc-${SLOT}[ada]
+                                        <dev-lang/ada-bootstrap-${SLOT}
+                                        dev-lang/gnat-gpl[ada]
+                                )
+                        )
+                "
+	fi
 fi
 
 # TODO: Add a pkg_setup & pkg_pretend check for whether the active compiler

commit 539606eab3df9987f76778505c08e190b42ddb0d

toolchain.eclass: fix indentation
Reported by negril.

Fixes: 14e674bb7a8cb8aa50498582e392d0ecd21f2d4a
Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -429,15 +429,15 @@ if [[ ${PN} != gnat-gpl ]] && tc_has_feature ada ; then
 			)
 		"
 	else
-                BDEPEND+="
-                        ada? (
-                                || (
-                                        sys-devel/gcc:${SLOT}[ada]
-                                        <sys-devel/gcc-${SLOT}[ada]
-                                        <dev-lang/ada-bootstrap-${SLOT}
-                                )
-                        )
-                "
+		BDEPEND+="
+			ada? (
+				|| (
+					sys-devel/gcc:${SLOT}[ada]
+					<sys-devel/gcc-${SLOT}[ada]
+					<dev-lang/ada-bootstrap-${SLOT}
+				)
+			)
+		"
 	fi
 fi
 