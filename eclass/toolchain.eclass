gentoo/gentoo

commit 87c9c1899a56f388493ecbec3b45d175d99581f2

toolchain.eclass: hackily fixup libstdc++.modules.json
Bug: https://bugs.gentoo.org/948394
Bug: https://gcc.gnu.org/PR119266
Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -2646,6 +2646,13 @@ toolchain_src_install() {
 		fi
 	fi
 
+	# Hack for C++ modules
+	if ! is_crosscompile; then
+		# PR19266 (bug #948394)
+		sed -e 's,\.\./lib/gcc/${CHOST}/${GCCMAJOR}/include/,../../../../include/,' \
+			"${ED}"/usr/lib/gcc/${CHOST}/${GCCMAJOR}/libstdc++.modules.json || die
+	fi
+
 	# As gcc installs object files built against both ${CHOST} and ${CTARGET}
 	# ideally we will need to strip them using different tools:
 	# Using ${CHOST} tools:

commit 221897e8182089a650784e019457d8525700a905

toolchain.eclass: hackily fixup libstdc++.modules.json (more)
Fixes: 87c9c1899a56f388493ecbec3b45d175d99581f2
Bug: https://bugs.gentoo.org/948394
Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -2649,7 +2649,7 @@ toolchain_src_install() {
 	# Hack for C++ modules
 	if ! is_crosscompile; then
 		# PR19266 (bug #948394)
-		sed -e 's,\.\./lib/gcc/${CHOST}/${GCCMAJOR}/include/,../../../../include/,' \
+		sed -i -e "s,\.\./lib/gcc/${CHOST}/${GCCMAJOR}/include/,../../../../include/," \
 			"${ED}"/usr/lib/gcc/${CHOST}/${GCCMAJOR}/libstdc++.modules.json || die
 	fi
 