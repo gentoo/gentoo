gentoo/gentoo

commit eed97d7edf59677017edc7feebb75b9a6056eebd

app-arch/lzma: use unpacker
Signed-off-by: NRK <nrk@disroot.org>
Signed-off-by: Sam James <sam@gentoo.org>

app-arch/lzma/lzma-19.00.ebuild
@@ -1,9 +1,9 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
 
-inherit toolchain-funcs
+inherit toolchain-funcs unpacker
 
 MY_P="${PN}${PV//.}"
 DESCRIPTION="LZMA Stream Compressor from the SDK"
@@ -15,7 +15,7 @@ SLOT="0"
 KEYWORDS="~alpha amd64 arm arm64 ~hppa ppc ppc64 ~riscv sparc x86 ~amd64-linux ~x86-linux ~ppc-macos"
 IUSE="doc"
 
-BDEPEND="app-arch/p7zip"
+BDEPEND="$(unpacker_src_uri_depends)"
 
 S=${WORKDIR}
 

commit d114b44311881516c61a91d1b66c623fed9c9e8b

app-arch/lzma: upgrade to EAPI 8
needed so that unpacker can unpack the 7z files.

Fixes: eed97d7edf59677017edc7feebb75b9a6056eebd
Closes: https://bugs.gentoo.org/952020
Signed-off-by: NRK <nrk@disroot.org>

app-arch/lzma/lzma-19.00.ebuild
@@ -1,7 +1,7 @@
 # Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-EAPI=7
+EAPI=8
 
 inherit toolchain-funcs unpacker
 