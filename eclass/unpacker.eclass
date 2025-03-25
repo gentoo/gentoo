gentoo/gentoo

commit 0dbde702f43e075d88332b42c93287b200028691

unpacker.eclass: add support for app-arch/7zip
Signed-off-by: NRK <nrk@disroot.org>
Signed-off-by: Sam James <sam@gentoo.org>

eclass/unpacker.eclass
@@ -375,10 +375,15 @@ unpack_7z() {
 	local p7z=$(find_unpackable_file "$1")
 	unpack_banner "${p7z}"
 
+	local cmd7z="7z"
+	if command -v 7zz 1>/dev/null 2>&1; then
+		cmd7z="7zz"
+	fi
+
 	# warning: putting local and command substitution in a single call
 	# discards the exit status!
 	local output
-	output="$(7z x -y "${p7z}")"
+	output="$($cmd7z x -y "${p7z}")"
 	if [ $? -ne 0 ]; then
 		echo "${output}" >&2
 		die "unpacking ${p7z} failed (arch=unpack_7z)"
@@ -610,7 +615,13 @@ unpacker_src_uri_depends() {
 		*.rar)
 			deps[rar]="app-arch/unrar" ;;
 		*.7z)
-			deps[7z]="app-arch/p7zip" ;;
+			deps[7z]="
+				|| (
+					app-arch/7zip
+					app-arch/p7zip
+				)
+			"
+			;;
 		*.xz)
 			deps[xz]="app-arch/xz-utils" ;;
 		*.zip)

commit b0d848e9ff18dff2ec1a47d4c4487e010edd80c8

unpacker.eclass: fix 7zip dep
We have to depend on the right minimum version (and USE=symlink) to get
'7z'.

Bug: https://bugs.gentoo.org/942397
Closes: https://bugs.gentoo.org/952020
Fixes: 0dbde702f43e075d88332b42c93287b200028691
Signed-off-by: Sam James <sam@gentoo.org>

eclass/unpacker.eclass
@@ -617,7 +617,7 @@ unpacker_src_uri_depends() {
 		*.7z)
 			deps[7z]="
 				|| (
-					app-arch/7zip
+					>=app-arch/7zip-24.09[symlink]
 					app-arch/p7zip
 				)
 			"