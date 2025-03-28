2d286567f966fc348fabf40e4bf51324bfd8612c
Ionen Wolkens <ionen@gentoo.org>
2025-03-27 21:23:05 -0400

    media-video/ffmpeg: drop 4.4.5-r1, 6.1.2-r0
    Would have been better to keep these for a bit more in case of new
    issues being spotted in stable with the new ebuilds, but want to
    get rid of this wall of patches and handle the profile cleanups now.
    
    Signed-off-by: Ionen Wolkens <ionen@gentoo.org>

--- media-video/ffmpeg/files/chromium.c
+++
@@ -1,8 +0,0 @@
-#include "libavformat/avformat.h"
-#include "libavformat/internal.h"
-
-int64_t av_stream_get_first_dts(const AVStream *st);
-int64_t av_stream_get_first_dts(const AVStream *st)
-{
-    return cffstream(st)->first_dts;
-}
