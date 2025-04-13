gentoo/gentoo

commit 06b98b354413cbe2e87d14cd0f6f772b12d5a0d9

dev-perl/Locale-gettext: fix final object with libintl on Solaris
Ensure we reference libintl from Prefix, so ld won't fail while using
the object.

Signed-off-by: Fabian Groffen <grobian@gentoo.org>

dev-perl/Locale-gettext/Locale-gettext-1.70.0_p20181130.ebuild
@@ -31,3 +31,9 @@ PATCHES=(
 	"${FILESDIR}/${PN}-1.70.0-tests.patch"
 	"${FILESDIR}/${PN}-1.70.0_p20181130-config-log.patch"
 )
+
+src_compile() {
+	# Makefile.PL cannot know we use libintl over system
+	[[ ${CHOST} == *-solaris* ]] && export LDLOADLIBS="-lintl"
+	default
+}

commit 10b772d35032e04b4c01dc95f1a9f964e2b60d86

dev-perl/Locale-gettext: default -> perl-module_src_compile
Fixes: 06b98b354413cbe2e87d14cd0f6f772b12d5a0d9
Signed-off-by: Sam James <sam@gentoo.org>

dev-perl/Locale-gettext/Locale-gettext-1.70.0_p20181130.ebuild
@@ -35,5 +35,5 @@ PATCHES=(
 src_compile() {
 	# Makefile.PL cannot know we use libintl over system
 	[[ ${CHOST} == *-solaris* ]] && export LDLOADLIBS="-lintl"
-	default
+	perl-module_src_compile
 }