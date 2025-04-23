960b56a225d05a8893442591c604e1944c9c4d29
Ulrich Müller <ulm@gentoo.org>
2025-04-23 20:32:50 +0200

    media-sound/lilypond: Drop header line from Emacs site-init file
    elisp-site-file-install() adds the line by itself.
    
    Signed-off-by: Ulrich Müller <ulm@gentoo.org>

--- media-sound/lilypond/files/50lilypond-gentoo.el
+++ media-sound/lilypond/files/50lilypond-gentoo.el
@@ -1,6 +1,3 @@
-
-;;; lilypond site-lisp configuration
-
 (add-to-list 'load-path "@SITELISP@")
 (autoload 'LilyPond-mode "lilypond-mode" "LilyPond Editing Mode" t)
 (add-to-list 'auto-mode-alist '("\\.i?ly\\'" . LilyPond-mode))
