83ca4be89b08e8766e662cef5a7cbe3ebdd5cf54
Ulrich Müller <ulm@gentoo.org>
2025-04-23 20:32:36 +0200

    app-emacs/puppet-mode: Drop header line from Emacs site-init file
    elisp-site-file-install() adds the line by itself.
    
    Signed-off-by: Ulrich Müller <ulm@gentoo.org>

--- app-emacs/puppet-mode/files/50puppet-mode-1-gentoo.el
+++ app-emacs/puppet-mode/files/50puppet-mode-1-gentoo.el
@@ -1,6 +1,3 @@
-
-;;; puppet-mode site-lisp configuration
-
 (add-to-list 'load-path "@SITELISP@")
 (autoload 'puppet-mode "puppet-mode" "Major mode for editing puppet manifests")
 (add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
