gentoo/gentoo

commit 752cede35d699832cffee64cedfd68fa427dbbaa

app-office/libreoffice: update git repo location
Closes: https://bugs.gentoo.org/954401
Signed-off-by: Sam James <sam@gentoo.org>

app-office/libreoffice/libreoffice-9999.ebuild
@@ -341,7 +341,7 @@ src_unpack() {
 
 	if [[ ${MY_PV} = *9999* ]]; then
 		local base_uri branch mypv
-		base_uri="https://anongit.freedesktop.org/git"
+		base_uri="https://git.libreoffice.org"
 		branch="master"
 		mypv=${MY_PV/.9999}
 		[[ ${mypv} != ${MY_PV} ]] && branch="${PN}-${mypv/./-}"

app-office/libreoffice/libreoffice-25.2.9999.ebuild
@@ -350,7 +350,7 @@ src_unpack() {
 
 	if [[ ${MY_PV} = *9999* ]]; then
 		local base_uri branch mypv
-		base_uri="https://anongit.freedesktop.org/git"
+		base_uri="https://git.libreoffice.org"
 		branch="master"
 		mypv=${MY_PV/.9999}
 		[[ ${mypv} != ${MY_PV} ]] && branch="${PN}-${mypv/./-}"

commit 03824f4490eb544dd0363cf0dc8929eb58ee7f34

app-office/libreoffice: fix git repo
Fixes: 752cede35d699832cffee64cedfd68fa427dbbaa
Bug: https://bugs.gentoo.org/954401
Thanks-to: jospezial
Signed-off-by: Sam James <sam@gentoo.org>

app-office/libreoffice/libreoffice-9999.ebuild
@@ -345,8 +345,8 @@ src_unpack() {
 		branch="master"
 		mypv=${MY_PV/.9999}
 		[[ ${mypv} != ${MY_PV} ]] && branch="${PN}-${mypv/./-}"
-		git-r3_fetch "${base_uri}/${PN}/core" "refs/heads/${branch}"
-		git-r3_checkout "${base_uri}/${PN}/core"
+		git-r3_fetch "${base_uri}/core" "refs/heads/${branch}"
+		git-r3_checkout "${base_uri}/core"
 		LOCOREGIT_VERSION=${EGIT_VERSION}
 
 		git-r3_fetch "${base_uri}/${PN}/help" "refs/heads/master"