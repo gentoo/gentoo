gentoo/gentoo

commit 42c855fd2687e39bd37635ad639dfa36f4c7a88f

dev-tex/biblatex: fix pdf doc installation
Signed-off-by: Florian Schmaus <flow@gentoo.org>
Closes: https://github.com/gentoo/gentoo/pull/40664
Signed-off-by: Florian Schmaus <flow@gentoo.org>

dev-tex/biblatex/biblatex-3.20-r1.ebuild
@@ -1,0 +1,45 @@
+# Copyright 1999-2025 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit latex-package
+
+DESCRIPTION="Reimplementation of the bibliographic facilities provided by LaTeX"
+HOMEPAGE="https://www.ctan.org/tex-archive/macros/latex/contrib/biblatex https://github.com/plk/biblatex/"
+SRC_URI="https://downloads.sourceforge.net/${PN}/${P}.tds.tgz"
+
+S="${WORKDIR}"
+
+LICENSE="LPPL-1.3"
+SLOT="0"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~loong ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86"
+
+IUSE="+biber doc examples"
+
+DEPEND="
+	dev-texlive/texlive-bibtexextra
+	dev-texlive/texlive-latexextra
+	dev-texlive/texlive-plaingeneric
+"
+RDEPEND="${DEPEND}"
+# biblatex and biber must always have compatible versions
+PDEPEND="biber? ( ~dev-tex/biber-2.$(ver_cut 2) )"
+
+TEXMF=/usr/share/texmf-site
+
+src_install() {
+	insinto "${TEXMF}"
+	doins -r bibtex tex
+
+	dodoc doc/latex/biblatex/{README,CHANGES.md}
+	if use doc; then
+		pushd doc/latex/biblatex >/dev/null || die
+		LATEX_ENGINE=lualatex latex-package_src_doinstall doc
+		popd >/dev/null || die
+	fi
+
+	if use examples ; then
+		dodoc -r doc/latex/biblatex/examples
+	fi
+}

commit dd3398e9a84220595e11fdef586402537c61a890

dev-tex/biblatex: missing texlive-luatex dependency for doc use flag
Closes: https://bugs.gentoo.org/951127
Fixes: 42c855fd2687e39bd37635ad639dfa36f4c7a88f
Signed-off-by: Alfred Wingate <parona@protonmail.com>

dev-tex/biblatex/biblatex-3.20-r1.ebuild
@@ -28,6 +28,7 @@ PDEPEND="biber? ( ~dev-tex/biber-2.$(ver_cut 2) )"
 # corefonts for "Courier New"
 BDEPEND="
 	doc? (
+		dev-texlive/texlive-luatex
 		media-fonts/corefonts
 		virtual/latex-base
 	)