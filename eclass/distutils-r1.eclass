gentoo/gentoo

commit d5b7d1b42500509d31a385fbae77facab02769b7

distutils-r1.eclass: Set script shebangs to venv in PEP 517
Alter the shebangs of generated scripts to point out the to venv Python
executables in PEP 517 mode.  Otherwise, the executables are run with
system Python during the test run and therefore do not use the venv.

For convenience, rather than modifying them back just copy them
immediately into the python-exec directory.  This means that instead of
reverting some of our python_compile() changes in python_install()
and then moving scripts to the python-exec directory, we can just
discard /usr/bin and let the wrapping logic recreate it.

While at it, stop repeating ${root}${EPREFIX}/usr/bin multiple times
in python_compile().  Just shove it into a helper variable.  Also move
pyvenv.cfg inside bindir for convenience — Python is fine with either
location.

Signed-off-by: Michał Górny <mgorny@gentoo.org>

eclass/distutils-r1.eclass
@@ -982,14 +982,30 @@ distutils-r1_python_compile() {
 			esetup.py clean -a
 		fi
 
+		# copy executables to python-exec directory
+		# we do it early so that we can alter bindir recklessly
+		local bindir=${root}${EPREFIX}/usr/bin
+		local rscriptdir=${root}$(python_get_scriptdir)
+		[[ -d ${rscriptdir} ]] &&
+			die "${rscriptdir} should not exist!"
+		if [[ -d ${bindir} ]]; then
+			mkdir -p "${rscriptdir}" || die
+			cp -a --reflink=auto "${bindir}"/. "${rscriptdir}"/ || die
+		fi
+
 		# enable venv magic inside the install tree
-		mkdir -p "${root}${EPREFIX}"/usr/bin || die
-		ln -s "${PYTHON}" "${root}${EPREFIX}/usr/bin/${EPYTHON}" || die
-		ln -s "${EPYTHON}" "${root}${EPREFIX}/usr/bin/python3" || die
-		ln -s "${EPYTHON}" "${root}${EPREFIX}/usr/bin/python" || die
-		cat > "${root}${EPREFIX}"/usr/pyvenv.cfg <<-EOF || die
+		mkdir -p "${bindir}" || die
+		ln -s "${PYTHON}" "${bindir}/${EPYTHON}" || die
+		ln -s "${EPYTHON}" "${bindir}/python3" || die
+		ln -s "${EPYTHON}" "${bindir}/python" || die
+		cat > "${bindir}"/pyvenv.cfg <<-EOF || die
 			include-system-site-packages = true
 		EOF
+
+		# we need to change shebangs to point to the venv-python
+		find "${bindir}" -type f -exec sed -i \
+			-e "1s@^#!\(${EPREFIX}/usr/bin/\(python\|pypy\)\)@#!${root}\1@" \
+			{} + || die
 	fi
 }
 
@@ -1105,16 +1121,9 @@ distutils-r1_python_install() {
 	local scriptdir=${EPREFIX}/usr/bin
 	if [[ ${DISTUTILS_USE_PEP517} ]]; then
 		local root=${BUILD_DIR}/install
-		local rscriptdir=${root}$(python_get_scriptdir)
-		[[ -d ${rscriptdir} ]] &&
-			die "${rscriptdir} should not exist!"
-		# remove venv magic
-		rm "${root}${EPREFIX}"/usr/{pyvenv.cfg,bin/{python,python3,${EPYTHON}}} || die
-		find "${root}${EPREFIX}"/usr/bin -empty -delete || die
-		if [[ ! ${DISTUTILS_SINGLE_IMPL} && -d ${root}${EPREFIX}/usr/bin ]]; then
-			mkdir -p "${rscriptdir%/*}" || die
-			mv "${root}${EPREFIX}/usr/bin" "${rscriptdir}" || die
-		fi
+		# remove the altered bindir, executables from the package
+		# are already in scriptdir
+		rm -r "${root}${scriptdir}" || die
 	else
 		local root=${D%/}/_${EPYTHON}
 		[[ ${DISTUTILS_SINGLE_IMPL} ]] && root=${D%/}

commit 3adc7e900eba944aafa51675355df21de79be358

distutils-r1.eclass: Fix pyvenv correctly for Python 3.14
Python 3.14 cleaned up the venv logic, and we have been apparently
relying on some hacks that worked by accident.  More specifically:

1. pyvenv.cfg location is used explicitly as sys.prefix, so we can
   no longer put it inside bin/ subdirectory (no clue why we did that
   -- probably a long chain of historical reasons).

2. "home =" must always be present for venv detection to work.

With these changes, Python 3.14 seems now able to correctly detect
the venv and set prefixes accordingly.  Since they improve correctness,
they should also be fine for other Python versions.

Apparently, the pyvenv.cfg file was moved into bindir back in 2022,
for the trivial reason of using a single directory for all created
files.  Mea culpa!

Fixes: d5b7d1b42500509d31a385fbae77facab02769b7
Signed-off-by: Michał Górny <mgorny@gentoo.org>

eclass/distutils-r1.eclass
@@ -1662,7 +1662,7 @@ distutils-r1_python_install() {
 		# let's explicitly verify these assumptions
 
 		# remove files that we've created explicitly
-		rm "${reg_scriptdir}"/{"${EPYTHON}",python3,python,pyvenv.cfg} || die
+		rm "${reg_scriptdir}"/{"${EPYTHON}",python3,python,../pyvenv.cfg} || die
 
 		# Automagically do the QA check to avoid issues when bootstrapping
 		# prefix.
@@ -2030,9 +2030,11 @@ _distutils-r1_post_python_compile() {
 		ln -s "${PYTHON}" "${bindir}/${EPYTHON}" || die
 		ln -s "${EPYTHON}" "${bindir}/python3" || die
 		ln -s "${EPYTHON}" "${bindir}/python" || die
-		# python3.14t seems to require "home" being present
-		# (though it does not seem to care about the actual value)
-		cat > "${bindir}"/pyvenv.cfg <<-EOF || die
+		# python3.14 changed venv logic so that:
+		# 1) pyvenv.cfg location explicitly determines prefix
+		#    (i.e. we no longer can be put in bin/)
+		# 2) "home =" key must be present
+		cat > "${bindir}"/../pyvenv.cfg <<-EOF || die
 			home = ${EPREFIX}/usr/bin
 			include-system-site-packages = true
 		EOF