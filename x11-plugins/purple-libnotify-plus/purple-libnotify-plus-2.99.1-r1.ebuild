gentoo/gentoo

commit 8a296ea717812962ffe8a8aef243e3e8c34646d6

x11-plugins/purple-libnotify-plus: port to EAPI 7
Closes: https://bugs.gentoo.org/717422
Package-Manager: Portage-3.0.8, Repoman-3.0.1
Signed-off-by: Jakov Smolic <jakov.smolic@sartura.hr>
Signed-off-by: David Seifert <soap@gentoo.org>

x11-plugins/purple-libnotify-plus/files/purple-libnotify-plus-2.99.1-fno-common.patch
@@ -1,0 +1,11 @@
+--- a/src/purple-libnotify+-common.h
++++ b/src/purple-libnotify+-common.h
+@@ -27,7 +27,7 @@
+ 
+ extern PurplePlugin *notify_plus;
+ 
+-struct
++extern struct
+ {
+ 	gboolean modify_notification;
+ 	gboolean use_svg;

x11-plugins/purple-libnotify-plus/purple-libnotify-plus-2.99.1.ebuild
@@ -1,9 +1,9 @@
-# Copyright 1999-2018 Gentoo Foundation
+# Copyright 1999-2020 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-EAPI=5
+EAPI=7
 
-inherit autotools eutils
+inherit autotools
 
 DESCRIPTION="Provide libnotify interface to Pidgin and Finch"
 HOMEPAGE="http://purple-libnotify-plus.sardemff7.net/"
@@ -12,18 +12,21 @@ SRC_URI="https://github.com/sardemff7/purple-libnotify-plus/archive/v${PV}.tar.g
 LICENSE="GPL-3"
 SLOT="0"
 KEYWORDS="amd64 x86"
-IUSE=""
 
 RDEPEND="
 	net-im/pidgin
 	>=net-im/purple-events-0.99.1
 	x11-libs/gdk-pixbuf
 	>=x11-libs/libnotify-0.7.0"
-DEPEND="${RDEPEND}
+DEPEND="${RDEPEND}"
+BDEPEND="
 	dev-util/intltool
 	sys-devel/gettext
 	virtual/pkgconfig"
 
+PATCHES=( "${FILESDIR}"/${P}-fno-common.patch )
+
 src_prepare() {
+	default
 	eautoreconf
 }

commit b28ab1cec66081d2f177f4d9e7990a524a22005a

x11-plugins/purple-libnotify-plus: Update gcc-11 patch
The old patch did not fix the code correctly, so update it to enable the
plugin to work. Thanks to Bill Prendergast for providing the patch.

Fixes: 8a296ea717812962ffe8a8aef243e3e8c34646d6
Closes: https://bugs.gentoo.org/955791
Bug: https://bugs.gentoo.org/717422
Signed-off-by: Jakov Smolić <jsmolic@gentoo.org>

x11-plugins/purple-libnotify-plus/files/purple-libnotify-plus-2.99.1-fno-common.patch
@@ -1,11 +1,33 @@
+# https://bugs.gentoo.org/955791
+# https://bugs.gentoo.org/717422
+# From: Bill Prendergast <dek.devel@baisenvar.info>
+
 --- a/src/purple-libnotify+-common.h
 +++ b/src/purple-libnotify+-common.h
-@@ -27,7 +27,7 @@
+@@ -27,13 +27,14 @@
  
  extern PurplePlugin *notify_plus;
  
 -struct
-+extern struct
++struct notify_plus_data_
  {
  	gboolean modify_notification;
  	gboolean use_svg;
+ 	gboolean overlay_icon;
+ 	gboolean set_transcient;
+ 	gboolean actions;
+-} notify_plus_data;
++};
++extern struct notify_plus_data_ notify_plus_data;
+ 
+ #endif /* __PURPLE_LIBNOTIFY_PLUS_COMMON_H__ */
+--- a/src/purple-libnotify+.c
++++ b/src/purple-libnotify+.c
+@@ -30,6 +30,7 @@
+ #include "purple-libnotify+-frames.h"
+ 
+ PurplePlugin *notify_plus = NULL;
++struct notify_plus_data_ notify_plus_data;
+ 
+ static void
+ _purple_notify_plus_signed_on(PurpleBuddy *buddy, PurplePlugin *plugin)

x11-plugins/purple-libnotify-plus/purple-libnotify-plus-2.99.1-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2022 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7