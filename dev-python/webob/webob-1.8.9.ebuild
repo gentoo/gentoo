gentoo/gentoo

commit 85288c830ba8da025ad5db25820a0641140252ba

dev-python/webob: enable py3.14
Signed-off-by: Sam James <sam@gentoo.org>

dev-python/webob/webob-1.8.9.ebuild
@@ -5,7 +5,7 @@ EAPI=8
 
 DISTUTILS_USE_PEP517=setuptools
 PYPI_PN="WebOb"
-PYTHON_COMPAT=( pypy3 pypy3_11 python3_{10..13} )
+PYTHON_COMPAT=( pypy3_11 python3_{11..14} )
 
 inherit distutils-r1 pypi
 
@@ -23,8 +23,23 @@ KEYWORDS="~alpha amd64 arm arm64 hppa ~loong ~m68k ~mips ppc ppc64 ~riscv ~s390 
 RDEPEND="
 	$(python_gen_cond_dep '
 		>=dev-python/legacy-cgi-2.6[${PYTHON_USEDEP}]
-	' 3.13)
+	' 3.{13..14})
 "
+
+PATCHES=(
+	"${FILESDIR}"/webob-1.8.9-timeout.patch
+)
 
 distutils_enable_sphinx docs 'dev-python/alabaster'
 distutils_enable_tests pytest
+
+python_test() {
+	if [[ ${EPYTHON} == python3.14* ]] ; then
+		EPYTEST_DESELECT+=(
+			# https://github.com/Pylons/webob/issues/479
+			tests/test_in_wsgiref.py::test_interrupted_request
+		)
+	fi
+
+	epytest
+}

commit 7afe82e9a31268ac88fd104fc8f5af16a8393c97

dev-python/webob: drop missing patch reference
I dropped it last-minute as it wasn't needed in the end. Reported by
mgorny.

Fixes: 85288c830ba8da025ad5db25820a0641140252ba
Signed-off-by: Sam James <sam@gentoo.org>

dev-python/webob/webob-1.8.9.ebuild
@@ -26,10 +26,6 @@ RDEPEND="
 	' 3.{13..14})
 "
 
-PATCHES=(
-	"${FILESDIR}"/webob-1.8.9-timeout.patch
-)
-
 distutils_enable_sphinx docs 'dev-python/alabaster'
 distutils_enable_tests pytest
 