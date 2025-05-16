gentoo/gentoo

commit df2788666f786b7416a3a7f931b8b2bf07fe9c3d

app-misc/screen: Add missing pty.h header
Closes: https://bugs.gentoo.org/953896

Source:
 - https://patchwork.ozlabs.org/project/buildroot/patch/ZrWionLijDXHPUjW@waldemar-brodkorb.de/#3360344
 - https://lists.buildroot.org/pipermail/buildroot/2024-August/760074.html

Signed-off-by: Sungjoon Moon <sumoon@seoulsaram.org>
Part-of: https://github.com/gentoo/gentoo/pull/42084
Closes: https://github.com/gentoo/gentoo/pull/42084
Signed-off-by: Sam James <sam@gentoo.org>

app-misc/screen/files/screen-4.9.1-add-missing-pty.h-header.patch
@@ -1,0 +1,12 @@
+diff --git a/src/pty.c b/src/pty.c
+index 6791fd5..093273d 100644
+--- a/src/pty.c
++++ b/src/pty.c
+@@ -29,6 +29,7 @@
+ #include <sys/types.h>
+ #include <sys/stat.h>
+ #include <fcntl.h>
++#include <pty.h>
+ #include <signal.h>
+ 
+ #if defined(__OpenBSD__)

app-misc/screen/screen-4.9.1-r1.ebuild
@@ -34,6 +34,7 @@ PATCHES=(
 	# Don't use utempter even if it is found on the system.
 	"${FILESDIR}"/${PN}-4.3.0-no-utempter.patch
 	"${FILESDIR}"/${PN}-4.9.1-utmp-exit.patch
+	"${FILESDIR}"/${PN}-4.9.1-add-missing-pty.h-header.patch
 )
 
 src_prepare() {

commit 5c23bc78c2378935d5f005e1f4dcbdadab7a0ebf

app-misc/screen: Fix wrong patch file path
Bug: https://bugs.gentoo.org/953896
Fixes: df2788666f786b7416a3a7f931b8b2bf07fe9c3d
Signed-off-by: Sungjoon Moon <sumoon@seoulsaram.org>

app-misc/screen/files/screen-4.9.1-add-missing-pty.h-header.patch
@@ -1,0 +1,10 @@
+--- a/pty.c
++++ b/pty.c
+@@ -29,6 +29,7 @@
+ #include <sys/types.h>
+ #include <sys/stat.h>
+ #include <fcntl.h>
++#include <pty.h>
+ #include <signal.h>
+ 
+ #if defined(__OpenBSD__)

app-misc/screen/screen-4.9.1-r1.ebuild
@@ -34,6 +34,7 @@ PATCHES=(
 	# Don't use utempter even if it is found on the system.
 	"${FILESDIR}"/${PN}-4.3.0-no-utempter.patch
 	"${FILESDIR}"/${PN}-4.9.1-utmp-exit.patch
+	"${FILESDIR}"/${PN}-4.9.1-add-missing-pty.h-header.patch
 )
 
 src_prepare() {