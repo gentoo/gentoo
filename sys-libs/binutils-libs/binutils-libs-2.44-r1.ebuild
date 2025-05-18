gentoo/gentoo

commit b2d68e3a4bae7c7b144fabe58efdccf68c862628

sys-libs/binutils-libs: always guarantee fat LTO objects
I was preoccupied earlier by the test issue. libiberty.a is always
installed, regardless of USE=startic-libs, so we need to always call
lto-guarantee-fat.

Pointed out by Eli.

Bug: https://bugs.gentoo.org/924360
Fixes: a00c963757178daa0cd775d36f6fa5443516a29b
Signed-off-by: Sam James <sam@gentoo.org>

sys-libs/binutils-libs/binutils-libs-2.44-r1.ebuild
@@ -126,7 +126,7 @@ pkgversion() {
 }
 
 src_configure() {
-	use static-libs && lto-guarantee-fat
+	lto-guarantee-fat
 	multilib-minimal_src_configure
 }
 

sys-libs/binutils-libs/binutils-libs-9999.ebuild
@@ -126,7 +126,7 @@ pkgversion() {
 }
 
 src_configure() {
-	use static-libs && lto-guarantee-fat
+	lto-guarantee-fat
 	multilib-minimal_src_configure
 }
 

commit 4b1e2a5cf472ea42ffed7ceef1c5ece155e09662

sys-libs/binutils-libs: properly strip LTO bytecode from static libs
The fix in b2d68e3a4bae7c7b144fabe58efdccf68c862628 still wasn't
right but I didn't notice because of the filter-lto.

Bug: https://bugs.gentoo.org/924360
Fixes: b2d68e3a4bae7c7b144fabe58efdccf68c862628
Fixes: a00c963757178daa0cd775d36f6fa5443516a29b
Signed-off-by: Sam James <sam@gentoo.org>

sys-libs/binutils-libs/binutils-libs-2.44-r1.ebuild
@@ -227,5 +227,6 @@ multilib_src_install() {
 
 multilib_src_install_all() {
 	use static-libs || find "${ED}"/usr -name '*.la' -delete
-	strip-lto-bytecode
+	# Explicit "${ED}" as we need it to do things even w/ USE=-static-libs
+	strip-lto-bytecode "${ED}"
 }

sys-libs/binutils-libs/binutils-libs-9999.ebuild
@@ -227,5 +227,6 @@ multilib_src_install() {
 
 multilib_src_install_all() {
 	use static-libs || find "${ED}"/usr -name '*.la' -delete
-	strip-lto-bytecode
+	# Explicit "${ED}" as we need it to do things even w/ USE=-static-libs
+	strip-lto-bytecode "${ED}"
 }