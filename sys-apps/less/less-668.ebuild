gentoo/gentoo

commit a2df6c1cd08019b86197978bf8974cadc4cedf24

sys-apps/less: add 678
Signed-off-by: Sam James <sam@gentoo.org>

sys-apps/less/less-678.ebuild
@@ -25,7 +25,7 @@ else
 	SRC_URI="https://www.greenwoodsoftware.com/less/${MY_P}.tar.gz"
 
 	if [[ ${PV} != *_beta* ]] ; then
-		KEYWORDS="~alpha amd64 arm arm64 hppa ~loong ~m68k ~mips ppc ppc64 ~riscv ~s390 sparc x86 ~amd64-linux ~x86-linux ~arm64-macos ~ppc-macos ~x64-macos ~x64-solaris"
+		KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~loong ~m68k ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86 ~amd64-linux ~x86-linux ~arm64-macos ~ppc-macos ~x64-macos ~x64-solaris"
 	fi
 fi
 
@@ -34,7 +34,8 @@ S="${WORKDIR}"/${MY_P/?beta}
 LICENSE="|| ( GPL-3 BSD-2 )"
 SLOT="0"
 IUSE="pcre test"
-RESTRICT="!test? ( test )"
+# chinese1, utf8-2
+RESTRICT="test !test? ( test )"
 
 DEPEND="
 	>=app-misc/editor-wrapper-3

sys-apps/less/Manifest
@@ -1,2 +1,3 @@
 DIST less-643.tar.gz 592291 BLAKE2B 6dc60dc2e8db05afdae466877a1d26a3008ff5378bbbf2fbdf9efc4f87c0fcfde5703d44a24d4355c98d3a5f438bdb51173150f2a69f801d9c8e4a7401d71b53 SHA512 6a324ac54e22429ac652dc303bc1fe48933555d1cbf8ad7ecf345940910c014fef9551a3219743cfb7115e356b5841ae97d6ce62e7a1ba1e3300d243efca34d9
 DIST less-668.tar.gz 649770 BLAKE2B 0f6a85a1c66577dd2a28682a6f8399e42fdbe9fc1498b2e89c6bb7c47109e8d3ab934abe5dd998b2c8dfacfb174ad9daeb79b3d4c13df22fa035ea792b2eaf5e SHA512 6527741a609849ec27b6ab35de46f2c5c2b2771391e3efe263207bee28915946f44331a42f832a7cb42231a19b135cbadc15ae02cd997b83a13cdfbc7213fc27
+DIST less-678.tar.gz 876971 BLAKE2B 5e6644a8c66f35f1b3ab72a0727c76922b7d3cf299d5d1cf1c94882e8b0d3ec0461cd351e6b2db554b331d993760db8eb17209f41f271f5704fee0caf8d6540e SHA512 ad1983229bfb5a30c6139db60012d3a12b613011f044acb4c0ec5c0fc809b8dc9be40255bf420f65d51425152c4cfc943076ccd2ee63b196165d6ab1426ea187

commit 85b66d31a9a67ad139435042e3424ee3d9b65fc1

sys-apps/less: fix test change
I meant to unrestrict them in 678.

Closes: https://bugs.gentoo.org/956309
Fixes: a2df6c1cd08019b86197978bf8974cadc4cedf24
Signed-off-by: Sam James <sam@gentoo.org>

sys-apps/less/less-668.ebuild
@@ -34,7 +34,8 @@ S="${WORKDIR}"/${MY_P/?beta}
 LICENSE="|| ( GPL-3 BSD-2 )"
 SLOT="0"
 IUSE="pcre test"
-RESTRICT="!test? ( test )"
+# chinese1, utf8-2
+RESTRICT="test !test? ( test )"
 
 DEPEND="
 	>=app-misc/editor-wrapper-3

sys-apps/less/less-678.ebuild
@@ -34,8 +34,7 @@ S="${WORKDIR}"/${MY_P/?beta}
 LICENSE="|| ( GPL-3 BSD-2 )"
 SLOT="0"
 IUSE="pcre test"
-# chinese1, utf8-2
-RESTRICT="test !test? ( test )"
+RESTRICT="!test? ( test )"
 
 DEPEND="
 	>=app-misc/editor-wrapper-3

sys-apps/less/less-9999.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -25,7 +25,7 @@ else
 	SRC_URI="https://www.greenwoodsoftware.com/less/${MY_P}.tar.gz"
 
 	if [[ ${PV} != *_beta* ]] ; then
-		KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~loong ~m68k ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86 ~amd64-linux ~x86-linux ~arm64-macos ~ppc-macos ~x64-macos ~x64-solaris"
+		KEYWORDS="~alpha amd64 arm arm64 hppa ~loong ~m68k ~mips ppc ppc64 ~riscv ~s390 sparc x86 ~amd64-linux ~x86-linux ~arm64-macos ~ppc-macos ~x64-macos ~x64-solaris"
 	fi
 fi
 