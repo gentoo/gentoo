gentoo/gentoo

commit a240fd5c2da438f3e9a7142a8bbb2028e6a7182d

media-gfx/mypaint: use tc-check-openmp
Signed-off-by: Sam James <sam@gentoo.org>

media-gfx/mypaint/mypaint-2.0.1-r4.ebuild
@@ -5,7 +5,7 @@ EAPI=8
 
 PYTHON_COMPAT=( python3_{10..13} )
 
-inherit desktop edo python-single-r1 xdg
+inherit desktop edo python-single-r1 toolchain-funcs xdg
 
 DESCRIPTION="Fast and easy graphics application for digital painters"
 HOMEPAGE="http://mypaint.app/"
@@ -57,6 +57,14 @@ PATCHES=(
 	"${FILESDIR}"/${P}-setuptools.patch
 	"${FILESDIR}"/${PN}-2.0.1-python3.11.patch
 )
+
+pkg_pretend() {
+	[[ ${MERGE_TYPE} != binary ]] && use openmp && tc-check-openmp
+}
+
+pkg_setup() {
+	[[ ${MERGE_TYPE} != binary ]] && use openmp && tc-check-openmp
+}
 
 src_compile() {
 	# --disable-openmp can't be passed to setup.py build,

commit 997187678d2019791e2fec41328de5a038f814d4

media-gfx/mypaint: fix pkg_setup
Fixes: a240fd5c2da438f3e9a7142a8bbb2028e6a7182d
Signed-off-by: Sam James <sam@gentoo.org>

media-gfx/mypaint/mypaint-2.0.1-r4.ebuild
@@ -64,6 +64,7 @@ pkg_pretend() {
 
 pkg_setup() {
 	[[ ${MERGE_TYPE} != binary ]] && use openmp && tc-check-openmp
+	python-single-r1_pkg_setup
 }
 
 src_compile() {