gentoo/gentoo

commit 53b9c858f0f1177c4fc9c34758c74c2d8b6399ce

app-emulation/faudio: add multilib
Signed-off-by: Lee Zhi Wei (Dennis) <zhiweilee.com@gmail.com>
Part-of: https://github.com/gentoo/gentoo/pull/41669
Closes: https://github.com/gentoo/gentoo/pull/41669
Signed-off-by: Conrad Kostecki <conikost@gentoo.org>

app-emulation/faudio/faudio-25.04.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-inherit cmake flag-o-matic
+inherit cmake-multilib flag-o-matic
 
 DESCRIPTION="Accuracy-focused XAudio reimplementation for open platforms"
 HOMEPAGE="https://fna-xna.github.io/"
@@ -17,8 +17,8 @@ IUSE="debug dumpvoices sdl3 test"
 RESTRICT="!test? ( test )"
 
 RDEPEND="
-	sdl3? ( media-libs/libsdl3 )
-	!sdl3? ( media-libs/libsdl2[sound] )
+	sdl3? ( media-libs/libsdl3[${MULTILIB_USEDEP}] )
+	!sdl3? ( media-libs/libsdl2[${MULTILIB_USEDEP},sound] )
 "
 DEPEND="${RDEPEND}"
 
@@ -32,7 +32,7 @@ src_configure() {
 		-DDUMP_VOICES="$(usex dumpvoices)"
 	)
 
-	cmake_src_configure
+	cmake-multilib_src_configure
 }
 
 src_test() {

commit a1b96c8eb5d09d60d671b9734bc4e16f7d712b5b

app-emulation/faudio: fix multilib tests
This broke with 53b9c858f0f1177c4fc9c34758c74c2d8b6399ce.

Closes: https://bugs.gentoo.org/957400
Signed-off-by: Sam James <sam@gentoo.org>

app-emulation/faudio/faudio-25.04.ebuild
@@ -35,7 +35,7 @@ src_configure() {
 	cmake-multilib_src_configure
 }
 
-src_test() {
+multilib_src_test() {
 	einfo "Running faudio_tests, this may take some time without output..."
 	SDL_AUDIODRIVER=dummy "${BUILD_DIR}"/faudio_tests || die
 }