gentoo/gentoo

commit b68dfd3e172b0f70464d00c95913d322b6bc520f

net-misc/icaclient: add proper libxml2 subslot + compat
Not going to open the can of worms on selfservice any further...

Signed-off-by: Alfred Wingate <parona@protonmail.com>
Part-of: https://github.com/gentoo/gentoo/pull/41919
Signed-off-by: Sam James <sam@gentoo.org>

net-misc/icaclient/icaclient-24.11.0.85-r1.ebuild
@@ -54,6 +54,10 @@ RDEPEND="
 	>=app-accessibility/at-spi2-core-2.46.0:2
 	app-crypt/libsecret
 	dev-libs/glib:2
+	|| (
+		dev-libs/libxml2:2/0
+		dev-libs/libxml2-compat
+	)
 	media-fonts/font-adobe-100dpi
 	media-fonts/font-cursor-misc
 	media-fonts/font-misc-ethiopic
@@ -98,7 +102,6 @@ RDEPEND="
 	!hdx? ( !media-plugins/hdx-realtime-media-engine )
 	usb? ( virtual/libudev )
 	selfservice? (
-		dev-libs/libxml2
 		net-libs/webkit-gtk:4.1
 		dev-libs/xerces-c
 	)

net-misc/icaclient/icaclient-24.2.0.65-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -54,7 +54,10 @@ RDEPEND="
 	>=app-accessibility/at-spi2-core-2.46.0:2
 	app-crypt/libsecret
 	dev-libs/glib:2
-	dev-libs/libxml2
+	|| (
+		dev-libs/libxml2:2/0
+		dev-libs/libxml2-compat
+	)
 	media-fonts/font-adobe-100dpi
 	media-fonts/font-cursor-misc
 	media-fonts/font-misc-ethiopic

commit 2c057a00a0695fb57dcd7308a6fb91d3344f72ee

net-misc/icaclient: fix libxml2 subslot dep
"When the sub-slot part is omitted from the SLOT definition, the package is
considered to have an implicit sub-slot which is equal to the regular slot."

Fixes: b68dfd3e172b0f70464d00c95913d322b6bc520f
Signed-off-by: Sam James <sam@gentoo.org>

net-misc/icaclient/icaclient-24.11.0.85-r1.ebuild
@@ -55,7 +55,7 @@ RDEPEND="
 	app-crypt/libsecret
 	dev-libs/glib:2
 	|| (
-		dev-libs/libxml2:2/0
+		dev-libs/libxml2:2/2
 		dev-libs/libxml2-compat
 	)
 	media-fonts/font-adobe-100dpi

net-misc/icaclient/icaclient-24.2.0.65-r1.ebuild
@@ -55,7 +55,7 @@ RDEPEND="
 	app-crypt/libsecret
 	dev-libs/glib:2
 	|| (
-		dev-libs/libxml2:2/0
+		dev-libs/libxml2:2/2
 		dev-libs/libxml2-compat
 	)
 	media-fonts/font-adobe-100dpi