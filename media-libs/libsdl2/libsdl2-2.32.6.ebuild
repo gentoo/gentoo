gentoo/gentoo

commit af83cdda1378eaa8c8521d92c7d15efc3210e443

media-libs/libsdl2: use dot-a.eclass
... to avoid installing broken static libraries w/ LTO.

Especially important because of libSDL2_test.a.

Signed-off-by: Sam James <sam@gentoo.org>

media-libs/libsdl2/libsdl2-2.32.6.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-inherit cmake-multilib flag-o-matic
+inherit cmake-multilib dot-a flag-o-matic
 
 MY_P="SDL2-${PV}"
 DESCRIPTION="Simple Direct Media Layer"
@@ -121,6 +121,7 @@ src_prepare() {
 
 src_configure() {
 	use custom-cflags || strip-flags
+	lto-guarantee-fat
 
 	local mycmakeargs=(
 		-DSDL_STATIC=$(usex static-libs)
@@ -211,4 +212,5 @@ multilib_src_install_all() {
 	rm -r "${ED}"/usr/share/licenses/ || die
 	dodoc {BUGS,CREDITS,README-SDL,TODO,WhatsNew}.txt README.md docs/README*.md
 	use doc && dodoc -r docs/output/html/
+	strip-lto-bytecode
 }

commit ed625ebb88a52ca4bfd48d6f069d7146c1892da3

media-libs/libsdl2: fix dot-a.eclass usage
This package has USE=static-libs, so we need to prompt it with "${ED}"
to strip bytecode.

Fixes: af83cdda1378eaa8c8521d92c7d15efc3210e443
Signed-off-by: Eli Schwartz <eschwartz@gentoo.org>

media-libs/libsdl2/libsdl2-2.32.6.ebuild
@@ -212,5 +212,5 @@ multilib_src_install_all() {
 	rm -r "${ED}"/usr/share/licenses/ || die
 	dodoc {BUGS,CREDITS,README-SDL,TODO,WhatsNew}.txt README.md docs/README*.md
 	use doc && dodoc -r docs/output/html/
-	strip-lto-bytecode
+	strip-lto-bytecode "${ED}"
 }

media-libs/libsdl2/libsdl2-2.32.8.ebuild
@@ -212,5 +212,5 @@ multilib_src_install_all() {
 	rm -r "${ED}"/usr/share/licenses/ || die
 	dodoc {BUGS,CREDITS,README-SDL,TODO,WhatsNew}.txt README.md docs/README*.md
 	use doc && dodoc -r docs/output/html/
-	strip-lto-bytecode
+	strip-lto-bytecode "${ED}"
 }