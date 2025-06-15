gentoo/gentoo

commit 9fb1c782ddb3d63a69971a26c856cde3f0ba83a7

media-sound/gtick: fix build w/ C23, unrestrict tests
* Fix build w/ C23
* Unrestrict tests by dropping dmalloc requirement

  It dates back to:

  commit a46f63115ab7e5db64009173f0b8662f0965f943
  Author: Samuli Suominen <ssuominen@gentoo.org>
  Date:   Thu Jun 4 19:02:15 2009 +0000

      RESTRICT test because it needs too old dmalloc we don't ship anymore.
      (Portage version: 2.1.6.13/cvs/Linux x86_64)

  But tests for memory leaks aren't really something we're interested in
  from a packaging PoV, plus dmalloc was removed a little while ago. There's
  tests which work fine w/o dmalloc so let's allow running those.

Closes: https://bugs.gentoo.org/946659
Signed-off-by: Sam James <sam@gentoo.org>

media-sound/gtick/gtick-0.5.5-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -22,11 +22,10 @@ DEPEND="${RDEPEND}"
 BDEPEND="virtual/pkgconfig
 	nls? ( sys-devel/gettext )"
 
-RESTRICT="test"
-
 PATCHES=(
 	"${FILESDIR}"/${PN}-0.5.5-desktop.patch
 	"${FILESDIR}"/${PN}-0.5.5-autotools.patch
+	"${FILESDIR}"/${PN}-0.5.5-no-dmalloc-tests.patch
 )
 
 src_prepare() {
@@ -39,6 +38,8 @@ src_prepare() {
 }
 
 src_configure() {
+	#append-cflags -std=gnu17
+
 	local myeconfargs=(
 		$(use_enable nls)
 		$(use_with sndfile)

media-sound/gtick/files/gtick-0.5.5-c23.patch
@@ -1,0 +1,12 @@
+https://bugs.gentoo.org/946659
+--- a/src/optionlexer.h
++++ b/src/optionlexer.h
+@@ -24,7 +24,7 @@
+ #define OPTIONLEXER_H
+ 
+ extern FILE *option_in;
+-int option_parse();
++int option_parse(void *option_list);
+ 
+ void option_lexer_init(char* filename);
+ void option_lexer_deinit();

media-sound/gtick/files/gtick-0.5.5-no-dmalloc-tests.patch
@@ -1,0 +1,12 @@
+We don't want to require last-rited dmalloc for tests.
+--- a/testsuite/Makefile.am
++++ b/testsuite/Makefile.am
+@@ -63,7 +63,7 @@ DEFS = -DLOCALEDIR=\"$(localedir)\" @DEFS@
+ LIBS = @LIBINTL@ @LIBS@
+ 
+ AM_CPPFLAGS = -I../src -I../intl -I$(top_srcdir)/intl @DEPS_CFLAGS@ @SNDFILE_CFLAGS@ @CHECK_CFLAGS@ @DMALLOC_CFLAGS@
+-AM_CFLAGS = -DVERSION='"@VERSION@"' -DPACKAGE='"@PACKAGE@"' -DUSE_DMALLOC
++AM_CFLAGS = -DVERSION='"@VERSION@"' -DPACKAGE='"@PACKAGE@"'
+ 
+ AM_YFLAGS = -d
+ AM_LFLAGS=-olex.yy.c

commit a03a9186f5b2d51c9686bcaa21a97a67fdf6be40

media-sound/gtick: actually apply C23 patch
As spotted by Alexander Tsoy at https://github.com/gentoo/gentoo/pull/42602#issuecomment-2973869822.

Fixes: 9fb1c782ddb3d63a69971a26c856cde3f0ba83a7
Signed-off-by: Sam James <sam@gentoo.org>

media-sound/gtick/gtick-0.5.5-r1.ebuild
@@ -26,6 +26,7 @@ PATCHES=(
 	"${FILESDIR}"/${PN}-0.5.5-desktop.patch
 	"${FILESDIR}"/${PN}-0.5.5-autotools.patch
 	"${FILESDIR}"/${PN}-0.5.5-no-dmalloc-tests.patch
+	"${FILESDIR}"/${PN}-0.5.5-c23.patch
 )
 
 src_prepare() {
@@ -38,8 +39,6 @@ src_prepare() {
 }
 
 src_configure() {
-	#append-cflags -std=gnu17
-
 	local myeconfargs=(
 		$(use_enable nls)
 		$(use_with sndfile)