gentoo/gentoo

commit 26cf224e489af307643dcac107c7240a991dca2a

media-libs/libtheora: workaround broken dist tarball for arm
Bug: https://gitlab.xiph.org/xiph/theora/-/issues/2338
Signed-off-by: Sam James <sam@gentoo.org>

media-libs/libtheora/libtheora-1.2.0-r1.ebuild
@@ -8,6 +8,12 @@ inherit autotools multilib-minimal
 DESCRIPTION="The Theora Video Compression Codec"
 HOMEPAGE="https://www.theora.org"
 SRC_URI="https://downloads.xiph.org/releases/theora/${P/_}.tar.xz"
+# Workaround for https://gitlab.xiph.org/xiph/theora/-/issues/2338 (arm
+# files missing from dist tarball):
+SRC_URI+="
+	https://gitlab.xiph.org/xiph/theora/-/raw/v${PV}/lib/arm/armenc.c -> ${P}-armenc.c
+	https://gitlab.xiph.org/xiph/theora/-/raw/v${PV}/lib/arm/armloop.s -> ${P}-armloop.s
+"
 S="${WORKDIR}"/${P/_}
 
 LICENSE="BSD"
@@ -40,6 +46,10 @@ PATCHES=(
 
 src_prepare() {
 	default
+
+	# Workaround for broken 1.2.0 dist tarball
+	cp "${DISTDIR}"/${P}-{armenc.c,armloop.s} lib/arm || die
+
 	eautoreconf
 }
 

media-libs/libtheora/Manifest
@@ -1,2 +1,4 @@
 DIST libtheora-1.1.1.tar.xz 1434496 BLAKE2B f5df62c68c31537582f8a2ea64dba6b01cc056f364aa06fe905e42cc7aa8c78d7de89419dd52d586210ea29cdfdb24c2ff01e490c9b137e5d2f1845a2aa430de SHA512 032fb60f10ac0074ec87d5f8c586702309edc50dc0d2842d2f9b9c46ebd1500acc399e9309f858f318fff5b8fbe953234db3afb54ed74a03d0f2c6ee8c3ed481
+DIST libtheora-1.2.0-armenc.c 2129 BLAKE2B 83e5ada6560ef7e6b0a26bb72278b62f34c815e07d741ba64b11599b88e817805fe54a9f82c9d0d312995beaae3862be4e1017ce3b3507cd0de678267dc218d1 SHA512 f83045841b17362f133ca45926af687ab1535970f9da215b81b2e4e613ae3cdf295357eb497ebd4e3e94cee283ab388db51ef4dd28cc13d535651defdf223879
+DIST libtheora-1.2.0-armloop.s 23216 BLAKE2B 75bb61eda41c86eecb9d75449358f1363f7f4a9ed356ea3e5ba54fc63d8ef853f37421e5d27b0b1de3e915a35b471c9272b214cb13d93a4ae9a9f05bc1c87261 SHA512 8774bea8ed3adfd51fc727eef3a256fa3817a4e8554995afd572bd8f22b0979b2573c54cfb183dcc254ecce34ade6ef6748339ba91e1dc12a03d163f453032e8
 DIST libtheora-1.2.0.tar.xz 1803124 BLAKE2B f1264e9d5b0624f41780d7b6815f295309529f3c21a1aff9536239c9691b916912c9d3a1877e58506c7e1a37f1367c48d9b0765fe27f6c1db69a361aa9865eee SHA512 849db0c916f0f0b1237a4893594ff4b9e72d4ad1dca42f3f2979af5971f42c3fe77a75b181dfdd1c1353bbca4c832d6ab43f6d509c0650901c692e96ceab5f26

commit 685b7e3a509490c52422cac7c70bbf2d90f3c240

media-libs/libtheora: fixup ARM workaround
We don't want a ${P}- prefix for the actual files once copied.

Fixes: 26cf224e489af307643dcac107c7240a991dca2a
Signed-off-by: Sam James <sam@gentoo.org>

media-libs/libtheora/libtheora-1.2.0-r1.ebuild
@@ -48,7 +48,8 @@ src_prepare() {
 	default
 
 	# Workaround for broken 1.2.0 dist tarball
-	cp "${DISTDIR}"/${P}-{armenc.c,armloop.s} lib/arm || die
+	cp "${DISTDIR}"/${P}-armenc.c lib/arm || die
+	cp "${DISTDIR}"/${P}-armloop.s lib/arm || die
 
 	eautoreconf
 }