gentoo/gentoo

commit 685b7e3a509490c52422cac7c70bbf2d90f3c240

media-libs/libtheora: fixup ARM workaround
We don't want a ${P}- prefix for the actual files once copied.

Fixes: 26cf224e489af307643dcac107c7240a991dca2a
Signed-off-by: Sam James <sam@gentoo.org>

media-libs/libtheora/libtheora-1.2.0-r1.ebuild
@@ -48,7 +48,8 @@ src_prepare() {
 	default
 
 	# Workaround for broken 1.2.0 dist tarball
-	cp "${DISTDIR}"/${P}-{armenc.c,armloop.s} lib/arm || die
+	cp "${DISTDIR}"/${P}-armenc.c lib/arm || die
+	cp "${DISTDIR}"/${P}-armloop.s lib/arm || die
 
 	eautoreconf
 }

commit 8dd11e0b69432ee2027f25af3ce5fdb9ff6a63d3

media-libs/libtheora: fix arm workaround again
Fixes: 685b7e3a509490c52422cac7c70bbf2d90f3c240
Signed-off-by: Sam James <sam@gentoo.org>

media-libs/libtheora/libtheora-1.2.0-r1.ebuild
@@ -48,8 +48,8 @@ src_prepare() {
 	default
 
 	# Workaround for broken 1.2.0 dist tarball
-	cp "${DISTDIR}"/${P}-armenc.c lib/arm || die
-	cp "${DISTDIR}"/${P}-armloop.s lib/arm || die
+	cp "${DISTDIR}"/${P}-armenc.c lib/arm/armenc.c || die
+	cp "${DISTDIR}"/${P}-armloop.s lib/arm/armloop.s || die
 
 	eautoreconf
 }