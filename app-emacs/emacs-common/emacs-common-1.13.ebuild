gentoo/gentoo

commit 57119bdc51dc0cfd9907dfc6c746117bc758f069

app-emacs/emacs-common: Fix sed expression
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

app-emacs/emacs-common/emacs-common-1.13.ebuild
@@ -34,7 +34,7 @@ SITEFILE="10${PN}-gentoo.el"
 src_prepare() {
 	default
 	if [[ -n ${EPREFIX} ]]; then
-		sed -i -E -e "s,/(bin|sbin|usr)/,${EPREFIX}&," \
+		sed -i -E -e "s,/(bin|sbin|usr)/,${EPREFIX},g" \
 			subdirs.el.in emacs.initd emacs.service \
 			emacs.desktop emacsclient.desktop || die
 	fi

commit faf713e938862596e5928b6ad65fefa4f07c69ba

app-emacs/emacs-common: Fix previous commit
Fixes: 57119bdc51dc0cfd9907dfc6c746117bc758f069
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

app-emacs/emacs-common/emacs-common-1.13.ebuild
@@ -34,7 +34,7 @@ SITEFILE="10${PN}-gentoo.el"
 src_prepare() {
 	default
 	if [[ -n ${EPREFIX} ]]; then
-		sed -i -E -e "s,/(bin|sbin|usr)/,${EPREFIX},g" \
+		sed -i -E -e "s,/(bin|sbin|usr)/,${EPREFIX}&,g" \
 			subdirs.el.in emacs.initd emacs.service \
 			emacs.desktop emacsclient.desktop || die
 	fi