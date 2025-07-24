gentoo/gentoo

commit ea4662b23662390eca5b9865852047e4143ec86b

dev-perl/Cache-Memcached-Fast: Version bump 0.27
Closes: https://bugs.gentoo.org/722848
Package-Manager: Portage-3.0.18, Repoman-3.0.2
Signed-off-by: Andreas K. Hüttel <dilfridge@gentoo.org>

dev-perl/Cache-Memcached-Fast/files/Cache-Memcached-Fast-0.270.0-no-flto.patch
@@ -1,0 +1,17 @@
+diff -ruN Cache-Memcached-Fast-0.27.orig/Makefile.PL Cache-Memcached-Fast-0.27/Makefile.PL
+--- Cache-Memcached-Fast-0.27.orig/Makefile.PL	2021-05-22 20:25:50.000000000 +0200
++++ Cache-Memcached-Fast-0.27/Makefile.PL	2021-05-25 21:41:10.991310429 +0200
+@@ -24,6 +24,13 @@
+     },
+ );
+ 
++sub MY::cflags {
++  my $self = shift;
++  my $orig = $self->MM::cflags(@_);
++  $orig =~ s/-flto\b//g;
++  return $orig;
++}
++
+ sub MY::postamble { <<'MAKE' }
+ $(MYEXTLIB): src/Makefile
+ 	cd src && $(MAKE) $(PASTHRU)

dev-perl/Cache-Memcached-Fast/Cache-Memcached-Fast-0.270.0.ebuild
@@ -1,0 +1,60 @@
+# Copyright 1999-2021 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+
+DIST_AUTHOR=RAZ
+DIST_VERSION=0.27
+DIST_EXAMPLES=("script/*")
+inherit perl-module
+
+DESCRIPTION="Perl client for memcached, in C language"
+
+# License note: Perl 5.x or newer, + "when C parts used as standalone library"
+# Bug: https://bugs.gentoo.org/718946#c4
+LICENSE="|| ( Artistic GPL-1+ ) LGPL-2.1+"
+
+SLOT="0"
+KEYWORDS="~amd64 ~x86"
+IUSE="test"
+RESTRICT="!test? ( test )"
+
+RDEPEND="virtual/perl-Storable"
+BDEPEND="${RDEPEND}
+	virtual/perl-ExtUtils-MakeMaker
+	test?	(
+		net-misc/memcached
+		virtual/perl-Test-Simple
+	)
+"
+
+PATCHES=(
+	"${FILESDIR}/0.230.0-parallel-make.patch"
+	"${FILESDIR}/${PN}-0.270.0-no-flto.patch"
+)
+
+PERL_RM_FILES=(
+	"t/pod-coverage.t"
+	"t/pod.t"
+)
+
+src_compile() {
+	mymake=(
+		"OPTIMIZE=${CFLAGS}"
+	)
+	perl-module_src_compile
+}
+
+src_test() {
+	ewarn "t/commands.t is known to fail: https://bugs.gentoo.org/722848"
+	local memcached_opts=( -d -P "${T}/memcached.pid" -p 11211 -l 127.0.0.1 )
+	[[ ${EUID} == 0 ]] && memcached_opts+=( -u portage )
+	memcached "${memcached_opts[@]}" || die "Can't start memcached test server"
+
+	local exit_status
+	perl-module_src_test
+	exit_status=$?
+
+	kill "$(<"${T}/memcached.pid")"
+	return ${exit_status}
+}

dev-perl/Cache-Memcached-Fast/Manifest
@@ -1,1 +1,2 @@
 DIST Cache-Memcached-Fast-0.26.tar.gz 87887 BLAKE2B d7877a56ba94bd173290ccc66ea91d8ff86ee22a9d381a0a8be5049194ffae97b8084f7fdb464c377e632525a9f08084258153e7c358f32926b687f46ca94a53 SHA512 aea565be2d1aaae186c6d985d88e444fa39311338a851f7f05b013affcab0bfcabd258a3df08849b9429b7168ff0d1e59ab27b1e843b217093b2b64786e890b1
+DIST Cache-Memcached-Fast-0.27.tar.gz 88712 BLAKE2B a889e719ae36721dea203b54488d86e52b40df2f1aceab1bca386a5619e80f40e27ef7fdd09ba5ef5c0678659be44b43c9416066585a0b746cf46025e546496b SHA512 6e3f7cb6ae317312a16aad7888fb75ec03ff7be5b79aef1702c8bc8a1499995379b42c1afd6c587d419ea4dbe880836c874d4929f968f7a384ab152e5baa03fa

commit c32f3b741accf04f23ed04142ecbb889b7988e15

dev-perl/Cache-Memcached-Fast: re-add missing patch metadata
Setting the historical record back up.

Original patch contents from 3a948a110ad8e44beba6a6ae3edf67b8cca66981
and researched as originating via an upstream PR

Bug: https://github.com/JRaspass/Cache-Memcached-Fast/pull/11
Fixes: ea4662b23662390eca5b9865852047e4143ec86b
Signed-off-by: Eli Schwartz <eschwartz@gentoo.org>

dev-perl/Cache-Memcached-Fast/Cache-Memcached-Fast-0.280.0-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -33,6 +33,7 @@ BDEPEND="
 
 PATCHES=(
 	"${FILESDIR}/0.230.0-parallel-make.patch"
+	# https://github.com/JRaspass/Cache-Memcached-Fast/pull/11
 	"${FILESDIR}/${PN}-0.270.0-no-flto.patch"
 )
 

dev-perl/Cache-Memcached-Fast/files/Cache-Memcached-Fast-0.270.0-no-flto.patch
@@ -1,6 +1,20 @@
-diff -ruN Cache-Memcached-Fast-0.27.orig/Makefile.PL Cache-Memcached-Fast-0.27/Makefile.PL
---- Cache-Memcached-Fast-0.27.orig/Makefile.PL	2021-05-22 20:25:50.000000000 +0200
-+++ Cache-Memcached-Fast-0.27/Makefile.PL	2021-05-25 21:41:10.991310429 +0200
+https://github.com/JRaspass/Cache-Memcached-Fast/pull/11
+
+From 4bba22fac564a0798022197476f63fb6b481129f Mon Sep 17 00:00:00 2001
+From: Kent Fredric <kentnl@gentoo.org>
+Date: Tue, 3 Oct 2017 20:20:14 +1300
+Subject: [PATCH] Strip -flto from CFLAGS
+
+-flto breaks compilation of static archives and needs significant  magic
+to not break, including sentience about which CC you're using.
+---
+ Makefile.PL | 7 +++++++
+ 1 file changed, 7 insertions(+)
+
+diff --git a/Makefile.PL b/Makefile.PL
+index 0054164..ad249b9 100644
+--- a/Makefile.PL
++++ b/Makefile.PL
 @@ -24,6 +24,13 @@
      },
  );