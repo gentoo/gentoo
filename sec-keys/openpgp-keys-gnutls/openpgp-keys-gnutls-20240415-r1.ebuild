gentoo/gentoo

commit 1061fd37f9491f2601a8b5b6c92ffc3a2f42d7c9

sec-keys/openpgp-keys-gnutls: add 20231129
Pull updates of Daiki Ueno's key from the keyservers. The copy uploaded
to the website is expired, so we merge the website copy and the single
updated key together.

Unblocks packaging of the recent gnutls update.

Bug: https://gitlab.com/gnutls/web-pages/-/issues/6
Bug: https://bugs.gentoo.org/918663
Signed-off-by: Eli Schwartz <eschwartz93@gmail.com>
Signed-off-by: Sam James <sam@gentoo.org>

sec-keys/openpgp-keys-gnutls/Manifest
@@ -1,2 +1,4 @@
 DIST openpgp-keys-gnutls-20220320-release-keyring.gpg 20850 BLAKE2B 06865e4ac4e69237f7c52f551ce902281f6699bd144cb16c97e81aa1323c99187637c3b62730e700bc1c7f9de4608da55d3a80c8ffc240ee533e42b4707e4fbe SHA512 ebf592298142c7f05d8298a9e42aa4e5579eda54bf3c16f5a97cd22c22f3904a635a20cb1af6587d03b1a58545b925bddb769b098c664a336abce8ec9f10b9d5
 DIST openpgp-keys-gnutls-20221017-release-keyring.gpg 26256 BLAKE2B c42024bade07f5e0d189c653a052aaa89803c71c8c3a5653417fb1ae3961c392257b5ef719b9236d291d449490d5dba90ae238b76391933f864458c0805e02e0 SHA512 5c14d83f4f37bd319c652db0d76fc5bb04752fb461bbe853e25b20ffe41d6d14faae6c0bdd0193ac6242975bf1205ce606a9d0082261cc4581fd680abfcdbd4d
+DIST openpgp-keys-gnutls-20231129-daiki-updated.gpg 7345 BLAKE2B 0a6a0079483320bbd191f3c04a21ac52b159f755518ae7d6e27869b7cda06548bd7772ce5b9def07dece791537cbfb750bd227023f7665cbd860b5ce200fc4d0 SHA512 55af264087eceb244c59bec2aa4c62df3fc0b25ceb13728e7c3fc9042ae2fb990282367e6b46eda7ceb7f267add6dcd8948a887d825c7759cc61c642d6e0a3a7
+DIST openpgp-keys-gnutls-20231129-release-keyring.gpg 26256 BLAKE2B c42024bade07f5e0d189c653a052aaa89803c71c8c3a5653417fb1ae3961c392257b5ef719b9236d291d449490d5dba90ae238b76391933f864458c0805e02e0 SHA512 5c14d83f4f37bd319c652db0d76fc5bb04752fb461bbe853e25b20ffe41d6d14faae6c0bdd0193ac6242975bf1205ce606a9d0082261cc4581fd680abfcdbd4d

sec-keys/openpgp-keys-gnutls/openpgp-keys-gnutls-20231129.ebuild
@@ -1,0 +1,30 @@
+# Copyright 1999-2023 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=7
+
+DESCRIPTION="OpenPGP keys used by GnuTLS"
+HOMEPAGE="https://www.gnutls.org/download.html"
+SRC_URI="
+	https://gnutls.org/gnutls-release-keyring.gpg -> ${P}-release-keyring.gpg
+	https://keys.openpgp.org/vks/v1/by-fingerprint/462225C3B46F34879FC8496CD605848ED7E69871 -> ${P}-daiki-updated.gpg
+"
+S="${WORKDIR}"
+
+LICENSE="public-domain"
+SLOT="0"
+KEYWORDS="~alpha amd64 arm arm64 hppa ~ia64 ~loong ~m68k ~mips ppc ppc64 ~riscv ~s390 sparc x86"
+
+src_compile() {
+	local files=( ${A} )
+	local -x GNUPGHOME=${T}/.gnupg
+
+	mkdir "${GNUPGHOME}" || die
+	gpg --import "${files[@]/#/${DISTDIR}/}" || die "Key import failed"
+	gpg --export --armor --export-options export-minimal > gnutls.asc || die
+}
+
+src_install() {
+	insinto /usr/share/openpgp-keys
+	doins gnutls.asc
+}

commit cfb46f4ff4ff2f5327b105dbe0bd96505befef67

sec-keys/openpgp-keys-gnutls: update to use sec-keys.eclass
Bump EAPI 7 -> 8 as required by eclass. ;)

This is a nice demonstration of using the eclass. It showcases some
advanced usage, in particular, how to augment a single key within a
keyring published on upstream's website with an extended expiration date
from the openpgp.org keyserver.

The ebuild was previously updated in commit
1061fd37f9491f2601a8b5b6c92ffc3a2f42d7c9 with some explanatory text
about why we needed to do that. It wasn't a very good commit (sorry,
past self!) because it was kind of inscrutable whether the key "should"
have been included. That commit would have instead modified "manual" to
"openpgp" for daiki, had the eclass existed exactly a year ago.

Bug: https://gitlab.com/gnutls/web-pages/-/issues/6
Fixes: 1061fd37f9491f2601a8b5b6c92ffc3a2f42d7c9
Signed-off-by: Eli Schwartz <eschwartz@gentoo.org>

sec-keys/openpgp-keys-gnutls/Manifest
@@ -1,3 +1,4 @@
+DIST openpgp-keys-daiki-openpgp-20240415.asc 7345 BLAKE2B 0a6a0079483320bbd191f3c04a21ac52b159f755518ae7d6e27869b7cda06548bd7772ce5b9def07dece791537cbfb750bd227023f7665cbd860b5ce200fc4d0 SHA512 55af264087eceb244c59bec2aa4c62df3fc0b25ceb13728e7c3fc9042ae2fb990282367e6b46eda7ceb7f267add6dcd8948a887d825c7759cc61c642d6e0a3a7
 DIST openpgp-keys-gnutls-20220320-release-keyring.gpg 20850 BLAKE2B 06865e4ac4e69237f7c52f551ce902281f6699bd144cb16c97e81aa1323c99187637c3b62730e700bc1c7f9de4608da55d3a80c8ffc240ee533e42b4707e4fbe SHA512 ebf592298142c7f05d8298a9e42aa4e5579eda54bf3c16f5a97cd22c22f3904a635a20cb1af6587d03b1a58545b925bddb769b098c664a336abce8ec9f10b9d5
 DIST openpgp-keys-gnutls-20221017-release-keyring.gpg 26256 BLAKE2B c42024bade07f5e0d189c653a052aaa89803c71c8c3a5653417fb1ae3961c392257b5ef719b9236d291d449490d5dba90ae238b76391933f864458c0805e02e0 SHA512 5c14d83f4f37bd319c652db0d76fc5bb04752fb461bbe853e25b20ffe41d6d14faae6c0bdd0193ac6242975bf1205ce606a9d0082261cc4581fd680abfcdbd4d
 DIST openpgp-keys-gnutls-20231129-daiki-updated.gpg 7345 BLAKE2B 0a6a0079483320bbd191f3c04a21ac52b159f755518ae7d6e27869b7cda06548bd7772ce5b9def07dece791537cbfb750bd227023f7665cbd860b5ce200fc4d0 SHA512 55af264087eceb244c59bec2aa4c62df3fc0b25ceb13728e7c3fc9042ae2fb990282367e6b46eda7ceb7f267add6dcd8948a887d825c7759cc61c642d6e0a3a7

sec-keys/openpgp-keys-gnutls/openpgp-keys-gnutls-20240415-r1.ebuild
@@ -1,0 +1,22 @@
+# Copyright 1999-2025 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+SEC_KEYS_VALIDPGPKEYS=(
+	'E987AB7F7E89667776D05B3BB0E9DD20B29F1432:alexander.sosedken:manual'
+	# GnuTLS website has expired key for daiki
+	'462225C3B46F34879FC8496CD605848ED7E69871:daiki:openpgp'
+	'1CB27DBC98614B2D5841646D08302DB6A2670428:tim.ruehsen:manual'
+	'5D46CB0F763405A7053556F47A75A648B3F9220C:zfridrich:manual'
+)
+
+inherit sec-keys
+
+DESCRIPTION="OpenPGP keys used by GnuTLS"
+HOMEPAGE="https://www.gnutls.org/download.html"
+SRC_URI+="
+	https://gnutls.org/gnutls-release-keyring.gpg -> ${P}-release-keyring.gpg
+"
+
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~loong ~m68k ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86"