gentoo/gentoo

commit 962499a101dac3519c965ab4ea49f76fb46c3cb4

dev-java/openjdk: drop USE=lto, use tc-is-lto instead
As we've done in other packages, migrate away from USE=lto towards
tc-is-lto so users enable via -flto in *FLAGS.

This doesn't fix/workaround bug #960257 but it does make it very easy
for us to add such a workaround later.

Bug: https://bugs.gentoo.org/960257
Signed-off-by: Sam James <sam@gentoo.org>

dev-java/openjdk/openjdk-8.462_p08.ebuild
@@ -172,6 +172,8 @@ src_prepare() {
 }
 
 src_configure() {
+	local myconf=()
+
 	if ! use system-bootstrap; then
 		local xpakvar="${ARCH^^}_XPAK"
 		export JDK_HOME="${WORKDIR}/openjdk-bootstrap-${!xpakvar}"
@@ -189,9 +191,9 @@ src_configure() {
 	# Strip some flags users may set, but should not. #818502
 	filter-flags -fexceptions
 
-	# Strip lto related flags, no support in this version.
-	# https://bugs.gentoo.org/833097
-	# https://bugs.gentoo.org/833098
+	# Strip lto related flags, we rely on --with-jvm-features=link-time-opt
+	# See bug #833097 and bug #833098.
+	tc-is-lto && myconf+=( --with-jvm-features=link-time-opt )
 	filter-lto
 	filter-flags -fdevirtualize-at-ltrans
 
@@ -200,7 +202,7 @@ src_configure() {
 
 	tc-export_build_env CC CXX PKG_CONFIG STRIP
 
-	local myconf=(
+	myconf+=(
 			--disable-ccache
 			--disable-freetype-bundling
 			--disable-precompiled-headers

dev-java/openjdk/openjdk-11.0.27_p6-r2.ebuild
@@ -55,7 +55,7 @@ LICENSE="GPL-2-with-classpath-exception"
 SLOT="${MY_PV%%[.+]*}"
 KEYWORDS="amd64 ~arm arm64 ppc64 ~riscv x86"
 
-IUSE="alsa big-endian cups debug doc examples headless-awt javafx +jbootstrap lto selinux source system-bootstrap systemtap"
+IUSE="alsa big-endian cups debug doc examples headless-awt javafx +jbootstrap selinux source system-bootstrap systemtap"
 
 REQUIRED_USE="
 	javafx? ( alsa !headless-awt )
@@ -161,6 +161,8 @@ src_prepare() {
 }
 
 src_configure() {
+	local myconf=()
+
 	if ! use system-bootstrap; then
 		local xpakvar="${ARCH^^}_XPAK"
 		export JDK_HOME="${WORKDIR}/openjdk-bootstrap-${!xpakvar}"
@@ -175,9 +177,9 @@ src_configure() {
 	# Strip some flags users may set, but should not. #818502
 	filter-flags -fexceptions
 
-	# Strip lto related flags, we rely on USE=lto and --with-jvm-features=link-time-opt
-	# https://bugs.gentoo.org/833097
-	# https://bugs.gentoo.org/833098
+	# Strip lto related flags, we rely on --with-jvm-features=link-time-opt
+	# See bug #833097 and bug #833098.
+	tc-is-lto && myconf+=( --with-jvm-features=link-time-opt )
 	filter-lto
 	filter-flags -fdevirtualize-at-ltrans
 
@@ -188,7 +190,7 @@ src_configure() {
 	# explicitly disabled, the flag will get auto-enabled if pandoc and
 	# graphviz are detected. pandoc has loads of dependencies anyway.
 
-	local myconf=(
+	myconf+=(
 		--disable-ccache
 		--disable-precompiled-headers
 		--enable-full-docs=no
@@ -217,8 +219,6 @@ src_configure() {
 		$(tc-is-clang && echo "--with-toolchain-type=clang")
 	)
 	! use riscv && myconf+=( --with-jvm-features=shenandoahgc )
-
-	use lto && myconf+=( --with-jvm-features=link-time-opt )
 
 	if use javafx; then
 		# this is not useful for users, just for upstream developers

dev-java/openjdk/openjdk-11.0.28_p6.ebuild
@@ -55,7 +55,7 @@ LICENSE="GPL-2-with-classpath-exception"
 SLOT="${MY_PV%%[.+]*}"
 KEYWORDS="~amd64 ~arm ~arm64 ~ppc64 ~riscv ~x86"
 
-IUSE="alsa big-endian cups debug doc examples headless-awt javafx +jbootstrap lto selinux source system-bootstrap systemtap"
+IUSE="alsa big-endian cups debug doc examples headless-awt javafx +jbootstrap selinux source system-bootstrap systemtap"
 
 REQUIRED_USE="
 	javafx? ( alsa !headless-awt )
@@ -161,6 +161,8 @@ src_prepare() {
 }
 
 src_configure() {
+	local myconf=()
+
 	if ! use system-bootstrap; then
 		local xpakvar="${ARCH^^}_XPAK"
 		export JDK_HOME="${WORKDIR}/openjdk-bootstrap-${!xpakvar}"
@@ -175,9 +177,9 @@ src_configure() {
 	# Strip some flags users may set, but should not. #818502
 	filter-flags -fexceptions
 
-	# Strip lto related flags, we rely on USE=lto and --with-jvm-features=link-time-opt
-	# https://bugs.gentoo.org/833097
-	# https://bugs.gentoo.org/833098
+	# Strip lto related flags, we rely on --with-jvm-features=link-time-opt
+	# See bug #833097 and bug #833098.
+	tc-is-lto && myconf+=( --with-jvm-features=link-time-opt )
 	filter-lto
 	filter-flags -fdevirtualize-at-ltrans
 
@@ -188,7 +190,7 @@ src_configure() {
 	# explicitly disabled, the flag will get auto-enabled if pandoc and
 	# graphviz are detected. pandoc has loads of dependencies anyway.
 
-	local myconf=(
+	myconf+=(
 		--disable-ccache
 		--disable-precompiled-headers
 		--enable-full-docs=no
@@ -217,8 +219,6 @@ src_configure() {
 		$(tc-is-clang && echo "--with-toolchain-type=clang")
 	)
 	! use riscv && myconf+=( --with-jvm-features=shenandoahgc )
-
-	use lto && myconf+=( --with-jvm-features=link-time-opt )
 
 	if use javafx; then
 		# this is not useful for users, just for upstream developers

dev-java/openjdk/openjdk-17.0.15_p6-r2.ebuild
@@ -55,7 +55,7 @@ LICENSE="GPL-2-with-classpath-exception"
 SLOT="${MY_PV%%[.+]*}"
 KEYWORDS="amd64 ~arm arm64 ppc64 ~riscv x86"
 
-IUSE="alsa big-endian cups debug doc examples headless-awt javafx +jbootstrap lto selinux source system-bootstrap systemtap"
+IUSE="alsa big-endian cups debug doc examples headless-awt javafx +jbootstrap selinux source system-bootstrap systemtap"
 
 REQUIRED_USE="
 	javafx? ( alsa !headless-awt )
@@ -167,6 +167,8 @@ src_prepare() {
 }
 
 src_configure() {
+	local myconf=()
+
 	if has_version dev-java/openjdk:${SLOT}; then
 		export JDK_HOME=${BROOT}/usr/$(get_libdir)/openjdk-${SLOT}
 	elif use !system-bootstrap ; then
@@ -189,9 +191,9 @@ src_configure() {
 	# Strip some flags users may set, but should not. #818502
 	filter-flags -fexceptions
 
-	# Strip lto related flags, we rely on USE=lto and --with-jvm-features=link-time-opt
-	# https://bugs.gentoo.org/833097
-	# https://bugs.gentoo.org/833098
+	# Strip lto related flags, we rely on --with-jvm-features=link-time-opt
+	# See bug #833097 and bug #833098.
+	tc-is-lto && myconf+=( --with-jvm-features=link-time-opt )
 	filter-lto
 	filter-flags -fdevirtualize-at-ltrans
 
@@ -199,7 +201,7 @@ src_configure() {
 	# explicitly disabled, the flag will get auto-enabled if pandoc and
 	# graphviz are detected. pandoc has loads of dependencies anyway.
 
-	local myconf=(
+	myconf+=(
 		--disable-ccache
 		--disable-precompiled-headers
 		--disable-warnings-as-errors
@@ -228,8 +230,6 @@ src_configure() {
 		--enable-headless-only=$(usex headless-awt yes no)
 		$(tc-is-clang && echo "--with-toolchain-type=clang")
 	)
-
-	use lto && myconf+=( --with-jvm-features=link-time-opt )
 
 	if use javafx; then
 		local zip="${EPREFIX}/usr/$(get_libdir)/openjfx-${SLOT}/javafx-exports.zip"

dev-java/openjdk/openjdk-17.0.16_p8.ebuild
@@ -55,7 +55,7 @@ LICENSE="GPL-2-with-classpath-exception"
 SLOT="${MY_PV%%[.+]*}"
 KEYWORDS="~amd64 ~arm ~arm64 ~ppc64 ~riscv ~x86"
 
-IUSE="alsa big-endian cups debug doc examples headless-awt javafx +jbootstrap lto selinux source system-bootstrap systemtap"
+IUSE="alsa big-endian cups debug doc examples headless-awt javafx +jbootstrap selinux source system-bootstrap systemtap"
 
 REQUIRED_USE="
 	javafx? ( alsa !headless-awt )
@@ -167,6 +167,8 @@ src_prepare() {
 }
 
 src_configure() {
+	local myconf=()
+
 	if has_version dev-java/openjdk:${SLOT}; then
 		export JDK_HOME=${BROOT}/usr/$(get_libdir)/openjdk-${SLOT}
 	elif use !system-bootstrap ; then
@@ -189,9 +191,9 @@ src_configure() {
 	# Strip some flags users may set, but should not. #818502
 	filter-flags -fexceptions
 
-	# Strip lto related flags, we rely on USE=lto and --with-jvm-features=link-time-opt
-	# https://bugs.gentoo.org/833097
-	# https://bugs.gentoo.org/833098
+	# Strip lto related flags, we rely on --with-jvm-features=link-time-opt
+	# See bug #833097 and bug #833098.
+	tc-is-lto && myconf+=( --with-jvm-features=link-time-opt )
 	filter-lto
 	filter-flags -fdevirtualize-at-ltrans
 
@@ -199,7 +201,7 @@ src_configure() {
 	# explicitly disabled, the flag will get auto-enabled if pandoc and
 	# graphviz are detected. pandoc has loads of dependencies anyway.
 
-	local myconf=(
+	myconf+=(
 		--disable-ccache
 		--disable-precompiled-headers
 		--disable-warnings-as-errors
@@ -228,8 +230,6 @@ src_configure() {
 		--enable-headless-only=$(usex headless-awt yes no)
 		$(tc-is-clang && echo "--with-toolchain-type=clang")
 	)
-
-	use lto && myconf+=( --with-jvm-features=link-time-opt )
 
 	if use javafx; then
 		local zip="${EPREFIX}/usr/$(get_libdir)/openjfx-${SLOT}/javafx-exports.zip"

dev-java/openjdk/openjdk-21.0.7_p6-r2.ebuild
@@ -51,7 +51,6 @@ LICENSE="GPL-2-with-classpath-exception"
 SLOT="${MY_PV%%[.+]*}"
 KEYWORDS="amd64 arm64 ppc64 ~riscv ~x86"
 
-# lto temporarily disabled due to https://bugs.gentoo.org/916735
 IUSE="alsa big-endian cups debug doc examples headless-awt javafx +jbootstrap selinux source +system-bootstrap systemtap"
 
 REQUIRED_USE="
@@ -165,6 +164,8 @@ src_prepare() {
 }
 
 src_configure() {
+	local myconf=()
+
 	if has_version dev-java/openjdk:${SLOT}; then
 		export JDK_HOME=${BROOT}/usr/$(get_libdir)/openjdk-${SLOT}
 	elif use !system-bootstrap ; then
@@ -187,9 +188,12 @@ src_configure() {
 	# Strip some flags users may set, but should not. #818502
 	filter-flags -fexceptions
 
-	# Strip lto related flags, we rely on USE=lto and --with-jvm-features=link-time-opt
-	# https://bugs.gentoo.org/833097
-	# https://bugs.gentoo.org/833098
+	# Strip lto related flags, we rely on --with-jvm-features=link-time-opt
+	# See bug #833097 and bug #833098.
+	#
+	# .. but because of -Werror=odr (bug #916735), we disable it
+	# entirely for now.
+	#tc-is-lto && myconf+=( --with-jvm-features=link-time-opt )
 	filter-lto
 	filter-flags -fdevirtualize-at-ltrans
 
@@ -197,7 +201,7 @@ src_configure() {
 	# explicitly disabled, the flag will get auto-enabled if pandoc and
 	# graphviz are detected. pandoc has loads of dependencies anyway.
 
-	local myconf=(
+	myconf+=(
 		--disable-ccache
 		--disable-precompiled-headers
 		--disable-warnings-as-errors
@@ -229,12 +233,6 @@ src_configure() {
 	)
 
 	use riscv && myconf+=( --with-boot-jdk-jvmargs="-Djdk.lang.Process.launchMechanism=vfork" )
-
-	# Werror=odr
-	# https://bugs.gentoo.org/916735
-	#
-	# Disable it for now.
-	#use lto && myconf+=( --with-jvm-features=link-time-opt )
 
 	if use javafx; then
 		local zip="${EPREFIX}/usr/$(get_libdir)/openjfx-${SLOT}/javafx-exports.zip"

dev-java/openjdk/openjdk-21.0.8_p9.ebuild
@@ -51,7 +51,6 @@ LICENSE="GPL-2-with-classpath-exception"
 SLOT="${MY_PV%%[.+]*}"
 KEYWORDS="~amd64 ~arm64 ~ppc64 ~riscv ~x86"
 
-# lto temporarily disabled due to https://bugs.gentoo.org/916735
 IUSE="alsa big-endian cups debug doc examples headless-awt javafx +jbootstrap selinux source +system-bootstrap systemtap"
 
 REQUIRED_USE="
@@ -165,6 +164,8 @@ src_prepare() {
 }
 
 src_configure() {
+	local myconf=()
+
 	if has_version dev-java/openjdk:${SLOT}; then
 		export JDK_HOME=${BROOT}/usr/$(get_libdir)/openjdk-${SLOT}
 	elif use !system-bootstrap ; then
@@ -187,9 +188,12 @@ src_configure() {
 	# Strip some flags users may set, but should not. #818502
 	filter-flags -fexceptions
 
-	# Strip lto related flags, we rely on USE=lto and --with-jvm-features=link-time-opt
-	# https://bugs.gentoo.org/833097
-	# https://bugs.gentoo.org/833098
+	# Strip lto related flags, we rely on --with-jvm-features=link-time-opt
+	# See bug #833097 and bug #833098.
+	#
+	# .. but because of -Werror=odr (bug #916735), we disable it
+	# entirely for now.
+	#tc-is-lto && myconf+=( --with-jvm-features=link-time-opt )
 	filter-lto
 	filter-flags -fdevirtualize-at-ltrans
 
@@ -197,7 +201,7 @@ src_configure() {
 	# explicitly disabled, the flag will get auto-enabled if pandoc and
 	# graphviz are detected. pandoc has loads of dependencies anyway.
 
-	local myconf=(
+	myconf+=(
 		--disable-ccache
 		--disable-precompiled-headers
 		--disable-warnings-as-errors
@@ -229,12 +233,6 @@ src_configure() {
 	)
 
 	use riscv && myconf+=( --with-boot-jdk-jvmargs="-Djdk.lang.Process.launchMechanism=vfork" )
-
-	# Werror=odr
-	# https://bugs.gentoo.org/916735
-	#
-	# Disable it for now.
-	#use lto && myconf+=( --with-jvm-features=link-time-opt )
 
 	if use javafx; then
 		local zip="${EPREFIX}/usr/$(get_libdir)/openjfx-${SLOT}/javafx-exports.zip"

dev-java/openjdk/openjdk-25_p31.ebuild
@@ -52,7 +52,6 @@ LICENSE="GPL-2-with-classpath-exception"
 SLOT="${MY_PV%%[.+]*}"
 #	KEYWORDS="" # LTS but not yet released
 
-# lto temporarily disabled due to https://bugs.gentoo.org/916735
 IUSE="alsa big-endian cups debug doc examples headless-awt javafx +jbootstrap selinux source +system-bootstrap systemtap"
 
 REQUIRED_USE="
@@ -166,6 +165,8 @@ src_prepare() {
 }
 
 src_configure() {
+	local myconf=()
+
 	if has_version dev-java/openjdk:${SLOT}; then
 		export JDK_HOME=${BROOT}/usr/$(get_libdir)/openjdk-${SLOT}
 	elif use !system-bootstrap ; then
@@ -185,9 +186,12 @@ src_configure() {
 	# Strip some flags users may set, but should not. #818502
 	filter-flags -fexceptions
 
-	# Strip lto related flags, we rely on USE=lto and --with-jvm-features=link-time-opt
-	# https://bugs.gentoo.org/833097
-	# https://bugs.gentoo.org/833098
+	# Strip lto related flags, we rely on --with-jvm-features=link-time-opt
+	# See bug #833097 and bug #833098.
+	#
+	# .. but because of -Werror=odr (bug #916735), we disable it
+	# entirely for now.
+	#tc-is-lto && myconf+=( --with-jvm-features=link-time-opt )
 	filter-lto
 	filter-flags -fdevirtualize-at-ltrans
 
@@ -195,7 +199,7 @@ src_configure() {
 	# explicitly disabled, the flag will get auto-enabled if pandoc and
 	# graphviz are detected. pandoc has loads of dependencies anyway.
 
-	local myconf=(
+	myconf+=(
 		--disable-ccache
 		--disable-precompiled-headers
 		--disable-warnings-as-errors
@@ -227,12 +231,6 @@ src_configure() {
 	)
 
 	use riscv && myconf+=( --with-boot-jdk-jvmargs="-Djdk.lang.Process.launchMechanism=vfork" )
-
-	# Werror=odr
-	# https://bugs.gentoo.org/916735
-	#
-	# Disable it for now.
-	#use lto && myconf+=( --with-jvm-features=link-time-opt )
 
 	if use javafx; then
 		local zip="${EPREFIX}/usr/$(get_libdir)/openjfx-${SLOT}/javafx-exports.zip"

commit 635396b0d6c86471a67a6e6c21a4da20ae3fd97c

dev-java/openjdk: fix openjdk 8 configure args
The option doesn't exist for 8, I botched the mass conversion.

Fixes: 962499a101dac3519c965ab4ea49f76fb46c3cb4
Closes: https://bugs.gentoo.org/961026
Signed-off-by: Sam James <sam@gentoo.org>

dev-java/openjdk/openjdk-8.462_p08.ebuild
@@ -192,8 +192,7 @@ src_configure() {
 	filter-flags -fexceptions
 
 	# Strip lto related flags, we rely on --with-jvm-features=link-time-opt
-	# See bug #833097 and bug #833098.
-	tc-is-lto && myconf+=( --with-jvm-features=link-time-opt )
+	# in newer JDKs. See bug #833097 and bug #833098.
 	filter-lto
 	filter-flags -fdevirtualize-at-ltrans
 