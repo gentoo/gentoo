3c6009e3d48bcc80cfc714e4365ae24856114c5f
Sam James <sam@gentoo.org>
2025-08-04 20:17:59 +0100

    eclass/tests: dot-a: use strip -p to avoid spurious differences within archive
    Like in https://sourceware.org/bugzilla/show_bug.cgi?id=33246#c21, but it
    showed up in our tests too.
    
    Signed-off-by: Sam James <sam@gentoo.org>

--- eclass/tests/dot-a.sh
+++ eclass/tests/dot-a.sh
@@ -428,7 +428,7 @@ test_strip_lto() {
 		$(tc-getCC) a.c -o a.o -c -flto -ggdb3 || return 1
 		$(tc-getAR) q foo.a a.o 2>/dev/null || return 1
 		cp foo.a foo.a.bak || return 1
-		$(tc-getSTRIP) -d foo.a || return 1
+		$(tc-getSTRIP) -p -d foo.a || return 1
 
 		# They should NOT differ after stripping because it
 		# can't be safely stripped without special arguments.
@@ -447,7 +447,7 @@ test_strip_lto() {
 		$(tc-getCC) a.c -o a.o -c -flto -ffat-lto-objects -ggdb3 || return 1
 		$(tc-getAR) q foo.a a.o 2>/dev/null || return 1
 		cp foo.a foo.a.bak || return 1
-		$(tc-getSTRIP) -d foo.a || return 1
+		$(tc-getSTRIP) -p -d foo.a || return 1
 
 		# They should NOT differ after stripping because it
 		# can't be safely stripped without special arguments.
@@ -525,7 +525,7 @@ test_strip_nolto() {
 		$(tc-getCC) a.c -o a.o -c -ggdb3 || return 1
 		$(tc-getAR) q foo.a a.o 2>/dev/null || return 1
 		cp foo.a foo.a.bak || return 1
-		$(tc-getSTRIP) -d foo.a || return 1
+		$(tc-getSTRIP) -p -d foo.a || return 1
 
 		# They should differ after stripping.
 		cmp -s foo.a foo.a.bak && return 1
@@ -549,7 +549,7 @@ test_strip_nolto() {
 		$(tc-getCC) a.c -o a.o -c -ggdb3 || return 1
 		$(tc-getAR) q foo.a a.o 2>/dev/null || return 1
 		cp foo.a foo.a.bak || return 1
-		$(tc-getSTRIP) -d foo.a || return 1
+		$(tc-getSTRIP) -p -d foo.a || return 1
 
 		# They should differ after stripping.
 		cmp -s foo.a foo.a.bak && return 1
