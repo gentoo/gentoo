gentoo/gentoo

commit 3e834a4250810d62cd9e541aa1ac5d3110b38728

media-libs/libheif: fix auto magic deps
Add dependencies for openjpeg and kvazaar
Add USE-flag for jpeg.

Closes: https://bugs.gentoo.org/955025
Signed-off-by: Paul Zander <negril.nx+gentoo@gmail.com>
Part-of: https://github.com/gentoo/gentoo/pull/43070
Signed-off-by: Sam James <sam@gentoo.org>

media-libs/libheif/libheif-9999.ebuild
@@ -18,7 +18,7 @@ fi
 
 LICENSE="GPL-3 MIT"
 SLOT="0/$(ver_cut 1-2)"
-IUSE="+aom dav1d doc examples ffmpeg gdk-pixbuf openh264 rav1e svt-av1 test +threads +webp x265"
+IUSE="+aom dav1d +de265 doc examples ffmpeg gdk-pixbuf +jpeg +jpeg2k +kvazaar openh264 rav1e svt-av1 test +threads +webp x265"
 # IUSE+=" vvdec vvenc"
 RESTRICT="!test? ( test )"
 
@@ -26,15 +26,16 @@ BDEPEND="
 	doc? ( app-text/doxygen )
 "
 DEPEND="
-	media-libs/libde265[${MULTILIB_USEDEP}]
-	media-libs/libjpeg-turbo:=[${MULTILIB_USEDEP}]
 	media-libs/libpng:=[${MULTILIB_USEDEP}]
 	media-libs/tiff:=[${MULTILIB_USEDEP}]
 	sys-libs/zlib:=[${MULTILIB_USEDEP}]
 	aom? ( >=media-libs/libaom-2.0.0:=[${MULTILIB_USEDEP}] )
 	dav1d? ( media-libs/dav1d:=[${MULTILIB_USEDEP}] )
+	de265? ( media-libs/libde265[${MULTILIB_USEDEP}] )
 	ffmpeg? ( media-video/ffmpeg:=[${MULTILIB_USEDEP}] )
 	gdk-pixbuf? ( x11-libs/gdk-pixbuf[${MULTILIB_USEDEP}] )
+	jpeg? ( media-libs/libjpeg-turbo:=[${MULTILIB_USEDEP}] )
+	jpeg2k? ( media-libs/openjpeg:=[${MULTILIB_USEDEP}] )
 	openh264? ( media-libs/openh264:=[${MULTILIB_USEDEP}] )
 	rav1e? ( media-video/rav1e:= )
 	svt-av1? ( media-libs/svt-av1:=[${MULTILIB_USEDEP}] )
@@ -54,7 +55,7 @@ multilib_src_configure() {
 		$(cmake_use_find_package doc Doxygen)
 		-DBUILD_TESTING=$(usex test)
 		-DENABLE_PLUGIN_LOADING=true
-		-DWITH_LIBDE265=true
+		-DWITH_LIBDE265=$(usex de265)
 		-DWITH_AOM_DECODER=$(usex aom)
 		-DWITH_AOM_ENCODER=$(usex aom)
 		-DWITH_DAV1D=$(usex dav1d)
@@ -69,11 +70,11 @@ multilib_src_configure() {
 		# -DWITH_VVDEC=$(usex vvdec) # vvdec not yet packaged, in ::guru
 		# -DWITH_VVENC=$(usex vvenc) # vvenc not yet packaged, in ::guru
 		-DWITH_X265=$(usex x265)
-		-DWITH_KVAZAAR=true
-		-DWITH_JPEG_DECODER=true
-		-DWITH_JPEG_ENCODER=true
-		-DWITH_OpenJPEG_DECODER=true
-		-DWITH_OpenJPEG_ENCODER=true
+		-DWITH_KVAZAAR=$(usex kvazaar)
+		-DWITH_JPEG_DECODER=$(usex jpeg)
+		-DWITH_JPEG_ENCODER=$(usex jpeg)
+		-DWITH_OpenJPEG_DECODER=$(usex jpeg2k)
+		-DWITH_OpenJPEG_ENCODER=$(usex jpeg2k)
 	)
 
 	cmake_src_configure

media-libs/libheif/libheif-1.19.8-r1.ebuild
@@ -18,7 +18,7 @@ fi
 
 LICENSE="GPL-3 MIT"
 SLOT="0/$(ver_cut 1-2)"
-IUSE="+aom dav1d doc examples ffmpeg gdk-pixbuf openh264 rav1e svt-av1 test +threads +webp x265"
+IUSE="+aom dav1d +de265 doc examples ffmpeg gdk-pixbuf +jpeg +jpeg2k +kvazaar openh264 rav1e svt-av1 test +threads +webp x265"
 # IUSE+=" vvdec vvenc"
 RESTRICT="!test? ( test )"
 
@@ -26,15 +26,16 @@ BDEPEND="
 	doc? ( app-text/doxygen )
 "
 DEPEND="
-	media-libs/libde265[${MULTILIB_USEDEP}]
-	media-libs/libjpeg-turbo:=[${MULTILIB_USEDEP}]
 	media-libs/libpng:=[${MULTILIB_USEDEP}]
 	media-libs/tiff:=[${MULTILIB_USEDEP}]
 	sys-libs/zlib:=[${MULTILIB_USEDEP}]
 	aom? ( >=media-libs/libaom-2.0.0:=[${MULTILIB_USEDEP}] )
 	dav1d? ( media-libs/dav1d:=[${MULTILIB_USEDEP}] )
+	de265? ( media-libs/libde265[${MULTILIB_USEDEP}] )
 	ffmpeg? ( media-video/ffmpeg:=[${MULTILIB_USEDEP}] )
 	gdk-pixbuf? ( x11-libs/gdk-pixbuf[${MULTILIB_USEDEP}] )
+	jpeg? ( media-libs/libjpeg-turbo:=[${MULTILIB_USEDEP}] )
+	jpeg2k? ( media-libs/openjpeg:=[${MULTILIB_USEDEP}] )
 	openh264? ( media-libs/openh264:=[${MULTILIB_USEDEP}] )
 	rav1e? ( media-video/rav1e:= )
 	svt-av1? ( media-libs/svt-av1:=[${MULTILIB_USEDEP}] )
@@ -54,7 +55,7 @@ multilib_src_configure() {
 		$(cmake_use_find_package doc Doxygen)
 		-DBUILD_TESTING=$(usex test)
 		-DENABLE_PLUGIN_LOADING=true
-		-DWITH_LIBDE265=true
+		-DWITH_LIBDE265=$(usex de265)
 		-DWITH_AOM_DECODER=$(usex aom)
 		-DWITH_AOM_ENCODER=$(usex aom)
 		-DWITH_DAV1D=$(usex dav1d)
@@ -69,11 +70,11 @@ multilib_src_configure() {
 		# -DWITH_VVDEC=$(usex vvdec) # vvdec not yet packaged, in ::guru
 		# -DWITH_VVENC=$(usex vvenc) # vvenc not yet packaged, in ::guru
 		-DWITH_X265=$(usex x265)
-		-DWITH_KVAZAAR=true
-		-DWITH_JPEG_DECODER=true
-		-DWITH_JPEG_ENCODER=true
-		-DWITH_OpenJPEG_DECODER=true
-		-DWITH_OpenJPEG_ENCODER=true
+		-DWITH_KVAZAAR=$(usex kvazaar)
+		-DWITH_JPEG_DECODER=$(usex jpeg)
+		-DWITH_JPEG_ENCODER=$(usex jpeg)
+		-DWITH_OpenJPEG_DECODER=$(usex jpeg2k)
+		-DWITH_OpenJPEG_ENCODER=$(usex jpeg2k)
 	)
 
 	cmake_src_configure

media-libs/libheif/metadata.xml
@@ -5,8 +5,10 @@
 	<use>
 	<flag name="aom">Use <pkg>media-libs/libaom</pkg> for AVIF encoding/decoding</flag>
 	<flag name="dav1d">Enable support for the dav1d codec decoding using <pkg>media-libs/dav1d</pkg></flag>
+	<flag name="de265">Enable H.265 support using <pkg>media-libs/libde265</pkg></flag>
 	<flag name="gdk-pixbuf">Enable <pkg>x11-libs/gdk-pixbuf</pkg> plugin</flag>
 	<flag name="go">Enable building <pkg>dev-lang/go</pkg> code/examples</flag>
+	<flag name="kvazaar">Enable H.265/HEVC encoding support via <pkg>media-libs/kvazaar</pkg></flag>
 	<flag name="openh264">Enable H.264 support using <pkg>media-libs/openh264</pkg></flag>
 	<flag name="rav1e">Use <pkg>media-video/rav1e</pkg> for AVIF encoding</flag>
 	<flag name="svt-av1">Use <pkg>media-libs/svt-av1</pkg> for SVT-AV1 encoding</flag>

commit b566b2741f7691c886f7eb92c0849e69b2dbfda8

media-libs/libheif: add kvazaar dep
Bug: https://bugs.gentoo.org/955025
Fixes: 3e834a4250810d62cd9e541aa1ac5d3110b38728
Signed-off-by: Paul Zander <negril.nx+gentoo@gmail.com>

media-libs/libheif/libheif-1.19.8-r2.ebuild
@@ -36,6 +36,7 @@ DEPEND="
 	gdk-pixbuf? ( x11-libs/gdk-pixbuf[${MULTILIB_USEDEP}] )
 	jpeg? ( media-libs/libjpeg-turbo:=[${MULTILIB_USEDEP}] )
 	jpeg2k? ( media-libs/openjpeg:=[${MULTILIB_USEDEP}] )
+	kvazaar? ( media-libs/kvazaar:=[${MULTILIB_USEDEP}] )
 	openh264? ( media-libs/openh264:=[${MULTILIB_USEDEP}] )
 	rav1e? ( media-video/rav1e:= )
 	svt-av1? ( media-libs/svt-av1:=[${MULTILIB_USEDEP}] )

media-libs/libheif/libheif-1.20.1-r1.ebuild
@@ -37,6 +37,7 @@ DEPEND="
 	gdk-pixbuf? ( x11-libs/gdk-pixbuf[${MULTILIB_USEDEP}] )
 	jpeg? ( media-libs/libjpeg-turbo:=[${MULTILIB_USEDEP}] )
 	jpeg2k? ( media-libs/openjpeg:=[${MULTILIB_USEDEP}] )
+	kvazaar? ( media-libs/kvazaar:=[${MULTILIB_USEDEP}] )
 	openh264? ( media-libs/openh264:=[${MULTILIB_USEDEP}] )
 	rav1e? ( media-video/rav1e:= )
 	svt-av1? ( media-libs/svt-av1:=[${MULTILIB_USEDEP}] )

media-libs/libheif/libheif-9999.ebuild
@@ -37,6 +37,7 @@ DEPEND="
 	gdk-pixbuf? ( x11-libs/gdk-pixbuf[${MULTILIB_USEDEP}] )
 	jpeg? ( media-libs/libjpeg-turbo:=[${MULTILIB_USEDEP}] )
 	jpeg2k? ( media-libs/openjpeg:=[${MULTILIB_USEDEP}] )
+	kvazaar? ( media-libs/kvazaar:=[${MULTILIB_USEDEP}] )
 	openh264? ( media-libs/openh264:=[${MULTILIB_USEDEP}] )
 	rav1e? ( media-video/rav1e:= )
 	svt-av1? ( media-libs/svt-av1:=[${MULTILIB_USEDEP}] )