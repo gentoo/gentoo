gentoo/gentoo

commit 64fb6d4f52ca6c0d086289743b74f3a1183e538d

sci-libs/ginkgo: add 1.10.0
Signed-off-by: Matthias Maier <tamiko@gentoo.org>

sci-libs/ginkgo/files/ginkgo-1.10.0-disable_automagic_dependencies.patch
@@ -1,0 +1,24 @@
+diff --git a/examples/CMakeLists.txt b/examples/CMakeLists.txt
+index 8f4c848..cf2b69b 100644
+--- a/examples/CMakeLists.txt
++++ b/examples/CMakeLists.txt
+@@ -48,8 +48,7 @@ if(GINKGO_BUILD_EXTLIB_EXAMPLE)
+     list(APPEND EXAMPLES_LIST external-lib-interfacing)
+ endif()
+ 
+-find_package(OpenCV QUIET)
+-if(OpenCV_FOUND)
++if(FALSE)
+     list(APPEND EXAMPLES_LIST heat-equation schroedinger-splitting)
+ else()
+     message(STATUS "No OpenCV found, disabling examples with video output")
+@@ -68,8 +67,7 @@ if(GINKGO_BUILD_MPI)
+     )
+ endif()
+ 
+-find_package(Kokkos 4.1.00 QUIET)
+-if(Kokkos_FOUND)
++if(FALSE)
+     list(APPEND EXAMPLES_LIST kokkos-assembly)
+ else()
+     message(STATUS "No Kokkos found, disabling examples with Kokkos assembly.")

sci-libs/ginkgo/ginkgo-1.10.0.ebuild
@@ -1,0 +1,60 @@
+# Copyright 1999-2025 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit cmake flag-o-matic toolchain-funcs
+
+DESCRIPTION="Numerical linear algebra software package"
+HOMEPAGE="https://ginkgo-project.github.io/"
+
+if [[ ${PV} = *9999* ]]; then
+	EGIT_REPO_URI="https://github.com/ginkgo-project/ginkgo"
+	inherit git-r3
+else
+	SRC_URI="https://github.com/${PN}-project/${PN}/archive/v${PV}.tar.gz -> ${P}.tar.gz"
+	KEYWORDS="~amd64 ~x86"
+fi
+
+LICENSE="BSD-with-attribution"
+SLOT="0"
+IUSE="cuda hwloc +openmp"
+
+RDEPEND="
+	cuda? ( dev-util/nvidia-cuda-toolkit )
+	hwloc? ( sys-apps/hwloc:= )
+"
+DEPEND="${RDEPEND}"
+
+PATCHES=(
+	"${FILESDIR}"/${P}-disable_automagic_dependencies.patch
+)
+
+pkg_pretend() {
+	[[ ${MERGE_TYPE} != binary ]] && use openmp && tc-check-openmp
+}
+
+pkg_setup() {
+	[[ ${MERGE_TYPE} != binary ]] && use openmp && tc-check-openmp
+}
+
+src_configure() {
+	# -Werror=strict-aliasing
+	# https://bugs.gentoo.org/862705
+	# https://github.com/ginkgo-project/ginkgo/issues/1657
+	#
+	# Do not trust it with LTO either.
+	append-flags -fno-strict-aliasing
+	filter-lto
+
+	local mycmakeargs=(
+		-DGINKGO_DEVEL_TOOLS=OFF
+		-DGINKGO_BUILD_TESTS=OFF
+		-DGINKGO_BUILD_BENCHMARKS=OFF
+		-DGINKGO_BUILD_REFERENCE=ON
+		-DGINKGO_BUILD_CUDA=$(usex cuda)
+		-DGINKGO_BUILD_HWLOC=$(usex hwloc)
+		-DGINKGO_BUILD_OMP=$(usex openmp)
+	)
+	cmake_src_configure
+}

sci-libs/ginkgo/Manifest
@@ -1,3 +1,4 @@
+DIST ginkgo-1.10.0.tar.gz 13304835 BLAKE2B 863f6a2f465427b2c79ae8816805ddffbab00b8e94471af94f6bc817b50a4487f5b6237f5f1f7cdf7f18adc26d758b1955edbe7301b4268888c87ba8bb9ec44e SHA512 b48f47c593172cf3a28ca926cf8e8dd2d080a7e0c4d4344fe9c1b60e036431d5e5ed93e2f67f56fb979eb6f03dad3f273594ca86dd0f6ddadd3b2e0bc3abde53
 DIST ginkgo-1.4.0.tar.gz 2574052 BLAKE2B 99677d9d75831991cac40312095d32c44bef18cd05b120d80fdb9e64ea6dc15796c09b26f35dc8230db6b55c7be4dd76efd6d1cd8ca13c83e1f3087550db0dca SHA512 9bfcb2c415c7a1a70cf8e49f20adf62d02cab83bb23b6fcecfeaeeb322b2d4e1ad8d8fa6582735073753f9a05eac8688b9bd1ff1d4203957c1a80702d117e807
 DIST ginkgo-1.6.0.tar.gz 12788830 BLAKE2B c452e20eddb3673b5863106d8a8d42aab253cb5a17cf0445194903ebadcffe53ef0df6b5f26e2602e137faaa4a23dddd0b192f3453b4bd440907385fc09d004a SHA512 507a17bc9ad010c235c4ae49ac4bef3f4d5b65b4ea02bfa5cad5ea578fa65d28f564d1faf0a1f5618a6e72d744217f58bdff68c5f1fffc9cfb484800f7f84c50
 DIST ginkgo-1.8.0.tar.gz 13140660 BLAKE2B 4b1800566ec2852b3dd7e58b01ec58e2cab45b55134fdf3d478832435c40b45320dc066d281a07127d1fc42e57c6f3d99440209d2286adc1294e44a1f660ad36 SHA512 9d121a5eec9f5d17d1bd4b8924ebb32985a68e8087addc7385b619e365ed260a40ab73eb7a8a16f46a590d3162a78c9311ff41dd3dc74a9117a61e0445d96c52

commit ab2669fe160f3ecc0a8f64f0eb3a08e149699bd1

sci-libs/ginkgo: fix missing dep by not building examples
examples are not installed, but add ~50 new C++ build targets. In
1.10.0, they also add a DEPEND on nlohmann_json, which then fails by
falling back to git cloning it if a system copy is not found.

```
CMake Error at /usr/share/cmake/Modules/FetchContent.cmake:1918 (message):
  Build step for nlohmann_json failed: 1
Call Stack (most recent call first):
  /usr/share/cmake/Modules/FetchContent.cmake:1609 (__FetchContent_populateSubbuild)
  /usr/share/cmake/Modules/FetchContent.cmake:2145:EVAL:2 (__FetchContent_doPopulation)
  /usr/share/cmake/Modules/FetchContent.cmake:2145 (cmake_language)
  /usr/share/cmake/Modules/FetchContent.cmake:2384 (__FetchContent_Populate)
  third_party/nlohmann_json/CMakeLists.txt:10 (FetchContent_MakeAvailable)

-- Configuring incomplete, errors occurred!
 * ERROR: sci-libs/ginkgo-1.10.0::gentoo failed (configure phase):
```

But we wouldn't need this at all, if we refrained from building examples
we don't use, not even for tests which are already disabled.

Fixes: 64fb6d4f52ca6c0d086289743b74f3a1183e538d
Signed-off-by: Eli Schwartz <eschwartz@gentoo.org>

sci-libs/ginkgo/files/ginkgo-1.10.0-disable_automagic_dependencies.patch
@@ -1,24 +1,0 @@
-diff --git a/examples/CMakeLists.txt b/examples/CMakeLists.txt
-index 8f4c848..cf2b69b 100644
---- a/examples/CMakeLists.txt
-+++ b/examples/CMakeLists.txt
-@@ -48,8 +48,7 @@ if(GINKGO_BUILD_EXTLIB_EXAMPLE)
-     list(APPEND EXAMPLES_LIST external-lib-interfacing)
- endif()
- 
--find_package(OpenCV QUIET)
--if(OpenCV_FOUND)
-+if(FALSE)
-     list(APPEND EXAMPLES_LIST heat-equation schroedinger-splitting)
- else()
-     message(STATUS "No OpenCV found, disabling examples with video output")
-@@ -68,8 +67,7 @@ if(GINKGO_BUILD_MPI)
-     )
- endif()
- 
--find_package(Kokkos 4.1.00 QUIET)
--if(Kokkos_FOUND)
-+if(FALSE)
-     list(APPEND EXAMPLES_LIST kokkos-assembly)
- else()
-     message(STATUS "No Kokkos found, disabling examples with Kokkos assembly.")

sci-libs/ginkgo/files/ginkgo-1.6.0-disable_automagic_dependencies.patch
@@ -1,24 +1,0 @@
-diff --git a/examples/CMakeLists.txt b/examples/CMakeLists.txt
-index 41ed77d900..9a0c7d5128 100644
---- a/examples/CMakeLists.txt
-+++ b/examples/CMakeLists.txt
-@@ -40,8 +40,7 @@ if(GINKGO_BUILD_EXTLIB_EXAMPLE)
-     list(APPEND EXAMPLES_LIST external-lib-interfacing)
- endif()
- 
--find_package(OpenCV QUIET)
--if(OpenCV_FOUND)
-+if(FALSE)
-     list(APPEND EXAMPLES_LIST heat-equation schroedinger-splitting)
- else()
-     message(STATUS "No OpenCV found, disabling examples with video output")
-@@ -55,8 +54,7 @@ if(GINKGO_BUILD_MPI)
-     list(APPEND EXAMPLES_LIST distributed-solver)
- endif()
- 
--find_package(Kokkos QUIET)
--if(Kokkos_FOUND)
-+if(FALSE)
-     if(GINKGO_WITH_CCACHE)
-         message(WARNING "The CMAKE_CXX_COMPILER_LAUNCHER is set due to "
-             "GINKGO_WITH_CCACHE=ON which is known to casue issues with CUDA enabled "

sci-libs/ginkgo/files/ginkgo-1.8.0-disable_automagic_dependencies.patch
@@ -1,24 +1,0 @@
-diff --git a/examples/CMakeLists.txt b/examples/CMakeLists.txt
-index 653d52a..c56af59 100644
---- a/examples/CMakeLists.txt
-+++ b/examples/CMakeLists.txt
-@@ -41,8 +41,7 @@ if(GINKGO_BUILD_EXTLIB_EXAMPLE)
-     list(APPEND EXAMPLES_LIST external-lib-interfacing)
- endif()
- 
--find_package(OpenCV QUIET)
--if(OpenCV_FOUND)
-+if(FALSE)
-     list(APPEND EXAMPLES_LIST heat-equation schroedinger-splitting)
- else()
-     message(STATUS "No OpenCV found, disabling examples with video output")
-@@ -56,8 +55,7 @@ if(GINKGO_BUILD_MPI)
-     list(APPEND EXAMPLES_LIST distributed-solver)
- endif()
- 
--find_package(Kokkos 4.1.00 QUIET)
--if(Kokkos_FOUND)
-+if(FALSE)
-     list(APPEND EXAMPLES_LIST kokkos-assembly)
- else()
-     message(STATUS "No Kokkos found, disabling examples with Kokkos assembly.")

sci-libs/ginkgo/ginkgo-1.10.0.ebuild
@@ -26,10 +26,6 @@ RDEPEND="
 "
 DEPEND="${RDEPEND}"
 
-PATCHES=(
-	"${FILESDIR}"/${P}-disable_automagic_dependencies.patch
-)
-
 pkg_pretend() {
 	[[ ${MERGE_TYPE} != binary ]] && use openmp && tc-check-openmp
 }
@@ -52,6 +48,7 @@ src_configure() {
 		-DGINKGO_BUILD_TESTS=OFF
 		-DGINKGO_BUILD_BENCHMARKS=OFF
 		-DGINKGO_BUILD_REFERENCE=ON
+		-DGINKGO_BUILD_EXAMPLES=OFF
 		-DGINKGO_BUILD_CUDA=$(usex cuda)
 		-DGINKGO_BUILD_HWLOC=$(usex hwloc)
 		-DGINKGO_BUILD_OMP=$(usex openmp)

sci-libs/ginkgo/ginkgo-1.4.0-r3.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -48,6 +48,7 @@ src_configure() {
 		-DGINKGO_BUILD_TESTS=OFF
 		-DGINKGO_BUILD_BENCHMARKS=OFF
 		-DGINKGO_BUILD_REFERENCE=ON
+		-DGINKGO_BUILD_EXAMPLES=OFF
 		-DGINKGO_BUILD_CUDA=$(usex cuda)
 		-DGINKGO_BUILD_HWLOC=$(usex hwloc)
 		-DGINKGO_BUILD_OMP=$(usex openmp)

sci-libs/ginkgo/ginkgo-1.6.0.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -28,7 +28,6 @@ DEPEND="${RDEPEND}"
 
 PATCHES=(
 	"${FILESDIR}"/${P}-fix_type_mismatch.patch
-	"${FILESDIR}"/${P}-disable_automagic_dependencies.patch
 )
 
 pkg_pretend() {
@@ -53,6 +52,7 @@ src_configure() {
 		-DGINKGO_BUILD_TESTS=OFF
 		-DGINKGO_BUILD_BENCHMARKS=OFF
 		-DGINKGO_BUILD_REFERENCE=ON
+		-DGINKGO_BUILD_EXAMPLES=OFF
 		-DGINKGO_BUILD_CUDA=$(usex cuda)
 		-DGINKGO_BUILD_HWLOC=$(usex hwloc)
 		-DGINKGO_BUILD_OMP=$(usex openmp)

sci-libs/ginkgo/ginkgo-1.8.0.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -26,10 +26,6 @@ RDEPEND="
 "
 DEPEND="${RDEPEND}"
 
-PATCHES=(
-	"${FILESDIR}"/${P}-disable_automagic_dependencies.patch
-)
-
 pkg_pretend() {
 	[[ ${MERGE_TYPE} != binary ]] && use openmp && tc-check-openmp
 }
@@ -52,6 +48,7 @@ src_configure() {
 		-DGINKGO_BUILD_TESTS=OFF
 		-DGINKGO_BUILD_BENCHMARKS=OFF
 		-DGINKGO_BUILD_REFERENCE=ON
+		-DGINKGO_BUILD_EXAMPLES=OFF
 		-DGINKGO_BUILD_CUDA=$(usex cuda)
 		-DGINKGO_BUILD_HWLOC=$(usex hwloc)
 		-DGINKGO_BUILD_OMP=$(usex openmp)