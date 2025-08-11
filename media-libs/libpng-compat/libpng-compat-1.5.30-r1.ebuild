gentoo/gentoo

commit 7ab70686f97e7b45b1f665ba0aa4b1ea7f569f72

media-libs/libpng-compat: drop cpu_flags_arm_neon check
Do not rely on the internal checking code as it is deprecated and poorly
supported.

Bug: https://bugs.gentoo.org/939819
Signed-off-by: Paul Zander <negril.nx+gentoo@gmail.com>
Part-of: https://github.com/gentoo/gentoo/pull/43408
Signed-off-by: Sam James <sam@gentoo.org>

media-libs/libpng-compat/libpng-compat-1.5.30-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -16,7 +16,6 @@ S="${WORKDIR}/${MY_P}"
 LICENSE="libpng"
 SLOT="1.5"
 KEYWORDS="~alpha amd64 ~arm ~arm64 ~hppa ~m68k ~mips ~ppc ~ppc64 ~s390 ~sparc x86 ~amd64-linux ~x86-linux ~ppc-macos ~x64-macos ~x64-solaris"
-IUSE="cpu_flags_arm_neon"
 
 RDEPEND="
 	sys-libs/zlib:=[${MULTILIB_USEDEP}]
@@ -37,14 +36,6 @@ src_prepare() {
 	default
 
 	elibtoolize
-}
-
-multilib_src_configure() {
-	local myeconfargs=(
-		$(use_enable cpu_flags_arm_neon arm-neon check)
-	)
-
-	ECONF_SOURCE="${S}" econf "${myeconfargs[@]}"
 }
 
 multilib_src_compile() {

commit f2a31a53f2a84608a0840ee553646d58c02867e0

media-libs/libpng-compat: restore multilib_src_configure
Fixes: 7ab70686f97e7b45b1f665ba0aa4b1ea7f569f72
Closes: https://bugs.gentoo.org/961325
Signed-off-by: Mike Gilbert <floppym@gentoo.org>

media-libs/libpng-compat/libpng-compat-1.5.30-r1.ebuild
@@ -38,6 +38,10 @@ src_prepare() {
 	elibtoolize
 }
 
+multilib_src_configure() {
+	ECONF_SOURCE="${S}" econf
+}
+
 multilib_src_compile() {
 	emake libpng15.la
 }