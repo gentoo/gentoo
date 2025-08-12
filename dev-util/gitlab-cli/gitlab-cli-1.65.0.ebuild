gentoo/gentoo

commit 723ddb4206f51e6a47bbada0a40acf95f9938ca4

dev-util/gitlab-cli: add 1.65.0
Signed-off-by: William Hubbs <williamh@gentoo.org>

dev-util/gitlab-cli/gitlab-cli-1.65.0.ebuild
@@ -1,0 +1,39 @@
+# Copyright 2023-2025 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+inherit go-module
+
+# update on every bump
+GIT_COMMIT=8514bc42ef2c15a0ff27782a22b3add584b0d21c/
+go-mod/
+
+DESCRIPTION="the official gitlab command line interface"
+HOMEPAGE="https://gitlab.com/gitlab-org/cli"
+SRC_URI="https://gitlab.com/gitlab-org/cli/-/archive/v${PV}/${PN}-v${PV}.tar.bz2 -> ${P}.tar.bz2"
+SRC_URI+=" https://dev.gentoo.org/~williamh/dist/${P}-deps.tar.xz"
+SRC_URI+=" https://dev.gentoo.org/~williamh/dist/${P}-man.tar.xz"
+S="${WORKDIR}/cli-v${PV}-${GIT_COMMIT}"
+
+LICENSE="MIT"
+SLOT="0"
+KEYWORDS="~amd64"
+
+# tests communicate with gitlab.com and require a personal access token
+RESTRICT="test"
+
+QA_PRESTRIPPED=usr/bin/glab
+
+src_compile() {
+	emake \
+		BUILD_COMMIT_SHA=${GIT_COMMIT::8} \
+		GLAB_VERSION=v${PV} \
+		build
+	mv ../share "${T}" || die
+}
+
+src_install() {
+	dobin bin/glab
+	dodoc README.md
+	doman "${T}"/share/man/man1/*
+}

dev-util/gitlab-cli/Manifest
@@ -2,3 +2,6 @@ DIST gitlab-cli-1.55.0-deps.tar.xz 21737008 BLAKE2B 6c993fa949c6e0bec789e786b890
 DIST gitlab-cli-1.55.0.tar.bz2 16901867 BLAKE2B 2f18ceaa683c95b1f173851845f4ff96fb7fd897af39bd9081271da0b47f1205d6694f82ff1e733dfbbebc4305f5a4a74ab2f9f590b62af3054c4e33a653c40e SHA512 cde8882f982eb369d366eab6fdd5188889cb96736d21919ed97863484fefb885d02c1c169b6697f6d7af231b660cbbfd99070d7cc3c737739cd4f8fdbf51cc36
 DIST gitlab-cli-1.57.0-deps.tar.xz 18167468 BLAKE2B 27794aa056d061caa6307c0e49f74ec8877962f9da80a586c4a81d1253bebd8232151f4d6dda7c746f0ab620ad2b19838fa71137d614d05a31da2d1f4e87169f SHA512 80193b98e89db78c0a6f60b80b4319804acc8af9fbbc0f44045d4c6e4fd05e6c5922dc7666b3895740e3c2078b33b57bf989a0f0a069f172aadf0a5ad1fd0437
 DIST gitlab-cli-1.57.0.tar.bz2 16910116 BLAKE2B 3a914685937bf742a7ba211dc58af719525cdf9a88acd0ab0e92747a0a901fa5fef1402b4cad442eaa4b47ab8d3c21d5e2cb148c6f9320495df6a76ce4ab5aaa SHA512 89e06ac69a9ab5f56d3b27d187beaa069e4ef27c32c500fe55474c995e1100acb97e79ebd257d1ea37523aa2ecbdc8fa76ea6e2aa32c22727cb8b982048f2cd1
+DIST gitlab-cli-1.65.0-deps.tar.xz 45676844 BLAKE2B 62def35751dfb2fed5ac811520f45452468a71ae24947154a4731ea7011430626d3696cdad5e7887abc32abd74a587a2ceb56f6c7e2118e7ed619d5328f17dc7 SHA512 87edf69548b80277050bfd52ef53f4a3830042c512e4329353aa8c9ddc24442f2227d2c7ee4aa857b52760032e4e0b421f2ea0290f43d4dcbc280505e5183436
+DIST gitlab-cli-1.65.0-man.tar.xz 30832 BLAKE2B cf0aacd559b9a98bb1ac85137020ea48f2ed7ae4fc61ae8159b0d019cbd3573c9140e5f6036ae31f77b578ce2ff28929eb8c44f4da9ed9ff8e3ebe7997d96c92 SHA512 b828de026c1f3c891e4a5da411c3d292933a9f82ddadeb1eaadf640934746bd9955ed19b8ebb509374057dfd91386451986cacb48b015c4b8a9495f70eaf3d7c
+DIST gitlab-cli-1.65.0.tar.bz2 16964412 BLAKE2B 5cb5d073c5624acfe890eb73fd504aadadd8cfd2d33040db25234f840a1d78e0afd149eb1c2d7f23d277e6f74cbfd5fd53734d7b185f26b5c3d59c3134d455d4 SHA512 e01548eb4419ebd441c991b7e30037ddc7a6184a7ddbcde4206a45a0d64a41baaf044db137f56e781eeadef85421f793d59b162922fb32a31f886092f44406e9

commit 95037a28e68b83c050e464390d4c05a80e4a26ef

dev-util/gitlab-cli: fix stray line
Reported by xgqt and parona. It breaks metadata generation with pkgcraft too:
> ERROR invalid pkg: dev-util/gitlab-cli-1.65.0::gentoo: line 9: go-mod/: restricted: cannot specify `/' in command names

Fixes: 723ddb4206f51e6a47bbada0a40acf95f9938ca4
Signed-off-by: Sam James <sam@gentoo.org>

dev-util/gitlab-cli/gitlab-cli-1.65.0.ebuild
@@ -6,7 +6,6 @@ inherit go-module
 
 # update on every bump
 GIT_COMMIT=8514bc42ef2c15a0ff27782a22b3add584b0d21c/
-go-mod/
 
 DESCRIPTION="the official gitlab command line interface"
 HOMEPAGE="https://gitlab.com/gitlab-org/cli"