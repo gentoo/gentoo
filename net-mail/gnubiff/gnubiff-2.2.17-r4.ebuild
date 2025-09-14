gentoo/gentoo

commit a0b6e2a772ca60c1af8c6b49972a13e16b42eb25

net-mail/gnubiff: add 2.2.17-r3
- adds a new LICENSE GPL-3+-with-openssl-exception
- cleans up all old 2.2.15 patches
- fixes compilation errors on clang-only systems by removing
  '-export-dynamic' from Makefile

Closes: https://bugs.gentoo.org/880267
Closes: https://bugs.gentoo.org/930500
Closes: https://bugs.gentoo.org/933241
Signed-off-by: Phil Stracchino <phils@caerllewys.net>
Closes: https://github.com/gentoo/gentoo/pull/37513
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

net-mail/gnubiff/files/gnubiff-2.2.17-clang.patch
@@ -1,0 +1,15 @@
+--- gnubiff-2.2.17-r3/src/Makefile.am
++++ gnubiff-2.2.17-r3/src/Makefile.am
+@@ -6,11 +6,11 @@
+ 	-DGNUBIFF_DATADIR=\""$(pkgdatadir)"\"			\
+ 	-DGNUBIFF_ICONDIR=\""$(datadir)/pixmaps"\"		\
+ 	-DGNUBIFF_UIDIR=\""$(datadir)/gnome-2.0/ui"\"	\
+ 	-DREAL_DATADIR=\""$(datadir)"\"					\
+ 	-D_GNU_SOURCE									\
+-	-Wall -ansi -pedantic -export-dynamic
++	-Wall -ansi -pedantic
+ 
+ bin_PROGRAMS = gnubiff
+ 
+ gnome_sources =  \
+ 	ui-applet-gnome.cc 		ui-applet-gnome.h

licenses/GPL-3+-with-openssl-exception
@@ -1,0 +1,16 @@
+GNU General Public License, version 3 or any later version.
+See GPL-3 for the full text of this license.
+
+
+                            Exception
+
+  In addition, as a special exception, the copyright holders give permission
+to link the code of portions of this program with the OpenSSL library under
+certain conditions as described in each individual source file, and distribute
+linked combinations including the two.
+You must obey the GNU General Public License in all respects for all of the
+code used other than OpenSSL.  If you modify file(s) with this exception, you
+may extend this exception to your version of the file(s), but you are not
+obligated to do so.  If you do not wish to do so, delete this exception
+statement from your version.  If you delete this exception statement from all
+source files in the program, then also delete it here.

net-mail/gnubiff/files/gnubiff-2.2.15-gold.patch
@@ -1,17 +1,0 @@
-Author: Eray Aslan <eras@gentoo.org>
-Date:   Sat Jun 4 19:54:16 2011 +0000
-
-    do not underlink - needed for gold
-
---- a/configure.ac
-+++ b/configure.ac
-@@ -117,6 +117,9 @@ AC_CHECK_LIB(ssl, AES_encrypt, AC_DEFINE_UNQUOTED(HAVE_AES, 1,
- 			 Define to 1 if AES encryption is available), [
- 	AC_CHECK_LIB(crypto, AES_encrypt, AC_DEFINE_UNQUOTED(HAVE_AES, 1))])
- 
-+dnl needed for gold
-+AC_CHECK_LIB([X11],[XSync])
-+
- dnl GLIB, GTK, ...
- GTK_REQUIRED="gtk+-2.0 >= 2.6"
- GDK_PIXBUF_REQUIRED="gdk-pixbuf-2.0 >= 2.4"

net-mail/gnubiff/files/gnubiff-2.2.15-underlink.patch
@@ -1,12 +1,0 @@
-Bug #456302
---- configure.ac	2013-02-15 13:15:01.306855243 +0200
-+++ configure.ac	2013-02-15 13:20:07.921188143 +0200
-@@ -120,6 +120,8 @@
- dnl needed for gold
- AC_CHECK_LIB([X11],[XSync])
- 
-+AC_CHECK_LIB([gthread-2.0],[g_thread_init])
-+
- dnl GLIB, GTK, ...
- GTK_REQUIRED="gtk+-3.0 >= 3.0.0"
- GMODULE_REQUIRED="gmodule-export-2.0 >= 2.4"

net-mail/gnubiff/files/gnubiff-2.2.17-configure.patch
@@ -1,0 +1,18 @@
+--- gnubiff-2.2.17/configure.ac
++++ gnubiff-2.2.17/configure.ac
+@@ -115,10 +115,15 @@
+ dnl LIBSSL (for AES encryption/decryption)
+ AC_CHECK_LIB(ssl, AES_encrypt, AC_DEFINE_UNQUOTED(HAVE_AES, 1, 
+ 			 Define to 1 if AES encryption is available), [
+ 	AC_CHECK_LIB(crypto, AES_encrypt, AC_DEFINE_UNQUOTED(HAVE_AES, 1))])
+ 
++dnl needed for gold
++AC_CHECK_LIB([X11],[XSync])
++
++AC_CHECK_LIB([gthread-2.0],[g_thread_init])
++
+ dnl GLIB, GTK, ...
+ GTK_REQUIRED="gtk+-3.0 >= 3.0.0"
+ GMODULE_REQUIRED="gmodule-export-2.0 >= 2.4"
+ X11="xproto x11"
+ PKG_CHECK_MODULES(GNUBIFF_DEP, 

net-mail/gnubiff/files/gnubiff-2.2.17-fix-nls.patch
@@ -1,6 +1,8 @@
---- src/nls.h	2009-01-01 16:45:39.000000000 -0800
-+++ src/nls.h	2010-10-01 17:13:03.595458860 -0700
-@@ -49,11 +49,6 @@
+--- gnubiff-2.2.17/src/nls.h
++++ gnubiff-2.2.17/src/nls.h
+@@ -43,13 +43,8 @@
+ #    define N_(String) (String)
+ #  endif
  #else
  #  define _(String) (String)
  #  define N_(String) (String)

net-mail/gnubiff/gnubiff-2.2.17-r3.ebuild
@@ -1,0 +1,57 @@
+# Copyright 1999-2024 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit autotools
+
+DESCRIPTION="A mail notification program"
+HOMEPAGE="https://gnubiff.sourceforge.net/"
+SRC_URI="https://downloads.sourceforge.net/${PN}/${P}.tar.gz"
+
+LICENSE="GPL-3+-with-openssl-exception"
+SLOT="0"
+KEYWORDS="~amd64 ~x86"
+IUSE="debug fam nls password"
+
+# This package depends upon deprecated gnome-base/libglade.  An
+# upstream but has been filed on this issue.
+# (https://sourceforge.net/p/gnubiff/bugs/67/)
+
+RDEPEND="
+	dev-libs/popt
+	>=gnome-base/libglade-2.3
+	x11-libs/gdk-pixbuf
+	>=x11-libs/gtk+-3:3
+	x11-libs/libX11
+	x11-libs/pango
+	fam? ( virtual/fam )
+	password? ( dev-libs/openssl:0= )
+"
+DEPEND="${RDEPEND}"
+BDEPEND="
+	virtual/pkgconfig
+"
+
+PATCHES=(
+	"${FILESDIR}"/${PN}-2.2.17-fix-nls.patch
+	"${FILESDIR}"/${PN}-2.2.17-configure.patch
+	"${FILESDIR}"/${PN}-2.2.17-clang.patch
+)
+
+src_prepare() {
+	default
+	eautoreconf
+}
+
+src_configure() {
+	local myeconfargs=(
+		--disable-gnome # avoid deprecated gnome-panel-2.x
+		$(use_enable debug)
+		$(use_enable nls)
+		$(use_enable fam)
+		$(use_with password)
+		$(use_with password password-string ${RANDOM}${RANDOM}${RANDOM}${RANDOM})
+	)
+	econf "${myeconfargs[@]}"
+}

profiles/license_groups
@@ -15,7 +15,7 @@
 # https://www.gnu.org/licenses/license-list.html
 # GPL or LGPL with various exceptions are also included here, because
 # they are more permissive than the licenses they are based on.
-GPL-COMPATIBLE AGPL-3 AGPL-3+ Apache-2.0 Apache-2.0-with-LLVM-exceptions Artistic-2 Boost-1.0 BSD BSD-2 CC0-1.0 CeCILL-2 Clarified-Artistic Clear-BSD ECL-2.0 FSFAP FTL gcc-runtime-library-exception-3.1 GPL-1 GPL-1+ GPL-2 GPL-2+ GPL-2+-with-openssl-exception GPL-2+-with-eCos-exception-2 GPL-2-with-classpath-exception GPL-2-with-exceptions GPL-2-with-font-exception GPL-2-with-linking-exception GPL-2-with-MySQL-FLOSS-exception GPL-2+-with-Pyinstaller-Bootloader-exception GPL-3 GPL-3+ GPL-3+-with-autoconf-exception GPL-3+-with-font-exception GPL-3-with-font-exception GPL-3-with-openssl-exception HPND IJG ISC LGPL-2 LGPL-2+ LGPL-2-with-linking-exception LGPL-2.1 LGPL-2.1+ LGPL-2.1-with-linking-exception LGPL-3 LGPL-3+ LGPL-3-with-linking-exception libgcc libstdc++ metapackage MIT MPL-2.0 Nokia-Qt-LGPL-Exception-1.1 OPENLDAP PSF-2 PSF-2.4 public-domain PYTHON qwt Ruby Ruby-BSD SGI-B-2.0 Sleepycat tanuki-community Transmission-OpenSSL-exception Unicode-DFS-2016 Unlicense UoI-NCSA UPL-1.0 UPX-exception vim W3C WTFPL-2 wxWinLL-3.1 ZLIB ZPL
+GPL-COMPATIBLE AGPL-3 AGPL-3+ Apache-2.0 Apache-2.0-with-LLVM-exceptions Artistic-2 Boost-1.0 BSD BSD-2 CC0-1.0 CeCILL-2 Clarified-Artistic Clear-BSD ECL-2.0 FSFAP FTL gcc-runtime-library-exception-3.1 GPL-1 GPL-1+ GPL-2 GPL-2+ GPL-2+-with-openssl-exception GPL-2+-with-eCos-exception-2 GPL-2-with-classpath-exception GPL-2-with-exceptions GPL-2-with-font-exception GPL-2-with-linking-exception GPL-2-with-MySQL-FLOSS-exception GPL-2+-with-Pyinstaller-Bootloader-exception GPL-3 GPL-3+ GPL-3+-with-autoconf-exception GPL-3+-with-font-exception GPL-3+-with-font-exception GPL-3+-with-openssl-exception GPL-3-with-openssl-exception HPND IJG ISC LGPL-2 LGPL-2+ LGPL-2-with-linking-exception LGPL-2.1 LGPL-2.1+ LGPL-2.1-with-linking-exception LGPL-3 LGPL-3+ LGPL-3-with-linking-exception libgcc libstdc++ metapackage MIT MPL-2.0 Nokia-Qt-LGPL-Exception-1.1 OPENLDAP PSF-2 PSF-2.4 public-domain PYTHON qwt Ruby Ruby-BSD SGI-B-2.0 Sleepycat tanuki-community Transmission-OpenSSL-exception Unicode-DFS-2016 Unlicense UoI-NCSA UPL-1.0 UPX-exception vim W3C WTFPL-2 wxWinLL-3.1 ZLIB ZPL
 
 # Free software licenses approved by the FSF
 FSF-APPROVED @GPL-COMPATIBLE AFL-2.1 AFL-3.0 Apache-1.0 Apache-1.1 APSL-2 BSD-4 CDDL CeCILL-B CeCILL-C CNRI CPAL-1.0 CPL-1.0 EPL-1.0 EPL-2.0 EUPL-1.1 EUPL-1.2 FraunhoferFDK gnuplot IBM LPL-1.02 LPPL-1.2 LPPL-1.3a MPL-1.0 MPL-1.1 Ms-PL Ms-RL NPL-1.1 openssl OSL-1.1 OSL-2.0 OSL-2.1 PHP-3.01 QPL-1.0 Zend-2.0

commit bc4613b68fd85c94554567130b53dfbae8e6a03c

net-mail/gnubiff: fix Clang patch
Closes: https://bugs.gentoo.org/930500
Fixes: a0b6e2a772ca60c1af8c6b49972a13e16b42eb25
Signed-off-by: Sam James <sam@gentoo.org>

net-mail/gnubiff/files/gnubiff-2.2.17-clang.patch
@@ -7,7 +7,7 @@
  	-DREAL_DATADIR=\""$(datadir)"\"					\
  	-D_GNU_SOURCE									\
 -	-Wall -ansi -pedantic -export-dynamic
-+	-Wall -ansi -pedantic
++	-Wall -ansi -pedantic -Wl,--export-dynamic
  
  bin_PROGRAMS = gnubiff
  

net-mail/gnubiff/gnubiff-2.2.17-r4.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8