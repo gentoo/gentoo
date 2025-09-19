gentoo/gentoo

commit 70367ff022b2a27b7d1ba91da9a09b25c7b0d017

sec-keys/minisig-keys-ccache: new package, add 20250914
Signed-off-by: Sam James <sam@gentoo.org>

sec-keys/minisig-keys-ccache/minisig-keys-ccache-20250914.ebuild
@@ -1,0 +1,19 @@
+# Copyright 1999-2025 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+DESCRIPTION="OpenPGP keys used for ccache"
+HOMEPAGE="https://ccache.dev/download.html"
+S="${WORKDIR}"
+
+LICENSE="public-domain"
+SLOT="0"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~loong ~m68k ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86"
+
+src_install() {
+	insinto /usr/share/openpgp-keys
+	newins - ccache.minisig <<-EOF
+		RWQX7yXbBedVfI4PNx6FLdFXu9GHUFsr28s4BVGxm4BeybtnX3P06saF
+	EOF
+}

sec-keys/minisig-keys-ccache/metadata.xml
@@ -1,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE pkgmetadata SYSTEM "https://www.gentoo.org/dtd/metadata.dtd">
+<pkgmetadata>
+	<maintainer type="project">
+		<email>toolchain@gentoo.org</email>
+		<name>Gentoo Toolchain Project</name>
+	</maintainer>
+	<stabilize-allarches/>
+</pkgmetadata>

commit fb297734124f0603c368ee498820c6af085fa7e8

sec-keys/minisig-keys-ccache: update DESCRIPTION
I hadn't meant to push 70367ff022b2a27b7d1ba91da9a09b25c7b0d017 when
I did, but didn't bother to revert it as it wasn't strictly wrong otherwise
(just nothing using it yet, and the DESCRIPTION).

Fixes: 70367ff022b2a27b7d1ba91da9a09b25c7b0d017
Signed-off-by: Sam James <sam@gentoo.org>

sec-keys/minisig-keys-ccache/minisig-keys-ccache-20250914.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-DESCRIPTION="OpenPGP keys used for ccache"
+DESCRIPTION="minisig keys used for ccache"
 HOMEPAGE="https://ccache.dev/download.html"
 S="${WORKDIR}"
 