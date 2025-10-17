gentoo/gentoo

commit c05064e5a16cf71ced2e20b31b04ba3d7ce33db5

dev-util/pwntools: block broken dependencies
see https://github.com/unicorn-engine/unicorn/issues/2134

Signed-off-by: Mario Haustein <mario.haustein@hrz.tu-chemnitz.de>
Part-of: https://github.com/gentoo/gentoo/pull/44207
Signed-off-by: Sam James <sam@gentoo.org>

dev-util/pwntools/pwntools-4.15.0_beta1-r1.ebuild
@@ -29,6 +29,7 @@ RDEPEND="
 	>=dev-libs/capstone-3.0.5[python,${PYTHON_USEDEP}]
 	>=dev-util/ROPgadget-5.3[${PYTHON_USEDEP}]
 	>=dev-util/unicorn-1.0.2[python,${PYTHON_USEDEP}]
+	!=dev-util/unicorn-2.1.4-r0[python,${PYTHON_USEDEP}]
 	dev-python/colored-traceback[${PYTHON_USEDEP}]
 	>=dev-python/intervaltree-3.0[${PYTHON_USEDEP}]
 	>=dev-python/mako-1.0.0[${PYTHON_USEDEP}]

commit 4e7d61f19a6041ec9a9785aeecd228b5dfd407a8

dev-util/pwntools: avoid using blocker for broken dep
Portage doesn't backtrack around blockers like this, (weak) blockers are purely
for file collisions (or in exceptional cases, strong blockers where the
existence of one package totally breaks another -- but they don't work
as "forward dependencies").

Just use a < dep for now. When a fixed version of unicorn is available,
we can swap to || ( ... ).

Fixes: c05064e5a16cf71ced2e20b31b04ba3d7ce33db5
Signed-off-by: Sam James <sam@gentoo.org>

dev-util/pwntools/pwntools-4.15.0.ebuild
@@ -29,7 +29,7 @@ RDEPEND="
 	>=dev-libs/capstone-3.0.5[python,${PYTHON_USEDEP}]
 	>=dev-util/ROPgadget-5.3[${PYTHON_USEDEP}]
 	>=dev-util/unicorn-1.0.2[python,${PYTHON_USEDEP}]
-	!=dev-util/unicorn-2.1.4-r0[python,${PYTHON_USEDEP}]
+	<dev-util/unicorn-2.1.4[python,${PYTHON_USEDEP}]
 	dev-python/colored-traceback[${PYTHON_USEDEP}]
 	>=dev-python/intervaltree-3.0[${PYTHON_USEDEP}]
 	>=dev-python/mako-1.0.0[${PYTHON_USEDEP}]

dev-util/pwntools/pwntools-4.15.0_beta1-r1.ebuild
@@ -29,7 +29,7 @@ RDEPEND="
 	>=dev-libs/capstone-3.0.5[python,${PYTHON_USEDEP}]
 	>=dev-util/ROPgadget-5.3[${PYTHON_USEDEP}]
 	>=dev-util/unicorn-1.0.2[python,${PYTHON_USEDEP}]
-	!=dev-util/unicorn-2.1.4-r0[python,${PYTHON_USEDEP}]
+	<dev-util/unicorn-2.1.4[python,${PYTHON_USEDEP}]
 	dev-python/colored-traceback[${PYTHON_USEDEP}]
 	>=dev-python/intervaltree-3.0[${PYTHON_USEDEP}]
 	>=dev-python/mako-1.0.0[${PYTHON_USEDEP}]

dev-util/pwntools/pwntools-9999.ebuild
@@ -29,7 +29,7 @@ RDEPEND="
 	>=dev-libs/capstone-3.0.5[python,${PYTHON_USEDEP}]
 	>=dev-util/ROPgadget-5.3[${PYTHON_USEDEP}]
 	>=dev-util/unicorn-1.0.2[python,${PYTHON_USEDEP}]
-	!=dev-util/unicorn-2.1.4-r0[python,${PYTHON_USEDEP}]
+	<dev-util/unicorn-2.1.4[python,${PYTHON_USEDEP}]
 	dev-python/colored-traceback[${PYTHON_USEDEP}]
 	>=dev-python/intervaltree-3.0[${PYTHON_USEDEP}]
 	>=dev-python/mako-1.0.0[${PYTHON_USEDEP}]