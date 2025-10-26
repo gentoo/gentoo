gentoo/gentoo

commit 97fc44852d225d13cc8f60030ddf30c1e30552df

net-libs/socket_wrapper: add 1.4.2
Signed-off-by: Sam James <sam@gentoo.org>

net-libs/socket_wrapper/Manifest
@@ -1,2 +1,3 @@
 DIST socket_wrapper-1.3.4.tar.gz 81997 BLAKE2B 5d290ef59c16fbdde362749196cc91c3f050518f82cb14fbafa3e00d5fb51a6b085b94b08413ca601731440d8eadb5dfbe8d011567fd67b1240b0cc380504813 SHA512 6e6a940e26d0f4dc4f1c0f84643d5a124bb2cccd1713481e9c42ea7986884928d736b1ef8da8a2aef3d3803938b6e1ebd30bdbe9784fda95a9a59cdbd1d19f61
 DIST socket_wrapper-1.4.0.tar.gz 87353 BLAKE2B 67c2a4bae2e1031399137e404e95e6e6cfa1143ff659a7d34533e908f9169b8a79d1dfffaa51a6280a8cfc9e03dc07585ff41b0e087527b1ad0bb5adb282156e SHA512 944e5ff2b6919e414f9045fe294b9b4813272fe7c00f5ac144c999b99572be6b3ed2eb975ec1d07bd853cffea6dffe339280e4961520d34667f808f76b47db57
+DIST socket_wrapper-1.4.2.tar.gz 90641 BLAKE2B 8a43f48ed8f3809eac8c86ee55e740ac37068d729f3f36955b9f017f29c40fa86f1f4455d2e33c8bb9413e25d8e7cb4bab84cd2688f54febb17d41c37ceb4d79 SHA512 30de1dfa6bfae51114b0557a00b87c785b7a00cf62d495b5c4bd837f32016ed58f942e7de0c33d09aa72e533f58283af6b5ae50f276ad7e58e853e32a8400024

net-libs/socket_wrapper/socket_wrapper-1.4.2.ebuild
@@ -1,0 +1,34 @@
+# Copyright 1999-2024 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit cmake-multilib flag-o-matic
+
+DESCRIPTION="Library passing all socket communications through unix sockets"
+HOMEPAGE="https://cwrap.org/socket_wrapper.html"
+SRC_URI="https://ftp.samba.org/pub/cwrap/${P}.tar.gz"
+
+LICENSE="BSD"
+SLOT="0"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~ia64 ~loong ~ppc ~ppc64 ~riscv ~sparc ~x86"
+IUSE="test"
+RESTRICT="!test? ( test )"
+
+BDEPEND="test? ( >=dev-util/cmocka-1.1.0 )"
+
+src_configure() {
+	# https://gcc.gnu.org/PR46596
+	# https://gcc.gnu.org/PR101270
+	filter-flags -fno-semantic-interposition
+
+	local mycmakeargs=(
+		-DUNIT_TESTING=$(usex test ON OFF)
+	)
+	cmake-multilib_src_configure
+}
+
+src_test() {
+	ewarn "test_echo_tcp_poll takes a while to run!"
+	cmake_src_test
+}

commit 24696ef3ec54aa0b7293f81b5673100f823adfd1

net-libs/socket_wrapper: add 1.5.1
The previous warning added in 97fc44852d225d13cc8f60030ddf30c1e30552df actually
stopped running tests at all. Fix that.

Fixes: 97fc44852d225d13cc8f60030ddf30c1e30552df
Signed-off-by: Sam James <sam@gentoo.org>

net-libs/socket_wrapper/Manifest
@@ -1,3 +1,4 @@
 DIST socket_wrapper-1.4.0.tar.gz 87353 BLAKE2B 67c2a4bae2e1031399137e404e95e6e6cfa1143ff659a7d34533e908f9169b8a79d1dfffaa51a6280a8cfc9e03dc07585ff41b0e087527b1ad0bb5adb282156e SHA512 944e5ff2b6919e414f9045fe294b9b4813272fe7c00f5ac144c999b99572be6b3ed2eb975ec1d07bd853cffea6dffe339280e4961520d34667f808f76b47db57
 DIST socket_wrapper-1.4.2.tar.gz 90641 BLAKE2B 8a43f48ed8f3809eac8c86ee55e740ac37068d729f3f36955b9f017f29c40fa86f1f4455d2e33c8bb9413e25d8e7cb4bab84cd2688f54febb17d41c37ceb4d79 SHA512 30de1dfa6bfae51114b0557a00b87c785b7a00cf62d495b5c4bd837f32016ed58f942e7de0c33d09aa72e533f58283af6b5ae50f276ad7e58e853e32a8400024
 DIST socket_wrapper-1.5.0.tar.gz 89865 BLAKE2B fec431443f48aaa50f866da5aa4b8acab1ff5c6799264464451e7576bb35d209a5a366bd0454887699e3ae7c039f840c14411a394b0d4871cc593a962b991eb6 SHA512 2ab501e592e6ece88a3e1f5f34a81d6504a07a3df10d21aa4d21b2c96637e7c3660be309624ba9f743e46deb8ac75e8152e082d313cd7b31e510291c5b0ebd20
+DIST socket_wrapper-1.5.1.tar.gz 89297 BLAKE2B 62e4103db7d574eb125b1fb36b0191c27a22f8286e2ede33a22a0fb527c960fd9a62f02de331e984117f9a76859011c3f4989932753bebf3f3f1f81815b3392d SHA512 00b6dcc749b6aef2d20f509f5be26b8a792a819c482abe328381bf53a20ff144247848124f01ebaad76fb4d6afd23e4251dff843a4760ca64b9a9c6fd8ed1c17

net-libs/socket_wrapper/socket_wrapper-1.5.1.ebuild
@@ -1,0 +1,58 @@
+# Copyright 1999-2025 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit cmake-multilib flag-o-matic
+
+DESCRIPTION="Library passing all socket communications through unix sockets"
+HOMEPAGE="https://cwrap.org/socket_wrapper.html"
+SRC_URI="https://ftp.samba.org/pub/cwrap/${P}.tar.gz"
+
+LICENSE="BSD"
+SLOT="0"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~loong ~ppc ~ppc64 ~riscv ~sparc ~x86"
+IUSE="test"
+RESTRICT="!test? ( test )"
+
+BDEPEND="test? ( >=dev-util/cmocka-1.1.0 )"
+
+PATCHES=(
+	"${FILESDIR}"/${PN}-1.5.0-stdint.patch
+)
+
+src_configure() {
+	# https://gcc.gnu.org/PR46596
+	# https://gcc.gnu.org/PR101270
+	filter-flags -fno-semantic-interposition
+
+	local mycmakeargs=(
+		-DUNIT_TESTING=$(usex test ON OFF)
+	)
+	cmake-multilib_src_configure
+}
+
+src_test() {
+	CMAKE_SKIP_TESTS=(
+		# These fail because no privileges to set priority to avoid
+		# deadlock
+		test_tcp_listen
+		test_echo_tcp_connect
+		test_echo_tcp_socket_options
+		test_echo_tcp_sendmsg_recvmsg
+		test_echo_tcp_sendmmsg_recvmmsg
+		test_echo_tcp_write_read
+		test_echo_tcp_writev_readv
+		test_echo_tcp_get_peer_sock_name
+		test_echo_udp_sendto_recvfrom
+		test_echo_udp_send_recv
+		test_echo_udp_sendmsg_recvmsg
+		test_thread_echo_tcp_connect
+		test_thread_echo_tcp_write_read
+		test_thread_echo_tcp_sendmsg_recvmsg
+		test_thread_echo_udp_send_recv
+	)
+
+	ewarn "test_echo_tcp_poll takes a while to run!"
+	cmake-multilib_src_test
+}