gentoo/gentoo

commit 2fa125d055dcfebe34acf09ea04ff89c42eeb19b

ninja-utils.eclass: use app-alternatives/ninja
Needed now that it's stable.

Bug: https://bugs.gentoo.org/919969
Closes: https://bugs.gentoo.org/917623
Signed-off-by: Sam James <sam@gentoo.org>

eclass/ninja-utils.eclass
@@ -55,17 +55,7 @@ _NINJA_UTILS_ECLASS=1
 
 inherit multiprocessing
 
-case "${NINJA}" in
-	ninja)
-		NINJA_DEPEND=">=dev-util/ninja-1.8.2"
-	;;
-	samu)
-		NINJA_DEPEND="dev-util/samurai"
-	;;
-	*)
-		NINJA_DEPEND=""
-	;;
-esac
+NINJA_DEPEND="app-alternatives/ninja"
 
 # @FUNCTION: get_NINJAOPTS
 # @DESCRIPTION:
@@ -84,7 +74,14 @@ get_NINJAOPTS() {
 # by the supplied arguments.  This function dies if ninja fails.  It
 # also supports being called via 'nonfatal'.
 eninja() {
-	[[ -n "${NINJA_DEPEND}" ]] || ewarn "Unknown value '${NINJA}' for \${NINJA}"
+	case "${NINJA}" in
+		ninja|samu)
+			;;
+		*)
+			ewarn "Unknown value '${NINJA}' for \${NINJA}"
+			;;
+	esac
+
 	local v
 	case "${NINJA_VERBOSE}" in
 		OFF) ;;

commit 44a4ba54bd0cf2c1c633ce326c68ad45d76584b8

ninja-utils.eclass: Respect NINJA for NINJA_DEPEND again
Make NINJA_DEPEND respect the value of NINJA again.  This was changed
while updating for app-alternatives/ninja.  However, it is still valid
to override NINJA in ebuilds, in which case we want NINJA_DEPEND
to refer to the appropriate implementation rather than any alternative.

Fixes: 2fa125d055dcf ("ninja-utils.eclass: use app-alternatives/ninja")
Bug: https://bugs.gentoo.org/922639
Signed-off-by: Michał Górny <mgorny@gentoo.org>

eclass/ninja-utils.eclass
@@ -60,7 +60,17 @@ _NINJA_UTILS_ECLASS=1
 
 inherit multiprocessing
 
-NINJA_DEPEND="app-alternatives/ninja"
+case ${NINJA} in
+	ninja)
+		NINJA_DEPEND="app-alternatives/ninja"
+		;;
+	ninja-reference)
+		NINJA_DEPEND="dev-build/ninja"
+		;;
+	samu)
+		NINJA_DEPEND="dev-build/samurai"
+		;;
+esac
 
 # @FUNCTION: get_NINJAOPTS
 # @DESCRIPTION: