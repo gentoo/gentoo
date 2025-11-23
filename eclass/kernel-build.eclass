gentoo/gentoo

commit d5b0c16d1d3fdf63d743b22e7192e88e04556b05

kernel-build.eclass: Support merging config files
Add a convenience function to merge kernel config files from within
the build tree, and support merging user configs.

Signed-off-by: Michał Górny <mgorny@gentoo.org>

eclass/kernel-build.eclass
@@ -197,6 +197,38 @@ kernel-build_pkg_postinst() {
 	savedconfig_pkg_postinst
 }
 
+# @FUNCTION: kernel-build_merge_configs
+# @USAGE: [distro.config...]
+# @DESCRIPTION:
+# Merge the config files specified as arguments (if any) into
+# the '.config' file in the current directory, then merge
+# any user-supplied configs from ${BROOT}/etc/kernel/config.d/*.config.
+# The '.config' file must exist already and contain the base
+# configuration.
+kernel-build_merge_configs() {
+	debug-print-function ${FUNCNAME} "${@}"
+
+	[[ -f .config ]] || die "${FUNCNAME}: .config does not exist"
+	has .config "${@}" &&
+		die "${FUNCNAME}: do not specify .config as parameter"
+
+	local shopt_save=$(shopt -p nullglob)
+	shopt -s nullglob
+	local user_configs=( "${BROOT}"/etc/kernel/config.d/*.config )
+	shopt -u nullglob
+
+	if [[ ${#user_configs[@]} -gt 0 ]]; then
+		elog "User config files are being applied:"
+		local x
+		for x in "${user_configs[@]}"; do
+			elog "- ${x}"
+		done
+	fi
+
+	./scripts/kconfig/merge_config.sh -m -r \
+		.config "${@}" "${user_configs[@]}" || die
+}
+
 _KERNEL_BUILD_ECLASS=1
 fi
 

commit 185e62134732aee519a377c51965ba62ae1fd965

kernel-build.eclass: restore nullglob state in kernel-build_merge_configs()
Presently, the kernel-build_merge_configs() function captures a command
that would restore the prior state of the nullglob shell option. Yet, it
never subsequently bothers to evaluate that command. Instead, it ends up
disabling the option, irrespective of its prior state.

Address this issue by evaluating the content of the 'shopt_save'
variable after the 'user_configs' array variable has been assigned. Note
that to use eval is wholly proper; those eclasses that do not, should.

Fixes: d5b0c16d1d3fdf63d743b22e7192e88e04556b05
Signed-off-by: Kerin Millar <kfm@plushkava.net>
Signed-off-by: Sam James <sam@gentoo.org>

eclass/kernel-build.eclass
@@ -713,7 +713,7 @@ kernel-build_merge_configs() {
 	local shopt_save=$(shopt -p nullglob)
 	shopt -s nullglob
 	local user_configs=( "${BROOT}"/etc/kernel/config.d/*.config )
-	shopt -u nullglob
+	eval "${shopt_save}"
 
 	local merge_configs=( "${@}" )
 