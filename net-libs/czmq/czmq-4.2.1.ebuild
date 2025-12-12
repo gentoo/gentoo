gentoo/gentoo

commit 1c4168493ca6cf243985a6f4efebc5986438295c

net-libs/czmq: update EAPI 7 -> 8, add missing dep for 4.2.1
Signed-off-by: Joonas Niilola <juippis@gentoo.org>

net-libs/czmq/czmq-4.2.1.ebuild
@@ -1,7 +1,7 @@
 # Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
-EAPI="7"
+EAPI="8"
 
 DESCRIPTION="High-level C Binding for ZeroMQ"
 HOMEPAGE="http://czmq.zeromq.org"
@@ -20,9 +20,12 @@ RDEPEND=">=net-libs/zeromq-4:=[drafts?]
 	http-client? ( net-misc/curl )
 	http-server? ( net-libs/libmicrohttpd:= )
 	lz4? ( app-arch/lz4:= )
-	nss? ( dev-libs/nss:= )
+	nss? (
+		dev-libs/nspr
+		dev-libs/nss
+	)
 	systemd? ( sys-apps/systemd )
-	uuid? ( sys-apps/util-linux:0= )"
+	uuid? ( sys-apps/util-linux:0 )"
 
 DEPEND="${RDEPEND}"
 

commit d2834a1c36fb4dc55da297de48dd7f032bfa9d9c

net-libs/czmq: remove nonfunctional USE=static-libs
Nobody has noticed for over 4 years that ever since the port to EAPI 8,
this didn't actually install any static libs. Reason is because EAPI 8
started passing --enable-static by default, and the ebuild implemented
the USE "incorrectly" by building useless stuff per EAPI 7 default and
then rm'ing it in src_install only if it existed. With the bump, it was
not built at all.

Given nobody can have ever relied on this in recent years, it is clear
nobody uses it, so no need to make it work.

Fixes: 1c4168493ca6cf243985a6f4efebc5986438295c
Fixes: 5afce7b17163b47ff8fb4df89c9f93aa3ef94a08
Signed-off-by: Eli Schwartz <eschwartz@gentoo.org>

net-libs/czmq/czmq-4.2.1.ebuild
@@ -10,7 +10,7 @@ SRC_URI="https://github.com/zeromq/${PN}/releases/download/v${PV}/${P}.tar.gz"
 LICENSE="MPL-2.0"
 SLOT="0/4"
 KEYWORDS="amd64 arm arm64 ~hppa ~ppc64 ~riscv x86"
-IUSE="curl drafts http-client http-server lz4 nss static-libs systemd test +uuid"
+IUSE="curl drafts http-client http-server lz4 nss systemd test +uuid"
 RESTRICT="!test? ( test )"
 
 BDEPEND="app-text/asciidoc
@@ -55,7 +55,4 @@ src_install() {
 	default
 
 	find "${ED}" -type f -name "*.la" -delete || die
-	if ! use static-libs ; then
-		find "${ED}" -type f -name "*.a" -delete || die
-	fi
 }