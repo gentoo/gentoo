gentoo/gentoo

commit 23bede4dba7eb9a84042110a0418ec8fd39ac442

sys-apps/s390-tools: version bump to 1.36.1

sys-apps/s390-tools/Manifest
@@ -2,3 +2,4 @@ DIST e2fsprogs-1.41.3.tar.gz 4365234 SHA256 ce77edae680d090cb8275eca9be282bdbc5f
 DIST linux-2.6.27.tar.bz2 50355835 SHA256 0e99bf9e83f4d1ae0c656741a84dfddfa9c4d2469cf35475f5939d02dc3e4393 SHA512 fcafcea4148e15ffacb57e4eef731d8a2655bba15c6445a84f7345579bd6b6b9a68ad1a44a58f78e78a619efd84ce79c63ce3de3931ae7857af647a06ac052e2 WHIRLPOOL 537aed46a0e37dbc90605f8bfcf05895933d384e1db9d7bbd3a9054d2867569749446f677e55ffd839677cbd04f6ca0eec06cc7eee9c6e63e7554e520d83fe5d
 DIST s390-tools-1.17.0.tar.bz2 608123 SHA256 4026a93851771c957a880f331ca73d469c171777ed8e79d3027a0877b3220112 SHA512 5d5b82f56ff0990c42b1db15115db5b6924680c7985e20572232ae5f85438b6c28bd25502d162f718b07e24d6837ec3bd15dd5b38a783692d2ead48c4fd2af92 WHIRLPOOL ae885ff376313afecc48159fa952b126e606408c1cb299959763be0d093e7cec8c45bd72b265e819143c131e735ab5c13e7d31f3039c9359da4d0fa52a014c46
 DIST s390-tools-1.23.1.tar.bz2 625295 SHA256 11175b1d7d0c10832dc0295880656f9e10866f004c07770379052a9f7c6ffd8f SHA512 c1f0fe79cfcbf6217e1aba74172e7644eee25f8886b70885e8558ad48342d6b41b35cefd398c3f5ff430d382ea6103b93ed8f666f0038d4f5abc3c3bc787d0eb WHIRLPOOL 501e0233a1f463bd156b2ab199bc75a30fccb4630c62654c4769eb5592e88735bfb718eeaae22b130e380b91b0c16e28f526763263cebbb8ccc45e0fca350fd8
+DIST s390-tools-1.36.1.tar.bz2 827505 SHA256 010bfa8af71e1df6d9019891bde2b5067cc2b7786374f60541c1b1f71ca9e675 SHA512 83b3456bce8af0d5e1f6a1b96335809a21b676b2371ca101e5f588a18e0de65b8e15a9a973a7dcc2cad800341ecf5997e8230a68b9670751cdf1d6f8d59be22c WHIRLPOOL c38653d81dd11b3b14963185e9b59191d375b3d0aa4b7aed1539141027380f44b677fae12cc47bf79b9c4d4e767b1589c6f7fa57a10316d532ab2ad4efc4f6d2

sys-apps/s390-tools/metadata.xml
@@ -11,9 +11,8 @@ zSeries (s390) Linux kernel and device drivers
 	<use>
 		<flag name="fuse">build cmsfs-fuse to read files stored on a z/VM CMS disk</flag>
 		<flag name="ncurses">build hyptop monitoring program</flag>
+		<flag name="pfm">build the cpacfstats tool</flag>
 		<flag name="zfcpdump">build the kernel disk dumping utility</flag>
+		<flag name="zlib">build the zgetdump utility</flag>
 	</use>
-	<upstream>
-		<remote-id type="sourceforge">e2fsprogs</remote-id>
-	</upstream>
 </pkgmetadata>

sys-apps/s390-tools/s390-tools-1.36.1.ebuild
@@ -1,0 +1,51 @@
+# Copyright 1999-2016 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI="5"
+
+inherit toolchain-funcs udev
+
+DESCRIPTION="User space utilities for the zSeries (s390) Linux kernel and device drivers"
+HOMEPAGE="http://www.ibm.com/developerworks/linux/linux390/s390-tools.html"
+SRC_URI="http://download.boulder.ibm.com/ibmdl/pub/software/dw/linux390/ht_src/${P}.tar.bz2"
+
+LICENSE="GPL-2"
+SLOT="0"
+KEYWORDS="-* ~s390"
+IUSE="fuse ncurses pfm snmp zlib"
+
+RDEPEND="fuse? ( sys-fs/fuse )
+	ncurses? ( sys-libs/ncurses:0= )
+	pfm? ( app-misc/pfm )
+	snmp? ( net-analyzer/net-snmp )
+	zlib? ( sys-libs/zlib )"
+DEPEND="${RDEPEND}
+	dev-util/indent
+	app-admin/genromfs"
+
+src_configure() {
+	export MAKEOPTS+=" V=1"
+	export HAVE_DRACUT=0
+	export HAVE_FUSE=$(usex fuse 1 0)
+	export HAVE_NCURSES=$(usex ncurses 1 0)
+	export HAVE_SNMP=$(usex snmp 1 0)
+	export HAVE_PFM=$(usex pfm 1 0)
+	export HAVE_ZLIB=$(usex zlib 1 0)
+	tc-export AR BUILD_CC CC CXX LD NM OBJCOPY
+}
+
+src_compile() {
+	emake \
+		AR="${AR}" \
+		HOSTCC="${BUILD_CC}" \
+		CC="${CC}" LINK="${CC}" \
+		CXX="${CXX}" LINKXX="${CXX}" \
+		LD="${LD}" \
+		NM="${NM}" \
+		OBJCOPY="${OBJCOPY}"
+}
+
+src_install() {
+	default
+	udev_dorules etc/udev/rules.d/*.rules
+}

commit ff147ad296de6982fb1b12b7f4e5cfe00cf83649

sys-apps/s390-tools: add 2.40.0
* Make various deps disabled rather than automagic, including new Rust
  support. Can revisit this...

* USE=pfm seems wrong, we want dev-libs/libpfm for that, introduced back
  in 2016. Fixed accordingly.

* Fixes build w/ GCC 15 (inline asm needed updating).

Bug: https://bugs.gentoo.org/967616
Bug: https://bugs.gentoo.org/967675
Fixes: 23bede4dba7eb9a84042110a0418ec8fd39ac442
Signed-off-by: Sam James <sam@gentoo.org>

sys-apps/s390-tools/Manifest
@@ -1,3 +1,4 @@
 DIST s390-tools-2.12.0.tar.gz 1186816 BLAKE2B bdf7975d236ec939539221ba548d67582836ae1ef8fab29ff19ac32596ba60686d41db47412bfe7a86d74a1728e3ee41356403ba21557cec4400a2aae98165df SHA512 4903ded5368966513e1090faca04482316870cf5fc53206d17ab98b821541e2836a016bd18cd1ca49ed6b5df0dff92dd1ed257ff712920a7aa3fd74e4a6bc6a0
 DIST s390-tools-2.27.0.tar.gz 1848002 BLAKE2B 5b71cffee4eefeb834eed4e34abc1d1c3e8d8df4e7862f0af5d0b1b26172060168059569f6ac27a0de1d28e0bf3155cd9d789098b12b8b11b69c4858d9def3a2 SHA512 8ec83718639d17fe7b1990f4e492f1e1bdd11e814b7838c921733eeccb212f417141042118eadf1e8db498e3ff2ce0c8d404189e436cefe17d9a3dacea22429f
+DIST s390-tools-2.40.0.tar.gz 2279464 BLAKE2B 9aeb5316d36abb2317a1ffc4053f98d77b4964f7441c4170b28577860e517cf71f156fbdc47e482cefe53a9fabd8f1feff12015546d77b460c64840b57bfa2e1 SHA512 1943429581b280bf9ea370da3700a234d509837cb45b2354cdf79b38db87d2183010d18d9132defd77b9d8e11b7e002c447ffd7c912e20a50da4cb6877758c0e
 DIST s390-tools-2.9.0.tar.gz 1162090 BLAKE2B a86b8b1859d125f476a96546163881723cf87536a654871269c23ec6e72266776083f5d49bff3c65c2a01af7de985d2079cc9504a605e3d42de297a464b8ee69 SHA512 23315e8914a5c797b0678298ff51fd95a3c9f065b0fa9f6cf954573fd5ad394890d7f8dbd9cf2501d845c76c3fe31952b25cb7c2b2d59744305b2f37cf3a65bb

sys-apps/s390-tools/s390-tools-2.40.0.ebuild
@@ -1,0 +1,86 @@
+# Copyright 1999-2025 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit toolchain-funcs udev
+
+DESCRIPTION="User space utilities for the zSeries (s390) Linux kernel and device drivers"
+HOMEPAGE="https://github.com/ibm-s390-tools/s390-tools"
+SRC_URI="https://github.com/ibm-${PN}/${PN}/archive/v${PV}.tar.gz -> ${P}.tar.gz"
+
+LICENSE="MIT"
+SLOT="0"
+KEYWORDS="-* ~s390"
+IUSE="abi_s390_32 cryptsetup curl fuse ncurses openssl pfm snmp zlib"
+
+RDEPEND="
+	curl? ( net-misc/curl )
+	fuse? ( sys-fs/fuse:3= )
+	ncurses? ( sys-libs/ncurses:= )
+	openssl? (
+		dev-libs/openssl:=
+		cryptsetup? (
+			>=sys-fs/cryptsetup-2.0.3:=
+			dev-libs/json-c:=
+		)
+	)
+	pfm? ( dev-libs/libpfm:= )
+	snmp? ( net-analyzer/net-snmp:= )
+	zlib? ( virtual/zlib:= )
+"
+DEPEND="${RDEPEND}"
+BDEPEND="
+	app-admin/genromfs
+	virtual/pkgconfig
+"
+
+src_prepare() {
+	default
+	sed -i -e 's/-lncurses/-lncurses -ltinfo/' "${S}"/hyptop/Makefile || die
+
+	# zipl only builds on 64bit
+	if use abi_s390_32 ; then
+		sed -i -e 's/^TOOL_DIRS = zipl /TOOL_DIRS = /' "${S}"/Makefile || die
+	fi
+}
+
+src_configure() {
+	# https://github.com/ibm-s390-linux/s390-tools/tree/master?tab=readme-ov-file#dependencies
+	export MAKEOPTS+=" V=1"
+	export HAVE_DRACUT=0
+	export HAVE_CARGO=0
+	export HAVE_FUSE=$(usex fuse 1 0)
+	export HAVE_NCURSES=$(usex ncurses 1 0)
+	export HAVE_SNMP=$(usex snmp 1 0)
+	export HAVE_PFM=$(usex pfm 1 0)
+	export HAVE_ZLIB=$(usex zlib 1 0)
+	export HAVE_OPENSSL=$(usex openssl 1 0)
+	export HAVE_CRYPTSETUP2=$(usex cryptsetup 1 0)
+	export HAVE_JSONC=$(usex cryptsetup 1 0)
+	export HAVE_LIBCURL=$(usex curl 1 0)
+	# These need checking, but disabled for now to avoid automagic
+	export HAVE_GLIB2=0
+	export HAVE_LIBXML2=0
+	export HAVE_SYSTEMD=0
+	export HAVE_LIBUDEV=0
+	export HAVE_LIBNL3=0
+	tc-export AR BUILD_CC CC CXX LD NM OBJCOPY
+}
+
+src_compile() {
+	emake \
+		AR="${AR}" \
+		HOSTCC="${BUILD_CC}" \
+		CC="${CC}" LINK="${CC}" \
+		CXX="${CXX}" LINKXX="${CXX}" \
+		LD="${LD}" \
+		NM="${NM}" \
+		OBJCOPY="${OBJCOPY}"
+}
+
+src_install() {
+	default
+	udev_dorules etc/udev/rules.d/*.rules
+	keepdir /var/log/ts-shell
+}