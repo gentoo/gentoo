gentoo/gentoo

commit a628990882113048762734368940f0c98682b8b1

media-libs/quirc: build only the library for non-native-abi
Bug: https://bugs.gentoo.org/941260
Signed-off-by: Paul Zander <negril.nx+gentoo@gmail.com>
Signed-off-by: Sam James <sam@gentoo.org>

media-libs/quirc/quirc-1.2-r3.ebuild
@@ -8,7 +8,7 @@ HOMEPAGE="https://github.com/dlbeer/quirc"
 
 inherit flag-o-matic multilib-minimal toolchain-funcs
 
-if [[ ${PV} = *9999* ]] ; then
+if [[ "${PV}" == *9999* ]] ; then
 	inherit git-r3
 	EGIT_REPO_URI="https://github.com/dlbeer/${PN}.git"
 else
@@ -19,11 +19,24 @@ else
 fi
 
 DEPEND="
-	media-libs/libjpeg-turbo:=[${MULTILIB_USEDEP}]
-	media-libs/libpng:=[${MULTILIB_USEDEP}]
-	media-libs/libsdl:=[${MULTILIB_USEDEP}]
-	opencv? ( media-libs/opencv:=[${MULTILIB_USEDEP}] )
-	sdl? ( media-libs/sdl-gfx:=[${MULTILIB_USEDEP}] )
+	opencv? (
+		media-libs/libjpeg-turbo:=
+		media-libs/libpng:=
+		media-libs/opencv:=
+	)
+	sdl? (
+		media-libs/libjpeg-turbo:=
+		media-libs/libpng:=
+		media-libs/libsdl:=
+		media-libs/sdl-gfx:=
+	)
+	tools? (
+		media-libs/libjpeg-turbo:=
+		media-libs/libpng:=
+	)
+	v4l? (
+		media-libs/libjpeg-turbo:=
+	)
 "
 RDEPEND="
 	${DEPEND}
@@ -35,38 +48,58 @@ SLOT="0/${PV}"
 IUSE="opencv sdl tools v4l"
 
 src_prepare() {
-	LIB_VERSION=$(grep '^LIB_VERSION = ' "${S}/Makefile" | cut -d ' ' -f 3 || die)
+	read -r LIB_VERSION <<< "$(grep '^LIB_VERSION = ' "${S}/Makefile" | cut -d ' ' -f 3 || die)"
+	export LIB_VERSION
+
 	sed -r \
 		-e "s#\.o libquirc.a#.o libquirc.so.${LIB_VERSION}#g" \
 		-e '/^QUIRC_CFLAGS/ s/$/ -fPIC/' \
 		-i Makefile || die
 
 	default
+
 	multilib_copy_sources
 }
 
 multilib_src_configure() {
 	tc-export CC CXX
-
-	targets=( libquirc.so )
-	use opencv && targets+=( opencv )
-	use sdl && targets+=( sdl )
-	use tools && targets+=( qrtest )
-	use v4l && targets+=( v4l )
 }
 
 multilib_src_compile() {
 	append-ldflags "-Wl,-soname,libquirc.so.${LIB_VERSION}"
+
+	local targets=( "libquirc.so" )
+
+	if multilib_is_native_abi; then
+		if use opencv; then
+			targets+=( "opencv" )
+		fi
+
+		if use sdl; then
+			targets+=( "sdl" )
+		fi
+
+		if use tools; then
+			targets+=( "qrtest" )
+		fi
+
+		if use v4l; then
+			targets+=( "v4l" )
+		fi
+	fi
+
 	emake V=1 DESTDIR="${D}" PREFIX="${EPREFIX}/usr" "${targets[@]}"
 }
 
 multilib_src_install() {
 	dolib.so "libquirc.so.${LIB_VERSION}"
+
 	dosym "libquirc.so.${LIB_VERSION}" "${EPREFIX}/usr/$(get_libdir)/libquirc.so"
 	dosym "libquirc.so.${LIB_VERSION}" "${EPREFIX}/usr/$(get_libdir)/libquirc.so.$(ver_cut 1 "${LIB_VERSION}")"
 
 	if multilib_is_native_abi; then
 		into "/usr/libexec/${PN}"
+
 		if use opencv; then
 			dobin inspect-opencv
 			dobin quirc-demo-opencv

media-libs/quirc/quirc-9999.ebuild
@@ -8,22 +8,35 @@ HOMEPAGE="https://github.com/dlbeer/quirc"
 
 inherit flag-o-matic multilib-minimal toolchain-funcs
 
-if [[ ${PV} = *9999* ]] ; then
+if [[ "${PV}" == *9999* ]] ; then
 	inherit git-r3
 	EGIT_REPO_URI="https://github.com/dlbeer/${PN}.git"
 else
 	SRC_URI="
 		https://github.com/dlbeer/${PN}/archive/refs/tags/v${PV}.tar.gz -> ${P}.tar.gz
 	"
-	KEYWORDS="~amd64 ~x86"
+	KEYWORDS="~amd64 ~arm ~arm64 ~loong ~ppc ~ppc64 ~riscv ~sparc ~x86"
 fi
 
 DEPEND="
-	media-libs/libjpeg-turbo:=[${MULTILIB_USEDEP}]
-	media-libs/libpng:=[${MULTILIB_USEDEP}]
-	media-libs/libsdl:=[${MULTILIB_USEDEP}]
-	opencv? ( media-libs/opencv:=[${MULTILIB_USEDEP}] )
-	sdl? ( media-libs/sdl-gfx:=[${MULTILIB_USEDEP}] )
+	opencv? (
+		media-libs/libjpeg-turbo:=
+		media-libs/libpng:=
+		media-libs/opencv:=
+	)
+	sdl? (
+		media-libs/libjpeg-turbo:=
+		media-libs/libpng:=
+		media-libs/libsdl:=
+		media-libs/sdl-gfx:=
+	)
+	tools? (
+		media-libs/libjpeg-turbo:=
+		media-libs/libpng:=
+	)
+	v4l? (
+		media-libs/libjpeg-turbo:=
+	)
 "
 RDEPEND="
 	${DEPEND}
@@ -35,38 +48,58 @@ SLOT="0/${PV}"
 IUSE="opencv sdl tools v4l"
 
 src_prepare() {
-	LIB_VERSION=$(grep '^LIB_VERSION = ' "${S}/Makefile" | cut -d ' ' -f 3 || die)
+	read -r LIB_VERSION <<< "$(grep '^LIB_VERSION = ' "${S}/Makefile" | cut -d ' ' -f 3 || die)"
+	export LIB_VERSION
+
 	sed -r \
 		-e "s#\.o libquirc.a#.o libquirc.so.${LIB_VERSION}#g" \
 		-e '/^QUIRC_CFLAGS/ s/$/ -fPIC/' \
 		-i Makefile || die
 
 	default
+
 	multilib_copy_sources
 }
 
 multilib_src_configure() {
 	tc-export CC CXX
-
-	targets=( libquirc.so )
-	use opencv && targets+=( opencv )
-	use sdl && targets+=( sdl )
-	use tools && targets+=( qrtest )
-	use v4l && targets+=( v4l )
 }
 
 multilib_src_compile() {
 	append-ldflags "-Wl,-soname,libquirc.so.${LIB_VERSION}"
+
+	local targets=( "libquirc.so" )
+
+	if multilib_is_native_abi; then
+		if use opencv; then
+			targets+=( "opencv" )
+		fi
+
+		if use sdl; then
+			targets+=( "sdl" )
+		fi
+
+		if use tools; then
+			targets+=( "qrtest" )
+		fi
+
+		if use v4l; then
+			targets+=( "v4l" )
+		fi
+	fi
+
 	emake V=1 DESTDIR="${D}" PREFIX="${EPREFIX}/usr" "${targets[@]}"
 }
 
 multilib_src_install() {
 	dolib.so "libquirc.so.${LIB_VERSION}"
+
 	dosym "libquirc.so.${LIB_VERSION}" "${EPREFIX}/usr/$(get_libdir)/libquirc.so"
 	dosym "libquirc.so.${LIB_VERSION}" "${EPREFIX}/usr/$(get_libdir)/libquirc.so.$(ver_cut 1 "${LIB_VERSION}")"
 
 	if multilib_is_native_abi; then
 		into "/usr/libexec/${PN}"
+
 		if use opencv; then
 			dobin inspect-opencv
 			dobin quirc-demo-opencv

commit 185270c19f404148d4a21f53f9212ca9138ade85

media-libs/quirc: allow sdl'less build
Bug: https://bugs.gentoo.org/941260
Fixes: a628990882113048762734368940f0c98682b8b1
See-Also: 8b5c597a0b9d6b174264227f1d7a4b54fc5551e2
Signed-off-by: Alfred Wingate <parona@protonmail.com>

media-libs/quirc/files/quirc-1.2-allow-sdl-less-build.patch
@@ -1,0 +1,51 @@
+https://bugs.gentoo.org/941260
+https://github.com/dlbeer/quirc/pull/149
+
+Note: This will mask errors for pkg-config sdl.
+
+From 39a51ffb71173803f158a9f91234b40ea0d43269 Mon Sep 17 00:00:00 2001
+From: YAMAMOTO Takashi <yamamoto@midokura.com>
+Date: Sun, 2 Feb 2025 11:09:19 +0900
+Subject: [PATCH] Ignore pkg-config error messages
+
+To avoid errors like the following:
+
+```
+cc -Ilib -O3 -Wall -fPIC Package sdl was not found in the pkg-config search path. Perhaps you should add the directory containing `sdl.pc' to the PKG_CONFIG_PATH environment variable Package 'sdl' not found -o lib/decode.o -c lib/decode.c
+/bin/sh: -c: line 0: unexpected EOF while looking for matching ``'
+/bin/sh: -c: line 1: syntax error: unexpected end of file
+gmake: *** [Makefile:90: lib/decode.o] Error 2
+```
+
+This (at least the SDL part) was neccessary on my environment:
+
+* macOS 15.2
+* Xcode 16.2
+* pkg-config 2.3.0 (homebrew)
+* GNU make 4.4.1 (homebrew)
+* opencv 4.11.0 (homebrew)
+* no SDL installed
+--- a/Makefile
++++ b/Makefile
+@@ -15,8 +15,8 @@
+ 
+ CC ?= gcc
+ PREFIX ?= /usr/local
+-SDL_CFLAGS := $(shell pkg-config --cflags sdl 2>&1)
+-SDL_LIBS = $(shell pkg-config --libs sdl)
++SDL_CFLAGS := $(shell pkg-config --cflags sdl 2> /dev/null)
++SDL_LIBS = $(shell pkg-config --libs sdl 2> /dev/null)
+ 
+ LIB_VERSION = 1.2
+ 
+@@ -43,8 +43,8 @@ DEMO_UTIL_OBJ = \
+     demo/dthash.o \
+     demo/demoutil.o
+ 
+-OPENCV_CFLAGS := $(shell pkg-config --cflags opencv4 2>&1)
+-OPENCV_LIBS = $(shell pkg-config --libs opencv4)
++OPENCV_CFLAGS := $(shell pkg-config --cflags opencv4 2> /dev/null)
++OPENCV_LIBS = $(shell pkg-config --libs opencv4 2> /dev/null)
+ QUIRC_CXXFLAGS = $(QUIRC_CFLAGS) $(OPENCV_CFLAGS) --std=c++17
+ 
+ .PHONY: all v4l sdl opencv install uninstall clean

media-libs/quirc/quirc-1.2-r3.ebuild
@@ -47,6 +47,10 @@ SLOT="0/${PV}"
 
 IUSE="opencv sdl tools v4l"
 
+PATCHES=(
+	"${FILESDIR}/quirc-1.2-allow-sdl-less-build.patch"
+)
+
 src_prepare() {
 	read -r LIB_VERSION <<< "$(grep '^LIB_VERSION = ' "${S}/Makefile" | cut -d ' ' -f 3 || die)"
 	export LIB_VERSION

media-libs/quirc/quirc-9999.ebuild
@@ -47,6 +47,10 @@ SLOT="0/${PV}"
 
 IUSE="opencv sdl tools v4l"
 
+PATCHES=(
+	"${FILESDIR}/quirc-1.2-allow-sdl-less-build.patch"
+)
+
 src_prepare() {
 	read -r LIB_VERSION <<< "$(grep '^LIB_VERSION = ' "${S}/Makefile" | cut -d ' ' -f 3 || die)"
 	export LIB_VERSION