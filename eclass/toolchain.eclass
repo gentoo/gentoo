gentoo/gentoo

commit 46191b478ebcb6bc6831627526da44f04dfcd1be

toolchain.eclass: default to -mtls-dialect=gnu2 with GCC 16 on x86_64
Bug: https://gcc.gnu.org/PR120933
Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -1581,6 +1581,10 @@ toolchain_src_configure() {
 				--enable-__cxa_atexit
 				--enable-clocale=gnu
 			)
+
+			if [[ ${CTARGET} == *linux* ]] && tc_version_is_at_least 16.0.0_p20251214 ${PV} ; then
+				confgcc+=( --with-tls=gnu2 )
+			fi
 			;;
 		*-solaris*)
 			confgcc+=( --enable-__cxa_atexit )

commit 088f602f72e1ac0fa7503fba3c9b70c1f7ea4d59

toolchain.eclass: only pass --with-tls for x86_64 as intended
Fixes: 46191b478ebcb6bc6831627526da44f04dfcd1be
Signed-off-by: Sam James <sam@gentoo.org>

eclass/toolchain.eclass
@@ -1582,7 +1582,7 @@ toolchain_src_configure() {
 				--enable-clocale=gnu
 			)
 
-			if [[ ${CTARGET} == *linux* ]] && tc_version_is_at_least 16.0.0_p20251214 ${PV} ; then
+			if [[ ${CTARGET} == x86_64*-*-linux-* ]] && tc_version_is_at_least 16.0.0_p20251214 ${PV} ; then
 				confgcc+=( --with-tls=gnu2 )
 			fi
 			;;