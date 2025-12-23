gentoo/gentoo

commit 65a9d253fdf9c5d58af13e9f1cde8775ca1a432f

app-editors/gvim: fix automagic Wayland (and X on gtk)
USE=X only presently does something if USE=-motif.

Bug: https://bugs.gentoo.org/966502
Signed-off-by: Sam James <sam@gentoo.org>

app-editors/gvim/gvim-9.1.1652-r2.ebuild
@@ -33,7 +33,7 @@ S="${WORKDIR}"/vim-${PV}
 
 LICENSE="vim"
 SLOT="0"
-IUSE="acl crypt cscope debug lua minimal motif netbeans nls perl python racket ruby selinux session sound tcl ${GENTOO_PERL_USESTRING}"
+IUSE="acl crypt cscope debug lua minimal motif netbeans nls perl python racket ruby selinux session sound tcl wayland X ${GENTOO_PERL_USESTRING}"
 REQUIRED_USE="
 	lua? ( ${LUA_REQUIRED_USE} )
 	python? ( ${PYTHON_REQUIRED_USE} )
@@ -50,7 +50,7 @@ RDEPEND="
 	acl? ( kernel_linux? ( sys-apps/acl ) )
 	motif? ( >=x11-libs/motif-2.3:0 )
 	!motif? (
-		x11-libs/gtk+:3
+		x11-libs/gtk+:3[X?]
 		x11-libs/libXft
 	)
 	crypt? ( dev-libs/libsodium:= )
@@ -71,6 +71,7 @@ RDEPEND="
 	session? ( x11-libs/libSM )
 	sound? ( media-libs/libcanberra )
 	tcl? ( dev-lang/tcl:0= )
+	wayland? ( dev-libs/wayland )
 "
 DEPEND="${RDEPEND}
 	x11-base/xorg-proto"
@@ -206,6 +207,7 @@ src_configure() {
 		$(use_enable selinux)
 		$(use_enable session xsmp)
 		$(use_enable tcl tclinterp)
+		$(use_with wayland)
 	)
 
 	if use lua; then
@@ -229,6 +231,8 @@ src_configure() {
 		myconf+=( --enable-gtk3-check )
 		einfo "Building gvim with the gtk+-3 GUI"
 		myconf+=( --enable-gui=gtk3 )
+
+		use X || append-flags -DGENTOO_GTK_HIDE_X11
 	fi
 	echo ; echo
 

commit 18d80c61edb19387b5b3e58177daed1369df5a97

app-editors/gvim: always needs gtk+[X]
Closes: https://bugs.gentoo.org/967878
Fixes: 65a9d253fdf9c5d58af13e9f1cde8775ca1a432f
Signed-off-by: Sam James <sam@gentoo.org>

app-editors/gvim/gvim-9.1.1652-r2.ebuild
@@ -34,7 +34,7 @@ S="${WORKDIR}"/vim-${PV}
 
 LICENSE="vim"
 SLOT="0"
-IUSE="acl crypt cscope debug lua minimal motif netbeans nls perl python racket ruby selinux session sound tcl wayland X ${GENTOO_PERL_USESTRING}"
+IUSE="acl crypt cscope debug lua minimal motif netbeans nls perl python racket ruby selinux session sound tcl wayland ${GENTOO_PERL_USESTRING}"
 REQUIRED_USE="
 	lua? ( ${LUA_REQUIRED_USE} )
 	python? ( ${PYTHON_REQUIRED_USE} )
@@ -51,7 +51,7 @@ RDEPEND="
 	acl? ( kernel_linux? ( sys-apps/acl ) )
 	motif? ( >=x11-libs/motif-2.3:0 )
 	!motif? (
-		x11-libs/gtk+:3[X?]
+		x11-libs/gtk+:3[X]
 		x11-libs/libXft
 	)
 	crypt? ( dev-libs/libsodium:= )
@@ -232,8 +232,6 @@ src_configure() {
 		myconf+=( --enable-gtk3-check )
 		einfo "Building gvim with the gtk+-3 GUI"
 		myconf+=( --enable-gui=gtk3 )
-
-		use X || append-flags -DGENTOO_GTK_HIDE_X11
 	fi
 	echo ; echo
 

app-editors/gvim/gvim-9999.ebuild
@@ -34,7 +34,7 @@ S="${WORKDIR}"/vim-${PV}
 
 LICENSE="vim"
 SLOT="0"
-IUSE="acl crypt cscope debug lua minimal motif netbeans nls perl python racket ruby selinux session sound tcl wayland X ${GENTOO_PERL_USESTRING}"
+IUSE="acl crypt cscope debug lua minimal motif netbeans nls perl python racket ruby selinux session sound tcl wayland ${GENTOO_PERL_USESTRING}"
 REQUIRED_USE="
 	lua? ( ${LUA_REQUIRED_USE} )
 	python? ( ${PYTHON_REQUIRED_USE} )
@@ -51,7 +51,7 @@ RDEPEND="
 	acl? ( kernel_linux? ( sys-apps/acl ) )
 	motif? ( >=x11-libs/motif-2.3:0 )
 	!motif? (
-		x11-libs/gtk+:3[X?]
+		x11-libs/gtk+:3[X]
 		x11-libs/libXft
 	)
 	crypt? ( dev-libs/libsodium:= )
@@ -232,8 +232,6 @@ src_configure() {
 		myconf+=( --enable-gtk3-check )
 		einfo "Building gvim with the gtk+-3 GUI"
 		myconf+=( --enable-gui=gtk3 )
-
-		use X || append-flags -DGENTOO_GTK_HIDE_X11
 	fi
 	echo ; echo
 