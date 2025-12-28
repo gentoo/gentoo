gentoo/gentoo

commit 5f4f7d9b26107213e0ebd8f9bddd27a0d87f60fd

app-misc/jdupes: fix dev-libs/libjodycode minimum version
When building 1.30.0, I saw:
```
filestat.c: In function 'file_has_changed':
filestat.c:31:18: error: storage size of 's' isn't known
   31 |   struct JC_STAT s;
      |                  ^
filestat.c:41:7: error: implicit declaration of function 'jc_stat' [-Wimplicit-function-declaration]
   41 |   if (jc_stat(file->d_name, &s) != 0) return -2;
      |       ^~~~~~~
filestat.c:55:8: error: implicit declaration of function 'JC_S_ISLNK'; did you mean 'S_ISLNK'? [-Wimplicit-function-declaration]
   55 |   if ((JC_S_ISLNK(s.st_mode) > 0) ^ ISFLAG(file->flags, FF_IS_SYMLINK)) return 1;
      |        ^~~~~~~~~~
      |        S_ISLNK
filestat.c:31:18: warning: unused variable 's' [-Wunused-variable]
   31 |   struct JC_STAT s;
      |                  ^
filestat.c: In function 'getfilestats':
filestat.c:64:18: error: storage size of 's' isn't known
   64 |   struct JC_STAT s;
      |                  ^
```

Just crank the minimum version for all versions in-tree to the latest
stable dev-libs/libjodycode.

Signed-off-by: Sam James <sam@gentoo.org>

app-misc/jdupes/jdupes-9999.ebuild
@@ -1,4 +1,4 @@
-# Copyright 2021-2023 Gentoo Authors
+# Copyright 2021-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -20,7 +20,7 @@ SLOT="0"
 
 # Please keep a careful eye on the minimum libjoycode version! (Just pick
 # latest released at the time if necessary.)
-DEPEND=">=dev-libs/libjodycode-3.0"
+DEPEND=">=dev-libs/libjodycode-3.1.2"
 RDEPEND="${DEPEND}"
 
 IUSE="+dedupe hardened"

app-misc/jdupes/jdupes-1.28.0-r1.ebuild
@@ -13,14 +13,14 @@ if [[ "${PV}" == *9999 ]] ; then
 else
 	SRC_URI="https://codeberg.org/jbruchon/jdupes/archive/v${PV}.tar.gz -> ${P}.tar.gz"
 	S="${WORKDIR}"/${PN}
-	KEYWORDS="~amd64 ~arm64 ~riscv"
+	KEYWORDS="amd64 ~arm64 ~riscv"
 fi
 LICENSE="MIT"
 SLOT="0"
 
 # Please keep a careful eye on the minimum libjoycode version! (Just pick
 # latest released at the time if necessary.)
-DEPEND=">=dev-libs/libjodycode-3.1"
+DEPEND=">=dev-libs/libjodycode-3.1.2"
 RDEPEND="${DEPEND}"
 
 IUSE="+dedupe hardened"

app-misc/jdupes/jdupes-1.30.0-r1.ebuild
@@ -13,14 +13,14 @@ if [[ "${PV}" == *9999 ]] ; then
 else
 	SRC_URI="https://codeberg.org/jbruchon/jdupes/archive/v${PV}.tar.gz -> ${P}.tar.gz"
 	S="${WORKDIR}"/${PN}
-	KEYWORDS="amd64 ~arm64 ~riscv"
+	KEYWORDS="~amd64 ~arm64 ~riscv"
 fi
 LICENSE="MIT"
 SLOT="0"
 
 # Please keep a careful eye on the minimum libjoycode version! (Just pick
 # latest released at the time if necessary.)
-DEPEND=">=dev-libs/libjodycode-3.1"
+DEPEND=">=dev-libs/libjodycode-3.1.2"
 RDEPEND="${DEPEND}"
 
 IUSE="+dedupe hardened"

commit 15dd88f3886a173badc779c06a264708de4b77b5

app-misc/jdupes: crank min libjodycode for 1.30.0 further
Fixes: 5f4f7d9b26107213e0ebd8f9bddd27a0d87f60fd
Signed-off-by: Sam James <sam@gentoo.org>

app-misc/jdupes/jdupes-1.30.0-r2.ebuild
@@ -20,7 +20,7 @@ SLOT="0"
 
 # Please keep a careful eye on the minimum libjoycode version! (Just pick
 # latest released at the time if necessary.)
-DEPEND=">=dev-libs/libjodycode-3.1.2"
+DEPEND=">=dev-libs/libjodycode-4.0.1"
 RDEPEND="${DEPEND}"
 
 IUSE="+dedupe hardened"

app-misc/jdupes/jdupes-9999.ebuild
@@ -20,7 +20,7 @@ SLOT="0"
 
 # Please keep a careful eye on the minimum libjoycode version! (Just pick
 # latest released at the time if necessary.)
-DEPEND=">=dev-libs/libjodycode-3.1.2"
+DEPEND=">=dev-libs/libjodycode-4.0.1"
 RDEPEND="${DEPEND}"
 
 IUSE="+dedupe hardened"