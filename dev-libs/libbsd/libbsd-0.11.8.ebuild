gentoo/gentoo

commit 364bead27d48aee14736e76d03c3ce6f72f73e9f

dev-libs/libbsd: use dot-a.eclass
... to avoid installing broken static libraries w/ LTO.

Signed-off-by: Eli Schwartz <eschwartz@gentoo.org>

dev-libs/libbsd/libbsd-0.11.8.ebuild
@@ -4,7 +4,7 @@
 EAPI=8
 
 VERIFY_SIG_OPENPGP_KEY_PATH=/usr/share/openpgp-keys/guillemjover.asc
-inherit flag-o-matic libtool multilib multilib-minimal verify-sig
+inherit dot-a flag-o-matic libtool multilib multilib-minimal verify-sig
 
 DESCRIPTION="Library to provide useful functions commonly found on BSD systems"
 HOMEPAGE="https://libbsd.freedesktop.org/wiki/ https://gitlab.freedesktop.org/libbsd/libbsd"
@@ -29,6 +29,8 @@ src_prepare() {
 }
 
 multilib_src_configure() {
+	lto-guarantee-fat
+
 	# Broken (still) with lld-17 (bug #922342, bug #915068)
 	append-ldflags $(test-flags-CCLD -Wl,--undefined-version)
 
@@ -43,6 +45,8 @@ multilib_src_configure() {
 
 multilib_src_install() {
 	emake DESTDIR="${D}" install
+	# always strip due to libbsd-ctor.a
+	strip-lto-bytecode #"${ED}"
 
 	find "${ED}" -type f -name "*.la" -delete || die
 

dev-libs/libbsd/libbsd-0.12.1.ebuild
@@ -4,7 +4,7 @@
 EAPI=8
 
 VERIFY_SIG_OPENPGP_KEY_PATH=/usr/share/openpgp-keys/guillemjover.asc
-inherit flag-o-matic libtool multilib multilib-minimal verify-sig
+inherit dot-a flag-o-matic libtool multilib multilib-minimal verify-sig
 
 DESCRIPTION="Library to provide useful functions commonly found on BSD systems"
 HOMEPAGE="https://libbsd.freedesktop.org/wiki/ https://gitlab.freedesktop.org/libbsd/libbsd"
@@ -30,6 +30,8 @@ src_prepare() {
 }
 
 multilib_src_configure() {
+	lto-guarantee-fat
+
 	# bug #911726
 	filter-flags -fno-semantic-interposition
 
@@ -41,6 +43,8 @@ multilib_src_configure() {
 
 multilib_src_install() {
 	emake DESTDIR="${D}" install
+	# always strip due to libbsd-ctor.a
+	strip-lto-bytecode #"${ED}"
 
 	find "${ED}" -type f -name "*.la" -delete || die
 

commit 31d2a94eac6e785ae20e003c849ed66f2f3a90bc

dev-libs/libbsd: fix erroneous dot-a.eclass commented-out usage
I have no idea where the comment char came from but it totally defeats
the purpose of using it.

Fixes: 364bead27d48aee14736e76d03c3ce6f72f73e9f
Closes: https://bugs.gentoo.org/962783
Signed-off-by: Eli Schwartz <eschwartz@gentoo.org>

dev-libs/libbsd/libbsd-0.11.8.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -46,7 +46,7 @@ multilib_src_configure() {
 multilib_src_install() {
 	emake DESTDIR="${D}" install
 	# always strip due to libbsd-ctor.a
-	strip-lto-bytecode #"${ED}"
+	strip-lto-bytecode "${ED}"
 
 	find "${ED}" -type f -name "*.la" -delete || die
 

dev-libs/libbsd/libbsd-0.12.1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -44,7 +44,7 @@ multilib_src_configure() {
 multilib_src_install() {
 	emake DESTDIR="${D}" install
 	# always strip due to libbsd-ctor.a
-	strip-lto-bytecode #"${ED}"
+	strip-lto-bytecode "${ED}"
 
 	find "${ED}" -type f -name "*.la" -delete || die
 