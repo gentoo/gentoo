gentoo/gentoo

commit ed14b872cd0d005d49a58bf15e9f616adf80b34e

profiles/arch: powerpc, sparc: mask media-video/pipewire[libcamera]
Too many unkeyworded dependencies, including libyuv which IIRC is broken
on BE at least.

Bug: https://bugs.gentoo.org/968238
Signed-off-by: Sam James <sam@gentoo.org>

media-video/pipewire/pipewire-1.4.9-r1.ebuild
@@ -52,7 +52,7 @@ else
 		PIPEWIRE_DOCS_USEFLAG="man"
 	fi
 
-	KEYWORDS="amd64 arm arm64 ~loong ~mips ppc ppc64 ~riscv ~sparc x86"
+	KEYWORDS="amd64 arm arm64 ~loong ~mips ~ppc ~ppc64 ~riscv ~sparc x86"
 fi
 
 SRC_URI+=" https://dev.gentoo.org/~sam/distfiles/${CATEGORY}/${PN}/${PN}-1.4.7-0001-don-t-include-standard-C-headers-inside-of-extern-C.patch.xz"

media-video/pipewire/pipewire-1.4.9-r3.ebuild
@@ -52,7 +52,7 @@ else
 		PIPEWIRE_DOCS_USEFLAG="man"
 	fi
 
-	KEYWORDS="~amd64 ~arm64 ~loong ~mips"
+	KEYWORDS="~amd64 ~arm64 ~loong ~mips ~ppc ~ppc64 ~sparc"
 fi
 
 DESCRIPTION="Multimedia processing graphs"

profiles/arch/powerpc/package.use.mask
@@ -1,5 +1,9 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
+
+# Sam James <sam@gentoo.org> (2026-01-16)
+# Unkeyworded dependencies (bug #968238)
+media-video/pipewire libcamera
 
 # Alfred Wingate <parona@protonmail.com> (2025-12-24)
 # media-libs/jasper is not keyworded here

profiles/arch/sparc/package.use.mask
@@ -1,5 +1,9 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
+
+# Sam James <sam@gentoo.org> (2026-01-16)
+# Unkeyworded dependencies (bug #968238)
+media-video/pipewire libcamera
 
 # Lukas Schmelting <lschmelting@posteo.com> (2025-11-09)
 # gnome-online-accounts is unkeyworded and in use.mask for sparc

commit b2b7c4d32021060d6eddffdf5f25d4a173cc8b28

media-video/pipewire: revert keywords changed with previous mask
Maybe it was meant to restore keywords in -r3, but the -r1
destabilization broke the tree. sam seems away right now so
reverting for now.

Bug: https://bugs.gentoo.org/968238
Fixes: ed14b872cd0d005d49a58bf15e9f616adf80b34e
Signed-off-by: Ionen Wolkens <ionen@gentoo.org>

media-video/pipewire/pipewire-1.4.9-r1.ebuild
@@ -52,7 +52,7 @@ else
 		PIPEWIRE_DOCS_USEFLAG="man"
 	fi
 
-	KEYWORDS="amd64 arm arm64 ~loong ~mips ~ppc ~ppc64 ~riscv ~sparc x86"
+	KEYWORDS="amd64 arm arm64 ~loong ~mips ppc ppc64 ~riscv ~sparc x86"
 fi
 
 SRC_URI+=" https://dev.gentoo.org/~sam/distfiles/${CATEGORY}/${PN}/${PN}-1.4.7-0001-don-t-include-standard-C-headers-inside-of-extern-C.patch.xz"

media-video/pipewire/pipewire-1.4.9-r3.ebuild
@@ -52,7 +52,7 @@ else
 		PIPEWIRE_DOCS_USEFLAG="man"
 	fi
 
-	KEYWORDS="~amd64 ~arm64 ~loong ~mips ~ppc ~ppc64 ~sparc"
+	KEYWORDS="~amd64 ~arm64 ~loong ~mips"
 fi
 
 DESCRIPTION="Multimedia processing graphs"