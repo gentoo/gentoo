gentoo/gentoo

commit f6d63c17137757f96d7a7bb4c202b8fdd7bbb1bf

net-misc/kea: wire up verify-sig
Signed-off-by: Sam James <sam@gentoo.org>

net-misc/kea/kea-3.0.2-r1.ebuild
@@ -4,7 +4,9 @@
 EAPI=8
 
 PYTHON_COMPAT=( python3_{11..14} )
-inherit eapi9-ver flag-o-matic meson python-r1 systemd tmpfiles toolchain-funcs
+VERIFY_SIG_OPENPGP_KEY_PATH=/usr/share/openpgp-keys/isc.asc
+inherit eapi9-ver flag-o-matic meson python-r1 systemd tmpfiles
+inherit toolchain-funcs verify-sig
 
 DESCRIPTION="High-performance production grade DHCPv4 & DHCPv6 server"
 HOMEPAGE="https://www.isc.org/kea/"
@@ -16,6 +18,7 @@ else
 	SRC_URI="
 		https://downloads.isc.org/isc/kea/${PV}/${P}.tar.xz
 		!doc? ( https://codeberg.org/peter1010/kea-manpages/archive/kea-manpages-${PV}.tar.gz )
+		verify-sig? ( https://downloads.isc.org/isc/kea/${PV}/${P}.tar.xz.asc )
 	"
 	KEYWORDS="amd64 arm arm64 ~x86"
 fi
@@ -50,15 +53,16 @@ RDEPEND="${COMMON_DEPEND}
 	acct-user/dhcp
 "
 BDEPEND="
+	${PYTHON_DEPS}
 	>=dev-build/meson-1.8
+	virtual/pkgconfig
 	doc? (
 		$(python_gen_any_dep '
 			dev-python/sphinx[${PYTHON_USEDEP}]
 			dev-python/sphinx-rtd-theme[${PYTHON_USEDEP}]
 		')
 	)
-	virtual/pkgconfig
-	${PYTHON_DEPS}
+	verify-sig? ( sec-keys/openpgp-keys-isc )
 "
 
 PATCHES=(
@@ -73,6 +77,12 @@ python_check_deps() {
 
 pkg_setup() {
 	python_setup
+}
+
+src_unpack() {
+	use verify-sig && verify-sig_verify_detached "${DISTDIR}"/${P}.tar.xz{,.asc}
+
+	default
 }
 
 src_prepare() {

net-misc/kea/kea-9999.ebuild
@@ -4,7 +4,9 @@
 EAPI=8
 
 PYTHON_COMPAT=( python3_{11..14} )
-inherit eapi9-ver flag-o-matic meson python-r1 systemd tmpfiles toolchain-funcs
+VERIFY_SIG_OPENPGP_KEY_PATH=/usr/share/openpgp-keys/isc.asc
+inherit eapi9-ver flag-o-matic meson python-r1 systemd tmpfiles
+inherit toolchain-funcs verify-sig
 
 DESCRIPTION="High-performance production grade DHCPv4 & DHCPv6 server"
 HOMEPAGE="https://www.isc.org/kea/"
@@ -13,8 +15,11 @@ if [[ ${PV} == 9999 ]]; then
 	inherit git-r3
 	EGIT_REPO_URI="https://gitlab.isc.org/isc-projects/kea.git"
 else
-	SRC_URI="https://downloads.isc.org/isc/kea/${PV}/${P}.tar.xz"
-	KEYWORDS="~amd64 ~arm ~arm64 ~x86"
+	SRC_URI="
+		https://downloads.isc.org/isc/kea/${PV}/${P}.tar.xz
+		verify-sig? ( https://downloads.isc.org/isc/kea/${PV}/${P}.tar.xz.asc )
+	"
+	KEYWORDS="amd64 arm arm64 ~x86"
 fi
 
 LICENSE="MPL-2.0"
@@ -47,15 +52,16 @@ RDEPEND="${COMMON_DEPEND}
 	acct-user/dhcp
 "
 BDEPEND="
+	${PYTHON_DEPS}
 	>=dev-build/meson-1.8
+	virtual/pkgconfig
 	doc? (
 		$(python_gen_any_dep '
 			dev-python/sphinx[${PYTHON_USEDEP}]
 			dev-python/sphinx-rtd-theme[${PYTHON_USEDEP}]
 		')
 	)
-	virtual/pkgconfig
-	${PYTHON_DEPS}
+	verify-sig? ( sec-keys/openpgp-keys-isc )
 "
 
 python_check_deps() {
@@ -66,6 +72,12 @@ python_check_deps() {
 
 pkg_setup() {
 	python_setup
+}
+
+src_unpack() {
+	use verify-sig && verify-sig_verify_detached "${DISTDIR}"/${P}.tar.xz{,.asc}
+
+	default
 }
 
 src_prepare() {

net-misc/kea/Manifest
@@ -1,2 +1,3 @@
 DIST kea-3.0.2.tar.xz 6634300 BLAKE2B 8c7dccbb118f09e0c4dbd0fbb1889fba03bda633bedd6b05f91445d0979bbbedd8c7150ceaad73a280e58401eba4671398c65717f747b661fa0891e310dac1ea SHA512 454081be248d6021aa99bfe027111f093795b123c827c6062e29a215856d29ec827f5757a1a6fc3351e74276563f101b52f26db2098cdd0b4e6f86e1b3449ba3
+DIST kea-3.0.2.tar.xz.asc 833 BLAKE2B 7345cfc7fb6bec6a9ec06f7d9861cc43b0b11bdc6e80acdd6016dc7677cbe0af659fb4cb0d62b6acfcf1c53e47e936400e75096abcd8d66b7dd5e6ef8b520b58 SHA512 0735968604d1ac0821f00a01e3f61134e118ec0fb26846eb6413867c855923a148316ef20022e9a51af8507711eeadaf68b2da85504332cf8c882e75fffa8793
 DIST kea-manpages-3.0.2.tar.gz 6743 BLAKE2B 30d254a0db2b635493a0f9fe5480cf731978532f860d9acf74f8d6114539f0f9e594aa68aeb1181ee05da51707fd35a900be72c79bc7abf31124c1c5ea439ef4 SHA512 cfcf91abfd34e44319d4ea58de15bc068e5c7be7c94f211e85890009ef285ab84ca310dbe4ba8879a4797574715086861755cd528ea90a65e88980adeabc43a1

commit 8f4faa115b24ad1b1c2b5ead4116acb78627c35a

net-misc/kea: fix live ebuild
Closes: https://bugs.gentoo.org/969053
Fixes: f6d63c17137757f96d7a7bb4c202b8fdd7bbb1bf
Signed-off-by: Sam James <sam@gentoo.org>

net-misc/kea/kea-3.0.2-r1.ebuild
@@ -80,7 +80,14 @@ pkg_setup() {
 }
 
 src_unpack() {
-	use verify-sig && verify-sig_verify_detached "${DISTDIR}"/${P}.tar.xz{,.asc}
+	if [[ ${PV} == 9999 ]] ; then
+		git-r3_src_unpack
+		return
+	fi
+
+	if use verify-sig; then
+		verify-sig_verify_detached "${DISTDIR}"/${P}.tar.xz{,.asc}
+	fi
 
 	default
 }

net-misc/kea/kea-9999.ebuild
@@ -75,7 +75,14 @@ pkg_setup() {
 }
 
 src_unpack() {
-	use verify-sig && verify-sig_verify_detached "${DISTDIR}"/${P}.tar.xz{,.asc}
+	if [[ ${PV} == 9999 ]] ; then
+		git-r3_src_unpack
+		return
+	fi
+
+	if use verify-sig; then
+		verify-sig_verify_detached "${DISTDIR}"/${P}.tar.xz{,.asc}
+	fi
 
 	default
 }