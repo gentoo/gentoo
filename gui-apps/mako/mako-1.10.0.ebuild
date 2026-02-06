gentoo/gentoo

commit bdb50e29bb15660c857c57fc61431073567587f7

gui-apps/mako: enable verify-sig
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

gui-apps/mako/mako-1.10.0.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -12,8 +12,10 @@ if [[ ${PV} == 9999 ]]; then
 	inherit git-r3
 	EGIT_REPO_URI="https://github.com/emersion/${PN}.git"
 else
-	SRC_URI="https://github.com/emersion/${PN}/archive/v${PV}.tar.gz -> ${P}.tar.gz"
-	KEYWORDS="amd64 arm64 ~ppc64 x86"
+	inherit verify-sig
+	SRC_URI="https://github.com/emersion/${PN}/releases/download/v${PV}/${P}.tar.gz -> ${P}.gh.tar.gz
+		https://github.com/emersion/${PN}/releases/download/v${PV}/${P}.tar.gz.sig -> ${P}.gh.tar.gz.sig"
+	KEYWORDS="~amd64 ~arm64 ~ppc64 ~x86"
 fi
 
 LICENSE="MIT"
@@ -43,6 +45,11 @@ BDEPEND="
 	dev-util/wayland-scanner
 	virtual/pkgconfig
 "
+
+if [[ ${PV} != 9999 ]]; then
+	BDEPEND+=" verify-sig? ( sec-keys/openpgp-keys-emersion )"
+	VERIFY_SIG_OPENPGP_KEY_PATH="/usr/share/openpgp-keys/emersion.asc"
+fi
 
 src_configure() {
 	local emesonargs=(

gui-apps/mako/mako-1.8.0.ebuild
@@ -12,8 +12,10 @@ if [[ ${PV} == 9999 ]]; then
 	inherit git-r3
 	EGIT_REPO_URI="https://github.com/emersion/${PN}.git"
 else
-	SRC_URI="https://github.com/emersion/${PN}/archive/v${PV}.tar.gz -> ${P}.tar.gz"
-	KEYWORDS="amd64 arm64 ~ppc64 x86"
+	inherit verify-sig
+	SRC_URI="https://github.com/emersion/${PN}/releases/download/v${PV}/${P}.tar.gz -> ${P}.gh.tar.gz
+		https://github.com/emersion/${PN}/releases/download/v${PV}/${P}.tar.gz.sig -> ${P}.gh.tar.gz.sig"
+	KEYWORDS="~amd64 ~arm64 ~ppc64 ~x86"
 fi
 
 LICENSE="MIT"
@@ -44,6 +46,11 @@ BDEPEND="
 	dev-util/wayland-scanner
 	virtual/pkgconfig
 "
+
+if [[ ${PV} != 9999 ]]; then
+	BDEPEND+=" verify-sig? ( sec-keys/openpgp-keys-emersion )"
+	VERIFY_SIG_OPENPGP_KEY_PATH="/usr/share/openpgp-keys/emersion.asc"
+fi
 
 src_configure() {
 	local emesonargs=(

gui-apps/mako/mako-1.9.0.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -12,8 +12,10 @@ if [[ ${PV} == 9999 ]]; then
 	inherit git-r3
 	EGIT_REPO_URI="https://github.com/emersion/${PN}.git"
 else
-	SRC_URI="https://github.com/emersion/${PN}/archive/v${PV}.tar.gz -> ${P}.tar.gz"
-	KEYWORDS="amd64 arm64 ~ppc64 x86"
+	inherit verify-sig
+	SRC_URI="https://github.com/emersion/${PN}/releases/download/v${PV}/${P}.tar.gz -> ${P}.gh.tar.gz
+		https://github.com/emersion/${PN}/releases/download/v${PV}/${P}.tar.gz.sig -> ${P}.gh.tar.gz.sig"
+	KEYWORDS="~amd64 ~arm64 ~ppc64 ~x86"
 fi
 
 LICENSE="MIT"
@@ -43,6 +45,11 @@ BDEPEND="
 	dev-util/wayland-scanner
 	virtual/pkgconfig
 "
+
+if [[ ${PV} != 9999 ]]; then
+	BDEPEND+=" verify-sig? ( sec-keys/openpgp-keys-emersion )"
+	VERIFY_SIG_OPENPGP_KEY_PATH="/usr/share/openpgp-keys/emersion.asc"
+fi
 
 src_configure() {
 	local emesonargs=(

gui-apps/mako/Manifest
@@ -1,3 +1,6 @@
-DIST mako-1.10.0.tar.gz 66644 BLAKE2B 35d43b9d7ec85fd2abcc2473efc7e6819a5074830558c637656c679c115e89fac8918427bf4e190d3e6c23527d22ff0d101df2c05c8340fe76eedfda3abcec47 SHA512 5621117377505468bf1e8d4eecc2dd37748acfc62d578c992e12a13c78bf681cf9d65d1159fd1fb236ec7e449a8115a170659055f2fa66f3c32d9a72cc03f640
-DIST mako-1.8.0.tar.gz 63143 BLAKE2B da39c1d2a1748649432d2760831cee7c9b135114b8e36c48a87900d9a3610fc5103c15f2ed511344d84a3142aa76ddd3680177318d0e096ad0574952632dc0b7 SHA512 d187c00296f3c3d4fb2c5964a95e1ac6ab74deb7dc27633d8e8301d60d90e4dc92b0ee66953670c42a3c08160536a4126ee747214c4ea05a30a80fd5e2004ad0
-DIST mako-1.9.0.tar.gz 63415 BLAKE2B 932f045cede44f79e4039a40118cdd4283413f86524d74703e84a3bf999af58d9ee605054480d369a33f81c7fcaed60415cb203fd32a22333adc1e88876f6a33 SHA512 da9c89dc698e44100960c9911c9da59e6f3dd5e95c8c0fbc489c9597dd614bb46074170b9621eba6ff692eff140f7be11ecc9da1a285b8e5c0975b7b53daabee
+DIST mako-1.10.0.gh.tar.gz 66796 BLAKE2B d357c2fdb67cfe89330981d69cdf83258dd345a19cf8e444d59cac0a3105eb0ce285d6fe35ed98c2b48cd91ba5f64cf659e101b93ca4357e5373819e8546af6e SHA512 e1c55cec033ce399748a29db450a39ee81cbe28a61d22fdbaa0e45f5dd454047643dd5a85ddb63c59004b20ba767f030edd6869b87f2520a17102b67d60c57e8
+DIST mako-1.10.0.gh.tar.gz.sig 566 BLAKE2B af2f83c8b55e8447cec0863765be2d7e5f4405da5bc915c740d26565045351015677526cc88fbdb701e24a6b11cb70d73d7f1c75a23c91da8ff79cbd051619ae SHA512 a552f72c028fda795abbf5b42ea130f23b134ae99287fab049abf4c6d61479764b63ca735a61dc457320b76abcbc3cdd9270ec5f0721c36d97258825189138ff
+DIST mako-1.8.0.gh.tar.gz 63337 BLAKE2B 703bf372b2654720e2601b544cbfd3f50bdea82fd382a17cd547eeb9a8c0f25077f97eab4ab3846f69a6f72f643d22303a5e66c82a53bfd2c83212c55c6d923d SHA512 f8f94244961efa364c5848cb3f66793f254adf3547dda1beedbff36f6f81572b29d531acb08a89154cf262e0714053b981edbc4372e6827b519a69277801a169
+DIST mako-1.8.0.gh.tar.gz.sig 566 BLAKE2B dbbca8efb865e49a1194ed20e4b9a43b9c9d2f447eb602b6bddee6a4c4d859db92b33d2d87c57e8f7b23c44565bb240196494a6b3018d3c75bc2115119831351 SHA512 e0256b6a9c431b4036899faff6c362af96d03c583f22d55f93f2972772563f509f0452937aeeaa916ea91931f7b2f6f1571713850d06517d99c10d05a4d7290d
+DIST mako-1.9.0.gh.tar.gz 63614 BLAKE2B 86aa844c5b2efaaa89948da60f4bb40b0f11fe36da5accb5a3da1db72b2069e6b6c334d4b11bf6c7ef6365bceb8d2c2d4aff91e8201c96c65b0e5a17929524af SHA512 24d133659c366d73326ac406e8dd68e5ac63007017ef6a2da4a5167a2ba4d760526c0939013dc592ac008a9dde5114e1aff84702aaa6017c3a21e21a32a44b56
+DIST mako-1.9.0.gh.tar.gz.sig 566 BLAKE2B 63e34b0c41e0bbc97e89344d680a1a42b11f21177762b2e30f87c25c1983e185293be3f77b8b7dbc7addb923c04f737fc12999087e319ce44a58467b9b175b82 SHA512 41ff21a48998c2dbdeed560de3d62d87e35c856d71471b5e79906d584f7ebad01e13130483e8b54969a6b4b0e74c65a7a8d12ccc1fd6e19912e07ef2a3718428

gui-apps/mako/mako-9999.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -12,7 +12,9 @@ if [[ ${PV} == 9999 ]]; then
 	inherit git-r3
 	EGIT_REPO_URI="https://github.com/emersion/${PN}.git"
 else
-	SRC_URI="https://github.com/emersion/${PN}/archive/v${PV}.tar.gz -> ${P}.tar.gz"
+	inherit verify-sig
+	SRC_URI="https://github.com/emersion/${PN}/releases/download/v${PV}/${P}.tar.gz -> ${P}.gh.tar.gz
+		https://github.com/emersion/${PN}/releases/download/v${PV}/${P}.tar.gz.sig -> ${P}.gh.tar.gz.sig"
 	KEYWORDS="~amd64 ~arm64 ~ppc64 ~x86"
 fi
 
@@ -43,6 +45,11 @@ BDEPEND="
 	dev-util/wayland-scanner
 	virtual/pkgconfig
 "
+
+if [[ ${PV} != 9999 ]]; then
+	BDEPEND+=" verify-sig? ( sec-keys/openpgp-keys-emersion )"
+	VERIFY_SIG_OPENPGP_KEY_PATH="/usr/share/openpgp-keys/emersion.asc"
+fi
 
 src_configure() {
 	local emesonargs=(

commit 0f823d981bb1b15db6ea9142907a447a5e05f1b2

gui-apps/mako: restore stable keywords
Reported by prom and also at https://forums.gentoo.org/viewtopic-p-8879579.html.

Fixes: bdb50e29bb15660c857c57fc61431073567587f7
Signed-off-by: Sam James <sam@gentoo.org>

gui-apps/mako/mako-1.10.0.ebuild
@@ -15,7 +15,7 @@ else
 	inherit verify-sig
 	SRC_URI="https://github.com/emersion/${PN}/releases/download/v${PV}/${P}.tar.gz -> ${P}.gh.tar.gz
 		https://github.com/emersion/${PN}/releases/download/v${PV}/${P}.tar.gz.sig -> ${P}.gh.tar.gz.sig"
-	KEYWORDS="~amd64 ~arm64 ~ppc64 ~x86"
+	KEYWORDS="amd64 arm64 ~ppc64 x86"
 fi
 
 LICENSE="MIT"

gui-apps/mako/mako-1.8.0.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -15,7 +15,7 @@ else
 	inherit verify-sig
 	SRC_URI="https://github.com/emersion/${PN}/releases/download/v${PV}/${P}.tar.gz -> ${P}.gh.tar.gz
 		https://github.com/emersion/${PN}/releases/download/v${PV}/${P}.tar.gz.sig -> ${P}.gh.tar.gz.sig"
-	KEYWORDS="~amd64 ~arm64 ~ppc64 ~x86"
+	KEYWORDS="amd64 arm64 ~ppc64 x86"
 fi
 
 LICENSE="MIT"

gui-apps/mako/mako-1.9.0.ebuild
@@ -15,7 +15,7 @@ else
 	inherit verify-sig
 	SRC_URI="https://github.com/emersion/${PN}/releases/download/v${PV}/${P}.tar.gz -> ${P}.gh.tar.gz
 		https://github.com/emersion/${PN}/releases/download/v${PV}/${P}.tar.gz.sig -> ${P}.gh.tar.gz.sig"
-	KEYWORDS="~amd64 ~arm64 ~ppc64 ~x86"
+	KEYWORDS="amd64 arm64 ~ppc64 x86"
 fi
 
 LICENSE="MIT"