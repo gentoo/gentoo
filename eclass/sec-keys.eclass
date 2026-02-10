gentoo/gentoo

commit c74965ebc0f5455522979ae6470785a763d5706d

sec-keys.eclass: pass 'keep-expired-subkeys' to gpg if supported
This is needed for sec-keys/openpgp-keys-pam because of an expired subkey.

Bug: https://dev.gnupg.org/T7990
Bug: https://bugs.gentoo.org/967745
Acked-by: Eli Schwartz <eschwartz@gentoo.org>
Signed-off-by: Sam James <sam@gentoo.org>

eclass/sec-keys.eclass
@@ -1,4 +1,4 @@
-# Copyright 2024-2025 Gentoo Authors
+# Copyright 2024-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 # @ECLASS: sec-keys.eclass
@@ -117,7 +117,11 @@ BDEPEND="
 sec-keys_src_compile() {
 	local -x GNUPGHOME=${WORKDIR}/gnupg
 	local fingerprint
-	local gpg_command=(gpg --export-options export-minimal)
+	if has_version -b ">=app-crypt/gnupg-2.5.17"; then
+		local gpg_command=(gpg --export-options export-minimal,keep-expired-subkeys)
+	else
+		local gpg_command=(gpg --export-options export-minimal)
+	fi
 
 	mkdir -m700 -p "${GNUPGHOME}" || die
 	cat <<- EOF > "${GNUPGHOME}"/gpg.conf || die

commit 220e84c85339d21f4d1d595c1c8351b386b63d52

sec-keys.eclass: fix src_test with expired subkeys
We need to keep expired subkeys in src_test too, otherwise we can
get spurious test failures both from the inconsistency vs src_compile
and it's also not a difference we care about either (and in fact losing
such an expired subkey could cause failures to verify some distfile).

Bug: https://bugs.gentoo.org/967745
Fixes: c74965ebc0f5455522979ae6470785a763d5706d
Reviewed-by: Eli Schwartz <eschwartz@gentoo.org>
Signed-off-by: Sam James <sam@gentoo.org>

eclass/sec-keys.eclass
@@ -168,7 +168,11 @@ sec-keys_src_compile() {
 sec-keys_src_test() {
 	local -x GNUPGHOME=${WORKDIR}/gnupg
 	local key fingerprint name server
-	local gpg_command=(gpg --export-options export-minimal)
+	if has_version -b ">=app-crypt/gnupg-2.5.17"; then
+		local gpg_command=(gpg --export-options export-minimal,keep-expired-subkeys)
+	else
+		local gpg_command=(gpg --export-options export-minimal)
+	fi
 
 	# Best-effort attempt to check for updates. keyservers can and usually
 	# do fail for weird reasons, (such as being unable to import a key