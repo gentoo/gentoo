gentoo/gentoo

commit 2911d5ba5878e82ee45ae45bca1666d580b3a3b9

app-containers/runc: drop hardened/PIC hacks
... as done in Docker some time ago, see 68d4571e78f348eac41d64ba306ec2a8226ad17c.

But we didn't even have the proper sanity check that we did in Docker!

Thanks to parona for the nudge.

Bug: https://bugs.gentoo.org/900849
Closes: https://bugs.gentoo.org/935459
Signed-off-by: Sam James <sam@gentoo.org>

app-containers/runc/runc-1.2.4.ebuild
@@ -38,11 +38,6 @@ BDEPEND="
 RESTRICT+=" test"
 
 src_compile() {
-	# Taken from app-containers/docker-1.7.0-r1
-	CGO_CFLAGS+=" -I${ESYSROOT}/usr/include"
-	CGO_LDFLAGS+=" $(usex hardened '-fno-PIC ' '')
-		-L${ESYSROOT}/usr/$(get_libdir)"
-
 	# build up optional flags
 	local options=(
 		$(usev apparmor)

commit 4a51c9e629cb69f6b56dfe2d846b76de438061d2

app-containers/runc: drop noop USE=hardened
Fixes: 2911d5ba5878e82ee45ae45bca1666d580b3a3b9
Signed-off-by: Sam James <sam@gentoo.org>

app-containers/runc/runc-1.2.4.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -19,7 +19,7 @@ S="${WORKDIR}/${PN}-${MY_PV}"
 LICENSE="Apache-2.0 BSD-2 BSD MIT"
 SLOT="0"
 KEYWORDS="amd64 ~arm arm64 ppc64 ~riscv ~x86"
-IUSE="apparmor hardened +kmem +seccomp selinux test"
+IUSE="apparmor +kmem +seccomp selinux test"
 
 COMMON_DEPEND="
 	apparmor? ( sys-libs/libapparmor )

app-containers/runc/runc-1.2.5.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -20,7 +20,7 @@ S="${WORKDIR}/${PN}-${MY_PV}"
 LICENSE="Apache-2.0 BSD-2 BSD MIT"
 SLOT="0"
 KEYWORDS="amd64 ~arm arm64 ppc64 ~riscv ~x86"
-IUSE="apparmor hardened +kmem +seccomp selinux test"
+IUSE="apparmor +kmem +seccomp selinux test"
 
 COMMON_DEPEND="
 	apparmor? ( sys-libs/libapparmor )

app-containers/runc/runc-1.2.6.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -20,7 +20,7 @@ S="${WORKDIR}/${PN}-${MY_PV}"
 LICENSE="Apache-2.0 BSD-2 BSD MIT"
 SLOT="0"
 KEYWORDS="amd64 ~arm arm64 ppc64 ~riscv ~x86"
-IUSE="apparmor hardened +kmem +seccomp selinux test"
+IUSE="apparmor +kmem +seccomp selinux test"
 
 COMMON_DEPEND="
 	apparmor? ( sys-libs/libapparmor )

app-containers/runc/runc-1.2.8.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -20,7 +20,7 @@ S="${WORKDIR}/${PN}-${MY_PV}"
 LICENSE="Apache-2.0 BSD-2 BSD MIT"
 SLOT="0"
 KEYWORDS="amd64 ~arm arm64 ppc64 ~riscv ~x86"
-IUSE="apparmor hardened +kmem +seccomp selinux test"
+IUSE="apparmor +kmem +seccomp selinux test"
 
 COMMON_DEPEND="
 	apparmor? ( sys-libs/libapparmor )

app-containers/runc/runc-1.3.0.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -20,7 +20,7 @@ S="${WORKDIR}/${PN}-${MY_PV}"
 LICENSE="Apache-2.0 BSD-2 BSD MIT"
 SLOT="0"
 KEYWORDS="~amd64 ~arm ~arm64 ~ppc64 ~riscv ~x86"
-IUSE="apparmor hardened +kmem +seccomp selinux test"
+IUSE="apparmor +kmem +seccomp selinux test"
 
 COMMON_DEPEND="
 	apparmor? ( sys-libs/libapparmor )

app-containers/runc/runc-1.3.1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -20,7 +20,7 @@ S="${WORKDIR}/${PN}-${MY_PV}"
 LICENSE="Apache-2.0 BSD-2 BSD MIT"
 SLOT="0"
 KEYWORDS="amd64 ~arm arm64 ppc64 ~riscv ~x86"
-IUSE="apparmor hardened +kmem +seccomp selinux test"
+IUSE="apparmor +kmem +seccomp selinux test"
 
 COMMON_DEPEND="
 	apparmor? ( sys-libs/libapparmor )

app-containers/runc/runc-1.3.3.ebuild
@@ -18,7 +18,7 @@ S="${WORKDIR}/${PN}-${MY_PV}"
 LICENSE="Apache-2.0 BSD-2 BSD MIT"
 SLOT="0"
 KEYWORDS="amd64 ~arm ~arm64 ppc64 ~riscv ~x86"
-IUSE="apparmor hardened +kmem +seccomp selinux test"
+IUSE="apparmor +kmem +seccomp selinux test"
 
 COMMON_DEPEND="
 	apparmor? ( sys-libs/libapparmor )

app-containers/runc/runc-1.4.0-r1.ebuild
@@ -20,7 +20,7 @@ S="${WORKDIR}/${PN}-${MY_PV}"
 LICENSE="Apache-2.0 BSD-2 BSD MIT"
 SLOT="0"
 KEYWORDS="~amd64 ~arm ~arm64 ~ppc64 ~riscv ~x86"
-IUSE="apparmor hardened +kmem +seccomp selinux test"
+IUSE="apparmor +kmem +seccomp selinux test"
 
 COMMON_DEPEND="
 	apparmor? ( sys-libs/libapparmor )