gentoo/gentoo

commit 72f1203db72baaf7fe3052cc7849379e24e1ce58

dev-python/pyqt5: Drop IUSE designer
Signed-off-by: Andreas Sturmlechner <asturm@gentoo.org>

dev-python/pyqt5/pyqt5-5.15.11.ebuild
@@ -18,7 +18,7 @@ SLOT="0"
 KEYWORDS="amd64 arm arm64 ~loong ~ppc ppc64 ~riscv x86"
 
 IUSE="
-	dbus debug declarative designer examples gles2-only gui multimedia
+	dbus debug declarative examples gles2-only gui multimedia
 	network opengl printsupport sql +ssl svg testlib widgets x11extras
 "
 
@@ -26,7 +26,6 @@ IUSE="
 # in project.py and from the output of 'grep -r "%Import " ${S}/sip'
 REQUIRED_USE="
 	declarative? ( gui network )
-	designer? ( widgets )
 	multimedia? ( gui network )
 	opengl? ( gui widgets )
 	printsupport? ( gui widgets )
@@ -45,7 +44,6 @@ DEPEND="
 		sys-apps/dbus
 	)
 	declarative? ( >=dev-qt/qtdeclarative-5.15:5[widgets?] )
-	designer? ( >=dev-qt/designer-5.15:5 )
 	gui? ( >=dev-qt/qtgui-5.15:5[gles2-only=] )
 	multimedia? ( >=dev-qt/qtmultimedia-5.15:5[widgets?] )
 	network? ( >=dev-qt/qtnetwork-5.15:5[ssl=] )
@@ -103,7 +101,6 @@ python_configure_all() {
 		$(pyqt_use_enable dbus QtDBus)
 		$(pyqt_use_enable declarative QtQml QtQuick \
 			$(usev widgets QtQuickWidgets))
-		$(pyqt_use_enable designer QtDesigner)
 		$(pyqt_use_enable gui QtGui \
 			$(use gles2-only && echo _QOpenGLFunctions_ES2 || echo _QOpenGLFunctions_{2_0,2_1,4_1_Core}))
 		$(pyqt_use_enable multimedia QtMultimedia \
@@ -120,6 +117,7 @@ python_configure_all() {
 
 		# no longer supported in Gentoo for PyQt5, use PyQt6
 		--disable=QtBluetooth
+		--disable=QtDesigner
 		--disable=QtHelp
 		--disable=QtLocation
 		--disable=QtPositioning

dev-python/pyqt5/metadata.xml
@@ -8,7 +8,6 @@
 	<use>
 		<flag name="dbus">Build bindings for the QtDBus module</flag>
 		<flag name="declarative">Build bindings for the QtQml and QtQuick modules and enable the qmlscene plugin</flag>
-		<flag name="designer">Build bindings for the QtDesigner module and enable the designer plugin</flag>
 		<flag name="gui">Build bindings for the QtGui module</flag>
 		<flag name="multimedia">Build bindings for the QtMultimedia module</flag>
 		<flag name="network">Build bindings for the QtNetwork module</flag>

commit 3da595d36f1a0775607bdac569bc5d27f3e17ffb

dev-python/pyqt5: remove leftover designer usage
Fixes: 72f1203db72baaf7fe3052cc7849379e24e1ce58
Signed-off-by: Ionen Wolkens <ionen@gentoo.org>

dev-python/pyqt5/pyqt5-5.15.11.ebuild
@@ -127,6 +127,7 @@ python_configure_all() {
 		--disable=QtWebChannel
 		--disable=QtWebSockets
 		--disable=QtXmlPatterns
+		--no-designer-plugin
 
 		$(usev debug '--debug --qml-debug --tracing')
 
@@ -135,7 +136,6 @@ python_configure_all() {
 		# plugins when using wheels w/ pep517 so, *if* something does need
 		# them, it will need to be handled manually
 		$(usev !declarative --no-qml-plugin)
-		$(usev !designer --no-designer-plugin)
 
 		$(usev gles2-only --disabled-feature=PyQt_Desktop_OpenGL)
 		$(usev !ssl --disabled-feature=PyQt_SSL)