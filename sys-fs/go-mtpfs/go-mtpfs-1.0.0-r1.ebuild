gentoo/gentoo

commit 7824cef4c02b5f7698c8bd5d2b13de49f2f20819

sys-fs/go-mtpfs: drop -fno-PIC
Unnecessary, see 68d4571e78f348eac41d64ba306ec2a8226ad17c.

Closes: https://bugs.gentoo.org/956352
Signed-off-by: Sam James <sam@gentoo.org>

sys-fs/go-mtpfs/go-mtpfs-1.0.0-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -26,11 +26,11 @@ DEPEND="${RDEPEND}
 RESTRICT+=" test"
 
 src_compile() {
-	ego build -ldflags '-extldflags=-fno-PIC' .
+	ego build 6.23.0.
 }
 
 src_test() {
-	ego test -ldflags '-extldflags=-fno-PIC' fs usb mtp
+	ego test 6.23.0fs usb mtp
 }
 
 src_install() {

sys-fs/go-mtpfs/go-mtpfs-1.0.0.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2022 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
@@ -24,13 +24,13 @@ RDEPEND="${COMMON_DEPEND}"
 RESTRICT+=" test"
 
 src_compile() {
-	ego build -ldflags '-extldflags=-fno-PIC' .
+	ego build .
 }
 
 src_test() {
-	ego test -ldflags '-extldflags=-fno-PIC' fs
-	ego test -ldflags '-extldflags=-fno-PIC' usb
-	ego test -ldflags '-extldflags=-fno-PIC' mtp
+	ego test fs
+	ego test usb
+	ego test mtp
 }
 
 src_install() {

commit 04ea3e434b21a6721e36f3255b25ad40170d968e

sys-fs/go-mtpfs: fix broken replacement
Fixes: 7824cef4c02b5f7698c8bd5d2b13de49f2f20819
Signed-off-by: Sam James <sam@gentoo.org>

sys-fs/go-mtpfs/go-mtpfs-1.0.0-r1.ebuild
@@ -26,11 +26,11 @@ DEPEND="${RDEPEND}
 RESTRICT+=" test"
 
 src_compile() {
-	ego build 6.23.0.
+	ego build .
 }
 
 src_test() {
-	ego test 6.23.0fs usb mtp
+	ego test fs usb mtp
 }
 
 src_install() {

sys-fs/go-mtpfs/go-mtpfs-1.0.0.ebuild
@@ -35,5 +35,5 @@ src_test() {
 
 src_install() {
 	dobin go-mtpfs
-dodoc README.md
+	dodoc README.md
 }