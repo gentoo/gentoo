gentoo/gentoo

commit 89876c7b01a7404bd047ec122821665794d8dff6

media-libs/gegl: respect NM
Closes: https://bugs.gentoo.org/968162
Signed-off-by: Alfred Wingate <parona@protonmail.com>
Part-of: https://github.com/gentoo/gentoo/pull/45388
Signed-off-by: Sam James <sam@gentoo.org>

media-libs/gegl/gegl-0.4.66.ebuild
@@ -73,6 +73,10 @@ BDEPEND="
 
 DOCS=( AUTHORS docs/ChangeLog docs/NEWS.adoc )
 
+PATCHES=(
+	"${FILESDIR}"/gegl-0.4.66-respect-NM.patch
+)
+
 pkg_pretend() {
 	[[ ${MERGE_TYPE} != binary ]] && use openmp && tc-check-openmp
 }

media-libs/gegl/files/gegl-0.4.66-respect-NM.patch
@@ -1,0 +1,25 @@
+https://bugs.gentoo.org/968162
+
+diff --git a/tools/defcheck.py b/tools/defcheck.py
+index 3103481..30d875f 100644
+--- a/tools/defcheck.py
++++ b/tools/defcheck.py
+@@ -31,7 +31,7 @@ Needs the tool "nm" to work
+ 
+ import sys, subprocess
+ 
+-from os import path
++from os import getenv, path
+ 
+ def_files = sys.argv[1:]
+ 
+@@ -65,7 +65,7 @@ for df in def_files:
+    #      unsortindex = i+1
+    #      break;
+ 
+-   status, nm = subprocess.getstatusoutput ("nm --defined-only --extern-only " +
++   status, nm = subprocess.getstatusoutput (getenv("NM", default="nm") + " --defined-only --extern-only " +
+                                             libname)
+    if status != 0:
+       print("trouble reading {} - has it been compiled?".format(libname))
+

commit 35de94a4dd628b7a699d5f5b8f8013a0b022e497

media-libs/gegl: -native-symlinks fix without NM/READLINK in make.conf
Tested in an environment without the ~workaround of CC/CXX/NM/READLINK/…
set in make.conf

Fixes: 89876c7b01a7404bd047ec122821665794d8dff6
Signed-off-by: Haelwenn (lanodan) Monnier <contact@hacktivis.me>
Part-of: https://codeberg.org/gentoo/gentoo/pulls/236
Signed-off-by: Sam James <sam@gentoo.org>

media-libs/gegl/gegl-0.4.66.ebuild
@@ -114,6 +114,8 @@ src_configure() {
 	# Bug #859901
 	filter-lto
 
+	tc-export READELF NM
+
 	use vala && vala_setup
 
 	local emesonargs=(