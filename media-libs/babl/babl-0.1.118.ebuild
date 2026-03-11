gentoo/gentoo

commit 3e40484cf090c3356399704700a59cfafcb2e649

media-libs/babl: respect NM
Closes: https://bugs.gentoo.org/968162
Signed-off-by: Alfred Wingate <parona@protonmail.com>
Part-of: https://github.com/gentoo/gentoo/pull/45388
Signed-off-by: Sam James <sam@gentoo.org>

media-libs/babl/babl-0.1.118.ebuild
@@ -33,6 +33,10 @@ RDEPEND="
 "
 DEPEND="${RDEPEND}"
 
+PATCHES=(
+	"${FILESDIR}"/babl-0.1.118-respect-NM.patch
+)
+
 src_prepare() {
 	default
 	gnome2_environment_reset

media-libs/babl/babl-0.1.120.ebuild
@@ -33,6 +33,10 @@ RDEPEND="
 "
 DEPEND="${RDEPEND}"
 
+PATCHES=(
+	"${FILESDIR}"/babl-0.1.118-respect-NM.patch
+)
+
 src_prepare() {
 	default
 	gnome2_environment_reset

media-libs/babl/files/babl-0.1.118-respect-NM.patch
@@ -1,0 +1,25 @@
+https://bugs.gentoo.org/968162
+
+diff --git a/tools/defcheck.py b/tools/defcheck.py
+index 3103481..30d875f 100644
+--- a/tools/defcheck.py
++++ b/tools/defcheck.py
+@@ -31,7 +31,7 @@ Needs the tool "nm" to work
+ 
+ import sys, subprocess
+ 
+-from os import path
++from os import getenv, path
+ 
+ def_files = sys.argv[1:]
+ 
+@@ -65,7 +65,7 @@ for df in def_files:
+    #      unsortindex = i+1
+    #      break;
+ 
+-   status, nm = subprocess.getstatusoutput ("nm --defined-only --extern-only " +
++   status, nm = subprocess.getstatusoutput (getenv("NM", default="nm") + " --defined-only --extern-only " +
+                                             libname)
+    if status != 0:
+       print("trouble reading {} - has it been compiled?".format(libname))
+

commit c4a00e85af4a8f895884460f3d9bdd1ea700f0f8

media-libs/babl: -native-symlinks fix without NM/READLINK in make.conf
Tested in an environment without the ~workaround of CC/CXX/NM/READLINK/…
set in make.conf

Fixes: 3e40484cf090c3356399704700a59cfafcb2e649
Signed-off-by: Haelwenn (lanodan) Monnier <contact@hacktivis.me>
Part-of: https://codeberg.org/gentoo/gentoo/pulls/236
Signed-off-by: Sam James <sam@gentoo.org>

media-libs/babl/babl-0.1.118.ebuild
@@ -5,7 +5,7 @@ EAPI=8
 
 VALA_USE_DEPEND=vapigen
 
-inherit meson gnome2-utils vala
+inherit meson gnome2-utils vala toolchain-funcs
 
 if [[ ${PV} == *9999* ]]; then
 	inherit git-r3
@@ -44,6 +44,8 @@ src_prepare() {
 }
 
 src_configure() {
+	tc-export READELF NM
+
 	use vala && vala_setup
 
 	# Automagic rsvg support is just for website generation we do not call,

media-libs/babl/babl-0.1.122.ebuild
@@ -5,7 +5,7 @@ EAPI=8
 
 VALA_USE_DEPEND=vapigen
 
-inherit meson gnome2-utils vala
+inherit meson gnome2-utils vala toolchain-funcs
 
 if [[ ${PV} == *9999* ]]; then
 	inherit git-r3
@@ -43,6 +43,8 @@ src_prepare() {
 }
 
 src_configure() {
+	tc-export READELF NM
+
 	use vala && vala_setup
 
 	# Automagic rsvg support is just for website generation we do not call,