gentoo/gentoo

commit ca7b3a0b88dadc613d6c1547aeb2b6a79d9a515c

media-gfx/gimp: respect NM
Closes: https://bugs.gentoo.org/968162
Signed-off-by: Alfred Wingate <parona@protonmail.com>
Part-of: https://github.com/gentoo/gentoo/pull/45388
Signed-off-by: Sam James <sam@gentoo.org>

media-gfx/gimp/gimp-3.2.0_rc2.ebuild
@@ -155,6 +155,7 @@ DOCS=( "AUTHORS" "NEWS" "README" "README.i18n" )
 
 PATCHES=(
 	"${FILESDIR}"/gimp-3.0.6-fix-tests.patch
+	"${FILESDIR}"/gimp-3.0.6-respect-NM.patch
 )
 
 pkg_pretend() {

media-gfx/gimp/files/gimp-3.0.6-respect-NM.patch
@@ -1,0 +1,25 @@
+https://bugs.gentoo.org/968162
+
+diff --git a/tools/defcheck.py b/tools/defcheck.py
+index 3103481..30d875f 100644
+--- a/tools/defcheck.py
++++ b/tools/defcheck.py
+@@ -31,7 +31,7 @@ Needs the tool "nm" to work
+ 
+ import sys, subprocess
+ 
+-from os import path
++from os import getenv, path
+ 
+ def_files = sys.argv[1:]
+ 
+@@ -65,7 +65,7 @@ for df in def_files:
+    #      unsortindex = i+1
+    #      break;
+ 
+-   status, nm = subprocess.getstatusoutput ("nm --defined-only --extern-only " +
++   status, nm = subprocess.getstatusoutput (getenv("NM", default="nm") + " --defined-only --extern-only " +
+                                             libname)
+    if status != 0:
+       print("trouble reading {} - has it been compiled?".format(libname))
+

media-gfx/gimp/gimp-3.0.6.ebuild
@@ -123,6 +123,7 @@ DOCS=( "AUTHORS" "NEWS" "README" "README.i18n" )
 
 PATCHES=(
 	"${FILESDIR}"/gimp-3.0.6-fix-tests.patch
+	"${FILESDIR}"/gimp-3.0.6-respect-NM.patch
 )
 
 pkg_pretend() {

commit b20e4256ca04968bc0a8708797e49459bf950ac6

media-gfx/gimp: -native-symlinks fix without NM/READLINK in make.conf
Tested in an environment without the ~workaround of CC/CXX/NM/READLINK/…
set in make.conf

Bug: https://bugs.gentoo.org/968162
Fixes: ca7b3a0b88dadc613d6c1547aeb2b6a79d9a515c
Signed-off-by: Haelwenn (lanodan) Monnier <contact@hacktivis.me>
Part-of: https://codeberg.org/gentoo/gentoo/pulls/236
Merges: https://codeberg.org/gentoo/gentoo/pulls/236
Signed-off-by: Sam James <sam@gentoo.org>

media-gfx/gimp/files/gimp-3.2.0-respect-NM.patch
@@ -1,0 +1,22 @@
+https://bugs.gentoo.org/968162
+
+--- a/tools/defcheck.py
++++ b/tools/defcheck.py
+@@ -31,7 +31,7 @@
+ 
+ import os, sys, subprocess
+ 
+-from os import path
++from os import getenv, path
+ 
+ def_files = (
+    "libgimpbase/gimpbase.def",
+@@ -55,7 +55,7 @@
+       sys.exit (-1)
+ 
+ libextension   = ".so"
+-command        = "nm --defined-only --extern-only "
++command        = getenv("NM", default="nm") + " --defined-only --extern-only "
+ platform_linux = True
+ 
+ if sys.platform in ['win32', 'cygwin']:

media-gfx/gimp/gimp-3.2.0_rc2.ebuild
@@ -202,6 +202,8 @@ src_configure() {
 	use wayland || append-cppflags -DGENTOO_GTK_HIDE_WAYLAND
 	use X || append-cppflags -DGENTOO_GTK_HIDE_X11
 
+	tc-export NM READELF
+
 	use vala && vala_setup
 
 	local emesonargs=(

media-gfx/gimp/gimp-3.2.0_rc3.ebuild
@@ -156,6 +156,10 @@ BDEPEND="
 
 DOCS=( "AUTHORS" "NEWS" "README" "README.i18n" )
 
+PATCHES=(
+	"${FILESDIR}"/gimp-3.2.0-respect-NM.patch
+)
+
 pkg_pretend() {
 	[[ ${MERGE_TYPE} != binary ]] && use openmp && tc-check-openmp
 }
@@ -214,6 +218,8 @@ src_configure() {
 	# defang automagic dependencies. Bug 943164
 	use wayland || append-cppflags -DGENTOO_GTK_HIDE_WAYLAND
 	use X || append-cppflags -DGENTOO_GTK_HIDE_X11
+
+	tc-export NM READELF
 
 	use vala && vala_setup
 