gentoo/gentoo

commit be04841726cae8da2f7589e1d8b30f6f5eab94d0

dev-debug/dtrace: update HOMEPAGE, remote-id
Signed-off-by: Sam James <sam@gentoo.org>

dev-debug/dtrace/dtrace-9999.ebuild
@@ -1,4 +1,4 @@
-# Copyright 2024-2025 Gentoo Authors
+# Copyright 2024-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -6,15 +6,15 @@ EAPI=8
 inherit edo flag-o-matic linux-info multilib systemd toolchain-funcs udev
 
 DESCRIPTION="Dynamic BPF-based system-wide tracing tool"
-HOMEPAGE="https://github.com/oracle/dtrace-utils https://wiki.gentoo.org/wiki/DTrace"
+HOMEPAGE="https://github.com/oracle/dtrace https://wiki.gentoo.org/wiki/DTrace"
 
 if [[ ${PV} == 9999 ]]; then
 	EGIT_BRANCH="devel"
-	EGIT_REPO_URI="https://github.com/oracle/dtrace-utils"
+	EGIT_REPO_URI="https://github.com/oracle/dtrace"
 	inherit git-r3
 else
-	SRC_URI="https://github.com/oracle/dtrace-utils/archive/refs/tags/${PV}.tar.gz -> ${P}.tar.gz"
-	S="${WORKDIR}"/dtrace-utils-${PV}
+	SRC_URI="https://github.com/oracle/dtrace/archive/refs/tags/${PV}.tar.gz -> ${P}.tar.gz"
+	S="${WORKDIR}"/dtrace-${PV}
 
 	KEYWORDS="-* ~amd64 ~arm64"
 fi
@@ -129,6 +129,7 @@ src_configure() {
 	filter-lto
 
 	local confargs=(
+		--disable-dependency-tracking
 		# TODO: Maybe we should set the UNPRIV_UID to something? -3 is a bit... kludgy
 		--prefix="${EPREFIX}"/usr
 		--mandir="${EPREFIX}"/usr/share/man
@@ -152,7 +153,7 @@ src_compile() {
 		! has_multilib_profile && myemakeargs+=( NATIVE_BITNESS_ONLY=1 )
 	fi
 
-	# -j1: https://github.com/oracle/dtrace-utils/issues/82
+	# -j1: https://github.com/oracle/dtrace/issues/82
 	emake -j1 "${myemakeargs[@]}"
 }
 

dev-debug/dtrace/metadata.xml
@@ -20,6 +20,6 @@
 		</flag>
 	</use>
 	<upstream>
-		<remote-id type="github">oracle/dtrace-utils</remote-id>
+		<remote-id type="github">oracle/dtrace</remote-id>
 	</upstream>
 </pkgmetadata>

commit b70df51dada6919325d4d7c5ce6723d751027157

dev-debug/dtrace: handle rename affecting tarball layout
In be04841726cae8da2f7589e1d8b30f6f5eab94d0, I naively didn't refetch
the tarballs from upstream so didn't notice that the renamed repo affected
the existing autogenerated tarballs from GH. I've diffed them and indeed
it's only the directory name which changed.

Closes: https://bugs.gentoo.org/971212
Fixes: be04841726cae8da2f7589e1d8b30f6f5eab94d0
Signed-off-by: Sam James <sam@gentoo.org>

dev-debug/dtrace/Manifest
@@ -1,2 +1,2 @@
-DIST dtrace-2.0.4.tar.gz 1699279 BLAKE2B 3fb610e2c233accbe36c372ab8e4f5012cdd314750dd64f7d411955038cdafeaeda038e63a06f6995f8cb89fd2df482f34fd5583f130f21f2b7a03747e33c778 SHA512 63623ce70d21dadf9e1de5d85e6d0a1a3043c0bd5f2255d32793209c1774e9be774699fdd70218a840f7060a1027388bf20d1d6c5e06b9e5671113c133ce390f
-DIST dtrace-2.0.5.tar.gz 1727649 BLAKE2B 387f0de25f04ed884ffbf4184b2c5e42330ea575f62a2c9f23d1413f945906e8ec6ddccdd00755ce2f2c43f41928201782b46519cbafeb17f526bfa06c0e15ab SHA512 131120602147b5cdbf7ac0c53a5c42547f80eeb5239f642bee4ea7083d991070514b838b850ded51b0bc8a31b64062c8102cd41517a6cd9bbc898afc20a87466
+DIST dtrace-2.0.4.renamed.tar.gz 1699313 BLAKE2B 223791d1c39f8c7a116f9141ecf563242f14cd820a01c669463a7dfb402fa6e136dd082fde51b6efbaa6e4ac282f5a94755997cd1465a832bd7620e550ab863e SHA512 cf55236a6130af5783f52487c4226d91eb3402f23eb308e6c2ea15ecaf1ab4ceeed8e7bf7f5b2bf7774229e71ea6fd9c7c0089ea39ca2bab393c1c5c97a6e252
+DIST dtrace-2.0.5.renamed.tar.gz 1727452 BLAKE2B 614d6e54cc7436ef99fd8e94a087fd39bffa44b49465b0992bcb919525a3229e70638da6490b36e15c8520f5f6c4544b96bd52738859c972c944b80083041dcc SHA512 5837c8a937ebed7b044516e00ccd5df154453f71ade8eddfd6c3299d909080509a6d1a31aafe64e5512a9ef51f983a6899344f5f579a118fde18849b6ac9b511

dev-debug/dtrace/dtrace-2.0.4.ebuild
@@ -1,4 +1,4 @@
-# Copyright 2024-2025 Gentoo Authors
+# Copyright 2024-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -6,14 +6,14 @@ EAPI=8
 inherit edo flag-o-matic linux-info multilib systemd toolchain-funcs udev
 
 DESCRIPTION="Dynamic BPF-based system-wide tracing tool"
-HOMEPAGE="https://github.com/oracle/dtrace-utils https://wiki.gentoo.org/wiki/DTrace"
+HOMEPAGE="https://github.com/oracle/dtrace https://wiki.gentoo.org/wiki/DTrace"
 
 if [[ ${PV} == 9999 ]]; then
 	EGIT_BRANCH="devel"
-	EGIT_REPO_URI="https://github.com/oracle/dtrace-utils"
+	EGIT_REPO_URI="https://github.com/oracle/dtrace"
 	inherit git-r3
 else
-	SRC_URI="https://github.com/oracle/dtrace-utils/archive/refs/tags/${PV}.tar.gz -> ${P}.tar.gz"
+	SRC_URI="https://github.com/oracle/dtrace/archive/refs/tags/${PV}.tar.gz -> ${P}.renamed.tar.gz"
 	S="${WORKDIR}"/dtrace-utils-${PV}
 
 	KEYWORDS="-* amd64 arm64"

dev-debug/dtrace/dtrace-2.0.5.ebuild
@@ -6,15 +6,14 @@ EAPI=8
 inherit edo flag-o-matic linux-info multilib systemd toolchain-funcs udev
 
 DESCRIPTION="Dynamic BPF-based system-wide tracing tool"
-HOMEPAGE="https://github.com/oracle/dtrace-utils https://wiki.gentoo.org/wiki/DTrace"
+HOMEPAGE="https://github.com/oracle/dtrace https://wiki.gentoo.org/wiki/DTrace"
 
 if [[ ${PV} == 9999 ]]; then
 	EGIT_BRANCH="devel"
-	EGIT_REPO_URI="https://github.com/oracle/dtrace-utils"
+	EGIT_REPO_URI="https://github.com/oracle/dtrace"
 	inherit git-r3
 else
-	SRC_URI="https://github.com/oracle/dtrace-utils/archive/refs/tags/${PV}.tar.gz -> ${P}.tar.gz"
-	S="${WORKDIR}"/dtrace-utils-${PV}
+	SRC_URI="https://github.com/oracle/dtrace/archive/refs/tags/${PV}.tar.gz -> ${P}.renamed.tar.gz"
 
 	KEYWORDS="-* ~amd64 ~arm64"
 fi

dev-debug/dtrace/dtrace-9999.ebuild
@@ -14,7 +14,6 @@ if [[ ${PV} == 9999 ]]; then
 	inherit git-r3
 else
 	SRC_URI="https://github.com/oracle/dtrace/archive/refs/tags/${PV}.tar.gz -> ${P}.tar.gz"
-	S="${WORKDIR}"/dtrace-${PV}
 
 	KEYWORDS="-* ~amd64 ~arm64"
 fi