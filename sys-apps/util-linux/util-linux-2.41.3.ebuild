gentoo/gentoo

commit 15addb9d5957b061750c7ef59130dc3ead89e3b8

sys-apps/util-linux: adapt to GNU Hurd
Disable Linux-specific features. The agetty check failed for me but
it worked when forcing it, needs checking.

Signed-off-by: Sam James <sam@gentoo.org>

sys-apps/util-linux/util-linux-2.41.3.ebuild
@@ -341,6 +341,21 @@ multilib_src_configure() {
 		)
 	fi
 
+	if [[ ${CTARGET} == *-gnu ]] ; then
+		# Disable Linux-specific features
+		myeconfargs+=(
+			--disable-partx
+			--disable-rfkill
+			--disable-schedutils
+			--disable-fsck
+		)
+
+		# This is explicitly needed for some reason? TODO
+		myeconfargs+=(
+			--enable-agetty
+		)
+	fi
+
 	ECONF_SOURCE="${S}" econf "${myeconfargs[@]}"
 
 	if multilib_is_native_abi && use python ; then

commit fcd9cc940c6664ec07e3085b7fc9cdcc1e998dd0

sys-apps/util-linux: fix CHOST check
CTARGET isn't defined unless we go out of our way to do it, use CHOST
which is obviously more appropriate here anyway (u-l isn't a toolchain).

Fixes: 15addb9d5957b061750c7ef59130dc3ead89e3b8
Signed-off-by: Sam James <sam@gentoo.org>

sys-apps/util-linux/util-linux-2.41.3.ebuild
@@ -341,7 +341,7 @@ multilib_src_configure() {
 		)
 	fi
 
-	if [[ ${CTARGET} == *-gnu ]] ; then
+	if [[ ${CHOST} == *-gnu ]] ; then
 		# Disable Linux-specific features
 		myeconfargs+=(
 			--disable-partx