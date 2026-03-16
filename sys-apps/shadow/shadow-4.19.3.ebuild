gentoo/gentoo

commit 3608e32cd12295171adbe29b771e8dcbf15195b6

sys-apps/shadow: port to GNU Hurd
Only build btrfs support (what does that even do for shadow?) on Linux
and avoid collision on /bin/login, as sys-kernel/hurd provides its own.

Signed-off-by: Sam James <sam@gentoo.org>

sys-apps/shadow/shadow-4.19.3.ebuild
@@ -41,7 +41,7 @@ COMMON_DEPEND="
 "
 DEPEND="
 	${COMMON_DEPEND}
-	>=sys-kernel/linux-headers-4.14
+	kernel_linux? ( >=sys-kernel/linux-headers-4.14 )
 "
 RDEPEND="
 	${COMMON_DEPEND}
@@ -86,7 +86,7 @@ src_configure() {
 		--enable-lastlog
 		--disable-account-tools-setuid
 		--disable-static
-		--with-btrfs
+		$(use_enable kernel_linux btrfs)
 		# Use bundled replacements for readpassphrase and freezero
 		--without-libbsd
 		--without-group-name-max-length
@@ -220,6 +220,11 @@ src_install() {
 
 	if use elibc_musl; then
 		QA_CONFIG_IMPL_DECL_SKIP+=( sgetsgent )
+	fi
+
+	if use kernel_Hurd ; then
+		# sys-kernel/hurd provides this instead
+		rm "${ED}"/bin/login || die
 	fi
 }
 

sys-apps/shadow/shadow-4.14.8-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -42,7 +42,7 @@ COMMON_DEPEND="
 "
 DEPEND="
 	${COMMON_DEPEND}
-	>=sys-kernel/linux-headers-4.14
+	kernel_linux? ( >=sys-kernel/linux-headers-4.14 )
 "
 RDEPEND="
 	${COMMON_DEPEND}

sys-apps/shadow/shadow-4.14.8-r2.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -42,7 +42,7 @@ COMMON_DEPEND="
 "
 DEPEND="
 	${COMMON_DEPEND}
-	>=sys-kernel/linux-headers-4.14
+	kernel_linux? ( >=sys-kernel/linux-headers-4.14 )
 "
 RDEPEND="
 	${COMMON_DEPEND}

sys-apps/shadow/shadow-4.14.8.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -42,7 +42,7 @@ COMMON_DEPEND="
 "
 DEPEND="
 	${COMMON_DEPEND}
-	>=sys-kernel/linux-headers-4.14
+	kernel_linux? ( >=sys-kernel/linux-headers-4.14 )
 "
 RDEPEND="
 	${COMMON_DEPEND}

sys-apps/shadow/shadow-4.19.2.ebuild
@@ -41,7 +41,7 @@ COMMON_DEPEND="
 "
 DEPEND="
 	${COMMON_DEPEND}
-	>=sys-kernel/linux-headers-4.14
+	kernel_linux? ( >=sys-kernel/linux-headers-4.14 )
 "
 RDEPEND="
 	${COMMON_DEPEND}

commit e48a907ad0a77a142aa516f51080bf3d59f81504

sys-apps/shadow: fix btrfs typo
Fixes: 3608e32cd12295171adbe29b771e8dcbf15195b6
Signed-off-by: Sam James <sam@gentoo.org>

sys-apps/shadow/shadow-4.19.3.ebuild
@@ -86,7 +86,7 @@ src_configure() {
 		--enable-lastlog
 		--disable-account-tools-setuid
 		--disable-static
-		$(use_enable kernel_linux btrfs)
+		$(use_with kernel_linux btrfs)
 		# Use bundled replacements for readpassphrase and freezero
 		--without-libbsd
 		--without-group-name-max-length