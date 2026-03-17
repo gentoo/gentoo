gentoo/gentoo

commit 5e96f155c88b738bece43585c2bfa19b5fd267a9

dev-libs/elfutils: don't enable debuginfod for multilib
I can't see value in this, it would mean a 32-bit application or library
linked against elfutils needed to use the debuginfod APIs. We can
restore it on request but the state before this change means many users have to
add p.use ABI_X86_32 entries.

Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/elfutils/elfutils-0.193-r2.ebuild
@@ -28,21 +28,20 @@ fi
 
 LICENSE="|| ( GPL-2+ LGPL-3+ ) utils? ( GPL-3+ )"
 SLOT="0"
-IUSE="bzip2 +debuginfod +libarchive +lzma nls static-libs stacktrace test +utils valgrind zstd"
+IUSE="bzip2 +debuginfod +lzma nls static-libs stacktrace test +utils valgrind zstd"
 RESTRICT="!test? ( test )"
-REQUIRED_USE="debuginfod? ( libarchive )"
 
 RDEPEND="
 	!dev-libs/libelf
+	>=app-arch/libarchive-3.1.2:=
 	>=virtual/zlib-1.2.8-r1:=[static-libs?,${MULTILIB_USEDEP}]
 	bzip2? ( >=app-arch/bzip2-1.0.6-r4[static-libs?,${MULTILIB_USEDEP}] )
 	debuginfod? (
 		dev-db/sqlite:3=
-		>=dev-libs/json-c-0.11:=[${MULTILIB_USEDEP}]
+		>=dev-libs/json-c-0.11:=
 		>=net-libs/libmicrohttpd-0.9.33:=
-		>=net-misc/curl-7.29.0[static-libs?,${MULTILIB_USEDEP}]
+		>=net-misc/curl-7.29.0[static-libs?]
 	)
-	libarchive? ( >=app-arch/libarchive-3.1.2:= )
 	lzma? ( >=app-arch/xz-utils-5.0.5-r1[static-libs?,${MULTILIB_USEDEP}] )
 	stacktrace? ( dev-util/sysprof )
 	zstd? ( app-arch/zstd:=[static-libs?,${MULTILIB_USEDEP}] )
@@ -66,7 +65,7 @@ BDEPEND+="
 PATCHES=(
 	"${FILESDIR}"/${PN}-0.189-musl-aarch64-regs.patch
 	"${FILESDIR}"/${PN}-0.191-musl-macros.patch
-	"${FILESDIR}"/${P}-tests.patch
+	"${FILESDIR}"/${P}-perf.patch
 )
 
 src_prepare() {
@@ -86,8 +85,6 @@ src_prepare() {
 	# "section [14] '.rel.plt': relocation 55: relocation type invalid for the file type"
 	printf "#!/bin/sh\nexit 77" > tests/run-elflint-self.sh || die
 	printf "#!/bin/sh\nexit 77" > tests/run-reverse-sections-self.sh || die
-	# TODO: Fails with SFrames
-	printf "#!/bin/sh\nexit 77" > tests/run-strip-strmerge.sh || die
 
 	# https://sourceware.org/PR23914
 	sed -i 's:-Werror::' */Makefile.in || die
@@ -109,14 +106,13 @@ multilib_src_configure() {
 	local myeconfargs=(
 		$(use_enable nls)
 		$(multilib_native_use_enable debuginfod)
-		# Could do dummy if needed? We could also split libdebuginfod
-		# (client support) into its own USE if required.
-		$(use_enable debuginfod libdebuginfod)
+		# Could do dummy if needed?
+		$(multilib_native_use_enable debuginfod libdebuginfod)
 		$(multilib_native_use_enable stacktrace)
 		$(use_enable valgrind valgrind-annotations)
 
-		# Explicitly disable thread safety, it's not recommended by upstream
-		# (marked experimental in configure) and doesn't build either on musl.
+		# explicitly disable thread safety, it's not recommended by upstream
+		# doesn't build either on musl.
 		--disable-thread-safety
 
 		# Valgrind option is just for running tests under it; dodgy under sandbox
@@ -125,7 +121,6 @@ multilib_src_configure() {
 		--program-prefix="eu-"
 		--with-zlib
 		$(use_with bzip2 bzlib)
-		$(multilib_native_use_with libarchive)
 		$(use_with lzma)
 		$(use_with zstd)
 	)

dev-libs/elfutils/elfutils-0.194-r1.ebuild
@@ -28,20 +28,21 @@ fi
 
 LICENSE="|| ( GPL-2+ LGPL-3+ ) utils? ( GPL-3+ )"
 SLOT="0"
-IUSE="bzip2 +debuginfod +lzma nls static-libs stacktrace test +utils valgrind zstd"
+IUSE="bzip2 +debuginfod +libarchive +lzma nls static-libs stacktrace test +utils valgrind zstd"
 RESTRICT="!test? ( test )"
+REQUIRED_USE="debuginfod? ( libarchive )"
 
 RDEPEND="
 	!dev-libs/libelf
-	>=app-arch/libarchive-3.1.2:=
 	>=virtual/zlib-1.2.8-r1:=[static-libs?,${MULTILIB_USEDEP}]
 	bzip2? ( >=app-arch/bzip2-1.0.6-r4[static-libs?,${MULTILIB_USEDEP}] )
 	debuginfod? (
 		dev-db/sqlite:3=
-		>=dev-libs/json-c-0.11:=[${MULTILIB_USEDEP}]
+		>=dev-libs/json-c-0.11:=
 		>=net-libs/libmicrohttpd-0.9.33:=
-		>=net-misc/curl-7.29.0[static-libs?,${MULTILIB_USEDEP}]
+		>=net-misc/curl-7.29.0[static-libs?]
 	)
+	libarchive? ( >=app-arch/libarchive-3.1.2:= )
 	lzma? ( >=app-arch/xz-utils-5.0.5-r1[static-libs?,${MULTILIB_USEDEP}] )
 	stacktrace? ( dev-util/sysprof )
 	zstd? ( app-arch/zstd:=[static-libs?,${MULTILIB_USEDEP}] )
@@ -65,7 +66,7 @@ BDEPEND+="
 PATCHES=(
 	"${FILESDIR}"/${PN}-0.189-musl-aarch64-regs.patch
 	"${FILESDIR}"/${PN}-0.191-musl-macros.patch
-	"${FILESDIR}"/${P}-perf.patch
+	"${FILESDIR}"/${P}-tests.patch
 )
 
 src_prepare() {
@@ -85,6 +86,8 @@ src_prepare() {
 	# "section [14] '.rel.plt': relocation 55: relocation type invalid for the file type"
 	printf "#!/bin/sh\nexit 77" > tests/run-elflint-self.sh || die
 	printf "#!/bin/sh\nexit 77" > tests/run-reverse-sections-self.sh || die
+	# TODO: Fails with SFrames
+	printf "#!/bin/sh\nexit 77" > tests/run-strip-strmerge.sh || die
 
 	# https://sourceware.org/PR23914
 	sed -i 's:-Werror::' */Makefile.in || die
@@ -106,13 +109,14 @@ multilib_src_configure() {
 	local myeconfargs=(
 		$(use_enable nls)
 		$(multilib_native_use_enable debuginfod)
-		# Could do dummy if needed?
-		$(use_enable debuginfod libdebuginfod)
+		# Could do dummy if needed? We could also split libdebuginfod
+		# (client support) into its own USE if required.
+		$(multilib_native_use_enable debuginfod libdebuginfod)
 		$(multilib_native_use_enable stacktrace)
 		$(use_enable valgrind valgrind-annotations)
 
-		# explicitly disable thread safety, it's not recommended by upstream
-		# doesn't build either on musl.
+		# Explicitly disable thread safety, it's not recommended by upstream
+		# (marked experimental in configure) and doesn't build either on musl.
 		--disable-thread-safety
 
 		# Valgrind option is just for running tests under it; dodgy under sandbox
@@ -121,6 +125,7 @@ multilib_src_configure() {
 		--program-prefix="eu-"
 		--with-zlib
 		$(use_with bzip2 bzlib)
+		$(multilib_native_use_with libarchive)
 		$(use_with lzma)
 		$(use_with zstd)
 	)

dev-libs/elfutils/elfutils-9999.ebuild
@@ -38,9 +38,9 @@ RDEPEND="
 	bzip2? ( >=app-arch/bzip2-1.0.6-r4[static-libs?,${MULTILIB_USEDEP}] )
 	debuginfod? (
 		dev-db/sqlite:3=
-		>=dev-libs/json-c-0.11:=[${MULTILIB_USEDEP}]
+		>=dev-libs/json-c-0.11:=
 		>=net-libs/libmicrohttpd-0.9.33:=
-		>=net-misc/curl-7.29.0[static-libs?,${MULTILIB_USEDEP}]
+		>=net-misc/curl-7.29.0[static-libs?]
 	)
 	libarchive? ( >=app-arch/libarchive-3.1.2:= )
 	lzma? ( >=app-arch/xz-utils-5.0.5-r1[static-libs?,${MULTILIB_USEDEP}] )
@@ -110,7 +110,7 @@ multilib_src_configure() {
 		$(multilib_native_use_enable debuginfod)
 		# Could do dummy if needed? We could also split libdebuginfod
 		# (client support) into its own USE if required.
-		$(use_enable debuginfod libdebuginfod)
+		$(multilib_native_use_enable debuginfod libdebuginfod)
 		$(multilib_native_use_enable stacktrace)
 		$(use_enable valgrind valgrind-annotations)
 

commit 0e3462839debee13e6baa44c9eeb0fcb1c116060

dev-libs/elfutils: fix debuginfod build
Closes: https://bugs.gentoo.org/971289
Fixes: 5e96f155c88b738bece43585c2bfa19b5fd267a9
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/elfutils/elfutils-0.193-r2.ebuild
@@ -97,6 +97,8 @@ src_configure() {
 	# bug 660738
 	filter-flags -fno-asynchronous-unwind-tables
 
+	use debuginfod && MULTILIB_WRAPPED_HEADERS+=( /usr/include/elfutils/debuginfod.h )
+
 	multilib-minimal_src_configure
 }
 

dev-libs/elfutils/elfutils-0.194-r1.ebuild
@@ -100,6 +100,8 @@ src_configure() {
 	# bug 660738
 	filter-flags -fno-asynchronous-unwind-tables
 
+	use debuginfod && MULTILIB_WRAPPED_HEADERS+=( /usr/include/elfutils/debuginfod.h )
+
 	multilib-minimal_src_configure
 }
 

dev-libs/elfutils/elfutils-9999.ebuild
@@ -99,6 +99,8 @@ src_configure() {
 	# bug 660738
 	filter-flags -fno-asynchronous-unwind-tables
 
+	use debuginfod && MULTILIB_WRAPPED_HEADERS+=( /usr/include/elfutils/debuginfod.h )
+
 	multilib-minimal_src_configure
 }
 