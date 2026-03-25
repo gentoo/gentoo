gentoo/gentoo

commit 16cee051bd12a98a8969fc5f70feb3710611fd61

dev-lang/orc: add 0.4.42
Drop IUSE gtk-doc.

Upstream have replaced gtk-doc with hotdoc for generating documentation,
which is not widely packaged by distros.  As this package is unavailable
in Gentoo yet, we have no docs.

Signed-off-by: Brett A C Sheffield <bacs@librecast.net>
Part-of: https://codeberg.org/gentoo/gentoo/pulls/279
Signed-off-by: Sam James <sam@gentoo.org>

dev-lang/orc/orc-0.4.42.ebuild
@@ -1,0 +1,35 @@
+# Copyright 1999-2026 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit meson-multilib
+
+DESCRIPTION="The Oil Runtime Compiler, a just-in-time compiler for array operations"
+HOMEPAGE="https://gstreamer.freedesktop.org/ https://gitlab.freedesktop.org/gstreamer/orc"
+SRC_URI="https://gstreamer.freedesktop.org/src/${PN}/${P}.tar.xz"
+
+LICENSE="BSD BSD-2"
+SLOT="0"
+KEYWORDS="~alpha amd64 arm arm64 ~hppa ~loong ppc ppc64 ~riscv x86 ~x64-macos ~x64-solaris"
+IUSE="static-libs test"
+RESTRICT="!test? ( test )"
+
+# in orc-0.4.42 upstream dropped gtk-doc in favour of hotdoc, which isn't widely
+# packaged yet. This means we have no docs, presently.
+
+DOCS=( CONTRIBUTING.md README RELEASE )
+
+multilib_src_configure() {
+	# FIXME: handle backends per arch? What about cross-compiling for the other arches?
+	local emesonargs=(
+		-Ddefault_library=$(usex static-libs both shared)
+		-Dorc-target=all
+		-Dorc-test=enabled
+		-Dbenchmarks=disabled
+		-Dexamples=disabled
+		$(meson_feature test tests)
+		-Dtools=enabled # requires orc-test
+	)
+	meson_src_configure
+}

dev-lang/orc/Manifest
@@ -1,3 +1,4 @@
 DIST orc-0.4.38.tar.xz 227152 BLAKE2B f7babfec5f4d792fb52eb0ca23743b6c06a12625edcf0cecbf8e3cb88c13171a760c6874061a3e55a43df5389346b3c3d0ba3463d6e1ef32f685162f7478e631 SHA512 49f34be85f6980e4b5e94f848016f5788b658323f3a120110bc237722ac99938c02976efbe96022d148054330432899533305d4dd21be8fab76fd1995179339a
 DIST orc-0.4.40.tar.xz 233276 BLAKE2B 2f2be5b7eaf5ea8c2fc55ff3938b5a9e9ef25a1ff3c5de4c02f9847b2df9039c8f412b9d780a172e78838c22f5006544a8181d7aaa22c47be98e6f050c04435e SHA512 2cbc0b8b9f5f429e0c4b24b7b9a8bc5d249c013470d8a595fdb3969a68a90c95c3b1e79063851f2d0ca7f7888d33901f227d2e04f919df00caf37eca6270c0e3
 DIST orc-0.4.41.tar.xz 234212 BLAKE2B 8c14462bc2420a96176592c8362d302667041c70086cbad8785bfde182a6131b86c5293d607d0c87bfdc400c1e22dc83dda5bd3a4c799e8f398ed7b72ad0fae5 SHA512 8c1dc497b2e2dd9a071ea0cb66bbf4bda0bad64f41c1ce0b9c87eb61c1fcf27d4cbef6c30ef00fed57c3623458aa683d39904f7c37072072eda6c51bcdd94091
+DIST orc-0.4.42.tar.xz 280792 BLAKE2B 0146c9c65e9c0c48ac39911b6307cad014a255331defcc6669cd158581b211138b900d62c8799ca135d6e669c5878029d0dcba373fdfc86e190743d575f071c0 SHA512 3a1ebe31a49255df10dbf2eb2aa72265dfc8a3806107d57b92c187aab8be32e01ad5652fb385d6886a15c23017a4db5374c0234be4c6d69b9af18347f83b1e6f

commit 8e6777c0846bd05bb6316dc50583681cadde2a0e

dev-lang/orc: fix meson BDEPEND in 0.4.42
The minimum is greater than the eclass provides.

Fixes: 16cee051bd12a98a8969fc5f70feb3710611fd61
Signed-off-by: Sam James <sam@gentoo.org>

dev-lang/orc/orc-0.4.42.ebuild
@@ -15,6 +15,8 @@ KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~loong ~ppc ~ppc64 ~riscv ~x86 ~x64-ma
 IUSE="static-libs test"
 RESTRICT="!test? ( test )"
 
+BDEPEND=">=dev-build/meson-1.3.0"
+
 # in orc-0.4.42 upstream dropped gtk-doc in favour of hotdoc, which isn't widely
 # packaged yet. This means we have no docs, presently.
 