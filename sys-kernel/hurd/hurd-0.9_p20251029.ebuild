gentoo/gentoo

commit e244fab5adada9219d04e6b4d97d9ceeb5d17609

sys-kernel/hurd: arrange to install libshouldbeinlibc for cross-*
For cross-*, we always want a minimal build even with USE=-headers-only,
as we will need libshouldbeinlibc. This becomes apparent since
02810362d5f2bb31cbd06e17fdeceb0f75ea56f9 where sys-block/parted needs that,
but Hurd itself depends on parted.

Missing libshouldbeinlibc means that initial @system build in the sysroot
w/ i686-gnu-emerge falis because while we have Hurd, we don't have a Hurd
that provides any libraries.

There will be corresponding changes in crossdev.

Signed-off-by: Sam James <sam@gentoo.org>

sys-kernel/hurd/hurd-0.9_p20251029.ebuild
@@ -81,6 +81,9 @@ src_configure() {
 		--prefix="${EPREFIX}${sysroot}/usr"
 		--datadir="${EPREFIX}${sysroot}/usr/share"
 		--host=${CTARGET}
+
+		# Builds everything twice and needs profiling libs
+		--disable-profile
 	)
 
 	if ! use custom-cflags ; then
@@ -92,7 +95,9 @@ src_configure() {
 
 	append-flags -Wl,--no-error-rwx-segments -Wl,--no-error-execstack -Wl,-z,notext
 
-	if use headers-only ; then
+	# For cross-*, we always want a minimal build even with USE=-headers-only,
+	# as we will need libshouldbeinlibc.
+	if use headers-only || is_crosspkg ; then
 		myeconfargs+=(
 			ac_cv_func_file_exec_paths=no
 			ac_cv_func_exec_exec_paths=no
@@ -104,17 +109,21 @@ src_configure() {
 			--without-acpica
 			--without-libcrypt
 			--without-libbz2
+			--without-libdaemon
 			--without-libz
 			--without-libtirpc
 			--without-parted
 			--without-rump
 			--disable-ncursesw
 		)
+
+		cat <<-EOF >> config.make.in || die "Failed to amend config.make.in"
+		HAVE_LIBGCRYPT = no
+		HAVE_LIBPCIACCESS = no
+		HAVE_XKBCOMMON = no
+		EOF
 	else
 		myeconfargs+=(
-			# Builds everything twice and needs profiling libs
-			--disable-profile
-
 			# Unpackaged
 			--without-acpica
 
@@ -136,8 +145,16 @@ src_configure() {
 }
 
 src_compile() {
-	use headers-only && return
-	default
+	if use headers-only ; then
+		return
+	elif is_crosspkg ; then
+		emake \
+			lib-subdirs="libshouldbeinlibc libihash libstore" \
+			prog-subdirs= \
+			other-subdirs=
+	else
+		default
+	fi
 }
 
 src_test() {
@@ -146,7 +163,18 @@ src_test() {
 }
 
 src_install() {
-	emake DESTDIR="${D}" $(usex headers-only install-headers install) no_deps=t
+	if use headers-only ; then
+		emake DESTDIR="${D}" install-headers no_deps=t
+	elif is_crosspkg ; then
+		emake DESTDIR="${D}" install-headers no_deps=t
+		emake DESTDIR="${D}" install \
+			lib-subdirs="libshouldbeinlibc libihash libstore" \
+			prog-subdirs= \
+			other-subdirs= \
+			no_deps=t
+	else
+		emake DESTDIR="${D}" install
+	fi
 
 	if target_is_not_host ; then
 		# TODO: Is this needed?

sys-kernel/hurd/hurd-9999.ebuild
@@ -80,6 +80,9 @@ src_configure() {
 		--prefix="${EPREFIX}${sysroot}/usr"
 		--datadir="${EPREFIX}${sysroot}/usr/share"
 		--host=${CTARGET}
+
+		# Builds everything twice and needs profiling libs
+		--disable-profile
 	)
 
 	if ! use custom-cflags ; then
@@ -91,7 +94,9 @@ src_configure() {
 
 	append-flags -Wl,--no-error-rwx-segments -Wl,--no-error-execstack -Wl,-z,notext
 
-	if use headers-only ; then
+	# For cross-*, we always want a minimal build even with USE=-headers-only,
+	# as we will need libshouldbeinlibc.
+	if use headers-only || is_crosspkg ; then
 		myeconfargs+=(
 			ac_cv_func_file_exec_paths=no
 			ac_cv_func_exec_exec_paths=no
@@ -103,17 +108,21 @@ src_configure() {
 			--without-acpica
 			--without-libcrypt
 			--without-libbz2
+			--without-libdaemon
 			--without-libz
 			--without-libtirpc
 			--without-parted
 			--without-rump
 			--disable-ncursesw
 		)
+
+		cat <<-EOF >> config.make.in || die "Failed to amend config.make.in"
+		HAVE_LIBGCRYPT = no
+		HAVE_LIBPCIACCESS = no
+		HAVE_XKBCOMMON = no
+		EOF
 	else
 		myeconfargs+=(
-			# Builds everything twice and needs profiling libs
-			--disable-profile
-
 			# Unpackaged
 			--without-acpica
 
@@ -135,8 +144,16 @@ src_configure() {
 }
 
 src_compile() {
-	use headers-only && return
-	default
+	if use headers-only ; then
+		return
+	elif is_crosspkg ; then
+		emake \
+			lib-subdirs="libshouldbeinlibc libihash libstore" \
+			prog-subdirs= \
+			other-subdirs=
+	else
+		default
+	fi
 }
 
 src_test() {
@@ -145,7 +162,18 @@ src_test() {
 }
 
 src_install() {
-	emake DESTDIR="${D}" $(usex headers-only install-headers install) no_deps=t
+	if use headers-only ; then
+		emake DESTDIR="${D}" install-headers no_deps=t
+	elif is_crosspkg ; then
+		emake DESTDIR="${D}" install-headers no_deps=t
+		emake DESTDIR="${D}" install \
+			lib-subdirs="libshouldbeinlibc libihash libstore" \
+			prog-subdirs= \
+			other-subdirs= \
+			no_deps=t
+	else
+		emake DESTDIR="${D}" install
+	fi
 
 	if target_is_not_host ; then
 		# TODO: Is this needed?

commit 6d6afb9d970422778c7973cb8400422a8899109d

sys-kernel/hurd: fix libdir for cross-*
This goes back further I suppose but it only started to matter with
e244fab5adada9219d04e6b4d97d9ceeb5d17609 as otherwise we installed
just headers.

Avoids cross-i686-gnu/hurd installing to the wrong libdir:
```
/usr/i686-gnu/usr/lib64
/usr/i686-gnu/usr/lib64/libihash.a
/usr/i686-gnu/usr/lib64/libihash.so
/usr/i686-gnu/usr/lib64/libihash.so.0.3
/usr/i686-gnu/usr/lib64/libihash_pic.a
/usr/i686-gnu/usr/lib64/libirqhelp.a
/usr/i686-gnu/usr/lib64/libirqhelp.so
/usr/i686-gnu/usr/lib64/libirqhelp.so.0.3
/usr/i686-gnu/usr/lib64/libirqhelp_pic.a
/usr/i686-gnu/usr/lib64/libshouldbeinlibc.a
/usr/i686-gnu/usr/lib64/libshouldbeinlibc.so
/usr/i686-gnu/usr/lib64/libshouldbeinlibc.so.0.3
/usr/i686-gnu/usr/lib64/libshouldbeinlibc_pic.a
/usr/i686-gnu/usr/lib64/libstore.a
/usr/i686-gnu/usr/lib64/libstore.so
/usr/i686-gnu/usr/lib64/libstore.so.0.3
/usr/i686-gnu/usr/lib64/libstore_concat.a
/usr/i686-gnu/usr/lib64/libstore_copy.a
/usr/i686-gnu/usr/lib64/libstore_device.a
/usr/i686-gnu/usr/lib64/libstore_file.a
/usr/i686-gnu/usr/lib64/libstore_ileave.a
/usr/i686-gnu/usr/lib64/libstore_memobj.a
/usr/i686-gnu/usr/lib64/libstore_module.a
/usr/i686-gnu/usr/lib64/libstore_mvol.a
/usr/i686-gnu/usr/lib64/libstore_nbd.a
/usr/i686-gnu/usr/lib64/libstore_pic.a
/usr/i686-gnu/usr/lib64/libstore_remap.a
/usr/i686-gnu/usr/lib64/libstore_task.a
/usr/i686-gnu/usr/lib64/libstore_zero.a
```

Fixes: e244fab5adada9219d04e6b4d97d9ceeb5d17609
Signed-off-by: Sam James <sam@gentoo.org>

sys-kernel/hurd/hurd-0.9_p20251029.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-inherit crossdev flag-o-matic
+inherit crossdev multilib flag-o-matic
 
 DESCRIPTION="GNU Hurd is the GNU project's replacement for UNIX"
 HOMEPAGE="https://www.gnu.org/software/hurd/hurd.html"
@@ -74,6 +74,11 @@ src_configure() {
 	if target_is_not_host ; then
 		local sysroot=/usr/${CTARGET}
 		export MIG="${CTARGET}-mig"
+
+		# Ensure we get compatible libdir
+		unset DEFAULT_ABI MULTILIB_ABIS
+		multilib_env
+		ABI=${DEFAULT_ABI}
 	fi
 
 	local myeconfargs=(

sys-kernel/hurd/hurd-9999.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-inherit crossdev flag-o-matic
+inherit crossdev multilib flag-o-matic
 
 DESCRIPTION="GNU Hurd is the GNU project's replacement for UNIX"
 HOMEPAGE="https://www.gnu.org/software/hurd/hurd.html"
@@ -73,6 +73,11 @@ src_configure() {
 	if target_is_not_host ; then
 		local sysroot=/usr/${CTARGET}
 		export MIG="${CTARGET}-mig"
+
+		# Ensure we get compatible libdir
+		unset DEFAULT_ABI MULTILIB_ABIS
+		multilib_env
+		ABI=${DEFAULT_ABI}
 	fi
 
 	local myeconfargs=(