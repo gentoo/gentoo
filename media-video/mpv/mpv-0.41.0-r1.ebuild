gentoo/gentoo

commit 0be221509df85a70deef147c4cb78853ad01be9d

media-video/mpv: strip ffnvcodec from mpv.pc for now
Dropped a note in #mpv-devel and seems being included is only because
it does not make the distinction between build-only and runtime. Would
need a fix like libplacebo did for xxhash.

For now, just force drop it here. The sed is re-used from libplacebo
for (another) similar issue, and should be proven to work fine by now.

Revbump is not super worth it, but mpv does not take long to build.

Closes: https://bugs.gentoo.org/971646
Signed-off-by: Ionen Wolkens <ionen@gentoo.org>

media-video/mpv/mpv-0.41.0-r1.ebuild
@@ -243,6 +243,12 @@ src_install() {
 		mv "${ED}"/usr/share/doc/{mpv,${PF}/examples} || die
 	fi
 
+	# prevent build-only ffnvcodec from leaking into the .pc (bug #971646)
+	if use nvenc; then
+		sed -Ee '/^Requires/s/ffnvcodec[^,]*,? ?//;s/, $//;/^Requires[^:]*: $/d' \
+			-i "${ED}"/usr/$(get_libdir)/pkgconfig/mpv.pc || die
+	fi
+
 	local GLOBIGNORE=*/*build*:*/*policy*
 	dodoc RELEASE_NOTES DOCS/*.{md,rst}
 }

media-video/mpv/mpv-9999.ebuild
@@ -245,6 +245,12 @@ src_install() {
 		mv "${ED}"/usr/share/doc/{mpv,${PF}/examples} || die
 	fi
 
+	# prevent build-only ffnvcodec from leaking into the .pc (bug #971646)
+	if use nvenc; then
+		sed -Ee '/^Requires/s/ffnvcodec[^,]*,? ?//;s/, $//;/^Requires[^:]*: $/d' \
+			-i "${ED}"/usr/$(get_libdir)/pkgconfig/mpv.pc || die
+	fi
+
 	local GLOBIGNORE=*/*build*:*/*policy*
 	dodoc RELEASE_NOTES DOCS/*.{md,rst}
 }

commit a591b71dbdfcfddde5f961fbe3f45c94d5dae164

media-video/mpv: check for libmpv too for nvenc workaround
Oops, libmpv is default enabled so I kind of forgot about it.

Closes: https://bugs.gentoo.org/971785
Fixes: 0be221509df85a70deef147c4cb78853ad01be9d
Signed-off-by: Ionen Wolkens <ionen@gentoo.org>

media-video/mpv/mpv-0.41.0-r1.ebuild
@@ -244,7 +244,7 @@ src_install() {
 	fi
 
 	# prevent build-only ffnvcodec from leaking into the .pc (bug #971646)
-	if use nvenc; then
+	if use libmpv && use nvenc; then
 		sed -Ee '/^Requires/s/ffnvcodec[^,]*,? ?//;s/, $//;/^Requires[^:]*: $/d' \
 			-i "${ED}"/usr/$(get_libdir)/pkgconfig/mpv.pc || die
 	fi

media-video/mpv/mpv-9999.ebuild
@@ -246,7 +246,7 @@ src_install() {
 	fi
 
 	# prevent build-only ffnvcodec from leaking into the .pc (bug #971646)
-	if use nvenc; then
+	if use libmpv && use nvenc; then
 		sed -Ee '/^Requires/s/ffnvcodec[^,]*,? ?//;s/, $//;/^Requires[^:]*: $/d' \
 			-i "${ED}"/usr/$(get_libdir)/pkgconfig/mpv.pc || die
 	fi