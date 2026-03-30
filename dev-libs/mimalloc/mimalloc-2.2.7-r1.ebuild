gentoo/gentoo

commit cbe818c40bc220ebeb39da471245f31a6225867c

dev-libs/mimalloc: drop USE=hardened, USE=valgrind for now
Having either enabled means that the library gets renamed which breaks
reverse dependencies. They can be restored at a later time w/ multibuild.eclass.

Closes: https://bugs.gentoo.org/970985
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/mimalloc/mimalloc-2.2.7-r1.ebuild
@@ -18,14 +18,16 @@ RESTRICT="!test? ( test )"
 DEPEND="valgrind? ( dev-debug/valgrind )"
 
 src_configure() {
+	# hardened + valgrind could be restored w/ multibuild.eclass. They
+	# install renamed libraries which break reverse dependencies.
 	local mycmakeargs=(
 		-DMI_DEBUG_FULL=$(usex debug)
-		-DMI_SECURE=$(usex hardened)
+		-DMI_SECURE=OFF
 		-DMI_INSTALL_TOPLEVEL=ON
 		-DMI_BUILD_TESTS=$(usex test)
 		-DMI_BUILD_OBJECT=OFF
 		-DMI_BUILD_STATIC=OFF
-		-DMI_TRACK_VALGRIND=$(usex valgrind)
+		-DMI_TRACK_VALGRIND=OFF
 		-DMI_LIBC_MUSL=$(usex elibc_musl)
 		# Don't inject -march=XXX
 		-DMI_OPT_ARCH=OFF

dev-libs/mimalloc/mimalloc-3.2.8-r1.ebuild
@@ -18,14 +18,16 @@ RESTRICT="!test? ( test )"
 DEPEND="valgrind? ( dev-debug/valgrind )"
 
 src_configure() {
+	# hardened + valgrind could be restored w/ multibuild.eclass. They
+	# install renamed libraries which break reverse dependencies.
 	local mycmakeargs=(
 		-DMI_DEBUG_FULL=$(usex debug)
-		-DMI_SECURE=$(usex hardened)
+		-DMI_SECURE=OFF
 		-DMI_INSTALL_TOPLEVEL=ON
 		-DMI_BUILD_TESTS=$(usex test)
 		-DMI_BUILD_OBJECT=OFF
 		-DMI_BUILD_STATIC=OFF
-		-DMI_TRACK_VALGRIND=$(usex valgrind)
+		-DMI_TRACK_VALGRIND=OFF
 		-DMI_LIBC_MUSL=$(usex elibc_musl)
 		# Don't inject -march=XXX
 		-DMI_OPT_ARCH=OFF

commit e34d17b815de6195c413da3eb348a0537f923e59

dev-libs/mimalloc: cleanup debris
Fixes: cbe818c40bc220ebeb39da471245f31a6225867c
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/mimalloc/mimalloc-2.2.7-r1.ebuild
@@ -12,10 +12,8 @@ SRC_URI="https://github.com/microsoft/mimalloc/archive/refs/tags/v${PV}.tar.gz -
 LICENSE="MIT"
 SLOT="0/2"
 KEYWORDS="~amd64 ~arm ~arm64 ~loong ~m68k ~ppc ~ppc64 ~riscv ~sparc ~x86"
-IUSE="debug hardened test valgrind"
+IUSE="debug test"
 RESTRICT="!test? ( test )"
-
-DEPEND="valgrind? ( dev-debug/valgrind )"
 
 src_configure() {
 	# hardened + valgrind could be restored w/ multibuild.eclass. They

dev-libs/mimalloc/mimalloc-3.2.8-r1.ebuild
@@ -12,10 +12,8 @@ SRC_URI="https://github.com/microsoft/mimalloc/archive/refs/tags/v${PV}.tar.gz -
 LICENSE="MIT"
 SLOT="0/3"
 KEYWORDS="~amd64 ~arm ~arm64 ~loong ~m68k ~ppc ~ppc64 ~riscv ~sparc ~x86"
-IUSE="debug hardened test valgrind"
+IUSE="debug test"
 RESTRICT="!test? ( test )"
-
-DEPEND="valgrind? ( dev-debug/valgrind )"
 
 src_configure() {
 	# hardened + valgrind could be restored w/ multibuild.eclass. They