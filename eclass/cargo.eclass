gentoo/gentoo

commit 4bf5c8c63455def273c2c27006a1363d23515eb8

cargo.eclass: avoid useless use of assert / pipestatus
The preceding command pipeline is `printf | xargs`, and printf is a bash
builtin. It cannot fail. `die`ing if xargs fails to consume the pipe
buffer is sufficient.

assert is banned in EAPI 9, so it needs to go regardless. There's no
point to using `inherit eapi9-pipestatus` here.

Signed-off-by: Eli Schwartz <eschwartz@gentoo.org>

eclass/cargo.eclass
@@ -529,8 +529,7 @@ cargo_src_unpack() {
 		ebegin "Unpacking crates"
 		printf '%s\0' "${crates[@]}" |
 			xargs -0 -P "$(makeopts_jobs)" -n 1 -t -- \
-				tar -x -C "${ECARGO_VENDOR}" -f
-		assert
+				tar -x -C "${ECARGO_VENDOR}" -f || die
 		eend $?
 
 		while read -d '' -r shasum archive; do
@@ -556,6 +555,7 @@ cargo_src_unpack() {
 			eqawarn "a crate tarball instead and fetch it via SRC_URI.  You can use"
 			eqawarn "'pycargoebuild --crate-tarball' to create one."
 		fi
+		einfo "this package uses ${#crates[@]} number of crates: you are all good"
 	fi
 
 	cargo_gen_config

commit 4cfe7e822b9b56954e25c169025d8f3cf8dc0286

cargo.eclass: remove ridiculously local einfo
This is something I have locally for verbosity reasons, because I
personally dislike warning people to make giant uncached per-CPV
tarballs full of other tarballs. Possibly evident from "you are all
good"...

But my personal taste here should never have been pushed to ::gentoo and
I'm at a loss to explain how the diff hunk ended up in a totally
unrelated commit. Purge it with fire from the public repo. Sorry...

Fixes: 4bf5c8c63455def273c2c27006a1363d23515eb8
Signed-off-by: Eli Schwartz <eschwartz@gentoo.org>

eclass/cargo.eclass
@@ -555,7 +555,6 @@ cargo_src_unpack() {
 			eqawarn "a crate tarball instead and fetch it via SRC_URI.  You can use"
 			eqawarn "'pycargoebuild --crate-tarball' to create one."
 		fi
-		einfo "this package uses ${#crates[@]} number of crates: you are all good"
 	fi
 
 	cargo_gen_config