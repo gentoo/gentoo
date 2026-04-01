gentoo/gentoo

commit 629470dfcf6579213ebeb53ca0adfbe643de692b

www-servers/vinyl-cache: add 9.0.0
9.0.0 is the first release with the name change to vinyl-cache. All
binary names, directory paths, user and group etc. have changed.

Link: https://vinyl-cache.org/docs/9.0/whats-new/upgrading-9.0.html
Signed-off-by: Brett A C Sheffield <bacs@librecast.net>
Part-of: https://codeberg.org/gentoo/gentoo/pulls/462
Signed-off-by: Sam James <sam@gentoo.org>

www-servers/vinyl-cache/vinyl-cache-9.0.0.ebuild
@@ -1,0 +1,128 @@
+# Copyright 1999-2026 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+PYTHON_COMPAT=( python3_{11..14} )
+
+inherit autotools systemd python-r1
+
+DESCRIPTION="A high-performance caching HTTP reverse proxy"
+HOMEPAGE="https://vinyl-cache.org/"
+SRC_URI="https://vinyl-cache.org/downloads/${P}.tgz"
+
+LICENSE="BSD-2 GPL-2"
+SLOT="0/9"
+KEYWORDS="~amd64 ~arm ~arm64 ~ppc ~ppc64 ~riscv ~x86"
+IUSE="jit selinux static-libs unwind"
+
+COMMON_DEPEND="
+	sys-libs/readline:=
+	dev-libs/libedit
+	dev-libs/libpcre2[jit?]
+	sys-libs/ncurses:=
+	unwind? ( sys-libs/libunwind:= )
+"
+
+# varnish compiles stuff at run time
+RDEPEND="
+	${PYTHON_DEPS}
+	${COMMON_DEPEND}
+	acct-user/vinyl
+	acct-group/vinyl
+	sys-devel/gcc
+	selinux? ( sec-policy/selinux-varnishd )
+"
+DEPEND="
+	${COMMON_DEPEND}
+	dev-python/docutils
+	dev-python/sphinx
+	virtual/pkgconfig
+"
+
+REQUIRED_USE="${PYTHON_REQUIRED_USE}"
+
+# Upstream acknowledge that a small number of tests fail sporadically, including on their
+# test boxes. We SKIP problematic tests here.  Check https://vinyl-cache.org/vtest/
+SKIP_TESTS=(
+	"u00021.vtc" # Commit: 83d9fbb2961c
+	"r02686.vtc" # Bug: 880627
+	"r02990.vtc" # Bug: 880627
+	"u00008.vtc" # Bug: 880627
+	"u00009.vtc" # Bug: 880627
+	"t02014.vtc" # Bug: 964041
+)
+
+src_prepare() {
+	default
+
+	# Fix python shebangs for python-exec[-native-symlinks], #909330
+	local shebangs=($(grep -rl "#!/usr/bin/env python3" || die))
+	python_setup
+	python_fix_shebang -q ${shebangs[*]}
+
+	# SKIP unreliable tests
+	local t
+	for t in ${SKIP_TESTS[*]}; do
+		sed -i -e '/^vtest.*$/a feature cmd false' bin/vinyltest/tests/${t} || die
+	done
+
+	# Remove -Werror bug #528354
+	sed -i -e 's/-Werror\([^=]\)/\1/g' configure.ac || die
+
+	# Upstream doesn't put vinyl.m4 in the m4/ directory
+	# We link because the Makefiles look for the file in
+	# the original location
+	ln -sf ../vinyl.m4 m4/vinyl.m4 || die
+
+	eautoreconf
+}
+
+src_configure() {
+	local myeconfargs=(
+		--with-contrib
+		$(use_enable static-libs static)
+		$(use_enable jit pcre2-jit)
+		$(use_with unwind)
+		--without-jemalloc
+	)
+	econf "${myeconfargs[@]}"
+}
+
+src_install() {
+	emake DESTDIR="${D}" install
+
+	python_replicate_script "${D}/usr/share/vinyl-cache/vmodtool.py"
+
+	newinitd "${FILESDIR}"/vinyllog.initd vinyllog
+	newconfd "${FILESDIR}"/vinyllog.confd vinyllog
+
+	newinitd "${FILESDIR}"/vinylncsa.initd vinylncsa
+	newconfd "${FILESDIR}"/vinylncsa.confd vinylncsa
+
+	newinitd "${FILESDIR}"/vinyld.initd vinyld
+	newconfd "${FILESDIR}"/vinyld.confd vinyld
+
+	insinto /etc/logrotate.d/
+	newins "${FILESDIR}/vinyld.logrotate" vinyld
+
+	diropts -m750
+
+	keepdir /var/lib/vinyl-cache
+	keepdir /var/log/vinyl-cache
+
+	systemd_dounit "${FILESDIR}/vinyld.service"
+
+	insinto /etc/vinyl-cache/
+	doins vmod/vmod_*.vcc
+	doins etc/example.vcl
+
+	dodoc README.md
+	dodoc doc/changes.rst
+
+	fowners root:vinyl /etc/vinyl-cache/
+	fowners vinyl:vinyl /var/lib/vinyl-cache/
+	fperms 0750 /var/lib/vinyl-cache/ /etc/vinyl-cache/
+
+	find "${ED}" -name "*.la" -delete || die
+}

www-servers/vinyl-cache/Manifest
@@ -1,2 +1,3 @@
 DIST varnish-8.0.0.tgz 4385715 BLAKE2B ebc6103ef2375eddf7ce6b89f8cb8b89900c3533dba97916d953bee20935cbccae814340d3a83b0fe1f6be211c950ba0a0d874dd0503621d8baeeb86333e4f23 SHA512 c381928e23deaacb863dcf389a494f30a56d22a4e88fe0c5dc7d4a93828f3dc0595c7ae41837f3549795828aca1a30e08f4456d4a752a6d12c19b61943dd99e9
 DIST varnish-8.0.1.tgz 4309436 BLAKE2B 802368affb3706968d2a5538bcf560d5875e9856046fb2a0e6cd2d110394ec6e2efd111d0284eb5f40b09eaac9686e249ae9a140b8ddea06e3e188db1842a0ec SHA512 6ca63160e068d4ae6aca9c7f2e848aabc5fe327bae18b3a008e573a27c0ed2ac958740fe129477cb384dded94b1c93cbd54efec5fd6f17098c23263d11ffc9ff
+DIST vinyl-cache-9.0.0.tgz 6353519 BLAKE2B 6cf36a66e74037a9b11d5048e0797fa2b4d6665d9463363d7d9816854d2a58744c00f684f6e274600459e8c124cfeff8405015e378c7ce12497ba65051331adb SHA512 c7be55b13609a0842bb938af39203b77a1ed17257e18f7ea29b89760687317c135dee48b8b2936561c00aeabe321e503b209b5b9af876588dd824c6c6847d17e

www-servers/vinyl-cache/files/vinyld.confd
@@ -1,0 +1,20 @@
+# /etc/conf.d/vinyld
+
+VINYLD="/usr/sbin/vinyld"
+VINYLADM="/usr/bin/vinyladm"
+
+CONFIGFILE="/etc/vinyl-cache/default.vcl"
+
+# Listen on localhost:8080 and connect to backend localhost:80
+#VINYLD_OPTS="-a localhost:8080 -a localhost:8080"
+
+# Alternatively, don't listen to a backend
+VINYLD_OPTS="-a localhost:8080"
+
+# Listen on all interfaces
+#VINYLD_OPTS="-a [::]:8080"
+
+# You may need to increase the number of open files (-n)
+# and the maximum amount off locked memory (-l)
+# See bug #459142
+#rc_ulimit="-n 32786 -l 82000"

www-servers/vinyl-cache/files/vinyld.initd
@@ -1,0 +1,77 @@
+#!/sbin/openrc-run
+# Copyright 1999-2026 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+VINYLD_PID=${VINYLD_PID:-/run/${SVCNAME}.pid}
+CONFIGFILES="${CONFIGFILE:-/etc/vinyl/default.vcl}"
+
+command="${VINYLD:-/usr/sbin/vinyld}"
+command_args="-j unix,user=vinyl -P ${VINYLD_PID} -f ${CONFIGFILE} ${VINYLD_OPTS}"
+pidfile="${VINYLD_PID}"
+
+extra_commands="configtest"
+extra_started_commands="reload"
+
+description_configtest="Run syntax tests for configuration files."
+description_reload="Reloads the configuration."
+
+depend() {
+	need net
+}
+
+configtest() {
+	ebegin "Checking ${SVCNAME} configuration"
+	checkconfig
+	eend $?
+}
+
+checkconfig() {
+	${VINYLD} -C -f ${CONFIGFILE} >/dev/null 2>&1
+	ret=$?
+	if [ $ret -ne 0 ]; then
+		eerror "${SVCNAME} has detected an error in your setup:"
+		${VINYLD} -C -f ${CONFIGFILE}
+	fi
+
+	return $ret
+}
+
+start_pre() {
+	checkconfig || return 1
+}
+
+stop_pre() {
+	if [ "${RC_CMD}" = "restart" ]; then
+		checkconfig || return 1
+	fi
+}
+
+reload() {
+	checkconfig || return 1
+
+	ebegin "Reloading vinyl"
+
+	$VINYLADM vcl.list >/dev/null 2>&1
+	ret=$?
+	if [ $ret -ne 0 ]; then
+		eerror "${SVCNAME} cannot list configuration"
+		return 1
+	fi
+
+	new_config="reload_$(date +%FT%H:%M:%S)"
+	$VINYLADM vcl.load $new_config $CONFIGFILE >/dev/null 2>&1
+	ret=$?
+	if [ $ret -ne 0 ]; then
+		eerror "${SVCNAME} cannot load configuration"
+		return 1
+	fi
+
+	$VINYLADM vcl.use $new_config >/dev/null 2>&1
+	ret=$?
+	if [ $ret -ne 0 ]; then
+		eerror "${SVCNAME} cannot switch configuration"
+		return 1
+	fi
+
+	eend 0
+}

www-servers/vinyl-cache/files/vinyld.logrotate
@@ -1,0 +1,31 @@
+/var/log/vinyl/vinylncsa.log {
+	daily
+	rotate 31
+	copytruncate
+	dateext
+	dateyesterday
+	delaycompress
+	compress
+	notifempty
+	missingok
+
+	prerotate
+		/etc/init.d/vinylncsa -q status && /etc/init.d/vinylncsa -q flush
+	endscript
+}
+
+/var/log/vinyl/vinyllog.log {
+	daily
+	rotate 31
+	copytruncate
+	dateext
+	dateyesterday
+	delaycompress
+	compress
+	notifempty
+	missingok
+
+	prerotate
+		/etc/init.d/vinyllog -q status && /etc/init.d/vinyllog -q flush
+	endscript
+}

www-servers/vinyl-cache/files/vinyld.service
@@ -1,0 +1,16 @@
+[Unit]
+Description=A high-performance caching HTTP reverse proxy
+After=network.target
+
+[Service]
+Type=forking
+LimitNOFILE=32786
+LimitMEMLOCK=82000
+PIDFile=/var/run/vinyld.pid
+ExecStart=/usr/sbin/vinyld -a 0.0.0.0:80 -P /var/run/vinyld.pid
+
+#If you wish to use a config file, then use the following ExecStart line
+#ExecStart=/usr/sbin/vinyld -a 0.0.0.0:80 -f /etc/vinyl-cache/default.vcl -P /var/run/vinyld.pid
+
+[Install]
+WantedBy=multi-user.target

www-servers/vinyl-cache/files/vinyllog.confd
@@ -1,0 +1,3 @@
+# /etc/conf.d/vinyllog
+
+VINYLLOG_OPTS="-a -w /var/log/vinyl/vinyllog.log"

www-servers/vinyl-cache/files/vinyllog.initd
@@ -1,0 +1,31 @@
+#!/sbin/openrc-run
+# Copyright 1999-2026 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+
+VINYLLOG_PID=${VINYLLOG_PID:-/run/${SVCNAME}.pid}
+
+command="${VINYLLOG:-/usr/bin/vinyllog}"
+command_args="-D -P ${VINYLLOG_PID} ${VINYLLOG_OPTS}"
+pidfile="${VINYLLOG_PID}"
+
+extra_started_commands="reload rotate flush"
+
+description_rotate="Rotate the log file"
+description_flush="Flush any outstanding transactions"
+
+rotate() {
+	ebegin "Rotating log file"
+	start-stop-daemon -p ${VINYLLOG_PID} -s SIGHUP
+	eend $?
+}
+
+flush() {
+	ebegin "Flushing any outstanding transactions"
+	start-stop-daemon -p ${VINYLLOG_PID} -s SIGUSR1
+	eend $?
+}
+
+reload() {
+	flush
+	rotate
+}

commit 0dae1b505a1491a487caf33fbe97b8247711627b

www-servers/vinyl-cache: apply shebang fix to 9.0.0
This fix was applied to 8.0.1 but wasn't carried forward to 9.0.0.

e655150e9eb8 ("www-servers/varnish: fix python shebang once")

We call fix-python-shebangs in src_prepare as some python programs are
used during configure (wflags.py) and build.

We skip vmodtool.py as this gets handled later in src_install by
python_replicate_script.

Bug: https://bugs.gentoo.org/971373
Closes: https://bugs.gentoo.org/971848
Fixes: 629470dfcf6579213ebeb53ca0adfbe643de692b
Signed-off-by: Brett A C Sheffield <bacs@librecast.net>
Part-of: https://codeberg.org/gentoo/gentoo/pulls/478
Merges: https://codeberg.org/gentoo/gentoo/pulls/478
Signed-off-by: Sam James <sam@gentoo.org>

www-servers/vinyl-cache/vinyl-cache-9.0.0.ebuild
@@ -57,7 +57,8 @@ src_prepare() {
 	default
 
 	# Fix python shebangs for python-exec[-native-symlinks], #909330
-	local shebangs=($(grep -rl "#!/usr/bin/env python3" || die))
+	# skip vmodtool, as that gets installed by python_replicate_script later
+	local shebangs=($(grep -rl "#!/usr/bin/env python3"|grep -v vmodtool || die))
 	python_setup
 	python_fix_shebang -q ${shebangs[*]}
 