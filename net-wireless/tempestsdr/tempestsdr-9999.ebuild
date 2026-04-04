gentoo/gentoo

commit 3188cf98818a10360c8a0485169c06613f4adecb

net-wireless/tempestsdr: add 0.0_p20250404, drop 0.0_p20250122-r2
Closes: https://bugs.gentoo.org/969330
Signed-off-by: Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>

net-wireless/tempestsdr/Manifest
@@ -1,1 +1,1 @@
-DIST tempestsdr-0.0_p20250122.tar.gz 67322644 BLAKE2B 20f8a91ef69462016b5cc4b83408f81780bd6c9299f82d35a96d5c3259c67ea1b63b2a22991443cfbd8a19c1eb2f4489e9e28c0703399e47ea804edf2c19a557 SHA512 1fbbca5926019028a2b1d6fa9ddecee83849d6a95b7dc0d87fcddbf59fe54b3a9f6f903eb38d187ef1de5932bc109cadd6144e5b4ce7f7f89ed10e1c1b608399
+DIST tempestsdr-0.0_p20250404.tar.gz 67322612 BLAKE2B 215c31a70835a926ac66b05784cd0b175b27e7da88b39f7c565666d7335c129a4f9c4694ef68ef98e1efdb1a2562d2a73f9144e8228cb7261c05680c2a0c9905 SHA512 c2f53182ba9a8cfab5cc3586820d2f9454b9cd68428aea28432fea1322d66a7836233b5f28ff4adc04fd6a43e5297ffe90606bab2111f5f813d1036b2c89c041

net-wireless/tempestsdr/tempestsdr-0.0_p20250404.ebuild
@@ -16,7 +16,7 @@ if [ "${PV}" = "9999" ]; then
 	EGIT_REPO_URI="https://github.com/deltj/TempestSDR.git"
 else
 	KEYWORDS="~amd64 ~x86"
-	COMMIT="59201a2cb21ab193125719eb318dcfbbf979c32e"
+	COMMIT="99afe0536a92db20f48afeff32e8027d9c11cfec"
 	SRC_URI="https://github.com/deltj/TempestSDR/archive/${COMMIT}.tar.gz -> ${P}.tar.gz"
 	S="${WORKDIR}/TempestSDR-${COMMIT}"
 fi

commit 6b9f811d75e8c07a35511da11b7cf061fe290b32

net-wireless/tempestsdr: fix typo in version
Fixes: 3188cf98818a10360c8a0485169c06613f4adecb
Signed-off-by: Rick Farina (Zero_Chaos) <zerochaos@gentoo.org>

net-wireless/tempestsdr/Manifest
@@ -1,1 +1,1 @@
-DIST tempestsdr-0.0_p20250404.tar.gz 67322612 BLAKE2B 215c31a70835a926ac66b05784cd0b175b27e7da88b39f7c565666d7335c129a4f9c4694ef68ef98e1efdb1a2562d2a73f9144e8228cb7261c05680c2a0c9905 SHA512 c2f53182ba9a8cfab5cc3586820d2f9454b9cd68428aea28432fea1322d66a7836233b5f28ff4adc04fd6a43e5297ffe90606bab2111f5f813d1036b2c89c041
+DIST tempestsdr-0.0_p20260404.tar.gz 67322612 BLAKE2B 215c31a70835a926ac66b05784cd0b175b27e7da88b39f7c565666d7335c129a4f9c4694ef68ef98e1efdb1a2562d2a73f9144e8228cb7261c05680c2a0c9905 SHA512 c2f53182ba9a8cfab5cc3586820d2f9454b9cd68428aea28432fea1322d66a7836233b5f28ff4adc04fd6a43e5297ffe90606bab2111f5f813d1036b2c89c041

net-wireless/tempestsdr/tempestsdr-9999.ebuild
@@ -16,7 +16,7 @@ if [ "${PV}" = "9999" ]; then
 	EGIT_REPO_URI="https://github.com/deltj/TempestSDR.git"
 else
 	KEYWORDS="~amd64 ~x86"
-	COMMIT="59201a2cb21ab193125719eb318dcfbbf979c32e"
+	COMMIT="99afe0536a92db20f48afeff32e8027d9c11cfec"
 	SRC_URI="https://github.com/deltj/TempestSDR/archive/${COMMIT}.tar.gz -> ${P}.tar.gz"
 	S="${WORKDIR}/TempestSDR-${COMMIT}"
 fi