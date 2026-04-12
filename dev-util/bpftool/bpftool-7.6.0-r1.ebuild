gentoo/gentoo

commit 23eae8ba4c0d28cdbe82d1d30e4148cab2fedde8

dev-util/bpftool: fix kernel config check
Call linux-info_pkg_setup so we actually process CONFIG_CHECK.

Also, revbump, because this causes breakage in reverse dependencies
like libcap-ng[bpf] even if bpftool builds and installs fine.

Closes: https://bugs.gentoo.org/972555
Signed-off-by: Sam James <sam@gentoo.org>

dev-util/bpftool/bpftool-7.6.0-r1.ebuild
@@ -66,7 +66,8 @@ BDEPEND="
 CONFIG_CHECK="~DEBUG_INFO_BTF"
 
 pkg_setup() {
-	python-any-r1_pkg_setup
+	linux-info_pkg_setup
+	linux-info_pkg_setup
 	use llvm && llvm-r1_pkg_setup
 }
 

dev-util/bpftool/bpftool-7.7.0-r1.ebuild
@@ -66,6 +66,7 @@ BDEPEND="
 CONFIG_CHECK="~DEBUG_INFO_BTF"
 
 pkg_setup() {
+	linux-info_pkg_setup
 	python-any-r1_pkg_setup
 	use llvm && llvm-r1_pkg_setup
 }

dev-util/bpftool/bpftool-9999.ebuild
@@ -66,6 +66,7 @@ BDEPEND="
 CONFIG_CHECK="~DEBUG_INFO_BTF"
 
 pkg_setup() {
+	linux-info_pkg_setup
 	python-any-r1_pkg_setup
 	use llvm && llvm-r1_pkg_setup
 }

commit f0b7e544d608e25da2d86536c8a2528aa81aa2ad

dev-util/bpftool: fix pasto
Fixes: 23eae8ba4c0d28cdbe82d1d30e4148cab2fedde8
Signed-off-by: Sam James <sam@gentoo.org>

dev-util/bpftool/bpftool-7.6.0-r1.ebuild
@@ -67,7 +67,7 @@ CONFIG_CHECK="~DEBUG_INFO_BTF"
 
 pkg_setup() {
 	linux-info_pkg_setup
-	linux-info_pkg_setup
+	python-any-r1_pkg_setup
 	use llvm && llvm-r1_pkg_setup
 }
 