gentoo/gentoo

commit 9ca375c806a17c7d66dedbfe34b1511680e447d3

dev-build/cmake: set jobs to 1
Higher values break tests.

Signed-off-by: Paul Zander <negril.nx+gentoo@gmail.com>
Signed-off-by: Sam James <sam@gentoo.org>

dev-build/cmake/cmake-4.1.2-r1.ebuild
@@ -293,6 +293,8 @@ src_test() {
 
 	local myctestargs=(
 		--output-on-failure
+		# higher number breaks tests
+		-j1
 	)
 
 	local -x QT_QPA_PLATFORM=offscreen

dev-build/cmake/cmake-4.1.4.ebuild
@@ -294,6 +294,8 @@ src_test() {
 
 	local myctestargs=(
 		--output-on-failure
+		# higher number breaks tests
+		-j1
 	)
 
 	local -x QT_QPA_PLATFORM=offscreen

dev-build/cmake/cmake-4.1.5.ebuild
@@ -294,6 +294,8 @@ src_test() {
 
 	local myctestargs=(
 		--output-on-failure
+		# higher number breaks tests
+		-j1
 	)
 
 	local -x QT_QPA_PLATFORM=offscreen

dev-build/cmake/cmake-4.2.4.ebuild
@@ -293,6 +293,8 @@ src_test() {
 
 	local myctestargs=(
 		--output-on-failure
+		# higher number breaks tests
+		-j1
 	)
 
 	local -x QT_QPA_PLATFORM=offscreen

dev-build/cmake/cmake-4.3.1.ebuild
@@ -293,6 +293,8 @@ src_test() {
 
 	local myctestargs=(
 		--output-on-failure
+		# higher number breaks tests
+		-j1
 	)
 
 	local -x QT_QPA_PLATFORM=offscreen

dev-build/cmake/cmake-9999.ebuild
@@ -293,6 +293,8 @@ src_test() {
 
 	local myctestargs=(
 		--output-on-failure
+		# higher number breaks tests
+		-j1
 	)
 
 	local -x QT_QPA_PLATFORM=offscreen

commit 26c8b0fabda71989e3c6a4ae7c4e050a0fb1b147

dev-build/cmake: no longer set jobs to 1
The intended behaviour was/is to honour the user set jobs. `ctest` calls nested
`ctest` leading to higher jobs then wanted.

Using `CTEST_PARALLEL_LEVEL` would be the correct way to do this, as it only
affects `ctest` calls where `-j`/`--parallel` is not set as we do in
`cmake.eclass`.

Fixes: 9ca375c806a17c7d66dedbfe34b1511680e447d3
Signed-off-by: Paul Zander <negril.nx+gentoo@gmail.com>

dev-build/cmake/cmake-4.1.2-r1.ebuild
@@ -291,11 +291,6 @@ src_test() {
 		"RunCMake.CMP0125"
 	)
 
-	local myctestargs=(
-		# higher number breaks tests
-		-j1
-	)
-
 	local -x QT_QPA_PLATFORM=offscreen
 
 	cmake_src_test

dev-build/cmake/cmake-4.1.4.ebuild
@@ -292,11 +292,6 @@ src_test() {
 		"RunCMake.CMP0125"
 	)
 
-	local myctestargs=(
-		# higher number breaks tests
-		-j1
-	)
-
 	local -x QT_QPA_PLATFORM=offscreen
 
 	cmake_src_test

dev-build/cmake/cmake-4.1.5.ebuild
@@ -292,11 +292,6 @@ src_test() {
 		"RunCMake.CMP0125"
 	)
 
-	local myctestargs=(
-		# higher number breaks tests
-		-j1
-	)
-
 	local -x QT_QPA_PLATFORM=offscreen
 
 	cmake_src_test

dev-build/cmake/cmake-4.2.4.ebuild
@@ -291,11 +291,6 @@ src_test() {
 		"RunCMake.CMP0125"
 	)
 
-	local myctestargs=(
-		# higher number breaks tests
-		-j1
-	)
-
 	local -x QT_QPA_PLATFORM=offscreen
 
 	cmake_src_test

dev-build/cmake/cmake-4.3.1.ebuild
@@ -291,11 +291,6 @@ src_test() {
 		"RunCMake.CMP0125"
 	)
 
-	local myctestargs=(
-		# higher number breaks tests
-		-j1
-	)
-
 	local -x QT_QPA_PLATFORM=offscreen
 
 	cmake_src_test

dev-build/cmake/cmake-9999.ebuild
@@ -291,11 +291,6 @@ src_test() {
 		"RunCMake.CMP0125"
 	)
 
-	local myctestargs=(
-		# higher number breaks tests
-		-j1
-	)
-
 	local -x QT_QPA_PLATFORM=offscreen
 
 	cmake_src_test