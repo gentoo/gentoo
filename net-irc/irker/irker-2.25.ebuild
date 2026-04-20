gentoo/gentoo

commit 283e0a122c9511c12b2d5e061ce8e5a9cca2731d

net-irc/irker: add 2.25
Signed-off-by: Sam James <sam@gentoo.org>

net-irc/irker/irker-2.25.ebuild
@@ -1,0 +1,66 @@
+# Copyright 1999-2026 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+PYTHON_COMPAT=( python3_{11..14} )
+PYTHON_REQ_USE="ssl"
+
+inherit optfeature python-single-r1 systemd
+
+DESCRIPTION="Submission tools for IRC notifications"
+HOMEPAGE="http://www.catb.org/esr/irker/ https://gitlab.com/esr/irker"
+SRC_URI="http://www.catb.org/esr/${PN}/${P}.tar.gz"
+
+LICENSE="BSD-2"
+SLOT="0"
+KEYWORDS="~amd64 ~arm ~arm64 ~hppa ~ppc ~ppc64 ~s390 ~sparc ~x86"
+REQUIRED_USE="${PYTHON_REQUIRED_USE}"
+
+# Dependency notes:
+# NOTE: No pkgconfig dep here because of the systemd sed below
+# NOTE: No need for asciidoc here as it's only used for the
+# 'release' makefile target.
+RDEPEND="${PYTHON_DEPS}"
+BDEPEND="
+	app-text/docbook-xml-dtd:4.1.2
+	app-text/xmlto
+	${PYTHON_DEPS}
+"
+
+DOCS=( NEWS README hacking.adoc security.adoc )
+HTML_DOCS=( irkerd.html irkerhook.html )
+
+PATCHES=(
+	"${FILESDIR}"/${PN}-2.24-password-file-typo.patch
+)
+
+src_prepare() {
+	default
+
+	# Rely on systemd eclass for systemd service install
+	sed -i -e "/^SYSTEMDSYSTEMUNITDIR/d" Makefile || die "sed failed"
+
+	# Prefix support
+	sed -e "s|@EPREFIX@|${EPREFIX}|" "${FILESDIR}"/irkerd.service > "${WORKDIR}"/irkerd.service || die "sed failed"
+}
+
+src_install() {
+	default
+
+	python_doscript "${ED}"/usr/bin/irkerd
+	# Not installed with the default Makefile
+	python_doscript irk irkerhook.py
+
+	newinitd "${FILESDIR}"/irkerd.initd-r1 irkerd
+	newconfd "${FILESDIR}"/irkerd.confd-r1 irkerd
+
+	systemd_dounit "${WORKDIR}"/irkerd.service
+
+	docinto examples
+	dodoc filter-example.py filter-test.py
+}
+
+pkg_postinst() {
+	optfeature "SOCKS5 proxy support" dev-python/pysocks
+}

net-irc/irker/Manifest
@@ -1,1 +1,2 @@
 DIST irker-2.24.tar.gz 45728 BLAKE2B e1db85ea21163eb831cdfc9c9961bd77377890caef5af60d979c6fdc523c7b3680af6b1ce2e36bfda620e1785067381e1ac4ffb96e47314cac68be6014885f78 SHA512 a4f686c05c8018be8caf480d6e0d368042f82905cd4d67ede4a6cb0ff65a067d25116f68c3d3ea849db6f15a8f88c48ef2a314fa92a96bce38796a2b236b4a43
+DIST irker-2.25.tar.gz 49079 BLAKE2B 99c238c480065fb99e1a645626914f027392c48ff2cb95b3abbdf81745b7263e995e06e47ef5380e87e1695d1b24ecbc8ac9f690cf3cbf2532f5ca3efd134d7a SHA512 ee9572d346e5ff11150db6e238b2af579e2f21a2bc7da043ddc1cc19a9f64702d8be7aec7cec54200b7fa095ecd8836ca1961c857d21c380a31bdd1ce6d13b05

commit f7c3034e14ffd78c49eeef1b56b12656e874808e

net-irc/irker: fix 2.25
When bumping to 2.25 quickly in 283e0a122c9511c12b2d5e061ce8e5a9cca2731d,
after fixing my error from a12428d8c185cf8001265c3950049d8041e7e2ba, I was
clearly in a hurry and copied the wrong ebuild.

Closes: https://bugs.gentoo.org/973010
Fixes: 283e0a122c9511c12b2d5e061ce8e5a9cca2731d
Signed-off-by: Sam James <sam@gentoo.org>

net-irc/irker/irker-2.25.ebuild
@@ -28,15 +28,14 @@ BDEPEND="
 	${PYTHON_DEPS}
 "
 
-DOCS=( NEWS README hacking.adoc security.adoc )
+DOCS=( NEWS {README,hacking,security}.adoc )
 HTML_DOCS=( irkerd.html irkerhook.html )
-
-PATCHES=(
-	"${FILESDIR}"/${PN}-2.24-password-file-typo.patch
-)
 
 src_prepare() {
 	default
+
+	# 'install' should not depend on 'uninstall'
+	sed -i -e '/^install:/s:uninstall::' Makefile || die
 
 	# Rely on systemd eclass for systemd service install
 	sed -i -e "/^SYSTEMDSYSTEMUNITDIR/d" Makefile || die "sed failed"
@@ -46,7 +45,8 @@ src_prepare() {
 }
 
 src_install() {
-	default
+	emake DESTDIR="${D}" PREFIX="${EPREFIX}"/usr install
+	einstalldocs
 
 	python_doscript "${ED}"/usr/bin/irkerd
 	# Not installed with the default Makefile