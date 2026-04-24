gentoo/gentoo

commit 56ba436ee30df1ed54a88e9cd895505c3955d184

net-misc/httpstat: enable py3.13, py3.14
Closes: https://bugs.gentoo.org/952612
Signed-off-by: Sam James <sam@gentoo.org>

net-misc/httpstat/httpstat-1.3.1-r1.ebuild
@@ -1,10 +1,10 @@
-# Copyright 1999-2024 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
 
 DISTUTILS_USE_PEP517=setuptools
-PYTHON_COMPAT=( python3_{10..12} pypy3 )
+PYTHON_COMPAT=( python3_{11..14} pypy3 )
 inherit distutils-r1
 
 DESCRIPTION="httpstat visualizes cURL statistics in a way of beauty and clarity"

commit 06b78ad6eddbbc6c8ea9e66c0098950cdb7b6b58

net-misc/httpstat: disable py3.14
py3.14 doesn't work:
```
  File "/usr/lib/python3.14/site-packages/setuptools/build_meta.py", line 317, in run_setup
    exec(code, locals())
    ~~~~^^^^^^^^^^^^^^^^
  File "<string>", line 30, in <module>
  File "<string>", line 17, in get_version
AttributeError: 'Constant' object has no attribute 's'
 * ERROR: net-misc/httpstat-1.3.1-r1::gentoo failed (compile phase):
 *   Wheel build failed
 *
```

I didn't notice because of wheel reuse (now disabled it).

Fixes: 56ba436ee30df1ed54a88e9cd895505c3955d184
Signed-off-by: Sam James <sam@gentoo.org>

net-misc/httpstat/httpstat-1.3.1-r1.ebuild
@@ -1,10 +1,10 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
 
 DISTUTILS_USE_PEP517=setuptools
-PYTHON_COMPAT=( python3_{11..14} pypy3 )
+PYTHON_COMPAT=( python3_{11..13} pypy3 )
 inherit distutils-r1
 
 DESCRIPTION="httpstat visualizes cURL statistics in a way of beauty and clarity"