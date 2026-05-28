gentoo/gentoo

commit d9c4fce156e78b321e8a05683759cee6f55f7f58

media-video/vlc: fix cxx standard passing
Make the standard explicit instead of implicit. This should fix building
with `<=gcc-15`.

Fixes: 7d1d350e22b8ac72d988d9986822372cbae343fa
Closes: https://bugs.gentoo.org/976173
Signed-off-by: Paul Zander <negril.nx+gentoo@gmail.com>
Part-of: https://github.com/gentoo/gentoo/pull/46315
Signed-off-by: Sam James <sam@gentoo.org>

media-video/vlc/vlc-3.0.23-r1.ebuild
@@ -60,6 +60,7 @@ REQUIRED_USE="
 "
 # live+snapshots need bison+flex
 BDEPEND="
+	dev-build/autoconf-archive
 	sys-devel/bison
 	sys-devel/flex
 	>=sys-devel/gettext-0.19.8
@@ -258,6 +259,19 @@ src_prepare() {
 	if ! use dbus ; then
 		sed -i 's/ --started-from-file//' share/vlc.desktop.in || die
 	fi
+
+	# old bundled version
+	# so we need to call AX_CXX_COMPILE_STDCXX directly
+	rm \
+		m4/ax_cxx_compile_stdcxx.m4 \
+		m4/ax_cxx_compile_stdcxx_17.m4 \
+		|| die
+	local CXXSTD="17"
+	if has_version ">=dev-cpp/abseil-cpp-20260107.0"; then
+		# needs >=c++20
+		CXXSTD="20"
+	fi
+	sed -i -e "/AX_CXX_COMPILE_STDCXX/{s/_17(/(${CXXSTD}, /}" configure.ac || die
 
 	eautoreconf
 }

media-video/vlc/vlc-3.0.23.ebuild
@@ -61,6 +61,7 @@ REQUIRED_USE="
 "
 # live+snapshots need bison+flex
 BDEPEND="
+	dev-build/autoconf-archive
 	sys-devel/bison
 	sys-devel/flex
 	>=sys-devel/gettext-0.19.8
@@ -275,8 +276,18 @@ src_prepare() {
 		sed -i 's/ --started-from-file//' share/vlc.desktop.in || die
 	fi
 
-	# do not pass -std=c++17 if it's not needed for c++17 features
-	sed -e '/AX_CXX_COMPILE_STDCXX_17/s/\[noext\]/[]/' -i configure.ac || die
+	# old bundled version
+	# so we need to call AX_CXX_COMPILE_STDCXX directly
+	rm \
+		m4/ax_cxx_compile_stdcxx.m4 \
+		m4/ax_cxx_compile_stdcxx_17.m4 \
+		|| die
+	local CXXSTD="17"
+	if has_version ">=dev-cpp/abseil-cpp-20260107.0"; then
+		# needs >=c++20
+		CXXSTD="20"
+	fi
+	sed -i -e "/AX_CXX_COMPILE_STDCXX/{s/_17(/(${CXXSTD}, /}" configure.ac || die
 
 	eautoreconf
 }

media-video/vlc/vlc-3.0.9999.ebuild
@@ -60,6 +60,7 @@ REQUIRED_USE="
 "
 # live+snapshots need bison+flex
 BDEPEND="
+	dev-build/autoconf-archive
 	sys-devel/bison
 	sys-devel/flex
 	>=sys-devel/gettext-0.19.8
@@ -259,8 +260,18 @@ src_prepare() {
 		sed -i 's/ --started-from-file//' share/vlc.desktop.in || die
 	fi
 
-	# do not pass -std=c++17 if it's not needed for c++17 features
-	sed -e '/AX_CXX_COMPILE_STDCXX_17/s/\[noext\]/[]/' -i configure.ac || die
+	# old bundled version
+	# so we need to call AX_CXX_COMPILE_STDCXX directly
+	rm \
+		m4/ax_cxx_compile_stdcxx.m4 \
+		m4/ax_cxx_compile_stdcxx_17.m4 \
+		|| die
+	local CXXSTD="17"
+	if has_version ">=dev-cpp/abseil-cpp-20260107.0"; then
+		# needs >=c++20
+		CXXSTD="20"
+	fi
+	sed -i -e "/AX_CXX_COMPILE_STDCXX/{s/_17(/(${CXXSTD}, /}" configure.ac || die
 
 	eautoreconf
 }

media-video/vlc/vlc-4.0.0_pre20260418.ebuild
@@ -61,6 +61,7 @@ REQUIRED_USE="
 "
 # live+snapshots need bison+flex
 BDEPEND="
+	dev-build/autoconf-archive
 	sys-devel/bison
 	sys-devel/flex
 	>=sys-devel/gettext-0.19.8
@@ -278,8 +279,18 @@ src_prepare() {
 		sed -i 's/ --started-from-file//' share/vlc.desktop.in || die
 	fi
 
-	# do not pass -std=c++17 if it's not needed for c++17 features
-	sed -e '/AX_CXX_COMPILE_STDCXX_17/s/\[noext\]/[]/' -i configure.ac || die
+	# old bundled version
+	# so we need to call AX_CXX_COMPILE_STDCXX directly
+	rm \
+		m4/ax_cxx_compile_stdcxx.m4 \
+		m4/ax_cxx_compile_stdcxx_17.m4 \
+		|| die
+	local CXXSTD="17"
+	if has_version ">=dev-cpp/abseil-cpp-20260107.0"; then
+		# needs >=c++20
+		CXXSTD="20"
+	fi
+	sed -i -e "/AX_CXX_COMPILE_STDCXX/{s/_17(/(${CXXSTD}, /}" configure.ac || die
 
 	eautoreconf
 }

media-video/vlc/vlc-4.0.0_pre20260515.ebuild
@@ -61,6 +61,7 @@ REQUIRED_USE="
 "
 # live+snapshots need bison+flex
 BDEPEND="
+	dev-build/autoconf-archive
 	sys-devel/bison
 	sys-devel/flex
 	>=sys-devel/gettext-0.19.8
@@ -276,6 +277,19 @@ src_prepare() {
 	if ! use dbus ; then
 		sed -i 's/ --started-from-file//' share/vlc.desktop.in || die
 	fi
+
+	# old bundled version
+	# so we need to call AX_CXX_COMPILE_STDCXX directly
+	rm \
+		m4/ax_cxx_compile_stdcxx.m4 \
+		m4/ax_cxx_compile_stdcxx_17.m4 \
+		|| die
+	local CXXSTD="17"
+	if has_version ">=dev-cpp/abseil-cpp-20260107.0"; then
+		# needs >=c++20
+		CXXSTD="20"
+	fi
+	sed -i -e "/AX_CXX_COMPILE_STDCXX/{s/_17(/(${CXXSTD}, /}" configure.ac || die
 
 	eautoreconf
 }

media-video/vlc/vlc-9999.ebuild
@@ -61,6 +61,7 @@ REQUIRED_USE="
 "
 # live+snapshots need bison+flex
 BDEPEND="
+	dev-build/autoconf-archive
 	sys-devel/bison
 	sys-devel/flex
 	>=sys-devel/gettext-0.19.8
@@ -276,8 +277,18 @@ src_prepare() {
 		sed -i 's/ --started-from-file//' share/vlc.desktop.in || die
 	fi
 
-	# do not pass -std=c++17 if it's not needed for c++17 features
-	sed -e '/AX_CXX_COMPILE_STDCXX_17/s/\[noext\]/[]/' -i configure.ac || die
+	# old bundled version
+	# so we need to call AX_CXX_COMPILE_STDCXX directly
+	rm \
+		m4/ax_cxx_compile_stdcxx.m4 \
+		m4/ax_cxx_compile_stdcxx_17.m4 \
+		|| die
+	local CXXSTD="17"
+	if has_version ">=dev-cpp/abseil-cpp-20260107.0"; then
+		# needs >=c++20
+		CXXSTD="20"
+	fi
+	sed -i -e "/AX_CXX_COMPILE_STDCXX/{s/_17(/(${CXXSTD}, /}" configure.ac || die
 
 	eautoreconf
 }

commit fde53bd52286e6f60323d2b9025d8cad20f5cfe8

media-video/vlc: fix cxx standard passing for 3.0.x
Fixes: d9c4fce156e78b321e8a05683759cee6f55f7f58
Closes: https://bugs.gentoo.org/976256
Signed-off-by: Paul Zander <negril.nx+gentoo@gmail.com>

media-video/vlc/vlc-3.0.23-r1.ebuild
@@ -260,18 +260,12 @@ src_prepare() {
 		sed -i 's/ --started-from-file//' share/vlc.desktop.in || die
 	fi
 
-	# old bundled version
-	# so we need to call AX_CXX_COMPILE_STDCXX directly
-	rm \
-		m4/ax_cxx_compile_stdcxx.m4 \
-		m4/ax_cxx_compile_stdcxx_17.m4 \
-		|| die
 	local CXXSTD="17"
 	if has_version ">=dev-cpp/abseil-cpp-20260107.0"; then
 		# needs >=c++20
 		CXXSTD="20"
 	fi
-	sed -i -e "/AX_CXX_COMPILE_STDCXX/{s/_17(/(${CXXSTD}, /}" configure.ac || die
+	sed -i -e "/AX_CXX_COMPILE_STDCXX/{s/_[0-9]*(/(${CXXSTD}, /}" configure.ac || die
 
 	eautoreconf
 }

media-video/vlc/vlc-3.0.23.ebuild
@@ -276,18 +276,12 @@ src_prepare() {
 		sed -i 's/ --started-from-file//' share/vlc.desktop.in || die
 	fi
 
-	# old bundled version
-	# so we need to call AX_CXX_COMPILE_STDCXX directly
-	rm \
-		m4/ax_cxx_compile_stdcxx.m4 \
-		m4/ax_cxx_compile_stdcxx_17.m4 \
-		|| die
 	local CXXSTD="17"
 	if has_version ">=dev-cpp/abseil-cpp-20260107.0"; then
 		# needs >=c++20
 		CXXSTD="20"
 	fi
-	sed -i -e "/AX_CXX_COMPILE_STDCXX/{s/_17(/(${CXXSTD}, /}" configure.ac || die
+	sed -i -e "/AX_CXX_COMPILE_STDCXX/{s/_[0-9]*(/(${CXXSTD}, /}" configure.ac || die
 
 	eautoreconf
 }

media-video/vlc/vlc-3.0.9999.ebuild
@@ -260,18 +260,12 @@ src_prepare() {
 		sed -i 's/ --started-from-file//' share/vlc.desktop.in || die
 	fi
 
-	# old bundled version
-	# so we need to call AX_CXX_COMPILE_STDCXX directly
-	rm \
-		m4/ax_cxx_compile_stdcxx.m4 \
-		m4/ax_cxx_compile_stdcxx_17.m4 \
-		|| die
 	local CXXSTD="17"
 	if has_version ">=dev-cpp/abseil-cpp-20260107.0"; then
 		# needs >=c++20
 		CXXSTD="20"
 	fi
-	sed -i -e "/AX_CXX_COMPILE_STDCXX/{s/_17(/(${CXXSTD}, /}" configure.ac || die
+	sed -i -e "/AX_CXX_COMPILE_STDCXX/{s/_[0-9]*(/(${CXXSTD}, /}" configure.ac || die
 
 	eautoreconf
 }