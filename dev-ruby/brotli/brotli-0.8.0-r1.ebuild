gentoo/gentoo

commit 8b72a01e0209d925b24d782d8b85438e38e87070

dev-ruby/brotli: add 0.8.0
Signed-off-by: Arthur Zamarin <arthurzam@gentoo.org>

dev-ruby/brotli/Manifest
@@ -1,2 +1,3 @@
 DIST brotli-0.6.0.tar.gz 9546 BLAKE2B 82e6bf5d49466b1f08c3ffdf6d4607e9af33d2e5ae8b1e006c518a1effadd294068aac94ac2566a81930168525c0e55ffc6274c5a90d7429be6599734b948fa5 SHA512 b48dad753d823a4a26ca98614a681ac14b2191b0d4ea3be2a8bdc3f457ddf21504ad1703ab307821d408785301e1554ac91f8ffed35609c89a9e9a38b1640760
 DIST brotli-0.7.0.tar.gz 11792 BLAKE2B fd373108ed2c54d6856f917dcd9b54306ee22693cfd2e752a07eccf7270cd3ed8e8412e39e176689f4fe32713a01d40044a7d48ede969c03bdf6052b9691c4d8 SHA512 134890f1110449a0ea96489b3ede51df075ced7d298e9c2e9f1d9e06fc0abc7c253599a80ecda4e04fa94e76a9fb19d95b46b26f91234de7f3316dc428853053
+DIST brotli-0.8.0.tar.gz 17684 BLAKE2B f7cb0de39a50a125ddaa0a822ce0f4d75a14553650d0db2cf7a21f8a1edc6839202445abea72f5c7c311a5118c0f01dc1a87d3b7e03097b59accc8751e25f5b0 SHA512 85f8cd4d5edcc22e64f6b42e02936152f3a0e46d4ce9438f0424ecb768be51bb9b7d05af6c51144c5b1e150bc6bd4fba38171820aeb78ca66337fcb07bf7e14d

dev-ruby/brotli/brotli-0.8.0.ebuild
@@ -1,0 +1,32 @@
+# Copyright 1999-2026 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+USE_RUBY="ruby32 ruby33 ruby34 ruby40"
+RUBY_FAKEGEM_EXTRADOC="README.md"
+RUBY_FAKEGEM_RECIPE_TEST="rspec3"
+RUBY_FAKEGEM_BINWRAP=""
+RUBY_FAKEGEM_GEMSPEC="brotli.gemspec"
+RUBY_FAKEGEM_EXTENSIONS=(ext/brotli/extconf.rb)
+
+inherit ruby-fakegem
+
+DESCRIPTION="Brotli compressor/decompressor"
+HOMEPAGE="https://github.com/miyucy/brotli"
+SRC_URI="https://github.com/miyucy/brotli/archive/v${PV}.tar.gz -> ${P}.tar.gz"
+
+LICENSE="MIT"
+SLOT="$(ver_cut 1)"
+KEYWORDS="~amd64 ~ppc64"
+# Depends on the test data in app-arch/brotli
+RESTRICT="test"
+
+DEPEND=">=app-arch/brotli-1.1.0:="
+RDEPEND="${DEPEND}"
+
+all_ruby_prepare() {
+	sed -e 's/git ls-files -z -- spec/find spec -print0/' \
+		-e 's/git ls-files -z/find * -print0/' \
+		-i ${RUBY_FAKEGEM_GEMSPEC} || die
+}

commit b4a51ea0e75914933666f3abb806f9a5e980953c

dev-ruby/brotli: fix gemspec, enable tests
* Due to upstream changes the gemspec no longer worked for Gentoo. It
  was installed as an empty file due to errors.
* Hookup tests and drop ppc64 due to a new test dependency. Dropping
  isn't a big deal as this release wasn't fully working either way.

Fixes: 8b72a01e0209d925b24d782d8b85438e38e87070
Signed-off-by: Alfred Wingate <parona@protonmail.com>
Part-of: https://codeberg.org/gentoo/gentoo/pulls/1553
Signed-off-by: Sam James <sam@gentoo.org>

dev-ruby/brotli/Manifest
@@ -1,3 +1,7 @@
 DIST brotli-0.6.0.tar.gz 9546 BLAKE2B 82e6bf5d49466b1f08c3ffdf6d4607e9af33d2e5ae8b1e006c518a1effadd294068aac94ac2566a81930168525c0e55ffc6274c5a90d7429be6599734b948fa5 SHA512 b48dad753d823a4a26ca98614a681ac14b2191b0d4ea3be2a8bdc3f457ddf21504ad1703ab307821d408785301e1554ac91f8ffed35609c89a9e9a38b1640760
 DIST brotli-0.7.0.tar.gz 11792 BLAKE2B fd373108ed2c54d6856f917dcd9b54306ee22693cfd2e752a07eccf7270cd3ed8e8412e39e176689f4fe32713a01d40044a7d48ede969c03bdf6052b9691c4d8 SHA512 134890f1110449a0ea96489b3ede51df075ced7d298e9c2e9f1d9e06fc0abc7c253599a80ecda4e04fa94e76a9fb19d95b46b26f91234de7f3316dc428853053
-DIST brotli-0.8.0.tar.gz 17684 BLAKE2B f7cb0de39a50a125ddaa0a822ce0f4d75a14553650d0db2cf7a21f8a1edc6839202445abea72f5c7c311a5118c0f01dc1a87d3b7e03097b59accc8751e25f5b0 SHA512 85f8cd4d5edcc22e64f6b42e02936152f3a0e46d4ce9438f0424ecb768be51bb9b7d05af6c51144c5b1e150bc6bd4fba38171820aeb78ca66337fcb07bf7e14d
+DIST brotli-028fb5a2-alice29.txt 152089 BLAKE2B ea900856d3ae0ed2fea1923e557824bd09583f7c1be25aa778a43812d945318e1d911e682e318861979b5a479765b34e15a926d257f883ff2fb0df418ebf9966 SHA512 d93d674d66b227d7b3f4e1b7c35b102c40800e728bff68c7821109e7db7adf2f0b76a67bc9bd53b0202ac8daa0b22145f004dbdc6b59a48a6c8c72061bf1989f
+DIST brotli-028fb5a2-alice29.txt.compressed 50096 BLAKE2B 6a7de6277c8d95b6ad6d75efb9071ca260dc238fe8c6267bca19c0bcce0dad7bda31b99d251b49c2b4f8747140451fdf235a8aa12af890f7bfa5c654f859e766 SHA512 6cf23938a93c2ca9278db8486a593f0927ce64f4074fe8e94e310d998033fe93008942b15b659e926bb1a56a0b277fc8893639566c73ff680b933f32358e4a1c
+DIST brotli-028fb5a2-zerosukkanooa 262263 BLAKE2B a251f2ffcd05f1d05f9fda83b98e255d05aad7065d23c37a3c56014cd502ef2af2822e45c3551ca346900c3dc2010133e1225fb309c2e9ec9e26e587e000df7c SHA512 bbf6a610eb0a0ed4eb9b34188133db41cba5e719d0faedde79d854b9f884989db9e623f018c00de95d6b0a2d8fe1a5807148f4a1a428d1279117235cd7906a58
+DIST brotli-028fb5a2-zerosukkanooa.compressed 107 BLAKE2B 598c997f8cf8dd62fb8e4dd394ce3362068cc66a4de03e20e1de81b38658cba1ea2d1a2730067c29c1b273f69e416370e5ce1e0419fe9880bc51534aab3c6a95 SHA512 4edd0519b1251b9e32247fb1d2332d605dfc5c726f0f0eb0495815724b721a3ee67aacfc5e2a2e64788dd9d2c5a3181baa83048bac0dd51a0b114fd7de610aa2
+DIST ruby-brotli-0.8.0.tar.gz 17684 BLAKE2B f7cb0de39a50a125ddaa0a822ce0f4d75a14553650d0db2cf7a21f8a1edc6839202445abea72f5c7c311a5118c0f01dc1a87d3b7e03097b59accc8751e25f5b0 SHA512 85f8cd4d5edcc22e64f6b42e02936152f3a0e46d4ce9438f0424ecb768be51bb9b7d05af6c51144c5b1e150bc6bd4fba38171820aeb78ca66337fcb07bf7e14d

dev-ruby/brotli/brotli-0.8.0-r1.ebuild
@@ -1,0 +1,72 @@
+# Copyright 1999-2026 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+USE_RUBY="ruby32 ruby33 ruby34 ruby40"
+RUBY_FAKEGEM_EXTRADOC="README.md"
+RUBY_FAKEGEM_BINWRAP=""
+RUBY_FAKEGEM_GEMSPEC="brotli.gemspec"
+RUBY_FAKEGEM_EXTENSIONS=(ext/brotli/extconf.rb)
+RUBY_FAKEGEM_EXTENSION_LIBDIR="lib/brotli"
+
+inherit ruby-fakegem
+
+# upstream brotli aggressively strips files from generated git archives. So you need to fetch the test files one by one.
+BROTLI_COMMIT="028fb5a23661f123017c060daa546b55cf4bde29"
+
+DESCRIPTION="Brotli compressor/decompressor"
+HOMEPAGE="https://github.com/miyucy/brotli"
+SRC_URI="
+	https://github.com/miyucy/brotli/archive/v${PV}.tar.gz -> ruby-${P}.tar.gz
+	test? (
+		https://github.com/google/brotli/raw/${BROTLI_COMMIT}/tests/testdata/alice29.txt
+			-> brotli-${BROTLI_COMMIT:0:8}-alice29.txt
+		https://github.com/google/brotli/raw/${BROTLI_COMMIT}/tests/testdata/alice29.txt.compressed
+			-> brotli-${BROTLI_COMMIT:0:8}-alice29.txt.compressed
+		https://github.com/google/brotli/raw/${BROTLI_COMMIT}/tests/testdata/zerosukkanooa
+			-> brotli-${BROTLI_COMMIT:0:8}-zerosukkanooa
+		https://github.com/google/brotli/raw/${BROTLI_COMMIT}/tests/testdata/zerosukkanooa.compressed
+			-> brotli-${BROTLI_COMMIT:0:8}-zerosukkanooa.compressed
+	)
+"
+
+LICENSE="MIT"
+SLOT="$(ver_cut 1)"
+KEYWORDS="~amd64"
+
+DEPEND=">=app-arch/brotli-1.1.0:="
+RDEPEND="${DEPEND}"
+
+ruby_add_bdepend "
+	test? (
+		dev-ruby/bundler
+		dev-ruby/rantly:*
+		>=dev-ruby/test-unit-3.0
+		dev-ruby/test-unit-rr
+	)
+"
+
+all_ruby_prepare() {
+	sed -e 's/__dir__/"."/' \
+		-e 's/git ls-files -z/find * -print0/' \
+		-e 's:require_relative ":require "./:' \
+		-i ${RUBY_FAKEGEM_GEMSPEC} || die
+
+	# Avoid rake-compiler, we handle it via the eclass
+	sed -e '/rake-compiler/ s:^:#:' \
+		-i Gemfile || die
+	sed -e '/require "rake\/extensiontask"/ s:^:#:' \
+		-e '/Rake::ExtensionTask/,/^end/ s:^:#:' \
+		-e '/task build: :compile/ s:^:#:' \
+		-e '/task test: :compile/ s:^:#:' \
+		-i Rakefile || die
+
+	if use test; then
+		mkdir -p vendor/brotli/tests/testdata || die
+		cp {"${DISTDIR}"/brotli-${BROTLI_COMMIT:0:8}-,vendor/brotli/tests/testdata/}alice29.txt || die
+		cp {"${DISTDIR}"/brotli-${BROTLI_COMMIT:0:8}-,vendor/brotli/tests/testdata/}alice29.txt.compressed || die
+		cp {"${DISTDIR}"/brotli-${BROTLI_COMMIT:0:8}-,vendor/brotli/tests/testdata/}zerosukkanooa || die
+		cp {"${DISTDIR}"/brotli-${BROTLI_COMMIT:0:8}-,vendor/brotli/tests/testdata/}zerosukkanooa.compressed || die
+	fi
+}

dev-ruby/brotli/brotli-0.8.0.ebuild
@@ -1,32 +1,0 @@
-# Copyright 1999-2026 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=8
-
-USE_RUBY="ruby32 ruby33 ruby34 ruby40"
-RUBY_FAKEGEM_EXTRADOC="README.md"
-RUBY_FAKEGEM_RECIPE_TEST="rspec3"
-RUBY_FAKEGEM_BINWRAP=""
-RUBY_FAKEGEM_GEMSPEC="brotli.gemspec"
-RUBY_FAKEGEM_EXTENSIONS=(ext/brotli/extconf.rb)
-
-inherit ruby-fakegem
-
-DESCRIPTION="Brotli compressor/decompressor"
-HOMEPAGE="https://github.com/miyucy/brotli"
-SRC_URI="https://github.com/miyucy/brotli/archive/v${PV}.tar.gz -> ${P}.tar.gz"
-
-LICENSE="MIT"
-SLOT="$(ver_cut 1)"
-KEYWORDS="~amd64 ~ppc64"
-# Depends on the test data in app-arch/brotli
-RESTRICT="test"
-
-DEPEND=">=app-arch/brotli-1.1.0:="
-RDEPEND="${DEPEND}"
-
-all_ruby_prepare() {
-	sed -e 's/git ls-files -z -- spec/find spec -print0/' \
-		-e 's/git ls-files -z/find * -print0/' \
-		-i ${RUBY_FAKEGEM_GEMSPEC} || die
-}