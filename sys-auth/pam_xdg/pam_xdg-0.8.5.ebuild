gentoo/gentoo

commit 5867fd4f83ee6cad7f6e35f63d9b260149f1f5d8

sys-auth/pam_xdg: respect *FLAGS, PAM module dir
Bug: https://bugs.gentoo.org/908431
Signed-off-by: Sam James <sam@gentoo.org>

sys-auth/pam_xdg/pam_xdg-0.8.5.ebuild
@@ -1,9 +1,9 @@
-# Copyright 2025 Gentoo Authors
+# Copyright 2025-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
 
-inherit pam
+inherit pam toolchain-funcs
 
 DESCRIPTION="PAM module that manages XDG Base Directories"
 HOMEPAGE="https://www.sdaoden.eu/code.html"
@@ -15,6 +15,17 @@ KEYWORDS="~amd64"
 
 RDEPEND="sys-libs/pam"
 DEPEND="${RDEPEND}"
+
+src_compile() {
+	tc-export CC
+
+	# Do we want to set XDG_CONFIG_DIR for prefix?
+	emake \
+		CFLAGS="${CFLAGS} -DNDEBUG" \
+		LDFLAGS="-shared ${LDFLAGS}" \
+		PREFIX="${EPREFIX}" \
+		LIBDIR="${EPREFIX}$(getpam_mod_dir)"
+}
 
 src_install() {
 	dopammod pam_xdg.so

commit 311bcb1e85d01ef59b3281fbdcbedfb32deeb70c

sys-auth/pam_xdg: add -fPIC
I didn't see this on my main machine because of an unusual configuration
I have with -mno-direct-extern-access.

Bug: https://bugs.gentoo.org/908431
Fixes: 5867fd4f83ee6cad7f6e35f63d9b260149f1f5d8
Signed-off-by: Sam James <sam@gentoo.org>

sys-auth/pam_xdg/pam_xdg-0.8.5.ebuild
@@ -22,7 +22,7 @@ src_compile() {
 	# Do we want to set XDG_CONFIG_DIR for prefix?
 	emake \
 		CFLAGS="${CFLAGS} -DNDEBUG" \
-		LDFLAGS="-shared ${LDFLAGS}" \
+		LDFLAGS="-shared -fPIC ${LDFLAGS}" \
 		PREFIX="${EPREFIX}" \
 		LIBDIR="${EPREFIX}$(getpam_mod_dir)"
 }