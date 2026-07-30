gentoo/gentoo

commit f20e80717e1ac81b98218b63e627a205fa039953

sys-auth/pambase: wire up pam_openrc
This is for user services autostart.

... on USE=-systemd. I didn't include this in 9c8c048c41a650ca49fbfcb1ff30df8aedbc9766
as I was leaning towards a USE=openrc or similar but I think having a separate
USE for it is likely to be harmful/confusing/poor UX.

Note that this is a noop by default anyway (no runlevels defined).

Signed-off-by: Sam James <sam@gentoo.org>

sys-auth/pambase/pambase-999999999.ebuild
@@ -87,7 +87,7 @@ src_configure() {
 		$(usex securetty '--securetty' '') \
 		$(usex selinux '--selinux' '') \
 		$(usex sha512 '--sha512' '') \
-		$(usex systemd '--systemd' '') \
+		$(usex systemd '--systemd' '--openrc') \
 		$(usex yescrypt '--yescrypt' '') \
 		$(usex sssd '--sssd' '') \
 	|| die

sys-auth/pambase/pambase-20250228-r1.ebuild
@@ -87,7 +87,7 @@ src_configure() {
 		$(usex securetty '--securetty' '') \
 		$(usex selinux '--selinux' '') \
 		$(usex sha512 '--sha512' '') \
-		$(usex systemd '--systemd' '') \
+		$(usex systemd '--systemd' '--openrc') \
 		$(usex yescrypt '--yescrypt' '') \
 		$(usex sssd '--sssd' '') \
 	|| die

commit 8041bdbe8943c10aae8d7cabc02b7739720a7075

sys-auth/pambase: depend on sys-apps/openrc[pam] w/ USE=-systemd
This controls pam_openrc. Note that it is specified with '-' in the
PAM config so the dep arguably isn't needed but you definitely do need
USE=pam on openrc if wanting to use that, so..

Again, adding a USE=openrc is awkward because there's no 'OpenRC' profiles
per se.

We could perhaps tie this to minimal if we have to.

Fixes: f20e80717e1ac81b98218b63e627a205fa039953
Signed-off-by: Sam James <sam@gentoo.org>

sys-auth/pambase/pambase-999999999.ebuild
@@ -56,6 +56,7 @@ RDEPEND="
 	sha512? ( >=sys-libs/pam-${MIN_PAM_REQ} )
 	homed? ( sys-apps/systemd[homed] )
 	systemd? ( sys-apps/systemd[pam] )
+	!systemd? ( sys-apps/openrc[pam] )
 	yescrypt? ( sys-libs/libxcrypt[system] )
 	sssd? ( sys-auth/sssd )
 	turnstile? ( sys-apps/turnstile )