df810e25235dbf253141bbc4a726475f8a79397c
Ulrich Müller <ulm@gentoo.org>
2026-07-31 13:56:52 +0200

    eclass/tests/cargo-bench.sh: Source version-funcs.sh
    Needed by cargo.eclass for ver_test.
    
    Signed-off-by: Ulrich Müller <ulm@gentoo.org>

--- eclass/tests/cargo-bench.sh
+++ eclass/tests/cargo-bench.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# Copyright 2023-2024 Gentoo Authors
+# Copyright 2023-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
 source tests-common.sh || exit
+source version-funcs.sh || exit
 
 export LC_ALL=C
 
