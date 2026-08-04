gentoo/gentoo

commit 5d027f1f0b7833f57c9f97125f18728f6d48de06

x11-drivers/nvidia-drivers: workaround to build with clang+OF_GPIO
Was under the impression that this may also affect newly released
versions of older branches given they had a similar change around
gpio but could only reproduce with 610.57.04.

Signed-off-by: Ionen Wolkens <ionen@gentoo.org>

x11-drivers/nvidia-drivers/nvidia-drivers-610.57.04.ebuild
@@ -169,6 +169,13 @@ src_prepare() {
 	# makefile attempts to install wayland library even if not built
 	use wayland || sed -i 's/ WAYLAND_LIB_install$//' \
 		nvidia-settings/src/Makefile || die
+
+	# temporary dirty workaround for clang being stricter than gcc,
+	# reproducing requires a kernel with CONFIG_OF_GPIO enabled
+	# nv-linux.h:1737:51: error: passing 'const struct gpio_device *' [...]
+	CC=${KERNEL_CC} tc-is-clang &&
+		sed -e 's/-Wno-error/& -Wno-error=incompatible-pointer-types-discards-qualifiers/' \
+			-i kernel-module-source/kernel-open/Kbuild || die
 }
 
 src_compile() {

commit eccd7085b2c9340e9eb017d220bd24c66cf8e440

x11-drivers/nvidia-drivers: fix clang condition
Closes: https://bugs.gentoo.org/980357
Fixes: 5d027f1f0b7833f57c9f97125f18728f6d48de06
Signed-off-by: Ionen Wolkens <ionen@gentoo.org>

x11-drivers/nvidia-drivers/nvidia-drivers-610.57.04.ebuild
@@ -173,9 +173,10 @@ src_prepare() {
 	# temporary dirty workaround for clang being stricter than gcc,
 	# reproducing requires a kernel with CONFIG_OF_GPIO enabled
 	# nv-linux.h:1737:51: error: passing 'const struct gpio_device *' [...]
-	CC=${KERNEL_CC} tc-is-clang &&
+	if CC=${KERNEL_CC} tc-is-clang; then
 		sed -e 's/-Wno-error/& -Wno-error=incompatible-pointer-types-discards-qualifiers/' \
 			-i kernel-module-source/kernel-open/Kbuild || die
+	fi
 }
 
 src_compile() {