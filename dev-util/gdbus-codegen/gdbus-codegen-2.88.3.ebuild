gentoo/gentoo

commit bcb0d439667be5c210bf17a676265b880e48f414

dev-util/gdbus-codegen: add 2.88.3
Bug: https://bugs.gentoo.org/980618
Signed-off-by: Lukas Schmelting <lschmelting@posteo.com>
Part-of: https://codeberg.org/gentoo/gentoo/pulls/1644
Signed-off-by: Sam James <sam@gentoo.org>

dev-util/gdbus-codegen/gdbus-codegen-2.88.3.ebuild
@@ -1,0 +1,83 @@
+# Copyright 1999-2026 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+GNOME_ORG_MODULE="glib"
+PYTHON_COMPAT=( python3_{12..15} )
+PYTHON_REQ_USE="xml(+)"
+DISTUTILS_USE_PEP517=setuptools
+DISTUTILS_SINGLE_IMPL=1
+
+inherit gnome.org distutils-r1
+
+DESCRIPTION="GDBus code and documentation generator"
+HOMEPAGE="https://www.gtk.org/"
+SRC_URI+=" https://distfiles.gentoo.org/pub/dev/sam%40gentoo.org/dev-libs/glib/glib-2.88.2-const-whoops.patch.xz"
+
+S="${WORKDIR}/glib-${PV}/gio/gdbus-2.0/codegen"
+
+LICENSE="LGPL-2+"
+SLOT="0"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~loong ~m68k ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86 ~x64-macos"
+
+RDEPEND="
+	${PYTHON_DEPS}
+	>=dev-libs/glib-${PV}
+"
+DEPEND="${RDEPEND}"
+BDEPEND="
+	dev-libs/libxslt
+	app-text/docbook-xsl-stylesheets
+	>=dev-python/docutils-0.21.1
+"
+
+python_prepare_all() {
+	cd "${WORKDIR}"/glib-${PV} || die
+	eapply "${WORKDIR}/glib-2.88.2-const-whoops.patch"
+	cd "${S}" || die
+
+	PATCHES=(
+		"${FILESDIR}/${PN}-2.56.1-sitedir.patch"
+	)
+	distutils-r1_python_prepare_all
+
+	local MAJOR_VERSION=$(ver_cut 1)
+	local MINOR_VERSION=$(ver_cut 2)
+	sed -e 's:@PYTHON@:python:' gdbus-codegen.in > gdbus-codegen || die
+	sed -e "s:@VERSION@:${PV}:" \
+		-e "s:@MAJOR_VERSION@:${MAJOR_VERSION}:" \
+		-e "s:@MINOR_VERSION@:${MINOR_VERSION}:" config.py.in > config.py || die
+	cp "${FILESDIR}/setup.py-2.32.4" setup.py || die "cp failed"
+	sed -e "s/@PV@/${PV}/" -i setup.py || die "sed setup.py failed"
+}
+
+do_xsltproc_command() {
+	# Taken from meson.build for manual manpage building - keep in sync (also copied to dev-util/glib-utils)
+	xsltproc \
+		--nonet \
+		--stringparam man.output.quietly 1 \
+		--stringparam funcsynopsis.style ansi \
+		--stringparam man.th.extra1.suppress 1 \
+		--stringparam man.authors.section.enabled 0 \
+		--stringparam man.copyright.section.enabled 0 \
+		-o "${2}" \
+		http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.xsl \
+		"${1}" || die "manpage generation failed"
+}
+
+src_compile() {
+	distutils-r1_src_compile
+	rst2man \
+		"${WORKDIR}/glib-${PV}/docs/reference/gio/gdbus-codegen.rst" \
+		"${WORKDIR}/glib-${PV}/docs/reference/gio/gdbus-codegen.1"
+}
+
+src_test() {
+	einfo "Skipping tests. This package is tested by dev-libs/glib"
+	einfo "when merged with FEATURES=test"
+}
+
+python_install_all() {
+	distutils-r1_python_install_all # no-op, but prevents QA warning
+	doman "${WORKDIR}/glib-${PV}/docs/reference/gio/gdbus-codegen.1"
+}

dev-util/gdbus-codegen/Manifest
@@ -1,2 +1,3 @@
 DIST glib-2.88.2-const-whoops.patch.xz 19144 BLAKE2B 7b9cd36900acab507702f53e0e70f7b623958810d604b5ac60b7c8a458887c001f871119fcfe9bf8b3266d63d74030aa87305c44ce501bb08fab6e072bc4f774 SHA512 0f8b2b3890250725277addeb1ab42dcb5f53bdb58980d7679564c9b701ec3f68af8fa77b5b04aa9b24370fa77a6121646434aa8d811d2c5ad12966fa0caeed0e
 DIST glib-2.88.2.tar.xz 5782636 BLAKE2B fecce4ad82e62e429d30f4e9050a83b53715644fc8c7c89d767ecdaefa603530e81e024dd3450ce25bcd93b00fa6aeb8551b6240d3a04862dadeb895b0177e10 SHA512 d6f40d1ff0f73faccdff8060fabd29b8217cc46139cf33e5792fee05af0665f96cd04e7866995bd84fcd99c9e40c40367d1326f8417bad99c1242dd841ec72a5
+DIST glib-2.88.3.tar.xz 5794356 BLAKE2B 8c291c0c05e2bf1dc7c5ca7d077f7d87ea7d81eff56de80991001c113718fe314db771f8901a26b21dace763a9d9898cccf6d4c11462372e594b6b39cdf4b95c SHA512 9e2b4985d5e4e06c6897a33cf8c100b9303528ebd72152c10e4cb734fa62fc011c84b816b20f686a3b2aac9a4a5a46f33e7517b4c4c7f3c25c75ac3ddc11f844

commit a9535d5f229f3721020c1196b8ee07cb74026af8

dev-util/gdbus-codegen: drop already-applied patch
Fixes: bcb0d439667be5c210bf17a676265b880e48f414
Signed-off-by: Sam James <sam@gentoo.org>

dev-util/gdbus-codegen/gdbus-codegen-2.88.3.ebuild
@@ -32,10 +32,6 @@ BDEPEND="
 "
 
 python_prepare_all() {
-	cd "${WORKDIR}"/glib-${PV} || die
-	eapply "${WORKDIR}/glib-2.88.2-const-whoops.patch"
-	cd "${S}" || die
-
 	PATCHES=(
 		"${FILESDIR}/${PN}-2.56.1-sitedir.patch"
 	)