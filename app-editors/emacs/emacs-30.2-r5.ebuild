gentoo/gentoo

commit e7b5fa1b727adb038a4ecb8789e619bda05b2996

app-editors/emacs: Disable lisp/proced-tests.el
Bug: https://bugs.gnu.org/76898
Reported-by: Eli Schwartz <eschwartz@gentoo.org>
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

app-editors/emacs/emacs-30.2-r5.ebuild
@@ -469,6 +469,9 @@ src_test() {
 		# Reason: tries to access network
 		# internet-is-working
 		%src/process-tests.el
+
+		# Reason: intermittent hangs, https://bugs.gnu.org/76898
+		%lisp/proced-tests.el
 	)
 	use elibc_musl && exclude_tests+=(
 			# Reason: newlocale(3) lenient locale validation #906012

app-editors/emacs/emacs-31.0.91.ebuild
@@ -469,6 +469,9 @@ src_test() {
 		# Reason: tries to access network
 		# internet-is-working
 		%src/process-tests.el
+
+		# Reason: intermittent hangs, https://bugs.gnu.org/76898
+		%lisp/proced-tests.el
 	)
 	use threads || exclude_tests+=(
 			%src/emacs-module-tests.el

app-editors/emacs/emacs-31.0.9999-r1.ebuild
@@ -469,6 +469,9 @@ src_test() {
 		# Reason: tries to access network
 		# internet-is-working
 		%src/process-tests.el
+
+		# Reason: intermittent hangs, https://bugs.gnu.org/76898
+		%lisp/proced-tests.el
 	)
 	use threads || exclude_tests+=(
 			%src/emacs-module-tests.el

app-editors/emacs/emacs-32.0.9999.ebuild
@@ -469,6 +469,9 @@ src_test() {
 		# Reason: tries to access network
 		# internet-is-working
 		%src/process-tests.el
+
+		# Reason: intermittent hangs, https://bugs.gnu.org/76898
+		%lisp/proced-tests.el
 	)
 	use threads || exclude_tests+=(
 			%src/emacs-module-tests.el

commit 76578fc52d65e0b9f759482e87447323bb2e0a56

app-editors/emacs: Drop duplicate entry for lisp/proced-tests.el
Fixes: e7b5fa1b727adb038a4ecb8789e619bda05b2996
Signed-off-by: Ulrich Müller <ulm@gentoo.org>

app-editors/emacs/emacs-30.2-r5.ebuild
@@ -462,6 +462,10 @@ src_test() {
 
 		%lisp/progmodes/eglot-tests.el  #966957
 
+		# Reason: flaky (https://bugs.gnu.org/73441, fails even with the fix)
+		# proced-refine-test
+		%lisp/proced-tests.el
+
 		# Reason: test not skipped if tree-sitter-cpp is missing #979386
 		# c-ts-mode-test-filling
 		%lisp/progmodes/c-ts-mode-tests.el
@@ -469,9 +473,6 @@ src_test() {
 		# Reason: tries to access network
 		# internet-is-working
 		%src/process-tests.el
-
-		# Reason: intermittent hangs, https://bugs.gnu.org/76898
-		%lisp/proced-tests.el
 	)
 	use elibc_musl && exclude_tests+=(
 			# Reason: newlocale(3) lenient locale validation #906012

app-editors/emacs/emacs-31.0.91.ebuild
@@ -469,9 +469,6 @@ src_test() {
 		# Reason: tries to access network
 		# internet-is-working
 		%src/process-tests.el
-
-		# Reason: intermittent hangs, https://bugs.gnu.org/76898
-		%lisp/proced-tests.el
 	)
 	use threads || exclude_tests+=(
 			%src/emacs-module-tests.el

app-editors/emacs/emacs-31.0.9999-r1.ebuild
@@ -469,9 +469,6 @@ src_test() {
 		# Reason: tries to access network
 		# internet-is-working
 		%src/process-tests.el
-
-		# Reason: intermittent hangs, https://bugs.gnu.org/76898
-		%lisp/proced-tests.el
 	)
 	use threads || exclude_tests+=(
 			%src/emacs-module-tests.el

app-editors/emacs/emacs-32.0.9999.ebuild
@@ -469,9 +469,6 @@ src_test() {
 		# Reason: tries to access network
 		# internet-is-working
 		%src/process-tests.el
-
-		# Reason: intermittent hangs, https://bugs.gnu.org/76898
-		%lisp/proced-tests.el
 	)
 	use threads || exclude_tests+=(
 			%src/emacs-module-tests.el