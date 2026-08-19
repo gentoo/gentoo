gentoo/gentoo

commit 002a59b5a6829163250f1dc964af398a3e48c170

apache-2.eclass: wire up verify-sig
Signed-off-by: Sam James <sam@gentoo.org>

eclass/apache-2.eclass
@@ -13,7 +13,9 @@
 # and inter-module dependency checking.
 
 LUA_COMPAT=( lua5-{1..4} )
+VERIFY_SIG_OPENPGP_KEY_PATH=/usr/share/openpgp-keys/apache-httpd.asc
 inherit autotools fcaps flag-o-matic lua-single multilib ssl-cert toolchain-funcs
+inherit verify-sig
 
 [[ ${CATEGORY}/${PN} != www-servers/apache ]] \
 	&& die "Do not use this eclass with anything else than www-servers/apache ebuilds!"
@@ -69,8 +71,11 @@ esac
 # Defaults to the name of the patchset, with a datestamp.
 [[ -n "${GENTOO_PATCH_A}" ]] || GENTOO_PATCH_A="${GENTOO_PATCHNAME}-${GENTOO_PATCHSTAMP}.tar.bz2"
 
-SRC_URI="mirror://apache/httpd/httpd-${PV}.tar.bz2
-	https://dev.gentoo.org/~${GENTOO_DEVELOPER}/dist/apache/${GENTOO_PATCH_A}"
+SRC_URI="
+	mirror://apache/httpd/httpd-${PV}.tar.bz2
+	https://dev.gentoo.org/~${GENTOO_DEVELOPER}/dist/apache/${GENTOO_PATCH_A}
+	verify-sig? ( mirror://apache/httpd/httpd-${PV}.tar.bz2.asc )
+"
 
 # @VARIABLE: IUSE_MPMS_FORK
 # @DESCRIPTION:
@@ -437,6 +442,17 @@ apache-2_pkg_setup() {
 	fi
 }
 
+# @FUNCTION: apache-2_src_unpack
+# @DESCRIPTION:
+# Standard unpack to support verify-sig.
+apache-2_src_unpack() {
+	if use verify-sig ; then
+		verify-sig_verify_detached "${DISTDIR}"/httpd-${PV}.tar.bz2{,.asc}
+	fi
+
+	default
+}
+
 # @FUNCTION: apache-2_src_prepare
 # @DESCRIPTION:
 # This function applies patches, configures a custom file-system layout and
@@ -692,4 +708,4 @@ apache-2_pkg_postinst() {
 
 }
 
-EXPORT_FUNCTIONS pkg_setup src_prepare src_configure src_install pkg_postinst
+EXPORT_FUNCTIONS pkg_setup src_unpack src_prepare src_configure src_install pkg_postinst

www-servers/apache/Manifest
@@ -1,3 +1,5 @@
 DIST gentoo-apache-2.4.66-20251205.tar.bz2 26982 BLAKE2B 20d0c947c2ad6486af87a588a2851998905f18427da6b5c546819daa043d7d4d5d57b5a0cc0788eb7a7f8b8368079dbc96d47e7771d0b90c8177638f9b0fedf2 SHA512 3e5f25c92531516eca6bd257082931d5df703e1138913513e6d930d04bfbbb621d673899231dad9aac37973484fd11987c6f964b943a44473fb6e60be9a86c62
 DIST httpd-2.4.67.tar.bz2 7493067 BLAKE2B c7f389588b8081080bf81b2f2ffcc585742811a5c7ddc3b63838bb8aad61586e7b8e8854cdb20b56963cc70a952124849b499a7affbf2bad44162f236bb5fbed SHA512 5ae29fc8edb253453271613cb18754de0d943d1f82361059c81f8ebe1f057b894675506550bd1341bfa9416226b569a7bac08f497c53e8bb6dede87f4f41eae4
+DIST httpd-2.4.67.tar.bz2.asc 870 BLAKE2B e632dea646edc1b943b78c4bf670778b101539a0913e7da70ada67337533326c582d7b7bb845896970f97221923333076ec63c81cb1787e2a4895f4e40b3b415 SHA512 1c91218b7811ec8c8f99960bdf607317fda3b7f8aab00115735345128990c5594839a003cf4672dcbb982eaa06beff7a322ba1435ec8145480ec1257c4336422
 DIST httpd-2.4.68.tar.bz2 7738512 BLAKE2B 5c2d4b21915db3ca62ee374e89bb04dcec21a6f2f1334f6306db079de03a974709ef71f3728c56e020c844e331b800306fcf5a865dcd2e02576d41ae6cac6b70 SHA512 134a5bbd3ffe97523ac750490531bcb9441c532cedf0229aa006aa1368e9d25040507c6f519bed6754044f62c371f7c7f9a11b6a311f79b9680d478bf4373eb3
+DIST httpd-2.4.68.tar.bz2.asc 870 BLAKE2B 1544232c7115cebba1f0a6766078a02b44dc2940a3063ccdea5f1fe03a056f9760a3182ad7a0bbb3c5d435a019bf420dbf92b96da8657a0387b3d73383f39ce6 SHA512 87bf940f5041a9550726ca426162aaf5fb1c0433e4ecdb6777cd280751ac3938f40dc5ece44e2ed39642435039b4865fc2bbb5141df15fe4c3a16a748c8061a4

commit c79fe312f3a2fb8147ec8763c150e7a35401d24e

apache-2.eclass: fix BDEPEND for verify-sig
Fixes: 002a59b5a6829163250f1dc964af398a3e48c170
Signed-off-by: Sam James <sam@gentoo.org>

eclass/apache-2.eclass
@@ -179,6 +179,7 @@ DEPEND="${RDEPEND}"
 BDEPEND="
 	virtual/pkgconfig
 	suexec? ( suexec-caps? ( sys-libs/libcap ) )
+	verify-sig? ( sec-keys/openpgp-keys-apache-httpd )
 "
 PDEPEND="~app-admin/apache-tools-${PV}"
 