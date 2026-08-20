gentoo/gentoo

commit 6cc11dab674346112870b1359501901890fe5043

net-libs/libssh: avoid sandbox violation
For tests, we try to find Dropbear which uses dev-libs/libtomcrypt that
in turn tries to open /dev/urandom causing a sandbox violation. Dropbear
isn't used anyway as we don't currently run client/server tests, so stop
trying to find it.

Closes: https://bugs.gentoo.org/964307
Signed-off-by: Sam James <sam@gentoo.org>

net-libs/libssh/libssh-0.11.5.ebuild
@@ -117,6 +117,9 @@ multilib_src_configure() {
 		-DWITH_SERVER=$(usex server)
 		-DWITH_SFTP=$(usex sftp)
 		-DBUILD_STATIC_LIB=$(usex static-libs)
+		# libtomcrypt tries to open /dev/urandom -> sandbox issue, but we don't
+		# do client/server testing yet anyway (bug #964307)
+		-DDROPBEAR_EXECUTABLE=
 		# TODO: try enabling {CLIENT,SERVER}_TESTING
 		-DUNIT_TESTING=$(usex test)
 		-DWITH_ZLIB=$(usex zlib)

net-libs/libssh/libssh-0.12.2.ebuild
@@ -125,6 +125,9 @@ multilib_src_configure() {
 		-DWITH_SFTP=$(usex sftp)
 		-DBUILD_STATIC_LIB=$(usex static-libs)
 
+		# libtomcrypt tries to open /dev/urandom -> sandbox issue, but we don't
+		# do client/server testing yet anyway (bug #964307)
+		-DDROPBEAR_EXECUTABLE=
 		# TODO: Still needs some work, some tests hang and some fail
 		# Would need net-misc/openssh and net-misc/putty too
 		#-DCLIENT_TESTING=$(multilib_native_usex test)

net-libs/libssh/libssh-9999.ebuild
@@ -125,6 +125,9 @@ multilib_src_configure() {
 		-DWITH_SFTP=$(usex sftp)
 		-DBUILD_STATIC_LIB=$(usex static-libs)
 
+		# libtomcrypt tries to open /dev/urandom -> sandbox issue, but we don't
+		# do client/server testing yet anyway (bug #964307)
+		-DDROPBEAR_EXECUTABLE=
 		# TODO: Still needs some work, some tests hang and some fail
 		# Would need net-misc/openssh and net-misc/putty too
 		#-DCLIENT_TESTING=$(multilib_native_usex test)

commit ea0989690c90a40ef1aa128eaedcd7199db5fc07

net-libs/libssh: fix comment wrt /dev/urandom
It's actually dropbear itself: https://github.com/mkj/dropbear/blob/1442f00d3f0d755d9f8ba83c5edcd893aa4d71db/src/dbrandom.c#L309

Bug: https://bugs.gentoo.org/964307
Fixes: 6cc11dab674346112870b1359501901890fe5043
Signed-off-by: Sam James <sam@gentoo.org>

net-libs/libssh/libssh-0.11.5.ebuild
@@ -117,8 +117,8 @@ multilib_src_configure() {
 		-DWITH_SERVER=$(usex server)
 		-DWITH_SFTP=$(usex sftp)
 		-DBUILD_STATIC_LIB=$(usex static-libs)
-		# libtomcrypt tries to open /dev/urandom -> sandbox issue, but we don't
-		# do client/server testing yet anyway (bug #964307)
+		# dropbear's dhclient tries to open /dev/urandom for writing in
+		# seedrandom. And we don't do client/server testing yet anyway (bug #964307)
 		-DDROPBEAR_EXECUTABLE=
 		# TODO: try enabling {CLIENT,SERVER}_TESTING
 		-DUNIT_TESTING=$(usex test)

net-libs/libssh/libssh-0.12.2.ebuild
@@ -125,8 +125,8 @@ multilib_src_configure() {
 		-DWITH_SFTP=$(usex sftp)
 		-DBUILD_STATIC_LIB=$(usex static-libs)
 
-		# libtomcrypt tries to open /dev/urandom -> sandbox issue, but we don't
-		# do client/server testing yet anyway (bug #964307)
+		# dropbear's dhclient tries to open /dev/urandom for writing in
+		# seedrandom. And we don't do client/server testing yet anyway (bug #964307)
 		-DDROPBEAR_EXECUTABLE=
 		# TODO: Still needs some work, some tests hang and some fail
 		# Would need net-misc/openssh and net-misc/putty too

net-libs/libssh/libssh-9999.ebuild
@@ -125,8 +125,8 @@ multilib_src_configure() {
 		-DWITH_SFTP=$(usex sftp)
 		-DBUILD_STATIC_LIB=$(usex static-libs)
 
-		# libtomcrypt tries to open /dev/urandom -> sandbox issue, but we don't
-		# do client/server testing yet anyway (bug #964307)
+		# dropbear's dhclient tries to open /dev/urandom for writing in
+		# seedrandom. And we don't do client/server testing yet anyway (bug #964307)
 		-DDROPBEAR_EXECUTABLE=
 		# TODO: Still needs some work, some tests hang and some fail
 		# Would need net-misc/openssh and net-misc/putty too