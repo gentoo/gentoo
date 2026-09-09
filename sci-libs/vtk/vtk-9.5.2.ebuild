gentoo/gentoo

commit ea049291cc43355d4edda3e970c1515f58578f33

sci-libs/vtk: drop broken bundled HDF5 module
The upstream CMake version works fine w/ HDF5 2.2.0.

Closes: https://bugs.gentoo.org/970413
Signed-off-by: Sam James <sam@gentoo.org>

sci-libs/vtk/vtk-9.5.2.ebuild
@@ -13,6 +13,9 @@ PYTHON_REQ_USE="tk?"
 WEBAPP_OPTIONAL=yes
 WEBAPP_MANUAL_SLOT=yes
 
+# bug #982155
+CMAKE_REMOVE_MODULES_LIST=( vtkModule )
+
 inherit check-reqs cmake cuda java-pkg-opt-2 multiprocessing python-single-r1 toolchain-funcs virtualx webapp
 
 # Short package version

commit 5f12a922f9160880488eccde1102f7a7f114bb22

sci-libs/vtk: fix HDF hack
I did try a few options like deleting FindHDF5 instead, which failed
because VTK wants to find the 'patch' FindHDF5.cmake variant. I also tried
copying the dev-build/cmake version in but that didn't work either, so
hack in the old option instead.

Thanks to tdr for testing it and Geoff for proposing it.

Bug: https://bugs.gentoo.org/970413
Closes: https://bugs.gentoo.org/982169
Fixes: ea049291cc43355d4edda3e970c1515f58578f33
Signed-off-by: Sam James <sam@gentoo.org>

sci-libs/vtk/vtk-9.5.2.ebuild
@@ -13,9 +13,6 @@ PYTHON_REQ_USE="tk?"
 WEBAPP_OPTIONAL=yes
 WEBAPP_MANUAL_SLOT=yes
 
-# bug #982155
-CMAKE_REMOVE_MODULES_LIST=( vtkModule )
-
 inherit check-reqs cmake cuda java-pkg-opt-2 multiprocessing python-single-r1 toolchain-funcs virtualx webapp
 
 # Short package version
@@ -473,6 +470,8 @@ src_configure() {
 		# -DVTK_MODULE_ENABLE_VTK_glad
 		# -DVTK_MODULE_ENABLE_VTK_h5part
 		-DVTK_MODULE_ENABLE_VTK_hdf5="YES"
+		# bug #982169
+		-DHDF5_IS_PARALLEL="$(usex mpi "YES" "NO")"
 		# -DVTK_MODULE_ENABLE_VTK_ioss
 		-DVTK_MODULE_ENABLE_VTK_jpeg="YES"
 		-DVTK_MODULE_ENABLE_VTK_jsoncpp="YES"