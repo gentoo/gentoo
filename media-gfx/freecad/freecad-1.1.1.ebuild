gentoo/gentoo

commit f57b1d5e40f86959e134203197d0233fe4f15aae

media-gfx/freecad: added 1.1.1 / updated 9999
Signed-off-by: Norbert Bolanowski <bolanowski.n@gmail.com>
Part-of: https://codeberg.org/gentoo/gentoo/pulls/765
Merges: https://codeberg.org/gentoo/gentoo/pulls/765
Signed-off-by: Nowa Ammerlaan <nowa@gentoo.org>

media-gfx/freecad/freecad-1.1.1.ebuild
@@ -75,13 +75,12 @@ RDEPEND="
 	sci-libs/opencascade:=[json]
 	virtual/zlib:=
 	$(python_gen_cond_dep '
-		dev-python/lark[${PYTHON_USEDEP}]
 		dev-python/numpy[${PYTHON_USEDEP}]
 		dev-python/pybind11[${PYTHON_USEDEP}]
 		dev-python/pycxx[${PYTHON_USEDEP}]
 		dev-python/pyyaml[${PYTHON_USEDEP}]
 	')
-	assembly? ( >=sci-libs/ondselsolver-1.0.1_p20260211 )
+	assembly? ( sci-libs/ondselsolver )
 	fem? (
 		sci-libs/vtk:=
 		$(python_gen_cond_dep 'dev-python/ply[${PYTHON_USEDEP}]')
@@ -139,7 +138,8 @@ BDEPEND="
 PATCHES=(
 	"${FILESDIR}"/${PN}-9999-tests-src-Qt-only-build-test-for-BUILD_GUI-ON.patch
 	"${FILESDIR}"/${PN}-9999-Gentoo-specific-don-t-check-vcs.patch
-	"${FILESDIR}/${PN}-1.0.2-pybind11-latent-slots-macro-conflicts-with-Qt.patch" # fixed in pybind-3.0.1
+	"${FILESDIR}"/${PN}-1.1.1-fix-sketcher-toolbars.patch
+	"${FILESDIR}"/${PN}-1.0.2-pybind11-latent-slots-macro-conflicts-with-Qt.patch # fixed in pybind-3.0.1
 )
 
 DOCS=( CODE_OF_CONDUCT.md README.md )
@@ -551,7 +551,6 @@ src_install() {
 	done
 
 	rm -r "${ED}/usr/$(get_libdir)/${PN}/include/E57Format" || die "failed to drop unneeded include directory E57Format"
-	rm -r "${ED}/usr/$(get_libdir)/${PN}/include/clipper2" || die "failed to drop unneeded include directory clipper2"
 	rmdir "${ED}/usr/$(get_libdir)/${PN}/include/" || die "failed to drop unneeded include directory"
 
 	python_optimize "${ED}/usr/share/${PN}/data/Mod/Start/" "${ED}/usr/$(get_libdir)/${PN}/"{Ext,Mod}/

media-gfx/freecad/Manifest
@@ -1,3 +1,5 @@
+DIST AddonManager.tar.gz 849951 BLAKE2B 322f84799253c25530418efb44dee50601ca13b9a036d7741ceeccf914ff40d968c5627d7fb23b5543b4f1c27aeda085e8c33dde66dad1d6b30c8200e8c2e7ca SHA512 44685be9893de85759b9296f5ee8bf0da518a064232f41f42b30db6d5ea83646c2ca4adb21490644966f182fd33f2ec9fe9ad73ba9e8020487b9044c36f61358
 DIST freecad-1.0.2.tar.gz 91635756 BLAKE2B d71e356ad64ff1dd9a6433752dd1ae71e981a7711494a5e8e9fc79e467da70c1cd2a9d2fa8a8be61785cc6132f5e3c8d34a26ac6102ee9ac7a3baacb90df792e SHA512 927cf3a4784c99f7e3e9ce45b8ef7a035f4772cc360b4cbb4b537806cd4218b193938129a4c1d1ba7f67a6ac351d718bddb65879cafbcbcd27c14cf7af322c20
+DIST freecad-1.1.1.tar.gz 91720496 BLAKE2B 598326c9d1aee79864bbabb6b521dff44b9f8ee4767d9a2d254f6aeb6f350455c88c1d024978279217e53b283dc57a04ce496f63216b07556567b887cfb78306 SHA512 47e9a251f228112f7037a1e36f9a372ab5f9925fc3117983cf32d91006572eed430316d42539dc8c37c36f7179f8ffe2a66e392f1391e58adf6ccdec82d0c344
 DIST freecad-20710.patch 902 BLAKE2B ce69e5714c06e969bd533c54be9f7de0c23fc881a5259979c444aecc1f5cbb175b01c7a61bf3ebc35e0eeb7f9aabfba729499e449cbf2a06055df6aecc8465cd SHA512 1df1b0941d1ccbf9aef0f882cd17e647448acb9267a2fae503b4427b80620374e5d4e1a1bb651fab8b631db3ff4d53748951d75e244a9ffddda817b6d4971a84
 DIST freecad-21433.patch 1151 BLAKE2B 88945020844ba6df0b5ae70a6e889191886165e21711c021c56a8902a881aeebca1974d12a92e0f3c45ba354221acc2c65ffd84b0cb2055aa307f693a0b8f235 SHA512 2b28cabb53fa0d2bc8ee2fab9b5672c225baff20705731c89491f0b2c89877e31c3b622396ecdcf806208b6c7bacb231ad635681183ece14f4102def96381493

media-gfx/freecad/files/freecad-1.1.1-fix-sketcher-toolbars.patch
@@ -1,0 +1,41 @@
+diff --git a/src/Gui/ToolBarManager.cpp b/src/Gui/ToolBarManager.cpp
+index 61232d64bf8e..3a73a0b398bc 100644
+--- a/src/Gui/ToolBarManager.cpp
++++ b/src/Gui/ToolBarManager.cpp
+@@ -841,21 +841,8 @@ void ToolBarManager::onTimer()
+ void ToolBarManager::saveState() const
+ {
+     auto ignoreSave = [](QAction* action) {
+-        // If the toggle action is invisible then it's controlled by the application.
+-        // In this case the current state is not saved.
+-        if (!action->isVisible()) {
+-            return true;
+-        }
+-
+-        QVariant property = action->property("DefaultVisibility");
+-        if (property.isNull()) {
+-            return false;
+-        }
+-
+-        // If DefaultVisibility is Unavailable then never save the state because it's
+-        // always controlled by the client code.
+-        auto value = static_cast<ToolBarItem::DefaultVisibility>(property.toInt());
+-        return value == ToolBarItem::DefaultVisibility::Unavailable;
++        // Only save state for toolbars whose toggle action is user-visible.
++        return !action->isVisible();
+     };
+ 
+     QList<ToolBar*> toolbars = toolBars();
+diff --git a/src/Mod/Sketcher/Gui/ViewProviderSketch.cpp b/src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
+index a5c80f39f7..1fa3a9ba8a 100644
+--- a/src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
++++ b/src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
+@@ -3915,8 +3915,6 @@ void ViewProviderSketch::unsetEdit(int ModNum)
+     auto gridnode = getGridNode();
+     pcRoot->removeChild(gridnode);
+ 
+-    Workbench::leaveEditMode();
+-
+     if (listener) {
+         Gui::getMainWindow()->removeEventFilter(listener.get());
+         listener.reset();

sci-libs/ondselsolver/ondselsolver-1.0.1_p20260211.ebuild
@@ -14,7 +14,7 @@ S="${WORKDIR}/OndselSolver-${COMMIT}"
 
 LICENSE="LGPL-2.1"
 SLOT="0"
-KEYWORDS="amd64"
+KEYWORDS="~amd64"
 
 IUSE="test"
 RESTRICT="!test? ( test )"

commit 5b81897ba9346f7adcd189e6add89c7219f60d43

media-gfx/freecad: 1.1.1 snapshot -9999 patches
Fixes: f57b1d5e40f86959e134203197d0233fe4f15aae
Signed-off-by: Paul Zander <negril.nx+gentoo@gmail.com>
Part-of: https://codeberg.org/gentoo/gentoo/pulls/1856
Signed-off-by: Sam James <sam@gentoo.org>

media-gfx/freecad/files/freecad-1.1.1-Gentoo-specific-don-t-check-vcs.patch
@@ -1,0 +1,14 @@
+--- a/src/Tools/SubWCRev.py
++++ b/src/Tools/SubWCRev.py
+@@ -511,11 +511,6 @@ def main():
+ 
+     vcs = [
+         GitControl(),
+-        DebianGitHub(),
+-        BazaarControl(),
+-        Subversion(),
+-        MercurialControl(),
+-        DebianChangelog(),
+         UnknownControl(),
+     ]
+     for i in vcs:

media-gfx/freecad/files/freecad-1.1.1-tests-src-Qt-only-build-test-for-BUILD_GUI-ON.patch
@@ -1,0 +1,38 @@
+From 9e10e873affcadbad27b0b71a7a4bb9bb7644ba4 Mon Sep 17 00:00:00 2001
+From: Paul Zander <negril.nx@gmail.com>
+Date: Mon, 14 Jul 2025 15:18:26 +0200
+Subject: [PATCH] tests src Qt only build test for BUILD_GUI=ON
+
+Signed-off-by: Paul Zander <negril.nx+gentoo@gmail.com>
+
+diff --git a/tests/src/Base/CMakeLists.txt b/tests/src/Base/CMakeLists.txt
+index 0bdc34ba5c..850772b5b3 100644
+--- a/tests/src/Base/CMakeLists.txt
++++ b/tests/src/Base/CMakeLists.txt
+@@ -30,7 +30,9 @@ add_executable(Base_tests_run
+         Writer.cpp
+ )
+ 
++if(BUILD_GUI)
+ setup_qt_test(InventorBuilder)
++endif()
+ 
+ target_link_libraries(Base_tests_run PRIVATE
+     GTest::gtest_main
+diff --git a/tests/src/Gui/CMakeLists.txt b/tests/src/Gui/CMakeLists.txt
+index bf6b87d0fd..0b186fbf5c 100644
+--- a/tests/src/Gui/CMakeLists.txt
++++ b/tests/src/Gui/CMakeLists.txt
+@@ -8,7 +8,9 @@ add_executable(Gui_tests_run
+ )
+ 
+ # Qt tests
++if(BUILD_GUI)
+ setup_qt_test(QuantitySpinBox)
++endif()
+ 
+ 
+ target_link_libraries(Gui_tests_run PRIVATE
+-- 
+2.50.1
+

media-gfx/freecad/freecad-1.1.1.ebuild
@@ -136,8 +136,8 @@ BDEPEND="
 "
 
 PATCHES=(
-	"${FILESDIR}"/${PN}-9999-tests-src-Qt-only-build-test-for-BUILD_GUI-ON.patch
-	"${FILESDIR}"/${PN}-9999-Gentoo-specific-don-t-check-vcs.patch
+	"${FILESDIR}"/${PN}-1.1.1-tests-src-Qt-only-build-test-for-BUILD_GUI-ON.patch
+	"${FILESDIR}"/${PN}-1.1.1-Gentoo-specific-don-t-check-vcs.patch
 	"${FILESDIR}"/${PN}-1.1.1-fix-sketcher-toolbars.patch
 	"${FILESDIR}"/${PN}-1.1.1-qt-6.11.patch
 	"${FILESDIR}"/${PN}-1.0.2-pybind11-latent-slots-macro-conflicts-with-Qt.patch # fixed in pybind-3.0.1