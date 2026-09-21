gentoo/gentoo

commit 51daf12255ddef7fb1eebb6c379c3bbb7e81bc0c

dev-db/mariadb-connector-c: add blockers for (short-lived) broken mariadb
Portage needs permission to move the file ownership around if someone was
unlucky wrt install order.

Bug: https://bugs.gentoo.org/982644
Closes: https://bugs.gentoo.org/983009
Signed-off-by: Sam James <sam@gentoo.org>

dev-db/mariadb-connector-c/mariadb-connector-c-9999.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -9,7 +9,7 @@ if [[ ${PV} == 9999 ]] ; then
 else
 	MY_PN=${PN#mariadb-}
 	MY_PV=${PV/_b/-b}
-	SRC_URI="https://downloads.mariadb.com/Connectors/c/connector-c-${PV}/${P}-src.tar.gz"
+	SRC_URI="https://archive.mariadb.org/connector-c-${PV}/${P}-src.tar.gz"
 	S="${WORKDIR%/}/${PN}-${MY_PV}-src"
 	KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~loong ~mips ~ppc ~ppc64 ~riscv ~s390 ~x86"
 fi
@@ -39,7 +39,11 @@ DEPEND="
 	)
 "
 BDEPEND="test? ( dev-db/mariadb[server] )"
-RDEPEND="${DEPEND}"
+RDEPEND="${DEPEND}
+	!<dev-db/mariadb-11.4.7-r1
+	!=dev-db/mariadb-11.8.9-r1
+	!=dev-db/mariadb-12.3.3-r1
+"
 
 MULTILIB_CHOST_TOOLS=( /usr/bin/mariadb_config )
 MULTILIB_WRAPPED_HEADERS+=( /usr/include/mariadb/mariadb_version.h )

dev-db/mariadb-connector-c/mariadb-connector-c-3.4.9-r1.ebuild
@@ -41,6 +41,8 @@ DEPEND="
 BDEPEND="test? ( dev-db/mariadb[server] )"
 RDEPEND="${DEPEND}
 	!<dev-db/mariadb-11.4.7-r1
+	!=dev-db/mariadb-11.8.9-r1
+	!=dev-db/mariadb-12.3.3-r1
 "
 
 MULTILIB_CHOST_TOOLS=( /usr/bin/mariadb_config )

commit 905f90a3a4e093e94967ec8c4da85e6fbe569c36

dev-db/mariadb-connector-c: fix blockers
Block the broken versions which collide, not the fixed ones..

Closes: https://bugs.gentoo.org/982644
Fixes: 51daf12255ddef7fb1eebb6c379c3bbb7e81bc0c
Signed-off-by: Sam James <sam@gentoo.org>

dev-db/mariadb-connector-c/mariadb-connector-c-3.4.9-r2.ebuild
@@ -40,9 +40,9 @@ DEPEND="
 "
 BDEPEND="test? ( dev-db/mariadb[server] )"
 RDEPEND="${DEPEND}
-	!<dev-db/mariadb-11.4.7-r1
-	!=dev-db/mariadb-11.8.9-r1
-	!=dev-db/mariadb-12.3.3-r1
+	!<dev-db/mariadb-11.4.7-r0
+	!=dev-db/mariadb-11.8.9-r0
+	!=dev-db/mariadb-12.3.3-r0
 "
 
 MULTILIB_CHOST_TOOLS=( /usr/bin/mariadb_config )

dev-db/mariadb-connector-c/mariadb-connector-c-9999.ebuild
@@ -40,9 +40,9 @@ DEPEND="
 "
 BDEPEND="test? ( dev-db/mariadb[server] )"
 RDEPEND="${DEPEND}
-	!<dev-db/mariadb-11.4.7-r1
-	!=dev-db/mariadb-11.8.9-r1
-	!=dev-db/mariadb-12.3.3-r1
+	!<dev-db/mariadb-11.4.7-r0
+	!=dev-db/mariadb-11.8.9-r0
+	!=dev-db/mariadb-12.3.3-r0
 "
 
 MULTILIB_CHOST_TOOLS=( /usr/bin/mariadb_config )