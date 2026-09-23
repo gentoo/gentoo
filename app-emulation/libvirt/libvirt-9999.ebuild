gentoo/gentoo

commit d9dd362d40b5abc54aa869c73e78d9cc0bec8827

app-emulation/libvirt: require older/newer sec-keys dep
Cap older as the file location changed.

Signed-off-by: Sam James <sam@gentoo.org>

app-emulation/libvirt/libvirt-9999.ebuild
@@ -54,7 +54,7 @@ BDEPEND="
 	dev-python/docutils
 	virtual/pkgconfig
 	bash-completion? ( >=app-shells/bash-completion-2.0 )
-	verify-sig? ( sec-keys/openpgp-keys-libvirt )"
+	verify-sig? ( >=sec-keys/openpgp-keys-libvirt-20260831 )"
 
 # gettext.sh command is used by the libvirt command wrappers, and it's
 # non-optional, so put it into RDEPEND.

app-emulation/libvirt/libvirt-11.10.0.ebuild
@@ -53,7 +53,7 @@ BDEPEND="
 	dev-python/docutils
 	virtual/pkgconfig
 	bash-completion? ( >=app-shells/bash-completion-2.0 )
-	verify-sig? ( sec-keys/openpgp-keys-libvirt )"
+	verify-sig? ( <sec-keys/openpgp-keys-libvirt-20260831 )"
 
 # gettext.sh command is used by the libvirt command wrappers, and it's
 # non-optional, so put it into RDEPEND.

app-emulation/libvirt/libvirt-12.0.0.ebuild
@@ -54,7 +54,7 @@ BDEPEND="
 	dev-python/docutils
 	virtual/pkgconfig
 	bash-completion? ( >=app-shells/bash-completion-2.0 )
-	verify-sig? ( sec-keys/openpgp-keys-libvirt )"
+	verify-sig? ( <sec-keys/openpgp-keys-libvirt-20260831 )"
 
 # gettext.sh command is used by the libvirt command wrappers, and it's
 # non-optional, so put it into RDEPEND.

app-emulation/libvirt/libvirt-12.1.0-r1.ebuild
@@ -54,7 +54,7 @@ BDEPEND="
 	dev-python/docutils
 	virtual/pkgconfig
 	bash-completion? ( >=app-shells/bash-completion-2.0 )
-	verify-sig? ( sec-keys/openpgp-keys-libvirt )"
+	verify-sig? ( <sec-keys/openpgp-keys-libvirt-20260831 )"
 
 # gettext.sh command is used by the libvirt command wrappers, and it's
 # non-optional, so put it into RDEPEND.

app-emulation/libvirt/libvirt-12.2.0.ebuild
@@ -54,7 +54,7 @@ BDEPEND="
 	dev-python/docutils
 	virtual/pkgconfig
 	bash-completion? ( >=app-shells/bash-completion-2.0 )
-	verify-sig? ( sec-keys/openpgp-keys-libvirt )"
+	verify-sig? ( <sec-keys/openpgp-keys-libvirt-20260831 )"
 
 # gettext.sh command is used by the libvirt command wrappers, and it's
 # non-optional, so put it into RDEPEND.

app-emulation/libvirt/libvirt-12.6.0.ebuild
@@ -11,7 +11,7 @@ EAPI=8
 # Please bump them together!
 
 PYTHON_COMPAT=( python3_{11..14} )
-VERIFY_SIG_OPENPGP_KEY_PATH=/usr/share/openpgp-keys/libvirt.org.asc
+VERIFY_SIG_OPENPGP_KEY_PATH=/usr/share/openpgp-keys/libvirt.asc
 inherit meson flag-o-matic linux-info python-any-r1 readme.gentoo-r1
 inherit tmpfiles verify-sig
 
@@ -54,7 +54,7 @@ BDEPEND="
 	dev-python/docutils
 	virtual/pkgconfig
 	bash-completion? ( >=app-shells/bash-completion-2.0 )
-	verify-sig? ( sec-keys/openpgp-keys-libvirt )"
+	verify-sig? ( >=sec-keys/openpgp-keys-libvirt-20260831 )"
 
 # gettext.sh command is used by the libvirt command wrappers, and it's
 # non-optional, so put it into RDEPEND.

commit 5614a1359d8339782424d46365c3dd789a194666

app-emulation/libvirt: Update live ebuild
The location of openpgp signature file has changed and regular
ebuilds were udpated accordingly except the live one.
Then, the minimal required app-emulation/qemu version is now 7.2
(per upstream commit of [1]).
And lastly, the fix-paths-for-apparmor.patch was rebased, but the
live ebuild still applies the old one.

1: https://gitlab.com/libvirt/libvirt/-/commit/d19d088a3f9722977a2a144de27b4ff74c32ba1a
Fixes: d9dd362d40b5abc54aa869c73e78d9cc0bec8827
Fixes: 924245c8167878ef6404490e8eeeada4eb6ea2c2
Signed-off-by: Michal Privoznik <michal.privoznik@gmail.com>
Part-of: https://codeberg.org/gentoo/gentoo/pulls/1909
Merges: https://codeberg.org/gentoo/gentoo/pulls/1909
Signed-off-by: Sam James <sam@gentoo.org>

app-emulation/libvirt/libvirt-9999.ebuild
@@ -11,7 +11,7 @@ EAPI=8
 # Please bump them together!
 
 PYTHON_COMPAT=( python3_{11..14} )
-VERIFY_SIG_OPENPGP_KEY_PATH=/usr/share/openpgp-keys/libvirt.org.asc
+VERIFY_SIG_OPENPGP_KEY_PATH=/usr/share/openpgp-keys/libvirt.asc
 inherit meson flag-o-matic linux-info python-any-r1 readme.gentoo-r1
 inherit tmpfiles verify-sig
 
@@ -109,7 +109,7 @@ RDEPEND="
 		>=sys-auth/polkit-0.9
 	)
 	qemu? (
-		>=app-emulation/qemu-4.2
+		>=app-emulation/qemu-7.2
 		app-crypt/swtpm
 		dev-libs/json-c:=
 	)
@@ -157,7 +157,7 @@ PDEPEND="
 
 PATCHES=(
 	"${FILESDIR}"/${PN}-11.0.0-Fix-paths-in-libvirt-guests.sh.in.patch
-	"${FILESDIR}"/${PN}-11.3.0-fix-paths-for-apparmor.patch
+	"${FILESDIR}"/${PN}-12.6.0-fix-paths-for-apparmor.patch
 	"${FILESDIR}"/${PN}-12.2.0-do-not-use-sysconfig.patch
 )
 